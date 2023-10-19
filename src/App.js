20f6910e99221424c73d02a4a5bad145df04d517
김영은 <jessicak0828@gmail.com>
2023-10-19 21:07:18 +0900

    예외 상황들 코드로 작성

--- src/App.js
+++ src/App.js
@@ -3,17 +3,38 @@
 //같은 수가 다른 자리에 있으면 볼
 //같은 수가 전혀 없으면 낫싱
 
+
+
+
 class App {
-  async play(num) {
-    //예외 상황 1 - 입력값이 없을 때
-    num
-    //예외 상황 2 - 입력값이 숫자가 아닌 경우
-    typeof num !== 'number'
-    //예외 상황 3 - 세 자리 숫자가 아닌 경우
-    const numArr = String(num).split('').map((num)=> +num)
-    numArr.length !== 3 
-    // 예외 상황 4 - 숫자가 중복될 때
+  async play() {
+    function startGame(){
+      alert('숫자 야구 게임을 시작합니다')
+      alert('숫자를 입력해주세요 : ')
+    }
+    function askInput(input){
+      console.log(`숫자를 입력해주세요 : ${input}`)
+    }
+    function validateInput(input){
+      //예외 상황 1 - 입력값이 없을 때
+      !num
+      //예외 상황 2 - 입력값이 숫자가 아닌 경우
+      typeof num !== 'number'
+      //예외 상황 3 - 세 자리 숫자가 아닌 경우
+        const numArr = String(num).split('').map((num)=> +num)
+      numArr.length !== 3 
+      // 예외 상황 4 - 숫자가 중복될 때(고유하지 않을 때)
+      !isExceptional(numArr)
+        const isExceptional = (arr) => {
+         if(arr[0] !== arr[1] && arr[1] !== arr[2] && arr[2] !== arr[0]){
+          return true
+         } else {return false}
+      }
+
+    }
+   
     
+
 
   }
 }
