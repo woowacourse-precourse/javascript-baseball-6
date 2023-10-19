d3366218cff1d9a030bcce43f153b197218af56a
Jin Kwan Kim <kkole3897@gmail.com>
2023-10-19 16:30:23 +0900

    feat: 야구 게임 시작 문구 출력 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils"
+
+const INTRO_SENTENCE = "숫자 야구 게임을 시작합니다.\n";
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print(INTRO_SENTENCE);
+  }
 }
 
 export default App;
