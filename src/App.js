b8da22fffc572f00c15051919499a80acfd43921
jypark38 <jury124@naver.com>
2023-10-19 23:57:27 +0900

    refactor : 주석 및 코드 정리

--- src/App.js
+++ src/App.js
@@ -13,19 +13,18 @@ class App {
     while(true){
       if(this.start) this.initialization();
       const USER_INPUT = await this.userInput();
-      if(USER_INPUT === null) continue;
       const [STRIKE,BALL] = this.checkInput(USER_INPUT);
       const SCORE = this.returnScore(STRIKE,BALL)
       Console.print(SCORE);
       // 스트라이크가 3이 아니면 continue
       if(STRIKE!==3) continue;
-      // 3이면 아래 진행
       Console.print(LOGS.GAME_END);
       Console.print(LOGS.RESTART_PROMPT)
+      // 재시작 여부 묻기
       const IS_RESTART = await this.checkRestart();
       // restart 하지 않겠다고 하면 while문 종료
       if(!IS_RESTART) break;
-      // this.start true로 초기화 하여 initialization 재실행
+      // 재시작 : this.start true로 초기화 하여 initialization 재실행
       this.start = true
     }
   }
@@ -43,27 +42,27 @@ class App {
 
   async userInput(){
     const input = await Console.readLineAsync(LOGS.INPUT_PROMPT)
+    // 서로 다른 3자리의 숫자 정규식
+    const REGEX = /^(?!.*(.).*\1)^[1-9]{3}$/;
     // 유효성 테스트 통과하면 배열 생성
-    if(this.validInput(input)) return input.split('').map(item=>parseInt(item))
-    // 못하면 에러 발생
+    if(REGEX.test(input)) return input.split('').map(item=>parseInt(item))
+    // 아니면 에러 발생
     throw new Error(LOGS.INVALID_INPUT);
   }
 
-  validInput(input){
-    // 아래 check들 정규표현식 처리
-    // 서로 다른 3자리의 숫자
-    const REGEX = /^(?!.*(.).*\1)^[1-9]{3}$/;
-    return REGEX.test(input);
-  }
 
+  // 재시작 여부 묻기
   async checkRestart() {
     const INPUT = await Console.readLineAsync(LOGS.INPUT_PROMPT2)
-    // input이 1 혹은 2인지 검사
-    if(/^[12]$/.test(INPUT)) return INPUT==='1'
+    // input이 1 혹은 2 정규식
+    const REGEX = /^[12]$/
+    // 유효성 테스트 통과하면 1인지 boolean 반환
+    if(REGEX.test(INPUT)) return INPUT==='1'
     // 아니면 에러 발생
     throw new Error(LOGS.INVALID_INPUT);
   }
   
+  // 입력값 처리
   checkInput(input){
     const STRIKE = this.checkStrike(input);
     // 볼 = 같은 수 - 스트라이크
@@ -81,6 +80,7 @@ class App {
     return input.filter((item,idx) => item === this.computer[idx]).length;
   }
 
+  // 채점후 메세지 반환
   returnScore(strike,ball){
     const messages = [];
     if (ball) messages.push(`${ball}볼`);
