623fecbcc084dc164ab6519f1a6f9895faa6a61e
Jules <dali1999@naver.com>
2023-10-20 04:40:35 +0900

    feat: 게임 종료 후 input에 따라 재실행 여부 결정되는 기능 구현

--- src/game/gamePlay.js
+++ src/game/gamePlay.js
@@ -1,6 +1,16 @@
 import { Console } from "@woowacourse/mission-utils";
 import handleUserInput from "../handleAnswer/handleUserInput.js";
 
+function reset() {
+    Console.readLine("게임이 끝났습니다. 다시 플레이 하시겠습니까?", (replay) => {
+      if (replay === 1) {
+        computerAnswer = makeAnswer();
+        init();
+      } else {
+        throw new Error("Game over");
+      }
+    });
+  }
 
 function typeAnswer(computerAnswer) {
   Console.readLine("숫자를 입력해주세요 :", (userInput) => {
