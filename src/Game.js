014b1a0a9fd93d1ad31cd51acdfd0cf8263cda76
Rki0 <pky00823@gmail.com>
2023-10-19 18:38:11 +0900

    refactor: 같은 목적의 코드 모듈화
    같은 목적을 가지고 있는 코드를 하나의 메서드로 만들어 가독성을 높였다.
    가령, 게임 시작 메세지, 컴퓨터 랜덤 숫자 생성, 플레이어 숫자 입력은
    게임 시작과 동시에 진행해야하므로 하나의 목적을 가진다고 볼 수 있다.

--- src/Game.js
+++ src/Game.js
@@ -6,24 +6,33 @@ export default class Game {
   constructor() {
     this.answer = null;
     this.player = null;
+    this.ball = 0;
+    this.strike = 0;
   }
 
   async start() {
+    await this.setGame();
+
+    this.compareTwoNumber();
+
+    Print.showHint(this.ball, this.strike);
+  }
+
+  async setGame() {
     Print.startMessage();
     this.createComputerNumber();
-
     this.player = await Print.getPlayerNumber();
-
-    const calculate = new Calculate();
-    calculate.compareAnsAndPlayer(this.answer, this.player);
-    const [ball, strike] = calculate.getResult();
-
-    Print.showHint(ball, strike);
   }
 
   createComputerNumber() {
     const computer = new Computer();
     computer.makeThreeRandomNumber();
     this.answer = computer.getComputerNumber();
+  }
+
+  compareTwoNumber() {
+    const calculate = new Calculate();
+    calculate.compareAnsAndPlayer(this.answer, this.player);
+    [this.ball, this.strike] = calculate.getResult();
   }
 }
