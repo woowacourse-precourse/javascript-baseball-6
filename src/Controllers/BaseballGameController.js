9f6dfb7a2ea7709adae3320ac77f518bf66fe5c5
thewronghand <penfreak77@gmail.com>
2023-10-20 01:58:57 +0900

    refactor: 게임 루프를 재귀 구조에서 반복 구조로 변경
    재귀 구조에서 에러 발생을 테스트 케이스가 캡쳐하지 못하므로, 재귀 대신 게임 상태에 따라 다른 동작 반복하도록 게임 구조를 수정

--- src/Controllers/BaseballGameController.js
+++ src/Controllers/BaseballGameController.js
@@ -1,60 +1,67 @@
 import BaseballGame from "../Models/BaseballGame.js";
 import InputView from "../Views/InputView.js";
 import OutputView from "../Views/OutputView.js";
-import { GAME_CONSTANTS, USER_COMMANDS } from "../utils/constants.js";
+import {
+  GAME_STATES,
+  GAME_CONSTANTS,
+  USER_COMMANDS,
+} from "../utils/constants.js";
 import { validateUtils } from "../utils/validateUtils.js";
+
+const { PLAYING, COMMAND, QUIT } = GAME_STATES;
 
 export default class BaseballGameController {
   #inputView;
   #outputView;
+  #gameState;
 
-  async play() {
+  constructor() {
     this.#inputView = new InputView();
     this.#outputView = new OutputView();
+    this.#gameState = PLAYING;
+  }
+
+  async play() {
     const baseballGame = new BaseballGame();
     baseballGame.setNewAnswer();
-    this.readNumbers(baseballGame);
-  }
 
-  async readNumbers(game) {
-    const input = await this.#inputView.readUserInputNumbers();
-    await this.processNumbers(input, game);
-  }
-
-  async readCommand(game) {
-    const input = await this.#inputView.readUserInputCommand();
-    await this.processCommand(input, game);
-  }
-
-  async processNumbers(input, game) {
-    try {
-      validateUtils.validateNumbers(input);
-      const matchResult = game.calculateBallStrikeScore(input);
-      const [ball, strike] = matchResult;
-      this.#outputView.printMatchResult(matchResult);
-      if (strike === GAME_CONSTANTS.STRIKE_OUT_COUNT) {
-        this.readCommand(game);
-        return;
+    while (this.#gameState !== QUIT) {
+      switch (this.#gameState) {
+        case PLAYING:
+          await this.readAndProcessNumbers(baseballGame);
+          break;
+        case COMMAND:
+          await this.readAndProcessCommand(baseballGame);
+          break;
       }
-      this.readNumbers(game);
-    } catch (error) {
-      throw error;
     }
   }
 
-  async processCommand(input, game) {
-    try {
-      validateUtils.validateCommand(input);
-      if (input === USER_COMMANDS.RESTART) {
+  async readAndProcessNumbers(game) {
+    const input = await this.#inputView.readUserInputNumbers();
+    validateUtils.validateNumbers(input);
+
+    const matchResult = game.calculateBallStrikeScore(input);
+    const [ball, strike] = matchResult;
+    this.#outputView.printMatchResult(matchResult);
+
+    if (strike === GAME_CONSTANTS.STRIKE_OUT_COUNT) {
+      this.#gameState = COMMAND;
+    }
+  }
+
+  async readAndProcessCommand(game) {
+    const input = await this.#inputView.readUserInputCommand();
+    validateUtils.validateCommand(input);
+
+    switch (input) {
+      case USER_COMMANDS.RESTART:
         game.setNewAnswer();
-        this.readNumbers(game);
-        return;
-      }
-      if (input === USER_COMMANDS.QUIT) {
-        return;
-      }
-    } catch (error) {
-      throw error;
+        this.#gameState = PLAYING;
+        break;
+      case USER_COMMANDS.QUIT:
+        this.#gameState = QUIT;
+        break;
     }
   }
 }
