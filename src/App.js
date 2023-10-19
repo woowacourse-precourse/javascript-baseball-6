94f4ae2b915fffe2a8395b7df50a036547c19c89
lee juyoung <juyung0903@naver.com>
2023-10-20 01:13:49 +0900

    feat: 컴퓨터가 랜덤으로 3개의 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const COMPUTER_NUMBER = [];
+
 class App {
-  async play() {}
+  generateRandomNumber () {
+    while(COMPUTER_NUMBER.length <3){
+      const number = MissionUtils.Random.pickNumberInRange(1,9);
+      if(!COMPUTER_NUMBER.includes(number)) {
+        COMPUTER_NUMBER.push(number);
+      }
+    }
+  }
+  async play() {
+    this.generateRandomNumber();
+    console.log(COMPUTER_NUMBER);
+    
+  }
 }
+const app = new App();
+app.play();
+
+
 
 export default App;
