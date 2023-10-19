e43bd6488419f02892e320d63b61b262c2b37e46
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-10-19 16:29:45 +0900

    feat: npm module width CommonJS
    apply a module to a class called App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play(num) {
+    let arr = [];
+    console.log("숫자 야구 게임을 시작합니다.");
+    console.log("숫자를 입력해주세요.");
+    console.log(typeof num);
+
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+  }
 }
+
+const app = new App();
+app.play(123);
 
 export default App;
