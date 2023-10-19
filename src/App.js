aa08a00a80a34319008ec7b6c5eccb171d17d3e7
Taeyooooon <eric568@naver.com>
2023-10-19 18:01:53 +0900

    feat: 유저 입력 받기

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다');
     const computerNumber = generateComputerNumber();
-    console.log('computer : ', computerNumber);
+    const userInputNumber = await Console.readLineAsync('숫자를 입력해주세요 : ');
+    console.log(userInputNumber);
   }
 }
 
