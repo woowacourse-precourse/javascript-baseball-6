92d7581245c23af2429361d12a40252e58b4bbe7
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-19 19:18:18 +0900

    feat : 게임 시작 문구 출력 기능 추가
    printStartMessage() : 게임 시작 문구를 출력함

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const printStartMessage = () => {
+  Console.print('숫자 야구 게임을 시작합니다.');
+};
+
 class App {
-  async play() {}
+  async play() {
+    printStartMessage();
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
