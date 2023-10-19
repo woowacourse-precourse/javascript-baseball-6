f6f80129c2eceefaeb9e92d41ad30eadf9d8a31c
Hong sung soo <sow9707@daum.net>
2023-10-20 00:20:43 +0900

    컴퓨터 랜덤 수 생성 구현

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ class App {
   async play() {
     const Consol = MissionUtils.Consol;
     const Random = MissionUtils.Random;
+    const Computer = [];
 
     function startText(){
       Consol.print('숫자 야구 게임을 시작합니다.');
     }
+
+    function ComputerNumberSet() {
+      Computer = [];
+      for(let i = 0; i<3; i++) {
+        const Number = Random.pickNumberInRange(1, 9);
+        Computer.push(Number);
+      }
+    }
+
+
+
   }
 }
 
