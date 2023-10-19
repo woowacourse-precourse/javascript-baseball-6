12aff95a484e4e419d6829916a5d6f2bcafd315d
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-19 23:12:57 +0900

    :recycle: refactor: 중복되는 코드를 최소화시킴

--- __tests__/RandomPickerTest.js
+++ __tests__/RandomPickerTest.js
@@ -1,22 +1,18 @@
 import RandomPicker from "../src/models/RandomPicker";
 
-const randomPicker = new RandomPicker();
-const computerNumber = randomPicker.getComputerNumber();
-
 describe('RandomPicker', () => {
+  const randomPicker = new RandomPicker();
+  const computerNumber = randomPicker.getComputerNumber();
+
   it('생성된 임의의 숫자가 3자리인지 테스트', () => {
     expect(computerNumber.toString().length).toBe(3);
-  })
-});
+  });
 
-describe('RandomPicker', () => {
   it('데이터 타입이 숫자타입인지 테스트', () => {
     expect(typeof computerNumber).toBe('number');
     expect(isNaN(computerNumber)).toBe(false);
   });
-});
 
-describe('RandomPicker', () => {
   it('중복없이 3자리인지 테스트', () => {
     const digitSet = new Set(computerNumber.toString());
     expect(digitSet.size).toBe(3);
