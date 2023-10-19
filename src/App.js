8a3ac1ea9c5e59b85aabd0628de7f744c7d5f10a
jinjuseo <wjbbdq1223@knu.ac.kr>
2023-10-20 00:08:35 +0900

    feat : check 몇 볼, 몇 스트라이크 인지 검증 readLineAsync 코드 에러 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import * as MissionUtils from "@woowacourse/mission-utils";
+import {MissionUtils} from "@woowacourse/mission-utils";
 class App {
   async play() {
     this.start();
@@ -12,15 +12,81 @@ class App {
         computer.push(number);
       }
     }
-
-    //MissionUtils.Console.print(computer); - 잘들어갔는지확인
-
-
+    
+    let result = 0;
+    //while(result < 3){
+      result = this.check(computer);
+      if(result === 3 ){
+        MissionUtils.Console.print("게임종료");
+        //break;
+      }
+      //MissionUtils.Console.print(result);
+    //}
+    
   }
+  error(){
+    MissionUtils.Console.print("에러테스트");
+  }
+  async getNumber(){
+      try{
+        const number = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+        return number;
+      } catch(error){
+        
+      }
+  };
+  check(numbers){
+    console.log(numbers);
+    let ball = 0;
+    let strike = 0;
+    let output ="";
+    let input = [];
+    
+      this.getNumber().then((res) =>{
+        input = res.split('');
+        //형변환 
+        for(let i=0;i<input.length;i++){
+          input[i] = Number(input[i]);
+        }
+        
+        //ball, strike 계산
+        for(let i=0;i<input.length;i++){
+          if(numbers.includes(input[i])){
+            ball += 1;
+              if(numbers[i] === input[i]){
+                ball -= 1;
+                strike +=1;
+              }
+          }
+        }
+        if(ball){
+          output += `${ball}볼`
+        }
+        if(strike){
+          output += output.length ? ` ${strike}스트라이크`:`${strike}스트라이크`
+        }
+        if(ball ===0 && strike ===0){
+          output = `낫싱`;
+        }
+        
+        MissionUtils.Console.print(output);
+        if(strike === 3){
+          output = `3개의 숫자를 모두 맞히셨습니다! 게임 종료`;
+          MissionUtils.Console.print(output);
+        }
+        return strike;
+      })
+      .catch((msg)=>{
+        console.log(msg);
+      })
+    }
+    
+      
+  }  
 
+  
 
-
-}
+//}
 const app = new App();
 app.play();
 export default App;
