dc43e46fa84266d5798aac5e545ea6a13907378b
Jiwon Lee <giwon.yi339@gmail.com>
2023-10-20 01:35:31 +0900

    feat : 사용자 입력 값 console 출력 기능 구현, 테스트" "

--- src/script.js
+++ src/script.js
@@ -1,1 +1,16 @@
 console.log("hello");
+
+const userValue = document.querySelector("#user--input");
+const submit = document.querySelector("#user--submit");
+
+// 컴퓨터 숫자 자동생성 (라이브러리 사용하기)
+
+// 사용자(플레이어) 기능 구현
+// 1. 버튼 클릭 시, 새로고침 방지하고 사용자 입력 값 출력하기
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(userValue.value);
+
+  userValue.value = "";
+  console.log("clicked");
+});
