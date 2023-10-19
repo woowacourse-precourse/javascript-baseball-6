63004fee62c70ece8e9b848d7631f76e4340214a
JsPark <junseongpark.dev@gmail.com>
2023-10-19 18:35:19 +0900

    feat: add retry message

--- src/App.js
+++ src/App.js
@@ -125,6 +125,11 @@ class App {
     while (this.strikeCount !== 3) {
       await this.userInputProcess(computerNumbers);
     }
+
+    if (this.strikeCount === 3) {
+      Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+    }
   }
 }
 
