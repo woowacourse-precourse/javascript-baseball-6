165b98f7a4667b8b2544b3e3ab60568684476375
BangDori <qudwns8865@naver.com>
2023-10-19 16:22:17 +0900

    feat: 사용자에게 게임 시작 알리기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    // 게임 시작을 사용자에게 알리기
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
