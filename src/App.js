5f856847f319b911f43a87ce7e53df32ee983a03
Hong sung soo <sow9707@daum.net>
2023-10-20 00:03:47 +0900

    인삿말 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const Consol = MissionUtils.Consol;
+    const Random = MissionUtils.Random;
+
+    function startText(){
+      Consol.print('숫자 야구 게임을 시작합니다.');
+    }
+  }
 }
 
 export default App;
