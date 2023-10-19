d1ba4f7e48be127f31249dc3aa23432e9252f7ea
thewronghand <penfreak77@gmail.com>
2023-10-19 20:54:11 +0900

    feat: 점수 계산 함수, 게임 리셋 함수 구현
    입력값과 정답을 대조하여 점수를 반환하는 함수와, 게임이 재시작될 때 정답을 리셋하는 함수를 구현한다.

--- src/Models/BaseballGame.js
+++ src/Models/BaseballGame.js
@@ -11,4 +11,24 @@ export default class BaseballGame {
       ANSWER_LENGTH
     );
   }
+
+  calculateBallStrikeScore(userInput) {
+    let strike = 0;
+    let ball = 0;
+    const pitchedBallNumbers = userInput.split("").map(Number);
+    for (let i = 0; i < pitchedBallNumbers.length; i += 1) {
+      if (this.#answer[i] === pitchedBallNumbers[i]) {
+        strike += 1;
+        return;
+      }
+      if (this.#answer.includes(pitchedBallNumbers[i])) {
+        ball += 1;
+      }
+    }
+    return [ball, strike];
+  }
+
+  resetAnswer() {
+    this.setNewAnswer();
+  }
 }
