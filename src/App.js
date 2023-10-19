2528426448b2acba4ccc2eca519ac4abc3cf4aff
yui880 <kimyuna521@gmail.com>
2023-10-19 17:03:23 +0900

    feat: 게임 시작 메세지 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import BaseballGame from './BaseballGame.js';
 import { Console } from '@woowacourse/mission-utils';
+import { MESSAGE } from './Constant.js';
 
 class App {
   async play() {
+    Console.print(MESSAGE.START_GAME);
     const game = await new BaseballGame();
   }
 }
