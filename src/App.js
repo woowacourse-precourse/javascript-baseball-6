4f256dc0aab181ff76a88157ac9006219761cf19
beomkwan.choi <3031011@naver.com>
2023-10-19 22:33:18 +0900

    ✨ feat: 입력값이 유효한 값인지 판별 기능

--- src/App.js
+++ src/App.js
@@ -20,6 +20,17 @@ class App {
     this.#computerNumber = this.threeDigitsGenerator();
   }
 
+  isValidInput(input) {
+    const STR_USER_INPUT = String(input);
+    const SET_USER_INPUT = new Set([...STR_USER_INPUT]);
+
+    if (/^[1-9]{3}$/.test(STR_USER_INPUT) && SET_USER_INPUT.size === 3) {
+      return true;
+    }
+
+    return false;
+  }
+
   async play() {}
 }
 
