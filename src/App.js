a848eef6282266c19138843d99d086bdd78da34a
Jihyeong00 <iolpd42@gmail.com>
2023-10-19 18:25:44 +0900

    입력한 숫자와의 랜던한 값의 일치 검사기능 추가

--- src/App.js
+++ src/App.js
@@ -25,6 +25,39 @@ class App {
             if (input[0] === input[1] || input[1] === input[2] || input[2] === input[0]) throw new Error();
         }
 
+        // 3. 입력한 숫자와의 랜던한 값의 일치 검사
+
+        function baseGame(randomNumberArray, inputNumber) {
+            const RANDOM_STRING = randomNumberArray.join('')
+            const INPUT_STRING = String(inputNumber)
+            let strike_count = 0;
+            let ball_count = 0;
+
+            // 3.1 모든 숫자가 일치하는지 확인하기
+
+            if (RANDOM_STRING === INPUT_STRING) return "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+
+            const INPUT_NUMBER_ARRAY = INPUT_STRING.split('');
+
+            for (let i in INPUT_NUMBER_ARRAY) {
+                if (INPUT_NUMBER_ARRAY[i].includes(RANDOM_STRING)) {
+
+                    // 3.2 자리와 값이 일치하는 수 골라내기 = strike
+                    // 3.3 자리는 다르지만 값이 일치하는 수 골라내기 = ball
+
+                    RANDOM_STRING[i] === INPUT_NUMBER_ARRAY[i] ? strike_count++ : ball_count++;
+                }
+            }
+
+            if (!strike_count && !ball_count) return "낫싱";
+
+            if (!strike_count && ball_count) return `${ball_count}볼`;
+
+            if (strike_count && !ball_count) return `${strike_count}스트라이크`;
+
+            return `${ball_count}볼 ${strike_count}스트라이크`
+        }
+
         const RANDOM_NUMBER_ARRAY = randomNumberMaker;
 
     }
