b921006f62722b9af96b4c04937af7a54fd936c9
songudeum <songudeum@gmail.com>
2023-10-20 01:18:55 +0900

    refactor: import{MissionUtils}에서 Random, Console로 변경

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Random, Console } from "@woowacourse/mission-utils";
 class App {
     async play() {
         console.log("숫자 야구 게임을 시작합니다.");
         let answer = [];
         while (answer.length < 3) {
-            let number = MissionUtils.Random.pickNumberInRange(1, 9);
+            let number = Random.pickNumberInRange(1, 9);
             if (!answer.includes(number)) {
                 answer.push(number);
             }
         }
-        console.log(answer);
         console.log("숫자를 입력해주세요.");
     }
 }
