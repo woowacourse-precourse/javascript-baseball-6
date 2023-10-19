3a5cb38f66ba386906183521e196c0cbf215ae6f
Jin Kwan Kim <kkole3897@gmail.com>
2023-10-19 16:44:20 +0900

    feat: 컴퓨터 랜덤 3자리 숫자 선택 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import { MissionUtils } from "@woowacourse/mission-utils"
 
 const INTRO_SENTENCE = "숫자 야구 게임을 시작합니다.\n";
+const NUMBER_LENGTH = 3;
 
 class App {
+  constructor() {
+    this.computer = [];
+  }
+
   async play() {
     MissionUtils.Console.print(INTRO_SENTENCE);
+
+    this.setComputer();
+  }
+
+  setComputer() {
+    const numbers = [];
+
+    while (numbers.length < NUMBER_LENGTH) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (!numbers.includes(number)) {
+        numbers.push(number);
+      }
+    }
+
+    this.computer = [...numbers];
   }
 }
 
