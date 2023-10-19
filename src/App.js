9dd4f4a8d39da48664622897a220f0c22195e6e3
JsPark <junseongpark.dev@gmail.com>
2023-10-19 17:07:59 +0900

    feat: add computer numbers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import { Random, Console } from '@woowacourse/mission-utils';
 
 class App {
+  getThreeNumbers() {
+    const numbers = new Set();
+
+    while (numbers.size < 3) {
+      const randomNumber = Random.pickNumberInRange(1, 9);
+      numbers.add(randomNumber);
+    }
+
+    return [...numbers];
+  }
+
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
+
+    const computerNumbers = this.getThreeNumbers();
   }
 }
 
