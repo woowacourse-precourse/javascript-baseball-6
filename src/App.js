0fb3770ba6f85e4e04dc8ccdd687c745c09b2a1b
Kled <1017yu@daum.net>
2023-10-19 16:33:13 +0900

    feat: util API를 이용하여 3개의 랜덤한 number 요소를 가진 배열 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const { Console, Random } = MissionUtils;
 class App {
-  async play() {}
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    this.getRandomNumber();
+  }
+
+  // 1부터 9까지의 다른 수로 이루어진 3개의 숫자를 요소로 갖는 배열 생성
+  async getRandomNumber() {
+    const randomNumber = Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
 }
+
+const app = new App();
+
+app.play();
 
 export default App;
