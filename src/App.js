3b2e58b1e96ab8162ef8bb24c1a133cdf8b1f6b1
seunghwan <sun1238799@naver.com>
2023-10-20 02:23:12 +0900

    feat: 유저가 입력한 숫자 예외처리

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 const GREETING = "숫자 야구 게임을 시작합니다.";
+const ERROR_USER_NUM = "숫자를 3개만 입력해주세요.";
+const ERROR_USER_OVERLAPPING = "숫자가 중복되었어요.";
 
 class App {
   async play() {
     MissionUtils.Console.print(GREETING);
-
+    let userNums = [];
     const computerNum = this.makeComputerNumber();
 
     MissionUtils.Console.print(computerNum);
+
+    userNumDetermine(computerNum);
   }
 
   makeComputerNumber() {
@@ -20,6 +24,26 @@ class App {
       }
     }
     return computer;
+  }
+
+  userNumDetermine(computerNum) {
+    while (1) {
+      let userNum =
+        MissionUtils.Console.readLineAsync("숫자를 입력해주세요 :").splice(" ");
+      if (userNum.length !== 3) {
+        MissionUtils.Console.print(ERROR_USER_NUM);
+        continue;
+      } else {
+        if (
+          userNum[0] === userNum[1] ||
+          userNum[1] === userNum[2] ||
+          userNum[2] === userNum[0]
+        ) {
+          MissionUtils.Console.print(ERROR_USER_OVERLAPPING);
+          continue;
+        }
+      }
+    }
   }
 }
 const app = new App();
