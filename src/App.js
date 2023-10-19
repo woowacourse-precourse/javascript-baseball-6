eaf5862d54f128c0188f4291f8d706cbbf1c3de5
fecapark <fecapark@gmail.com>
2023-10-19 17:54:11 +0900

    feat: 새로 시작 여부를 출력하고, 입력받게 만들기

--- src/App.js
+++ src/App.js
@@ -14,10 +14,19 @@ class App {
     }
 
     Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    await this.askReplay();
   }
 
   printStartMessage() {
     Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  async askReplay() {
+    const response = await Console.readLineAsync(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. : "
+    );
+
+    Console.print(response);
   }
 
   async playOneRound() {
