06e41203ee28b0cc26a03a1e4638300165906f7d
이용성 <ukal5006@kakao.com>
2023-10-20 02:03:20 +0900

    무작위 세자리 숫자 설정 기능

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    const computerNumberArray = this.setComputerNumber();
+  }
+
+  setComputerNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
   }
 }
 
