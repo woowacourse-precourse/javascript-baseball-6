198142bbed59f2415889b009a61a948d27757976
novice-hero <highero700@gmail.com>
2023-10-19 21:41:14 +0900

    Feat: 컴퓨터의 랜덤한 숫자 3개 배열 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const computerNumber = [];
+    while (computerNumber.length < 3) {
+      const randomNumber = Random.pickNumberInRange(1, 9);
+      if (!computerNumber.includes(randomNumber)) {
+        computerNumber.push(randomNumber);
+      }
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
