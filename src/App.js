22978d446eea96dfee7b50cadf65e27554564819
jypark38 <jury124@naver.com>
2023-10-19 23:51:39 +0900

    add : 정적 메세지 상수처리

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import { MissionUtils, Console } from '@woowacourse/mission-utils';
+import { LOGS } from './logs.js';
 
 class App {
   constructor(){
-    this.start = true
+    this.start = true;
     this.computer; // [number]
   }
 
   async play() {
-    Console.print('숫자 야구 게임을 시작합니다.');
+    Console.print(LOGS.GAME_START);
 
     while(true){
       if(this.start) this.initialization();
       const USER_INPUT = await this.userInput();
       if(USER_INPUT === null) continue;
       const [STRIKE,BALL] = this.checkInput(USER_INPUT);
-      const MESSAGE = this.returnMessage(STRIKE,BALL)
-      Console.print(MESSAGE);
+      const SCORE = this.returnScore(STRIKE,BALL)
+      Console.print(SCORE);
       // 스트라이크가 3이 아니면 continue
       if(STRIKE!==3) continue;
       // 3이면 아래 진행
-      Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-      Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.')
+      Console.print(LOGS.GAME_END);
+      Console.print(LOGS.RESTART_PROMPT)
       const IS_RESTART = await this.checkRestart();
       // restart 하지 않겠다고 하면 while문 종료
       if(!IS_RESTART) break;
@@ -41,11 +42,11 @@ class App {
   }
 
   async userInput(){
-    const input = await Console.readLineAsync('숫자를 입력해주세요')
+    const input = await Console.readLineAsync(LOGS.INPUT_PROMPT)
     // 유효성 테스트 통과하면 배열 생성
     if(this.validInput(input)) return input.split('').map(item=>parseInt(item))
     // 못하면 에러 발생
-    throw new Error("[ERROR]");
+    throw new Error(LOGS.INVALID_INPUT);
   }
 
   validInput(input){
@@ -56,11 +57,11 @@ class App {
   }
 
   async checkRestart() {
-    const INPUT = await Console.readLineAsync('')
+    const INPUT = await Console.readLineAsync(LOGS.INPUT_PROMPT2)
     // input이 1 혹은 2인지 검사
     if(/^[12]$/.test(INPUT)) return INPUT==='1'
     // 아니면 에러 발생
-    throw new Error("[ERROR]");
+    throw new Error(LOGS.INVALID_INPUT);
   }
   
   checkInput(input){
@@ -68,14 +69,6 @@ class App {
     // 볼 = 같은 수 - 스트라이크
     const BALL = this.checkSame(input) - STRIKE;
     return [STRIKE,BALL]
-  }
-
-  returnMessage(strike,ball){
-    const messages = [];
-    if (ball) messages.push(`${ball}볼`);
-    if (strike) messages.push(`${strike}스트라이크`);
-    // 메세지 배열이 비어있으면 낫싱 리턴
-    return messages.length ? messages.join(' ') : '낫싱';
   }
 
   // 같은 수 갯수 구하기
@@ -86,6 +79,14 @@ class App {
   // 스트라이크 수 구하기
   checkStrike(input){
     return input.filter((item,idx) => item === this.computer[idx]).length;
+  }
+
+  returnScore(strike,ball){
+    const messages = [];
+    if (ball) messages.push(`${ball}볼`);
+    if (strike) messages.push(`${strike}스트라이크`);
+    // 메세지 배열이 비어있으면 낫싱 리턴
+    return messages.length ? messages.join(' ') : LOGS.NOTHING;
   }
 }
 
---
+++ src/logs.js
@@ -0,0 +1,11 @@
+export const LOGS = {
+    INPUT_PROMPT: '숫자를 입력해주세요',
+    INPUT_PROMPT2: '',
+    RESTART_PROMPT: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+
+    GAME_START: '숫자 야구 게임을 시작합니다.',
+    GAME_END: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+    NOTHING: '낫싱',
+    
+    INVALID_INPUT: '[ERROR]',
+}
