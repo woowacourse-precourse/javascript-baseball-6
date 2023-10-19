28380ca241a290726cb4f85ca8ecf3306c180dd8
meaculpa9450 <132068319+meaculpa9450@users.noreply.github.com>
2023-10-19 21:05:43 +0900

    test: 간단한 테스트 코드 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -57,4 +57,96 @@ describe("숫자 야구 게임", () => {
 
     await expect(app.play()).rejects.toThrow("[ERROR]");
   });
+
+  test("0에 대한 처리", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["102"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+
+  test("0의 순서에 대한 처리(개인 확인용)", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["012"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow("[ERROR] 입력은 1부터 9 사이의 정수값이어야 합니다.");
+  });
+
+  test("중복에 대한 처리", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["112"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+
+  
+  test("숫자 아닌 입력에 대한 처리(추측)", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["abc"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+
+  test("부적절한 명령어", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["135", "3"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+  
+  test("랜덤값 중복 확인", async () => {
+    // given
+    const randoms = [1, 5, 5, 5, 8, 7, 7, 7, 1, 9, 4, 5, 1];
+    const answers = ["246", "158", "1", "597", "719", "2"];
+    const logSpy = getLogSpy();
+    const messages = ["낫싱", "3스트라이크", "2볼", "3스트라이크", "게임 종료"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when
+    const app = new App();
+    await expect(app.play()).resolves.not.toThrow();
+
+    // then
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
 });
