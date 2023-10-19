657f22fdfc9b9d1d58c2df6529a4c3cd364813e9
gouz7514 <gouz7514@gmail.com>
2023-10-19 16:51:38 +0900

    Update(feature) : 컴퓨터가 임의의 수 3개를 선택하는 메소드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Console, Random } from '@woowacourse/mission-utils'
+
 class App {
-  async play() {}
-}
+  getCompuerNumbers() {
+    const computerNumbers = [];
+
+    while (computerNumbers.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computerNumbers.includes(number)) {
+        computerNumbers.push(number);
+      };
+    };
+
+    return computerNumbers;
+  };
+
+  async play() {
+    const computerNumbers = this.getCompuerNumbers();
+    Console.print(computerNumbers);
+  };
+};
+
+const app = new App();
+
+app.play();
 
 export default App;
