23b3e59ff7b48998ac6c491af81fe564a73eeaf6
thewronghand <penfreak77@gmail.com>
2023-10-19 20:44:47 +0900

    feat: 정답 설정 메서드 구현
    gameUtils의 함수를 사용하여 무작위 3자리수 정답을 생성하고 이를 #answer 프로퍼티에 저장하는 함수를 구현한다.

--- src/Models/BaseballGame.js
+++ src/Models/BaseballGame.js
@@ -3,4 +3,12 @@ import { gameUtils } from "../utils/gameUtils.js";
 
 export default class BaseballGame {
   #answer;
+
+  setNewAnswer() {
+    this.#answer = gameUtils.generateAnswer(
+      MIN_NUMBER,
+      MAX_NUMBER,
+      ANSWER_LENGTH
+    );
+  }
 }
