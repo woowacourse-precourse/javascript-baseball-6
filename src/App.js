1dc51721c5af5bdbafff7db10367a0e9f2be3a35
Taeyooooon <eric568@naver.com>
2023-10-19 18:17:08 +0900

    feat: 유저 입력 검증 함수

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
 import { generateComputerNumber } from './utils/generateComputerNumber.js';
+import { validateUserNumber } from './utils/validateUserNumber.js';
 
 class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다');
     const computerNumber = generateComputerNumber();
     const userInputNumber = await Console.readLineAsync('숫자를 입력해주세요 : ');
-    console.log(userInputNumber);
+    validateUserNumber(userInputNumber);
   }
 }
 
