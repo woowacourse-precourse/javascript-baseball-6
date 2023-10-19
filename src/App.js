6df8e55a5bbb0c6179697fccc2e79e30fb6dde36
ejinn1 <ejinn1@naver.com>
2023-10-19 23:52:44 +0900

    fix: 낫싱 추가

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ class App {
           Console.print(`${strike}스트라이크`);
         } else if (ball !== 0 && strike !== 0) {
           Console.print(`${ball}볼 ${strike}스트라이크`);
+        } else {
+          Console.print(`낫싱`);
         }
       } else {
         Console.print("종료");
