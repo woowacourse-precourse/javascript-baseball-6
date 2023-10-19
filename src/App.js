2b21a1d9be1618feeef45820f2d814e8401fcb65
77JY <kwn03111@naver.com>
2023-10-20 01:42:00 +0900

    feat: 숫자 야구 게임 초기 버전
    모든 테스트케이스는 통과, 예외도 몇개 넣어보았으나 모두 통과

--- src/App.js
+++ src/App.js
@@ -1,5 +1,93 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.Numbers = [];
+  }
+
+  randomNumbers() {
+    this.Numbers = [];
+    while (this.Numbers.length < 3) {
+      const NUM = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.Numbers.includes(NUM)) {
+        this.Numbers.push(NUM)
+      }
+    }
+  }
+  // 숫자 판별기, 랜덤숫자 3개와 유저 숫자 3개를 비교해야함!
+  matchNumberPositions(USER_INPUT) {
+    let BALLS = 0
+    let STRIKES = 0
+    for (let i = 0; i < 3; i++) {
+      if (this.Numbers[i] == USER_INPUT[i]) {
+        STRIKES += 1
+      }
+      else if (this.Numbers.includes(USER_INPUT[i])) {
+        BALLS += 1
+      }
+    }
+    return {BALLS, STRIKES}
+  }
+  // 유저 인풋 3숫자 검사기
+  checkNumericPattern(USER_INPUT) {
+    const DIGITS = USER_INPUT.split("").map(Number)
+    if (DIGITS.length != 3){
+      throw new Error("[ERROR] 숫자는 최소 3개여야 합니다")
+    }
+    if (DIGITS.some((DIGIT) => DIGIT < 1 || 9 < DIGIT)){
+      throw new Error("[ERROR] 1~9 사이의 숫자만 골라주세요")
+    }
+    if (new Set(DIGITS).size != 3){
+      throw new Error("[ERROR] 중복된 숫자는 고를수 없습니다")
+    }
+    return DIGITS
+  }
+  // 숫자 야구 결과 출력기
+  checkBaseballResult(BALLS, STRIKES) {
+    if (STRIKES == 3){
+      MissionUtils.Console.print("3스트라이크")
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+      return true
+    }
+    else if (BALLS && STRIKES) {
+      MissionUtils.Console.print(`${BALLS}볼 ${STRIKES}스트라이크`)
+    }
+    else if (BALLS) {
+      MissionUtils.Console.print(`${BALLS}볼`)
+    }
+    else if (STRIKES) {
+      MissionUtils.Console.print(`${STRIKES}스트라이크`)
+    }
+    else {
+      MissionUtils.Console.print(`낫싱`)
+    }
+    return false
+  }
+
+  async play() {
+    this.randomNumbers()
+
+    while (true) {
+      const USER_INPUT = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ")
+      const DIGIT = this.checkNumericPattern(USER_INPUT);
+      const { BALLS, STRIKES } = this.matchNumberPositions(DIGIT)
+      const RESULT = this.checkBaseballResult(BALLS, STRIKES)
+
+      if (RESULT) {
+        const NEW_GAME_OR_STOP = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        if (NEW_GAME_OR_STOP == "1") {
+          this.randomNumbers()
+          continue
+        }
+        else if (NEW_GAME_OR_STOP == "2") {
+          return
+        }
+        else {
+          throw new Error("[ERROR] 잘못된 숫자를 입력 하셨습니다 종료합니다")
+        }
+      }
+    }
+  }
 }
 
 export default App;
