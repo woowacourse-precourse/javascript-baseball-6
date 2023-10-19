bda5baf5ccf4957795f6176bc7ecaf5a29c917b8
meaculpa9450 <132068319+meaculpa9450@users.noreply.github.com>
2023-10-19 20:23:10 +0900

    fix: 상수 오류 사항 수정

--- src/lib/Constants.js
+++ src/lib/Constants.js
@@ -5,17 +5,22 @@ const INPUT_MESSAGE ={
 
 const VALIDATION_RULE = {
     VALID_GUESS_LENGTH: 3,
-    VALID_COMMAND: [0, 1],
+    VALID_COMMAND: ['1', '2'],
     GUESS_MIN_VALUE_INCLUSIVE: 1,
-    GUESS_MAX_VALUE_INCLUSIVE: 9,    
+    GUESS_MAX_VALUE_INCLUSIVE: 9,
 };
+
+const COMMAND_HASH = {
+    '1' : true,
+    '2' : false,
+}
 
 const _Error_HEAD = '[ERROR]'
 const ERROR_MESSAGE = {
     NOT_AN_INT: `입력은 정수형이어야 합니다.`,
     INVALID_GUESS_LENGTH: `${_Error_HEAD} 입력은 ${VALIDATION_RULE.VALID_GUESS_LENGTH} 자리의 정수형이어야 합니다.`,
     INVALID_GUESS_VALUE: `${_Error_HEAD} 입력은 ${VALIDATION_RULE.GUESS_MIN_VALUE_INCLUSIVE}부터 ${VALIDATION_RULE.GUESS_MAX_VALUE_INCLUSIVE} 사이의 정수값이어야 합니다.`,
-    INVALID_COMMAND: `${_Error_HEAD} 명령어는 [${VALIDATION_RULE.VALID_COMMAND.join(' ')}] 중 하나로 제한됩니다.`,
+    INVALID_COMMAND: `${_Error_HEAD} 명령어는 [${VALIDATION_RULE.VALID_COMMAND.join(' ,')}] 중 하나로 제한됩니다.`,
     UNIQUE_CONSTRAINT_VIOLATED: `중복된 값은 입력할 수 없습니다.`
 };
 
@@ -27,4 +32,4 @@ const OUTPUT_MESSAGE = {
     WIN: `3개의 숫자를 모두 맞히셨습니다! 게임 종료`
 };
 
-module.exports = {INPUT_MESSAGE, VALIDATION_RULE, ERROR_MESSAGE, OUTPUT_MESSAGE};
+module.exports = {COMMAND_HASH, ERROR_MESSAGE, INPUT_MESSAGE, OUTPUT_MESSAGE, VALIDATION_RULE};
