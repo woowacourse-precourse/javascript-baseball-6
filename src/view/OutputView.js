3a04e631d57a413d4c0d7f09d8c31e1de57b41d3
swarvy <swarvy0826@naver.com>
2023-10-19 16:47:02 +0900

    feat : 게임 내 메시지 출력을 담당하는 OutputView 구현

---
+++ src/view/OutputView.js
@@ -0,0 +1,26 @@
+import { Console } from '@woowacourse/mission-utils';
+import { NOTIFICATION_MESSAGE, STATUS_MESSAGE } from '../constants/Message.js';
+
+export function printGameStart() {
+  return Console.print(NOTIFICATION_MESSAGE.gameStart);
+}
+
+export function printGameEnd() {
+  return Console.print(NOTIFICATION_MESSAGE.gameEnd);
+}
+
+export function printGameStatus(status) {
+  const message = [];
+
+  if (status.ball) {
+    message.push(`${status.ball}${STATUS_MESSAGE.ball}} `);
+  }
+  if (status.strike) {
+    message.push(`${status.strike}${STATUS_MESSAGE.strike}`);
+  }
+  if (!message.length) {
+    message.push(`${STATUS_MESSAGE.nothing}`);
+  }
+
+  Console.print(message.join(''));
+}
