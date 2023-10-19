ee3c4f76b4778a6a85a75686c5abbf9fa1b10843
ooherin <555ohr@naver.com>
2023-10-20 01:12:58 +0900

    feat: 게임 재시작 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import gameStart from "./game/start";
+const { Console } = MissionUtils;
 class App {
-  async play() {}
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    gameStart();
+  }
 }
 
 export default App;
--- src/game/start.js
+++ src/game/start.js
@@ -2,35 +2,37 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 const { Random, Console } = MissionUtils;
 
-//겹치지 않는 숫자 생성하기
-const makeRandomNumber = (length) => {
-  const computer = [];
+//게임 시작하기
+const gameStart = () => {
+  makeRandomNumber();
+};
 
-  while (computer.length < length) {
+//겹치지 않는 숫자 생성하기
+const makeRandomNumber = () => {
+  const computer = [];
+  while (computer.length < 3) {
     let newNumber = Random.pickNumberInRange(1, 9);
     if (computer.indexOf(newNumber) == -1) {
       computer.push(newNumber);
     }
   }
-  return computer;
+  tryNumbers(computer);
 };
 
+//숫자 입력하기
 const tryNumbers = async (computer) => {
-  try {
-    const userInput = await Console.readLineAsync("숫자를 입력해 주세요 : ");
-    let user = [...userInput];
-    user = user.map((num) => parseInt(num));
-    return [computer, user];
-  } catch (err) {
-    throw err;
-  }
+  let user = await Console.readLineAsync("숫자를 입력해 주세요 : ");
+  user = [...user].map((num) => parseInt(num));
+  checkNumber(computer, user);
 };
 
+//숫자가 정답인지 체크하기
 const checkNumber = (computer, user) => {
   let result = {
     ball: 0,
     strike: 0,
   };
+
   user.map((num, idx) => {
     if (computer[idx] === num) {
       result.strike++;
@@ -38,61 +40,45 @@ const checkNumber = (computer, user) => {
       result.ball++;
     }
   });
-  return result;
-};
 
-const checkEndGame = (result) => {
   if (result.strike === 3) {
-    return true;
-  }
-  return false;
-};
-
-const makeText = (result) => {
-  let answer = "";
-  if (result.ball > 0) {
-    answer += `${result.ball}볼`;
-  }
-  if (result.strike > 0) {
-    answer += `${result.strike}스트라이크`;
-  }
-  if (!answer.length) {
-    return "낫싱";
+    askMoreGame();
   } else {
-    return answer;
+    makeText(result, computer);
+    tryNumbers(computer);
   }
 };
 
-const runGame = async () => {
-  let answer = false;
-  const computer = makeRandomNumber(3);
-  console.log(computer);
-  while (!answer) {
-    const numbers = await tryNumbers(computer);
-    const result = checkNumber(numbers[0], numbers[1]);
-    if (checkEndGame(result)) {
-      answer = true;
-    } else {
-      Console.print(makeText(result));
-    }
+//텍스트 생성하기
+const makeText = (result) => {
+  const { ball, strike } = result;
+  let answer = [];
+  if (ball > 0) {
+    answer.push(`${ball}볼`);
   }
-  endGame();
+  if (strike > 0) {
+    answer.push(`${strike}스트라이크`);
+  }
+  if (ball == 0 && strike == 0) {
+    answer.pop("낫싱");
+  }
+  Console.print(answer.join(" "));
 };
 
-const endGame = () => {
-  const endChoice = Console.readLineAsync(
-    "3개의 숫자를 모두 맞히셨습니다! 게임 종료/n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+//한번 더 게임할건지 묻기
+const askMoreGame = async () => {
+  Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  const endChoice = await Console.readLineAsync(
+    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
   );
   if (endChoice === "1") {
-    runGame();
+    gameStart();
   } else if (endChoice === "2") {
     return;
+  } else {
+    throw new Error();
   }
 };
 
-const gameStart = async () => {
-  Console.print("숫자 야구 게임을 시작합니다.");
-  runGame();
-};
-
-console.log(gameStart());
+gameStart();
+export default gameStart;
