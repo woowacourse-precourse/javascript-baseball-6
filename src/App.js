db6cfb4a5deb21793c3c0d062eb1e5fbae3e2078
fecapark <fecapark@gmail.com>
2023-10-19 16:06:49 +0900

    feat: 숫자 입력 문구 출력하기

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ class App {
     Console.print("숫자 야구 게임을 시작합니다.");
   }
 
+  playOneLoop() {
+    Console.print("숫자를 입력해주세요 : ");
+  }
+
   async play() {
     this.printStartMessage();
+    this.playOneLoop();
   }
 }
 
