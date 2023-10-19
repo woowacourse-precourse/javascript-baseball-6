b2de7a52a179a069b64f12d0b77030b9d270dbb8
kimdw524 <kimdw524@naver.com>
2023-10-20 00:35:41 +0900

    feat: 메시지 상수

---
+++ src/Constants.js
@@ -0,0 +1,12 @@
+export const MESSAGE = Object.freeze({
+  START: '숫자 야구 게임을 시작합니다.',
+  INPUT: '숫자를 입력해주세요 : ',
+  SUCCESS: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  RETRY: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+  INVALID_NUMBER: '1~9로 구성된 서로 다른 세 자리의 숫자를 입력해 주세요.',
+  INVALID_RETRY_ANSWER: '1 또는 2만 입력해 주세요.',
+  STRIKE: '스트라이크',
+  BALL: '볼',
+});
+
+export default { MESSAGE };
