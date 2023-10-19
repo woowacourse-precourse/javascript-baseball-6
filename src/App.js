a42012c751e0ef52eb31b5a19ad9152b2dfd96a8
Hyurim <kyo2301@naver.com>
2023-10-19 16:45:12 +0900

    savetest

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 class App {
-  async play() {}
+  async play() {
+      const computer = [];
+
+      const randomNumber = () => {
+      while (computer.length < 3) {
+        const number = MissionUtils.Random.pickNumberInRange(1, 9);
+        console.log(number);
+        if (!computer.includes(number)) {
+          computer.push(number);
+        }
+      }
+    }
+    randomNumber();
+  }
 }
 
 export default App;
