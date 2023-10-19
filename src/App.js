b10fd08541da3ebd63f7670efd8b3dfd5d490416
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-10-19 19:35:20 +0900

    Feat: 사용자 숫자 입력 함수 구현 및 입력에 따른 유효성 검사

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ class App {
   constructor() {
     this.pick = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     this.message = MissionUtils.Console.print(MESSAGES.GAME_PROCESS.START);
+    this.userAnswer;
+  }
+
+  userPickNums() {
+    MissionUtils.Console.readLine(
+      "1~9 숫자 내에서 중복 없이 임의의 숫자 3자리를 입력하세요.",
+      (nums) => {
+        const PICK_NUMS_IN_ARRAY = nums.split("").map(Number);
+        if (PICK_NUMS_IN_ARRAY.length !== 3)
+          throw new Error(MESSAGES.GAME_ERROR.LENGTH_VALID);
+        const CORRECT_NUMS_IN_RANGE = (nums) => 1 <= nums && nums < 10;
+        if (!PICK_NUMS_IN_ARRAY.every(CORRECT_NUMS_IN_RANGE))
+          throw new Error(MESSAGES.GAME_ERROR.RANGE_VALID);
+        const DUPLICATE_NUMS_IN_ARRAY = new Set([...PICK_NUMS_IN_ARRAY]);
+        if (DUPLICATE_NUMS_IN_ARRAY.length !== nums.length)
+          throw new Error(MESSAGES.GAME_ERROR.DUPLICATE_VALID);
+        else this.userAnswer = nums;
+      }
+    );
   }
 }
 
