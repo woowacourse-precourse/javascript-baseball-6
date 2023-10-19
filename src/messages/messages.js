77d192568c82bfb03581984e716fce96594bd639
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-10-19 18:01:28 +0900

    Feat: 게임 안내 메시지 상수화

---
+++ src/messages/messages.js
@@ -0,0 +1,14 @@
+export const MESSAGES = {
+  GAME_PROCESS: {
+    START: "숫자 야구 게임을 시작합니다.",
+    END: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+    RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+    EXIT: "게임을 종료합니다.",
+  },
+
+  GAME_ERROR: {
+    LENGTH_VALID: "3개의 숫자를 입력해주세요.",
+    RANGE_VALID: "1~9까지의 숫자를 입력해주세요.",
+    DUPLICATE_VALID: "중복되지 않은 숫자를 입력해주세요.",
+  },
+};
