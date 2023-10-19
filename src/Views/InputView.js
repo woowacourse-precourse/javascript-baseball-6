018fc0a35d7127330a0effaad0b8557048aedd85
thewronghand <penfreak77@gmail.com>
2023-10-20 00:11:59 +0900

    fix: 잘못된 import문과 메세지 변경

--- src/Views/InputView.js
+++ src/Views/InputView.js
@@ -1,12 +1,12 @@
-import { GAME_MESSAGES } from "../utils/constants";
-import { readLineAsync } from "../utils/missionUtils";
+import { GAME_MESSAGES } from "../utils/constants.js";
+import { readLineAsync } from "../utils/missionUtils.js";
 
 export default class InputView {
   async readUserInputNumbers() {
-    return await readLineAsync(GAME_MESSAGES.INPUT_NUMBER);
+    return await readLineAsync(GAME_MESSAGES.INPUT_NUMBERS);
   }
 
   async readUserInputCommand() {
-    return await readLineAsync(GAME_MESSAGES.INPUT_NUMBERS);
+    return await readLineAsync(GAME_MESSAGES.INPUT_COMMAND);
   }
 }
