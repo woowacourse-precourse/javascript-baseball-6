3eb62ec933ad73d44d69bd151bf5140649bb7e82
Jules <dali1999@naver.com>
2023-10-20 04:33:36 +0900

    feat: 유저로부터 받은 Input의 유효성 검사(예외처리)

---
+++ src/handleAnswer/handleUserInput.js
@@ -0,0 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+
+const validLength = (userInput) => userInput.length !== 3;
+const validType = (userInput) => isNaN(userInput);
+
+//유저 인풋 확인
+export default function handleUserInput(userInput) {
+  //valid 확인
+  if (validLength(userInput) || validType(userInput)) {
+    throw new Error("잘못된 입력입니다.");
+  }
+
+  const listedInput = userInput;
+  return listedInput;
+}
