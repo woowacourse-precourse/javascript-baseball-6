e5b36e404ce1a3cf0fe98b2d415b054b1d95830a
Goonco <s07019@naver.com>
2023-10-19 21:56:06 +0900

    :sparkles: 1차 구현 : initGame 메소드 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.target;
+    this.targetLength = 3;
+    this.firstGame = true;
+    this.guess;
+  }
+
+  async play() {
+    this.initGame();
+    Console.print(this.target);
+  }
+
+  initGame() {
+    // 컴퓨터 내 랜덤 숫자 생성
+    this.target = [];
+    while (this.target.length < this.targetLength) {
+      let number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.target.includes(number)) this.target.push(number);
+    }
+
+    // 첫 게임일 경우 시작 멘트 발생
+    if (this.firstGame) {
+      Console.print("숫자 야구 게임을 시작합니다.");
+      this.firstGame = false;
+    }
+    return;
+  }
 }
 
-export default App;
+const game = new App();
+game.play();
+
+// export default App;
