17316f15c183de1554eea69966d8abb656a70c2e
Jihyeong00 <iolpd42@gmail.com>
2023-10-19 18:24:32 +0900

    입력한 숫자의 유효성 검사 기능추가

--- src/App.js
+++ src/App.js
@@ -15,6 +15,16 @@ class App {
             return computer
         }
 
+        // 2. 입력한 숫자의 유효성 검사
+        function checkNumber(input) {
+            // 2.1 숫자가 입력이 되었는가?
+            if (typeof input !== "number") throw new Error();
+            // 2.2 3자리수가 입력이 되었는가?
+            if (String(input).length !== 3) throw new Error();
+            // 모두 다른 숫자가 입력이 되었는가?
+            if (input[0] === input[1] || input[1] === input[2] || input[2] === input[0]) throw new Error();
+        }
+
         const RANDOM_NUMBER_ARRAY = randomNumberMaker;
 
     }
