bceb5ba30909d170631bf062286562a44ac42eda
thewronghand <penfreak77@gmail.com>
2023-10-19 23:41:31 +0900

    feat: printMatchResult 함수 구현
    매치 결과를 받아 알맞은 메세지를 출력하는 printMatchResult를 구현한다.

--- src/Views/OutputView.js
+++ src/Views/OutputView.js
@@ -1,9 +1,27 @@
-import { GAME_MESSAGES, GAME_RESULTS } from "../utils/constants";
+import {
+  GAME_CONSTANTS,
+  GAME_MESSAGES,
+  GAME_RESULTS,
+} from "../utils/constants";
 import { print } from "../utils/missionUtils";
 
 export default class OutputView {
   printStart() {
     print(GAME_MESSAGES.START);
+  }
+
+  printMatchResult(matchResult) {
+    const [ball, strike] = matchResult;
+    if (strike === GAME_CONSTANTS.STRIKE_OUT_COUNT) {
+      this.printGameWin(matchResult);
+      return;
+    }
+    if (!ball && !strike) {
+      this.printNoMatch();
+      return;
+    }
+    const resultText = this.getResultString(matchResult);
+    print(resultText);
   }
 
   getResultString(matchResult) {
