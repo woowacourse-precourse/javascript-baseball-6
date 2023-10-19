90f407d1d36cb4b45cd080bd7684f033c6dcdf42
jypark38 <jury124@naver.com>
2023-10-19 21:52:06 +0900

    add : 인풋 유효성 검사 압축 & 주요 기능 추가

--- src/App.js
+++ src/App.js
@@ -8,18 +8,26 @@ class App {
   }
 
   async play() {
-    this.initialization();
     if(this.start) {
       Console.print('숫자 야구 게임을 시작합니다.');
       this.start=false;
     }
-    while(1){
-      const res = await this.user_input();
-      console.log(res)
-      
+
+    this.initialization();
+
+    while(true){
+      const userInput = await this.user_input();
+      if(userInput === null) continue;
+      const result = this.check_input(userInput);
+      Console.print(result);
+      if(result==='3스트라이크'){
+        Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.')
+      }
     }
-    // while
-    console.log(this.computer)
+    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.')
+    whi
   }
 
   initialization () {
@@ -33,46 +41,60 @@ class App {
 
   async user_input(){
     const input = await Console.readLineAsync('숫자를 입력해주세요')
-    console.log(input.length)
-    console.log(this.check_input(input))
-    if(!(this.check_number(input) && this.check_input_length(input) && this.check_duplicate(input))) return 0;
-    console.log('통과')
+    if(this.valid_input(input)){
+      return input.split('').map(item=>parseInt(item))
+    } else{
+      Console.print('올바르지 않은 입력')
+      return null
+    }
   }
 
-  check_input(input){
+  async restart_input() {
+    while(true){
+      const input = await Console.readLineAsync('숫자를 입력해주세요')
+      if(/^[12]$/.test(input)){
+        
+      }
+    }
+    
+  }
+
+  valid_input(input){
     // 아래 check들 정규표현식 처리
     const regex = /^(?!.*(.).*\1)^[1-9]{3}$/;
     return regex.test(input);
   }
 
-  check_number(input){
-    // 1~9가 아닌 문자 찾기
-    // 숫자로만 이뤄져있으면 true
-    // 아니면 false
-    return /^[1-9]+$/.test(input)
+  check_input(input){
+    const strike = this.check_strike(input);
+    const ball = this.check_same(input) - strike;
+    
+    let result_str = '';
+    if(ball) result_str += `${ball}볼 `;
+    if(strike) result_str += `${strike}스트라이크`
+    if(ball===0 && strike===0) result_str += '낫싱'
+
+    return result_str
   }
 
-  check_duplicate(input){
-    // 중복입력 체크
-    return new Set(input).size === 3;
+  check_same(input) {
+    let same=0;
+    for(let i=0;i<3;i++){
+      if(this.computer.includes(input[i])) same++;
+    }
+    console.log("same",same)
+    return same;
   }
 
-  check_input_length(input){
-    // 입력길이가 3이면 true
-    // 아니면 false
-    console.log(input.length)
-    return input.length === 3;
+  check_strike(input){
+    let strike=0;
+    for(let i=0;i<3;i++){
+      if(this.computer[i] === input[i]) strike++;
+    }
+    console.log("strike",strike)
+    return strike;
   }
-
-  check_strike(){
-
-  }
-  check_ball() {
-
-  }
-  check_nothing() {
-
-  }
+  
 }
 
 export default App;
