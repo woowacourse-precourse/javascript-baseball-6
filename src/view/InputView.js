d025cca104ad98103cb69d2bf90a07d373935093
swarvy <swarvy0826@naver.com>
2023-10-19 16:04:05 +0900

    fix : 입력 문구 수정

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,7 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 
 export async function getPlayerInput() {
   try {
-    return Console.readLineAsync('숫자를 입력해주세요');
+    return Console.readLineAsync('숫자를 입력해주세요 : ');
   } catch (error) {
     return Console.print(error);
   }
