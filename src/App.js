4b3ea6bc62a4816ef306e510d9e391f60b5ece57
minseok cho <whalstjr199927@gmail.com>
2023-10-19 20:01:21 +0900

    inputValidation 메서드 구현, play 메서드에서 유저 입력 받고 입력을 검사하고 에러처리 하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -15,9 +15,48 @@ class App {
             }
         }
     }
+
+    inputValidation(userNumber) {
+        //올바른 숫자인지 검사하는 메서드
+
+        userNumber = new Set(userNumber); // set으로 중복 제거
+
+        if (userNumber.size !== 3) {
+            // 3자리 숫자인지 확인
+            return false;
+        }
+
+        for (let number of userNumber) {
+            number = parseInt(number, 10); // 각 문자를 10진수로 변경
+
+            if (isNaN(number)) {
+                // 숫자 아니면 false
+                return false;
+            } else if (number < 1 || number > 9) {
+                // 1부터 9사이 아니면 false
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     async play() {
         this.computerNumberCreate(); // 컴퓨터 숫자 생성 메서드 호출
         MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+        while (true) {
+            // 계속 반복
+            const userNumber = await MissionUtils.Console.readLineAsync(
+                "숫자를 입력해주세요 : "
+            ); // 숫자를 입력한다.
+
+            if (this.inputValidation(userNumber)) {
+            } else {
+                //잘못된 값을 입력했을 경우 에러 반환.
+                throw Error("[ERROR]");
+            }
+        }
     }
 }
 
