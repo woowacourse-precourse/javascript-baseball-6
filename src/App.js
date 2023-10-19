c1b7056a9c070cb2aa397b64991f69bf937effd3
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-10-19 18:07:59 +0900

    Feat: 인스턴스 객체 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { MESSAGES } from "./messages/messages";
+
 class App {
-  async play() {}
+  constructor() {
+    this.pick = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    this.message = MissionUtils.Console.print(MESSAGES.GAME_PROCESS.START);
+  }
 }
 
-
+const app = new App();
+app.play();
 
 export default App;
