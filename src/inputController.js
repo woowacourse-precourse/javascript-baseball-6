2833554f1e2577920f19aa18b466773aade3b559
이태훈 <rhymint@gmail.com>
2023-10-20 07:11:06 +0900

    modify for error test

--- src/inputController.js
+++ src/inputController.js
@@ -2,11 +2,10 @@ import {Console} from "@woowacourse/mission-utils";
 
 export async function guessInput() {
     let input = '';
-    try{input = await Console.readLineAsync('숫자를 입력해주세요 : ');}
-    catch(error){}
+    input = await Console.readLineAsync('숫자를 입력해주세요 : ');
 
     if (isNaN(input) || input.length !==3 || isDuplicated(input)){
-        throw Error("서로 다른 3자리의 수를 입력해 주세요")
+        throw Error("[ERROR]서로 다른 3자리의 수를 입력해 주세요")
     } else {
         return input
     }
@@ -24,12 +23,11 @@ function isDuplicated(str) {
 export async function restartInput() {
     Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
     let input = '';
-    try{input = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');}
-    catch(error) {}
+    input = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
 
     if (input == '1' || input == '2'){
         return input
     } else {
-        throw Error("1 혹은 2를 입력해 주세요")
+        throw Error("[ERROR]1 혹은 2를 입력해 주세요")
     }
 }
