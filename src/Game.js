780dfffabbc2acef59f49796b3a287f0cd91433c
6puritans9 <6puritans9@gmail.com>
2023-10-19 19:33:37 +0900

    2. feat: 중복되지 않는 임의의 수 3개를 배열로 생성하는 메서드 구현

--- src/Game.js
+++ src/Game.js
@@ -3,14 +3,19 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 export class Game {
   constructor() {
     this.welcome();
-    // this.randomGenerator();
+    this.randomGenerator();
   }
 
   welcome() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
 
-  //   randomGenerator() {
-  //     const number = MissionUtils.Random.pickNumberInRange(1, 9);
-  //   }
+  randomGenerator() {
+    const set = new Set();
+    while (set.size < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      set.add(number);
+    }
+    return [...set];
+  }
 }
