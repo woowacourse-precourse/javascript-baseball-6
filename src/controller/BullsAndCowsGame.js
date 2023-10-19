f339b0a1ed4ccbba3823b0e0eb448d69a1471593
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-19 20:36:39 +0900

    :sparkles: feat: 숫자야구게임 컨트롤러 클래스 생성 게임시작시 초기 필요한 메서드 틀 생성

---
+++ src/controller/BullsAndCowsGame.js
@@ -0,0 +1,14 @@
+class BullsAndCowsGame {
+
+  constructor() {
+    // 컴퓨터의 3자리 랜덤숫자 생성하기
+  }
+
+  async startGame() {
+    // 게임시작 문구 출력
+    // 사용자의 입력값 받기
+  }
+
+};
+
+export default BullsAndCowsGame;
