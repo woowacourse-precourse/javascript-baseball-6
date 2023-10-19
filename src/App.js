e7a2002c22b2f50b80518599cd418eb4b103a81c
jadewisemann <jadewisemann@gmail.com>
2023-10-20 06:58:59 +0900

    init: test 'Console.pirnt()' method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    Console.print("test")
+  }
 }
 
+const app = new App();
+app.play();
+
+
 export default App;
+
