23631f1ce10c3a1784ff72b9914a0adf2aececde
Byunggwon Park <82306066+startartart@users.noreply.github.com>
2023-10-20 01:18:48 +0900

    [Feat] 숫자를 입력할 수 있다.
    * docs: 기능 요구사항 및 프로그래밍 요구사항 체크
    프로그래밍 요구 사항 추가
    
    * feat: 프로그램 실행 및 사용자 숫자 입력 구현
    사용자 입력 조건 에외처리를 정규식으로 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    const number = await InputNumber();
+  }
 }
+
+const REG_INPUT_NUMBER = /^(?!.*(\d).*\1)\d{3}$/;
+
+async function InputNumber() {
+  try {
+    const yourNumber = await Console.readLineAsync('숫자를 입력해주세요 : ');
+    checkString(yourNumber);
+  } catch (error) {
+    throw new Error(error, 'readLineAsync Promise Rejected.');
+  }
+}
+
+function checkString(input) {
+  if (!REG_INPUT_NUMBER.test(input))
+    throw new Error('Input Value is Incorrect.');
+}
+
+const app = new App();
+app.play();
 
 export default App;
