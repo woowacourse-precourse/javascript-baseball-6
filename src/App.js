ebb1c3a7dfc5c3981c8ff89bf7e8f0901465d45d
jypark38 <jury124@naver.com>
2023-10-19 19:38:19 +0900

    add : App.js 초기상태 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+
+import { MissionUtils, Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  constructor(){
+    this.computer;
+    this.strike;
+    this.ball;
+  }
+
+  async play() {
+    this.initialization();
+    Console.print('숫자 야구 게임을 시작합니다.')
+    console.log(this.computer)
+  }
+
+  initialization () {
+    const computer = [];
+    while(computer.length < 3){
+      const number = MissionUtils.Random.pickNumberInRange(1,9)
+      if (!computer.includes(number)) computer.push(number);
+    }
+    this.computer = computer;
+  }
 }
 
 export default App;
+
