27e02c7aa36d4a0adb143faf6d97d7cf61df9db2
jinjuseo <wjbbdq1223@knu.ac.kr>
2023-10-19 20:25:02 +0900

    feat : start 게임 시작 시 안내 문구 출력 및 숫자 랜덤값 설정(초기화)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
 class App {
-  async play() {}
-}
+  async play() {
+    this.start();
+  }
+  start(){
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
 
+    //MissionUtils.Console.print(computer); - 잘들어갔는지확인
+
+
+  }
+
+
+
+}
+const app = new App();
+app.play();
 export default App;
+
+
