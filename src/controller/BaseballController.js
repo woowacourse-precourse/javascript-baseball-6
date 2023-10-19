493e525200558717309dbd47abcbb84b8041a369
swarvy <swarvy0826@naver.com>
2023-10-19 16:27:45 +0900

    feat : Controller에 Referee 객체를 생성하고 값을 비교하는 기능 구현

--- src/controller/BaseballController.js
+++ src/controller/BaseballController.js
@@ -1,26 +1,47 @@
 import { getPlayerInput } from '../view/InputView.js';
 import Player from '../model/Player.js';
 import Computer from '../model/Computer.js';
+import Referee from '../model/Referee.js';
 
 export default class BaseballController {
   #Player;
 
   #Computer;
 
+  #Referee;
+
   constructor() {
     this.#Player = new Player();
     this.#Computer = new Computer();
+    this.#Referee = new Referee();
   }
 
   play() {
     getPlayerInput().then((input) => {
       const selectedNumber = new Set(input.split('').map(Number));
       this.#Player.setSelectNumber(selectedNumber);
-      this.test();
+      this.#test();
+      this.#check();
     });
   }
 
-  test() {
+  #check() {
+    const playerNumber = this.#Player.getSelectNumber();
+    const computerNumber = this.#Computer.getSelectNumber();
+    const result = this.#Referee.compare(playerNumber, computerNumber);
+
+    console.log(result);
+    if (result.strike === 3) {
+      this.#retry();
+    }
+    this.play();
+  }
+
+  static #retry() {
+    console.log('finish');
+  }
+
+  #test() {
     console.log(this.#Player.getSelectNumber());
     console.log(this.#Computer.getSelectNumber());
   }
