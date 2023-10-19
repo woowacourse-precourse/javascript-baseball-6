5b00b6ff7a3bf0bb4f743769d331570e54ec118d
abi-hong <flffjakfwm20@gmail.com>
2023-10-19 15:57:41 +0900

    feat: 기능별 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 class App {
   async play() {}
+
+  // 게임 시작문 출력 함수
+  printGameStart() {}
+
+  makeRamdomNumber() {}
+
+  inputUserNumber() {}
+
+  compareUserAndRamdomNumber() {}
+
+  notifyGameEnd() {}
 }
+
+const app = new App();
 
 export default App;
