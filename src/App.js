323e55eee424ad2699af6eca4a851e6e5af247d8
Hyuk <poiu03290@gmail.com>
2023-10-20 01:03:00 +0900

    정답 만드는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    generateAnswer();
+  }
+}
+
+const app = new App();
+app.play();
+
+function generateAnswer() {
+  const answer = [];
+  while(answer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if(!answer.includes(number)) {
+      answer.push(number)
+    }
+  }
+
+  console.log(answer);
 }
 
 export default App;
