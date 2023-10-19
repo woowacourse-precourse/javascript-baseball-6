35662be2c59067c55cef085786062f7d24d16696
yoovin <soo22839@gmail.com>
2023-10-19 22:44:25 +0900

    feat: 정답 생성 함수 구현

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ class App {
     async play() {
         let userinput;
         MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+        const answer = this.createAnswer();
         while (true) {
             userinput = await MissionUtils.Console.readLineAsync(
                 '숫자를 입력해주세요.'
@@ -11,6 +12,20 @@ class App {
         }
     }
     // 정답을 생성하는 함수
+    createAnswer() {
+        const answerList = [];
+        const answerDict = {};
+        while (answerList.length < 3) {
+            const number = MissionUtils.Random.pickNumberInRange(1, 9);
+            if (!answerList.includes(number)) {
+                answerList.push(number);
+            }
+        }
+        for (let i = 0; i < answerList.length; i += 1) {
+            answerDict[answerList[i]] = i;
+        }
+        return answerDict;
+    }
 
     // 유저의 입력을 판단하는 함수
 
