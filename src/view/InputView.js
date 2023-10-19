26bc2162855dfc39254e88f376edced427b53f27
swarvy <swarvy0826@naver.com>
2023-10-19 16:46:27 +0900

    feat : 분리된 상수 적용, 재시작 입력 기능 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,9 +1,22 @@
 import { Console } from '@woowacourse/mission-utils';
+import { NOTIFICATION_MESSAGE } from '../constants/Message.js';
 
 export async function getPlayerInput() {
   try {
-    return Console.readLineAsync('숫자를 입력해주세요 : ');
+    return Console.readLineAsync(NOTIFICATION_MESSAGE.needInput);
   } catch (error) {
-    return Console.print(error);
+    return Console.print(error.log);
   }
+}
+
+export async function getRetryInput() {
+  try {
+    return Console.readLineAsync(NOTIFICATION_MESSAGE.gameRetry);
+  } catch (error) {
+    return Console.print(error.log);
+  }
+}
+
+export async function exit() {
+  return Console.close();
 }
