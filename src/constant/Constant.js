1967e1ef9adf2b2cef32b6c9d5c7474bd0af68eb
jaeochoii <jaeochoiii0716@inha.edu>
2023-10-20 01:07:10 +0900

    feat: 상수화 파일 생성

---
+++ src/constant/Constant.js
@@ -0,0 +1,17 @@
+const GuideMessage = Object.freeze({
+  START_GAME: "숫자 야구 게임을 시작합니다.",
+  RESTART_GAME: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  INPUT_NUMBER: "숫자를 입력해주세요 :",
+  CORRECT_NUMBER: "3개의 숫자를 모두 맞히셨습니다.",
+});
+
+const ErrorMessage = Object.freeze({
+  RESTART_NUMBER_ERROR: "[ERROR] 입력값은 1 또는 2로 이루어진 숫자여야 합니다.",
+  RESTART_COUNT_ERROR: "[ERROR] 입력값은 1 또는 2, 둘 중 하나여야 합니다.",
+  INPUT_LENGTH_ERROR: "[ERROR] 입력값은 3자리로 구성되어야 합니다.",
+  INPUT_NUMBER_ERROR: "[ERROR] 입력값은 숫자로 구성되어야 합니다.",
+  INPUT_DUPLICATE_ERROR: "[ERROR] 입력값은 모두 다른 숫자로 구성되어야 합니다.",
+  INPUT_INCLUDE_ZERO_ERROR: "[ERROR] 입력값에 0은 포함될 수 없습니다.",
+});
+
+module.exports = { GuideMessage, ErrorMessage };
