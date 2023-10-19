b77fd69459d3f01cb68c37e0fdc36dfc58827704
kimdaye77 <kimdaye77@naver.com>
2023-10-19 20:35:42 +0900

    feat: 컴퓨터 숫자 랜덤 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+import generateComputerNumber from "./generateComputerNumber.js";
 class App {
-  async play() {}
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    generateComputerNumber();
+  }
 }
+
+(() => {
+  const app = new App();
+  app.play();
+})();
 
 export default App;
