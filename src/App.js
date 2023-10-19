4e66909d590c14be166fb91acbcfc1c168cf3c34
Lim-shweety <smile70200@naver.com>
2023-10-19 22:34:20 +0900

    3스트라이크시 입력 받는 것 수정

--- src/App.js
+++ src/App.js
@@ -74,9 +74,9 @@ class App {
             }
 
             if (strike_num === 3) {
-                Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+                Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 try {
-                    Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+                    continue_num = await Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
 
                     if (continue_num != 1 && continue_num != 2) {
                         throw new Error("종료시 잘못 입력됨");
