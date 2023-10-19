53256074691b1ee46f4d3737b6a549c2c7dc3ea7
abi-hong <flffjakfwm20@gmail.com>
2023-10-19 16:00:32 +0900

    feat: 게임 시작문 출력 함수

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
   async play() {}
 
-  // 게임 시작문 출력 함수
-  printGameStart() {}
+  printGameStart() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
 
   makeRamdomNumber() {}
 
@@ -14,5 +17,6 @@ class App {
 }
 
 const app = new App();
+app.printGameStart();
 
 export default App;
