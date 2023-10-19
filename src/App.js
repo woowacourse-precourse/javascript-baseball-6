3d8b3edcc8daf7a2d7898adf6269eb50caa2cb35
kd02109 <57277708+kd02109@users.noreply.github.com>
2023-10-19 19:44:28 +0900

    Feat: 사용자 숫자 입력 및, 스트라이크 기능 구현 (#3)
    * Docs : 구현 기능 목록 정리
    
    * Feat: 컴퓨터 숫자 선택 메서드 생성
    
    What is this PR?
    -  `makeRandomComputerNumber` 메서드를 통한 컴퓨터 정답 생성
    
    * Feat: 사용자 숫자 입력 및, 스트라이크 기능 구현
    
    What is this PR?
    - 사용자가 올바른 입력을 한다고 전제했을 때, 정답 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+const MESSAGE = {
+  START: "숫자 야구 게임을 시작합니다.",
+  TYPE: "숫자를 입력해주세요 : ",
+  ANSWER: "3스트라이크"
+}
 
 const {Random, Console} = MissionUtils;
 class App {
-
   constructor(){
-    this.computerNumber = []
+    this.computerNumber = [];
   }
 
   makeRandomComputerNumber(){
     while(this.computerNumber.length <3){
-      const number = Random.pickNumberInRange(1, 9)
-      if(!this.computerNumber.includes(number)) this.computerNumber.push(number)
+      const number = Random.pickNumberInRange(1, 9);
+      if(!this.computerNumber.includes(number)) this.computerNumber.push(number);
     }
+  }
+
+  async play(){
+    this.makeRandomComputerNumber()
+    Console.print(MESSAGE.START);
+    Console.print(this.computerNumber);
+    while(true){
+      const number = await Console.readLineAsync(MESSAGE.TYPE);
+      const answer = this.checkStrikeAndBall(number);
+      Console.print(answer);
+      if(answer === MESSAGE.ANSWER){
+
+      }
+    }
+  }
+
+  checkStrikeAndBall(number){
+    const check = {
+      ball : 0,
+      strike: 0
+    }
+
+    for(let i=0; i<number.length; i++ ){
+      const num = Number(number[i]);
+      const index = this.computerNumber.indexOf(num);
+      if(index === -1) continue
+      else if(index > -1) {
+        if(index === i) check.strike+=1;
+        else check.ball+=1;
+      }
+    }
+
+    return this.makeMessage(check);
+  }
+
+  makeMessage({ball, strike}){
+    if(ball === 0 && strike ===0) return `낫싱`;
+    if(ball > 0 && strike > 0) return `${ball}볼 ${strike}스트라이크`;
+    if(ball > 0) return `${ball}볼`;
+    else return `${strike}스트라이크`;
   }
 }
 
-const app = new App()
-app.play()
+const app = new App();
+app.play();
 export default App;
