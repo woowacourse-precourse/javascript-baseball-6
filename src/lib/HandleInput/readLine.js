2a921767194e74828f5d9c4462dde6e7d8ecc834
meaculpa9450 <132068319+meaculpa9450@users.noreply.github.com>
2023-10-19 22:21:53 +0900

    style: 1줄로 끝나는 함수 본문 줄바꿈 삭제

--- src/lib/HandleInput/readLine.js
+++ src/lib/HandleInput/readLine.js
@@ -2,15 +2,11 @@ const { Console } = require("@woowacourse/mission-utils");
 const { INPUT_MESSAGE } = require("../Constants");
 
 const readGuessInput = async function () {
-    return await Console.readLineAsync(INPUT_MESSAGE.GUESS, (answer) => {
-        return answer;
-    });
+    return await Console.readLineAsync(INPUT_MESSAGE.GUESS, (answer) => answer);
 };
 
 const readCommandInput = async function () {
-    return await Console.readLineAsync(INPUT_MESSAGE.COMMAND, (answer) => {
-        return answer;
-    });
+    return await Console.readLineAsync(INPUT_MESSAGE.COMMAND, (answer) => answer);
 };
 
 module.exports = { readCommandInput, readGuessInput };
