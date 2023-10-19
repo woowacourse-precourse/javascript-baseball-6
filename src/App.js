131b2a60ea6a33ccffa0f4dbfc7337190bec9a0e
prgmr99 <johnyeom24@naver.com>
2023-10-20 02:34:30 +0900

    개발환경 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print("안녕하세요!");
+  }
 }
 
 export default App;
