b93ff5b892e577babbaaed856721aae4b3d43a7b
Jeongwook Park <hatchling3713@gmail.com>
2023-10-19 16:33:03 +0900

    임의의 수 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
+}
+
+const threeRandomInts = function createThreeRandomIntegers() {
+  const result = new Array(3);
+  while (result.length < 3) {
+    const number = Random.pickNumberInRange(1, 9);
+    if (!result.includes(number)) {
+      result.push(number);
+    }
+  }
+
+  return result;
 }
 
 export default App;
