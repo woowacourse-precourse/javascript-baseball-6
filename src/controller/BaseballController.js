7becc7bd71cbf2062c1f6ed7a62c384f4d1f4779
swarvy <swarvy0826@naver.com>
2023-10-19 16:03:46 +0900

    feat : Controller에 Player, Computer 생성

--- src/controller/BaseballController.js
+++ src/controller/BaseballController.js
@@ -1,10 +1,27 @@
 import { getPlayerInput } from '../view/InputView.js';
+import Player from '../model/Player.js';
+import Computer from '../model/Computer.js';
 
 export default class BaseballController {
+  #Player;
+
+  #Computer;
+
+  constructor() {
+    this.#Player = new Player();
+    this.#Computer = new Computer();
+  }
+
   play() {
     getPlayerInput().then((input) => {
-      this.input = input;
-      console.log(input);
+      const selectedNumber = new Set(input.split('').map(Number));
+      this.#Player.setSelectNumber(selectedNumber);
+      this.test();
     });
+  }
+
+  test() {
+    console.log(this.#Player.getSelectNumber());
+    console.log(this.#Computer.getSelectNumber());
   }
 }
