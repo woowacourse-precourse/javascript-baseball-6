7ac84febfd5f636bb6759c91e208a315d3f4edb1
songudeum <songudeum@gmail.com>
2023-10-20 01:58:49 +0900

    feat: 유저가 입력한 값 받아온 후 숫자형인 배열로 전환 및 관련 문구 출력 구현 완료

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ class App {
                 answer.push(number);
             }
         }
-        console.log("숫자를 입력해주세요.");
+        let inputNumber = Console.readLineAsync();
+        let inputNumberList = await inputNumber.then(
+            (value) => value.split("").map((i) => i / 1) //문자형인 배열에서 숫자형인 배열로 변환
+        );
+        console.log(`숫자를 입력해주세요 : ${inputNumberList.join("")}`); //하나의 숫자로 보이게 하기 위해  
     }
 }
 
