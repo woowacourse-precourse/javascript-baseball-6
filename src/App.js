e2d5a5688ac0f1be35d815ecc29619780ffddaa0
kmkkkp <kmkkkp@ajou.ac.kr>
2023-10-20 00:18:40 +0900

    test utils api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import {MissionUtils} from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    console.log(MissionUtils.Random.pickNumberInRange(1, 9));
+    console.log(MissionUtils.Random.pickNumberInRange(1, 9));
+    console.log(MissionUtils.Random.pickNumberInRange(1, 9));
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
