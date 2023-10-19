7fe58d9c3408ddc993d0f4d321a91f4dc0a43901
swarvy <swarvy0826@naver.com>
2023-10-19 16:45:58 +0900

    feat : 분리된 상수를 활용하여 Controller 로직 재작성

--- src/controller/BaseballController.js
+++ src/controller/BaseballController.js
@@ -2,6 +2,12 @@ import { getPlayerInput } from '../view/InputView.js';
 import Player from '../model/Player.js';
 import Computer from '../model/Computer.js';
 import Referee from '../model/Referee.js';
+import { GAME_SETTINGS } from '../constants/GameSettings.js';
+import {
+  printGameEnd,
+  printGameStart,
+  printGameStatus,
+} from '../view/OutputView.js';
 
 export default class BaseballController {
   #Player;
@@ -14,6 +20,7 @@ export default class BaseballController {
     this.#Player = new Player();
     this.#Computer = new Computer();
     this.#Referee = new Referee();
+    printGameStart();
   }
 
   play() {
@@ -30,15 +37,15 @@ export default class BaseballController {
     const computerNumber = this.#Computer.getSelectNumber();
     const result = this.#Referee.compare(playerNumber, computerNumber);
 
-    console.log(result);
-    if (result.strike === 3) {
+    printGameStatus(result);
+    if (result.strike === GAME_SETTINGS.numberLength) {
       this.#retry();
     }
     this.play();
   }
 
   static #retry() {
-    console.log('finish');
+    printGameEnd();
   }
 
   #test() {
