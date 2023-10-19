810774d7a7091813182ab60642a619f3a9c0ab8e
yui880 <kimyuna521@gmail.com>
2023-10-19 17:30:19 +0900

    feat: 오류 메세지로 사용될 ERROR 상수 선언

--- src/Constant.js
+++ src/Constant.js
@@ -8,3 +8,11 @@ export const MESSAGE = {
   CORRECT_ANSWER: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
   WANT_RESTART: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
 };
+
+export const ERROR = {
+  NOT_A_NUMBER: '오류 - 입력값이 숫자가 아닙니다.',
+  NOT_THREE_NUMBERS: '오류 - 입력값이 세자리 숫자가 아닙니다.',
+  HAS_DUPLICATE: '오류 - 입력값이 중복된 숫자를 가지고 있습니다.',
+  HAS_ZERO: '오류 - 입력값이 0을 가지고 있습니다. 1-9사이의 숫자만 입력 가능합니다.',
+  NOT_ONE_OR_TWO: '오류 - 1이나 2를 입력해야 합니다.',
+};
