7fd4529b297ae71c8294b45fa0c9de8051a47117
lee juyoung <juyung0903@naver.com>
2023-10-20 02:59:21 +0900

    feat: 컴퓨터와 사용자의 숫자를 비교하는 기능

--- src/App.js
+++ src/App.js
@@ -16,8 +16,19 @@ class App {
       }
     }
   }
+  //유저의 숫자와 컴퓨터의 숫자 비교
   compareNumbers(guessNumber){
-    console.log(guessNumber);
+    let strikeNumbers=0,ballNumbers = 0;
+    console.log(COMPUTER_NUMBER);
+    for (let i=0; i<3; i++){
+      if(parseInt(guessNumber[i])===COMPUTER_NUMBER[i]) {
+        strikeNumbers += 1;
+      } else if (COMPUTER_NUMBER.includes(parseInt(guessNumber[i]))){
+        ballNumbers += 1;
+      }
+    }
+    console.log(strikeNumbers,ballNumbers);
+
   }
   askNumber() {
     rl.question('숫자를 입력해주세요 : ',(guessNumber)=>{
@@ -27,17 +38,16 @@ class App {
       }
       else {
         this.compareNumbers(guessNumber);
+        this.askNumber();
       }
     })
   }
   async play() {
     console.log('숫자 야구 게임을 시작합니다.');
     this.generateRandomNumber();
+    let STRIKE_NUMBERS = 0;
+    let BALLNUMBERS = 0;
     this.askNumber();
-    
-    
-    
-    
   }
 }
 const app = new App();
