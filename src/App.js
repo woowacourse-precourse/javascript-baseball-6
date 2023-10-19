930b420833c31d7f4404103c27777488842d97f8
yoovin <soo22839@gmail.com>
2023-10-19 23:24:01 +0900

    feat: 에러가 발생하면 게임을 종료시킨다.

--- src/App.js
+++ src/App.js
@@ -3,29 +3,34 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 class App {
     async play() {
         let userinput;
-        while (true) {
-            MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-            const answer = this.createAnswer();
-            console.log(answer);
+        try {
             while (true) {
-                userinput = await MissionUtils.Console.readLineAsync(
-                    '숫자를 입력해주세요.'
+                MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+                const answer = this.createAnswer();
+                console.log(answer);
+                while (true) {
+                    userinput = await MissionUtils.Console.readLineAsync(
+                        '숫자를 입력해주세요.'
+                    );
+                    const judgelist = this.getBallandStrike(userinput, answer);
+                    console.log(judgelist);
+                    if (this.isUserWin(judgelist[0], judgelist[1])) {
+                        break;
+                    }
+                }
+                MissionUtils.Console.print(
+                    '3개의 숫자를 모두 맞히셨습니다! 게임 종료'
                 );
-                const judgelist = this.getBallandStrike(userinput, answer);
-                console.log(judgelist);
-                if (this.isUserWin(judgelist[0], judgelist[1])) {
-                    break;
+                userinput = await MissionUtils.Console.readLineAsync(
+                    '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
+                );
+                if (userinput === '2') {
+                    return;
                 }
             }
-            MissionUtils.Console.print(
-                '3개의 숫자를 모두 맞히셨습니다! 게임 종료'
-            );
-            userinput = await MissionUtils.Console.readLineAsync(
-                '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
-            );
-            if (userinput === '2') {
-                return;
-            }
+        } catch (e) {
+            MissionUtils.Console.print('[ERROR]');
+            return;
         }
     }
     // 정답을 생성하는 함수
