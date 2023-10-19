548950703ed6ff8bdb89bab864ede54ad76b6546
Trophy198 <spsp8426@naver.com>
2023-10-19 19:27:30 +0900

    Feat : 숫자 야구 게임 시작 타이틀 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    startGame();
+  }
 }
 
+function startGame () {
+  return Console.print("숫자 야구 게임을 시작합니다.");
+}
+
+
+const app = new App();
+app.play();
 export default App;
