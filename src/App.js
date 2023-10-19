032accae3e015bfd4f80a0981b31149bcc4e5da3
eunsung <esk114477@gmail.com>
2023-10-19 23:48:09 +0900

    feat: computer 랜덤 숫자 생성 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-class App {
-  async play() {}
+import { MissionUtils } from "@woowacourse/mission-utils";
+function cpuPickNum(){
+  const cpuNumArr = [];
+
+  while(cpuNumArr.length < 3){
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+    if(!cpuNumArr.includes(number)){
+      cpuNumArr.push(number);
+    }
+  }
+  return cpuNumArr;
 }
+class App {
+  async play() {
+    let cpu = cpuPickNum();
+
+    return;
+  }
+}
+
+const app = new App();
+
+app.play();
 
 export default App;
