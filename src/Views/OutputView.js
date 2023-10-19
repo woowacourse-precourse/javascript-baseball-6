76137812ca5f3b5ef2f27dcfa995c2dfb1d3b834
thewronghand <penfreak77@gmail.com>
2023-10-19 23:28:23 +0900

    feat: printNoMatch 함수 구현

--- src/Views/OutputView.js
+++ src/Views/OutputView.js
@@ -1,8 +1,12 @@
-import { GAME_MESSAGES } from "../utils/constants";
+import { GAME_MESSAGES, GAME_RESULTS } from "../utils/constants";
 import { print } from "../utils/missionUtils";
 
 export default class OutputView {
   printStart() {
     print(GAME_MESSAGES.START);
+  }
+
+  printNoMatch() {
+    print(GAME_RESULTS.NO_MATCH);
   }
 }
