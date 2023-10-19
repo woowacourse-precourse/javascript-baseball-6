a9619f44f1c892957a9bc975f6c023bd42b4ac15
thewronghand <penfreak77@gmail.com>
2023-10-20 00:29:38 +0900

    feat: 예외처리를 위한 함수 구현 : 올바른 수 확인
    입력값이 숫자가 맞는지, 범위를 벗어나지 않았는지 확인하는 isValidInteger 함수를 구현한다.

---
+++ src/utils/validateUtils.js
@@ -0,0 +1,14 @@
+import { ERROR_MESSAGES, GAME_CONSTANTS, USER_COMMANDS } from "./constants.js";
+
+function isValidIntegers(input) {
+  const arr = input.split("");
+  arr.forEach((item) => {
+    if (parseFloat(item) !== parseInt(item)) {
+      return false;
+    }
+    if (parseInt(item) < GAME_CONSTANTS.MIN_NUMBER) {
+      return false;
+    }
+  });
+  return true;
+}
