36af145408484ecc0bfa48b8da9422156502d406
yui880 <kimyuna521@gmail.com>
2023-10-19 18:24:38 +0900

    feat: 볼의 개수 구하는 기능 구현

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -20,11 +20,22 @@ class BaseballGame {
 
   compareUserNumbersWithAnswer(userNumbers) {
     const numsOfStrike = this.#countStrike(userNumbers);
+    const numsOfBall = this.#countBall(userNumbers);
   }
 
   #countStrike(userNumbers) {
     return userNumbers.reduce((acc, cur, i) => {
       if (cur === this.#answer[i]) return acc + 1;
+      return acc;
+    }, 0);
+  }
+
+  #countBall(userNumbers) {
+    return userNumbers.reduce((acc, cur, i) => {
+      const index = this.#answer.indexOf(cur);
+      if (index !== i && index >= 0) {
+        return acc + 1;
+      }
       return acc;
     }, 0);
   }
