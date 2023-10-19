1e947eb9e220f2d6e4502c799cf7261b2cdd1492
myeongin <mungin0926@naver.com>
2023-10-19 17:18:08 +0900

    [feat] 게임 시작 메세지 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
 class App {
-  async play() {}
+  async play() {
+    outputMessage(START_MESSAGE);
+  }
+}
+
+function outputMessage(message) {
+  console.log(message);
 }
 
 export default App;
