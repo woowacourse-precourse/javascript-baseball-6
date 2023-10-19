eab7bbb48cfb3043a948933fa8b96160ea5872e7
seokho10007 <sukho1007@naver.com>
2023-10-20 02:49:44 +0900

    🎨 style: jsDoc 수정

--- src/App.js
+++ src/App.js
@@ -10,7 +10,16 @@ import {
 } from "./constants.js";
 
 class App {
+	/**
+	 * @type {boolean}
+	 * @description 게임이 진행 여부
+	 */
 	isPlaying;
+
+	/**
+	 * @type {number}
+	 * @description 랜덤한 3개의 숫자
+	 */
 	randomNumber;
 
 	constructor() {
@@ -18,13 +27,10 @@ class App {
 	}
 
 	/**
-	 * @description 게임을 진행하는 함수입니다.
-	 * @description 게임이 진행 중일 때, 사용자로부터 입력을 받습니다.
-	 * @description 입력이 유효하지 않을 경우, 에러 메시지를 출력합니다.
-	 * @description 입력이 유효할 경우, 스트라이크와 볼의 개수에 따라 메시지를 출력합니다.
-	 * @description 스트라이크가 3개일 경우, 게임을 재시작할지 확인합니다.
-	 * @description 게임을 재시작할 경우, 게임을 초기화합니다.
-	 * @description 게임을 재시작하지 않을 경우, 게임을 종료합니다.
+	 * @description 게임을 시작하는 메서드
+	 * - 사용자의 입력을 잗아 유효성을 확인하고 스트라이크와 볼의 개수를 출력합니다.
+	 * - 스트라이크가 3개일 경우, 게임을 재시작할지 확인합니다.
+	 * - 에러가 발생할 경우, 에러 메시지를 출력합니다.
 	 */
 	async play() {
 		Console.print(LOG.START);
@@ -65,7 +71,9 @@ class App {
 	}
 
 	/**
-	 * @description 게임을 초기화합니다.
+	 * @description 게임을 초기화하는 메서드
+	 * - 게임 진행여부를 true로 설정합니다.
+	 * - 랜덤한 3개의 숫자를 생성합니다.
 	 */
 	init() {
 		this.isPlaying = true;
@@ -73,7 +81,8 @@ class App {
 	}
 
 	/**
-	 * @description 1부터 9까지의 숫자 중 중복되지 않은 랜덤한 3개의 숫자를 생성합니다.
+	 * @description 랜덤한 3개의 숫자를 생성하는 메서드
+	 * - 1부터 9까지의 숫자 중 중복되지 않은 랜덤한 3개의 숫자를 생성합니다.
 	 * @returns {string}
 	 */
 	generateRandomNumbers() {
@@ -94,8 +103,9 @@ class App {
 
 	/**
 	 *
-	 * @param {string} input
-	 * @description 입력한 데이터가 숫자가 아니거나, 0이 포함되거나, 3자리가 아니거나, 중복된 숫자가 있는지 확인합니다.
+	 * @param {string} input - 사용자가 입력한 데이터
+	 * @description 사용자가 입력한 데이터가 유효한지 확인하는 메서드
+	 * - 입력한 데이터가 숫자가 아니거나, 0이 포함되거나, 3자리가 아니거나, 중복된 숫자가 있는지 확인합니다.
 	 * @returns {boolean}
 	 */
 	validateInput(input) {
@@ -112,9 +122,9 @@ class App {
 	}
 
 	/**
-	 * @param {string} input
-	 * @description 스트라이크와 볼의 개수를 계산합니다.
-	 * @description 스트라이크는 자릿수와 값이 모두 같을 경우, 볼은 자릿수는 다르지만 값이 같을 경우입니다.
+	 * @param {string} input - 사용자가 입력한 데이터
+	 * @description 스트라이크와 볼의 개수를 계산하는 메서드
+	 * - 스트라이크는 자릿수와 값이 모두 같을 경우, 볼은 자릿수는 다르지만 값이 같을 경우입니다.
 	 * @returns {{strike: number, ball: number}}
 	 */
 	calculateStrikeBall(input) {
@@ -141,10 +151,10 @@ class App {
 	}
 
 	/**
-	 * @param {number} strike
-	 * @param {number} ball
-	 * @description 스트라이크와 볼의 개수에 따라 메시지를 생성합니다.
-	 * @description 볼이 0 이상일 경우 "n볼"을, 스트라이크가 0 이상일 경우 "n스트라이크"를, 둘 다 0일 경우 "낫싱"을 리턴합니다.
+	 * @param {number} strike - 스트라이크의 개수
+	 * @param {number} ball - 볼의 개수
+	 * @description 스트라이크와 볼의 개수에 따라 메시지를 생성하는 메서드
+	 * - 볼이 0 이상일 경우 "n볼"을, 스트라이크가 0 이상일 경우 "n스트라이크"를, 둘 다 0일 경우 "낫싱"을 리턴합니다.
 	 * @returns {string}
 	 */
 	makeMessage(strike, ball) {
@@ -166,9 +176,9 @@ class App {
 	}
 
 	/**
-	 * @description 게임을 재시작할지 확인합니다.
-	 * @description 입력받은 숫자가 1이나 2가 아닐 경우, 에러 메시지를 출력합니다.
-	 * @description 입력받은 숫자가 1일 경우, true를 반환합니다.
+	 * @description 게임을 재시작할지 확인하는 메서드
+	 * - 입력받은 숫자가 1이나 2가 아닐 경우, 에러 메시지를 출력합니다.
+	 * - 입력받은 숫자가 1일 경우, true를 반환합니다.
 	 * @returns {boolean}
 	 */
 	async confirmRestart() {
--- src/constants.js
+++ src/constants.js
@@ -1,21 +1,67 @@
+/**
+ * 게임에 사용되는 상수들을 모아놓은 파일
+ * - 다음 프로퍼티가 존재합니다.
+ * - START, RESTART, INPUT_NUMBER, CORRECT, ERROR, STRIKE, BALL, NOTHING
+ */
 const LOG = {
+	/**
+	 * 게임 시작 시 출력되는 메시지
+	 */
 	START: "숫자 야구 게임을 시작합니다.",
+
+	/**
+	 * 게임을 재시작할지 묻는 메시지
+	 */
 	RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+
+	/**
+	 * 사용자 입력을 받는 메시지
+	 */
 	INPUT_NUMBER: "숫자를 입력해주세요 : ",
+
+	/**
+	 * 정답을 맞혔을 때 출력되는 메시지
+	 */
 	CORRECT: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+
+	/**
+	 * 에러 발생 시 출력되는 메시지
+	 */
+	ERROR: "[ERROR]",
+
 	STRIKE: "스트라이크",
 	BALL: "볼",
 	NOTHING: "낫싱",
-	ERROR: "[ERROR]",
 };
 
+/**
+ * 사용자 입력의 최대 길이
+ */
 const MAX_INPUT_LENGTH = 3;
+
+/**
+ * 스트라이크의 최대 개수
+ */
 const MAX_STRIKE_COUNT = 3;
 
+/**
+ * 랜덤 숫자의 최소 값
+ */
 const MIN_RANDOM_NUMBER = 1;
+
+/**
+ * 랜덤 숫자의 최대 값
+ */
 const MAX_RANDOM_NUMBER = 9;
 
+/**
+ * 게임을 재시작할 경우, 사용자 입력
+ */
 const RESTART_NUMBER = 1;
+
+/**
+ * 게임을 종료할 경우, 사용자 입력
+ */
 const END_NUMBER = 2;
 
 export {
