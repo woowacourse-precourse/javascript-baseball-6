164c98564f1b0ab3770a7f18e1c7f08095179c65
lee juyoung <juyung0903@naver.com>
2023-10-20 01:30:26 +0900

    feat: 사용자로부터 값을 입력받는 기능

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-
+import readline from 'readline'
 const COMPUTER_NUMBER = [];
 
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 class App {
   generateRandomNumber () {
     while(COMPUTER_NUMBER.length <3){
@@ -12,8 +17,13 @@ class App {
     }
   }
   async play() {
+    console.log('숫자 야구 게임을 시작합니다.');
     this.generateRandomNumber();
-    console.log(COMPUTER_NUMBER);
+    
+    rl.question('숫자를 입력해주세요 : ',(guessNumber)=>{
+      console.log(guessNumber)
+    })
+    
     
   }
 }
