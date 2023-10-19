54c98c6bd47fbb79fb60a7e0700d9c4f12336113
ejinn1 <ejinn1@naver.com>
2023-10-19 19:59:52 +0900

    feat: 숫자 입력 받기

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
-    // try {
-    //   const number = await Console.readLineAsync("숫자를 입력해주세요 : ");
-    //   Console.print(number);
-    // } catch (err) {
-    //   console.log(err);
-    // }
+    try {
+      const number = await Console.readLineAsync("숫자를 입력해주세요 : ");
+      Console.print(number);
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
 
