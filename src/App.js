c59bd303a2f988f67ac5032c2350b252b7fc2718
beomkwan.choi <3031011@naver.com>
2023-10-19 22:30:49 +0900

    ✨ feat: 컴퓨터 측 임의 3자리 수 생성 기능

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
+  #computerNumber;
+
+  threeDigitsGenerator() {
+    let computerStr = '';
+
+    while (computerStr.length < 3) {
+      const RANDOM_NUM = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerStr.includes(String(RANDOM_NUM))) {
+        computerStr += String(RANDOM_NUM);
+      }
+    }
+
+    return Number(computerStr);
+  }
+
+  assignComputerNumber() {
+    this.#computerNumber = this.threeDigitsGenerator();
+  }
+
   async play() {}
 }
 
