55ba4beea0912b21ac6255bd52f6f98914deec91
yoovin <soo22839@gmail.com>
2023-10-19 22:22:17 +0900

    chore: mission utils import 및 구현에 필요한 함수 정의

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+    async play() {}
+
+    // 유저의 입력을 받는 함수
+
+    // 유저의 입력을 판단하는 함수
+
+    // 게임을 다시 시작할지 판단하는 함수
 }
 
 export default App;
