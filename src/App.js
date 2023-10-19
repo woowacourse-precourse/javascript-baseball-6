be490c6c968b058a5e6a99de61224448571c6545
Kled <1017yu@daum.net>
2023-10-19 22:00:10 +0900

    feat: play 메서드 호출 시 player와 opponent 호출

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import Player from './player/index.js';
+import Opponent from './opponent/index.js';
+const { Console } = MissionUtils;
 
-const { Console, Random } = MissionUtils;
 class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
-    this.getRandomNumber();
-  }
-
-  // 1부터 9까지의 다른 수로 이루어진 3개의 숫자를 요소로 갖는 배열 생성
-  async getRandomNumber() {
-    const randomNumber = Random.pickUniqueNumbersInRange(1, 9, 3);
+    this.player = new Player();
+    this.opponent = new Opponent();
   }
 }
 
