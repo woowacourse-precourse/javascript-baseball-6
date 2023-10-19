fae8d5a246b7c8efdd3f56f527dcc77d672585ef
yoovin <soo22839@gmail.com>
2023-10-19 22:32:19 +0900

    feat: 첫 실행 문구 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
-    async play() {}
-
-    // 유저의 입력을 받는 함수
+    async play() {
+        let userinput;
+        MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+        while (true) {
+            userinput = await MissionUtils.Console.readLineAsync(
+                '숫자를 입력해주세요.'
+            );
+        }
+    }
+    // 정답을 생성하는 함수
 
     // 유저의 입력을 판단하는 함수
 
