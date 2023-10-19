c5c6c7b9262a4679510d22431b6ab409830afc6b
Joseph Park <rkfnahs12e@gmail.com>
2023-10-19 23:11:03 +0900

    스트라이크, 볼, 낫싱 여부를 판단하는 checkStrike 함수 생성

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import * as MissionUtils from '@woowacourse/mission-utils'
 // 입력 : X
 // 출력 : 랜덤한 3자리 수(랜덤한 값이며 각각 다릅니다.)가 포함된 리스트
 const getComputerNum = () => {
-  const computerNum = []
-  while (computerNum.length < 3) {
+  const computerNumList = []
+  while (computerNumList.length < 3) {
     const randomNum = MissionUtils.Random.pickNumberInRange(1,9)
-    if(!computerNum.includes(randomNum)) {
-      computerNum.push(randomNum)
+    if(!computerNumList.includes(randomNum)) {
+      computerNumList.push(randomNum)
     }
   }
-  return computerNum
+  return computerNumList
 }
 
 // 설명 : 유저가 입력한 수를 리스트로 출력해줍니다.
-// 입력 : 컴퓨터의 세자리 수
+// 입력 : 1. 컴퓨터의 세자리 수
 // 출력 : 유저가 입력한 수 리스트로 출력
+// 특이사항 : 3자리 수 이상의 자릿수를 입력하면 throw문으로 애플리케이션을 종료시킨다.
 const getUserNum = async (computer) => {
   const userNum = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ')
   const userNumList = []
@@ -28,6 +29,36 @@ const getUserNum = async (computer) => {
   }
   return userNumList
 }
+// 설명 : 유저의 세자리 수와 컴퓨터의 세자리 수를 비교하여 스트라이크, 볼, 낫싱 여부를 판단하는 함수
+// 입력 : 1. 유저의 세자리 수 리스트
+//        2. 컴퓨터의 세자리 수 리스트
+// 출력 : 스트라이크, 볼, 나싱 여부에 대한 결과
+const checkStrike = (computerList, userList) => {
+  let strikeCount = 0
+  let ballCount = 0
+  for (let i = 0; i < computerList.length; i++){
+    for(let j = 0; j< userList.length; j++) {
+      if (computerList[i] === userList[j]) {
+        if(i === j){
+          strikeCount += 1
+        } else{
+          ballCount += 1
+        }
+      } 
+    }
+  }
+  if (strikeCount === 0 && ballCount === 0){
+    MissionUtils.Console.print('낫싱')
+  } else{
+    MissionUtils.Console.print(`${ballCount > 0 ? ballCount+'볼 ' : ''}${strikeCount > 0 ? strikeCount+'스트라이크' : ''}`)
+  }
+}
+
+const main = async () => {
+const cc =  getComputerNum()
+checkStrike(cc,await getUserNum(cc))
+}
+main()
 
 class App {
   async play() {
