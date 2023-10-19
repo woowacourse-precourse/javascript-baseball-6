01043fb927f9474007643fabe0982da117d73c09
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-19 22:07:44 +0900

    :sparkles: feat: 게임시작시 생성자를 통해 랜덤숫자모델 호출 컴퓨터의 숫자를 랜덤하게 생성해주는 기능을 담당하는 모델과 연결시킴

--- src/controller/BullsAndCowsGame.js
+++ src/controller/BullsAndCowsGame.js
@@ -1,14 +1,17 @@
+import RandomPicker from "../models/RandomPicker.js";
+
 class BullsAndCowsGame {
 
   constructor() {
-    // 컴퓨터의 3자리 랜덤숫자 생성하기
+    this.randomPicker = new RandomPicker();
+    this.randomPicker.getComputerNumber();
+    // console.log(this.randomPicker.computerNumber);
   }
 
   async startGame() {
     // 게임시작 문구 출력
     // 사용자의 입력값 받기
   }
-
 };
 
 export default BullsAndCowsGame;
