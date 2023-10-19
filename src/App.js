db685db59b020841b58c44a40b6ebd5762960865
seokho10007 <sukho1007@naver.com>
2023-10-19 23:59:08 +0900

    ✨ feat: 랜덤한 3개의 번호를 생성하는 `generateRandomNumbers` 메서드 추가
    - 1 ~ 9 사이의 중복없는 랜덤한 숫자를 생성합니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { MAX_INPUT_LENGTH, MAX_RANDOM_NUMBER, MIN_RANDOM_NUMBER } from "./constants.js";
+
 class App {
-  async play() {}
+	async play() {}
+
+	/**
+	 * @description 1부터 9까지의 숫자 중 중복되지 않은 랜덤한 3개의 숫자를 생성합니다.
+	 * @returns {string}
+	 */
+	generateRandomNumbers() {
+		let randomNumber = "";
+
+		while (randomNumber.length < MAX_INPUT_LENGTH) {
+			const number = MissionUtils.Random.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+
+			if (randomNumber.includes(number)) {
+				continue;
+			}
+
+			randomNumber += number;
+		}
+
+		return randomNumber;
+	}
 }
 
 export default App;
