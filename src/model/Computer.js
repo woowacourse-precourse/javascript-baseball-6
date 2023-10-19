1aeafd35aeb9958bf2a8e96082f2b5f5800fef2b
swarvy <swarvy0826@naver.com>
2023-10-19 16:02:05 +0900

    feat : 생성된 숫자를 저장하는 Computer 모델 생성

---
+++ src/model/Computer.js
@@ -0,0 +1,25 @@
+import { Random } from '@woowacourse/mission-utils';
+
+export default class Computer {
+  #selectNumber;
+
+  constructor() {
+    this.generate();
+  }
+
+  generate() {
+    const generatedNumber = new Set();
+
+    while (generatedNumber.size < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!generatedNumber.has(number)) {
+        generatedNumber.add(number);
+      }
+    }
+    this.#selectNumber = generatedNumber;
+  }
+
+  getSelectNumber() {
+    return this.#selectNumber;
+  }
+}
