62632d6e309fe9ccea602c645e8f794e7ffd2372
jijiseong <jiseong5036@gmail.com>
2023-10-19 16:32:07 +0900

    [feat] 랜덤 수 생성 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+import generateNum from "./generateNum.js";
+
 class App {
-  async play() {}
+  async play() {
+    const computer = generateNum({ length: 3 });
+    Console.print(computer);
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
---
+++ src/generateNum.js
@@ -0,0 +1,15 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+function generateNum({ length }) {
+  const computer = [];
+  while (computer.length < length) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+
+  return computer;
+}
+
+export default generateNum;
