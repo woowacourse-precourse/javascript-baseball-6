639a54ae29a04008f88cbe9f6c64dc961f124412
fecapark <fecapark@gmail.com>
2023-10-19 16:04:45 +0900

    feat: 게임 시작 문구 출력하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  printStartMessage() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  async play() {
+    this.printStartMessage();
+  }
 }
+
+// 콘솔 테스팅 용도
+new App().play();
 
 export default App;
