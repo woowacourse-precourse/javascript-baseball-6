8dc24b16c0f142646bc4066d059fedfbff300e28
Hong Boem Sun <qjatjs123123@naver.com>
2023-10-19 18:43:27 +0900

    숫자 입력값 유효성 테스트 중복되었는지 확인

--- src/Util/Message.js
+++ src/Util/Message.js
@@ -5,7 +5,8 @@ const GAME_MESSAGE = Object.freeze({
 
 const ERROR_MESSAGE = Object.freeze({
   LENGTH : "숫자의 길이는 3이여야 합니다.",
-  NUMBER : "숫자를 입력하셔야 합니다."
+  NUMBER : "숫자를 입력하셔야 합니다.",
+  DUPLICATE : "숫자가 중복되면 안됩니다.",
 })
 
 export { GAME_MESSAGE, ERROR_MESSAGE };
--- src/Util/Validation.js
+++ src/Util/Validation.js
@@ -4,6 +4,7 @@ const validation = {
   checkBaseBallNumber(baseballNumber){
     commonValidation.checkLength(baseballNumber, 3);
     commonValidation.checkNumber(baseballNumber);
+    commonValidation.checkDuplicate(baseballNumber);
   }
 }
 
@@ -14,6 +15,11 @@ const commonValidation = {
 
   checkNumber(input){
     if(isNaN(input)) throw new Error(ERROR_MESSAGE.NUMBER);
+  },
+
+  checkDuplicate(input){
+    const baseballNumberSet = new Set(input.split(""));
+    if([...baseballNumberSet].length !== 3) throw new Error(ERROR_MESSAGE.DUPLICATE);
   }
 }
 
