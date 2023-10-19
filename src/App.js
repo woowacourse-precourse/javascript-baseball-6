b797d78814cd613bdfa6cc3ab90baf2001ec0697
Jin Kwan Kim <kkole3897@gmail.com>
2023-10-19 16:46:11 +0900

    feat: 최소, 최대 숫자 상수화

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { MissionUtils } from "@woowacourse/mission-utils"
 
 const INTRO_SENTENCE = "숫자 야구 게임을 시작합니다.";
 const NUMBER_LENGTH = 3;
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 9;
 
 class App {
   constructor() {
@@ -18,7 +20,7 @@ class App {
     const numbers = [];
 
     while (numbers.length < NUMBER_LENGTH) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      const number = MissionUtils.Random.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
 
       if (!numbers.includes(number)) {
         numbers.push(number);
