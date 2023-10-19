589d5881269ee8d1af121f278cdd802a07877ff4
Jin Kwan Kim <kkole3897@gmail.com>
2023-10-19 18:08:45 +0900

    feat: 사용자 예측 숫자 입력 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { MissionUtils } from "@woowacourse/mission-utils"
 
 const INTRO_SENTENCE = "숫자 야구 게임을 시작합니다.";
+const INPUT_NUMBERS_SENTENCE = "숫자를 입력해주세요 : ";
+
 const NUMBER_LENGTH = 3;
 const MIN_NUMBER = 1;
 const MAX_NUMBER = 9;
@@ -8,12 +10,14 @@ const MAX_NUMBER = 9;
 class App {
   constructor() {
     this.computer = [];
+    this.user = [];
   }
 
   async play() {
     MissionUtils.Console.print(INTRO_SENTENCE);
 
     this.setComputer();
+    await this.requestPredictedNumbers();
   }
 
   setComputer() {
@@ -28,6 +32,18 @@ class App {
     }
 
     this.computer = [...numbers];
+  }
+
+  async requestPredictedNumbers() {
+    const text = await MissionUtils.Console.readLineAsync(INPUT_NUMBERS_SENTENCE);
+
+    const numbers = [
+      ...new Set(
+        text
+          .split("")
+          .map((character) => parseInt(character))
+      )
+    ];
   }
 }
 
