d60e5729a81ea800ed9e796dea7c9351b1285024
Hong sung soo <sow9707@daum.net>
2023-10-20 03:37:12 +0900

    재시작 구현

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ class App {
   async play() {
     let STIRKE = 0;
     let BALL = 0;
+    let GAMEMODE = 1;
     let computerNumber = [];
     let userNumber = [];
+    
     
 
 
--- src/game.model.js
+++ src/game.model.js
@@ -54,6 +54,14 @@ class Game {
       Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
     }
   }
+
+  restart() {
+    if(STRIKE == 3) {
+      Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (answer) => {
+        GAMEMODE = answer;
+      })
+    }
+  }
   
 }
 
