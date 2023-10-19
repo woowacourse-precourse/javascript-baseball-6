841b4883a9632f311685efa49c83dfda8099fadf
JonghyunLEE12 <najo7609@gmail.com>
2023-10-19 20:22:10 +0900

    Style : [ Make Structure ]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+const GamePlay = require('./views/GamePlay');
+
 class App {
-  async play() {}
+  async play() {
+    const game_play = new GamePlay();
+  }
 }
 
 export default App;
---
+++ src/views/GamePlay.js
@@ -0,0 +1,13 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+class GamePlay {
+    constructor() {
+        this.test();
+    }
+
+    test() {
+        MissionUtils.Console.print('test');
+    }
+}
+
+module.exports = GamePlay;
