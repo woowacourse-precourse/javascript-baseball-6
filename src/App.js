73508110c444e2037c79ea2a63b98ea70c657c29
yu-retel <dbdudals100@naver.com>
2023-10-20 05:47:25 +0900

    feat: 게임결과 계산기능 구현

--- src/App.js
+++ src/App.js
@@ -9,10 +9,28 @@ class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
     try {
+      console.log(this.computer);
       const userInput = await Console.readLineAsync('숫자를 입력해주세요 :');
+      console.log(this.gameResult(userInput));
     } catch (error) {
       
     }
+  }
+
+  gameResult(userInput) {
+    const userNumber = userInput.split('').map((number) => Number(number));
+    let [strike, ball] = [0, 0];
+
+    this.computer.forEach((number, idx) => {
+      if (number === userNumber[idx]) {
+        return strike++;
+      }
+      if (this.computer.includes(userNumber[idx])) {
+        return ball++;
+      }
+    });
+
+    return [strike, ball];
   }
 }
 
