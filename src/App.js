f025bee5f0ceb363a3bcff69b696a6108b76a367
Byunggwon Park <82306066+startartart@users.noreply.github.com>
2023-10-20 01:57:47 +0900

    [Feat] 컴퓨터 숫자가 제공된다.
    * feat: 컴퓨터 숫자 생성 구현
    InputNumber 함수의 return 값 반영
    
    * fix: 숫자 범위 수정
    기본 요구 사항에 따라 1~9로 변경
    
    * fix: 정규식 범위 수정

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import { Console } from '@woowacourse/mission-utils';
+import { MissionUtils, Console } from '@woowacourse/mission-utils';
+
+const REG_INPUT_NUMBER = /^(?!.*([1-9]).*\1)[1-9]{3}$/;
 
 class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
+    CreateComputerNumber(3);
     const number = await InputNumber();
   }
 }
 
-const REG_INPUT_NUMBER = /^(?!.*(\d).*\1)\d{3}$/;
-
 async function InputNumber() {
   try {
     const yourNumber = await Console.readLineAsync('숫자를 입력해주세요 : ');
-    checkString(yourNumber);
+    CheckString(yourNumber);
+    return yourNumber;
   } catch (error) {
     throw new Error(error, 'readLineAsync Promise Rejected.');
   }
 }
 
-function checkString(input) {
+function CheckString(input) {
   if (!REG_INPUT_NUMBER.test(input))
     throw new Error('Input Value is Incorrect.');
+}
+
+function CreateComputerNumber(digit) {
+  const number = [...Array(digit)].reduce((result) => {
+    let randomDigit;
+    do {
+      randomDigit = MissionUtils.Random.pickNumberInRange(1, 9);
+    } while (result.includes(randomDigit));
+
+    result.push(randomDigit);
+    return result;
+  }, []);
+
+  Console.print(number);
 }
 
 const app = new App();
