ce249d9c30fe4b989eba1f1eb1f5b413ab9abf76
Joseph Park <rkfnahs12e@gmail.com>
2023-10-19 21:54:25 +0900

    READNE.md에 기능 목록 작성 및 컴퓨터가 제시한 수를 구하는 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import * as MissionUtils from '@woowacourse/mission-utils'
+const userNum = MissionUtils.Console.readLineAsync('')
+
+const getRandomNum = () => {
+  const computerNum = []
+  while (computerNum.length < 3) {
+    const randomNum = MissionUtils.Random.pickNumberInRange(1,9)
+    if(!computerNum.includes(randomNum)) {
+      computerNum.push(randomNum)
+    }
+  }
+}
+
 class App {
-  async play() {}
+  async play() {
+    
+    
+  }
 }
 
 export default App;
