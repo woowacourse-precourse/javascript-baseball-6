541e4b499d1515569486b7e4e7b6d522332047cb
BangDori <qudwns8865@naver.com>
2023-10-19 17:59:35 +0900

    feat: 비교 결과가 일치할 경우 정답 메시지와 재시작 여부 출력

--- src/App.js
+++ src/App.js
@@ -15,26 +15,33 @@ class App {
     // 게임 시작을 사용자에게 알리기
     Console.print("숫자 야구 게임을 시작합니다.");
 
-    // 컴퓨터 숫자 랜덤 생성
-    const computerNumbers = generateComputerNumbers();
-
     while (true) {
-      // 사용자에게 입력 받기
-      const userNumbers = await Console.readLineAsync("숫자를 입력해주세요 : ");
-      isLengthError(userNumbers);
-      isDuplicationError(userNumbers);
-      isNumberError(userNumbers);
+      // 컴퓨터 숫자 랜덤 생성
+      const computerNumbers = generateComputerNumbers();
 
-      // 컴퓨터와 사용자의 숫자를 비교하기
-      const result = compareNumbers(computerNumbers, userNumbers);
+      while (true) {
+        // 사용자에게 입력 받기
+        const userNumbers = await Console.readLineAsync(
+          "숫자를 입력해주세요 : "
+        );
+        isLengthError(userNumbers);
+        isDuplicationError(userNumbers);
+        isNumberError(userNumbers);
 
-      // 결과 화면에 출력하기
-      const resultMessage = generateResultMessage(result);
-      Console.print(resultMessage);
+        // 컴퓨터와 사용자의 숫자를 비교하기
+        const result = compareNumbers(computerNumbers, userNumbers);
 
-      if (computerNumbers === userNumbers) {
-        break;
+        // 결과 화면에 출력하기
+        const resultMessage = generateResultMessage(result);
+        Console.print(resultMessage);
+
+        if (computerNumbers === userNumbers) {
+          break;
+        }
       }
+
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
   }
 }
