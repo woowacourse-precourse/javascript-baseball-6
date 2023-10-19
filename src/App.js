b7240a770899f0cd818cdabb70be6d7fa091949e
Trophy198 <spsp8426@naver.com>
2023-10-20 00:32:52 +0900

    Feat : 컴퓨터 숫자 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ function startGame () {
   return Console.print("숫자 야구 게임을 시작합니다.");
 }
 
+function createComputerNumber () {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+  }
+}
+}
 
 const app = new App();
 app.play();
