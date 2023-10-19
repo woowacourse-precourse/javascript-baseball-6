bc12fa88990d4e9a9147763de87dfb47d39b1fab
Jeongwook Park <hatchling3713@gmail.com>
2023-10-19 16:33:49 +0900

    세 자리 수 입력 데이터 변환 기능 구현

--- src/App.js
+++ src/App.js
@@ -16,4 +16,24 @@ const threeRandomInts = function createThreeRandomIntegers() {
   return result;
 }
 
+const convert = async function convertUserInputIntoArray() {
+  try {
+    const input = await Console.readLineAsync('숫자를 입력해주세요 : ')
+
+    if (input.length !== 3) {
+      throw new Error('입력한 값이 3자리가 아닙니다.');
+    }
+
+    const numbers = input.split('').map(c => parseInt(c));
+
+    if (characters.includes(NaN)) {
+      throw new Error('입력한 값이 수가 아닙니다.');
+    }
+
+    return numbers;
+  } catch (error) {
+    Console.print(error.message);
+  }
+}
+
 export default App;
