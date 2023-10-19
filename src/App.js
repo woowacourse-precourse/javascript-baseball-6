f96115e1f0ac5dbd0306907a3d08b38d709f0ba4
Jihyeong00 <iolpd42@gmail.com>
2023-10-19 18:22:10 +0900

    랜덤한 숫자만들기 함수추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import {Console, MissionUtils} from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+    async play() {
+
+        // 1. 중복되지 않는 랜덤한 숫자 (3자릿수) 만들기
+        function randomNumberMaker() {
+            const computer = [];
+            while (computer.length < 3) {
+                const number = MissionUtils.Random.pickNumberInRange(1, 9);
+                if (!computer.includes(number)) {
+                    computer.push(number);
+                }
+            }
+            return computer
+        }
+
+        const RANDOM_NUMBER_ARRAY = randomNumberMaker;
+
+    }
 }
 
 export default App;
