b0e21f143c4f6100fd8dc577f6edbea93e1ec578
soominnnn <marisol03@naver.com>
2023-10-20 03:20:05 +0900

    랜덤 숫자 테스트

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async play() {
+    let RANDOM = {
+      RANDOM_ARR : [],
+      random_create : function(){
+        this.RANDOM_ARR = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+        return console.log(this.RANDOM_ARR);
+      },
+    }
+    RANDOM.random_create();
   }
 }
 const app = new App();
