15a258447f8c853e4bde4c230dc319949a0b3188
Steadystudy <sgmin206@naver.com>
2023-10-20 05:11:32 +0900

    Feat: App에서 MC, Player, Oppoenent가 모여 게임을 진행

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import Game from './game.js';
+import Mc from './mc.js';
+import Opponent from './opponent.js';
 
 class App {
+  opponent;
+  mc;
+  gameOver = false;
+
+  constructor() {
+    this.opponent = new Opponent();
+    this.mc = new Mc();
+  }
+
   async play() {
-    const game = new Game();
-    game.start();
+    this.mc.startMention();
+    this.opponent.makeNumbers();
+    await this.askQnA();
+
+    if (this.gameOver) {
+      const playerAnswer = await this.mc.askRegame();
+      if (playerAnswer === '1') {
+        this.gameOver = false;
+        this.opponent.resetNumbers();
+        this.opponent.makeNumbers();
+        await this.askQnA();
+      }
+    }
+  }
+
+  async askQnA() {
+    const numbers = await this.mc.askQuestion();
+    this.mc.validateNumbers(numbers);
+    const { strike, ball } = this.opponent.tellResultOf(numbers);
+    this.mc.tellOf(strike, ball);
+
+    if (strike === 3) {
+      this.gameOver = true;
+    } else {
+      await this.askQnA();
+    }
   }
 }
 
