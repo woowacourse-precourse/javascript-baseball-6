6df549bc91542ace26163e168d3a0ba33c43b15b
seunghwan <sun1238799@naver.com>
2023-10-20 01:55:53 +0900

    feat: 컴퓨터 중복되지 않는 랜덤값 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-class App {
-  async play() {}
-}
+import { MissionUtils } from "@woowacourse/mission-utils";
 
+const GREETING = "숫자 야구 게임을 시작합니다.";
+
+class App {
+  async play() {
+    MissionUtils.Console.print(GREETING);
+
+    const computerNum = this.makeComputerNumber();
+
+    MissionUtils.Console.print(computerNum);
+  }
+
+  makeComputerNumber() {
+    let computer = [];
+    while (computer.length < 3) {
+      let num = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(num)) {
+        computer.push(num);
+      }
+    }
+    return computer;
+  }
+}
+const app = new App();
+app.play();
 export default App;
