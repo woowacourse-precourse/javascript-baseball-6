a6909f9443e3d63f4a5431cce23b70d1da47cf5a
kd02109 <57277708+kd02109@users.noreply.github.com>
2023-10-19 19:16:10 +0900

    Feat: 컴퓨터 숫자 선택 메서드 생성  (#2)
    * Docs : 구현 기능 목록 정리
    
    * Feat: 컴퓨터 숫자 선택 메서드 생성
    
    What is this PR?
    -  `makeRandomComputerNumber` 메서드를 통한 컴퓨터 정답 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+
+const {Random, Console} = MissionUtils;
 class App {
-  async play() {}
+
+  constructor(){
+    this.computerNumber = []
+  }
+
+  makeRandomComputerNumber(){
+    while(this.computerNumber.length <3){
+      const number = Random.pickNumberInRange(1, 9)
+      if(!this.computerNumber.includes(number)) this.computerNumber.push(number)
+    }
+  }
 }
 
+const app = new App()
+app.play()
 export default App;
