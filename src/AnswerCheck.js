f9891cca942584b48dd21523887713a41550c1ae
dyddks <dyddks0115@naver.com>
2023-10-20 00:40:28 +0900

    정답과 입력값 비교

---
+++ src/AnswerCheck.js
@@ -0,0 +1,30 @@
+import { Console } from '@woowacourse/mission-utils';
+
+function AnswerCheck(correctAnswer ,answer) {
+    let ball = 0;
+    let strike = 0;
+
+    for(let i = 0 ; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+            if(correctAnswer[i] === answer[j] && i === j){
+                strike++;
+            }else if(correctAnswer[i] === answer[j] && i !== j){
+                ball++;
+            }
+        }
+    }
+    
+    if(ball === 0 && strike === 0){
+        Console.print('낫싱');
+    }else if(strike === 3){
+        Console.print(strike + '스트라이크');
+        Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        return 1;
+    }else if(strike === 0){
+        Console.print(ball + '볼');
+    }else{
+        Console.print(ball + '볼 ' + strike + '스트라이크');
+    }
+}
+
+export default AnswerCheck;
--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { Console } from '@woowacourse/mission-utils';
 import RandomPick from './RandomPick';
 import Input from './Input';
+import AnswerCheck from './AnswerCheck';
 
 class App {
   async play() {
     let correctAnswer;
     let answer;
+    let answerCheck;
 
     Console.print('숫자 야구 게임을 시작합니다.');
     correctAnswer = RandomPick();
-
+    
     while(true){
       answer = await Input();
+      answerCheck = AnswerCheck(correctAnswer, answer);
     }
   }
 }
