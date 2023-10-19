0e09f8526c3424649c117838545fbc9485a0bf9f
rlarlduf20 <kky8274@naver.com>
2023-10-19 17:05:25 +0900

    입력받은 값을 크기3인 배열에 자릿수 별로 나눠담기 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { getRandomNumber } from "./utils.js";
+import { getRandomNumberArr, getInputNumberArr } from "./utils.js";
 
 class App {
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    console.log(getRandomNumber());
+    const randomArr = getRandomNumberArr();
+    console.log(randomArr);
+    const inputNum = await MissionUtils.Console.readLineAsync(
+      "숫자를 입력해주세요 : "
+    );
+    const inputArr = getInputNumberArr(inputNum);
+    console.log(inputArr);
   }
 }
 
--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,22 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-export const getRandomNumber = () => {
+export const getRandomNumberArr = () => {
   const randomNumberArr = [];
   while (randomNumberArr.length < 3) {
-    const number = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!randomNumberArr.includes(number)) {
-      randomNumberArr.push(number);
+    const random = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!randomNumberArr.includes(random)) {
+      randomNumberArr.push(random);
     }
   }
   return randomNumberArr;
+};
+
+export const getInputNumberArr = (number) => {
+  const inputNumberArr = [];
+  const strNum = number.toString();
+  for (let i = 0; i < 3; i++) {
+    inputNumberArr.push(Number(strNum[i]));
+  }
+
+  return inputNumberArr;
 };
