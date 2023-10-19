b74ff9dd73f1a89e75e08f2a3aa114bb584c92b1
Steadystudy <sgmin206@naver.com>
2023-10-19 22:47:57 +0900

    Feat: 상대방 역할을 하는 Opponent 클래스

---
+++ src/opponent.js
@@ -0,0 +1,34 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+export default class Opponent {
+  #numbers = [];
+
+  resetNumbers() {
+    this.#numbers = [];
+  }
+
+  makeNumbers() {
+    while (this.#numbers.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (!this.#numbers.includes(number)) {
+        this.#numbers.push(number);
+      }
+    }
+  }
+
+  getNumbers(numbers) {
+    let strike = 0;
+    let ball = 0;
+
+    this.#numbers.forEach((num, idx) => {
+      if (numbers[idx] === String(num)) {
+        strike++;
+      } else if (numbers.includes(num)) {
+        ball++;
+      }
+    });
+
+    return { strike, ball };
+  }
+}
