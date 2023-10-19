973267554026e98351422719f38678da85c5fce5
jypark38 <jury124@naver.com>
2023-10-19 23:14:01 +0900

    refactor : check_same 간소화, 메세지 메서드 분리

--- src/App.js
+++ src/App.js
@@ -2,26 +2,31 @@ import { MissionUtils, Console } from '@woowacourse/mission-utils';
 
 class App {
   constructor(){
+    this.start = true
     this.computer; // [number]
   }
 
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
-    this.initialization();
 
     while(true){
-      const userInput = await this.user_input();
-      if(userInput === null) continue;
-      const result = this.check_input(userInput);
-      Console.print(result);
-      if(result==='3스트라이크'){
+      if(this.start){
+        this.initialization();
+      }
+      const USER_INPUT = await this.userInput();
+      if(USER_INPUT === null) continue;
+      const [STRIKE,BALL] = this.checkInput(USER_INPUT);
+      const MESSAGE = this.returnMessage(STRIKE,BALL)
+      Console.print(MESSAGE);
+      if(STRIKE===3){
         Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
         Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.')
-        const restart = await this.restart_input();
-        if(restart){
-          this.initialization();
+        const IS_RESTART = await this.checkRestart();
+        if(IS_RESTART) {
+          this.start = true
+        }else{
+          break
         }
-        break
       }
     }
   }
@@ -29,62 +34,57 @@ class App {
   initialization () {
     const computer = [];
     while(computer.length < 3){
-      const number = MissionUtils.Random.pickNumberInRange(1,9)
-      if (!computer.includes(number)) computer.push(number);
+      const NUMBER = MissionUtils.Random.pickNumberInRange(1,9)
+      if (!computer.includes(NUMBER)) computer.push(NUMBER);
     }
     this.computer = computer;
+    this.start = false;
+    return ;
   }
 
-  async user_input(){
+  async userInput(){
     const input = await Console.readLineAsync('숫자를 입력해주세요')
-    if(this.valid_input(input)){
-      return input.split('').map(item=>parseInt(item))
-    } 
+    if(this.validInput(input)) return input.split('').map(item=>parseInt(item))
     throw new Error("[ERROR]");
   }
-  valid_input(input){
+
+  validInput(input){
     // 아래 check들 정규표현식 처리
-    // 서로다른 3자리의 숫자
-    const regex = /^(?!.*(.).*\1)^[1-9]{3}$/;
-    return regex.test(input);
+    // 서로 다른 3자리의 숫자
+    const REGEX = /^(?!.*(.).*\1)^[1-9]{3}$/;
+    return REGEX.test(input);
   }
 
-  async restart_input() {
-    const input = await Console.readLineAsync('')
-    if(/^[12]$/.test(input)) {
-      return input==='1'
-    } 
+  async checkRestart() {
+    const INPUT = await Console.readLineAsync('')
+    if(/^[12]$/.test(INPUT)) return INPUT==='1'
     throw new Error("[ERROR]");
   }
   
-  check_input(input){
-    const strike = this.check_strike(input);
-    const ball = this.check_same(input) - strike;
-    
-    let result_str = '';
-    if(ball) result_str += `${ball}볼 `;
-    if(strike) result_str += `${strike}스트라이크`
-    if(ball===0 && strike===0) result_str += '낫싱'
+  checkInput(input){
+    const STRIKE = this.checkStrike(input);
+    // 같은 수 - 스트라이크 = 볼
+    const BALL = this.checkSame(input) - STRIKE;
 
-    return result_str
+    return [STRIKE,BALL]
   }
 
-  check_same(input) {
-    let same=0;
-    for(let i=0;i<3;i++){
-      if(this.computer.includes(input[i])) same++;
-    }
-    return same;
+  returnMessage(strike,ball){
+    const messages = [];
+    if (ball) messages.push(`${ball}볼`);
+    if (strike) messages.push(`${strike}스트라이크`);
+    return messages.length ? messages.join(' ') : '낫싱';
   }
 
-  check_strike(input){
-    let strike=0;
-    for(let i=0;i<3;i++){
-      if(this.computer[i] === input[i]) strike++;
-    }
-    return strike;
+  // 같은 수 갯수 구하기
+  checkSame(input) {
+    return input.filter(item=> this.computer.includes(item)).length;
   }
-  
+
+  // 스트라이크 수 구하기
+  checkStrike(input){
+    return input.filter((item,idx) => item === this.computer[idx]).length;
+  }
 }
 
 export default App;
