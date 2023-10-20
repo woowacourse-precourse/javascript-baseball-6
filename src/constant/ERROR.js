75fa60cb68cba00f30d69827fb726df851227f14
j8won <joowoni99@gmail.com>
2023-10-20 20:26:14 +0900

    feat: 추가 공지 요청사항에 따라 에러 메시지 앞에 [ERROR] 추가

--- src/constant/ERROR.js
+++ src/constant/ERROR.js
@@ -1,7 +1,7 @@
 const ERROR = {
-  EMPTY_INPUT: "입력된 값이 없습니다.",
-  INVALID_NUMBER: "1부터 9까지의 숫자 3자리를 입력해 주세요.",
-  NON_UNIQUE_DIGITS: "입력한 숫자는 서로 중복되면 안 됩니다.",
+  EMPTY_INPUT: "[ERROR] 입력된 값이 없습니다.",
+  INVALID_NUMBER: "[ERROR] 1부터 9까지의 숫자 3자리를 입력해 주세요.",
+  NON_UNIQUE_DIGITS: "[ERROR] 입력한 숫자는 서로 중복되면 안 됩니다.",
 };
 
 Object.freeze(ERROR);
