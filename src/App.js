889053a65454fa36eb50ac6e0d4502a666fdfb83
이호연 <ho991217@kakao.com>
2023-10-20 10:43:37 +0900

    feat: 1번 요구사항 구현
    1번 요구사항 `숫자 야구 게임을 시작합니다.` 문구 출력 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  /**
+   * 생성자
+   */
+  constructor() {
+    this.score = {
+      strike: 0,
+      ball: 0,
+      nothing: 0,
+    };
+    this.answer = [];
+    this.setAnswer();
+  }
+
+  async play() {
+    this.greet();
+  }
+
+  /**
+   * 환영 인사를 출력하는 메서드
+   */
+  greet() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
 }
 
 export default App;
