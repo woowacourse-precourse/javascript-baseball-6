0673b28d071fee62bb7c8ade6f68fa9e926f6ea2
hyunjun9788 <namhyunjun9788@gmail.com>
2023-10-20 18:23:00 +0900

    feat: 난수 생성 기능 및 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Console, Random } from "@woowacourse/mission-utils";
-
+import readline from "readline";
 class App {
   constructor() {
     this.computer = [];
@@ -7,6 +7,40 @@ class App {
   }
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    this.createRandomNum();
+  }
+
+  async createRandomNum() {
+    while (this.computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!this.computer.includes(number)) {
+        this.computer.push(number);
+      }
+    }
+    Console.print(this.computer);
+    await this.myInputNum();
+  }
+
+  async myInputNum() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    //예외 (0포함X)
+    rl.question("숫자를 입력해주세요 : ", (myNum) => {
+      if (myNum.includes("0")) {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다");
+      }
+      //예외(숫자만 허용)
+      this.inputNum = myNum.split("").map((element) => parseInt(element));
+      //예외(중복X, 3자리)
+      if (this.computer.length !== new Set(this.inputNum).size) {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다");
+      }
+
+      rl.close();
+    });
   }
 }
 
