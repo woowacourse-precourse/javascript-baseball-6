d5787d14117dc062357552aae62a1a122c619578
박지윤 <tkrhkfld123@naver.com>
2023-10-21 00:09:32 +0900

    feat: 컴퓨터 숫자 랜덤으로 정하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import MissionUtils from "@woowacourse/mission-utils";
+
 class App {
   async play() {
     this.startGame();
+    this.selectComputer();
   }
 
   startGame() {
     const START = "숫자 야구 게임을 시작합니다.";
     console.log(START);
+  }
+
+  selectComputer() {
+    const computer = [];
+
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+
+    return computer.join('');
   }
 }
 
