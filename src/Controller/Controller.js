6ced185e0bf61b723bbdca60f5a002f65f129ec3
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-10-20 22:03:26 +0900

    feat: 컴퓨터 객체 생성, 컴퓨터에게 랜덤숫자값 세팅

--- src/Controller/Controller.js
+++ src/Controller/Controller.js
@@ -2,12 +2,15 @@ import OutputView from '../View/OutputView.js';
 import InputView from '../View/InputView.js';
 
 import User from '../Model/User.js';
+import Computer from '../Model/Computer.js';
+import RandomNumbersMaker from '../Model/RandomNumbersMaker.js';
 
 import ErrorCatcher from '../ErrorCatcher.js';
 
 class Controller {
   constructor() {
     this.user = new User();
+    this.computer = new Computer();
   }
 
   async init() {
@@ -37,6 +40,13 @@ class Controller {
 
   setUserAnswer(numbers) {
     this.user.setAnswer(numbers);
+
+    this.setComputerAnswer();
+  }
+
+  setComputerAnswer() {
+    const randomNumbersMaker = new RandomNumbersMaker();
+    this.computer.setAnswer(randomNumbersMaker.makeNumbers());
   }
 }
 
---
+++ src/Model/Computer.js
@@ -0,0 +1,15 @@
+class Computer {
+  constructor() {
+    this.answer = '';
+  }
+
+  setAnswer(numbers) {
+    this.answer = numbers;
+  }
+
+  getAnswer() {
+    return this.answer;
+  }
+}
+
+export default Computer;
