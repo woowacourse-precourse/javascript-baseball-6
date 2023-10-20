6e6f4feea5cc72e5426cc354faaad06c88e1df54
이용성 <ukal5006@kakao.com>
2023-10-20 15:44:12 +0900

    볼 스트라이크 판별

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ class App {
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     const computerNumberArray = this.setComputerNumber();
-    console.log(computerNumberArray);
-    const userNumberArray = await this.getUserNumber();
-    console.log(userNumberArray);
+
+    while (1) {
+      const answers = await this.checkNumber(computerNumberArray);
+      if (answers == 1) break;
+    }
+    MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
   }
 
   setComputerNumber() {
@@ -46,6 +49,41 @@ class App {
       return numberArray;
     } catch (error) {
       MissionUtils.Console.print(`에러발생 ${error}`);
+    }
+  }
+
+  async checkNumber(computerNumberArray) {
+    const userNumberArray = await this.getUserNumber();
+
+    let strikeCount = 0;
+    let ballCount = 0;
+    const ballCheckArray = [];
+
+    for (let i = 0; i < computerNumberArray.length; i++) {
+      if (computerNumberArray[i] == userNumberArray[i]) {
+        strikeCount++;
+        continue;
+      }
+      ballCheckArray.push(userNumberArray[i]);
+    }
+    if (strikeCount == 3) {
+      MissionUtils.Console.print(`3스트라이크`);
+      return 1;
+    }
+    for (const number of ballCheckArray) {
+      if (computerNumberArray.includes(number)) ballCount++;
+    }
+    if (strikeCount == 0 && ballCount == 0) {
+      MissionUtils.Console.print(`낫싱`);
+      return 0;
+    } else if (strikeCount != 0 && ballCount != 0) {
+      MissionUtils.Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
+      return 0;
+    } else {
+      MissionUtils.Console.print(
+        strikeCount ? `${strikeCount}스트라이크` : `${ballCount}볼`
+      );
+      return 0;
     }
   }
 }
