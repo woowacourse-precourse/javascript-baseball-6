ae8b0e3d4f2fc3944bb02e0b903ed41cfd8e6743
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-10-20 22:44:47 +0900

    chore:answer 위치 변경

--- src/App.js
+++ src/App.js
@@ -101,8 +101,9 @@ async function getSomting() {
 class App {
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    const answer = makeRandom();
+
     getSomting();
+    const answer = makeRandom();
   }
 }
 
