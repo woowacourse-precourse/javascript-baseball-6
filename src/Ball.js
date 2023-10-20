719996f6c9e2f8c69cba036acea2008f3b5e5407
today-is-first <diligent0644@gmail.com>
2023-10-20 16:28:02 +0900

    feat: 랜덤한 3개의 공 값 생성, docs: 해당 기능 목록 수정

---
+++ src/Ball.js
@@ -0,0 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+/**
+ * 겹치지 않는 랜덤한 3개의 공 숫자 생성
+ * @returns {number}
+ */
+
+const generateRandomBallNumber = () => {
+    const number = MissionUtils.Random.pickUniqueNumbersInRange(1,9,3);
+    return number;
+}
