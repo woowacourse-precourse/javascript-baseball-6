98d51a158abbe894832a1c7fd95f44e9509e7cd3
이호연 <ho991217@kakao.com>
2023-10-20 16:06:22 +0900

    refactor: App 메서드명 리팩토링
    예약어`continue` 로 쓰인 메서드명을 `askContinue로 변경

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ class App {
   async play() {
     const input = await this.#input();
     // 게임 로직
-    const isContinue = await this.#continue();
+    const isContinue = await this.#askContinue();
     if (isContinue) {
       this.play();
     }
   }
 
   /** 계속할지 여부 */
-  async #continue() {
+  async #askContinue() {
     const input = await Console.readLineAsync(
       '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
     );
