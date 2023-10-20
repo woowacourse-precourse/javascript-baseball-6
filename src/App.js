c54a999e8b9c68e3c0239b5bf1b6046258030a55
yu-heejin <jkgt0521@naver.com>
2023-10-20 18:56:28 +0900

    feat: 사용자 숫자 정보 입력 받기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    const inputNumber = MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+    
+  }
 }
 
 export default App;
