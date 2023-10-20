5ec3ab48a2897b2e94859745e3f961fd8f1995a6
양진성 <anfakt0606@naver.com>
2023-10-20 17:54:36 +0900

    게임 시작 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const GameStart = () => {
+      const baseball = [];
+
+      while (baseball.length < 3) {
+        const number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+        if (!baseball.includes(number)) {
+          baseball.push(number);
+        }
+      }
+
+      MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+      
+      return baseball;
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
