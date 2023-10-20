8fb9e4b14cb48c4b5c0bb5f17c1015f3840aa332
Nevacat <nevacattery217@naver.com>
2023-10-20 17:14:39 +0900

    feat: 게임 시작 문구 출력, 숫자 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-class App {
-  async play() {}
+import GameUtil from './Util';
+import { Console } from '@woowacourse/mission-utils';
+
+class App extends GameUtil {
+  constructor() {
+    super();
+    this.init();
+  }
+  init() {
+    this.randomNumber = super.generateRandomNumber();
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  async play() {
+    const RESULT = Console.readLineAsync('숫자를 입력해주세요 : ');
+    if (RESULT !== 3 || RESULT.includes('0')) {
+      throw new Error(RESULT);
+    }
+  }
 }
 
 export default App;
