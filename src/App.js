1f88302485427bf3484b606daf2efdf235216506
seok-pyo <hongseokpyou@gmail.com>
2023-10-20 17:36:27 +0900

    Feat : makeStrikeZoneNumber 메서드 작성

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   constructor() {
     // 상수는 대문자로 짓고, _로 구분한다.
-    this.message = {
+    this.MESSAGE = {
       START: "숫자 야구 게임을 시작합니다.",
       INPUT: "숫자를 입력해주세요 : ",
       RETRY: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
@@ -12,9 +12,19 @@ class App {
       NOTHING: "낫싱",
       CONGRAT: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
     };
+    this.strikeZoneNumber = [];
   }
   printMsgIs(message) {
     MissionUtils.Console.print(message);
+  }
+
+  makeStrikeZoneNumber() {
+    while (this.strikeZoneNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.strikeZoneNumber.includes(number)) {
+        this.strikeZoneNumber.push(number);
+      }
+    }
   }
 
   async play() {}
