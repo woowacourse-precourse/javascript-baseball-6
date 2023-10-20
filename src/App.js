38f56e1a62494b5280dec9fc137cfa35f5775c01
Seo San <dannysir@naver.com>
2023-10-20 14:10:39 +0900

    feat :시작문구 출력, 입력받기 시작 문구 출력을 구현하였고, 랜덤한 값을 구해서 저장, 숫자를 입력하여 저장하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import {makeRandomNumber} from './MakeRandomNumber.js'
+import {MissionUtils} from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  async play() {
+    console.log('숫자 야구 게임을 시작합니다.');
+    let randomNumber = makeRandomNumber();
+    const inputNum = await MissionUtils.Console.readLineAsync('숫자를 입력해주세 :');
+
+  }
 }
+
+const san = new App();
+san.play();
 
 export default App;
--- src/MakeRandomNumber.js
+++ src/MakeRandomNumber.js
@@ -1,11 +1,11 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-function makeRandomNumber() {
+export function makeRandomNumber() {
     let setArr = new Set();
     while (setArr.size !== 3) {
         setArr.add(MissionUtils.Random.pickNumberInRange(1, 9));
     }
-    return setArr;
+    return [...setArr];
 }
 
 console.log(makeRandomNumber());
