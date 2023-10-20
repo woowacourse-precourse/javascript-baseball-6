d72884f3adfc6d2e0be9d34d489f1b2a78f745e3
eunsung <esk114477@gmail.com>
2023-10-20 23:24:02 +0900

    feat: user 숫자 입력 받기 추가

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-function cpuPickNum(){
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
+function startGame() {
+  return Console.print("숫자 야구 게임을 시작합니다.");
+}
+
+function cpuPickNum() {
   const cpuNumArr = [];
 
-  while(cpuNumArr.length < 3){
+  while (cpuNumArr.length < 3) {
     const number = MissionUtils.Random.pickNumberInRange(1, 9);
-
-    if(!cpuNumArr.includes(number)){
+    if (!cpuNumArr.includes(number)) {
       cpuNumArr.push(number);
     }
   }
   return cpuNumArr;
 }
+
+async function userPickNum() {
+  const userNum = await Console.readLineAsync("숫자를 입력해주세요 : ");
+
+  const num = userNum.split("");
+
+  const intNum = num.map(function (e) {
+    return Number(e);
+  });
+
+  return intNum;
+}
+
+function compareNumber() {}
+
 class App {
   async play() {
-    let cpu = cpuPickNum();
-
-    return;
+    startGame();
   }
 }
 
