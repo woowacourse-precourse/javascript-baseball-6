f548d54fc2d4eb38e620c8d114e4dfab1a3a9835
j8won <joowoni99@gmail.com>
2023-10-20 17:12:14 +0900

    feat: 에러 메시지 관련 상수 생성

---
+++ src/constant/ERROR.js
@@ -0,0 +1,9 @@
+const ERROR = {
+  EMPTY_INPUT: "입력된 값이 없습니다.",
+  INVALID_NUMBER: "1부터 9까지의 숫자 3자리를 입력해 주세요.",
+  NON_UNIQUE_DIGITS: "입력한 숫자는 서로 중복되면 안 됩니다.",
+};
+
+Object.freeze(ERROR);
+
+export default ERROR;
