f1a080576db186b4ddfdedeb5f9ad54ed2358f56
JUNU <bokjunwoo@gmail.com>
2023-10-21 01:14:06 +0900

    feat : 무작위로 서로 다른 3개의 숫자 생성 기능 구현

---
+++ src/BaseballGame.js
@@ -0,0 +1,20 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+class BaseballGame {
+  constructor() {
+    this.targetNumbers = this.generateRandomNumbers();
+  }
+
+  generateRandomNumbers() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+      return computer;
+    }
+  }
+}
+
+export default BaseballGame;
