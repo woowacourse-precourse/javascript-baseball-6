ffbe3412f02adcddeba63969161d4606a330493f
daeun-Kim <82816029+ddaeunbb@users.noreply.github.com>
2023-10-20 17:20:38 +0900

    [Feat]: 시작 문구 출력 기능 (#1)
    - 게임이 시작되면 `숫자 야구 게임을 시작합니다.` 문구를 출력하는 기능을 만들었습니다.
    - 문구는 MESSAGE 상수로 관리합니다.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-class App {
-  async play() {}
-  
+const MissionUtils = require('@woowacourse/mission-utils');
+const { Console, Random } = MissionUtils;
+
+const MESSAGE = {
+  START: "숫자 야구 게임을 시작합니다.",
 }
+class App {
+  async play() {
+    this.printStartNotification();
+  }
+
+  printStartNotification(){
+    Console.print(MESSAGE.START);
+  }
+}
+
+const app = new App();
+app.play();
 
 export default App;
