0a13e854ffd4cc6fdd5d01b95b20f1faee192880
JUNU <bokjunwoo@gmail.com>
2023-10-20 23:53:38 +0900

    feat : 게임시작 메세지 출력기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  play() {
+    this.gameStartMessage();
+  }
+
+  gameStartMessage() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
