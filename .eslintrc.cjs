0a7c70e763685717c1e63e5de8577218bd178971
ryan-dia <cjfdnjs158@naver.com>
2023-10-20 11:08:51 +0900

    chore: eslint configure files 설정 변경
    - 확장자 변경 (js -> cjs)
    - ESM 사용으로 인한 import/extensions 규칙 수정
    - test 파일에 no-undef & arrow-body-style 규칙 미적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,9 +4,13 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: ['airbnb', 'plugin:jsdoc/recommended', 'prettier'],
+  extends: ['airbnb-base', 'plugin:jsdoc/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
+  },
+  rules: {
+    // ESM 사용으로 인한 파일 확장자 표기 의무화
+    'import/extensions': ['error', 'always', { ignorePackages: true }],
   },
   overrides: [
     {
@@ -14,7 +18,10 @@ module.exports = {
       rules: {
         'max-lines-per-function': 'off',
         'no-new': 'off',
+        'no-undef': 'off',
+        'arrow-body-style': 'off',
       },
     },
   ],
 };
+
