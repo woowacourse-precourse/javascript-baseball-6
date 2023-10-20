5cb365a272c81a89da4b11e89aa282f79dea2632
손진영 <sm099@naver.com>
2023-10-20 22:33:56 +0900

    refactor(phase1): 시작 시 App 내 initGameSetting 메서드에서 Computer를 생성하도록 설정
    App 내 게임 세팅을 초기화 하는 로직을 명시하기 위한 리팩터링 작업 완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { Computer } from './model';
+import { InputView } from './views';
 
 class App {
   #computer;
 
+  #inputView;
+
   constructor() {
+    this.#inputView = InputView;
+  }
+
+  #initGameSetting() {
     this.#computer = new Computer();
   }
 
-  async play() {}
+  async play() {
+    this.#initGameSetting();
+  }
 }
 
 const app = new App();
