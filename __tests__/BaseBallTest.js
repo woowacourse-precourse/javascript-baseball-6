cb77356242e0ef366e6c21b89ecc953c0f392526
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-20 22:28:38 +0900

    test : _checkNothing 메서드 테스트
    test-01 : userInput과 password에 같은 숫자가 하나도 없다면 true 반환 테스트
    test-02 : strike가 있으면 false 반환 테스트
    test-03 : ball이 있으면 false 반환 테스트

--- __tests__/BaseBallTest.js
+++ __tests__/BaseBallTest.js
@@ -16,8 +16,37 @@ describe('_checkStrikesAmount 메서드 테스트', () => {
     const baseBall = new BaseBall();
     baseBall._setPassword(423);
     const userInput = 123;
-    const ballsAmount = baseBall._checkStrikesAmount(userInput);
+    const strikeAmount = baseBall._checkStrikesAmount(userInput);
 
-    expect(ballsAmount).toBe(2);
+    expect(strikeAmount).toBe(2);
+  });
+});
+
+describe('_checkNothing 메서드 테스트', () => {
+  test('userInput이 123일때 password가 456이라면 true가 반환이 되는가 ?', () => {
+    const baseBall = new BaseBall();
+    baseBall._setPassword(456);
+    const userInput = 123;
+    const isNothing = baseBall._checkNothing(userInput);
+
+    expect(isNothing).toBe(true);
+  });
+
+  test('strike가 하나라도 있으면 false가 반환되는가 ?', () => {
+    const baseBall = new BaseBall();
+    baseBall._setPassword(145);
+    const userInput = 123;
+    const isNothing = baseBall._checkNothing(userInput);
+
+    expect(isNothing).toBe(false);
+  });
+
+  test('ball이 하나라도 있으면 false가 반환되는가 ?', () => {
+    const baseBall = new BaseBall();
+    baseBall._setPassword(345);
+    const userInput = 123;
+    const isNothing = baseBall._checkNothing(userInput);
+
+    expect(isNothing).toBe(false);
   });
 });
