d767c6525c016ed1612e477555ffccbfece92be9
Trophy198 <spsp8426@naver.com>
2023-10-21 00:56:03 +0900

    Fix : gamePlayResult 함수 내부의 gameEnd 변수명 오타 수정

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ async function gamePlay(computerNumber) {
 async function gamePlayResult() {
   Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
   const gameEnd = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
-  if(gmaeEnd === '1') {
+  if(gameEnd === '1') {
     return gamePlay(createComputerNumber());
   }
 }
