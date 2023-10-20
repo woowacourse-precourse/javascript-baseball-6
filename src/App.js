36f9932903203879c81be8130f8d87c994752edc
teawon <xodnjsvkvk9@gmail.com>
2023-10-21 06:30:53 +0900

    feat : 입력된 숫자에 대한 정답처리 함수 구현

--- src/App.js
+++ src/App.js
@@ -7,23 +7,42 @@ class App {
 
     while (!isGameEnded) {
       const answer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-
+      console.log(answer);
       while (true) {
         const userResponse = await Console.readLineAsync(
           "숫자를 입력해주세요: "
         );
         // TODO 입력값 예외처리 필요
 
-        // TODO 입력 결과에 대한 케이스 연산 필요
-        break;
+        const { strike, ball } = this.compareNumbers(answer, userResponse);
+
+        console.log(strike, ball);
+        // TODO 결과에 대한 출력 필요
+        if (strike === 3) {
+          Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+          break;
+        }
       }
 
-      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       Console.readLineAsync(
         "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
       );
       // TODO 재시작, 종료에 대한 처리 필요
     }
+  }
+
+  compareNumbers(answer, userResponse) {
+    let strike = 0;
+    let ball = 0;
+
+    for (let i = 0; i < 3; i++) {
+      if (userResponse.charAt(i) - "0" === answer[i]) {
+        strike++;
+      } else if (answer.includes(userResponse.charAt(i) - "0")) {
+        ball++;
+      }
+    }
+    return { strike, ball };
   }
 }
 
