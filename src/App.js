a5482b2dc200c1426f8d72d0b945d90b271b86f4
양진성 <anfakt0606@naver.com>
2023-10-20 23:33:52 +0900

    예외처리 테스트 케이스 통과

--- src/App.js
+++ src/App.js
@@ -1,62 +1,75 @@
-import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  async play() {
-    const GameStart = () => {
-      const baseball = [];
+  async play() {  
+    let answer = this.GameStart();
+    let userAnswer = '';
 
-      while (baseball.length < 3) {
-        const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    while(answer != userAnswer){
+      userAnswer = await MissionUtils.Console.readLineAsync('숫자를 입력해 주세요 : ');  
 
-        if (!baseball.includes(number)) {
-          baseball.push(number);
-        }
+      try{
+        this.Exception(userAnswer);
+        MissionUtils.Console.print(this.Hint(answer,userAnswer));
+      }catch (error){
+        MissionUtils.Console.print(error);
+        throw new Error('[ERROR]');
       }
 
-      MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-      
-      return baseball;
-    }
-
-    const Hint = (answer,userAnswer) => {
-      let hint = '';
-      let ball = 0;
-      let strike = 0;
-
-      userAnswer.forEach((number,i) => {
-        const check = answer.indexOf(+number);
-        if(check != -1){
-          check === i ? strike++ : ball++
-        }
-      })
-
-      if(ball > 0) hint += `${ball}볼`;
-      if(strike > 0) hint += `${strike}스트라이크`;
-      if(ball === 0 && strike === 0) hint += '낫싱';
-
-      return hint
-    }
-    
-    let answer = GameStart();
-    let userAnswer = [];
-
-    while(answer.join('') != userAnswer.join('')){
-      const USER_INPUT = await MissionUtils.Console.readLineAsync('숫자를 입력해 주세요 : ');      
-      userAnswer = USER_INPUT.split('');
-
-      MissionUtils.Console.print(Hint(answer,userAnswer));
-
-      if(answer.join('') === userAnswer.join('')){
+      if(answer === userAnswer){
         MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-        const GameSelect = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
-        if(GameSelect === '1'){
-          answer = GameStart();
+        
+        const GAME_SELECTED = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+        if(GAME_SELECTED === '1'){
+          answer = this.GameStart();
         }
       }
     }
 
-    MissionUtils.Console.print('숫자 야구 게임을 종료합니다.')
+    return MissionUtils.Console.print('숫자 야구 게임을 종료합니다.')
   }
+
+  GameStart(){
+    const baseball = [];
+
+    while (baseball.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (!baseball.includes(number)) {
+        baseball.push(number);
+      }
+    }
+
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    
+    return baseball.join('');
+  }
+
+  Exception(string){
+    if(string.length != 3) throw '3자리 숫자여야 합니다.';
+  }
+
+  Hint(answer,userAnswer){
+    let hint = '';
+    let ball = 0;
+    let strike = 0;
+
+    userAnswer.split('').forEach((char,i) => {
+      const CHECK = answer.indexOf(+char);
+      if(CHECK != -1){
+        CHECK === i ? strike++ : ball++
+      }
+    })
+
+    if(ball > 0) hint += `${ball}볼`;
+    if(strike > 0) hint += `${strike}스트라이크`;
+    if(ball === 0 && strike === 0) hint += '낫싱';
+
+    return hint
+  }
+  
+
+
 }
 
 const app = new App();
