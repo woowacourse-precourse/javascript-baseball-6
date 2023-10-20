4c0eabec5e9f7cb0cfb275fdc915f195bf9b25a7
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-20 23:54:06 +0900

    refactor : beforeEach로 userInput, baseball 인스턴스 고정
    userInput : 123이 들어왔다는걸 가정해 고정

--- __tests__/BaseBallTest.js
+++ __tests__/BaseBallTest.js
@@ -1,52 +1,51 @@
 import BaseBall from '../src/BaseBall';
+describe('class BaseBall test', () => {
+  let userInput;
+  let baseBall;
 
-describe('_checkBallsAmount 메서드 테스트', () => {
-  test('userInput이 123일때 password가 213이라면 ball은 2가 되는가 ?', () => {
-    const baseBall = new BaseBall();
-    baseBall._setPassword(213);
-    const userInput = 123;
-    const ballsAmount = baseBall._checkBallsAmount(userInput);
-
-    expect(ballsAmount).toBe(2);
-  });
-});
-
-describe('_checkStrikesAmount 메서드 테스트', () => {
-  test('userInput이 123일때 password가 423이라면 strike는 2가 되는가 ?', () => {
-    const baseBall = new BaseBall();
-    baseBall._setPassword(423);
-    const userInput = 123;
-    const strikeAmount = baseBall._checkStrikesAmount(userInput);
-
-    expect(strikeAmount).toBe(2);
-  });
-});
-
-describe('_checkNothing 메서드 테스트', () => {
-  test('userInput이 123일때 password가 456이라면 true가 반환이 되는가 ?', () => {
-    const baseBall = new BaseBall();
-    baseBall._setPassword(456);
-    const userInput = 123;
-    const isNothing = baseBall._checkNothing(userInput);
-
-    expect(isNothing).toBe(true);
+  beforeEach(() => {
+    userInput = 123;
+    baseBall = new BaseBall();
   });
 
-  test('strike가 하나라도 있으면 false가 반환되는가 ?', () => {
-    const baseBall = new BaseBall();
-    baseBall._setPassword(145);
-    const userInput = 123;
-    const isNothing = baseBall._checkNothing(userInput);
+  describe('method test : _checkBallsAmount() ', () => {
+    test('_checkBallsAmount() password가 213이라면 ball은 2가 되는가 ?', () => {
+      baseBall._setPassword(213);
+      const ballsAmount = baseBall._checkBallsAmount(userInput);
 
-    expect(isNothing).toBe(false);
+      expect(ballsAmount).toBe(2);
+    });
   });
 
-  test('ball이 하나라도 있으면 false가 반환되는가 ?', () => {
-    const baseBall = new BaseBall();
-    baseBall._setPassword(345);
-    const userInput = 123;
-    const isNothing = baseBall._checkNothing(userInput);
+  describe('method test : _checkStrikesAmount() ', () => {
+    test('password가 423이라면 strike는 2가 되는가 ?', () => {
+      baseBall._setPassword(423);
+      const strikeAmount = baseBall._checkStrikesAmount(userInput);
 
-    expect(isNothing).toBe(false);
+      expect(strikeAmount).toBe(2);
+    });
+  });
+
+  describe('method test : _checkNothing() ', () => {
+    test('no ball no strike일때 true가 반환이 되는가 ?', () => {
+      baseBall._setPassword(456);
+      const isNothing = baseBall._checkNothing(userInput);
+
+      expect(isNothing).toBe(true);
+    });
+
+    test('strike가 있으면 false가 반환되는가 ?', () => {
+      baseBall._setPassword(145);
+      const isNothing = baseBall._checkNothing(userInput);
+
+      expect(isNothing).toBe(false);
+    });
+
+    test('ball이 있으면 false가 반환되는가 ?', () => {
+      baseBall._setPassword(345);
+      const isNothing = baseBall._checkNothing(userInput);
+
+      expect(isNothing).toBe(false);
+    });
   });
 });
