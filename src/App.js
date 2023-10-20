4b6c50ab6d17f8b6b53fb651af4163d5670b5227
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-20 15:52:52 +0900

    feat: 세 자리 난수 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const NUMBER_LENGTH = 3;
+
 class App {
-  async play() {}
+  answer = [];
+
+  setAnswer() {
+    while (this.answer.length < NUMBER_LENGTH) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.answer.includes(number)) {
+        this.answer.push(number);
+      }
+    }
+  }
+
+  async play() {
+    this.setAnswer();
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
