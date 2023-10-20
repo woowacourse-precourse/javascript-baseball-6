30aba958d5df7a4ad043be77eb35ef2640d2d5bf
solssak <gswoong1012@gmail.com>
2023-10-20 23:58:17 +0900

    feat: 기능2 - 랜덤 숫자 생성 기능 구현 (컴퓨터)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   showStartMessage() {
-    Console.print('숫자 야구 게임을 시작합니다.');
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  setRandomNumber() {
+    const randomNumber = [];
+    while (randomNumber.length < 3) {
+      let number = Random.pickNumberInRange(1, 9);
+      if (!randomNumber.includes(number)) {
+        randomNumber.push(number);
+      }
+    }
+    return randomNumber;
   }
 
   async play() {
