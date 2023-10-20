8c3e847e69979bfc66d471532761f3a75aff709b
bmj0053 <bmj05378@gmail.com>
2023-10-21 00:21:42 +0900

    사용법 익히는 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
+console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]));
+
 class App {
-  async play() {}
+  async play() {
+    // Console.readLine('닉네임을 입력해주세요.', (answer) => {
+    //   console.log(`닉네임: ${answer}`);
+    // });
+    Console.print('안녕하세요.');
+
+    // async function getUsername() {
+    //   try {
+    //     const username = await Console.readLineAsync('닉네임을 입력해주세요.');
+    //   } catch (error) {
+    //     // reject 되는 경우
+    //   }
+    // }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
