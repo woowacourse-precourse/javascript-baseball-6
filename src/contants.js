74301269ae0c378ea38e0e105ead5cef657a0e1f
Ryong-E <think8867@gmail.com>
2023-10-20 20:18:31 +0900

    fix: 누락된 메세지 추가 후 개행

--- src/contants.js
+++ src/contants.js
@@ -12,7 +12,7 @@ export const OUTPUT_MASSAGE = Object.freeze({
   RESTART_GAME_MASSAGE: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. \n',
   START_GAME: '숫자 야구 게임을 시작합니다.',
   END_GAME: '숫자 야구 게임을 종료합니다.',
-  CORRECT_ANSWER: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  CORRECT_ANSWER: '3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료',
   ENTER_NUMBER: '숫자를 입력해주세요 : ',
 });
 
