170ade98038335ff053bfedcea50a70e2a2ff9fd
MinGu Jeong <mingu830@gmail.com>
2023-10-21 00:33:59 +0900

    feat: 사용자 수 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
 }
 
+function getUserNumber() {
+  console.log(
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      console.log(`입력한 숫자 : ${input}`);
+    })
+  );
+}
+
 export default App;
+
+console.log(MissionUtils.Console.print("숫자 야구 게임을 시작합니다."));
+getUserNumber();
