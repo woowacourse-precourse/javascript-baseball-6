5c715b3bd0a4b4d801f67278646ce803f17fe38b
Jules <dali1999@naver.com>
2023-10-20 18:27:00 +0900

    fix: [ERROR] 메세지 포함

--- src/handleAnswer/handleUserInput.js
+++ src/handleAnswer/handleUserInput.js
@@ -1,11 +1,10 @@
-const validLength = (userInput) => userInput.length !== 3;
-const validType = (userInput) => isNaN(userInput);
-
 //유저 인풋 확인
 export default function handleUserInput(userInput) {
   //valid 확인
+  const validLength = (userInput) => userInput.length !== 3;
+  const validType = (userInput) => isNaN(userInput);
   if (validLength(userInput) || validType(userInput)) {
-    throw new Error("잘못된 입력입니다.");
+    throw new Error("[ERROR] 잘못된 입력입니다.");
   }
 
   return userInput;
