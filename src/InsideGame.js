512a2a4da154daf76c6599a6e03ea251806dad48
Hyurim <kyo2301@naver.com>
2023-10-20 22:42:17 +0900

    입력 오류 수정

--- src/InsideGame.js
+++ src/InsideGame.js
@@ -9,7 +9,7 @@ class InsideGame {
         computer.push(number);
       }
     }
-    return computer;
+    return computer.join('');
   }
 
   vaild(answer){
@@ -34,27 +34,34 @@ class InsideGame {
     for (let i = 0; i < 3; i++) {
       if (computerNumber[i] === userNumber[i]) strike++;
     }
+
+    return strike;
   }
   ballCheck(computerNumber, userNumber) {
     let ball = 0;
 
     for (let j = 0; j < 3; j++) {
-      if (computerNumber[j] === userNumber[j]) ball++;
+      if (computerNumber[j] !== userNumber[j] && computerNumber.includes(userNumber[j])){
+        ball++;
+      } 
     }
+    return ball;
   }
 
   outputHint(ball, strike) {
     if (strike === 3) {
       return "3스트라이크";
-    } else if (strike === 0 && ball === 0) {
-      return "낫싱";
-    } else if (strike === 0 && ball !== 0) {
+    } 
+    if (strike === 0 && ball !== 0) {
       return `${ball}볼`;
-    } else if (strike !== 0 && ball === 0) {
+    }
+    if (strike !== 0 && ball === 0) {
       return `${strike}스트라이크`;
-    } else {
+    }
+    if (strike !== 0 && ball !== 0){
       return `${ball}볼 ${strike}스트라이크`;
     }
+    return "낫싱";
   }
 }
 export default InsideGame;
--- src/test_folder/save.js
+++ src/test_folder/save.js
@@ -7,37 +7,38 @@ class App extends InsideGame {
     this.start();
   }
   // 시작과 동시에 3자리의 숫자 랜덤 생성.
-  start() {
+  async start() {
     this.randoms = super.randomNumber();
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    console.log(this.randoms);
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");  
   }
   // 사용자가 숫자를 입력하는 구간.
-  // 입력 값에 따라 에러가 발생할지 계속 진행할 지 정해짐
-  play() {
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+  // 입력 값에 따라 에러가 발생할지 계속 진행할 지 정해짐 
+ async play() {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
       try {
-        this.inputVaild(answer);
+        const result = super.vaild(answer);
+        
+        if (result === "Normal Value"){
+          this.ballAndStrike(answer);
+        }
       } catch (error) {
         MissionUtils.Console.print(`오류 발생: ${error.message}`);
+        
       }
     });
-  }
-
-  inputVaild(answer) {
-    const result = super.vaild(answer);
-
-    if (result === "Normal Value") {
-      this.ballAndStrike(answer);
-    }
   }
   ballAndStrike(answer) {
     const ball = super.ballCheck(this.randoms, answer);
     const strike = super.strikeCheck(this.randoms, answer);
 
+    this.print(ball,strike);
+    // MissionUtils.Console.print(super.outputHint(ball, strike));
 
-    MissionUtils.Console.print(super.outputHint(ball, strike));
     return this.play();
+  }
+
+  print(ball, strike) {
+    MissionUtils.Console.print(super.outputHint(ball, strike));
   }
 }
 
