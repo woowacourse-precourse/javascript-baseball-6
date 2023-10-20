43de8f4d823666c9618c700db6135cc7c10bdbce
Nevacat <nevacattery217@naver.com>
2023-10-20 17:16:09 +0900

    feat: 랜덤 숫자 생성

---
+++ src/Util.js
@@ -0,0 +1,15 @@
+import { Random } from '@woowacourse/mission-utils';
+
+class GameUtil {
+  generateRandomNumber() {
+    const RANDOM_NUMBER = [];
+    while (RANDOM_NUMBER.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!RANDOM_NUMBER.includes(number)) {
+        RANDOM_NUMBER.push(number);
+      }
+    }
+  }
+}
+
+export default GameUtil;
