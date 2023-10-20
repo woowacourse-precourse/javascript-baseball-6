754fbf51286c3f860782e01e5bc140ec5037fa3c
WOOJINCHO98 <steamedbunwith@gmail.com>
2023-10-20 10:09:13 +0900

    feat : 게임 초기화

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+// 컴퓨터가 3개의 난수 배열을 생성하는 함수
+const getComputerNumber = () => {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer;
+};
+
 class App {
   async play() {
-    MissionUtils.Console.readLine("닉네임을 입력해주세요.", (answer) => {
-      console.log(`닉네임: ${answer}`);
-    });
-    console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]));
-
-    const computer = [];
-    while (computer.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer.push(number);
-      }
-    }
-    MissionUtils.Console.print(computer);
+    MissionUtils.Console.print(getComputerNumber());
   }
 }
 
+MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 const app = new App();
 app.play();
 
