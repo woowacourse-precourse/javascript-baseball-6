4981e087c303ab9c6b16289223afa2d32ca4e018
lurgi <wahoo9040@naver.com>
2023-10-20 11:32:23 +0900

    refactor(App,BaseBallGame): SRP 적용

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import BaseballGame from "./BaseballGame";
 class App {
   async play() {
     const baseballGame = new BaseballGame();
-    let isEnd = false;
 
-    while (!isEnd) {
-      await baseballGame.inputUserNumber();
-      baseballGame.printResult();
-      isEnd = baseballGame.IsGameEnd();
-    }
+    await this.startPlay(baseballGame);
+  }
 
-    const IS_RETRY = await baseballGame.endGame();
-    if (IS_RETRY) this.play();
+  async startPlay(game) {
+    game.startGame();
+
+    await this.startUser(game);
+
+    const IS_RETRY = await game.endGame();
+    if (IS_RETRY) await this.startPlay(game);
+  }
+
+  async startUser(game) {
+    await game.startUserInput();
+    const IS_REINPUT = game.startUserResult();
+    if (IS_REINPUT) await this.startUser(game);
   }
 }
 
--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -6,56 +6,65 @@ class BaseballGame {
   #computerNumbers;
   /**@type {number[]} */
   #userNumbers;
-  #gameEnd = false;
 
   constructor() {
-    this.gameStart();
+    console.log("숫자 야구 게임을 시작합니다.");
   }
-  gameStart() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    this.getRandomNumbers();
+
+  startGame() {
+    this.#computerNumbers = this.getRandomNumbers();
   }
+
   getRandomNumbers() {
     const returnNumbers = [];
 
     while (returnNumbers.length !== this.#GAME_NUMBER_LEN) {
-      const RANDOME_NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!returnNumbers.includes(RANDOME_NUMBER))
-        returnNumbers.push(RANDOME_NUMBER);
+      const RANDOM_NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!returnNumbers.includes(RANDOM_NUMBER))
+        returnNumbers.push(RANDOM_NUMBER);
     }
 
-    this.#computerNumbers = returnNumbers;
+    return returnNumbers;
   }
 
-  async inputUserNumber() {
-    const input = (
-      await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ")
-    )
-      .split("")
-      .map(Number);
-    this.validUserGameNumber(input);
-    this.#userNumbers = input;
+  async startUserInput() {
+    const USER_INPUT = await this.getUserInput();
+    const INPUT_NUMBERS = USER_INPUT.split("").map(Number);
+    this.validUserGameNumber(INPUT_NUMBERS);
+    this.#userNumbers = INPUT_NUMBERS;
+  }
+
+  /**
+   *
+   * @returns {string}
+   */
+  async getUserInput() {
+    const input = await MissionUtils.Console.readLineAsync(
+      "숫자를 입력해주세요 : "
+    );
+    return input;
   }
 
   validUserGameNumber(input) {
     if (input.length !== this.#GAME_NUMBER_LEN)
-      throw new Error("서로 다른 3자리 수를 입력해주세요.");
+      throw new Error("[ERROR] 입력한 값은 3자리가 아닙니다.");
     if (new Set(input).size !== this.#GAME_NUMBER_LEN)
-      throw new Error("서로 다른 3자리 수를 입력해주세요.");
+      throw new Error("[ERROR] 입력한 값에 중복이 있습니다.");
     for (const value of input) {
-      if (isNaN(value)) throw new Error("서로 다른 3자리 수를 입력해주세요.");
+      if (isNaN(value))
+        throw new Error("[ERROR] 입력한 값에 숫자가 아닌 값이 있습니다.");
     }
   }
 
-  printResult() {
+  startUserResult() {
     const { strike, ball } = this.getStrikeAndBall();
 
     this.printStrikeAndBall({ strike, ball });
 
     if (strike === 3) {
-      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-      this.#gameEnd = true;
+      return false;
     }
+    return true;
   }
 
   getStrikeAndBall() {
@@ -75,14 +84,10 @@ class BaseballGame {
   }
 
   printStrikeAndBall({ strike, ball }) {
-    if (strike === 0 && ball === 0) MissionUtils.Console.print(`낫싱`);
+    if (strike === 0 && ball === 0) MissionUtils.Console.print("낫싱");
     else if (strike === 0) MissionUtils.Console.print(`${ball}볼`);
     else if (ball === 0) MissionUtils.Console.print(`${strike}스트라이크`);
-    MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
-  }
-
-  IsGameEnd() {
-    return this.#gameEnd;
+    else MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
   }
 
   /**
@@ -91,18 +96,21 @@ class BaseballGame {
    */
   async endGame() {
     const input = await MissionUtils.Console.readLineAsync(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. \n"
+      "3개의 숫자를 모두 맞히셨습니다! 게임 종료 \n 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. \n"
     );
 
     this.validGameEndInput(input);
 
     if (input === "1") return true;
-    if (input === "2") return false;
+    if (input === "2") {
+      MissionUtils.Console.print("게임 종료");
+      return false;
+    }
   }
 
   validGameEndInput(input) {
     if (input !== "1" && input !== "2")
-      throw new Error("잘못 입력 하셨습니다.");
+      throw new Error("[ERROR] 잘못 입력 하셨습니다.");
   }
 }
 
