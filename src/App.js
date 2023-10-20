4a89f7c457ccb0795d51ac546f0db0919269b6a8
sohyun <kshyun0215@gmail.com>
2023-10-20 19:04:43 +0900

    feat: 값 입력받고 입력값 검증하기

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import { Random, Console } from '@woowacourse/mission-utils';
 
 class App {
   constructor() {
-    this.randomNumber = null;
+    this.randomNumArr = null;
+  }
+
+  startGame() {
+    this.createRandomNumber();
+    this.getUserNumber();
   }
 
   createRandomNumber() {
@@ -13,12 +18,34 @@ class App {
         computer.push(number);
       }
     }
-    this.randomNumber = computer;
+    this.randomNumArr = computer;
+  }
+
+  getUserNumber() {
+    Console.readLineAsync('숫자를 입력해주세요 : ')
+      .then((input) => {
+        if (!this.isInputValid(input)) {
+          throw new Error('[Error] 숫자가 잘못된 형식입니다.');
+        }
+      })
+      .catch((error) => {
+        Console.print(error.message);
+      });
+  }
+
+  isInputValid(userInput) {
+    // 1~9 사이의 숫자로 이루어진 3자리 숫자인지 확인 하는 정규 표현식
+    const checkOnlyNumRegExp = /^[1-9]{3}$/;
+
+    // 중복되는 숫자가 있는지 확인
+    const numSet = new Set(userInput);
+
+    return numSet.size === 3 && checkOnlyNumRegExp.test(userInput);
   }
 
   async play() {
-    this.createRandomNumber();
     Console.print('숫자 야구 게임을 시작합니다.');
+    this.startGame();
   }
 }
 
