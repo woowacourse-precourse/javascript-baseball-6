393198dc5384ab27cc547a7cfa18557998158320
parkjeehoon <gkstj8300@naver.com>
2023-10-21 01:48:55 +0900

    feat : 사용자의 값 입출력 및 예외사항 로직 추가

---
+++ src/data/inputUserNumber.js
@@ -0,0 +1,28 @@
+/* 사용자의 수 입력과 입력값 확인을 위한 JS */
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+export default async function inputUserNumber(){
+    const number = MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ')
+    .then((res) => {
+        const valudateNumber = inputNumberValidate(res);
+        return valudateNumber;
+    })
+    .catch((error) => {
+        MissionUtils.Console.print(error);
+    });
+    
+    return number;
+}
+
+function inputNumberValidate(inputNumber){
+    const numberLength = inputNumber.length;
+    const numberSize = new Set(inputNumber.split("")).size;
+    const test = isNaN(inputNumber);
+
+    if(numberLength === 3 && numberSize === 3 && test == false){
+        return inputNumber;
+    } else {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+    
+}
