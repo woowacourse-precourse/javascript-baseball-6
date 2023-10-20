896e104a9612733174817c89626fb18fecb3de3c
Ryong-E <think8867@gmail.com>
2023-10-20 20:29:19 +0900

    feature: controller 클래스 생성
    docs/READEME.md에 작성된 기능 구현 완료

---
+++ src/controller/controller.js
@@ -0,0 +1,53 @@
+import { OUTPUT_MASSAGE } from '../contants.js';
+import Computer from '../model/computer.js';
+import scoreConversion from '../util/score-conversion.js';
+import { guessAnswerValidate, retryAnswerValidate } from '../util/validation.js';
+import View from '../view/view.js';
+
+class Controller {
+  #firstEnter = 0;
+
+  constructor() {
+    this.computer = new Computer();
+  }
+
+  async play() {
+    if (this.#firstEnter <= 0) {
+      View.print(OUTPUT_MASSAGE.START_GAME);
+      this.#firstEnter += 1;
+    }
+    await this.#askAnswer();
+  }
+
+  async #askAnswer() {
+    const answer = await View.input(OUTPUT_MASSAGE.ENTER_NUMBER);
+    guessAnswerValidate(answer);
+    await this.#checkAnswer(answer);
+  }
+
+  async #checkAnswer(answer) {
+    const score = this.computer.compareNumber(answer);
+    const result = scoreConversion(score);
+    if (result === OUTPUT_MASSAGE.CORRECT_ANSWER) {
+      View.print(result);
+      this.#askRetry();
+    } else {
+      View.print(result);
+      await this.#askAnswer();
+    }
+  }
+
+  async #askRetry() {
+    const answer = await View.input(OUTPUT_MASSAGE.RESTART_GAME_MASSAGE);
+    retryAnswerValidate(answer);
+    if (answer === '1') {
+      this.computer.setNumber();
+      this.play();
+    }
+    if (answer === '2') {
+      View.print('게임 종료');
+    }
+  }
+}
+
+export default Controller;
