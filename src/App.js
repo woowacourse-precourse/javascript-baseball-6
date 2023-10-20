10f36c31087f17e7a297c61ac74cd80ef00b722d
seokho10007 <sukho1007@naver.com>
2023-10-20 18:09:22 +0900

    ♻️ refactor: 기능별 로직 분리
    - 입력, 출력, 종료 별로 로직 분리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { MissionUtils, Console } from "@woowacourse/mission-utils";
+import { printMessage, pickNumberInRange, readLineAsync } from "./utils.js";
 import {
 	LOG,
 	MAX_INPUT_LENGTH,
@@ -7,6 +7,7 @@ import {
 	MAX_RANDOM_NUMBER,
 	END_NUMBER,
 	RESTART_NUMBER,
+	ERROR_MESSAGE,
 } from "./constants.js";
 
 class App {
@@ -17,57 +18,13 @@ class App {
 	isPlaying;
 
 	/**
-	 * @type {number}
+	 * @type {number[]}
 	 * @description 랜덤한 3개의 숫자
 	 */
-	randomNumber;
+	randomNumbers;
 
 	constructor() {
 		this.init();
-	}
-
-	/**
-	 * @description 게임을 시작하는 메서드
-	 * - 사용자의 입력을 잗아 유효성을 확인하고 스트라이크와 볼의 개수를 출력합니다.
-	 * - 스트라이크가 3개일 경우, 게임을 재시작할지 확인합니다.
-	 * - 에러가 발생할 경우, 에러 메시지를 출력합니다.
-	 */
-	async play() {
-		Console.print(LOG.START);
-
-		try {
-			while (this.isPlaying) {
-				const input = (await Console.readLineAsync(LOG.INPUT_NUMBER)).trim();
-
-				const isValid = this.validateInput(input);
-
-				if (!isValid) {
-					throw new Error(LOG.ERROR);
-				}
-
-				const { strike, ball } = this.calculateStrikeBall(input);
-
-				const message = this.makeMessage(strike, ball);
-
-				Console.print(message);
-
-				if (strike !== MAX_STRIKE_COUNT) {
-					continue;
-				}
-
-				Console.print(LOG.CORRECT);
-
-				const isRestart = await this.confirmRestart();
-
-				if (isRestart) {
-					this.init();
-				} else {
-					this.isPlaying = false;
-				}
-			}
-		} catch (e) {
-			throw new Error(e.message);
-		}
 	}
 
 	/**
@@ -77,7 +34,79 @@ class App {
 	 */
 	init() {
 		this.isPlaying = true;
-		this.randomNumber = this.generateRandomNumbers();
+		this.randomNumbers = this.generateRandomNumbers();
+	}
+
+	/**
+	 * @description 게임을 시작하는 메서드
+	 * - 사용자의 입력을 잗아 유효성을 확인하고 스트라이크와 볼의 개수를 출력합니다.
+	 * - 스트라이크가 3개일 경우, 게임을 재시작할지 확인합니다.
+	 * - 에러가 발생할 경우, 에러 메시지를 출력합니다.
+	 */
+	async play() {
+		printMessage(LOG.START);
+
+		try {
+			while (this.isPlaying) {
+				const numbers = await this.getUserNumbers();
+
+				const { strike, ball } = this.calculateStrikeBall(numbers);
+
+				const message = this.makeMessage(strike, ball);
+
+				printMessage(message);
+
+				if (strike !== MAX_STRIKE_COUNT) {
+					continue;
+				}
+
+				printMessage(LOG.CORRECT);
+
+				await this.confirmRestart();
+			}
+		} catch (e) {
+			this.isPlaying = false;
+			console.log(e.message, "@@@@@@@@@");
+			throw new Error(`${ERROR_MESSAGE.HEADER} ${e.message}`);
+		}
+	}
+
+	/**
+	 * @description 사용자의 입력을 받는 메서드
+	 * - 사용자의 입력을 받고 유효성을 검사합니다.
+	 * - 유효하지 않은 입력일 경우, 에러 메시지를 출력합니다.
+	 * @returns {number[]} 사용자가 입력한 3자리 숫자
+	 */
+	async getUserNumbers() {
+		const input = (await readLineAsync(LOG.INPUT_NUMBER)).trim();
+
+		this.validateInput(input);
+
+		return input.split("").map(Number);
+	}
+
+	/**
+	 * @description 게임을 재시작할지 확인하는 메서드
+	 * - 입력받은 숫자가 1이나 2가 아닐 경우, 에러 메시지를 출력합니다.
+	 * - 입력받은 숫자가 1일 경우, 게임을 초기화합니다.
+	 * - 입력받은 숫자가 2일 경우, 게임을 종료합니다.
+	 */
+	async confirmRestart() {
+		const input = await readLineAsync(LOG.RESTART);
+
+		const restartNumber = Number(input);
+
+		if (restartNumber !== RESTART_NUMBER && restartNumber !== END_NUMBER) {
+			throw new Error(ERROR_MESSAGE.NOT_RESTART_OR_END);
+		}
+
+		const isRestart = restartNumber === RESTART_NUMBER;
+
+		if (isRestart) {
+			this.init();
+		} else {
+			this.isPlaying = false;
+		}
 	}
 
 	/**
@@ -86,19 +115,19 @@ class App {
 	 * @returns {string}
 	 */
 	generateRandomNumbers() {
-		let randomNumber = "";
+		const randomNumbers = [];
 
-		while (randomNumber.length < MAX_INPUT_LENGTH) {
-			const number = MissionUtils.Random.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+		while (randomNumbers.length < MAX_INPUT_LENGTH) {
+			const number = pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
 
-			if (randomNumber.includes(number)) {
+			if (randomNumbers.includes(number)) {
 				continue;
 			}
 
-			randomNumber += number;
+			randomNumbers.push(number);
 		}
 
-		return randomNumber;
+		return randomNumbers;
 	}
 
 	/**
@@ -111,38 +140,47 @@ class App {
 	validateInput(input) {
 		const set = new Set(input.split(""));
 
-		const isValid = !(
-			isNaN(Number(input)) ||
-			input.length !== MAX_INPUT_LENGTH ||
-			set.size !== MAX_INPUT_LENGTH ||
-			set.has("0")
-		);
+		if (isNaN(Number(input))) {
+			throw new Error(ERROR_MESSAGE.NOT_NUMBER);
+		}
 
-		return isValid;
+		if (input.length !== MAX_INPUT_LENGTH) {
+			throw new Error(ERROR_MESSAGE.NOT_THREE_DIGITS);
+		}
+
+		if (set.size !== MAX_INPUT_LENGTH) {
+			throw new Error(ERROR_MESSAGE.NOT_UNIQUE);
+		}
+
+		if (set.has("0")) {
+			throw new Error(ERROR_MESSAGE.NOT_RANGE);
+		}
+
+		return true;
 	}
 
 	/**
-	 * @param {string} input - 사용자가 입력한 데이터
+	 * @param {number[]} input - 사용자가 입력한 데이터
 	 * @description 스트라이크와 볼의 개수를 계산하는 메서드
 	 * - 스트라이크는 자릿수와 값이 모두 같을 경우, 볼은 자릿수는 다르지만 값이 같을 경우입니다.
 	 * @returns {{strike: number, ball: number}}
 	 */
 	calculateStrikeBall(input) {
-		const randomNumberToString = String(this.randomNumber);
+		const randomNumbers = this.randomNumbers;
 
 		let strike = 0;
 		let ball = 0;
 
 		for (let i = 0; i < MAX_INPUT_LENGTH; i++) {
 			const inputNumber = input[i];
-			const randomNumber = randomNumberToString[i];
+			const randomNumber = randomNumbers[i];
 
 			if (inputNumber === randomNumber) {
 				strike++;
 				continue;
 			}
 
-			if (randomNumberToString.includes(inputNumber)) {
+			if (randomNumbers.includes(inputNumber)) {
 				ball++;
 			}
 		}
@@ -173,24 +211,6 @@ class App {
 		}
 
 		return messages.join(" ");
-	}
-
-	/**
-	 * @description 게임을 재시작할지 확인하는 메서드
-	 * - 입력받은 숫자가 1이나 2가 아닐 경우, 에러 메시지를 출력합니다.
-	 * - 입력받은 숫자가 1일 경우, true를 반환합니다.
-	 * @returns {boolean}
-	 */
-	async confirmRestart() {
-		const input = await Console.readLineAsync(LOG.RESTART);
-
-		const restartNumber = Number(input);
-
-		if (restartNumber !== RESTART_NUMBER && restartNumber !== END_NUMBER) {
-			throw new Error(LOG.ERROR);
-		}
-
-		return restartNumber === RESTART_NUMBER;
 	}
 }
 
