562fdf3cf33bd13ecc6ed9a35a9a6fc57f0c3b95
WOOJINCHO98 <steamedbunwith@gmail.com>
2023-10-20 10:12:51 +0900

    feat : 플레이어로부터의 입력

--- src/App.js
+++ src/App.js
@@ -12,9 +12,25 @@ const getComputerNumber = () => {
   return computer;
 };
 
+// 플레이어에게 숫자를 입력받는 함수
+const readFromPlayer = () => {
+  return new Promise((resolve) => {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+      resolve(readToList(answer));
+    });
+  });
+};
+
+// 입력받은 숫자를 리스트 형식으로 반환하는 함수
+const readToList = (answer) => {
+  const arr = [...answer].map(Number);
+  return arr;
+};
+
 class App {
   async play() {
     MissionUtils.Console.print(getComputerNumber());
+    MissionUtils.Console.print(await readFromPlayer());
   }
 }
 
