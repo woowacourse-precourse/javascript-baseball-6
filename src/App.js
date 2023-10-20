1464623561b49d44250d45e8a4f274623b59d75c
ohgus <anakin1108@naver.com>
2023-10-20 12:33:07 +0900

    랜덤 숫자 생성 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.random = MissionUtils.Random;
+    this.console = MissionUtils.Console;
+  }
+
+  async play() {
+    this.console.print("숫자 야구 게임을 시작합니다.");
+    const answer = this.makeRandomNum();
+    // this.console.print(answer);
+  }
+
+  makeRandomNum() {
+    const nums = [];
+    while (nums.length < 3) {
+      const num = this.random.pickNumberInRange(1, 9);
+      if (!nums.includes(num)) nums.push(num);
+    }
+    return nums;
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
