41b59f45d3a1d2c8f179f773e4f4cc0bef0e4402
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-10-20 21:16:38 +0900

    feat: 채점함수 로직

--- src/App.js
+++ src/App.js
@@ -29,16 +29,30 @@ function checkError(number) {
 let SCORE = {
   볼: 0,
   스트라이크: 0,
-  낫싱: 0,
-  성공: 0,
 };
 function review(answer, number) {
   //컴퓨터의 숫자를 순회하여 사용자의 숫자와 비교한다
   //findindex 로 스트라이크 갯수 변수와 볼 변수 낫싱 변수를 체크한다.
   //세변수 모두 0이라면 3개의 숫자를 모두 맞히셨습니다! 게임 종료 출력하고
   console.log(answer, number);
-  score.성공 = 1;
-  return;
+  for (let i = 0; i < answer.length; i++) {
+    let index = answer.findIndex((el) => el === number[i]);
+    if (index === i) {
+      SCORE.스트라이크++;
+    }
+    if (index > 0 && index !== i) {
+      SCORE.볼++;
+    }
+  }
+  // if (SCORE.볼 === 0 && SCORE.스트라이크 === 0) {
+  //   SCORE.낫싱++;
+  //   return;
+  // }
+  // if (SCORE.스트라이크 === 3) {
+  //   score.성공 = 1;
+  //   return;
+  // }
+  return SCORE;
 }
 class App {
   constructor() {
@@ -47,8 +61,6 @@ class App {
   async play() {
     //게임 시작문구를 출력한다.
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    //컴퓨터는 1~9까지 서로다른 임의수 3개를 선택
-    const answer = makeAnswer();
 
     //사용자에게 서로다른 숫자 3개를 입력받는다. 숫자를 입력해주세요 :
     let number = getNumber();
@@ -63,6 +75,8 @@ class App {
         console.log("검증통과");
         review(answer, num);
       });
+    //컴퓨터는 1~9까지 서로다른 임의수 3개를 선택
+    const answer = makeAnswer();
 
     // MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     // 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. 것을 출력한다.
