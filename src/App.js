c0dfcd7d678c7c27a21fedbf17c2663f96203b58
suyeon <moa0823@naver.com>
2023-10-20 15:14:53 +0900

    중복없이 랜덤 숫자 세자리 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  constructor() {
+    this.number = this.number();
+  }
+
+  number() {
+    const RANDOM_NUMBER = new Set();
+    while (RANDOM_NUMBER.size !== 3) {
+      RANDOM_NUMBER.add(MissionUtils.Random.pickNumberInRange(1, 9));
+    }
+
+    return RANDOM_NUMBER;
+  }
+
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    // Console.print(this.number);
   }
 }
 
