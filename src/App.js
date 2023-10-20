7b13aa390d228ceab4c0657b857478adc829e2cc
novice0840 <magnolia0840@gmail.com>
2023-10-21 01:01:15 +0900

    Feat: 결과값 출력 완료
    모든 경우의 수의 결과값을 알맞게 출력하도록 작성 완료

--- src/App.js
+++ src/App.js
@@ -30,35 +30,37 @@ class App {
     if (strike !== 0) {
       result += `${strike}스트라이크 `;
     }
+    if (ball === 0 && strike === 0) {
+      result = "낫싱";
+    }
     Console.print(result);
     return strike === 3 ? true : false;
   }
 
   async play() {
     const answerNumber = this.createAnswerNumber();
-    console.log(answerNumber);
     Console.print("숫자 야구 게임을 시작합니다.");
     let inputNumber;
-    try {
-      while (true) {
-        inputNumber = await Console.readLineAsync("숫자를 입력해주세요 : ");
-        if (isNaN(inputNumber) || inputNumber.length !== this.LENGTH_OF_NUMBER) {
-          throw new Error();
-        }
-        const answer = this.checkAnswer(answerNumber, inputNumber);
-        if (answer) {
-          Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-          break;
-        }
+    while (true) {
+      inputNumber = await Console.readLineAsync("숫자를 입력해주세요 : ");
+      if (isNaN(inputNumber) || inputNumber.length !== this.LENGTH_OF_NUMBER) {
+        throw new Error("[ERROR]");
       }
-    } catch (error) {
-      console.log(error);
-      // Console.print("[ERROR] 숫자가 잘못된 형식입니다.");
+      const answer = this.checkAnswer(answerNumber, inputNumber);
+      if (answer) {
+        Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        break;
+      }
     }
-    await Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    const restart = await Console.readLineAsync(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    );
+    if (restart === "1") {
+      this.play();
+    } else if (restart === "2") {
+    } else {
+      throw new Error();
+    }
   }
 }
 export default App;
-
-const app = new App();
-app.play();
