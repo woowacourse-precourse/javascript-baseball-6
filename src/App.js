6a384653bde3f032b7f1e53f6ad6b7f600475372
aaminha <dks4857@gmail.com>
2023-10-21 01:54:21 +0900

    feat: 게임 시작 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+const NUMBER_LENGTH = 3;
+
+function start() {
+  Console.print('숫자 야구 게임을 시작합니다.');
+
+  // 컴퓨터의 랜덤값 생성
+  const computer = [];
+  while(computer.length < NUMBER_LENGTH) {
+    const number = Random.pickNumberInRange(1, 9);
+    if(!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer;
+}
 class App {
-  async play() {}
+  async play() {
+    while(true) {
+      const computer = start();
+    }
+  }
 }
 
 export default App;
