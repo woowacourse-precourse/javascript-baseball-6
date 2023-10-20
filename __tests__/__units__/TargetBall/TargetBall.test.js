da09b0af88c685abdf4a546d88d016da20a4ce9f
cobocho <rlaalsrb1111@naver.com>
2023-10-20 13:52:04 +0900

    Test: `TargetBall` 테스트 코드 수정

--- __tests__/__units__/TargetBall/TargetBall.test.js
+++ __tests__/__units__/TargetBall/TargetBall.test.js
@@ -11,7 +11,7 @@ describe('TargetBall 테스트', () => {
     { input: 9 }
   ])('입력받은 숫자에 따라 number 필드에 숫자를 가진다. (input: $input) ', ({ input }) => {
     const targetBall = new TargetBall(input);
-    expect(targetBall.number).toBeTruthy(input);
+    expect(targetBall.number).toBe(input);
   });
 
   it.each([
@@ -21,6 +21,8 @@ describe('TargetBall 테스트', () => {
   ])('정적 valueOf를 통해 미리 만들어놓은 인스턴스를 반환한다.', ({ input }) => {
     const targetBall = TargetBall.valueOf(input);
     const sameTargetBall = TargetBall.valueOf(input);
+    expect(targetBall.number).toBe(input);
+    expect(sameTargetBall.number).toBe(input);
     expect(targetBall).toBe(sameTargetBall);
   });
 });
