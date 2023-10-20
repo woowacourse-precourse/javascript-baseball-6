c280942dbacc310bffe785fb4cc9ea07a665ab57
chiman <rlaclghks123@naver.com>
2023-10-20 14:26:30 +0900

    feat: Refree 객체 생성
    사용자와 상대방의 번호를 받아 볼, 스트라이크를 판단할 객체

---
+++ src/Refree.js
@@ -0,0 +1,10 @@
+class Refree {
+  #playerNumber;
+  #opponentNumber;
+
+  constructor(playerNumber, opponentNumber) {
+    this.#playerNumber = playerNumber;
+    this.#opponentNumber = opponentNumber;
+  }
+}
+export default Refree;
