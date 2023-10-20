64e985483af0ee26739317bfc779394c6cf7615e
bada308 <203190@jnu.ac.kr>
2023-10-20 22:54:04 +0900

    feat: 스트라이크/볼/낫싱 출력 구현 - 같은 수가 같은 자리에 있으면 스트라이크 - 같은 수가 다른 자리에 있으면 볼 - 같은 수가 전혀 없으면 낫싱

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
+  userNumber = "";
+
   async play() {
     const answer = [];
     while (answer.length < 3) {
@@ -9,7 +11,43 @@ class App {
         answer.push(number);
       }
     }
+
+    let isCorrect = false;
+
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    while (!isCorrect) {
+      try {
+        const input = await this.getUserNumber();
+        this.userNumber = input;
+        if (input.includes("[ERROR]")) {
+          return Promise.reject(new Error(input));
+        }
+      } catch (error) {}
+      let strike = 0;
+      let ball = 0;
+
+      this.userNumber.split("").forEach((number, index) => {
+        if (answer[index] === parseInt(number)) strike++;
+        else if (answer.includes(parseInt(number))) ball++;
+      });
+
+      if (strike === 3) {
+        isCorrect = true;
+        MissionUtils.Console.print("3스트라이크");
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        break;
+      }
+
+      if (ball === 0 && strike === 0) MissionUtils.Console.print("낫싱");
+      if (ball === 0 && strike !== 0)
+        MissionUtils.Console.print(`${strike}스트라이크`);
+      if (ball !== 0 && strike === 0) MissionUtils.Console.print(`${ball}볼`);
+      if (ball !== 0 && strike !== 0)
+        MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+    }
   }
+
   async getUserNumber() {
     try {
       const number = await MissionUtils.Console.readLineAsync(
