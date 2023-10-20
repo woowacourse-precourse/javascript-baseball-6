ba35319aa360b816a76068b6fd2e989bb9ecb9e9
yu-heejin <jkgt0521@naver.com>
2023-10-20 19:04:47 +0900

    feat: 컴퓨터 랜덤 숫자 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
+    const computerNumber = this.getComputerNumber();
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     const inputNumber = MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
-    
+  }
+
+  getComputerNumber() {
+    const computerNumber = new Set();
+
+    while (computerNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      computerNumber.add(number);
+    }
+
+    return computerNumber;
   }
 }
 
