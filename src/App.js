93d71a17950c71712db835367f59c89cd2b604bb
ooherin <555ohr@naver.com>
2023-10-20 23:36:25 +0900

    feat: 테스트 진행 중

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import gameStart from "./game/start";
 const { Console } = MissionUtils;
+
 class App {
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
--- src/game/start.js
+++ src/game/start.js
@@ -1,5 +1,5 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-
+import isValidNumber from "./validation.js";
 const { Random, Console } = MissionUtils;
 
 //게임 시작하기
@@ -16,12 +16,16 @@ const makeRandomNumber = () => {
       computer.push(newNumber);
     }
   }
+  console.log(computer);
   tryNumbers(computer);
 };
 
 //숫자 입력하기
 const tryNumbers = async (computer) => {
   let user = await Console.readLineAsync("숫자를 입력해 주세요 : ");
+  if (!isValidNumber(user)) {
+    return Console.print("[ERROR] 3자리의 숫자가 아닙니다.");
+  }
   user = [...user].map((num) => parseInt(num));
   checkNumber(computer, user);
 };
@@ -40,7 +44,6 @@ const checkNumber = (computer, user) => {
       result.ball++;
     }
   });
-
   if (result.strike === 3) {
     askMoreGame();
   } else {
@@ -60,25 +63,26 @@ const makeText = (result) => {
     answer.push(`${strike}스트라이크`);
   }
   if (ball == 0 && strike == 0) {
-    answer.pop("낫싱");
+    answer.push("낫싱");
   }
   Console.print(answer.join(" "));
 };
 
 //한번 더 게임할건지 묻기
 const askMoreGame = async () => {
-  Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  Console.print("3스트라이크");
   const endChoice = await Console.readLineAsync(
-    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+    "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
   );
   if (endChoice === "1") {
     gameStart();
   } else if (endChoice === "2") {
     return;
   } else {
-    throw new Error();
+    throw Error("[ERROR] 1,2 중에 하나를 골라주세요");
   }
 };
 
 gameStart();
+
 export default gameStart;
