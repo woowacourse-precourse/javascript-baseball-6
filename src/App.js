0170e55ce4e2da92eff9a25d3b2a2ea524efae17
고세종 <fulfilled7418@gmail.com>
2023-10-20 14:55:58 +0900

    Feat: 구현 랜덤 3자리 숫자생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import {Random} from '@woowacourse/mission-utils';
+
 class App {
   async play() {}
 }
 
+const makeRandomNumber = () => {
+  const randomNumber = [];
+  while (randomNumber.length < 3) {
+    let number = Random.pickNumberInRange(1, 9);
+    if (!randomNumber.includes(number)) {
+      randomNumber.push(number);
+    }
+  }
+
+  return randomNumber;
+}
 export default App;
