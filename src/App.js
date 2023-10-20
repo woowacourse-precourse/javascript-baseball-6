7b55f23a40309480cd083e8432ea43d2318e0063
조민기 <58856846+Nail0606@users.noreply.github.com>
2023-10-20 22:00:36 +0900

    Fix : 입력 값이 문자열일 경우 오류 발생하게 수정

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    const computerNumber = this.createComNum();
-    const inputNumber = this.input();
+    const answerNumber = this.createAnswerNumber();
+    const inputNumber = await this.createInputNumber();
+    console.log(answerNumber);
+    this.compareNumber(answerNumber, inputNumber);
   }
 
   //사용자가 입력한 숫자에 대해 유효성 검사를 진행하고 반환하는 메서드
-  async input() {
+  async createInputNumber() {
+    let inputNumArray = [];
     try {
       const userInput = await MissionUtils.Console.readLineAsync(
         "숫자를 입력해주세요 : "
       );
       const userInputSet = new Set(userInput);
-      if (userInput.length !== userInputSet.size || userInput.length !== 3) {
+      inputNumArray = userInput.split("").map(Number);
+      if (
+        inputNumArray.length !== userInputSet.size ||
+        inputNumArray.length !== 3 ||
+        Number.isNaN(parseInt(userInput))
+      ) {
         throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
       } else {
-        return userInput;
+        return inputNumArray;
       }
     } catch (error) {
       MissionUtils.Console.print(error.message);
@@ -25,16 +33,21 @@ class App {
   }
 
   //서로다른 무작위 3개의 수가 들어있는 배열을 반환
-  createComNum() {
-    const computerNumArray = [];
-    while (computerNumArray.length < 3) {
+  createAnswerNumber() {
+    const answerNumArray = [];
+    while (answerNumArray.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computerNumArray.includes(number)) {
-        computerNumArray.push(number);
+      if (!answerNumArray.includes(number)) {
+        answerNumArray.push(number);
       }
     }
-    return computerNumArray;
+    return answerNumArray;
   }
+
+  compareNumber(answer, input) {
+    let strike = 0;
+    let ball = 0;
+    
 }
 
 const app = new App();
