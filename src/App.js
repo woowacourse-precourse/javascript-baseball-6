1c761ad84001ba29aa8340019cfed71f373d357a
leeyulgok <dbfrhr20@gmail.com>
2023-10-20 15:37:16 +0900

    Feat: 컴퓨터 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
   async play() {
-    console.log("git commit test");
+    console.log("숫자 야구 게임을 시작합니다.");
+    const COM_NUMBER = this.createNumber();
+    console.log(COM_NUMBER);
+  }
+
+  createNumber() {
+    const COMPUTER = [];
+    while (COMPUTER.length < 3) {
+      const NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!COMPUTER.includes(NUMBER)) {
+        COMPUTER.push(NUMBER);
+      }
+    }
+    return COMPUTER.join("");
   }
 }
 
