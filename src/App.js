4cf80bc0c9cd048da64311eea25662529605d93b
myeongin <mungin0926@naver.com>
2023-10-20 14:41:28 +0900

    [Fix] 비동기 작업 처리

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
 import generateRandomNumber from "./randomNumber.js";
-import userInput from "./userInput.js";
 import successCheck from "./successCheck.js";
 import { Console } from "@woowacourse/mission-utils";
-
+import { userInputValidation, userInput } from "./userInput.js";
 class App {
-  play() {
-    this.gameStart();
-  }
-
-  gameStart() {
+  async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
-    this.createComputerNumber();
+    try {
+      await this.createComputerNumber();
+    } catch (error) {
+      throw new Error(`[ERROR] ${error}`);
+    }
   }
-
-  createComputerNumber() {
+  async createComputerNumber() {
     this.computerNumber = generateRandomNumber();
-    this.userInputCheck();
+    await this.userInputCheck();
   }
 
   async userInputCheck() {
     this.userNumber = await userInput();
-    this.scoreResult(this.userNumber);
+    const isValidationSuccess = userInputValidation(this.userNumber);
+    if (isValidationSuccess) await this.scoreResult();
+    else throw new Error("숫자가 잘못된 형식입니다.");
   }
 
-  scoreResult() {
+  async scoreResult() {
     const isSuccess = successCheck(this.computerNumber, this.userNumber);
-    isSuccess ? this.gameClear() : this.userInputCheck();
+    if (isSuccess) this.gameClear();
+    else return this.userInputCheck();
   }
 
-  async gameClear() {
+  gameClear() {
     Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    this.gameRestartCheck();
+  }
+
+  async gameRestartCheck() {
     Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-    const gameRestart = await Console.readLineAsync("");
-    
+    const isGameRestart = await Console.readLineAsync("");
+    if (isGameRestart === "1") return this.createComputerNumber();
+    else if (isGameRestart !== "2") return this.gameRestartCheck();
   }
 }
 
 const app = new App();
 app.play();
-
 export default App;
--- src/userInput.js
+++ src/userInput.js
@@ -1,35 +1,34 @@
 import { Console } from "@woowacourse/mission-utils";
 
-export default async function userInput() {
+export async function userInput() {
   const userInputValue = await Console.readLineAsync("숫자를 입력해주세요: ");
-  const validatedInput = userInputValidation(userInputValue);
-  return stringToNumberArr(validatedInput);
+  return stringToNumberArr(userInputValue);
 }
 
 function stringToNumberArr(value) {
   return value.split("").map(Number);
 }
 
-function userInputValidation(nums) {
-  userInputLengthCheck(nums);
-  userInputUniqueCheck(nums);
-  userInputDuplicationCheck(nums);
-  return nums;
+export function userInputValidation(nums) {
+  return (
+    userInputLengthCheck(nums) && userInputUniqueCheck(nums) && userInputDuplicationCheck(nums)
+  );
 }
 
 function userInputLengthCheck(nums) {
-  if (nums.length !== 3) throw new Error("길이는 반드시 3글자여야 합니다");
+  return nums.length === 3;
 }
 
 function userInputUniqueCheck(nums) {
   const uniqueNumbers = new Set(nums);
-  if (uniqueNumbers.size !== 3) throw new Error("중복된 숫자가 있습니다.");
+  return uniqueNumbers.size === 3;
 }
 
 function userInputDuplicationCheck(nums) {
   for (const num of nums) {
     if (isNaN(num) || num < 1 || num > 9) {
-      throw new Error("숫자는 1에서 9 사이여야 합니다.");
+      return false;
     }
   }
+  return true;
 }
