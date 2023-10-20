bcd89a004f25b69d92089e5f25050363cd538eff
ryan-dia <cjfdnjs158@naver.com>
2023-10-20 11:14:40 +0900

    feat: 메시지 상수화

---
+++ src/constants/Messages.js
@@ -0,0 +1,12 @@
+export const OUTPUT_MESSAGES = Object.freeze({
+  GAME_START: '숫자 야구 게임을 시작합니다.',
+  GAME_NUMBER: '숫자를 입력해주세요 : ',
+});
+
+export const ERROR_MESSAGE = Object.freeze({
+  STRING: '숫자만 입력 가능합니다.',
+  ZERO: '0 은 포함될 수 없습니다. 1~9 사이의 숫자만 입력해 주세요',
+  DUPLICATION: '서로 다른 숫자를 입력해야 합니다.',
+  THREE_DIGIT: '3자리 숫자만 가능합니다.',
+});
+
