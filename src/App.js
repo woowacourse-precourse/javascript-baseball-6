a25ba42be7b1e7927745d4c08d14282e3ce10672
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-10-20 14:50:20 +0900

    chore: 구현전 요구사항 주석달기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 class App {
-  async play() {}
+  async play() {
+    //컴퓨터는 1~9까지 서로다른 임의수 3개를 선택
+    //사용자에게 서로다른 숫자 3개를 입력받는다.
+    //
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
