5a2eb5496109d5b9f5d1dc6188ab270b96f872a8
Jules <dali1999@naver.com>
2023-10-20 22:56:31 +0900

    feat: userInput 에 중복된 숫자가 있는지 확인하는 기능 추가

--- src/handleAnswer/handleUserInput.js
+++ src/handleAnswer/handleUserInput.js
@@ -3,8 +3,19 @@ export default function handleUserInput(userInput) {
   //valid 확인
   const validLength = (userInput) => userInput.length !== 3;
   const validType = (userInput) => isNaN(userInput);
-  if (validLength(userInput) || validType(userInput)) {
-    throw new Error("[ERROR] 잘못된 입력입니다.");
+  const validOverlap = (userInput) => {
+    return (
+      userInput[0] === userInput[1] ||
+      userInput[1] === userInput[2] ||
+      userInput[0] === userInput[2]
+    );
+  };
+  if (
+    validLength(userInput) ||
+    validType(userInput) ||
+    validOverlap(userInput)
+  ) {
+    throw new Error(`[ERROR] 잘못된 입력입니다. ${userInput}`);
   }
 
   return userInput;
