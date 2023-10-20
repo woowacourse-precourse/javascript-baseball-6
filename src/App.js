97b415995025683c8159dcb2bc5fabeefaba4561
ktyoung <ktyoung0507@gmail.com>
2023-10-20 12:12:23 +0900

    feat: Enhance number baseball game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 const { Console } = MissionUtils;
 
-// TODO 1 : 게임종료 후 재시작 여부 출력 필요
-// TODO 2 : 예외 처리 필요
-
 class App {
-  play() {
-    function StartGame() {
-      Console.print(`숫자 야구 게임을 시작합니다.`);
-      const computerNumber = GetRandomNumber();
-      PlayGame(computerNumber);
+  async play() {
+    async function StartGame() {
+      return await PlayGame(); // StartGame이 PlayGame의 완료를 기다리고 결과를 반환하도록 합니다.
     }
 
-    async function PlayGame(computerNumber) {
+    async function PlayGame() {
+      const computerNumber = GetRandomNumber();
       let strikeCount = 0;
+
+      Console.print(`숫자 야구 게임을 시작합니다.`);
 
       while (strikeCount !== 3) {
         const userNumber = await InputNumber();
+
         strikeCount = CompareNumber(computerNumber, userNumber);
         if (strikeCount === 3) {
-          break;
+          const answer = await AskForRestart();
+          if (answer === "1") {
+            return await PlayGame(); // 재귀적으로 PlayGame을 호출하고 그 결과를 반환합니다.
+          } else {
+            Console.print("게임을 종료합니다.");
+            return; // 함수에서 빠져나와 play 함수가 종료될 수 있게 합니다.
+          }
         }
       }
     }
@@ -33,12 +38,18 @@ class App {
           computer.push(number);
         }
       }
+      Console.print(`컴퓨터의 숫자는 ${computer}`);
 
       return computer;
     }
 
     async function InputNumber() {
       const user = await Console.readLineAsync(`숫자를 입력해주세요 : `);
+
+      if (!/^[1-9]{3}$/.test(user) || new Set(user).size !== 3) {
+        throw new Error("[ERROR] 잘못된 입력으로 인해 게임이 종료됩니다.");
+      }
+
       const userArray = user.split("").map(Number);
 
       return userArray;
@@ -74,7 +85,20 @@ class App {
       return strike;
     }
 
-    StartGame();
+    async function AskForRestart() {
+      const answer = await Console.readLineAsync(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요 : "
+      );
+
+      if (answer !== "1" && answer !== "2") {
+        Console.print("1 또는 2를 입력해주세요.");
+        return await AskForRestart();
+      }
+
+      return answer;
+    }
+
+    return await StartGame();
   }
 }
 
