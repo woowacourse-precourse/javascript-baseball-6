fff912829f00880a8ce3dbca569c180e37fa0249
chiman <rlaclghks123@naver.com>
2023-10-20 15:09:01 +0900

    feat: OutputView에서 printResult 함수 구현
    ball, strike값을 통해 상황별로 결과값을 출력하는 함수 구현

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -4,6 +4,13 @@ const OutputView = {
   printStart() {
     Console.print('숫자 야구 게임을 시작합니다.');
   },
+
+  printResult(ball, strike) {
+    if (!ball && !strike) Console.print('낫싱');
+    if (ball && !strike) Console.print(`${ball}볼`);
+    if (!ball && strike) Console.print(`${strike}스트라이크`);
+    if (ball && strike) Console.print(`${ball}볼 ${strike}스트라이크`);
+  },
 };
 
 export default OutputView;
