93d86c3a355b96e0c9485165871e9a533bcfbc3a
gouz7514 <gouz7514@gmail.com>
2023-10-20 22:39:23 +0900

    Update(feature) : 잘못된 입력 예시 추가 - 중복된 숫자

--- src/App.js
+++ src/App.js
@@ -21,10 +21,13 @@ class App {
   async getUserNumbers() {
     return new Promise((resolve) => {
       Console.readLineAsync('숫자를 입력해주세요 : ').then((userNumbers) => {
-        // 사용자가 잘못된 값을 입력한 경우 throw문을 사용해 예외를 발생새캐고 애플리케이션을 종료한다.
+        // 사용자가 잘못된 값을 입력한 경우 throw문을 사용해 예외를 발생시키고 애플리케이션을 종료한다.
+        // 1. 숫자인지, 숫자라면 3자리인지 체크
         const isValid = /^\d{3}$/.test(userNumbers)
+        // 2.숫자가 중복되는지 체크
+        const isDuplicated = userNumbers.split('').some((number, index, numbers) => numbers.indexOf(number) !== index);
 
-        if (!isValid) {
+        if (!isValid || isDuplicated) {
           this.throwError();
         }
 
