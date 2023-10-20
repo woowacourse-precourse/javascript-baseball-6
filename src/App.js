c39baf38bc57c236896d6e4e2b342e686678c1d5
Jeongeun Lee <leeju1009@gmail.com>
2023-10-20 19:29:45 +0900

    feat: 컴퓨터(상대)가 랜덤으로 3자리수를 선택하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    const selectedNumber = await this.pickRandomNumber();
+  }
+
+  async pickRandomNumber() {
+    let randomNumber = '';
+    while (randomNumber <= 2) {
+      const newRandomNumber = Random.pickNumberInRange(1, 9);
+      if (!randomNumber.includes(newRandomNumber)) {
+        randomNumber += newRandomNumber;
+      }
+    }
+  }
 }
 
 export default App;
