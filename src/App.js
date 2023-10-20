28a59351c33c3ad17857423fc0bb09c108503aaf
myireland8125 <hhyjs2r@gmail.com>
2023-10-21 05:24:30 +0900

    [feat] input Value 입력 처리, 결과 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,40 +1,73 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
-  computerNum = [];
+  computerNum;
   NUMBER_LENGTH = 3;
 
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    this.computerNum = [];
     this.createRandomNum();
+    console.log("--컴퓨터 함수--");
+    console.log(this.computerNum);
     this.inputNum();
   }
 
   async inputNum() {
     const inputValue = await Console.readLineAsync("숫자를 입력해주세요.");
-
     this.checkInputNum(inputValue);
     this.checkResult(inputValue);
   }
 
-  checkResult(inputValue) {
+  async checkResult(inputValue) {
+    console.log("checkResult");
     const inputNumArr = inputValue.split("");
     let strike = 0;
     let ball = 0;
 
     for (let i = 0; i < this.NUMBER_LENGTH; i++) {
-      for (let j = 0; j < 3; j++) {
+      for (let j = 0; j < this.NUMBER_LENGTH; j++) {
         if (i === j && this.computerNum[i] === inputNumArr[j]) {
           strike++;
         }
-        if (this.computerNum[i] === inputNumArr[j]) {
+        if (i !== j && this.computerNum[i] === inputNumArr[j]) {
           ball++;
         }
       }
     }
 
     if (ball === 0 && strike === 0) {
-      console.log("낫싱,,");
+      Console.print("낫싱");
+      await this.inputNum();
+    }
+
+    if (ball == 0 && strike !== 0) {
+      Console.print(`${strike}스트라이크`);
+      await this.inputNum();
+    }
+
+    if (strike == 0 && ball !== 0) {
+      Console.print(`${ball}볼`);
+      await this.inputNum();
+    }
+
+    if (ball !== 0 && strike !== 0) {
+      Console.print(`${ball}볼 ${strike}스트라이크`);
+      await this.inputNum();
+    }
+
+    if (strike === 3) {
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+      const inputValue = await Console.readLineAsync(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+      );
+
+      if (parseInt(inputValue) === 1) {
+        this.play();
+      } else {
+        console.log("겜 종료");
+      }
     }
   }
 
