50c609d462f94924347748b0fbd41993cd58bdc3
haesa <xdhaesa@gmail.com>
2023-10-20 15:10:09 +0900

    [Test]: Computer 숫자는 1과 9 사이의 정수여야 한다.

--- __tests__/ComputerTest.js
+++ __tests__/ComputerTest.js
@@ -1,8 +1,62 @@
 import Computer from '../src/Computer.js';
 
+let computer;
+beforeEach(() => {
+  computer = new Computer();
+});
+
 describe('컴퓨터의 야구 숫자 뽑기', () => {
   test('숫자 3개를 뽑았는가', () => {
-    const computer = new Computer();
     expect(computer.numbers).toHaveLength(3);
   });
+
+  test('1과 9 사이의 숫자인가', () => {
+    computer.numbers.forEach((number) => {
+      expect(number).toBeWithinRange(1, 9);
+    });
+  });
+
+  test('숫자가 정수인가', () => {
+    computer.numbers.forEach((number) => {
+      expect(number).toBeIntNumber();
+    });
+  });
+});
+
+expect.extend({
+  toBeWithinRange(received, start, end) {
+    const pass = received >= start && received <= end;
+
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${received} to be within range ${start} - ${end}`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () =>
+          `expected ${received} not to be within range ${start} - ${end}`,
+        pass: false,
+      };
+    }
+  },
+});
+
+expect.extend({
+  toBeIntNumber(received) {
+    const pass = Math.floor(received) === received;
+
+    if (pass) {
+      return {
+        message: () => `expected ${received} to be Int number`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () => `expected ${received} not to be Int number`,
+        pass: false,
+      };
+    }
+  },
 });
