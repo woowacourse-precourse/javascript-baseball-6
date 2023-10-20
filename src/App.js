c4b3caf4cec369b4a9ef81f0ee78929fcd5dd714
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-10-20 12:25:54 +0900

    Feat: 시작 문구 출력과 랜덤 숫자 생성 기능완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Console,MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  constructor() {
+    this.computerNumbers = this.computerNumbers();
+  }
+
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    Console.print(this.computerNumbers)
+  }
+
+  computerNumbers() {
+    const numbers = [];
+    while (numbers.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!numbers.includes(number)) {
+        numbers.push(number);
+      }
+    }
+    return numbers;
+  }
 }
+
+const app = new App();
+
+app.play();
 
 export default App;
