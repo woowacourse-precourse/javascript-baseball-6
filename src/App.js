71901e58070e6acc2acb63ce4e3045573d7827e9
chiman <rlaclghks123@naver.com>
2023-10-20 14:27:45 +0900

    feat: App.js 파일에서 play 기능 구현
    시작 메세지와 함께 사용자, 상대방 객체로부터 숫자를 받아와 refree 객체로 넘겨주는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import InputView from './View/InputView.js';
+import OutputView from './View/OutputView.js';
+
+import Opponent from './Opponent.js';
+import Player from './Player.js';
+import Refree from './Refree.js';
+
 class App {
-  async play() {}
+  #refree;
+
+  async play() {
+    OutputView.printStart();
+
+    const inputNumber = await InputView.getUserNumber();
+    const player = new Player(inputNumber);
+    const opponent = new Opponent();
+
+    this.#refree = new Refree(player.userNumber, opponent.makeRandomNumber());
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
