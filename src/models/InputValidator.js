0ac6f48aac8b01a9e673da8e59b353fcf8aa4585
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-21 06:03:10 +0900

    :sparkles: feat: 재시작 또는 종료 명령어의 유효성 검증 기능 추가 1 또는 2와 일치할 경우에만 true를 리턴시킴

--- src/models/InputValidator.js
+++ src/models/InputValidator.js
@@ -1,10 +1,15 @@
 const InputValidator = {
-  numberValidate (input) {
+  
+  numberValidate(input) {
     const typeValidate = /^\d+$/.test(input);
     const digitsValidate = new Set(input).size === 3 && input.length === 3;
     const rangeValidate = !input.includes('0');
 
     return typeValidate && digitsValidate && rangeValidate;
+  },
+
+  commandValidate(input) {
+    return input === '1' || input === '2';
   },
 };
 
