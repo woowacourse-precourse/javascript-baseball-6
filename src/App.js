2c5cafd6f43541284fd567a213a9e28426e000b2
novice0840 <magnolia0840@gmail.com>
2023-10-20 20:44:55 +0900

    Docs: README.md check box 추가
    각 기능의 구현 여부를 구분하기 위해 체크 박스를 추가하였다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
+  createAnswerNumber() {
+    const result = "";
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    return result;
+  }
   async play() {}
 }
-
 export default App;
+
+const app = new App();
+app.play();
