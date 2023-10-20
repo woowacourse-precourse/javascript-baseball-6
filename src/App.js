539e0b91b9ce7554e2ede2a0c8fd3e15ec4ee5ad
hellosehyun <sehyun030526@gmail.com>
2023-10-20 22:13:18 +0900

    fix func getComputerNum

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-    async play() {
-        // 상대방 숫자 생성기
-        function getComputerNum() {
-            let l = [];
-            for (let i = 0; i < 3; i++) {
-                l.push(MissionUtils.Random.pickNumberInRange(1, 9));
-            }
-            return l;
+    // 상대방 숫자 생성기
+    async getComputerNum() {
+        let l = [];
+        for (let i = 0; i < 3; i++) {
+            l.push(MissionUtils.Random.pickNumberInRange(1, 9));
         }
-        let computerNum = [...getComputerNum()];
+        return Promise.resolve(l);
+    }
+
+    async play() {
+        let data = await this.getComputerNum();
+        MissionUtils.Console.print(data);
     }
 }
 
