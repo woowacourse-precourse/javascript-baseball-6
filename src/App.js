ff33882789e2871376497cf504e9bffa43387b30
김용민 <dydals3440@gmail.com>
2023-10-20 20:11:54 +0900

    FEAT: Computer: 1~9까지 랜덤한 숫자 3개 선택하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { Console, MissionUtils, Random } from '@woowacourse/mission-utils';
+
 class App {
   async play() {
-    console.log('hi');
+    console.log('숫자 야구 게임을 시작합니다.');
+    const computer = this.generateRandomNumber();
+    console.log(`1번 조건 테스트: ${computer}`);
+  }
+
+  // 1. 컴퓨터가 1~9까지의 랜덤한 숫자 3개를 선택.
+  generateRandomNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
   }
 }
 
