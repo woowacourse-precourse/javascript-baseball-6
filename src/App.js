0d738c61c453f83cf5394915cd5fa2cbe1f53203
myireland8125 <hhyjs2r@gmail.com>
2023-10-21 01:28:40 +0900

    [fix] 예외처리 수정

--- src/App.js
+++ src/App.js
@@ -5,55 +5,63 @@ class App {
   numberLength = 3;
 
   async play() {
-    console.log("play 함수 실행");
+    Console.print("숫자 야구 게임을 시작합니다.");
     this.createRandomNum();
     this.inputNum();
   }
 
   async inputNum() {
-    const INPUT_RESULT = await Console.readLineAsync("숫자를 입력하세요.");
+    const INPUT_RESULT = await Console.readLineAsync("숫자를 입력해주세요.");
+
     this.checkInputNum(INPUT_RESULT);
+    this.checkResult(INPUT_RESULT);
+  }
+
+  checkResult(INPUT_RESULT) {
+    const arr = INPUT_RESULT.split("");
+    console.log(arr);
   }
 
   createRandomNum() {
-    while (this.computerNum.length < 3) {
-      const NUMBER = Random.pickNumberInRange(1, 9); // 랜덤 숫자를 돌린다.
-      const IS_INCLUDE = this.computerNum.includes(NUMBER); // 중복 숫자가 있으면 true, 없으면 false 를 반환한다.
+    while (this.computerNum.length < this.numberLength) {
+      const RANDOM_NUM = Random.pickNumberInRange(1, 9); // 랜덤 숫자를 돌린다.
+      const IS_INCLUDE = this.computerNum.includes(RANDOM_NUM); // 중복 숫자가 있으면 true, 없으면 false 를 반환한다.
 
       if (!IS_INCLUDE) {
-        this.computerNum.push(NUMBER);
+        this.computerNum.push(RANDOM_NUM);
       }
     }
     console.log(this.computerNum);
   }
 
-  checkInputNum(INPUT_RESULT) {
-    const arr = INPUT_RESULT.toString().split("");
-    if (this.isDuplicate(arr)) {
+  checkInputNum(inputValue) {
+    const NUM_ARR = inputValue.split("");
+    const NEW_NUM = parseInt(inputValue);
+
+    if (this.isDuplicate(NUM_ARR)) {
       throw new Error("입력한 값이 중복일 수 없습니다.");
     }
 
-    if (this.isNotInteger(INPUT_RESULT)) {
+    if (this.isNotInteger(NEW_NUM)) {
       throw new Error("입력 값이 숫자 혹은 정수가 아닙니다.");
     }
 
-    if (INPUT_RESULT.length != this.numberLength) {
+    if (inputValue.length != this.numberLength) {
       throw new Error("입력 값의 길이가 3이 아닙니다.");
     }
   }
 
   //예외처리 함수
-
   isDuplicate(arr) {
     return arr.some((x) => arr.indexOf(x) !== arr.lastIndexOf(x));
   }
 
-  isNotInteger(value) {
-    if (value % 1 !== 0) {
+  isNotInteger(inputValue) {
+    if (inputValue % 1 !== 0) {
       return true;
     }
 
-    if (typeof value !== "number") {
+    if (typeof inputValue !== "number") {
       return true;
     }
 
