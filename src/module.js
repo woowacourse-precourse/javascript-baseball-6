487b605ee4b9526e18ff18572b1a5eac2b9abc99
soominnnn <marisol03@naver.com>
2023-10-20 19:34:57 +0900

    숫자 비교

--- src/module.js
+++ src/module.js
@@ -14,9 +14,10 @@ export let start = {
 }
 // USER 입력
 export let USER = {
-  RESULT : [],
-  COUNT : [],
   getUserInput : async function(){
+    const TEMP = [];
+    const RESULT = [];
+    const COUNT = [0,0,0];
     try{
       const userinput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요. : ');
       const USER_INPUT = userinput.split('');
@@ -24,29 +25,43 @@ export let USER = {
         MissionUtils.Console.print(`3자리 숫자를 입력해주세요.`);
         return this.getUserInput();
       }
-
       for(let i = 0; i<RANDOM.RANDOM_ARR.length; i++){
-        const TEMP = RANDOM.RANDOM_ARR.indexOf(USER_INPUT[i]);
-        console.log(RANDOM.RANDOM_ARR)
-        console.log(USER_INPUT);
-        console.log(this.RESULT)
+        TEMP[i] = RANDOM.RANDOM_ARR.indexOf(Number(USER_INPUT[i]));
         if( TEMP[i] == '-1' ){
-          this.RESULT[i] = '-1'
-          this.COUNT[2]++;
+          RESULT[i] = '-1'
+          COUNT[2] += 1;
         }
         else {
           if( TEMP[i] !== i){
-            this.RESULT[i] = '0'
-            this.COUNT[1]++; // 숫자와 자리가 일지하지 않는 경우 0
+            RESULT[i] = '0'
+            COUNT[1]+= 1; // 숫자와 자리가 일지하지 않는 경우 0
           }
           else{
-            this.RESULT[i] = '1'
-            this.COUNT[0]++; // 숫자와 자리가 일치하는 경우 1
+            RESULT[i] = '1'
+            COUNT[0]+= 1; // 숫자와 자리가 일치하는 경우 1
           }
         }
       }
-      if( this.COUNT[0] != null){
-        MissionUtils.Console.print(`${this.COUNT[0]}스트라이크`);
+      /*console.log(RANDOM.RANDOM_ARR)
+      console.log(USER_INPUT)
+      console.log(TEMP)
+      console.log(RESULT)
+      console.log(COUNT)*/
+      if( COUNT[0] != 0){
+        MissionUtils.Console.print(`${COUNT[0]}스트라이크`);
+      }
+      if( COUNT[1] != 0){
+        MissionUtils.Console.print(`${COUNT[1]}볼`);
+      }
+      if( COUNT[2] == 3){
+        MissionUtils.Console.print(`낫싱`);
+      } 
+      if( COUNT[0] == 3){
+        MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        MissionUtils.Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+      }
+      else{
+        return this.getUserInput();
       }
     }
     catch(error){}
