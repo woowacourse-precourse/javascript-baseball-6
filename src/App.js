800c8adc959ec0bf8e9b18eb0cffa295abb05d7f
eyucherin <yu24c@mtholyoke.edu>
2023-10-20 13:56:47 -0400

    final refactor

--- src/App.js
+++ src/App.js
@@ -17,36 +17,43 @@ class App {
   }
 
   checkUserInput(answer) {
-    return new Promise((resolve, reject) => {
-      let answerList = Array.from(answer).map((num) => parseInt(num));
-      if (
-        answerList.length != 3 ||
-        new Set(answerList).size !== 3 ||
-        !answerList.every((num) => num >= 1 && num <= 9)
-      ) {
-        reject(new Error("[ERROR]"));
-      }
-      resolve(answerList);
-    });
+    const answerList = Array.from(answer).map((num) => parseInt(num));
+    if (
+      answerList.length !== 3 ||
+      new Set(answerList).size !== 3 ||
+      !answerList.every((num) => num >= 1 && num <= 9)
+    ) {
+      throw new Error("[ERROR]1부터 9까지 서로 다른 수로 이루어진 3자리의 랜덤 값만 입력 가능합니다");
+    }
+    return answerList;
   }
 
   getHint(userVal) {
     let strikeCount = 0;
     let ballCount = 0;
-    let nothing = 0;
-    return new Promise((resolve) => {
-      for (let i = 0; i < userVal.length; i++) {
-        if (userVal[i] == this.computerVal[i]) {
-          strikeCount++;
-        } else if (this.computerVal.includes(userVal[i])) {
-          ballCount++;
-        } else {
-          nothing++;
-        }
+    
+    for (let i = 0; i < userVal.length; i++) {
+      if (userVal[i] === this.computerVal[i]) {
+        strikeCount++;
+      } else if (this.computerVal.includes(userVal[i])) {
+        ballCount++;
       }
-      resolve([strikeCount, ballCount, nothing]);
-    });
+    }
+
+    if (strikeCount === 3) {
+      Console.print(`3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
+      return "restart";
+    } else if (ballCount && strikeCount) {
+      Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
+    } else if (ballCount) {
+      Console.print(`${ballCount}볼`);
+    } else if (strikeCount) {
+      Console.print(`${strikeCount}스트라이크`);
+    } else {
+      Console.print("낫싱");
+    }
   }
+
 
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
@@ -54,27 +61,19 @@ class App {
     while (keepPlaying) {
       try {
         let userInput = await Console.readLineAsync("숫자를 입력해주세요 : ");
-        let userVal = await this.checkUserInput(userInput);
-        let [strikeCount, ballCount, nothing] = await this.getHint(
-          userVal);
-        if (nothing === 3) {
-          Console.print("낫싱");
-        } else if (strikeCount === 3) {
-          Console.print(
-            `3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`
-          );
+        const userVal = this.checkUserInput(userInput);
+        const hintResult = this.getHint(userVal);
+        if (hintResult){
           let replay = await Console.readLineAsync(
-            `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`
+            `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`
           );
           if (replay === "1") {
             this.computerVal = this.getRandomValue();
           } else if (replay === "2") {
             keepPlaying = false;
           } else {
-            throw new Error("[ERROR]");
+            throw new Error("[ERROR]1 아니면 2 만 입력가능하니다.");
           }
-        } else {
-          Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
         }
       } catch (e) {
         return Promise.reject(e);
