3b90d7826d40aa9bda2a970047627cf06ce2a309
soosoo22 <lllllh00@naver.com>
2023-10-21 05:34:12 +0900

    add play()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+//import readline from "readline";
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { TEXT, ERROR, RESTART } from "./constants.js";
+
 class App {
-  async play() {}
+  constructor() {
+    this.initialPrint();
+  }
+
+  initialPrint() {
+    MissionUtils.Console.print(TEXT.INITIAL);
+  }
+
+  async play() {
+    this.computerNumber = this.makeRandomNumber();
+    MissionUtils.Console.print(this.computerNumber);
+    await this.inputGuessNumber();
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
