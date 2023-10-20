7eac854637a821e00bf022d49843651f9af32691
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-10-20 21:43:44 +0900

    feat: 숫자 입력했을 시, 유효성 체크

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Computer from "./Computer";
+
 class App {
-  async play() {}
-}
+  constructor() {
+    this.computer = new Computer();
+  }
+
+  async play() {
+    MissionUtils.Console.print("게임을 시작합니다.");
+    this.startGame();
+  }
+  
+  async startGame() {
+    const input = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+    this.duringGameInput(input);
+  }
+
+  duringGameInput(input) {
+    if(!this.computer.checkInputValid(input)){
+      throw new Error('[ERROR] 잘못된 형식입니다.');
+    }
+  }
+};
 
 export default App;
