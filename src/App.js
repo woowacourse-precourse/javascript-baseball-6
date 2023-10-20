2ade833be62c617075ac3b793f6c8ffb5eb89ac4
leedbswo107 <ljyu1@naver.com>
2023-10-20 19:45:45 +0900

    feat:upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+// Console.readLineAsync
+// Console.print
+
 class App {
-  async play() {}
+  async play(computer, user) {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    Console.print(computer);
+    // Console.print(answers);
+  }
+}
+const computer = [];
+while (computer.length < 3) {
+  const number = MissionUtils.Random.pickNumberInRange(1, 9);
+  if (!computer.includes(number)) {
+    computer.push(number);
+  }
 }
 
+const app = new App();
+app.play();
 export default App;
