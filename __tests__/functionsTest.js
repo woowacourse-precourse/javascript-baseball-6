a2c74b80b05ce5fb1bcb11a905a7cae5c9ff412f
박지윤 <tkrhkfld123@naver.com>
2023-10-21 00:08:12 +0900

    test: 게임 시작 메세지 띄우기 테스트 완료

---
+++ __tests__/functionsTest.js
@@ -0,0 +1,13 @@
+import App from '../src/App'
+
+describe("야구 게임 테스트", () => {
+  test("시작 메세지 출력", () => {
+    const logSpy = jest.spyOn(console, 'log');
+    logSpy.mockClear();
+
+    const app = new App();
+    app.startGame();
+
+    expect(logSpy).toHaveBeenCalledWith('숫자 야구 게임을 시작합니다.');
+  });
+});
