3c5415d074589620e72e680ab7b453639122a39a
soyoung125 <sypark041754@gmail.com>
2023-10-21 00:03:18 +0900

    feat: 상대방의 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+
+  constructor() {
+    this.numbers = this.getRandomNumbers();
+    MissionUtils.Console.print(this.numbers);
+  }
+
+  getRandomNumbers() {
+    const numbers = new Set();
+    while (numbers.size < 3) {
+      numbers.add(Math.floor(Math.random() * 9 + 1))
+    }
+    return [...numbers];
+  }
+  
+  async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.")
+  }
 }
 
 export default App;
