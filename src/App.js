0e6deedcdf3867696c4a5e2c73fc392d919e07a4
lee juyoung <juyung0903@naver.com>
2023-10-21 01:59:00 +0900

    feat: 3스트라이크시 종료 및 새로시작여부 입력받기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import readline from "readline";
 const COMPUTER_NUMBER = [];
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -17,11 +16,17 @@ class App {
   }
   showResults(strikeNumbers, ballNumbers) {
     if (strikeNumbers === 0 && ballNumbers === 0) {
-      console.log('낫싱');
-    } else{
-      (strikeNumbers===0&&ballNumbers!==0)&&(console.log(`${ballNumbers}볼`));
-      (strikeNumbers!==0&&ballNumbers===0)&&(console.log(`${strikeNumbers}스트라이크`));
-      (strikeNumbers!==0&&ballNumbers!==0)&&(console.log(`${ballNumbers}볼 ${strikeNumbers}스트라이크`))
+      console.log("낫싱");
+    } else {
+      strikeNumbers === 0 &&
+        ballNumbers !== 0 &&
+        console.log(`${ballNumbers}볼`);
+      strikeNumbers !== 0 &&
+        ballNumbers === 0 &&
+        console.log(`${strikeNumbers}스트라이크`);
+      strikeNumbers !== 0 &&
+        ballNumbers !== 0 &&
+        console.log(`${ballNumbers}볼 ${strikeNumbers}스트라이크`);
     }
   }
   //유저의 숫자와 컴퓨터의 숫자 비교
@@ -36,24 +41,53 @@ class App {
         ballNumbers += 1;
       }
     }
-    this.showResults(strikeNumbers,ballNumbers);
-
+    this.showResults(strikeNumbers, ballNumbers);
+    return strikeNumbers;
   }
+
   askNumber() {
     rl.question("숫자를 입력해주세요 : ", (guessNumber) => {
       if (guessNumber.length !== 3 || !guessNumber || isNaN(guessNumber)) {
-        console.log("3개의 숫자만 입력가능합니다.");
+        console.log("올바른 값을 입력하세요.");
       } else {
-        this.compareNumbers(guessNumber);
+        if (this.compareNumbers(guessNumber) === 3) {
+          this.gameExiter();
+          return;
+        }
       }
       this.askNumber();
     });
   }
-  async play() {
-    console.log("숫자 야구 게임을 시작합니다.");
+
+  startGame() {
     this.generateRandomNumber();
     this.askNumber();
   }
+  gameExiter() {
+    console.log("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    rl.question(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+      (exitNumber) => {
+        if (exitNumber === "1") {
+          COMPUTER_NUMBER.length = 0;
+        } else if (exitNumber === "2") {
+          console.log("게임을 종료합니다.");
+          rl.close();
+          return;
+        } else {
+          console.log("올바른 값을 입력하세요");
+          this.gameExiter();
+          return;
+        }
+        this.startGame();
+      }
+    );
+  }
+  async play() {
+    console.log("숫자 야구 게임을 시작합니다.");
+    this.startGame();
+  }
+
 }
 const app = new App();
 app.play();
