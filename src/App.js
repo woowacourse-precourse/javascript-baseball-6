ebccc12a6fc92ec7e90d1a33051b5b58692ca998
lij0825 <dldlswns890@gmail.com>
2023-10-21 03:42:02 +0900

    기능 수정

--- src/App.js
+++ src/App.js
@@ -2,25 +2,42 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
     async play() {
-        await MissionUtils.Console.print("숫자를 입력해주세요:");
-        for (let times = 1; ; times++) {
+        await MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+        while (true) {
             const COMNUM = this.ComputerNumber();
-            const USERINPUT = await MissionUtils.Console.readLineAsync();
+            const USERINPUT = await this.UserInput();
             if (this.CheckNum(USERINPUT)) {
                 const result = this.BSResult(COMNUM, USERINPUT);
+                await MissionUtils.Console.print(result);
                 if (result === "3스트라이크") {
-                    await MissionUtils.Console.print(result);
-                    await MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    await MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                    const USERINPUT = await MissionUtils.Console.readLineAsync();
-                    break;
-                } else {
-                    await MissionUtils.Console.print(result);
+                    await MissionUtils.Console.print(
+                        "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+                    );
+                    const restart = await this.Restart();
+                    if (restart === "2") {
+                        break;
+                    }
                 }
             } else {
-                // await MissionUtils.Console.print("[ERROR] 숫자가 잘못된 형식입니다.");
                 throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
             }
+        }
+    }
+
+    async UserInput() {
+        await MissionUtils.Console.print("숫자를 입력해주세요:");
+        return await MissionUtils.Console.readLineAsync();
+    }
+
+    async Restart() {
+        await MissionUtils.Console.print(
+            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+        );
+        const choice = await MissionUtils.Console.readLineAsync();
+        if (choice === "1" || choice === "2") {
+            return choice;
+        } else {
+            throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
         }
     }
 
@@ -36,11 +53,10 @@ class App {
     }
 
     CheckNum(USERINPUT) {
-        if(USERINPUT.length == 3 && typeof value === 'RANDOMNUM'){
-            return true
-        }else{
-            return false
+        if (USERINPUT.length === 3 && !isNaN(USERINPUT)) {
+            return true;
         }
+        return false;
     }
 
     BSResult(COMNUM, USERINPUT) {
@@ -49,22 +65,23 @@ class App {
         for (let i = 0; i < 3; i++) {
             if (COMNUM.includes(USERINPUT[i])) {
                 BALLS++;
-            } else if (USERINPUT[i] === COMNUM[i]) {
+            }
+            if (USERINPUT[i] === COMNUM[i]) {
                 STRIKES++;
                 BALLS--;
             }
         }
         if (STRIKES === 3) {
             return "3스트라이크";
-        } else if (STRIKES > 0 || BALLS > 0) {
+        }
+        if (STRIKES > 0 || BALLS > 0) {
             const ANSWERNUM = [
                 BALLS > 0 ? `${BALLS}볼 ` : "",
-                STRIKES > 0 ? `${STRIKES}스트라이크` : ""
+                STRIKES > 0 ? `${STRIKES}스트라이크` : "",
             ];
-            return ANSWERNUM.join(' ');
-        } else {
-            return "낫싱";
+            return ANSWERNUM.join(" ");
         }
+        return "낫싱";
     }
 }
 
