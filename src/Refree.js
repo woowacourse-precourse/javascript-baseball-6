620d07dfadaaba459cf84f8b95207c1c47a04f33
chiman <rlaclghks123@naver.com>
2023-10-20 14:32:18 +0900

    feat: countStrike 메서드 구현
    사용자의 숫자와 상대방의 숫자를 비교해서 스트라이크의 개수를 count하는 메서드

--- src/Refree.js
+++ src/Refree.js
@@ -9,11 +9,20 @@ class Refree {
 
   playGame() {
     const ball = this.countBall();
+    const strike = this.countStrike();
   }
 
   countBall() {
     return this.#playerNumber.reduce((count, target, index) => {
       return this.#opponentNumber.includes(target) && this.#opponentNumber[index] !== target
+        ? count + 1
+        : count;
+    }, 0);
+  }
+
+  countStrike() {
+    return this.#playerNumber.reduce((count, target, index) => {
+      return this.#opponentNumber.includes(target) && this.#opponentNumber[index] === target
         ? count + 1
         : count;
     }, 0);
