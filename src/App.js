d47663950e02bafa8e84f0455dc1f03e39b53ffd
hyunjun9788 <namhyunjun9788@gmail.com>
2023-10-20 17:34:41 +0900

    feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.computer = [];
+    this.inputNum = [];
+  }
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
 }
+
+let app = new App();
+app.play();
 
 export default App;
