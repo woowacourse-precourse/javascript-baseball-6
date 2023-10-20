ebc60e27bd43d9bcc0b205ae3f1cf16876fc1ace
chiman <rlaclghks123@naver.com>
2023-10-20 15:48:58 +0900

    feat: printDone 함수 구현
    3스트라이크를 맞춘 경우 메세지를 출력하는 함수

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -11,6 +11,10 @@ const OutputView = {
     if (!ball && strike) Console.print(`${strike}스트라이크`);
     if (ball && strike) Console.print(`${ball}볼 ${strike}스트라이크`);
   },
+
+  printDone() {
+    Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
+  },
 };
 
 export default OutputView;
