222efa6a95918ef9960b4338d808c665571d86b5
kang <kangju2000@naver.com>
2023-10-21 00:45:18 +0900

    feat: OutputView 추가

---
+++ src/view/OutputView.js
@@ -0,0 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+
+const OutputView = {
+  printStartMessage: () => {
+    Console.print("숫자 야구 게임을 시작합니다.\n");
+  },
+  printGuessResult: (ball, strike) => {
+    Console.print(`${ball}볼 ${strike}스트라이크\n`);
+  },
+  printEndMessage: () => {
+    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+  },
+}
+
+export default OutputView;
