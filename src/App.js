e13955ca55557bedd6a20d7eb73e4cf5bfcab385
Jules <dali1999@naver.com>
2023-10-20 23:56:17 +0900

    fix(computerAnswer): 클래스 밖에서 호출된 함수로 인한 오류 수정

--- src/App.js
+++ src/App.js
@@ -4,27 +4,31 @@ import handleUserInput from "./handleAnswer/handleUserInput.js";
 import printHint from "./game/gameHint.js";
 import makeAnswer from "./handleAnswer/makeAnswer.js";
 
-let computerAnswer = makeAnswer();
 class App {
   async play() {
+    let computerAnswer = makeAnswer();
+    Console.print("답: " + computerAnswer);
     while (true) {
-      Console.print(computerAnswer);
-
-      const userInput = await Console.readLineAsync("숫자를 입력해주세요 :");
+      const userInput = await Console.readLineAsync("숫자를 입력해주세요 : ");
       let userAnswer = handleUserInput(userInput); //예외 처리 후 userInput을 반환
-      //  ballmanager(computerAnswer, userAnswer);
+      Console.print(userAnswer);
+
       if (computerAnswer === userAnswer) {
         Console.print("3스트라이크");
         Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
         const replay = await Console.readLineAsync(
           "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
         );
         if (replay === "1") {
           computerAnswer = makeAnswer();
+          Console.print("답: " + computerAnswer);
           continue;
         } else if (replay === "2") {
           Console.print("게임 종료");
           break;
+        } else {
+          throw new Error("[ERROR] 잘못된 입력입니다");
         }
       } else {
         printHint(computerAnswer, userAnswer);
