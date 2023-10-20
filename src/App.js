8bdbf675cb8c7cfa4eaf3084202649446d4e97a4
bada308 <203190@jnu.ac.kr>
2023-10-20 22:47:00 +0900

    feat: 랜덤 정답 생성 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  async play() {}
+  async play() {
+    const answer = [];
+    while (answer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!answer.includes(number)) {
+        answer.push(number);
+      }
+    }
+  }
   async getUserNumber() {
     try {
       const number = await MissionUtils.Console.readLineAsync(
