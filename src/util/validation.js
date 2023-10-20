caf0cc2b929d5cc9e9c8182fa73bc98495821f3b
Ryong-E <think8867@gmail.com>
2023-10-20 20:10:15 +0900

    fix: validation 메시지 테스트코드와 일하도록 [ERROR] 구문 추가

--- src/util/validation.js
+++ src/util/validation.js
@@ -4,20 +4,20 @@ export function guessAnswerValidate(answer) {
   const duplicate = new Set(answer);
 
   if (!answer.split('').every((number) => number.charCodeAt() >= 49 && number.charCodeAt() <= 57)) {
-    throw new Error(ERROR_MASSAGE.OUT_OF_RANGE);
+    throw new Error(`[ERROR] ${ERROR_MASSAGE.OUT_OF_RANGE}`);
   }
 
   if (answer.length !== 3 || duplicate.size !== 3) {
-    throw new Error(ERROR_MASSAGE.DIFFRENT_NUMBERS);
+    throw new Error(`[ERROR] ${ERROR_MASSAGE.DIFFRENT_NUMBERS}`);
   }
 }
 
 export function retryAnswerValidate(answer) {
   if (!answer.split('').every((number) => number.charCodeAt() >= 49 && number.charCodeAt() <= 50)) {
-    throw new Error(ERROR_MASSAGE.NOT_CORRECT);
+    throw new Error(`[ERROR] ${ERROR_MASSAGE.NOT_CORRECT}`);
   }
 
   if (answer.length !== 1) {
-    throw new Error(ERROR_MASSAGE.ONLY_NUMBER);
+    throw new Error(`[ERROR] ${ERROR_MASSAGE.ONLY_ONE_NUMBER}`);
   }
 }
