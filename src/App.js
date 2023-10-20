e9caf6da9d88f301dfeef5fc03d8c01662617097
rbgksqkr <rbgks1937@naver.com>
2023-10-20 15:21:27 +0900

    Feat: 컴퓨터가 가질 1에서 9까지 서로 다른 임의의 수 3개 결정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const number = this.getRandomNumber();
+    console.log(number);
+  }
+
+  getRandomNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
