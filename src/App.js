8ff4830179477e20496d8089a0f5b0c3c318ab04
byunjiin <jibyun5808@naver.com>
2023-10-21 04:29:22 +0900

    feat: 기능2.Input 작성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { Console } from "@woowacourse/mission-utils";
 import { random } from "./Random.js";
+import { input } from "./Input.js";
 
 class App {
   async play() {
-    let computer = random();
     
+    let computer = random(); // 컴퓨터값을 랜덤으로 받기
+    while(true){
+      try{
+        let user = input(await Console.readLineAsync('숫자를 입력해주세요 : ')); // 사용자입력 애러 찾기
+      }catch (error){
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다."); // 발생하는 그 외 애러
+      }
+    }
   }
 }
 
