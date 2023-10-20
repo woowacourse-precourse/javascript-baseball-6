7aaca4e62ddc2efda8732fed960f5a3da1c559dd
김용민 <dydals3440@gmail.com>
2023-10-21 04:32:16 +0900

    FEAT: 게임 종료 / 초기화 선택 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import { Console, Random } from '@woowacourse/mission-utils';
 class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
-    const computer = this.generateRandomNumber();
-    await this.userInputCheck();
+    try {
+      this.computerNumber = this.generateRandomNumber();
+      await this.userInputCheck();
+    } catch (e) {
+      throw new Error(`[ERROR] ${e}`);
+    }
   }
 
   // 1. 컴퓨터가 1~9까지의 랜덤한 숫자 3개를 선택. 단, 숫자는 겹쳐서는 안된다.
   generateRandomNumber() {
-    const computer = new Set();
-    while (computer.size < 3) {
+    const computerNums = new Set();
+    while (computerNums.size < 3) {
       const randomNumber = Random.pickNumberInRange(1, 9);
-      computer.add(randomNumber);
+      computerNums.add(randomNumber);
     }
-    return Array.from(computer);
+    return Array.from(computerNums);
   }
 
   // 2. 유저, 입력 받기(123 => [1, 2, 3])
@@ -28,24 +32,20 @@ class App {
   }
 
   // 3. 유효성 검사 (1~9 숫자 3개 입력 가능)
-  userInputValidation(numArr) {
-    const inputStr = numArr.join('');
+  userInputValidation(nums) {
+    const inputStr = nums.join('');
     const regex = /^[1-9]{3}$/;
     return regex.test(inputStr);
   }
 
   async userInputCheck() {
     this.userNumber = await this.userInput();
-    const isChecked = this.userInputValidation(this.userNumber);
-    if (isChecked) {
-      // 4. 옳은 형식일 시, 컴퓨터와 유저의 숫자 서로 비교.
-      await this.compareResult;
-    } else {
-      throw new Error('숫자가 잘못된 형식입니다.');
-    }
+    const isValidationSuccess = this.userInputValidation(this.userNumber);
+    if (isValidationSuccess) await this.compareResult();
+    else throw new Error('숫자가 잘못된 형식입니다.');
   }
 
-  // 4-1. 컴퓨터와 유저의 숫자 서로 비교.
+  // 4. 컴퓨터와 유저의 숫자 서로 비교.
   checkResult(computerNumber, userNumber) {
     let ball = 0;
     let strike = 0;
@@ -53,10 +53,8 @@ class App {
     for (let i = 0; i < computerNumber.length; i++) {
       if (computerNumber[i] === userNumber[i]) {
         strike++;
-      } else {
-        if (computerNumber.includes(userNumber[i])) {
-          ball++;
-        }
+      } else if (computerNumber.includes(userNumber[i])) {
+        ball++;
       }
     }
 
@@ -77,10 +75,25 @@ class App {
   async compareResult() {
     const isSuccess = this.checkResult(this.computerNumber, this.userNumber);
     if (isSuccess) {
-      // 게임 초기화
+      // 6. 게임 종료 or 초기화
+      return this.finishGame();
     } else {
-      this.userInputCheck();
+      return this.userInputCheck();
     }
+  }
+
+  // 6-1. 게임 종료
+  finishGame() {
+    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    this.checkResetGame();
+  }
+
+  // 6-2. 게임 초기화 / 종료 선택
+  async checkResetGame() {
+    Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+    const isGameRestart = await Console.readLineAsync('');
+    if (isGameRestart === '1') return this.play();
+    else if (isGameRestart !== '2') return this.checkResetGame();
   }
 }
 
