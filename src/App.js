099673f8b5b2183f07e46070d23286b2c026ef29
criticbanggu <criticbanggu@gmail.com>
2023-10-20 13:43:25 +0900

    Feat :사용자에게 값입력 받아 숫자로 변환하는 기능
    async input(string Question), userInputDataToNumber(string getInputs)

--- src/App.js
+++ src/App.js
@@ -21,9 +21,25 @@ class App {
     }
   }
 
+  //사용자 입력값 숫자로 변환 하는 함수
+  userInputDataToNumber(getInputs) {
+    return getInputs.split("").map(Number);
+  }
+
+  //사용자에게 입력 받는 함수
+  async input(Question) {
+    let userNumber = await Console.readLineAsync(Question);
+    this.userInputs = this.userInputDataToNumber(userNumber);
+  }
+
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
     this.makeComputerRandomNumber();
+    while (this.score[STRIKE_INDEX] !== MAX_SCORE) {
+      try {
+        await this.input("숫자를 입력해주세요 : ");
+      } catch (error) {}
+    }
   }
 }
 const APP = new App();
