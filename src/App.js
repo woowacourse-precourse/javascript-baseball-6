c3658d402a3b771dc166b00d4db20e6374d879be
Goonco <s07019@naver.com>
2023-10-20 21:49:01 +0900

    :bulb: 1차 구현 : 코드 내 주석 수정

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   constructor() {
-    this.target;
-    this.targetLength = 3;
+    this.target; // 생성된 랜덤 숫자
+    this.targetLength = 3; // 랜덤 숫자의 길이
     this.gameState = 0; // 0 - 시작 | 1 - 재시작 | 2 - 종료
-    this.guess;
+    this.guess; // 사용자가 추측한 숫자
 
-    this.strike;
-    this.ball;
-    this.out;
+    this.strike; // 스트라이크 수
+    this.ball; // 볼 수
+    this.out; // out 여부
   }
 
   async play() {
@@ -29,21 +29,28 @@ class App {
     Console.print("게임 종료");
   }
 
+  /*
+    @initGame
+      #1 랜덤 숫자를 생성한다
+  */
   initGame() {
-    // 컴퓨터 내 랜덤 숫자 생성
+    // #1
     this.target = [];
     while (this.target.length < this.targetLength) {
       let number = Random.pickNumberInRange(1, 9);
       if (!this.target.includes(number)) this.target.push(number);
     }
-
-    // 첫 게임일 경우 시작 멘트 발생
-    // if (this.gameState == 0) Console.print("숫자 야구 게임을 시작합니다.");
     return;
   }
 
+  /*
+    @runSingleGuess
+      #0 사용자의 한번의 guess에 대한 입력부터 출력이 이루어진다
+      #1 입력 길이에 대한 예외를 처리한다
+  */
   async runSingleGuess() {
     await this.getGuess();
+    // #1
     try {
       if (this.guess.length > 3)
         throw new Error("[Error] 입력의 길이는 3 자리이여야 합니다.");
@@ -55,8 +62,13 @@ class App {
     return true;
   }
 
+  /*
+    @getGuess
+      #0 비동기 처리가 필요하다
+      #1 사용자로부터 입력을 받는다
+  */
   async getGuess() {
-    // 사용자 입력 받기, 비동기 처리 필요
+    // #1
     try {
       this.guess = await Console.readLineAsync(
         "숫자 야구 게임을 시작합니다.\n숫자를 입력해주세요 : "
@@ -67,13 +79,18 @@ class App {
     return;
   }
 
+  /*
+    @evalGuess
+      #1 결과값을 초기화 한다
+      #2 사용자의 입력을 평가한다
+  */
   evalGuess() {
-    // 결과값 초기화
+    // #1
     this.strike = 0;
     this.ball = 0;
     this.out = false;
 
-    // 사용자 입력 평가
+    // #2
     let toNum;
     this.target.map((val, idx) => {
       toNum = parseInt(this.guess[idx]);
@@ -84,6 +101,10 @@ class App {
     if (this.strike + this.ball == 0) this.out = true;
   }
 
+  /*
+    @tellResult
+      #1 입력결과를 평가한다
+  */
   tellResult() {
     let res = "";
     if (this.out) res += "낫싱";
@@ -94,9 +115,17 @@ class App {
     Console.print(res);
   }
 
+  /*
+    @endGame
+      #1 정답을 맞추었는지 확인한다
+      #2 정답 문구를 출력한다
+      #3 게임을 종료하거나 다시 시작한다
+  */
   async endGame() {
-    Console.print(this.strike);
-    if (this.strike != 3) return false; // 계속 질문하기
+    // #1
+    if (this.strike != 3) return false;
+
+    // #2 #3
     try {
       this.gameState = await Console.readLineAsync(
         "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
