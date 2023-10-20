df5d3856aa18b44dff01409c32fd4c6bc3a68f4c
kang <kangju2000@naver.com>
2023-10-20 15:30:29 +0900

    feat: 3자리 수인지 검증하는 메서드 추가

--- src/validator/Validator.js
+++ src/validator/Validator.js
@@ -1,8 +1,15 @@
 const Validator = {
-  isValidLength: (input) => { },
+  isValidLength: (input) => {
+    const SIZE = 3;
+
+    return String(input).length === SIZE;
+  },
   hasUniqueNumbers: (input) => { },
   isValidRange: (input) => { },
   isExitOrRestart: (input) => { },
+  isNumber: (input) => {
+    return !isNaN(input);
+  }
 }
 
 export default Validator;
