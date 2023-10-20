21105b01739bc1c319d2cbc2315de0b2dd878ae3
teawon <xodnjsvkvk9@gmail.com>
2023-10-21 06:01:11 +0900

    feat : 게임 시작 문구 및 초기 설정 로직 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    const answer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+
+    console.log(answer);
+  }
 }
 
 export default App;
