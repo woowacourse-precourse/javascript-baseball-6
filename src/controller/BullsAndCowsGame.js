7ca1de9da771ea5ff21c36586860bf50c3ecc77f
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-21 06:29:20 +0900

    :sparkles: feat: 게임종료시 사용될 메서드 추가 게임시작 안내 문구를 처음에만 나올수 있도록 생성자 안으로 위치 이동시킴, 사용자의 숫자와 컴퓨터의 숫자가 일치할 경우 게임종료 메서드 실행, 그 외의 경우 다시 입력받을 수 있도록 구현함

--- src/controller/BullsAndCowsGame.js
+++ src/controller/BullsAndCowsGame.js
@@ -7,15 +7,21 @@ class BullsAndCowsGame {
 
   constructor() {
     this.randomPicker = new RandomPicker();
-    this.randomPicker.getComputerNumber();
+    this.computerNumber = this.randomPicker.getComputerNumber();
+    OutputView.printStaticMessage(MESSAGES.START_GUIDE);
   }
 
   async startGame() {
-    OutputView.printStaticMessage(MESSAGES.START_GUIDE);
-    
     const userNumber = await InputView.getUserNumber(MESSAGES.INPUT_GUIDE);
-    console.log(userNumber);
+    console.log(userNumber, this.randomPicker.computerNumber);
+    
+    if (userNumber === this.computerNumber) return this.finishGame();
+    this.startGame();
   }
+
+  finishGame() {
+    return console.log('통과');
+  };
 };
 
 export default BullsAndCowsGame;
