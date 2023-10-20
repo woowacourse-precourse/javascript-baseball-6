39074a870431de604a90466178cf00cd3febb955
leeyulgok <dbfrhr20@gmail.com>
2023-10-20 16:21:38 +0900

    Feat: 힌트 및 결과생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -28,6 +28,31 @@ class App {
       console.error("숫자만 입력해주세요 : ", error);
     }
   }
+
+  checkNumber(com, user) {
+    let strikes = 0;
+    let balls = 0;
+
+    for(let i = 0; i < 3; i++) {
+      if(com[i] === user[i]) {
+        strikes++;
+      } else if(com.includes(user[i])) {
+        balls++;
+      }
+    }
+
+    if(strikes === 3) {
+      return "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    } else if(strikes === 0 && balls === 0) {
+      return "낫싱";
+    } else if(strikes === 0 && balls > 0) {
+      return `${balls}볼`;
+    } else if(strikes > 0 && balls === 0) {
+      return `${strikes}스트라이크`;
+    } else {
+      return `${balls}볼 ${strikes}스트라이크`;
+    }
+  }
 }
 
 const app = new App();
