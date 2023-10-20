f3715ec3c13e53eb830961b9a50eb593e4cab9bd
jw <always1997@kakao.com>
2023-10-20 22:59:00 +0900

    feat: 컴퓨터가 선택한 3개 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 class App {
-  async play() {}
+  async play() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    Console.print(computer);
+  }
 }
 
 export default App;
