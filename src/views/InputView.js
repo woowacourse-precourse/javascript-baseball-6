d726b6d3e44197488e03d69ec4e8ded249e0e68a
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-21 07:17:43 +0900

    :sparkles: feat: 재시작시 사용자 명령어 입력로직 구현 입력값이 1또는 2가 아닐 경우 오류 메세지 출력후 종료, 입력값을 숫자타입으로 변환해서 반환시킴

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -7,7 +7,14 @@ const InputView = {
       const userNumber = await Console.readLineAsync(message);
       if (!InputValidator.numberValidate(userNumber)) throw new Error (MESSAGES.INPUT_ERROR);
       return parseInt(userNumber, 10);
-  }
+  },
+
+  async getUserCommand (message) {
+    const userCommand = await Console.readLineAsync(message);
+    if (!InputValidator.commandValidate(userCommand)) throw new Error (MESSAGES.INPUT_ERROR);
+    return parseInt(userCommand, 10);
+  },
+
 };
 
 export default InputView;
