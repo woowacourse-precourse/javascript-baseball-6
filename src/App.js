cd7b6ea0d4ab54d92aeeb53f4a5e12b51e1c7547
chestnut1044 <wbk1620@gmail.com>
2023-10-20 19:13:56 +0900

    feat : 게임 시작 기능 구현
    게임 시작 메세지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import startGame from "./startGame.js";
 class App {
-  async play() {}
+  async play() {
+    startGame();
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
---
+++ src/startGame.js
@@ -0,0 +1,3 @@
+export default function startGame() {
+  console.log("숫자 야구 게임을 시작합니다.");
+}
