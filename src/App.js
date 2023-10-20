1a5c8c82c87ee5a12a3cd7b366112a3868587d6e
leeyulgok <dbfrhr20@gmail.com>
2023-10-20 16:46:36 +0900

    Feat: 재시작/종료 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,28 @@ class App {
   async play() {
     console.log("숫자 야구 게임을 시작합니다.");
     const COM_NUMBER = this.createNumber();
-    let userNumber = this.userNumber();
+    while (true) {
+      let userNumber = await this.userNumber();
+      let { strikes, balls } = this.checkNumber(COM_NUMBER, userNumber);
+
+      if (strikes === 3) {
+        console.log("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        let answer = await this.restart();
+        if (answer === 1) {
+          return this.play();
+        } else {
+          break;
+        }
+      } else if (strikes === 0 && balls === 0) {
+        console.log("낫싱");
+      } else if (strikes === 0 && balls > 0) {
+        console.log(`${balls}볼`);
+      } else if (strikes > 0 && balls === 0) {
+        console.log(`${strikes}스트라이크`);
+      } else {
+        console.log(`${balls}볼 ${strikes}스트라이크`);
+      }
+    }
   }
 
   createNumber() {
@@ -18,9 +39,9 @@ class App {
     return COMPUTER.join("");
   }
 
-  userNumber() {
+  async userNumber() {
     try {
-      const USERINPUT = MissionUtils.Console.readLineAsync(
+      const USERINPUT = await MissionUtils.Console.readLineAsync(
         "숫자를 입력해주세요 : "
       );
       return USERINPUT;
@@ -33,24 +54,25 @@ class App {
     let strikes = 0;
     let balls = 0;
 
-    for(let i = 0; i < 3; i++) {
-      if(com[i] === user[i]) {
+    for (let i = 0; i < 3; i++) {
+      if (com[i] === user[i]) {
         strikes++;
-      } else if(com.includes(user[i])) {
+      } else if (com.includes(user[i])) {
         balls++;
       }
     }
 
-    if(strikes === 3) {
-      return "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    } else if(strikes === 0 && balls === 0) {
-      return "낫싱";
-    } else if(strikes === 0 && balls > 0) {
-      return `${balls}볼`;
-    } else if(strikes > 0 && balls === 0) {
-      return `${strikes}스트라이크`;
-    } else {
-      return `${balls}볼 ${strikes}스트라이크`;
+    return { strikes, balls };
+  }
+
+  async restart() {
+    try {
+      const USERINPUT = await MissionUtils.Console.readLineAsync(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+      );
+      return USERINPUT;
+    } catch (error) {
+      console.error("숫자만 입력해주세요 : ", error);
     }
   }
 }
