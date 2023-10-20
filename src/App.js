cf407812a258fa8993ff4de396007839560bc654
nijuy <youjin6325@naver.com>
2023-10-20 18:03:57 +0900

    feat: MissionUtils.Random을 사용한 랜덤 숫자 생성 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    // 랜덤 값 생성하기
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    const random = computer.join('');
+    return random;
+  }
 }
 
 export default App;
