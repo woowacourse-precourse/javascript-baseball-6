af74260af774b323e414d8ae633f259a1083aa43
yoovin <soo22839@gmail.com>
2023-10-21 00:23:28 +0900

    chore: 정답 멘트 isUserWin 함수로 집어넣음

--- src/App.js
+++ src/App.js
@@ -16,9 +16,6 @@ class App {
                         break;
                     }
                 }
-                MissionUtils.Console.print(
-                    '3개의 숫자를 모두 맞히셨습니다! 게임 종료'
-                );
                 userinput = await MissionUtils.Console.readLineAsync(
                     '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
                 );
@@ -98,6 +95,9 @@ class App {
             printText += ` ${strike}스트라이크`;
             if (strike === 3) {
                 MissionUtils.Console.print(printText.trim());
+                MissionUtils.Console.print(
+                    '3개의 숫자를 모두 맞히셨습니다! 게임 종료'
+                );
                 return true;
             }
         }
