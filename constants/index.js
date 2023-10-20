56ab67f6230be9e6e2dc493a4f0f0aceb7c3fb0d
Kled <1017yu@daum.net>
2023-10-20 14:44:58 +0900

    fix: 에러 메시지 및 정규표현식 수정 (서로 다른 3자리 수)

--- constants/index.js
+++ constants/index.js
@@ -3,10 +3,10 @@ const constants = Object.freeze({
     START: '숫자 야구 게임을 시작합니다.',
     INPUT: '숫자를 입력해주세요 : ',
     ERROR: {
-      INVALID_COUNT_NUMBER: '서로 다른 3자리의 수를 입력해주세요.',
+      INVALID_COUNT_NUMBER: '[ERROR] 서로 다른 3자리의 수를 입력해주세요.',
     },
   },
-  THREE_DIGIT_PATTERN: new RegExp(/^[1-9]{3}$/g),
+  THREE_DIGIT_PATTERN: new RegExp(/^([1-9])(?!.*\1)([1-9])(?!.*\1|\2)([1-9])$/),
 });
 
 export default constants;
