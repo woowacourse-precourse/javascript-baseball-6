547edfbd3759db37733d32a37b5f076de9fbefd0
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-21 07:16:28 +0900

    :sparkles: feat: 게임종료와 재시작 문구 추가

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -2,4 +2,6 @@ export const MESSAGES = {
   START_GUIDE: '숫자 야구 게임을 시작합니다.',
   INPUT_GUIDE: '숫자를 입력해주세요 : ',
   INPUT_ERROR: '[ERROR] 숫자가 잘못된 형식입니다.',
+  END_GUIDE: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  REPLAY_GUIDE: `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`,
 };
