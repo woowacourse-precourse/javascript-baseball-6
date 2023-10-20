a238d85ff1209bd469f47edc5e5e274ef95c42bb
yyeonkim <seyeon.dev@gmail.com>
2023-10-20 16:32:39 +0900

    feat: 컴퓨터 숫자 랜덤으로 3개 선택

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.nothing = false;
+    this.strike = 0;
+    this.ball = 0;
+    this.computer = [];
+    this.user = [];
+  }
+
+  async play() {
+    // 컴퓨터 랜덤으로 숫자 3개 선택
+    while (this.computer.length < 3) {
+      const num = MissionUtils.Random.pickNumberInRange(1, 9);
+      this.computer.push(num);
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
