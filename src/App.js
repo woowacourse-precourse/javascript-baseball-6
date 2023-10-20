c4d5644996186a43933ee7d18a047cd44a09317e
sohyun <kshyun0215@gmail.com>
2023-10-20 16:04:09 +0900

    feat: 컴퓨터 랜덤값 생성 및 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  constructor() {
+    this.randomNumber = null;
+  }
+
+  createRandomNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    this.randomNumber = computer;
+  }
+
+  async play() {
+    this.createRandomNumber();
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
