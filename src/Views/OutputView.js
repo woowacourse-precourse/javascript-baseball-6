bddc9b04f648588518fee38695d7ccb93efa5402
thewronghand <penfreak77@gmail.com>
2023-10-20 23:24:34 +0900

    refactor: OutputView 책임 분리
    결과에 따라 승리 메세지, 낫싱 메세지, 점수 메세지로 결과 문자열을 설정하는 부분을 별도 파일로 분리

--- src/Views/OutputView.js
+++ src/Views/OutputView.js
@@ -1,9 +1,6 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import {
-  GAME_CONSTANTS,
-  GAME_MESSAGES,
-  GAME_RESULTS,
-} from "../utils/constants.js";
+import { GAME_MESSAGES } from "../utils/constants.js";
+import { getResultString } from "../utils/viewUtils.js";
 
 export default class OutputView {
   printStart() {
@@ -11,34 +8,7 @@ export default class OutputView {
   }
 
   printMatchResult(matchResult) {
-    const [ball, strike] = matchResult;
-    if (strike === GAME_CONSTANTS.STRIKE_OUT_COUNT) {
-      this.#printGameWin(matchResult);
-      return;
-    }
-    if (!ball && !strike) {
-      this.#printNoMatch();
-      return;
-    }
-    const resultText = this.#getResultString(matchResult);
+    const resultText = getResultString(matchResult);
     MissionUtils.Console.print(resultText);
-  }
-
-  #getResultString(matchResult) {
-    const text = [GAME_RESULTS.BALL, GAME_RESULTS.STRIKE];
-    const parsedResults = matchResult.map((item, idx) => {
-      if (!item) return;
-      return item.toString() + text[idx];
-    });
-    return parsedResults.join(" ");
-  }
-
-  #printGameWin(matchResult) {
-    const strikeText = this.#getResultString(matchResult);
-    MissionUtils.Console.print(`${strikeText}\n${GAME_MESSAGES.FINISH}`);
-  }
-
-  #printNoMatch() {
-    MissionUtils.Console.print(GAME_RESULTS.NO_MATCH);
   }
 }
---
+++ src/utils/viewUtils.js
@@ -0,0 +1,17 @@
+import { GAME_CONSTANTS, GAME_MESSAGES, GAME_RESULTS } from "./constants";
+
+export function getResultString([ball, strike]) {
+  if (!ball && !strike) {
+    return GAME_RESULTS.NO_MATCH;
+  }
+  const text = [GAME_RESULTS.BALL, GAME_RESULTS.STRIKE];
+  const parsedResults = [ball, strike].map((item, idx) => {
+    if (!item) return;
+    return item.toString() + text[idx];
+  });
+  const resultString = parsedResults.join(" ");
+  if (strike === GAME_CONSTANTS.STRIKE_OUT_COUNT) {
+    return `${resultString}\n${GAME_MESSAGES.FINISH}`;
+  }
+  return resultString;
+}
