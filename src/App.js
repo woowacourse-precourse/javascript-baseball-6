1ddff520559e87108a1dc8ea964d382d1f4011be
eternalclash <suchasucha@naver.com>
2023-10-20 11:37:16 +0900

    Feat: 게임 시작 출력값 및 입력값 받기 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { Console } from "@woowacourse/mission-utils";
 class App  {
+  constructor(){
+    this.init();
+  }
+
+  init () {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
+  
   async play() {
+    try{
+      const numberInput = await Console.readLineAsync("숫자를 입력해주세요 : ")
+    }
+    catch (error) {
+
+    }
 
   }
-}
+  async invalidateInput (query) {
 
+  }
+  
+}
+const app = new App()
+app.play()
 export default App;
