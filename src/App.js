ae6227b49af1891ad36bb8bb7c5b5c4efce7e10f
장호빈 <devhobin@gmail.com>
2023-10-20 19:14:38 +0900

    refactor: 전체적으로 더 적절한 인터페이스로 수정

--- src/App.js
+++ src/App.js
@@ -13,35 +13,23 @@ class App {
     await this._transition("playing");
   }
 
-  async restart() {
+  async _restart() {
     this._answer = this._createAnswer();
     await this._transition("playing");
   }
 
   async _transition(status) {
     this._status = status;
-    await this._entryEffect(this._status);
+    await this._transitionEffect(this._status);
   }
 
-  _createAnswer() {
-    const answer = [];
-    while (answer.length < 3) {
-      const number = String(MissionUtils.Random.pickNumberInRange(1, 9));
-      if (!answer.includes(number)) {
-        answer.push(number);
-      }
-    }
-
-    return answer.join("");
-  }
-
-  async _entryEffect(status) {
+  async _transitionEffect(status) {
     switch (status) {
       case "playing": {
         const playerInput = await Console.readLineAsync(
           "숫자를 입력해주세요 : "
         );
-        this._invalid(playerInput);
+        this._invariant(playerInput);
 
         const score = this._createScore(playerInput);
         const { strike } = score;
@@ -55,6 +43,7 @@ class App {
         }
         break;
       }
+
       case "clear": {
         Console.print("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
@@ -66,7 +55,7 @@ class App {
         }
 
         if (playerInput === RESTART) {
-          await this.restart();
+          await this._restart();
         } else if (playerInput === QUIT) {
           await this._transition("end");
         }
@@ -75,6 +64,18 @@ class App {
       default:
         break;
     }
+  }
+
+  _createAnswer() {
+    const answer = [];
+    while (answer.length < 3) {
+      const number = String(MissionUtils.Random.pickNumberInRange(1, 9));
+      if (!answer.includes(number)) {
+        answer.push(number);
+      }
+    }
+
+    return answer.join("");
   }
 
   _createScore(playerInput) {
@@ -93,18 +94,6 @@ class App {
     return score;
   }
 
-  _invalid(answer) {
-    const numberRegex = /^[1-9]+$/;
-
-    if (answer.length === 0) throw new Error("[ERROR] 숫자를 입력해야합니다.");
-    if (answer.includes(" "))
-      throw new Error("[ERROR] 입력값에 공백이 포함되어 있습니다.");
-    if (!numberRegex.test(answer) || new Set(answer).size !== 3)
-      throw new Error(
-        "[ERROR] 1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 입력해야합니다."
-      );
-  }
-
   _createHint(score) {
     const { strike, ball } = score;
 
@@ -113,6 +102,19 @@ class App {
     if (strike > 0) hints.push(`${strike}스트라이크`);
 
     return hints.length === 0 ? "낫싱" : hints.join(" ");
+  }
+
+  _invariant(playerInput) {
+    const numberRegex = /^[1-9]+$/;
+
+    if (playerInput.length === 0)
+      throw new Error("[ERROR] 숫자를 입력해야합니다.");
+    if (playerInput.includes(" "))
+      throw new Error("[ERROR] 입력값에 공백이 포함되어 있습니다.");
+    if (!numberRegex.test(playerInput) || new Set(playerInput).size !== 3)
+      throw new Error(
+        "[ERROR] 1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 입력해야합니다."
+      );
   }
 }
 
