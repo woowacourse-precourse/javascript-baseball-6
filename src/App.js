5cf06e6fee7a7239b8aacc826d22159e26c80fa8
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-10-20 22:43:58 +0900

    feat: 로직 완성 낫싱에서 버그 0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 // let Console = MissionUtils.Console;
 // let Random = MissionUtils.Random;
-function makeAnswer() {
+function makeRandom() {
   const answer = [];
   while (answer.length < 3) {
     const number = MissionUtils.Random.pickNumberInRange(1, 9);
@@ -12,11 +12,9 @@ function makeAnswer() {
   console.log(answer);
   return answer;
 }
-async function getNumber() {
+async function getUserInput(message) {
   try {
-    const number = await MissionUtils.Console.readLineAsync(
-      "숫자를 입력해주세요 :"
-    );
+    const number = await MissionUtils.Console.readLineAsync(message);
     return number;
   } catch (error) {
     throw error;
@@ -33,7 +31,6 @@ let SCORE = {
 function review(answer, number) {
   //컴퓨터의 숫자를 순회하여 사용자의 숫자와 비교한다
   //findindex 로 스트라이크 갯수 변수와 볼 변수 낫싱 변수를 체크한다.
-  //세변수 모두 0이라면 3개의 숫자를 모두 맞히셨습니다! 게임 종료 출력하고
   console.log(answer, number);
   for (let i = 0; i < answer.length; i++) {
     let index = answer.findIndex((el) => el === number[i]);
@@ -44,43 +41,68 @@ function review(answer, number) {
       SCORE.볼++;
     }
   }
-  // if (SCORE.볼 === 0 && SCORE.스트라이크 === 0) {
-  //   SCORE.낫싱++;
-  //   return;
-  // }
-  // if (SCORE.스트라이크 === 3) {
-  //   score.성공 = 1;
-  //   return;
-  // }
+
   return SCORE;
 }
-class App {
-  constructor() {
-    this.answer = answer;
+function resetScore() {
+  SCORE.볼 = 0;
+  SCORE.스트라이크 = 0;
+}
+function printResult(SCORE) {
+  if (SCORE.볼 === 0 && SCORE.스트라이크 === 0) {
+    //낫싱
+    MissionUtils.Console.print("낫싱");
+    return true;
   }
-  async play() {
-    //게임 시작문구를 출력한다.
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-
-    //사용자에게 서로다른 숫자 3개를 입력받는다. 숫자를 입력해주세요 :
-    let number = getNumber();
-    number
-      .then((num) => {
-        let isError = checkError(num);
-        if (!isError) {
-          return num;
+  if (SCORE.스트라이크 === 3) {
+    //성공
+    MissionUtils.Console.print(
+      "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+    );
+    return false;
+  }
+  if (!SCORE.볼 && !SCORE.스트라이크) {
+    MissionUtils.Console.print(
+      SCORE.볼 + "볼" + " " + SCORE.스트라이크 + "스트라이크"
+    );
+    return true;
+  }
+  let text = SCORE.볼 ? SCORE.볼 + "볼" : SCORE.스트라이크 + "스트라이크";
+  MissionUtils.Console.print(text);
+  return true;
+}
+async function getSomting() {
+  getUserInput("숫자를 입력해주세요 :")
+    .then((num) => {
+      if (!checkError(num)) {
+        return num;
+      }
+    })
+    .then((num) => {
+      return review(answer, num);
+    })
+    .then((result) => {
+      let isContinue = printResult(result);
+      if (isContinue) {
+        resetScore();
+        getSomting();
+      }
+      getUserInput("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.").then(
+        (num) => {
+          if (num === "1") {
+            app.play();
+          }
+          //종료
+          return;
         }
-      })
-      .then((num) => {
-        console.log("검증통과");
-        review(answer, num);
-      });
-    //컴퓨터는 1~9까지 서로다른 임의수 3개를 선택
-    const answer = makeAnswer();
-
-    // MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    // 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. 것을 출력한다.
-    //입력받은 수가 1이면 시작 2를 하면 종료를 한다.
+      );
+    });
+}
+class App {
+  async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    const answer = makeRandom();
+    getSomting();
   }
 }
 
