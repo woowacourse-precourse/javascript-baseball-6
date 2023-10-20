668078176c258ae265cea363315c3e4e4a174b3e
ChoiYongWon <yongwon0824@naver.com>
2023-10-20 20:48:16 +0900

    feat: App 생성자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.initialize();
+  }
+
+  initialize(){
+    this.randomNumberList = [];
+    this.ball = 0;
+    this.strike = 0;
+    this.isPlaying = true;
+    while (this.randomNumberList.length < 3) {
+      const RANDOM_NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.randomNumberList.includes(RANDOM_NUMBER))
+        this.randomNumberList.push(RANDOM_NUMBER);
+    }
+  }
+
+
+
+  async play() {
+
+  }
 }
+
 
 export default App;
