98b1f9f0bc971e54fc29fcaec6a582034c135b18
silver0108 <kk1170@naver.com>
2023-10-21 02:33:51 +0900

    feat: 컴퓨터 정답 생성 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+
+function generateComputeNum(){
+  const computerNum = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computerNum.includes(number)) {
+      computerNum.push(number);
+    }
+  }
+  return computerNum;
+}
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+
+    let computerNum = generateComputeNum();
+    MissionUtils.Console.print(c_num);
+  }
 }
 
 export default App;
