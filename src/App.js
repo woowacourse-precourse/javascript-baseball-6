2c706957c6feaf28a0b6a1c3c9bd0a13cd583eb0
ChoiYongWon <yongwon0824@naver.com>
2023-10-20 20:57:43 +0900

    feat: 스트라이크 & 볼 검증 로직

--- src/App.js
+++ src/App.js
@@ -17,6 +17,15 @@ class App {
     }
   }
 
+  executeInput(input) {
+    let input_list = input.split("").map((data) => Number(data));
+    this.ball = 0;
+    this.strike = 0;
+    for (let i = 0; i < input_list.length; i++) {
+      if (this.randomNumberList[i] == Number(input_list[i])) this.strike++;
+      else if (this.randomNumberList.includes(input_list[i])) this.ball++;
+    }
+  }
 
   async play() {
 
@@ -25,8 +34,7 @@ class App {
   
       while(this.strike < 3){
         let input = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
-
-        
+        this.executeInput(input);
         if(this.ball == 0 && this.strike == 0) MissionUtils.Console.print("낫싱")
         else {
           MissionUtils.Console.print(`${this.ball > 0 ? `${this.ball}볼` : ""}${this.ball > 0 && this.strike > 0 ? " " : ""}${this.strike > 0 ? `${this.strike}스트라이크` : ""}`);
