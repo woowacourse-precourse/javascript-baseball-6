8657d371cfad0a5a851ae3d4153cdb4eaae1e03a
MinGu Jeong <mingu830@gmail.com>
2023-10-21 00:40:17 +0900

    feat: 랜덤한 컴퓨터 수 생성

--- src/App.js
+++ src/App.js
@@ -12,7 +12,16 @@ function getUserNumber() {
   );
 }
 
+function getComputerNumber() {
+  const computerNumber = Array.from({ length: 3 }, () =>
+    MissionUtils.Random.pickNumberInRange(1, 9)
+  ).join("");
+  console.log(`컴퓨터가 생성한 숫자 : ${computerNumber}`);
+  return computerNumber;
+}
+
 export default App;
 
 console.log(MissionUtils.Console.print("숫자 야구 게임을 시작합니다."));
 getUserNumber();
+getComputerNumber();
