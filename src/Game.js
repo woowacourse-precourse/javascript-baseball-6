ad6d778ef9ef46dc1e17d82d3d3926a34a40788a
dabinchiii <dabinchi388@naver.com>
2023-10-21 03:24:36 +0900

    feat: 서로 다른 3개의 숫자를 임의로 선택하여 저장하는 메소드 추가

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,20 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 class Game {
+  computerNums = [];
+
   start = () => {
     Console.print("숫자 야구 게임을 시작합니다.");
+  };
+
+  setComputerNums = () => {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    this.computerNums = computer;
   };
 }
 
