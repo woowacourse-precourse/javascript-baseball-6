ca1849ceb222617f661e039c0d00123610c37762
박지윤 <tkrhkfld123@naver.com>
2023-10-21 00:51:51 +0900

    test: 사용자 숫자 입력 받기 테스트 완료

--- __tests__/functionsTest.js
+++ __tests__/functionsTest.js
@@ -1,6 +1,15 @@
 import App from '../src/App'
-import MissionUtils from "@woowacourse/mission-utils";
+import {MissionUtils} from "@woowacourse/mission-utils";
 
+
+const mockQuestions = (answers) => {
+    MissionUtils.Console.readLine = jest.fn();
+    answers.reduce((acc, input) => {
+      return acc.mockImplementationOnce((question, callback) => {
+        callback(input);
+      });
+    }, MissionUtils.Console.readLine);
+  };
 
 describe("야구 게임 테스트", () => {
   test("시작 메세지 출력", () => {
@@ -11,6 +20,17 @@ describe("야구 게임 테스트", () => {
     app.startGame();
 
     expect(logSpy).toHaveBeenCalledWith('숫자 야구 게임을 시작합니다.');
+  });
+
+  test("사용자 숫자 입력 받기", () => {
+    const userNumber = ["456"];
+
+    mockQuestions(userNumber);
+
+    const app = new App();
+    const result = app.selectUser(); 
+
+    expect(result).toEqual("456");
   });
 
 });
