748bdb7ffa813ae28f9e3093dad6582fa210b6e4
hellosehyun <sehyun030526@gmail.com>
2023-10-20 09:41:39 +0900

    add func getComputerNum

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+    async play() {
+        // 상대방 숫자 생성기
+        function getComputerNum() {
+            let l = [];
+            for (let i = 0; i < 3; i++) {
+                l.push(MissionUtils.Random.pickNumberInRange(1, 9));
+            }
+            return l;
+        }
+        let computerNum = [...getComputerNum()];
+    }
 }
+
+const app = new App();
+app.play();
 
 export default App;
