2a48462a0f863822091d94b704060f8f4a7f0813
j8won <joowoni99@gmail.com>
2023-10-20 21:31:10 +0900

    feat: 컴퓨터와 사용자의 숫자를 비교해 볼과 스트라이크 개수를 판단하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Console } from "@woowacourse/mission-utils";
 import MESSAGE from "./constant/MESSAGE.js";
 import Computer from "./Computer.js";
 import User from "./User.js";
+import NUMBER from "./constant/NUMBER.js";
 
 class App {
   constructor() {
@@ -11,6 +12,15 @@ class App {
 
   async play() {
     await this.startGame();
+
+    let judgeResult = this.calculateResult(
+      this.computerNumber,
+      this.userNumber
+    );
+
+    this.printMessage(this.computerNumber)
+      .printMessage("볼 " + judgeResult.ballCount)
+      .printMessage("스트라이크: " + judgeResult.strikeCount);
   }
 
   printMessage(message) {
@@ -22,6 +32,29 @@ class App {
     this.printMessage(MESSAGE.START_GAME);
     this.computerNumber = new Computer().selectedNumberArray;
     this.userNumber = await new User().inputNumberArray;
+  }
+
+  calculateResult(computer, user) {
+    let strikeCount = this.judgeStrike(computer, user);
+    let ballCount = this.judgeBall(computer, user);
+
+    return { strikeCount, ballCount };
+  }
+
+  judgeStrike(arrayX, arrayY) {
+    let strikeCount = 0;
+    for (let i = 0; i < NUMBER.LENGTH; i++) {
+      if (arrayX[i] === arrayY[i]) strikeCount++;
+    }
+    return strikeCount;
+  }
+
+  judgeBall(arrayX, arrayY) {
+    let ballCount = 0;
+    for (let i = 0; i < NUMBER.LENGTH; i++) {
+      if (arrayX.includes(arrayY[i]) && arrayX[i] !== arrayY[i]) ballCount++;
+    }
+    return ballCount;
   }
 }
 
