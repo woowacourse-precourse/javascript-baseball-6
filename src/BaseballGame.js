b658c6591a91d59fd60d0a0d45f69bcd5d6547df
kimdw524 <kimdw524@naver.com>
2023-10-20 22:13:11 +0900

    feat: ball, strike를 구하는 함수

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -4,7 +4,7 @@ class BaseballGame {
   #answer
 
   constructor() {
-    this.#answer = 0;
+    this.#answer = [];
   }
 
   // 각 자리 수가 중복되지 않은 무작위의 세 자리의 수를 반환
@@ -14,6 +14,18 @@ class BaseballGame {
       set.add(Random.pickNumberInRange(1, 9));
     }
     return [...set];
+  }
+
+  // 입력값과 정답을 비교해 ball, strike 수를 반환
+  getResult(guess) {
+    const result = this.#answer.reduce(({ ball, strike }, value, index) =>
+      ({
+        ball: ball + (guess[index] != value && guess.includes(value) ? 1 : 0),
+        strike: strike + (guess[index] == value ? 1 : 0)
+      }),
+      { ball: 0, strike: 0 }
+    );
+    return result;
   }
 }
 
