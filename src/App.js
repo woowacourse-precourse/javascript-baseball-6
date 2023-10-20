e23f6d3926fb0f6ad19fddd8517465defa822e3b
daeun-Kim <82816029+ddaeunbb@users.noreply.github.com>
2023-10-20 17:54:44 +0900

    [Feat]: 사용자 입력값과 입력값을 판별하는 기능 구현 (#3)
    - `getUserInput` 메서드로 사용자의 입력값을 받습니다.
    - `validateInput` 메서드를 사용해서 숫자수, 정수, 음수인지 판별합니다.
    - 판별을 통과한다면 `userInput`라는 변수로 초기화해 배열로 저장합니다.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const { Console, Random } = MissionUtils;
 
 const MESSAGE = {
-  START: "숫자 야구 게임을 시작합니다.",
+  START: '숫자 야구 게임을 시작합니다.',
+  INPUT: '숫자를 입력해주세요 : ',
+}
+
+const ERROR_MESSAGE = {
+  LENGTH: "[ERROR] 세 자리 숫자만 입력해야합니다.",
+  INT: "[ERROR] 정수만 입력해야합니다.",
+  NEGATIVE: "[ERROR] 양수만 입력해야합니다.",
 }
 class App {
   constructor(){
     this.randomNumber = [];
+    this.userInput = [];
+
+    this.validation = {
+      isLenThree: (input) => input.length === 3,
+      isInt: (input) => Number.isInteger(+input),
+      isNegative: answer => Math.sign(answer) === -1,
+    }
   }
 
   async play() {
     this.printStartNotification();
     this.generateRandomNumber();
+    await this.getUserInput();
   }
 
   printStartNotification(){
@@ -25,6 +40,20 @@ class App {
       if(!randomNumber.includes(number)) randomNumber.push(number);
     }
     // Console.prit(randomNumber); [1,3,5]
+  }
+
+  async getUserInput(){
+    const input = await Console.readLineAsync(MESSAGE.INPUT);
+    this.validateInput(input);
+
+    this.userInput = [...input];
+  }
+
+  validateInput(input){
+    const { isLenThree, isInt, isNegative } = this.validation;
+    if(!isLenThree(input)) throw new Error(ERROR_MESSAGE.LENGTH);
+    else if(!isInt(input)) throw new Error(ERROR_MESSAGE.INT);
+    else if(!isNegative(input)) throw new Error(ERROR_MESSAGE.NEGATIVE);
   }
 }
 
