52cea26dff73332da3ba07aa45a7f8b7954dce2c
jaeochoii <jaeochoiii0716@inha.edu>
2023-10-20 17:38:01 +0900

    fix: 메시지 출력 값 수정

--- src/constant/Constant.mjs
+++ src/constant/Constant.mjs
@@ -2,7 +2,7 @@ const GuideMessage = Object.freeze({
   START_GAME: "숫자 야구 게임을 시작합니다.",
   RESTART_GAME: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
   INPUT_NUMBER: "숫자를 입력해주세요 :",
-  CORRECT_NUMBER: "3개의 숫자를 모두 맞히셨습니다.",
+  CORRECT_NUMBER: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
 });
 
 const ErrorMessage = Object.freeze({
