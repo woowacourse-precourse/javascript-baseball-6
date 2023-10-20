1bec778adf27bc9a751f07820acf02cc4b538b5b
solssak <gswoong1012@gmail.com>
2023-10-20 18:08:02 +0900

    feat: 기능1 - 게임 시작 알림 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  showStartMessage() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  async play() {
+    this.showStartMessage();
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
