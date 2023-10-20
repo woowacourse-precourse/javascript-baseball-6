abce0d495fd12e465e1ad92e1e644ee058eed8b6
daeun-Kim <82816029+ddaeunbb@users.noreply.github.com>
2023-10-20 17:34:50 +0900

    [Feat]: 랜덤으로 숫자를 뽑는 기능 구현 (#2)
    - `generateRandomNumber` 메서드를 구현하여, 랜덤으로 숫자를 뽑게 하였습니다.
    - 순차적으로 1~9 사이의 숫자를 뽑으며 중복은 없습니다.
    - 뽑힌 숫자는 `randomNumber`로 초기화하여 배열로 저장하였습니다.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,26 @@ const MESSAGE = {
   START: "숫자 야구 게임을 시작합니다.",
 }
 class App {
+  constructor(){
+    this.randomNumber = [];
+  }
+
   async play() {
     this.printStartNotification();
+    this.generateRandomNumber();
   }
 
   printStartNotification(){
     Console.print(MESSAGE.START);
+  }
+  
+  generateRandomNumber(){
+    const { randomNumber } = this;
+    while(randomNumber.length < 3){
+      const number = Random.pickNumberInRange(1, 9);
+      if(!randomNumber.includes(number)) randomNumber.push(number);
+    }
+    // Console.prit(randomNumber); [1,3,5]
   }
 }
 
