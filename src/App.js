b563c14def6141766a072cdb85491c70a069211a
Jules <dali1999@naver.com>
2023-10-20 17:27:15 +0900

    feat: 게임 실행 로직 while문에 넣어서 break로 종료되도록 구현 및 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import playBaseball from "./game/gamePlay.js";
+import { Console } from "@woowacourse/mission-utils";
+// import playBaseball from "./game/gamePlay.js";
+import handleUserInput from "./handleAnswer/handleUserInput.js";
+import printHint from "./game/gameHint.js";
+import makeAnswer from "./handleAnswer/makeAnswer.js";
 
+let computerAnswer = makeAnswer();
 class App {
   async play() {
-    playBaseball();
+    while (true) {
+      Console.print(computerAnswer);
+
+      const userInput = await Console.readLineAsync("숫자를 입력해주세요 :");
+      let userAnswer = handleUserInput(userInput); //예외 처리 후 userInput을 반환
+      //  ballmanager(computerAnswer, userAnswer);
+      if (computerAnswer === userAnswer) {
+        Console.print("3스트라이크");
+        Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        const replay = await Console.readLineAsync(
+          "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+        );
+        if (replay === "1") {
+          computerAnswer = makeAnswer();
+          continue;
+        } else if (replay === "2") {
+          Console.print("게임 종료");
+          break;
+        }
+      } else {
+        printHint(computerAnswer, userAnswer);
+        continue;
+      }
+    }
   }
 }
 
--- src/game/gamePlay.js
+++ src/game/gamePlay.js
@@ -3,37 +3,74 @@ import handleUserInput from "../handleAnswer/handleUserInput.js";
 import printHint from "./gameHint.js";
 import makeAnswer from "../handleAnswer/makeAnswer.js";
 
-function reset() {
-  Console.readLine("게임이 끝났습니다. 다시 플레이 하시겠습니까?", (replay) => {
-    if (replay === '1') {
-      computerAnswer = makeAnswer();
-      init();
-    } else {
-      throw new Error("Game over");
-    }
-  });
-}
+// const computerAnswer = makeAnswer();
+// while (true) {
+//   Console.print(computerAnswer);
 
-function ballmanager(computerAnswer, userAnswer) {
-  if (computerAnswer === userAnswer) {
-    Console.print("3스트라이크");
-    reset();
-  } else {
-    printHint(computerAnswer, userAnswer);
-    typeAnswer(computerAnswer);
-  }
-}
+//   const userInput = await Console.readLineAsync("숫자를 입력해주세요 :");
+//   let userAnswer = handleUserInput(userInput); //예외 처리 후 userInput을 반환
+//   //  ballmanager(computerAnswer, userAnswer);
+//   if (computerAnswer === userAnswer) {
+//     Console.print("3스트라이크");
+//     Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+//     const replay = await Console.readLineAsync(
+//       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+//     );
+//     if (replay === "1") {
+//       continue;
+//     } else if (replay === "2") {
+//       Console.print("게임 종료");
+//       break;
+//     }
+//   } else {
+//     printHint(computerAnswer, userAnswer);
+//     continue;
+//   }
+// }
+// async function readUserInput() {
+//   const userInput = await Console.readLineAsync("숫자를 입력해주세요 :");
+//   let userAnswer = handleUserInput(userInput); //예외 처리 후 userInput을 반환
+//   // Console.print(userAnswer);
+//   ballmanager(computerAnswer, userAnswer);
+// }
+// function typeAnswer(computerAnswer) {
+//   async function readUserInput() {
+//     const userInput = await Console.readLineAsync("숫자를 입력해주세요 :");
+//     let userAnswer = handleUserInput(userInput); //예외 처리 후 userInput을 반환
+//     // Console.print(userAnswer);
+//     ballmanager(computerAnswer, userAnswer);
+//   }
+//   readUserInput();
+// }
 
-function typeAnswer(computerAnswer) {
-  Console.readLine("숫자를 입력해주세요 :", (userInput) => {
-    const userAnswer = handleUserInput(userInput);
-    Console.print(userAnswer);
-    ballmanager(computerAnswer, userAnswer);
-  });
-}
+// function ballmanager(computerAnswer, userAnswer) {
+//   if (computerAnswer === userAnswer) {
+//     Console.print("3스트라이크");
+//     Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+//     reset();
+//   } else {
+//     printHint(computerAnswer, userAnswer); //볼, 스트라이크, 낫싱을 출력
+//     typeAnswer(computerAnswer);
+//   }
+// }
 
-export default function playBaseball() {
-  const computerAnswer = makeAnswer();
-  Console.print(computerAnswer);
-  typeAnswer(computerAnswer);
-}
+// function reset() {
+//   async function readUserInput() {
+//     const replay = await Console.readLineAsync(
+//       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+//     );
+//     if (replay === "1") {
+//       playBaseball();
+//     } else if (replay === "2") {
+//       Console.print("게임 종료");
+//       process.exit();
+//     }
+//   }
+//   readUserInput();
+// }
+
+// export default function playBaseball() {
+//   const computerAnswer = makeAnswer();
+//   Console.print(computerAnswer);
+//   typeAnswer(computerAnswer);
+// }
