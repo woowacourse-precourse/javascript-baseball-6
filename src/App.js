5a7ae505204f2798f0539decbb232d6dc4b265bf
GimGayeon <kky1373@naver.com>
2023-10-20 23:15:49 +0900

    feat: Add userInput

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+
 class App {
-  async play() {}
+  RANDOM_NUMBER;
+  USER_NUMBER;
+
+  getUserInput() {
+    MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ", (number) => {
+      if (!this.testNumber(number))
+        throw new Error(
+          "잘못된 값을 입력하셨습니다. 숫자를 다시 입력해주세요."
+        );
+      this.USER_NUMBER = number;
+      this.numberOfHits(number);
+      // this.getPrint(number);
+    });
+  }
+
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    getUsername();
+  }
 }
 
-export default App;
+const app = new App();
+app.play();
+export default App.js;
