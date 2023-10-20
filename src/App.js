4c98e56e205adef704199321e9d8cee16700a898
Y.W KIM <kyw624@gmail.com>
2023-10-20 16:34:36 +0900

    Feat: 게임 클래스의 init(), getRandomAnswer() 메서드 구현
    - init(): 게임을 초기화하는 메서드.
    - getRandomAnswer(): 서로 다른 3자리 숫자의 정답을 생성하는 메서드.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,53 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 class App {
-  async play() {}
+  async play() {
+    try {
+      const game = new NumberBaseball();
+
+      await game.run();
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
+class NumberBaseball {
+  constructor() {
+    this.computer = null;
+  }
+
+  // 게임 시작
+  async run() {
+    try {
+      this.init();
+    } catch (err) {}
+  }
+
+  // 게임 초기화
+  init() {
+    this.computer = this.getRandomAnswer();
+  }
+
+  // 컴퓨터 정답 랜덤하게 생성
+  getRandomAnswer() {
+    const computer = [];
+
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+
+    return computer;
+  }
+
+  // 유효값 검증
+  validateInput() {}
 }
 
 export default App;
+
+const app = new App();
+app.play();
