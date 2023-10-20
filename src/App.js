f4d98b234144b0e2274d3bf37a19a5fbebb46096
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-10-20 15:51:30 +0900

    feat:비밀숫자생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    let computer = "";
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer += number;
+      }
+    }
+    // console.log(computer, "출력값");
+  }
 }
 
 export default App;
