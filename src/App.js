e4ac3cbae82c784d7e569f3fe0b4359b362a4931
gouz7514 <gouz7514@gmail.com>
2023-10-20 12:05:24 +0900

    Update : 게임 시작 문구 추가

--- src/App.js
+++ src/App.js
@@ -34,8 +34,8 @@ class App {
   };
 
   async play() {
+    Console.print('숫자 야구 게임을 시작합니다.')
     const computerNumbers = this.getCompuerNumbers();
-    Console.print(computerNumbers);
     const userNumbers = await this.getUserNumbers();
     Console.print(userNumbers);
   };
