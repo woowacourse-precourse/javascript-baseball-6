19180331b9ea91fd9f2ae501a3b19f29f4252ef0
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-10-20 13:48:53 +0900

    Feat: 유저 입력 기능 완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import { Console,MissionUtils } from "@woowacourse/mission-utils";
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import readline from "readline";
+
 class App {
   constructor() {
     this.computerNumbers = this.computerNumbers();
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
   }
 
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
-    Console.print(this.computerNumbers)
+    this.playGame();
+  }
+
+  playGame() {
+    this.rl.question(`숫자를 입력해주세요: `, (answer) => {
+      const playerNumbers = answer.split("").map((number) => parseInt(number));
+      Console.print(playerNumbers)
+      this.rl.close();
+    });
   }
 
   computerNumbers() {
@@ -19,6 +33,7 @@ class App {
     }
     return numbers;
   }
+
 }
 
 const app = new App();
