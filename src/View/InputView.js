309e8dcdfd89e01e732a6ae7b48527bbb33ae20f
chiman <rlaclghks123@naver.com>
2023-10-20 15:51:35 +0900

    feat: InputView의 confirmContinue 함수 구현
    3스트라이크를 맞춘 경우 게임을 새로 시작할지, 종료할지 사용자에게 값을 입력받는 함수

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -8,6 +8,14 @@ const InputView = {
       console.log('getUserNumber Error', error);
     }
   },
+
+  async confirmContinue() {
+    try {
+      return await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+    } catch (error) {
+      console.log('confirmContinue Error =>  ', error);
+    }
+  },
 };
 
 export default InputView;
