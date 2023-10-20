0701f4ade52fb2294a6bcf27a6fc43d59673e621
haesa <xdhaesa@gmail.com>
2023-10-20 20:45:19 +0900

    [Feat]: 플레이어 야구 숫자 입력 받기
    3개의 숫자, 1과 9 사이의 숫자, 중복 없는 숫자만 받을 수 있도록 구현했습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { GAME, ERROR } from './constant.js';
+import Computer from './Computer.js';
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print(GAME.START);
+    const computer = new Computer();
+    const input = await MissionUtils.Console.readLineAsync(GAME.INPUT_NUMBER);
+    App.validate(input);
+  }
+
+  static validate(input) {
+    if (input.length !== 3) {
+      throw new Error(ERROR.LENGTH);
+    }
+
+    const result = /^[1-9]+$/.test(input);
+    if (result === false) {
+      throw new Error(ERROR.RANGE);
+    }
+
+    const unique = [...input].reduce(
+      (unique, number) =>
+        unique.includes(number) ? unique : [...unique, number],
+
+      []
+    );
+
+    if (unique.length !== 3) {
+      throw new Error(ERROR.UNIQUE);
+    }
+  }
 }
 
 export default App;
---
+++ src/constant.js
@@ -0,0 +1,10 @@
+export const GAME = {
+  START: '숫자 야구 게임을 시작합니다.',
+  INPUT_NUMBER: '숫자를 입력해주세요 : ',
+};
+
+export const ERROR = {
+  LENGTH: '[ERROR] 야구 숫자는 3개여야 합니다.',
+  RANGE: '[ERROR] 야구 숫자는 1과 9 사이의 숫자여야 합니다.',
+  UNIQUE: '[ERROR] 야구 숫자는 중복이 없어야 합니다.',
+};
