a5d2c677665474cbf87b75658443942932d4725e
rbgksqkr <rbgks1937@naver.com>
2023-10-20 16:05:37 +0900

    Feat: 입력한 숫자에 대한 결과 출력

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    const number = this.getRandomNumber();
+    const computerNumber = this.getRandomNumber();
     MissionUtils.Console.print("숫자 야구를 시작합니다.");
-    const inputNumber = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+    const userNumber = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+    this.getBaseballResult(userNumber, computerNumber);
   }
+
+  async getBaseballResult(userNumber, computerNumber) {
+    const inputIntArray = Array.from(userNumber).map((data) => parseInt(data));
+
+    let strike = 0;
+    let ball = 0;
+    let nothing = 0;
+
+    for (let i = 0; i < 3; i++) {
+      if (inputIntArray[i] === computerNumber[i]) {
+        strike += 1;
+      } else if (computerNumber.includes(inputIntArray[i])) {
+        ball += 1;
+      }
+    }
+
+    if (strike === 0 && ball === 0) nothing = 1;
+
+    MissionUtils.Console.print(`숫자 야구 결과 : ${strike} ${ball} ${nothing}`);
+  }
+
   getRandomNumber() {
     const computer = [];
     while (computer.length < 3) {
