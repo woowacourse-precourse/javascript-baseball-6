449cb7e48135aed49745c7a6df5e3b795165dfda
김용민 <dydals3440@gmail.com>
2023-10-21 03:01:18 +0900

    FEAT: 유저의 입력 값을 받아, 숫자 배열로 변환

--- src/App.js
+++ src/App.js
@@ -15,6 +15,16 @@ class App {
     }
     return Array.from(computer);
   }
+
+  // 2. 유저, 입력 받기(123 => [1, 2, 3])
+  async userInput() {
+    const userInput = await Console.readLineAsync('숫자를 입력해주세요: ');
+    return this.changeNumArr(userInput);
+  }
+
+  changeNumArr(number) {
+    return number.split('').map(Number);
+  }
 }
 
 export default App;
