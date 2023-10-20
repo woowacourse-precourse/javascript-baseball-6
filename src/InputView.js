d56c8381552bb132d91fb5879fd7a250b0af793d
PARK-CHANUK <doosan1224@naver.com>
2023-10-20 22:52:51 +0900

    feat: 재시도 입력값 받아오기 기능구현 InputView클래스의 readRetryAnswer를 통해 재시도 여부를 받아옵니다.

--- src/InputView.js
+++ src/InputView.js
@@ -5,4 +5,8 @@ export default class InputView {
     const answer = await Console.readLineAsync('숫자를 입력해주세요 : ');
     return answer;
   }
+  static async readRetryAnswer() {
+    const answer = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+    return answer;
+  }
 }
