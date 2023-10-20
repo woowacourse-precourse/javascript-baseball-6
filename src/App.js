fc0918c66e5a1c7d4d43a2f5c59f5e59b2f88c76
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2023-10-20 10:38:45 +0900

    feat: 랜덤 숫자 생성 기능 작성 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    Console.print('숫자야구 게임을 시작합니다.');
+    const answer = genNum();
+  }
 }
+
+const genNum = function generateRandomNumber() {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer;
+};
+
+const app = new App();
+app.play();
 
 export default App;
