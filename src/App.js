5df23ad758661c4025d9a02cc1feb9f0b62ddd76
jijiseong <jiseong5036@gmail.com>
2023-10-20 13:06:03 +0900

    [refactor] 메시지 분리

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import throwBall from "./functions/throwBall.js";
 import askReplayAsync from "./functions/askReplayAsync.js";
 import askUserNumAsync from "./functions/askUserNumAsync.js";
 import printResult from "./functions/printResult.js";
+import { MESSAGE } from "./constant/message.js";
 
 class App {
   async playOneGame() {
     const computerNum = generateNum({ length: 3 });
 
     while (true) {
-      const userNum = await askUserNumAsync("숫자를 입력해주세요 : ");
+      const userNum = await askUserNumAsync(MESSAGE.INPUT_NUM);
       // Todo. userNum 유효성 검사
 
       const result = throwBall({
@@ -20,16 +21,14 @@ class App {
       printResult(result);
 
       if (result.strike === 3) {
-        Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        const replay = await askReplayAsync(
-          "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
-        );
+        Console.print(MESSAGE.CORRECT);
+        const replay = await askReplayAsync(MESSAGE.INPUT_REPLAY);
         return replay;
       }
     }
   }
   async play() {
-    Console.print("숫자 야구 게임을 시작합니다.");
+    Console.print(MESSAGE.GAME_START);
 
     let isFinish = false;
     while (!isFinish) {
---
+++ src/constant/message.js
@@ -0,0 +1,6 @@
+export const MESSAGE = {
+  INPUT_NUM: "숫자를 입력해주세요 : ",
+  INPUT_REPLAY: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  CORRECT: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  GAME_START: "숫자 야구 게임을 시작합니다.",
+};
