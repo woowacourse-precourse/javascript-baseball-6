2d6429cc60efe0633a3fd85a57c0ebe8c7667bcf
hongst222 <tmdxordusdo@gmail.com>
2023-10-20 22:11:26 +0900

    [fix] makeRandomNumber()메소드 범위설정 관련 오류 수정

--- src/App.js
+++ src/App.js
@@ -17,18 +17,23 @@ class App {
     }
 
     /**
-     * 중복되지 않은 숫자를 생성해준다. (length=3)
+     * 중복되지 않은 숫자배열을 생성해주는 메소드  (number.length=3)
+     * @return {number[]}
      */
     makeRandomNumber() {
         const randomNumber = [];
         while(randomNumber.length <= INPUT_LIMIT){
-            const pickNumber = Random.pickNumberInRange(RANDOM_NUMBER_MAX, RANDOM_NUMBER_MIN);
+            const pickNumber = Random.pickNumberInRange(RANDOM_NUMBER_MIN, RANDOM_NUMBER_MAX);
             if(!randomNumber.includes(pickNumber)){
                 randomNumber.push(pickNumber);
             }
         }
         return randomNumber;
     }
+    /**
+     * 사용자에게 입력받는 메서드
+     * 
+     */
     
 }
 
