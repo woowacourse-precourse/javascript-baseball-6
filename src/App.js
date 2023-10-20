44e6fa2f91036ef18fd9bfccfdc2a3412e70df77
jadewisemann <jadewisemann@gmail.com>
2023-10-20 09:08:17 +0900

    feat: add two function that compare two 3 digit numbers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
+
 class App {
   async play() {
-    const userInputNumber = await Console.readLineAsync('숫자를 입력해 주세요 : ')
-    Console.print(userInputNumber)
+    // const userInputNumber = await Console.readLineAsync('숫자를 입력해 주세요 : ')
+    const result = compare([1, 2, 3], numberToArray(123))
+
+    Console.print(result)
   }
 }
+
 
 async function getRandomNumber() {
     const number = []; 
@@ -16,7 +20,23 @@ async function getRandomNumber() {
       }
     }
     return number
-  }
+}
+  
+function compare(targetNumber, userInputNumber) {
+  let strike = 0
+  let ball = 0
+  for (let i =0 ; i < targetNumber.length; i++) {
+    targetNumber.includes(userInputNumber[i])
+      && targetNumber[i] == userInputNumber[i]
+        ? strike ++
+        : ball ++
+    }
+  return [ball, strike]
+}
+
+function numberToArray(targetNumber) { 
+  return Array.from(String(targetNumber),x => Number(x))
+}
 
 const app = new App();
 app.play();
