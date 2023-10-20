14bcaad27d41c4307fcceaa59a373db9e9dd7577
ChoiYongWon <yongwon0824@naver.com>
2023-10-20 21:03:03 +0900

    feat: 예외처리

--- src/App.js
+++ src/App.js
@@ -17,6 +17,20 @@ class App {
     }
   }
 
+  validateInput(input, type){
+    console.log(Number(input))
+    if(type == 'INPUT'){
+      if(input.length != 3) throw Error("[ERROR] 3개의 숫자를 입력해주세요")
+      if(input.split("").includes('0')) throw Error("[ERROR] 1부터 9까지의 숫자를 입력해주세요")
+      if(new Set(input.split("")).size != 3) throw Error("[ERROR] 숫자가 중복되면 안됩니다.")
+      if(isNaN(Number(input))) throw Error("[ERROR] 숫자 형식이 잘못되었습니댜.")
+    }
+    else if(type == "RESTART"){
+      if(input.length != 1) throw Error("[ERROR] 1개의 숫자를 입력해주세요")
+      if(!(input == '1' || input == '2')) throw Error("[ERROR] 1 아니면 2를 입력해주세요")
+    }
+  }
+
   executeInput(input) {
     let input_list = input.split("").map((data) => Number(data));
     this.ball = 0;
@@ -34,6 +48,7 @@ class App {
   
       while(this.strike < 3){
         let input = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+        this.validateInput(input, 'INPUT')
         this.executeInput(input);
         if(this.ball == 0 && this.strike == 0) MissionUtils.Console.print("낫싱")
         else {
@@ -44,6 +59,7 @@ class App {
       MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       
       let input = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+      this.validateInput(input, 'RESTART')
       switch (input) {
         case '1':
           this.initialize()
