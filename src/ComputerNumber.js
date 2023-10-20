6d06544e4000338a329fcda61de4c272b656564a
PARK-CHANUK <doosan1224@naver.com>
2023-10-20 23:02:49 +0900

    refactor: 컴퓨터 수 리팩토링 Set자료형의 size를 비교할 수 있도록 리팩토링했습니다.

--- src/ComputerNumber.js
+++ src/ComputerNumber.js
@@ -3,7 +3,7 @@ import { Random } from '@woowacourse/mission-utils';
 export default class ComputerNumber {
   static generateComputerNumber() {
     const computerNumber = new Set();
-    while (computerNumber < 3) {
+    while (computerNumber.size < 3) {
       computerNumber.add(Random.pickNumberInRange(1, 9));
     }
     return [...computerNumber];
