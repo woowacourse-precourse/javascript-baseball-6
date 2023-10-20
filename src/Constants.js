e6abe9e050a36df0872294fb067a9bf4ed1cd564
kimdw524 <kimdw524@naver.com>
2023-10-21 01:48:14 +0900

    fix: 에러 문구 변경

--- src/Constants.js
+++ src/Constants.js
@@ -3,8 +3,8 @@ export const MESSAGE = Object.freeze({
   INPUT: '숫자를 입력해주세요 : ',
   SUCCESS: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
   RETRY: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
-  INVALID_NUMBER: '1~9로 구성된 서로 다른 세 자리의 숫자를 입력해 주세요.',
-  INVALID_RETRY_ANSWER: '1 또는 2만 입력해 주세요.',
+  INVALID_NUMBER: '[ERROR] 1~9로 구성된 서로 다른 세 자리의 숫자를 입력해 주세요.',
+  INVALID_RETRY_ANSWER: '[ERROR] 1 또는 2만 입력해 주세요.',
   STRIKE: '스트라이크',
   BALL: '볼',
 });
