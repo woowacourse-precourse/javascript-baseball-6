e70ead5fad20b24b5316ff36168a6ab041cc0baa
novice0840 <magnolia0840@gmail.com>
2023-10-20 21:32:12 +0900

    Feat: 잘못된 인풋값 처리
    숫자가 아닌 값을 입력하거나 숫자의 길이가 잘못된 경우 에러 메세지를 출력한다

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
+  constructor() {
+    this.LENGTH_OF_NUMBER = 3;
+  }
   createAnswerNumber() {
-    const LENGTH_OF_NUMBER = 3;
     let result = "";
-    for (let i = 0; i < LENGTH_OF_NUMBER; i++) {
+    for (let i = 0; i < this.LENGTH_OF_NUMBER; i++) {
       const number = Random.pickNumberInRange(1, 9);
       result += number.toString();
     }
@@ -12,8 +14,15 @@ class App {
   }
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
-    const inputNumber = await Console.readLineAsync("숫자를 입력해주세요 : ");
-    console.log(inputNumber);
+    try {
+      const inputNumber = await Console.readLineAsync("숫자를 입력해주세요 : ");
+      if (isNaN(inputNumber) || inputNumber.length !== this.LENGTH_OF_NUMBER) {
+        throw new Error();
+      }
+      console.log(inputNumber);
+    } catch (error) {
+      Console.print("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
   }
 }
 export default App;
