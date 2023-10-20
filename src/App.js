23db648adaa93cfe586b970e7351f5b705ff140a
hardlife0 <hellosy76@naver.com>
2023-10-20 16:41:58 +0900

    완성

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils } from '@woowacourse/mission-utils';
 
-class NumberBaseballGame{
+function printMessage(message) {
+  MissionUtils.Console.print(message);
+}
 
-  constructor(){
-    this.computerNumber=this.generateRanNum();
-    this.attempts=0;
+class NumberBaseballGame {
+  constructor() {
+    this.computerNumber = this.generateRandomNumber();
+    this.attempts = 0;
   }
 
-  generateRanNum() {
+  generateRandomNumber() {
     const computer = [];
     while (computer.length < 3) {
-    const number = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!computer.includes(number)) {
-      computer.push(number);
-    }
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
     }
     return computer.join('');
   }
 
   checkGuess(userGuess) {
     if (userGuess.length !== 3 || !/^[1-9]{3}$/.test(userGuess)) {
-      throw new Error('잘못된 입력입니다. 1부터 9까지 서로 다른 3자리의 숫자를 입력하세요.');
+      throw new Error('[ERROR] 잘못된 입력입니다. 1부터 9까지 서로 다른 3자리의 숫자를 입력하세요.');
     }
-
     this.attempts++;
     let strikes = 0;
     let balls = 0;
@@ -42,63 +44,50 @@ class NumberBaseballGame{
   }
 }
 
-
-
 class App {
-  constructor(){
+  constructor() {
     this.game = new NumberBaseballGame();
   }
 
   async play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    await this.playRound();
+  }
 
-    const playRound = async() => {
-      try{
-      const userInput= await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+  async playRound() {
+    const userInput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
 
-      if (userInput === '2') {
-        MissionUtils.Console.print('게임을 종료합니다.');
-        return;
-      }
-
-      if (userInput.length !== 3 || !/^[1-9]{3}$/.test(userInput)) {
-        MissionUtils.Console.print('잘못된 입력입니다. 1부터 9까지 서로 다른 3자리의 숫자를 입력하세요.');
-        await playRound();
-        return;
-      }
-
-      const result = this.game.checkGuess(userInput);
-      const { strikes, balls } = result;
-
-      if (strikes === 3) {
-        MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-        MissionUtils.Console.print(`총 시도 횟수: ${this.game.attempts}`);
-        const choice = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ');
-
-        if (choice === '1') {
-          this.game = new NumberBaseballGame();
-          await playRound();
-        } else if (choice === '2') {
-          MissionUtils.Console.print('게임을 종료합니다.');
-        }
-      } else if (strikes === 0 && balls === 0) {
-        MissionUtils.Console.print('낫싱');
-        await playRound();
-      } else {
-        MissionUtils.Console.print(`${balls > 0 ? `${balls}볼 ` : ''}${strikes > 0 ? `${strikes}스트라이크` : ''}`);
-        await playRound();
-      }
-    } catch (error) {
-      MissionUtils.Console.print(error.message);
-      await playRound();
+    if (userInput === '2') {
+      printMessage('게임을 종료합니다.');
+      return;
     }
-  };
 
-  await playRound();
-}
+    const result = this.game.checkGuess(userInput);
+    const { strikes, balls } = result;
+
+    if (strikes === 3) {
+      printMessage('3스트라이크');
+      printMessage('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      printMessage(`총 시도 횟수: ${this.game.attempts}`);
+      const choice = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ');
+
+      if (choice === '1') {
+        this.game = new NumberBaseballGame();
+        await this.playRound();
+      } else if (choice === '2') {
+        printMessage('게임을 종료합니다.');
+      }
+    } else if (strikes === 0 && balls === 0) {
+      printMessage('낫싱');
+      await this.playRound();
+    } else {
+      printMessage(`${balls > 0 ? `${balls}볼 ` : ''}${strikes > 0 ? `${strikes}스트라이크` : ''}`);
+      await this.playRound();
+    }
+  }
 }
 
-const app=new App();
+const app = new App();
 app.play();
 
 export default App;
