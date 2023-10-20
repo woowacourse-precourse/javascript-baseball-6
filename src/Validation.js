aba092725378b91761e0cfbf9e832342ff5637d7
Trophy198 <spsp8426@naver.com>
2023-10-21 01:07:24 +0900

    Feat : validateInput 함수에 입력값 길이 검증 로직 추가

--- src/Validation.js
+++ src/Validation.js
@@ -2,5 +2,8 @@ export function validateInput(input) {
   if (typeof input !== 'string') {
     throw new Error('[ERROR] 입력값은 문자열이어야 합니다.');
   }
+  if (input.length !== 3) {
+    throw new Error('[ERROR] 입력값은 3자리 숫자이어야 합니다.');
+  }
 }
   
