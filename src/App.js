b6ae38d949f0f246620fe16b0ac2875836ba87c0
Lim-shweety <smile70200@naver.com>
2023-10-20 10:36:51 +0900

    완료!

--- src/App.js
+++ src/App.js
@@ -1,133 +1,149 @@
 import {Console, MissionUtils} from "@woowacourse/mission-utils";
 
-import readline from "readline";
+const Count_Num = (computer, input, LENGTH) => {
 
-class App {
-    async play() {
-        //게임 시작
-        Console.print("숫자 야구 게임을 시작합니다.\n");
+    let strike_num=0;
+    let ball_num=0;
 
-        //컴퓨터 랜덤 숫자 저장하기 (3자리 숫자)
-        const computer = [];
-
-        while (computer.length < 3) {
-            const number = MissionUtils.Random.pickNumberInRange(1, 9);
-            if (!computer.includes(number)) {
-                computer.push(number);
-            }
-        }
-        Console.print(computer);
-
-        //게임이 끝나는지 확인하는 flag
-        let flag = 1;
-        //게임을 계속할건지 결정하는 입력
-        let continue_num = 1;
-        //입력받을 변수 선언
-        let input;
-
-        let strike_num;
-        let ball_num;
-
-        while (flag && continue_num != 2) {
-
-            //입력받기, 입력 type : string
-
-
-            //길이가 3이 아닐 때, 서로 다른 수가 아닐 때
-            try{
-                input = await Console.readLineAsync("숫자를 입력해주세요 : ");
-                if (input.length !== 3 || input[0] === input[1] || input[1] === input[2] || input[2] === input[0] ) {
-                    throw new Error("숫자 잘못 입력하였다.");
-                }
-            }catch (e){
-                flag=0
-                continue;
-            }
-
-            strike_num = 0;
-            ball_num = 0;
-
-            for (let i = 0; i < 3; i++) {
-                for (let j = 0; j < 3; j++) {
-                    //[strike_num, ball_num] = Check(computer, input, i, j, strike_num, ball_num);
-                    if (i === j ) {
-                        //스트라이크일 경우
-                        if (computer[i] == input[j]) {
-                            strike_num += 1;
-                        }
-                    }
-                    if (i !== j) {
-                        if (computer[i] == input[j]) {
-                            ball_num += 1;
-                        }
-                    }
-                }
-            }
-            if (ball_num > 0) {
-                Console.print(ball_num + '볼 ');
-            }
-            if (strike_num > 0) {
-                Console.print(strike_num + '스트라이크');
-            }
-            if (ball_num===0 && strike_num===0){
-                Console.print("낫싱");
-            }
-
-            if (strike_num === 3) {
-                Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                try {
-                    continue_num = await Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
-
-                    if (continue_num != 1 && continue_num != 2) {
-                        throw new Error("종료시 잘못 입력됨");
-                    }
-                }catch (e){
-                    flag=0
-                }
-                if(continue_num===1){
-                    const computer = [];
-
-                    while (computer.length < 3) {
-                        const number = MissionUtils.Random.pickNumberInRange(1, 9);
-                        if (!computer.includes(number)) {
-                            computer.push(number);
-                        }
-                    }
-                }
-                if(continue_num===2){
-                    flag=1
-                }
-
-
-            }
-
-
-        }
-
-
-    }
-
-
-}
-
-const Check = ({computer, input, i, j, strike_num, ball_num}) => {
-    for (let j = 0; j < 3; j++) {
-        if (i === j ) {
-            //스트라이크일 경우
-            if (computer[i] == input[j]) {
+    for (let i = 0; i < LENGTH; i++) {
+        for (let j = 0; j < LENGTH; j++) {
+            if (i === j && computer[i] == input[j]) {//스트라이크인 경우
                 strike_num += 1;
             }
-        }
-        if (i !== j) {
-            if (computer[i] == input[j]) {
+
+            else if (i !== j && computer[i] == input[j]) {//볼인 경우
                 ball_num += 1;
             }
         }
     }
-    return strike_num, ball_num
+
+    return [strike_num, ball_num];
+}
+
+const Print_Result = (strike_num, ball_num) => {
+    if (ball_num > 0 && strike_num > 0) {
+        Console.print(ball_num + '볼 ' + strike_num + '스트라이크');
+    } else if (strike_num > 0) {
+        Console.print(strike_num + '스트라이크');
+    } else if (ball_num > 0) {
+        Console.print(ball_num + '볼');
+    } else if (ball_num === 0 && strike_num === 0) {
+        Console.print("낫싱");
+    }
+}
+
+const Check_Input = (input, computer)=>{
+    //길이 확인, 중복되는 입력값이 있는지 확인, 0이 포함되어 있는지 확인
+    if (input.length !== computer.length
+        || (input[0] === input[1] || input[1] === input[2] || input[2] === input[0])
+        || (input[0] === '0' || input[1] === '0' || input[2] === '0') ) {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+}
+
+const Check_Continue=(input)=>{ // 유효한 입력
+    if (input != 1 && input != 2) {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+}
+
+const Set_Computer=(digit_num=3)=>{//컴퓨터 랜덤 숫자 저장하기 (3자리 숫자)
+    let computer = [];
+    while (computer.length < digit_num) {
+        const number = MissionUtils.Random.pickNumberInRange(1, 9);
+        if (!computer.includes(number)) {
+            computer.push(number);
+        }
+    }
+    return computer;
+}
+
+
+class App {
+    async play() {
+        //게임 시작
+        Console.print("숫자 야구 게임을 시작합니다.");
+
+        //컴퓨터 랜덤 숫자 저장하기 (3자리 숫자)
+        let computer = Set_Computer();
+
+
+
+        //게임의 종료 판단 flag
+        let flag = 1;
+
+        //게임을 계속할건지 결정하는 입력 저장 변수
+        let continue_num = 1;
+
+        //입력값 저장 변수 (string)
+        let user;
+
+        //스트라이크와 볼의 값을 저장 변수
+        let strike_num;
+        let ball_num;
+
+        while (flag) {
+
+            try {
+
+                //사용자 입력받기
+                user = await Console.readLineAsync("숫자를 입력해주세요 : ");
+                //올바른 입력인지 확인
+                Check_Input(user, computer);
+
+            } catch (e) {
+                flag = 0;
+                continue;
+            }
+
+            //스트라이크, 볼 값 초기화
+            strike_num = 0;
+            ball_num = 0;
+
+            //strike와 ball개수 확인
+            [strike_num, ball_num] = Count_Num(computer, user, computer.length);
+
+
+            //스트라이크, 볼 출력하기
+            Print_Result(strike_num, ball_num);
+
+            //게임 종료
+            if (strike_num === computer.length) {
+
+                Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+                //종료시 입력 받기 및 예외처리
+                try {
+                    continue_num = await Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+                    //유효한 입력인지 확인
+                    Check_Continue(continue_num);
+                } catch (e) {
+                    flag = 0
+                    continue;
+                }
+
+                if (continue_num === '1') {//새로 시작
+                    //컴퓨터 값 변경하기
+                   computer=Set_Computer();
+                }
+                else if (continue_num === '2') {//종료
+                    flag = 0;
+                }
+
+
+            }
+
+
+        }
+
+
+    }
+
+
 }
 
 const app = new App();
 app.play();
+
 export default App;
 
