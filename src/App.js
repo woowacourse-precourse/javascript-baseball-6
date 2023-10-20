6016c52e857dc50cdf6fe5d03446bd2d9acd5d11
seok-pyo <hongseokpyou@gmail.com>
2023-10-20 18:23:20 +0900

    Fix : 상수 메세지 리턴 메서드로 수정

--- src/App.js
+++ src/App.js
@@ -2,23 +2,12 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   constructor() {
-    // 상수는 대문자로 짓고, _로 구분한다.
-    this.MESSAGE = {
-      START: "숫자 야구 게임을 시작합니다.",
-      INPUT: "숫자를 입력해주세요 : ",
-      RETRY: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
-      STRIKE: "스트라이크",
-      BALL: "볼",
-      NOTHING: "낫싱",
-      CONGRAT: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-    };
-    this.ERROR_MESSAGE = {
-      LENGTH: "[ERROR] 입력값은 3자리 수이어야 합니다.",
-      NOT_NUMBER: "[ERROR] 입력값은 숫자여야 합니다.",
-      SAME_NUMBER: "[ERROR] 입력값은 서로 다른 숫자여야 합니다.",
-    };
     this.strikeZoneNumber = [];
     this.pitchingNumber = [];
+    this.result = {
+      strikes: 0,
+      balls: 0,
+    };
   }
 
   printMsgIs(message) {
@@ -35,20 +24,46 @@ class App {
   }
 
   async makePitchingNumber() {
-    const inputNumber = await MissionUtils.Console.readLineAsync(
-      this.MESSAGE.INPUT
-    );
+    const START_MESSAGE = this.message("START");
+    const inputNumber = await MissionUtils.Console.readLineAsync(START_MESSAGE);
     // 입력값 유효성 검사
-    const reg = /[^1-9]/g;
-    if (inputNumber.length !== 3) throw new Error(this.ERROR_MESSAGE.LENGTH);
-    if (reg.test(inputNumber)) throw new Error(this.ERROR_MESSAGE.NOT_NUMBER);
-    if (inputNumber.length !== new Set(inputNumber).size)
-      throw new Error(this.ERROR_MESSAGE.SAME_NUMBER);
-    // 유효성 검사 통과하면 배열로 변환
-    this.pitchingNumber = new Array(...inputNumber);
+    // const reg = /[^1-9]/g;
+    // if (inputNumber.length !== 3) throw new Error(this.ERROR_MESSAGE.LENGTH);
+    // if (reg.test(inputNumber)) throw new Error(this.ERROR_MESSAGE.NOT_NUMBER);
+    // if (inputNumber.length !== new Set(inputNumber).size)
+    //   throw new Error(this.ERROR_MESSAGE.SAME_NUMBER);
+    // 유효성 검사 통과하면 숫자 배열로 변환
+    this.pitchingNumber = new Array(...inputNumber).map((number) =>
+      parseInt(number)
+    );
+  }
+
+  message(NAME) {
+    const MESSAGE = {
+      // 상수는 대문자로 짓고, _로 구분한다.
+      START: "숫자 야구 게임을 시작합니다.",
+      INPUT: "숫자를 입력해주세요 : ",
+      RETRY: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+      STRIKE: "스트라이크",
+      BALL: "볼",
+      NOTHING: "낫싱",
+      CONGRAT: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+    };
+    return MESSAGE[NAME];
+  }
+
+  errorMessage(NAME) {
+    const ERROR_MESSAGE = {
+      // 상수는 대문자로 짓고, _로 구분한다.
+      LENGTH: "[ERROR] 입력값은 3자리 수이어야 합니다.",
+      NOT_NUMBER: "[ERROR] 입력값은 숫자여야 합니다.",
+      SAME_NUMBER: "[ERROR] 입력값은 서로 다른 숫자여야 합니다.",
+    };
+    return ERROR_MESSAGE[NAME];
   }
 
   async play() {
+    this.makeStrikeZoneNumber();
     this.makePitchingNumber();
   }
 }
