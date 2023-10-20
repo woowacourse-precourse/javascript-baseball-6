6de5a8a4a3a2ee47217c017b2e9bbbd87cd5eb1a
gouz7514 <gouz7514@gmail.com>
2023-10-20 12:00:52 +0900

    Update(feature) : 사용자의 잘못된 입력에 대해 예외를 발생시키는 기능 구현

--- src/App.js
+++ src/App.js
@@ -21,6 +21,13 @@ class App {
   async getUserNumbers() {
     return new Promise((resolve) => {
       Console.readLineAsync('숫자를 입력해주세요 : ').then((userNumbers) => {
+        // 사용자가 잘못된 값을 입력한 경우 throw문을 사용해 예외를 발생새캐고 애플리케이션을 종료한다.
+        const isValid = /^\d{3}$/.test(userNumbers)
+
+        if (!isValid) {
+          this.throwError();
+        }
+
         resolve(userNumbers.split('').map((number) => parseInt(number)));
       });
     });
