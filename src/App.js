8b0589bcfec074e67131fd8d34c583ae17b6b242
novice0840 <magnolia0840@gmail.com>
2023-10-21 03:08:00 +0900

    Refactor: 숫자 유효성 검사 기능 분리
    메인 play 함수에 로직이 몰려 있어  유효성을 검사하는 함수를 별도로의 validateNumber로 분리하였다

--- src/App.js
+++ src/App.js
@@ -37,30 +37,39 @@ class App {
     return strike === 3 ? true : false;
   }
 
-  async play() {
-    const answerNumber = this.createAnswerNumber();
-    Console.print("숫자 야구 게임을 시작합니다.");
-    let inputNumber;
-    while (true) {
-      inputNumber = await Console.readLineAsync("숫자를 입력해주세요 : ");
-      if (isNaN(inputNumber) || inputNumber.length !== this.LENGTH_OF_NUMBER) {
-        throw new Error("[ERROR]");
-      }
-      const answer = this.checkAnswer(answerNumber, inputNumber);
-      if (answer) {
-        Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        break;
-      }
-    }
+  async restart() {
     const restart = await Console.readLineAsync(
       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
     );
     if (restart === "1") {
       this.play();
     } else if (restart === "2") {
+      Console.print("종료 완료");
     } else {
-      throw new Error();
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
     }
+  }
+
+  validateNumber(number) {
+    if (isNaN(number) || number.length !== this.LENGTH_OF_NUMBER) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+  }
+
+  async play() {
+    const answerNumber = this.createAnswerNumber();
+    this.validateNumber(answerNumber);
+    Console.print("숫자 야구 게임을 시작합니다.");
+    while (true) {
+      const inputNumber = await Console.readLineAsync("숫자를 입력해주세요 : ");
+      this.validateNumber(inputNumber);
+      const correct = this.checkAnswer(answerNumber, inputNumber);
+      if (correct) {
+        Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        break;
+      }
+    }
+    await this.restart();
   }
 }
 export default App;
