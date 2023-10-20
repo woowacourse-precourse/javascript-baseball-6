260a3ee9b29fa08b260c63dea35dec157bcc8b4f
박지윤 <tkrhkfld123@naver.com>
2023-10-20 23:42:11 +0900

    feat: 게임 시작 메세지 띄우기 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 class App {
-  async play() {}
+  async play() {
+    this.startGame();
+  }
+
+  startGame() {
+    const START = "숫자 야구 게임을 시작합니다.";
+    console.log(START);
+  }
 }
 
 export default App;
