d7af41d9bb6dc2b1e55f069fd3088eabe850cc22
soominnnn <marisol03@naver.com>
2023-10-20 13:13:10 +0900

    Module 파일 분리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import {RANDOM} from './module.js';
+import {USER} from './module.js';
 class App {
   async play() {
-    let RANDOM = {
-      RANDOM_ARR : [],
-      random_create : function(){
-        this.RANDOM_ARR = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-        return console.log(this.RANDOM_ARR);
-      },
-    }
     RANDOM.random_create();
   }
 }
