8fceab3806c2b7c026d5d3fc9cdea494ca8a72f3
양진성 <anfakt0606@naver.com>
2023-10-20 19:01:30 +0900

    볼,스트라이크,낫싱 출력 기능 and 유저 정답 입력 기능

--- src/App.js
+++ src/App.js
@@ -17,6 +17,37 @@ class App {
       
       return baseball;
     }
+
+    const Hint = (answer,userAnswer) => {
+      let hint = '';
+      let ball = 0;
+      let strike = 0;
+
+      userAnswer.forEach((number,i) => {
+        const check = answer.indexOf(+number);
+        if(check != -1){
+          check === i ? strike++ : ball++
+        }
+      })
+
+      if(ball > 0) hint += `${ball}볼`;
+      if(strike > 0) hint += `${strike}스트라이크`;
+      if(ball === 0 && strike === 0) hint += '낫싱';
+
+      return hint
+    }
+    
+    let answer = GameStart();
+    let userAnswer = [];
+
+    while(answer.join('') != userAnswer.join('')){
+      const USER_INPUT = await MissionUtils.Console.readLineAsync('숫자를 입력해 주세요 : ');      
+      userAnswer = USER_INPUT.split('');
+
+      MissionUtils.Console.print(Hint(answer,userAnswer));
+    }
+
+    MissionUtils.Console.print('숫자 야구 게임을 종료합니다.')
   }
 }
 
