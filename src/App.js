7a5fb1d9f57c91bf132e37ff10ac8ae02652448e
bada308 <203190@jnu.ac.kr>
2023-10-20 22:45:35 +0900

    feat: 사용자 입력 및 검증 구현 예외 상황 - 입력값이 숫자가 아닐 경우 - 3자리 숫자가 아닐 경우 - 입력값에 중복된 숫자가 있을 경우

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
+  async getUserNumber() {
+    try {
+      const number = await MissionUtils.Console.readLineAsync(
+        "숫자를 입력하세요: "
+      );
+
+      if (isNaN(number)) return "[ERROR] 숫자가 잘못된 형식입니다.";
+      if (number.length !== 3) return "[ERROR] 3자리 숫자를 입력해주세요.";
+      if ([...new Set(number)].length !== 3)
+        return "[ERROR] 중복된 숫자가 있습니다.";
+
+      return number;
+    } catch (error) {}
+  }
 }
 
 export default App;
