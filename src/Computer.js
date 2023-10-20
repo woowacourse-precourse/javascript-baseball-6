32b1787c7d3f4894d4bafac07a72473d714f23a3
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-10-20 21:58:05 +0900

    feat: 정답과 비교해서 힌트 출력

--- src/Computer.js
+++ src/Computer.js
@@ -1,3 +1,5 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Computer {
     constructor() {
         this.answer = getRandAnswer();
@@ -24,9 +26,32 @@ class Computer {
         return this.isLenThree(input) && this.isMadeByDigit(input) && this.isUnique(input);
     }
 
+    setHint(input) {
+        const userInput = input.split('');
+        const correctAnswer = this.answer.split('');
+        let hint = '';
+        let allCnt = 0;
+        let strikeCnt = 0;
+        let ballCnt = 0;
+
+        for(let i = 0; i < correctAnswer.length; i++){
+            if(userInput[i] === correctAnswer[i])   strikeCnt++;
+        }
+        
+        for(let i = 0; i < userInput.length; i++){
+            if(correctAnswer.includes(userInput[i]))    allCnt++;
+        }
+        ballCnt = allCnt - strikeCnt;
+
+        if(ballCnt > 0)   hint += `${ballCnt}볼`;
+        if(strikeCnt > 0)   hint += `${strikeCnt}스트라이크`;
+        if(hint === '')   hint += `낫싱`;
+        
+        return hint;
+    };
 }
 
-function getRandAnswer(){
+function getRandAnswer() {
     const answerSet = new Set();
     
     while(answerSet.size < 3){
