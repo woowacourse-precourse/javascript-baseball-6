e00d96b09e5142cf90ed7cebea3c29b0665c2434
장호빈 <devhobin@gmail.com>
2023-10-20 19:29:31 +0900

    chore: 상수화 및 코드 정리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
 
-const RESTART = "1";
-const QUIT = "2";
+const ANSWER_LENGTH = 3;
+const BASEBALL_NUMBER_REGEX = /^[1-9]+$/;
+const COMMAND_RESTART = "1";
+const COMMAND_QUIT = "2";
 
 class App {
   _status = "idle";
@@ -32,9 +34,8 @@ class App {
         this._invariant(playerInput);
 
         const score = this._createScore(playerInput);
-        const { strike } = score;
 
-        if (strike === 3) {
+        if (score.strike === ANSWER_LENGTH) {
           await this._transition("clear");
         } else {
           const hint = this._createHint(score);
@@ -45,18 +46,23 @@ class App {
       }
 
       case "clear": {
-        Console.print("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        Console.print(
+          `${ANSWER_LENGTH}스트라이크\n${ANSWER_LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료`
+        );
 
         let playerInput = null;
-        while (playerInput !== RESTART && playerInput !== QUIT) {
+        while (
+          playerInput !== COMMAND_RESTART &&
+          playerInput !== COMMAND_QUIT
+        ) {
           playerInput = await Console.readLineAsync(
             "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
           );
         }
 
-        if (playerInput === RESTART) {
+        if (playerInput === COMMAND_RESTART) {
           await this._restart();
-        } else if (playerInput === QUIT) {
+        } else if (playerInput === COMMAND_QUIT) {
           await this._transition("end");
         }
       }
@@ -68,7 +74,7 @@ class App {
 
   _createAnswer() {
     const answer = [];
-    while (answer.length < 3) {
+    while (answer.length < ANSWER_LENGTH) {
       const number = String(MissionUtils.Random.pickNumberInRange(1, 9));
       if (!answer.includes(number)) {
         answer.push(number);
@@ -80,7 +86,7 @@ class App {
 
   _createScore(playerInput) {
     const score = { strike: 0, ball: 0 };
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
       const current = playerInput[i];
       if (current === this._answer[i]) {
         score.strike++;
@@ -105,15 +111,16 @@ class App {
   }
 
   _invariant(playerInput) {
-    const numberRegex = /^[1-9]+$/;
-
     if (playerInput.length === 0)
       throw new Error("[ERROR] 숫자를 입력해야합니다.");
     if (playerInput.includes(" "))
       throw new Error("[ERROR] 입력값에 공백이 포함되어 있습니다.");
-    if (!numberRegex.test(playerInput) || new Set(playerInput).size !== 3)
+    if (
+      !BASEBALL_NUMBER_REGEX.test(playerInput) ||
+      new Set(playerInput).size !== ANSWER_LENGTH
+    )
       throw new Error(
-        "[ERROR] 1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 입력해야합니다."
+        `[ERROR] 1부터 9까지 서로 다른 수로 이루어진 ${ANSWER_LENGTH}자리의 수를 입력해야합니다.`
       );
   }
 }
