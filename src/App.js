dfa5354d668cab89590912ba6ff958eef71f4cc0
장호빈 <devhobin@gmail.com>
2023-10-20 14:15:46 +0900

    feat: 게임 시작 기능 구현 (idle 상태일 경우)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  _status = "idle";
+  _answer = [];
+
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    this._createAnswer();
+    this._transition("playing");
+  }
+
+  _transition(status) {
+    this._status = status;
+  }
+
+  _createAnswer() {
+    const answer = [];
+    while (answer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!answer.includes(number)) {
+        answer.push(number);
+      }
+    }
+
+    this._answer = answer;
+  }
 }
 
 export default App;
