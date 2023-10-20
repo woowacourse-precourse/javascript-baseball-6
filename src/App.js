3388385d2c2ef27026ddc9887c80c52b321566c4
hongst222 <tmdxordusdo@gmail.com>
2023-10-20 21:50:25 +0900

    [feat] 게임 시작 안내문구 및 랜덤숫자 메소드 makeRandomNumber() 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { STRING,INPUT_LIMIT, RANDOM_NUMBER_MAX, RANDOM_NUMBER_MIN, STRIKE_GAME_CLEAR, RESTART_NUMBER, END_NUMBER } from "./constants/index.js"
+import { Console, Random } from './utils/index.js'
 class App {
-  
-    async play() {
+    isplaying;
+    randomNumber;
 
-  }
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        this.isplaying=true;
+        //this.randomNumber=[];
+    }
+    async play() {
+        Console.print(STRING.START);
+    }
+
+    /**
+     * 중복되지 않은 숫자를 생성해준다. (length=3)
+     */
+    makeRandomNumber() {
+        const randomNumber = [];
+        while(randomNumber.length <= INPUT_LIMIT){
+            const pickNumber = Random.pickNumberInRange(RANDOM_NUMBER_MAX, RANDOM_NUMBER_MIN);
+            if(!randomNumber.includes(pickNumber)){
+                randomNumber.push(pickNumber);
+            }
+        }
+    }
+    
 }
 
 export default App;
