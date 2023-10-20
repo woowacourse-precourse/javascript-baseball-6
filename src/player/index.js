d617dd53ff713461acecaee2740df660e7d5f2be
Kled <1017yu@daum.net>
2023-10-20 14:48:34 +0900

    feat: 유효성 검사 책임 분리, player numberSet 필드 추가

--- src/player/index.js
+++ src/player/index.js
@@ -1,27 +1,40 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import constants from '../../constants/index.js';
+import InputValidator from './inputValidator.js';
+import Opponent from '../opponent/index.js';
 
 const { Console } = MissionUtils;
-const { MESSAGES, THREE_DIGIT_PATTERN } = constants;
+const { MESSAGES } = constants;
 
 class Player {
+  /**
+   * 유저가 입력한 서로 다른 3자리 수를 담은 배열
+   * @type {[number, number, number]}
+   */
+  static playerNumberSet;
+
   constructor() {
     this.getPlayerNumber();
+    this.opponent = new Opponent();
   }
 
   // P-1 플레이어는 3자리 숫자를 맞추기 위해 숫자를 입력
   getPlayerNumber = () => {
-    Console.readLine(MESSAGES.INPUT, (input) => {
-      if (this.isValidNumberSet(input)) return Console.print(input);
-
-      // P-1-a 유효성 검사에 통과하지 못하면 예외를 발생시켜 메시지와 함께 게임을 종료.
-      throw new Error(MESSAGES.ERROR.INVALID_COUNT_NUMBER);
-    });
+    Console.readLine(MESSAGES.INPUT, (input) => this.handleNumberSet(input));
   };
 
-  // P-1 입력한 숫자에 대한 유효성 검사 로직
-  isValidNumberSet = (input) => {
-    return THREE_DIGIT_PATTERN.test(input);
+  handleNumberSet = (input) => {
+    // P-1-a 유효성 검사에 통과하지 못한 경우 에러 메시지와 함께 종료
+    if (!InputValidator.isValidNumberSet(input))
+      throw new Error(MESSAGES.ERROR.INVALID_COUNT_NUMBER);
+
+    // 유효하다면 [number, number, number] 형태의 배열화
+    this.playerNumberSet = input.split('').map(Number);
+    this.isCorrected();
+  };
+
+  isCorrected = () => {
+    this.opponent.checkBallCount(this.playerNumberSet);
   };
 }
 
