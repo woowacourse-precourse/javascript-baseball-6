30a6820cfd00c2d2fd9b9f3d5c7b2d611b8cb736
Trophy198 <spsp8426@naver.com>
2023-10-21 01:08:38 +0900

    Feat : validateInput 함수에 입력값 중복 검사 로직 추가

--- src/Validation.js
+++ src/Validation.js
@@ -5,5 +5,8 @@ export function validateInput(input) {
   if (input.length !== 3) {
     throw new Error('[ERROR] 입력값은 3자리 숫자이어야 합니다.');
   }
+  if (new Set(input).size !== input.length) {
+    throw new Error('[ERROR] 입력값은 중복된 숫자를 포함할 수 없습니다.');
+  }
 }
   
