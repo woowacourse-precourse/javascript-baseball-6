d95355c2e46cb026bb8ca023e4c28defe8e3feef
kang <kangju2000@naver.com>
2023-10-20 15:25:34 +0900

    test: Validator 테스트 작성

---
+++ __tests__/ValidationTest.js
@@ -0,0 +1,30 @@
+import Validator from "../src/validator/Validator";
+
+describe("입력값 검증", () => {
+  test("3자리 수인지 검증한다.", () => {
+    expect(Validator.isValidLength("123")).toBeTruthy();
+    expect(Validator.isValidLength("1234")).toBeFalsy();
+    expect(Validator.isValidLength("12a")).toBeFalsy();
+  });
+
+  test("서로 다른 수인지 검증한다.", () => {
+    expect(Validator.hasUniqueNumbers("123")).toBeTruthy();
+    expect(Validator.hasUniqueNumbers("112")).toBeFalsy();
+    expect(Validator.hasUniqueNumbers("121")).toBeFalsy();
+    expect(Validator.hasUniqueNumbers("12a")).toBeFalsy();
+  });
+
+  test("1~9 사이의 수인지 검증한다.", () => {
+    expect(Validator.isValidRange("123")).toBeTruthy();
+    expect(Validator.isValidRange("149")).toBeTruthy();
+    expect(Validator.isValidRange("023")).toBeFalsy();
+    expect(Validator.isValidRange("a12")).toBeFalsy();
+  });
+
+  test("재시작/종료를 구분하는 1 또는 2인지 검증한다.", () => {
+    expect(Validator.isExitOrRestart("1")).toBeTruthy();
+    expect(Validator.isExitOrRestart("2")).toBeTruthy();
+    expect(Validator.isExitOrRestart("3")).toBeFalsy();
+    expect(Validator.isExitOrRestart("a")).toBeFalsy();
+  });
+});
---
+++ src/validator/Validator.js
@@ -0,0 +1,8 @@
+const Validator = {
+  isValidLength: (input) => { },
+  hasUniqueNumbers: (input) => { },
+  isValidRange: (input) => { },
+  isExitOrRestart: (input) => { },
+}
+
+export default Validator;
