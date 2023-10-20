69656a49eef09b53930dffafc372c24d825ec086
kyoungmin <lanos5019@ajou.ac.kr>
2023-10-20 21:53:09 +0900

    feat: setup in local and REAME.md add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      Console.print("낫싱");
+      Console.print("3스트라이크");
+      Console.print("1볼 1스트라이크");
+      Console.print("3스트라이크");
+      Console.print("게임 종료");
+      const username = await Console.readLineAsync("닉네임을 입력해주세요.");
+    } catch (error) {
+      // reject 되는 경우
+    }
+  }
 }
 
 export default App;
