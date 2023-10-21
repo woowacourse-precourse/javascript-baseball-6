541f56bd4e90e5e1640883294c6496164cdbd3ea
hong <ahi200@naver.com>
2023-10-21 17:09:00 +0900

    - makeRandomNumber 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Baseball from './Baseball.js';
 class App {
-  async play() {}
+  async play() {
+    const game = new Baseball();
+    game.getUserinput().then((input) => console.log(input));
+    game.makeRandomNumber();
+  }
 }
-
+const app = new App();
+app.play();
 export default App;
--- src/Baseball.js
+++ src/Baseball.js
@@ -51,5 +51,16 @@ class Baseball {
     }
     return false;
   }
+
+  makeRandomNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    this.#randomNumber = [...computer];
+  }
 }
 export default Baseball;
