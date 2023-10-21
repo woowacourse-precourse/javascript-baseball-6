4bafbf12920c1359db1412021fd62d9ae43c9828
rkdcodus <rcd7275@naver.com>
2023-10-22 02:26:57 +0900

    예외 처리, 오류 수정 시도

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   constructor() {
     this.replay = false;
-    this.reanswer = false;
+    this.isCorret = false;
     this.randomNum = [];
   }
   // 서로 다른 숫자 3자리 랜덤 생성하기
@@ -35,9 +35,34 @@ class App {
       result += `${strike}스트라이크`;
     }
     if (strike == 3) {
-      this.reanswer = true;
+      this.isCorret = true;
     }
     return result;
+  }
+
+  exceptionUserNum(str) {
+    if (str.length != 3) {
+      throw Error("[ERROR]");
+    }
+    if (isNaN(str)) {
+      throw Error("[ERROR]");
+    }
+    if (Number(str[0]) == 0 || Number(str[1]) == 0 || Number(str[2]) == 0) {
+      throw Error("[ERROR]");
+    }
+    if (
+      Number(str[0]) == Number(str[1]) ||
+      Number(str[0]) == Number(str[2]) ||
+      Number(str[1]) == Number(str[2])
+    ) {
+      throw Error("[ERROR]");
+    }
+  }
+
+  exceptionReplayAnswer(str) {
+    if (str != "1" && str != "2") {
+      throw Error("[ERROR]");
+    }
   }
   //사용자의 입력 값 받고 결과 반환하는 함수
   async getUserAnswer() {
@@ -47,6 +72,7 @@ class App {
       "숫자를 입력해주세요 : "
     );
 
+    this.exceptionUserNum(user_num);
     for (let i = 0; i < 3; i++) {
       if (this.randomNum[i] == Number(user_num[i])) {
         strike += 1;
@@ -59,33 +85,36 @@ class App {
   }
   // 입력 다시 받을지 끝낼지 결정하는 함수
   async againOrFinish() {
-    if (this.reanswer == false) {
+    if (this.isCorret == false) {
       this.getUserAnswer().then(() => {
         this.againOrFinish();
       });
-    } else if (this.reanswer == true) {
+    } else if (this.isCorret == true) {
       MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-      await MissionUtils.Console.readLineAsync(
+      MissionUtils.Console.readLineAsync(
         "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
       ).then((result) => {
-        if (result == 1) {
+        this.exceptionReplayAnswer(result);
+        if (result == "1") {
           this.replay = true;
-        } else if (result == 2) {
-          this.replay = false;
+          this.isCorret = false;
+          this.play();
+        }
+        if (result == "2") {
+          MissionUtils.Console.print("게임 종료");
         }
       });
     }
   }
+
   async play() {
     // 게임 시작 문구 출력하기
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    if (this.replay == false) {
+      MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    }
     this.randomNum = this.makeRandomNum();
     await this.getUserAnswer();
-    await this.againOrFinish().then(() => {
-      if (this.replay == true) {
-        this.play();
-      }
-    });
+    await this.againOrFinish();
   }
 }
 
