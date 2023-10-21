b226b170a9e601c5744e733902e67f0dd9783374
dongsuseo <poopoo0515@naver.com>
2023-10-21 16:28:49 +0900

    feat: 정답 값 랜덤으로 생성하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  async play() {
+    // 정답 값 랜덤으로 생성하기
+    const COMPUTER = [];
+
+    while (COMPUTER.length < 3) {
+      const NUMBER = MissionUtils.Random.pickNumberInRange(1, 9).toString();
+      if (!COMPUTER.includes(NUMBER)) {
+        COMPUTER.push(NUMBER);
+      }
+    }
+  }
 }
 
 export default App;
