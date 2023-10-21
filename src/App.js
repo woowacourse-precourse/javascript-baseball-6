0c5eb193c3f8def4088b6dbd2aa026dce65195bf
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-10-21 20:11:35 +0900

    feat: 추가 컴퓨터 숫자 생성 함수
    생성자에 컴퓨터(상대방) 숫자를 추가 및 해당 숫자를 초기화하는 함수 정의

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 export default class App {
-  constructor() {}
+  constructor() {
+    this.computerNumber = "";
+  }
+
+  initComputerNumber() {
+    while (this.computerNumber.length < 3) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.computerNumber.includes(randomNumber)) {
+        this.computerNumber += randomNumber;
+      }
+    }
+  }
 
   // 게임 진행 함수
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.initComputerNumber();
   }
 }
 
