3c8c9dd3801428077c6bd6af49c5501134527a8d
jonique98 <tnals9899@naver.com>
2023-10-21 14:51:13 +0900

    style: indent 맞춤 및 isValid 체크 추가

--- src/App.js
+++ src/App.js
@@ -5,20 +5,30 @@ import { MESSAGE , CONSTANT } from './constants/constants.js';
 
 export default class App{
 
-	#_opponent;
-	#_player;
-	#_view;
+  #_opponent;
+  #_player;
+  #_view;
 
-	//플레이어와 상대방 객체 생성 입출력 담당 view 객체 생성
+  //플레이어와 상대방 객체 생성 입출력 담당 view 객체 생성
   constructor() {
-		this.#_opponent = new opponent();
+    this.#_opponent = new opponent();
     this.#_player = new player();
     this.#_view = new views();
   }
 
-	//게임 시작
+  //게임 시작
   async play() {
     const isValid = await this.#_player.inputNumber();
+
+    // 에러가 발생하면 에러 throw
+    if (!isValid) {
+      throw new Error(MESSAGE.ERROR);
+    }
+    
+    // 에러가 발생하지 않으면 결과를 확인
+    if (isValid) {
+      this.judgeResult();
+    }
   }
 
 }
