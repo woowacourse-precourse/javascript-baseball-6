44dd8474033c71e4b41ca098dbac71f1544db2f0
seokuMac <cd1004s@gmail.com>
2023-10-21 10:19:19 +0900

    feat: Print game start message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+import Messages from "./Messages.js";
+
 class App {
-  async play() {}
+  async play() {
+    Console.print(Messages.MSG_START);
+  }
 }
+
+App.prototype.play();
 
 export default App;
