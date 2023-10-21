6e8eebab6fc289630e08f7a8d4e7e2b1c888b9c1
Trophy198 <spsp8426@naver.com>
2023-10-22 01:52:34 +0900

    Feat : 전체적인 게임 흐름을 진행하는 BaseballGame 클래스 추가

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -1,11 +1,38 @@
-export default class BaseballGame {
-    constructor() {
-        this.user = this.user;
-        this.computer = this.computer;
-        this.gameLogic = this.gameLogic;
-        this.display = this.display;
-    }
-    async start() {
+import User from "./User.js";
+import Computer from './Computer.js';
+import GameDisplay from "./GameDisplay.js";
+import StrikeAndBallCalculator from './StrikeAndBallCalculator.js';
 
-    }
+export default class BaseballGame {
+  constructor() {
+    this.computer = new Computer();
+    this.user = new User();
+    this.display = new GameDisplay();
+  }
+
+  async start() {
+    const computerNumbers = this.computer.generateNumbers();
+    this.display.showStartMessage();
+    await this.playGame(computerNumbers);
+  }
+
+  async playGame(computerNumbers) {
+      let isEqual = false;
+      while (!isEqual) {
+        const userNumbers = await this.user.getInput();
+        const { strike, ball } = StrikeAndBallCalculator.calculate(userNumbers, computerNumbers);
+        this.display.showResult(strike, ball);
+        isEqual = (strike === 3);
+        if(isEqual) {
+            this.display.showWinMessage();
+          }
+      }
+  
+      const gameEndChoice = await this.display.showEndMessage();
+      if(gameEndChoice === '1') {
+        return this.start();
+      }
+      
+      return;
+   } 
 }
