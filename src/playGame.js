bada5b102b0ba27af33aceab4c2d28e9e47ef419
nkEllaaa <dhrtns515@gmail.com>
2023-10-21 22:34:10 +0900

    Feat: 유저의 입력값 유효성 겁사 구현

--- src/playGame.js
+++ src/playGame.js
@@ -1,7 +1,7 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 const compareTwoNums = (random, user) => {
-  const userNumber = user.split("").map(Number);
+  const userNumber = user;
   const computerNumber = random;
 
   let strike = 0;
@@ -15,7 +15,6 @@ const compareTwoNums = (random, user) => {
       }
     }
   }
-  console.log("strike : ", strike, "ball", ball);
 
   if (strike === 0 && ball === 0) {
     console.log("낫싱");
@@ -24,26 +23,43 @@ const compareTwoNums = (random, user) => {
   } else if (!strike && ball) {
     console.log(`${ball}볼`);
   } else {
-    console.log(`${strike}스트라이크 `,`${ball}볼`);
+    console.log(`${strike}스트라이크 `, `${ball}볼`);
   }
 };
 
 const checkValidation = (random, user) => {
   if (user.length !== 3) {
-    console.log("[ERROR] 잘못된 형식의 숫자입니다.");
-    // MissionUtils.Console.readLine(user, error);
+    throw new Error("[ERROR] 세 자리의 숫자를 입력해주세요");
   }
-  console.log(user);
+  if (user) {
+    for (let i = 0; i < 3; i++) {
+      if (isNaN(user[i])) {
+        throw new Error("[ERROR] 세 자리의 숫자를 입력해주세요");
+      }
+    }
+  }
+  if (user) {
+    let count = 0;
+    for (let i = 0; i < 3; i++) {
+      for (let j = i + 1; j < 3; j++) {
+        if (user[i] === user[j]) {
+          count++;
+        }
+      }
+    }
+    if (count !== 0) {
+      throw new Error("[ERROR] 중복되지 않은 세 자리의 숫자를 입력해주세요");
+    }
+  }
+
   compareTwoNums(random, user);
 };
 
 const createNum = () => {
   const computer = [];
   while (computer.length < 3) {
-    const randomNum = MissionUtils.Random.pickNnkumberInRange(1, 9);
-    if (!computer.includes(randomNum)) {
-      computer.push(randomNum);
-    }
+    const randomNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    computer.push(randomNum);
     return randomNum;
   }
 };
@@ -65,8 +81,8 @@ const gameinit = () => {
   // const userNum = MissionUtils.Console.readLine("숫자를 입력해주세요 : ");
   console.log("숫자 야구 게임을 시작합니다.");
 
-  const userNum = prompt("숫자를 입력해주세요 : ");
-  // const userNumToArr = userNum.split('');
+  const userNum = prompt("숫자를 입력해주세요 : ").split("");
+  const userNumToNumber = userNum.map(Number);
 
   let generateNum = [];
   generateNum = testCreateNum();
