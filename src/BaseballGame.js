fe685516d8e884af4e5c3244e40ea3686e9c1e84
youjeong Jeong <wjddbwjd1205@naver.com>
2023-10-22 00:39:36 +0900

    feat: 컴퓨터와 사용자의 숫자를 비교해서 결과를 출력하는 기능 추가

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -1,10 +1,47 @@
+import { Console } from "@woowacourse/mission-utils";
 import { Computer } from "./Computer.js";
 
+//사용자의 입력받기
+
 export class BaseballGame {
-    //게임 시작
-    startGame() {
-        console.log("숫자 야구 게임을 사작합니다.");
+
+    /**게임 시작*/
+    async startGame() {
+        Console.print('숫자 야구 게임을 사작합니다.');
         const computer = new Computer();
         const answer = computer.createRandomNum();
+        try {
+            const userInput = await Console.readLineAsync('숫자를 입력해주세요: ');
+            const userNumber = userInput.split('').map(Number);
+            this.compare(answer, userNumber);
+        } catch (error) {
+            throw error;
+        }
+        
+    }
+
+    //숫자 비교 함수
+    compare(answer, userNumber) {
+        let strikeCount = 0;
+        let ballCount = 0;
+        answer.forEach((element1, index1) => {
+            userNumber.forEach((element2, index2) => {
+                if(element1 == element2 && index1 == index2) strikeCount++;
+                else if (element1 == element2) ballCount++;
+            })
+        });
+        
+        if(strikeCount != 0 && ballCount != 0){
+            Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
+        }
+        else if (strikeCount != 0 && ballCount == 0) {
+            Console.print(`${strikeCount}스트라이크`);
+        }
+        else if (strikeCount == 0 && ballCount != 0) {
+            Console.print(`${ballCount}볼`);
+        }
+        else {
+            Console.print('낫싱');
+        }
     }
 }
