456899d096e2214677d235eaa10d4f2afd853288
silver0108 <kk1170@naver.com>
2023-10-22 04:30:34 +0900

    feat: 종료 및 재시작 기능

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+// 정답 생성 함수
 function generateComputeNum(){
   const computerNum = [];
   while (computerNum.length < 3) {
-    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    const number = MissionUtils.Random.pickNumberInRange(1, 9).toString();
     if (!computerNum.includes(number)) {
       computerNum.push(number);
     }
@@ -11,11 +12,13 @@ function generateComputeNum(){
   return computerNum;
 }
 
+// 사용자 숫자 입력 함수
 async function inputUserNum(){
   const userNum = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
   return userNum;
 }
 
+// 사용자 입력 숫자 확인
 function checkUserNum(userNum){
   const userNumSet = new Set(userNum);
 
@@ -31,19 +34,21 @@ function checkUserNum(userNum){
   return true;
 }
 
+// 정답과 사용자 값 비교 및 판단 함수
 function compareNum(computerNum, userNum){
   let strike = 0;
   let ball = 0;
 
   for(let i=0; i<3; i++){
-    if(computerNum[i] === Number(userNum[i])){
+    if(computerNum[i] === userNum[i]){
       strike += 1;
-    }else if(computerNum.includes(Number(userNum[i]))){
+    }else if(computerNum.includes(userNum[i])){
       ball += 1;
     }
   }
 
   if(strike === 3){
+    MissionUtils.Console.print(strike + "스트라이크");
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     return true;
   }else if(strike !== 0 || ball !== 0){
@@ -54,30 +59,44 @@ function compareNum(computerNum, userNum){
     }else{
       MissionUtils.Console.print(ball + "볼 " + strike + "스트라이크");
     }
+    return false;
   }else{
     MissionUtils.Console.print("낫싱");
+    return false;
   }
+}
+
+// 재시작 및 종료 값 입력 함수
+async function inputRestartNum(){
+  const restartNum  = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+  return restartNum;
 }
 
 class App {
   async play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-
     let computerNum = generateComputeNum();
-    MissionUtils.Console.print(computerNum);
 
-    let userNum = await inputUserNum();
-    
-    if(!checkUserNum(userNum)){
-      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    while(true){
+      let userNum = await inputUserNum();
+      
+      if(!checkUserNum(userNum)){
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      }
+
+      if(compareNum(computerNum, userNum)){ // 3 스트라이크인 경우
+        let restartNum = await inputRestartNum();
+
+        if(restartNum === "1"){ // 1이면 재시작
+          computerNum = generateComputeNum();
+        }else if(restartNum === "2"){ // 2이면 종료
+          break;
+        }else{ // 이외는 에러 처리
+          throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+        }
+      }
     }
-
-    compareNum(computerNum, userNum);
-
   }
 }
-
-const app = new App();
-app.play();
 
 export default App;
