72a661f3f060221c1719ca386069afe6ee0a93ad
at8in <at8in@naver.com>
2023-10-22 06:25:26 +0900

    feat: 플레이어 입력, 예외 처리

--- src/App.js
+++ src/App.js
@@ -6,15 +6,52 @@ class App {
     const computer = [];
 
     while (computer.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1,9);
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!computer.includes(number)) {
         computer.push(number);
       }
     }
-    
+
     MissionUtils.Console.print(GAME_TEXT.START);
+
+    const player = await this.getUserInput();
+    console.log(player);
+  }
+
+  async getUserInput() {
+    try {
+      const number = await MissionUtils.Console.readLineAsync(GAME_TEXT.INPUT);
+
+      if (number.length !== 3) {
+        throw new Error(GAME_TEXT.ERROR(GAME_TEXT.NOT_MATCH_LENGTH));
+      }
+      if (!Number.isInteger(Number(number))) {
+        throw new Error(GAME_TEXT.ERROR(GAME_TEXT.INVALID_NUMBER));
+      }
+      if (Number(number) < 0) {
+        throw new Error(GAME_TEXT.ERROR(GAME_TEXT.IS_NEGATIVE_NUMBER));
+      }
+      if (this.checkDuplicateNumber(number)) {
+        throw new Error(GAME_TEXT.ERROR(GAME_TEXT.DUPLICATE_NUMBER));
+      }
+
+      return number;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  checkDuplicateNumber(number) {
+    const isDuplicate = Array(10).fill(false);
+
+    for (let index = 0; index < 3; index++) {
+      if (isDuplicate[Number(number[index])]) {
+        return true;
+      }
+      isDuplicate[Number(number[index])] = true;
+    }
+    return false;
   }
 }
 
 export default App;
-
--- src/constants/string.js
+++ src/constants/string.js
@@ -1,3 +1,11 @@
 export const GAME_TEXT = Object.freeze({
-    START: '숫자 야구 게임을 시작합니다.'
+  START: "숫자 야구 게임을 시작합니다.",
+  INPUT: "숫자를 입력해주세요 : ",
+  ERROR(message) {
+    return `[ERROR] ${message}`;
+  },
+  NOT_MATCH_LENGTH: "3개의 숫자가 아닙니다.",
+  INVALID_NUMBER: "숫자가 잘못된 형식입니다.",
+  DUPLICATE_NUMBER: "중복된 수가 있습니다.",
+  IS_NEGATIVE_NUMBER: "음수는 입력할 수 없습니다.",
 });
