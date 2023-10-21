f13287eb56025b2dce916b408e6da772d83bbc06
김경주 <popo505@naver.com>
2023-10-21 22:01:24 +0900

    feat:랜덤으로 숫자 생성

---
+++ src/RandomNumber.js
@@ -0,0 +1,12 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+export function randomNumber() {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer;
+}
