1b23efb0f673c826bad293c3b4bb8a809ecaee1c
Jihyeong00 <iolpd42@gmail.com>
2023-10-21 11:20:18 +0900

    에러문구 변경

--- src/util/helper.js
+++ src/util/helper.js
@@ -13,10 +13,10 @@ export const randomNumber = () => {
 
 export const checkNumber = (input) => {
     // 2.1.1 숫자가 입력이 되었는가?
-    if (isNaN(input)) throw new Error();
+    if (isNaN(input)) throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
     // 2.1.2 3자리수가 입력이 되었는가?
-    if (String(input).length !== 3) throw new Error();
+    if (String(input).length !== 3) throw new Error("[ERROR] 입력된 값이 3자리수가 아닙니다.");
     // 2.1.3 모두 다른 숫자가 입력이 되었는가?
     const STRING_INPUT = String(input)
-    if (STRING_INPUT[0] === STRING_INPUT[1] || STRING_INPUT[1] === STRING_INPUT[2] || STRING_INPUT[2] === STRING_INPUT[0]) throw new Error();
+    if (STRING_INPUT[0] === STRING_INPUT[1] || STRING_INPUT[1] === STRING_INPUT[2] || STRING_INPUT[2] === STRING_INPUT[0]) throw new Error("[ERROR] 입력된 값이 중복되는 수가 있습니다.");
 }
