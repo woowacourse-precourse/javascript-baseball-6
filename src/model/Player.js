fa63240410b786bb011c70692609a2376c2c8938
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-21 15:43:23 +0900

    feat: [Player] getter, setter 구현

--- src/model/Player.js
+++ src/model/Player.js
@@ -2,4 +2,12 @@ export default class Player {
   constructor() {
     this.number = null;
   }
+
+  getNumber() {
+    return this.number;
+  }
+
+  setNumber(newNumber) {
+    this.number = newNumber;
+  }
 }
