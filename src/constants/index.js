251ad261735a0fb43ff4ff52804c3b09c17648df
Collection50 <fkaus65@naver.com>
2023-10-21 22:01:56 +0900

    refactor: ERROR, COMMAND 객체 상수 변경

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,13 +5,13 @@ export const NUMS = {
 };
 
 export const ERROR = {
-  VALID_CONTROL_NUM: `${NUMS.REPLAY}, ${NUMS.END} 중 1개를 입력하세요`,
-  VALID_LENGTH: `숫자 ${NUMS.ASNWER_LENGTH}개를 입력하세요.`,
+  INVALID_CONTROL_NUM: `${NUMS.REPLAY}, ${NUMS.END} 중 1개를 입력하세요.`,
+  INVALID_LENGTH: `서로 다른 숫자 ${NUMS.ASNWER_LENGTH}개를 입력하세요.`,
 };
 
 export const COMMAND = {
   WELCOME: '숫자 야구 게임을 시작합니다.',
-  ASK_NUMBER: '숫자를 입력하세요: ',
-  ASK_REPLAY: `게임을 새로 시작하려면 ${NUMS.REPLAY}, 종료하려면 ${NUMS.END}를 입력하세요: `,
+  ASK_NUMBER: '숫자를 입력해주세요 : ',
+  ASK_REPLAY: `게임을 새로 시작하려면 ${NUMS.REPLAY}, 종료하려면 ${NUMS.END}를 입력하세요.\n`,
   MATCH: `${NUMS.ASNWER_LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료`,
 };
