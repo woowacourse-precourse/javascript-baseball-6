28041cd228ef0b7778f041ffed914526ed6f19cb
puranium235 <puranium235@gmail.com>
2023-10-22 00:51:27 +0900

    3자리 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-class App {
-  async play() {
+import {MissionUtils} from "@woowacourse/mission-utils";
 
-  }
+class App {
+    async play() {
+        // 컴퓨터가 임의의 3자리 숫자를 생성
+        const computer = [];
+        while (computer.length < 3) {
+            const number = MissionUtils.Random.pickNumberInRange(1, 9);
+            if (!computer.includes(number)) {
+                computer.push(number);
+            }
+        }
+
+    }
 }
 
 export default App;
