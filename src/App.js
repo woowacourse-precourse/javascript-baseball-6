ce9c34c0386705af31d5a5ff87f9612d6bccdc5f
easyhyun00 <leejihyun0324@gmail.com>
2023-10-21 12:47:12 +0900

    :sparkles: feat: 전체적인 야구 게임 틀 #1 - 상대방(컴퓨터)의 수 얻기 - 사용자가 2를 입력할 경우 'break'문을 통해 프로그램 완전 종료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    while(true) {
+      // 상대방(컴퓨터)의 수 얻기 - getComputerNumbers() - 구현 완료
+      const computerNumbers = this.getComputerNumbers();
+      MissionUtils.Console.print(computerNumbers);
+
+      /*
+        숫자 맞추기 - 미구현
+      */
+
+      // 재시작/종료 여부 얻기 - askForRestart() - 미구현
+      const restart = 2
+
+      // restart 값이 2일 때, 프로그램 완전 종료
+      if (restart === 2) break;
+    }
+  }
+
+  // 상대방(컴퓨터)의 수 얻기
+  getComputerNumbers() {
+    const computer = new Set();
+    while (computer.size < 3) {
+      computer.add(MissionUtils.Random.pickNumberInRange(1, 9));
+    }
+    return Array.from(computer);
+  }
 }
 
 export default App;
