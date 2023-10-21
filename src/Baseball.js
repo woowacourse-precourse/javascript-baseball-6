f9f380850ee33050c4fdaec610478032de88df07
hong <ahi200@naver.com>
2023-10-21 16:05:28 +0900

    - 유저의 입력받기 - 잘못된 값 입력시 예외처리

---
+++ src/Baseball.js
@@ -0,0 +1,55 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+class Baseball {
+  #randomNumber;
+  #isPlaying;
+
+  constructor() {
+    this.#isPlaying = true;
+  }
+
+  async getUserinput() {
+    try {
+      const userinput = await MissionUtils.Console.readLineAsync(
+        '숫자를 입력해주세요 : '
+      );
+      if (this.#isPlaying) {
+        if (this.checkInputGamePlaying(userinput)) {
+          return userinput;
+        }
+        throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+      } else {
+        if (this.checkInputGameOver(userinput)) {
+          return userinput;
+        }
+        throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+      }
+    } catch (error) {
+      // reject 되는 경우
+      console.log(error);
+    }
+  }
+
+  checkInputGamePlaying(userinput) {
+    if (userinput.length !== 3) {
+      return false;
+    }
+    const set = new Set();
+    for (let c of userinput) {
+      if ('0' <= c && c <= '9') {
+        set.add(c);
+      }
+    }
+    if (set.size === 3) {
+      return true;
+    }
+    return false;
+  }
+
+  checkInputGameOver(userinput) {
+    if (userinput === '1' || userinput === '2') {
+      return true;
+    }
+    return false;
+  }
+}
+export default Baseball;
