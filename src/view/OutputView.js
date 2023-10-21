0e93dc5a2dd55d2fdbb89f42b80207d4ca9217a7
kang <kangju2000@naver.com>
2023-10-21 17:03:41 +0900

    fix: OutputView 요구사항에 맞게 출력 - 0볼일 때, 0스트라이크일 때 출력하지 않도록 변경

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -2,7 +2,7 @@ import { Console } from "@woowacourse/mission-utils";
 
 const OutputView = {
   printStartMessage: () => {
-    Console.print("숫자 야구 게임을 시작합니다.\n");
+    Console.print("숫자 야구 게임을 시작합니다.");
   },
   printGuessResult: ({ ball, strike }) => {
     if (ball === 0 && strike === 0) {
@@ -10,10 +10,16 @@ const OutputView = {
       return;
     }
 
-    Console.print(`${ball}볼 ${strike}스트라이크\n`);
+    const printBall = ball ? `${ball}볼 ` : '';
+    const printStrike = strike ? `${strike}스트라이크` : '';
+
+    Console.print(`${printBall}${printStrike}`);
   },
   printEndMessage: () => {
-    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  },
+  printExitMessage: () => {
+    Console.print("숫자 야구 게임을 종료합니다.");
   },
 }
 
