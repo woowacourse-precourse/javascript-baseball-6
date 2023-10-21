66b2522a927df46be8ba83c43a4b5d11a381dc3a
Lee-Sunho <sunho9889@naver.com>
2023-10-22 03:12:56 +0900

    feat: 입력한 수에 대한 힌트 출력 기능 구현

--- src/GameController.js
+++ src/GameController.js
@@ -1,13 +1,46 @@
 import { RandomNumberGenerator } from "./utils/RandomNumberGenerator.js";
 import { User } from "./User.js";
-
+import { Console } from "@woowacourse/mission-utils";
 export class GameController {
   constructor() {
     this.computer = RandomNumberGenerator.generateRandomNumber();
     this.user = new User();
   }
 
-  startGame() {
-    this.user.inputAnswer();
+  async startGame() {
+    const userAnswer = await this.user.inputAnswer();
+    const strikeCount = this.getStrikeCount(userAnswer);
+    const ballCount = this.getBallCount(userAnswer, strikeCount);
+
+    this.printHint(strikeCount, ballCount);
+  }
+
+  getStrikeCount(userAnswer) {
+    let count = 0;
+    for (let i = 0; i < userAnswer.length; i++) {
+      if (this.computer[i] === userAnswer[i]) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  getBallCount(userAnswer, strikeCount) {
+    let count = 0;
+    for (let i = 0; i < userAnswer.length; i++) {
+      if (this.computer.includes(userAnswer[i])) {
+        count++;
+      }
+    }
+    return count - strikeCount;
+  }
+
+  printHint(strikeCount, ballCount) {
+    const hint = [];
+    if (ballCount !== 0) hint.push(`${ballCount}볼`);
+    if (strikeCount !== 0) hint.push(`${strikeCount}스트라이크`);
+    if (ballCount === 0 && strikeCount === 0) hint.push(`낫싱`);
+
+    Console.print(hint.join(" "));
   }
 }
