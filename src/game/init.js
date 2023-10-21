cfde0382c4d1aae9348332089b27a07086bd369f
yohan11 <yipco77@naver.com>
2023-10-21 22:32:49 +0900

    feat: 잘못된 입력 받을 시 예외 처리 구현

--- src/game/init.js
+++ src/game/init.js
@@ -11,26 +11,32 @@ const startGame = async () => {
         if (validateUserNumber(userNumber)) {
             getHint(computerAnswer, userNumber);
         } else {
-            MissionUtils.Console.print("[ERROR]") //throw로 예외처리 필요
-            break;
+            throw new Error("[ERROR]");
+
         }
         userNumber = await getUserNumber();
     }
-    if (computerAnswer === userNumber) { ////throw로 예외처리 후에는 안필요함
-        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        await choiceForGame();
-    }
+    MissionUtils.Console.print("3스트라이크");
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    await choiceForGame();
+
 }
 
 const choiceForGame = async () => {
     MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-    const userChoice = await getUserNumber();
-    // 유효성 검사
-    if (userChoice === "1") {
-        await startGame();
-    } else {
-        return 0;
+    try {
+        const userChoice = await MissionUtils.Console.readLineAsync("");
+        if (userChoice === "1") {
+            await startGame();
+        } else if (userChoice === "2") {
+            return;
+        } else {
+            throw new Error("[Error]");
+        }
+    } catch (error) {
+        throw new Error("[Error]");
     }
+
 }
 
 
