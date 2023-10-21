1aa565f1fd5194fc77e9fd269f325f6b89f91c56
Trophy198 <spsp8426@naver.com>
2023-10-21 15:34:25 +0900

    Refactor : Computer 클래스에서 매직 넘버를 상수로 교체

--- src/Computer.js
+++ src/Computer.js
@@ -1,5 +1,5 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { NUM_DIGITS } from "./constants/NumberConstants";
+import { NUM_DIGITS, MIN_NUMBER, MAX_NUMBER } from "./constants/NumberConstants";
 
 export default class Computer {
     constructor() {
@@ -9,7 +9,7 @@ export default class Computer {
     generateRandomNumbers() {
         const numbers = [];
         while(numbers.length < NUM_DIGITS) {
-            const num = MissionUtils.Random.pickNumberInRange(1, 9);
+            const num = MissionUtils.Random.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
             if(!numbers.includes(num)) {
                 numbers.push(num);
             }
