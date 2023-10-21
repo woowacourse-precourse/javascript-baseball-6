7c3823a25f214ccd276872f5d5e766b1ee7abc35
jaeml00 <jaeml0630@gmail.com>
2023-10-21 21:44:15 +0900

    feat: create method
    setRandomThreeNummber() : 랜덤 3개의 수 생성
    
     getRandomThreeNummber() : 랜덤 생성 수 배열 반환

---
+++ src/Computer.js
@@ -0,0 +1,20 @@
+import  { MissionUtils }  from "@woowacourse/mission-utils";
+
+export default class Computer{
+    #randomThreeNummber;
+    constructor() {
+    }
+
+    setRandomThreeNummber(){ // 랜덤 3개 수 생성
+            this.#randomThreeNummber = [];
+            while (this.#randomThreeNummber.length < 3) {
+                const number = MissionUtils.Random.pickNumberInRange(1, 9);
+                if (!this.#randomThreeNummber.includes(number)) {
+                    this.#randomThreeNummber.push(number);
+                }
+        }
+    }
+    getRandomThreeNummber(){ // 랜덤 생성 수 배열 반환
+        return this.#randomThreeNummber;
+    }
+}
