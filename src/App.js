be74deb95f41d5ab2ad195c0b014893ded9a18cb
kmkkkp <kmkkkp@ajou.ac.kr>
2023-10-21 20:27:20 +0900

    Input Code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import {MissionUtils} from "@woowacourse/mission-utils";
+import {MissionUtils,Random, Console} from "@woowacourse/mission-utils";
 
 class App {
   async play() {
     const Target = getRandom();
-    console.log(Target.join(''));  
+    Console.print(Target.join(''));
+    const input = await getInput();
+    Console.print(input);
+    // const Answer = getInput();
+    // console.log(Answer);
+
+
+    return;
   }
 }
 
@@ -17,7 +24,7 @@ function getRandom(){
   var count = 3;
   while(count>0){
     var a;
-    a = MissionUtils.Random.pickNumberInRange(1,9);
+    a = Random.pickNumberInRange(1,9);
     if(Target.includes(a)){
       continue;
     }
@@ -25,4 +32,32 @@ function getRandom(){
     count--;
   }
   return Target;
+}
+
+async function getInput(){
+  
+  const Target = [];
+  while(true){
+    let userInput = await Console.readLineAsync("입력 ㄱㄱ : ");
+    Console.print(userInput);
+    if(userInput.length!==3){
+      throw new Error("세 자리가 아닙니다.");
+    }
+    for(let i=0; i<3; i++){
+      const digit = parseInt(userInput[i]);
+      if(isNaN(digit) || digit===0){
+        throw new Error("1~9의 숫자만 입력해주세요.")
+      }
+      if(Target.includes(digit)){
+        throw new Error("중복되는 숫자가 입력되었습니다.");
+      }
+      Target.push(digit);
+      
+    }
+    return userInput;
+  }
+}
+
+function getAnswer(){
+
 }
