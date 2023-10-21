62f1b5f4fe00052800954144ed633b437ab4a88b
ReturnReason <reasonz@daum.net>
2023-10-22 05:17:40 +0900

    fix: key 값이 일치하지 않아 undefined로 나타나는 부분 수정

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -9,7 +9,7 @@ export const MESSAGE = Object.freeze({
   START: '숫자 야구 게임을 시작합니다.',
   INPUT: '숫자를 입력해주세요 : ',
   END: `${DONE_COUNT}개의 숫자를 모두 맞히셨습니다! 게임 종료`,
-  RESTART: `게임을 새로 시작하려면 ${RESTART_COMMAND.NEWGAME_NUM}, 종료하려면 ${RESTART_COMMAND.END_NUM}를 입력하세요.\n`,
+  RESTART: `게임을 새로 시작하려면 ${RESTART_COMMAND.NEWGAME}, 종료하려면 ${RESTART_COMMAND.QUIT}를 입력하세요.\n`,
   getGameResult: ({ ball, strike }) => {
     if (!ball && !strike) {
       return `낫싱\n`;
