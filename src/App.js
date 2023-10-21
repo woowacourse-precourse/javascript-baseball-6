5189abee945b73533a4b29f0c1b244d2639848a9
kang <kangju2000@naver.com>
2023-10-21 17:04:18 +0900

    feat: App 클래스의 play 메서드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import BaseballGameController from "./controller/BaseballGameController.js";
+
 class App {
-  async play() {}
+  async play() {
+    const baseballGameController = new BaseballGameController();
+    await baseballGameController.start();
+  }
 }
 
 export default App;
