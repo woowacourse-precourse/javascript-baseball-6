4fef1339cbe5446034d150114d9cadd3c4546749
jaeochoii <jaeochoiii0716@inha.edu>
2023-10-21 13:24:21 +0900

    feat: 입력받은 숫자로 스트라이크 / 볼 계산하는 기능 구현

---
+++ src/domain/Baseball.mjs
@@ -0,0 +1,33 @@
+import RandomNumberGenerator from "../utils/RandomNumberGenerator.mjs";
+
+class Baseball {
+  #answer;
+
+  constructor(randomNumber) {
+    this.#answer = randomNumber;
+  }
+
+  // input = Array 형태
+  getStrikeCount(input) {
+    let count = 0;
+
+    for (let i = 0; i < input.length; i++) {
+      if (input[i] === this.#answer[i]) count += 1;
+    }
+
+    return count;
+  }
+
+  // input = Array 형태
+  getBallCount(input, strikeCount) {
+    let count = 0;
+
+    for (let i = 0; i < input.length; i++) {
+      if (this.#answer.includes(input[i])) count += 1;
+    }
+
+    return count - strikeCount;
+  }
+}
+
+export default Baseball;
