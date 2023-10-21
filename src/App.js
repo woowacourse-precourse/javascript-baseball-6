c201e1562f827f9493d9330dbbfa8ba069d13319
donggyun <donggyunkim12@gmail.com>
2023-10-21 15:43:12 +0900

    feat(Computer): 랜덤 숫자 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Random } from "@woowacourse/mission-utils";
+
+class Computer {
+  createAnswer() {
+    const answer = [];
+    while (answer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!answer.includes(number)) {
+        answer.push(number);
+      }
+    }
+    return answer;
+  }
+}
+
 class App {
-  async play() {}
+  async play() {
+    const com = new Computer();
+    console.log(com.createAnswer());
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
