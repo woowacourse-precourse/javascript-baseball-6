feebc82149ed830425611d2daaf79c3ca8d8a1ac
anttiey <hanuuny@gmail.com>
2023-10-21 16:25:56 +0900

    :sparkles: 플레이어 숫자 입력 기능 추가
    플레이어가 숫자를 입력할 수 있는 기능을 추가함

--- src/App.js
+++ src/App.js
@@ -6,13 +6,21 @@ class App {
     this.computer = new Computer();
   }
 
+  userNumber = [];
+
   gameStart = () => {
     Console.print("숫자 야구 게임을 시작합니다.");
+  };
+
+  getUserNumberInput = async () => {
+    const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
+    this.userNumber = Array.from(input).map((el) => Number(el));
   };
 
   async play() {
     this.gameStart();
     this.computer.createRandomNumber();
+    this.getUserNumberInput();
   }
 }
 
