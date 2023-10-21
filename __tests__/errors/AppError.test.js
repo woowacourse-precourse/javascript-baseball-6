f65dd566ea413e41e6de7cae8c08a25f1ffe3c1e
손진영 <sm099@naver.com>
2023-10-21 15:39:01 +0900

    docs(test): AppError 및 Random 모듈에 대한 테스트 관련 주석 추가
    AppError.test.js, Random.test.js 내 given - when - then 추가

--- __tests__/errors/AppError.test.js
+++ __tests__/errors/AppError.test.js
@@ -5,6 +5,7 @@ describe('AppError 테스트', () => {
     throw new AppError('test');
   };
   test(`발생 된 에러 메시지는 ${AppError.PREFIX}으로 시작해야 한다.`, () => {
+    // given - when - then
     expect(throwAppError).toThrow(AppError.PREFIX);
   });
 });
