8ebcdeaf7bce2186d9248c7bd6d4f559205b5699
jbilee <julishahn@gmail.com>
2023-10-21 13:18:35 +0900

    feat: 시작 문구 출력 및 랜덤 값 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const answer = this.getRandNum();
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  getRandNum() {
+    const num = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    return num.join("");
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
