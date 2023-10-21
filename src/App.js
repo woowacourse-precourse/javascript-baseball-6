8e0c65da3a98fd70ae9fdb722eeae2ced42746e4
Hys-Lee <dltkdgus3275@gmail.com>
2023-10-21 17:33:21 +0900

    Feat: read input and check validity
    checked that the number of input length is wrong,
    and each of the character could be parsed into integer.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,31 @@ class App {
         computer.push(number);
       }
     }
+    //# 인풋 받기   ->3개의 숫자를 모두 맞히면 게임이 종료된다. -> 그 때까지 반복...
+    let correct = false;
+    while (!correct) {
+      const rawBaseballNum = await MissionUtils.Console.readLineAsync();
+
+      //# input은.. 서로 다른 3자리의 수
+
+      //# valid확인
+
+      //##   문자열 길이
+      if (rawBaseballNum.length != 3) {
+        throw new Error("[ERROR]");
+      }
+
+      //## 각 자리 값이 1~9외의 다른 것들이 들어갔는지.  (문자, 0 등)
+      for (let i = 0; i < 3; i++) {
+        const parsed = parseInt(rawBaseballNum[0]);
+        if (parsed === 0 || isNaN(parsed)) {
+          throw new Error("[ERROR]");
+        }
+      }
+
+      //# valid 인풋 값.
+      const parsedBaseballNum = parseInt(rawBaseballNum);
+    }
   }
 }
 
