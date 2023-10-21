0518e19471dc29d99a891b37c0534f65c0e9b313
JunSeok <ppu1234@naver.com>
2023-10-21 23:20:46 +0900

    Feat: 컴퓨터 random number 설정

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import { Console } from '@woowacourse/mission-utils'
 import { messages } from './message.js'
 
 class App {
+  constructor() {
+    this.computer = []
+  }
   async startMessage() {
     Console.print(messages.gameStart)
   }
+  async createRandomNumber() {
+    while (this.computer.length < 3) {
+      const random = MissionUtils.Random.pickNumberInRange(1, 9)
+      if (!this.computer.includes(random)) this.computer.push(random)
+    }
+  }
   async play() {
     this.startMessage()
+    await this.createRandomNumber()
   }
 }
 
