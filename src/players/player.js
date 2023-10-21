294b4ecd34ba445a69c3eea8b9399460990e64d2
jonique98 <tnals9899@naver.com>
2023-10-21 14:46:59 +0900

    style: indext 맞춤

--- src/players/player.js
+++ src/players/player.js
@@ -3,50 +3,50 @@ import { MESSAGE } from '../constants/constants.js';
 
 export default class player {
 
-	#_userNumber;
+  #_userNumber;
 
-	async inputNumber() {
-		const userInput = await Console.readLineAsync(MESSAGE.INPUT);
-		const isValid = this.isValidNumber(userInput);
+  async inputNumber() {
+    const userInput = await Console.readLineAsync(MESSAGE.INPUT);
+    const isValid = this.isValidNumber(userInput);
 
-		if (isValid){
-			this.#_userNumber = userInput.split('').map((num) => Number(num));
-		}
+    if (isValid){
+      this.#_userNumber = userInput.split('').map((num) => Number(num));
+    }
 
-		return isValid
-	}
-
-	getJudgeResultPaper(opponent) {
-		const result = opponent.judgeResult(this.#_userNumber);
-		return result;
-	}
-
-	isValidNumber(userInput) {
-		const userInputToArray = userInput.split('').map((num) => Number(num));
-    let isValid = true;
-	  
-		if (userInputToArray.includes(0)) {
-		  isValid = false;
-		}
-	  
-		if (userInputToArray.length !== 3) {
-		  isValid = false;
-		}
-	  
-		if (this.hasDuplicates(userInputToArray)) {
-		  isValid = false;
-		}
-
-	  return isValid
+    return isValid
   }
 
-	hasDuplicates(userNumber) {
-		for (let i = 0; i < userNumber.length; i++) {
-			if (userNumber.includes(userNumber[i], i + 1)) {
-				return true;
-			}
-		}
-		return false;
-	}
+  getJudgeResultPaper(opponent) {
+    const result = opponent.judgeResult(this.#_userNumber);
+    return result;
+  }
+
+  isValidNumber(userInput) {
+    const userInputToArray = userInput.split('').map((num) => Number(num));
+    let isValid = true;
+    
+    if (userInputToArray.includes(0)) {
+      isValid = false;
+    }
+    
+    if (userInputToArray.length !== 3) {
+      isValid = false;
+    }
+    
+    if (this.hasDuplicates(userInputToArray)) {
+      isValid = false;
+    }
+
+    return isValid
+  }
+
+  hasDuplicates(userNumber) {
+    for (let i = 0; i < userNumber.length; i++) {
+      if (userNumber.includes(userNumber[i], i + 1)) {
+        return true;
+      }
+    }
+    return false;
+  }
 
 }
