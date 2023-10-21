528064940fa6850399d4809b088824c66e3a422a
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-10-21 11:00:21 +0900

    feat: add feature to validate Number
    Number 객체가 생성될 때 유효성 검사를 수행하는 기능 구현 및 테스트 완료.
    1. 길이 검사
    2. 타입 검사
    3. 중복 검사

--- src/Numbers.js
+++ src/Numbers.js
@@ -1,11 +1,42 @@
+const LENGTH = 3;
+const DATA_TYPE_REGEX = /^[1-9]+$/;
+const NO_DUPLICATES_REGEX = /^([a-zA-Z0-9])\1*$/;
+
+
 class Numbers {
 
 	_value = null;
 
 	constructor(value) {
-		console.log('Numbers.js/Numbers/constructor\n' +
-			'value => ', value);
+		this._validate(value);
 		this._value = value;
+	}
+
+	_validate(value) {
+		this._checkLength(value);
+		this._checkDataType(value);
+		this._checkDuplicate(value);
+	}
+
+	_checkLength(value) {
+		if (value.length !== LENGTH) {
+			// TODO: 에러메시지
+			throw new Error('Error in checkLength')
+		}
+	}
+
+	_checkDataType(value) {
+		if (!DATA_TYPE_REGEX.test(value)) {
+			// TODO: 에러메시지
+			throw new Error('Error in checkDataType');
+		}
+	}
+
+	_checkDuplicate(value) {
+		if (!NO_DUPLICATES_REGEX.test(value)) {
+			// TODO: 에러메시지
+			throw new Error('Error in checkDuplicate');
+		}
 	}
 
 	compare(target) {
