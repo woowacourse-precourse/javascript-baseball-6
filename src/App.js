133981fe294ee7bc66b297afb1cdbdd4b422c433
soominnnn <marisol03@naver.com>
2023-10-21 09:52:18 +0900

    기능 구현 완료 테스트 실패 ver

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import {RANDOM} from './module.js';
-import {USER} from './module.js';
+import {game} from './module.js';
+import {start} from './module.js';
+
 class App {
   async play() {
     RANDOM.random_create();
+    start();
+    game();
   }
 }
 const app = new App();
 app.play();
+
 export default App;
