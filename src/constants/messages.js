5f25f163919d71e91bd9e366f57adc6d2353def0
ReturnReason <reasonz@daum.net>
2023-10-22 04:43:52 +0900

    chore(messages): export 구문 추가, 뉴라인 제거

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,4 +1,4 @@
-const DONE_COUNT = 3;
+export const DONE_COUNT = 3;
 
 const RESTART_COMMAND = Object.freeze({
   NEWGAME_NUM: 1,
@@ -8,7 +8,7 @@ const RESTART_COMMAND = Object.freeze({
 export const MESSAGE = Object.freeze({
   START: '숫자 야구 게임을 시작합니다.',
   INPUT: '숫자를 입력해주세요 : ',
-  END: `${DONE_COUNT}개의 숫자를 모두 맞히셨습니다! 게임 종료\n`,
+  END: `${DONE_COUNT}개의 숫자를 모두 맞히셨습니다! 게임 종료`,
   RESTART: `게임을 새로 시작하려면 ${RESTART_COMMAND.NEWGAME_NUM}, 종료하려면 ${RESTART_COMMAND.END_NUM}를 입력하세요.\n`,
   getGameResult: ({ ball, strike }) => {
     if (!ball && !strike) {
