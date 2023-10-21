c19c990acd7944e79352f97d587f579d5219ee44
Goongam <dlwjddn341@naver.com>
2023-10-21 20:24:01 +0900

    Test: Random 및 Console 사용 테스트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import {Console, MissionUtils, Random} from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+
+    Console.print(computer); //[1, 3, 5] 출력
+  }
 }
+
 
 export default App;
