a0326903937da1103e89ff8b12a12fb998dd246d
JUNU <bokjunwoo@gmail.com>
2023-10-22 03:06:51 +0900

    feat: 게임 재시작 또는 종료 입력 처리 기능 추가

--- src/UserInput.js
+++ src/UserInput.js
@@ -14,6 +14,17 @@ const UserInput = {
     }
   },
 
+  async playAgainInputAsync() {
+    try {
+      const userInput = await MissionUtils.Console.readLineAsync(
+        '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
+      );
+      return userInput;
+    } catch (error) {
+      throw error;
+    }
+  },
+
   isValidInput(userInput) {
     const numberArray = userInput.split('').map(Number);
 
