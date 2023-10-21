0ce84c04970791cef84f4ff990b2609229175758
zelkovaria <hyein396@gmail.com>
2023-10-21 10:37:56 +0900

    컴퓨터의 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console, Random, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  computerNum() {
+    const computerNum = [];
+    do {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerNum.includes(number)) {
+        computerNum.push(number);
+      }
+    } while (computerNum.length !== 3);
+    const computerBall = computerNum.join("");
+    console.log(computerBall);
+    return computerBall;
+  }
+
+  async play() {
+    const computerBall = this.computerNum();
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import App from "App.js";
+import App from "./App.js";
 
 const app = new App();
 app.play();
