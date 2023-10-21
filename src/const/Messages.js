8e8d3ffe2e5f7ea49ac4c34d2b47d6b2464bbf59
hyj5076 <hyj5076@hanmail.net>
2023-10-21 20:41:53 +0900

    docs(Messages): 상수 메시지 정리

---
+++ src/const/Messages.js
@@ -0,0 +1,13 @@
+// 게임 안내
+const GAME_START = "숫자 야구 게임을 시작합니다.";
+const NOTICE_INPUT = "숫자를 입력해주세요";
+const SUGGEST_REPLAY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+// 게임 결과
+const STRIKE = "스트라이크";
+const BALL = "볼";
+const NOTHING = "낫싱";
+const GOAL = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+// 오류 메시지
+const ERROR_MESSAGE = "올바르지 않은 입력값 입니다.";
