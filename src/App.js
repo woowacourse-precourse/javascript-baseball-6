f1ddfaf7c36706bf5a09deffa819cebba40207e0
박지윤 <tkrhkfld123@naver.com>
2023-10-21 23:56:54 +0900

    feat: 사용자 입력값에 0이 포함된 경우 종료 기능 구현

--- src/App.js
+++ src/App.js
@@ -47,6 +47,9 @@ class App {
     if (user.length !== 3 || isNaN(user)) {
       throw "3자리 숫자를 입력해주세요.";
     }
+    if (user.includes("0")) {
+      throw "1과 9 사이의 숫자 3개를 입력해주세요.";
+    }
     this.countScore(computer, user);
   }
 
