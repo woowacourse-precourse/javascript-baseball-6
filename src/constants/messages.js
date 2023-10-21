2b83efd9b640b8da276abb9e9b6935014d1a746e
ReturnReason <reasonz@daum.net>
2023-10-22 03:33:12 +0900

    refactor(messages): 에러 메시지를 조금 더 명확하게 수정

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -36,7 +36,7 @@ export const MESSAGE = Object.freeze({
 const ERROR_PREFIX = '[ERROR]';
 export const ERROR_MESSAGE = Object.freeze({
   INVALID_RANGE: `${ERROR_PREFIX} 1부터 9까지의 숫자 3개를 입력해주세요.\n`,
-  INVALID_INPUT: `${ERROR_PREFIX} 숫자만 입력할 수 있습니다.\n`,
+  INVALID_INPUT: `${ERROR_PREFIX} 숫자 외의 문자 및 공백은 입력할 수 없습니다.\n`,
   INVALID_END_COMMAND: `${ERROR_PREFIX} 새 게임 시작(${RESTART_COMMAND.NEWGAME_NUM}) 또는 게임 종료(${RESTART_COMMAND.END_NUM})중 하나의 숫자만 입력해주세요.`,
   DUPLICATION_INPUT: `${ERROR_PREFIX} 중복된 숫자는 입력할 수 없습니다.\n`,
   INVALID_COPUTER_RANGE: `${ERROR_PREFIX} 1부터 9까지 범위의 숫자 3개로 이루어져야 합니다.\n`,
