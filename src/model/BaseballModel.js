30f9b0f6064dddbebd8ff3d038e18494a05ed55d
ReturnReason <reasonz@daum.net>
2023-10-22 02:14:21 +0900

    refactor: BaseballModel 모델 인스턴스 생성 시 생성자 함수에서 generateNumbers 메서드를 호출하고 있어서 컨트롤러에서 에러를 받지 못하는 부분 수정

--- src/model/BaseballModel.js
+++ src/model/BaseballModel.js
@@ -1,16 +1,25 @@
 import { Random } from '@woowacourse/mission-utils';
 import { BASEBALL_RANGE } from '../constants/range.js';
+import { ERROR_MESSAGE } from '../constants/messages.js';
 
 export class BaseballModel {
-  constructor() {
-    this.computerNumbers = this.#generateNumbers();
+  computerNumber;
+
+  create() {
+    this.computerNumber = this.#generateNumbers();
   }
 
   #generateNumbers() {
     const pickedNumbers = new Set();
     while (pickedNumbers.size !== BASEBALL_RANGE.LENGTH) {
-      pickedNumbers.add(Random.pickNumberInRange(BASEBALL_RANGE.START, BASEBALL_RANGE.END));
+      const pickedNumber = Random.pickNumberInRange(BASEBALL_RANGE.START, BASEBALL_RANGE.END);
+      // this.#checkInvalidateGeneratedNumber(0);
+      pickedNumbers.add(pickedNumber);
     }
     return Array.from(pickedNumbers).join('');
   }
+
+  #checkInvalidateGeneratedNumber(generatedNumber) {}
+
+  #checkComputerNumberLength(computerNumber) {}
 }
