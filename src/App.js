da7cbffb6b2cb8f0bf997b34613c64c82a77b3df
daeun-Kim <82816029+ddaeunbb@users.noreply.github.com>
2023-10-21 13:03:41 +0900

    [Feat]: 결과 출력 기능 구현 (#5)
    - 사용자가 입력한 숫자에 따라 볼, 스트라이크 결과를 출력하는 `printResult` 기능을 만들었습니다.
    - 결과는 볼의 개수와, 스트라이크의 수, 낫싱을 출력합니다.
    - 스트라이크가 3이라면 게임종료관련 문구를 출력합니다.

--- src/App.js
+++ src/App.js
@@ -4,24 +4,31 @@ const { Console, Random } = MissionUtils;
 const MESSAGE = {
   START: '숫자 야구 게임을 시작합니다.',
   INPUT: '숫자를 입력해주세요 : ',
+  FINISH: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  RESTART: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+  END: '게임이 종료되었습니다.'
 }
 
 const ERROR_MESSAGE = {
-  LENGTH: "[ERROR] 세 자리 숫자만 입력해야합니다.",
-  INT: "[ERROR] 정수만 입력해야합니다.",
-  NEGATIVE: "[ERROR] 양수만 입력해야합니다.",
+  ERROR: '[ERROR] 세 자리 숫자만 입력해야합니다.',
+  RESTART: '[ERROR] 숫자가 잘못된 형식입니다.',
 }
+
 class App {
   constructor(){
     this.randomNumber = [];
     this.userInput = [];
-    this.gameResults = {};
+    this.gameResults = {
+      ball: 0,
+      strike: 0,
+    };
 
     this.validation = {
       isLenThree: (input) => input.length === 3,
       isInt: (input) => Number.isInteger(+input),
       isNegative: answer => Math.sign(answer) === -1,
     }
+
     this.compare = {
       isStrike: ((num, idx) => this.randomNumber[idx] === num),
       isBall: ((num, idx) =>  this.randomNumber[idx] !== num && this.randomNumber.includes(num)),
@@ -30,6 +37,10 @@ class App {
 
   async play() {
     this.printStartNotification();
+    await this.start();
+  }
+
+  async start(){
     this.generateRandomNumber();
     await this.getUserInput();
   }
@@ -53,13 +64,12 @@ class App {
 
     this.userInput = [...input];
     this.getResult();
+    this.printResult();
   }
 
   validateInput(input){
-    const { isLenThree, isInt, isNegative } = this.validation;
-    if(!isLenThree(input)) throw new Error(ERROR_MESSAGE.LENGTH);
-    else if(!isInt(input)) throw new Error(ERROR_MESSAGE.INT);
-    else if(!isNegative(input)) throw new Error(ERROR_MESSAGE.NEGATIVE);
+    const { isLenThree, isInt } = this.validation;
+    if(!isLenThree(input) || !isInt(input)) throw new Error(ERROR_MESSAGE.ERROR);
   }
 
   getResult(){
@@ -67,9 +77,20 @@ class App {
     const { isStrike, isBall } = this.compare;
 
     this.userInput.forEach((num, idx)=> {
-      if(isStrike(num, idx)) gameResults.strike = gameResults.strike + 1 || 1;
-      if(isBall(num, idx)) gameResults.ball = gameResults.ball + 1 || 1;
+      if(isStrike(num, idx)) gameResults.strike = gameResults.strike += 1;
+      if(isBall(num, idx)) gameResults.ball = gameResults.ball +=  1;
     })
+  }
+
+  printResult(){
+    const { strike, ball } = this.gameResults;
+    
+    if(strike === 0 && ball === 0) Console.print('낫싱');
+    if(ball > 0 && strike > 0)  Console.print(`${ball}볼 ${strike}스트라이크`);
+    if(ball > 0 && strike === 0) Console.print(`${ball}볼`);
+    if(strike > 0 && ball === 0) Console.print(`${strike}스트라이크`);
+
+    if(strike === 3) Console.print(MESSAGE.FINISH);
   }
 }
 
