b16fab08c95939196f425dfb491ab7e6ed88787f
박지윤 <tkrhkfld123@naver.com>
2023-10-22 02:09:31 +0900

    feat(baseball2): 사용자가 입력한 숫자가 범위를 벗어난 경우 예외 처리 구현

--- src/retry/UserValidation.js
+++ src/retry/UserValidation.js
@@ -4,11 +4,20 @@ const UserValidation = {
   validateUser(user) {
     this.checkLength(user);
     this.checkRange(user);
+    this.checkDuplicate(user);
   },
 
   checkLength(user) {
     if (user.length !== 3) {
       throw new ValidationError('[ERROR] 3자리 숫자를 입력할 수 있습니다.');
+    }
+  },
+
+  checkRange(user) {
+    const regex = /^[1-9]+$/g;
+
+    if (!regex.test(user)) {
+      throw new ValidationError('[ERROR] 1과 9 사이의 숫자를 입력할 수 있습니다.');
     }
   },
 
