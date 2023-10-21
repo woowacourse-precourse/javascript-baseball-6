d82a02f005d7648cafdaddd38b1c895283518276
Trophy198 <spsp8426@naver.com>
2023-10-22 01:50:05 +0900

    Feat : 메시지를 담은 상수 파일 추가

--- src/constants/MessageConstants.js
+++ src/constants/MessageConstants.js
@@ -1,2 +1,7 @@
 export const GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 export const USER_INPUT_MESSAGE = "숫자를 입력해주세요. : ";
+export const GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+export const GAME_END_INSTRUCTION = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.';
+export const STRIKE_TEXT = '스트라이크';
+export const BALL_TEXT = '볼';
+export const NOTHING_TEXT = '낫싱';
