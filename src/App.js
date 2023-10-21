d86ede571d10a5c0e95c828f4f0a26e95fab5ffb
현재연 <jen@hyeonjaeyeon-ui-MacBookAir.local>
2023-10-21 17:43:03 +0900

    add docs/README

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    //컴퓨터 랜덤값 지정
+    function getComputer() {
+      const computer = [];
+      while (computer.length < 3) {
+        const number = MissionUtils.Random.pickNumberInRange(1, 9);
+        if (!computer.includes(number)) {
+          computer.push(number);
+        }
+      }
+    }
+    //user 입력값 지정
+    async function getUsername() {
+      try {
+        const username = await MissionUtils.Console.readLineAsync(
+          "숫자를 입력해주세요 : "
+        );
+      } catch (error) {
+        // reject 되는 경우
+        // console.print(error);
+      }
+    }
+  }
 }
 
 export default App;
