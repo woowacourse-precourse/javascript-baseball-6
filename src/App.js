df49fa1214c8dfb54a7e891db2efa076152d2b24
김경주 <popo505@naver.com>
2023-10-22 02:04:44 +0900

    feat:랜덤으로 숫자 생성하기

--- src/App.js
+++ src/App.js
@@ -7,6 +7,19 @@ class App {
     Console.print("숫자 야구 게임을 시작합니다."); 
   }
   
+  //랜덤으로 숫자 생성하기
+  randomNumber() {
+    const computerArr = [];
+    while (computerArr.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerArr.includes(number)) {
+        computerArr.push(number);
+      }
+    }
+    const computer =  computerArr.join('');
+    return computer;
+  }
+  
   async play() {
     this.start();
   }
