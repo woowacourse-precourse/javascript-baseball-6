f5b704f6031cb48b229eedaceb763e692a46fc32
hyj5076 <hyj5076@hanmail.net>
2023-10-22 02:28:35 +0900

    feat: 스트라이크 횟수 카운트 기능 추가

--- src/play/GameResult.js
+++ src/play/GameResult.js
@@ -6,5 +6,19 @@ const arrNumber = (arr) => {
   return arr.split('').map(Number); // 배열로 바꿈
 } 
 
-const computerNumberArray = arrNumber(computerNumber);
-const playerNumberArray = arrNumber(playerNumber);
+const computerArray = arrNumber(computerNumber);
+const playerArray = arrNumber(playerNumber);
+
+// 스트라이크 횟수 카운트
+const countStrike = () => {
+  let strike = 0;
+
+  for (let i = 0; i < 3; i++) {
+      if (computerArray[i] === playerArray[i]) {
+        strike++; // 조건이 만족될 때마다 strike 증가
+      }
+  }
+
+  Console.print(`${strike} 스트라이크`);
+}
+  
