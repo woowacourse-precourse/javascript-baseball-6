995fde18cdb952c218525b46d14c01d3ffd0a718
alexgoni <goni000211@gmail.com>
2023-10-21 18:56:58 +0900

    feat: 숫자 야구 게임 종료/재시작 기능

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import BaseballGame from "./BaseballGame";
+import { Console } from "@woowacourse/mission-utils";
+import BaseballGame from "./BaseballGame.js";
 
 class App {
   constructor() {
     this.baseballGame = new BaseballGame();
   }
   async play() {
-    this.baseballGame.startGame();
+    while (true) {
+      await this.baseballGame.startGame();
+
+      // 게임이 끝나면 사용자 입력을 받아 게임을 재시작할지 종료할지 확인
+      const userInput = await Console.readLineAsync(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+      );
+      switch (userInput) {
+        case "1":
+          this.baseballGame = new BaseballGame();
+          break; // 새로운 게임 시작
+        case "2":
+          return; // 종료
+        default:
+          throw new Error("올바른 입력이 아닙니다.");
+      }
+    }
   }
 }
 
--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -1,12 +1,13 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
+// 하나의 숫자 야구 게임 클래스
 export default class BaseballGame {
   constructor() {
     this.computer = [];
     this.correct = false;
   }
 
-  startGame() {
+  async startGame() {
     Console.print("숫자 야구 게임을 시작합니다.");
 
     // Computer random number 생성
@@ -17,19 +18,21 @@ export default class BaseballGame {
       }
     }
 
-    while (!this.correct) this.getUserInput();
-    if (this.correct) this.correctGuess;
+    Console.print(this.computer);
+
+    while (!this.correct) await this.getUserInput();
+    this.correct
+      ? Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+      : null;
   }
 
-  getUserInput() {
-    (async () => {
-      try {
-        const guess = await Console.readLineAsync("숫자를 입력해주세요 : ");
-        this.validateGuess(guess);
-      } catch (error) {
-        Console.print(error);
-      }
-    })();
+  async getUserInput() {
+    try {
+      const guess = await Console.readLineAsync("숫자를 입력해주세요 : ");
+      this.validateGuess(guess);
+    } catch (error) {
+      Console.print(error);
+    }
   }
 
   validateGuess(guess) {
@@ -44,12 +47,12 @@ export default class BaseballGame {
     });
 
     const output =
-      (balls === 0 ? "" : `${balls}볼`) +
+      (balls === 0 ? "" : `${balls}볼 `) +
       (strikes === 0 ? "" : `${strikes}스트라이크`);
     Console.print(output || "낫싱");
 
     strikes === 3 ? (this.correct = true) : null;
   }
 
-  correctGuess() {}
+  checkError() {}
 }
