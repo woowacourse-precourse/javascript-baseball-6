75eddf32fd15963f2c685fba9efecc0389b1f2e2
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-10-21 22:44:05 +0900

    Fix: 상수명 수정

--- src/App.js
+++ src/App.js
@@ -8,20 +8,24 @@ class App {
     this.message;
   }
 
-  userPickNums() {
+  userNumbersInput() {
     Console.readLine(
       "1~9 숫자 내에서 중복 없이 임의의 숫자 3자리를 입력하세요.",
       (nums) => {
-        const PICK_NUMS_IN_ARRAY = nums.split("").map(Number);
-        if (PICK_NUMS_IN_ARRAY.length !== 3)
+        const PICK_USER_NUMBERS = nums.split("").map(Number);
+
+        if (PICK_USER_NUMBERS.length !== 3)
           throw new Error(MESSAGES.GAME_ERROR.LENGTH_VALID);
-        const CORRECT_NUMS_IN_RANGE = (nums) => 1 <= nums && nums < 10;
-        if (!PICK_NUMS_IN_ARRAY.every(CORRECT_NUMS_IN_RANGE))
+
+        const CORRECT_USER_NUMBERS_IN_RANGE = (nums) => 1 <= nums && nums < 10;
+        if (!PICK_USER_NUMBERS.every(CORRECT_USER_NUMBERS_IN_RANGE))
           throw new Error(MESSAGES.GAME_ERROR.RANGE_VALID);
-        const DUPLICATE_NUMS_IN_ARRAY = new Set([...PICK_NUMS_IN_ARRAY]);
-        if (DUPLICATE_NUMS_IN_ARRAY.length !== nums.length)
+
+        const DUPLICATE_USER_NUMBERS_IN_ARRAY = [...new Set(arr)];
+        if (DUPLICATE_USER_NUMBERS_IN_ARRAY.length !== nums.length)
           throw new Error(MESSAGES.GAME_ERROR.DUPLICATE_VALID);
-        else this.userAnswer = nums;
+
+        this.userAnswer = nums;
       }
     );
   }
