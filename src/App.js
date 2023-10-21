8a31f2766993cad6afdc16065c1f0e18d0686c79
soyoung125 <sypark041754@gmail.com>
2023-10-21 23:19:55 +0900

    feat: 입력 숫자 비교하고 힌트 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -15,13 +15,56 @@ class App {
     return [...numbers];
   }
 
+  compareNumbers(inputNumbers) {
+    const numbers = inputNumbers.split("");
+    let strike = 0;
+    let ball = 0;
+
+    numbers.map((n, index) => {
+      const number = parseInt(n);
+
+      if (this.numbers.includes(number)) {
+        if (this.numbers[index] === number) {
+          strike += 1;
+        } else {
+          ball += 1;
+        }
+      }
+    })
+
+    if (strike === 3) {
+      MissionUtils.Console.print(`${strike}스트라이크`);
+      return true;
+    }
+
+    if (strike === 0) {
+      if (ball === 0) {
+        MissionUtils.Console.print("낫싱");
+      } else {
+        MissionUtils.Console.print(`${ball}볼`);
+      }
+    } else {
+      if (ball === 0) {
+        MissionUtils.Console.print(`${strike}스트라이크`);
+      } else {
+        MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+      }
+    }
+
+    return false;
+  }
+
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.")
 
     while(1) {
       const input = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ")
-      MissionUtils.Console.print(input);
+      if (this.compareNumbers(input)){
+        break;
+      }
     }
+
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
   }
 }
 
