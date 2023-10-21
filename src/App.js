ff17f2fd7da999d1153bf5a7e94fba790d24b310
easyhyun00 <leejihyun0324@gmail.com>
2023-10-21 14:14:36 +0900

    :sparkles: feat: 숫자 맞추기 #2 - 사용자(플레이어) 숫자 입력 받기

--- src/App.js
+++ src/App.js
@@ -7,11 +7,23 @@ class App {
     while(true) {
       // 상대방(컴퓨터)의 수 얻기 - getComputerNumbers() - 구현 완료
       const computerNumbers = this.getComputerNumbers();
-      MissionUtils.Console.print(computerNumbers);
 
-      /*
-        숫자 맞추기 - 미구현
-      */
+      // 숫자 맞추기
+      while(true) {
+        // 플레이어의 숫자 입력 받기 - getUserInput() - 구현 완료
+        const userInput = await this.getUserInput();
+        MissionUtils.Console.print("숫자를 입력해주세요 : " + userInput);
+
+        // 숫자로 된 배열로 변경
+        const userInputArr = userInput.split('').map(Number);
+
+        /*
+          유효성 검사 - 미구현
+          입력한 수와 상대방(컴퓨터)수에 대한 결과 - 미구현
+        */
+        
+        break;
+      }
 
       // 재시작/종료 여부 얻기 - askForRestart() - 미구현
       const restart = 2
@@ -28,6 +40,11 @@ class App {
       computer.add(MissionUtils.Random.pickNumberInRange(1, 9));
     }
     return Array.from(computer);
+  }
+
+  // 플레이어의 숫자 입력 받기
+  async getUserInput() {
+    return await MissionUtils.Console.readLineAsync();
   }
 }
 
