fb4f4477d97af03104e7d0415bfa5a7f6e01c346
hyewon <hyewon.parkhw@gmail.com>
2023-10-21 23:13:21 +0900

    feat: 사용자 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+      MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+      let replay = 1;
+      while (replay==1){
+        let strike=0;
+        while (strike<3){
+          //strike 초기화
+          //strike=0;
+          const playerNumber = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 :");
+          //숫자 검사 -> strike라면
+          strike++
+        }
+        replay = await MissionUtils.Console.readLineAsync(
+          "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+        );
+      }
+      
+  }
 }
 
 export default App;
