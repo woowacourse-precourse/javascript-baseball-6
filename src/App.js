1d7b5fcd0b55652e88571365a918d2129c42e7e3
meunrud <113085963+meunrud@users.noreply.github.com>
2023-10-21 15:13:24 +0900

    feat : 랜덤한 3개의 숫자 생성 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 class App {
-  async play() {
+  async play() {}
+
+  StartMessage() {
     const StartMessage = '숫자 야구 게임을 시작합니다.';
     console.log(StartMessage);
+  }
+
+  MakeRandomNumbers() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
   }
 }
 
