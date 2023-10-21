e47f5c7bc0a39072cede6236480abc0bb0ba4023
brgndy <brgndy96@gmail.com>
2023-10-21 11:47:07 +0900

    feat(viewController): 입력값을 받기 위한 readLine.js 함수 작성

---
+++ src/viewControllers/readLine.js
@@ -0,0 +1,10 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+/**
+ * 입력값을 받아주는 함수
+ * @param {string} message
+ */
+
+export const readLine = async (message) => {
+  return await MissionUtils.Console.readLineAsync(message);
+};
