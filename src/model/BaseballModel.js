e4c8d05b7b5cdc9e6b41ec417bd13ea13470950c
ReturnReason <reasonz@daum.net>
2023-10-22 02:25:17 +0900

    feat: 컴퓨터가 생성한 숫자가 1부터 9까지로 이루어져 있는지, 3자리 숫자로 이루어져 있는지 확인 후 예외를 발생하는 메서드 추가

--- src/model/BaseballModel.js
+++ src/model/BaseballModel.js
@@ -7,19 +7,28 @@ export class BaseballModel {
 
   create() {
     this.computerNumber = this.#generateNumbers();
+    this.#checkComputerNumberLength(this.computerNumber);
   }
 
   #generateNumbers() {
     const pickedNumbers = new Set();
     while (pickedNumbers.size !== BASEBALL_RANGE.LENGTH) {
       const pickedNumber = Random.pickNumberInRange(BASEBALL_RANGE.START, BASEBALL_RANGE.END);
-      // this.#checkInvalidateGeneratedNumber(0);
+      this.#checkInvalidateGeneratedNumber(pickedNumber);
       pickedNumbers.add(pickedNumber);
     }
     return Array.from(pickedNumbers).join('');
   }
 
-  #checkInvalidateGeneratedNumber(generatedNumber) {}
+  #checkInvalidateGeneratedNumber(generatedNumber) {
+    if (generatedNumber < BASEBALL_RANGE.START || generatedNumber > BASEBALL_RANGE.END) {
+      throw ERROR_MESSAGE.INVALID_COPUTER_RANGE;
+    }
+  }
 
-  #checkComputerNumberLength(computerNumber) {}
+  #checkComputerNumberLength(computerNumber) {
+    if (computerNumber.length !== BASEBALL_RANGE.length) {
+      throw ERROR_MESSAGE.INVALID_COPUTER_RANGE;
+    }
+  }
 }
