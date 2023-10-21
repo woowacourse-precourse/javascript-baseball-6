e59846d2ba34f0eaa148ca93f08ec8da2c5f41c1
dongsuseo <poopoo0515@naver.com>
2023-10-21 17:06:22 +0900

    feat: 게임 종료 시 다시 시작 or 종료 처리하기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Console, MissionUtils } from "@woowacourse/mission-utils";
 let userInput = "";
 let strike = 0;
 let ball = 0;
+let stop = false;
 
 // 정답 값과 입력 값 비교하기
 const compare = (COMPUTER, userInput) => {
@@ -25,36 +26,56 @@ const compare = (COMPUTER, userInput) => {
     ball++;
   }
 };
+// 게임 종료 시 다시 시작 or 종료 처리하기
+const reStart = async () => {
+  strike = 0;
+  ball = 0;
+  const START = await Console.readLineAsync(
+    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+  );
+  if (START === "1") {
+  } else if (START === "2") {
+    Console.print("게임을 종료합니다.");
+    stop = true;
+  }
+};
+
 class App {
   async play() {
-    // 정답 값 랜덤으로 생성하기
-    const COMPUTER = [];
+    stop = false;
 
-    while (COMPUTER.length < 3) {
-      const NUMBER = MissionUtils.Random.pickNumberInRange(1, 9).toString();
-      if (!COMPUTER.includes(NUMBER)) {
-        COMPUTER.push(NUMBER);
-      }
-    }
-    while (strike < 3) {
-      // 유저 입력 값 받기
-      userInput = await Console.readLineAsync("숫자를 입력해주세요 : ");
-      compare(COMPUTER, userInput);
+    while (stop === false) {
+      Console.print("숫자 야구 게임을 시작합니다.");
+      // 정답 값 랜덤으로 생성하기
+      const COMPUTER = [];
 
-      if (strike === 0 && ball === 0) {
-        Console.print("낫싱");
-      } else if (strike === 0) {
-        Console.print(`${ball}볼`);
-      } else if (ball === 0) {
-        if (strike === 3) {
-          Console.print("3스트라이크");
-          Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-          break;
+      while (COMPUTER.length < 3) {
+        const NUMBER = MissionUtils.Random.pickNumberInRange(1, 9).toString();
+        if (!COMPUTER.includes(NUMBER)) {
+          COMPUTER.push(NUMBER);
         }
-        Console.print(`${strike}스트라이크`);
-      } else {
-        Console.print(`${ball}볼 ${strike}스트라이크`);
       }
+      while (strike < 3) {
+        // 유저 입력 값 받기
+        userInput = await Console.readLineAsync("숫자를 입력해주세요 : ");
+        compare(COMPUTER, userInput);
+
+        if (strike === 0 && ball === 0) {
+          Console.print("낫싱");
+        } else if (strike === 0) {
+          Console.print(`${ball}볼`);
+        } else if (ball === 0) {
+          if (strike === 3) {
+            Console.print("3스트라이크");
+            Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            break;
+          }
+          Console.print(`${strike}스트라이크`);
+        } else {
+          Console.print(`${ball}볼 ${strike}스트라이크`);
+        }
+      }
+      await reStart();
     }
   }
 }
