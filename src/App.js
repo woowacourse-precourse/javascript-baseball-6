831ccd7f03de68d30ce3ef2ed9769ca3d70c04de
eternalclash <suchasucha@naver.com>
2023-10-21 21:51:22 +0900

    Feat: 정답 생성

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import { Console } from "@woowacourse/mission-utils";
-class App  {
-  constructor(){
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+class App {
+  constructor() {
     this.init();
   }
 
-  init () {
+  init() {
     Console.print("숫자 야구 게임을 시작합니다.");
   }
-  
+
   async play() {
-    try{
-      const numberInput = await Console.readLineAsync("숫자를 입력해주세요 : ")
+    const answer = [];
+    while (answer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!answer.includes(number)) {
+        answer.push(number)
+      }
     }
-    catch (error) {
 
+    while (True) {
+      try {
+        const numberInput = await Console.readLineAsync(
+          "숫자를 입력해주세요 : "
+        );
+      } catch (error) {}
     }
-
   }
-  async invalidateInput (query) {
-
-  }
-  
+  async invalidateInput(query) {}
 }
-const app = new App()
-app.play()
+const app = new App();
+app.play();
 export default App;
