0a41943de03891f639a05852d12ac01ee54dacec
Su Hwan CHOI <hap_hazard@naver.com>
2023-10-22 02:58:28 +0900

    feat: 사용자 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-class App {
-  async play() {}
+import generateNumber from './utils/Number.js';
+import { print, input } from './view/View.js';
+import Message from './utils/Message.js';
+
+export default class App {
+  #answerNumber;
+
+  constructor() {
+    this.init();
+  }
+
+  async play() {
+    try {
+      print(Message.GAME_START);
+      const inputNumber = (await input(Message.INPUT_NUMBER)).trim();
+      console.log('사용자의 입력:', inputNumber);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  init() {
+    this.#answerNumber = generateNumber(1, 9);
+    console.log(this.#answerNumber);
+  }
 }
 
 const app = new App();
 app.play();
-
-export default App;
