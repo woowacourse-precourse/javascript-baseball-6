6ad7d132789d375bc4e8b7801fe97551bcb484c9
Trophy198 <spsp8426@naver.com>
2023-10-21 14:16:11 +0900

    Feat : BaseballGame과 App 클래스의 기본 구조 초기화

--- src/App.js
+++ src/App.js
@@ -1,101 +1,10 @@
-import { MissionUtils, Console } from "@woowacourse/mission-utils";
-import { validateInput,validateGameEndInput } from './validation.js'; 
-import { NUM_DIGITS, RESTART_GAME, GAME_OVER_MESSAGE,GAME_END_INSTRUCTION,STRIKE_TEXT,BALL_TEXT,NOTHING_TEXT, END_GAME } from "./Constants.js";
+import BaseballGame from "./BaseballGame";
 
 class App {
   async play() {
-    startGameTitle();
-    await gamePlay(createComputerNumber());
+    const game = new BaseballGame();
+   await game.start();
   }
 }
 
-function processGame(userNumber,computerNumber) {
-  displayCountResult(userNumber,computerNumber);
-  return numbersEqual(userNumber,computerNumber);
-}
-
-async function gamePlay(computerNumber) {
-  const userNumber = await createUserNumber();
-  const isEqual = processGame(userNumber,computerNumber);
-
-  if(!isEqual) return gamePlay(computerNumber);
-  await gamePlayResult();
-}
-
-async function gamePlayResult() {
-  Console.print(GAME_OVER_MESSAGE);
-  const gameEnd = await Console.readLineAsync(GAME_END_INSTRUCTION);
-  validateGameEndInput(gameEnd);
-  if(gameEnd === RESTART_GAME) {
-    return gamePlay(createComputerNumber());
-  } else if(gameEnd === END_GAME) {
-    return false;
-  }
-}
-
-function numbersEqual(userNumber, computerNumber) {
-  return userNumber.length === computerNumber.length && userNumber.every((number, index) => number === computerNumber[index]);
-}
-
-function startGameTitle() {
-  return Console.print("숫자 야구 게임을 시작합니다.");
-}
-
-function createComputerNumber() {
-  const computer = [];
-  while (computer.length < NUM_DIGITS) {
-    const number = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!computer.includes(number)) {
-      computer.push(number);
-  }
-}
-  return computer;
-}
-
-async function createUserNumber() {
-  const createNumber = await Console.readLineAsync('숫자를 입력해주세요 : ');
-  validateInput(createNumber);
-  const userNumber = Array.from(createNumber).map((value) => Number(value));
-
-  return userNumber;
-}
-
-function calculateStrikeCount(userNumber,computerNumber) {
-  let strike = 0;
-  for(let i = 0; i < computerNumber.length; i++) {
-    if(computerNumber[i] === userNumber[i]) {
-      strike++;
-    }
-  }
-  return strike;
-}
-
-function calculateBallCount(userNumber,computerNumber) {
-  let ball = 0;
-  const userNumberSet = new Set(userNumber);
-  for(let i = 0; i < computerNumber.length; i++) {
-    if(computerNumber[i] !== userNumber[i] && userNumberSet.has(computerNumber[i])) {
-      ball++;
-    }
-  }
-  return ball;
-}
-
-function displayCountResult(userNumber, computerNumber) {
-  const strike = calculateStrikeCount(userNumber, computerNumber);
-  const ball = calculateBallCount(userNumber, computerNumber);
-
-  if (strike > 0 && ball > 0) {
-    Console.print(`${ball}${BALL_TEXT} ${strike}${STRIKE_TEXT}`);
-  } else if (strike > 0) {
-    Console.print(`${strike}${STRIKE_TEXT}`);
-  } else if (ball > 0) {
-    Console.print(`${ball}${BALL_TEXT}`);
-  } else {
-    Console.print(NOTHING_TEXT);
-  }
-}
-
-const app = new App();
-app.play();
 export default App;
---
+++ src/BaseballGame.js
@@ -0,0 +1,5 @@
+export default class BaseballGame {
+    start() {
+        
+    }
+}
