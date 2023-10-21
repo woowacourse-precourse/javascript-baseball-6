94a326fd36255ae28bf64c16c109989680b10c8c
ReturnReason <reasonz@daum.net>
2023-10-22 01:20:01 +0900

    feat: BaseballController start 메서드 추가

--- src/controller/BaseballController.js
+++ src/controller/BaseballController.js
@@ -1,1 +1,12 @@
-class BaseballController {}
+import { MESSAGE } from '../constants/messages.js';
+
+export class BaseballController {
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+  }
+
+  async start() {
+    this.view.printMessage(MESSAGE.START);
+  }
+}
