eeae00a878792ecc6325ba1d4c5926d5a9ea1836
ReturnReason <reasonz@daum.net>
2023-10-22 05:48:55 +0900

    feat(BaseballController): 게임 종료 후 새게임 커맨드를 입력받고 유효한지 체크하는 로직 추가

--- src/controller/BaseballController.js
+++ src/controller/BaseballController.js
@@ -1,35 +1,50 @@
-import { MESSAGE } from '../constants/messages.js';
+import { RESTART_COMMAND } from '../constants/commands.js';
+import { ERROR_MESSAGE, MESSAGE } from '../constants/messages.js';
 
 export class BaseballController {
   constructor(model, view) {
     this.model = model;
     this.view = view;
+    this.view.printMessage(MESSAGE.START);
   }
 
   async start() {
-    this.view.printMessage(MESSAGE.START);
     try {
       this.model.create();
 
       while (!this.model.isDone) {
-        await this.playGame();
+        await this.#playGame();
       }
 
       const restartCommand = await this.view.getInputAsync(MESSAGE.RESTART);
+      this.checkRestartCommand(restartCommand);
+
+      restartCommand === RESTART_COMMAND.NEWGAME ? this.start() : this.quit();
     } catch (error) {
       this.view.printMessage(error);
     }
   }
 
-  async playGame() {
+  async #playGame() {
     const userNumber = await this.view.getInputAsync(MESSAGE.INPUT);
-    this.settingUserNumber(userNumber);
+    this.#settingUserNumber(userNumber);
 
     const gameResult = this.model.getGameResult();
     this.view.printGameResult(gameResult);
   }
 
-  settingUserNumber(userNumber) {
+  quit() {
+    return;
+  }
+
+  #settingUserNumber(userNumber) {
     this.model.setUserNumber(userNumber);
+  }
+
+  checkRestartCommand(command) {
+    const validCommands = [RESTART_COMMAND.NEWGAME, RESTART_COMMAND.QUIT];
+    if (!validCommands.includes(command)) {
+      throw ERROR_MESSAGE.INVALID_END_COMMAND;
+    }
   }
 }
