c3c0ac40b4611e3a554c76cbedd8b16e57b521d4
mywhzy <cmj2393a@naver.com>
2023-10-21 19:40:50 +0900

    feat: 두 값 비교 및 결과 반환 기능 구현

--- src/game.js
+++ src/game.js
@@ -1,9 +1,11 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import getComputerNumbers from './utils/getComputerNumbers.js';
 import getPlayerNumbers from './utils/getPlayerNumbers.js';
+import getResult from './utils/getResult.js';
 
-export default function game() {
+export default async function game() {
   MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-  getComputerNumbers();
-  getPlayerNumbers();
+  const computerNumbers = getComputerNumbers();
+  const playerNumbers = await getPlayerNumbers();
+  getResult(computerNumbers, playerNumbers);
 }
---
+++ src/utils/getResult.js
@@ -0,0 +1,51 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+export default function getResult(computerNumbers, playerNumbers) {
+  const computerArr = Array.from(computerNumbers + '');
+  const playerArr = Array.from(playerNumbers + '');
+
+  const hasNothing = getNothing(computerArr, playerArr);
+
+  let ball, strike;
+  if (!hasNothing) {
+    ball = getBall(computerArr, playerArr);
+    strike = getStrike(computerArr, playerArr);
+  }
+
+  const result = (ball ? ball : '') + (strike ? strike : '');
+  result !== '' && MissionUtils.Console.print(result);
+}
+
+const getNothing = (computerNum, playerNum) => {
+  const nothingArr = computerNum.filter((item) => !playerNum.includes(item));
+  if (nothingArr.length === 3) {
+    MissionUtils.Console.print('낫싱');
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+const getBall = (computerNum, playerNum) => {
+  const result = computerNum.map(
+    (item, index) =>
+      playerNum.includes(item) && playerNum.indexOf(item) !== index
+  );
+  const ballCount = result.filter((item) => item).length;
+
+  if (ballCount) {
+    return ballCount + '볼 ';
+  }
+};
+
+const getStrike = (computerNum, playerNum) => {
+  const result = computerNum.map(
+    (item, index) =>
+      playerNum.includes(item) && playerNum.indexOf(item) === index
+  );
+  const strikeCount = result.filter((item) => item).length;
+
+  if (strikeCount) {
+    return strikeCount + '스트라이크';
+  }
+};
