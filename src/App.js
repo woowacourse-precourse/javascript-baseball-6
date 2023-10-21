15c3e4a397b2421e8f54535d1725cc1d9cea7b61
김다인 <dbslzhs1218@naver.com>
2023-10-22 05:29:26 +0900

    feat: 게임 시작 문구 출력 및 컴퓨터 임의의 서로 다른 숫자 3개 랜덤 생성 기능 추가
    - MissionUtils를 임포트하여 사용
    - computerPickNumber(): 중복없는 숫자 3개가 들어있는 배열을 반환하는 메서드

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import {MissionUtils} from '@woowacourse/mission-utils';
 class App {
-  async play() {}
+  constructor() {
+  }
+
+  computerPickNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
+  
+
+  async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.computerPickNumber()
+  }
 }
 
+const app = new App();
+app.play();
+
 export default App;
+
