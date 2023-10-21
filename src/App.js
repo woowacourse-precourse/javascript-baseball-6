803fa0da8bdb6f13fd75a6014e399007644ef1d3
youjeong Jeong <wjddbwjd1205@naver.com>
2023-10-21 21:51:01 +0900

    feat: 게임을 시작하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { BaseballGame } from "./BaseballGame.js";
+
 class App {
-  async play() {}
+  async play() {
+    const baseballGame =  new BaseballGame();
+    baseballGame.startGame();
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
