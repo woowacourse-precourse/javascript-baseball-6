dca1f019ac0135c62f361b6aa0dc5dc383c59f00
soosoo22 <lllllh00@naver.com>
2023-10-21 20:26:40 +0900

    chore: 함수 설명 주석 제거
    기존에 함수 설명에 대한 주석을 달았는데 제거함

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ class App {
     await this.inputGuessNumber();
   }
 
-  //추측값 입력
   async inputGuessNumber() {
     try {
       const guessNumber = await MissionUtils.Console.readLineAsync(
@@ -36,14 +35,12 @@ class App {
     }
   }
 
-  //입력한 값이 서로 다른 3자리 수인지 체크
   validateInput(guessNumber) {
     return new Set(guessNumber).size !== 3 || isNaN(Number(guessNumber))
       ? false
       : true;
   }
 
-  //랜덤 값 생성
   makeRandomNumber() {
     const computer = [];
     while (computer.length < 3) {
@@ -55,7 +52,6 @@ class App {
     return computer;
   }
 
-  //볼, 스트라이크 개수 체크
   countBallAndStrike(computerNumber, guessNumber) {
     let b = 0;
     let s = 0;
@@ -68,7 +64,6 @@ class App {
     s === 3 ? this.restartMessage() : this.gameResultMessage(b, s);
   }
 
-  //게임 끝난 직후 재시작/종료 메시지 출력
   async restartMessage() {
     MissionUtils.Console.print(TEXT.THREE_STRKE_MESSAGE);
     const restartOrExit = await MissionUtils.Console.readLineAsync(
@@ -82,7 +77,6 @@ class App {
     }
   }
 
-  //게임 결과 출력
   gameResultMessage(ball, strike) {
     const ballmessage = ball > 0 ? `${ball}볼 ` : "";
     const strikeMessage = strike > 0 ? `${strike}스트라이크` : "";
