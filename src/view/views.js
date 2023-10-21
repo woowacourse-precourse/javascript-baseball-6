42333a7ea413188a45b1a3a5d51dd174b88a4ad0
jonique98 <tnals9899@naver.com>
2023-10-21 15:04:15 +0900

    correct 메세지를 출력해주는 메소드

--- src/view/views.js
+++ src/view/views.js
@@ -2,8 +2,24 @@ import { Console } from '@woowacourse/mission-utils';
 import { MESSAGE } from '../constants/constants.js'
 
 export default class views{
-
 	constructor() {
 		Console.print(MESSAGE.START);
+	}
+
+	correct() {
+		Console.print(MESSAGE.CORRECT);
+	}
+
+	result(result) {
+		Console.print(this.makeResultMessage(result));
+	}
+
+	async retry() {
+		const reryOrNot = Console.readLineAsync(MESSAGE.RETRY);
+		return reryOrNot;
+	}
+
+	gameOver() {
+		Console.print(MESSAGE.GAMEOVER);
 	}
 }
