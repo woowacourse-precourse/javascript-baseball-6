3c018c8887440eff77a7127052a1d59d69193c4e
jonique98 <tnals9899@naver.com>
2023-10-21 15:05:12 +0900

    feat: result 메세지를 만들어 주는 메소드

--- src/view/views.js
+++ src/view/views.js
@@ -13,4 +13,24 @@ export default class views{
 	result(result) {
 		Console.print(this.makeResultMessage(result));
 	}
+
+	makeResultMessage(result) {
+		let resultMESSAGE = '';
+	
+		if (result.NOTHING === true) {
+			resultMESSAGE = resultMESSAGE.concat(MESSAGE.NOTHING);
+			return resultMESSAGE;
+		}
+	
+		if (result.BALL !== 0) {
+			resultMESSAGE = resultMESSAGE.concat(String(result.BALL) + MESSAGE.BALL + ' ');
+		}
+	
+		if (result.STRIKE !== 0) {
+			resultMESSAGE = resultMESSAGE.concat(String(result.STRIKE) + MESSAGE.STRIKE + ' ');
+		}
+		
+		return resultMESSAGE;
+	}
+
 }
