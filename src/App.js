d4c9141b6ed7df67aa3aadf6674b9ff53252f9fe
노태균 <taegyunn123@gmail.com>
2023-10-21 17:38:49 +0900

    feat: baseBallGame을 실행하기 위한 play 함수 구현
    BaseBallGame 생성 후 start() 호출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import BaseBallGame from "./model/BaseBallGame.js";
+
 class App {
-  async play() {}
+  async play() {
+    // 새로운 게임 만들기
+    const baseBallGame = new BaseBallGame();
+    // 게임 시작
+    await baseBallGame.start();
+  }
 }
 
 export default App;
