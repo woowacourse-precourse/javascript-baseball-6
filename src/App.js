8e9317e6d365d50f20e3349f75191fa9dec763b9
anr4876 <dustjdanr@naver.com>
2023-10-21 14:31:44 +0900

    ball과 strike개수 세기 기능

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ async function getUserNumber() {
 }
 
 
-// 입력받은 문자열이 1~9의 숫자중 3개의 서로다른 수인지 체크
+// 입력받은 문자열이 1~9의 숫자중 3개의 서로다른 수인지 체크 (예외처리)
 function checkNumber(number) { 
   const TEST_NUMBER = /[^1-9]/.test(number);
   const NUMBER_SET = new Set(number);
@@ -29,19 +29,62 @@ function checkNumber(number) {
 }
 
 
+// ball, strike 개수 체크
+function countCheck(userNumber, computer_number){
+  let ball = 0;
+  let strike = 0;
+
+  userNumber.split('').forEach((num, idx) => {
+    if(computer_number.indexOf(num) === idx)
+      strike++;
+    else if(computer_number.split("").includes(num))
+      ball++;
+  })
+  return [ball, strike];
+}
+
+
+// 정답 출력 
+function answerPrint(ball, strike) {
+  let answer = "";
+  if(ball !== 0){
+    if(strike !== 0){
+      answer = ball + "볼 " + strike + "스트라이크";          
+    }else {
+      answer = ball + "볼";
+    }
+  }
+  else if(strike !== 0){
+    answer = strike + "스트라이크";
+  }
+  else {
+    answer = '낫싱';
+  }
+  return answer;
+}
+
+
 // 실행
 class App {
   async play() {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.')
+
     let computer_number = randomNumber();
+    let userNumber;
     
-    let userNumber = await getUserNumber();
 
-    if(checkNumber(userNumber)){
-      throw new Error("[ERROR]");
+    while(true){
+      userNumber = await getUserNumber();
+
+      if(checkNumber(userNumber)){
+        throw new Error("[ERROR]");
+      }
+
+      let [ball, strike] = countCheck(userNumber, computer_number);
+      
+      const ANSWER = answerPrint(ball, strike);
+      MissionUtils.Console.print(ANSWER);
     }
-
-    console.log(userNumber);
   }
 }
 
