b4521da64a4df3f99bdface8f2b18787da7934ad
anr4876 <dustjdanr@naver.com>
2023-10-21 14:24:03 +0900

    사용자의 입력과 예외 처리 기능

--- src/App.js
+++ src/App.js
@@ -14,12 +14,34 @@ function randomNumber() {
 }
 
 
+// 사용자의 숫자 입력받기
+async function getUserNumber() {
+  const userNumber = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+  return userNumber;
+}
+
+
+// 입력받은 문자열이 1~9의 숫자중 3개의 서로다른 수인지 체크
+function checkNumber(number) { 
+  const TEST_NUMBER = /[^1-9]/.test(number);
+  const NUMBER_SET = new Set(number);
+  return TEST_NUMBER || NUMBER_SET.size !== 3 || number.length > 3 ? true : false;
+}
+
+
 // 실행
 class App {
   async play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     let computer_number = randomNumber();
-    console.log(computer_number);
+    
+    let userNumber = await getUserNumber();
+
+    if(checkNumber(userNumber)){
+      throw new Error("[ERROR]");
+    }
+
+    console.log(userNumber);
   }
 }
 
