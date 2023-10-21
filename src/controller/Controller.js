39e25ae1e7a3334f761763c08c11ae439e9caa47
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-21 16:34:11 +0900

    feat: [Controller] 플레이어가 입력한 input값에 따른 결과 가공 로직 구현

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -34,4 +34,28 @@ export default class Controller {
 
     await this.checkPlayerInput();
   }
+
+  /**
+   * player가 입력한 input값을 가공한 후 가공 결과를 출력하고, 다음 단계로 진행합니다.
+   */
+  async checkPlayerInput() {
+    const COMPUTER_NUMBER = this.computer.getNumber();
+    const PLAYER_NUMBER = this.player.getNumber();
+    let ballStrikeCounts = [0, 0];
+
+    for(let i = 0; i < COMPUTER_NUMBER.length; i++) {
+      if(PLAYER_NUMBER[i] === COMPUTER_NUMBER[i]) {
+        ballStrikeCounts[1]+=1
+      }
+      else if(PLAYER_NUMBER[i] !== COMPUTER_NUMBER[i] &&
+          COMPUTER_NUMBER.includes(PLAYER_NUMBER[i])
+        ) {
+          ballStrikeCounts[0]+=1
+        }
+    }
+    
+    this.view.printPlayerGuessResult(ballStrikeCounts);
+
+    await this.checkEndGame(ballStrikeCounts);
+  }
 }
