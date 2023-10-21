452779be9291a35949e88d1b633b09bbf34d682e
seoye0ng <bsy0313@gmail.com>
2023-10-21 18:23:43 +0900

    Feat: player answer와 정답 비교 후 결과 출력해주는 기능 구현

--- src/App.js
+++ src/App.js
@@ -45,6 +45,40 @@ class App {
 
     return playerAnswerArr;
   }
+
+  // 숫자 비교, 결과 출력
+  compareAnswer(computer, player) {
+    if (player.join('') === computer.join('')) {
+      MissionUtils.Console.print('3스트라이크 \n 3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      return true;
+    } else {
+      const ballCount = this.ball(computer, player);
+      const strikeCount = this.strike(computer, player);
+
+      if (ballCount === 0 && strikeCount === 0) MissionUtils.Console.print('낫싱');
+      else if (ballCount > 0 && strikeCount === 0) MissionUtils.Console.print(`${ballCount}볼`);
+      else if (strikeCount > 0 && ballCount === 0) MissionUtils.Console.print(`${strikeCount}스트라이크`);
+      else if (strikeCount > 0 && ballCount > 0) MissionUtils.Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
+
+      return false;
+    }
+  }
+  // ballCount
+  ball(computer, player) {
+    let ballCount = 0;
+    for (const i in player) {
+      if (computer[i] !== player[i] && computer.includes(player[i])) ballCount++;
+    }
+    return ballCount;
+  }
+  // strikeCount
+  strike(computer, player) {
+    let strikeCount = 0;
+    for (const i in player) {
+      if (computer[i] === player[i]) strikeCount++;
+    }
+    return strikeCount;
+  }
 }
 
 export default App;
