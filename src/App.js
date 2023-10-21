53f3ef8a43466c6da22c1b6dde5e91028337a076
songbbang <rokaf340@gmail.com>
2023-10-21 18:47:05 +0900

    input_value

--- src/App.js
+++ src/App.js
@@ -7,30 +7,63 @@ class App {
     const computer = [];
     while (computer.length < 3) {
       const number = Random.pickNumberInRange(1, 9);
-      computer.push(number);
+      if (computer.indexOf(number) === -1) {
+        computer.push(number);
+      }
     }
     console.log(computer);
 
     // 입력값 받는곳
+    let input;
+    let input_arr = [];
 
-    const value = await Console.readLineAsync("숫자를 입력해주세요 : ");
-    const value_arr = value.split("").map(Number);
-    Console.print(value_arr);
+    const input_value = async () => {
+      input = await Console.readLineAsync("숫자를 입력해주세요 : ");
+      input_arr = input.split("").map((value) => {
+        // console.log("input_arr.indexOf(value) = ", input_arr.indexOf(value));
+        if (
+          input.indexOf(value) === -1
+          // ||
+          //   Number(value) <= 0 ||
+          //   !Number(value) ||
+          //   value.length != 3
+        ) {
+          throw new Error("올바른 형식이 아닙니다");
+        }
 
-    let ball = 0;
-    let strike = 0;
+        return +value;
+      });
+    };
+    await input_value();
+    console.log(input_arr);
+    // Console.print(input_arr);
 
+    // let ball = 0;
+    // let strike = 0;
+    const result = {
+      ball: 0,
+      strike: 0,
+    };
     for (const [computer_index, computer_value] of computer.entries()) {
-      for (const [index, value] of value_arr.entries()) {
+      // console.log("computer_value:", computer_value);
+      for (const [index, value] of input_arr.entries()) {
         if (computer_index == index && computer_value == value) {
-          strike++;
-          console.log(`${strike}스트라이크`);
+          result.strike++;
+          // console.log(`${result.strike}스트라이크`);
+          break;
         } else if (computer_value == value) {
-          ball++;
-          console.log(`${ball}볼`);
+          result.ball++;
+          // console.log("value:", value);
+
+          // console.log(`${result.ball}볼`);
         }
       }
     }
+
+    // for (const key in result) {
+    //   console.log(result[key]);
+    // }
+
     process.exit(1);
   }
 }
