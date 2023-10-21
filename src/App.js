860834cfef1bc070db119a465c8bae69e1b63c23
donggyun <donggyunkim12@gmail.com>
2023-10-21 19:00:42 +0900

    feat(Game): option에 따른 재시작 or 종료 구현

--- src/App.js
+++ src/App.js
@@ -90,6 +90,17 @@ class Game {
       const input = await user.guess();
       hint = com.getHint(answer, input);
     }
+
+    if (hint === "3스트라이크") {
+      // 게임 종료 or 새로 시작 로직
+      console.log("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+      const option = await user.selectOption();
+      if (option === "1") {
+        this.reStart();
+      } else if (option === "2")
+        this.shutDown();
+      else throw new Error("[Error] 잘못된 입력입니다.");
+    }
   }
 
   shutDown() {
