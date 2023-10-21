faf4e72a5ef8be582d0afd227adfd23af2cc8d21
Hys-Lee <dltkdgus3275@gmail.com>
2023-10-21 17:11:08 +0900

    Feat: generated computer number
    The mehtod's name given is 'pickNumberInRange',
    which means that it must pick 'number'.
    
    So I don't check the type from that mehtod's return value.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    // while로 반복해야 할지도>
+
+    //# 컴퓨터 값 생성.
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9); // 함수 이름이 number를 pick하는 것이므로, number형으로 준다고 생각하는게 맞겠지.
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+  }
 }
 
 export default App;
