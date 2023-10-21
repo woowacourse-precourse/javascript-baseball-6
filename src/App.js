ab74f794061cd89ec72155324135665e9f9bc4bd
Oh Dong Joon <ohdoju0905@gmail.com>
2023-10-21 21:01:00 +0900

    구현 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,79 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+	constructor() {
+		this.LIMIT = 3;
+		this.BALL = 0;
+		this.STRIKE = 0;
+		this.COMPUTER = [];
+		this.START = true;
+	}
+
+	// 컴퓨터 수 생성
+	generate_computer_number() {
+		const computer = [];
+		while (computer.length < this.LIMIT) {
+			const number = Random.pickNumberInRange(1, 9);
+			if (!computer.includes(number)) {
+				computer.push(number);
+			}
+		}
+		return computer;
+	}
+
+	async play() {
+		Console.print("숫자 야구 게임을 시작합니다.");
+		while (this.START) {
+			this.COMPUTER = this.generate_computer_number();
+			// console.log(this.COMPUTER);
+			while (this.STRIKE < 3) {
+				// 결과값 초기화
+				this.BALL = this.STRIKE = 0;
+
+				// 사용자 입력 받아서 유효성 체크
+				const input = await Console.readLineAsync(
+					"숫자를 입력해주세요 : "
+				);
+				const user = input.split("").map(Number);
+				if (
+					new Set(user).size !== this.LIMIT ||
+					user.some((c) => isNaN(c) || 0)
+				) {
+					throw new Error("[ERROR]");
+				}
+
+				// 볼, 스트라이크 카운트
+				for (let i = 0; i < this.LIMIT; i++) {
+					if (user[i] === this.COMPUTER[i]) this.STRIKE++;
+					else if (this.COMPUTER.includes(user[i])) this.BALL++;
+				}
+
+				// 결과 출력
+				let result = ``;
+				if (this.BALL) {
+					result += this.BALL + "볼";
+				}
+				if (this.STRIKE) {
+					result += (this.BALL && " ") + this.STRIKE + "스트라이크";
+				}
+				Console.print(result ? result : `낫싱`);
+			}
+			Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+			// 재시작 여부 판단
+			Console.print(
+				"게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+			);
+			const restart = +(await Console.readLineAsync(``));
+			if (restart === 2) {
+				this.START = false;
+			} else {
+				this.STRIKE = 0;
+			}
+		}
+	}
 }
+
+// new App().play().catch((e) => console.log(e.message));
 
 export default App;
