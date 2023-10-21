0a30ef4c9a896d4a0898a832cdfdd1b745d959c6
easyhyun00 <leejihyun0324@gmail.com>
2023-10-21 15:39:22 +0900

    :bug: fix: 숫자 맞추기 #2 - 입력한 수, 상대방(컴퓨터)수에 대한 결과 반환 값 공백 추가

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class App {
     if (strike === 0 && ball ===0) return "낫싱";
 
     if (ball > 0) {
-      return `${ball}볼${strike > 0 ? `${strike}스트라이크` : ''}`
+      return `${ball}볼 ${strike > 0 ? `${strike}스트라이크` : ''}`
     }
     return `${strike}스트라이크`;
   }
