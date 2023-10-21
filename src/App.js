123f0f8f4a654e282364b1f766af3715e05a85fd
blcklamb <whenucan35@gmail.com>
2023-10-21 17:16:39 +0900

    feat: game process - input user number - throw Error if number is invalid

--- src/App.js
+++ src/App.js
@@ -11,18 +11,47 @@ class App {
     return computer.join("");
   }
 
-  async play() {
-    const console = MissionUtils.Console.readLineAsync;
+  static isProperNumber(num) {
+    const regex = /^([1-9][^1-9]*[1-9][^1-9]*[1-9])$/g;
+    if (!regex.test(num)) return false;
 
-    const isStart = await console(
+    const repeatCheck = new Set(num.split(""));
+    if (repeatCheck.size !== num.length) return false;
+
+    return true;
+  }
+
+  async play() {
+    const _log = MissionUtils.Console.readLineAsync;
+
+    const isStart = await _log(
       `숫자 야구 게임입니다.\n게임을 시작하려면 1, 종료하려면 2를 입력하세요.`
     );
 
     if (isStart == 1) {
       const target = App.#pickRandomNumber();
-      await console(`숫자 야구 게임을 시작합니다.`);
+      await _log(`숫자 야구 게임을 시작합니다.`);
+      await _log(`컴퓨터 숫자는 ${target}`);
+
+      while (true) {
+        const userNumber = await _log("숫자를 입력해주세요 : ");
+        if (App.isProperNumber(userNumber)) {
+          if (userNumber === target) {
+            const restart = await _log(
+              `3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`
+            );
+            if (restart == 1) {
+              this.play();
+            }
+            throw new Error("[ERROR] 야구 게임 종료");
+          } else {
+          }
+        } else {
+          throw new Error("[ERROR] 올바른 숫자 형식이 아닙니다.");
+        }
+      }
     } else {
-      throw new Error("야구 게임 종료");
+      throw new Error("[ERROR] 야구 게임 종료");
     }
   }
 }
