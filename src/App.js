eb6ce04750a162f09371509a3689b0180b51a16f
hyunjun9788 <namhyunjun9788@gmail.com>
2023-10-21 12:13:01 +0900

    refactor: readLineAsync 모듈 사용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Console, Random } from "@woowacourse/mission-utils";
-import readline from "readline";
 class App {
   constructor() {
     this.computer = [];
     this.inputNum = [];
+    this.gameOverBtn = [];
   }
+
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
-    this.createRandomNum();
+    this.computer = [];
+    await this.createRandomNum();
   }
 
   async createRandomNum() {
@@ -17,30 +19,23 @@ class App {
         this.computer.push(number);
       }
     }
-    Console.print(this.computer);
+    console.log(this.computer);
     await this.myInputNum();
   }
 
   async myInputNum() {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
+    const myNum = await Console.readLineAsync("숫자를 입력해주세요 : ");
+    this.inputNum = myNum.split("").map((element) => parseInt(element));
 
-    //예외 (0포함X)
-    rl.question("숫자를 입력해주세요 : ", (myNum) => {
-      if (myNum.includes("0")) {
-        throw new Error("[ERROR] 숫자가 잘못된 형식입니다");
-      }
-      //예외(숫자만 허용)
-      this.inputNum = myNum.split("").map((element) => parseInt(element));
-      //예외(중복X, 3자리)
-      if (this.computer.length !== new Set(this.inputNum).size) {
-        throw new Error("[ERROR] 숫자가 잘못된 형식입니다");
-      }
-
-      rl.close();
-    });
+    if (myNum.includes("0")) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다");
+    }
+    if (this.computer.length !== new Set(this.inputNum).size) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다");
+    }
+    if (myNum.length !== 3) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다");
+    }
   }
 }
 
