717b0461c760e872f671da762b065171fd2111b7
donggyun <donggyunkim12@gmail.com>
2023-10-21 16:22:36 +0900

    feat(User): 유저 숫자입력 기능 개발

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Random } from "@woowacourse/mission-utils";
+import { Random,Console } from "@woowacourse/mission-utils";
 
 class Computer {
   createAnswer() {
@@ -13,10 +13,32 @@ class Computer {
   }
 }
 
+class User {
+  async guess() {
+    const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
+
+    if (input.length !== 3) {
+      throw new Error("[ERROR] 숫자의 길이가 올바르지 않습니다.");
+    }
+
+    if (isNaN(input)) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+
+    const numbers = input.split("");
+    const set = new Set(numbers);
+    if (set.size !== 3) {
+      throw new Error("[ERROR] 중복되는 숫자가 있습니다.");
+    }
+
+    return input;
+  }
+}
+
 class App {
   async play() {
     const com = new Computer();
-    console.log(com.createAnswer());
+    const user = new User();
   }
 }
 
