1cdb0275d0be168f0b7b21ee474c67869e8051f5
brgndy <brgndy96@gmail.com>
2023-10-21 12:08:48 +0900

    feat(error): 예외처리를 담당할 클래스 생성

---
+++ src/error/AppError.js
@@ -0,0 +1,22 @@
+class AppError extends Error {
+  /**
+   * @type {string}
+   */
+
+  static PREFIX = "[ERROR]";
+
+  /**
+   * @type {string}
+   */
+  name;
+
+  constructor(errorMessage) {
+    const message = `${AppError.PREFIX} ${
+      errorMessage || "알 수 없는 에러가 발생했어요!"
+    }`;
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+export default AppError;
