260e1c92439d13f69c17fc060dc68bd1f1e0c9ed
anr4876 <dustjdanr@naver.com>
2023-10-21 14:22:09 +0900

    컴퓨터 난수 생성 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+
+// 컴퓨터의 난수 생성
+function randomNumber() {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer.join("");
+}
+
+
+// 실행
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    let computer_number = randomNumber();
+    console.log(computer_number);
+  }
 }
 
 export default App;
