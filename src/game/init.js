b783a425e378c8cf43b5887283242d02efcf60a7
yohan11 <yipco77@naver.com>
2023-10-21 21:55:44 +0900

    feat: 게임 종료 시 재시작/종료 선택 기능 구현

--- src/game/init.js
+++ src/game/init.js
@@ -18,6 +18,18 @@ const startGame = async () => {
     }
     if (computerAnswer === userNumber) { ////throw로 예외처리 후에는 안필요함
         MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        await choiceForGame();
+    }
+}
+
+const choiceForGame = async () => {
+    MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    const userChoice = await getUserNumber();
+    // 유효성 검사
+    if (userChoice === "1") {
+        await startGame();
+    } else {
+        return 0;
     }
 }
 
