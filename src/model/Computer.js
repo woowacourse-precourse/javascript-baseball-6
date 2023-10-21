c1b275ceb341116ec8286fd3a4ee3b9a932c0693
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-21 15:42:38 +0900

    feat: [Computer] getter, setter 구현

--- src/model/Computer.js
+++ src/model/Computer.js
@@ -2,4 +2,12 @@ export default class Computer {
   constructor() {
     this.number = null;
   }
+
+  getNumber() {
+    return this.number;
+  }
+
+  setNumber(newNumber) {
+    this.number = newNumber;
+  }
 }
