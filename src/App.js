6b954bf75615a354c07be155390263ec792d1f2b
no-pla <songjihyun323@gmail.com>
2023-10-21 23:11:38 +0900

    feat: 컴퓨터 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  // 게임 시작 전
+  async play() {
+    const computerNumber = [];
+    while (computerNumber.length < 3) {
+      // 컴퓨터 숫자
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerNumber.includes(number)) {
+        computerNumber.push(number);
+      }
+    }
+
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.game(computerNumber);
+  }
+  game(number) {}
 }
+
+const app = new App();
+app.play();
 
 export default App;
