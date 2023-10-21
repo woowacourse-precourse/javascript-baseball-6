15c094af0b9b9b054289cb6d44a864a1f59221e8
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2023-10-21 21:23:23 +0900

    fix: 반복문 통일화

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { Console, Random } from '@woowacourse/mission-utils';
 class App {
   async play() {
     Console.print('숫자야구 게임을 시작합니다.');
+
     while (true) {
       const answer = genNum();
 
-      do {
+      while (true) {
         const user_input = await Console.readLineAsync('숫자를 입력해주세요 : ');
         if (!validate(user_input)) throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
         const { hint_msg, isCorrect } = check(answer, user_input);
         Console.print(hint_msg);
         if (isCorrect) break;
-      } while (true);
+      }
+
       Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
       const replay = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. \n');
       if (replay === '2') break;
