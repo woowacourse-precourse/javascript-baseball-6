0369fbb30542f93a71860d095012c5fa0d08d367
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-10-21 11:25:49 +0900

    feat(generateComputerNumber) : 랜덤 숫자 생성하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -10,17 +10,30 @@ class App {
   async play() {
     // 게임 시작 메세지 출력
     Console.print(GAME_MESSAGE.START);
+    // TODO : 1~9까지 랜덤 숫자를 생성하는 기능
+    this.compterNumber = generateComputerNumber();
     // 게임 시작 메서드(BaseballGame) 호출
-    this.BaseballGame();
+    return this.BaseballGame(this.compterNumber);
+  }
+  
+  generateComputerNumber() {
+    const computerNumber = [];
+    while (computerNumber.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computerNumber.includes(number)) {
+        computerNumber.push(number);
+      }
+    }
+
+    return computerNumber
   }
 
   BaseballGame() {
-    // TODO : 1~9까지 랜덤 숫자를 생성하는 기능
     // TODO : 숫자를 입력 받는 기능
     // TODO : 모든 숫자에 대해 유효한 값인지 확인하는 기능
     // TODO : 볼과 스트라이크를 계산하는 기능
   }
-  
+
   // TODO : 비교한 결과에 대해 출력하는 기능
   // TODO : 재시작 여부를 확인하는 기능
 }
