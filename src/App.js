46004f261a42b8fa323dc0d6d8fa8cef1f36551f
anttiey <hanuuny@gmail.com>
2023-10-21 15:27:49 +0900

    :sparkles: 숫자 야구 게임 시작 문구 추가
    숫자 야구 게임의 시작 문구를 출력하는 기능을 추가함

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  gameStart = () => {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  };
+
+  async play() {
+    this.gameStart();
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
