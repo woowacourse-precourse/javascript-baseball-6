bc82b9248d2949907125db46e1b12a0204f9aa4b
youjeong Jeong <wjddbwjd1205@naver.com>
2023-10-21 19:40:28 +0900

    feat: Computer 클래스 빈 생성자 정의

--- src/Computer.js
+++ src/Computer.js
@@ -1,3 +1,12 @@
 export class Computer {
-
+    function createRandomNum() {
+        let result = '';
+        while (result.length < 3) {
+            const randomNum = Math.floor(Math.random() * 9 + 1);
+            if (!result.includes(randomNum)) {
+                result += randomNum;
+            }
+        }
+        return result;
+    }
 }
