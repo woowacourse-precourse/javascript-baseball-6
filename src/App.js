fc6bb0bdf62b06b2d036002cd127e52b869b236b
김경주 <popo505@naver.com>
2023-10-22 02:03:44 +0900

    feat:게임시작 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Console } from "@woowacourse/mission-utils";
-import { randomNumber } from "./RandomNumber.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
+  //게임시작
+  start(){ 
+    Console.print("숫자 야구 게임을 시작합니다."); 
+  }
+  
   async play() {
-      Console.print("숫자 야구 게임을 시작합니다."); //게임시작
-      let computer = randomNumber(); //랜덤숫자 받아오기
+    this.start();
   }
 }
 
