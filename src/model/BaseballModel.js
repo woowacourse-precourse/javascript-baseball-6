e47d04e532c75137ce95dc9f42c4a78c5c38d3e4
ReturnReason <reasonz@daum.net>
2023-10-22 01:21:03 +0900

    feat: BaseballModel 인스턴스 생성 시 중복되지 않는 3개의 숫자를 생성하는 generateNumbers 메서드 추가

--- src/model/BaseballModel.js
+++ src/model/BaseballModel.js
@@ -1,1 +1,16 @@
-class BaseballModel {}
+import { Random } from '@woowacourse/mission-utils';
+import { BASEBALL_RANGE } from '../constants/range.js';
+
+export class BaseballModel {
+  constructor() {
+    this.computerNumbers = this.#generateNumbers();
+  }
+
+  #generateNumbers() {
+    const pickedNumbers = new Set();
+    while (pickedNumbers.size !== BASEBALL_RANGE.LENGTH) {
+      pickedNumbers.add(Random.pickNumberInRange(BASEBALL_RANGE.START, BASEBALL_RANGE.END));
+    }
+    return Array.from(pickedNumbers).join('');
+  }
+}
