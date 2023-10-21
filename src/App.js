240c9250192e5eca8d3248de77ad84401496f756
hyewon <hyewon.parkhw@gmail.com>
2023-10-21 23:21:13 +0900

    feat: 컴퓨터 임의의 수 선택

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
+      //시작 문구 출력
       MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
       let replay = 1;
       while (replay==1){
+        //컴퓨터의 임의의 수 선택
+        const computer = Array(3).fill().map((i) => MissionUtils.Random.pickNumberInRange(1, 9));
         let strike=0;
         while (strike<3){
           //strike 초기화
@@ -17,7 +21,6 @@ class App {
           "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
         );
       }
-      
   }
 }
 
