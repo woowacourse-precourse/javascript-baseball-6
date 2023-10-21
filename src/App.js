7deaaef2135fe1f905df0ab8c9a70866e6e58d3f
WOOJINCHO98 <steamedbunwith@gmail.com>
2023-10-21 17:38:41 +0900

    feat : 스트라이크시 게임 재시작

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Random, Console } from "@woowacourse/mission-utils";
+
+/**
+ * @todo 입력받은 수에서 중복된 값을 예외처리
+ * 배열로 처리하기 전에 분기했던 로직에서 배열로 처리한 후 분기하는 로직으로 변경 필요
+ */
 
 // 비교 할 수의 총 갯수를 지정하는 상수
 const INPUT_LENGTH = 3;
@@ -12,9 +17,10 @@ function InputException(message) {
 const getComputerNumber = () => {
   const computer = [];
   while (computer.length < INPUT_LENGTH) {
-    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    const number = Random.pickNumberInRange(1, 9);
     if (!computer.includes(number)) {
       computer.push(number);
+      console.log(computer);
     }
   }
   return computer;
@@ -23,8 +29,7 @@ const getComputerNumber = () => {
 // 플레이어에게 숫자를 입력받는 함수
 const readFromPlayer = () => {
   return new Promise((resolve) => {
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
-      console.log(answer);
+    Console.readLine("숫자를 입력해주세요 : ", (answer) => {
       if (answer.length === INPUT_LENGTH && answer >= 100 && answer <= 999) {
         resolve(readToList(answer));
       } else {
@@ -58,18 +63,32 @@ const compareNumber = (computerList, humanList) => {
   return { strikeCnt, ballCnt };
 };
 
+//
+const readRestart = () => {
+  return new Promise((resolve) => {
+    Console.readLine(
+      `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`,
+      (answer) => {
+        if (answer === "1" || answer === "2") {
+          resolve(answer);
+        } else {
+          throw new InputException("예외가 발생했습니다.");
+        }
+      }
+    );
+  });
+};
+
 // 결과를 출력하는 함수
 const printResult = (strikeCnt, ballCnt) => {
   if (strikeCnt === INPUT_LENGTH) {
-    MissionUtils.Console.print(`${strikeCnt}스트라이크`);
-    MissionUtils.Console.print(
-      `${strikeCnt}개의 숫자를 모두 맞히셨습니다! 게임 종료`
-    );
+    Console.print(`${strikeCnt}스트라이크`);
+    Console.print(`${strikeCnt}개의 숫자를 모두 맞히셨습니다! 게임 종료`);
     return true;
   } else {
     const ballText = ballCnt > 0 ? `${ballCnt}볼` : "";
     const strikeText = strikeCnt > 0 ? `${strikeCnt}스트라이크` : "";
-    MissionUtils.Console.print(
+    Console.print(
       ballText && strikeText
         ? `${ballText} ${strikeText}`
         : ballText
@@ -78,25 +97,39 @@ const printResult = (strikeCnt, ballCnt) => {
         ? `${strikeText}`
         : "낫싱"
     );
-    // MissionUtils.Console.print("다시 게임을 시작하는 기능 추가예정");
     return false;
   }
 };
 
 class App {
   async play() {
-    let success = false;
-    const computerList = getComputerNumber();
+    let isStop = false;
 
-    while (!success) {
-      const humanList = await readFromPlayer();
-      const { strikeCnt, ballCnt } = compareNumber(computerList, humanList);
-      success = printResult(strikeCnt, ballCnt);
-    }
+    let success = false;
+
+    Console.print("숫자 야구 게임을 시작합니다.");
+
+    do {
+      const computerList = getComputerNumber();
+
+      // 3스트라이크에 성공하지 못하면 성공할 때 까지 루프한다.
+      while (!success) {
+        const humanList = await readFromPlayer();
+        const { strikeCnt, ballCnt } = compareNumber(computerList, humanList);
+        success = printResult(strikeCnt, ballCnt);
+      }
+
+      const restart = await readRestart();
+      if (restart === "1") {
+        success = false;
+      } else if (restart === "2") {
+        isStop = true;
+        break;
+      }
+    } while (true);
   }
 }
 
-MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 const app = new App();
 app.play();
 
