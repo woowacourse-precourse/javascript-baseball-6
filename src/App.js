48a52f55899f9fff4343264c419f30df65476672
jang_yoonsu <shagrat8546@gmail.com>
2023-10-21 19:08:35 +0900

    feat : 1에서 9까지 서로다른 임의의 수 3개로 3자리수 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
+  constructor() {
+    this.computerNumber = this.generateRandomNumberArray();
+  }
+
+  /** 1에서 9까지 서로 다른 임의의 수 3개를 선택하여 3자리 수를 생성하는 메소드 */
+  generateRandomNumberArray() {
+    const randomNumberArray = [];
+    while (randomNumberArray.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!randomNumberArray.includes(number)) {
+        randomNumberArray.push(number);
+      }
+    }
+
+    return randomNumberArray;
+  }
+
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
