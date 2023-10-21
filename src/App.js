e88e4223846a3d93014474021d4a2f33599e93a4
at8in <at8in@naver.com>
2023-10-22 04:36:17 +0900

    feat: 컴퓨터 수 선택 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const computer = [];
+
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1,9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    
+    console.log(computer);
+  }
 }
 
 export default App;
+
