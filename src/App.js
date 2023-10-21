ce3603697f82a240ded5fbe3a451e6cf8b0e0bd4
JunSeok <ppu1234@naver.com>
2023-10-21 23:41:22 +0900

    Feat: 예측 과정을 반복하여 random number를 맞추면 게임 종료 또는 다시 시작

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
-import { Console } from '@woowacourse/mission-utils'
-import { messages } from './message.js'
+import { Console, MissionUtils } from '@woowacourse/mission-utils'
+import { feedbackMessage, messages } from './message.js'
 
 class App {
   constructor() {
     this.computer = []
     this.player = []
-    this.feedback = ''
+    this.flag = true
   }
-  async printFeedback() {
-    if (!ball && !strike) this.feedback = messages.nothing
-    else this.feedback = feedbackMessage(ball, strike)
+  async gameStart() {
+    await this.createRandomNumber()
+    try {
+      while (this.flag) {
+        await this.getPlayerNumber()
+        await this.isError()
+        await this.checkNumber()
+      }
+      await this.gameOver()
+    } catch (e) {
+      throw new Error(e)
+    }
+  }
+  async gameOver() {
+    Console.print(messages.correctNumber)
+    let rePlay = await Console.readLineAsync(messages.reStart)
+    if (rePlay == 1) {
+      await this.reset()
+      this.gameStart()
+    } else if (rePlay != 2) throw new Error(messages.errorMessage)
+  }
+  async reset() {
+    this.computer = []
+    this.flag = true
+  }
+  async printFeedback(strike, ball, feedback) {
+    if (!ball && !strike) feedback = messages.nothing
+    else feedback = feedbackMessage(ball, strike)
     if (strike === 3) this.flag = false
-    Console.print(this.feedback)
+    Console.print(feedback)
   }
   async checkNumber() {
     let strike = 0,
-      ball = 0
+      ball = 0,
+      feedback = ''
     for (let [idx, value] of this.computer.entries()) {
       const playerNum = Number(this.player[idx])
       if (value === playerNum) strike++
       else if (this.computer.includes(playerNum)) ball++
     }
-    this.printFeedback()
+    this.printFeedback(strike, ball, feedback)
   }
   async isError() {
     if (!(this.player.length === 3 && !!Number(this.player[0]) && !!Number(this.player[1]) && !!Number(this.player[2]))) {
@@ -42,9 +68,7 @@ class App {
   }
   async play() {
     this.startMessage()
-    await this.createRandomNumber()
-    await this.getPlayerNumber()
-    await this.isError()
+    await this.gameStart()
   }
 }
 
