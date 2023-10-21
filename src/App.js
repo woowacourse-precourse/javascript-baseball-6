653d2364b3b9fd3c3322b12c8c5ba933c9b186f5
JunSeok <ppu1234@naver.com>
2023-10-21 23:29:54 +0900

    Feat: random number와 guess number 비교하고 피드백 출력

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ class App {
   constructor() {
     this.computer = []
     this.player = []
+    this.feedback = ''
+  }
+  async printFeedback() {
+    if (!ball && !strike) this.feedback = messages.nothing
+    else this.feedback = feedbackMessage(ball, strike)
+    if (strike === 3) this.flag = false
+    Console.print(this.feedback)
+  }
+  async checkNumber() {
+    let strike = 0,
+      ball = 0
+    for (let [idx, value] of this.computer.entries()) {
+      const playerNum = Number(this.player[idx])
+      if (value === playerNum) strike++
+      else if (this.computer.includes(playerNum)) ball++
+    }
+    this.printFeedback()
+  }
+  async isError() {
+    if (!(this.player.length === 3 && !!Number(this.player[0]) && !!Number(this.player[1]) && !!Number(this.player[2]))) {
+      throw new Error(messages.errorMessage)
+    }
   }
   async getPlayerNumber() {
     this.player = [...(await Console.readLineAsync(messages.inputNumber))]
@@ -21,6 +43,8 @@ class App {
   async play() {
     this.startMessage()
     await this.createRandomNumber()
+    await this.getPlayerNumber()
+    await this.isError()
   }
 }
 
