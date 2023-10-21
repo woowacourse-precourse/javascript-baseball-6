905215434051df46d915085bf0d4519d18948522
EunHyeokJung <silverhyeok.dev@gmail.com>
2023-10-21 20:44:38 +0900

    Feat: 게임 시작
    - 시작 문구 출력
    - 랜덤한 세자리수 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+    async play() {
+        let replay = true;
+        while (replay) {
+            const baseBallGame = new BaseBallGame();
+        }
+    }
+}
+
+class BaseBallGame {
+    constructor() {
+        MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+        this.answer = [];
+        while (this.answer.length < 3) {
+            const number = MissionUtils.Random.pickNumberInRange(1, 9);
+            if (!this.answer.includes(number)) {
+                this.answer.push(number);
+            }
+        }
+    }
 }
 
 export default App;
