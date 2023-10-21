6161c0ccae2bc2786d862e99ed78d08b0288c037
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-10-22 04:14:51 +0900

    test: 같은 수가 같은 자리에 있는 경우 스트라이크

--- __tests__/UmpireTest.js
+++ __tests__/UmpireTest.js
@@ -8,4 +8,16 @@ describe("UmpireTest", () => {
     expect(umpire.umpire([1, 2, 3], [4, 5, 6])).toBe(BaseballTerms.NOTHING);
     expect(umpire.umpire([1, 5, 7], [3, 9, 6])).toBe(BaseballTerms.NOTHING);
   });
+
+  test("같은 수가 같은 자리에 있는 경우 스트라이크", () => {
+    expect(umpire.umpire([2, 4, 6], [2, 4, 6])).toBe(
+      `3${BaseballTerms.STRIKE}`
+    );
+    expect(umpire.umpire([1, 2, 3], [1, 2, 7])).toBe(
+      `2${BaseballTerms.STRIKE}`
+    );
+    expect(umpire.umpire([1, 2, 3], [1, 4, 7])).toBe(
+      `1${BaseballTerms.STRIKE}`
+    );
+  });
 });
