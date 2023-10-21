f40689d15be80d4597cea58c09562a51072981fd
박지윤 <tkrhkfld123@naver.com>
2023-10-22 02:11:03 +0900

    refactor(baseball2): 불필요한 함수 삭제

--- src/retry/Computer.js
+++ src/retry/Computer.js
@@ -2,10 +2,6 @@ import { Random } from '@woowacourse/mission-utils';
 
 class Computer {
   getNumbers() {
-    return this.generate();
-  }
-
-  generate() {
     return Random.pickUniqueNumbersInRange(1, 9, 3);
   }
 }
