46735dd8ca1d18f9872091f33243a5667f56a757
박지윤 <tkrhkfld123@naver.com>
2023-10-22 02:08:48 +0900

    feat(baseball2): 사용자가 입력한 숫자가 중복된 경우 예외 처리 구현

--- src/retry/UserValidation.js
+++ src/retry/UserValidation.js
@@ -3,11 +3,20 @@ const ValidationError = require('./ValidationError');
 const UserValidation = {
   validateUser(user) {
     this.checkLength(user);
+    this.checkRange(user);
   },
 
   checkLength(user) {
     if (user.length !== 3) {
       throw new ValidationError('[ERROR] 3자리 숫자를 입력할 수 있습니다.');
+    }
+  },
+
+  checkDuplicate(user) {
+    const userNumberDeletedDuplicate = new Set(user);
+
+    if (userNumberDeletedDuplicate.size !== 3) {
+      throw new ValidationError('[ERROR] 중복되는 숫자는 입력할 수 없습니다.');
     }
   },
 };
