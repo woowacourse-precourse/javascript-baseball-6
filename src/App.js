38003dd2c8eafa10556f05c7d8619ca80bc25aaf
blcklamb <whenucan35@gmail.com>
2023-10-21 16:34:11 +0900

    feat: game start part - select start / end - end -> application exit - start -> computer select number

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  static #pickRandomNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer.join("");
+  }
+
+  async play() {
+    const console = MissionUtils.Console.readLineAsync;
+
+    const isStart = await console(
+      `숫자 야구 게임입니다.\n게임을 시작하려면 1, 종료하려면 2를 입력하세요.`
+    );
+
+    if (isStart == 1) {
+      const target = App.#pickRandomNumber();
+      await console(`숫자 야구 게임을 시작합니다.`);
+    } else {
+      throw new Error("야구 게임 종료");
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
