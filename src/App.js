a62a8814d1c5fa408744606a841f777c5f75da64
HarenKei <seungmin1266@gmail.com>
2023-10-21 18:03:10 +0900

    Feat: 서로 다른 난수 3개 생성 기능 구현.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    function makeAnswer() {
+      const computer = new Set();
+      while(computer.size < 3) {
+        computer.add(Random.pickNumberInRange(1, 9));
+      }
+
+      return [...computer];
+    }
+
+    makeAnswer();
+    userInput();
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
