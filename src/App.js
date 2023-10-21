aa0e6aaa28440540a5ac7e5dfd556f03785cd20f
jeongin98 <eon7500@gmail.com>
2023-10-21 18:14:26 +0900

    feat: 3개 랜덤숫자  생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { MissionUtils } from "@woowacourse/mission-utils"; // 우테코 API
+
 class App {
-  async play() {}
+
+  randomNumber = [];
+
+  setRandomNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    this.randomNumber = [...computer];
+  }
+
+  async play() {
+    this.setRandomNumber();
+  }
+
+
 }
+
+const app = new App();
+app.play();
 
 export default App;
