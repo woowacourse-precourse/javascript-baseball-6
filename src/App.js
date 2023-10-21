d26aa17efd49e811efbd9e33925d110da40cb41e
intersoom <sallybig@naver.com>
2023-10-22 02:42:34 +0900

    feat: 게임 시작 기능 구현 - 게임 시작 문구 출력 - computer 배열에 지정숫자 랜덤 뽑아서 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+
+    const computer = [];
+
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+  }
 }
 
 export default App;
