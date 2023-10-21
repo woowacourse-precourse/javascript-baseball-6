2dd57a21c7a775ddc49dee5ad6726d59ec6c52d3
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2023-10-21 14:52:44 +0900

    feat: 답변 입력 및 확인 기능 작성 완료

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ class App {
   async play() {
     Console.print('숫자야구 게임을 시작합니다.');
     const answer = genNum();
+
+    do {
+      const user_input = await Console.readLineAsync('숫자를 입력해주세요 : ');
+      const { hint_msg, isCorrect } = check(answer, user_input);
+      Console.print(hint_msg);
+      if (isCorrect) break;
+    } while (true);
+    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
   }
 }
 
@@ -16,6 +24,29 @@ const genNum = function generateRandomNumber() {
     }
   }
   return computer;
+};
+
+const check = function checkAnswer(answer, input) {
+  let ball = 0;
+  let strike = 0;
+
+  answer.forEach((num, idx) => {
+    if (num == input[idx]) {
+      strike++;
+    } else if (input.includes(num)) {
+      ball++;
+    }
+  });
+
+  let hint_msg = '';
+  if (!ball && !strike) hint_msg = '낫싱';
+  else if (!ball) hint_msg = `${strike}스트라이크 `;
+  else if (!strike) hint_msg = `${ball}볼`;
+  else {
+    hint_msg = `${ball}볼 ${strike}스트라이크`;
+  }
+
+  return { hint_msg, isCorrect: strike === 3 };
 };
 
 const app = new App();
