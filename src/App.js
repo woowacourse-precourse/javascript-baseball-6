e9bc3545a503f2e1f6b7e94f99df7d262860c4fa
Jwirang <haha71119@gmail.com>
2023-10-21 10:02:33 +0900

    feat: 컴퓨터 숫자 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const computer = this.computerNumber();
+  }
+
+  computerNumber() {
+    const numbers = [];
+
+    while (numbers.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (!numbers.includes(number)) {
+        numbers.push(number);
+      }
+    }
+
+    return numbers;
+  }
 }
 
 export default App;
