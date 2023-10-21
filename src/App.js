42219be61cb14bf4e895e959ad012dd265f287dd
anttiey <hanuuny@gmail.com>
2023-10-21 16:59:35 +0900

    :sparkles: 플레이어 숫자 입력 유효성 검사 추가
    플레이어가 입력한 숫자에 대한 유효성을 검사하는 기능을 추가함

--- src/App.js
+++ src/App.js
@@ -12,9 +12,33 @@ class App {
     Console.print("숫자 야구 게임을 시작합니다.");
   };
 
+  /***   
+    1. Number
+    2. 1에서 9 사이의 숫자
+    3. 서로 다른 세 자리의 숫자
+  ***/
+  checkUserNumberValidation = (input) => {
+    const validRegex = /^[1-9]{3}$/;
+
+    if (validRegex.test(input)) {
+      if (new Set(input).size === 3) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
   getUserNumberInput = async () => {
-    const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
-    this.userNumber = Array.from(input).map((el) => Number(el));
+    try {
+      const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
+      if (!this.checkUserNumberValidation(input)) {
+        throw new Error("[ERROR] 입력이 잘못된 형식입니다.");
+      }
+      this.userNumber = Array.from(input).map((el) => Number(el));
+    } catch (err) {
+      Console.print(err.message);
+    }
   };
 
   async play() {
