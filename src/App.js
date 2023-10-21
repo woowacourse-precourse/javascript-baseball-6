7a6fb004b5ee9c6f1d4159649106da287653ccd3
JUNU <bokjunwoo@gmail.com>
2023-10-21 22:52:41 +0900

    refactor: 게임 시작 로직 개선 및 입력 로직 분리

--- src/App.js
+++ src/App.js
@@ -3,27 +3,17 @@ import BaseballGame from './BaseballGame.js';
 
 class App {
   constructor() {
-    this.baseballGame = new BaseballGame();
-  }
-
-  play() {
-    this.gameStartMessage();
-    this.getUserInputAsync();
+    this.baseBallGame = null;
   }
 
   gameStartMessage() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
 
-  async getUserInputAsync() {
-    try {
-      const userInput = await MissionUtils.Console.readLineAsync(
-        '숫자를 입력해 주세요 :'
-      );
-      await this.baseballGame.play(userInput);
-    } catch (error) {
-      throw error;
-    }
+  async play() {
+    this.gameStartMessage();
+    this.baseBallGame = new BaseballGame();
+    this.baseBallGame.play();
   }
 }
 
--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -1,5 +1,5 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { NUMBER_REGEX } from './constants.js';
+import UserInput from './UserInput.js';
 
 class BaseballGame {
   constructor() {
@@ -17,27 +17,8 @@ class BaseballGame {
     return computer;
   }
 
-  async play(userNumbers) {
-    this.isValidInput(userNumbers);
-  }
-
-  isValidInput(userNumbers) {
-    const numberArray = userNumbers.split('').map(Number);
-
-    const isValidNumber = numberArray.every((num) => NUMBER_REGEX.test(num));
-    if (!isValidNumber) {
-      throw new Error('유효한 숫자를 입력해야 합니다.');
-    }
-
-    if (numberArray.length !== 3) {
-      throw new Error('3개의 숫자를 입력해야 합니다.');
-    }
-
-    if (new Set(numberArray).size !== 3) {
-      throw new Error('숫자는 중복되지 않아야 합니다.');
-    }
-
-    return true;
+  async play() {
+    await UserInput.getUserInputAsync();
   }
 }
 
