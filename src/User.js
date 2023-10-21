0fa2b462198458dd6d32f08d8c70181310bd060e
Trophy198 <spsp8426@naver.com>
2023-10-22 01:51:11 +0900

    Feat : 유저의 숫자를 입력받는 클래스 추가

--- src/User.js
+++ src/User.js
@@ -1,9 +1,18 @@
-import { Console } from '@woowacourse/mission-utils';
-import { USER_INPUT_MESSAGE } from './constants/MessageConstants';
+import { Console } from "@woowacourse/mission-utils";
+import InputValidator from "./utils/InputValidator.js";
 
 export default class User {
-    async getUserInput() {
-        const userInput = await Console.readLineAsync(USER_INPUT_MESSAGE);
-        return Array.from(userInput).map(Number);
+  async getInput() {
+    let input;
+    while (true) {
+      try {
+        input = await Console.readLineAsync("숫자를 입력해주세요. : ");
+        InputValidator.validateInput(input);
+        return Array.from(input).map(Number);
+      } catch (error) {
+        Console.print(error.message);
+        throw error;
+      }
     }
+  }
 }
