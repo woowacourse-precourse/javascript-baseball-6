3d7bcaf063abd1284c988867cda3046bbe7ab7cb
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-21 16:09:37 +0900

    feat: [Validation] 플레이어가 입력한 input의 유효성 검증 로직 구현

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -3,4 +3,19 @@ export default class Validation {
     this.NUMBER_RANGE = /^[1-9]+$/;
     this.PLAYER_INPUT_VALIDATON = true;
   }
+
+  /**
+   * player가 입력한 input의 유효성을 검증합니다.
+   * @param {string[]} playerInput [player가 제시한 수]
+   */
+  async validatePlayerInput(playerInput) {
+    const playerInputToSet = new Set(playerInput);
+
+    if(!this.NUMBER_RANGE.test(+playerInput.join('')) ||
+      playerInput.length !== 3 ||
+      playerInputToSet.size !== 3
+    ) {
+      this.PLAYER_INPUT_VALIDATON = false;
+    }
+  }
 }
