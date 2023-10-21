1288d8498b5641232de20710f0db8fc7b66bd73e
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-21 10:59:55 +0900

    :sparkles: feat: 애플리케이션에 사용된 상수 추가

---
+++ src/constants/Constants.js
@@ -0,0 +1,16 @@
+export const USER_COMMAND = {
+  REPLAY: 1,
+  END: 2,
+};
+
+export const GAME_CONTROL = {
+  LIMIT_LENGTH: 3,
+  LIMIT_NUMBER: 0,
+  START_SCOPE: 1,
+  END_SCOPE: 9,
+};
+
+export const NO_BALL = 0;
+export const NO_STRIKE = 0;
+export const FIRST_DIGIT = 0;
+export const NUMBER_REGEX = /^\d+$/;
