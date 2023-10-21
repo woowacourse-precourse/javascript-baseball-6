234e671547801d45a433359ca99b03261e603559
brgndy <brgndy96@gmail.com>
2023-10-21 12:20:13 +0900

    feat(play): 상대방에게 무작위의 공을 만들어주고 게임에서 정의한다.

--- src/domains/Game.js
+++ src/domains/Game.js
@@ -1,7 +1,29 @@
 // 한판의 게임을 책임지는 클래스
 
+import Oppenent from "./Opponent.js";
+
 class Game {
-  constructor() {}
+  /**
+   * @type { Oppenent } 게임 내에서 상대방(컴퓨터) 정답 번호 정의
+   */
+
+  #opponent;
+
+  /**
+   * @type {[number, number, number]}
+   */
+
+  #opponentGongs;
+
+  /**
+   *
+   * @param {Opponent} opponent
+   */
+
+  constructor(opponent = new Oppenent()) {
+    this.#opponent = opponent;
+    this.#opponentGongs = this.#opponent.getGongs();
+  }
 }
 
 export default Game;
--- src/domains/Gong.js
+++ src/domains/Gong.js
@@ -1,7 +1,26 @@
 // 공들을 관리하는 클래스
 
 class Gong {
+  /** @type {number} */
+  static GONGS_LENGTH = 3;
+
   constructor() {}
+
+  /**
+   * 게임이 시작될때 상대방에게 새로운 정답을 리턴해주는 함수
+   * @returns {[number, number, number]}
+   */
+
+  static getNewGongs() {
+    const numbers = [];
+    while (numbers.length < Gong.GONGS_LENGTH) {
+      const randomNum = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!numbers.includes(randomNum)) {
+        numbers.push(randomNum);
+      }
+    }
+    return numbers;
+  }
 }
 
 export default Gong;
