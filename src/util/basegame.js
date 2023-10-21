0a658bfc53274cc63b8e96287d9be622bc1c49c9
Jihyeong00 <iolpd42@gmail.com>
2023-10-21 11:29:16 +0900

    기능명세서에 따른 함수 주석변경

--- src/util/basegame.js
+++ src/util/basegame.js
@@ -4,8 +4,7 @@ const baseGame = (randomNumberArray, inputNumber) => {
     let strike_count = 0;
     let ball_count = 0;
 
-    // 2.2.1 모든 숫자가 일치하는지 확인하기
-
+    // 4.1 모든 숫자가 일치하는지 확인한다.
     if (RANDOM_STRING === INPUT_STRING) {
         return "3스트라이크"
     }
@@ -14,13 +13,13 @@ const baseGame = (randomNumberArray, inputNumber) => {
 
     for (let i in INPUT_NUMBER_ARRAY) {
         if (RANDOM_STRING.includes(INPUT_NUMBER_ARRAY[i])) {
-
-            // 2.2.2 자리와 값이 일치하는 수 골라내기 = strike
-            // 2.2.3 자리는 다르지만 값이 일치하는 수 골라내기 = ball
+            // 4.2 자리와 값이 일치하는 수를 골라낸다.
+            // 4.3 자리는 다르지만 값이 일치하는 수를 골라낸다.
             RANDOM_STRING[i] === (INPUT_NUMBER_ARRAY[i]) ? strike_count++ : ball_count++;
         }
     }
 
+    // 4.4 결과에 따른 값 올바르게 출력한다.
     if (!strike_count && !ball_count) return "낫싱";
 
     if (!strike_count && ball_count) return `${ball_count}볼`;
--- src/util/helper.js
+++ src/util/helper.js
@@ -12,11 +12,12 @@ export const randomNumber = () => {
 }
 
 export const checkNumber = (input) => {
-    // 2.1.1 숫자가 입력이 되었는가?
+    // 3.1 숫자가 입력이 되었는가?
     if (isNaN(input)) throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
-    // 2.1.2 3자리수가 입력이 되었는가?
+    // 3.2 3자리수가 입력이 되었는가?
     if (String(input).length !== 3) throw new Error("[ERROR] 입력된 값이 3자리수가 아닙니다.");
-    // 2.1.3 모두 다른 숫자가 입력이 되었는가?
+    // 3.3 모두 다른 숫자가 입력이 되었는가?
     const STRING_INPUT = String(input)
     if (STRING_INPUT[0] === STRING_INPUT[1] || STRING_INPUT[1] === STRING_INPUT[2] || STRING_INPUT[2] === STRING_INPUT[0]) throw new Error("[ERROR] 입력된 값이 중복되는 수가 있습니다.");
+    // 3.4 유효성 검사에서 실패할경우 [ERROR]를 포함한 에러문구 출력하고 프로그램을 종료한다.
 }
