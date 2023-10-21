2276ead59bb0f848210262f17d938596ce00d0be
seokho10007 <sukho1007@naver.com>
2023-10-21 17:10:24 +0900

    ♻️ refactor: 메시지 생성 로직 컨트롤러로 이동 및 캡슐화
    - 메시지 생성 로직은 데이터에 따라 변화하는 로직이므로 컨트롤러로 이동
    - 외부에서 랜덤 숫자와 캐시 스토어, 랜텀 숫자 생성 메소드에 접근하지 못하도록 캡슐화

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -1,53 +1,24 @@
 import { pickNumberInRange } from "./utils.js";
-import {
-  BALL,
-  STRIKE,
-  NOTHING,
-  MIN_RANDOM_NUMBER,
-  MAX_RANDOM_NUMBER,
-  MAX_INPUT_LENGTH,
-} from "./constants.js";
 
 class BaseBallGame {
   /**
    * @type {number[]} 랜덤한 숫자 배열
    */
-  randomNumbers = [];
+  #randomNumbers = [];
 
   /**
-   * @type {Map<number, number>} 사용자가 입력한 숫자와 strike, ball의 개수를 저장하는 cache store
+   * @type {Map<number, {strike: number, ball: number}>} 사용자가 입력한 숫자와 strike, ball의 개수를 저장하는 cache store
    */
-  scoreStore = new Map();
-
-  /**
-   * @type {number} 랜덤 숫자의 최소값
-   */
-  min = MIN_RANDOM_NUMBER;
-
-  /**
-   * @type {number} 랜덤 숫자의 최대값
-   */
-  max = MAX_RANDOM_NUMBER;
-
-  /**
-   * @type {number} 사용자 입력의 최대 길이
-   */
-  maxInputLength = MAX_INPUT_LENGTH;
-
-  constructor({ min, max, maxInputLength }) {
-    this.min = min;
-    this.max = max;
-    this.maxInputLength = maxInputLength;
-  }
+  #scoreStore = new Map();
 
   /**
    * @description 게임을 초기화하는 메서드
    * - 랜덤 숫자의 최소값과 최대값, 사용자 입력의 최대 길이에 따라 랜덤한 숫자 배열을 생성합니다.
    * - cache store를 초기화합니다.
    */
-  init() {
-    this.randomNumbers = this.generateRandomNumbers();
-    this.scoreStore.clear();
+  init(props) {
+    this.#randomNumbers = this.#generateRandomNumbers(props);
+    this.#scoreStore.clear();
   }
 
   /**
@@ -58,41 +29,16 @@ class BaseBallGame {
    * - 최소값, 최대값, 생성할 숫재의 개수를 인자로 받아 중복되지 않은 랜덤한 숫자를 생성합니다.
    * @returns {number[]} 랜덤한 숫자 배열
    */
-  generateRandomNumbers() {
+  #generateRandomNumbers({ min, max, maxInputLength }) {
     const set = new Set();
 
-    while (set.size < this.maxInputLength) {
-      const number = pickNumberInRange(this.min, this.max);
+    while (set.size < maxInputLength) {
+      const number = pickNumberInRange(min, max);
 
       set.add(number);
     }
 
     return [...set];
-  }
-
-  /**
-   * @param {number} strike - 스트라이크의 개수
-   * @param {number} ball - 볼의 개수
-   * @description 스트라이크와 볼의 개수에 따라 메시지를 생성하는 메서드
-   * - 볼이 0 이상일 경우 "n볼"을, 스트라이크가 0 이상일 경우 "n스트라이크"를, 둘 다 0일 경우 "낫싱"을 리턴합니다.
-   * @returns {string}
-   */
-  generateScoreMessage(strike, ball) {
-    const messages = [];
-
-    if (ball > 0) {
-      messages.push(`${ball}${BALL}`);
-    }
-
-    if (strike > 0) {
-      messages.push(`${strike}${STRIKE}`);
-    }
-
-    if (strike === 0 && ball === 0) {
-      messages.push(NOTHING);
-    }
-
-    return messages.join(" ");
   }
 
   /**
@@ -105,11 +51,11 @@ class BaseBallGame {
   calculateStrikeBall(input) {
     const key = input.join("");
 
-    if (this.scoreStore.has(key)) {
-      return this.scoreStore.get(key);
+    if (this.#scoreStore.has(key)) {
+      return this.#scoreStore.get(key);
     }
 
-    const randomNumbers = this.randomNumbers;
+    const randomNumbers = this.#randomNumbers;
     const score = { strike: 0, ball: 0 };
 
     for (let i = 0; i < randomNumbers.length; i++) {
@@ -126,7 +72,7 @@ class BaseBallGame {
       }
     }
 
-    this.scoreStore.set(key, score);
+    this.#scoreStore.set(key, score);
 
     return score;
   }
