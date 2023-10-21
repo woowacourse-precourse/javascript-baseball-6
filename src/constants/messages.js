7b759095f73c6e0dcc11af144b0433c9d1df799c
ReturnReason <reasonz@daum.net>
2023-10-22 05:33:37 +0900

    chore(messages): 게임 종료 커맨드 문구 수정

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -30,7 +30,7 @@ const ERROR_PREFIX = '[ERROR]';
 export const ERROR_MESSAGE = Object.freeze({
   INVALID_RANGE: `${ERROR_PREFIX} 1부터 9까지의 숫자 3개를 입력해주세요.\n`,
   INVALID_INPUT: `${ERROR_PREFIX} 숫자 외의 문자 및 공백은 입력할 수 없습니다.\n`,
-  INVALID_END_COMMAND: `${ERROR_PREFIX} 새 게임 시작(${RESTART_COMMAND.NEWGAME_NUM}) 또는 게임 종료(${RESTART_COMMAND.END_NUM})중 하나의 숫자만 입력해주세요.`,
+  INVALID_END_COMMAND: `${ERROR_PREFIX} 잘못된 커맨드! 재시작(${RESTART_COMMAND.NEWGAME}) 또는 게임종료(${RESTART_COMMAND.QUIT})만 유효합니다.`,
   DUPLICATION_INPUT: `${ERROR_PREFIX} 중복된 숫자는 입력할 수 없습니다.\n`,
   INVALID_COPUTER_RANGE: `${ERROR_PREFIX} 1부터 9까지 범위의 숫자 3개로 이루어져야 합니다.\n`,
   INVALID_COPUTER_NUMBER: `${ERROR_PREFIX} 번호는 숫자로만 이루어져야 합니다.\n`,
