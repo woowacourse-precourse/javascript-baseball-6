c373cd14db28b426052fbca09a3d92dc0a81dc66
dali1999 <dali1999@naver.com>
2023-10-21 21:19:36 +0900

    refactor: 함수 이름 재정의

--- src/handleAnswer/handleUserInput.js
+++ src/handleAnswer/handleUserInput.js
@@ -1,24 +1,27 @@
 // 서로 다른 3개의 숫자를 입력한다.
 const handleUserInput = (userInput) => {
   // 길이가 3이어야 한다.
-  const validLength = (userInput) => userInput.length !== 3;
+  const isValidLength = (userInput) => userInput.length !== 3;
   // 숫자여야 한다.
-  const validType = (userInput) => isNaN(userInput);
+  const isNumeric = (userInput) => isNaN(userInput);
   // 모두 다른 숫자여야 한다.
-  const validOverlap = (userInput) => {
+  const isDuplicate = (userInput) => {
     return (
       userInput[0] === userInput[1] ||
       userInput[1] === userInput[2] ||
       userInput[0] === userInput[2]
     );
   };
+
   if (
-    validLength(userInput) ||
-    validType(userInput) ||
-    validOverlap(userInput)
+    isValidLength(userInput) ||
+    isNumeric(userInput) ||
+    isDuplicate(userInput)
   ) {
     throw new Error(`[ERROR] 잘못된 입력입니다. ${userInput}`);
   }
+
   return userInput;
 };
+
 export default handleUserInput;
