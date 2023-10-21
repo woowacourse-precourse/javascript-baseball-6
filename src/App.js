aae8e0e6587529ec28af6504b4bf7fcb5db01cf6
seoye0ng <bsy0313@gmail.com>
2023-10-21 17:13:23 +0900

    Feat: 랜덤한 숫자 3개를 생성하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
   async play() {}
+
+  // 랜덤한 숫자 3개를 생성
+  createAnswerNumber() {
+    const computerNumbers = [];
+    while (computerNumbers.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerNumbers.includes(number)) {
+        computerNumbers.push(number);
+      }
+    }
+    return computerNumbers;
+  }
 }
 
 export default App;
