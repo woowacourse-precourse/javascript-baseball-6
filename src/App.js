5e3bcd2f9bd11bc1e1e98df5b06b2a7b5768a9df
yu-retel <dbdudals100@naver.com>
2023-10-21 14:29:05 +0900

    feat: 재시작종료 기능구현

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ class App {
       this.firstStart = false;
     }
     try {
-      const userInput = await Console.readLineAsync('숫자를 입력해주세요 :');
+      const userInput = await Console.readLineAsync('숫자를 입력해주세요 : ');
       this.gameResultOutput(this.gameResult(userInput));
-      this.try();
+      this.playAgain();
     } catch (error) {
-      
+      Console.print(error.message);
     }
   }
 
@@ -39,11 +39,12 @@ class App {
   }
 
   gameResultOutput(score) {
+    console.log(this.computer);
     const [strike, ball] = score;
 
     if (strike === 3) {
       this.inputContinuation = false;
-      return Console.print('3스트라이크');
+      return Console.print('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료');
     }
     if (strike === 0 && ball === 0) {
       return Console.print('낫싱');
@@ -58,8 +59,20 @@ class App {
     return Console.print(`${ball}볼 ${strike}스트라이크`);
   }
 
-  try() {
-    if(this.inputContinuation) this.play();
+  async playAgain() {
+    if(this.inputContinuation) return this.play();
+
+    try {
+      const userInput = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
+      if (userInput === '1') {
+        this.computer = randomNumber();
+        this.inputContinuation = true;
+        this.play();
+      }
+      if (userInput === '2') return;
+    } catch(error) {
+      Console.print(error.message);
+    }
   }
 }
 
