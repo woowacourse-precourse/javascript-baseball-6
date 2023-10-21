00b97f1b5cbfcd15a421988b2a6757871347adfa
Mungjin01 <beckaljm@naver.com>
2023-10-22 01:37:45 +0900

    feat: 컴퓨터 임의의 수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const computerAnswer = this.generateRandomBallNumber();
+    MissionUtils.Console.print(computerAnswer)
+  }
+
+  generateRandomBallNumber(){
+    const computerAnswer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    return computerAnswer.join('');
+  }
 }
+
+const init = new App();
+init.play();
 
 export default App;
