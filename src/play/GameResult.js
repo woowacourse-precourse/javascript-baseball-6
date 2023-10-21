6d3592a826d49e87b887569bb7f624f594cd65ec
hyj5076 <hyj5076@hanmail.net>
2023-10-22 02:53:43 +0900

    feat: 볼 횟수 카운트 기능 추가

--- src/play/GameResult.js
+++ src/play/GameResult.js
@@ -9,20 +9,23 @@ const arrNumber = (arr) => {
 const computerArray = arrNumber(computerNumber);
 const playerArray = arrNumber(playerNumber);
 
-// 스트라이크 횟수 카운트
-const countStrike = () => {
+// 스트라이크 볼 횟수 카운트
+const countStrikeAndBall = () => {
   let strike = 0;
+  let ball = 0;
 
   for (let i = 0; i < 3; i++) {
       if (computerArray[i] === playerArray[i]) {
         strike++; // 조건이 만족될 때마다 strike 증가
+      } else if (playerArray.includes(computerArray[i])) {
+        ball++;
       }
   }
 
   if (strike === 3) {
     Console.print("성공!");
-  } else if (strike > 0) {
-    Console.print(`${strike} 스트라이크`);
+  } else {
+    if (strike > 0) { Console.print(`${strike} 스트라이크`); }
+    if (ball > 0) { Console.print(`${ball} 볼`); }
   }
 }
-  
