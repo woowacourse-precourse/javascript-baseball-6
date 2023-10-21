84fba02af020fb6d960cae7ecbc3ef31b71ff76c
Jwirang <haha71119@gamil.com>
2023-10-21 16:51:26 +0900

    feat: 사용자가 입력한 값과 컴퓨터의 랜덤값 대조

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ class App {
     while (true) {
       const user = await this.userNumber();
       this.validateInput(user);
+
+      const isEnd = this.check(computer, user);
+      
+      if (isEnd) break;
     }
+    
+    console.log('3개의 숫자를 모두 맞히셨습니다! 게임 종료');    
   }
 
   computerNumber() {
@@ -34,6 +40,36 @@ class App {
     if (!regex.test(value)) {
       throw new Error('[ERROR] 잘못된 입력값 입니다.');
     }
+  }
+
+  check(computer, user) {
+    let strikes = 0;
+    let balls = 0;
+    
+    for (let i = 0; i < 3; i++) {
+      if (user[i] == computer[i]) {
+        strikes++;
+      } else if (computer.includes(user[i])) {
+        balls++;
+      }
+    }
+
+    if (strikes == 3) {
+      Console.print('3스트라이크');
+      return true;
+    }
+    
+    if (strikes == 0 && balls == 0) {
+      Console.print('낫싱');
+    } else if (strikes == 0) {
+      Console.print(`${balls}볼`);
+    } else if (balls == 0) {
+      Console.print(`${strikes}스트라이크`);
+    } else {
+      Console.print(`${balls}볼 ${strikes}스트라이크 `);
+    }
+    
+    return false; 
   }
 }
 
