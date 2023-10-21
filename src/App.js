7280c918df51c5bb6dfaaf5d972d8008c019d0bd
puranium235 <puranium235@gmail.com>
2023-10-22 01:28:14 +0900

    플레이어에게 3자리 숫자 입력받기

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import {MissionUtils} from "@woowacourse/mission-utils";
 
 class App {
     async play() {
-        // 컴퓨터가 임의의 3자리 숫자를 생성
+        // 게임 시작 문구 출력
+        MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+        // 게임 실행
+        // 임의의 3자리 숫자를 생성
         const computer = [];
         while (computer.length < 3) {
             const number = MissionUtils.Random.pickNumberInRange(1, 9);
@@ -11,7 +15,15 @@ class App {
             }
         }
 
+        // 플레이어에게 3자리 숫자를 입력받는다.
+        const playerInput = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+        const player = playerInput.split("").map((number) => parseInt(number));
+        console.log(player);
     }
 }
+
+// 앱 테스트 실행용
+const app = new App();
+app.play();
 
 export default App;
