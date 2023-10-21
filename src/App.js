3de88da0a9a4aa2633ce1410046e4369f200bd2e
meunrud <113085963+meunrud@users.noreply.github.com>
2023-10-21 15:00:12 +0900

    feat : 게임 시작 문구 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 class App {
-  async play() {}
+  async play() {
+    const StartMessage = '숫자 야구 게임을 시작합니다.';
+    console.log(StartMessage);
+  }
 }
+
+const GameStart = new App(); // App 클래스의 인스턴스 생성
+GameStart.play();
 
 export default App;
