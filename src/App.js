f2241d1c5fb5f454360a58fb8f522b6e70b1625e
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-10-21 23:16:04 +0900

    feat: 추가 입력값 유효성 검증 함수
    사용자 입력값에 대해 빈 입력, 유효하지 않은 자릿수, 서로 다르지 않은 숫자 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+const VALID_INPUT_LENGTH = 3;
 export default class App {
   constructor() {
     this.computerNumber = "";
@@ -12,12 +13,34 @@ export default class App {
         this.computerNumber += randomNumber;
       }
     }
+    MissionUtils.Console.print(this.computerNumber);
+  }
+
+  async getUserNumber() {
+    return await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+  }
+
+  checkUserNumberValid(userNumber) {
+    if (!userNumber) {
+      throw new Error("빈 입력값입니다.");
+    }
+    // 입력한 값이 모두 숫자인지 확인
+    // ... 구현 필요 ...
+    if (userNumber.length != VALID_INPUT_LENGTH) {
+      throw new Error("유효한 자릿수가 아닙니다.");
+    }
+    const userNumberSet = new Set([...userNumber]);
+    if (userNumberSet.size != VALID_INPUT_LENGTH) {
+      throw new Error("각 자리의 숫자는 서로 다른 숫자여야 합니다.");
+    }
   }
 
   // 게임 진행 함수
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.initComputerNumber();
+    let userInput = await this.getUserNumber();
+    this.checkUserNumberValid(userInput);
   }
 }
 
