0de09c00b5c29fc71a50b6bec5cab495e69f06f1
novice-hero <highero700@gmail.com>
2023-10-21 18:16:04 +0900

    Feat: 랜덤 컴퓨터 숫자 로직을 함수로 분리

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
-  async play() {
+  async makeComputerNumber() {
     const computerNumber = [];
+
     while (computerNumber.length < 3) {
       const randomNumber = Random.pickNumberInRange(1, 9);
       if (!computerNumber.includes(randomNumber)) {
         computerNumber.push(randomNumber);
       }
     }
+
+    return computerNumber;
+  }
+
+  async play() {
+    let isStart = true;
+
+    Console.print("숫자 야구 게임을 시작합니다.");
+    while (isStart) {
+      let isCorrect = false;
+      const computerNumber = await this.makeComputerNumber();
+    }
   }
 }
-
-const app = new App();
-app.play();
 
 export default App;
