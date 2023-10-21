0f8fc177a1e039ddc41dcd1a88993ab9188c9abb
sihyun10 <hyhy200170@gmail.com>
2023-10-22 03:14:17 +0900

    feat: 게임이 끝난 경우 재시작/종료 기능 3개의 숫자를 모두 맞힌 다음 '3개의 숫자를  맞히셨습니다! 게임 종료' 문구를 띄워준 뒤, 재시작/종료를 구분하는 1과 2 중 하나의 수를 입력받아 1을 입력하면 다시 게임이 재시작되게끔 한다. 이때 this.play();를 사용해 play()를 다시 호출하였다. 따라서 게임을 다시 시작할 수 있게 되었다. 2를 입력하게 되면 종료를 뜻하는것이기에 '게임을 종료합니다' 출력해준뒤 종료시켰다.

--- src/App.js
+++ src/App.js
@@ -27,6 +27,14 @@ class App {
         break;
       }
     }
+
+    const playOption = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. \n");
+    
+    if(playOption === '1') {
+      this.play();
+    } else if(playOption === '2') {
+      MissionUtils.Console.print("게임을 종료합니다.");
+    }
   }
 }
 
