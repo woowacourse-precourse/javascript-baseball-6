ea77ceee7c4dfce62c055d49d27e230cecc65ebd
Trophy198 <spsp8426@naver.com>
2023-10-21 11:43:14 +0900

    Feat : END_GAME 상수 추가

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,6 @@
 export const NUM_DIGITS = 3;
 export const RESTART_GAME = '1';
+export const END_GAME = '2';
 export const GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 export const GAME_END_INSTRUCTION = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.';
 export const STRIKE_TEXT = '스트라이크';
