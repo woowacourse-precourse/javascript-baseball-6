c5555b686ccb6bcfc72249d8b1855f65521e76d9
hongst222 <tmdxordusdo@gmail.com>
2023-10-22 03:46:10 +0900

    [feat] throwError() 함수 추가

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,15 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import {ERRORS} from "../constants/index.js"
 const Console = MissionUtils.Console;
 const Random = MissionUtils.Random;
-
-export {Console, Random}
+/**
+ * 반복되는 에러 처리 과정 모듈화
+ * @param {boolean} condition
+ * @param {string} errorMessage
+ */
+const throwError= (condition, errorMessage) => {
+    if(condition){
+        throw new Error(`${ERRORS.FRONT} ${errorMessage}`)
+    }
+} 
+export {Console, Random, throwError}
