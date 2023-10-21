a9c3f917c325196e48003d120b382a43a323305d
Kled <1017yu@daum.net>
2023-10-21 23:31:39 +0900

    feat: 플레이어 승리, 재시작, 볼카운트 상수 추가

--- constants/index.js
+++ constants/index.js
@@ -2,8 +2,15 @@ const constants = Object.freeze({
   MESSAGES: {
     START: '숫자 야구 게임을 시작합니다.',
     INPUT: '숫자를 입력해주세요 : ',
+    PLAYER_WIN: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+    PLAY_AGAIN: `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`,
     ERROR: {
       INVALID_COUNT_NUMBER: '[ERROR] 서로 다른 3자리의 수를 입력해주세요.',
+    },
+    BALL_COUNTS: {
+      BALL: '볼',
+      STRIKE: '스트라이크',
+      NOTHING: '낫싱',
     },
   },
   THREE_DIGIT_PATTERN: new RegExp(/^([1-9])(?!.*\1)([1-9])(?!.*\1|\2)([1-9])$/),
