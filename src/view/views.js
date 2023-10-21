52d665fa904c545cb869f414cc1f4cb298dc9cc7
jonique98 <tnals9899@naver.com>
2023-10-21 15:03:19 +0900

    feat: view를 통해서 I/O  분리

---
+++ src/view/views.js
@@ -0,0 +1,9 @@
+import { Console } from '@woowacourse/mission-utils';
+import { MESSAGE } from '../constants/constants.js'
+
+export default class views{
+
+	constructor() {
+		Console.print(MESSAGE.START);
+	}
+}
