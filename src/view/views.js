7dc030a210315cd397460343076049642220beb4
jonique98 <tnals9899@naver.com>
2023-10-21 15:05:42 +0900

    style: indent 맞춤

--- src/view/views.js
+++ src/view/views.js
@@ -2,35 +2,34 @@ import { Console } from '@woowacourse/mission-utils';
 import { MESSAGE } from '../constants/constants.js'
 
 export default class views{
-	constructor() {
-		Console.print(MESSAGE.START);
-	}
+  constructor() {
+    Console.print(MESSAGE.START);
+  }
 
-	correct() {
-		Console.print(MESSAGE.CORRECT);
-	}
+  correct() {
+    Console.print(MESSAGE.CORRECT);
+  }
 
-	result(result) {
-		Console.print(this.makeResultMessage(result));
-	}
+  result(result) {
+    Console.print(this.makeResultMessage(result));
+  }
 
-	makeResultMessage(result) {
-		let resultMESSAGE = '';
-	
-		if (result.NOTHING === true) {
-			resultMESSAGE = resultMESSAGE.concat(MESSAGE.NOTHING);
-			return resultMESSAGE;
-		}
-	
-		if (result.BALL !== 0) {
-			resultMESSAGE = resultMESSAGE.concat(String(result.BALL) + MESSAGE.BALL + ' ');
-		}
-	
-		if (result.STRIKE !== 0) {
-			resultMESSAGE = resultMESSAGE.concat(String(result.STRIKE) + MESSAGE.STRIKE + ' ');
-		}
-		
-		return resultMESSAGE;
-	}
-
+  makeResultMessage(result) {
+    let resultMESSAGE = '';
+  
+    if (result.NOTHING === true) {
+      resultMESSAGE = resultMESSAGE.concat(MESSAGE.NOTHING);
+      return resultMESSAGE;
+    }
+  
+    if (result.BALL !== 0) {
+      resultMESSAGE = resultMESSAGE.concat(String(result.BALL) + MESSAGE.BALL + ' ');
+    }
+  
+    if (result.STRIKE !== 0) {
+      resultMESSAGE = resultMESSAGE.concat(String(result.STRIKE) + MESSAGE.STRIKE + ' ');
+    }
+    
+    return resultMESSAGE;
+  }
 }
