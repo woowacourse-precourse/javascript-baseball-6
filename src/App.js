897987114fa8819134710234a569cf1cf80e73a2
김경주 <popo505@naver.com>
2023-10-22 00:12:01 +0900

    feat:게임시작

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import { Console } from "@woowacourse/mission-utils";
+import { randomNumber } from "./RandomNumber.js";
+
 class App {
   async play() {
-    function start() {
-      console.log("숫자 야구 게임을 시작합니다.");
-    }
-    start();
+      console.print("숫자 야구 게임을 시작합니다."); //게임시작
   }
 }
 
