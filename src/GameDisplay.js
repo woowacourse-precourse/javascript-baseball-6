b349f3b642f16e361819e53a8b79ec1102c4c368
Trophy198 <spsp8426@naver.com>
2023-10-22 01:52:04 +0900

    Feat : 입력과 출력값들이 보이는 Display클래스 추가

---
+++ src/GameDisplay.js
@@ -0,0 +1,31 @@
+import { Console } from "@woowacourse/mission-utils";
+import { GAME_START_MESSAGE, GAME_OVER_MESSAGE, GAME_END_INSTRUCTION, STRIKE_TEXT, BALL_TEXT, NOTHING_TEXT } from "./constants/MessageConstants.js";
+
+export default class GameDisplay {
+  showStartMessage() {
+    Console.print(GAME_START_MESSAGE);
+  }
+
+  showResult(strike, ball) {
+    if (strike === 0 && ball === 0) {
+      Console.print(NOTHING_TEXT);
+    } else {
+      const results = [];
+      if (ball > 0) results.push(`${ball}${BALL_TEXT}`);
+      if (strike > 0) results.push(`${strike}${STRIKE_TEXT}`);
+      Console.print(results.join(' '));
+    }
+  }
+
+  showWinMessage() {
+    Console.print(GAME_OVER_MESSAGE);
+  }
+
+  async showEndMessage() {
+    return await Console.readLineAsync(GAME_END_INSTRUCTION);
+  }
+
+  showInvalidInputMessage(errorMessage) {
+    Console.print(`${errorMessage}`);
+  }
+}
