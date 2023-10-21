d11b3b0e0f860beb13a2ddbefdb48d7dceab858c
sihyun10 <hyhy200170@gmail.com>
2023-10-21 23:15:35 +0900

    feat: 컴퓨터가 랜덤으로 3자리수 선택, 사용자에게 입력받기 게임을 진행하기 위해서는 일단 상대방 역할인 컴퓨터가 1~9까지 서로 다른 임의의 수 3개를 선택해야한다. 따라서 MissionUtils.Random.pickNumberInRange()를 활용해서 서로 다른 임의의 수 3개를 선택하였고, 사용자에게는 Console.readLineAsync()를 활용해 수를 입력받았다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+async function getUsernumber() {
+  try {
+    const userInput = await MissionUtils.Console.readLineAsync(
+      "숫자를 입력해주세요 : "
+    );
+    return userInput;
+  } catch (error) {
+    // reject 되는 경우
+  }
+}
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    const computerAnswer = [];
+    while (computerAnswer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerAnswer.includes(number)) {
+        computerAnswer.push(number);
+      }
+    }
+
+    MissionUtils.Console.print(`${computerAnswer}`);
+
+    const userInput = await getUsernumber();
+    MissionUtils.Console.print(`${userInput}`);
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
