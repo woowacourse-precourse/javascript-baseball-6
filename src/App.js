f2847700aa26b73a6fcdf695cc0035b63a445593
solssak <gswoong1012@gmail.com>
2023-10-21 18:19:44 +0900

    feat: 기능4 - 사용자에게 입력 받은 값 유효성 검사하기 (컴퓨터)

--- src/App.js
+++ src/App.js
@@ -18,9 +18,34 @@ class App {
 
   setUserInput() {
     Console.readLine("숫자를 입력해주세요 : ", (inputNumber) => {
-      const userInputNumber = inputNumber.split("").map(Number);
+      const userInputNumber = this.userInputNumberValidation(inputNumber).split("").map(Number);
       Console.print(userInputNumber);
     });
+  }
+
+  lengthValidation(inputNumber) {
+    return inputNumber.length !== 3;
+  }
+
+  rangeValidation(inputNumber) {
+    return isNaN(inputNumber) || inputNumber.toString().includes("0");
+  }
+
+  duplicateValidation(inputNumber) {
+    for (let i = 1; i < inputNumber.length; i++) {
+      if (inputNumber[i - 1] === inputNumber[i]) return true;
+    }
+    return false;
+  }
+
+  userInputNumberValidation(inputNumber) {
+    if (
+      this.lengthValidation(inputNumber) ||
+      this.rangeValidation(inputNumber) ||
+      this.duplicateValidation(inputNumber)
+    )
+      throw new Error("잘못된 값 입력됨");
+    return inputNumber;
   }
 
   async play() {
