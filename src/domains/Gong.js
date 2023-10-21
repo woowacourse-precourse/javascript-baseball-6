a7442308d24ab065f7805729d8327a2709fdd22c
brgndy <brgndy96@gmail.com>
2023-10-21 12:05:21 +0900

    feat(play) : 공을 담은 클래스, 상대방을 정의할 클래스 생성

---
+++ src/domains/Gong.js
@@ -0,0 +1,7 @@
+// 공들을 관리하는 클래스
+
+class Gong {
+  constructor() {}
+}
+
+export default Gong;
---
+++ src/domains/Opponent.js
@@ -0,0 +1,6 @@
+// 상대방 (컴퓨터)를 정의하는 클래스
+class Oppenent {
+  constructor() {}
+}
+
+export default Oppenent;
