c52368b9cd71fb8d2d213a9e962c4d2cd3641087
Kled <1017yu@daum.net>
2023-10-21 23:32:53 +0900

    refactor: App Class 내 play 필드 수정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import Player from './player/index.js';
 import Opponent from './opponent/index.js';
+
 const { Console } = MissionUtils;
 
 class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
-    this.player = new Player();
-    this.opponent = new Opponent();
+    const player = new Player();
+    player.startGame();
   }
 }
-
-const app = new App();
-
-app.play();
 
 export default App;
