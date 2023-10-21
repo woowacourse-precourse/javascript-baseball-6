0f720c5210c86edb2b7d0cd0eed4aa3f5ac9aedf
BK <3031011@naver.com>
2023-10-21 19:14:01 +0900

    🦄 refactor: App 클래스 메소드 GameManager 클래스로 이동

--- src/App.js
+++ src/App.js
@@ -5,40 +5,7 @@ const CORRECT_NUMBER = 3;
 class App {
   computer = new Computer();
   player = new Player();
-
-  isValidInput(input) {
-    const STR_USER_INPUT = String(input);
-    const SET_USER_INPUT = new Set([...STR_USER_INPUT]);
-
-    if (/^[1-9]{3}$/.test(STR_USER_INPUT) && SET_USER_INPUT.size === 3) {
-      return true;
-    }
-
-    return false;
-  }
-
-  calculateStrikesAndBalls(userInput, computerValue) {
-    const USER_INPUT_ARRAY = [...String(userInput)];
-    const COMPUTER_VALUE_ARRAY = [...String(computerValue)];
-    const STRIKE = USER_INPUT_ARRAY.filter((v, i) => v === COMPUTER_VALUE_ARRAY[i]).length;
-    const BALL = COMPUTER_VALUE_ARRAY.filter((v, i) => (v !== USER_INPUT_ARRAY[i]) && USER_INPUT_ARRAY.includes(v)).length;
-
-    return { STRIKE, BALL };
-  }
-
-  gameResultMessage(strike, ball) {
-    let str = '';
-
-    if (strike === CORRECT_NUMBER) {
-      str = '3스트라이크';
-    } else if (strike === 0 && ball === 0) {
-      str = '낫싱';
-    } else if (strike > 0 || ball > 0) {
-      str = `${ball ? ball + '볼' : ''} ${strike ? strike + '스트라이크' : ''}`.trim();
-    }
-
-    return str;
-  }
+  gameManager = new GameManager();
 
   async play() {
     this.computer.generateThreeDigits();
@@ -47,11 +14,11 @@ class App {
       while (isPlaying) {
         const USER_INPUT = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
 
-        if (!this.isValidInput(USER_INPUT)) throw new Error('[ERROR]');
+        if (!this.gameManager.validatePlayerInput(USER_INPUT)) throw new Error('[ERROR] : 유효하지 않은 입력입니다.');
         this.player.setNumber(USER_INPUT);
 
-        const { STRIKE, BALL } = this.calculateStrikesAndBalls(this.player.number, this.computer.number);
-        MissionUtils.Console.print(this.gameResultMessage(STRIKE, BALL));
+        const { STRIKE, BALL } = this.gameManager.evaluatePlayerInput(this.player.number, this.computer.number);
+        MissionUtils.Console.print(this.gameManager.printResultMessage(STRIKE, BALL));
 
         if (STRIKE === CORRECT_NUMBER) {
           const INPUT = await MissionUtils.Console.readLineAsync('3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
@@ -103,7 +70,39 @@ class Player {
 }
 
 class GameManager {
-  
+  validatePlayerInput(input) {
+    const STR_USER_INPUT = String(input);
+    const SET_USER_INPUT = new Set([...STR_USER_INPUT]);
+
+    if (/^[1-9]{3}$/.test(STR_USER_INPUT) && SET_USER_INPUT.size === 3) {
+      return true;
+    }
+
+    return false;
+  }
+
+  evaluatePlayerInput(userInput, computerValue) {
+    const USER_INPUT_ARRAY = [...String(userInput)];
+    const COMPUTER_VALUE_ARRAY = [...String(computerValue)];
+    const STRIKE = USER_INPUT_ARRAY.filter((v, i) => v === COMPUTER_VALUE_ARRAY[i]).length;
+    const BALL = COMPUTER_VALUE_ARRAY.filter((v, i) => (v !== USER_INPUT_ARRAY[i]) && USER_INPUT_ARRAY.includes(v)).length;
+
+    return { STRIKE, BALL };
+  }
+
+  printResultMessage(strike, ball) {
+    let str = '';
+
+    if (strike === CORRECT_NUMBER) {
+      str = '3스트라이크';
+    } else if (strike === 0 && ball === 0) {
+      str = '낫싱';
+    } else if (strike > 0 || ball > 0) {
+      str = `${ball ? ball + '볼' : ''} ${strike ? strike + '스트라이크' : ''}`.trim();
+    }
+
+    return str;
+  }
 }
 
 export default App;
