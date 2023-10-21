8db68cd0f1845b62bd7f7a27a0810ad79a08e8df
youjeong Jeong <wjddbwjd1205@naver.com>
2023-10-22 00:36:33 +0900

    refact: 코드의 가독성을 높이기 위해 Random 객체를 직접 사용하도록 변경

--- src/Computer.js
+++ src/Computer.js
@@ -1,11 +1,11 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Random } from "@woowacourse/mission-utils";
 
 export class Computer {
 
     createRandomNum() {
         const computer = [];
         while (computer.length < 3) {
-            const number = MissionUtils.Random.pickNumberInRange(1, 9);
+            const number = Random.pickNumberInRange(1, 9);
             if (!computer.includes(number)) {
                 computer.push(number);
             }
