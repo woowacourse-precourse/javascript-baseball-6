bd6df1a6b19c6da063957bf529eca61e98e6043e
EunJu JO <dmswnehf@naver.com>
2023-10-21 20:47:55 +0900

    feat: 랜덤 숫자 생성 로직 구현

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import Computer from './Computer.js';
 import BaseballGame from './BaseballGame.js';
 
 class App {
-  async play() {}
+    constructor() {
+        this.computer = new Computer();
+    }
+
+    async play() {
+        
+    }
 }
+
+// 숫자아구 게임 시작
+const app = new App();
+app.play();
 
 export default App;
