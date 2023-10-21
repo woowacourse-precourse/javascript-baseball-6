c59f9036f5a957a4aa51a773e5213ddf4b3a043c
jonique98 <tnals9899@naver.com>
2023-10-21 14:28:32 +0900

    feat: 객체 구조 설계

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-class App {
-  async play() {}
-}
+import  opponent  from './players/opponent.js';
+import  views  from './view/views.js';
+import player from './players/player.js';
+import { MESSAGE , CONSTANT } from './constants/constants.js';
 
-export default App;
+export default class App{
+
+	#_opponent;
+	#_player;
+	#_view;
+
+	//플레이어와 상대방 객체 생성 입출력 담당 view 객체 생성
+  constructor() {
+		this.#_opponent = new opponent();
+    this.#_player = new player();
+    this.#_view = new views();
+  }
+
+	//게임 시작
+  async play() {
+    const isValid = await this.#_player.inputNumber();
+  }
+
+}
