415d0b83b4e81a43936809396f0ec4ff4a08da6c
jeongin98 <eon7500@gmail.com>
2023-10-21 20:32:48 +0900

    feat: 사용자 숫자 입력 유효성 검사

--- src/App.js
+++ src/App.js
@@ -15,10 +15,30 @@ class App {
     this.randomNumber = [...computer];
   }
 
-  async play() {
-    this.setRandomNumber();
+
+  async getUserInput() {
+    try {
+      const userInput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+      const result = this.isValidUserInput(userInput);
+
+    } catch (error) {
+      console.error('[Error] ', error.message)
+    }
   }
 
+  isValidUserInput(userInput) {
+    if (userInput.length !== 3) {
+      throw new Error('3자리를 입력해주세요');
+    }
+    // 이후 예외조건 추가
+  }
+
+
+  async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.')
+    this.setRandomNumber();
+    await this.getUserInput();
+  }
 
 }
 
