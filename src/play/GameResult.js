7d5f910080a1205f9c2bea3fd6daf366b2d1adb1
hyj5076 <hyj5076@hanmail.net>
2023-10-22 03:23:34 +0900

    refactor: 코드 정리

--- src/play/GameResult.js
+++ src/play/GameResult.js
@@ -2,9 +2,7 @@
 const computerNumber = "952";
 const playerNumber = "512";
 
-const arrNumber = (arr) => {
-  return arr.split('').map(Number); // 배열로 바꿈
-} 
+const arrNumber = (arr) => arr.split('').map(Number); // 배열로 바꿈
 
 const computerArray = arrNumber(computerNumber);
 const playerArray = arrNumber(playerNumber);
@@ -15,17 +13,14 @@ const showResult = () => {
   let ball = 0;
 
   for (let i = 0; i < 3; i++) {
-      if (computerArray[i] === playerArray[i]) {
-        strike++;
-      } else if (playerArray.includes(computerArray[i])) {
-        ball++;
-      }
+      if (computerArray[i] === playerArray[i]) { strike++; }
+      else if (playerArray.includes(computerArray[i])) { ball++; }
   }
 
   const result = [];
 
   if (strike === 3) return Console.print("성공!");
-  if (strike > 0) result.push(`${스트라이크} 스트라이크`);
+  if (strike > 0) result.push(`${strike} 스트라이크`);
   if (ball > 0) result.push(`${ball} 볼`);
 
   Console.print(result.length ? result.join(' ') : "낫싱");
