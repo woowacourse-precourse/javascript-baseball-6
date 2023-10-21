aae36257bbba6f6478ff3ae3f901b2d6479b923a
김경주 <popo505@naver.com>
2023-10-21 20:20:21 +0900

    feat:게임시작

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 class App {
-  async play() {}
+  async play() {
+    function start() {
+      console.log("숫자 야구 게임을 시작합니다.");
+    }
+    start();
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
