bfbb5dbd7f0b0624440da7cca573391262050b4d
hongst222 <tmdxordusdo@gmail.com>
2023-10-22 04:06:36 +0900

    [feat] API를 간단히 가져오는 함수와 Error를 처리하는 함수 추가

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,35 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import {ERRORS} from "../constants/index.js"
-const Console = MissionUtils.Console;
-const Random = MissionUtils.Random;
+
+
+/**
+ * 문자열을 출력한다.
+ * @param {string} message 
+ */
+function print(message) {
+    MissionUtils.Console.print(message);
+}
+
+/**
+ * 사용자에게 message 문자열을 보여준 후 데이터를 입력받는다.
+ * @param {string} message 
+ * @returns {string} 사용자에게 입력받은 데이터
+ */
+async function readLineAsync(message) {
+    const input = await MissionUtils.Console.readLineAsync(message);
+    return input;
+}
+
+/**
+ * 랜덤한 숫자를 생성한다.
+ * @param {number} min 
+ * @param {number} max 
+ * @returns {number} 랜덤한 숫자
+ */
+function pickNumberInRange(min, max){
+    return MissionUtils.Random.pickNumberInRange(min,max)
+}
+
 /**
  * 반복되는 에러 처리 과정 모듈화
  * @param {boolean} condition
@@ -12,4 +40,4 @@ const throwError= (condition, errorMessage) => {
         throw new Error(`${ERRORS.FRONT} ${errorMessage}`)
     }
 } 
-export {Console, Random, throwError}
+export {print, readLineAsync, pickNumberInRange, throwError}
