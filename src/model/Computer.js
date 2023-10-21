78014e1df8c04ca0eb2be9f2819b0951493c8b10
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-21 15:50:50 +0900

    feat: [Computer] 컴퓨터가 임의로 선택한 3개의 수 제작 로직 구현

--- src/model/Computer.js
+++ src/model/Computer.js
@@ -1,3 +1,5 @@
+import { Random } from '@woowacourse/mission-utils';
+
 export default class Computer {
   constructor() {
     this.number = null;
@@ -9,5 +11,21 @@ export default class Computer {
 
   setNumber(newNumber) {
     this.number = newNumber;
+  }
+
+  /**
+   * 서로 다른 3개의 수를 string형태로 배열에 담은 후 변수에 저장합니다.
+   */
+  chooseRandomNumber() {
+    const numbers = [];
+
+    while (numbers.length < 3) {
+      const randomNumberToString = String(Random.pickNumberInRange(1, 9));
+      if (!numbers.includes(randomNumberToString)) {
+        numbers.push(randomNumberToString);
+      }
+    }
+    
+    this.setNumber(numbers);
   }
 }
