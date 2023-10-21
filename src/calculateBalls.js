9e65892ff3f678b12a539d15fcc3480c668cd173
JiSeong Kim <12180543@inha.edu>
2023-10-21 17:24:24 +0900

    Feat: ball의 수를 체크하는 calculateBalls 함수 구현
    user가 입력한 숫자와 컴퓨터가 랜덤으로 생성한 숫자를 인수로 받아 볼의 개수를 계산하는 함수. userNumber를 반복문을 돌려가며 하나의 숫자마다 체크하고, 하나의 숫자가 computerNumber에 존재하고, 같은 인덱스번호에 존재하지 않는 경우에 cnt를 1씩 늘려감.

---
+++ src/calculateBalls.js
@@ -0,0 +1,10 @@
+const calculateBalls = (userNumber, computerNumber) => {
+    let cntBall = 0;
+    for (let ni = 0; ni < 3; ni++) {
+        const foundIdx = computerNumber.indexOf(userNumber[ni])
+        if (foundIdx !== -1 && ni !== foundIdx) {
+            cntBall += 1;
+        }
+    }
+    return cntBall
+}
