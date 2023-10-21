e3f8ee40fc952d05ccfe966b2b87109b98fe7e53
Lee sang Yeop <mma7710@naver.com>
2023-10-22 01:54:09 +0900

    Refactor: 변수명을 더욱 명시적으로 변경 및 매개변수 메서드 인자 기본값 설정

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,19 +1,19 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class UserInput {
-	async baseballInput(query = '') {
+	async baseballInput(question = '숫자를 입력해주세요 : ') {
 		try {
-			const val = await Console.readLineAsync(query);
-			return val;
+			const userInput = await Console.readLineAsync(question);
+			return userInput;
 		} catch (err) {
 			console.error(err);
 		}
 	}
 
-	async gameEndInput(query = '') {
+	async gameEndInput(question = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n') {
 		try {
-			const val = await Console.readLineAsync(query);
-			return val;
+			const userInput = await Console.readLineAsync(question);
+			return userInput;
 		} catch (err) {
 			console.error(err);
 		}
