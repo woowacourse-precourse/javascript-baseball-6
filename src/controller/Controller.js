17fa26523dc711bd47489599e25b9471d0ee6b8d
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-21 15:57:35 +0900

    feat: [Controller] constructor 구현

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,3 +1,13 @@
+import Computer from "../model/Computer.js";
+import Player from "../model/Player.js";
+import Validation from "../utils/Validation.js";
+import View from "../view/View.js";
+
 export default class Controller {
-  constructor() {}
+  constructor() {
+    this.view = new View();
+    this.computer = new Computer();
+    this.player = new Player();
+    this.validation = new Validation();
+  }
 }
