84bb483a7387ee242ed8fa4845d2829d11959d15
rustandbone <dlehdgh14@naver.com>
2023-10-21 11:27:46 +0900

    [기능 구현] getNumber 메서드 분리, 변수명 상세화

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ class App {
     await this.start();
   }
 
+  async start() {
+    await this.randomNumber();
+    await this.getNumber();
+  }
+
   async randomNumber() {
     while (this.computer.length < 3) {
       const number = Random.pickNumberInRange(1, 9);
@@ -20,71 +25,60 @@ class App {
     return this.computer;
   }
 
-  async start() {
-    await this.randomNumber();
+  async getNumber() {
+    try {
+      const threeNumber = await Console.readLineAsync("숫자를 입력해주세요 : ");
 
-    const getNumber = async () => {
-      try {
-        const threeNumber = await Console.readLineAsync(
-          "숫자를 입력해주세요 : "
-        );
+      const threeNumberSet = new Set(threeNumber.split(""));
 
-        const set = new Set(threeNumber.split(""));
-
-        if (
-          isNaN(+threeNumber) ||
-          threeNumber.length !== 3 ||
-          set.size !== threeNumber.split("").length
-        ) {
-          throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
-        }
-
-        let array = threeNumber.split("");
-        const ballCount = { strike: 0, ball: 0 };
-
-        array.map((item, index) => {
-          if (
-            this.computer.includes(+item) &&
-            this.computer[index] === +array[index]
-          )
-            ballCount.strike++;
-          else if (
-            this.computer.includes(+item) ||
-            this.computer[index] === +array[index]
-          )
-            ballCount.ball++;
-        });
-
-        if (ballCount.strike === 3) {
-          Console.print(
-            `3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`
-          );
-
-          const startCheck = await Console.readLineAsync(
-            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
-          );
-
-          if (startCheck === "1") {
-            const app = new App();
-            await app.start();
-          } else if (startCheck !== "2") {
-            throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
-          }
-          return;
-        } else if (ballCount.strike && ballCount.ball)
-          Console.print(`${ballCount.ball}볼 ${ballCount.strike}스트라이크`);
-        else if (ballCount.strike)
-          Console.print(`${ballCount.strike}스트라이크`);
-        else if (ballCount.ball) Console.print(`${ballCount.ball}볼`);
-        else Console.print("낫싱");
-
-        await getNumber();
-      } catch (error) {
+      if (
+        isNaN(+threeNumber) ||
+        threeNumber.length !== 3 ||
+        threeNumberSet.size !== threeNumber.split("").length
+      ) {
         throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
       }
-    };
 
-    await getNumber();
+      let threeNumberArray = threeNumber.split("");
+      const ballCount = { strike: 0, ball: 0 };
+
+      threeNumberArray.map((item, index) => {
+        if (
+          this.computer.includes(+item) &&
+          this.computer[index] === +threeNumberArray[index]
+        )
+          ballCount.strike++;
+        else if (
+          this.computer.includes(+item) ||
+          this.computer[index] === +threeNumberArray[index]
+        )
+          ballCount.ball++;
+      });
+
+      if (ballCount.strike === 3) {
+        Console.print(`3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
+
+        const startCheck = await Console.readLineAsync(
+          "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+        );
+
+        if (startCheck === "1") {
+          const app = new App();
+          await app.start();
+        } else if (startCheck !== "2") {
+          throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+        }
+        return;
+      } else if (ballCount.strike && ballCount.ball)
+        Console.print(`${ballCount.ball}볼 ${ballCount.strike}스트라이크`);
+      else if (ballCount.strike) Console.print(`${ballCount.strike}스트라이크`);
+      else if (ballCount.ball) Console.print(`${ballCount.ball}볼`);
+      else Console.print("낫싱");
+
+      await this.getNumber();
+    } catch (error) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
   }
 }
 
