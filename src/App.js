7c349cd611f5ee0f3e2dca646bf3580d3ccd1b1c
zelkovaria <hyein396@gmail.com>
2023-10-21 11:35:44 +0900

    ball과 strike 계산 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Console, Random, MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   computerNum() {
@@ -14,8 +14,39 @@ class App {
     return computerBall;
   }
 
+  async strikeBall(computerBall, user) {
+    let strike = 0;
+    let ball = 0;
+
+    for (let i = 0; i < 3; i++) {
+      if (computerBall[i] === user[i]) {
+        strike += 1;
+      }
+      if (computerBall.includes(user[i])) {
+        ball += 1;
+      }
+    }
+    if (strike && ball) {
+      if (strike === ball) {
+        MissionUtils.Console.print(`${strike}스트라이크`);
+      } else {
+        MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+      }
+    } else if (ball) {
+      MissionUtils.Console.print(`${ball}볼`);
+    } else if (strike) {
+      MissionUtils.Console.print(`${strike}스트라이크`);
+    } else {
+      MissionUtils.Console.print("낫싱");
+    }
+  }
   async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     const computerBall = this.computerNum();
+    const user = await MissionUtils.Console.readLineAsync(
+      "숫자를 입력해주세요 : "
+    );
+    const result = this.strikeBall(computerBall, user);
   }
 }
 
