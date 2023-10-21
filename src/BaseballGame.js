8443753943315125a7db4e43fc714d3f11e963d6
JUNU <bokjunwoo@gmail.com>
2023-10-21 14:40:24 +0900

    feat : 입력한 숫자를 검증하여 유효성 검사 구현

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -1,4 +1,5 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import { NUMBER_REGEX } from './constants.js';
 
 class BaseballGame {
   constructor() {
@@ -14,6 +15,29 @@ class BaseballGame {
       }
     }
     return computer;
+  }
+
+  async play(userNumbers) {
+    this.isValidInput(userNumbers);
+  }
+
+  isValidInput(userNumbers) {
+    const numberArray = userNumbers.split('').map(Number);
+
+    const isValidNumber = numberArray.every((num) => NUMBER_REGEX.test(num));
+    if (!isValidNumber) {
+      throw new Error('유효한 숫자를 입력해야 합니다.');
+    }
+
+    if (numberArray.length !== 3) {
+      throw new Error('3개의 숫자를 입력해야 합니다.');
+    }
+
+    if (new Set(numberArray).size !== 3) {
+      throw new Error('숫자는 중복되지 않아야 합니다.');
+    }
+
+    return true;
   }
 }
 
