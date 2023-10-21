75f07f850076bc76a860570de6d6fca530d10df9
JUNU <bokjunwoo@gmail.com>
2023-10-21 14:14:49 +0900

    feat : 플레이어가 3개의 숫자를 입력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 class App {
   play() {
     this.gameStartMessage();
+    this.getUserInputAsync();
   }
 
   gameStartMessage() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  async getUserInputAsync() {
+    try {
+      const userInput = await MissionUtils.Console.readLineAsync(
+        '숫자를 입력해 주세요 :'
+      );
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
