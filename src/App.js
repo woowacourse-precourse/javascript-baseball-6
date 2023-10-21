07d330c2e7b3cc36defad647cfde80da136bbf05
silver0108 <kk1170@naver.com>
2023-10-22 02:42:47 +0900

    feat: 정답 비교 및 판단

--- src/App.js
+++ src/App.js
@@ -31,6 +31,34 @@ function checkUserNum(userNum){
   return true;
 }
 
+function compareNum(computerNum, userNum){
+  let strike = 0;
+  let ball = 0;
+
+  for(let i=0; i<3; i++){
+    if(computerNum[i] === Number(userNum[i])){
+      strike += 1;
+    }else if(computerNum.includes(Number(userNum[i]))){
+      ball += 1;
+    }
+  }
+
+  if(strike === 3){
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    return true;
+  }else if(strike !== 0 || ball !== 0){
+    if(ball === 0){
+      MissionUtils.Console.print(strike + "스트라이크");
+    }else if(strike === 0){
+      MissionUtils.Console.print(ball + "볼");
+    }else{
+      MissionUtils.Console.print(ball + "볼 " + strike + "스트라이크");
+    }
+  }else{
+    MissionUtils.Console.print("낫싱");
+  }
+}
+
 class App {
   async play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
@@ -43,6 +71,9 @@ class App {
     if(!checkUserNum(userNum)){
       throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
     }
+
+    compareNum(computerNum, userNum);
+
   }
 }
 
