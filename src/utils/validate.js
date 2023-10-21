ed6f09be328a9f268cf29b57d4831c8676214385
mywhzy <cmj2393a@naver.com>
2023-10-22 02:20:50 +0900

    feat: 플레이어 옵션 입력 값 검증 기능 추가

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,14 +1,22 @@
-import { INPUT_REGEX } from '../constants.js';
+import { PLAY_INPUT_REGEX, OPTION_INPUT_REGEX } from '../constants.js';
 
 export default function validate(input) {
-  const inputArr = Array.from(input + '');
-  if (isDuplicate(inputArr)) {
-    return 0;
+  if ((input + '').length === 1) {
+    return OPTION_INPUT_REGEX.test(input);
   } else {
-    return INPUT_REGEX.test(input);
+    return playNumberValidate(input);
   }
 }
 
 const isDuplicate = (array) => {
   return array.some((item) => array.indexOf(item) !== array.lastIndexOf(item));
+};
+
+const playNumberValidate = (input) => {
+  const inputArr = Array.from(input + '');
+  if (isDuplicate(inputArr)) {
+    return 0;
+  } else {
+    return PLAY_INPUT_REGEX.test(input);
+  }
 };
