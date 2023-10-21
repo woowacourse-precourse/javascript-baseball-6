69f50a77c69b6317588219192ec8af72d236b215
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2023-10-21 15:19:06 +0900

    feat : 게임 재시작 기능 추가 및 유효성 검사 구문 수정

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import { Console, Random } from '@woowacourse/mission-utils';
 class App {
   async play() {
     Console.print('숫자야구 게임을 시작합니다.');
-    const answer = genNum();
+    while (true) {
+      const answer = genNum();
 
-    do {
-      const user_input = await Console.readLineAsync('숫자를 입력해주세요 : ');
-      if (!validate(user_input)) throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
-      const { hint_msg, isCorrect } = check(answer, user_input);
-      Console.print(hint_msg);
-      if (isCorrect) break;
-    } while (true);
-    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      do {
+        const user_input = await Console.readLineAsync('숫자를 입력해주세요 : ');
+        if (!validate(user_input)) throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+        const { hint_msg, isCorrect } = check(answer, user_input);
+        Console.print(hint_msg);
+        if (isCorrect) break;
+      } while (true);
+      Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      const replay = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. \n');
+      if (replay === '2') break;
+    }
   }
 }
 
@@ -29,8 +33,8 @@ const genNum = function generateRandomNumber() {
 
 const validate = function checkValidateInput(input) {
   if (input.length !== 3) return false;
-  const regex = /^[1-9]$/;
-  return regex.test(inputValue);
+  const regex = /^[1-9]\d{2}$/;
+  return regex.test(input);
 };
 
 const check = function checkAnswer(answer, input) {
@@ -55,8 +59,5 @@ const check = function checkAnswer(answer, input) {
 
   return { hint_msg, isCorrect: strike === 3 };
 };
-
-const app = new App();
-app.play();
 
 export default App;
