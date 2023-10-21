ce379b3ba365ffb4932cf7a6e2ae88f88274daec
Goongam <dlwjddn341@naver.com>
2023-10-21 20:37:05 +0900

    Feat: 수 입력 시 예외 처리
    - 3자리
    - 서로다른 숫자
    - 1 ~ 9까지의 수
    - 위의 경우 중 하나라도 예외일 경우 throw

--- src/App.js
+++ src/App.js
@@ -15,6 +15,13 @@ class App {
     //2. 3자리 숫자 입력
     const mynumber = await Console.readLineAsync("숫자를 입력해주세요 : ");
     Console.print(mynumber);
+    if(mynumber.length > 3 || // 3자리 초과
+        mynumber.contain('0') || //0포함
+        new Set(mynumber).size !== 3 //중복된 수
+      ){
+        throw new Error("[ERROR]");
+      }
+
   }
 }
 
