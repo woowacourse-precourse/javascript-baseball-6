406670b64c093698c1fe7c5f70e3da7a77b90847
hyewon <hyewon.parkhw@gmail.com>
2023-10-21 23:26:13 +0900

    feat: 사용자와 컴퓨터의 숫자 비교

--- src/App.js
+++ src/App.js
@@ -13,22 +13,26 @@ class App {
         let strike=0;
         while (strike<3){
           //strike 초기화
-          //strike=0;
+          strike=0;
           const playerNumber = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 :");
 
-          //자리수가 맞지 않으면 에러 처리
+          //에러: 입력 값이 3자리보다 많거나 입력하지 않은 경우
           if (playerNumber.length < 1 || playerNumber.length > 3) {
             throw new Error("[ERROR]");
           }
-          //1~9가 아니면 에러 처리
+          
           for (let i = 0; i < playerNumber.length; i++) {
             const num = Number(playerNumber[i]);
+            //에러: 입력 값이 1부터 9까지의 숫자가 아닌 경우
             if (!Array(9).fill().map((_, i) => i + 1).includes(num)) {
               throw new Error("[ERROR]");
             }
+
+            //숫자 비교
+            if (computer.includes(num)) {
+              if (computer[i] == num) { strike++; } else { ball++; }
+            }
           }
-          //숫자 검사 -> strike라면
-          strike++
         }
         replay = await MissionUtils.Console.readLineAsync(
           "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
