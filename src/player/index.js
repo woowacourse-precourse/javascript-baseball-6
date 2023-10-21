11943dba7fe2d98e4c1e07e3eec35d0981f1ad80
Kled <1017yu@daum.net>
2023-10-21 23:35:40 +0900

    feat: Async 메소드 변경 및 재시작/종료 기능 추가

--- src/player/index.js
+++ src/player/index.js
@@ -13,28 +13,57 @@ class Player {
    */
   static playerNumberSet;
 
-  constructor() {
+  startGame = () => {
     this.getPlayerNumber();
     this.opponent = new Opponent();
-  }
+  };
 
   // P-1 플레이어는 3자리 숫자를 맞추기 위해 숫자를 입력
-  getPlayerNumber = () => {
-    Console.readLine(MESSAGES.INPUT, (input) => this.handleNumberSet(input));
+  getPlayerNumber = async () => {
+    try {
+      const playerInput = await Console.readLineAsync(MESSAGES.INPUT);
+      this.handleNumberSet(playerInput);
+    } catch (error) {}
   };
 
   handleNumberSet = (input) => {
     // P-1-a 유효성 검사에 통과하지 못한 경우 에러 메시지와 함께 종료
-    if (!InputValidator.isValidNumberSet(input))
+    if (!InputValidator.isValidNumberSet(input)) {
       throw new Error(MESSAGES.ERROR.INVALID_COUNT_NUMBER);
+    }
 
     // 유효하다면 [number, number, number] 형태의 배열화
     this.playerNumberSet = input.split('').map(Number);
-    this.isCorrected();
+    this.requestBallCount();
   };
 
-  isCorrected = () => {
-    this.opponent.checkBallCount(this.playerNumberSet);
+  requestBallCount = () => {
+    const [ballCountMessage, strike] = this.opponent.checkBallCount(
+      this.playerNumberSet
+    );
+
+    Console.print(ballCountMessage);
+
+    if (strike === 3) {
+      this.getPlayAgain();
+    } else {
+      this.getPlayerNumber();
+    }
+  };
+
+  getPlayAgain = async () => {
+    try {
+      Console.print(MESSAGES.PLAYER_WIN);
+      const playerInput = await Console.readLineAsync(MESSAGES.PLAY_AGAIN);
+
+      if (playerInput === '1') {
+        this.startGame();
+      } else if (playerInput === '2') {
+        Console.print(MESSAGES.END);
+      } else {
+        throw new Error(MESSAGES.ERROR.INVALID_REPLAY_NUMBER);
+      }
+    } catch (error) {}
   };
 }
 
