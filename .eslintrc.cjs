7e9713e54eaf0dc2b3775e3b8e73799b4ddc1e33
brgndy <brgndy96@gmail.com>
2023-10-21 14:36:33 +0900

    style : eslint 및 prettier 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  extends: ['airbnb', 'plugin:jest/recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['off'],
+    'linebreak-style': 0,
+    'no-plusplus': 0,
+  },
+};
