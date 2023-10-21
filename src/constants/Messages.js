e280e3d63babf7b610b853d2670ea56aed904027
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-21 09:46:49 +0900

    :sparkles: feat: 낫싱, 볼, 스트라이크 메세지추가

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -4,4 +4,7 @@ export const MESSAGES = {
   INPUT_ERROR: '[ERROR] 숫자가 잘못된 형식입니다.',
   END_GUIDE: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
   REPLAY_GUIDE: `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`,
+  NATING: '낫싱',
+  BALL: '볼',
+  STRIKE: '스트라이크',
 };
