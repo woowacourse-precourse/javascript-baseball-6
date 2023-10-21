ad5a484ed45b370b5e963785a878ae8cdcc36aab
hyj5076 <hyj5076@hanmail.net>
2023-10-21 20:21:55 +0900

    feat: 사용자 수 유효성 검사 기능 추가 및 수정

--- src/utils/correctNumber.js
+++ src/utils/correctNumber.js
@@ -1,7 +1,8 @@
 export const correctNumber = (userNumber) => {
-  if (userNumber.length !== 3) {
+  if (!/^[1-9]{3}$/.test(userNumber)) {
     return false;
   }
   const uniqueChars = [...new Set([...userNumber])]; // 중복 숫자 제거
   return userNumber.length === uniqueChars.length;
 };
+  
