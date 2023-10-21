084bc5fa7c4f4e7c6ee4f0e7c651a7f6c6dff299
Lee sang Yeop <mma7710@naver.com>
2023-10-22 01:44:55 +0900

    Feat: 컴퓨터가 서로 다른 임의의 3자리 수를 배열로 만들어서 반환하는 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+	generateRandomNumber() {
+		const generateNumberResponse = Random.pickUniqueNumbersInRange(1, 10, 3);
+		return generateNumberResponse;
+	}
+
+	async play() {}
 }
+
+const app = new App();
 
 export default App;
