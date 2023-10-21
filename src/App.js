9880726e26eab802957b794508c7608afd20ac71
byunjiin <jibyun5808@naver.com>
2023-10-21 17:48:40 +0900

    feat: 기능4.Resrtart 작성

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Console } from "@woowacourse/mission-utils";
 import { random } from "./Random.js";
 import { input } from "./Input.js";
 import { game } from "./Game.js";
-import { restart } from "./Restart.js";
 
 class App {
   async play() {
@@ -10,16 +9,25 @@ class App {
     let computer = random(); // 컴퓨터값(배열)을 랜덤으로 받기
     Console.print(computer);
     while(true){
-      // try{
-        let user = input(await Console.readLineAsync('숫자를 입력해주세요 : ')); // 사용자입력값(문자열) 애러 찾기
-        let result = game(computer, user);
+      try{
+        let user1 = input(await Console.readLineAsync('숫자를 입력해주세요 : ')); // 사용자입력값(문자열) 애러 찾기
+        let result = game(computer, user1);
         Console.print(result)
         if(result==='3스트라이크'){
-          return ('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+          Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+          // 기능4. 게임이 종료되고 게임을 다시 시작할지 여부
+          let user2 = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
+          if(+user2 === 1){
+            computer = random();
+          }else if(+user2 === 2){
+            break;
+          }else{
+            throw new Error();
+          }        
         }
-      // }catch (error){
-        // throw new Error("[ERROR] 숫자가 잘못된 형식입니다."); // 발생하는 그 외 애러
-      // }
+      }catch (error){
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다."); // 발생하는 그 외 애러
+      }
     }
   }
 }
