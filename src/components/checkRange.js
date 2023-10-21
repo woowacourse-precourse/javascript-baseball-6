b79cff5560acf0c6d1bacb1b32f36e69b33611f4
rijfe <kyw42510@naver.com>
2023-10-21 15:33:56 +0900

    Feat: 겹치는 숫자 찾는 기능 추가

--- src/components/checkRange.js
+++ src/components/checkRange.js
@@ -4,6 +4,13 @@ const checkRange = (number) => {
   if (number.length != 3) {
     return false;
   }
+  const set = new Set(number);
+
+  const newNumber = [...set];
+
+  if (newNumber.length < 3) {
+    return false;
+  }
 
   return isNumber(number);
 };
