67200e74edb576678a5a239c123b517b9fe0ff43
osohyun0224 <osohyun0224@naver.com>
2023-10-21 17:25:41 +0900

    Feat: (사용자) 입력한 숫자가 3자리, 1-9 숫자인지 검증 구현
    - 구현 기능 목록에서 숫자 유효성 검사 부분을 구체적으로 수정하였습니다.
    - 사용자가 유효하지 않은 숫자를 입력했을 때 출력할 에러 메세지를 정의하였습니다.
    - 입력한 숫자가 3자리인지 확인하고, 1-9부터 숫자인지 검증하는 기능을 구현하였습니다.
    - 입력한 숫자가 유효하지 않다면, 오류 메시지를 출력하고 예외를 발생시키도록 구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
-const GAME_MESSAGE = require('./constants/Message');
+const { GAME_MESSAGE, ERROR_MESSAGE } = require('./constants/Message');
 const Computer = require('./Computer');
 
 class App {
@@ -11,7 +11,14 @@ class App {
     this.printStartMessage();
     const computerNumber = this.computer.generateNumber();
 
-    const userNumber = await this.getUserInput();
+    let userNumber;
+    do {
+      userNumber = await this.getUserInput();
+      if (!this.isValidNumberInput(userNumber)) {
+        this.printErrorMessage(ERROR_MESSAGE.INVALID_NUMBER);
+        throw new Error(ERROR_MESSAGE.INVALID_NUMBER);
+      }
+    } while (!this.isValidNumberInput(userNumber));
   }
 
   printStartMessage() {
@@ -22,7 +29,23 @@ class App {
     const input = await Console.readLineAsync(GAME_MESSAGE.NUMBER_INPUT);
     return input;
   }
+
+  isValidNumberInput(input) {
+    if (input.length !== 3) {
+      return false;
+    }
+
+    for (let char of input) {
+      if (char < '1' || char > '9') {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  printErrorMessage(message) {
+    Console.print(message);
+  }
 }
 
 export default App;
-
--- src/constants/Message.js
+++ src/constants/Message.js
@@ -2,7 +2,14 @@ const GAME_MESSAGE = Object.freeze({
   GAME_START: '숫자 야구 게임을 시작합니다.',
   NUMBER_INPUT: '숫자를 입력해주세요 : ',
   CORRECT_ANSWER: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
-  GAME_RESTART: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+  GAME_RESTART: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
 })
 
-module.exports = GAME_MESSAGE;
+const ERROR_MESSAGE = Object.freeze({
+  INVALID_NUMBER: '[ERROR] 숫자가 잘못된 형식입니다.'
+})
+
+module.exports = {
+  GAME_MESSAGE,
+  ERROR_MESSAGE
+};
