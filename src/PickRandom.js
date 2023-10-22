82d8885f926797658a0e8bd7cd0a53e855cff115
sayyyho <323psh@naver.com>
2023-10-22 21:33:06 +0900

    Feat: 랜덤값 생성 기능의 재사용을 위한 모듈 수정

--- src/PickRandom.js
+++ src/PickRandom.js
@@ -1,11 +1,18 @@
 import { Random } from "@woowacourse/mission-utils";
 
-const randomNumber = [];
-while (randomNumber.length < 3) {
-  const createdNumber = Random.pickNumberInRange(1, 9);
+const checkNumber = (randomNumber, createdNumber) => {
   if (!randomNumber.includes(createdNumber)) {
     randomNumber.push(createdNumber);
   }
-}
+};
 
-export default randomNumber;
+const pickRandomNumber = () => {
+  const randomNumber = [];
+  while (randomNumber.length < 3) {
+    const createdNumber = Random.pickNumberInRange(1, 9);
+    checkNumber(randomNumber, createdNumber);
+  }
+  return randomNumber;
+};
+
+export default pickRandomNumber;
