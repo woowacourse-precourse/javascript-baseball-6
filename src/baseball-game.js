91f30e9f9f6e0ba038c869296f9c59a955794ab8
lucaseunchae <lucaskim.dev@gmail.com>
2023-10-22 23:55:12 +0900

    feat: add printScore function

--- src/baseball-game.js
+++ src/baseball-game.js
@@ -1,3 +1,5 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 const calculateStrikeCount = (computerNums, answerNums) => {
   return answerNums.filter((answerNum, i) => answerNum === computerNums[i])
     .length;
@@ -18,4 +20,15 @@ export const calculateCounts = (computerNums, answerNums) => {
   const ballCount = calculateBallCount(computerNums, answerNums);
 
   return { strikeCount, ballCount };
+};
+
+export const printScore = (strikeCount, ballCount) => {
+  let result = "";
+
+  if (ballCount) result += `${ballCount}볼 `;
+  if (strikeCount) result += `${strikeCount}스트라이크`;
+
+  result = result.trimEnd();
+
+  MissionUtils.Console.print(result ? result : "낫싱");
 };
