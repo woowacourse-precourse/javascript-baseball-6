fa5a8e385a17e60dadc4f6bda959d45acb9e6c29
Goonco <s07019@naver.com>
2023-10-22 14:36:56 +0900

    :recycle: 2차 구현 : Baseball 클래스 생성
    * 하나의 Baseball 객체는 3쌍의 숫자를 의미
    * 두가지 타입의 생성자 추가
    * Baseball 간의 비교 메소드 추가

---
+++ src/Baseball.js
@@ -0,0 +1,55 @@
+import { Random } from "@woowacourse/mission-utils";
+
+class Baseball {
+  static BASEBALL_LENGTH = 3;
+
+  constructor(baseballString) {
+    this.baseball = [];
+    if (baseballString) {
+      if (this.isInvalidBaseballString(baseballString))
+        throw new Error("[ERROR]");
+      this.setStringToBaseball(baseballString);
+    } else this.setRandomBaseball();
+  }
+
+  setRandomBaseball() {
+    while (this.baseball.length < Baseball.BASEBALL_LENGTH) {
+      let randomNumber = Random.pickNumberInRange(1, 9);
+      if (!this.baseball.includes(randomNumber))
+        this.baseball.push(randomNumber);
+    }
+  }
+
+  setStringToBaseball(baseballString) {
+    for (let i = 0; i < Baseball.BASEBALL_LENGTH; i++) {
+      let number = parseInt(baseballString[i]);
+      this.baseball.push(number);
+    }
+  }
+
+  isInvalidBaseballString(baseballString) {
+    let a = "abc";
+    if (baseballString.length > 3) return true;
+  }
+
+  static compareBaseball(guessBall, answerBall) {
+    const guess = guessBall.baseball;
+    const answer = answerBall.baseball;
+
+    const result = {
+      strike: 0,
+      ball: 0,
+      out: false,
+    };
+
+    for (let i = 0; i < Baseball.BASEBALL_LENGTH; i++) {
+      if (answer[i] === guess[i]) result.strike++;
+      else if (answer.includes(guess[i])) result.ball++;
+    }
+
+    if (result.strike + result.ball === 0) result.out = true;
+    return result;
+  }
+}
+
+export default Baseball;
