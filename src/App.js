17352c02e40ecaf57ceb23f52a332907a2b138ea
baegyeong <beth0929@naver.com>
2023-10-22 23:22:32 +0900

    feat: input, answer 생성자 정의 및 랜덤 숫자 생성
    - MissionUtils 라이브러리 사용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  constructor(input, answer) {
+    this.input = input;
+    this.answer = answer;
+  }
+  async getNumber() {
+    this.answer = MissionUtils.Random.pickNumberInRange(100, 999);
+    return this.answer;
+  }
+
 }
 
 export default App;
