66b3c5479c03b46c070e61b4996a74dbb88acdfb
Dev-steadyBoy <85480086+Dev-steadyBoy@users.noreply.github.com>
2023-10-22 20:43:25 +0900

    feat: [App] App객체 구현
    test failed 발생하지 않는 로직 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
 
 import RandomNumber from '../core/RandomNumber.js';
+import Baseball from '../core/Baseball.js';
+import Exception from '../Exception/Root.js';
+import GameCondition from '../Exception/GameCondition.js';
 
 class App {
-  #basketball;
+  #baseball;
   #exception;
 
   constructor() {
-    this.#basketball;
-    this.#exception;
+    this.#baseball = Baseball;
+    this.#exception = Exception;
   }
 
   // 주어진 문자열을 출력하는 기능
@@ -20,9 +23,9 @@ class App {
     this.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
   }
 
-  play() {
+  async play() {
     this.print('숫자 야구 게임을 시작합니다.');
-    this.enterValue(RandomNumber.createNumber());
+    await this.enterValue(RandomNumber.createNumber());
   }
 
   async enterValue(random) {
@@ -30,17 +33,35 @@ class App {
       const userFeedback = await Console.readLineAsync(
         '숫자를 입력해주세요 : '
       );
-      // userFeedback에 따라 에러 발생하는 로직 (true면 error 발생)
-      // baseball 객체에게 값을 받아야함. 경기 상황을 받아와야함. 그럴려면 랜덤값과 입력값을 전달해야함.(여기로 왔다는 건 랜덤값, 입력값을 보장받은 상태)
 
-      // 만약에 3스트라이크면 게임의 종료와 재시작을 알리는 기능 - Console API를 사용해야함(여기서 함수를 실행하면 콜스택에 함수가 또 올라갈텐데 enterValue 함수의 종료는 어떻게 처리하지?)
-      // async의 재귀는 어떻게 구현하지? 기저조건은?
+      this.#exception.checkAllException(userFeedback);
+      this.print(this.#baseball.announceGameOutcome(random, userFeedback));
+
+      if (this.#baseball.isStrikeOut(random, userFeedback)) {
+        await this.askUserOpinion();
+        return;
+      }
+
+      await this.enterValue(random);
     } catch (error) {
-      console.error('[ERROR]', error);
+      throw new Error(`[ERROR] ${error.message}`);
+    }
+  }
+
+  async askUserOpinion(random) {
+    try {
+      this.end();
+      const ask = await Console.readLineAsync(
+        '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
+      );
+      GameCondition.checkAllError(ask);
+      this.print(ask);
+
+      if (ask === '1') await this.enterValue(RandomNumber.createNumber());
+    } catch (error) {
+      throw new Error(`[ERROR] ${error.message}`);
     }
   }
 }
 
-const app = new App();
-app.play();
 export default App;
