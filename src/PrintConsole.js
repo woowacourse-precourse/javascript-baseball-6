ae3b3c6d71fb5c50438f514392568ed25a5692ad
Lee sang Yeop <mma7710@naver.com>
2023-10-23 00:45:52 +0900

    Fix: 게임 결과 출력 시 0볼 0스트라이크인 경우 낫싱을 출력하도록 수정

--- src/PrintConsole.js
+++ src/PrintConsole.js
@@ -9,6 +9,7 @@ class PrintConsole {
 		let text = '';
 		if (ball) text += `${ball}볼 `;
 		if (strike) text += `${strike}스트라이크`;
+		if (ball === 0 && strike === 0) text += '낫싱';
 
 		Console.print(text);
 	}
