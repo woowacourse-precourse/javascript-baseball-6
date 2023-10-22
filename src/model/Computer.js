65a8d181c31302fc9e6487111a955bd1247bb0de
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-22 23:21:13 +0900

    refactor: [Computer] getter, setter 주석 추가

--- src/model/Computer.js
+++ src/model/Computer.js
@@ -5,10 +5,18 @@ export default class Computer {
     this.number = null;
   }
 
+  /**
+   * Computer 클래스에 저장된 값을 가져옵니다.
+   * @returns [컴퓨터가 설정한 수]
+   */
   getNumber() {
     return this.number;
   }
 
+  /**
+   * computer가 설정한 수를 Computer 클래스에 저장합니다.
+   * @param {string[]} newNumber [컴퓨터가 설정한 수]
+   */
   setNumber(newNumber) {
     this.number = newNumber;
   }
