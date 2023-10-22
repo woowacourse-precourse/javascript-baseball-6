e2aed920bc7396be5035dede3508bc0a565a390d
Jueun Park <84198371+jueun-park98@users.noreply.github.com>
2023-10-22 03:47:37 +0000

    Added validation of user input & edited random number generator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 class App {
   constructor() {
-    this.MIN_VALUE = 1;
+    this.MIN_VALUE = 111;
     this.MAX_VALUE = 999;
   }
 
   async play() {
-    const randomNumber = Random.pickNumberInRange(
-      this.MIN_VALUE,
-      this.MAX_VALUE,
-    ).toString();
-    let strikeCounts = 0;
-    let ballCounts = 0;
-    let isInGame = true;
+    try {
+      const randomNumber = this.generateRandomNumber(this.MIN_VALUE, this.MAX_VALUE);
+      let strikeCounts = 0;
+      let ballCounts = 0;
+      let isInGame = true;
 
-    Console.print("숫자 야구 게임을 시작합니다.");
+      Console.print("숫자 야구 게임을 시작합니다.");
 
-    while (isInGame) {
+      while (isInGame) {
         const number = await Console.readLineAsync("숫자를 입력해주세요. : ");
-        
+
+        if (!this.isInteger(number) || !this.isInRange(number)) {
+          throw new Error("[ERROR] 숫자가 잘못된 형식입니다." + number);
+        }
+      }
+    } catch (error) {
+      await Promise.reject(error);
     }
+  }
+
+  isInteger(string) {
+    return Number.isInteger(Number(string));
+  }
+
+  isInRange(n) {
+    return n <= this.MAX_VALUE && n >= this.MIN_VALUE;
+  }
+
+  generateRandomNumber(min, max){
+    let number = Random.pickNumberInRange(this.MIN_VALUE, this.MAX_VALUE);
+    while ( !number.toString().includes('0') ){
+      number = Random.pickNumberInRange(this.MIN_VALUE, this.MAX_VALUE);
+    }
+    return number;
   }
 }
 
