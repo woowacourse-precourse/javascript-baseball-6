399574adf4e2ba8f345b5fbd2a656298d0bb48f0
domikjs <domikjs@gmail.com>
2023-10-22 18:02:51 +0900

    fix: add console to import

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-import { Random } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 
 export function generateRandomThreeDigits() {
   const answerDigits = [];
