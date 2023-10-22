2807bddba1048de1e075cb0f8030aae554ba72e5
hamo-o <yheel369@gmail.com>
2023-10-23 03:24:40 +0900

    feat: #2 플레이어 입력 숫자 유효성 검사

--- src/App.js
+++ src/App.js
@@ -26,10 +26,37 @@ class App {
     this.#answer = computer;
   }
 
+  isThreeDigit(input) {
+    return input.length === 3;
+  }
+
+  isOneToNine(input) {
+    const pattern = /^[1-9]+$/;
+    return pattern.test(input.join(''));
+  }
+
+  isNotDuplicateNumber = (input) => {
+    const set = new Set(input);
+    return input.length === set.size;
+  };
+
+  checkValidNumber(input) {
+    if (!this.isThreeDigit(input)) {
+      throw '[ERROR] 세자리 숫자가 아닙니다.';
+    }
+    if (!this.isOneToNine(input)) {
+      throw '[ERROR] 1부터 9까지의 숫자가 아닙니다.';
+    }
+    if (!this.isNotDuplicateNumber(input)) {
+      throw '[ERROR] 중복된 자리수가 있습니다.';
+    }
+  }
+
   async getNumber() {
     try {
       const number = await Console.readLineAsync('숫자를 입력해주세요 : ');
       this.#number = [...number];
+      this.checkValidNumber(this.#number);
       Console.print(this.#number);
     } catch (error) {
       throw new Error(error);
