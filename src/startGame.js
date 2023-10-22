d17600e022ed01cb8c52ca3dbe4826e4f88078c4
chestnut1044 <wbk1620@gmail.com>
2023-10-23 00:19:16 +0900

    feat : 숫자야구 로직 구현
    입력된 값과 타깃값을 비교하는 로직 구현 / 결과 값 출력

--- src/startGame.js
+++ src/startGame.js
@@ -7,10 +7,40 @@ export default async function startGame() {
   Console.print(targetNumber);
   while (true) {
     const validInputNumber = await userInputNumber(3);
+    check(targetNumber, validInputNumber);
     if (validInputNumber === targetNumber) {
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       break;
     }
   }
+}
+
+function check(targetNumber, inputNumber) {
+  let ball = 0;
+  let strike = 0;
+  for (const [targetIndex, currentTargetNumber] of Object.entries(
+    targetNumber
+  )) {
+    for (const [inputIndex, currentInputNumber] of Object.entries(
+      inputNumber
+    )) {
+      if (currentTargetNumber === currentInputNumber) {
+        if (targetIndex === inputIndex) {
+          strike += 1;
+          continue;
+        }
+        ball += 1;
+      }
+    }
+  }
+
+  ball === 0 && strike === 0
+    ? Console.print("낫싱")
+    : ball === 0
+    ? Console.print(`${strike}스트라이크`)
+    : strike === 0
+    ? Console.print(`${ball}볼`)
+    : Console.print(`${ball}볼 ${strike}스트라이크`);
 }
 
 // 유효한 값이 입력됐는지 확인
