fa82d6de0ac99b27b66b60a2fa540af2f5fbf94b
shyeon0602 <july8560@naver.com>
2023-10-22 17:31:15 +0900

    feat: 입력받은 숫자를 컴퓨터와 비교 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,66 @@
 class App {
-  async play() {}
+  async play() {
+
+    // 컴퓨터의 랜덤숫자
+    let computer=[];
+    function computerNum(){
+      while(computer.length<3){
+        randomNum=(Math.floor(Math.random()*9));
+        if(computer.indexOf(randomNum)==-1){
+          computer.push(randomNum);
+        }
+      }
+      return computer;
+    }
+
+    let inputNum = [];
+    // inputNum의 유효성 검사
+    function check(input){
+      // 3자리 수인가?
+      if(input.length!=3){
+        return alert('[ERROR] 3자리 숫자를 입력하세요.');
+      }
+      // 중복값이 있는가?
+      else if(new Set(input).size!=3){
+        return alert('[ERROR] 중복되지 않는 3자리 숫자를 입력하세요.');
+      }
+      return inputNum.push(input);
+    }
+    
+    while(check==true){
+      computerNum();
+    }
+
+    do{
+    let strike=0; // 숫자 동일, 인덱스 동일
+    let ball=0; // 숫자 동일, 인덱스 다름
+    let nothing=0; // 숫자 다름, 인덱스 다름
+    
+    let inputStr = prompt('3자리 숫자를 입력하세요.(0~9 사이의 숫자): ');
+    inputNum=Number(inputStr.split(''));
+    inputNum++;
+
+    for(let i=0; i<computer.length; i++){
+      for(let j=0; j<inputNum.length; j++){
+        if(computer[i]==inputNum[j]){
+          if(i==j){
+            strike++;
+          }
+          else{
+            ball++;
+          }
+        } else{
+          nothing++;
+        }
+      }
+    }
+  }
+  while(strike<3);
+  alert('3스트라이크! 게임 종료합니다.')
 }
+}
+
+const app= new App();
+app.play();
 
 export default App;
