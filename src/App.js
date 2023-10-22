bc5337353051ffe08dad5d8ba8119f013d630f75
김진호 <rlawlsgh1227@gmail.com>
2023-10-22 11:40:24 +0900

    feat: Controll인 App 클래스 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import User from "./User.js";
+import Game from "./Game.js";
+import { outputMessage } from "./constants/Message.js";
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const user = new User(); 
+
+    while(true) {
+      const baseball = new Game();
+      baseball.startGame();
+  
+      while(true) {
+        const userThreeNumber = await user.inputNumber();
+        const gameResult = baseball.makeComputerGrade(userThreeNumber);
+
+        user.printGameResult(gameResult);
+
+        if (gameResult === outputMessage.STRIKE_OUT) {
+          break;
+        }
+      }     
+
+      const retryOrEnd = await user.inputRetryOrEnd();
+  
+      if (retryOrEnd === '2') {
+        Console.print("게임 종료");
+        break;
+      }
+    }
+  }
 }
 
 export default App;
