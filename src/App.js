eb26cbd62b724c95fce29060506578b29aa552df
조민기 <58856846+Nail0606@users.noreply.github.com>
2023-10-22 15:35:22 +0900

    노트북 이동용 commit

--- src/App.js
+++ src/App.js
@@ -4,34 +4,32 @@ class App {
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     const answerNumber = this.createAnswerNumber();
-    MissionUtils.Console.print(answerNumber);
     this.compareNumber(answerNumber);
   }
 
-  //사용자가 입력한 숫자에 대해 유효성 검사를 진행하고 반환하는 메서드
+  // 사용자가 입력한 숫자에 대해 유효성 검사를 진행하고 반환하는 메서드
   async createInputNumber() {
-    let inputNumArray = [];
-    try {
-      const userInput = await MissionUtils.Console.readLineAsync(
-        "숫자를 입력해주세요 : "
-      );
-      const userInputSet = new Set(userInput);
-      inputNumArray = userInput.split("").map(Number);
-      if (
-        inputNumArray.length !== userInputSet.size ||
-        inputNumArray.length !== 3 ||
-        Number.isNaN(parseInt(userInput))
-      ) {
-        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
-      } else {
-        return inputNumArray;
-      }
-    } catch (error) {
-      MissionUtils.Console.print(error.message);
+    const userInput = await MissionUtils.Console.readLineAsync(
+      "숫자를 입력해주세요 : "
+    );
+    return userInput;
+  }
+
+  validateAndParseInput(input) {
+    const inputNumArray = input.split("").map(Number);
+    const userInputSet = new Set(input);
+    if (
+      inputNumArray.length !== userInputSet.size ||
+      inputNumArray.length !== 3 ||
+      Number.isNaN(parseInt(input))
+    ) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    } else {
+      return inputNumArray;
     }
   }
 
-  //서로다른 무작위 3개의 수가 들어있는 배열을 반환
+  // 서로 다른 무작위 3개의 수가 들어있는 배열을 반환
   createAnswerNumber() {
     const answerNumArray = [];
     while (answerNumArray.length < 3) {
@@ -44,21 +42,24 @@ class App {
   }
 
   async compareNumber(answer) {
-    let input = await this.createInputNumber();
-    let strike = 0;
-    let ball = 0;
-    for (let i = 0; i < 3; i++) {
-      if (answer[i] === input[i]) {
-        strike++;
-      } else if (answer.includes(input[i])) {
-        ball++;
+    try {
+      const input = await this.createInputNumber();
+      const inputArray = this.validateAndParseInput(input);
+
+      let strike = 0;
+      let ball = 0;
+      for (let i = 0; i < 3; i++) {
+        if (answer[i] === inputArray[i]) {
+          strike++;
+        } else if (answer.includes(inputArray[i])) {
+          ball++;
+        }
       }
-    }
-    if (strike === 3) {
-      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-      this.restartQuestion();
-    } else if (strike !== 0 || ball !== 0) {
-      {
+      if (strike === 3) {
+        MissionUtils.Console.print("3스트라이크");
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        this.restartQuestion();
+      } else if (strike !== 0 || ball !== 0) {
         if (strike === 0) {
           MissionUtils.Console.print(`${ball}볼`);
         } else if (ball === 0) {
@@ -66,11 +67,13 @@ class App {
         } else {
           MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
         }
+        this.compareNumber(answer);
+      } else {
+        MissionUtils.Console.print("낫싱");
+        this.compareNumber(answer);
       }
-      return this.compareNumber(answer);
-    } else {
-      MissionUtils.Console.print("낫싱");
-      return this.compareNumber(answer);
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
     }
   }
 
@@ -81,7 +84,6 @@ class App {
       );
       if (restart === "1") {
         const answerNumber = this.createAnswerNumber();
-        MissionUtils.Console.print(answerNumber);
         this.compareNumber(answerNumber);
       } else if (restart === "2") {
         return;
@@ -93,6 +95,7 @@ class App {
     }
   }
 }
+
 const app = new App();
 app.play();
 
