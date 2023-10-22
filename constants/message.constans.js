44fb1dbd9196b7ce24446ca3f7678a779c9e4dbf
Doeunnkimm <doeunnkimm@gmail.com>
2023-10-22 14:48:44 +0900

    feat: 시작 멘트 출력하고 랜덤 3자리 수 생성 (게임 준비)

---
+++ constants/message.constans.js
@@ -0,0 +1,5 @@
+const MESSAGE = {
+  start: '숫자 야구 게임을 시작합니다.',
+};
+
+export default MESSAGE;
--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import MESSAGE from '../constants/message.constans.js';
+
 class App {
-  async play() {}
+  constructor() {
+    this.matchNumber = 0; // 사용자가 맞추어야 할 숫자
+  }
+
+  /**
+   * 시작 문구를 출력해주는 메서드
+   */
+  startPhrase() {
+    console.log(MESSAGE.start);
+  }
+
+  /**
+   * 사용자가 맞추어야 할 숫자를 세팅하는 메서드
+   */
+  setMatchNumber() {
+    const computer = [];
+    // 3자리 숫자를 세팅
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    this.matchNumber = computer.join('');
+  }
+
+  readyGame() {
+    this.startPhrase();
+    this.setMatchNumber();
+  }
+
+  async play() {
+    this.readyGame();
+  }
 }
 
 export default App;
