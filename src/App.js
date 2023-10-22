10614d29d1f2eef7a64a996bcfe78b1175718b0c
puranium235 <puranium235@gmail.com>
2023-10-22 18:54:19 +0900

    3스트라이크 추가, 메서드 밖으로 throw error하게 변경

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App {
                 if (ball === 0 && strike === 0) {
                     MissionUtils.Console.print("낫싱");
                 } else if (strike === 3) {
-                    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    MissionUtils.Console.print("3스트라이크 \n 3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                     win = true;
                 } else {
                     MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
@@ -61,6 +61,7 @@ class App {
             await game();
         } catch (e) {
             MissionUtils.Console.print(e.message);
+            throw e;
         }
     }
 }
