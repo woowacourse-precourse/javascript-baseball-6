6c8401c800452aab843dd8473fc8dbd93751b8fd
Ivan <ivan@broj.company>
2023-10-23 01:10:27 +0900

    feat : restar함수 구현

--- src/App.js
+++ src/App.js
@@ -18,30 +18,57 @@ class User {
     const numbers = [];
     try {
       if (strNumbers.length !== 3) {
-        throw new Error('3자리 숫자를 입력해주세요');
+        throw new Error('[ERROR] 3자리 숫자를 입력해주세요');
       }
       strNumbers.split('').map((strNumber) => {
         const number = Number(strNumber);
         if (Number.isNaN(number)) {
-          throw new Error('숫자를 입력해주세요');
+          throw new Error('[ERROR] 숫자를 입력해주세요');
         } else if (numbers.includes(number)) {
-          throw new Error('중복되지 않은 숫자를 입력해주세요');
+          throw new Error('[ERROR] 중복되지 않은 숫자를 입력해주세요');
         } else {
           numbers.push(number);
         }
       });
+      this.numbers = numbers;
     } catch (error) {
-      console.log(error.message);
+      throw new Error(error.message);
     }
-
-    this.numbers = numbers;
   }
 }
 class App extends User {
   async play() {
     const computer = new User();
     const user = new User();
+    Console.print('숫자야구게임을 시작합니다.');
     computer.setRandomNumber();
+    while (true) {
+      await user.setInputNumbers();
+      if (user.numbers.length < 3) break;
+      const { strike, ball } = this.compareNumbers(computer, user);
+      if (strike === 3) {
+        Console.print(`${strike}스트라이크`);
+        Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        const choose = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+        if (this.restart(Number(choose))) computer.setRandomNumber();
+        else break;
+      } else if (strike === 0 && ball === 0) {
+        Console.print('낫싱');
+      } else {
+        Console.print(`${ball}볼 ${strike}스트라이크`);
+      }
+    }
+  }
+  restart(number) {
+    try {
+      if (number !== 1 && number !== 2) {
+        throw new Error('[ERROR] 1 또는 2를 입력해주세요');
+      }
+      if (number === 1) return true;
+      return false;
+    } catch (error) {
+      throw new Error(error.message);
+    }
   }
   compareNumbers(computer, user) {
     const initial = {
@@ -59,6 +86,4 @@ class App extends User {
     return { strike, ball };
   }
 }
-const app = new App();
-app.play();
 export default App;
