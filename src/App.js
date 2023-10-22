16cfde075624e5562ee4102bcfcfbccf27c58d78
lbj23han <ljhan0215@gmail.com>
2023-10-22 14:39:48 +0900

    feat: 임의의 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+import { Console, Random, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
+  Get_Random_Number() {
+    const RandomNumbers = [];
+    while (RandomNumbers.length < 3) {
+      const Number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!RandomNumbers.includes(Number)) {
+        RandomNumbers.push(Number);
+      }
+    }
+    return RandomNumbers;
+  }
+
   async play() {}
 }
 
