86bf6fdc25ecdc746442e4b09b9812e29b2c7e8d
kwani6684 <jack0910@naver.com>
2023-10-22 19:55:17 +0900

    ✨ Com 객체 추가 #1 생성자로 랜덤숫자 세팅

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+class Com {
+  constructor() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    this.computer=computer;
+  }
+}
 class App {
   async play() {}
 }
