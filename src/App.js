5386fe63977c69e09e837ecc3d8664f97337bdb6
nagyeongyun <skruddbs99@naver.com>
2023-10-22 15:05:32 +0900

    feat: 서로 다른 3자리 수 추출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
+}
+
+function generateComputerNumber() {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
 }
 
 export default App;
