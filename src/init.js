f1a2badd53adcd36011c808ddcbfd13eb39bae7c
prgmr99 <johnyeom24@naver.com>
2023-10-22 22:06:24 +0900

    [fix] 메모리 누수 해결

--- src/init.js
+++ src/init.js
@@ -4,28 +4,38 @@ let STRIKE = 0;
 let BALL = 0;
 let NOTHING = 0;
 
-const comparePitches = (numbers) => {
-  // const randomNumber = await MissionUtils.Random.pickNumberInRange(111, 999);
-  // err: 랜덤 숫자가 생성 안됨. 계속 1임. 라이브러리 코드 사용
-  // README.md 참고하여 해결
+const exit = () => {
+  MissionUtils.Console.close();
+};
 
+const generateRandomNumber = () => {
   const computer = [];
-  const user = numbers.split("").map((e) => +e);
   while (computer.length < 3) {
     const number = MissionUtils.Random.pickNumberInRange(1, 9);
     if (!computer.includes(number)) {
       computer.push(number);
     }
   }
+  return computer;
+};
+
+const comparePitches = (userNum, computerNum) => {
+  // const randomNumber = await MissionUtils.Random.pickNumberInRange(111, 999);
+  // err: 랜덤 숫자가 생성 안됨. 계속 1임. 라이브러리 코드 사용
+  // README.md 참고하여 해결
+
+  // 유저숫자 -> 숫자배열로 변환
+  const user = userNum.split("").map((e) => +e);
+  console.log(computerNum);
   // 숫자 확인
-  console.log("computer:", computer);
-  console.log("user:", user);
+  // console.log("computer:", computer);
+  // console.log("user:", user);
 
   // 숫자 비교
-  for (let i = 0; i < 3; i++) {
-    console.log(`computer[${i}]:`, computer[i]);
-    console.log(`user[${i}]:`, user[i]);
-    let num = computer.indexOf(user[i]);
+  for (let i = 0; i < user.length; i++) {
+    // console.log(`computer[${i}]:`, computer[i]);
+    // console.log(`user[${i}]:`, user[i]);
+    let num = computerNum.indexOf(user[i]);
     if (num === i) {
       STRIKE++;
     } else if (num !== -1 && num !== i) {
@@ -35,26 +45,24 @@ const comparePitches = (numbers) => {
     }
   }
 
-  console.log(STRIKE, BALL, NOTHING);
-  printResult(STRIKE, BALL, NOTHING);
+  printResult(STRIKE, BALL);
   // 3스트라이크일 때 종료
   if (STRIKE === 3) {
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    askPlay();
-  } else {
-    inputPitches();
+    return askPlay();
   }
+  inputPitches(computerNum);
 };
 
-const printResult = (strike, ball, nothing) => {
-  if (nothing === 3) {
-    MissionUtils.Console.print("낫싱");
-  } else if (strike !== 0 && ball === 0) {
+const printResult = (strike, ball) => {
+  if (strike !== 0 && ball === 0) {
     MissionUtils.Console.print(`${strike}스트라이크`);
   } else if (strike === 0 && ball !== 0) {
     MissionUtils.Console.print(`${ball}볼`);
-  } else {
+  } else if (strike !== 0 && ball !== 0) {
     MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+  } else {
+    MissionUtils.Console.print("낫싱");
   }
 };
 
@@ -64,9 +72,9 @@ const askPlay = () => {
       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
       (num) => {
         if (num === "1") {
-          MissionUtils.Console.print("게임을 새로 시작하겠습니다.");
+          return init();
         } else if (num === "2") {
-          MissionUtils.Console.print("게임을 종료하겠습니다.");
+          return exit();
         } else {
           throw "[ERROR] 숫자가 잘못된 형식입니다.";
         }
@@ -77,17 +85,17 @@ const askPlay = () => {
   }
 };
 
-const inputPitches = async () => {
+const inputPitches = async (computerNum) => {
   STRIKE = BALL = NOTHING = 0;
   try {
     const userInput = await MissionUtils.Console.readLineAsync(
-      "숫자를 입력해주세요 :"
+      "숫자를 입력해주세요"
     );
     if (userInput.length > 3) {
       throw "[ERROR] 숫자가 잘못된 형식입니다.";
     }
-    MissionUtils.Console.print(`숫자를 입력해주세요 : ${userInput}`);
-    comparePitches(userInput);
+    MissionUtils.Console.print(userInput);
+    comparePitches(userInput, computerNum);
   } catch (error) {
     console.log(error);
   }
@@ -95,7 +103,8 @@ const inputPitches = async () => {
 
 const init = () => {
   MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-  inputPitches();
+  const computerNum = generateRandomNumber();
+  inputPitches(computerNum);
 };
 
 export default init;
