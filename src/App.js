7bad28c2b2ee90fa8b6284b847f13e52276404c1
parkgeonwoong <hk77kh@gmail.com>
2023-10-22 17:36:08 +0900

    Init : fork, 콘솔 띄우기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    this.init();
+    try {
+      const userInput = await this.playerInput();
+    } catch (error) {
+      Console.print(error);
+    }
+  }
+
+  init() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  async playerInput() {
+    try {
+      return await Console.readLineAsync("숫자를 입력해주세요 : ");
+    } catch (error) {
+      return Console.print(error);
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
