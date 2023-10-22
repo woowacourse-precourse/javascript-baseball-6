13811f1cf3e9a53c87ebe8f635b257814e47ba95
kimjaehyuk <jhkim0097@gmail.com>
2023-10-22 18:04:16 +0900

    add generateRandomNumbers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-class App {
-  async play() {}
-}
+import { Random, Console } from '@woowacourse/mission-utils';
 
+class App {
+  constructor() {
+    this.answerNumbers = [];
+  }
+
+  generateRandomNumbers(length) {
+    const randomNumbers = [];
+    while (randomNumbers.length < length) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!randomNumbers.includes(number)) {
+        randomNumbers.push(number);
+      }
+    }
+    return randomNumbers;
+  }
+
+  async play() {
+    this.answerNumbers = this.generateRandomNumbers(3);
+    const input = await Console.readLineAsync('input : ');
+    Console.print('ouput : '+ input);
+    console.log(this.answerNumbers);
+  }
+}
+const app = new App();
+app.play();
 export default App;
