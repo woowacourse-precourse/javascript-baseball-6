65edcba4bcaae40f9ff7c96b3663541eb540d5fb
useon <yoosk4986@naver.com>
2023-10-22 18:11:01 +0900

    feat: 1~9까지의 서로 다른 수 3개를 뽑는 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 class App {
   async play() {
     this.infoPrint('숫자 야구 게임을 시작합니다.');
+    this.computerNumber();
   }
 
   infoPrint(message) {
     MissionUtils.Console.print(message);
+  }
+
+  computerNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
   }
 }
 
