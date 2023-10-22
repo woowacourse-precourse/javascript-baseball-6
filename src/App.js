46ac336c29e34a804cb73ce6f7e9c436f611a3e4
Mungjin01 <beckaljm@naver.com>
2023-10-22 10:46:14 +0900

    feat: 사용자 입력값 검증

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async play() {
     const computerAnswer = this.generateRandomBallNumber();
-    MissionUtils.Console.print(computerAnswer)
+    //MissionUtils.Console.print(computerAnswer);
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.readLine('숫자를 입력해주세요: ', (answer) => {
+      if(!this.checkAnswer(answer)){
+        throw new Error();
+      }
+      //console.log(`${answer}`);
+    })
+
   }
 
   generateRandomBallNumber(){
     const computerAnswer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     return computerAnswer.join('');
+  }
+  checkAnswer(answer){
+    if(answer.length !==3){
+      return false;
+    }
+    if(new Set(answer).size !==3){
+      return false;
+    }
+    return true;
   }
 }
 
