e96911f40f2c39676db342d52e2b2c8e45de259a
00kang <kdb070970@gmail.com>
2023-10-22 14:48:13 +0900

    feat: 사용자에게 숫자 입력받기

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ class App {
     const endInclusive = 9;
     const numberLength = 3;
     let computerNum = "";
+    let userNum = "";
 
     try {
       const computerNumbers = [];
 
       while (computerNumbers.length < numberLength) {
         const number = Random.pickNumberInRange(startInclusive, endInclusive);
-        // Random.pickNumberInRange 로 랜덤 값 추출
+        // Random.pickNumberInRange 으로 랜덤 값 추출
         if (!computerNumbers.includes(number)) {
           // computerNumber에 있는 숫자와 중복되지 않은 숫자를 추가
           computerNumbers.push(number);
@@ -26,8 +27,12 @@ class App {
       }
       console.log("선택된 서로 다른 수:", computerNumbers);
       console.log("컴퓨터가 선택한 3자리 수:", computerNum);
+
+      const userInput = await Console.readLineAsync("3자리 숫자를 입력하세요 (1~9): ");
+      //Console.readLineAsync 으로 사용자 입력 값 받기
+      userNum = userInput.trim(); //양쪽 끝의 공백을 제거한 새로운 문자열을 반환
+      console.log("사용자가 선택한 3자리 수:", userNum);
     } catch (error) {
-      // console.error("에러 발생:", error.message);
       Console.print("에러 발생:", error.message);
     }
   }
