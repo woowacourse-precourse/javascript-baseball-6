12b5486cbcc88292f24b1db581d6a11391ced61a
Doeunnkimm <doeunnkimm@gmail.com>
2023-10-22 15:19:21 +0900

    feat: input에 따라 힌트를 출력하는 메서드 정의

--- constants/message.constans.js
+++ constants/message.constans.js
@@ -1,5 +1,10 @@
 const MESSAGE = {
   start: '숫자 야구 게임을 시작합니다.',
+  hint: {
+    ball: '볼',
+    strike: '스트라이크',
+    nothing: '낫싱',
+  },
 };
 
 export default MESSAGE;
--- src/App.js
+++ src/App.js
@@ -28,13 +28,69 @@ class App {
     this.matchNumber = computer.join('');
   }
 
+  /**
+   * 입력에 따라 힌트를 반환하는 메서드
+   */
+  inputToHint(number) {
+    const matching = { ball: 0, strike: 0 };
+
+    const matchingNumberArray = [...this.matchNumber];
+    const inputNumberArray = [...String(number)];
+
+    inputNumberArray.forEach((num, index) => {
+      // 스트라이크인 경우
+      if (
+        matchingNumberArray.includes(num) &&
+        matchingNumberArray.indexOf(num) === index
+      ) {
+        matching.strike += 1;
+      }
+
+      // 볼인 경우
+      if (
+        matchingNumberArray.includes(num) &&
+        matchingNumberArray.indexOf(num) !== index
+      ) {
+        matching.ball += 1;
+      }
+    });
+
+    // 같은 수가 전혀 없는 경우 -> 낫싱
+    if (!matching.ball && !matching.strike) {
+      console.log(MESSAGE.hint.nothing);
+      return;
+    }
+
+    // 볼과 스트라이크 둘 다 있을 때 -> N볼 M스트라이크
+    if (matching.ball && matching.strike) {
+      console.log(
+        `${matching.ball}${MESSAGE.hint.ball} ${matching.strike}${MESSAGE.hint.strike}`,
+      );
+      return;
+    }
+
+    // 볼만 있을 경우 -> N볼
+    if (matching.ball && !matching.strike) {
+      console.log(`${matching.ball}${MESSAGE.hint.ball}`);
+      return;
+    }
+
+    // 스트라이크만 있을 경우 -> M볼
+    if (matching.strike && !matching.ball) {
+      console.log(`${matching.strike}${MESSAGE.hint.strike}`);
+    }
+  }
+
   readyGame() {
     this.startPhrase();
     this.setMatchNumber();
   }
 
+  playingGame() {}
+
   async play() {
     this.readyGame();
+    this.inputToHint(356); // 확인용
   }
 }
 
