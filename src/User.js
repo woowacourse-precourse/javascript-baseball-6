ff5ddd46eeffc660a4d6b1797b1f4f8ba52cb7ad
yu-retel <dbdudals100@naver.com>
2023-10-23 01:34:56 +0900

    refactor: User Class 생성(입력받고 유효성체크)

--- src/User.js
+++ src/User.js
@@ -1,6 +1,29 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import ErrorHandler from './Error';
+
 class User {
   constructor() {
+    this.error = new ErrorHandler;
+  }
 
+  async progressInput() {
+    try {
+      const userInput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+      this.error.baseballNumberCheck(userInput);
+      return userInput;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async resetInput() {
+    try {
+      const userInput = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
+      this.error.resetNumberCheck(userInput);
+      return userInput;
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
