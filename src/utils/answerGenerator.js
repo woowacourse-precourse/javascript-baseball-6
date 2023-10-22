da436c26dcda688e7314c5d3fb7ffdf55f3d6ab2
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-10-23 03:16:07 +0900

    feat: 정답을 생성하는 유틸함수 추가

---
+++ src/utils/answerGenerator.js
@@ -0,0 +1,17 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { BASEBALL_NUMBER } from '../constants/gameConfig.js';
+
+export const answerGenerator = () => {
+  const numberSet = new Set();
+
+  while (numberSet.size < BASEBALL_NUMBER.DIGIT) {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(
+      BASEBALL_NUMBER.MIN,
+      BASEBALL_NUMBER.MAX
+    );
+
+    numberSet.add(randomNumber);
+  }
+
+  return [...numberSet];
+};
