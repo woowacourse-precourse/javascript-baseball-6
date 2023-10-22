034fecf6af138fb65c13e2d64b141b8dbab25c3b
Jueun Park <38892644+Juu98@users.noreply.github.com>
2023-10-22 03:15:29 +0000

    Revert "Added random number generator"
    This reverts commit b85f7263d58f31ef21af5f091fea19cbcf23d4ef.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
 class App {
-  constructor() {
-    this.MIN_VALUE = 1;
-    this.MAX_VALUE = 999;
-  }
-
-  async play() {
-    const randomNumber = Random.pickNumberInRange(
-      this.MIN_VALUE,
-      this.MAX_VALUE,
-    ).toString();
-    let strikeCounts = 0;
-    let ballCounts = 0;
-    let isInGame = true;
-
-    Console.print("숫자 야구 게임을 시작합니다.");
-  }
+  async play() {}
 }
 
 export default App;
