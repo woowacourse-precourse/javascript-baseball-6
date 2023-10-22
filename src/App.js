77d8a87e98c3e38ce64bf88c50dddc755894c60e
Mungjin01 <beckaljm@naver.com>
2023-10-22 22:41:54 +0900

    feat: 스트라이크와 볼의 개수 구하기

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async play() {
     const computerAnswer = this.generateRandomBallNumber();
-    //MissionUtils.Console.print(computerAnswer);
+    MissionUtils.Console.print(computerAnswer);
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     MissionUtils.Console.readLine('숫자를 입력해주세요: ', (answer) => {
       if(!this.checkAnswer(answer)){
         throw new Error();
       }
+      this.checkAllStrike(computerAnswer, answer);
       //console.log(`${answer}`);
     })
 
@@ -18,6 +19,7 @@ class App {
     const computerAnswer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     return computerAnswer.join('');
   }
+
   checkAnswer(answer){
     if(answer.length !==3){
       return false;
@@ -27,6 +29,27 @@ class App {
     }
     return true;
   }
+
+  checkAllStrike(computerAnswer, answer){
+    if(computerAnswer === answer) {
+      MissionUtils.Console.print('3스트라이크');
+    }
+
+    let strike = 0;
+    let ball = 0;
+    for(let i = 0; i < computerAnswer.length; i++) {
+      const index = answer.indexOf(computerAnswer[i]);
+      if(index > -1){
+        if(index === i){
+          strike += 1;
+        } else {
+          ball += 1;
+        }
+      }
+    }
+    MissionUtils.Console.print(`스트라이크: ${strike} 볼: ${ball} `);
+  }
+
 }
 
 const init = new App();
