a12348a4994c8df16766144fc8ae11a3b7dda513
김진호 <rlawlsgh1227@gmail.com>
2023-10-22 17:03:19 +0900

    test: 재시작, 종료 테스트에서 숫자가 한 자리가 아닌 경우 테스트 추가

--- __tests__/UserInputTest.js
+++ __tests__/UserInputTest.js
@@ -60,6 +60,16 @@ describe("사용자 입력을 검증합니다", () => {
     await expect(user.inputRetryOrEnd()).rejects.toThrow("[ERROR]");
   });
 
+  test("숫자가 한 자리가 아닌 경우 에러를 발생시킵니다", async () => {
+    // given
+    const input = "12";
+    mockQuestions(input);
+
+    // when & then
+    const user = new User();
+    await expect(user.inputRetryOrEnd()).rejects.toThrow("[ERROR]");
+  });
+
   test("숫자가 아닌 다른 문자가 들어갈 경우 에러를 발생시킵니다", async () => {
     // given
     const input = "공습경보";
