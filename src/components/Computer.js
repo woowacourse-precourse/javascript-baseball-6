875aea24c76447722043f4c0deacd9ea3e637f56
JUN <gurwns232@naver.com>
2023-10-23 00:25:12 +0900

    feat: 컴퓨터가 서로 다른 3개의 수 정하는 기능 추가

--- src/components/Computer.js
+++ src/components/Computer.js
@@ -1,10 +1,18 @@
+import { Random } from '@woowacourse/mission-utils';
+
 class Computer {
   constructor() {
     this.computer = [];
   }
 
   getNumber() {
+    while (this.computer.length < 3) this.createRandomNumber();
     return this.computer;
+  }
+
+  createRandomNumber() {
+    const number = Random.pickNumberInRange(1, 9);
+    if (!this.computer.includes(number)) this.computer.push(number);
   }
 }
 
