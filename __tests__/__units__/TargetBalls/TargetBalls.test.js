f9529c2b04eb0bd541c7c73840cf7d88f6c2e601
cobocho <rlaalsrb1111@naver.com>
2023-10-22 13:53:43 +0900

    Test: `TargetBalls` 테스트 코드 작성

---
+++ __tests__/__units__/TargetBalls/TargetBalls.test.js
@@ -0,0 +1,11 @@
+import { TargetBall, TargetBalls } from '../../../src/domain';
+
+describe('Balls 테스트', () => {
+  it.each([{ input: [1, 2] }])(
+    '입력받은 배열에 따라 `numbers` 필드에 `TargetBall`로 이루어진 배열을 가진다.',
+    ({ input }) => {
+      const balls = TargetBalls.of(input);
+      expect(balls.numbers).toEqual(input.map(TargetBall.valueOf));
+    },
+  );
+});
