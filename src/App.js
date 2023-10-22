7034e47aca0fe86fc8f45c88922a9d08af13c9e1
Hyunji Seok <alleyji126@gmail.com>
2023-10-22 13:40:13 +0900

    1. ball & strike count  2. error msg add

--- src/App.js
+++ src/App.js
@@ -16,12 +16,21 @@ class App {
     try { 
       user = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
 
+      // 예외 처리
       if (!Number(user)) {
         throw new Error("숫자 이외의 문자는 입력하실 수 없습니다.");
       }
 
+      if (Number(user) < 0) {
+        throw new Error("음수는 입력하실 수 없습니다.");
+      }
+
+      if (user.split("").includes("0")) {
+        throw new Error("숫자 0은 입력하실 수 없습니다.")
+      }
+
       if (user.length !== 3) {
-        throw new Error("세 자리 숫자만 입력이 가능합니다.");
+        throw new Error("세 자리 숫자만 입력하실 수 있습니다.");
       }
 
       if (user.split("").length !== [...new Set(user.split(""))].length) {
@@ -32,7 +41,37 @@ class App {
       return;
     }
 
-    MissionUtils.Console.print(`사용자 입력 : ${user}`);
+    // 숫자 값 비교
+    const DIGIT = 3;
+    let ball = 0;
+    let strike = 0;
+
+    user = user.split("").map(num => num = Number(num));
+
+    for (let i = 0; i < DIGIT; i++) {
+      if (user[i] === computer[i]) {
+        strike = strike + 1;
+      }
+
+      if (user[i] !== computer[i] && computer.includes(user[i])) {
+        ball = ball + 1;
+      }
+    }
+
+    let result = "낫싱";
+    if (ball > 0 && strike > 0) {
+      result = `${ball}볼 ${strike}스트라이크`;
+    }
+
+    if (ball === 0 && strike > 0) {
+      result = `${strike}스트라이크`;
+    }
+
+    if (ball > 0 && strike === 0) {
+      result = `${ball}볼`;
+    }
+
+    MissionUtils.Console.print(result);
   }
 }
 
