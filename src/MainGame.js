23f6394022d957dac525d0f2255ad60d0fe0b10d
SU_VIN <pyotnqls25@naver.com>
2023-10-22 12:49:01 +0900

    Feat: 컴퓨터가 임의의 3개의 수 선택 기능 구현

--- src/MainGame.js
+++ src/MainGame.js
@@ -5,12 +5,12 @@ class MainGame {
   //랜덤숫자 선택 후 시작 문구 프린트
   start = () => {
     this.computer = new Computer();
+    this.computerSelectNumber = this.computer.createRandomNumber();
     this.printStart();
   };
 
   printStart = () => {
     Console.print("숫자 야구 게임을 시작합니다.");
-    this.getUserInput();
   };
 }
 
