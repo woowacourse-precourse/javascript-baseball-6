6d2a1ef9989e28c6b2048296411583746fce325d
hyj5076 <hyj5076@hanmail.net>
2023-10-23 02:03:44 +0900

    feat(App): 게임 시작과 종료 선택 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import RandomNumberMaker from '../src/play/RandomNumberMaker';
-import UserNumberReader from '../src/play/UserNumberReader';
+import { MissionUtils } from "@woowacourse/mission-utils";
+import RandomNumberMaker from './play/RandomNumberMaker';
+import UserNumberReader from './play/UserNumberReader';
 import { correctNumber } from './Utils/CorrectNumber';
-import * as Constants from '../const/Messages';
+import * as Constants from './const/Messages';
 import { showResult } from './play/GameResult';
+const Console = MissionUtils.Console;
 
 class App {
   constructor() {
@@ -12,28 +14,36 @@ class App {
 
   async play() {
     try {
-      Console.print(Constants.START_MESSAGE); // 게임 시작
+      Console.print(Constants.GAME_START); // 게임 시작
       const uniqueNumber = this.maker.makeRandomNumber();
 
       while (true) {
-        Console.print(Constants.NOTICE_INPUT);
         const userNumber = await Console.readLineAsync(); // 사용자 수 읽기
         if (!correctNumber(userNumber)) {
-          throw new Error(Constants.WRONG_NUMBER); // 예외 발생
+          throw new Error("[ERROR]"); // 예외 발생
         }
 
         this.reader.setUserNumber(userNumber);
         const userAnswer = this.reader.getUserNumber();
-        Console.print(`사용자의 숫자: ${userAnswer}`);
+        Console.print(`${Constants.NOTICE_INPUT} : ${userAnswer}`);
         
         showResult(uniqueNumber, userAnswer); // 결과 문구
 
         if (this.isGameOver(uniqueNumber, userAnswer)) { 
-          break;
+          Console.print(Constants.GOAL);
+          Console.print(Constants.SUGGEST_REPLAY);
+          const userChoice = await Console.readLineAsync();
+          if (userChoice === '1') {
+            return this.play(); // 새로운 게임 시작
+          } else if (userChoice === '2') {
+            Console.print(Constants.GAME_OVER);
+            return; // 게임 종료
+          }
         }
       }
     } catch (error) {
-      Console.print(Constants.ERROR_MESSAGE); // 에러 메시지 출력
+      Console.print(Constants.GAME_OVER);
+      return Promise.reject(error);
     }
   }
 
