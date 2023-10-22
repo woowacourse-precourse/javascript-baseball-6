d41ac2524201105848cd2da0dbf34c481cf0f083
김진호 <rlawlsgh1227@gmail.com>
2023-10-22 11:37:24 +0900

    feat: 게임에서 사용할 메시지 상수화
    message, outputMessage, MAX_LENGTH를 미리 정의했습니다.

--- src/constants/Message.js
+++ src/constants/Message.js
@@ -2,7 +2,7 @@ export const message = {
   START_GAME: '숫자 야구 게임을 시작합니다.',
   INPUT_NUMBER: '숫자를 입력해주세요 : ',
   END_GAME: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
-  QUEST_RETRY_GAME: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+  QUEST_RETRY_GAME: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
 }
 
 export const outputMessage = {
@@ -14,6 +14,8 @@ export const outputMessage = {
   ONE_BALL_TWO_STRIKE: '1볼 2스트라이크',
   TWO_BALL_ONE_STRIKE: '2볼 1스트라이크',
   ONE_STRIKE: '1스트라이크',
-  THO_STRIKE: '2스트라이크',
-  THREE_STRIKE: '3스트라이크'
+  TWO_STRIKE: '2스트라이크',
+  STRIKE_OUT: '3스트라이크',
 }
+
+export const MAX_LENGTH = 3;
