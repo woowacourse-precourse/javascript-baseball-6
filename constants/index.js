360958f295ace6239e77b21b25387b2990d16df9
Kled <1017yu@daum.net>
2023-10-22 13:38:19 +0900

    refactor: 상수 기능별 목적에 맞게 세분화

--- constants/index.js
+++ constants/index.js
@@ -1,21 +1,24 @@
-const constants = Object.freeze({
-  MESSAGES: {
-    START: '숫자 야구 게임을 시작합니다.',
-    END: '게임을 종료합니다.',
-    INPUT: '숫자를 입력해주세요 : ',
-    PLAYER_WIN: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
-    PLAY_AGAIN: `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`,
-    ERROR: {
-      INVALID_COUNT_NUMBER: '[ERROR] 서로 다른 3자리의 수를 입력해주세요.',
-      INVALID_REPLAY_NUMBER: '[ERROR] 1 또는 2를 입력해주세요.',
-    },
-    BALL_COUNTS: {
-      BALL: '볼',
-      STRIKE: '스트라이크',
-      NOTHING: '낫싱',
-    },
-  },
+const GUIDE_MESSAGES = Object.freeze({
+  START: '숫자 야구 게임을 시작합니다.',
+  END: '게임을 종료합니다.',
+  INPUT: '숫자를 입력해주세요 : ',
+  PLAYER_WIN: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  PLAY_AGAIN: `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`,
+});
+
+const ERROR_MESSAGES = Object.freeze({
+  INVALID_COUNT_NUMBER: '[ERROR] 서로 다른 3자리의 수를 입력해주세요.',
+  INVALID_REPLAY_NUMBER: '[ERROR] 1 또는 2를 입력해주세요.',
+});
+
+const PATTERN = Object.freeze({
   THREE_DIGIT_PATTERN: new RegExp(/^([1-9])(?!.*\1)([1-9])(?!.*\1|\2)([1-9])$/),
 });
 
-export default constants;
+const BALL_COUNTS = Object.freeze({
+  BALL: '볼',
+  STRIKE: '스트라이크',
+  NOTHING: '낫싱',
+});
+
+export { GUIDE_MESSAGES, ERROR_MESSAGES, PATTERN, BALL_COUNTS };
