5211112f420a597a50c92d84d9f78ccbc97952ec
blcklamb <whenucan35@gmail.com>
2023-10-22 23:19:22 +0900

    feat: game process(pass test 2/2) - seperate input, output - #getResult: get ball, strike count per user answer - set resolve, reject

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
+  static async input(query) {
+    return await MissionUtils.Console.readLineAsync(query);
+  }
+
+  static output(message) {
+    return MissionUtils.Console.print(message);
+  }
+
   static #pickRandomNumber() {
     const computer = [];
     while (computer.length < 3) {
@@ -11,7 +19,7 @@ class App {
     return computer.join("");
   }
 
-  static isProperNumber(num) {
+  static #isProperNumber(num) {
     const regex = /^([1-9][^1-9]*[1-9][^1-9]*[1-9])$/g;
     if (!regex.test(num)) return false;
 
@@ -21,42 +29,59 @@ class App {
     return true;
   }
 
-  async play() {
-    const _log = MissionUtils.Console.readLineAsync;
-
-    const isStart = await _log(
-      `숫자 야구 게임입니다.\n게임을 시작하려면 1, 종료하려면 2를 입력하세요.`
-    );
-
-    if (isStart == 1) {
-      const target = App.#pickRandomNumber();
-      await _log(`숫자 야구 게임을 시작합니다.`);
-      await _log(`컴퓨터 숫자는 ${target}`);
-
-      while (true) {
-        const userNumber = await _log("숫자를 입력해주세요 : ");
-        if (App.isProperNumber(userNumber)) {
-          if (userNumber === target) {
-            const restart = await _log(
-              `3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`
-            );
-            if (restart == 1) {
-              this.play();
-            }
-            throw new Error("[ERROR] 야구 게임 종료");
-          } else {
-          }
-        } else {
-          throw new Error("[ERROR] 올바른 숫자 형식이 아닙니다.");
-        }
+  static #getResult(target, num) {
+    let ball = 0;
+    let strike = 0;
+    num.split("").forEach((ele, idx) => {
+      if (target.split("").includes(ele)) {
+        if (ele === target[idx]) strike += 1;
+        else ball += 1;
       }
-    } else {
-      throw new Error("[ERROR] 야구 게임 종료");
+    });
+    if (ball === 0 && strike === 0) return "낫싱";
+    if (ball) {
+      if (strike) return `${ball}볼 ${strike}스트라이크`;
+      return `${ball}볼`;
     }
+    return `${strike}스트라이크`;
+  }
+
+  static proStartGame() {
+    const target = App.#pickRandomNumber();
+    // App.output(`컴퓨터 숫자는 ${target}`);
+
+    return new Promise((resolve, reject) => {
+      const getNumber = () => {
+        App.input("숫자를 입력해주세요 : ").then((res) => {
+          if (App.#isProperNumber(res)) {
+            if (target === res) {
+              App.output("3스트라이크");
+              App.input(
+                `3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`
+              ).then((restart) => {
+                if (restart == 1) resolve(App.proStartGame());
+                else resolve(App.output("게임 종료"));
+              });
+            } else {
+              Promise.resolve(App.output(App.#getResult(target, res))).then(
+                getNumber
+              );
+            }
+          } else {
+            App.output("[ERROR] 올바른 숫자 형식이 아닙니다.");
+            reject(new Error("[ERROR] 올바른 숫자 형식이 아닙니다."));
+          }
+        });
+      };
+      getNumber();
+    });
+  }
+
+  async play() {
+    App.output(`숫자 야구 게임을 시작합니다.`);
+
+    await App.proStartGame();
   }
 }
-
-const app = new App();
-app.play();
 
 export default App;
