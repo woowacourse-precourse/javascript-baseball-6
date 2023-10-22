e6360928d3c5bc663dc88b4b3b9aeb07258ac5c2
ceh20002@naver.com <ceh20002@naver.com>
2023-10-23 00:13:33 +0900

    Docs 수정 및 사용자 입력 확인 기능 구현

---
+++ __tests__/FunctionTest.js
@@ -0,0 +1,3 @@
+const App = require('../src/App');
+const MissionUtils = require('@woowacourse/mission-utils');
+
--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  constructor(){
+    this.count = '';
+    this.input;
+    this.countResult;
+  }
+  async play() { 
+    await this.getUserInput();
+    await this.checkInput(this.input);
+  }
+
+  async getUserInput() {
+    const userInput = await MissionUtils.Console.readLineAsync('닉네임을 입력해주세요.');
+    this.input = userInput;
+    return this.input;
+  }
+
+  checkInput(input){
+    if (!this.isThreeNumber(input)) {
+      throw new Error("Is Not a Number");
+    }
+    if (!this.isSameNumber(input)){
+      throw new Error("Is Duplicate Input");
+    }
+  }
+
+  isThreeNumber(input){
+    return /[0-9]{3}/g.test(input);
+  }
+
+  isSameNumber(input){
+    const checkSame = input.split('').map(Number);
+    const compareSame = new Set(checkSame);
+    return (checkSame.length === compareSame.size);
+  }
 }
 
 export default App;
+
+const start = new App();
+start.play();
