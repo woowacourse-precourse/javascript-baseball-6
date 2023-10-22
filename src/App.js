572b2d4d0e4ea1cf74ad1bc8928075316e842351
f0ever0 <thddndud0830@gmail.com>
2023-10-23 01:30:50 +0900

    feat : startGame

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import { ERROR } from "./lib/constants/error.js";
+import { MESSAGE } from "./lib/constants/message.js";
+import { WORD } from "./lib/constants/word.js";
+
 class App {
-  async play() {}
+  async play() {
+    this.startGame();
+  }
+
+  startGame() {
+    Console.print(MESSAGE.START);
+  }
 }
 
-export default App;
+const app = new App();
+app.play();
+
+module.exports = App;
