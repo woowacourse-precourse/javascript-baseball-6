04cf6d8c6d823d6f9bac58038fffe44f3a65f4c9
1eecan <busyx2modernsociety@gmail.com>
2023-10-22 22:48:44 +0900

    feat: app.play() 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
+import { Console } from "@woowacourse/mission-utils";
+import numberGenerator from "./numberGenerator.js";
+import strike from "./strike.js";
+import ball from "./ball.js";
+import validateNumber from "./validateNumber.js";
+
 class App {
-  async play() {}
+  async play() {
+    let restart = "1";
+    let computer = numberGenerator();
+
+    Console.print("숫자 야구 게임을  시작합니다.");
+
+    while (restart === "1") {
+      const user_input = await Console.readLineAsync("숫자를 입력해주세요 : ");
+      if (!validateNumber(user_input)) {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      }
+
+      const ball_count = ball(user_input, computer);
+      const strike_count = strike(user_input, computer);
+      let message = ball_count + strike_count;
+
+      if (message == "") {
+        message = "낫싱";
+      }
+
+      if (strike_count == "3스트라이크") {
+        Console.print(strike_count);
+        Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        restart = await Console.readLineAsync("");
+
+        if (restart === "1") {
+          computer = numberGenerator();
+          Console.print("숫자 야구 게임을  시작합니다.");
+        } else if (restart !== "2") {
+          throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+        }
+      } else {
+        Console.print(message);
+      }
+    }
+  }
 }
 
 export default App;
