fd93ba0b109095f1385d64580a9bef35d8eb7739
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-10-22 17:26:53 +0900

    feat: 같은 숫자 입력하면 에러 표시

--- src/Player.js
+++ src/Player.js
@@ -2,7 +2,7 @@ import Message from "./Message.js";
 
 class Player {
   constructor() {
-    this.number = "111";
+    this.number = "123";
   }
 
   get number() {
@@ -10,10 +10,10 @@ class Player {
   }
 
   set number(value) {
-    const regex = new RegExp(/[1-9]/);
+    const regex = new RegExp(/^(?!.*(.).*\1)[1-9]{3}$/);
 
     Message.logIf(
-      value.length !== 3 || !regex.test(value),
+      !regex.test(value),
       "1부터 9까지 서로 다른 숫자 3개를 입력하세요."
     );
     this._number = value;
