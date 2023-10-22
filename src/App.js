848faf3ae5e7b94a2070f620f6a021160d676531
yebin <yebind3308@gmail.com>
2023-10-22 18:07:19 +0900

    docs: 구현 기능 작성 (수정)

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    MissionUtils.Console.print("게임 종료");
   }
+
 }
 
 export default App;
