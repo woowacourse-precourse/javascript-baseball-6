a7e880cfd2fdcafa2ba242275c83a6598749467a
Doeunnkimm <doeunnkimm@gmail.com>
2023-10-22 21:55:21 +0900

    feat: 반복해서 입력을 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from '@woowacourse/mission-utils';
+import getRandomArray from '../utils/getRandomArray.js';
+import printMsg from '../utils/printMsg.js';
+
 class App {
-  async play() {}
+  constructor() {
+    this.computer = getRandomArray(); // 사용자가 맞추어야 할 숫자
+    this.isPlaying = false;
+  }
+
+  async play() {
+    printMsg('숫자 야구 게임을 시작합니다.');
+    this.isPlaying = true; // 게임 시작
+
+    while (this.isPlaying) {
+      await Console.readLineAsync('숫자를 입력해주세요 : ');
+    }
+  }
 }
 
 export default App;
