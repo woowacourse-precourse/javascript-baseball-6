5eda8f4c7f96704b4649a5164c06db7e632145a4
zxxng <jeayeong123@naver.com>
2023-10-22 19:06:25 +0900

    feat: 플레이어 숫자의 점수를 반환하는 기능 추가

--- src/classes/GameEngine.js
+++ src/classes/GameEngine.js
@@ -1,4 +1,5 @@
 class GameEngine {
+  // 입력값을 배열로 변경하는 함수
   static inputToArray(input) {
     const playerNumber = input.toString().split('');
     if (playerNumber.length !== 3) {
@@ -9,6 +10,7 @@ class GameEngine {
     return playerNumber;
   }
 
+  // 문자배열을 숫자배열로 변경하는 함수
   static stringToNumber(playerNumber) {
     playerNumber.map((x) => {
       const num = Number(x);
@@ -19,6 +21,7 @@ class GameEngine {
     });
   }
 
+  // 플레이어 숫자가 유효한지 판별하는 함수
   static playerNumberValidity(playerNumber) {
     playerNumber.forEach((x) => {
       if (x < 1 || x > 9) {
@@ -28,6 +31,32 @@ class GameEngine {
         throw new Error('[ERROR] 중복값이 있습니다.');
       }
     });
+  }
+
+  // 플레이어 숫자의 점수를 반환하는 함수
+  static countPlayerScore(computerNumber, playerNumber) {
+    let ball = 0;
+    let strike = 0;
+
+    playerNumber.forEach((x, i) => {
+      if (computerNumber[i] === x) {
+        strike += 1;
+      } else if (computerNumber.includes(x)) {
+        ball += 1;
+      }
+    });
+
+    if (ball === 0 && strike === 0) {
+      return '낫싱';
+    }
+    let message = '';
+    if (ball > 0) {
+      message += `${ball}볼 `;
+    }
+    if (strike > 0) {
+      message += `${strike}스트라이크`;
+    }
+    return message.trim();
   }
 }
 
