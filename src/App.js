e8fff76bfec40282d17e8a38e2631d804ac09d43
kelvin3476 <winrain03226@gmail.com>
2023-10-22 23:30:39 +0900

    feat(App): 구현해야하는 기능 단위별 주석 추가

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
+import * as MissionUtils from '@woowacourse/mission-utils';
+
+// TODO: Mission 1: 메세지 출력 함수 구현
+
+// TODO: Mission 2: 메세지 입력 함수 구현
+
+// TODO: Mission 3: 상대방(컴퓨터)의 3자리 랜덤 숫자 생성 함수 구현
+
+// TODO: Mission 4: 사용자(인터페이스)가 입력한 수를 리스트로 출력하는 함수 구현
+
+// TODO: Mission 5: 스트라이크, 볼 계산 함수 구현
+
+// TODO: Mission 6: 스트라이크, 볼 계산 후 문구 출력하는 함수 구현
+
+// TODO: Mission 7: 게임 재시작 또는 종료 처리 함수 구현
+
+// TODO: Mission 8: "3스트라이크"이면, 게임 종료 하는 함수 구현
+
+// TODO: Mission 9: 전체 함수들을 실행 시켜주는 메인 함수 구현
+
 class App {
   async play() {}
 }
