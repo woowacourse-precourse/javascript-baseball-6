0c0723bb4e1def58068e1d4ad63a9350ba9e3379
rlaltj <we00425@gmail.com>
2023-10-23 00:45:01 +0900

    Feat: Add Judge Number
    (컴퓨터) 입력한 숫자에 대한 결과 출력 기능

--- src/computer.js
+++ src/computer.js
@@ -12,8 +12,32 @@ export function pickRandomNumber() {
   return computer;
 }
 
-export function judgeNumber() {
-    // 입력한 숫자에 대한 결과 출력
+export function judgeNumber(computerNum, playerNum) {
+  // 입력한 숫자에 대한 결과 출력
+
+  let strike = 0;
+  let ball = 0;
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      if (playerNum[i] == computerNum[j]) {
+        if (i == j) {
+          strike++;
+        } else {
+          ball++;
+        }
+        break;
+      }
+    }
+  }
+ 
+  if (strike == 0 && ball == 0) {
+    MissionUtils.Console.print('낫싱');
+  } else if (strike == 3) {
+    MissionUtils.Console.print('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+  } else {
+    MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+  }
+
 }
 
 export async function endGame() {
@@ -21,15 +45,15 @@ export async function endGame() {
 
   try {
     let gameStatus = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
-    
+
     // 예외사항 처리
     if (regex.test(gameStatus)) {
-        MissionUtils.Console.print('[ERROR] 공백 없이 숫자만 입력해주세요.');
-        throw new Error('숫자 외 문자 입력 오류');
+      MissionUtils.Console.print('[ERROR] 공백 없이 숫자만 입력해주세요.');
+      throw new Error('숫자 외 문자 입력 오류');
     } else if (gameStatus.length != 1) {
-        MissionUtils.Console.print('[ERROR] 1개의 숫자만 입력해주세요.');
-        throw new Error('숫자 개수 오류');
-    } 
+      MissionUtils.Console.print('[ERROR] 1개의 숫자만 입력해주세요.');
+      throw new Error('숫자 개수 오류');
+    }
 
     if (gameStatus == 1) {
       return true;
@@ -37,9 +61,9 @@ export async function endGame() {
       return false;
     } else {
       MissionUtils.Console.print('[ERROR] 1 또는 2만 입력해주세요.');
-        throw new Error('선택지 숫자 오류');
+      throw new Error('선택지 숫자 오류');
     }
   } catch (error) {
-      MissionUtils.Console.print(error);
+    MissionUtils.Console.print(error);
   }
 }
