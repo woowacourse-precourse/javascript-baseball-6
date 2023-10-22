65eb4af931697db4f656413068122ceaef40409d
yook-jongho <dbrwhdgh1203@naver.com>
2023-10-22 19:25:46 +0900

    정답 셋팅

--- src/App.js
+++ src/App.js
@@ -3,18 +3,34 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 class App {
   constructor() {
     this.userNum = '';
+    this.answer = [];
   }
 
   init() {
     this.userNum = '';
+    this.answer = [];
   }
 
-  async play() {}
+  async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    await this.userInput();
+    this.settingAnswer();
+  }
 
   // 유저 입력 처리
   async userInput() {
     this.userNum =
       await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+  }
+
+  // 정답 셋팅
+  settingAnswer() {
+    while (this.answer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.answer.includes(number)) {
+        this.answer.push(number);
+      }
+    }
   }
 }
 
