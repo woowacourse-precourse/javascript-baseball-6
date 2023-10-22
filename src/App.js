3bc58aa03455192c72834849eee9dceebfa22d62
baegyeong <beth0929@naver.com>
2023-10-23 00:20:23 +0900

    feat: 입력한 숫자가 정답에 포함되어 있는지 아닌지 검사하는 함수
    - 컴퓨터의 정답과 비교하여 한 숫자라도 있으면 true, 아니면 false 반환

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import { MissionUtils, Console } from "@woowacourse/mission-utils";
+
 class App {
   constructor(input, answer) {
     this.input = input;
     this.answer = answer;
   }
+
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    await this.setInputNumber();
+    await this.getNumber();
+    await Console.print(this.isIncluded());
+  }
+
   async getNumber() {
     this.answer = MissionUtils.Random.pickNumberInRange(100, 999);
     return this.answer;
   }
 
+  async setInputNumber() {
+    try {
+      this.input = await Console.readLineAsync("숫자를 입력해주세요 : ");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async isIncluded() {
+    const inputArr = this.input.toString().split("");
+    const answerArr = this.answer.toString().split("");
+    try {
+      const isInclude = (item) => answerArr.includes(item);
+      return inputArr.some(isInclude);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
