151f29b42dfda1c789500f62efc2b66c4df9cf9b
jypark38 <jury124@naver.com>
2023-10-22 15:37:56 +0900

    test : test코드 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -24,6 +24,25 @@ const getLogSpy = () => {
 };
 
 describe("숫자 야구 게임", () => {
+  test("게임 진행" , async() => {
+    const randoms = [7, 1, 3];
+    const answers = ["123", "145", "671", "216", "713", "2"];
+    const logSpy = getLogSpy();
+    const messages = ["1볼 1스트라이크", "1볼", "2볼", "1스트라이크", "3스트라이크", "게임 종료"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when
+    const app = new App();
+    await expect(app.play()).resolves.not.toThrow();
+
+    // then
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  })
+
   test("게임 종료 후 재시작", async () => {
     // given
     const randoms = [1, 3, 5, 5, 8, 9];
