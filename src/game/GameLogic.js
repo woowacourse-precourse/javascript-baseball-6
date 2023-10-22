00147e516505bd100d3320f87389f9e9c0266e4f
Yoonyesol <maniac970430@naver.com>
2023-10-22 19:12:46 +0900

    Feat: 게임 결과 연산 로직 구현하기

--- src/game/GameLogic.js
+++ src/game/GameLogic.js
@@ -17,7 +17,32 @@ class GameLogic {
     return numberArray.join("");
   }
 
-  checkGameResult(userNum) {}
+  checkGameResult(userNum) {
+    let strike = 0;
+    let ball = 0;
+    let gameResult = "";
+
+    for (let i = 0; i < 3; i++) {
+      if (this.computerNumber[i] === userNum[i]) {
+        strike++;
+      } else if (this.computerNumber.includes(userNum[i])) {
+        ball++;
+      }
+    }
+
+    if (strike === 0 && ball === 0) {
+      gameResult = "낫싱";
+    } else if (strike === 3) {
+      gameResult = "3스트라이크";
+    } else {
+      gameResult = `${ball}볼 ${strike}스트라이크`;
+    }
+
+    MissionUtils.Console.print(gameResult);
+    if (gameResult === "3스트라이크") {
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+  }
 }
 
 export default GameLogic;
