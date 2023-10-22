3eda66246b015885ebf279c01d206cee8f99a44c
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-10-23 03:33:20 +0900

    refactor: 야구번호 type 변경(Number => Array) 및 validation 엣지케이스 추가

--- src/Model/Player.js
+++ src/Model/Player.js
@@ -1,16 +1,27 @@
 import { CustomError } from './Error.js';
+import { BASEBALL_NUMBER } from '../constants/gameConfig.js';
+import { MESSAGE } from '../constants/message.js';
 
 export class Player {
-  #number;
+  numberList;
 
-  constructor(number) {
-    this.validateNumber(number);
+  constructor() {
+    this.validateNumber(numberList);
 
-    this.#number = number;
+    this.numberList = numberList;
   }
 
-  validateNumber(number) {
-    if (String(number).length === BASEBALL_NUMBER.DIGIT)
+  validateNumber(numberList) {
+    if (numberList.length !== BASEBALL_NUMBER.DIGIT)
       throw new CustomError(MESSAGE.ERROR.INVALID_DIGITS);
+
+    if (numberList.every(Number))
+      throw new CustomError(MESSAGE.ERROR.INVALID_TYPE);
+
+    if (!numberList.every(isBaseballNumber))
+      throw new CustomError(MESSAGE.ERROR.OUT_OF_RANGE);
   }
 }
+
+const isBaseballNumber = (number) =>
+  number <= BASEBALL_NUMBER.MAX && number >= BASEBALL_NUMBER.MIN;
--- src/constants/message.js
+++ src/constants/message.js
@@ -1,6 +1,6 @@
 const ERROR = Object.freeze({
+  INVALID_TYPE: '숫자가 아닙니다.',
   INVALID_DIGITS: '숫자는 3자리여야 합니다.',
-  DUPLICATED_NUMBER: '숫자는 중복되면 안됩니다.',
   OUT_OF_RANGE: '숫자는 1~9 사이여야 합니다.',
 });
 
