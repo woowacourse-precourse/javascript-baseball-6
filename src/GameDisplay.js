437c33109b66cbfdad0b9138ce0e23c4552ff557
Trophy198 <spsp8426@naver.com>
2023-10-22 19:31:13 +0900

    Refactor : GameDisplay의 showResult 메서드 간소화

--- src/GameDisplay.js
+++ src/GameDisplay.js
@@ -3,22 +3,24 @@ import { GAME_START_MESSAGE, GAME_OVER_MESSAGE, GAME_END_INSTRUCTION, STRIKE_TEX
 
 export default class GameDisplay {
   showStartMessage() {
-    Console.print(GAME_START_MESSAGE);
+   return Console.print(GAME_START_MESSAGE);
   }
 
   showResult(strike, ball) {
     if (strike === 0 && ball === 0) {
-      Console.print(NOTHING_TEXT);
-    } else {
-      const results = [];
-      if (ball > 0) results.push(`${ball}${BALL_TEXT}`);
-      if (strike > 0) results.push(`${strike}${STRIKE_TEXT}`);
-      Console.print(results.join(' '));
+      return Console.print(NOTHING_TEXT);
     }
+
+    const results = [
+      ball > 0 ? `${ball}${BALL_TEXT}` : null,
+      strike > 0 ? `${strike}${STRIKE_TEXT}` : null,
+    ].filter(Boolean).join(' ');
+
+    return Console.print(results);
   }
 
   showWinMessage() {
-    Console.print(GAME_OVER_MESSAGE);
+   return Console.print(GAME_OVER_MESSAGE);
   }
 
   async showEndMessage() {
