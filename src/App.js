c7320b11b4821459dc50dfe5cd8102aca4dcae33
myireland8125 <hhyjs2r@gmail.com>
2023-10-22 11:58:33 +0900

    [feat] 스트라이크,볼 계산하는 함수 리팩토링

--- src/App.js
+++ src/App.js
@@ -6,35 +6,32 @@ class App {
   NUMBER_LENGTH = 3;
 
   async play() {
-    Console.print("숫자 야구 게임을 시작합니돠.");
-    this.computerNum = [];
-    this.inputValue = "";
+    Console.print("숫자 야구 게임을 시작합니다.");
     this.createRandomNum();
     console.log(this.computerNum);
     await this.getInputNum();
   }
 
   async getInputNum() {
+    this.inputValue = "";
     this.inputValue = await Console.readLineAsync("숫자를 입력해주세요.");
     this.checkInputValidate(this.inputValue);
     await this.printStrikeAndBall(this.inputValue);
   }
 
   calculateStrikeAndBall(inputValue) {
-    const inputNumArr = inputValue.split("");
+    const inputNumArr = inputValue
+      .split("")
+      .map((numStr) => parseInt(numStr, 10));
     let strike = 0;
     let ball = 0;
-
-    for (let i = 0; i < this.NUMBER_LENGTH; i++) {
-      for (let j = 0; j < this.NUMBER_LENGTH; j++) {
-        if (i === j && this.computerNum[i] === inputNumArr[j]) {
-          strike++;
-        }
-        if (i !== j && this.computerNum[i] === inputNumArr[j]) {
-          ball++;
-        }
+    inputNumArr.forEach((num, i) => {
+      if (num === this.computerNum[i]) {
+        strike++;
+      } else if (this.computerNum.includes(num)) {
+        ball++;
       }
-    }
+    });
 
     return { strike, ball };
   }
@@ -45,44 +42,42 @@ class App {
     if (strike === 3) {
       Console.print(`${strike}스트라이크`);
       Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-
-      const inputValue = await Console.readLineAsync(
-        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
-      );
-
-      if (parseInt(inputValue) === 1) {
-        await this.play();
-      } else {
-        Console.print("겜 종료");
-        return;
-      }
-
+      this.selectPalyAgain();
       return;
     }
-
     if (ball === 0 && strike === 0) {
       Console.print("낫싱");
-      await this.getInputNum();
+    }
+    if (ball !== 0 && strike !== 0) {
+      Console.print(`${ball}볼 ${strike}스트라이크`);
     }
 
     if (ball == 0 && strike !== 0) {
-      await this.getInputNum();
+      Console.print(`${strike}스트라이크`);
     }
-
     if (strike == 0 && ball !== 0) {
       Console.print(`${ball}볼`);
-      await this.getInputNum();
     }
 
-    if (ball !== 0 && strike !== 0) {
-      Console.print(`${ball}볼 ${strike}스트라이크`);
-      await this.getInputNum();
+    await this.getInputNum();
+  }
+
+  async selectPalyAgain() {
+    const inputValue = await Console.readLineAsync(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    );
+
+    if (parseInt(inputValue) === 1) {
+      await this.play();
+    } else {
+      Console.print("게임을 종료합니다.");
     }
   }
 
   createRandomNum() {
+    this.computerNum = [];
     while (this.computerNum.length < this.NUMBER_LENGTH) {
-      const randomNum = Random.pickNumberInRange(1, 9).toString(); // 랜덤 숫자를 돌린다.
+      const randomNum = Random.pickNumberInRange(1, 9); // 랜덤 숫자를 돌린다.
       const isInclude = this.computerNum.includes(randomNum); // 중복 숫자가 있으면 true, 없으면 false 를 반환한다.
 
       if (!isInclude) {
@@ -116,7 +111,7 @@ class App {
   //예외처리 함수
   isHasZero(inputValue) {
     const numArr = inputValue.split("");
-    return numArr.some((num) => num === "0");
+    return numArr.some((num) => num === 0);
   }
 
   isDuplicate(inputValue) {
