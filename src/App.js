7408857df93e652f8352573f87d05a54cb6ab7e5
정해성 <gotjd0230@naver.com>
2023-10-22 16:54:37 +0900

    :sparkles: feat: 게임 시작 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 class App {
-  async play() {}
+  constructor() {
+    this.play();
+  }
+
+  async play() {
+    this.playGame();
+  }
+
+  playGame() {
+    console.log("숫자 야구 게임을 시작합니다.");
+  }
 }
+
+const app = new App();
 
 export default App;
