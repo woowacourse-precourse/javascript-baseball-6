64600089dadd72c4a59d6d46e689eaf8cabdbfc9
Trophy198 <spsp8426@naver.com>
2023-10-22 19:34:01 +0900

    Refactor : User클래스 메서드 분리

--- src/User.js
+++ src/User.js
@@ -1,10 +1,23 @@
 import { Console } from '@woowacourse/mission-utils';
 import InputValidator from './utils/InputValidator.js';
+import { USER_INPUT_MESSAGE } from './constants/MessageConstants.js';
 
 export default class User {
   async getInput() {
-    const input = await Console.readLineAsync('숫자를 입력해주세요. : ');
+    const input = await this.readInput();
+    this.validateInput(input);
+    return this.convertToNumberArray(input);
+  }
+
+  async readInput() {
+    return await Console.readLineAsync(USER_INPUT_MESSAGE);
+  }
+
+  validateInput(input) {
     InputValidator.validateInput(input);
+  }
+
+  convertToNumberArray(input) {
     return Array.from(input).map(Number);
   }
 }
