b775ad3eaf3fd3dc2e4050a1d988188446a1b6ec
KYHH <kyhh93@naver.com>
2023-10-22 15:36:18 +0900

    feat : JavaScript class test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+`use strict`;
 class App {
-  async play() {}
+  constructor(num1, num2, num3) {
+    this.one = num1;
+    this.two = num2;
+    this.thr = num3;
+  }
+
+  play() {
+    return Number(this.one + this.two + this.thr);
+  }
 }
+
+const app = new App(5, 7, 9);
+console.log(app.play());
 
 export default App;
