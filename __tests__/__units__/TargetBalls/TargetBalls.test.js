a6c40f0e988a5502d04580f0a425d8802c30eac6
cobocho <rlaalsrb1111@naver.com>
2023-10-22 13:56:57 +0900

    Test: `TargetBalls` 테스트 케이스 추가

--- __tests__/__units__/TargetBalls/TargetBalls.test.js
+++ __tests__/__units__/TargetBalls/TargetBalls.test.js
@@ -1,7 +1,7 @@
 import { TargetBall, TargetBalls } from '../../../src/domain';
 
 describe('TargetBalls 테스트', () => {
-  it.each([{ input: [1, 2] }])(
+  it.each([{ input: [1, 2, 3] }, { input: [1, 2, 4] }, { input: [5, 1, 2] }, { input: [7, 8, 9] }])(
     '입력받은 배열에 따라 `numbers` 필드에 `TargetBall`로 이루어진 배열을 가진다.',
     ({ input }) => {
       const balls = TargetBalls.of(input);
