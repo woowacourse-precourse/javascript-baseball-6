0e90dfeb43f898a914f1e5ec7d527b7eed14c470
lucaseunchae <lucaskim.dev@gmail.com>
2023-10-22 23:53:24 +0900

    feat: add calculateCounts function

---
+++ src/baseball-game.js
@@ -0,0 +1,21 @@
+const calculateStrikeCount = (computerNums, answerNums) => {
+  return answerNums.filter((answerNum, i) => answerNum === computerNums[i])
+    .length;
+};
+
+const calculateBallCount = (computerNums, answerNums) => {
+  const duplicateCount = answerNums.filter((answerNum) =>
+    computerNums.includes(answerNum)
+  ).length;
+
+  return duplicateCount
+    ? duplicateCount - calculateStrikeCount(computerNums, answerNums)
+    : 0;
+};
+
+export const calculateCounts = (computerNums, answerNums) => {
+  const strikeCount = calculateStrikeCount(computerNums, answerNums);
+  const ballCount = calculateBallCount(computerNums, answerNums);
+
+  return { strikeCount, ballCount };
+};
