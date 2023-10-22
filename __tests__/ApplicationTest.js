6b58977b6e70602ed4a73b91903c9c6fba33e72d
jypark38 <jury124@naver.com>
2023-10-22 17:05:17 +0900

    test 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,6 @@
 import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { LOGS } from "../src/lib/logs.js";
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -55,6 +56,6 @@ describe("숫자 야구 게임", () => {
     // when & then
     const app = new App();
 
-    await expect(app.play()).rejects.toThrow("[ERROR]");
+    await expect(app.play()).rejects.toThrow(LOGS.INVALID_INPUT1);
   });
 });
