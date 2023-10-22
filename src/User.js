b7864098cfbfeff40982d99d26c4ad0c7b71a540
Trophy198 <spsp8426@naver.com>
2023-10-22 12:40:11 +0900

    Refactor : User 클래스에 입력 검증 기능 추가

--- src/User.js
+++ src/User.js
@@ -3,16 +3,8 @@ import InputValidator from "./utils/InputValidator.js";
 
 export default class User {
   async getInput() {
-    let input;
-    while (true) {
-      try {
-        input = await Console.readLineAsync("숫자를 입력해주세요. : ");
-        InputValidator.validateInput(input);
-        return Array.from(input).map(Number);
-      } catch (error) {
-        Console.print(error.message);
-        throw error;
-      }
-    }
+    const input = await Console.readLineAsync("숫자를 입력해주세요. : ");
+    InputValidator.validateInput(input);
+    return Array.from(input).map(Number);
   }
 }
