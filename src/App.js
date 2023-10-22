d8115c325f264da422c658e2499e30c114738643
8ark <sorlti6952@gmail.com>
2023-10-22 17:00:27 +0900

    Feat : "게임 진행 구현"
    Feat : "게임 진행 구현"
    
    strike, ball 판정을 제외한 게임 흐름 개발
    let, const에 따른 변수명 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   constructor() {
-    this.IS_CORRECT = false; //strike 3이면 true
-    this.IS_END = false; //IS_CORRECT가 true일 때 2의 값을 가지면 true
+    this.is_correct = false; //strike 3이면 true
+    this.is_end = false; //IS_CORRECT가 true일 때 2의 값을 가지면 true
   }
   //사용자 숫자 입력
   async getUserNumber() {
-    const user = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
-    
+    let user = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+
     user.split("").forEach((num, index) => {
       if (isNaN(num)) {
         throw new Error("[ERROR] 숫자만 입력 가능합니다.");
@@ -25,16 +25,20 @@ class App {
       }
     });
 
+    //parse to Array
+    user = user.split("").map((num) => parseInt(num));
+
     return user;
   }
 
   //컴퓨터 숫자 생성
   getRandomNumber() {
-    const com = [] 
-    while(com.length < 3) {
-      const random = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!com.includes(random)) {
-        com.push(random); //중복되지 않는 숫자만 배열에 추가
+    let com = [];
+    while (com.length < 3) {
+      const TEMPNUM = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!com.includes(TEMPNUM)) {
+        //중복 검사
+        com.push(TEMPNUM);
       }
     }
 
@@ -43,26 +47,39 @@ class App {
 
   //숫자 검사
   compareNumber(com, user) {
-
-  }
-
-  //게임 재시작
-  restartGame() {
+    //is_correct change
   }
 
   //게임 진행
-  async playGame(com, user) {
-    let END_FLAG = true;
-    while(END_FLAG) {
-      const res = this.compareNumber(COM, USER);
+  async playGame() {
+    let com = this.getRandomNumber();
+    while (!this.is_end) {
+      let user = await this.getUserNumber();
 
+      console.log("com" + com);
+      console.log("user" + user);
+      this.compareNumber(com, user);
+      if (this.is_correct) {
+        let res = await MissionUtils.Console.readLineAsync(
+          "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+        );
+        if (res === "1") {
+          this.is_end = false;
+          this.is_correct = false;
+
+          com = this.getRandomNumber();
+        } else if (res === "2") {
+          this.is_end = true;
+        } else {
+          throw new Error("[ERROR] 1 또는 2만 입력해주세요.");
+        }
+      }
     }
   }
 
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    let user = await this.getUserNumber();
-    console.log(user);
+    await this.playGame();
   }
 }
 
