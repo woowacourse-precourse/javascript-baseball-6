fe5f3ec6ed8dc450596d0b97310a1f18f7cdb8ea
chestnut1044 <wbk1620@gmail.com>
2023-10-22 22:44:16 +0900

    feat : 사용자 인풋값 유효성 체크 구현
    사용자에게 입력받은 값이 유효한지 확인, 잘못된 값 입력시 프로그램 종료

--- src/startGame.js
+++ src/startGame.js
@@ -1,7 +1,28 @@
 import { Console } from "@woowacourse/mission-utils";
 import getRandomNumBer from "./getRandomNumber.js";
 
-export default function startGame() {
+export default async function startGame() {
   Console.print("숫자 야구 게임을 시작합니다.");
   const targetNumber = getRandomNumBer(3);
+  Console.print(targetNumber);
+  while (true) {
+    const validInputNumber = await userInputNumber(3);
+    if (validInputNumber === targetNumber) {
+      break;
+    }
+  }
 }
+
+// 유효한 값이 입력됐는지 확인
+const userInputNumber = async (digitNumber) => {
+  const guessInputNumber = await Console.readLineAsync(
+    "숫자를 입력해주세요 : "
+  );
+  if (isNaN(guessInputNumber)) {
+    throw new Error("guessInputNumber must be a Number");
+  }
+  if (guessInputNumber.length !== digitNumber) {
+    throw new Error(`guessInputNumber must be ${digitNumber} digit Number`);
+  }
+  return guessInputNumber;
+};
