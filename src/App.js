9faff53e7ef72ec4a19953ffaf25115fbd024083
jbilee <julishahn@gmail.com>
2023-10-22 12:09:37 +0900

    feat: 힌트 출력 기능, 게임 종료 분기점 추가 및 게임 재시작 기능 구현

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     const input = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
     const isValid = this.validateInput(input);
-    if (isValid) console.log(input);
+    if (isValid) this.checkAnswer(input, answer);
     else throw new Error("[ERROR]");
   }
 
@@ -31,6 +31,35 @@ class App {
     const set = new Set(arr);
     if (arr.length !== set.size) return true;
     return false;
+  }
+
+  checkAnswer(guess, answer) {
+    let ball = 0;
+    let strike = 0;
+
+    const guessArr = guess.split("").map((x) => { return Number(x) });
+    answer.forEach((x, i) => {
+      if (x === guessArr[i]) strike++;
+      if (x !== guessArr[i] && answer.indexOf(guessArr[i]) >= 0) ball++;
+    });
+
+    if (strike === 3) return this.endGame();
+    return this.continueGame(ball, strike, answer);
+  }
+
+  async continueGame(ball, strike, answer) {
+    const hint = ball > 0 && strike > 0 ? `${ball}볼 ${strike}스트라이크` : ball > 0 ? `${ball}볼` : strike > 0 ? `${strike}스트라이크` : "낫싱";
+    MissionUtils.Console.print(hint);
+    const input = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+    const isValid = this.validateInput(input);
+    if (isValid) this.checkAnswer(input, answer);
+    else throw new Error("[ERROR]");
+  }
+
+  async endGame() {
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    const input = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+    if (Number(input) === 1) this.play();
   }
 }
 
