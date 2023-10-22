e13b753c14c5289394649ca2cc04b9f980e0f8ec
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2023-10-23 02:25:24 +0900

    feat: 난수 생성 기능

--- src/Class/Computer.js
+++ src/Class/Computer.js
@@ -1,3 +1,20 @@
+import { MissionUtils } from "@woowacourse/mission-utils"
+
 export default class Computer{
     number = []
+
+    constructor(){
+        this.generateRandNum()
+    }
+
+    generateRandNum = () => {
+        this.number = []
+    
+        while(this.number.length < 3){
+            const number = MissionUtils.Random.pickNumberInRange(1, 9)
+            if(!this.number.includes(number)) this.number.push(number)
+        }
+        
+        return this.number
+    }
 }
