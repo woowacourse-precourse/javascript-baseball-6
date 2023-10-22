3caddb0835c00e423fb2c4a96d7dec99b734e746
6puritans9 <6puritans9@gmail.com>
2023-10-22 18:20:23 +0900

    feat: 사용자의 입력값과 목표값을 비교해 문자 출력하는 메소드 구현

--- src/Game.js
+++ src/Game.js
@@ -3,7 +3,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 export class Game {
   constructor() {
     this.welcome();
-    this.getUserInput();
+    this.playBall();
   }
 
   welcome() {
@@ -27,13 +27,11 @@ export class Game {
         );
         return inputPromise;
       } catch (error) {
-        MissionUtils.Console.print("error");
+        MissionUtils.Console.print("Unexpected Error Has Occured");
       }
     }
 
-    getInputPromise().then((input) => {
-      this.validateUserInput(input);
-    });
+    getInputPromise().then((input) => this.validateUserInput(input));
   }
 
   validateUserInput(input) {
@@ -41,11 +39,39 @@ export class Game {
     for (const element of input) {
       set.add(parseInt(element));
     }
-    set.forEach((el) => {
-      if (isNaN(el)) {
-        throw new Error("[ERROR] 숫자를 입력해주세요");
-      }
-    });
-    if (set.size !== 3) throw new Error("[ERROR] 세 자리 숫자가 아닙니다");
+    try {
+      set.forEach((el) => {
+        if (isNaN(el)) {
+          throw new Error("[ERROR] 숫자를 입력해주세요");
+        }
+      });
+      if (set.size !== 3) throw new Error("[ERROR] 세 자리 숫자가 아닙니다");
+    } catch (error) {
+      return 1;
+    }
+    console.log(`user: ${[...set]}`);
+    this.compareNumbers([...set]);
+  }
+
+  compareNumbers(inputArray) {
+    let balls = 0;
+    let strikes = 0;
+    for (let i = 0; i < this.targetArray.length; i++) {
+      if (this.targetArray[i] === inputArray[i]) strikes++;
+      else if (inputArray.includes(this.targetArray[i])) balls++;
+    }
+    if (balls === 0 && strikes === 0) MissionUtils.Console.print(`낫싱`);
+    else if (strikes === 3) {
+      MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다!`);
+      return 0;
+    } else MissionUtils.Console.print(`${balls}볼, ${strikes}스트라이크`);
+
+    this.getUserInput();
+  }
+
+  playBall() {
+    this.targetArray = this.randomGenerator();
+    console.log(`target: ${this.targetArray}`);
+    this.getUserInput();
   }
 }
