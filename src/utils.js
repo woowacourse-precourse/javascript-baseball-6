13fd25f4938d13a1791d639d828e113ffc9a99db
domikjs <domikjs@gmail.com>
2023-10-22 17:53:41 +0900

    feat: add guess digits reader function

--- src/utils.js
+++ src/utils.js
@@ -8,3 +8,14 @@ export function generateRandomThreeDigits() {
   }
   return answerDigits;
 }
+
+async function readGuessDigits() {
+  const guess = await Console.readLineAsync('숫자를 입력해주세요 : ');
+  const guessDigits = guess
+    .split('')
+    .map((numberString) => parseInt(numberString));
+  if (guess.length !== 3) {
+    throw new Error('[ERROR] 세 자리 수가 아닙니다.');
+  }
+  return guessDigits;
+}
