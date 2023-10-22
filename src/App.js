18d5134a27b2b6ca64e9bead7aa6cafbcfad9e6f
nagyeongyun <skruddbs99@naver.com>
2023-10-22 18:50:39 +0900

    feat: 입력한 숫자에 대한 결과 출력

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  async play() {}
+  async play() {
+    try {
+      MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+      compareNumber();
+    } catch (error) {
+      throw new Error("[ERROR] 게임 종료");
+    }
+  }
 }
 
 function generateComputerNumber() {
@@ -12,6 +19,7 @@ function generateComputerNumber() {
       computer.push(number);
     }
   }
+  return computer;
 }
 
 async function getUserNumber() {
@@ -19,6 +27,37 @@ async function getUserNumber() {
   if (user.length !== 3 || new Set(user).size !== 3) {
     throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
   }
+  return user.split("").map(Number);
+}
+
+async function compareNumber() {
+  const computer = generateComputerNumber();
+
+  let ball = 0;
+  let strike = 0;
+
+  while (strike !== 3) {
+    const user = await getUserNumber();
+    ball = 0;
+    strike = 0;
+    for (let num = 0; num < 3; num++) {
+      if (computer[num] === user[num]) {
+        strike++;
+      } else if (user.includes(computer[num])) {
+        ball++;
+      }
+    }
+
+    let printBall = ball !== 0 ? `${ball}볼 ` : "";
+    let printSkrike = strike !== 0 ? `${strike}스트라이크 ` : "";
+    if (ball !== 0 || strike !== 0) {
+      MissionUtils.Console.print(printBall + printSkrike);
+    } else {
+      MissionUtils.Console.print("낫싱");
+    }
+  }
+
+  MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 }
 
 export default App;
