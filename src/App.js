c0e94ce7d74678992d1eb5005def3bf674da6c8e
kwani6684 <jack0910@naver.com>
2023-10-23 00:34:07 +0900

    ✨User 객체 추가 #2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Com {
   constructor() {
-    const computer = [];
-    while (computer.length < 3) {
+    const comNum = [];
+    while (comNum.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer.push(number);
+      if (!comNum.includes(number)) {
+        comNum.push(number);
       }
     }
-    this.computer=computer;
+    console.log(comNum);
+    this.comNum = comNum;
   }
 }
-class App {
-  async play() {}
+class User {
+  constructor() {
+    this.userNum;
+    this.getNumber();
+  }
+
+  async getNumber() {
+    try {
+      let user = [];
+      let username = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+      if (username.length == 3) {
+        for (let i = 0; i < username.length; i++) {
+          user.push(Number(username[i]));
+        }
+      }
+      console.log(user);
+      MissionUtils.Console.print(user);
+      this.userNum = user;
+    } catch (error) {}
+  }
 }
+
+class App {
+  async play() {
+    let com = new Com();
+    let user = new User();
+  }
+}
+const app = new App();
+app.play();
 
 export default App;
