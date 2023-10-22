42dbda7c5b89cf4cf06cd07d48a537354dd426be
Hyelim Choi <HyelimChoi01@gmail.com>
2023-10-23 02:22:29 +0900

    feat: 컴퓨터가 정답 생성하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-class App {
-  async play() {}
+/** @format */
+
+import { Console, Random } from "@woowacourse/mission-utils";
+
+export default class BaseballGame {
+  play() {
+    this.start();
+  }
+
+  async start() {
+    this.computerNumbers = this.generateRandomNumbers();
+    // Console.print(this.computerNumbers); // 테스트용
+  }
+
+  generateRandomNumbers() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
 }
 
-export default App;
+const baseballGame = new BaseballGame();
+baseballGame.play();
