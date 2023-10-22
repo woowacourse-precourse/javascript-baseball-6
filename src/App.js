7eedb5a68c42d083cac4801863807885f455a2e8
yeokyoung song <spolice0324@gmail.com>
2023-10-22 23:56:22 +0900

    feat : 게임 시작 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  async play() {}
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    this.gameTurn();
+  }
+
+  gameTurn() {}
 }
+
+const app = new App();
+app.play();
 
 export default App;
