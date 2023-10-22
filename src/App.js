59b4594221c26bcd6e23ee3fe7ca02b2c4857b03
yhyem <yoonhyemin0120@gmail.com>
2023-10-23 02:53:39 +0900

    feat: 게임 시작시 랜덤 값 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
+function pickRandomNumber(array) {
+  while (array.length < 3) {
+    const number = Random.pickNumberInRange(1, 9);
+    if (!array.includes(number)) {
+      array.push(number);
+    }
+  }
+}
+
 class App {
-  async play() {}
+  async play() {
+    let randomArray = [];
+
+    pickRandomNumber(randomArray);
+  }
 }
 
 export default App;
