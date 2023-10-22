c5fea3ac9f06deec7dc6c80cf08a05b887ef9bf9
cobocho <rlaalsrb1111@naver.com>
2023-10-22 13:55:02 +0900

    Docs: `TargetBalls` 테스트명 수정

--- __tests__/__units__/TargetBalls/TargetBalls.test.js
+++ __tests__/__units__/TargetBalls/TargetBalls.test.js
@@ -1,6 +1,6 @@
 import { TargetBall, TargetBalls } from '../../../src/domain';
 
-describe('Balls 테스트', () => {
+describe('TargetBalls 테스트', () => {
   it.each([{ input: [1, 2] }])(
     '입력받은 배열에 따라 `numbers` 필드에 `TargetBall`로 이루어진 배열을 가진다.',
     ({ input }) => {
