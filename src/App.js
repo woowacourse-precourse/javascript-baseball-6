b451c1165b3f0687a8809bee4128604c0c4f8f79
kimmainsain <kmsid003@gmail.com>
2023-10-22 15:27:48 +0900

    :heavy_plus_sign: Feat : 컴퓨터는 1 ~ 9까지의 서로 다른 3가지 수를 선택한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+  }
 }
 
 export default App;
