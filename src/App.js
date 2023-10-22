d62f315b64f55fff1b7a6b0bb8060829d1734002
Moon Sojeong <sjeong35@naver.com>
2023-10-23 02:45:28 +0900

    feat: 사용자 숫자 입력받기, 랜덤 숫자 기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-class App {
-  async play() {}
+import {Console} from '@woowacourse/mission-utils';
+import GameUtil from './GameUtil';
+
+class App extends GameUtil{
+    constructor(){
+        super();
+        this.init();
+    }
+
+    init(){
+        this.randomNumber = super.generateRandomNumbers();
+        Console.print('숫자 야구 게임을 시작합니다.');
+    }
+
+    async play(){
+        try{
+        const userNumber = await Console.readLineAsync('숫자를 입력해주세요 : ');
+        }catch(error){
+            Console.print(error);
+        }
+    }
 }
+
+const app = new App();
+app.play().catch(error=> Console.print(error));
 
 export default App;
