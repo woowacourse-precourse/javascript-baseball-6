07cfb1ca442bed27ff84f3028d482c2cd5c1b5c2
jypark38 <jury124@naver.com>
2023-10-22 15:51:40 +0900

    test: test코드 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -28,7 +28,7 @@ describe("숫자 야구 게임", () => {
     const randoms = [7, 1, 3];
     const answers = ["123", "145", "671", "216", "713", "2"];
     const logSpy = getLogSpy();
-    const messages = ["1볼 1스트라이크", "1볼", "2볼", "1스트라이크", "3스트라이크", "게임 종료"];
+    const messages = ["1볼 1스트라이크", "1볼", "2볼", "1스트라이크", "3스트라이크", , "시도 횟수 : 5 회", "게임 종료"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -48,7 +48,7 @@ describe("숫자 야구 게임", () => {
     const randoms = [1, 3, 5, 5, 8, 9];
     const answers = ["246", "135", "1", "597", "589", "2"];
     const logSpy = getLogSpy();
-    const messages = ["낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료"];
+    const messages = ["낫싱", "3스트라이크", "시도 횟수 : 2 회", "1볼 1스트라이크", "3스트라이크", "시도 횟수 : 2 회", "게임 종료"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -63,7 +63,7 @@ describe("숫자 야구 게임", () => {
     });
   });
 
-  test("예외 테스트", async () => {
+  test("예외 테스트1 : 유저 입력 길이가 3이 아닐 때", async () => {
     // given
     const randoms = [1, 3, 5];
     const answers = ["1234"];
@@ -77,12 +77,40 @@ describe("숫자 야구 게임", () => {
     await expect(app.play()).rejects.toThrow("[ERROR]");
   });
 
-  test("예외 테스트2", async () => {
+  test("예외 테스트2 : 유저 입력이 서로 다른 세 수가 아닐 때", async () => {
     // given
     const randoms = [1, 3, 5];
-    const answers = ["135", "3", "4"];
+    const answers = ["333"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트3 : 유저 입력이 자연수가 아닐 경우", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["012"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트4 : 재시작 여부 물어볼 때 1 혹은 2가 아닐 경우", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["135", "3"];
     const logSpy = getLogSpy();
-    const messages = ["3스트라이크"];
+    const messages = ["3스트라이크", , "시도 횟수 : 1 회"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
