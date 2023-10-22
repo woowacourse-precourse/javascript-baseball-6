78298c0e326b1e5cbc3400dc538ba82ef3ba1ced
jaeml00 <jaeml0630@gmail.com>
2023-10-22 10:56:58 +0900

    feat: create game startcode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import BaseballGame from './BaseballGame';
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    const game = new BaseballGame();
+    if (!game.gameStart()) {
+      return;
+    }
+
+    let retryflag = await game.retry();
+    while (retryflag === 1) {
+      if (!game.gameStart()) {
+        return;
+      }
+      retryflag = await game.retry();
+    }
+  }
 }
 
 export default App;
