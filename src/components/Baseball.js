8bd3b1b45c53a081b0136dfe462a366194a10146
JUN <gurwns232@naver.com>
2023-10-23 00:01:37 +0900

    feat: baseball 게임 클래스 추가

---
+++ src/components/Baseball.js
@@ -0,0 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+
+class Baseball {
+  constructor() {
+    this.player = {
+      computer: [],
+      user: [],
+    };
+
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  startGame() {}
+}
+
+export default Baseball;
