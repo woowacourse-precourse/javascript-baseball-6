d1d70bcdcb495d0742950bed31b75509c8386ac0
jaeml00 <jaeml0630@gmail.com>
2023-10-22 16:44:42 +0900

    refactor: edit indentation
    4->2

--- src/Computer.js
+++ src/Computer.js
@@ -1,20 +1,21 @@
-import  { MissionUtils }  from "@woowacourse/mission-utils";
+import { MissionUtils } from '@woowacourse/mission-utils';
 
-export default class Computer{
-    #randomThreeNummber;
-    constructor() {
-    }
+export default class Computer {
+  #randomThreeNummber;
+  constructor() {}
 
-    setRandomThreeNummber(){ // 랜덤 3개 수 생성
-            this.#randomThreeNummber = [];
-            while (this.#randomThreeNummber.length < 3) {
-                const number = MissionUtils.Random.pickNumberInRange(1, 9);
-                if (!this.#randomThreeNummber.includes(number)) {
-                    this.#randomThreeNummber.push(number);
-                }
-        }
+  setRandomThreeNummber() {
+    // 랜덤 3개 수 생성
+    this.#randomThreeNummber = [];
+    while (this.#randomThreeNummber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.#randomThreeNummber.includes(number)) {
+        this.#randomThreeNummber.push(number);
+      }
     }
-    getRandomThreeNummber(){ // 랜덤 생성 수 배열 반환
-        return this.#randomThreeNummber;
-    }
+  }
+  getRandomThreeNummber() {
+    // 랜덤 생성 수 배열 반환
+    return this.#randomThreeNummber;
+  }
 }
