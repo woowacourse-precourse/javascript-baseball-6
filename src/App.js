b005f026c7bc842963bb942425e065d321d56b3b
suyeon <moa0823@naver.com>
2023-10-22 13:20:52 +0900

    입력받은 숫자 예외 처리

--- src/App.js
+++ src/App.js
@@ -15,13 +15,21 @@ class App {
   }
 
   async inputNumber() {
-    return await Console.readLineAsync("숫자를 입력해주세요: ");
+    const USER_NUMBER = await Console.readLineAsync("숫자를 입력해주세요: ");
+    if (
+      USER_NUMBER.length !== 3 ||
+      new Set(USER_NUMBER).size !== 3 ||
+      USER_NUMBER.includes("0")
+    ) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+    // Console.print(USER_NUMBER);
+    return USER_NUMBER;
   }
 
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
-    const USER_NUMBER = await this.inputNumber();
-    // Console.print(USER_NUMBER);
+    await this.inputNumber();
   }
 }
 
