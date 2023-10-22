e14bcf55c6514004c346d41fd0ef9ba8d9aa175e
SU_VIN <pyotnqls25@naver.com>
2023-10-22 14:10:01 +0900

    Feat: 숫자 입력 받는 기능 구현
    1. 입력값 에러처리 해주기

--- src/MainGame.js
+++ src/MainGame.js
@@ -9,8 +9,28 @@ class MainGame {
     this.printStart();
   };
 
+  //시작 문구 출력
   printStart = () => {
     Console.print("숫자 야구 게임을 시작합니다.");
+    this.getUserInput();
+  };
+
+  //입략값이 1.1-9사이의 숫자인지 2.3자리인지 를 확인해야함
+  isValidateNumber = (number) => {
+    if (!/^[1-9]{3}$/.test(number)) {
+      throw new Error("[ERROR] 1-9사이의 세자리 숫자가 아닙니다.");
+    }
+
+    const numberSet = new Set(number);
+    if (numberSet.size !== 3) {
+      throw new Error("[ERROR] 중복된 숫자가 존재합니다.");
+    }
+  };
+
+  //숫자를 입력 받음
+  getUserInput = async () => {
+    const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
+    this.isValidateNumber(input);
   };
 }
 
