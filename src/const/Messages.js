b0a7953262f258588a005a00441781e81b346173
hyj5076 <hyj5076@hanmail.net>
2023-10-23 01:55:47 +0900

    docs(Messages): 상수 메시지 수정 및 삭제

--- src/const/Messages.js
+++ src/const/Messages.js
@@ -2,13 +2,10 @@
 export const GAME_START = "숫자 야구 게임을 시작합니다.";
 export const NOTICE_INPUT = "숫자를 입력해주세요";
 export const SUGGEST_REPLAY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+export const GAME_OVER = "게임 종료";
 
 // 게임 결과
 export const STRIKE = "스트라이크";
 export const BALL = "볼";
 export const NOTHING = "낫싱";
 export const GOAL = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-
-// 오류 메시지
-export const WRONG_NUMBER = "올바르지 않은 입력값 입니다. 1-9 중 서로 다른 세 자리의 정수를 입력하세요.";
-export const ERROR_MESSAGE = "오류가 발생했습니다.";
