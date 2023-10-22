8e6b14d835003d4cb19c75439be55ae12ee3c683
Lee sang Yeop <mma7710@naver.com>
2023-10-23 01:16:27 +0900

    Feat: 유저의 입력값과 컴퓨터 3자리 숫자를 비교하는 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import { Random } from '@woowacourse/mission-utils';
+import UserInput from './UserInput.js';
+import PrintConsole from './PrintConsole.js';
 
 class App {
+	#computerAnswer = [];
+
 	generateRandomNumber() {
 		const generateNumberResponse = Random.pickUniqueNumbersInRange(1, 10, 3);
-		return generateNumberResponse;
+		this.#computerAnswer = generateNumberResponse;
 	}
 
-	async play() {}
+	#compareUserAndComputer(inputVal) {
+		const printConsole = new PrintConsole();
+		let strike = 0,
+			ball = 0;
+		const inputDigits = Array.from(inputVal, Number);
+
+		for (let index = 0; index < 3; index++) {
+			if (this.#computerAnswer.includes(inputDigits[index])) {
+				if (this.#computerAnswer[index] === inputDigits[index]) strike++;
+				else ball++;
+			}
+		}
+
+		return printConsole.gameResult(ball, strike);
+	}
+
+	async #playGame() {
+		const userInput = new UserInput();
+		const inputVal = await userInput.baseballInput();
+		this.#compareUserAndComputer(inputVal);
+	}
+
+	async play() {
+		this.generateRandomNumber();
+		this.#playGame();
+	}
 }
 
 const app = new App();
+app.play();
 
 export default App;
