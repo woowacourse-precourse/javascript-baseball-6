ffc15228349dd16bf3b079a5539c0390e5271075
kimmainsain <kmsid003@gmail.com>
2023-10-22 17:11:55 +0900

    :heavy_plus_sign: Feat : 올바르지 않은 플레이어의 입력 에러 처리

--- src/messages/GameMessages.js
+++ src/messages/GameMessages.js
@@ -1,6 +1,8 @@
 const Messages = {
   GAME_START: "숫자 야구 게임을 시작합니다.",
   GAME_INPUT_NUMBER: "숫자를 입력해주세요 : ",
+  INVALID_INPUT_ERROR:
+    "[ERROR] 1부터 9까지의 서로 다른 3자리의 수를 입력해야만 합니다.",
 };
 
 export default Messages;
--- src/utils/CustomUtils.js
+++ src/utils/CustomUtils.js
@@ -6,8 +6,30 @@ const getUserNumber = async () => {
     const USER_NUMBER = await MissionUtils.Console.readLineAsync(
       Messages.GAME_INPUT_NUMBER
     );
+    if (!isValidInput(USER_NUMBER)) {
+      throw new Error(Messages.INVALID_INPUT_ERROR);
+    }
     return USER_NUMBER;
-  } catch (error) {}
+  } catch (error) {
+    MissionUtils.Console.print(error.message);
+  }
+};
+
+const isValidInput = (input) => {
+  if (!/^\d+$/.test(input)) {
+    return false;
+  }
+
+  if (input.length !== 3) {
+    return false;
+  }
+
+  const uniqueNumbers = new Set(input.split(""));
+  if (uniqueNumbers.size !== 3) {
+    return false;
+  }
+
+  return true;
 };
 
 export default { getUserNumber };
