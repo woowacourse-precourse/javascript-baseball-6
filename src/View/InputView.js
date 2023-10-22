d6b77f9588fc2d17666b74363e8a4e324ee4bb13
chiman <rlaclghks123@naver.com>
2023-10-22 22:52:15 +0900

    refactor: InputView의 에러처리
    catch 부분에서 error 발생시 에러를 throw함으로써 비동기 함수를 reject처리 완료

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -5,7 +5,7 @@ const InputView = {
     try {
       return await Console.readLineAsync('숫자를 입력해주세요 : ');
     } catch (error) {
-      console.log('getUserNumber Error', error);
+      throw new Error(`[ERROR] ${error}`);
     }
   },
 
@@ -13,7 +13,7 @@ const InputView = {
     try {
       return await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
     } catch (error) {
-      console.log('confirmContinue Error =>  ', error);
+      throw new Error(`[ERROR] ${error}`);
     }
   },
 };
