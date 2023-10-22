574693f65d2bf7002ccbf30303f9ae32204e2326
cys4585 <choizerowater@gmail.com>
2023-10-22 11:53:37 +0900

    feat: 라이브러리를 쉽게 사용할 수 있도록  래핑 함수 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { print, readLineAsync } from "./utils/console.js";
+import { pickNumberInRange } from "./utils/random.js";
+
 class App {
-  async play() {}
+  async play() {
+    const t = await readLineAsync("입출력 테스트: ");
+    print(t);
+
+    const number = pickNumberInRange(1, 10);
+    print(number);
+  }
 }
 
 export default App;
