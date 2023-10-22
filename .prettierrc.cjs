fbb857b336a07357bd518671903142f526e80b87
Si Hyun Lee <pongpongi@Pongpongbook.local>
2023-10-22 20:36:59 +0900

    eslint, prettier modified

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  useTabs: false,
+  trailingComma: "all",
+};
--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async play() {}
 }
+
+
+console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]));
 
 export default App;
