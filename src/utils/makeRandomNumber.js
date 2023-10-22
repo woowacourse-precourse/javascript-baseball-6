5f8825ca72395872f93ed4fc680fb3b3e73fcea6
yeokyoung song <spolice0324@gmail.com>
2023-10-23 00:04:42 +0900

    feat : 게임 정답 문자열을 생성하는 난수 생성 함수 구현
    - 게임의 정답 문자열을 만드는데 사용되는 난수 생성 함수 구현
    - 제공된 Random API 사용
    - 랜덤 값 추출에 Random.pickNumberInRange() 활용

---
+++ src/utils/makeRandomNumber.js
@@ -0,0 +1,15 @@
+const { Random } = reuqire("@woowacourse/mission-utils");
+
+const makeRandomNumber = () => {
+  const answer = [];
+
+  while (answer.length < 3) {
+    const randomNumber = Random.pickNumberInRange(1, 9);
+    if (!answer.includes(randomNumber)) {
+      answer.push(randomNumber);
+    }
+  }
+  return answer.join("");
+};
+
+export default makeRandomNumber;
