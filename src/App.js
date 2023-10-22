698658aa46fbd688b16a7e9f364b166a4ea659da
yook-jongho <dbrwhdgh1203@naver.com>
2023-10-22 19:10:43 +0900

    게임 내 유저 입력 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
+  constructor() {
+    this.userNum = '';
+  }
+
+  init() {
+    this.userNum = '';
+  }
+
   async play() {}
+
+  // 유저 입력 처리
+  async userInput() {
+    this.userNum =
+      await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
