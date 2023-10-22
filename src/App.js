e020fa8aca437ce50491916bdda128ccdd5beb76
Kled <1017yu@daum.net>
2023-10-22 13:39:10 +0900

    refactor: Player 인스턴스 캡슐화

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import Player from './player/index.js';
-import Opponent from './opponent/index.js';
 
 const { Console } = MissionUtils;
 
 class App {
+  #player = new Player();
+
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
-    const player = new Player();
-    player.startGame();
+    await this.#player.startGame();
   }
 }
 
