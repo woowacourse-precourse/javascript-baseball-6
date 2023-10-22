7ec3508ec6036afccb9919d23e49d3ab35f14574
sayyyho <323psh@naver.com>
2023-10-22 17:13:29 +0900

    Feat: App.js에 모듈 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from "@woowacourse/mission-utils";
+import randomNumber from "./PickRandom.js";
+import getNumber from "./Input.js";
+import compareNumber from "./Compare.js";
+
 class App {
-  async play() {}
+  async play() {
+    const answer = randomNumber;
+    while (true) {
+      const input = await getNumber();
+      const { result, message } = compareNumber(answer, input);
+      Console.print(message);
+      if (result === true) break;
+    }
+  }
 }
+
+const game = new App();
+game.play();
 
 export default App;
