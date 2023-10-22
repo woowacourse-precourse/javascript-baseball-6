c36c7234213286b2e8d28bd36e852dbc03874154
dragon1254 <dkvz@naver.com>
2023-10-22 17:14:10 +0900

    함수형으로 싹 갈아엎음 문제는 class 였다

--- src/App.js
+++ src/App.js
@@ -1,155 +1,143 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
 
+class App {
 
-
-// 컴퓨터는 랜덤값 추출
-// ㄴ 아래 있는 `Random.pickNumberInRange()`를 활용 예시는 README.md참조
-// 나는 내가 입력한 값이 맞는지 확인하는 것을 구현
-
-// 컴퓨터 랜덤값 추출
-// 숫자 입력값 받음
-// 컴퓨터가 입력한 값을 배열로 표현 - 배열1
-// ㄴ 배열로 입력값 받음
-// 내가 입력한 값을 배열로 표현 - 배열2
-
-
-// 만약 배열의 길이가 3이 아니면 예외 발생시킨후 throw문을 사용하여 애플리케이션 종료
-// ㄴ에러([ERROR]) 표시
-// 배열1의 length(혹은 3만큼)반복시키면서 배열2만큼 다시 반복 시키면서 같은 값이 있는지 확인
-// 만약 같은 값이 있으면 index값이 같은지 확인하고(for문 썼으니 i==j인지) 같으면 카운트++
-// 만약 같은 값이 있있는데 index값이 다르면(else if) 다르면 카운트2++ 
-// 카운트만큼 스트라이크 카운트2만큼 볼
 // 만약 카운트가 0이면 스트라이크는 표시하지 않음 카운트2가 0이면 볼은 표시하지 않음
 // 만약 카운트==0 && 카운트2==0이면 낫싱 표시
 // 카운트==0&& 카운트2==0 도 아니고 카운트==3도 아니면 숫자 입력 다시 받음
 // 만약 카운트가 3이면 3스트라이크 표시 후 '3개의 숫자를 모두 맞히셨습니다! 게임 종료' 표시
-// 이후 '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.' 표시후 입력값 받음
-// ㄴ 입력값이 1이면 App재실행 2이면 애플리케이션 종료(위에 예외 발생과는 다른듯)
-
-// - `@woowacourse/mission-utils`의 `Random` 및 `Console` API를 사용하여 구현해야 한다.
-//   - Random 값 추출은 `Random.pickNumberInRange()`를 활용한다.
-//   - 사용자의 값을 입력 받고 출력하기 위해서는 `Console.readLineAsync`, `Console.print`를 활용한다.
 
 
-
-
-
-// -----------------------------이것은 async쓰기 전 promise 썼을때로 예상되는 코드
-
-// const promise = new Promise((resolve, reject) =>{
-
-//   let na = String(Console.readLineAsync);
-//   let naArray = Array.from(na);
-
-//   if(naArray.length != 3){
-//     reject(new Error("[ERROR]]"));
-//   } else if (naArray.length ==3){
-//    resolve(naArray) 
-//   }
-// })
-// promise
-// .then(naArray=>{
-//   let strike =0;
-//   let ball =0;
-//   let gameset =0;
-  
-//   for(let i=0; i<computer.length; i++){
-//     for(let j=0; j<naArray.length;j++){
-//       if(computer[i]==naArray[j]){
-//         if(i==j){
-//           strike++
-//         } else if(i!=j){
-//           ball++
-//         }
-//       }
-//     }
-//   }
-//   if(strike==0 && ball == 0){
-//     Console.print(`낫싱\n`);
-//   } else if(strike!=0 && ball !=0){
-//     Console.print(`${ball}볼 ${strike}스트라이크\n`);
-//   } else if(strike!=0 && ball ==0){
-//     Console.print(`${strike}스트라이크\n`);
-//   } else if(strike==0 && ball !=0){
-//     Console.print(`${ball}볼\n`);
-//   }
-//   if(strike==3){
-//     Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`)
-//     gameset = Console.readLineAsync;
-//     if(gameset ==1){
-//       let re = new App.play();
-//       // 위와 같이 하면 내가 입력하는 값은 몰라도 랜덤생성 컴퓨터 값은 안 바뀔수 있으니 확인해야 함
-//     } else if(gameset ==2){
-//       return
-//     }
-//   } else if(strike!=3){
-//     na = Console.readLineAsync;
-//   }
-// })
-// .catch(Console.print("[ERROR]"))
-
-// ---------------------------------------------
-
-class App {
-  // constructor(){
-  //   return this.play();
-  // }
+// 시작하면 숫자야구 게임을 시작합니다 프린트 하고
+// 1. 랜덤 숫자 생성
+// 2. 게임 시작
   async play() {
-
-    let computer =[];
-    while (computer.length<3){
-    let number = MissionUtils.Random.pickNumberInRange(1,9);
-    if(!computer.includes(number)){
-      computer.push(number);
-    }
-    }
-
-  let na = String(Console.readLineAsync);
-  let naArray = Array.from(na);
-
-  if(naArray.length != 3){
-    throw new Error("[ERROR] 3자리 숫자를 입력하세요");
-  } else if (naArray.length ==3){
-   return [computer, naArray];
+    MissionUtils.Console.print(`숫자 야구 게임을 시작합니다.`)
+    const COM_NUMBER = this.ranNumber();
+    this.gameStart(COM_NUMBER);
   }
-  }}  
-this.play().then(([computer,naArray]) =>{
-  let strike =0;
-  let ball =0;
-  let gameset =0;
-  
-  for(let i=0; i<computer.length; i++){
-    for(let j=0; j<naArray.length;j++){
-      if(computer[i]==naArray[j]){
-        if(i==j){
-          strike++
-        } else if(i!=j){
-          ball++
+
+//   1. 랜덤 숫자 생성
+// 아무것도 없는 배열 생성하여 랜덤 숫자 생성하여 같은 숫자가 아니면 push
+// 하여 배열이 3개가 되면 return한다
+ranNumber(){
+  const COMPUTER = [];
+while (COMPUTER.length < 3) {
+  const NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+  if (!COMPUTER.includes(NUMBER)) {
+    COMPUTER.push(NUMBER);
+  }
+}
+return COMPUTER;
+}
+
+
+// 2. 게임 시작
+// 숫자를 입력해주세요 표시후 
+// 3. 내 숫자 생성하여 숫자를 받음
+// 그 숫자는 이제부터 쓸 것이기에 변수에 저장후 
+// 컴퓨터 숫자와 내 숫자가 맞는지 
+// 4. 체크한다
+
+gameStart(COM_NUMBER){
+let MY_NUM = this.newNumber();
+this.checkResult(COM_NUMBER,MY_NUM)
+}
+
+// 3. 내 숫자 생성
+// 숫자 입력 옆의 숫자를 읽어와서
+// 문자로 변경후 배열로 만들어서 그 배열의 길이가 3이 아니면 에러,
+// 3이면 배열로 숫자 생성
+newNumber(){
+  MissionUtils.Console.readLineAsync(`숫자를 입력해주세요 : `,(myNum)=>{
+  let MY_NUMBER_STRING = String(myNum);
+  let MY_NUMBER_ARRAY = Array.from(MY_NUMBER_STRING);
+  if(MY_NUMBER_ARRAY.length != 3){
+    throw new Error("[ERROR] 3자리 숫자를 입력하세요");
+  } else if (MY_NUMBER_ARRAY.length ==3){
+    return MY_NUMBER_ARRAY;
+  }
+})
+}
+
+// 4. 체크한다
+// 5. 볼과 6. 스크라이크를 생성하고
+// 볼과 스트라이크를 표시한다
+// 3스트라이크면 3개숫자 맞았다는 것을 표시하고 새게임 여부 묻는다
+// ㄴ 7. 게임셋 여부
+// ㄴ 다른 것들은 글자 표시후 다시 
+// 8. 게임 스타트
+// ㄴ 내 숫자를 정하는 것 부터. 컴퓨터 숫자는 그대로 있어야 함.
+checkResult (COM_NUMBER,MY_NUM){
+const BALLS = this.ball(COM_NUMBER,MY_NUM);
+const STRIKES = this.strike(COM_NUMBER,MY_NUM);
+
+if(STRIKES==0 && BALLS == 0){
+  MissionUtils.Console.print(`낫싱`);
+} else if(STRIKES!=0 && BALLS !=0){
+  MissionUtils.Console.print(`${BALLS}볼 ${STRIKES}스트라이크`);
+} else if(STRIKES!=0 && BALLS ==0){
+  MissionUtils.Console.print(`${STRIKES}스트라이크`);
+} else if(STRIKES==0 && BALLS !=0){
+  MissionUtils.Console.print(`${BALLS}볼`);
+} else if(STRIKES==3){
+  MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`)
+  this.gameset();
+}
+this.gameStart(COM_NUMBER);
+}
+
+// 5.볼 6. 스크라이크 생성
+// 컴퓨터 숫자의 length(혹은 3만큼)반복시키면서 내 숫자만큼 다시 반복 시키면서 같은 값이 있는지 확인
+// 만약 같은 값이 있으면 index값이 같은지 확인하고(for문 썼으니 i==j인지) 다르면 볼++
+// 만약 같은 값이 있있는데 index값이 다르면(else if) 같으면 스트라이크++ 
+// 스트라이크 볼 생성 함수는 위에서 볼과 스트라이크를 만드는데 쓰인다
+
+ball(COM_NUMBER,MY_NUM){
+  let COUNT_BALL = 0
+  for(let i=0; i<COM_NUMBER.length; i++){
+    for(let j=0; j<MY_NUM.length;j++){
+      if(COM_NUMBER[i]==MY_NUM[j]){
+        if(i!=j){
+          COUNT_BALL++
         }
       }
     }
   }
-  if(strike==0 && ball == 0){
-    Console.print(`낫싱\n`);
-  } else if(strike!=0 && ball !=0){
-    Console.print(`${ball}볼 ${strike}스트라이크\n`);
-  } else if(strike!=0 && ball ==0){
-    Console.print(`${strike}스트라이크\n`);
-  } else if(strike==0 && ball !=0){
-    Console.print(`${ball}볼\n`);
-  }
-  if(strike==3){
-    Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`)
-    gameset = Console.readLineAsync;
-    if(gameset ==1){
-      new App.play();
-      // 위와 같이 했을때 랜덤생성 컴퓨터 값 포함하여 제대로 재시작 되는지 확인
-    } else if(gameset ==2){
-      return
+return COUNT_BALL;
+}
+strike(COM_NUMBER,MY_NUM){
+  let COUNT_STRIKE = 0
+  for(let i=0; i<COM_NUMBER.length; i++){
+    for(let j=0; j<MY_NUM.length;j++){
+      if(COM_NUMBER[i]==MY_NUM[j]){
+        if(i==j){
+          COUNT_STRIKE++
+        }
+      }
     }
-  } else if(strike!=3){
-    na = Console.readLineAsync;
   }
-})
+return COUNT_STRIKE;
+}
+
+
+// 7. 게임셋 여부
+// 이후 '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.' 표시후 입력값 받음
+// 입력값이 1이면 게임스타트. 단 여기에서 랜덤 숫자를 다시 생성
+// 입력값이 2이면 게임끝
+// 둘다 아니면 에러 메세지 띄우고 종료
+
+gameset(){
+  MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',(setNum)=>{
+    if(setNum==1){
+      this.gameStart(this.ranNumber());
+    } else if(setNum==2){
+      return 
+    } else if(setNum !=1 && setNum!=2){
+      throw MissionUtils.Console.print('[ERROR]1 또는 2를 입력하세요')
+    }
+  })
+} 
+}
 
 export default App;
