75bf309e09bb570d6f0c8414d5c7957901d03e77
yhyem <yoonhyemin0120@gmail.com>
2023-10-23 05:11:43 +0900

    feat: 숫자 일치 여부에 따른 결과 값에 따른 게임 종료 여부 판별

--- src/App.js
+++ src/App.js
@@ -82,6 +82,11 @@ class App {
 
       if (strikeCount === 3) {
         Console.print("3스트라이크");
+        Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+        resetCode = await finish();
+
+        initalizeArray(randomArray);
       } else if (strikeCount > 0 || ballCount > 0) {
         Console.print(ballCount + "볼 " + strikeCount + "스트라이크");
       } else {
