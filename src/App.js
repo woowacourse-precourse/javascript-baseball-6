00408db7a20a85375acfff404ff82b3d4a4aaf34
insidepixce <insidepixce@gmail.com>
2023-10-22 14:41:44 +0900

    계속 숫자가 잘못된 형식이라고 출력됨. 이는 입ㄹ혁값이 형식이 올바르지 않을 때 발생하는데 현재 입력값을 검증하는 isVaildInput 함수가 이루어진 3자리 문자열인지 확인하고 중복된 숫자가 없는지 검사하고 있다. input이 숫자로 이루어진 3자리 문자열인지 확인하는 정규식 ^\d{3}$ 가 입력값에 부합하는지 확인한다 이 정규식은 3자리 숫자만 허용한다
    중복된 숫자가 있는지 확인하는 부분은 이미 구현되어 있는데, 이를 통과하면 true를 반환한다
    
    현재 코드에서는 "낫싱"과 "[ERROR] 숫자가 잘못된 형식입니다."를 구분하지 않고 출력하고 있습니다. "낫싱"인 경우와 숫자 형식이 잘못된 경우를 구분하여 출력하려면, checkGuess 함수에서 조건을 추가하여 구분하고 다른 메시지를 출력하도록 변경할 수 있다.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+const { MissionUtils } = require("@woowacourse/mission-utils");
+
+
 class App {
   constructor() {
     this.secretNumber = this.generateRandomNumber();
@@ -5,7 +8,6 @@ class App {
   }
 
   generateRandomNumber() {
-    // 1부터 9까지 서로 다른 수로 이루어진 3자리 수 생성
     const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     const secretNumber = [];
     for (let i = 0; i < 3; i++) {
@@ -16,20 +18,64 @@ class App {
     return secretNumber.join('');
   }
 
-  play() {
-    console.log("숫자 야구 게임을 시작합니다.");
+  async getInput() {
+    const input = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+    return input;
+  }
 
+  isValidInput(input) {
+    if (typeof input === 'string' && /^\d{3}$/.test(input)) {
+      const guess = input.split('').map(Number);
+      const uniqueDigits = new Set(guess);
+      return uniqueDigits.size === 3;
+    }
+    return false;
+  }
+
+  checkGuess(guess) {
+    if (this.isValidInput(guess)) {
+        // 입력값을 문자열에서 숫자 배열로 변환
+        const guessArray = guess.split('').map(Number);
+
+        let strikes = 0;
+        let balls = 0;
+
+        for (let i = 0; i < 3; i++) {
+            if (guessArray[i] === this.secretNumber[i]) {
+                strikes++;
+            } else if (this.secretNumber.includes(guessArray[i])) {
+                balls++;
+            }
+        }
+
+        if (strikes === 3) {
+            return "3스트라이크";
+        } else if (strikes > 0 || balls > 0) {
+            return `${balls}볼 ${strikes}스트라이크`;
+        } else {
+            return "낫싱";
+        }
+    } else {
+        // 숫자가 잘못된 형식일 때 "[ERROR] 숫자가 잘못된 형식입니다."를 반환
+        return "[ERROR] 숫자가 잘못된 형식입니다.";
+    }
+}
+
+
+  async play() {
+    console.log("숫자 야구 게임을 시작합니다.");
+  
     while (true) {
-      const input = this.getInput();
+      const input = await this.getInput();
       if (input === "2") {
         console.log("게임을 종료합니다.");
         break;
       }
-
+  
       if (this.isValidInput(input)) {
         const result = this.checkGuess(input);
         console.log(result);
-
+  
         if (result === "3스트라이크") {
           console.log("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
           break;
@@ -39,43 +85,5 @@ class App {
       }
     }
   }
-  
-  async getInput() {
-    const input = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
-    return input;
-  }
-  
-  isValidInput(input) {
-    // 입력값이 3자리 숫자인지 확인
-    if (/^\d{3}$/.test(input)) {
-      // 중복된 숫자가 있는지 확인
-      const uniqueDigits = new Set(input.split(''));
-      return uniqueDigits.size === 3;
-    }
-    return false;
-  }
-  
-
-  checkGuess(guess) {
-    let strikes = 0;
-    let balls = 0;
-  
-    for (let i = 0; i < 3; i++) {
-      if (guess[i] === this.secretNumber[i]) {
-        strikes++;
-      } else if (this.secretNumber.includes(guess[i])) {
-        balls++;
-      }
-    }
-  
-    if (strikes === 3) {
-      return "3스트라이크";
-    } else if (strikes > 0 || balls > 0) {
-      return `${balls}볼 ${strikes}스트라이크`;
-    } else {
-      return "낫싱";
-    }
-  }
 }  
-
 module.exports = App;
