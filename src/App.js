495873a36d8b739cb7c571fc9e69e321f5d6c5e7
Yoonyesol <maniac970430@naver.com>
2023-10-22 12:10:17 +0900

    Feat: 컴퓨터 랜덤수 지정 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import GameLogic from "./game/GameLogic.js";
+
 class App {
+  #gameLogic;
+
+  constructor() {
+    this.#gameLogic = new GameLogic();
+  }
+
   async play() {}
 }
+
+const app = new App();
+app.play();
 
 export default App;
