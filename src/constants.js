86180b76a9a4c5698797988a5d6b4eab967eb8d5
mywhzy <cmj2393a@naver.com>
2023-10-22 15:52:19 +0900

    chore: error 코드 대문자로 수정

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ export const PLAY_INPUT_REGEX = /^[1-9]{3}$/;
 export const OPTION_INPUT_REGEX = /^[1-2]{1}$/;
 
 export const ERROR_MESSAGE =
-  '[Error] 입력 값이 잘못된 형식입니다. 게임을 종료합니다.';
+  '[ERROR] 입력 값이 잘못된 형식입니다. 게임을 종료합니다.';
