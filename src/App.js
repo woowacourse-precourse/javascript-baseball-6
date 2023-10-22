3e1876e240d45a7eee8300dcedaf7a7bd6b3b53c
hamo-o <yheel369@gmail.com>
2023-10-23 02:42:48 +0900

    feat: #1 컴퓨터 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
   #answer;
@@ -13,8 +13,25 @@ class App {
     Console.print('숫자 야구 게임을 시작합니다.');
   }
 
+  createAnswer() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    this.#answer = computer;
+  }
+
+  game() {
+    this.createAnswer();
+    Console.print(this.#answer);
+  }
+
   async play() {
     this.printStartMessage();
+    this.game();
   }
 }
 
