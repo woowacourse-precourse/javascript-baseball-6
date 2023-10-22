90e34305246c91463a439fff62794f0036d33e61
Doeunnkimm <doeunnkimm@gmail.com>
2023-10-22 21:52:32 +0900

    feat: size3짜리 랜덤한 수를 배열에 담아주는 유틸 함수

---
+++ utils/getRandomArray.js
@@ -0,0 +1,14 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+function getRandomArray() {
+  const random = [];
+  while (random.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!random.includes(number)) {
+      random.push(number);
+    }
+  }
+  return random;
+}
+
+export default getRandomArray;
