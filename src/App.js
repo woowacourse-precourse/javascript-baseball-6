2fc31082aa6f67047196c7a92ae25eea8b75fb98
hyeona <90515062+BaeHyeonA@users.noreply.github.com>
2023-10-23 02:00:09 +0900

    feat : 컴퓨터 랜덤 값 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
+
+  getRandomNumber(){
+    this.computer = [];
+    while (this.computer.length < 3){
+      const number = Random.pickNumberInRange(1, 9);
+      if(!this.computer.includes(number))
+        this.computer.push(number);
+    }
+  }
+
   async play() {}
 }
 
