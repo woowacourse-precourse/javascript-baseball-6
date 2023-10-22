06e67256d8db2b8f72a7777e5eedd2e9f5f79cd1
useon <yoosk4986@naver.com>
2023-10-22 18:01:51 +0900

    feat: 문구를 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    this.infoPrint('숫자 야구 게임을 시작합니다.');
+  }
+
+  infoPrint(message) {
+    MissionUtils.Console.print(message);
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
