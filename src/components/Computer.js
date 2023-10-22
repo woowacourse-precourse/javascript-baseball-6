51c4a43a8d96196c578e2db83eb23c74c15fab5b
JUN <gurwns232@naver.com>
2023-10-23 01:06:16 +0900

    refactor: Computer클래스 함수 주석 수정

--- src/components/Computer.js
+++ src/components/Computer.js
@@ -5,11 +5,18 @@ class Computer {
     this.computer = [];
   }
 
+  /**
+   * 1~9까지 수 중에서 랜던한 3개의 숫자 리턴하는 함수
+   * @returns {number[]} 랜덤한 3개의 숫자 배열 형태로 리턴
+   */
   getNumber() {
     while (this.computer.length < 3) this.createRandomNumber();
     return this.computer;
   }
 
+  /**
+   * 1~9까지 수 중에서 중복없이 3개의 랜덤한 숫자 뽑는 함수
+   */
   createRandomNumber() {
     const number = Random.pickNumberInRange(1, 9);
     if (!this.computer.includes(number)) this.computer.push(number);
