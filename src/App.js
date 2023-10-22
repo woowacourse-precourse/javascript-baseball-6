b85f7263d58f31ef21af5f091fea19cbcf23d4ef
Jueun Park <38892644+Juu98@users.noreply.github.com>
2023-10-22 03:13:59 +0000

    Added random number generator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 class App {
-  async play() {}
+  constructor() {
+    this.MIN_VALUE = 1;
+    this.MAX_VALUE = 999;
+  }
+
+  async play() {
+    const randomNumber = Random.pickNumberInRange(
+      this.MIN_VALUE,
+      this.MAX_VALUE,
+    ).toString();
+    let strikeCounts = 0;
+    let ballCounts = 0;
+    let isInGame = true;
+
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
 }
 
 export default App;
