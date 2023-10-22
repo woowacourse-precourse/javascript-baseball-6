59f5aa2180e13b3bc6d07b02e7f7b2ba2e6ef8f7
novice-hero <highero700@gmail.com>
2023-10-22 15:27:05 +0900

    Feat: constuctor, 유효성 검사 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
-  async makeComputerNumber() {
+  constructor() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    this.computerNumber = this.makeComputerNumber();
+  }
+
+  makeComputerNumber() {
     const computerNumber = [];
 
     while (computerNumber.length < 3) {
@@ -14,7 +19,7 @@ class App {
     return computerNumber;
   }
 
-  async checkStrikeAndBall(computerNumber, userNumber) {
+  checkStrikeAndBall(computerNumber, userNumber) {
     let strike = 0;
     let ball = 0;
     // 스트라이크, 볼, 낫싱 판단
@@ -22,17 +27,34 @@ class App {
       if (computerNumber[i] === userNumber[i]) strike += 1;
       else if (computerNumber.includes(userNumber[i])) ball += 1;
     }
-
     return [strike, ball];
   }
 
-  async play() {
-    let isStart = true;
+  checkValidateInput(input) {
+    // 잘못된 입력 시 에러 던지기
+    for (let i = 0; i < input.length; i++) {
+      if (Number.isNaN(input[i])) {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      }
+    }
+    if (input.length !== 3) {
+      throw new Error("[ERROR] 숫자가 3자리가 아닙니다.");
+    }
+  }
 
-    Console.print("숫자 야구 게임을 시작합니다.");
-    while (isStart) {
-      let isCorrect = false;
-      const computerNumber = await this.makeComputerNumber();
+  async play() {
+    // 맞출 때까지 반복
+    while (true) {
+      // 숫자 입력
+      const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
+      const userInputNumber = input.split("").map((num) => parseInt(num));
+
+      this.checkValidateInput(userInputNumber);
+
+      const [strike, ball] = this.checkStrikeAndBall(
+        this.computerNumber,
+        userInputNumber
+      );
     }
   }
 }
