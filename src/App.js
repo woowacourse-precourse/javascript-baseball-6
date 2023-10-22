bcd344de26b187a1885f9fc6dcc010e7af8226fd
YeungJin <143728232+0calCOKE@users.noreply.github.com>
2023-10-22 21:38:34 +0900

    임시저장

--- src/App.js
+++ src/App.js
@@ -1,4 +1,28 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
+
+  start() {
+    Console.print("숫자 야구 게임을 시작합니다.")
+  }
+
+  generateRandomNumber() {
+    const computerNumber = [];
+    while (randomNumber.length < 3) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerNumber.includes(randomNumber)) {
+        computerNumber.push(number)
+      }
+    }
+
+    const computerNumberToString = computerNumber.join();
+    return computerNumberToString;
+  }
+
+  getUserInput () {
+    
+  }
+
   async play() {}
 }
 
