a7a21e5b3cd9d8b53ea633b1ac890d0067d72db8
Jueun Park <84198371+jueun-park98@users.noreply.github.com>
2023-10-22 04:29:12 +0000

    Implemented score validation

--- src/App.js
+++ src/App.js
@@ -17,14 +17,30 @@ class App {
 
       while (isInGame) {
         const answer = await Console.readLineAsync("숫자를 입력해주세요. : ");
-        
+
         if (!this.isInteger(answer) || !this.isInRange(answer)) {
           throw new Error("[ERROR] 숫자가 잘못된 형식입니다." + answer);
         }
 
         score = this.getScore(randomNumber, answer);
+
+        if (score[0] === 3) {
+          Console.print("3스트라이크");
+          Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+          //this.restartGame();
+          isInGame = false;
+        } else if (score[0] === 0 && score[1] === 0) {
+          Console.print("낫싱");
+        } else if (score[0] > 0 && score[1] === 0) {
+          Console.print(score[0] + "스트라이크");
+        } else if (score[0] === 0 && score[1] > 0) {
+          Console.print(score[1] + "볼");
+        } else {
+          Console.print(score[1] + "볼 " + score[0] + "스트라이크");
+        }
       }
     } catch (error) {
+      isInGame = false;
       await Promise.reject(error);
     }
   }
@@ -33,11 +49,11 @@ class App {
     const randomNumberArray = randomNumber.split("");
     const answerArray = answer.split("");
     const score = [0, 0];
-    
+
     answerArray.forEach((number, index) => {
-      if( number === randomNumberArray[index] ){
+      if (number === randomNumberArray[index]) {
         score[0]++;
-      } else if ( randomNumberArray.includes(number) ){
+      } else if (randomNumberArray.includes(number)) {
         score[1]++;
       }
     });
