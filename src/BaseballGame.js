66af04d1932279b8b3bd256da9d8cfbc454ed6b6
youjeong Jeong <wjddbwjd1205@naver.com>
2023-10-22 16:40:08 +0900

    feat: 플레이어에게 숫자를 입력받는 기능 추가

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -10,38 +10,39 @@ export class BaseballGame {
         Console.print('숫자 야구 게임을 사작합니다.');
         const computer = new Computer();
         const answer = computer.createRandomNum();
-        try {
-            const userInput = await Console.readLineAsync('숫자를 입력해주세요: ');
-            const userNumber = userInput.split('').map(Number);
-            this.compare(answer, userNumber);
-        } catch (error) {
-            throw error;
+        console.log(answer); //테스트를 위한 출력
+        let pitch = false;
+        while (!pitch) {
+            try {
+                const userInput = await Console.readLineAsync('숫자를 입력해주세요: ');
+                if (isNaN(userInput)) {
+                    throw new Error("[ERROR]");
+                }
+
+                const userNumber = userInput.split('').map(Number);
+                if (userNumber.length != 3) {
+                    throw new Error("[ERROR]");
+                }
+                if (userNumber[0] == userNumber[1] || userNumber[1] == userNumber[2] || userNumber[0] == userNumber[2]) {
+                    throw new Error("[ERROR]");
+                }
+
+                pitch = this.compare(answer, userNumber);
+
+            } catch (error) {
+                Console.print(error.message);
+                return false;
+            }
         }
-        
+        if (pitch) {
+            Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+            return true;
+        }
+
     }
 
     //숫자 비교 함수
     compare(answer, userNumber) {
-        let strikeCount = 0;
-        let ballCount = 0;
-        answer.forEach((element1, index1) => {
-            userNumber.forEach((element2, index2) => {
-                if(element1 == element2 && index1 == index2) strikeCount++;
-                else if (element1 == element2) ballCount++;
-            })
-        });
         
-        if(strikeCount != 0 && ballCount != 0){
-            Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
-        }
-        else if (strikeCount != 0 && ballCount == 0) {
-            Console.print(`${strikeCount}스트라이크`);
-        }
-        else if (strikeCount == 0 && ballCount != 0) {
-            Console.print(`${ballCount}볼`);
-        }
-        else {
-            Console.print('낫싱');
-        }
     }
 }
