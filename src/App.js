8c015d3de136a9ae1e3aea816d102ca4399db9c6
김경주 <popo505@naver.com>
2023-10-22 16:52:25 +0900

    feat:유효성 확인

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import { GuildEmojiRoleManager } from "discord.js";
 
 class App {
   //게임시작
-  start(){ 
-    Console.print("숫자 야구 게임을 시작합니다."); 
+  start() {
+    Console.print("숫자 야구 게임을 시작합니다.");
   }
-  
+
   //랜덤으로 숫자 생성하기
   randomNumber() {
     const computerArr = [];
@@ -15,22 +16,30 @@ class App {
         computerArr.push(number);
       }
     }
-    const computer =  computerArr.join('');
+    const computer = computerArr.join('');
     return computer;
   }
 
   //사용자로부터 값 입력받기
   async getNumbers() {
-      const numbers = await Console.readLineAsync('숫자를 입력해주세요 : ');
-      Console.print('');
-      return numbers;
+    const numbers = await Console.readLineAsync('숫자를 입력해주세요 : ');
+    Console.print('');
+    return numbers;
   }
-  
-  //값이 유효한지 확인
+
+  //값이 유효한지 확인, 유효하지 않으면 throw
+
+  exception(numbers) {
+    if(numbers.length !==3){
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+    if(!/^\d+$/.test(numbers)){
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+  }
 
   async play() {
     this.start();
-    this.getNumbers();
   }
 }
 
