12736f227966e2d3832155aa67fea0f17a41d967
SU_VIN <pyotnqls25@naver.com>
2023-10-22 15:58:06 +0900

    Feat: 재시작 여부 출력 후 기능 구현

--- src/MainGame.js
+++ src/MainGame.js
@@ -68,7 +68,19 @@ class MainGame {
     } else this.getUserInput();
   };
 
-  isRestartOrExit = () => {};
+  isRestartOrExit = async () => {
+    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    const input = await Console.readLineAsync("");
+
+    if (input === "1") {
+      this.start();
+    } else if (input === "2") {
+      return;
+    } else {
+      throw new Error("[ERROR] 선택지에 있지 않습니다.");
+    }
+  };
 }
 
 export { MainGame };
