3a0b9cbf320d4d73c360e761bdf2a0d83551e5bd
hanyoung <phanyoung@naver.com>
2023-10-22 18:19:59 +0900

    feat: 정답으로 사용할 서로 다른 숫자 세 자리를 무작위로 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    const answer = App.#pickRandomThreeNums();
+  }
+
+  static #pickRandomThreeNums() {
+    const RESULT_SIZE = 3;
+    const pickedNumbers = new Array(RESULT_SIZE).fill().map((_) => Random.pickNumberInRange(1, 9));
+
+    const isDuplicate = new Set(pickedNumbers).size !== RESULT_SIZE;
+    return !isDuplicate ? pickedNumbers : App.#pickRandomThreeNums();
   }
 }
 
