50a1fe201e8cebf941b59b34732999229c415ba5
Trophy198 <spsp8426@naver.com>
2023-10-22 19:42:45 +0900

    Refactor : Computer 클래스에서 숫자를 상수로 변경 및 let을 const로 변경

--- src/Computer.js
+++ src/Computer.js
@@ -1,10 +1,10 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { MAX_NUMBER, MIN_NUMBER } from "./constants/NumberConstants";
+import { MAX_NUMBER, MIN_NUMBER, NUM_DIGITS } from "./constants/NumberConstants.js";
 
 export default class Computer {
   generateNumbers() {
-    let numbers = [];
-    while (numbers.length < 3) {
+    const numbers = [];
+    while (numbers.length < NUM_DIGITS) {
       const number = MissionUtils.Random.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
       if (!numbers.includes(number)) {
         numbers.push(number);
