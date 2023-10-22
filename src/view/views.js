6809bc18330bd32dda8c2764653a940a5feb1000
jonique98 <tnals9899@naver.com>
2023-10-22 23:24:47 +0900

    refactor: 결과값에 대한 string을 만들어 출력해주는 메소드 작성

--- src/view/views.js
+++ src/view/views.js
@@ -5,9 +5,28 @@ export default class views{
   constructor() {
     Console.print(MESSAGE.START);
   }
+
+  printResultMessage(result) {
+    
+    if (result.BALL === 0 && result.STRIKE === 0) {
+      return Console.print(MESSAGE.NOTHING);
+    }
+
+    let resultMessage = '';
+
+    if (result.BALL !== 0) {
+      resultMesage = resultMesage.concat(String(result.BALL) + Mesage.BALL + ' ');
+    }
+  
+    if (result.STRIKE !== 0) {
+      resultMesage = resultMesage.concat(String(result.STRIKE) + Mesage.STRIKE + ' ');
+    }
+
+    return Console.print(resultMESSAGE);
+  }
   
   printMessage(message) {
-    Console.print(message);
+    return Console.print(message);
   }
 
   async readInput(message) {
