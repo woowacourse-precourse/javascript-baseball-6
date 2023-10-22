e4ad17bb3cc0f8fe0e995a12efb7081c4479b10e
bmj0053 <bmj05378@gmail.com>
2023-10-22 23:50:41 +0900

    Validation.js 추가

---
+++ src/Validation.js
@@ -0,0 +1,14 @@
+function validationNumbers(inputNumbers) {
+  const inputNumbersSet = new Set(inputNumbers);
+
+  return (
+    // 문자열의 길이 확인
+    inputNumbers.length === 3
+    // 문자를 숫자로 변환
+    && inputNumbers.every((inputNumber) =>
+      Number.isInteger(parseInt(inputNumber, 10)) 
+    )
+    // 중복값 확인
+    && inputNumbers.length === inputNumbersSet.size
+  )
+}
