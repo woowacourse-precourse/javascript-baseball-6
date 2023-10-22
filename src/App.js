1b4059d5017715d0d9ccf1c9edb3c396bbadd9e6
김다인 <dbslzhs1218@naver.com>
2023-10-23 04:34:02 +0900

    feat: 숫자 입력 및 입력 값 유효성 검사 기능 추가
    - MissionUtils 라이브러리의 Console.readLineAsync() 메서드를 통해 입력 기능 구현
    - isValidUserNum(): 사용자가 잘못된 값을 입력했을 때 false를 리턴
      - 입력 값 길이가 3이 아닐 때
      - 입력 값이 중복되었을 때
      - 입력 값에 숫자가 아닐 때
    - numCorrect(): isValidUserNum()이 false를 리턴했을 때 throw문을 사용해 예외 발생,
    true를 리턴했을 때 게임 진행

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import {MissionUtils} from '@woowacourse/mission-utils';
+
+// 숫자 입력 예외 처리하기 
 class App {
   constructor() {
+    this.init();
   }
 
-  computerPickNumber() {
+  init() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.computerNum = this.randomPickNum();
+  }
+
+  randomPickNum() {
     const computer = [];
     while (computer.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
@@ -14,10 +22,34 @@ class App {
     return computer;
   }
   
+  isValidUserNum(userNum) {
+    if(userNum.length !== 3 ) {
+      return false
+    }
+    if(userNum.charAt(0) === userNum.charAt(1) || userNum.charAt(1) === userNum.charAt(2) || userNum.charAt(2) === userNum.charAt(0)) {
+      return false
+    }
+    if(!/^[1-9]{3}$/.test(userNum)) {
+      return false
+    }
+    return true
+  }
 
+  async numCorrect() {
+    try {
+      const userNum = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+      if(this.isValidUserNum(userNum)) {
+        console.log('00');
+      }else{
+        throw new Error('서로 다른 숫자 3개를 입력해주세요.');
+      }
+    }catch(e) {
+      console.log(e)
+    }
+  }
+  
   async play() {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    this.computerPickNumber()
+    this.numCorrect();
   }
 }
 
