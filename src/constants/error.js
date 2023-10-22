ade594093654b6131d2fd6167b740d27e630682b
cobocho <rlaalsrb1111@naver.com>
2023-10-22 14:13:01 +0900

    Feat: `TargetBalls` 예외 처리 구현

--- src/constants/error.js
+++ src/constants/error.js
@@ -6,9 +6,15 @@ export const ERROR_MESSAGE = Object.freeze({
   COMMON: Object.freeze({
     NOT_NUMBER: '숫자를 입력해주세요!',
     NOT_INTEGER: '정수를 입력해주세요!',
+    NOT_ARRAY: '배열을 입력해주세요!',
   }),
 
   TARGET_BALL: Object.freeze({
     OUT_OF_RANGE: ERROR_MESSAGE_GENERATOR.OUT_OF_RANGE(1, 9),
+  }),
+
+  TARGET_BALLS: Object.freeze({
+    NOT_VALID_QUANTITY: '3개의 숫자를 가진 배열을 입력해주세요!',
+    IS_DUPLICATED: '중복되지 않는 숫자들로 입력해주세요!',
   }),
 });
--- src/domain/TargetBalls.js
+++ src/domain/TargetBalls.js
@@ -1,9 +1,15 @@
+import { ERROR_MESSAGE } from '../constants/error';
+import CustomError from '../exceptions/CustomError';
+import { isDuplicated } from '../utils/validator';
 import { TargetBall } from './TargetBall';
 
 export class TargetBalls {
   #balls;
 
+  static BALL_QUANTITY = 3;
+
   constructor(numbers) {
+    this.#validate(numbers);
     this.#balls = numbers.map(TargetBall.valueOf);
   }
 
@@ -13,5 +19,17 @@ export class TargetBalls {
 
   get balls() {
     return this.#balls;
+  }
+
+  #validate(numbers) {
+    if (!Array.isArray(numbers)) {
+      throw new CustomError(ERROR_MESSAGE.COMMON.NOT_ARRAY);
+    }
+    if (numbers.length !== TargetBalls.BALL_QUANTITY) {
+      throw new CustomError(ERROR_MESSAGE.TARGET_BALLS.NOT_VALID_QUANTITY);
+    }
+    if (isDuplicated(numbers)) {
+      throw new CustomError(ERROR_MESSAGE.TARGET_BALLS.IS_DUPLICATED);
+    }
   }
 }
