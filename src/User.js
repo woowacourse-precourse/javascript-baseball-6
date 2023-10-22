efc16f51712392aae4bce3ee44594d57340a8233
jaeml00 <jaeml0630@gmail.com>
2023-10-22 10:45:21 +0900

    refactor: edit indentation
    4 -> 2

--- src/User.js
+++ src/User.js
@@ -1,37 +1,36 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils } from '@woowacourse/mission-utils';
 export default class User {
-    #threeNummer; // 입력받은 숫자 배열
-    constructor() {
+  #threeNummer; // 입력받은 숫자 배열
+  constructor() {}
 
+  setThreeNummber() {
+    const number = this.getnumber();
+    this.numberToArray(number);
+    if (this.#threeNummer.length !== 3) {
+      return 1;
     }
+    return 0;
+  }
 
-    setThreeNummber(){
-        const number = this.getnumber();
-        this.numberToArray(number);
-        if (this.#threeNummer.length !== 3) {
-            return 1;
-        } 
-    }
+  numberToArray(n) {
+    this.#threeNummer = [];
+    const str = String(n);
+    const mapfn = (arg) => Number(arg);
+    this.#threeNummer = str.split('').map(mapfn);
+  }
 
-    numberToArray(n) {
-        this.#threeNummer = [];
-        const str = String(n);
-        const mapfn = (arg) => Number(arg);
-        this.#threeNummer = str.split('').map(mapfn);
-    }
+  getThreeNummber() {
+    return this.#threeNummer;
+  }
 
-    getThreeNummber(){
-        return this.#threeNummer;
+  async getnumber() {
+    try {
+      const number = await MissionUtils.Console.readLineAsync(
+        '숫자를 입력해주세요 : '
+      );
+      return number;
+    } catch (error) {
+      return 'nertwork err';
     }
-
-    async getnumber() {
-        try {
-            const number = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
-            return number;
-        }
-        catch (error) {
-            return "nertwork err";
-        }
-    }
+  }
 }
-
