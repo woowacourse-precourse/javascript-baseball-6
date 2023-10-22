c79ee36703805bf0ef47c3e2850a0f8fd87d666b
YeungJin <143728232+0calCOKE@users.noreply.github.com>
2023-10-22 23:22:30 +0900

    초기화 및 랜덤숫자 생성 및 예측결과 재작성

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-
-  start() {
-    Console.print("숫자 야구 게임을 시작합니다.")
-  }
-
-  generateRandomNumber() {
-    const computerNumber = [];
-    while (randomNumber.length < 3) {
-      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computerNumber.includes(randomNumber)) {
-        computerNumber.push(number)
-      }
-    }
-
-    const computerNumberToString = computerNumber.join();
-    return computerNumberToString;
-  }
-
-  getUserInput () {
-    
+  constructor() {
+    this.answerNumber = [];
+    this.gameStart = true;
   }
 
   async play() {}
+
+  generateRandomNumber() {
+    this.answerNumber = [];
+    while (this.answerNumber.length < 3) {
+      const RANDOM_NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.answerNumber.includes(RANDOM_NUMBER)) {
+        this.answerNumber.push(RANDOM_NUMBER);
+      }
+    }
+  }
+
+  checkGuessResult(guess) {
+    let ball = 0;
+    let strike = 0;
+
+    for (let i = 0; i < 3; this.answerNumber[i]) {
+      if (guess[i] == this.answerNumber[i]) {
+        strike++;
+      } else if (this.answerNumber.includes(Number(guess[i]))) {
+        ball++;
+      }
+    }
+
+    if (strike === 0 && ball === 0) {
+      return "낫싱";
+    }
+    // trim으로 결과가 없을때 나오는 공백을 제거
+    return `${ball ? ball + "볼" : ""} ${strike ? strike + "스트라이크" : ""}`.trim();
+  }
 }
 
 export default App;
