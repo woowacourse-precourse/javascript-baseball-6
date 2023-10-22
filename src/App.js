667059be2eb28e8afb966290d11b69afa44031a0
eternalclash <suchasucha@naver.com>
2023-10-22 16:32:23 +0900

    Feat: 정답 채점 기능 및 재시작/종료 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import {
+  NOTHING,
+  QUESTION_MESSAGE,
+  START_MESSAGE,
+  STRIKE,
+  SUCCESS_MESSAGE,
+  BALL,
+  END_MESSAGE
+} from "./Utills.js";
 class App {
   constructor() {
     this.init();
@@ -17,16 +26,58 @@ class App {
       }
     }
 
-    while (True) {
+  
+    while (true) {
+      let strikeResult = 0
+      let ballResult = 0
       try {
-        const numberInput = await Console.readLineAsync(
+        let numberInput = await Console.readLineAsync(
           "숫자를 입력해주세요 : "
         );
-      } catch (error) {}
+        numberInput = numberInput.split("")
+        for (let i = 0; i < answer.length ; i++) {
+          if (answer[i] == numberInput[i]) {
+            strikeResult++
+          }
+          else if (answer.includes(Number(numberInput[i]))) {
+            ballResult++
+          }
+          else {
+
+          }
+        }
+        if (strikeResult==3) {
+          Console.print(strikeResult+STRIKE)
+          break
+        }
+
+        if (strikeResult > 0 && ballResult > 0) {
+          Console.print(ballResult + BALL + " " + strikeResult + STRIKE);
+        } else if (strikeResult == 0 && ballResult > 0) {
+          Console.print(ballResult + BALL);
+        } else if (strikeResult > 0 && ballResult == 0) {
+          Console.print(strikeResult + STRIKE);
+        } else {
+          Console.print(NOTHING);
+        }
+      } catch (error) {
+        console.log("error", error);
+      }
     }
+  return this.endOrReset()
   }
-  async invalidateInput(query) {}
+
+
+  async endOrReset () {
+   const endMessage = await Console.readLineAsync(END_MESSAGE)
+   if (endMessage == 1) {
+      this.play()
+   }
+   else {
+      Console.print("게임 종료")
+   }
+  }
+
 }
-const app = new App();
-app.play();
+
 export default App;
---
+++ src/Utills.js
@@ -0,0 +1,7 @@
+export const STRIKE = "스트라이크";
+export const BALL = "볼";
+export const NOTHING = "낫싱";
+export const SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+export const START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+export const QUESTION_MESSAGE = "숫자를 입력해주세요 : ";
+export const END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
