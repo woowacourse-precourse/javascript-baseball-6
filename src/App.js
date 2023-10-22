dbd2ce6826cf949178c23c690f0f115e33e39062
Hyunji Seok <alleyji126@gmail.com>
2023-10-22 15:28:11 +0900

    restart process add

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ class App {
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 
+    const DIGIT = 3;
     const computer = [];
-    while (computer.length < 3) {
+    while (computer.length < DIGIT) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!computer.includes(number)) {
         computer.push(number);
       }
     }
 
+    console.log(`==== ${computer} ====`);
+
     let user;
     try { 
       user = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
 
-      // 예외 처리
       if (!Number(user)) {
         throw new Error("숫자 이외의 문자는 입력하실 수 없습니다.");
       }
@@ -26,7 +28,7 @@ class App {
       }
 
       if (user.split("").includes("0")) {
-        throw new Error("숫자 0은 입력하실 수 없습니다.")
+        throw new Error("숫자 0은 입력하실 수 없습니다.");
       }
 
       if (user.length !== 3) {
@@ -41,8 +43,6 @@ class App {
       return;
     }
 
-    // 숫자 값 비교
-    const DIGIT = 3;
     let ball = 0;
     let strike = 0;
 
@@ -72,6 +72,29 @@ class App {
     }
 
     MissionUtils.Console.print(result);
+
+    if (strike === 3) {
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      
+      try {
+        const restart = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. \n");
+        
+        if (!(restart === "1" || restart === "2")) {
+          throw new Error("숫자가 잘못된 형식입니다.");
+        }
+
+        if (restart === "1") {
+          this.play();
+        }
+
+        if (restart === "2") {
+          return;
+        }
+      } catch (error) {
+        MissionUtils.Console.print(`[ERROR] ${error.message}`);
+        return;
+      }
+    }
   }
 }
 
