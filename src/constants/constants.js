4a24db759f922487ae1a5dec165de5877a48fb3c
JUN <gurwns232@naver.com>
2023-10-23 01:33:05 +0900

    feat: constants.js 상수값 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,10 @@
 export const GAME_MESSAGE = Object.freeze({
   GAME_START: '숫자 야구 게임을 시작합니다.',
+  INPUT_MESSAGE: '숫자를 입력해주세요 : ',
+});
+
+export const ERROR_MESSAGE = Object.freeze({
+  INPUT_ONLY_NUMBER: '[ERROR] 오직 숫자만 입력해야 합니다.',
+  NUMBER_LENGTH_MUST_THREE: '[ERROR] 숫자의 길이는 3이여야 합니다.',
+  DUPLICATE_NUMBER: '[ERROR] 3개의 숫자는 서로 다른 숫자여야 합니다',
 });
