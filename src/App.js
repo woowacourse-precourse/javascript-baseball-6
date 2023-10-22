070f5174676d540bd7f99d52759b36d23eb25ba6
정해성 <gotjd0230@naver.com>
2023-10-22 17:51:44 +0900

    :sparkles: feat: 예외처리 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import readline from "readline";
+
 class App {
   constructor() {
-    this.play();
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
   }
 
   async play() {
@@ -9,9 +14,35 @@ class App {
 
   playGame() {
     console.log("숫자 야구 게임을 시작합니다.");
+    this.playRound();
+  }
+
+  playRound() {
+    this.rl.question("숫자를 입력해주세요 : ", userInput => {
+      try {
+        this.isValidGuess(userInput);
+        this.evaluateGuess(userInput);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.rl.close();
+      }
+    });
+  }
+
+  isValidGuess(inputNumber) {
+    const regex = /^[1-9]{3}$/;
+    if (!regex.test(inputNumber)) {
+      throw new Error("Invalid guess");
+    }
+  }
+
+  evaluateGuess(inputNumber) {
+    console.log("evaluate", inputNumber);
   }
 }
 
 const app = new App();
+app.play();
 
 export default App;
