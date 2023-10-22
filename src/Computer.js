c8a0708f8f987425162968b74f522b273b7fd2d0
Trophy198 <spsp8426@naver.com>
2023-10-22 12:50:16 +0900

    Refactor : 숫자를 상수명으로 변경

--- src/Computer.js
+++ src/Computer.js
@@ -1,10 +1,11 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { MAX_NUMBER, MIN_NUMBER } from "./constants/NumberConstants";
 
 export default class Computer {
   generateNumbers() {
     let numbers = [];
     while (numbers.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      const number = MissionUtils.Random.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
       if (!numbers.includes(number)) {
         numbers.push(number);
       }
