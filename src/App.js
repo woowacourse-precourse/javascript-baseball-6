cb947483e9aa7241b8895e857a803d74ef30eb92
starcradle101 <dreamerdev12@gmail.com>
2023-10-22 22:16:09 +0900

    🛠️Feat: getUsreInput 메서드 구현 사용자의 입력을 받고 유효성 검사를 진행하는 메서드인 getUserInput 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    // 여기서 부터 시작
+    Console.print('숫자 야구 게임을 시작합니다.');
+    let userGuess = await this.getUserInput();
+    console.log(userGuess);
+    const computerAnswer = this.createComputerAnswer();
+    console.log(computerAnswer);
+  }
+
+  // 사용자 입력을 받는 getUserInput
+  async getUserInput() {
+    try {
+      const answer = await new Promise((resolve) => {
+        Console.readLine('숫자를 입력해주세요 : ', (userInput) => {
+          resolve(userInput);
+        });
+      });
+
+      const userInput = answer.split('');
+
+      if (
+        userInput.length !== 3 ||
+        userInput.some((char) => isNaN(parseInt(char, 10)))
+      ) {
+        throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+      }
+
+      return userInput.map((char) => parseInt(char, 10));
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
 }
 
 export default App;
