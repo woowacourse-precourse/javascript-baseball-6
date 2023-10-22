99907f1f23a54be246d2a1413cf4c98fbb026485
sbemjr1 <sbemjr1@naver.com>
2023-10-22 18:46:09 +0900

    feat:정답 처리 및 재실시(아직 오류 수정 중)

--- src/Game/game.js
+++ src/Game/game.js
@@ -2,13 +2,13 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 import { MakeRandomNumber, isValidUserNumber } from './ball.js';
 
 //game start
-async function getUserInputNumber(randomNumber) {
+export const getUserInputNumber = async (randomNumber) => {
   const USER_INPUT_NUMBER = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
   if (!isValidUserNumber(USER_INPUT_NUMBER)) {
     throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
   }
   HandleGameProcess(stringToNumberArray(USER_INPUT_NUMBER), randomNumber);
-}
+};
 
 //game set
 export const GameSet = async () => {
@@ -21,16 +21,21 @@ export const GameSet = async () => {
   }
 };
 
-const HandleGameProcess = (randomNumber, userInputNumber) => {
+export const HandleGameProcess = (randomNumber, userInputNumber) => {
   const result = CompareNumber(randomNumber, userInputNumber);
   PrintResult(result);
+  if (isCorrectAnswer(result)) {
+    askRetry();
+    return;
+  }
+  getUserInputNumber(randomNumber);
 };
 
-const stringToNumberArray = (string) => {
+export const stringToNumberArray = (string) => {
   return [...string].map((char) => Number(char));
 };
 
-const CompareNumber = (userInputNumber, randomNumber) => {
+export const CompareNumber = (randomNumber, userInputNumber) => {
   const result = {
     ball: 0,
     strike: 0,
@@ -43,7 +48,7 @@ const CompareNumber = (userInputNumber, randomNumber) => {
   return result;
 };
 
-const PrintResult = (result) => {
+export const PrintResult = (result) => {
   const { strike, ball } = result;
   if (strike === 0 && ball === 0) {
     MissionUtils.Console.print('낫싱');
@@ -54,4 +59,32 @@ const PrintResult = (result) => {
   if (ball > 0) resultText.push(ball + '볼');
   if (strike > 0) resultText.push(strike + '스트라이크');
   MissionUtils.Console.print(resultText.join(' '));
+};
+
+export const isCorrectAnswer = (result) => {
+  if (result.strike === 3) {
+    MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    return true;
+  }
+  return false;
+};
+
+export const askRetry = () => {
+  MissionUtils.Console.readLine(
+    '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+    (answer) => {
+      retryOrExit(answer);
+    },
+  );
+};
+
+export const retryOrExit = (answer) => {
+  if (answer === '1') {
+    GameSet();
+    return;
+  }
+  if (answer === '2') {
+    return;
+  }
+  throw new Error('1이나 2가 아닌 값을 잘못 입력하였습니다.');
 };
