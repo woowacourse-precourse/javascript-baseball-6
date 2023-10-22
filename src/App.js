dba821a73689aa407c1076ff2d41315215140405
77JY <kwn03111@naver.com>
2023-10-23 06:48:33 +0900

    refactor: App의 play 메서드 내 while 루프 제어 방식 변경

--- src/App.js
+++ src/App.js
@@ -13,17 +13,16 @@ class App {
   }
 
   async play() {
+    let loop = true
     this.Numbers = randomNumbers(gameSettings.ballSize,gameSettings.ballMinSize,gameSettings.ballMaxSize);
     MissionUtils.Console.print(gameSettings.gameStartMassege);
-    while (true) {
+  
+    while (loop) {
       const USER_INPUT = await MissionUtils.Console.readLineAsync(gameSettings.userInput);
       const DIGIT = InputValidator(USER_INPUT, gameSettings);
       const { BALLS, STRIKES } = matchNumberPositions(this.Numbers, DIGIT, gameSettings.ballSize);
       const RESULT = checkBaseballResult(BALLS, STRIKES, gameSettings);
-      const RESTART = await GameRestartPrompter(RESULT, gameSettings, this, errorTexts);
-      if (!RESTART){
-        break
-      }
+      loop = await GameRestartPrompter(RESULT, gameSettings, this, errorTexts);
     }
   }
 }
