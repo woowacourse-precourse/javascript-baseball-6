f170d5b853fd863802ac27256ec4b1025d207298
meunrud <113085963+meunrud@users.noreply.github.com>
2023-10-22 23:37:01 +0900

    test

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App {
   }
 
   displayGameStartMessage() {
-    const gameStartMessage = '숫자 야구 게임을 시작합니다.';
+    const gameStartMessage = 'test';
     Console.print(gameStartMessage);
   }
 
