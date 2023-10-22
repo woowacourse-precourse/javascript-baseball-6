2a621be927c60e1d5841d4786cfca1683581d346
nagyeongyun <skruddbs99@naver.com>
2023-10-22 16:58:28 +0900

    feat: 서로 다른 3자리 수 입력

--- src/App.js
+++ src/App.js
@@ -14,4 +14,11 @@ function generateComputerNumber() {
   }
 }
 
+async function getUserNumber() {
+  let user = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+  if (user.length !== 3 || new Set(user).size !== 3) {
+    throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+  }
+}
+
 export default App;
