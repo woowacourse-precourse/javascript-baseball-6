aba5a78fe781551ee875bfcb20c9461c685788d5
unboxing96 <unboxing96@gmail.com>
2023-10-23 03:34:03 +0900

    feat: 기능1. 정답 생성 [완료]

---
+++ src/Computer/Computer.js
@@ -0,0 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class Computer {
+  generateAnswer() {
+    const computer = [];
+
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
+}
+
+export default Computer;
