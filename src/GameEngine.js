3a55e128dac1a7e9ccbea53a00dd10c0ed15ebc8
j1hwan <jihwan@pm.me>
2023-10-22 20:00:11 +0900

    feat: 게임시작, 게임승리, 재시작, 카운트 상태알림 기능 추가

---
+++ src/GameEngine.js
@@ -0,0 +1,44 @@
+import { Console } from '@woowacourse/mission-utils';
+import { BASEBALL_BALL, BASEBALL_NOTHING, BASEBALL_STRIKE, GAME_END_MESSAGE, GAME_START_MESSAGE, GAME_WIN_MESSAGE } from './constants.js';
+
+export default class GameEngine {
+  static showStartMessage() {
+    Console.print(GAME_START_MESSAGE);
+  }
+  
+  static ballCount(userNumbers, computerNumbers) {
+    let strike = 0;
+    let ball = 0;
+
+    const computerNumberSet = new Set(computerNumbers);
+
+    for (let i = 0; i < 3; i++) {
+      if (userNumbers[i] === computerNumbers[i]) {
+        strike += 1;
+      } else if (computerNumberSet.has(userNumbers[i])) {
+        ball += 1;
+      }
+    }
+
+    return { strike, ball };
+  }
+
+  static showGameStatus(strike, ball) {
+    if (strike === 0 && ball === 0) {
+      Console.print(BASEBALL_NOTHING);
+    } else {
+      const results = [];
+      if (ball > 0) results.push(`${ball}${BASEBALL_BALL}`);
+      if (strike > 0) results.push(`${strike}${BASEBALL_STRIKE}`);
+      Console.print(results.join(' '));
+    }
+  }
+
+  showWinMessage() {
+    Console.print(GAME_WIN_MESSAGE);
+  }
+
+  async showEndMessage() {
+    return await Console.readLineAsync(GAME_END_MESSAGE);
+  }
+}
