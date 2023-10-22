3765fbf60980a495ec58f8ec42fc0948b7eea767
insidepixce <insidepixce@gmail.com>
2023-10-22 12:20:04 +0900

    타임아웃 에러 개선

--- src/App.js
+++ src/App.js
@@ -1,115 +1,81 @@
-const readline = require('readline');
-const { MissionUtils } = require("@woowacourse/mission-utils");
-
 class App {
   constructor() {
-    this.computerNumbers = this.generateRandomNumbers();
+    this.secretNumber = this.generateRandomNumber();
     this.attempts = 0;
-    this.rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
   }
 
-  async play() {
-    console.log('숫자 야구 게임을 시작합니다.');
+  generateRandomNumber() {
+    // 1부터 9까지 서로 다른 수로 이루어진 3자리 수 생성
+    const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const secretNumber = [];
+    for (let i = 0; i < 3; i++) {
+      const randomIndex = Math.floor(Math.random() * digits.length);
+      secretNumber.push(digits[randomIndex]);
+      digits.splice(randomIndex, 1);
+    }
+    return secretNumber.join('');
+  }
+
+  play() {
+    console.log("숫자 야구 게임을 시작합니다.");
 
     while (true) {
-      const userInput = await this.getUserInput();
-
-      if (userInput === '2') {
-        console.log('게임을 종료합니다.');
+      const input = this.getInput();
+      if (input === "2") {
+        console.log("게임을 종료합니다.");
         break;
       }
 
-      if (this.isValidInput(userInput)) {
-        const result = this.calculateResult(userInput);
+      if (this.isValidInput(input)) {
+        const result = this.checkGuess(input);
+        console.log(result);
 
-        if (result === '3스트라이크') {
-          console.log('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-          if (await this.askForRestart()) {
-            this.computerNumbers = this.generateRandomNumbers();
-            this.attempts = 0;
-          } else {
-            console.log('게임을 종료합니다.');
-            break;
-          }
-        } else {
-          console.log(result);
+        if (result === "3스트라이크") {
+          console.log("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+          break;
         }
       } else {
-        console.log('[ERROR] 숫자가 잘못된 형식입니다.');
+        console.log("[ERROR] 숫자가 잘못된 형식입니다.");
       }
     }
   }
-
-  generateRandomNumbers() {
-    const computer = [];
-    while (computer.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer.push(number);
-      }
+  
+  async getInput() {
+    const input = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+    return input;
+  }
+  
+  isValidInput(input) {
+    // 입력값이 3자리 숫자인지 확인
+    if (/^\d{3}$/.test(input)) {
+      // 중복된 숫자가 있는지 확인
+      const uniqueDigits = new Set(input.split(''));
+      return uniqueDigits.size === 3;
     }
-    return computer;
+    return false;
   }
+  
 
-  async getUserInput() {
-    while (true) {
-      const userInput = await this.questionAsync('숫자를 입력해주세요 : ');
-
-      if (this.isValidInput(userInput)) {
-        return userInput;
-      } else if (userInput === '2') {
-        return userInput;
-      } else {
-        console.log('[ERROR] 숫자가 잘못된 형식입니다.');
-      }
-    }
-  }
-
-  async askForRestart() {
-    const choice = await this.questionAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: ');
-    return choice === '1';
-  }
-
-  questionAsync(prompt) {
-    return new Promise((resolve) => {
-      this.rl.question(prompt, resolve);
-    });
-  }
-
-  isValidInput(userInput) {
-    return /^\d{3}$/.test(userInput) &&
-      new Set(userInput.split('')).size === 3;
-  }
-
-  calculateResult(userInput) {
-    this.attempts++;
-    const userNumbers = userInput.split('').map(Number);
-    let strike = 0;
-    let ball = 0;
-
+  checkGuess(guess) {
+    let strikes = 0;
+    let balls = 0;
+  
     for (let i = 0; i < 3; i++) {
-      if (userNumbers[i] === this.computerNumbers[i]) {
-        strike++;
-      } else if (this.computerNumbers.includes(userNumbers[i])) {
-        ball++;
+      if (guess[i] === this.secretNumber[i]) {
+        strikes++;
+      } else if (this.secretNumber.includes(guess[i])) {
+        balls++;
       }
     }
-
-    if (strike > 0 && ball > 0) {
-      return `${ball}볼 ${strike}스트라이크`;
-    } else if (strike > 0) {
-      return `${strike}스트라이크`;
-    } else if (ball > 0) {
-      return `${ball}볼`;
+  
+    if (strikes === 3) {
+      return "3스트라이크";
+    } else if (strikes > 0 || balls > 0) {
+      return `${balls}볼 ${strikes}스트라이크`;
     } else {
-      return '낫싱';
+      return "낫싱";
     }
   }
-}
+}  
 
-const app = new App();
-app.play();
 module.exports = App;
