83fd6075e3580a3025ef5f13c91a4ac64146b3f0
Jueun Park <84198371+jueun-park98@users.noreply.github.com>
2023-10-22 03:26:48 +0000

    Added user input to play with 3 numbers

--- src/App.js
+++ src/App.js
@@ -14,6 +14,11 @@ class App {
     let isInGame = true;
 
     Console.print("숫자 야구 게임을 시작합니다.");
+
+    while (isInGame) {
+        const number = await Console.readLineAsync("숫자를 입력해주세요. : ");
+        
+    }
   }
 }
 
