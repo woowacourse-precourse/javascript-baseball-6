f4e2dba84bfa33537755dee18104a6105c350034
no-pla <songjihyun323@gmail.com>
2023-10-22 15:55:33 +0900

    refactor: 게임 제작 로직 분리

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   // 게임 시작 전
   async play() {
+    try {
+      const computerNumber = this.generateGame();
+      await this.game(computerNumber);
+    } catch (error) {
+      throw error;
+    }
+  }
+  generateGame() {
+    // 게임 제작
     const computerNumber = [];
     while (computerNumber.length < 3) {
-      // 컴퓨터 숫자
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!computerNumber.includes(number)) {
         computerNumber.push(number);
       }
     }
-
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    this.game(computerNumber);
+    return computerNumber;
   }
-  game(number) {}
+  async game(number) {}
 }
 
 const app = new App();
