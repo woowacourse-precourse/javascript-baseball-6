6fd5392a44914776ccc59522af0b8bdfd171b4f2
no-pla <songjihyun323@gmail.com>
2023-10-22 18:00:38 +0900

    feat: 유저 정답 입력 기능 add

--- src/App.js
+++ src/App.js
@@ -21,7 +21,30 @@ class App {
     }
     return computerNumber;
   }
-  async game(number) {}
+  async getUserAnswer() {
+    // 유저 입력 get
+    const answer = await MissionUtils.Console.readLineAsync(
+      "숫자를 입력해주세요 : "
+    );
+    const checkNumber = new RegExp(/[1-9]/); // 1 ~ 9까지의 숫자인지 판별
+    const checkLength = answer.length !== 3; //"" 길이 체크
+    const checkSameNumber = new Set(answer).size !== 3; // 중복된 숫자 유무 판별
+
+    if (!checkNumber.test(answer)) {
+      throw new Error(
+        "[ERROR] 형식이 옳지 않습니다. 답은 1부터 9까지의 숫자를 입력해 주세요."
+      );
+    } else if (checkLength) {
+      throw new Error("[ERROR] 답변은 3자리로 입력해 주세요.");
+    } else if (checkSameNumber) {
+      throw new Error("[ERROR] 답변에 중복된 숫자는 들어갈 수 없습니다.");
+    }
+    return answer;
+  }
+
+  async game(correctAnswer) {
+    const userAnswer = await this.getUserAnswer();
+  }
 }
 
 const app = new App();
