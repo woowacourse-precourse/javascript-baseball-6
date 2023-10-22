5ad6606b5d50023a5b68d5d9b084a328eed2b77c
정성준 <jvn4develop@gmail.com>
2023-10-22 22:38:52 +0900

    fix(isAllNumberDifferent): make isValidate when each index of user input number is unique

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ class App {
 
       // 비교 결과를 출력
       MissionUtils.Console.print(resultText);
-
     }
   }
 
@@ -59,7 +58,7 @@ class App {
 class PlayerInput {
   isValidate(userInput) {
     // 유저 입력값이 유효한지 검사
-    return this.isLengthValid(userInput) && this.isNumberValid(userInput);
+    return this.isLengthValid(userInput) && this.isNumberValid(userInput) && this.isAllNumberDifferent(userInput);
   }
 
   isLengthValid(userInput) {
@@ -75,6 +74,17 @@ class PlayerInput {
     // 유저 입력값이 숫자인지 검사
     if (isNaN(userInput)) {
       MissionUtils.Console.print('숫자를 입력해주세요.');
+      return false;
+    }
+    return true;
+  }
+
+  isAllNumberDifferent(userInput) {
+    // 유저 입력값이 서로 다른 숫자인지 검사
+    const userInputList = userInput.split('');
+    const uniqueUserInputList = [...new Set(userInputList)];
+    if (userInputList.length !== uniqueUserInputList.length) {
+      MissionUtils.Console.print('서로 다른 숫자를 입력해주세요.');
       return false;
     }
     return true;
