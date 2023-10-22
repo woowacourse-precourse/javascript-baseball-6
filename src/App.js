3f0f697e0bf95cb1e54b1e88ee748538719b689b
youjeong Jeong <wjddbwjd1205@naver.com>
2023-10-22 16:42:41 +0900

    feat: 게임을 다시 시작하거나 완전히 종료하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import { BaseballGame } from "./BaseballGame.js";
-
+import { Console } from "@woowacourse/mission-utils";
 class App {
   async play() {
-    const baseballGame =  new BaseballGame();
-    baseballGame.startGame();
+    const baseballGame = new BaseballGame();
+
+    let playAgain = true;
+    while (playAgain) {
+      try {
+        let inning = await baseballGame.startGame();
+        if (!inning) {
+          throw new Error("[ERROR]");
+        }
+        //재시작 여부를 묻는 부분
+        const userInput = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
+        if (userInput == 1) playAgain = true;
+        else playAgain = false;
+      } catch (error) {
+          throw error;
+          break;
+      }
+    }
   }
 }
 
