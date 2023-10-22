aab251b349dbbd731d42591e206efe9d9ecefedf
songhyunseop <gustjq99455352@gmail.com>
2023-10-22 14:18:53 +0900

    Feat: 게임종료 후 재시작 또는 완전종료 기능구현

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   constructor() {
-    this.game = new Game();
+    this.game = new Game(this);
+    this.isPlaying = false;
+    this.userNum = "";
   }
 
   async play() {
-    this.game.randomNum();
-    this.game.isNumber_Same();
+    Console.print("숫자 야구 게임을 시작합니다.");
+    this.isPlaying = true;
+    const data = {};
+    let result;
+
+    while (this.isPlaying) {
+      data["me"] = await this.game.numberValidChk();
+      data["com"] = await this.game.randomNum();
+
+      result = await this.game.isNumber_Same(data);
+
+      if (result === "FAIL") {
+        data["me"] = await this.game.numberValidChk();
+        result = await this.game.isNumber_Same(data);
+      }
+      if (result === "WIN") {
+        await this.game.newGameSwitch();
+      }
+    }
   }
 }
 
--- src/GameControl.js
+++ src/GameControl.js
@@ -1,19 +1,131 @@
-import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
 
 class Game {
-  constructor() {
-    this.Computer = [0, 0, 0];
+  constructor(App) {
+    this.app = App;
   }
 
-  randomNum() {
-    this.Computer.forEach(
-      (_, idx, arr) => (arr[idx] = MissionUtils.Random.pickNumberInRange(1, 9))
-    );
+  async randomNum() {
+    const Computer = [];
 
-    return this.Computer;
+    for (let i = 0; i < 3; i++) {
+      let Numbers = await MissionUtils.Random.pickNumberInRange(1, 9);
+      Computer.push(Numbers);
+    }
+
+    return Computer;
   }
 
-  isNumber_Same() {}
+  async numberValidChk() {
+    const regex = /^[1-9]+$/;
+    let userNum;
+
+    try {
+      userNum = await Console.readLineAsync(`숫자를 입력해주세요 : `);
+
+      console.log(`유저 ${userNum}`);
+
+      const isNumber = regex.test(userNum);
+      const isError = !isNumber || userNum === undefined;
+
+      if (userNum === "1") {
+        return;
+      }
+
+      if (isError || userNum.length !== 3) {
+        console.log(`에러로넘어가${userNum}`);
+        throw Error("[ERROR]");
+      }
+    } catch (e) {
+      Console.print(e.message);
+      this.app.isPlaying = false;
+      throw Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+
+    const userArr = userNum.split("");
+    const User = userArr.map(Number);
+
+    return User;
+  }
+
+  async isNumber_Same(data) {
+    let strike = 0;
+    let ball = 0;
+
+    console.log("데이터");
+    console.log(data);
+    console.log("데이터");
+
+    data["com"].forEach((el, idx) => {
+      if (el === data["me"][idx]) strike++;
+      else if (data["me"].includes(el)) ball++;
+    });
+
+    if (strike === 0 && ball === 0) {
+      Console.print("낫싱");
+      this.app.isPlaying = "FAIL";
+      return "FAIL";
+    }
+    if (strike === 0 && ball !== 0) {
+      Console.print(`${ball}볼`);
+      this.app.isPlaying = "FAIL";
+      return "FAIL";
+    }
+    if (strike !== 0 && strike !== 3 && ball === 0) {
+      Console.print(`${strike}스트라이크`);
+      this.app.isPlaying = "FAIL";
+      return "FAIL";
+    }
+    if (strike !== 0 && strike !== 3 && ball !== 0) {
+      Console.print(`${ball}볼 ${strike}스트라이크`);
+      this.app.isPlaying = "FAIL";
+      return "FAIL";
+    }
+
+    if (strike === 3) {
+      Console.print("3스트라이크");
+      this.app.isPlaying = "FAIL";
+      return "WIN";
+    }
+  }
+
+  //   async restart(Computer) {
+  //     const data = {};
+
+  //     data["me"] = await this.numberValidChk();
+  //     data["com"] = Computer;
+
+  //     const result = await this.isNumber_Same(data);
+
+  //     if (result === "WIN") {
+  //       Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  //       Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+  //       return "WIN";
+  //     }
+  //     if (result === "FAIL") {
+  //       return "FAIL";
+  //     }
+  //   }
+
+  async newGameSwitch() {
+    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    try {
+      const userNum = await Console.readLineAsync(`숫자를 입력해주세요 : `);
+
+      console.log(`뭐누름 ${userNum}`);
+
+      if (userNum === "1") {
+        this.app.isPlaying = "reStart";
+      } else if (userNum === "2") {
+        Console.print("게임 종료");
+        this.app.isPlaying = false;
+        return;
+      } else throw Error("[ERROR]");
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 }
 
 export default Game;
