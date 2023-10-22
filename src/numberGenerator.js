2d913a6b42b42b2f47c4d58ca65bfae6220344ba
1eecan <busyx2modernsociety@gmail.com>
2023-10-22 22:07:43 +0900

    feat: 랜덤숫자생성함수 구현

---
+++ src/numberGenerator.js
@@ -0,0 +1,12 @@
+import { Random } from "@woowacourse/mission-utils";
+
+export default function numberGenerator() {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer;
+}
