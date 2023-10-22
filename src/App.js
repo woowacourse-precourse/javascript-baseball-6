5fd89f3237afe98693d2f38667181d9841f1aace
westofsky <westofsky159@gmail.com>
2023-10-22 22:53:10 +0900

    feat: 게임 초기 세팅 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from '@woowacourse/mission-utils';
+import { COMMAND } from './Constant.js';
 class App {
-  async play() {}
+	async play() {
+		this.showStartMessage();
+		this.gameStart();
+	}
+	showStartMessage() {
+		Console.print(COMMAND.START);
+	}
+	async gameStart() {
+		const userInput = await Console.readLineAsync('숫자를 입력해주세요 : ');
+	}
 }
-
+const app = new App();
+app.play();
 export default App;
---
+++ src/Constant.js
@@ -0,0 +1,3 @@
+export const COMMAND = {
+	START: '숫자 야구 게임을 시작합니다.',
+};
