c69f535825a57f064a0e9221b3fe575cf118e349
hamo-o <yheel369@gmail.com>
2023-10-23 02:37:22 +0900

    feat: #1 게임 시작 메시지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  #answer;
+  #hint;
+
+  constructor() {
+    this.#answer = [];
+    this.#hint = { strike: 0, ball: 0 };
+  }
+
+  printStartMessage() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  async play() {
+    this.printStartMessage();
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
