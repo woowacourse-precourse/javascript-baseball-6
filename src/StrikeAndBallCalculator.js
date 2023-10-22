31437bd3f6f47f2cebd3eae79004d6f51b2268e0
Trophy198 <spsp8426@naver.com>
2023-10-22 19:39:09 +0900

    Refactor : StrikeAndBallCalculator 클래스를 함수로 변경

--- src/StrikeAndBallCalculator.js
+++ src/StrikeAndBallCalculator.js
@@ -1,18 +1,16 @@
-export default class StrikeAndBallCalculator {
-  static calculate(userNumbers, computerNumbers) {
-    let strike = 0;
-    let ball = 0;
+export function calculateStrikeAndBall(userNumbers, computerNumbers) {
+  let strike = 0;
+  let ball = 0;
 
-    const computerNumberSet = new Set(computerNumbers);
+  const computerNumberSet = new Set(computerNumbers);
 
-    for (let i = 0; i < 3; i++) {
-      if (userNumbers[i] === computerNumbers[i]) {
-        strike++;
-      } else if (computerNumberSet.has(userNumbers[i])) {
-        ball++;
-      }
+  userNumbers.forEach((number, index) => {
+    if (number === computerNumbers[index]) {
+      strike++;
+    } else if (computerNumberSet.has(number)) {
+      ball++;
     }
+  });
 
-    return { strike, ball };
-  }
+  return { strike, ball };
 }
