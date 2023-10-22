99f23ee9a67a33a147272adab614d726d78fc4c0
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-10-23 00:13:07 +0900

    Refactor: 함수명 수정 및 사용 중인 모듈 변경

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App {
     this.message;
   }
 
-  computerInputNumbers() {
+  createComputerAnswers() {
     const computer = [];
     while (computer.length < 3) {
       const number = Random.pickNumberInRange(1, 9);
@@ -19,32 +19,32 @@ class App {
     this.computerAnswers = computer;
   }
 
-  userInputNumbers() {
-    Console.readLine(
-      "1~9 숫자 내에서 중복 없이 임의의 숫자 3자리를 입력하세요.",
-      (nums) => {
-        const PICK_USER_NUMBERS = nums.split("").map(Number);
-
-        if (PICK_USER_NUMBERS.length !== 3)
-          throw new Error(MESSAGES.GAME_ERROR.LENGTH_VALID);
-
-        const CORRECT_USER_NUMBERS_IN_RANGE = (nums) => 1 <= nums && nums < 10;
-        if (!PICK_USER_NUMBERS.every(CORRECT_USER_NUMBERS_IN_RANGE))
-          throw new Error(MESSAGES.GAME_ERROR.RANGE_VALID);
-
-        const DUPLICATE_USER_NUMBERS_IN_ARRAY = [...new Set(arr)];
-        if (DUPLICATE_USER_NUMBERS_IN_ARRAY.length !== nums.length)
-          throw new Error(MESSAGES.GAME_ERROR.DUPLICATE_VALID);
-
-        this.userAnswer = nums;
-      }
+  async getUserAnswers() {
+    const user = await Console.readLineAsync(
+      "1~9 숫자 내에서 중복 없이 임의의 숫자 3자리를 입력하세요."
     );
+
+    const userAnswers = user.split("").map(Number);
+
+    if (userAnswers.length !== 3)
+      throw new Error(MESSAGES.GAME_ERROR.LENGTH_VALID);
+
+    const CORRECT_USER_NUMBERS_IN_RANGE = (userAnswer) =>
+      1 <= userAnswer && userAnswer < 10;
+    if (!userAnswers.every(CORRECT_USER_NUMBERS_IN_RANGE))
+      throw new Error(MESSAGES.GAME_ERROR.RANGE_VALID);
+
+    const DUPLICATE_USER_NUMBERS_IN_ARRAY = [...new Set(userAnswers)];
+    if (DUPLICATE_USER_NUMBERS_IN_ARRAY.length !== user.length)
+      throw new Error(MESSAGES.GAME_ERROR.DUPLICATE_VALID);
+
+    this.userAnswers = userAnswers;
   }
 
-  play() {
+  async play() {
     Console.print(MESSAGES.GAME_PROCESS.START);
-    this.computerInputNumbers();
-    this.userInputNumbers();
+    this.createComputerAnswers();
+    await this.getUserAnswers();
   }
 }
 
