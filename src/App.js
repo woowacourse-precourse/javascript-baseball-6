75db82136a8418e916ddb3fbaf3cf51e83ba8cde
hanyoung <phanyoung@naver.com>
2023-10-22 18:41:39 +0900

    feat: 사용자 입력 값에 대해 유효성 검사

--- src/App.js
+++ src/App.js
@@ -3,9 +3,26 @@ import { Console, Random } from "@woowacourse/mission-utils";
 class App {
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+
     const answer = App.#pickRandomThreeNums();
 
     const answerInput = await Console.readLineAsync("숫자를 입력해주세요 : ");
+    const parsedInput = answerInput.split("").map((char) => parseInt(char, 10));
+
+    // 유효성 검사
+    const EXPECTED_LENGTH = 3;
+    if (parsedInput.some((char) => typeof char !== "number" || Number.isNaN(char))) {
+      throw new Error("[ERROR] 모든 자리가 숫자로 이루어져야 합니다.");
+    }
+    if (parsedInput.length !== EXPECTED_LENGTH) {
+      throw new Error("[ERROR] 세 자리를 입력해주세요.");
+    }
+    if (parsedInput.some((char) => char === 0)) {
+      throw new Error("[ERROR] 1이상 9이하의 숫자로 이루어져야 합니다. 0은 포함될 수 없습니다.");
+    }
+    if (new Set(parsedInput).size !== parsedInput.length) {
+      throw new Error("[ERROR] 모든 자리 수의 값은 서로 달라야 합니다.");
+    }
   }
 
   static #pickRandomThreeNums() {
