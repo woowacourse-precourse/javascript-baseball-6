d4f4eca7111e1f82fd00df6634f54934215d0240
Sungwk <73461032+Sungwk@users.noreply.github.com>
2023-10-22 19:11:44 +0900

    :sparkles: Feat : 사용자로부터  3자리 수 입력받는 기능 추가
    사용자에게 3자리 수 입력 받는 함수 구현

--- src/Game.js
+++ src/Game.js
@@ -4,10 +4,21 @@ class Game {
   start() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     this.getComputerNumber();
+    this.getPlayerNumber();
   }
 
   getComputerNumber() {
     this.computerNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
+
+  async getPlayerNumber() {
+    MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ')
+      .then((number) => {
+        this.playerNumber = number;
+      })
+      .catch((err) => {
+        MissionUtils.Console.print(err);
+      });
   }
 }
 
