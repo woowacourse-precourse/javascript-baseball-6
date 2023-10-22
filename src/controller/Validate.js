c160977b00ca5788cfe87106ad8372ec710cfb42
JonghyunLEE12 <najo7609@gmail.com>
2023-10-23 00:34:04 +0900

    Style : [ Validate Method Style Change ( JS Code Convention) ]

--- src/controller/Validate.js
+++ src/controller/Validate.js
@@ -2,12 +2,12 @@ const { ERROR_MSG } = require('../models/OutputMsg');
 
 
 class Validate {
-    numberValidate = (number) => {
+    numberValidate(number) {
         if (number.length !== 3) throw new Error(ERROR_MSG.NUMBER_LENGTH_ERROR);
         if (isNaN(number)) throw new Error(ERROR_MSG.IS_NUMBER);
         if (number.includes('0')) throw new Error(ERROR_MSG.IS_ZERO);
     }
-    reGameValidate = (number) => {
+    reGameValidate(number) {
         if (isNaN(number)) throw new Error(ERROR_MSG.IS_NUMBER);
         if ((parseInt(number) <= 0 || parseInt(number) > 2))  throw new Error(ERROR_MSG.RE_GAME_NUMBER_ERROR);
     }
