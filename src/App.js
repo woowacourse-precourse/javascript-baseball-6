478b2ee50bbf54a35003472be0a3aad05ff23ce9
kimmainsain <kmsid003@gmail.com>
2023-10-23 00:34:29 +0900

    :hammer: Refactor : App.js 모듈화

--- src/App.js
+++ src/App.js
@@ -5,21 +5,8 @@ import CustomUtils from "./utils/CustomUtils.js";
 class App {
   async play() {
     try {
-      const computer = [];
-      while (computer.length < 3) {
-        const number = MissionUtils.Random.pickNumberInRange(1, 9);
-        if (!computer.includes(number)) {
-          computer.push(number);
-        }
-      }
-      MissionUtils.Console.print(Messages.GAME_START);
-      let gameContinue = true;
-      while (gameContinue) {
-        const userNumber = await CustomUtils.getUserNumber();
-        const score = CustomUtils.getScore(computer, userNumber);
-        // MissionUtils.Console.print(computer, score);
-        gameContinue = CustomUtils.printScore(score);
-      }
+      const computer = this.generateComputerNumbers();
+      await this.playGame(computer);
       if (await CustomUtils.getRestartChoice()) {
         this.play();
       } else {
@@ -28,6 +15,27 @@ class App {
     } catch (error) {
       MissionUtils.Console.print(error.message);
       throw error;
+    }
+  }
+
+  generateComputerNumbers() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
+
+  async playGame(computer) {
+    MissionUtils.Console.print(Messages.GAME_START);
+    let gameContinue = true;
+    while (gameContinue) {
+      const userNumber = await CustomUtils.getUserNumber();
+      const score = CustomUtils.getScore(computer, userNumber);
+      gameContinue = CustomUtils.printScore(score);
     }
   }
 }
