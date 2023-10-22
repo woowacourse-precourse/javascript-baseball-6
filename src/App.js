744429a52e9e050f9c0e7bd703c930d46f13b4ae
ReturnReason <reasonz@daum.net>
2023-10-22 18:51:02 +0900

    feat(App): try-catch 구문 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { BaseballView } from './view/BaseballView.js';
 import { BaseballModel } from './model/BaseballModel.js';
 import { BaseballController } from './controller/BaseballController.js';
+import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
-    const view = new BaseballView();
-    const model = new BaseballModel();
-    const controller = new BaseballController(model, view);
-    controller.start();
+    try {
+      const view = new BaseballView();
+      const model = new BaseballModel();
+      const controller = new BaseballController(model, view);
+      controller.start();
+    } catch (err) {
+      Console.print(err);
+    }
   }
 }
 
