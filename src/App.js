cb59cf95ff056fc0a9036a3aaff270bc3a254826
8ark <sorlti6952@gmail.com>
2023-10-22 17:29:30 +0900

    Feat : "숫자 비교 메소드 구현"
    Feat : "숫자 비교 메소드 구현"
    
    숫자 비교에 따라 strike, ball설정 및 텍스트 출력부 구현

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App {
   }
   //사용자 숫자 입력
   async getUserNumber() {
-    let user = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+    let user = await MissionUtils.Console.readLineAsync(
+      "숫자를 입력해주세요 : "
+    );
 
     user.split("").forEach((num, index) => {
       if (isNaN(num)) {
@@ -45,9 +47,68 @@ class App {
     return com;
   }
 
+  //게임 진행 출력
+  printText(strike, ball) {
+    if (strike === 3) {
+      MissionUtils.Console.print("3스트라이크");
+      this.is_correct = true;
+    }
+
+    if (strike === 2) {
+      MissionUtils.Console.print("2스트라이크");
+    }
+
+    if (strike === 1) {
+      if (ball === 2) {
+        MissionUtils.Console.print("2볼 1스트라이크");
+      }
+
+      if (ball === 1) {
+        MissionUtils.Console.print("1볼 1스트라이크");
+      }
+
+      if (ball === 0) {
+        MissionUtils.Console.print("1스트라이크");
+      }
+    }
+
+    if (strike === 0) {
+      if (ball === 3) {
+        MissionUtils.Console.print("3볼");
+      }
+
+      if (ball === 2) {
+        MissionUtils.Console.print("2볼");
+      }
+
+      if (ball === 1) {
+        MissionUtils.Console.print("1볼");
+      }
+
+      if (ball === 0) {
+        MissionUtils.Console.print("낫싱");
+      }
+    }
+  }
+
   //숫자 검사
   compareNumber(com, user) {
     //is_correct change
+    //com [ 1 , 2, 3 ] user [ 1 , 2 , 3]
+
+    let strike = 0;
+    let ball = 0;
+
+    for (let i = 0; i < 3; i++) {
+      if (com[i] === user[i]) {
+        strike++;
+      }
+      if (com.includes(user[i]) && com[i] !== user[i]) {
+        ball++;
+      }
+    }
+
+    this.printText(strike, ball);
   }
 
   //게임 진행
@@ -56,10 +117,11 @@ class App {
     while (!this.is_end) {
       let user = await this.getUserNumber();
 
-      console.log("com" + com);
-      console.log("user" + user);
+      // console.log("com" + com);
+      // console.log("user" + user);
       this.compareNumber(com, user);
       if (this.is_correct) {
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         let res = await MissionUtils.Console.readLineAsync(
           "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
         );
