11dfe093b3f43e38759b7d5a4d4ae7a7c0944b93
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-10-23 21:07:06 +0900

    [feat] : Add computer func - random num

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ class App {
   async play() {
     let userAnswer = 1;
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    while (userAnswer == 1) {}
+    while (userAnswer == 1) {
+      const computer = [];
+      while (computer.length < 3) {
+        const number = MissionUtils.Random.pickNumberInRange(1, 9);
+        if (!computer.includes(number)) {
+          computer.push(number);
+        }
+      }
+    }
   }
 }
 
