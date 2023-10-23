21845acdde7b64aee3735a69097a2dde608c6a04
6puritans9 <6puritans9@gmail.com>
2023-10-23 20:46:50 +0900

    refactor: 테스트 코드에 맞추어 비동기 입력 async/await 구조 재작성 및 게임 재시작 로직 재구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 import { Game } from "./game.js";
 
 class App {
   async play() {
+    // let isGameEnded = false;
     const newGame = new Game();
+    await newGame.playBall();
+    this.suggestNewGame();
+  }
+
+  async suggestNewGame() {
+    const input = await MissionUtils.Console.readLineAsync(
+      `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`
+    );
+    if (input === "1") {
+      await this.play();
+    } else return;
   }
 }
 
 export default App;
 
-const app = new App();
-app.play();
+// const app = new App();
+// app.play();
--- src/Game.js
+++ src/Game.js
@@ -2,7 +2,8 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 export class Game {
   constructor() {
-    this.playBall();
+    this.gameEnded = false;
+    // this.playBall();
   }
 
   welcome() {
@@ -18,74 +19,68 @@ export class Game {
     return [...set];
   }
 
-  getUserInput(string) {
-    async function getInputPromise() {
-      const inputPromise = await MissionUtils.Console.readLineAsync(
-        `${Game.prototype.stringList(string)}`
+  async getUserInput() {
+    try {
+      const input = await MissionUtils.Console.readLineAsync(
+        `서로 다른 세 자리 숫자를 입력해주세요: `
       );
-      return inputPromise;
-    }
-    if (string === "New") {
-      getInputPromise().then((input) => this.suggestNewGame(input));
-    } else {
-      getInputPromise().then((input) => this.validateUserInput(input));
+      this.validateUserInput(input);
+      return this.compareNumbers(input);
+    } catch (e) {
+      MissionUtils.Console.print(e);
     }
   }
 
   validateUserInput(input) {
-    try {
-      const set = new Set();
-      for (const element of input) {
-        set.add(parseInt(element));
-      }
-      set.forEach((el) => {
-        if (isNaN(el)) {
-          throw new Error("[ERROR] 숫자를 입력해주세요");
-        } else if (set.size !== 3)
-          throw new Error("[ERROR] 세 자리 숫자가 아닙니다");
-      });
-      console.log(`user: ${[...set]}`);
-      this.compareNumbers([...set]);
-    } catch (error) {
-      console.log(error);
+    const set = new Set();
+    for (const element of input) {
+      set.add(+element);
     }
+    this.checkSet(set);
+    return [...set];
   }
 
-  compareNumbers(inputArray) {
+  checkSet(set) {
+    set.forEach((el) => {
+      if (isNaN(el)) {
+        throw new Error("[ERROR] 숫자를 입력해주세요");
+      } else if (set.size !== 3)
+        throw new Error("[ERROR] 세 자리 숫자가 아닙니다");
+    });
+  }
+
+  async compareNumbers(inputArray) {
     let balls = 0;
     let strikes = 0;
     for (let i = 0; i < this.targetArray.length; i++) {
-      if (this.targetArray[i] === inputArray[i]) strikes++;
+      if (this.targetArray[i] === +inputArray[i]) strikes++;
       else if (inputArray.includes(this.targetArray[i])) balls++;
     }
     if (balls === 0 && strikes === 0) {
       MissionUtils.Console.print(`낫싱`);
-      this.getUserInput();
     } else if (strikes === 3) {
+      MissionUtils.Console.print(`${strikes}스트라이크`);
       MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
-      this.getUserInput("New");
+      // this.returnGameState(strikes);
+      return;
     } else {
-      MissionUtils.Console.print(`${balls}볼, ${strikes}스트라이크`);
-      this.getUserInput();
+      MissionUtils.Console.print(`${balls}볼 ${strikes}스트라이크`);
     }
+    return this.getUserInput();
   }
 
-  suggestNewGame(input) {
-    if (input === "1") {
-      this.playBall();
-    }
-  }
+  // returnGameState(strikes) {
+  //   return new Promise((resolve) => {
+  //     MissionUtils.Console.print(`${strikes}스트라이크`);
+  //     MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
+  //     resolve(true);
+  //   });
+  // }
 
-  stringList(value) {
-    if (value === "New") {
-      return "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
-    } else return "서로 다른 세 자리 숫자를 입력해주세요: ";
-  }
-
-  playBall() {
+  async playBall() {
     this.welcome();
     this.targetArray = this.randomGenerator();
-    // console.log(`target: ${this.targetArray}`);
-    this.getUserInput();
+    console.log(`target: ${this.targetArray}`);
+    await this.getUserInput();
   }
 }
