2e37d7de17722c4e9a8d964844669408240fe686
yebin <yebind3308@gmail.com>
2023-10-23 21:09:57 +0900

    fix: snake_case 수정

--- src/App.js
+++ src/App.js
@@ -2,42 +2,42 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    let restartGame = true;
+    console.log("숫자 야구 게임을 시작합니다.");
+    let restart_game = true;
 
-    if (restartGame) {
-      restartGame = await this.playGame(); // 게임 재시작 여부를 확인
+    if (restart_game) {
+      restart_game = await this.play_game(); // 게임 재시작 여부를 확인
     } else { MissionUtils.Console.print("게임 종료")}
   }
 
-  async playGame() {
+  async play_game() {
     // 컴퓨터가 선택한 임의의 숫자 3개 생성
-    const computerNumbers = this.generateRandomNumbers();
+    const computer_numbers = this.random_numbers();
 
-    let gameEnd = false;
+    let game_end = false;
 
-    while (!gameEnd) {
-      if (gameEnd) {
+    while (!game_end) {
+      if (game_end) {
         break; // 게임 종료 조건 충족 시 게임 종료
       }
       // 사용자로부터 3자리 숫자 입력 받기
-      const userGuess = await this.getUserInput();
+      const user_enter = await this.user_input();
 
       // 입력한 숫자에 대한 스트라이크, 볼, 낫싱 결과 계산
-      const result = this.calculateResult(userGuess, computerNumbers);
+      const result = this.calculate_result(user_enter, computer_numbers);
 
-      this.printResult(result); // 결과 출력
+      this.print_result(result); // 결과 출력
 
       if (result.strikes === 3) { // 게임 종료 조건 확인
         console.log("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        gameEnd = true;
+        game_end = true;
       }
     }
 
-    return await this.askForRestart(); // 게임 재시작 여부 확인
+    return await this.ask_for_restart(); // 게임 재시작 여부 확인
   }
 
-  generateRandomNumbers() {
+  random_numbers() {
     const computer = []; // 1에서 9까지 서로 다른 임의의 수 3개 선택
 
     while (computer.length < 3) {
@@ -51,7 +51,7 @@ class App {
     return computer;
   }
 
-  async getUserInput() {
+  async user_input() {
     // "숫자를 입력해주세요" 출력 후 사용자로부터 3자리 숫자 입력 받기
     const input = await MissionUtils.Console.readLineAsync();
 
@@ -62,25 +62,25 @@ class App {
     return input;
   }
 
-  calculateResult(userGuess, computerNumbers) {
+  calculate_result(user_enter, computer_numbers) {
     // 입력한 숫자에 대한 스트라이크, 볼, 낫싱 결과 계산
-    const userDigits = userGuess.split('').map(Number);
-    const computerDigits = computerNumbers.map(Number);
+    const user_digits = user_enter.split('').map(Number);
+    const computer_digits = computer_numbers.map(Number);
 
     let strikes = 0;
     let balls = 0;
 
     for (let i = 0; i < 3; i++) {
-      if (userDigits[i] === computerDigits[i]) {
+      if (user_digits[i] === computer_digits[i]) {
         strikes++; // 스트라이크인 경우
-      } else if (computerDigits.includes(userDigits[i])) {
+      } else if (computer_digits.includes(user_digits[i])) {
         balls++; // 볼인 경우
       }
     }
     return { strikes, balls }; // 낫싱인 경우는 별도로 계산할 필요 없음
   }
 
-  printResult(result) {
+  print_result(result) {
     let message = "";
 
     if (result.strikes === 0 && result.balls === 0) {
@@ -101,10 +101,10 @@ class App {
     MissionUtils.Console.print(message);
   }
 
-  async askForRestart() { // 게임을 다시 시작할지 종료할지 선택하는 옵션 제공
+  async ask_for_restart() { // 게임을 다시 시작할지 종료할지 선택하는 옵션 제공
     console.log("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     
-    async function getRestartOption() {
+    async function restart_option() {
       return new Promise(async (resolve) => {
         const input = await MissionUtils.Console.readLineAsync();
         if (input === '1') {
@@ -113,12 +113,12 @@ class App {
           resolve(false);
         } else {
           console.log("올바른 옵션을 선택하세요 (1: 재시작, 2: 종료)");
-          resolve(await getRestartOption());
+          resolve(await restart_option());
         }
       });
     }
 
-    return await getRestartOption();
+    return await restart_option();
   }
 }
 
