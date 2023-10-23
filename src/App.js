7830493fff2c898446e6a4075a425d561c6a8d46
lbj23han <ljhan0215@gmail.com>
2023-10-23 15:45:19 +0900

    feat: 플레이어의 숫자 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { Console, Random, MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
+  constructor() {
+    this.Computer_Number = this.Get_Random_Number();
+  }
+
   Get_Random_Number() {
     const RandomNumbers = [];
     while (RandomNumbers.length < 3) {
@@ -12,7 +16,33 @@ class App {
     return RandomNumbers;
   }
 
-  async play() {}
+  async Get_User_Input() {
+    const userInput = await MissionUtils.Console.readLineAsync(
+      "숫자를 입력해 주세요."
+    );
+
+    // 입력값이 숫자가 아니거나 중복된 숫자가 있는 경우 예외 발생
+    if (!/^[1-9]{3}$/.test(userInput) || new Set(userInput).size !== 3) {
+      throw new Error("올바른 형식의 숫자를 입력해주세요.");
+    }
+
+    return userInput;
+  }
+
+  async play() {
+    this.Computer_Number = await this.Get_Random_Number();
+    console.log(this.Computer_Number);
+
+    console.log("숫자 야구 게임을 시작합니다.");
+
+    while (true) {
+      const User_Input = await this.Get_User_Input();
+      console.log(User_Input);
+    }
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
