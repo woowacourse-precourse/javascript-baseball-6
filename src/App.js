442e66a6cba5da3877bdf3fe27fc2918abef0ffa
guswns8431 <guswns8431@gmail.com>
2023-10-23 19:07:14 +0900

    add : 입력 결과 출력 및 재시작/종료 선택 기능
    게임 결과 출력과 게임 재시작/종료를 위해서 code 변수 사용

--- src/App.js
+++ src/App.js
@@ -2,33 +2,41 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    const computer = [];
-    while (computer.length < 3) {
-      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(randomNumber)) {
-        computer.push(randomNumber);
+    let code = 1;
+    while (code != 2) {
+      const computer = [];
+      while (computer.length < 3) {
+        const randomNumber = await MissionUtils.Random.pickNumberInRange(1, 9);
+        if (!computer.includes(randomNumber)) {
+          computer.push(randomNumber);
+        }
       }
+      console.log(computer); //DEBUG
+      MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+      code = await getUserInput(computer);
     }
-    console.log(computer); //DEBUG
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    getUserInput(computer);
   }
 }
 
 async function getUserInput(computer) {
-  try {
-    const userInput = await MissionUtils.Console.readLineAsync(
-      "숫자를 입력해주세요 : "
-    );
-    console.log(`사용자가 입력한 숫자 : ${userInput}`); //DEBUG
-    checkInput(userInput, computer);
-  } catch (error) {
-    // reject 되는 경우
-    console.log("getUserInput() catch문");
+  let code = 3;
+  //code 3 사용자가 맞추지 못함
+  while (code == 3) {
+    try {
+      const userInput = await MissionUtils.Console.readLineAsync(
+        "숫자를 입력해주세요 : "
+      );
+      console.log(`사용자가 입력한 숫자 : ${userInput}`); //DEBUG
+      code = await checkInput(userInput, computer);
+    } catch (error) {
+      // reject 되는 경우
+      console.log("getUserInput() catch문");
+    }
   }
+  return code;
 }
 
-function checkInput(userInput, computer) {
+async function checkInput(userInput, computer) {
   const states = [0, 0, 0];
   console.log(`사용자가 입력한 숫자 : ${userInput}(checkInput함수))`); //DEBUG
   console.log(`컴퓨터가 입력한 숫자 : ${computer}(checkInput함수))`); //DEBUG
@@ -40,7 +48,7 @@ function checkInput(userInput, computer) {
         } else {
           states[1]++;
         }
-        break ;
+        break;
       }
       if (j == 2) {
         states[2]++;
@@ -48,13 +56,23 @@ function checkInput(userInput, computer) {
     }
   }
   if (states[2] == 3) {
-    MissionUtils.Console.print('낫싱');
+    MissionUtils.Console.print("낫싱");
+    return 3;
   } else if (states[0] == 3) {
     MissionUtils.Console.print(`${states[0]}스트라이크`);
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+    MissionUtils.Console.print("");
+    const reGameCodeInput = await MissionUtils.Console.readLineAsync(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+    );
+    console.log("reGameCodeInput : ", reGameCodeInput);
+    return reGameCodeInput;
   } else {
     MissionUtils.Console.print(`${states[1]}볼 ${states[0]}스트라이크`);
+    return 3;
   }
 }
+
 const app = new App();
 app.play();
 
