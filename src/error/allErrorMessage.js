adf16fcc35cfb7e7093678f2100b9b4820002882
Hchan2 <seongchan_@naver.com>
2023-10-23 21:04:04 +0900

    모든 에러 메시지 변수 선언

---
+++ src/error/allErrorMessage.js
@@ -0,0 +1,5 @@
+export const messageNotRestartNumber = "[ERROR] 숫자 1 혹은 숫자 2를 입력해 주세요.";
+export const messageNotOneToNine = "[ERROR] 숫자 1과 9 사이로 입력해주세요.";
+export const messageIsOverlap = "[ERROR] 숫자를 중복으로 입력하셨습니다. 다른 숫자를 입력해주세요.";
+export const messageNotNumber = "[ERROR] 숫자만 입력해주세요.";
+export const messageNotThreeDigits = "[ERROR] 세 자리를 입력해주세요.";
