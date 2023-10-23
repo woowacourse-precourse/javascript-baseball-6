a83c65fd56282a5b83adf57927ed5eb24295f020
Jaewon <wellbird@naver.com>
2023-10-23 15:16:53 +0900

    feat : 컴퓨터의 세 가지 난수 생성 함수 구현

--- src/CreateComputerNumber.js
+++ src/CreateComputerNumber.js
@@ -1,9 +1,18 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 /**
  * 컴퓨터의 숫자를 만드는 함수
  * @returns {Array} 랜덤으로 생성된 서로 다른 세 가지 숫자를 담은 배열
  */
 function CreateComputerNumber() {
-  return;
+  const computerNumber = [];
+  while (computerNumber.length < 3) {
+    const RandomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computerNumber.includes(RandomNumber)) {
+      computerNumber.push(RandomNumber);
+    }
+  }
+  return computerNumber;
 }
 
 export default CreateComputerNumber;
