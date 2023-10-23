7446f28390a9fae6fc4de3d0e1f8a9886af8f793
Myomyoring <narcissuscat@naver.com>
2023-10-23 23:21:32 +0900

    [feat] 틀렸을 경우 추가 문구 출력 및 게임 종료 문구 출력

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App {
     Console.print(computer);
     Console.print('숫자 야구 게임을 시작합니다.');
     try {
-      const inputNumber = await Console.readLineAsync(
+      let inputNumber = await Console.readLineAsync(
         '0을 제외한 서로 다른 숫자를 3자리 입력해주세요: '
       );
       // 예외 구문
@@ -34,27 +34,35 @@ class App {
       if (deduplicateObj.size !== inputNumber.length) {
         throw new Error('서로 다른 숫자를 입력해야 합니다.');
       }
-      // 값 판별
-      let ball = 0;
-      let strike = 0;
-      for (let i = 0; i < 3; i++) {
-        if (computer[i] === Number(inputNumber[i])) {
-          strike += 1;
-        } else if (computer.includes(Number(inputNumber[i]))) {
-          ball += 1;
-        }
-      }
 
-      if (ball === 0 && strike === 0) {
-        Console.print('낫싱');
-      } else if (ball === 0) {
-        // 게임 끝
-        Console.print(`${strike}스트라이크`);
-        Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-      } else if (strike === 0) {
-        Console.print(`${ball}볼`);
-      } else {
-        Console.print(`${ball}볼 ${strike}스트라이크`);
+      let handle = true;
+      while (handle) {
+        // 값 판별
+        let ball = 0;
+        let strike = 0;
+
+        for (let i = 0; i < 3; i++) {
+          if (computer[i] === Number(inputNumber[i])) {
+            strike += 1;
+          } else if (computer.includes(Number(inputNumber[i]))) {
+            ball += 1;
+          }
+        }
+
+        if (ball === 0 && strike === 0) {
+          Console.print('낫싱');
+        } else if (ball === 0) {
+          Console.print(`${strike}스트라이크`);
+          if (strike === 3) {
+            Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+          }
+        } else if (strike === 0) {
+          Console.print(`${ball}볼`);
+        } else {
+          Console.print(`${ball}볼 ${strike}스트라이크`);
+        }
+        inputNumber = await Console.readLineAsync('숫자를 입력해주세요 :');
+        continue;
       }
     } catch (error) {
       console.log('[ERROR]', error.message);
