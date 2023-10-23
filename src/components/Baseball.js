226c7bb915621a52c1c27278336664f425faad6b
JUN <gurwns232@naver.com>
2023-10-24 04:17:14 +0900

    Comment: 주석 수정

--- src/components/Baseball.js
+++ src/components/Baseball.js
@@ -23,14 +23,14 @@ class Baseball {
   async startGame() {
     this.isClear = false;
 
-    /** 컴퓨터 숫자 */
+    /** 컴퓨터(상대) 숫자 */
     this.player.computer = new Computer().getNumber();
 
     while (!this.isClear) {
       /** 유저 숫자 */
       this.player.user = await new User().getNumber();
 
-      /** 힌트 출력, 게임클리어 여부 */
+      /** 비교 후 힌트 출력, 게임클리어 여부 리턴 */
       this.isClear = new Hint(this.player).checkBaseballWin();
     }
 
