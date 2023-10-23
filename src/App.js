f400ce7e31da6bc1d1ad30528f93202d5bf13cdc
apfwnls14 <apfwnls14@naver.com>
2023-10-23 14:46:08 +0900

    feat: 숫자 야구 게임 시작 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    Console.print(computer);
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
