c992e1fdaa666f2f958e5ceb6feaf6fef9b43cf0
Jiwon Oh <64193469+jiwon-o@users.noreply.github.com>
2023-10-23 23:46:34 +0900

    feat: 사용자의 입력값을 받아오는 메서드 생성

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   constructor() {
     this.randomNumbers = [];
+    this.userInput = "";
   }
 
   async play() {
     console.log("숫자 야구 게임을 시작합니다.");
+    this.startGame();
+  }
+
+  startGame() {
     this.generateRandomNumbers();
+    this.getUserInput();
   }
 
   generateRandomNumbers() {
@@ -17,6 +23,13 @@ class App {
         this.randomNumbers.push(number);
       }
     }
+  }
+
+  async getUserInput() {
+    const userInput = await MissionUtils.Console.readLineAsync(
+      "서로 다른 3자리의 숫자를 입력해주세요 : "
+    );
+    this.userInput = userInput;
   }
 }
 
