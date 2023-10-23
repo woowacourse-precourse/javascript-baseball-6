1e5921b1373715122c18a97bcb66143e2474c105
hyun812 <sh980812@naver.com>
2023-10-23 22:36:17 +0900

    [feat] 숫자 비교 후 볼,스트라이크 수 반환

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
-import target from './getComputerNum.js';
-import checkNum from './validation.js';
+import checkNum from './Validation.js';
+import { getComputerNumber, compareNum } from './Computer.js';
 
 class App {
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    let computerNum = getComputerNumber(); 
     while(true){
       try{
         const userNum = await Console.readLineAsync("숫자를 입력해주세요 : ");
         checkNum(userNum);
-
+        Console.print(computerNum);
+        const result = compareNum(computerNum, userNum);
+        Console.print(result);
       }catch (error){
         throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
       }
     }
-    Console.print(target);
   }
 }
 
---
+++ src/Computer.js
@@ -0,0 +1,30 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+
+export const getComputerNumber = () =>{
+  const computer = [];
+  while (computer.length < 3) {
+    const number = String(MissionUtils.Random.pickNumberInRange(1, 9));
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer;
+}
+
+export const compareNum = (computer, user) =>{
+  let strike = 0;
+  let ball = 0;
+  for (let i = 0; i < 3; i++) {
+    if (computer[i] === user[i]) strike += 1;
+    else {
+      if (computer.includes(user[i])) ball += 1;
+    }
+  }
+  
+  return { strike, ball };
+}
+
+
+
+// export default computer;
