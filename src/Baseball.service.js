a844b4f2e28d53e6bffe3d0f523cdcb3b832b5b7
wonkyum-kim <dodo12120@naver.com>
2023-10-23 11:46:33 +0900

    feat: baseballQuery 메서드 작성 - 숫자를 생성하고 - 숫자를 입력받아 비교합니다. - 모두 맞히면 재시도 여부를 입력받습니다.

--- src/Baseball.service.js
+++ src/Baseball.service.js
@@ -15,5 +15,19 @@ export class BaseballService {
 
   validateRetryInput(retry) {}
 
-  async baseballQuery() {}
+  async baseballQuery() {
+    const computer = this.generateRandomNumbers();
+    while (true) {
+      Console.print('숫자를 입력해주세요 : ');
+      const baseballQueryInput = await Console.readLineAsync(this.query);
+      this.validateBaseballQueryInput(baseballQueryInput);
+      const { ball, strike } = this.refree(computer, baseballQueryInput);
+      this.printResult(ball, strike);
+      if (strike === 3) {
+        Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        break;
+      }
+    }
+    await this.retry();
+  }
 }
