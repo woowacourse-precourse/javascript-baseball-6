fb86bcb89ed4932daf27d285bf1f11f83cf4c0e3
kimjinwoo <rlawlsdn177@naver.com>
2023-10-23 23:43:04 +0900

    feat: 숫자 야구(스트라이크, 볼) 체크 함수 구현

--- src/Compare.js
+++ src/Compare.js
@@ -1,3 +1,8 @@
+import {
+  DIGIT_MIN,
+  DIGIT_MAX,
+} from "./Define";
+
 export const IS_VALID_INPUT = (INPUT) => {
     if (!INPUT) return false;
     const NUMBER_SET = new Set(INPUT);
@@ -5,7 +10,21 @@ export const IS_VALID_INPUT = (INPUT) => {
     return (
       INPUT.length === 3 &&
       NUMBER_SET.size === 3 &&
-      [...NUMBER_SET].every((num) => num >= '1' && num <= '9')
+      [...NUMBER_SET].every((num) => num >= DIGIT_MIN && num <= DIGIT_MAX)
     );
 };
-  
+
+export const COMPARE_NUMBER = (PLAYER, COMPUTER) => {
+  let strikes = 0;
+  let balls = 0;
+
+  PLAYER.forEach((digitOfPlayer, idx) => {
+    if (digitOfPlayer === COMPUTER[idx]) {
+      strikes += 1;
+    } else if (COMPUTER.includes(digitOfPlayer)) {
+      balls += 1;
+    }
+  });
+
+  return { strikes, balls };
+};
