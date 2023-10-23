2e10bf8d0390de3ce8d0ad9a3cbf2f2a5138db9a
Kysiam <how2logout@gmail.com>
2023-10-23 22:50:33 +0900

    Modified: GameMessages.js

--- src/GameMessages.js
+++ src/GameMessages.js
@@ -1,6 +1,13 @@
 export const GameMessages = Object.freeze({
   GAME_START: '숫자 야구 게임을 시작합니다.',
+  PITCH_THE_BALL: '숫자를 입력하세요 : ',
   PITCHBALL_CHECKER: '서로 다른 3개의 숫자를 입력해야합니다.',
   PITCHBALL_CHECKER2: '0을 포함하지 않은 서로 다른 3개의 숫자를 입력해야합니다.',
+  GAME_RESULT_BALL: '볼',
+  GAME_RESULT_STRIKE: '스트라이크',
+  GAME_RESULT_NOTHING: '낫싱',
+  GAME_RESULT_EXIT: '3개의 숫자를 모두 맞혔습니다! 게임 종료',
+  GAME_SELECT: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+  GAME_SELECT_VALID: '숫자 1 또는 2를 입력하세요.',
 });
 
