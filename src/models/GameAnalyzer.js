25e25ebe1bf55a8431ec1cae5794bd6a78986f86
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-24 07:26:10 +0900

    :bulb: style: 계산로직의 param타입과 return타입 주석 추가

--- src/models/GameAnalyzer.js
+++ src/models/GameAnalyzer.js
@@ -3,6 +3,12 @@ const GameAnalyzer = {
     return this.calcBallAndStrike(String(computerNumber), String(userNumber));
   },
 
+  /**
+   * 컴퓨터와 사용자의 값을 비교해서 볼과 스트라이크의 개수를 구한다.
+   * @param {string} computerString 
+   * @param {string} userString 
+   * @returns {{ball: number, strike: number}} 볼과 스트라이크의 개수를 가진 객체
+   */
   calcBallAndStrike(computerString, userString) {
     let ball = 0; 
     let strike = 0;
