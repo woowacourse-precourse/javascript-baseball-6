8dda7e2177b3aebd6b049bd4809e1255e0253254
meunrud <113085963+meunrud@users.noreply.github.com>
2023-10-23 23:26:26 +0900

    feat : 스트라이크와 볼 판정 조건문 추가

--- src/App.js
+++ src/App.js
@@ -100,6 +100,14 @@ class App {
       return nothing;
     }
 
+    if (strike === 0 || ball !== 0) {
+      return `${ball}볼`;
+    }
+
+    if (ball === 0 || strike !== 0) {
+      return `${strike}스트라이크`;
+    }
+
     return `${ball}볼 ${strike}스트라이크`;
   }
 
