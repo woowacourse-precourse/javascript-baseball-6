b9500dbc6f31ce9991cc58063ccd1cbbe85a6e88
luna <luna@rtzr.ai>
2023-10-23 14:17:26 +0900

    feat: 스트라이크와 볼의 개수 계산 기능 추가

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -1,5 +1,5 @@
 
-import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 class BaseballGame {
     computer = [];
@@ -8,15 +8,29 @@ class BaseballGame {
         this.computer = [];
     }
 
-    async generateRandomNumbers() {
+    generateRandomNumbers() {
         while (this.computer.length < 3) {
             const number = MissionUtils.Random.pickNumberInRange(1, 9);
             if (!this.computer.includes(number)) {
                 this.computer.push(number);
-            } else {
-                continue;
             }
         }
+    }
+
+    calculateBallAndStrike(userNumbers) {
+        const userNumbersArray = Array.from(userNumbers).map(Number);
+
+        let ballCount = 0;
+        let strikeCount = 0;
+
+        for (let i = 0; i < 3; i++) {
+            if (this.computer[i] === userNumbersArray[i]) {
+                strikeCount++;
+            } else if (this.computer.includes(userNumbersArray[i])) {
+                ballCount++;
+            }
+        }
+        return { ball: ballCount, strike: strikeCount };
     }
 }
 
