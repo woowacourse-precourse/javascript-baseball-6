58b33adeebf49a62cc276e079c3c54d8a8126189
FirstBright <mipo8890@gmail.com>
2023-10-23 16:54:37 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  computerNumber(){
+    const computer = [];
+    while(computer.length < 3 ){
+      const number = MissionUtils.Random.pickNumberInRange(1,9);
+      if(!computer.includes(number)){
+        computer.push(number);
+      }
+    }
+  }
+  
+
+  async play() {
+    
+  }
 }
 
 export default App;
