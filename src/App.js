6636e5a5c4845e3ade7c1016eb65f0d93d4ffe87
bmj0053 <bmj05378@gmail.com>
2023-10-23 23:15:41 +0900

    setAnalysis, printResult 함수 위치 변경

--- src/App.js
+++ src/App.js
@@ -7,6 +7,49 @@ class App {
     const gameData = new GameData();
     Console.print("숫자 야구 게임을 시작합니다.");
 
+    // ball, strike 확인하는 함수
+    function setAnalysis(gameData, userInputs, randomNumber) {
+      // userInput과 비교해서 randomNumber와 다른 값을 찾음
+      const noStrikeNumbers = userInputs.filter((userInput, i) => 
+        randomNumber[i] !== userInput
+      );
+      // strike가 아닌 값들 중 randomNumber에 포함되어있는 값을 찾음 === ball 개수
+      const ballNumbers = noStrikeNumbers.filter((noStrikeNumber) => 
+        randomNumber.includes(noStrikeNumber)
+      );
+      const strike = 3 - noStrikeNumbers.length;
+      const ball = ballNumbers.length;
+
+      // 값 업데이트
+      gameData.setBall(ball);
+      gameData.setStrike(strike);
+    }
+
+    // 결과 출력하는 함수
+    function printResult(gameData) {
+      const ball = gameData.getBall();
+      const strike = gameData.getStrike();
+
+      // 0볼 0스트라이크
+      if (ball === 0 && strike === 0) {
+        Console.print('낫싱');
+        return
+      } else if (ball === 0) { // 0볼 n스트라이크
+        Console.print(`${strike}스트라이크`);
+        if (strike === 3) {
+          Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+          gameData.setThreeStrike(true); // 삼진 = true
+        }
+        return
+      } else if (strike === 0) { // n볼 0스트라이크
+        Console.print(`${ball}볼`);
+        return
+      }
+      // n볼 n스트라이크
+      Console.print(`${ball}볼 ${strike}스트라이크`);
+    } 
+
+    
     // state가 true일 때 반복
     while (gameData.getState()) {
       // 랜덤 숫자
@@ -27,47 +70,7 @@ class App {
       // 제대로 된 값을 입력했다면 숫자로 변환
       const userInputsNumber = userInputs.split('').map((userInput) => parseInt(userInput, 10));
 
-      // ball, strike 확인하는 함수
-      function setAnalysis(gameData, userInputs, randomNumber) {
-        // userInput과 비교해서 randomNumber와 다른 값을 찾음
-        const noStrikeNumbers = userInputs.filter((userInput, i) => 
-          randomNumber[i] !== userInput
-        );
-        // strike가 아닌 값들 중 randomNumber에 포함되어있는 값을 찾음 === ball 개수
-        const ballNumbers = noStrikeNumbers.filter((noStrikeNumber) => 
-          randomNumber.includes(noStrikeNumber)
-        );
-        const strike = 3 - noStrikeNumbers.length;
-        const ball = ballNumbers.length;
-
-        // 값 업데이트
-        gameData.setBall(ball);
-        gameData.setStrike(strike);
-      }
-
-      // 결과 출력하는 함수
-      function printResult(gameData) {
-        const ball = gameData.getBall();
-        const strike = gameData.getStrike();
-
-        // 0볼 0스트라이크
-        if (ball === 0 && strike === 0) {
-          Console.print('낫싱');
-          return
-        } else if (ball === 0) { // 0볼 n스트라이크
-          Console.print(`${strike}스트라이크`);
-          if (strike === 3) {
-            Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-            gameData.setThreeStrike(true); // 삼진 = true
-          }
-          return
-        } else if (strike === 0) { // n볼 0스트라이크
-          Console.print(`${ball}볼`);
-          return
-        }
-        // n볼 n스트라이크
-        Console.print(`${ball}볼 ${strike}스트라이크`);
-      }
+      
     }
 
     // Console.print(randomNumber)
