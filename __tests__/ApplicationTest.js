ae7097d8a84a93251d740099cf3fee6ee38b23e9
JiSeong Kim <12180543@inha.edu>
2023-10-24 01:29:36 +0900

    Feat: getUserNumberInput의 error에 대한 테스트 추가
    5가지의 에러 코드를 테스트 하기 위해 테스트 코드 함수 5개를 추가하였음.

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -57,4 +57,74 @@ describe("숫자 야구 게임", () => {
 
     await expect(app.play()).rejects.toThrow("[ERROR]");
   });
+
+  test("빈칸 포함 예외 테스트", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["1 3 5"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow('[ERROR] 3자리의 수를 공백없이 이어서 입력해야 합니다.');
+  });
+
+  test("3자리가 아닌 입력 예외 테스트", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["12"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow('[ERROR] 3자리의 숫자가 아닙니다.');
+  });
+
+  test("중복 숫자 예외 테스트", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["121"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow('[ERROR] 각 자리의 수는 중복되지 않아야 합니다.');
+  });
+
+  test("숫자 이외 입력 예외 테스트", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["-b?"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow('[ERROR] 입력에는 숫자만 포함되어야 합니다.');
+  });
+
+  test("0 포함 입력 예외 테스트", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["120"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow('[ERROR] 입력에는 0이 포함되지 않아야 합니다.');
+  });
 });
--- src/view/View.js
+++ src/view/View.js
@@ -6,15 +6,19 @@ class View {
     printStartMsg() {
         MissionUtils.Console.print('숫자 야구 게임을 시작합니다.')
     }
+
     printEndMsg() {
         MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료')
     }
+
     printHintMsg(hint) {
         MissionUtils.Console.print(hint);
     }
+
     async getUserGameDecision() {
         return await getUserGameDecision();
     }
+    
     async getUserNumberInput() {
         return await getUserNumberInput();
     }
