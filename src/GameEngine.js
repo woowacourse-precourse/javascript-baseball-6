b1797ea0ec65c39c879856a588d4d9bb98002544
j1hwan <jihwan@pm.me>
2023-10-23 12:28:42 +0900

    refactor: 메시지 출력과 볼 카운트 연산 분리

--- src/GameEngine.js
+++ src/GameEngine.js
@@ -1,12 +1,4 @@
-import { Console } from '@woowacourse/mission-utils';
-import { BASEBALL_BALL, BASEBALL_NOTHING, BASEBALL_STRIKE, GAME_END_MESSAGE, GAME_START_MESSAGE, GAME_WIN_MESSAGE } from './constants.js';
-
-export default class GameEngine {
-  static showStartMessage() {
-    Console.print(GAME_START_MESSAGE);
-  }
-  
-  static ballCount(userNumbers, computerNumbers) {
+export function ballCount(userNumbers, computerNumbers) {
     let strike = 0;
     let ball = 0;
 
@@ -22,23 +14,3 @@ export default class GameEngine {
 
     return { strike, ball };
   }
-
-  static showCountStatus(strike, ball) {
-    if (strike === 0 && ball === 0) {
-      Console.print(BASEBALL_NOTHING);
-    } else {
-      const results = [];
-      if (ball > 0) results.push(`${ball}${BASEBALL_BALL}`);
-      if (strike > 0) results.push(`${strike}${BASEBALL_STRIKE}`);
-      Console.print(results.join(' '));
-    }
-  }
-
-  showWinMessage() {
-    Console.print(GAME_WIN_MESSAGE);
-  }
-
-  async showEndMessage() {
-    return await Console.readLineAsync(GAME_END_MESSAGE);
-  }
-}
