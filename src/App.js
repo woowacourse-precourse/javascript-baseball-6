6d2b16d71e8e762e83a0ef9fa5c7111829204565
CHUIWWWW <hwsg0912@naver.com>
2023-10-24 02:48:35 +0900

    Feat: 게임 초기 출력문 및 랜덤 숫자 생성 기능 구현 1. play(): App.js 실행시 게임 안내멘트 출력 및 랜덤 숫자 출력 2. gererateRandomNumbers(): 서로 다른 임의의 3개의 숫자를 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-class App {
-  async play() {}
-}
+import { Console, Random } from "@woowacourse/mission-utils";
 
-export default App;
+class App {
+
+  async play() {
+    Console.print('숫자야구 게임을 시작합니다.');
+    Console.print(this.generateRandomNumbers())
+  }
+  generateRandomNumbers() {
+    const differentRandomNumbers = [];
+    while (differentRandomNumbers.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!differentRandomNumbers.includes(number)) {
+        differentRandomNumbers.push(number);
+      }
+    }
+    return differentRandomNumbers;
+  }
+  
+}
+let app = new App();
+app.play();
+
+
