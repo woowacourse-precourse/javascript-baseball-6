c143936bc8a35ff030001d549fa8ebf4bf3de4a4
bl0ckp1n9 <blockp1n9@gmail.com>
2023-10-23 15:56:50 +0900

    feat: 랜덤값 생성 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
+
+  #makeRandomNumber() {
+    let result = "";
+
+    while (true) {
+      if (result.length >= 3) break;
+
+      const random = Random.pickNumberInRange(1, 9);
+      if (!result.includes(random)) result += random;
+    }
+
+    return result;
+  }
 }
 
 const app = new App();
