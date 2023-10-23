9e3fe564090c0178ea27263d0d0e8dec94192c78
hyunyeee <5004ohy@naver.com>
2023-10-24 01:53:17 +0900

    feat: 서로 다른 임의의 수 3개 생성

--- BaseBallGame.js
+++ BaseBallGame.js
@@ -5,6 +5,17 @@ const START_MESSAGE = "숫자 야구 게임을 시작합니다."
 class BaseBallGame {
   async play() {
     MissionUtils.Console.print(START_MESSAGE);
+    this.#generateComputerNumbers();
+  }
+
+  #generateComputerNumbers() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
   }
 }
 
