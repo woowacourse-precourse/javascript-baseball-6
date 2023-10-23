21ff5538a55179aa1ba739d85d06b2d7ee0c05a4
yeokyoung song <spolice0324@gmail.com>
2023-10-23 16:19:42 +0900

    refactor : 정의한 상수 값 난수 생성 함수에 적용
    - 해당 문구 및 변수를 사용하는 부분에 적용

--- src/utils/makeRandomNumber.js
+++ src/utils/makeRandomNumber.js
@@ -1,9 +1,11 @@
+import { ANSWER_LENGTH } from "../Constants";
+
 const { Random } = require("@woowacourse/mission-utils");
 
 const makeRandomNumber = () => {
   const answer = [];
 
-  while (answer.length < 3) {
+  while (answer.length < ANSWER_LENGTH) {
     const randomNumber = Random.pickNumberInRange(1, 9);
     if (!answer.includes(randomNumber)) {
       answer.push(randomNumber);
