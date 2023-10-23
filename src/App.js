c9bcd95588d16d0b03e581b080e9b33406e2b722
kwani6684 <jack0910@naver.com>
2023-10-23 10:18:05 +0900

    ✨ 컴퓨터와 사용자의 숫자 비교 추가 #3 스트라이크==3 일때 반복문 탈출

--- src/App.js
+++ src/App.js
@@ -39,9 +39,18 @@ class App {
     while (newGame % 2) {
       //1일때 실행, 2일때 종료
       let com = new Com();
-      let user = new User();
-      await user.getNumber();
-      newGame =await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+      while (1) {
+        let ballCnt =0;
+        let strikeCnt=0;
+        let user = new User();
+        await user.getNumber();
+        if(strikeCnt==3){
+          MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+          break;
+        }
+      }
+
+      newGame = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
   }
 }
