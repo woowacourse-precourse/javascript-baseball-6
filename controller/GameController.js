e2cd53ea67ff5aaec4a6aacee38a0333f4f625fb
SH-indeyong <ohsh1115@gmail.com>
2023-10-24 03:26:20 +0900

    feat: 게임 재시작, 종료 기능

--- controller/GameController.js
+++ controller/GameController.js
@@ -2,49 +2,61 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 import InputView from "../views/InputView.js";
 import OutputView from "../views/OutputView.js";
 import createRandomNumber from "../utils/CreateRandomNumber.js";
-import NumberComparison from "../utils/NumberComparison.js";
 
 class GameController {
-    constructor() {
-        this.computer = createRandomNumber();
-        this.ballCount = 0;
-        this.strikeCount = 0;
-    }
+  startGame() {
+    OutputView.printStartMessage();
+    this.resetCounts()
+    this.createComputerNumber();
+    this.inputNumber();
+  }
 
-    startGame() {
-        OutputView.printStartMessage();
-        this.inputNumber();
-    }
+  resetCounts() {
+    this.ballCount = 0;
+    this.strikeCount = 0;
+  }
 
-    async inputNumber() {
-        const userInput = await InputView.inputUserNumber();
-        this.validCheck(userInput.split("").map(Number));
-    }
+  createComputerNumber() {
+    this.computer = createRandomNumber();
+  }
 
-    validCheck(number) {
-        this.userInputNumber = number;
-        this.countBallStrike();
-    }
+  async inputNumber() {
+    this.resetCounts()
+    const userInput = await InputView.inputUserNumber();
+    this.validCheck(userInput.split("").map(Number));
+  }
 
-    countBallStrike() {
-        this.userInputNumber.forEach((item, index) => {
-            if (item === this.computer[index]) {
-                this.strikeCount += 1;
-            } else if (this.computer.includes(Number(item))) {
-                this.ballCount += 1;
-            }
-        });
-        this.printGameResult();
-    }
+  validCheck(number) {
+    this.userInputNumber = number;
+    this.countBallStrike();
+  }
 
-    printGameResult() {
-        OutputView.printResult(this.ballCount, this.strikeCount);
-        if (this.strikeCount !== 3){
-            this.ballCount = 0;
-            this.strikeCount = 0;
-            this.inputNumber();
-        }
-    }
+  countBallStrike() {
+    this.userInputNumber.forEach((item, index) => {
+      if (item === this.computer[index]) {
+        this.strikeCount += 1;
+      } else if (this.computer.includes(Number(item))) {
+        this.ballCount += 1;
+      }
+    });
+    this.printGameResult();
+  }
+
+  printGameResult() {
+    MissionUtils.Console.print(OutputView.printResult(this.ballCount, this.strikeCount));
+    if (this.strikeCount === 3)
+      this.reGame();
+    if (this.strikeCount !== 3)
+      this.inputNumber();
+  }
+
+  async reGame() {
+    const inputRegame = await InputView.inputReGame();
+    if (inputRegame == 1)
+      this.startGame();
+    else if (inputRegame == 2)
+      return;
+  }
 
 }
 
--- views/OutputView.js
+++ views/OutputView.js
@@ -8,15 +8,15 @@ const OutputView = {
 
     printResult(ballCount, strikeCount){
         if (strikeCount === 3){
-            MissionUtils.Console.print(`${strikeCount}스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
+            return `${strikeCount}스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`;
         } else if (ballCount === 0 && strikeCount === 0) {
-            MissionUtils.Console.print("낫싱");
+            return "낫싱";
         } else if (ballCount === 0) {
-            MissionUtils.Console.print(`${strikeCount}스트라이크`);
+            return `${strikeCount}스트라이크`;
         } else if (strikeCount === 0) {
-            MissionUtils.Console.print(`${ballCount}볼`);
+            return `${ballCount}볼`;
         } else {
-            MissionUtils.Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
+            return `${ballCount}볼 ${strikeCount}스트라이크`;
         }
     }
 }
