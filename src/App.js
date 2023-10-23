78f265dd85d9a4611ba35473a227079dbd93e22c
KYHH <kyhh93@naver.com>
2023-10-23 16:10:48 +0900

    feat : User 클래스의 Console 중복없게 입력하기

--- src/App.js
+++ src/App.js
@@ -40,21 +40,20 @@ class User {
   constructor() { }
 
   async createInputs() {
-    const userInput = [];
 
     try {
       const input = await MissionUtils.Console.readLineAsync(`숫자를 입력해주세요 : `);
-      userInput.push(parseInt(input));
+      // 숫자로 구성된 배열로 전환
+      const numbers = input.split('').map(num => parseInt(num)); // num은 map함수의 매개변수 ex) const map1 = array1.map((x) => x * 2);
+
+      if (numbers.some(isNaN) || numbers.length !== 3) {
+        throw new Error(`숫자 3개를 올바르게 입력해주세요.`); // some 함수로 numbers 배열 중 NaN이 있는지 확인한다.
+      }
+      // userInput 배열에 숫자를 넣는다
+      const userInput = numbers;
       console.log(userInput);
 
-      // parseInt() 함수는 주어진 문자열을 정수로 변환할 수 있을 때까지 반환을 시도하고, 변환이 불가능한 경우 NaN을 반환한다
-      // 따라서 parseInt() 함수를 사용하면 문자나 숫자 상관없이 항상 NaN이 아닌 값을 반환한다
-      // 사용자가 숫자로 입력하더라도 parseInt()함수로 변환할 때 숫자로 변환될 수 없는 부분까지 변환을 시도하면, 결과적으로 NaN이 된다.
-      // 이 경우에 Number.isNaN()을 사용하면 true가 반환되기에 문자를 넣어도 에러가 발생하지 않고 그냥 출력된거 같다.
-      // 따라서 isNaN을 사용하여 사용자의 입력이 숫자인지만 확인한다.
-      if (isNaN(input)) {
-        throw new Error(`숫자를 입력하셔야 합니다. 프로그램이 종료 됩니다.`);
-      }
+
     } catch (error) {
       throw error;
     }
