9fd50ad2b29045f12c33e4bbee808d435312f765
bl0ckp1n9 <blockp1n9@gmail.com>
2023-10-23 16:30:28 +0900

    test: 랜덤값 테스트 코드

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -24,37 +24,64 @@ const getLogSpy = () => {
 };
 
 describe("숫자 야구 게임", () => {
-  test("게임 종료 후 재시작", async () => {
-    // given
-    const randoms = [1, 3, 5, 5, 8, 9];
-    const answers = ["246", "135", "1", "597", "589", "2"];
-    const logSpy = getLogSpy();
-    const messages = ["낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료"];
+  // test("게임 종료 후 재시작", async () => {
+  //   // given
+  //   const randoms = [1, 3, 5, 5, 8, 9];
+  //   const answers = ["246", "135", "1", "597", "589", "2"];
+  //   const logSpy = getLogSpy();
+  //   const messages = [
+  //     "낫싱",
+  //     "3스트라이크",
+  //     "1볼 1스트라이크",
+  //     "3스트라이크",
+  //     "게임 종료",
+  //   ];
+  //
+  //   mockRandoms(randoms);
+  //   mockQuestions(answers);
+  //
+  //   // when
+  //   const app = new App();
+  //   await expect(app.play()).resolves.not.toThrow();
+  //
+  //   // then
+  //   messages.forEach((output) => {
+  //     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+  //   });
+  // });
+  //
+  // test("예외 테스트", async () => {
+  //   // given
+  //   const randoms = [1, 3, 5];
+  //   const answers = ["1234"];
+  //
+  //   mockRandoms(randoms);
+  //   mockQuestions(answers);
+  //
+  //   // when & then
+  //   const app = new App();
+  //
+  //   await expect(app.play()).rejects.toThrow("[ERROR]");
+  // });
 
-    mockRandoms(randoms);
-    mockQuestions(answers);
-
-    // when
-    const app = new App();
-    await expect(app.play()).resolves.not.toThrow();
-
-    // then
-    messages.forEach((output) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+  describe("랜덤값 테스트", () => {
+    let app;
+    beforeEach(() => {
+      app = new App();
     });
-  });
+    test("자리수가 3자리인지 확인", async () => {
+      const randomNumber = app.getRandomNumber();
 
-  test("예외 테스트", async () => {
-    // given
-    const randoms = [1, 3, 5];
-    const answers = ["1234"];
+      expect(randomNumber.length).toBe(3);
+    });
 
-    mockRandoms(randoms);
-    mockQuestions(answers);
+    test("중복값 확인", async () => {
+      for (let i = 0; i < 50; i++) {
+        const randomNumber = app.getRandomNumber();
 
-    // when & then
-    const app = new App();
-
-    await expect(app.play()).rejects.toThrow("[ERROR]");
+        const regEx = /(\d).*\1/;
+        expect(regEx.test(randomNumber)).toBe(false);
+      }
+    });
   });
 });
