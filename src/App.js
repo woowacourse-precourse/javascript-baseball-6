7ebc75be5b95c7f2ea7b100f129867f57b64e4cc
guswns8431 <guswns8431@gmail.com>
2023-10-23 17:06:52 +0900

    add : 사용자 입력 받는 기능 추가
    사용자의 숫자 입력 값을 받기 위해 readLineAsync 사용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import Random from "/node_modules/@woowacourse/mission-utils/src/random.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
     const computer = [];
     while (computer.length < 3) {
-      const randomNumber = Random.pickNumberInRange(1, 9);
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!computer.includes(randomNumber)) {
         computer.push(randomNumber);
       }
     }
-    console.log(computer);
+    console.log(computer); //DEBUG
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    getUserInput();
   }
 }
+
+async function getUserInput() {
+  try {
+    const userInput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+    console.log(`사용자가 입력한 숫자: ${userInput}`); //DEBUG
+  } catch (error) {
+    // reject 되는 경우
+    console.log('getUserInput() catch문');
+  }
+}
+
+const app = new App();
+app.play();
 
 export default App;
