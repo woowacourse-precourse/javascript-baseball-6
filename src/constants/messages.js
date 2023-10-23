c2c58ec4db0d470185bdaab99ba7c77e32f54cbe
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-24 08:18:12 +0900

    :recycle: refactor: 진행상황 문자 상수화

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,9 +1,18 @@
 import { USER_COMMAND, GAME_CONDITION } from "./conditions.js";
 
-export const MESSAGES = Object.freeze({
+const MESSAGES = Object.freeze({
   startGuide: '숫자 야구 게임을 시작합니다.',
   inputGuide: '숫자를 입력해주세요 : ',
   inputError: '[ERROR] 숫자가 잘못된 형식입니다.',
   endGuide: `${GAME_CONDITION.maxLength}개의 숫자를 모두 맞히셨습니다! 게임 종료`,
   replayGuide: `게임을 새로 시작하려면 ${USER_COMMAND.replay}, 종료하려면 ${USER_COMMAND.end}를 입력하세요.\n`,
 });
+
+const PROGRESS = Object.freeze({
+  nothing: '낫싱',
+  ball: '볼',
+  strike: '스트라이크',
+  and: ' ',
+});
+
+export { MESSAGES, PROGRESS };
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,4 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
+import { PROGRESS } from "../constants/messages.js";
 
 const OutputView = {
   printStaticMessage(message) {
@@ -14,11 +15,11 @@ const OutputView = {
   printGameProgress(ball, strike) {
     let message = '';
 
-    if (ball === 0 && strike === 0) message = '낫싱';
-    if (ball > 0) message += `${ball}볼`;
+    if (ball === 0 && strike === 0) message = PROGRESS.nothing;
+    if (ball > 0) message += `${ball}${PROGRESS.ball}`;
     if (strike > 0) {
-      if (message.length > 0) message += ' ';
-      message += `${strike}스트라이크`;
+      if (message.length > 0) message += PROGRESS.and;
+      message += `${strike}${PROGRESS.strike}`;
     }
     return this.printStaticMessage(message);
   },
