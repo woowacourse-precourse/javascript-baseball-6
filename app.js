098d042a87fba2ba03289bc97accd87d2cbf5bd5
조경은 <kavvy99@naver.com>
2023-10-23 23:47:52 +0900

    feat: 유저의 숫자 배열 생성 및 유효성 검사

--- app.js
+++ app.js
@@ -7,3 +7,41 @@ while (computerNumbersArray.length < 3) {
     }
 }
 console.log(computerNumbersArray);
+
+// 확인버튼 클릭했을 때 numArrayCompareEvent() 발생
+function numArrayCompareEvent() {
+
+    // 1. 유저의 입력 배열 userNumbersArray 생성
+    // 사용자의 입력 값 배열 생성
+    var userNum = document.querySelector('.userNum').value; 
+    var userNumbersArray = userNum.toString().split('').map(Number);
+
+    console.log(userNumbersArray);
+
+    // 2. userNumbersArray의 유효성 검사
+    // 값이 세 자리가 아니라면?
+    if (userNumbersArray.length!==3) {
+        alert("[ERROR] 세 자리 숫자가 아닙니다.");
+    } else {
+        var isValid = true;
+        var uniqueUserNumbersArray = new Set(userNumbersArray);
+        console.log(uniqueUserNumbersArray);
+        console.log(uniqueUserNumbersArray.size);
+        console.log(userNumbersArray.length);
+        if (uniqueUserNumbersArray.size !== userNumbersArray.length) {
+            alert("[ERROR] 중복된 값이 존재합니다.")
+            isValid = false;
+        }
+        for (var i=0; i<3; i++) {
+            if (userNumbersArray[i] < 1 || userNumbersArray[i] > 9) {
+                alert("[ERROR] 1에서 9 사이의 범위를 벗어나는 숫자가 있습니다.");
+                isValid = false;
+                break;
+            }
+        }
+        if (isValid) {
+            // 3. computerNumbersArray와 userNumbersArray 비교
+            console.log('이제 시작이다')
+        }
+    }
+}
