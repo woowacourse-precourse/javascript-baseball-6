79f9ceff1fd4e8f454bb7b771d93f08218b557a8
pakxe <pigkill40@naver.com>
2023-10-23 21:04:28 +0900

    feat(Output): 터미널 출력을 위한 클래스 작성
    시작 문구,
    결과 출력 문구,
    클리어 문구,
    종료 문구 를 출력하는 4개의 출력 메서드를 작성하였다.

--- src/io/output/Output.js
+++ src/io/output/Output.js
@@ -5,6 +5,32 @@ class Output {
 	static printStart() {
 		Console.print(GAME_MESSAGE.start);
 	}
+
+	static printResult(counts) {
+		const [ballCount, strikeCount, isAllZero] = counts;
+
+		let judgeComment = '';
+
+		// nothing일 경우
+		if (isAllZero) return Console.print(GAME_MESSAGE.nothing);
+
+		judgeComment += ballCount !== 0 ? ballCount + GAME_MESSAGE.ball + ' ' : '';
+		judgeComment += strikeCount !== 0 ? strikeCount + GAME_MESSAGE.strike : '';
+
+		return Console.print(judgeComment);
+	}
+
+	static printClear() {
+		Console.print(`${GAME_MESSAGE.clear} ${GAME_MESSAGE.exit}`);
+	}
+
+	static printExit() {
+		Console.print();
+	}
+
+	// static printMore() {
+	// 	Console.print(GAME_MESSAGE.more);
+	// }
 }
 
 export default Output;
