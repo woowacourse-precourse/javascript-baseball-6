49361def254ea7d0a310226af7b33f1f81f9f90a
limdumb <dlaruddls@nate.com>
2023-10-23 16:58:25 +0900

    [Refact] User Input Function if문 수정

--- src/function/userInput.js
+++ src/function/userInput.js
@@ -4,9 +4,9 @@ async function userInput() {
   const inputValue = await Console.readLineAsync("숫자를 입력해주세요 : ");
   if (inputValue.length > 3 || isNaN(Number(inputValue))) {
     throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
-  } else {
-    return inputValue;
   }
+
+  return inputValue;
 }
 
 export default userInput;
