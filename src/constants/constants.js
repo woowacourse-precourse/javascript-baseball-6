96ac736b513e5743127c8fff73255f61b270c09b
JUN <gurwns232@naver.com>
2023-10-23 18:18:37 +0900

    feat: baseball 카운트 상수 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -8,3 +8,9 @@ export const ERROR_MESSAGE = Object.freeze({
   NUMBER_LENGTH_MUST_THREE: '[ERROR] 숫자의 길이는 3이여야 합니다.',
   DUPLICATE_NUMBER: '[ERROR] 3개의 숫자는 서로 다른 숫자여야 합니다',
 });
+
+export const BALL_COUNT = Object.freeze({
+  BALL: '볼',
+  STRIKE: '스트라이크',
+  NOTHING: '낫싱',
+});
