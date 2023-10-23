2eea58888732f55b13168a01cfb3b6350af28c2b
inhwa <dlsghk0711@naver.com>
2023-10-24 01:49:54 +0900

    주석 처리

--- src/App.js
+++ src/App.js
@@ -2,51 +2,74 @@ import { Console } from "@woowacourse/mission-utils";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  generateComputerNumber() {
-    const computerNumber = [];
-    while (computerNumber.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computerNumber.includes(number)) {
-        computerNumber.push(number);
+  generateComputerNumber() { //컴퓨터 랜덤 숫자 생성
+    const COMPUTER_NUMBER = [];
+    while (COMPUTER_NUMBER.length < 3) {
+      const RANDOM_NUM = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!COMPUTER_NUMBER.includes(RANDOM_NUM)) {
+        COMPUTER_NUMBER.push(RANDOM_NUM);
       }
     }
-    return computerNumber;
+    return COMPUTER_NUMBER;
   }
 
-  CalcStrike(ComputerRandomNum, UserNum){
-    let Strike=0
+  CalcStrike(COM_RANDOM_NUM, USER_NUM){ //스트라이크 카운트
+    let STRIKE_COUNT=0
     for(let i = 0; i<3; i++){
-      if (ComputerRandomNum[i]==UserNum[i]){
-        Strike+=1;
+      if (COM_RANDOM_NUM[i]==USER_NUM[i]){
+        STRIKE_COUNT+=1;
       }
     }
-    return Strike;
+    return STRIKE_COUNT;
   }
 
-  CalcBall(ComputerRandomNum, UserNum){
-    let Ball=0
+  CalcBall(COM_RANDOM_NUM, USER_NUM){ //볼 카운트
+    let BALL_COUNT=0
     for(let i = 0; i<3; i++){
       for (let j = 0; j < 3; j++) {
-        if ((i != j) && (ComputerRandomNum[i] == UserNum[j])) {
-          Ball += 1;
+        if ((i != j) && (COM_RANDOM_NUM[i] == USER_NUM[j])) {
+          BALL_COUNT += 1;
         }
       }
     }
-    return Ball;
+    return BALL_COUNT;
   }
 
-  Check(UserNum){
-    const inputToSet = new Set(UserNum.split('').map(Number));
-    if (UserNum.length!=3){
+  PrintCount(STRIKE_COUNT, BALL_COUNT){ //스트라이크&볼 카운트 출력
+    if ((STRIKE_COUNT==0)&&(BALL_COUNT==0)){
+      Console.print('낫싱')
+      return false;
+    }
+    if ((STRIKE_COUNT!=0)&&(BALL_COUNT==0)){
+      Console.print(STRIKE_COUNT+'스트라이크')
+      if(STRIKE_COUNT==3){
+        Console.print("3개의 숫자를 모두 맞히셨습니다!");
+        return true;
+      }
+      return false;
+    }
+    if ((STRIKE_COUNT==0)&&(BALL_COUNT!=0)){
+      Console.print(BALL_COUNT+'볼')
+      return false
+    }
+    if ((STRIKE_COUNT!=0)&&(BALL_COUNT!=0)){
+      Console.print(BALL_COUNT+"볼 "+STRIKE_COUNT+'스트라이크')
+      return false
+    }
+  }
+
+  Check(USER_NUM){ //사용자 입력 숫자 예외처리
+    const inputToSet = new Set(USER_NUM.split('').map(Number));
+    if (USER_NUM.length!=3){
       throw new Error();
     }
     if ([...inputToSet].length !== 3) {
       throw new Error();
     }
-    if (UserNum.includes(' ')){
+    if (USER_NUM.includes(' ')){
       throw new Error();
     }
-    if (Number.isNaN(UserNum)){
+    if (Number.isNaN(USER_NUM)){
       throw new Error();
     }
     else{
@@ -54,35 +77,13 @@ class App {
     }
   }
 
-  PrintCount(StrikeConut,BallCount){
-    if ((StrikeConut==0)&&(BallCount==0)){
-      Console.print('낫싱')
-      return false;
-    }
-    if ((StrikeConut!=0)&&(BallCount==0)){
-      Console.print(StrikeConut+'스트라이크')
-      if(StrikeConut==3){
-        Console.print("3개의 숫자를 모두 맞히셨습니다!");
-        return true;
-      }
-      return false;
-    }
-    if ((StrikeConut==0)&&(BallCount!=0)){
-      Console.print(BallCount+'볼')
-      return false
-    }
-    if ((StrikeConut!=0)&&(BallCount!=0)){
-      Console.print(BallCount+"볼 "+StrikeConut+'스트라이크')
-      return false
-    }
-  }
-  async Restart(){
+  async Restart(){ //게임 재시작 & 종료 여부 확인
     try {
-      const KeeporStop = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요');
-      if (KeeporStop=='1'){
+      const KEEP_OR_STOP = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요 \n');
+      if (KEEP_OR_STOP=='1'){
         return this.play();
       }
-      else if (KeeporStop=='2'){
+      else if (KEEP_OR_STOP=='2'){
         Console.print('게임 종료');
       }
     } catch (error) {
@@ -90,17 +91,16 @@ class App {
     }
   }
 
-  async Gamestart(ComputerRandomNum){
+  async Gamestart(COM_RANDOM_NUM){ //게임 시작 및 사용자 숫자 입력
     while(true){
       try {
-        const UserNum = await Console.readLineAsync('숫자를 입력해주세요 : ');
-        Console.print(UserNum);
-        this.Check(UserNum);
+        const USER_NUM = await Console.readLineAsync('숫자를 입력해주세요 : ');
+        this.Check(USER_NUM);
       
-        const StrikeConut = this.CalcStrike(ComputerRandomNum, UserNum);
-        const BallCount = this.CalcBall(ComputerRandomNum, UserNum);
-        const result = this.PrintCount(StrikeConut,BallCount);
-        if (result==true){
+        const STRIKE_COUNT = this.CalcStrike(COM_RANDOM_NUM, USER_NUM);
+        const BALL_COUNT = this.CalcBall(COM_RANDOM_NUM, USER_NUM);
+        const RESULT = this.PrintCount(STRIKE_COUNT,BALL_COUNT);
+        if (RESULT==true){
           return this.Restart();
         }
 
@@ -111,10 +111,9 @@ class App {
   }
 
   async play(){
-    const ComputerRandomNum=this.generateComputerNumber();
-    Console.print('컴퓨터 랜덤 숫자 : '+ComputerRandomNum);
+    const COM_RANDOM_NUM=this.generateComputerNumber();
     Console.print('숫자 야구 게임을 시작합니다.');
-    return this.Gamestart(ComputerRandomNum);
+    return this.Gamestart(COM_RANDOM_NUM);
   }
 }
 
