1a04440ee4cc6d97a006fcb60faa0215f7eda2f1
Hchan2 <seongchan_@naver.com>
2023-10-23 22:24:25 +0900

    게임 종료 메시지 추가

--- src/constant/allPrintMessage.js
+++ src/constant/allPrintMessage.js
@@ -1,5 +1,6 @@
 export const messageGameStart = "숫자 야구 게임을 시작합니다.";
 export const messageAllStrike = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+export const messageGameEnd = "게임이 종료됩니다.";
 
 export const messageInputNumber = "숫자를 입력해주세요 : ";
 export const messageRestartInputNumber = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요 : ";
