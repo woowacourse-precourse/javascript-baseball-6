8d13f76673f07310d0cd015b146bf3f8915ce02b
hojkim77 <rlaghwnd4534@gmail.com>
2023-10-23 22:23:37 +0900

    [feat] 게임Class - 입력에 대한 출력 관련 메소드 생성/입출력 관련 로직 완성

--- src/Game.js
+++ src/Game.js
@@ -1,23 +1,43 @@
 import { Console } from "@woowacourse/mission-utils";
+import Computer from "./Computer.js";
 
 class Game {
+  constructor(computer = new Computer()) {
+    this.computer = computer;
+  }
+
   gameStart() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    Console.print(this.computer.answer);
     this.getInput();
   }
 
   async getInput() {
     const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
-    this.inputValidator(input);
+    this.inputController(input);
   }
 
-  inputController = (input) => {
+  inputController(input) {
     if (input.length !== 3)
       throw new Error("[ERROR] 잘못된 값을 입력하셨습니다.");
     if (!/^[1-9]{3}$/.test(input))
       throw new Error("[ERROR] 잘못된 값을 입력하셨습니다.");
+
+    const newInput = input.split("").map(Number);
+    const result = this.computer.resultGenerator(newInput);
+
+    this.getOutput(result);
+  }
+
+  getOutput(result) {
+    //test
+    if (result.strike == 3) return;
+
+    const printBall = result.ball ? `${result.ball}볼 ` : "";
+    const printStrike = result.strike ? `${result.strike}스트라이크` : "";
+    Console.print(printBall + printStrike);
     this.getInput();
-  };
+  }
 }
 
 export default Game;
