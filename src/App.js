2994f0a5910c104dc1d58e1d22f2c18410d7c19f
byungyong <quddyddlrk@naver.com>
2023-10-23 15:37:10 +0900

    feat:check user number for length

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ class App {
     }
 
     const NUMBER = await Console.readLineAsync("숫자를 입력해주세요 : ");
+
+    // 사용자 입력 숫자의 길이 확인
+    if (NUMBER.length !== 3) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+    const USER = NUMBER.split("").map(Number);
   }
 }
 
