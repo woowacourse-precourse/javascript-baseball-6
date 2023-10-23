d741423f0d1520d186abfbd4b7ea712b740973d7
JUN <gurwns232@naver.com>
2023-10-24 01:22:42 +0900

    refactor: 함수 jsdocs 주석 추가

--- src/utils/countBall.js
+++ src/utils/countBall.js
@@ -1,3 +1,8 @@
+/**
+ * 컴퓨터와 유저 숫자 비교해서 볼, 스트라이크 카운트 세는 함수
+ * @param {{computer:number[], user:number[]}} 컴퓨터, 유저 숫자
+ * @returns {{ball:number, strike:number}} 볼, 스트라이크 값
+ */
 export default function counteBall({ computer, user }) {
   let ball = 0;
   let strike = 0;
