af6ebb2f2eb1c0b165c8f9310a0e770f028e777f
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-10-24 00:12:17 +0900

    refactor: hint 생성 부분 직관적인 코드로 수정

--- src/Computer.js
+++ src/Computer.js
@@ -34,10 +34,10 @@ class Computer {
 
     convertHintToString(ballCounts, strikeCounts) {
         let hint = '';
-        if(ballCounts > 0)  hint += `${ballCounts}볼`;
-        if(ballCounts > 0 && strikeCounts > 0)  hint += ' ';
-        if(strikeCounts > 0)  hint += `${strikeCounts}스트라이크`;
-        if(hint === '')  hint += '낫싱';
+        if(ballCounts)  hint += `${ballCounts}볼`;
+        if(ballCounts && strikeCounts)  hint += ' ';
+        if(strikeCounts)  hint += `${strikeCounts}스트라이크`;
+        if(ballCounts === 0 && strikeCounts === 0)  hint += '낫싱';
 
         return hint;
     }
