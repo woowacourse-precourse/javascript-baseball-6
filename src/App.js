22f37993f04d0ea74a72d28505d8b6f0e221340f
yesiamok3 <jaehyunok@cau.ac.kr>
2023-10-23 23:46:13 +0900

    게임이 시작할 때 정답 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import * as readline from 'readline';
+
 class App {
-  async play() {}
+  async play() {
+    console.log('숫자 야구 게임을 시작합니다.');
+    var input = 1;    
+    while (input == 1) {
+      input = await playGame();
+    }
+  }
+}
+
+async function playGame(number) {
+  var number = setNumber();
+}
+
+function setNumber() {
+  var number = [0,0,0];
+  var pool = [1,2,3,4,5,6,7,8,9];
+  var temp = rand(9);
+  number[0] = pool[temp];
+  pool.splice(temp, 1);
+
+  temp = rand(8);
+  number[1] = pool[temp];
+  pool.splice(temp, 1);
+
+  temp = rand(7);
+  number[2] = pool[temp];
+
+  return number;
+}
+
+function rand(max) {
+  var temp = Math.random() * max;
+  temp = Math.floor(temp);
+
+  return temp;
 }
 
 export default App;
