2eec3c5f880bb2af3443679d3aa62f748f159aa4
KmYongHwan <kyh5678@gmail.com>
2023-10-24 03:50:30 +0900

    modify ' to `
    ' 이걸 ` 이거로 변경, 억음 부호 조심

--- src/App.js
+++ src/App.js
@@ -43,15 +43,15 @@ class App {
         }
       }
     }
-    
+
     if(ball !== 0 && strike !== 0){
-      MissionUtils.Console.print('${ball}볼 ${strike}스트라이크');
+      MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
     }
     else if(ball !== 0 && strike == 0){
-      MissionUtils.Console.print('${ball}볼');
+      MissionUtils.Console.print(`${ball}볼`);
     }
     else if(ball == 0 && strike !== 0){
-      MissionUtils.Console.print('${strike}스트라이크');
+      MissionUtils.Console.print(`${strike}스트라이크`);
     }
     else{
       MissionUtils.Console.print('낫싱');
