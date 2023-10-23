3825bff9cab9f88070304a6105d361f6eccaa4eb
Jihyun_Kim <jihyun3313@daum.net>
2023-10-23 21:26:35 +0900

    비교 결과 출력 및 게임 재시작 구현

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class App {
 export default App;
 
 
-
 const gameStart = () => {
   MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   computerNum(); // 컴퓨터에서 임의의 수 3개 선택
@@ -23,23 +22,24 @@ const computerNum = () => {
       computer.push(number);
     }
   }
+  console.log('computer', computer);
   userInputNum(computer); // 사용자로부터 3자리 수 입력받기
-
 }
 
 const userInputNum = async (computer) => {
   let userInput;
   try {
     userInput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+    console.log('user', userInput);
+    checkNum(userInput, computer);  // 입력값 유효성 체크
   } catch (error) {
-    // reject 되는 경우
+    MissionUtils.Console.print(error);
   }
-  checkNum(userInput, computer);  // 입력값 유효성 체크
 }
 
 const checkNum = (userInput, computer) => {
   let user = userInput.split("");
-  if (user.length == 3) {
+  if (user.length === 3) {
     user.forEach((e, index) => {
       user[index] = parseInt(e);
       if (isNaN(e)) throw new Error("[ERROR] 숫자가 잘못된 형식입니다.")
@@ -62,16 +62,47 @@ const compare = (user, computer) => {
     })
     user === computer[index] ? strikeCount++ : null;
   })
-  ballCount === 0 ? nothing=true : null;
+  ballCount === 0 ? nothing = true : null;
   ballCount -= strikeCount; // 스트라이크카운트(같은수가 같은자리에)는 볼카운에서 제외
-
-  console.log("ballCount", ballCount);
-  console.log("strikeCount", strikeCount);
-  console.log("nothing", nothing);
+  compareResult(ballCount, strikeCount, nothing, computer);
 }
 
+const compareResult = (ball, strike, nothing, computer) => {
+  if (nothing === true) {
+    MissionUtils.Console.print('낫싱');
+    userInputNum(computer);
+  }
+  else if (strike === 3) {
+    MissionUtils.Console.print('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    try {
+      askNewGame();
+    } catch {
+      MissionUtils.Console.print(error);
+    }
+  }
+  else {
+    MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+    userInputNum(computer);
+  }
+}
 
-
+const askNewGame = async () => {
+  let userInput;
+  try {
+    userInput = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
+  } catch (error) {
+    MissionUtils.Console.print(error);
+  }
+  console.log("userInput", userInput);
+  if (userInput === '1') {
+    try {
+      computerNum();
+    } catch {
+      MissionUtils.Console.print(error);
+    }
+  }
+  else null;
+}
 
 const app = new App();
 app.play();
