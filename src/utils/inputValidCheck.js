e4a2186ae2b7eb822b2e4399dbd30989a270fe9b
yeokyoung song <spolice0324@gmail.com>
2023-10-24 01:10:50 +0900

    feat : 유효성 검사 함수 구현
    - 입력값에 대한 유효성 검사를 진행함

---
+++ src/utils/inputValidCheck.js
@@ -0,0 +1,22 @@
+import { ANSWER_LENGTH } from "../Constants";
+
+const inputValidCheck = (input) => {
+  if (input.length !== ANSWER_LENGTH) {
+    throw new Error("[ERROR] 입력 값의 길이는 3이어야 합니다.");
+  }
+  if (Number.isNaN(Number(input))) {
+    throw new Error("[ERROR] 입력 값은 숫자여야 합니다.");
+  }
+
+  if (new Set(input).size !== ANSWER_LENGTH) {
+    throw new Error("[ERROR] 입력 값은 중복되는 숫자가 없어야 합니다.");
+  }
+
+  if (input.includes("0")) {
+    throw new Error("[ERROR] 입력 값은 1~9의 숫자여야 합니다.");
+  }
+
+  return true;
+};
+
+export default inputValidCheck;
