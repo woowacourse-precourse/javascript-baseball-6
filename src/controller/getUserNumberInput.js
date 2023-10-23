4a7adafbf3059501ea84e33b849df48ad12a96ee
JiSeong Kim <12180543@inha.edu>
2023-10-23 15:59:37 +0900

    Refactor: 상황에 맞는 에러메시지를 throw하기 위해 getUserNumberInput 함수 수정
     기존에는 4가지의 검증 함수를 통과하면 인풋을 return하고, 통과하는 도중 검증 함수로부터 false가 반환되면 throw new Error를 하는 로직이었음. 각 검증에 맞게 오류메시지를 throw하기 위해 각각의 검증 함수에 올바르지 않은 인풋이 들어왔을 때 return false를 하는 동작에서 각 함수 별 Error를 throw하는 동작으로 바꾸었음. 이에 따라 return boolean이 아닌, 에러를 던져주는 동작만 하므로 의미를 위해 함수의 앞에 check라는 이름으로 변경하였음.

--- src/controller/getUserNumberInput.js
+++ src/controller/getUserNumberInput.js
@@ -1,50 +1,30 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-const isThreeDigit = (input) => {
-    if (input.length === 3) {
-        return true
-    } else {
-        throw new Error("[ERROR] 3자리의 숫자가 아닙니다.")
-    }
+const checkThreeDigit = (input) => {
+    if (input.length !== 3) { throw new Error("[ERROR] 3자리의 숫자가 아닙니다.") }
 }
 
-const isNotDuplicated = (input) => {
-    if (new Set(input).size === input.length) {
-        return true
-    } else {
-        throw new Error("[ERROR] 각 자리의 수는 중복되지 않아야 합니다.")
-    }
+const checkNotDuplicated = (input) => {
+    if (new Set(input).size !== input.length) { throw new Error("[ERROR] 각 자리의 수는 중복되지 않아야 합니다.") }
 }
 
-const isAllNumber = (input) => {
-    for (const str of input) {
-        if (isNaN(parseInt(str))) {
-            throw new Error("[ERROR] 입력에는 숫자만 포함되어야 합니다.")
-        }
-    }
-    return true
+const checkAllNumber = (input) => {
+    const throwNotNumberError = () => { throw new Error("[ERROR] 입력에는 숫자만 포함되어야 합니다.") }
+    input.split('').reduce((idx, inputElement) => isNaN(parseInt(inputElement)) && throwNotNumberError())
 }
 
-const hasNoZero = (input) => {
-    for (const str of input) {
-        if (parseInt(str) === 0) {
-            throw new Error("[ERROR] 입력에는 0이 포함되지 않아야 합니다.")
-        }
-    }
-    return true
+const checkNoZero = (input) => {
+    const throwHasZeroError = () => { throw new Error("[ERROR] 입력에는 0이 포함되지 않아야 합니다.") }
+    input.split('').reduce((idx, inputElement) => parseInt(inputElement) === 0 && throwHasZeroError())
 }
 
 export const getUserNumberInput = async () => {
-    return await MissionUtils.Console.readLineAsync('숫자를 입력하세요 : ')
-        .then(userNumberInput => {
-            if (isThreeDigit(userNumberInput) && isAllNumber(userNumberInput)  
-                && isNotDuplicated(userNumberInput) && hasNoZero(userNumberInput)) {
-                return userNumberInput
-            } else {
-                throw new Error("[ERROR]");
-            }
-        })
-        .catch(err => {
-            throw err;
-        });
+    const userNumberInput = await MissionUtils.Console.readLineAsync('숫자를 입력하세요 : ')
+    
+    checkThreeDigit(userNumberInput)
+    checkAllNumber(userNumberInput)
+    checkNotDuplicated(userNumberInput)
+    checkNoZero(userNumberInput)
+
+    return userNumberInput
 }
