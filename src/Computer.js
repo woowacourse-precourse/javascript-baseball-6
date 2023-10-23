27a4081f3749babf73f070af0c9dd10ec6fd481f
hojkim77 <rlaghwnd4534@gmail.com>
2023-10-23 21:13:56 +0900

    [feat] 컴퓨터Class - 랜덤한 숫자 생성 메서드 완성

--- src/Computer.js
+++ src/Computer.js
@@ -1,1 +1,13 @@
-class Computer {}
+import { Random } from "@woowacourse/mission-utils";
+class Computer {
+  randomNumGenerate() {
+    const numbers = [];
+    while (numbers.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!numbers.includes(number)) {
+        numbers.push(number);
+      }
+    }
+    return numbers;
+  }
+}
