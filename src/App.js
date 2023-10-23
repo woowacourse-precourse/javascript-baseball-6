8db4d13dff13a815c824f4f827f25152ce4c52b2
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-10-23 21:05:25 +0900

    [feat] : Add basic settings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    let userAnswer = 1;
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    while (userAnswer == 1) {}
+  }
 }
 
 export default App;
