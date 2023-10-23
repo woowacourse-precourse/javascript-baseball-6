610e30effa27ad5aff70629d06311e5acb83fe3d
hyj5076 <hyj5076@hanmail.net>
2023-10-23 17:27:24 +0900

    refactor: 게임 리플레이 선택 로직 개선

--- src/play/ReplayManager.js
+++ src/play/ReplayManager.js
@@ -2,19 +2,19 @@ import { Console } from "@woowacourse/mission-utils";
 import * as Constants from '../const/Messages';
 
 class ReplayManager {
-  async handleReplay(newGame) {
-    // Console.print(Constants.GOAL);
-    Console.print(Constants.SUGGEST_REPLAY);
-    const userChoice = await Console.readLineAsync();
-
-    if (userChoice === '1') {
-      return newGame.play(); // 새로운 게임 시작
-    } else if (userChoice === '2') {
-      Console.print(Constants.GAME_OVER);
-      return; // 게임 종료
+    async handleReplay() {
+      Console.print(Constants.SUGGEST_REPLAY);
+      const userChoice = await Console.readLineAsync();
+      if (userChoice === '1') {
+        return true; // 새로운 게임 시작
+      } else if (userChoice === '2') {
+        return false; // 게임 종료
+      } else {
+        Console.print("올바르지 않음");
+        return false;
+      }
     }
   }
-}
 
 export default ReplayManager;
   
