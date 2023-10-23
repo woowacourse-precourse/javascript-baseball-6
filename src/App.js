1a0e6a3c03fb293559392dc74a51fbc37ac0c555
JungHun98 <j7papa@naver.com>
2023-10-23 15:04:15 +0900

    feat: App play 메소드 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
 import tryMatch from './modules/tryMatch';
 import getRandomUniqueNumber from './modules/getRandomUniqueNumber';
 import getNextStep from './modules/getNextStep';
+
+const START_PROMPT = '숫자 야구 게임을 시작합니다.';
+const EXIT_PROMPT = '게임 종료';
 
 const EXIT = '2';
 
@@ -20,7 +24,11 @@ class App {
     }
   }
 
-  async play() {}
+  async play() {
+    Console.print(START_PROMPT);
+    await this.startGame();
+    Console.print(EXIT_PROMPT);
+  }
 }
 
 export default App;
