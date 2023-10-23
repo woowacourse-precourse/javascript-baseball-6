02369636d648a0f1747cfdd642afe61b55aa00c4
Parksangwoo <psu8430@gmail.com>
2023-10-23 14:34:16 +0900

    Feat: 3자리 랜덤 숫자 생성 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
+
+// 3자리의 랜덤 숫자를 생성하는 함수
+const create_random_number = () => {
+  let number_list = "";
+  while (number_list.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+    number_list += number.toString();
+  }
+
+  return number_list;
+};
+
 class App {
   async play() {}
 }
