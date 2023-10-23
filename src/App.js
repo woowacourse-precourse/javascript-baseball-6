d6dbca24c5a8c9aca0e72dca7faedda80cfe2283
조예진 <joyejin@joyejins-MacBook-Air.local>
2023-10-23 20:12:05 +0900

    feat: 3자리의 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
+  // 램덤 숫자 생성
+  createRamdomNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+  }
+
   async play() {}
 }
 
