ded383573436051f1e0e913ec6a0675e96b5cfcc
thumbthing <ilscc0902@gmail.com>
2023-10-24 01:37:28 +0900

    feat: answer 생성 기능 구현

---
+++ src/util/createAnswer.js
@@ -0,0 +1,25 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+function createAnswer(savedAnswer) {
+  function createRandomNumber(answer, index) {
+    const RANDOM_NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (index === 0) {
+      return RANDOM_NUMBER;
+    } else {
+      if (answer.includes(RANDOM_NUMBER)) {
+        return createRandomNumber(answer, index);
+      } else {
+        return RANDOM_NUMBER;
+      }
+    }
+  }
+
+  const newAnswer = [];
+  for (let i = 0; i < 3; i++) {
+    const SINGLE_ANSWER = createRandomNumber(newAnswer, i);
+    newAnswer.push(SINGLE_ANSWER);
+  }
+  return [...savedAnswer, ...newAnswer];
+}
+
+export default createAnswer;
