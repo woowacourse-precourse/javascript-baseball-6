6b1db14ef4801217509e2332f202f9289a89a695
Kysiam <how2logout@gmail.com>
2023-10-23 19:38:53 +0900

    feature: 포수의 무작위 값 리턴하는 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 class App {
   async play() {}
+
+  getCatcherNumbers() {
+    const catcherNumbers = [];
+    while (catcherNumbers < 3) {
+      const catcherNumber = Random.pickNumberInRange(1, 9);
+      if (!catcherNumbers.includes(catcherNumber)) {
+        catcherNumbers.push(catcherNumber);
+      }
+    }
+    return catcherNumbers;
+  }
 }
 
 export default App;
+
