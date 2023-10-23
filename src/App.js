fd941ec30c9e748f5649e679f2e241cb0720dc48
unknown <pih9134@naver.com>
2023-10-24 03:23:01 +0900

    feat: 유저에게 입력받는 기능

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다');
+    try {
+      const userInput = await this.getUserInput();
+    } catch (error) {
+      throw new Error('[ERROR]');
+    }
   }
+
+  async getUserInput() {
+    return Console.readLineAsync('숫자를 입력해주세요 : ');
+  }
+
 }
+
+const app = new App();
+app.play();
 
 export default App;
