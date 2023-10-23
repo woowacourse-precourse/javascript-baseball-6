d6359316d2429bb19aa3ec4ecb69687c4f48237f
Nahyun <from.owol@gmail.com>
2023-10-23 21:17:20 +0900

    feat: add user number input
    사용자 숫자 입력받고 유효성 검사

--- src/controller/BaseballGameController.js
+++ src/controller/BaseballGameController.js
@@ -1,8 +1,16 @@
+import Validator from '../utils/Validator.js';
+import { Console } from '@woowacourse/mission-utils';
 import OutputView from '../view/OutputView.js';
 
 class BaseballGameController {
   start() {
     OutputView.printStart();
+    return Console.readLineAsync('숫자를 입력해주세요 : ').then(
+      (userNumbers) => {
+        Validator.validateNumber(userNumbers);
+        console.log(userNumbers);
+      }
+    );
   }
 }
 
---
+++ src/utils/Validator.js
@@ -0,0 +1,21 @@
+class Validator {
+  static validateNumber(userNumbers) {
+    if (/\s/.test(userNumbers.trim())) {
+      throw new Error('[ERROR] 중간에 공백은 입력할 수 없습니다.');
+    }
+    if (isNaN(Number(userNumbers))) {
+      throw new Error('[ERROR] 숫자가 아닙니다.');
+    }
+    if (userNumbers.trim().length !== 3) {
+      throw new Error('[ERROR] 길이가 3이여야 합니다.');
+    }
+    if (userNumbers.includes('0')) {
+      throw new Error('[ERROR] 0이 포함되어서는 안됩니다.');
+    }
+    if (new Set(userNumbers).size !== userNumbers.length) {
+      throw new Error('[ERROR] 중복된 숫자를 입력해서는 안됩니다.');
+    }
+  }
+}
+
+export default Validator;
