5b54a872192485f5ab953a0920f411c582615ca0
Kled <1017yu@daum.net>
2023-10-23 16:21:39 +0900

    refactor: JSDoc을 통한 코드 구조와 선언한 변수의 역할 상세화

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import { Console } from '@woowacourse/mission-utils';
 import { GUIDE_MESSAGES } from '../constants/index.js';
 import Player from './player/index.js';
 
+//숫자야구 어플리케이션의 메인 클래스
 class App {
   #player = new Player();
 
+  /**
+   * @async
+   * @returns {void}
+   */
   async play() {
     Console.print(GUIDE_MESSAGES.START);
     await this.#player.startGame();
