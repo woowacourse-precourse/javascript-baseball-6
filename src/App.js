7e27db03626c6683bfb9a2c2176ae110fafdfbc5
whddbsl <whddbs04140@naver.com>
2023-10-23 16:15:10 +0900

    feat: 잘못된 입력 값에 대한 예외 처

--- src/App.js
+++ src/App.js
@@ -19,6 +19,40 @@ class App {
       "숫자를 입력해주세요 : "
     );
     Console.print(userInputNumber);
+    // 1~9까지 중복되지 않은 세자리 수 정규식
+    // const regex = /^(?!.*(.).*\1)[1-9]{3}$/;
+    // if (!regex.test(userInputNumber)) {
+    //   throw new Error('[Error] 중복되지 않은 1부터 9 사이의 세자리 수를 입력해주세요')
+    // }
+
+    // 예외사항 처리
+    // 1. 세자리 숫자가 입력되지 않을 경우
+    // 2. 숫자가 아닌 문자가 입력 될 경우
+    // 3. 중복된 숫자가 입력 될 경우
+    // 4. 1~9가 아닌 숫자가 입력 될 경우
+
+    if (userInputNumber.length !== 3) {
+      throw new Error("[Error] 세자리 수를 입력해주세요");
+    }
+
+    if (isNaN(userInputNumber)) {
+      throw new Error("[Error] 숫자를 입력해주세요");
+    }
+
+    if (
+      userInputNumber[0] === userInputNumber[1] ||
+      userInputNumber[0] === userInputNumber[2] ||
+      userInputNumber[1] === userInputNumber[2]
+    ) {
+      throw new Error("[Error] 중복되지 않은 숫자를 입력해주세요");
+    }
+
+    if (userInputNumber.includes(0)) {
+      throw new Error("[Error] 1~9 사이의 숫자를 입력해주세요");
+    }
+
+    const userAnswer = userInputNumber.split("").map((number) => +number);
+    Console.print(userAnswer);
   }
 
   static play() {
