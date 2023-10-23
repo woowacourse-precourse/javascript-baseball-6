6ae591ee0a9e8174178ccd4b6331d8f6309d3e6a
apfwnls14 <apfwnls14@naver.com>
2023-10-23 15:19:04 +0900

    feat: 숫자 야구 힌트 - 스트라이크, 볼 개수 구하기 구현

--- src/App.js
+++ src/App.js
@@ -10,13 +10,27 @@ class App {
         computer.push(number);
       }
     }
-    Console.print(computer);
+    // Console.print(computer);
     const number = await Console.readLineAsync("숫자를 입력해주세요 : ");
     const user = number.split("").map(Number);
     if (user.length !== 3 || number === null || new Set(user).size !== 3) {
       throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
     }
-    Console.print(user);
+    // Console.print(user);
+    let strike = 0;
+    let ball = 0;
+    for (let i = 0; i < computer.length; i++) {
+      const index = user.indexOf(computer[i]);
+      if (index > -1) {
+        // 같은 숫자가 존재하는 경우
+        if (index === i) {
+          strike++; // 자리까지 동일한 경우
+        } else {
+          ball++;
+        }
+      }
+    }
+    Console.print(`스트라이크 : ${strike}, 볼: ${ball}`);
   }
 }
 
