a6971921c1f59bceca8006b3ed16e449fc2bffb8
zxxng <jeayeong123@naver.com>
2023-10-23 19:12:42 +0900

    feat: 숫자 입력하는 기능 추가

--- src/classes/GamePlayer.js
+++ src/classes/GamePlayer.js
@@ -2,7 +2,20 @@ import { Console } from '@woowacourse/mission-utils';
 import NumberGenerator from './NumberGenerator';
 import GameEngine from './GameEngine';
 
+const gameEngine = new GameEngine();
+
 class GamePlayer {
+  // 숫자를 입력받는 함수
+  async receiveNumber(computerNumber) {
+    let isThreeStrike = false;
+    while (!isThreeStrike) {
+      const input = await Console.readLineAsync('숫자를 입력해주세요 : ');
+      const playerNumber = gameEngine.inputToArray(input);
+      isThreeStrike = gameEngine.countScore(computerNumber, playerNumber);
+    }
+    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+  }
+
   // 재시작할지 선택하는 함수
   async isRestart() {
     const isRestart = await Console.readLineAsync(
