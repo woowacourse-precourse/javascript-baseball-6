dfeebe91c24a3345e89b42a7b964274318a83a16
devGEP <ju0032564@gmail.com>
2023-10-24 03:38:42 +0900

    feat: 사용자 숫자 입력받기

--- src/game/GameController.js
+++ src/game/GameController.js
@@ -6,6 +6,14 @@ class GameController {
     this.gameLogic = new GameLogic();
   }
 
+  async getUserInput(question) {
+    const answer = await MissionUtils.Console.readLineAsync(question);
+    if (answer === undefined) {
+      throw new Error("[ERROR] 올바른 입력 형태가 아닙니다.");
+    }
+    return answer;
+  }
+
   async startGame() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     await this.playGame();
@@ -13,6 +21,19 @@ class GameController {
 
   async playGame() {
     let computerNumber = this.gameLogic.createComputerNumber();
+    let userNumber = '';
+
+    while(true) {
+      const answer = await this.getUserInput('숫자를 입력해주세요 : ');
+
+      const userNumberToString = String(answer);
+      const mapfn = (arg) => Number(arg);
+      const userNumberArray = Array.from(userNumberToString, mapfn);
+
+      userNumber = this.gameLogic.checkNumber(computerNumber, userNumberArray);
+      MissionUtils.Console.print(userNumber);
+
+    }
   }
 }
 
