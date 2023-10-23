8b6dd849258349ff2d326efaf4676393182a93cf
mingi1123 <min_gi1123@naver.com>
2023-10-24 04:40:18 +0900

    feat: print gamestart & make random number

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    Console.print(this.makeNumber());
+  }
+
+  makeNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
