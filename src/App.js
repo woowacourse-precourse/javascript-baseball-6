a4c6e03b3b6e83a895be82df018d451c04f61bee
seoyeon-jung <jungluchia1204@gmail.com>
2023-10-23 19:09:52 +0900

    feat: 숫자 유효성 검사 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  async play() {}
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ", (answer) => {
+      if (!this.isValidBallNumber(answer)) {
+        throw new Error();
+      }
+    });
+  }
+
+  isValidBallNumber(answer) {
+    if (answer.length !== 3) {
+      return false;
+    }
+    if (
+      answer[0] === answer[1] ||
+      answer[1] === answer[2] ||
+      answer[2] === answer[0]
+    ) {
+      return false;
+    }
+    if (isNaN(Number(answer))) {
+      return false;
+    }
+    return true;
+  }
 }
 
-export default App;
+const init = new App();
+init.play();
+
+module.exports = App;
