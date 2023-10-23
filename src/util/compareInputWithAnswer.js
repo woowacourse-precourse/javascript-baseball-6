66dabe419baa78b5087c54b46fafe1c89a224937
thumbthing <ilscc0902@gmail.com>
2023-10-24 08:42:34 +0900

    fix: 비동기 함수에 누락된 try-catch 추가

--- src/util/compareInputWithAnswer.js
+++ src/util/compareInputWithAnswer.js
@@ -1,26 +1,32 @@
+import throwInvalidInputErrorMessage from "./throwInvalidInputErrorMessage.js";
+
 async function compareInputWithAnswer(answer, input) {
-  const LAST_ANSWER = answer.slice(-3);
-  let strikeCount = 0;
-  let ballCount = 0;
+  try {
+    const LAST_ANSWER = answer.slice(-3);
+    let strikeCount = 0;
+    let ballCount = 0;
 
-  for (let aIndex = 0, iIndex = 0; iIndex < LAST_ANSWER.length; ) {
-    if (LAST_ANSWER[aIndex] === input[iIndex]) {
-      strikeCount = strikeCount + 1;
-    } else {
-      if (LAST_ANSWER.includes(input[iIndex])) {
-        ballCount = ballCount + 1;
+    for (let aIndex = 0, iIndex = 0; iIndex < LAST_ANSWER.length; ) {
+      if (LAST_ANSWER[aIndex] === input[iIndex]) {
+        strikeCount = strikeCount + 1;
+      } else {
+        if (LAST_ANSWER.includes(input[iIndex])) {
+          ballCount = ballCount + 1;
+        }
       }
+      aIndex = aIndex + 1;
+      iIndex = iIndex + 1;
     }
-    aIndex = aIndex + 1;
-    iIndex = iIndex + 1;
+
+    const RESULT = {
+      strike: strikeCount,
+      ball: ballCount,
+    };
+
+    return RESULT;
+  } catch (error) {
+    throwInvalidInputErrorMessage(error);
   }
-
-  const RESULT = {
-    strike: strikeCount,
-    ball: ballCount,
-  };
-
-  return RESULT;
 }
 
 export default compareInputWithAnswer;
