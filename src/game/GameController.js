d830a31b902f4d6140abd69f638b81b10ad426b7
devGEP <ju0032564@gmail.com>
2023-10-24 03:40:34 +0900

    feat: 게임 종료 및 이후 사용자 입력

--- src/game/GameController.js
+++ src/game/GameController.js
@@ -32,6 +32,27 @@ class GameController {
 
       userNumber = this.gameLogic.checkNumber(computerNumber, userNumberArray);
       MissionUtils.Console.print(userNumber);
+
+      if (userNumber === '3스트라이크') {
+        return this.strike();
+      }
+    }
+  }
+
+  strike() {
+    MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    return this.restartGame();
+  }
+
+  async restartGame() {
+    const answer = await this.getUserInput('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+
+    if (answer === '1') {
+      return this.playGame();
+    } else if (answer === '2') {
+      return false;
+    } else {
+      throw new Error('[ERROR] 입력이 잘못된 형식입니다.');
     }
   }
 }
