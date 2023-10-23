51e642fdd2ce954e7dc9cd88a15b9d84831469cb
JiSeong Kim <12180543@inha.edu>
2023-10-23 15:15:27 +0900

    Fix: isAllNumber 함수에서 숫자 이외의 값이 포함되었을 때를 검증하지 못하는 오류를 수정
    parseInt(str) === NaN이라는 조건으로 함수를 작성하였는데, 제대로 동작하지 않아 isNaN 함수를 감싸 처리하였음.

--- src/controller/getUserNumberInput.js
+++ src/controller/getUserNumberInput.js
@@ -4,7 +4,7 @@ const isThreeDigit = (input) => {
     if (input.length === 3) {
         return true
     } else {
-        return false
+        throw new Error("[ERROR] 3자리의 숫자가 아닙니다.")
     }
 }
 
@@ -12,14 +12,14 @@ const isNotDuplicated = (input) => {
     if (new Set(input).size === input.length) {
         return true
     } else {
-        return false
+        throw new Error("[ERROR] 각 자리의 수는 중복되지 않아야 합니다.")
     }
 }
 
 const isAllNumber = (input) => {
     for (const str of input) {
-        if (parseInt(str) === NaN) {
-            return false
+        if (isNaN(parseInt(str))) {
+            throw new Error("[ERROR] 입력에는 숫자만 포함되어야 합니다.")
         }
     }
     return true
@@ -28,7 +28,7 @@ const isAllNumber = (input) => {
 const hasNoZero = (input) => {
     for (const str of input) {
         if (parseInt(str) === 0) {
-            return false
+            throw new Error("[ERROR] 입력에는 0이 포함되지 않아야 합니다.")
         }
     }
     return true
@@ -37,8 +37,8 @@ const hasNoZero = (input) => {
 export const getUserNumberInput = async () => {
     return await MissionUtils.Console.readLineAsync('숫자를 입력하세요 : ')
         .then(userNumberInput => {
-            if (isThreeDigit(userNumberInput) && isNotDuplicated(userNumberInput) 
-                && isAllNumber(userNumberInput) && hasNoZero(userNumberInput)) {
+            if (isThreeDigit(userNumberInput) && isAllNumber(userNumberInput)  
+                && isNotDuplicated(userNumberInput) && hasNoZero(userNumberInput)) {
                 return userNumberInput
             } else {
                 throw new Error("[ERROR]");
