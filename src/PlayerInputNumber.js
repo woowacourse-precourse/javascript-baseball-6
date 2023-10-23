40aeedf9a420b2b3162750f9c5f9c36f6230248f
soi-ha <soy2302ten@gmail.com>
2023-10-23 18:26:07 +0900

    chore: Exception 정의

--- src/PlayerInputNumber.js
+++ src/PlayerInputNumber.js
@@ -1,20 +1,26 @@
 import { Console } from '@woowacourse/mission-utils';
 
+function Exception(name, message) {
+	this.name = name;
+	this.message = message;
+}
+
 export default async function PlayerInputNumber() {
 	try {
 		const answer = await Console.readLineAsync('숫자를 입력해주세요 : ');
 
 		if (!Boolean(Number(answer))) {
-			throw { name: 'NotANumber', message: '숫자를 입력하지 않았습니다.' };
+			// throw { name: 'NotANumber', message: '숫자를 입력하지 않았습니다.' };
+			throw new Exception('NotANumber', '숫자를 입력하지 않았습니다.');
 		}
 		if (!Number.isInteger(Number(answer))) {
-			throw { name: 'NotAnInteger', message: '정수를 입력하지 않았습니다.' };
+			throw new Exception('NotAnInteger', '정수를 입력하지 않았습니다.');
 		}
 		if (answer.length !== 3) {
-			throw { name: 'LengthNotMath', message: '3자리를 입력하지 않았습니다.' };
+			throw new Exception('LengthNotMath', '3자리를 입력하지 않았습니다.');
 		}
 		if (answer.includes('0')) {
-			throw { name: 'ContainsZero', message: '입력한 값에 0이 포함되어 있습니다.' };
+			throw new Exception('ContainsZero', '입력한 값에 0이 포함되어 있습니다.');
 		}
 	} catch (error) {
 		console.log(`[ERROR] ${error.message}`);
