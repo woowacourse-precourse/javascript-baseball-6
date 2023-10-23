dadc21acbfbaf0f5bd6cd48626b41ad1fdcd2d6f
Junseo <monstazo911@gmail.com>
2023-10-24 01:17:06 +0900

    Feat: 추가 시작 인사 출력 메소드
    시작 인사를 하는 것도 게임기의 하나의 행동이므로 메소드로 구현했다.
    메소드명은 시작인사를 출력하는 것이니 printGreeting으로 했다.
    woowacourse/mission-utils 모듈의 index.js에서 Random, Console을 export하고 있으니 Named Import를 하였다.
    Console API의 print 메소드는 출력할 값 'message'를 인수로 받으므로 print 메소드를 코드와 같이 사용했다.
    JavaScript 코드 컨벤션 9.7에 따라 인스턴스를 참조하지 않으므로 static 메소드로 구현했다.
    play 메소드의 최상단에서 사용하였다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
   constructor() {
     this.secretNumber = null;
   }
-  async play() {}
+
+  static printGreeting() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  async play() {
+    printGreeting();
+  }
 }
 
 export default App;
