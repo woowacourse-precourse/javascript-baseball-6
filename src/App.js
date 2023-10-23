9f3e06ae470b9addefbe0303f12931e4c2a5a05c
whddbsl <whddbs04140@naver.com>
2023-10-23 16:14:23 +0900

    feat: 숫자 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { Random, Console } from '@woowacourse/mission-utils';
-
+import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
   // 답 생성
@@ -11,13 +10,21 @@ class App {
         randomNumber.push(number);
       }
     }
-    
+
     Console.print(randomNumber); // 확인
   }
 
+  static async getUserNumber() {
+    const userInputNumber = await Console.readLineAsync(
+      "숫자를 입력해주세요 : "
+    );
+    Console.print(userInputNumber);
+  }
+
   static play() {
-    Console.print('숫자 야구 게임을 시작합니다')
+    Console.print("숫자 야구 게임을 시작합니다.");
     this.setRandomNumber();
+    this.getUserNumber();
   }
 }
 
