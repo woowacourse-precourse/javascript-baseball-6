249a50247053d57dc6a6258da188745a5f2ef4be
kwani6684 <jack0910@naver.com>
2023-10-23 14:55:22 +0900

    🐛 예외처리 고침#2.5 [...new Set(number)]

--- src/App.js
+++ src/App.js
@@ -37,7 +37,8 @@ class User {
       throw new Error("[ERROR] 1 ~ 9 사이의 숫자를 입력하지 않았습니다.");
     }
     // #2.5 같은 숫자를 입력한 경우
-    if(new Set(number).length!=3){
+    if([...new Set(number)].length!=3){
+      
       throw new Error("[ERROR] 서로 다른 숫자를 입력하지 않았습니다.");
 
     }
@@ -101,6 +102,9 @@ class App {
       }
 
       newGame = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+      if(newGame=='1'||newGame=='2'){
+        throw new Error("1 또는 2 를 입력해주세요")
+      }
     }
   }
 }
