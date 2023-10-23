0491cf05266b4284b2e4d505fde9f6719fd7c5a8
hwinkr <dnddl8280@naver.com>
2023-10-23 22:16:59 +0900

    config: 참여자가 잘못된 입력을 했을 경우 출력해야 할 에러 메세지를 상수 객체로 설정

---
+++ src/constants/ErrorMessages.js
@@ -0,0 +1,9 @@
+const ERROR_MESSAGES = Object.freeze({
+  DUPLICATED: "[ERROR] 입력한 3개의 숫자는 모두 달라야 해요!",
+  INVALID_STRING:
+    "[ERROR] 문자를 포함하지 않는 서로 다른 3자리 숫자를 입력해 주세요!",
+  INVALID_INPUT_LENGTH: "[ERROR] 3자리 숫자를 입력해 주세요!",
+  INVALID_RESTART_SIGN: "[ERROR] 1 또는 2를 입력해 주세요!",
+});
+
+export default ERROR_MESSAGES;
