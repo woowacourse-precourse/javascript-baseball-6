51a936f4d043c70719bbd26eabeb724fdcec5bea
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-23 19:07:45 +0900

    :recycle: refactor: 단항증가문 변경과 변수선언부분 수정 바로 string타입으로 변환해서 값을 얻을수있도록 불필요한 선언 제거, 단항증가문 ++ 대신에 += 1 을 사용함, 선언문을 분리시킴

--- src/models/GameAnalyzer.js
+++ src/models/GameAnalyzer.js
@@ -1,17 +1,17 @@
 const GameAnalyzer = {
 
   getBullsAndCows (computerNumber, userNumber) {
-    const computerString = String(computerNumber), userString = String(userNumber);
 
-    return this.calcBallAndStrik(computerString, userString);
+    return this.calcBallAndStrik(String(computerNumber), String(userNumber));
   },
 
   calcBallAndStrik (computerString, userString) {
-    let ball = 0, strike = 0;
+    let ball = 0; 
+    let strike = 0;
 
     [...computerString].forEach((char, index) => {
-      if (userString[index] === char) strike++;
-      else if (userString.includes(char)) ball++;
+      if (userString[index] === char) strike += 1;
+      else if (userString.includes(char)) ball += 1;
     });
     
     return { ball, strike };
