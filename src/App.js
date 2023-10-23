ea4b2467b932586b650e3653e1278017fbba7064
yeeun426 <thsudkcla7@naver.com>
2023-10-23 12:23:36 +0900

    Feat : 컴퓨터가 생각하고 있는 랜덤값 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+
+    // 컴퓨터가 생각하고 있는 랜덤값
+    const computer = Random.pickUniqueNumbersInRange(1, 9, 3);
+    const RandomNum = Number(computer.join(""));
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
