4b17622345586126074b82b4dea14fc171040f9c
8ark <sorlti6952@gmail.com>
2023-10-23 18:04:51 +0900

    Style : "Airbnb 스타일 가이드에 맞춰 변경"
    Style : "Airbnb 스타일 가이드에 맞춰 변경"
    
    변수 camelCase로 변경
    캐스팅 parseInt -> Number로 변경
    변수명 명확하게 변경

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   constructor() {
-    this.is_correct = false; //strike 3이면 true
-    this.is_end = false; //IS_CORRECT가 true일 때 2의 값을 가지면 true
+    this.isCorrect = false;
+    this.isEnd = false;
   }
-  //사용자 숫자 입력
+
   async getUserNumber() {
-    let user = await MissionUtils.Console.readLineAsync(
+    let userNum = await MissionUtils.Console.readLineAsync(
       "숫자를 입력해주세요 : "
     );
-
-    user.split("").forEach((num, index) => {
+    userNum.split("").forEach((num, index) => {
       if (isNaN(num)) {
         throw new Error("[ERROR] 숫자만 입력 가능합니다.");
       }
-      if (user.includes(num) && user.indexOf(num) !== index) {
+      if (userNum.includes(num) && userNum.indexOf(num) !== index) {
         throw new Error("[ERROR] 중복되지 않는 숫자를 입력해주세요.");
       }
 
@@ -22,36 +21,35 @@ class App {
         throw new Error("[ERROR] 1~9 사이의 숫자만 입력 가능합니다.");
       }
 
-      if (user.length !== 3) {
+      if (userNum.length !== 3) {
         throw new Error("[ERROR] 3자리 숫자만 입력 가능합니다.");
       }
     });
 
-    //parse to Array
-    user = user.split("").map((num) => parseInt(num));
+    userNum = userNum.split("").map((num) => {
+      return Number(num);
+    });
 
-    return user;
+    return userNum;
   }
 
-  //컴퓨터 숫자 생성
   getRandomNumber() {
     let com = [];
+    let tempNum = 0;
     while (com.length < 3) {
-      const TEMPNUM = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!com.includes(TEMPNUM)) {
-        //중복 검사
-        com.push(TEMPNUM);
+      tempNum = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!com.includes(tempNum)) {
+        com.push(tempNum);
       }
     }
 
     return com;
   }
 
-  //게임 진행 출력
   printText(strike, ball) {
     if (strike === 3) {
       MissionUtils.Console.print("3스트라이크");
-      this.is_correct = true;
+      this.isCorrect = true;
     }
 
     if (strike === 2) {
@@ -84,18 +82,13 @@ class App {
       if (ball === 1) {
         MissionUtils.Console.print("1볼");
       }
-
       if (ball === 0) {
         MissionUtils.Console.print("낫싱");
       }
     }
   }
 
-  //숫자 검사
   compareNumber(com, user) {
-    //is_correct change
-    //com [ 1 , 2, 3 ] user [ 1 , 2 , 3]
-
     let strike = 0;
     let ball = 0;
 
@@ -111,27 +104,25 @@ class App {
     this.printText(strike, ball);
   }
 
-  //게임 진행
   async playGame() {
-    let com = this.getRandomNumber();
-    while (!this.is_end) {
-      let user = await this.getUserNumber();
+    let comNum = this.getRandomNumber();
+    while (!this.isEnd) {
+      let userNum = await this.getUserNumber();
+      this.compareNumber(comNum, userNum);
 
-      // console.log("com" + com);
-      // console.log("user" + user);
-      this.compareNumber(com, user);
-      if (this.is_correct) {
+      if (this.isCorrect) {
         MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         let res = await MissionUtils.Console.readLineAsync(
           "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
         );
-        if (res === "1") {
-          this.is_end = false;
-          this.is_correct = false;
 
-          com = this.getRandomNumber();
+        if (res === "1") {
+          this.isEnd = false;
+          this.isCorrect = false;
+
+          comNum = this.getRandomNumber();
         } else if (res === "2") {
-          this.is_end = true;
+          this.isEnd = true;
         } else {
           throw new Error("[ERROR] 1 또는 2만 입력해주세요.");
         }
@@ -146,6 +137,3 @@ class App {
 }
 
 export default App;
-
-// const app = new App();
-// app.play();
