bce93659502d4bf2dec6c0f72da4bc3d3f13f8b3
badahertz52 <badahertz52@gmail.com>
2023-10-23 23:07:05 +0900

    [test] Add item to answers in "예외 테스트"

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -27,7 +27,7 @@ describe("숫자 야구 게임", () => {
   test("게임 종료 후 재시작", async () => {
     // given
     const randoms = [1, 3, 5, 5, 8, 9];
-    const answers = ["246", "135", "1", "597", "589", "2"];
+    const answers = ["246", "135", "1", "597","589", "2"];
     const logSpy = getLogSpy();
     const messages = ["낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료"];
 
@@ -47,7 +47,7 @@ describe("숫자 야구 게임", () => {
   test("예외 테스트", async () => {
     // given
     const randoms = [1, 3, 5];
-    const answers = ["1234"];
+    const answers = ["1234" , "가나1", "12" , "012"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
