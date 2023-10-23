5dc66656c426135e2d8827d777291d46d2f24586
thumbthing <ilscc0902@gmail.com>
2023-10-24 03:41:15 +0900

    feat: 애플리케이션 entry point에 기능 적용, 지속여부 판단에 필요한 변수 선언

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import createAnswer from "./util/createAnswer.js";
+import handleInput from "./util/handleInput.js";
+import compareInputWithAnswer from "./util/compareInputWithAnswer.js";
+import returnResultMessage from "./util/returnResultMessage.js";
+import handleProgress from "./util/handleProgress.js";
+
 class App {
-  async play() {}
+  async play() {
+    let answerLog = [];
+    let progressStatus = "1";
+
+    while (progressStatus === "1") {
+      answerLog = createAnswer([...answerLog]);
+      let threeStrike = false;
+      let input;
+      let ballsAndStrike;
+      let message;
+
+      while (!threeStrike) {
+        input = await handleInput();
+        ballsAndStrike = await compareInputWithAnswer(answerLog, input);
+        threeStrike = ballsAndStrike.strike === 3 && ballsAndStrike.ball === 0;
+        message = await returnResultMessage(ballsAndStrike);
+        progressStatus = await handleProgress(message);
+      }
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
