684ccf46abbbd3d3c2fadc08e22afd9801a1177b
sang-il Yoon <yunsangil7749@gmail.com>
2023-10-23 12:32:11 +0900

    First Commit

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,18 +1,18 @@
 import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-const mockQuestions = (inputs) => {
+const mockQuestions = (inputs) => { //가짜 입력값 설정
   MissionUtils.Console.readLineAsync = jest.fn();
 
-  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+  MissionUtils.Console.readLineAsync.mockImplementation(() => { // mockImplementation를 이용하여 함수를 즉석으로 구현한다.
     const input = inputs.shift();
-    return Promise.resolve(input);
+    return Promise.resolve(input); // answer 배열의 앞에 있는 인자 도출
   });
 };
 
-const mockRandoms = (numbers) => {
+const mockRandoms = (numbers) => { // 가짜 랜덤값을 설정
   MissionUtils.Random.pickNumberInRange = jest.fn();
-  numbers.reduce((acc, number) => {
+  numbers.reduce((acc, number) => { // MissionUtils.Random.pickNumberInRange에 numbers 배열의 값들을 매기는 기능
     return acc.mockReturnValueOnce(number);
   }, MissionUtils.Random.pickNumberInRange);
 };
@@ -35,12 +35,14 @@ describe("숫자 야구 게임", () => {
     mockQuestions(answers);
 
     // when
+    // app.play() 함수가 Promise를 반환하고 해당 Promise가 성공적으로 해결되며 예외를 던지지 않는 경우에만 테스트가 통과한다는 것을 의미
     const app = new App();
     await expect(app.play()).resolves.not.toThrow();
 
     // then
-    messages.forEach((output) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    messages.forEach((output) => { 
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output)); //logspy 함수가 output 문자열을 포함하는 문자열을 인수로 호출하였는가
+      //expect.stringContaining(output) 해당 문자열을 포함하였는가 확인
     });
   });
 
@@ -54,7 +56,8 @@ describe("숫자 야구 게임", () => {
 
     // when & then
     const app = new App();
-
+    
+    // app.play() 함수가 Promise를 반환하고 해당 Promise가 거부되며 [ERROR] 예외 메시지가 발생해야 테스트가 통과한다는 것을 의미
     await expect(app.play()).rejects.toThrow("[ERROR]");
   });
 });
