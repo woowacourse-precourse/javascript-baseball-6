603adcc527553ef1f26de706c5cbe0d88dbdbf58
Sungwk <73461032+Sungwk@users.noreply.github.com>
2023-10-23 13:24:15 +0900

    :sparkles: Feat : 입력값이 모두 숫자인지 확인하는 기능 추가
    입력한 숫자가 숫자로만 이루어져 있는지 확인

--- src/Validate.js
+++ src/Validate.js
@@ -1,10 +1,18 @@
 const doValidate = (input) => {
   isInputLengthRight(input);
+  isInputNumber(input);
 };
 
 // 입력한 숫자의 자릿수가 세 자리 인지 확인하는 함수
 const isInputLengthRight = (input) => {
   if (input.length !== 3) throw new Error('[ERROR] 세 자리를 입력하세요.');
+};
+
+// 입력한 숫자가 모두 숫자인지 확인하는 함수
+const isInputNumber = (input) => {
+  if (Number.isNaN(input * 1)) {
+    throw new Error('[ERROR] 숫자를 입력하세요.');
+  }
 };
 
 export default doValidate;
