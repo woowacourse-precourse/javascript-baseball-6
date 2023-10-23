1c1bb670fe1be5177b447583eb5c8e22503d7828
Hyung-Jung Son <born2ocode@gmail.com>
2023-10-23 23:02:40 +0900

    작업 브랜치 생성 및 코드작성중

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.randomNumbers = [];
+  }
+
+  generateRandomNumbers() {
+    // 3개의 난수를 생성하는 랜덤 숫자 생성
+  }
+  async play() {
+    this.generateRandomNumbers();
+
+    while (true) {
+      const userInput = await MissionUtils.Console.readLineAsync("숫자 입력");
+
+      if (userInput.length !== 3) {
+        throw new Error("error");
+      }
+
+      const result = this.compareNumbers(userInput);
+
+      MissionUtils.Console.print(result);
+
+      if (result === "스트라이크") {
+        MissionUtils.Console.print("게임 종료");
+        break;
+      }
+    }
+  }
+
+  compareNumbers(userInput) {}
 }
 
 export default App;
