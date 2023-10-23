726ba2e7b4c7e538a8de4a3f428e4c8df6ce8ae7
Doeunnkimm <doeunnkimm@gmail.com>
2023-10-23 09:52:50 +0900

    feat: 볼과 스트라이크 개수를 count해주는 메서드

--- src/baseBallGame.js
+++ src/baseBallGame.js
@@ -11,6 +11,24 @@ const BaseBallGame = {
     }
     return random;
   },
+
+  getBallAndStrikeCount(targetArray, number) {
+    const count = { ball: 0, strike: 0 };
+
+    const numberArray = [...String(number)].map(Number);
+    numberArray.forEach((num, index) => {
+      if (targetArray.includes(num)) {
+        if (targetArray.indexOf(num) === index) {
+          // 포함하면서 위치도 일치 : 스트라이크
+          count.strike += 1;
+          return;
+        }
+        // 포함하지만 위치는 불필치 : 볼
+        count.ball += 1;
+      }
+    });
+    return count;
+  },
 };
 
 export default BaseBallGame;
