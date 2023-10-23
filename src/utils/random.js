d7a30a3a1701f5818c849a0b54cacb19a890bdb6
BHyeonKim <rlaqhguse@gmail.com>
2023-10-24 03:06:47 +0900

    fix: 첫번째 테스트가 통과하지 않는 문제 수정
    우테코에서 제공하는 유틸라이브러리에 pickUniqueNumbersInRange가 있어서 유일한 3개의 숫자를 생성할 때 사용했는데
    jest에서 테스트에 pickNumberInRange를 모킹하고 있어서 테스트가 통과가 안되었습니다.
    pickNumberInRange로 변경하여 문제를 해결하였습니다.

--- src/utils/random.js
+++ src/utils/random.js
@@ -2,9 +2,13 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 import { MIN_NUM, MAX_NUM, NUM_OF_BALLS } from '../constants/index.js';
 
 export function generateThreeNumber() {
-  return MissionUtils.Random.pickUniqueNumbersInRange(
-    MIN_NUM,
-    MAX_NUM,
-    NUM_OF_BALLS
-  );
+  const computer = [];
+  while (computer.length < NUM_OF_BALLS) {
+    const number = MissionUtils.Random.pickNumberInRange(MIN_NUM, MAX_NUM);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+
+  return computer;
 }
