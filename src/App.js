a100a40f3c388e6b5aa8d8c77ce45b233b90ebb3
Jiwon Oh <64193469+jiwon-o@users.noreply.github.com>
2023-10-23 23:14:28 +0900

    feat: 3자리의 서로 다른 랜덤값 추출하는 메서드 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.randomNumbers = [];
+  }
+
+  async play() {
+    console.log("숫자 야구 게임을 시작합니다.");
+    this.generateRandomNumbers();
+  }
+
+  generateRandomNumbers() {
+    while (this.randomNumbers.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.randomNumbers.includes(number)) {
+        this.randomNumbers.push(number);
+      }
+    }
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
