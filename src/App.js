30a3a23736bc0a84d5ed4a28a2fef82fe11ca108
Jiwon Lee <giwon.yi339@gmail.com>
2023-10-23 13:57:46 +0900

    refactor : 프로젝트 메뉴얼 참고하여 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,97 +1,34 @@
-class App {
-  constructor() {
-    this.strikeCount = 0;
-    this.ballCount = 0;
-  }
+import * as MissionUtils from "@woowacourse/mission-utils";
 
+export default class App {
   async play() {
-    console.log("게임 시작");
-    this.handleSubmitInput();
+    // console.log("게임 시작");
+    MissionUtils.Console.print("게임 시작");
+    // this.handleSubmitInput();
+    this.getUsername();
   }
 
-  handleSubmitInput() {
-    // const submitBtn = document.querySelector("#user--submit");
-    const userValue = document.querySelector("#user--input");
-    const selectUlElement = document.querySelector(".result--table");
-
-    const computer = [];
-    while (computer.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer.push(number);
-      }
-    }
-
-    // submitBtn.addEventListener("click", (e) => {
-    //   e.preventDefault();
-    // submitBtn.addEventListener("click", (e) => {
-    //   e.preventDefault();
-    if (computer.length === 3) {
-      // user input 배열로 변환
-      const userInputValue = userValue.value;
-      const playerScores = [...userInputValue].map((el) => Number(el));
-      // 조건문
-      if (playerScores.length !== 3) {
-        // message.innerHTML = MESSAGE.ERROR;
-        alert("1~9까지의 수로 이루어진 3자리의 중복없는 숫자를 입력해 주세요.");
-        userValue.innerHTML = "";
-        return;
-      }
-      console.log(playerScores);
-      console.log(computer);
-
-      //iteration
-      const strikesArray = [];
-      // let strikeCount = 0;
-      // let ballCount = 0;
-      const ballsArray = [];
-      for (let i = 0; i < 3; i++) {
-        // playerScore index가 computer index와 일치하는 경우
-        if (playerScores[i] === computer[i]) {
-          // strikeCount += 1;
-          this.strikeCount += 1;
-          strikesArray.push(this.strikeCount);
-          // console.log("strikeCount  = ", strikesArray.length);
-          // alert(strikeCount);
-        } else if (computer.includes(playerScores[i])) {
-          this.ballCount += 1;
-          ballsArray.push(this.ballCount);
-
-          // console.log("ballCount = ", ballsArray.length);
+  async getUsername() {
+    try {
+      const computer = [];
+      while (computer.length < 3) {
+        const number = MissionUtils.Random.pickNumberInRange(1, 9);
+        if (!computer.includes(number)) {
+          computer.push(number);
         }
       }
 
-      if (strikesArray.length >= 3) {
-        alert(`🎉 3스트라이크 
-        3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
-      } else if (strikesArray.length > 0 && ballsArray.length > 0) {
-        const strikeAndBallMessage = document.createElement("li");
-        strikeAndBallMessage.textContent = ` 볼${ballsArray.length} 스트라이크${strikesArray.length}`;
-        selectUlElement.appendChild(strikeAndBallMessage);
-        console.log("strikeCount  = ", strikesArray.length);
-      } else if (strikesArray.length > 0) {
-        const strikesLi = document.createElement("li");
-        strikesLi.textContent = ` 스트라이크 ${strikesArray.length}`;
-        selectUlElement.appendChild(strikesLi);
-      } else if (ballsArray.length > 0) {
-        const ballLi = document.createElement("li");
-        ballLi.textContent = `볼${ballsArray.length}`;
-        selectUlElement.appendChild(ballLi);
-      } else {
-        const nothingMsg = document.createElement("li");
-        nothingMsg.textContent = `낫싱`;
-        selectUlElement.appendChild(ballLi);
-      }
-
-      // const ballLi = document.createElement("li");
-      // ballLi.textContent = `볼${ballsArray.length}`;
-      // selectUlElement.appendChild(ballLi);
-
-      // console.log("ballCount = ", ballsArray.length);
+      const username = await MissionUtils.Console.readLineAsync(
+        "1~9까지의 수로 이루어진 3자리의 중복없는 숫자를 입력해 주세요."
+      );
+      MissionUtils.Console.print(computer);
+      MissionUtils.Console.print(username);
+    } catch (error) {
+      // reject 되는 경우
+      MissionUtils.Console.print(error);
     }
   }
 }
 
 const app = new App();
 app.play();
-export default App;
