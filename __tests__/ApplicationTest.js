4a6cfdf3d40c1819ea1e0597b5c4b74300c79d03
wonkyum-kim <dodo12120@naver.com>
2023-10-23 11:17:14 +0900

    test: refree 판정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,6 @@
 import App from '../src/App.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
+import { BaseballService } from '../src/Baseball.service.js';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -41,6 +42,27 @@ describe('숫자 야구 게임', () => {
     expect(logSpy).toHaveBeenCalledWith(
       expect.stringContaining('숫자 야구 게임을 시작합니다.')
     );
+  });
+
+  test('refree1', () => {
+    const baseballService = new BaseballService();
+    const { ball, strike } = baseballService.refree(['1', '2', '3'], '123');
+    expect(strike).toBe(3);
+    expect(ball).toBe(0);
+  });
+
+  test('refree2', () => {
+    const baseballService = new BaseballService();
+    const { ball, strike } = baseballService.refree(['1', '4', '2'], '124');
+    expect(strike).toBe(1);
+    expect(ball).toBe(2);
+  });
+
+  test('refree3', () => {
+    const baseballService = new BaseballService();
+    const { ball, strike } = baseballService.refree(['1', '4', '2'], '987');
+    expect(strike).toBe(0);
+    expect(ball).toBe(0);
   });
 
   test('게임 종료 후 재시작', async () => {
