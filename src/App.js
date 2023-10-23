0ad9ee959ea36cbc79e5292d0172926397075051
lucaseunchae <lucaskim.dev@gmail.com>
2023-10-23 22:59:51 +0900

    refactor: seperate baseball game logic into function

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { calculateCounts, isThreeStrike, printScore } from "./baseball-game.js";
-import { getThreeDistinctNumbers, stringToNumberArray } from "./utils/array.js";
-import { validateNumberInput } from "./utils/input.js";
+import { playAGame } from "./baseball-game.js";
+import { getThreeDistinctNumbers } from "./utils/array.js";
 
 class App {
-  RESTART_GAME = "1";
-  END_GAME = "2";
-
   async play() {
     while (true) {
       const computerNumbers = getThreeDistinctNumbers();
 
       MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 
-      while (true) {
-        const numberInput = await MissionUtils.Console.readLineAsync(
-          "숫자를 입력해주세요 : "
-        );
-        validateNumberInput(numberInput);
-
-        const { strikeCount, ballCount } = calculateCounts(
-          computerNumbers,
-          stringToNumberArray(numberInput)
-        );
-
-        printScore(strikeCount, ballCount);
-
-        if (!isThreeStrike(strikeCount)) continue;
-
-        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-
-        const continueInput = await MissionUtils.Console.readLineAsync(
-          "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
-        );
-
-        if (continueInput === this.RESTART_GAME) break;
-        if (continueInput === this.END_GAME) return;
-      }
+      const { isContinue } = await playAGame(computerNumbers);
+      
+      if (!isContinue) return;
     }
   }
 }
--- src/baseball-game.js
+++ src/baseball-game.js
@@ -1,4 +1,6 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { stringToNumberArray } from "./utils/array.js";
+import { validateNumberInput } from "./utils/input.js";
 
 const calculateStrikeCount = (computerNums, answerNums) => {
   return answerNums.filter((answerNum, i) => answerNum === computerNums[i])
@@ -15,14 +17,14 @@ const calculateBallCount = (computerNums, answerNums) => {
     : 0;
 };
 
-export const calculateCounts = (computerNums, answerNums) => {
+const calculateCounts = (computerNums, answerNums) => {
   const strikeCount = calculateStrikeCount(computerNums, answerNums);
   const ballCount = calculateBallCount(computerNums, answerNums);
 
   return { strikeCount, ballCount };
 };
 
-export const printScore = (strikeCount, ballCount) => {
+const printScore = (strikeCount, ballCount) => {
   let result = "";
 
   if (ballCount) result += `${ballCount}볼 `;
@@ -33,4 +35,38 @@ export const printScore = (strikeCount, ballCount) => {
   MissionUtils.Console.print(result ? result : "낫싱");
 };
 
-export const isThreeStrike = (strikeCount) => strikeCount === 3;
+const isThreeStrike = (strikeCount) => strikeCount === 3;
+
+const checkIsContinue = (continueInput) => {
+  const CONTINUE_GAME = "1";
+  const END_GAME = "2";
+
+  if (continueInput === CONTINUE_GAME) return true;
+  if (continueInput === END_GAME) return false;
+};
+
+export const playAGame = async (computerNumbers) => {
+  while (true) {
+    const numberInput = await MissionUtils.Console.readLineAsync(
+      "숫자를 입력해주세요 : "
+    );
+    validateNumberInput(numberInput);
+
+    const { strikeCount, ballCount } = calculateCounts(
+      computerNumbers,
+      stringToNumberArray(numberInput)
+    );
+
+    printScore(strikeCount, ballCount);
+
+    if (!isThreeStrike(strikeCount)) continue;
+
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+    const continueInput = await MissionUtils.Console.readLineAsync(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+    );
+
+    return { isContinue: checkIsContinue(continueInput) };
+  }
+};
