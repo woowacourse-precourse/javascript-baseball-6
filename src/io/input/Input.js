6aad88b5464d212a884cbb59d631b8c95475923b
pakxe <pigkill40@naver.com>
2023-10-23 21:03:35 +0900

    feat(Input): 입력을 받기 위한 클래스 작성
    맞추기 시도하는 숫자의 입력과 재시작 여부의 입력을 작성하였다.

--- src/io/input/Input.js
+++ src/io/input/Input.js
@@ -2,8 +2,12 @@ import { Console } from '@woowacourse/mission-utils';
 import GAME_MESSAGE from '../../constants/GameMessage.js';
 
 class Input {
-	static async getTryNumber() {
-		return await Console.readLineAsync(GAME_MESSAGE.getNumber); // 입력으로 받아온 값을 반환한다.
+	static getTryNumber() {
+		return Console.readLineAsync(GAME_MESSAGE.getNumber); // 입력으로 받아온 값을 반환한다.
+	}
+
+	static getRestart() {
+		return Console.readLineAsync(GAME_MESSAGE.restart);
 	}
 }
 
