79ae00b0487fd5c737fc0342d05638fa45866546
inhwa <dlsghk0711@naver.com>
2023-10-23 14:20:23 +0900

    1차 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,114 @@
+import { Console } from "@woowacourse/mission-utils";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  generateComputerNumber() {
+    const computerNumber = [];
+    while (computerNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerNumber.includes(number)) {
+        computerNumber.push(number);
+      }
+    }
+    return computerNumber;
+  }
+
+  CalcStrike(ComputerRandomNum, UserNum){
+    let Strike=0
+    for(let i = 0; i<3; i++){
+      if (ComputerRandomNum[i]==UserNum[i]){
+        Strike+=1;
+      }
+    }
+    return Strike;
+  }
+
+  CalcBall(ComputerRandomNum, UserNum){
+    let Ball=0
+    for(let i = 0; i<3; i++){
+      for (let j = 0; j < 3; j++) {
+        if ((i != j) && (ComputerRandomNum[i] == UserNum[j])) {
+          Ball += 1;
+        }
+      }
+    }
+    return Ball;
+  }
+
+  Check(UserNum){
+    if (UserNum.length!=3){
+      throw new Error();
+    }
+    else{
+      return true;
+    }
+  }
+
+  PrintCount(StrikeConut,BallCount){
+    if ((StrikeConut==0)&&(BallCount==0)){
+      Console.print('낫싱')
+      return false;
+    }
+    if ((StrikeConut!=0)&&(BallCount==0)){
+      Console.print(StrikeConut+'스트라이크')
+      if(StrikeConut==3){
+        Console.print("3개의 숫자를 모두 맞히셨습니다!");
+        return true;
+      }
+      return false;
+    }
+    if ((StrikeConut==0)&&(BallCount!=0)){
+      Console.print(BallCount+'볼')
+      return false
+    }
+    if ((StrikeConut!=0)&&(BallCount!=0)){
+      Console.print(BallCount+"볼 "+StrikeConut+'스트라이크')
+      return false
+    }
+  }
+  async Restart(){
+    try {
+      const KeeporStop = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요');
+      if (KeeporStop=='1'){
+        return this.play();
+      }
+      else if (KeeporStop=='2'){
+        Console.print('게임 종료');
+      }
+    } catch (error) {
+      throw new Error('[ERROR] 유효하지 않은 숫자입니다.');
+    }
+  }
+
+  async Gamestart(ComputerRandomNum){
+    while(true){
+      try {
+        const UserNum = await Console.readLineAsync('숫자를 입력해주세요 : ');
+        Console.print(UserNum);
+        this.Check(UserNum);
+      
+        const StrikeConut = this.CalcStrike(ComputerRandomNum, UserNum);
+        const BallCount = this.CalcBall(ComputerRandomNum, UserNum);
+        const result = this.PrintCount(StrikeConut,BallCount);
+        if (result==true){
+          return this.Restart();
+        }
+
+      } catch (error) {
+        throw new Error('[ERROR] 유효하지 않은 숫자입니다.');
+      }
+    }
+  }
+
+  async play(){
+    const ComputerRandomNum=this.generateComputerNumber();
+    Console.print('컴퓨터 랜덤 숫자 : '+ComputerRandomNum);
+    Console.print('숫자 야구 게임을 시작합니다.');
+    return this.Gamestart(ComputerRandomNum);
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
