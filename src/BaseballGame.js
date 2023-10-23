359650313cf48485bd5b6f07c9823eedb420ca28
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-10-23 21:40:36 +0900

    refactor: play, startGame 중복되는 역할 삭제

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -6,21 +6,17 @@ class BaseballGame {
     this.computer = new Computer();
   }
 
-  async play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    await this.startGame();
-  }
-
   async startGame() {
     await this.getUserInput();
   }
 
   async getUserInput() {
     const input = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
-      await this.duringGameInput(input);
+    await this.duringGameInput(input);
   }
 
   async getRestartInput() {
+    MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
     const input = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
     return input;
   }
@@ -40,7 +36,6 @@ class BaseballGame {
   }
 
   async restart() {
-    MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
     const input = await this.getRestartInput();
     if (input !== '1' && input !== '2') {
       throw new Error('[ERROR] 잘못된 형식입니다.');
