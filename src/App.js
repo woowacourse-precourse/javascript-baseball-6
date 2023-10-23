9c9af50982c40a421f6aae269e4178a4e1d785ff
Mijin Sim <202110106@bssm.hs.kr>
2023-10-23 09:30:25 +0900

    fix(Computer) : 함수 아닌 객체 불러오기

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ const {
   END_OPTION,
   ERROR_MESSAGE,
 } = require("./constants");
-const { generateComputerNumber } = require("./Computer");
+const { Computer } = require("./Computer");
 
 class App {
   async play() {
     // 게임 시작 메세지 출력
     Console.print(GAME_MESSAGE.START);
     // TODO : 1~9까지 랜덤 숫자를 생성하는 기능
-    this.computerNumber = generateComputerNumber();
+    const computer = new Computer()
+    this.computerNumber = computer.generateComputerNumber();
     // 게임 시작 메서드(BaseballGame) 호출
     return this.BaseballGame(this.computerNumber);
   }
