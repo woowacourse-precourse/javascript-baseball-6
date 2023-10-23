beeb6f80de2f739f741101450bbf332105748ec7
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-10-23 23:11:03 +0900

    feat: 컴퓨터의 숫자와 일치할 때까지 게임 반복하는 기능 추가

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -1,14 +1,71 @@
 const { Console } = require('@woowacourse/mission-utils');
-const { checkValidNumberDuringGame } = require('./Validation');
+const { checkValidNumber } = require('./Validation');
+const Computer = require('./Computer');
+const { getHintToUser } = require('./Hint');
 
-class UserInputHandler {
-  async handleUserInputDuringGame() {
-    Console.print('숫자 야구 게임을 시작합니다.');
-    const userNumber = await Console.readLineAsync('숫자를 입력하세요: ');
+class BaseballGame {
+  constructor() {
+    this.computer = new Computer();
+  }
 
-    const userInput = userNumber.join('');
-    checkValidNumberDuringGame(userInput);
+  startGame() {
+    this.getUserInput();
+  }
+
+  getUserInput() {
+    Console.readLineAsync('숫자를 입력해주세요 : ', (input) => {
+      this.handleUserInputDuringGame(input);
+    });
+  }
+
+  recommendRestart() {
+    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+
+    Console.readLineAsync(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+      (input) => {
+        this.handleUserInputEndGame(input);
+      },
+    );
+  }
+
+  handleUserInputDuringGame(input) {
+    if (!checkValidNumber(input)) {
+      throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+    }
+
+    const hintMessage = getHintToUser(this.computer.computerNumber, input);
+    Console.print(hintMessage);
+
+    if (hintMessage === '3스트라이크') {
+      this.recommendRestart();
+      return;
+    }
+    this.getUserInput();
+  }
+
+  handleUserInputEndGame(input) {
+    const correctInputDigits = ['1', '2'];
+
+    if (!correctInputDigits.includes(input)) {
+      throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+    }
+    // TODO: 주석 삭제 필요
+    // eslint-disable-next-line default-case
+    switch (input) {
+      case '1':
+        this.restartGame();
+        break;
+      case '2':
+        Console.close();
+        break;
+    }
+  }
+
+  restartGame() {
+    this.computer.generateNewRandomNumber();
+    this.startGame();
   }
 }
 
-module.exports = UserInputHandler;
+module.exports = BaseballGame;
