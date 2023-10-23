f75c717d0f38c1aba16ddda448645ce2a68d5b33
greeneeya <suzythetravelerr@gmail.com>
2023-10-23 13:24:16 +0900

    [feature] 랜덤 세자리 수를 결정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Random, Console } from '@woowacourse/mission-utils';
 class App {
   async play() {}
+
+  getRandomNumber() {
+    let num = '';
+    for (let i = 0; i < 3; i++) {
+      num += Random.pickNumberInRange(1, 9);
+    }
+    return num;
+  }
 }
+
+const app = new App();
+app.getRandomNumber();
 
 export default App;
