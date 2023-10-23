010bd0f03d20a9778c924f7711530e6e84424c39
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-10-23 23:09:38 +0900

    feat: 게임 시작 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const { Console } = require('@woowacourse/mission-utils');
+const BaseballGame = require('./BaseballGame');
+
 class App {
-  async play() {}
+  constructor() {
+    this.game = new BaseballGame();
+  }
+
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    this.game.startGame();
+  }
 }
 
 export default App;
