67870a4d4f226836a2860967f382e55fa408061e
SeoDongSu <poopoo0515@naver.com>
2023-10-23 19:39:51 +0900

    style: 에러 메세지 출력

--- src/errorThrow.js
+++ src/errorThrow.js
@@ -5,12 +5,13 @@ export const errorThrow = (userInput) => {
 
   if (userInput === null) {
     error = true;
+    Console.print("[ERROR] 숫자가 잘못된 형식입니다.");
     throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
   }
   if (userInput.length !== 3 || /^[1-9]+$/.test(userInput) === false) {
     error = true;
     Console.print("[ERROR] 숫자가 잘못된 형식입니다.");
-    throw new Error("[ERROR]");
+    throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
   }
   return error;
 };
