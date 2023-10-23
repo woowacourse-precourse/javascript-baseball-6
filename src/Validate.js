3316ded8ffe48ac6f23be81e56de0efca206a6de
Sungwk <73461032+Sungwk@users.noreply.github.com>
2023-10-23 13:34:25 +0900

    :sparkles: Feat : 입력값에 0이 포함되는지 검사 기능 추가
    입력한 숫자에 0이 포함되는지 확인하는 함수 구현

--- src/Validate.js
+++ src/Validate.js
@@ -1,6 +1,7 @@
 const doValidate = (input) => {
   isInputLengthRight(input);
   isInputNumber(input);
+  isInputIncludeZero(input);
 };
 
 // 입력한 숫자의 자릿수가 세 자리 인지 확인하는 함수
@@ -15,4 +16,14 @@ const isInputNumber = (input) => {
   }
 };
 
+// 입력한 숫자에 0이 포함되는지 확인하는 함수
+const isInputIncludeZero = (input) => {
+  for (let i = 0; i < input.length; i += 1) {
+    checkIsZero(input[i] * 1);
+  }
+};
+
+const checkIsZero = (number) => {
+  if (number === 0) throw new Error('[ERROR] 0을 포함할 수 없습니다.');
+};
 export default doValidate;
