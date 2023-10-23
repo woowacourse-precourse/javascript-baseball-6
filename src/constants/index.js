450a3ac3e8449651eff6c9889896d4706112283c
Mijin Sim <202110106@bssm.hs.kr>
2023-10-23 10:26:33 +0900

    chore(constants) : 형식에 맞게 수정

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,8 +2,8 @@ const GAME_MESSAGE = {
   START: "숫자 야구 게임을 시작합니다.",
   INPUT: "숫자를 입력해주세요 : ",
   SUCCESS: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-  RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
-  END: "게임을 종료합니다.",
+  RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+  END: "게임 종료",
 };
 
 const BASEBALL_MESSAGE = {
