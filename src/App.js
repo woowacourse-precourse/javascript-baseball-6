a56606d0b51fd5318ac33fa65552228560a39559
unknown <pih9134@naver.com>
2023-10-24 03:25:06 +0900

    feat: 컴퓨터에게 랜덤값 주는 메서드

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다');
     try {
-      const userInput = await this.getUserInput();
+      let userInput = await this.getUserInput();
+      let computer = this.computerPicksNumber();
     } catch (error) {
       throw new Error('[ERROR]');
     }
@@ -13,7 +14,20 @@ class App {
   async getUserInput() {
     return Console.readLineAsync('숫자를 입력해주세요 : ');
   }
+  
+  computerPicksNumber() {
+    const computerPickArr = [];
 
+    while (computerPickArr.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computerPickArr.includes(number)) {
+        computerPickArr.push(number);
+      }
+    }
+
+    const computerPick = computerPickArr.join('');
+    return computerPick;
+  }
 }
 
 const app = new App();
