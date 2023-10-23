4a644bc061d513bdb71a22c97c4d986b074c7102
hojkim77 <rlaghwnd4534@gmail.com>
2023-10-23 22:02:33 +0900

    [feat] 컴퓨터Class - 입력값에 대한 결과 메서드 완성

--- src/Computer.js
+++ src/Computer.js
@@ -1,5 +1,8 @@
 import { Random } from "@woowacourse/mission-utils";
 class Computer {
+  constructor() {
+    this.answer = this.randomNumGenerate();
+  }
   randomNumGenerate() {
     const numbers = [];
     while (numbers.length < 3) {
@@ -10,4 +13,27 @@ class Computer {
     }
     return numbers;
   }
+
+  countStrike(input) {
+    let strike = 0;
+    input.forEach((num, idx) => {
+      if (num == this.answer[idx]) strike++;
+    });
+    return strike;
+  }
+  countBall(input) {
+    let ball = 0;
+    input.forEach((num, idx) => {
+      if (this.answer.includes(num) && num != this.answer[idx]) ball++;
+    });
+    return ball;
+  }
+  resultGenerator(input) {
+    const count = { strike: 0, ball: 0 };
+    count.strike = this.countStrike(input);
+    count.ball = this.countBall(input);
+    return count;
+  }
 }
+
+export default Computer;
