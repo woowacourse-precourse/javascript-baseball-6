b54dcc6f91598eaa4817a7fffa6ab55562d49eb6
csk6314 <37896060+csk6314@users.noreply.github.com>
2023-10-23 13:47:54 +0900

    feat: 2. 숫자 입력

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
-const getComputerNumber = () => {
+const setComputerNumber = () => {
   const computer = [];
   while (computer.length < 3) {
     const number = Random.pickNumberInRange(1, 9);
@@ -12,8 +12,44 @@ const getComputerNumber = () => {
   return computer;
 };
 
-class App {
-  async play() {}
-}
+const getUserNumber = async () => {
+  let userNumber = await Console.readLineAsync("숫자를 입력해주세요 : ");
+  if (userNumber.length !== 3) {
+    throw new Error("Error: 잘못된 문자열 길이");
+  }
+  if (!isDigit(userNumber)) {
+    throw new Error("Error: 숫자가 아닌 잘못된 문자 입력");
+  }
+  if (isDuplicated(userNumber)) {
+    throw new Error("Error: 중복된 숫자 입력");
+  }
+  return userNumber;
+};
+const isDigit = (input) => {
+  for (let i = 0; i < input.length; i++) {
+    const ch = input.charAt(i).charCodeAt();
+    if (ch < 48 || ch > 57) {
+      return false;
+    }
+  }
+  return true;
+};
+const isDuplicated = (input) => {
+  for (let i = 0; i < input.length; i++) {
+    const ch = input.charAt(i);
+    if (input.indexOf(ch) !== input.lastIndexOf(ch)) {
+      console.log(ch);
+      return true;
+    }
+  }
+  return false;
+};
 
+class App {
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
+}
+getUserNumber();
+//isDigit("gggg");
 export default App;
