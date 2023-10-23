1279e28cc4a37313995b0b9c88812e0b17d77cd0
gyucheolshin <nsfsgc2295@gmail.com>
2023-10-23 19:36:46 +0900

    feat : USER 숫자 입력 및 잘못 입력했을 시 예외발생

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-
 class App {
-  async play() {}
+  async play() {
+    this.gameStart(this.setComputerNumber());
+  }
+
+  // 게임 시작.
+  gameStart() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ").then(
+      (answer) => {
+        this.answerCheck(answer);
+      }
+    );
+  }
 
   setComputerNumber() {
-    const COMPUTER = [];
-    while (COMPUTER.length < 3) {
+    const computer = [];
+    while (computer.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!COMPUTER.includes(number)) {
-        COMPUTER.push(number);
+      if (!computer.includes(number)) {
+        computer.push(number);
       }
     }
-    return COMPUTER;
+    return computer;
+  }
+
+  duplicateNumber(answer) {
+    const userAnswer = [...new Set(answer)];
+    if (userAnswer.length !== 3) return false;
+    return true;
+  }
+
+  answerCheck(answer) {
+    if (isNaN(Number(answer))) throw new Error("숫자만 입력해주세요.");
+    if (answer.length !== 3) throw new Error("3자리 숫자를 입력해주세요.");
+    if (!this.duplicateNumber(answer))
+      throw new Error("중복되지 않은 숫자를 입력해주세요.");
   }
 }
 
