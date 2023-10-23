ab6806707b8114a3897072608c73ec73c0a25996
dabinchiii <dabinchi388@naver.com>
2023-10-24 05:00:35 +0900

    feat: 메시지 상수를 별도의 파일로 분리

--- src/Game.js
+++ src/Game.js
@@ -1,16 +1,5 @@
 import { Console, Random } from "@woowacourse/mission-utils";
-
-const MESSAGE = {
-  GAME_START: "숫자 야구 게임을 시작합니다.",
-  SUCCESS: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-  PROMPT: {
-    USER_NUMS: "숫자를 입력해주세요 : ",
-    RESTART_GAME: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
-  },
-  ERROR: {
-    INVALID_INPUT: "올바른 입력이 아닙니다.",
-  },
-};
+import MESSAGE from "./message.js";
 
 class Game {
   computerNums = [];
---
+++ src/message.js
@@ -0,0 +1,13 @@
+const MESSAGE = {
+  GAME_START: "숫자 야구 게임을 시작합니다.",
+  SUCCESS: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  PROMPT: {
+    USER_NUMS: "숫자를 입력해주세요 : ",
+    RESTART_GAME: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+  },
+  ERROR: {
+    INVALID_INPUT: "올바른 입력이 아닙니다.",
+  },
+};
+
+export default MESSAGE;
