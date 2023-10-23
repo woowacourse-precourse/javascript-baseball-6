2a51c5fbb8be25a775644128d1b91e84d95f677a
solssak <gswoong1012@gmail.com>
2023-10-23 23:28:49 +0900

    feat: 상수 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-
+import { SETTING, SCORE, MESSAGE } from "./constants/gameConfig.js";
 class App {
   showStartMessage() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
@@ -7,8 +7,8 @@ class App {
 
   setRandomNumber() {
     const randomNumber = [];
-    while (randomNumber.length < 3) {
-      let number = MissionUtils.Random.pickNumberInRange(1, 9);
+    while (randomNumber.length < SETTING.INPUT_NUMBER_LENGTH) {
+      let number = MissionUtils.Random.pickNumberInRange(SETTING.MIN_NUMBER, SETTING.MAX_NUMBER);
       if (!randomNumber.includes(number)) {
         randomNumber.push(number);
       }
@@ -19,22 +19,22 @@ class App {
   async setUserInput(randomNumber) {
     console.log(randomNumber);
     try {
-      const inputNumber = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+      const inputNumber = await MissionUtils.Console.readLineAsync(MESSAGE.GAME.INPUT_NUMBER);
       const userInputNumber = this.userInputNumberValidation(inputNumber).split("").map(Number);
       const baseBallCount = this.calcBallStrike(userInputNumber, randomNumber);
       if (baseBallCount.strike === 3) {
-        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        MissionUtils.Console.print(MESSAGE.GAME.FINISH);
         this.gameRestart();
       } else {
         this.setUserInput(randomNumber);
       }
     } catch (error) {
-      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      throw new Error(MESSAGE.ERROR.WRONG_VALUE);
     }
   }
 
   lengthValidation(inputNumber) {
-    return inputNumber.length !== 3;
+    return inputNumber.length !== SETTING.INPUT_NUMBER_LENGTH;
   }
 
   rangeValidation(inputNumber) {
@@ -54,7 +54,7 @@ class App {
       this.rangeValidation(inputNumber) ||
       this.duplicateValidation(inputNumber)
     ) {
-      throw new Error("[ERROR]");
+      throw new Error(MESSAGE.ERROR.WRONG_VALUE);
     }
     return inputNumber;
   }
@@ -82,13 +82,15 @@ class App {
     });
 
     if (baseBallCount.ball > 0 && baseBallCount.strike > 0) {
-      MissionUtils.Console.print(`${baseBallCount.ball}볼 ${baseBallCount.strike}스트라이크`);
+      MissionUtils.Console.print(
+        `${baseBallCount.ball}${SCORE.BALL} ${baseBallCount.strike}${SCORE.STRIKE}`,
+      );
     } else if (baseBallCount.ball > 0) {
-      MissionUtils.Console.print(`${baseBallCount.ball}볼`);
+      MissionUtils.Console.print(`${baseBallCount.ball}${SCORE.BALL}`);
     } else if (baseBallCount.strike > 0) {
-      MissionUtils.Console.print(`${baseBallCount.strike}스트라이크`);
+      MissionUtils.Console.print(`${baseBallCount.strike}${SCORE.STRIKE}`);
     } else if (baseBallCount.strike === 0 && baseBallCount.ball === 0) {
-      MissionUtils.Console.print(`낫싱`);
+      MissionUtils.Console.print(SCORE.NOTHING);
     }
 
     return baseBallCount;
@@ -96,19 +98,17 @@ class App {
 
   async gameRestart() {
     try {
-      const userInput = await MissionUtils.Console.readLineAsync(
-        `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. \n`,
-      );
+      const userInput = await MissionUtils.Console.readLineAsync(`${MESSAGE.GAME.RESTART}\n`);
 
       if (userInput === "1") {
         this.setUserInput(this.setRandomNumber());
       } else if (userInput === "2") {
-        MissionUtils.Console.print("게임 종료");
+        MissionUtils.Console.print(MESSAGE.GAME.END);
       } else {
         throw error();
       }
     } catch (error) {
-      throw error("[ERROR] 숫자가 잘못된 형식입니다.");
+      throw error(MESSAGE.ERROR.WRONG_VALUE);
     }
   }
 
