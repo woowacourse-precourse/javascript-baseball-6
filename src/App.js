79c555c68a65b15e1e9761e722b8ca0efa37ea1f
이예지 <oyatpeach@naver.com>
2023-10-24 08:56:47 +0900

    chore : 테스트 케이스에 맞춰 코드 수정

--- src/App.js
+++ src/App.js
@@ -3,41 +3,56 @@ import { Console, Random } from "@woowacourse/mission-utils";
 class App {
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    await this.gameStart();
+  }
+
+  async gameStart() {
     const randomNum = this.createRandom();
-    Console.print(randomNum);
     await this.inputNum(randomNum);
   }
 
-  async confirmInput(userInput, randomNum) {
+  createRandom() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
+
+  async inputNum(randomNum) {
+    const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
+    this.confirmInput(Number(input), randomNum);
+  }
+
+  confirmInput(userInput, randomNum) {
+    Console.print([userInput]);
     if (isNaN(userInput)) {
-      throw new Error("[ERROR] 숫자를 입력하세요");
+      throw new Error("[ERROR] 숫자가 아닙니다.");
     } else if (userInput.toString().length !== 3) {
-      throw new Error(" [ERROR] 3자리 숫자를 입력하세요");
+      throw new Error(" [ERROR] 3자리 숫자가 아닙니다.");
     } else if (userInput.toString().includes(0)) {
-      throw new Error("[ERROR] 0이 아닌 숫자를 입력하세요");
+      throw new Error("[ERROR] 0이 포함되어 있습니다.");
     } else {
       const changeInput = (arg) => Number(arg);
-      const compareInput = Array.from(String(userInput), changeInput); //Number type input을 string으로 바꿔서 배열로 전환하고 Number로 재변경
+      const compareInput = Array.from(String(userInput), changeInput);
       this.compareNum(compareInput, randomNum);
     }
   }
 
   compareNum(compareInput, randomNum) {
-    //userInput은 Number
     let ball = 0;
     let strike = 0;
     let result = "";
 
     for (let r = 0; r < randomNum.length; r++) {
-      // 값 비교
       for (let i = 0; i < compareInput.length; i++) {
         if (randomNum[r] == compareInput[i]) {
-          // 값이 같다면
           if (r == i) {
-            // 같은 위치에 있다면
             strike++;
           } else {
-            // 다른 위치에 있다면
             ball++;
           }
         }
@@ -58,7 +73,7 @@ class App {
       }
     }
     Console.print(result);
-    this.inputNum(randomNum); // 모두 맞을 때까지 다시 인풋 받기
+    this.inputNum(randomNum);
   }
 
   gameEnd() {
@@ -68,50 +83,20 @@ class App {
   }
 
   async gameRestart() {
-    //다시 시작 or 종료
     Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     const isRestart = await Console.readLineAsync("");
 
-    //try {
     if (Number(isRestart) == 1) {
-      const randomNum = this.createRandom();
-      Console.print(randomNum);
-      this.inputNum(randomNum);
+      this.gameStart();
     } else if (Number(isRestart) == 2) {
       return 0;
     } else {
       this.gameRestart();
     }
-    //} catch (err) {
-    //Console.print(err);
-    //}
-  }
-
-  async inputNum(randomNum) {
-    //사용자로부터 입력 받기
-
-    //try {
-    const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
-    await this.confirmInput(Number(input), randomNum);
-    //} catch (err) {
-    //await Console.print(err);
-    //
-  }
-
-  createRandom() {
-    // 컴퓨터 랜덤 3자리 수 생성
-    const computer = [];
-    while (computer.length < 3) {
-      const number = Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer.push(number);
-      }
-    }
-    return computer;
   }
 }
 
-const start = new App();
-start.play();
+//const start = new App();
+//start.play();
 
 export default App;
