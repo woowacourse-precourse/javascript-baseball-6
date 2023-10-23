e6283e2b9b4803a913e91096e651b3ac3655c72c
innerstella <suji0509@naver.com>
2023-10-24 01:19:09 +0900

    ✨ 1. 게임 시작 - 1) 초기화

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 class App {
+  constructor() {
+    this.userNum = "";
+    this.answer = [];
+    this.continueGame = true;
+  }
+
+  init() {
+    this.userNum = "";
+    this.answer = [];
+    this.continueGame = true;
+  }
+
   async play() {}
 }
+
+const game = new App();
+game.play();
 
 export default App;
