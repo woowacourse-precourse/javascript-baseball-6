932cdf0a3c193cbb15968fb87521dbba07aa65ae
YOON-CC <whdbscks77@gmail.com>
2023-10-24 02:16:20 +0900

    feat(Controller.js): compareNumbers 메서드 호출

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -12,8 +12,9 @@ class Controller {
         if (!this.model.isValidUserNumber(userInput)) {
             //에러처리
         }
-        
 
+        const result = this.model.compareNumbers(userInput);
+        //View.js에서 출력 메서드 구현
 
     }
 }
