1f8f1cb9811c7b6f141c0883b0519067c1470d59
hyeonjun-L <beeminor60@gmail.com>
2023-10-23 12:52:30 +0900

    refactor- User 컴포넌트 리팩토링

--- src/User.js
+++ src/User.js
@@ -1,25 +1,58 @@
 export default class User {
   constructor({ initialState }) {
+    this.validationState(initialState);
     this.state = initialState;
   }
 
-  setState(nextState) {
-    this.state = [...this.validationState(nextState)];
+  validationState(state) {
+    if (typeof state !== "object") {
+      throw new Error("State must be an object");
+    }
+
+    if (state.hasOwnProperty("replay")) {
+      if (typeof state.replay !== "boolean") {
+        throw new Error("'replay' must be a boolean");
+      }
+    }
+
+    if (state.hasOwnProperty("userInput")) {
+      if (this.state === undefined && state.userInput === "") {
+        return;
+      }
+
+      const inputString = String(state.userInput);
+
+      if (inputString.length !== 3) {
+        throw new Error("User input must be exactly three digits.");
+      }
+
+      const uniqueDigits = [...new Set(inputString.split(""))];
+
+      if (uniqueDigits.length !== inputString.length) {
+        throw new Error("All digits in the user input must be unique.");
+      }
+
+      for (const digit of state.userInput) {
+        if (typeof digit !== "number") {
+          throw new Error("User input should contain only numbers.");
+        }
+      }
+    }
   }
 
-  validationState(state) {
-    const inputString = String(state);
+  setState(nextState) {
+    this.validationState(nextState);
 
-    if (inputString.length !== 3) {
-      throw new Error("Input must be exactly three digits.");
+    if (nextState.hasOwnProperty("userInput")) {
+      nextState.userInput = this.stringToNumberArray(
+        String(nextState.userInput)
+      );
     }
 
-    const uniqueDigits = [...new Set(inputString.split(""))];
+    this.state = { ...this.state, ...nextState };
+  }
 
-    if (uniqueDigits.length !== inputString.length) {
-      throw new Error("All digits in the input must be unique.");
-    }
-
-    return uniqueDigits;
+  stringToNumberArray(nextState) {
+    return nextState.split("").map(Number);
   }
 }
