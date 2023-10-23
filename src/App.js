262a998b866708a791c79759f4c61f479960c53e
jeongminsang <zjzj@hanmail.net>
2023-10-23 23:46:29 +0900

    feat: 관심사 분리, 에러 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,77 +1,84 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import readline from 'readline';
 
-const computer = [];
-  while (computer.length < 3) {
-    const number = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!computer.includes(number)) {
-      computer.push(number);
+class App {
+  constructor() {
+    this.computer = [];
+  }
+
+  async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    await this.playNumberGame();
+  }
+
+  async playNumberGame() {
+    this.generateRandomNumber();
+    let printResult;
+    while (printResult !== "3스트라이크") {
+      const userInput = await MissionUtils.Console.readLineAsync(
+        "숫자를 입력해 주세요 : "
+      );
+      if (userInput.length !== 3) {
+        throw new Error("[ERROR] 3자리 숫자만 입력하세요");
+      }
+
+      if (Number.isNaN(Number(userInput))) {
+        throw new Error("[ERROR] 숫자만 입력하세요");
+      }
+
+      if (new Set(userInput).size !== 3) {
+        throw new Error("[ERROR] 중복된 숫자가 입력되었습니다.");
+      }
+      printResult = this.numberBaseballResult(userInput);
+      MissionUtils.Console.print(printResult);
+    }
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    await this.restartInput();
+  }
+
+  generateRandomNumber() {
+    this.computer = [];
+    while (this.computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.computer.includes(number)) {
+        this.computer.push(number);
+      }
+      console.log(this.computer);
     }
   }
 
-class App {
-  async play() {
-    
-    console.log(computer);
-    console.log("숫자 야구 게임을 시작합니다.");
-
-    return new Promise(() => {
-      const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-      });
-
-      const handleUserInput = (userInput) => {
-        if (userInput.length !== 3) {
-          throw new Error("[ERROR] 입력값은 3자리 숫자여야 합니다.");
-        }
-      
-        if (Number.isNaN(Number(userInput))) {
-          throw new Error("[ERROR] 입력값은 숫자여야 합니다.");
-        }
-        const NumberArr = userInput.split("").map(Number);
-
-        let Strike = 0;
-        let Ball = 0;
-
-        for (let i = 0; i < 3; i++) {
-          for (let j = 0; j < 3; j++) {
-            if (NumberArr[i] === computer[j]) {
-              if (i === j) {
-                Strike++;
-              } else {
-                Ball++;
-              }
-            }
+  numberBaseballResult(userInput) {
+    const NumberArr = userInput.split("").map(Number);
+    let balls = 0;
+    let strikes = 0;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (NumberArr[i] === this.computer[j]) {
+          if (i === j) {
+            strikes++;
+          } else {
+            balls++;
           }
         }
-
-        if (Strike === 0 && Ball === 0) {
-          console.log("낫싱");
-        } else if (Strike !== 0 && Ball === 0) {
-          console.log(Strike + "스트라이크");
-        } else if (Strike === 0 && Ball !== 0) {
-          console.log(Ball + "볼");
-        } else {
-          console.log(Strike + "스트라이크 " + Ball + "볼");
-        }
-        if (Strike !== 3) {
-          rl.question("숫자를 입력해 주세요: ", handleUserInput);
-        } else {
-          console.log("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-          rl.question("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n", handleRestartInput);
-        }
-      };
-      const handleRestartInput = (userInput) => {
-        if(userInput === 1){
-          rl.question("숫자를 입력해 주세요: ", handleUserInput);
-        }
-        else {
-          rl.close();
-        }
       }
-      rl.question("숫자를 입력해 주세요: ", handleUserInput);
-    });
+    }
+    if (strikes === 0 && balls === 0) {
+      return "낫싱";
+    } else if (strikes !== 0 && balls === 0) {
+      return strikes + "스트라이크";
+    } else if (strikes === 0 && balls !== 0) {
+      return balls + "볼";
+    } else {
+      return balls + "볼 " + strikes + "스트라이크 ";
+    }
+  }
+
+  async restartInput() {
+    const restart = await MissionUtils.Console.readLineAsync(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    );
+    if (restart === "1") {
+      await this.playNumberGame();
+    }
   }
 }
 
