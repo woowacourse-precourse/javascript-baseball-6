8c8d403812e58accf97a44036e8a684220f95a4a
dabinchiii <dabinchi388@naver.com>
2023-10-24 04:16:25 +0900

    feat: 전역 에러를 처리하는 catch 블럭 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import Game from "./Game.js";
 class App {
   async play() {
     const game = new Game();
-    game.start();
+    try {
+      await game.start();
+    } catch (err) {
+      throw new Error(`[ERROR] ${err.message}`);
+    }
   }
 }
 
