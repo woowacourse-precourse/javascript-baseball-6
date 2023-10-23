f9f01e4044d1f8bd0d030b81d09d8595ad840a64
sryung <sryung1225@gmail.com>
2023-10-23 16:05:14 +0900

    [🥁 feat] 볼/스트라이크 분석 및 코드 정리 - 실행에 문제가 있던 코드 수정 및 정리

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ class App {
   async play() {
     // 1. 게임 안내
     Console.print("숫자 야구 게임을 시작합니다.");
+
     // 2. 컴퓨터의 정답 생성
-    computerAnswer = this.makeRandomAnswer;
+    const computerAnswer = this.makeRandomAnswer();
+
     // 3. 게임 시작! 정답 맞추기
-    return this.playBaseBallGame(computerAnswer);
+    this.playBaseballGame(computerAnswer);
   }
 
   // 랜덤 숫자로 이루어진 배열 만들기
@@ -22,37 +24,61 @@ class App {
     return answer;
   }
 
-  // 숫자 야구 게임 진행하기
-  async playBaseBallGame(computerAnswer) {
-    const BALL_STATUS = ["스트라이크", "볼", "낫싱"];
+  // 숫자 야구 게임하기
+  async playBaseballGame(computerAnswer) {
     try {
-      // 사용자 답안 입력 받기
-      let userInput = await Console.readLineAsync("숫자를 입력해주세요 : ");
-      const userAnswer = userInput.split("");
-      // 사용자 답안 유효성 검사
-      this.validateUserAnswer(userAnswer, computerAnswer);
-      // 정답과 사용자 답안 비교 대조
+      let result = "";
       do {
-        this.compareAnswer(userAnswer, computerAnswer);
-      } while (userAnswer === computerAnswer);
+        // 사용자 답안 입력 받기
+        let userAnswer = await Console.readLineAsync("숫자를 입력해주세요 : ");
+        // 사용자 답안 유효성 검사
+        this.validateUserAnswer(userAnswer);
+        userAnswer = userAnswer.split("").map((v) => +v);
+        // 정답과 사용자 답안을 비교해서 볼/스트라이크 판단
+        result = this.compareAnswer(userAnswer, computerAnswer);
+        Console.print(result);
+      } while (result !== "3스트라이크");
+      Console.print("3의 숫자를 모두 맞히셨습니다! 게임 종료");
     } catch (error) {
       Console.print(error.message);
     }
   }
 
-  // 숫자 입력 유효성 검사
-  validateUserAnswer(userAnswer, computerAnswer) {
-    const setUserAnswer = new Set(userAnswer);
+  // 입력된 숫자에 대한 유효성 검사
+  validateUserAnswer(userAnswer) {
     if (
       userAnswer.length !== 3 ||
-      isNaN(userAnswer) ||
-      setUserAnswer.size !== 3
+      isNaN(+userAnswer) ||
+      new Set(userAnswer).size !== 3
     ) {
-      throw new Error("[EROOR] 숫자가 잘못된 형식입니다.");
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
     }
   }
 
-  //
+  // 볼/스트라이크 분석하기
+  compareAnswer(userAnswer, computerAnswer) {
+    let answer = "";
+    const count = {
+      ball: 0,
+      strike: 0,
+    };
+    // 볼/스트라이크 계산
+    for (let i = 0; i < 3; i++) {
+      if (computerAnswer.indexOf(userAnswer[i]) === i) {
+        count["strike"]++;
+      } else if (computerAnswer.indexOf(userAnswer[i]) !== -1) {
+        count["ball"]++;
+        12;
+      }
+    }
+    if (count["strike"] === 0 && count["ball"] === 0) {
+      answer = "낫싱";
+    } else {
+      answer += count["ball"] > 0 ? `${count["ball"]}볼` : ``;
+      answer += count["strike"] > 0 ? `${count["strike"]}스트라이크` : ``;
+    }
+    return answer;
+  }
 }
 
 const app = new App();
