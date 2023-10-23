e1a9a23ba79e16b54c449b8ab445d35e9128bca3
Sungwk <73461032+Sungwk@users.noreply.github.com>
2023-10-23 14:50:29 +0900

    :sparkles: Feat : 입력값과 임의의 3자리 수를 비교하여 strike, ball 카운트 기능 추가
    사용자로부터 입력받은 값과 임의의 3자리 수를 비교하여 Strike, Ball 갯수 카운트하는 함수 구현

--- src/Game.js
+++ src/Game.js
@@ -2,10 +2,14 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 import doValidate from './Validate.js';
 
 class Game {
-  start() {
+  async start() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.result = {
+      ball: 0,
+      strike: 0,
+    };
     this.getComputerNumber();
-    this.getPlayerNumber();
+    await this.getPlayerNumber();
   }
 
   getComputerNumber() {
@@ -17,13 +21,25 @@ class Game {
       .then((number) => {
         this.playerNumber = number;
         this.validatePlayerNumber();
+        this.getResult();
       })
       .catch((err) => {
         MissionUtils.Console.print(err);
       });
   }
+
   validatePlayerNumber() {
     doValidate(this.playerNumber);
+  }
+
+  getResult() {
+    for (let i = 0; i < 3; i += 1) {
+      if (parseInt(this.playerNumber[i], 10) === this.computerNumber[i]) {
+        this.result.strike += 1;
+      } else if (this.computerNumber.includes(parseInt(this.playerNumber[i], 10))) {
+        this.result.ball += 1;
+      }
+    }
   }
 }
 
