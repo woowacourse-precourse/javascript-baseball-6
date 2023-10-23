001515ddf8386a5ae30f0f07c5169d8e7dff2fdd
JUN <gurwns232@naver.com>
2023-10-23 18:05:48 +0900

    feat: 두 숫자 비교해서 볼 카운트하는 기능 추가

--- src/components/Baseball.js
+++ src/components/Baseball.js
@@ -11,16 +11,35 @@ class Baseball {
       user: [],
     };
 
+    /** @type {{ball: number, strike: number}} */
+    this.count = {
+      ball: 0,
+      strike: 0,
+    };
+
     Console.print(GAME_MESSAGE.GAME_START);
   }
 
-  /** 숫자 야구 게임 시작 함수 */
+  /** 숫자 야구 게임 시작 */
   async startGame() {
     const computer = new Computer();
     this.player.computer = computer.getNumber();
 
     const user = new User();
     this.player.user = await user.getNumber();
+
+    this.compareNumber();
+  }
+
+  /** 컴퓨터와 유저의 숫자 비교 */
+  compareNumber() {
+    const { computer, user } = this.player;
+
+    user.forEach((num, idx) => {
+      const exist = computer.indexOf(num);
+      if (exist === idx) this.count.strike++;
+      else if (exist !== idx && exist !== -1) this.count.ball++;
+    });
   }
 }
 
