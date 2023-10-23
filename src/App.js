1505249f9df3df2a021bcfd593d173ffa98ced80
nocheol <qkdl60@gmail.com>
2023-10-24 00:31:42 +0900

    refactor: 에러 체크 수정

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import {MissionUtils} from "@woowacourse/mission-utils";
 import * as CONSTANT from "./constants.js";
 /*
-기능 요구 사항
-  기본적으로 1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 맞추는 게임이다.
-
-  같은 수가 같은 잘에 있으면 스트라이크, 
-  다른 자리에 있으면 볼,
-  같은 수가 전혀 없으면 낫싱,
-  상대방-컴퓨터
-  1~9까지 서로 다른 임의 숫자 3개 선택
-  플레이어
-  1~9까지 서로 다른 임의 숫자 3개 선택
-  게임 결과 출력, 상대방 숫자 3개 모두 맞힐때 승리
- 
-  게임종료 후 다시 시작하거나 완전 종료 선택 가능 (1, 2로 선택)
-
-  사용자가 잘못된 값을 입력시 예외 처리로 종료
-
-
- 프로그래밍 요구 사항
-
- 과제 진행 요국 사항
+  기능 목록
+  [x] 컴퓨터는 상대방이고 랜덤한 수를 가진다.
+  [x] 플레이어는 서로 다른 3자리 숫자를 입력받는다.
+  [x] 상배당 숫자와, 플레이어 숫자를 비교 했을때, 같은 수가 같은 자리에 있다면 strike, 다른 자리에 있다면 ball, 같은 수가 전혀 없다면 낫싱이다. 
+  [x] 게임은 3 strike가 나올때까지 반복, 3 strike이면 플레이어의 승리, 다시 시작 또는 게임종료를 선택하도록 한다. 
+  [x] 사용자 입력이 잘못되었을때 throw 롤 에러 던지고 애플리케이션 종료   
+  []
  */
 
 class App {
@@ -32,10 +19,10 @@ class App {
       const computerNumberArray = createComputerNumbers();
       let isWin = false;
       while (!isWin) {
-        let userNumber = await MissionUtils.Console.readLineAsync(CONSTANT.QUESTION_MESSAGE);
+        const userNumber = await MissionUtils.Console.readLineAsync(CONSTANT.QUESTION_MESSAGE);
         checkUserNumber(userNumber);
         const userNumberArray = [...userNumber.split("")];
-        const {ball, strike} = createGameScore(userNumberArray, computerNumberArray);
+        const {ball, strike} = countGameScore(userNumberArray, computerNumberArray);
         const message = getMessage(ball, strike);
         MissionUtils.Console.print(message);
         if (strike === 3) {
@@ -57,18 +44,21 @@ const getMessage = (ball = 0, strike = 0) => {
   return ball === 0 && strike === 0 ? "낫싱" : `${ball}볼 ${strike}스트라이크`;
 };
 
+const checkInputType = (input) => {
+  if (input === undefined || input === null) throw Error(CONSTANT.ERROR_MESSAGE_TYPE);
+  if (typeof input !== "string" || Number.isNaN(input)) throw Error(CONSTANT.ERROR_MESSAGE_TYPE);
+};
+
 const checkUserNumber = (userNumber) => {
-  if (userNumber === undefined || userNumber === null) throw Error(CONSTANT.ERROR_MESSAGE);
-  if (typeof userNumber !== "string") throw Error(CONSTANT.ERROR_MESSAGE);
-  if (userNumber.length !== 3) throw Error(CONSTANT.ERROR_MESSAGE);
-  if ([...new Set(userNumber.split(""))].length !== 3) throw Error(CONSTANT.ERROR_MESSAGE);
+  checkInputType(userNumber);
+  if (userNumber.length !== 3) throw Error(CONSTANT.ERROR_MESSAGE_LENGTH);
+  if ([...new Set(userNumber.split(""))].length !== 3) throw Error(CONSTANT.ERROR_MESSAGE_DUPLICATION);
 };
 
 const checkEndAnswer = (endAnswer) => {
-  if (endAnswer === undefined || endAnswer === null) throw Error(CONSTANT.ERROR_MESSAGE);
-  if (typeof endAnswer !== "string") throw Error(CONSTANT.ERROR_MESSAGE);
-  if (endAnswer.length !== 1) throw Error(CONSTANT.ERROR_MESSAGE);
-  if (endAnswer !== "1" && endAnswer !== "2") throw Error(CONSTANT.ERROR_MESSAGE);
+  checkInputType(endAnswer);
+  if (endAnswer.length !== 1) throw Error(CONSTANT.ERROR_MESSAGE_LENGTH);
+  if (endAnswer !== "1" && endAnswer !== "2") throw Error(CONSTANT.ERROR_MESSAGE_RANGE);
 };
 
 const createComputerNumbers = () => {
@@ -80,7 +70,7 @@ const createComputerNumbers = () => {
   return computerNumbersArray;
 };
 
-const createGameScore = (userNumberArray, computerNumberArray) => {
+const countGameScore = (userNumberArray, computerNumberArray) => {
   let ball = 0;
   let strike = 0;
   userNumberArray.forEach((number, index) => {
@@ -88,6 +78,5 @@ const createGameScore = (userNumberArray, computerNumberArray) => {
     if (integerNumber === computerNumberArray[index]) strike++;
     else if (computerNumberArray.includes(integerNumber)) ball++;
   });
-
   return {ball, strike};
 };
