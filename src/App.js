fe233e63caec0c4313abf7891daa35b140a1f775
j8won <joowoni99@gmail.com>
2023-10-23 21:45:21 +0900

    refactor: App 클래스 변수와 메서드 은닉화

--- src/App.js
+++ src/App.js
@@ -8,18 +8,22 @@ import { MESSAGE, INPUT } from "./constant/MESSAGE.js";
 import ERROR from "./constant/ERROR.js";
 
 class App {
+  #computer;
+  #user;
+  #game;
+  #isAppRunning = true;
+
   constructor() {
-    this.computer = new Computer();
-    this.user = new User();
-    this.game = new Baseball(this.computer, this.user);
-    this.isAppRunning = true;
+    this.#computer = new Computer();
+    this.#user = new User();
+    this.#game = new Baseball(this.#computer, this.#user);
   }
 
   async play() {
     this.startApp();
-    while (this.isAppRunning) {
-      const shouldContinue = await this.replayGame();
-      if (!shouldContinue) this.isAppRunning = false;
+    while (this.#isAppRunning) {
+      const shouldContinue = await this.#replayGame();
+      if (!shouldContinue) this.#isAppRunning = false;
     }
   }
 
@@ -27,8 +31,8 @@ class App {
     Console.print(MESSAGE.START_GAME);
   }
 
-  async replayGame() {
-    const isGameFinished = !(await this.game.play());
+  async #replayGame() {
+    const isGameFinished = !(await this.#game.playGame());
 
     if (isGameFinished) {
       return this.askGameRestartOrExit();
