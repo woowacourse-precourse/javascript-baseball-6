2f31de3ad5e9853769e41c7edc20f6c2bfc6d5c8
SeoDongSu <poopoo0515@naver.com>
2023-10-23 19:37:39 +0900

    style: 중복 코드 제거

--- src/errorThrow.js
+++ src/errorThrow.js
@@ -3,18 +3,14 @@ import { Console } from "@woowacourse/mission-utils";
 export const errorThrow = (userInput) => {
   let error = false;
 
-  Console.print(userInput);
   if (userInput === null) {
     error = true;
     throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
   }
-  if (userInput.length !== 3) {
+  if (userInput.length !== 3 || /^[1-9]+$/.test(userInput) === false) {
     error = true;
     Console.print("[ERROR] 숫자가 잘못된 형식입니다.");
     throw new Error("[ERROR]");
-  } else if (/^[1-9][1-9][1-9]$/.test(userInput) === false) {
-    error = true;
-    throw new Error("[ERROR] 숫자가 잘못된 형식입니다..");
   }
   return error;
 };
