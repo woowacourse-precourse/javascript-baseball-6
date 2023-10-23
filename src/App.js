68141d57b033f677f4f5b00217df62fe9f8bd709
hyun812 <sh980812@naver.com>
2023-10-23 20:51:12 +0900

    [feat] 사용자로부터 입력받은 숫자 유효성 검사 및 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
 import target from './getComputerNum.js';
+import checkNum from './validation.js';
 
 class App {
   async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    while(true){
+      try{
+        const userNum = await Console.readLineAsync("숫자를 입력해주세요 : ");
+        checkNum(userNum);
+
+      }catch (error){
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      }
+    }
     Console.print(target);
   }
 }
---
+++ src/validation.js
@@ -0,0 +1,10 @@
+const checkNum = (userNum) =>{
+    if(userNum.length !== 3 ||
+        userNum[0] === userNum[1] ||
+        userNum[1] === userNum[2] ||
+        userNum[0] === userNum[2]
+    )
+    throw new Error();
+}
+
+export default checkNum;
