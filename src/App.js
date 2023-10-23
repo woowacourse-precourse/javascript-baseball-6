7b459676d1bfb98b559fe5140ebff6b245b83d99
parkjonggyeong18 <gd122572@gmail.com>
2023-10-24 01:32:32 +0900

    구현 & readme checkbox 체크

--- src/App.js
+++ src/App.js
@@ -1,5 +1,91 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+    async play() {
+        let playing = true; // 게임 진행 여부를 확인하는 변수
+
+        Console.print('숫자 야구 게임을 시작합니다.');
+        while (playing) {
+            const computer = this.computerNumber();
+            await this.userNumber(computer);
+            playing = await this.restart();
+        }
+    }
+
+    // 컴퓨터의 숫자를 무작위로 설정
+    computerNumber() {
+        
+        const computer = [];
+        while (computer.length < 3) {
+            const number = Random.pickNumberInRange(1, 9);
+            if (!computer.includes(number)) {
+                computer.push(number);
+            }
+        }
+        return computer;
+    }
+
+    // 유저의 숫자를 입력 받는다.
+    async userNumber(ComputerInput) {
+        while (true) {
+            const input = await Console.readLineAsync('숫자를 입력해주세요 : ');
+            const userInput = input.trim().split('').map(Number);
+
+            if (userInput.length !== 3 || userInput.some(isNaN)) {
+                throw Error("[ERROR] 올바른 형식이 아닙니다.");
+            }
+
+            const result = this.checkNumber(userInput, ComputerInput);
+            Console.print(result);
+
+            if (result === '3스트라이크') {
+                Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+                break;
+            }
+
+        }
+
+    }
+    // 스트라이크와 볼의 횟수를 측정
+    countNumber(userInput, computerInput) {
+        let strike = 0;
+        let ball = 0;
+        for (let i = 0; i < 3; i++) {
+            if (userInput[i] === computerInput[i]) {
+                strike++;
+            } else if (computerInput.includes(userInput[i])) {
+                ball++;
+            }
+        }
+        return [strike, ball];
+    }
+
+    //측정된 스트라이크와 볼을 출력
+    checkNumber(userInput, computerInput) {
+        const [strike, ball] = this.countNumber(userInput, computerInput);
+        if (strike === 0 && ball === 0) {
+            return '낫싱'
+        } else if (strike === 0) {
+            return `${ball}볼`
+        } else if (ball === 0) {
+            return `${strike}스트라이크`
+        } else {
+            return `${ball}볼 ${strike}스트라이크`
+        }
+    }
+
+    //게임 재시작 여부 확인 및 재시작
+    async restart() {
+        Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.')
+        const input = await Console.readLineAsync()
+        if (input === '1') {
+            return true
+        } else if (input === '2') {
+            return false
+        } else {
+            throw new Error("[ERROR] 올바른 형식이 아닙니다.");
+        }
+    }
 }
 
 export default App;
