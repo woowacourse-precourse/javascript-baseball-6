2d79ba6f2bb31369963cd80ddce60eb54f9d8c66
ceh20002@naver.com <ceh20002@naver.com>
2023-10-23 23:02:22 +0900

    돌아가는 쓰레기 구현 및 Docs 수정

--- src/App.js
+++ src/App.js
@@ -11,47 +11,43 @@ class App {
     await this.gameStartText();
     await this.generateRandomNumber();
     console.log("컴퓨터 정답값", this.randomNumber);
-
-    while (true) {
-      await this.getUserInput();
-      await this.checkValidInput(this.input);
-      console.log(this.input);
-      await this.checkStrikeBall(this.input, this.randomNumber);
-      this.countResult = this.checkStrikeBall(this.input, this.randomNumber);
-      await this.printHintMessage(this.countResult);
-
-      if (this.countResult.strike !== 3) {
-        await this.retry();
-      } 
-
-      if (this.countResult.strike === 3) {
-        await this.printGameWinMessage();
-        await this.replay();
-      }
-    }
-  }
-
-  async retry() {
-    this.input = '';
     await this.getUserInput();
     await this.checkValidInput(this.input);
-    console.log(this.input);
     await this.checkStrikeBall(this.input, this.randomNumber);
     this.countResult = this.checkStrikeBall(this.input, this.randomNumber);
     await this.printHintMessage(this.countResult);
-    console.log(this.countResult.strike, "스트라이크가 3개가 나오고 종료되는지 확인을 위한 코드");
+
+    if (this.countResult.strike !== 3) {
+      await this.retry();
+    } 
+
+    if (this.countResult.strike === 3) {
+      await this.printGameWinMessage();
+      await this.replay();
+    }
+    
+  }
+
+  // 왜 while로 안하면 2번에 끊기는지 이해하기 
+  async retry() {
+    while (this.countResult.strike !== 3) {
+      this.input = '';
+      await this.getUserInput();
+      await this.checkValidInput(this.input);
+      await this.checkStrikeBall(this.input, this.randomNumber);
+      this.countResult = this.checkStrikeBall(this.input, this.randomNumber);
+      await this.printHintMessage(this.countResult);
+    }
   }
 
   async replay(){
     const userResponse =  await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
-    console.log("사용자의 재도전 응답 여부",userResponse);
     if (Number(userResponse) === 1){
       this.input = '';
       this.countResult = '';
       this.play();
     }
     if (Number(userResponse) === 2){
-      console.log("하기 싫어");
       process.exit();
     }
     if (Number(userResponse) !== (1 || 2)){
@@ -60,30 +56,25 @@ class App {
   }
 
   printGameWinMessage(){
-    console.log("3개의 숫자를 모두 맞히셨습니다.");
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
   }
   
   //힌트 제공 기능 - 스트라이크 개수와 볼 개수 출력 {strike, ball} 가져와 표현
   printHintMessage(input) {
-    console.log("힌트 제공을 위한 스트라이크 개수 확인",input);
     if (input.strike === 0 && input.ball === 0){
-      console.log("낫싱");
-    }
-    if (input.strike === 3){
-      console.log("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      MissionUtils.Console.print("낫싱");
     }
 
-    // 스트라이크가 1 ~ 2개 존재하고 볼은 0개가 아닐때
     if ((input.strike !== 0) && input.ball !== 0){
-      console.log(`${this.countResult.ball}볼 ${this.countResult.strike}스트라이크`);
+      MissionUtils.Console.print(`${this.countResult.ball}볼 ${this.countResult.strike}스트라이크`);
     }
 
     if ((input.strike !== 0) && input.ball === 0){
-      console.log(`${input.strike}스트라이크`);
+      MissionUtils.Console.print(`${input.strike}스트라이크`);
     }
 
     if ((input.strike === 0) && input.ball !== 0) {
-      console.log(`${input.ball}볼`);
+      MissionUtils.Console.print(`${input.ball}볼`);
     }
   }
 
@@ -94,7 +85,7 @@ class App {
 
   // 사용자의 입력값이 유효한 값인지 체크 - 숫자인지, 3자리인지, 중복됬는지
   async getUserInput() {
-    const userInput = await MissionUtils.Console.readLineAsync('닉네임을 입력해주세요.');
+    const userInput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
     this.input = userInput;
     return this.input;
   }
