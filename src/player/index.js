50c113c8390e2aa1877a3621322f3a49a01caf2e
Kled <1017yu@daum.net>
2023-10-23 10:26:03 +0900

    refactor: async await 방식으로 코드 실행 순서 보장

--- src/player/index.js
+++ src/player/index.js
@@ -1,10 +1,7 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
-import constants from '../../constants/index.js';
+import { Console } from '@woowacourse/mission-utils';
+import { GUIDE_MESSAGES, ERROR_MESSAGES } from '../../constants/index.js';
 import InputValidator from './inputValidator.js';
 import Opponent from '../opponent/index.js';
-
-const { Console } = MissionUtils;
-const { MESSAGES } = constants;
 
 class Player {
   /**
@@ -13,31 +10,31 @@ class Player {
    */
   static playerNumberSet;
 
-  startGame = () => {
-    this.getPlayerNumber();
+  async startGame() {
     this.opponent = new Opponent();
-  };
+    await this.getPlayerNumber();
+  }
 
   // P-1 플레이어는 3자리 숫자를 맞추기 위해 숫자를 입력
-  getPlayerNumber = async () => {
+  async getPlayerNumber() {
     try {
-      const playerInput = await Console.readLineAsync(MESSAGES.INPUT);
+      const playerInput = await Console.readLineAsync(GUIDE_MESSAGES.INPUT);
+
+      // P-1-a 유효성 검사에 통과하지 못한 경우 에러 메시지와 함께 종료
+      InputValidator.isValidNumberSet(playerInput);
       this.handleNumberSet(playerInput);
-    } catch (error) {}
-  };
-
-  handleNumberSet = (input) => {
-    // P-1-a 유효성 검사에 통과하지 못한 경우 에러 메시지와 함께 종료
-    if (!InputValidator.isValidNumberSet(input)) {
-      throw new Error(MESSAGES.ERROR.INVALID_COUNT_NUMBER);
+    } catch (error) {
+      throw error;
     }
+  }
 
+  async handleNumberSet(input) {
     // 유효하다면 [number, number, number] 형태의 배열화
     this.playerNumberSet = input.split('').map(Number);
     this.requestBallCount();
-  };
+  }
 
-  requestBallCount = () => {
+  async requestBallCount() {
     const [ballCountMessage, strike] = this.opponent.checkBallCount(
       this.playerNumberSet
     );
@@ -49,22 +46,24 @@ class Player {
     } else {
       this.getPlayerNumber();
     }
-  };
+  }
 
-  getPlayAgain = async () => {
+  async getPlayAgain() {
     try {
-      Console.print(MESSAGES.PLAYER_WIN);
-      const playerInput = await Console.readLineAsync(MESSAGES.PLAY_AGAIN);
+      Console.print(GUIDE_MESSAGES.PLAYER_WIN);
+      const playerInput = await Console.readLineAsync(
+        GUIDE_MESSAGES.PLAY_AGAIN
+      );
 
       if (playerInput === '1') {
         this.startGame();
       } else if (playerInput === '2') {
-        Console.print(MESSAGES.END);
+        Console.print(GUIDE_MESSAGES.END);
       } else {
-        throw new Error(MESSAGES.ERROR.INVALID_REPLAY_NUMBER);
+        throw new Error(ERROR_MESSAGES.INVALID_REPLAY_NUMBER);
       }
     } catch (error) {}
-  };
+  }
 }
 
 export default Player;
