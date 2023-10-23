b5b553e723f84a55231adf087bd544f058ce1d77
kimjinwoo <rlawlsdn177@naver.com>
2023-10-23 23:33:25 +0900

    feat: 비교 결과 메세지 출력 / import 컨벤션 수정

--- src/Output.js
+++ src/Output.js
@@ -1,7 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
 import { 
     START_MESSAGE,
-    ERROR_MESSAGE
+    ERROR_MESSAGE,
+    NOTHING,
+    START_MESSAGE,
+    STRIKE,
 } from './Define';
 
 export const PRINT_START_MESSAGE = () => {
@@ -11,3 +14,13 @@ export const PRINT_START_MESSAGE = () => {
 export const PRINT_ERROR_MESSAGE = () => {
     throw new Error(ERROR_MESSAGE);
 };
+
+export const PRINT_RESULT_MESSAGE = ({ strikes, balls }) => {
+    if (strikes === 0 && balls === 0) {
+      Console.print(NOTHING);
+    } else if (strikes === 3) {
+      Console.print(CLOSE_MESSAGE);
+    } else {
+      Console.print(`${balls}${BALL} ${strikes}${STRIKE}`);
+    }
+  };
