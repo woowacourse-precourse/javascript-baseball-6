2f0be69547e8e7a72c374beb7eac19b9e103050a
Myomyoring <narcissuscat@naver.com>
2023-10-23 23:51:59 +0900

    [feat] 재시작 분기 처리

--- src/App.js
+++ src/App.js
@@ -4,74 +4,24 @@ const RANGE_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 class App {
   async play() {
-    // 랜덤 숫자 생성
-    const computer = [];
-    while (computer.length < 3) {
-      const randomNumber = Random.pickNumberInRange(1, 9);
-      if (!computer.includes(randomNumber)) {
-        computer.push(randomNumber);
-      }
-    }
-    // 시작
-    Console.print(computer);
-    Console.print('숫자 야구 게임을 시작합니다.');
-    try {
-      let inputNumber = await Console.readLineAsync(
-        '0을 제외한 서로 다른 숫자를 3자리 입력해주세요: '
-      );
-      // 예외 구문
-      let parseArr = inputNumber.split('');
-      for (let num of parseArr) {
-        let valid = RANGE_NUMBERS.includes(Number(num));
-        if (!valid) {
-          throw new Error('1~9 사이의 숫자를 입력해야 합니다.');
+    while (true) {
+      // 랜덤 숫자 생성
+      const computer = [];
+      while (computer.length < 3) {
+        const randomNumber = Random.pickNumberInRange(1, 9);
+        if (!computer.includes(randomNumber)) {
+          computer.push(randomNumber);
         }
       }
-      if (inputNumber.length !== 3) {
-        throw new Error('3자리의 숫자를 입력해야 합니다.');
-      }
-      const deduplicateObj = new Set(parseArr);
-      if (deduplicateObj.size !== inputNumber.length) {
-        throw new Error('서로 다른 숫자를 입력해야 합니다.');
-      }
-
-      while (true) {
-        // 값 판별
-        let ball = 0;
-        let strike = 0;
-
-        for (let i = 0; i < 3; i++) {
-          if (computer[i] === Number(inputNumber[i])) {
-            strike += 1;
-          } else if (computer.includes(Number(inputNumber[i]))) {
-            ball += 1;
-          }
-        }
-
-        if (ball === 0 && strike === 0) {
-          Console.print('낫싱');
-        } else if (ball === 0) {
-          Console.print(`${strike}스트라이크`);
-          if (strike === 3) {
-            Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-            const regameInput = await Console.readLineAsync(
-              '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: '
-            );
-            if (regameInput === '1') {
-            } else if (regameInput === '2') {
-              return;
-            } else {
-              throw new Error('1 또는 2를 입력해야 합니다.');
-            }
-          }
-        } else if (strike === 0) {
-          Console.print(`${ball}볼`);
-        } else {
-          Console.print(`${ball}볼 ${strike}스트라이크`);
-        }
-        // 틀렸을 시, 반복
-        inputNumber = await Console.readLineAsync('숫자를 입력해주세요 : ');
-        parseArr = inputNumber.split('');
+      // 시작
+      Console.print(computer);
+      Console.print('숫자 야구 게임을 시작합니다.');
+      try {
+        let inputNumber = await Console.readLineAsync(
+          '0을 제외한 서로 다른 숫자를 3자리 입력해주세요: '
+        );
+        // 예외 구문
+        let parseArr = inputNumber.split('');
         for (let num of parseArr) {
           let valid = RANGE_NUMBERS.includes(Number(num));
           if (!valid) {
@@ -85,10 +35,63 @@ class App {
         if (deduplicateObj.size !== inputNumber.length) {
           throw new Error('서로 다른 숫자를 입력해야 합니다.');
         }
-        continue;
+
+        while (true) {
+          // 값 판별
+          let ball = 0;
+          let strike = 0;
+
+          for (let i = 0; i < 3; i++) {
+            if (computer[i] === Number(inputNumber[i])) {
+              strike += 1;
+            } else if (computer.includes(Number(inputNumber[i]))) {
+              ball += 1;
+            }
+          }
+
+          if (ball === 0 && strike === 0) {
+            Console.print('낫싱');
+          } else if (ball === 0) {
+            Console.print(`${strike}스트라이크`);
+            if (strike === 3) {
+              Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+              const regameInput = await Console.readLineAsync(
+                '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: '
+              );
+              if (regameInput === '1') {
+                break;
+              } else if (regameInput === '2') {
+                return;
+              } else {
+                throw new Error('1 또는 2를 입력해야 합니다.');
+              }
+            }
+          } else if (strike === 0) {
+            Console.print(`${ball}볼`);
+          } else {
+            Console.print(`${ball}볼 ${strike}스트라이크`);
+          }
+          // 틀렸을 시, 반복
+          inputNumber = await Console.readLineAsync('숫자를 입력해주세요 : ');
+          parseArr = inputNumber.split('');
+          for (let num of parseArr) {
+            let valid = RANGE_NUMBERS.includes(Number(num));
+            if (!valid) {
+              throw new Error('1~9 사이의 숫자를 입력해야 합니다.');
+            }
+          }
+          if (inputNumber.length !== 3) {
+            throw new Error('3자리의 숫자를 입력해야 합니다.');
+          }
+          const deduplicateObj = new Set(parseArr);
+          if (deduplicateObj.size !== inputNumber.length) {
+            throw new Error('서로 다른 숫자를 입력해야 합니다.');
+          }
+          continue;
+        }
+      } catch (error) {
+        console.log('[ERROR]', error.message);
       }
-    } catch (error) {
-      console.log('[ERROR]', error.message);
     }
   }
 }
