cce71c8edcaa78a812cd97ee1f7eb194352c154c
bl0ckp1n9 <blockp1n9@gmail.com>
2023-10-23 16:52:01 +0900

    feat: 입력값 검증 기능

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
+const MESSAGE = {
+  start: "숫자 야구 게임을 시작합니다.",
+  end: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  continue: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  input: "숫자를 입력해주세요 : ",
+  nothing: "낫싱",
+  strike: (count) => `${count}스트라이크`,
+  ball: (count) => `${count}볼`,
+  error: (message) => `[ERROR] ${message}`,
+};
+
 class App {
   #randomNumber;
 
-  async play() {}
+  async play() {
+    Console.print(MESSAGE.start);
+
+    const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
+
+    if (this.#isValidInput(input)) {
+      throw new Error(MESSAGE.error("입력값이 유효하지 않습니다."));
+    }
+  }
 
   getRandomNumber() {
     this.#makeRandomNumber();
@@ -20,6 +39,12 @@ class App {
     }
 
     this.#randomNumber = result;
+  }
+
+  #isValidInput(input) {
+    const regEx = new RegExp("^(?!.*(\\d).*\\1)[1-9]{3}$");
+
+    return !regEx.test(input.trim());
   }
 }
 
