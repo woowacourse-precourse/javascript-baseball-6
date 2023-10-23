b2848c7633463d8c6ee19f1b3492252743e89860
badahertz52 <badahertz52@gmail.com>
2023-10-23 20:22:00 +0900

    [feats] Add methods  to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
   playing = true;
 
@@ -27,4 +29,144 @@ class App {
     text: '',
   };
 
+  setComputerNumbers() {
+    let array = [];
+    while (array.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!array.includes(number) && number) {
+        if (array.length) {
+          array.push(number);
+        } else {
+          array = [number];
+        }
+      }
+    }
+    this.computerNumbers = array;
+  }
+
+  // 스트라이크, 볼 초기화
+  resetStrikeAndBall() {
+    this.strike = 0;
+    this.ball = 0;
+  }
+
+  // 상태 초기화
+  resetState() {
+    this.playing = true;
+    this.success = false;
+    this.userText = '';
+    this.resetStrikeAndBall();
+    this.setComputerNumbers();
+  }
+  // 메세지 출력
+  // eslint-disable-next-line
+  printMessage(message) {
+    MissionUtils.Console.print(message);
+  }
+
+  startGame() {
+    this.resetState();
+    this.printMessage(this.sentence.start);
+  }
+
+  endGame() {
+    this.playing = false;
+    this.printMessage(this.sentence.end);
+  }
+
+  throwError(error) {
+    this.playing = false;
+    throw new Error(error);
+  }
+
+  // input 내용
+  async getUserNumbers() {
+    try {
+      if (!this.success) this.printMessage('숫자를 입력해주세요.');
+      const text =
+        await MissionUtils.Console.readLineAsync('숫자를 입력해주세요.');
+      this.userText = Number(text.replaceAll(' ', ''));
+    } catch (error) {
+      this.throwError(error);
+    }
+  }
+
+  // 유효성 검사
+  validNumbers() {
+    if (this.strike !== 3) {
+      return /^[1-9]{3}$/.test(this.userText);
+    }
+    return /^[1,2]$/.test(this.userText);
+  }
+
+  // 스트라이크, 볼 판정
+  testStrikeAndBall() {
+    const userNumbers = String(this.userText)
+      .split('')
+      .map(v => Number(v));
+
+    this.computerNumbers.forEach((v, i) => {
+      if (v === userNumbers[i]) {
+        this.strike += 1;
+      } else if (userNumbers.includes(v)) {
+        this.ball += 1;
+      }
+    });
+    const isNothing = !this.strike && !this.ball;
+    const isThreeStrike = this.strike === 3;
+    const strikeAndBall = isThreeStrike
+      ? '3스트라이크'
+      : `${this.strike ? this.strike + this.sentence.strike : ''} ${
+          this.ball ? this.ball + this.sentence.ball : ''
+        }`;
+    console.log(
+      'user',
+      this.userText,
+      'com',
+      this.computerNumbers,
+      's',
+      this.strike,
+      'b',
+      this.ball,
+    );
+    return isNothing ? this.sentence.nothing : strikeAndBall;
+  }
+
+  // 판정 결과
+  test() {
+    if (this.success) {
+      const isRestart = this.userText === 1;
+      console.log('usetext', this.userText);
+      if (isRestart) {
+        this.resetState();
+      } else {
+        this.endGame();
+      }
+    } else {
+      const result = this.testStrikeAndBall();
+      this.printMessage(result);
+      if (this.strike === 3) {
+        this.printMessage(this.sentence.restart);
+        this.success = true;
+      }
+      this.resetStrikeAndBall();
+    }
+  }
+
+  async run() {
+    await this.getUserNumbers();
+    const testPass = this.validNumbers();
+    if (testPass) {
+      this.test();
+      if (this.playing) this.run();
+    } else {
+      this.throwError(this.sentence.error);
+    }
+  }
+
+  async play() {
+    this.startGame();
+    this.run();
+  }
+}
 export default App;
