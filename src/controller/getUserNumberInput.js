8013fb605867848a5f87cff4be485e0fd53b7e5b
JiSeong Kim <12180543@inha.edu>
2023-10-23 18:01:17 +0900

    Refactor: getUserNumberInput에서 input을 array로 변환하는 부분 수정
    기존에 input.split('')으로 array로 변환하였는데, 컨벤션 4.4 부분에 iterable object는 spread 연산자를 통해 array로 변환하라고 되어있어 spread 연산자를 사용하는 코드로 리팩토링 하였음.

--- src/controller/getUserNumberInput.js
+++ src/controller/getUserNumberInput.js
@@ -2,7 +2,7 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 const checkBlank = (input) => {
     const throwBlankError = () => { throw new Error("[ERROR] 3자리의 수를 공백없이 이어서 입력해야 합니다.") }
-    input.split('').forEach(inputElement => { inputElement === ' ' && throwBlankError()});
+    [...input].forEach(inputElement => { inputElement === ' ' && throwBlankError()});
 }
 
 const checkThreeDigit = (input) => {
@@ -15,12 +15,12 @@ const checkNotDuplicated = (input) => {
 
 const checkAllNumber = (input) => {
     const throwNotNumberError = () => { throw new Error("[ERROR] 입력에는 숫자만 포함되어야 합니다.") }
-    input.split('').forEach(inputElement => isNaN(parseInt(inputElement)) && throwNotNumberError())
+    [...input].forEach(inputElement => isNaN(parseInt(inputElement)) && throwNotNumberError())
 }
 
 const checkNoZero = (input) => {
     const throwHasZeroError = () => { throw new Error("[ERROR] 입력에는 0이 포함되지 않아야 합니다.") }
-    input.split('').forEach(inputElement => parseInt(inputElement) === 0 && throwHasZeroError())
+    [...input].forEach(inputElement => parseInt(inputElement) === 0 && throwHasZeroError())
 }
 
 export const getUserNumberInput = async () => {
