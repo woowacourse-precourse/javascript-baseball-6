6c6f5de140bd681be9688b19437b379877d47556
DongjaJ <jdh981118@naver.com>
2023-10-23 23:57:46 +0900

    feat: 게임이 시작하면 텍스트를 출력하고 사용자로부터 입력을 받는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  conputer = [];
+
+  initComputer() {
+    this.computer = [];
+    while (this.computer.length < 3) {
+      const newNumber = Random.pickNumberInRange(1, 9).toString();
+      if (!this.computer.includes(newNumber)) {
+        this.computer.push(newNumber);
+      }
+    }
+  }
+
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    while (1) {
+      const input = await Console.readLineAsync();
+      break;
+    }
+  }
 }
 
 export default App;
