3d47c2d672612b14ad7cd9bddd92462dbe180e43
soi-ha <soy2302ten@gmail.com>
2023-10-24 00:42:08 +0900

    feat: 게임 리스타트 여부 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 import PlayerInputNumber from './PlayerInputNumber.js';
 import CheckBaseball from './CheckBaseball.js';
+import ResponseGameRestart from './ResponseGameRestart.js';
 
 class App {
 	async play() {
@@ -28,6 +29,10 @@ class App {
 		}
 		// 정답 문구 출력
 		Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+		let CHECKRESTART = await ResponseGameRestart();
+		if (CHECKRESTART === 1) {
+			app.play();
+		}
 	}
 	gameStart() {
 		Console.print('숫자 야구 게임을 시작합니다.');
---
+++ src/ResponseGameRestart.js
@@ -0,0 +1,12 @@
+import { Console } from '@woowacourse/mission-utils';
+export default async function ResponseGameRestart() {
+	try {
+		const answer = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
+		if (answer !== '1' && answer !== '2') {
+			throw { name: 'InputValueError', message: '1과 2외의 다른 값을 입력했습니다.' };
+		}
+		return Number(answer);
+	} catch (error) {
+		console.log(`[ERROR] ${error.message}`);
+	}
+}
