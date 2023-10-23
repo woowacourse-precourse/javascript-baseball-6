9171c8a504ec18503a85b276ed184954423ba729
wonkyum-kim <dodo12120@naver.com>
2023-10-23 11:45:29 +0900

    feat: 서비스 코드 구성 - docs를 기반으로 함수 작성 예정

---
+++ src/Baseball.service.js
@@ -0,0 +1,19 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
+export class BaseballService {
+  query = '';
+
+  generateRandomNumbers() {}
+
+  validateBaseballQueryInput() {}
+
+  refree() {}
+
+  async retry() {}
+
+  printResult() {}
+
+  validateRetryInput(retry) {}
+
+  async baseballQuery() {}
+}
