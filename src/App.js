ebbcee74ef0f7e67514a46c8feb5d243e91e05e5
JungHun98 <j7papa@naver.com>
2023-10-23 15:03:08 +0900

    feat: App 게임 사이클 구현 랜덤 숫자 불러오기 -> 정답 맞추기 -> 다음 단계

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
+import tryMatch from './modules/tryMatch';
+import getRandomUniqueNumber from './modules/getRandomUniqueNumber';
+import getNextStep from './modules/getNextStep';
+
+const EXIT = '2';
+
 class App {
+  constructor() {
+    this.exit = false;
+  }
+
+  async startGame() {
+    while (!this.exit) {
+      const randomNumber = getRandomUniqueNumber();
+
+      await tryMatch(randomNumber);
+
+      const nextStep = await getNextStep();
+      this.exit = nextStep === EXIT;
+    }
+  }
+
   async play() {}
 }
 
