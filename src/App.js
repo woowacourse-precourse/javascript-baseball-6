19a14c58cb40af39a9b156f51a63c8a608ea4899
badahertz52 <badahertz52@gmail.com>
2023-10-23 20:20:24 +0900

    [docs] Make properties of App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 class App {
-  async play() {}
-}
+  playing = true;
+
+  strike = 0;
+
+  ball = 0;
+
+  success = false;
+
+  userText = '';
+
+  computerNumbers = [];
+
+  sentence = {
+    start: '숫자 야구 게임을 시작합니다.',
+    end: '게임종료',
+    strike: '스트라이크',
+    ball: '볼',
+    nothing: '낫싱',
+    win: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+    restart: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+    error: '[ERROR] 숫자가 잘못된 형식입니다.',
+  };
+
+  message = {
+    type: '',
+    text: '',
+  };
 
 export default App;
