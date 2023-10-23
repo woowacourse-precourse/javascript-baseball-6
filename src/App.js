45988cb74ed276ef6c403b24ddbc3dee41b8871e
intersoom <sallybig@naver.com>
2023-10-23 23:07:23 +0900

    refactor: 에러 체크 모듈화

--- src/App.js
+++ src/App.js
@@ -14,6 +14,28 @@ class App {
     return computer;
   }
 
+  inputValidChecker(user) {
+    // 숫자가 아닌 문자를 입력한 경우
+    if (Number.isNaN(Number(user))) {
+      throw new Error("[ERROR] 숫자의 형태로 입력해주세요");
+    }
+
+    // 1-9가 아닌 숫자를 입력한 경우
+    if (user.includes("0")) {
+      throw new Error("[ERROR] 1-9 사이의 숫자를 입력해주세요");
+    }
+
+    // 세 자리수가 아닌 숫자를 입력한 경우
+    if (user.length !== 3) {
+      throw new Error("[ERROR] 세 자리로 입력해주세요");
+    }
+
+    // 서로 다른 숫자를 입력하지 않은 경우
+    if (user.length !== new Set(Array.from(user)).size) {
+      throw new Error("[ERROR] 서로 다른 세 자리 수를 입력해주세요");
+    }
+  }
+
   conditionCoverter(result) {
     if (result.ball === 0 && result.strike === 0) {
       return "낫싱";
@@ -52,26 +74,7 @@ class App {
       const userInput = [];
 
       const user = await Console.readLineAsync("숫자를 입력해주세요: ");
-
-      // 숫자가 아닌 문자를 입력한 경우
-      if (Number.isNaN(Number(user))) {
-        throw new Error("[ERROR] 숫자의 형태로 입력해주세요");
-      }
-
-      // 1-9가 아닌 숫자를 입력한 경우
-      if (user.includes("0")) {
-        throw new Error("[ERROR] 1-9 사이의 숫자를 입력해주세요");
-      }
-
-      // 세 자리수가 아닌 숫자를 입력한 경우
-      if (user.length !== 3) {
-        throw new Error("[ERROR] 세 자리로 입력해주세요");
-      }
-
-      // 서로 다른 숫자를 입력하지 않은 경우
-      if (user.length !== new Set(Array.from(user)).size) {
-        throw new Error("[ERROR] 서로 다른 세 자리 수를 입력해주세요");
-      }
+      this.inputValidChecker(user);
 
       user.split("").map((number) => {
         userInput.push(Number(number));
@@ -82,9 +85,11 @@ class App {
 
       if (determinedCondition.strike === 3) {
         Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
         const isRestart = await Console.readLineAsync(
           "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
         );
+
         switch (isRestart[0]) {
           case "1":
             isGameRunning = true;
