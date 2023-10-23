fa63883e029acbbaa47227e807d83fd17fcc0c51
WOOJINCHO98 <steamedbunwith@gmail.com>
2023-10-23 11:46:16 +0900

    feat : 숫자 중복시 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 
-/**
- * @todo 입력받은 수에서 중복된 값을 예외처리
- * 배열로 처리하기 전에 분기했던 로직에서 배열로 처리한 후 분기하는 로직으로 변경 필요
- */
-
 // 비교 할 수의 총 갯수를 지정하는 상수
 const INPUT_LENGTH = 3;
-
-function InputException(message) {
-  this.message = message;
-  this.name = "InputException";
-}
 
 // 컴퓨터가 3개의 난수 배열을 생성하는 함수
 const getComputerNumber = () => {
@@ -20,24 +10,51 @@ const getComputerNumber = () => {
     const number = Random.pickNumberInRange(1, 9);
     if (!computer.includes(number)) {
       computer.push(number);
-      console.log(computer);
     }
   }
   return computer;
 };
 
-// 플레이어에게 숫자를 입력받는 함수
-const readFromPlayer = () => {
-  return new Promise((resolve) => {
-    Console.readLine("숫자를 입력해주세요 : ", (answer) => {
-      if (answer.length === INPUT_LENGTH && answer >= 100 && answer <= 999) {
-        resolve(readToList(answer));
-      } else {
-        throw new InputException("예외가 발생했습니다.");
-      }
-    });
-  });
+// 사용자로부터 입력을 받는 함수
+const readFromPlayer = async () => {
+  let answer = "";
+  try {
+    answer = await Console.readLineAsync("숫자를 입력해주세요 : ");
+  } catch (e) {
+    // reject 되는 경우
+  }
+  if (isValid(answer)) {
+    return readToList(answer);
+  } else {
+    throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+  }
 };
+
+// 입력된 수의 유효성을 검사하는 함수
+const isValid = (answer) => {
+  if (
+    answer.length === INPUT_LENGTH &&
+    answer >= 100 &&
+    answer <= 999 &&
+    !isDuplicated(answer)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// 유효성 검사 항목 중 중복된 숫자가 있는지 검사하는 함수
+function isDuplicated(str) {
+  const charSet = new Set();
+  for (let char of str) {
+    if (charSet.has(char)) {
+      return true; // Found a duplicate character
+    }
+    charSet.add(char);
+  }
+  return false; // No duplicate characters found
+}
 
 // 입력받은 숫자를 리스트 형식으로 반환하는 함수
 const readToList = (answer) => {
@@ -63,20 +80,21 @@ const compareNumber = (computerList, humanList) => {
   return { strikeCnt, ballCnt };
 };
 
-//
-const readRestart = () => {
-  return new Promise((resolve) => {
-    Console.readLine(
-      `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`,
-      (answer) => {
-        if (answer === "1" || answer === "2") {
-          resolve(answer);
-        } else {
-          throw new InputException("예외가 발생했습니다.");
-        }
-      }
+// 게임을 재시작할지 종료할지 입력받는 함수
+const readRestart = async () => {
+  let answer = "";
+  try {
+    answer = await Console.readLineAsync(
+      `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`
     );
-  });
+  } catch (e) {
+    // reject 되는 경우
+  }
+  if (answer === "1" || answer === "2") {
+    return answer;
+  } else {
+    throw Error("[ERROR] 숫자가 잘못된 형식입니다.");
+  }
 };
 
 // 결과를 출력하는 함수
@@ -103,28 +121,34 @@ const printResult = (strikeCnt, ballCnt) => {
 
 class App {
   async play() {
-    let isStop = false;
-
     let success = false;
 
     Console.print("숫자 야구 게임을 시작합니다.");
 
     do {
       const computerList = getComputerNumber();
+      let humanList = [];
 
       // 3스트라이크에 성공하지 못하면 성공할 때 까지 루프한다.
       while (!success) {
-        const humanList = await readFromPlayer();
+        try {
+          humanList = await readFromPlayer();
+        } catch (e) {
+          throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+        }
         const { strikeCnt, ballCnt } = compareNumber(computerList, humanList);
         success = printResult(strikeCnt, ballCnt);
       }
-
-      const restart = await readRestart();
-      if (restart === "1") {
-        success = false;
-      } else if (restart === "2") {
-        isStop = true;
-        break;
+      try {
+        const restart = await readRestart();
+        if (restart === "1") {
+          success = false;
+        } else if (restart === "2") {
+          return;
+        }
+      } catch (e) {
+        // Console.print(e);
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
       }
     } while (true);
   }
