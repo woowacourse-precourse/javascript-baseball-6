44cf927584be51f5bcf8c6856f2c85439f111d4e
KYHH <kyhh93@naver.com>
2023-10-23 14:41:17 +0900

    feat : User 클래스의 사용자 입력기능

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  play() {
+  async play() {
+    console.log(`숫자 야구 게임을 시작합니다.`);
     const computer = new Computer(); // Computer 클래스의 인스턴스를 생성
     computer.createNumbers(); // Computer 클래스의 createNumbers() 메서드를 호출해서 app.play()로 쓸 수 있게 만든다.
+
+    try {
+      const user = new User();
+      await user.createInputs();
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
 
@@ -28,5 +36,33 @@ class Computer {
   }
 }
 
+class User {
+  constructor() { }
+
+  async createInputs() {
+    const userInput = [];
+
+    try {
+      const input = await MissionUtils.Console.readLineAsync(`숫자를 입력해주세요 : `);
+      userInput.push(parseInt(input));
+      console.log(userInput);
+
+      // parseInt() 함수는 주어진 문자열을 정수로 변환할 수 있을 때까지 반환을 시도하고, 변환이 불가능한 경우 NaN을 반환한다
+      // 따라서 parseInt() 함수를 사용하면 문자나 숫자 상관없이 항상 NaN이 아닌 값을 반환한다
+      // 사용자가 숫자로 입력하더라도 parseInt()함수로 변환할 때 숫자로 변환될 수 없는 부분까지 변환을 시도하면, 결과적으로 NaN이 된다.
+      // 이 경우에 Number.isNaN()을 사용하면 true가 반환되기에 문자를 넣어도 에러가 발생하지 않고 그냥 출력된거 같다.
+      // 따라서 isNaN을 사용하여 사용자의 입력이 숫자인지만 확인한다.
+      if (isNaN(input)) {
+        throw new Error(`숫자를 입력하셔야 합니다. 프로그램이 종료 됩니다.`);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+
 const app = new App();
-app.play();
+app.play().catch(error => {
+  console.error(error);
+});
