fc3c5eac1ccf074718d69457b887c88737857e19
kimjinwoo <rlawlsdn177@naver.com>
2023-10-23 23:36:33 +0900

    fix: 모듈화 오류 수정 (비동기 동작 오류)

--- src/Input.js
+++ src/Input.js
@@ -1,12 +1,10 @@
 import { Random } from '@woowacourse/mission-utils';
 
-const GENERATE_RANDOM_NUMBER = () => Random.pickNumberInRange(1, 9);
-
 export const CREATE_RANDOM_NUMBER = () => {
   const NUMBERS = new Set();
 
   while (NUMBERS.size < 3) {
-    NUMBERS.add(GENERATE_RANDOM_NUMBER);
+    NUMBERS.add(Random.pickNumberInRange(1, 9));
   }
 
   return [...NUMBERS];
