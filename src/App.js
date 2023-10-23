4a08a5e14c2433ac09f47425751083ef88483b4c
kwani6684 <jack0910@naver.com>
2023-10-23 09:51:24 +0900

    ✨ 재시작/종료 구분 #4 프로그램 재시작/종료

--- src/App.js
+++ src/App.js
@@ -34,10 +34,18 @@ class User {
 
 class App {
   async play() {
-    
-}
+    console.log("숫자 야구 게임을 시작합니다.");
+    let newGame = 1;
+    while (newGame % 2) {
+      //1일때 실행, 2일때 종료
+      let com = new Com();
+      let user = new User();
+      await user.getNumber();
+      newGame =await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    }
+  }
 }
 const app = new App();
-// await app.play();
+await app.play();
 
 export default App;
