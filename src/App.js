8a5f17a5485e376a77d332f44efdbc534ff5db54
useon <yoosk4986@naver.com>
2023-10-23 17:15:44 +0900

    feat: 입력값에 따라 게임을 다시 시작하거나, 종료하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,26 +2,8 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
-    let judgeResult = '';
     this.infoPrint('숫자 야구 게임을 시작합니다.');
-    const computerNumber = this.computerPick();
-    while (judgeResult !== '3스트라이크') {
-      const userPickValue = await this.userInput('숫자를 입력해주세요 : ');
-      const userPickValidationResult = this.userPickValidation(userPickValue);
-      if (!userPickValidationResult)
-        this.throwError('[ERROR] 서로 다른 숫자 3개를 입력해주세요.');
-      const userNumber = userPickValue.split('').map((element) => +element);
-      judgeResult = this.judge(computerNumber, userNumber);
-      this.infoPrint(judgeResult);
-    }
-    this.infoPrint('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-    const userRestartValue = await this.userInput(
-      `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`,
-    );
-    const userRestartValidationResult =
-      this.userRestartValidation(userRestartValue);
-    if (!userRestartValidationResult)
-      this.throwError('[ERROR] 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+    this.game();
   }
 
   infoPrint(message) {
@@ -90,6 +72,30 @@ class App {
     if (ball > 0 && strike === 0) return `${ball}볼`;
     if (strike > 0 && ball === 0) return `${strike}스트라이크`;
     if (ball > 0 && strike > 0) return `${ball}볼 ${strike}스트라이크`;
+  }
+
+  async game() {
+    let judgeResult = '';
+    const computerNumber = this.computerPick();
+    while (judgeResult !== '3스트라이크') {
+      const userPickValue = await this.userInput('숫자를 입력해주세요 : ');
+      const userPickValidationResult = this.userPickValidation(userPickValue);
+      if (!userPickValidationResult)
+        this.throwError('[ERROR] 서로 다른 숫자 3개를 입력해주세요.');
+      const userNumber = userPickValue.split('').map((element) => +element);
+      judgeResult = this.judge(computerNumber, userNumber);
+      this.infoPrint(judgeResult);
+    }
+    this.infoPrint('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    const userRestartValue = await this.userInput(
+      `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`,
+    );
+    const userRestartValidationResult =
+      this.userRestartValidation(userRestartValue);
+    if (!userRestartValidationResult)
+      this.throwError('[ERROR] 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+    if (userRestartValue === '1') this.game();
+    if (userRestartValue === '2') return;
   }
 }
 
