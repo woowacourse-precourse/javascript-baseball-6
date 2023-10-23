e338d8fe03873313e3d7636acaa0f4c71d750cfe
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-10-23 22:03:50 +0900

    feat: 메시지를 주입받아 렌더링하고 입력받은 값을 반환한다

--- src/View/View.js
+++ src/View/View.js
@@ -1,6 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
 
 export const View = {
+  async readLineAsync(message) {
+    return await Console.readLineAsync(message);
+  },
+
   print(message) {
     Console.print(message);
   },
