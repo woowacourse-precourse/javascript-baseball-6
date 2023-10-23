4dbd54ddd4e393124934347602510ddf69b3a73f
Youmi Kim <flordelavidaa@naver.com>
2023-10-23 23:33:24 +0900

    Docs: 기능 목록 정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+
+    const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+    Console.print(randomNumber);
+
+  }
 }
 
 export default App;
+
+
+
+const app = new App();
+app.play();
