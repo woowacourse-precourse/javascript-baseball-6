61eae43f2c6b0d254d63d190d1d39c10d7730ff3
YOON-CC <whdbscks77@gmail.com>
2023-10-23 23:35:00 +0900

    feat(Controller.js): 게임 시작 관리 메서드 gameStart 구현

---
+++ src/controller/Controller.js
@@ -0,0 +1,15 @@
+import Model from "../model/Model.js";
+import View from "../view/View.js";
+
+class Controller {
+    constructor() {
+        this.model = new Model();
+        this.view = new View();
+    }
+
+    async gameStart() {
+        
+    }
+}
+
+export default Controller;
