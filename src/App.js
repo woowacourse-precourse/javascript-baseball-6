5f0421cd4192088c7ebe50b8a2f34851ca2e6194
HarenKei <seungmin1266@gmail.com>
2023-10-24 01:37:17 +0900

    Refactor: 사용자 입력 범위 판별 방법 변경

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ class App {
     }
 
     async function getInput() {
-      const USER_INPUT = [...await Console.readLineAsync("숫자를 입력해주세요 : ")].map(element => parseInt(element));
+      const USER_INPUT = [
+        ...(await Console.readLineAsync("숫자를 입력해주세요 : ")),
+      ].map((element) => parseInt(element));
 
       try {
         chkUserInput(USER_INPUT);
@@ -33,12 +35,15 @@ class App {
 
       //1. 서로 다른 세 자리의 자연수로 이루어져 있을 것.
       //2. 1 ~ 9 사이의 수로 이루어져 있을 것.
-      if(new Set(userInput).size === userInput.length && userInput.length === 3) {
-        userInput.forEach(element => {
-          if(element < 1 || element > 9 || isNaN(element)) {
+      if (
+        new Set(userInput).size === userInput.length &&
+        userInput.length === 3
+      ) {
+        userInput.forEach((element) => {
+          if (element === 0 || isNaN(element)) {
             throw new Error(ERROR_MESSAGE);
           }
-        })
+        });
       } else {
         throw new Error(ERROR_MESSAGE);
       }
@@ -66,6 +71,7 @@ class App {
       let ball = 0;
       let answer = "";
       const USER_INPUT = await getInput();
+      const COUNT_LIMIT = 3;
 
       if (USER_INPUT) {
         computer.forEach((number, index) => {
@@ -77,22 +83,26 @@ class App {
         });
 
         if (strike > 0 || ball > 0) {
-          if (strike === 3) {
+          //strike와 ball 둘 중 하나라도 존재할 때의 처리.
+          if (strike === COUNT_LIMIT) {
             answer = `${strike}스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`;
             Console.print(answer);
             return false;
           } else if (strike === 0) {
+            //볼만 존재할 때의 처리
             answer = `${ball}볼`;
-            Console.print(answer);
+          } else if (ball === 0) {
+            //스트라이크만 존재할 때의 처리
+            answer = `${strike}스트라이크`;
           } else {
+            //볼과 스트라이크 모두 존재할 때의 처리
             answer = `${ball}볼 ${strike}스트라이크`;
-            Console.print(answer);
           }
         } else {
           answer = "낫싱";
-          Console.print(answer);
         }
 
+        Console.print(answer);
         return true;
       } else {
         throw new Error(ERROR_MESSAGE);
@@ -101,7 +111,7 @@ class App {
 
     async function gameStart() {
       const COMPUTER = makeAnswer();
-      Console.print(COMPUTER);
+      // Console.print(COMPUTER);
 
       while (true) {
         if (!(await chkValue(COMPUTER))) {
@@ -109,8 +119,8 @@ class App {
         }
       }
       try {
-        return await newGameChk() ? await gameStart() : 0;
-      } catch(error){
+        return (await newGameChk()) ? await gameStart() : 0;
+      } catch (error) {
         Console.print(error);
       }
     }
