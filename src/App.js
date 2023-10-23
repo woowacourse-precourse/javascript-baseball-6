5dc70fcfb02c69b80c6c6f238aa39c538de514d6
sehyun <e6d1fe@gmail.com>
2023-10-23 23:10:55 +0900

    feat: play 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
 }
 
+// console.log(MissionUtils.Random.pickNumberInRange(1, 9));
+
+const app = new App();
+app.play();
 export default App;
