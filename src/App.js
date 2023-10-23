0296e27aa856d02d61ea19a27c9f26a338004dd3
Jinjeon Shin <sjg729729@gmail.com>
2023-10-23 23:54:39 +0900

    feat: 게임 시작 메세지 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const Message = {
+  START: '숫자 야구 게임을 시작합니다.',
+};
+
 class App {
-  async play() {}
+  printStartMessage() {
+    MissionUtils.Console.print(Message.START);
+  }
+
+  async play() {
+    this.printStartMessage();
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
