20abd2d509b5ae0676235e935771f7671c379f68
limdumb <dlaruddls@nate.com>
2023-10-23 15:18:12 +0900

    [MISC] 컨벤션 수정

--- src/baseBallGame.js
+++ src/baseBallGame.js
@@ -6,16 +6,17 @@ import restartChecked from "./function/restartChecked.js";
 
 const baseBallGame = async (computerValue) => {
   const userValue = await userInput();
-  const userInputArr = userValue.split("").map((el) => {
-    return Number(el);
-  });
+  const userInputArr = userValue.split('').map((el) => Number(el));
+
   let strike = 0;
   let ball = 0;
 
   computerValue.forEach((el, index) => {
     if (el === userInputArr[index]) {
       strike += 1;
-    } else if (el !== userInputArr[index] && userInputArr.indexOf(el) !== -1) {
+    }
+
+    if (el !== userInputArr[index] && userInputArr.indexOf(el) !== -1) {
       ball += 1;
     }
   });
@@ -26,17 +27,16 @@ const baseBallGame = async (computerValue) => {
     return baseBallGame(computerValue);
   }
 
-  Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-  Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+  Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+  Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
 
   const reStartValue = await restartChecked();
-  Console.print(reStartValue);
 
-  if (reStartValue === "1") {
+  if (reStartValue === '1') {
     return baseBallGame(createRandomNumber());
   }
 
-  Console.print("게임 종료");
+  Console.print('게임 종료');
 };
 
 export default baseBallGame;
