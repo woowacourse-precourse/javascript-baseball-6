aed700aa6d30ac89854fa726a488103374ce7644
Myomyoring <narcissuscat@naver.com>
2023-10-23 23:24:00 +0900

    [feat] 게임이 끝난 경우 재시작/종료 입력 값 받기

--- src/App.js
+++ src/App.js
@@ -55,6 +55,13 @@ class App {
           Console.print(`${strike}스트라이크`);
           if (strike === 3) {
             Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+            const regameInput = await Console.readLineAsync(
+              '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: '
+            );
+            if (regameInput === '1') {
+            } else if (regameInput === '2') {
+            } else {
+            }
           }
         } else if (strike === 0) {
           Console.print(`${ball}볼`);
