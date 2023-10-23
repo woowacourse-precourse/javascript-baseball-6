2578e9b8b4576b2994759073c163bc07b0793249
Myomyoring <narcissuscat@naver.com>
2023-10-23 19:42:38 +0900

    [feat] 입력 값 문구 출력 및 에러 문구 출력

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    try {
+      const inputNumber = await Console.readLineAsync(
+        "0을 제외한 서로 다른 숫자를 3자리 입력해주세요: "
+      );
+    } catch (error) {
+      console.log("[ERROR]", error.message);
+    }
   }
 }
 
