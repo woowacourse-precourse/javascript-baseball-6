936157035a3c1171394314bee35383632c63a6cf
JungHun98 <j7papa@naver.com>
2023-10-23 23:56:32 +0900

    refactor: 상수 모듈 수정 모든 상수를 담은 객체로 반환, airbnb style guide에 맞춰 파일 이름 변경

---
+++ src/modules/Constant.js
@@ -0,0 +1,29 @@
+// const numbers
+const MAX_NUM_LENGTH = 3;
+const EXIT = '1';
+const RESTART = '2';
+
+// result
+const STRIKE = '스트라이크';
+const BALL = '볼';
+const NOTHING = '낫싱';
+
+// prompt
+const START_PROMPT = '숫자 야구 게임을 시작합니다.';
+const INPUT_PROMPT = '숫자를 입력해주세요 : ';
+const RESTART_PROMPT = `${MAX_NUM_LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료
+게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`;
+
+const Constant = {
+  MAX_NUM_LENGTH,
+  EXIT,
+  RESTART,
+  STRIKE,
+  BALL,
+  NOTHING,
+  START_PROMPT,
+  INPUT_PROMPT,
+  RESTART_PROMPT,
+};
+
+export default Constant;
