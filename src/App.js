b90adc8ba260ba5227d42ac06ebbdd165c1e9d96
hyun812 <sh980812@naver.com>
2023-10-23 20:27:35 +0900

    [feat] 서로 다른 세자리수 랜덤 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import target from './getComputerNum.js';
+
 class App {
-  async play() {}
+  async play() {
+    Console.print(target);
+  }
 }
 
 export default App;
---
+++ src/getComputerNum.js
@@ -0,0 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const computer = [];
+while (computer.length < 3) {
+  const number = MissionUtils.Random.pickNumberInRange(1, 9);
+  if (!computer.includes(number)) {
+    computer.push(number);
+  }
+}
+
+export default computer;
