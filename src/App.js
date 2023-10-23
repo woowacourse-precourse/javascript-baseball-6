c19835a9e0e6c811d8c3e0b393213f89507b9894
최기환 <rlghks3004@gmail.com>
2023-10-23 10:41:56 +0900

    기능 단위로 컴밋하자...리팩토링 진행(논리 분해, 함수명 변경)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   computer = [];
@@ -8,7 +8,7 @@ class App {
   keepPlaying = true;
 
   async play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     this.initializeComputerNumber();
     while (this.keepPlaying) {
       const userNumber = await this.getUserNumber();
@@ -31,38 +31,50 @@ class App {
 
   async getUserNumber() {
     const number = await MissionUtils.Console.readLineAsync(
-      "숫자를 입력해주세요 : "
+      '숫자를 입력해주세요 : '
     );
     return number;
   }
 
-  validateUserNumber(number) {
+  validateUserNumber(input) {
     try {
-      const value = Number(number);
+      this.checkValueIsNumber(input)
 
-      if (typeof value !== "number" || isNaN(value)) {
-        throw new Error();
-      }
-      const arr = this.formattingUserNumber(value);
+      const arr = this.formattingUserNumber(input);
 
-      if (arr.length !== 3) {
-        throw new Error();
-      }
+      this.checkIsLengthThree(arr);
+
       const set = new Set([...arr]);
 
-      if (set.size !== 3) {
-        throw new Error();
-      }
+      this.checkHasDuplicateNumber(set);
 
       return arr;
     } catch (error) {
-      throw new Error("[ERROR] 올바른 형식을 입력해 주세요.");
+      throw new Error('[ERROR] 올바른 형식을 입력해 주세요.');
     }
   }
 
-  formattingUserNumber(number) {
-    const stringifiedNumber = number + "";
-    const targetArr = stringifiedNumber.split("");
+  checkIsLengthThree(arr) {
+    if (arr.length !== 3) {
+      throw new Error();
+    }
+  }
+
+  checkHasDuplicateNumber(set) {
+    if(set.size !== 3) {
+      throw new Error();
+    }
+  }
+
+  checkValueIsNumber(input) {
+    const value = Number(input);
+    if (typeof value !== 'number' || isNaN(value)) {
+      throw new Error();
+    }
+  }
+
+  formattingUserNumber(input) {
+    const targetArr = input.split('');
     return targetArr;
   }
 
@@ -83,8 +95,8 @@ class App {
 
   compareUserComputerNumber() {
     this.user.forEach((element, index) => {
-      const result = this.computer.includes(element);
-      if (result) {
+      const isIncluded = this.computer.includes(element);
+      if (isIncluded) {
         this.checkIsStrike(index, element)
           ? this.strikeNumber++
           : this.ballNumber++;
@@ -101,42 +113,52 @@ class App {
   }
 
   printCompareResult() {
-    const ballResultOutput =
-      this.ballNumber === 0 ? "" : `${this.ballNumber}볼 `;
-
-    const strikeResultOutput =
-      this.strikeNumber === 0 ? "" : `${this.strikeNumber}스트라이크`;
-
-    if (ballResultOutput === "" && strikeResultOutput === "") {
-      MissionUtils.Console.print("낫싱");
+    if (this.isNothing()) {
+      this.printNothing();
       return;
     }
+    MissionUtils.Console.print(
+      this.getBallResultString() + this.getStrikeResultString()
+    );
+  }
 
-    MissionUtils.Console.print(ballResultOutput + strikeResultOutput);
-    return;
+  getBallResultString() {
+    return this.ballNumber === 0 ? '' : `${this.ballNumber}볼 `;
+  }
+
+  getStrikeResultString() {
+    return this.strikeNumber === 0 ? '' : `${this.strikeNumber}스트라이크`;
+  }
+
+  printNothing() {
+    MissionUtils.Console.print('낫싱');
+  }
+
+  isNothing() {
+    return this.ballNumber === 0 && this.strikeNumber === 0;
   }
 
   async answerKeepGoingOrOver() {
-    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
     MissionUtils.Console.print(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
     );
-    const number = await MissionUtils.Console.readLineAsync("");
+    const number = await MissionUtils.Console.readLineAsync('');
     return number;
   }
 
   setGameState(number) {
     switch (number) {
-      case "2":
+      case '2':
         this.keepPlaying = false;
         break;
-      case "1":
+      case '1':
         this.computer = [];
         this.initUserAndCount();
         this.initializeComputerNumber();
         break;
       default:
-        throw new Error("[ERROR] 올바르지 않은 입력입니다.");
+        throw new Error('[ERROR] 올바르지 않은 입력입니다.');
     }
   }
 
