fd5527cff5be1c5314c27337e819c3f54b006b64
Jiwon Oh <64193469+jiwon-o@users.noreply.github.com>
2023-10-24 01:01:46 +0900

    feat: 사용자의 입력값에 대한 예외처리 추가(숫자가 아닌 문자를 입력하는 경우, 중복되는 숫자가 있을 경우, 3자리의 숫자가 아닐 경우)

--- src/App.js
+++ src/App.js
@@ -11,9 +11,13 @@ class App {
     this.startGame();
   }
 
-  startGame() {
+  async startGame() {
     this.generateRandomNumbers();
-    this.getUserInput();
+    try {
+      await this.getUserInput();
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 
   generateRandomNumbers() {
@@ -29,7 +33,46 @@ class App {
     const userInput = await MissionUtils.Console.readLineAsync(
       "서로 다른 3자리의 숫자를 입력해주세요 : "
     );
+    this.validateUserInput(userInput);
     this.userInput = userInput;
+  }
+
+  validateUserInput(userInput) {
+    if (!this.checkUserInputLength(userInput)) {
+      throw new Error(
+        `[ERROR] ${userInput.length}자리의 숫자를 입력했습니다. 서로 다른 3자리 숫자를 입력해주세요.`
+      );
+    }
+    if (!this.checkUserInputIsNumbers(userInput)) {
+      throw new Error("[ERROR] 숫자가 아닌 문자가 포함되어 있습니다.");
+    }
+    if (!this.checkUserInputIsDiff(userInput)) {
+      throw new Error("[ERROR] 입력한 숫자 중에 중복된 숫자가 있습니다.");
+    }
+  }
+
+  checkUserInputLength(input) {
+    if (input.length !== 3) {
+      return false;
+    }
+    return true;
+  }
+
+  checkUserInputIsNumbers(input) {
+    for (let i = 0; i < 3; i++) {
+      if (isNaN(Number(input[i]))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  checkUserInputIsDiff(input) {
+    let uniqueNumber = new Set(input);
+    if (uniqueNumber.size !== input.length) {
+      return false;
+    }
+    return true;
   }
 }
 
