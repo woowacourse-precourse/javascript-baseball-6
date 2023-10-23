589fec2e86130cd18b494c5da0975ceca592149f
Blue-Kite <bluelemon112@gmail.com>
2023-10-23 19:06:58 +0900

    feat: 랜덤숫자 생성 기능 추가

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -3,9 +3,31 @@ import { Message } from "./constants/Message.js";
 
 export default class BaseballGame {
   start() {
+    const answer = '';
     Console.print(Message.INIT);
+    Console.readLine(Message.INPUT, (answer) => {
+      
+    }); 
   }
+
+  removeDuplicate(num, nums) {
+    if (!nums.includes(num)){
+      nums.push(num);
+    }
+    return nums;
+  }
+  makeCoumputerNum() {
+    const nums = []
+    while (nums.length < 3){
+      const num = Random.pickNumberInRange(1, 9);
+      this.removeDuplicate(num, nums);
+    }
+    const computerNum = nums.join('');
+    return computerNum;
+  }
+
   play() {
     this.start();
+    this.makeCoumputerNum();
   }
 }
