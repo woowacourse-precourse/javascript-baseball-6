7f1d68c0d9907a00c577284d885c258854add44c
HarenKei <seungmin1266@gmail.com>
2023-10-23 22:54:41 +0900

    Fix: 테스트 통과

--- src/App.js
+++ src/App.js
@@ -2,63 +2,114 @@ import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    
     function makeAnswer() {
-      const computer = new Set();
-      while(computer.size < 3) {
-        computer.add(Random.pickNumberInRange(1, 9));
+      const computer = [];
+      while (computer.length < 3) {
+        const number = Random.pickNumberInRange(1, 9);
+        if (!computer.includes(number)) {
+          computer.push(number);
+        }
       }
-
-      const computerInput = [...computer];
-      return computerInput;
+      return computer;
     }
 
-    function getInput() {
-      let input = Console.readLineAsync("숫자를 입력해주세요 : ");
-      
-      input.then((message) => {
-        const user = [...message].map(item => parseInt(item));
-        Console.print(typeof(user));
-        if(chkUserInput(user)) {
-          Console.print("조건 통과함");
-          chkValue(user, computer);
-        }
-      })
+    async function getInput() {
+      const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
+      const user = [...input].map((item) => parseInt(item));
+
+      try {
+        chkUserInput(user);
+        return user;
+      } catch (e) {
+        Console.print(e);
+      }
     }
 
     function chkUserInput(userInput) {
       //파라미터로 들어오는 배열의 값을 체크할것.
       //true / false 반환할것
-
-      if(userInput.length < 3 || userInput.length > 3) {
-        Console.print("되겠냐?");
-        return false;
+      if (userInput.length < 3 || userInput.length > 3) {
+        throw new Error("[ERROR]");
       } else {
-        for(let value of userInput) {
-          if(value < 1 || value > 10 || isNaN(value)) {
-            Console.print("하???");
-            return false;
+        for (let value of userInput) {
+          if (value < 1 || value > 10 || isNaN(value)) {
+            throw new Error("[ERROR]");
           }
         }
       }
-
       return true;
     }
 
-    function chkValue(userInput, computer) {
-      //chkUserInput의 값이 true라면 두 배열을 비교하여 결과를 출력할것.
-      Console.print(`user : ${userInput}, ${typeof(user)}`);
-      Console.print(`computer : ${computer}, ${typeof(computer)}`);
+    async function newGameChk() {
+      const chkNewGame = await Console.readLineAsync(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+      );
 
-      userInput.forEach((element, index) => {
-        Console.print(`element: ${element}, index: ${index}, type: ${typeof(element)}`);
-        Console.print(`computer ele: ${computer[index]}, type: ${typeof(computer[index])}`);
-      });
+      if (chkNewGame === "1") {
+        return true;
+      } else {
+        return false;
+      }
     }
 
-    const computer = makeAnswer();
+    async function chkValue(computer) {
+      //chkUserInput의 값이 true라면 두 배열을 비교하여 결과를 출력할것.
+      let strike = 0;
+      let ball = 0;
+      let answer = "";
+      let userInput = await getInput();
+
+      if (userInput) {
+        computer.forEach((number, index) => {
+          if (userInput.indexOf(number) === index) {
+            strike++;
+          } else if (userInput.includes(number)) {
+            ball++;
+          }
+        });
+
+        if (strike > 0 || ball > 0) {
+          if (strike === 3) {
+            answer = `${strike}스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`;
+            Console.print(answer);
+            return false;
+          } else if (strike === 0) {
+            answer = `${ball}볼`;
+            Console.print(answer);
+          } else {
+            answer = `${ball}볼 ${strike}스트라이크`;
+            Console.print(answer);
+          }
+        } else {
+          answer = "낫싱";
+          Console.print(answer);
+        }
+
+        return true;
+      } else {
+        throw new Error("[ERROR]");
+      }
+    }
+
+    async function gameStart() {
+      const computer = makeAnswer();
+      Console.print(computer);
+
+      while (true) {
+        if (!(await chkValue(computer))) {
+          break;
+        }
+      }
+
+      if(await newGameChk()) {
+        return await gameStart();
+      } else {
+        return 0;
+      }
+    }
+
     Console.print("숫자 야구 게임을 시작합니다.");
-    getInput();
+    await gameStart();
   }
 }
 
