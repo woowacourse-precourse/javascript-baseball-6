e51cb7c0a0d1c90691b4cd5dede21ae35722b6d2
HyunJaeyeon <bomul0109@gmail.com>
2023-10-24 02:45:10 +0900

    1차 구현

--- src/App.js
+++ src/App.js
@@ -1,29 +1,121 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+// class App {
+//   async play() {
+//     //컴퓨터 랜덤값 지정 함수
+//     function getComputer() {
+//       const computer = [];
+//       while (computer.length < 3) {
+//         const number = MissionUtils.Random.pickNumberInRange(1, 9);
+//         if (!computer.includes(number)) {
+//           computer.push(number);
+//         }
+//       }
+//     }
+//     //user 입력값 지정 함수
+//     async function getUserInput() {
+//       try {
+//         const input = await MissionUtils.Console.readLineAsync(
+//           "숫자를 입력해주세요 : "
+//         );
+//         //resolve 되는 경우 입력값 출력
+//         MissionUtils.Console.print(input);
+//       } catch (error) {
+//         // reject 되는 경우
+//         MissionUtils.Console.print(error);
+//       }
+//     }
 
-class App {
-  async play() {
-    //컴퓨터 랜덤값 지정
-    function getComputer() {
-      const computer = [];
-      while (computer.length < 3) {
-        const number = MissionUtils.Random.pickNumberInRange(1, 9);
-        if (!computer.includes(number)) {
-          computer.push(number);
+//     function gameStart() {
+//       const computer = getComputer();
+//       let strike = 0;
+//       let ball = 0;
+
+//       //3strike 될 때까지 while문
+//       while (strike !== 3) {
+//         //user input 3자리 받기
+//         let user = [...getUserInput()].map(Number);
+//         //한자리씩 비교
+//         for (let i = 0; i < computer.length; i++) {
+//           for (let j = 0; j < user.length; j++) {
+//             //숫자 같을 때
+//             if (computer[i] === user[j]) {
+//               if (i === j) strike += 1;
+//               else if (i !== j) ball += 1;
+//             }
+//           }
+//         }
+//         //TODO: 볼/스트라이크만 출력, 낫싱 추가
+//         //출력
+//         MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+//         strike = 0;
+//         ball = 0;
+//         continue;
+//       }
+
+//       //3strike (while문 빠져나옴)
+//       print("3스트라이크");
+//       let user = getUserInput();
+//       if (user === 1) gameStart(); //재귀
+//       else if (user === 2) return 0; //함수 종료
+//     }
+//     gameStart();
+//   }
+// }
+// export default App;
+
+//테스트 코드
+
+async function gameStart() {
+  //컴퓨터 랜덤값 지정 함수
+  function getComputer() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
+  const computer = getComputer();
+  console.log(computer);
+  let strike = 0;
+  let ball = 0;
+
+  //3strike 될 때까지 while문
+  while (strike !== 3) {
+    //user input 3자리 받기
+    let x = await MissionUtils.Console.readLineAsync("입력");
+    MissionUtils.Console.print(x);
+    let user = [...x].map(Number);
+    //한자리씩 비교
+    for (let i = 0; i < computer.length; i++) {
+      for (let j = 0; j < user.length; j++) {
+        //숫자 같을 때
+        if (computer[i] === user[j]) {
+          if (i === j) strike += 1;
+          else if (i !== j) ball += 1;
         }
       }
     }
-    //user 입력값 지정
-    async function getUsername() {
-      try {
-        const username = await MissionUtils.Console.readLineAsync(
-          "숫자를 입력해주세요 : "
-        );
-      } catch (error) {
-        // reject 되는 경우
-        // console.print(error);
-      }
+    if (strike === 3) {
+      break;
     }
+    //출력
+    console.log(`${ball}볼 ${strike}스트라이크`);
+    strike = 0;
+    ball = 0;
+    continue;
   }
+
+  //3strike (while문 빠져나옴)
+  console.log("3스트라이크, 빠져나왔다");
+  let user = await MissionUtils.Console.readLineAsync("입력");
+  console.log(user);
+
+  //TODO: 재귀 실패
+  if (user === 1) gameStart(); //재귀
+  else if (user === 2) return 0; //함수 종료
 }
 
-export default App;
+gameStart();
