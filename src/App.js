6e8f140e433f9d43b5874238260071903e7623a5
KangYunJi <dbswl701@ajou.ac.kr>
2023-10-23 15:13:17 +0900

    feat: 컴퓨터는 1부터 9까지 서로 다른 임의의 수 3개를 선택한다.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
+
+  constructor() {
+    // 컴퓨터는 1부터 9까지 서로 다른 임의의 수 3개를 선택한다.
+    this.computer = [];
+    while (this.computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.computer.includes(number)) {
+        this.computer.push(number);
+      }
+    }
+  }
+
   async play() {}
 }
 
