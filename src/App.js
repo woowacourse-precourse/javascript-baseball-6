2374f59298db29c489aa0edce061af1376b9cbeb
lord-potato <lordpotato1227@gmail.com>
2023-10-23 11:47:05 +0900

    feat : 정상 입력 범위에서의 게임 동작 구현

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ class App {
   async play() {
     // 게임 시작과 동시에 랜덤 숫자 (정답)을 생성합니다.
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    const RAND_ANSWER = func.createRandomNumber();
-    MissionUtils.Console.print(RAND_ANSWER);
+    let RAND_ANSWER = func.createRandomNumber();
+    MissionUtils.Console.print("<랜덤정답값>" + RAND_ANSWER);
+    let RESTART = false;
 
     while (true) {
       //3스트라이크, 혹은 오류가 발생할때까지 계속해서 입력값을 받습니다.
+      if (RESTART) {
+        RAND_ANSWER = func.createRandomNumber();
+        MissionUtils.Console.print("<랜덤정답값>" + RAND_ANSWER);
+        RESTART = false;
+      }
+
       let USER_ANSWER = [...(await func.getUserNumber())];
       USER_ANSWER = USER_ANSWER.map(Number);
-      MissionUtils.Console.print(USER_ANSWER);
+      MissionUtils.Console.print("<유저입력값>" + USER_ANSWER);
 
       let STRIKE = func.strikeCheck(RAND_ANSWER, USER_ANSWER);
       let BALL = func.ballCheck(RAND_ANSWER, USER_ANSWER);
@@ -30,11 +37,20 @@ class App {
 
       if (STRIKE === 3) {
         MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        break;
+        let USERCHOICE = await func.getUserChoice();
+        console.log(USERCHOICE);
+        if (USERCHOICE == 1) {
+          RESTART = true;
+          continue;
+        } else if (USERCHOICE == 2) {
+          MissionUtils.Console.print("숫자 야구 게임을 종료합니다.");
+          break;
+        }
       }
     }
   }
 }
+
 const app = new App();
 app.play();
 
--- src/Functions.js
+++ src/Functions.js
@@ -25,7 +25,7 @@ export async function getUserNumber() {
 export async function getUserChoice() {
   try {
     const USERCHOICE = await MissionUtils.Console.readLineAsync(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
     );
     return USERCHOICE;
   } catch (error) {
