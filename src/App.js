68f957af6f8eaef635049e960e5dc7892b5b9fd8
soyunim <togksk123@naver.com>
2023-10-24 08:55:54 +0900

    move: 코드 이동(Baseball -> App)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import { Baseball } from "./Baseball.js";
+import { Console } from "@woowacourse/mission-utils";
+import { Random } from "@woowacourse/mission-utils";
+import { Messages } from "./Messages";
 
+const computers = [];
 class App {
   play() {
-    const game = new Baseball();
-    game.gameStart();
+    // 0. 게임 시작 메세지 출력
+    Console.print(Messages.START);
+
+    // 1. 컴퓨터의 숫자 랜덤입력
+    while (computers.length < 3) {
+      let number = Random.pickNumberInRange(1, 9);
+      if (!computers.includes(number)) {
+        computers.push(number);
+      }
+    }
+    const COMPUTER = computers.join("");
+    Console.print(COMPUTER);
   }
 }
+
+const app = new App();
+app.play();
 
 export default App;
