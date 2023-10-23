12741cb7406fd10311c1e628a5ad09395e61f4ea
DingX2 <pourding@naver.com>
2023-10-23 14:33:02 +0900

    [FEAT] : 스켈레톤 코드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    console.log("시작");
+
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
