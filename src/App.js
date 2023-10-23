17e443ff873e5f24fcaba25deec05a0ed149f291
YOON-CC <whdbscks77@gmail.com>
2023-10-23 23:51:29 +0900

    feat(App.js): App.js에 View.js 인스턴스 생성 및 showInitialMessage 메서드 호출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import Controller from "./controller/Controller.js";
+import View from "./view/View.js";
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+
+  constructor() {
+    this.view = new View();
+    this.view.showInitialMessage();
+  }
+
+  async play() {
+    //Controller.js 메서드 호출
+  }
+
+
 }
 
 export default App;
+
+const app = new App();
+app.play();
