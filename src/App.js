0578760897ddf5987a141b3b52a8cc21b2ee1eb1
Hyebin Lee <qnd1215@naver.com>
2023-10-23 16:25:51 +0900

    not yet

--- src/App.js
+++ src/App.js
@@ -1,71 +1,127 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
-  async play() { // play함수이름.
-
-  }
-}
-
-export default App; // export 기본내보내기(장점. 가져올 때 다른이름으로 바꿔도 상관 ㄴ)
-
-function makeRandomNum() { // 정답 숫자 생성
-  let answer = ''; // 여기 랜덤 돌린거 넣을거임.
-  while(answer.size < 3) {
-    let pick = MissionUtils.Random.pickNumberInRange(1, 9); // 라이브러리 써서 범위 내에서 수 뽑기.
-    if(!answer.includes(pick)){ // 중복방지
-      answer += pick;
+  async play() {
+    // [직접 테스트용] 상대방의 수 직접 입력
+    let answer = this.makeRandomNum();
+    Console.print('random number : ' + answer);
+    const userInput = await Console.readLineAsync('computerInput: ');
+    
+    if(this.checkUserInput(userInput)) {
+      let strike = this.scoreInString(answer, userInput);
+      this.checkAnswer(strike);
     }
+    
+    // 이 아래부턴 함수들
   }
-  //console.log(`출제완료. 게임시작`);
-  print(`숫자 야구 게임을 시작합니다.`);
-  return answer; 
-}
-
-function getUserNumber(userInput) {
-  MissionUtils.console.readLineAsync(userInput);
-  return userInput;
-  //이걸로 입력값 받고 출력하고.. 문자인지 판단 해 주나? 근데 input이랑 매개변수랑 같은거임? 일단 패스.
-  //출력값은 뭘로 잡아야할지 모르겠음. 일단 여기까지.
-  //여기는 하나씩만 입력받을 수 있음 input메서드가 반환됨. 이 값을 가지고 조건 판단 해야 함.
-  //userInput은 지금 string형태
-}
-
-//유효성 확인 결과가 true/false로 나와야 함. 예외상황발생히 throw하고 종료해야하니까
-function checkUserInput(userInput) {
-  let input = userInput; // string형태
-
-  //숫자길이 3인지 확인
-  if (!input.length === 3) {
-    return false;
-  } 
   
-  //모두 숫자인지 확인
-  for(let i =0; i< 3; i++) {
-    if(Number.isNaN(Number(input[i]))) { // 안에 문자 있으면
-      return false;
+  makeRandomNum() { // 정답 숫자 생성
+    let answer = ''; // 여기 랜덤 돌린거 넣을거임.
+    while(answer.length < 3) {
+      let pick = MissionUtils.Random.pickNumberInRange(1, 9); // 라이브러리 써서 범위 내에서 수 뽑기.
+      if(!answer.includes(pick)){ // 중복방지
+        answer += pick;
+      }
+    }
+    Console.print(`숫자 야구 게임을 시작합니다.`);
+    return answer; 
+  }
+
+  checkUserInput(userInput) {
+    let input = userInput; // string형태
+  
+    //숫자길이 3인지 확인
+    if (!input.length === 3) {
+      throw "[ERROR] 숫자가 잘못된 형식입니다.";
+    } 
+    //모두 숫자인지 확인
+    for(let i =0; i< 3; i++) {
+      if(Number.isNaN(Number(input[i]))) { // 안에 문자 있으면
+        throw "[ERROR] 숫자가 잘못된 형식입니다.";
+      }
+    }
+    //1~9 사이 수인지
+    for(let i =0; i< 3; i++) {
+      if(Number(input[i]) < 1 || 9 < (Number(input[i]))) { 
+        throw "[ERROR] 숫자가 잘못된 형식입니다.";
+      }
+    }
+    //모두 다른 수인지 Set은 중복을 허용하지 않는다는 점을 이용
+    let checkDupl = new Set(input);
+    if(checkDupl.size !== input.length) { // 길이가 다르면 중복 존재
+      throw "[ERROR] 숫자가 잘못된 형식입니다.";
+    }
+    // 모든 조건을 지나오면 true값 반환.
+    return true;
+  }
+
+  scoreInarry(comsAnswer, userInput) {
+    let score = [0, 0]; // score[0] = strike, score[1] = ball
+  
+    for (let i = 0; i < 3; i++) {
+      if(comsAnswer[i] === userInput[i]) {
+        score[0] += 1; // strike
+      }
+      else if(comsAnswer.includes(userInput[i])) {
+        score[1] += 1; // ball
+      }
+    }
+    return score;
+  }
+
+  scoreInString(comsAnswer, userInput) {
+    const score = this.scoreInarry(comsAnswer, userInput); // score은 배열형태임
+    let answer = "";
+  
+    if((score[0] === 0) && (score[1] === 0)) { // 노 스트라이크, 노 볼
+      answer = Console.print(`낫싱`);
+    }
+    else if((score[0] > 0) && (score[1] === 0)) { // 예스 스트라이크, 노 볼
+      answer = Console.print(`${score[0]}스트라이크`);
+    }
+    else if((score[0] === 0) && (score[1] > 0)) { // 노 스트라이크, 예스 볼
+      answer = Console.print(`${score[1]}볼`);
+    }
+    else {
+      answer = Console.print(`${score[1]}볼 ${score[0]}스트라이크`);
+    }
+  
+    return answer;
+  }
+
+  checkAnswer(answer) { //scoreInString결과값을 매개변수로 받아야 함. (템플릿 문자열 써서 문자열)
+    if(answer === "3스트라이크") {
+     return Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n") + this.startNewgame();
+    }
+    else { // getUserNumber부터 
+      let userInput = this.getUserNumber(userInput);
+      //유효성검사
+      if(this.checkUserInput(userInput)) { //여기서 숫자 기준 안 맞는건 나가리 
+        let strike = this.scoreInString(answer, userInput); 
+        this.checkAnswer(strike); //자기함순데 이게 되나?
+      }
+    }
+  } 
+
+  startNewgame() {
+    Console.print(`게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`);
+    let reInput = Console.readLineAsync(); 
+  
+    if(Number(reInput) === 1) {
+      new App(); // 이 클래스 전체 다시실행
+    }
+    if(Number(reInput) === 2) {
+      process.exit(1) //일단 이걸 쓰자.
+      //완전히 종료. 단, process.exit()사용 불가. 그럼 어케함..? 노션에 시그널 쓰자.
     }
   }
-  //return true;
-
-  //1~9 사이 수인지
-  for(let i =0; i< 3; i++) {
-    if(Number(input[i]) < 1 || 9 < (Number(input[i]))) { 
-      return false;
-    }
-  }
-
-  //모두 다른 수인지 Set은 중복을 허용하지 않는다는 점을 이용
-  let checkDupl = new Set(input);
-  if(chechDupl.size !== input.length) { // 길이가 다르면 중복 존재
-    return false;
-  }
-
-  return true; // 모든 조건을 지나오면 true값 반환.
 }
 
+//[직접 테스트용]
+new App().play()
 
-//입력값과 정답 비교
-function strikeOrBall(comsAnswer, userInput) {
-  //consAnswer = makeRandomNum(); 이거 전체 함수 완성하면 거기 적으셈.
+export default App;
 
-}
+
+
