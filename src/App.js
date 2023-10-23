94a63eb77072ef5910752f30f3dab92c406640bb
gyucheolshin <nsfsgc2295@gmail.com>
2023-10-23 16:55:22 +0900

    feat : 정답 3자리 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
+
+  setComputerNumber() {
+    const COMPUTER = [];
+    while (COMPUTER.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!COMPUTER.includes(number)) {
+        COMPUTER.push(number);
+      }
+    }
+    return COMPUTER;
+  }
 }
 
 export default App;
