e6106feb1115015f1af2fbb85a9fece8bfa118bd
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-24 07:37:27 +0900

    :recycle: refactor: 게임진행 출력메서드의 분기를 줄임 + 연산자를 통해 문자열 이어붙이기 활용

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -5,13 +5,18 @@ const OutputView = {
     Console.print(message);
   },
   
+  /**
+   * 볼과 스트라이크의 개수를 통해 출력메세지를 분기함
+   * @param {number} ball 
+   * @param {number} strike 
+   * @returns {string} 게임진행 상황 출력
+   */
   printGameProgress(ball, strike) {
     let message = '';
 
     if (ball === 0 && strike === 0) message = '낫싱';
-    if (ball === 0 && strike > 0) message = `${strike}스트라이크`;
-    if (ball > 0 && strike === 0) message = `${ball}볼`;
-    if (ball > 0 && strike > 0) message = `${ball}볼 ${strike}스트라이크`;
+    if (ball > 0) message += `${ball}볼 `;
+    if (strike > 0) message += `${strike}스트라이크`;
     
     return this.printStaticMessage(message);
   },
