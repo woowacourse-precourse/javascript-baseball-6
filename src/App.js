20e8e15c5707200bc731734a499b0d882dbdd594
parkgeonwoong <hk77kh@gmail.com>
2023-10-23 22:21:32 +0900

    Feat : 사용자 입력값 저장 및 App 클래스에 적용

--- src/App.js
+++ src/App.js
@@ -2,32 +2,45 @@ import { Console } from "@woowacourse/mission-utils";
 import { Random } from "@woowacourse/mission-utils";
 
 export class App {
-  async play() {
-    this.init();
-    try {
-      const userInput = await this.playerInput();
-    } catch (error) {
-      Console.print(error);
-    }
+  #player;
+  #computer;
 
-    const computer = new Computer();
-    console.log(computer.getComputerNumber());
+  constructor() {
+    this.#player = new Player();
+    this.#computer = new Computer();
   }
 
-  init() {
+  async play() {
+    this.init();
+  }
+
+  async init() {
     Console.print("숫자 야구 게임을 시작합니다.");
+
+    try {
+      const getPlayerInput = await this.playerInput();
+      this.#player.setPlayerNumber(getPlayerInput);
+      this.test();
+    } catch (error) {
+      Console.print("에러:", error);
+    }
   }
 
   async playerInput() {
     try {
-      return await Console.readLineAsync("숫자를 입력해주세요 : ");
+      return Console.readLineAsync("숫자를 입력해주세요 : ");
     } catch (error) {
       return Console.print(error);
     }
   }
+
+  test() {
+    console.log("player: ", this.#player.getPlayerNumber());
+    console.log("computer: ", this.#computer.getComputerNumber());
+  }
 }
 
-// 컴퓨터가 랜덤한 숫자를 생성한다.
+// [저장] 컴퓨터 랜덤숫자
 export class Computer {
   #computerNumber;
 
@@ -49,6 +62,19 @@ export class Computer {
 
   getComputerNumber() {
     return this.#computerNumber;
+  }
+}
+
+// [저장] 플레이어 입력
+export class Player {
+  #playerNumber;
+
+  getPlayerNumber() {
+    return this.#playerNumber;
+  }
+
+  setPlayerNumber(playerNumber) {
+    this.#playerNumber = playerNumber;
   }
 }
 
