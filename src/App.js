2d6189072a7ec97b28e2c5786dea68a4b93e2fb2
Jiwon Lee <giwon.yi339@gmail.com>
2023-10-23 16:53:37 +0900

    feat : 플레이어 input 배열로 변환

--- src/App.js
+++ src/App.js
@@ -2,31 +2,71 @@ import * as MissionUtils from "@woowacourse/mission-utils";
 
 export default class App {
   async play() {
-    // console.log("게임 시작");
-    MissionUtils.Console.print("게임 시작");
-    // this.handleSubmitInput();
-    this.getUsername();
+    try {
+      MissionUtils.Console.print("게임 시작");
+      // this.getComputerInput();
+      // await this.getUserInput();
+      // this.checkAnswer();
+      // MissionUtils.Console.print(result);
+      const computerInput = this.getComputerInput();
+      const playerInput = await this.getUserInput();
+      const playerInputArray = [...playerInput].map((el) => Number(el));
+      this.checkAnswer(playerInputArray, computerInput);
+    } catch (err) {
+      MissionUtils.Console.print(err);
+    }
+  }
+  // 컴퓨터 랜덤 생성
+  getComputerInput() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+    // MissionUtils.Console.print(computer);
   }
 
-  async getUsername() {
-    try {
-      const computer = [];
-      while (computer.length < 3) {
-        const number = MissionUtils.Random.pickNumberInRange(1, 9);
-        if (!computer.includes(number)) {
-          computer.push(number);
-        }
-      }
+  async getUserInput() {
+    const userInput = await MissionUtils.Console.readLineAsync(
+      "1~9까지의 수로 이루어진 3자리의 중복없는 숫자를 입력해 주세요."
+    );
 
-      const username = await MissionUtils.Console.readLineAsync(
+    if (userInput.length !== 3) {
+      throw new Error(
         "1~9까지의 수로 이루어진 3자리의 중복없는 숫자를 입력해 주세요."
       );
-      MissionUtils.Console.print(computer);
-      MissionUtils.Console.print(username);
-    } catch (error) {
-      // reject 되는 경우
-      MissionUtils.Console.print(error);
     }
+    return userInput;
+  }
+
+  async checkAnswer(playerInput, computerInput) {
+    // console.log(userInputArray);
+    const strikeCount = 0;
+    const ballCount = 0;
+    const strikeArray = [];
+    const ballArray = [];
+
+    // const computer = this.getUserInput();
+    MissionUtils.Console.print(playerInput);
+    MissionUtils.Console.print(computerInput);
+    // const userInputArray = [...getPlayerData].map((el) => Number(el));
+
+    // for (let i = 0; i < 3; i++) {
+    //   // playerScore index가 computer index와 일치하는 경우
+    //   if (userInputArray[i] === computerInput[i]) {
+    //     strikeCount += 1;
+    //     strikeArray.push(strikeCount);
+    //     MissionUtils.Console.print(strikeArray);
+    //   } else if (this.getComputerInput.includes(userInputArray[i])) {
+    //     this.ballCount += 1;
+    //     // ballsArray.push(this.ballCount);
+
+    //     // console.log("ballCount = ", ballsArray.length);
+    //   }
+    // }
   }
 }
 
