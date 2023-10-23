845495c3b4bbc57903e89acd14c017d22be23188
KimDoToRi <60084235+KimKyungYun@users.noreply.github.com>
2023-10-23 17:04:34 +0900

    feat: 정적데이터 선언, 입력값 오류 판단 기능 제작

---
+++ src/utils/CheckInput.js
@@ -0,0 +1,24 @@
+import { INPUT_LENGTH } from './Constants';
+
+export const checkIsReplay = (input) => {
+	if ([1, 2].includes(input))
+		throw new Error('[ERROR] 1 또는 2만 입력가능합니다.');
+};
+
+export const checkIsPitch = (input) => {
+	if (input.length !== INPUT_LENGTH) {
+		throw new Error(`[ERROR] 입력가능한 길이는 ${INPUT_LENGTH}입니다.`);
+	}
+
+	const inputArray = [];
+	input.split('').forEach((element) => {
+		const convertedNumber = parseInt(element);
+		if (isNaN(convertedNumber)) {
+			throw new Error('[ERROR] 숫자만 입력가능합니다.');
+		}
+		if (inputArray.includes(convertedNumber)) {
+			throw new Error('[ERROR] 중복된 숫자가 입력되었습니다.');
+		}
+		inputArray.push(convertedNumber);
+	});
+};
