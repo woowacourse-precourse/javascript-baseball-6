35ff168b2ecb1b4ad35a4dc3524b3f3907d7de93
Goonco <s07019@naver.com>
2023-10-23 22:30:27 +0900

    :recycle: 3차 구현 : class Baseball field private으로 변환 및 getter메소드 생성

--- src/Baseball.js
+++ src/Baseball.js
@@ -1,10 +1,11 @@
 import { Random } from "@woowacourse/mission-utils";
 
 class Baseball {
-  static BASEBALL_LENGTH = 3;
+  static #BASEBALL_LENGTH = 3;
+  #baseball;
 
   constructor(baseballString) {
-    this.baseball = [];
+    this.#baseball = [];
     if (baseballString) {
       if (this.isInvalidBaseballString(baseballString))
         throw new Error("[ERROR]");
@@ -12,29 +13,32 @@ class Baseball {
     } else this.setRandomBaseball();
   }
 
+  getBaseball() {
+    return this.#baseball;
+  }
+
   setRandomBaseball() {
-    while (this.baseball.length < Baseball.BASEBALL_LENGTH) {
+    while (this.#baseball.length < Baseball.#BASEBALL_LENGTH) {
       let randomNumber = Random.pickNumberInRange(1, 9);
-      if (!this.baseball.includes(randomNumber))
-        this.baseball.push(randomNumber);
+      if (!this.#baseball.includes(randomNumber))
+        this.#baseball.push(randomNumber);
     }
   }
 
   setStringToBaseball(baseballString) {
-    for (let i = 0; i < Baseball.BASEBALL_LENGTH; i++) {
+    for (let i = 0; i < Baseball.#BASEBALL_LENGTH; i++) {
       let number = parseInt(baseballString[i]);
-      this.baseball.push(number);
+      this.#baseball.push(number);
     }
   }
 
   isInvalidBaseballString(baseballString) {
-    let a = "abc";
     if (baseballString.length > 3) return true;
   }
 
   static compareBaseball(guessBall, answerBall) {
-    const guess = guessBall.baseball;
-    const answer = answerBall.baseball;
+    const guess = guessBall.getBaseball();
+    const answer = answerBall.getBaseball();
 
     const result = {
       strike: 0,
@@ -42,7 +46,7 @@ class Baseball {
       out: false,
     };
 
-    for (let i = 0; i < Baseball.BASEBALL_LENGTH; i++) {
+    for (let i = 0; i < Baseball.#BASEBALL_LENGTH; i++) {
       if (answer[i] === guess[i]) result.strike++;
       else if (answer.includes(guess[i])) result.ball++;
     }
