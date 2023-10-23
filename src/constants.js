8f686ab461d1af82f982acce04b2db802f6fcc28
leehyewon0531 <ifpffk@naver.com>
2023-10-23 19:46:50 +0900

    chore: 문자열 상수 정리

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const NORMAL_MSG = {
+  START: '숫자 야구 게임을 시작합니다.',
+  INPUT: '숫자를 입력해 주세요 : ',
+  END: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  SELECT: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+};
+
+export const ERROR_MSG = {
+  NOT_A_NUMBER: '숫자가 아닌 값이 입력되었습니다.',
+  INVALID_LENGTH: '유효하지 않은 길이입니다.',
+  DUPLICATE: '중복 값이 입력되었습니다.'
+}
+
+export const BASEBALL = {
+  BALL: '볼',
+  STRIKE: '스트라이크',
+  NOTHING: '낫싱'
+};
