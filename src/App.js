87cea9ebe0198c43753858fa9954b81314a73703
songudeum <songudeum@gmail.com>
2023-10-23 23:51:44 +0900

    feat: 요구 사항 구현 완료 및 테스트 통과 완료

--- src/App.js
+++ src/App.js
@@ -7,26 +7,23 @@ class App {
         while (willBeRestarted) {
             const answer = this.makeAnswer();
             console.log(`play안에있는 ${answer}`); //테스트 확인용 코드
-
-            let strike = 0;
-            let ball = 0;
-
-            while (strike !== 3) {
-                strike = 0;
-                ball = 0;
+            let userWillRetry = true;
+            while (userWillRetry) {
                 const inputNumberList = await this.makeInputList();
 
-                const { strikeScore, ballScore } = this.scoreCalculator(
-                    { strike, ball },
-                    { answer, inputNumberList }
-                );
-                strike = strikeScore;
-                ball = ballScore;
+                const { strike, ball } = this.scoreCalculator({
+                    answer,
+                    inputNumberList,
+                });
+
                 console.log(`strike : ${strike} ball :${ball}`);
 
                 this.scoreScriptor(strike, ball);
+                if (strike === 3) {
+                    userWillRetry = false;
+                }
             }
-            willBeRestarted = this.userWantToRestart();
+            willBeRestarted = await this.userWantToRestart();
         }
     }
     makeAnswer() {
@@ -56,7 +53,9 @@ class App {
         return inputNumberList;
     }
 
-    scoreCalculator({ strike, ball }, { answer, inputNumberList }) {
+    scoreCalculator({ answer, inputNumberList }) {
+        let strike = 0;
+        let ball = 0;
         for (let i = 0; i < 3; i++) {
             if (answer[i] === inputNumberList[i]) {
                 strike += 1;
@@ -65,25 +64,27 @@ class App {
             }
         }
         console.log(`함수 리턴 직전 strike : ${strike} ball :${ball}`);
-        return { strikeScore: strike, ballScore: ball };
+        return { strike, ball };
     }
 
     scoreScriptor(strike, ball) {
-        if (strike && ball) {
-            Console.print(`${ball}볼 ${strike}스트라이크`);
-        } else if (strike && !ball && strike !== 3) {
-            Console.print(`${strike}스트라이크`);
-        } else if (!strike && ball) {
-            Console.print(`${ball}볼`);
-        } else if (!strike && !ball) {
-            Console.print("낫싱");
-        } else {
+        if (strike === 3) {
             Console.print(
                 `${strike}스트라이크입니다\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`
             );
             Console.print(
                 "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
             );
+            return;
+        }
+        if (strike && ball) {
+            Console.print(`${ball}볼 ${strike}스트라이크`);
+        } else if (strike && !ball) {
+            Console.print(`${strike}스트라이크`);
+        } else if (!strike && ball) {
+            Console.print(`${ball}볼`);
+        } else if (!strike && !ball) {
+            Console.print("낫싱");
         }
     }
 
