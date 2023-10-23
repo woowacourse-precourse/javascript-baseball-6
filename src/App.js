3d79ea2491f048c502449d40c4758fa6dd490a75
steve-shim <steveshim496@gmail.com>
2023-10-23 10:24:02 +0900

    Validating user input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  validationInput(userNumbers) {
+    MissionUtils.Console.print("userNumbers");
+    MissionUtils.Console.print(userNumbers);
+    if (
+      userNumbers.includes(NaN) ||
+      userNumbers.includes(0) ||
+      userNumbers.length !== 3 ||
+      [...new Set(userNumbers)].length !== 3
+    ) {
+      throw new Error("[ERROR]");
+    }
+  }
+
+  async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    let userNumberstr = await MissionUtils.Console.readLineAsync(
+      "숫자를 입력해주세요 : "
+    );
+    let userNumbers = [...userNumberstr].map(Number);
+    this.validationInput(userNumbers);
+  }
 }
 
 export default App;
