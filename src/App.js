8086df828a2088e5853fc8debad3df036d00fa04
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-24 01:23:54 +0900

    feat:  computer - 랜덤 숫자 3개를 선택한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import {Console} from "@woowacourse/mission-utils";
+import { CREATE_RANDOM_NUMBER } from './Computer.js'
 class App {
-  async play() {}
+  async play() {
+    //랜덤으로 값 입력
+    const RANDOM_NUMBER = CREATE_RANDOM_NUMBER();
+    console.log(RANDOM_NUMBER);
+  }
 }
 
+
+
+
+//실행
+const app = new App();
+app.play();
+
+
+
+
 export default App;
+
+
