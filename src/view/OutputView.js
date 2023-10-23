2363e834285ea98b44aa82692ad37ed417665983
Yoosun-Chang <12201321@inha.edu>
2023-10-23 11:58:10 +0900

    feat: 게임 시작시 start message 출력

---
+++ src/view/OutputView.js
@@ -0,0 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+import { Message } from "../constants/message"
+
+const OutputView = {
+  printStartMessage() {
+    Console.print(Message.START);
+  },
+
+};
+
+export default OutputView;
