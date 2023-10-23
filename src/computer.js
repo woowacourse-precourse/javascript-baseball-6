d0be6108c00fb40e0c0fd8199130d633c01371ac
rlaltj <we00425@gmail.com>
2023-10-23 15:02:40 +0900

    Style: Revise Error Message Simply
    에러 처리 문구를 간략하게 변경함

--- src/computer.js
+++ src/computer.js
@@ -2,6 +2,8 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 import { enterNumber } from "./player.js";
 
 export async function pickRandomNumber() {
+  MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+
   const computer = [];
   while (computer.length < 3) {
     const number = MissionUtils.Random.pickNumberInRange(1, 9);
@@ -9,7 +11,6 @@ export async function pickRandomNumber() {
       computer.push(number);
     }
   }
-  console.log(computer+ 'computerNum')
   await enterNumber(computer);
 }
 
--- src/player.js
+++ src/player.js
@@ -9,13 +9,13 @@ export async function enterNumber(computerNum) {
 
         // 예외사항 처리
         if (regex.test(playerNum)) {
-            MissionUtils.Console.print('[ERROR] 숫자가 아닌 문자가 문자열에 포함되어 있습니다. 공백 없이 숫자만 입력해주세요.');
+            MissionUtils.Console.print('[ERROR] 공백 없이 숫자만 입력해주세요.');
             throw new Error('숫자 외 문자 입력 오류');
         } else if (playerNum.length != 3) {
-            MissionUtils.Console.print('[ERROR] 입력한 숫자의 개수가 3이 아닙니다. 3개의 숫자만 입력해주세요.');
+            MissionUtils.Console.print('[ERROR] 3개의 숫자만 입력해주세요.');
             throw new Error('숫자 개수 오류');
         } else if (hasUniqueDigits(playerNum) == false) {
-            MissionUtils.Console.print('[ERROR] 중복된 숫자가 동일합니다. 서로 다른 숫자를 입력해주세요.');
+            MissionUtils.Console.print('[ERROR] 서로 다른 숫자를 입력해주세요.');
             throw new Error('숫자 중복 오류');
         }
         let playerNumArray = playerNum.split('');
