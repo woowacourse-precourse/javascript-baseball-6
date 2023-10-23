fac53152f2b5f25735041c4061de84afd570810f
JiSeong Kim <12180543@inha.edu>
2023-10-23 18:09:18 +0900

    Refactor: 컨벤션 6.1 적용, 문자열에는 single quotes 사용
    에러를 선언하는 부분에 double quotes가 사용되어있어 single quotes로 리팩토링 하였음.

--- src/controller/getUserNumberInput.js
+++ src/controller/getUserNumberInput.js
@@ -1,25 +1,25 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 const checkBlank = (input) => {
-    const throwBlankError = () => { throw new Error("[ERROR] 3자리의 수를 공백없이 이어서 입력해야 합니다.") }
+    const throwBlankError = () => { throw new Error('[ERROR] 3자리의 수를 공백없이 이어서 입력해야 합니다.') }
     [...input].forEach(inputElement => { inputElement === ' ' && throwBlankError()});
 }
 
 const checkThreeDigit = (input) => {
-    if (input.length !== 3) { throw new Error("[ERROR] 3자리의 숫자가 아닙니다.") }
+    if (input.length !== 3) { throw new Error('[ERROR] 3자리의 숫자가 아닙니다.') }
 }
 
 const checkNotDuplicated = (input) => {
-    if (new Set(input).size !== input.length) { throw new Error("[ERROR] 각 자리의 수는 중복되지 않아야 합니다.") }
+    if (new Set(input).size !== input.length) { throw new Error('[ERROR] 각 자리의 수는 중복되지 않아야 합니다.') }
 }
 
 const checkAllNumber = (input) => {
-    const throwNotNumberError = () => { throw new Error("[ERROR] 입력에는 숫자만 포함되어야 합니다.") }
+    const throwNotNumberError = () => { throw new Error('[ERROR] 입력에는 숫자만 포함되어야 합니다.') }
     [...input].forEach(inputElement => isNaN(parseInt(inputElement)) && throwNotNumberError())
 }
 
 const checkNoZero = (input) => {
-    const throwHasZeroError = () => { throw new Error("[ERROR] 입력에는 0이 포함되지 않아야 합니다.") }
+    const throwHasZeroError = () => { throw new Error('[ERROR] 입력에는 0이 포함되지 않아야 합니다.') }
     [...input].forEach(inputElement => parseInt(inputElement) === 0 && throwHasZeroError())
 }
 
