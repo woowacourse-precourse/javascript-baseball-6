a200d1ec55e709f9dcb597b496d603cdee13544e
thumbthing <ilscc0902@gmail.com>
2023-10-24 03:38:03 +0900

    feat: 결과 메세지 생성 분기문 새분화, 불필요한 case 삭제, error 핸들러 적용

--- src/util/returnResultMessage.js
+++ src/util/returnResultMessage.js
@@ -1,32 +1,43 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import throwInvalidInputErrorMessage from "./throwInvalidInputErrorMessage.js";
 
 async function returnResultMessage(result) {
   const STRIKE = result.strike;
   const BALL = result.ball;
 
-  // ball과 strike를 검사를 분리,
-  // return된 string에 따라서 문자열을 합쳐서 반환하는 형식으로 refactor
-
   try {
     let resultMessage = ``;
+    let ballMessage = ``;
+    let strikeMessage = ``;
+
+    if (BALL) {
+      ballMessage += `${BALL}볼`;
+    }
+
+    if (STRIKE) {
+      strikeMessage += `${STRIKE}스트라이크`;
+    }
 
     switch (true) {
-      case STRIKE === 3:
-        resultMessage = `3개의 숫자를 모두 맞히셨습니다! 게임 종료`;
+      case strikeMessage !== `` && ballMessage === ``:
+        resultMessage = `${strikeMessage}`;
         break;
-      case BALL !== 0 && STRIKE === 0:
-        resultMessage = `${BALL}볼`;
+      case strikeMessage !== `` && ballMessage !== ``:
+        resultMessage = `${ballMessage} ${strikeMessage}`;
         break;
-      case BALL !== 0 && STRIKE !== 0:
-        resultMessage = `${BALL}볼 ${STRIKE}스트라이크`;
-        break;
-      case BALL === 0 && STRIKE !== 0:
-        resultMessage = `${STRIKE}스트라이크`;
+      case strikeMessage === `` && ballMessage !== ``:
+        resultMessage = `${ballMessage}`;
         break;
       default:
         resultMessage = `낫싱`;
     }
-    return resultMessage;
+
+    if (resultMessage !== ``) {
+      return resultMessage;
+    } else {
+      const ERROR_MESSAGE = `[ERROR] 입력에 대한 결과를 출력할 수 없습니다`;
+      throwInvalidInputErrorMessage(ERROR_MESSAGE);
+    }
   } catch (error) {
     MissionUtils.Console.print(error);
   }
