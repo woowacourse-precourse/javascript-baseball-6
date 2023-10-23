9b224eba43a4b50731423e4276faf6d9b45802e1
이황제 <happy_kaiser@hotmail.com>
2023-10-23 13:16:36 +0900

    랜덤숫자생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  async play() {}
+  comNumberCreate() {
+    this.comNumberCreate = [];
+
+    while (this.comNumberCreate.length <= 2) {
+      //컴퓨터가 숫자 3개를 고름
+      const num = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (this.comNumberCreate.inCludes(num)) {
+        continue;
+      } else {
+        this.comNumberCreate.push(num);
+      }
+    }
+  }
+
+  inputValidation(userNumber) {
+    userNumber = new set(userNumber);
+
+    if (userNumber.size !== 3) {
+      return false;
+    }
+
+    for (let number of userNumber) {
+      number = parseInt(number, 10);
+
+      if (isNaN(number)) {
+        return false;
+      } else if (number < 1 || 9) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 export default App;
---
+++ src/contants/index.js
@@ -0,0 +1,13 @@
+const GAME_MESSAGE = {
+  START: "숫자 야구 게임을 시작합니다.",
+  INPUT: "숫자를 입력해주세요 : ",
+  SUCCESS: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  END: "게임을 종료합니다.",
+};
+
+const BASEBALL_MESSAGE = {
+  BALL: "볼",
+  STRIKE: "스트라이크",
+  NOTHING: "낫싱",
+};
