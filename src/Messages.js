7e7219eccb4e115a5a59a3895e0260a93a89243d
soyunim <togksk123@naver.com>
2023-10-24 01:10:55 +0900

    refactor: export

--- src/Messages.js
+++ src/Messages.js
@@ -1,4 +1,4 @@
-const Messages = {
+export const Messages = Object.freeze({
   START: "숫자 야구 게임을 시작합니다.",
   END: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
   RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
@@ -6,4 +6,4 @@ const Messages = {
   BALL: "볼",
   STRIKE: "스트라이크",
   NOTHING: "낫싱",
-};
+});
