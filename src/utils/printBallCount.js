44f9f271c3282e832640f7a2c6797f1ff7b4f0e4
yeokyoung song <spolice0324@gmail.com>
2023-10-23 16:21:50 +0900

    refactor : 정의한 상수 값 볼 카운트 출력 함수에 적용
    - 해당 문구 및 변수를 사용하는 부분에 적용

--- src/utils/printBallCount.js
+++ src/utils/printBallCount.js
@@ -1,19 +1,21 @@
+import { BALL_COUNT } from "../Constants";
+
 const { Console } = require("@woowacourse/mission-utils");
 
 const printBallCount = (scoreCount) => {
   let resultMessage = "";
 
   if (scoreCount.strike === 0 && scoreCount.ball === 0) {
-    resultMessage = "낫싱";
+    resultMessage = BALL_COUNT.nothing;
   }
   if (scoreCount.strike === 0 && scoreCount.ball !== 0) {
-    resultMessage = `${scoreCount.ball}볼`;
+    resultMessage = `${scoreCount.ball}${BALL_COUNT.ball}`;
   }
   if (scoreCount.strike !== 0 && scoreCount.ball === 0) {
-    resultMessage = `${scoreCount.strike}스트라이크`;
+    resultMessage = `${scoreCount.strike}${BALL_COUNT.strike}`;
   }
   if (scoreCount.strike !== 0 && scoreCount.ball !== 0) {
-    resultMessage = `${scoreCount.strike}스트라이크 ${scoreCount.ball}볼`;
+    resultMessage = ` ${scoreCount.ball}${BALL_COUNT.ball}${scoreCount.strike}${BALL_COUNT.strike}`;
   }
 
   Console.print(resultMessage);
