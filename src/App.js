83e5eca39dfa837d06a5f9ede758f2fec6550851
KimDoToRi <60084235+KimKyungYun@users.noreply.github.com>
2023-10-23 13:12:24 +0900

    feat: 컴퓨터 난수 저장, 유저입력 제작

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { MissionUtils, Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+	async start() {
+		const COMPUTER_NUMBER = [];
+		while (COMPUTER_NUMBER.length < 3) {
+			const number = MissionUtils.Random.pickNumberInRange(1, 9);
+			if (!COMPUTER_NUMBER.includes(number)) {
+				COMPUTER_NUMBER.push(number);
+			}
+		}
+
+		while (true) {
+			const USER_NUMBER = [];
+
+			try {
+				const input = await Console.readLineAsync('숫자를 입력해주세요 : ');
+				if (input.length !== 3 || parseInt(input).length === 3) {
+					throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+				}
+				input
+					.split('')
+					.forEach((element) => USER_NUMBER.push(parseInt(element)));
+			} catch (error) {
+				// return Promise.reject('[ERROR] 숫자가 잘못된 형식입니다.');
+				return error;
+			}
+
+      
+		}
+	}
+	async play() {
+		Console.print('숫자 야구 게임을 시작합니다.');
+		this.start();
+	}
 }
 
 export default App;
