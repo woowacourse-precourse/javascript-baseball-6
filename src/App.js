0ff96974c5dd545e33455390cbd21685cdae0370
gyucheolshin <nsfsgc2295@gmail.com>
2023-10-24 02:52:47 +0900

    feat : 입력한 값 결과 판정 및 정답 맞췄을 때 게임 종료

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
+  constructor() {
+    this.computer = [];
+    this.userAnswer = [];
+  }
+
   async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.gameStart(this.setComputerNumber());
   }
 
-  // 게임 시작.
   gameStart() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ").then(
       (answer) => {
         this.answerCheck(answer);
+        this.userAnswer = answer.split("").map((v) => Number(v));
+        this.calculateScore(this.computer, this.userAnswer);
       }
     );
   }
@@ -22,7 +28,7 @@ class App {
         computer.push(number);
       }
     }
-    return computer;
+    this.computer = [...computer];
   }
 
   duplicateNumber(answer) {
@@ -36,6 +42,32 @@ class App {
     if (answer.length !== 3) throw new Error("3자리 숫자를 입력해주세요.");
     if (!this.duplicateNumber(answer))
       throw new Error("중복되지 않은 숫자를 입력해주세요.");
+  }
+
+  calculateScore(computer, answer) {
+    const strikeNumber = computer.filter(
+      (number, index) => number === answer[index]
+    );
+
+    const ballNumber = computer
+      .filter((number) => answer.includes(number))
+      .filter((number) => !strikeNumber.includes(number));
+
+    this.scoreDisplay(strikeNumber.length, ballNumber.length);
+    if (strikeNumber.length == 3)
+      return MissionUtils.Console.print(
+        "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+      );
+    return this.gameStart();
+  }
+
+  scoreDisplay(strike, ball) {
+    if (strike === 0 && ball !== 0)
+      return MissionUtils.Console.print(`${ball}볼`);
+    if (strike !== 0 && ball === 0)
+      return MissionUtils.Console.print(`${strike}스트라이크`);
+    if (strike === 0 && ball === 0) return MissionUtils.Console.print("낫싱");
+    return MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
   }
 }
 
