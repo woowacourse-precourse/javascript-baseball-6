4ba9dac40ae8d5c8cf3b9b1407021340276dd489
Junseo <monstazo911@gmail.com>
2023-10-24 03:27:32 +0900

    Feat: 사용자에게 숫자 입력받는 메소드 추가
    사용자에게 숫자를 입력받는 것도 하나의 기능으로 생각하여 메소드를 만들었다.
    추측된 숫자를 입력받기 때문에 메소드명은 inputGuessNumber로 하였다.
    Console API의 readLineAsync를 통해 입력을 받았다.
    woowacourse 모듈의 console.js를 확인해보니,
    readLineAsync 함수는 Promise를 반환하는 비동기 함수이다. 따라서 앞에 await을 붙였고, 이를 사용하는 함수 inputGuessNumber는 async 함수로 작성하였다.
    입력을 받으면 3자리 수 문자열이기 때문에 split과 map을 통해 각각의 숫자로 나눠 배열로 저장하였다.
    해당 메소드를 통해 입력받은 수를 guessNumber 변수에 저장하였다.
    userNumber를 사용하지 않은 이유는, 실제 숫자야구 게임에서는 양쪽다 숫자가 있기 떄문에 사용자의 숫자와 의미가 헷갈릴 것 같아서이다.

--- src/App.js
+++ src/App.js
@@ -18,13 +18,21 @@ class App {
     }
   }
 
-  playBaseBall() {
-    this.generateSecretNumber();
+  // 사용자에게 3자리 숫자를 입력받은 후, 각각의 숫자를 요소로 가지는 배열로 변환 후 반환
+  static async inputGuessNumber() {
+    const userInput = await Console.readLineAsync("숫자를 입력해주세요 : ");
+    const userInputArray = userInput.split("").map(Number);
+    return userInputArray;
   }
 
-  async play() {
-    printGreeting();
-    playBaseBall();
+  playBaseBall() {
+    this.generateSecretNumber();
+    const guessNumber = App.inputGuessNumber();
+  }
+
+  play() {
+    App.printGreeting();
+    this.playBaseBall();
   }
 }
 
