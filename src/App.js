f994c3dbd9b9ac85d364c94f9c777ba55992c4b8
pakxe <pigkill40@naver.com>
2023-10-23 21:01:10 +0900

    feat: App 진입부 코드 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import BaseBallGameComputer from './Model/BaseBallGameComputer.js';
+import BaseBallGame from './controller/BaseBallGame.js';
+
 class App {
-  async play() {}
+	#baseBallGame;
+	#model;
+
+	constructor() {
+		this.#model = new BaseBallGameComputer();
+		this.#baseBallGame = new BaseBallGame(this.#model);
+	}
+
+	async play() {
+		await this.#baseBallGame.start();
+	}
 }
 
+// const a = new App();
+// a.play();
 export default App;
