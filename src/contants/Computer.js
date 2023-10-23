4c8686aaa143b23175d77e979c1f40e5b64053ef
이황제 <happy_kaiser@hotmail.com>
2023-10-23 15:39:09 +0900

    파일 잘못 넣음

--- src/contants/Computer.js
+++ src/contants/Computer.js
@@ -1,22 +1,17 @@
-const GAME_MESSAGE = {
-  START: "숫자 야구 게임을 시작합니다.",
-  INPUT: "숫자를 입력해주세요 : ",
-  SUCCESS: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-  RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
-  END: "게임을 종료합니다.",
-};
+const { Random } = require("@woowacourse/mission-utils");
+//컴퓨터 랜덤 숫자 생성
+class Computer {
+  generateComputerNumber() {
+    const computerNumber = [];
+    while (computerNumber.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computerNumber.includes(number)) {
+        computerNumber.push(number);
+      }
+    }
 
-const BASEBALL_MESSAGE = {
-  BALL: "볼",
-  STRIKE: "스트라이크",
-  NOTHING: "낫싱",
-};
+    return computerNumber;
+  }
+}
 
-const END_OPTION = {
-  NEW_GAME: 1,
-  EXIT: 2,
-};
-
-const ERROR_MESSAGE = {
-  IS_INVALID: "",
-};
+module.exports = { Computer };
