d53f9a092e43b22be4e005e1ac3c44fdc4557285
byungyong <quddyddlrk@naver.com>
2023-10-23 15:43:16 +0900

    feat:repeat until the game end

--- src/App.js
+++ src/App.js
@@ -12,54 +12,58 @@ class App {
       }
     }
 
-    const NUMBER = await Console.readLineAsync("숫자를 입력해주세요 : ");
+    let gameOver = false;
+    while (!gameOver) {
+      const NUMBER = await Console.readLineAsync("숫자를 입력해주세요 : ");
 
-    // 사용자 입력 숫자의 길이 확인
-    if (NUMBER.length !== 3) {
-      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
-    }
-    const USER = NUMBER.split("").map(Number);
+      // 사용자 입력 숫자의 길이 확인
+      if (NUMBER.length !== 3) {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      }
+      const USER = NUMBER.split("").map(Number);
 
-    // 사용자 입력 숫자 중 중복된 값 확인
-    for (let i = 0; i < USER.length; i++) {
-      for (let j = i + 1; j < USER.length; j++) {
-        if (USER[i] === USER[j]) {
+      // 사용자 입력 숫자 중 중복된 값 확인
+      for (let i = 0; i < USER.length; i++) {
+        for (let j = i + 1; j < USER.length; j++) {
+          if (USER[i] === USER[j]) {
+            throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+          }
+        }
+
+        // 사용자 입력 숫자 중 숫자 외 값 확인
+        if (Number.isNaN(USER[i])) {
           throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
         }
       }
 
-      // 사용자 입력 숫자 중 숫자 외 값 확인
-      if (Number.isNaN(USER[i])) {
-        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
-      }
-    }
+      let ballCount = 0;
+      let strikeCount = 0;
 
-    let ballCount = 0;
-    let strikeCount = 0;
-
-    // 상대방 입력 숫자와 사용자 입력 숫자 비교
-    for (let i = 0; i < USER.length; i++) {
-      for (let j = 0; j < COMPUTER.length; j++) {
-        if (i === j && USER[i] === COMPUTER[j]) {
-          strikeCount++;
-        } else if (USER[i] === COMPUTER[j]) {
-          ballCount++;
+      // 상대방 입력 숫자와 사용자 입력 숫자 비교
+      for (let i = 0; i < USER.length; i++) {
+        for (let j = 0; j < COMPUTER.length; j++) {
+          if (i === j && USER[i] === COMPUTER[j]) {
+            strikeCount++;
+          } else if (USER[i] === COMPUTER[j]) {
+            ballCount++;
+          }
         }
       }
-    }
 
-    // 비교 결과 출력
-    if (ballCount > 0 && strikeCount > 0) {
-      Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
-    } else if (ballCount > 0) {
-      Console.print(`${ballCount}볼`);
-    } else if (strikeCount > 0 && strikeCount < 3) {
-      Console.print(`${strikeCount}스트라이크`);
-    } else if (ballCount === 0 && strikeCount === 0) {
-      Console.print("낫싱");
-    } else if (strikeCount === 3) {
-      Console.print(`3스트라이크`);
-      Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
+      // 비교 결과 출력
+      if (ballCount > 0 && strikeCount > 0) {
+        Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
+      } else if (ballCount > 0) {
+        Console.print(`${ballCount}볼`);
+      } else if (strikeCount > 0 && strikeCount < 3) {
+        Console.print(`${strikeCount}스트라이크`);
+      } else if (ballCount === 0 && strikeCount === 0) {
+        Console.print("낫싱");
+      } else if (strikeCount === 3) {
+        Console.print(`3스트라이크`);
+        Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
+        gameOver = true;
+      }
     }
   }
 }
