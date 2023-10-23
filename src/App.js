19ea419dbd521134a4bdb5f7fb95abc6251948b6
Kysiam <how2logout@gmail.com>
2023-10-23 19:39:25 +0900

    feature: 투수가 던진 공(입력한 숫자)을 반환하는 함수

--- src/App.js
+++ src/App.js
@@ -11,6 +11,21 @@ class App {
     }
     return catcherNumbers;
   }
+
+  async getPitcherNumbers() {
+    const throwBalls = await Console.readLineAsync('숫자를 입력해주세요: ');
+    if (throwBalls.length !== 3) throw new Error('[ERROR] 서로 다른 3개의 숫자를 입력해야합니다.');
+    if (!/^[1-9]{3}$/.test(throwBalls))
+      throw new Error('[ERROR] 0을 제외한 서로 다른 3개의 숫자를 입력해야합니다.');
+
+    const ballsToNumbers = throwBalls.split('').map(Number);
+
+    if (new Set(ballsToNumbers).size !== 3) {
+      throw new Error('[ERROR] 서로 다른 3개의 숫자를 입력해야합니다.');
+    }
+
+    return ballsToNumbers;
+  }
 }
 
 export default App;
