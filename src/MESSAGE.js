d61c8890f4021f1f56434843ad19c987eaf177a4
sihyonn <sihyeon982@naver.com>
2023-10-24 02:26:25 +0900

    feat: MESSAGE.js 생성(플레이어에게 표시될 메세지 정의)

---
+++ src/MESSAGE.js
@@ -0,0 +1,13 @@
+const MESSAGE = Object.freeze({
+  GAME_START: "숫자 야구 게임을 시작합니다.",
+  INPUT_NUMBER: "숫자를 입력해주세요 : ",
+  INPUT_NUMBER_ERROR: "서로 다른 3개의 숫자를 입력해야합니다.",
+  BALL: "볼",
+  STRIKE: "스트라이크",
+  NOTHING: "낫싱",
+  RESULT_SUCCESS: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  END_SELECT: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  END_SELECT_ERROR: "잘못된 값을 입력했습니다.",
+});
+
+export default { MESSAGE };
