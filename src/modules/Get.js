17330222dbd2d730d1dad4497730bbcfd93a27f7
ss0526100 <ss0526100@gmail.com>
2023-10-23 22:56:54 +0900

    style(Get): rename intersectionSize

--- src/modules/Get.js
+++ src/modules/Get.js
@@ -12,13 +12,13 @@ class Get {
 
   static countsFrom(originList, comparisonList) {
     const counts = { ball: 0, strike: 0 };
-    counts.ball = Get.subsetSize(originList, comparisonList);
+    counts.ball = Get.intersectionSize(originList, comparisonList);
     counts.strike = Get.strikeCount(originList, comparisonList);
     counts.ball -= counts.strike;
     return counts;
   }
 
-  static subsetSize(originList, comparisonList) {
+  static intersectionSize(originList, comparisonList) {
     let size = 0;
     comparisonList.forEach((number) =>
       originList.includes(number) ? size++ : 0
