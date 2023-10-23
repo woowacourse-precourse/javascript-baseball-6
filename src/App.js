01113f77d8f2ab1d59b7b3292b684a7c5a2e8688
yesiamok3 <jaehyunok@cau.ac.kr>
2023-10-24 02:00:06 +0900

    주석 추가

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async play() {
     console.log('숫자 야구 게임을 시작합니다.');
-    var input = 1;    
+    var input = 1; // 1: 게임 실행, 2: 게임 종료
     while (input == 1) {
       input = await playGame();
     }
   }
 }
 
-async function playGame() {
+// 게임 실행
+async function playGame() { 
+  // 각 자리가 겹치지 않는 3자리 숫자 생성
   const computer = [];
   while (computer.length < 3) {
     const number = MissionUtils.Random.pickNumberInRange(1, 9);
@@ -19,44 +21,43 @@ async function playGame() {
     }
   }
 
+  // 정답을 맞출 때 까지 실행
   while (true) {
     var guess = 0;
-    try {
-      guess = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
-      guess = parseInt(guess);
-    } catch (error) {
-      MissionUtils.Console.print(error);
-      break;
-    }
+    // 사용자 입력
+    guess = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+    guess = parseInt(guess);
 
+    // 입력받은 숫자가 3자리가 아닌 경우
     if (guess < 100 || guess > 999) {
       throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
     }
 
+    // 입력받은 숫자와 정답 비교
     let result = compareGuess(computer, guess);
+    
+    // 결과 출력
     let interpretation = interpretResult(result);
     MissionUtils.Console.print(interpretation);
+    
+    // 정답을 맞힌 경우
     if (result[1] == 3) {
       MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
       break;
     }
   }
 
-  var restart = 2;
-
-  try {
-      restart = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1 종료하려면 2를 입력하세요.\n');
-    } catch (error) {
-      MissionUtils.Console.print(error);
-    }
-
+  // 재실행 여부 확인
+  let restart = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1 종료하려면 2를 입력하세요.\n');
   return restart;
 }
 
+// 입력받은 숫자와 정답 비교
 function compareGuess(computer, guess) {
   var strike = 0
   var ball = 0
 
+  // 입력받은 숫자의 각 자리 분리
   let first = Math.floor(guess / 100);
   let second = Math.floor(guess / 10) - 10 * first;
   let third = guess % 10;
@@ -79,10 +80,12 @@ function compareGuess(computer, guess) {
     ball += 1;
   }
 
+  // 볼, 스트라이크 개수 리턴
   let result = [ball, strike];
   return result;
 }
 
+// 결과 출력
 function interpretResult(result) {
   var interpretation = '';
   if (result[0] != 0) {
