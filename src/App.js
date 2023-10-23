2325e9407fba771fac9cd63c5782914fc85d617e
hyj5076 <hyj5076@hanmail.net>
2023-10-23 17:26:47 +0900

    fix: 리플레이 시 무한 루프 오류 해결

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import * as Constants from './const/Messages';
 import RandomNumberMaker from './play/RandomNumberMaker';
 import UserNumberReader from './play/UserNumberReader';
 import { correctNumber } from './Utils/CorrectNumber';
-import * as Constants from './const/Messages';
 import { showResult } from './play/GameResult';
+import ReplayManager from "./play/ReplayManager";
 const Console = MissionUtils.Console;
 
 class App {
   constructor() {
     this.maker = new RandomNumberMaker();
     this.reader = new UserNumberReader();
+    this.replayManager = new ReplayManager();
   }
 
   async play() {
     try {
-      Console.print(Constants.GAME_START); // 게임 시작
-      const uniqueNumber = this.maker.makeRandomNumber();
-
       while (true) {
+        Console.print(Constants.GAME_START); // 게임 시작
+        const uniqueNumber = this.maker.makeRandomNumber();
+  
         const userNumber = await Console.readLineAsync(); // 사용자 수 읽기
         if (!correctNumber(userNumber)) {
           throw new Error("[ERROR]"); // 예외 발생
@@ -30,19 +32,15 @@ class App {
         showResult(uniqueNumber, userAnswer); // 결과 문구
 
         if (this.isGameOver(uniqueNumber, userAnswer)) { 
-          Console.print(Constants.GOAL);
-          Console.print(Constants.SUGGEST_REPLAY);
-          const userChoice = await Console.readLineAsync();
-          if (userChoice === '1') {
-            return this.play(); // 새로운 게임 시작
-          } else if (userChoice === '2') {
-            Console.print(Constants.GAME_OVER);
-            return; // 게임 종료
+          await this.replayManager.handleReplay(this);
+          if (!replay) {
+            break; // 리플레이 하지 않으면 무한 루프 종료
           }
         }
       }
     } catch (error) {
       Console.print(Constants.GAME_OVER);
+      //Console.print(error.message);
       return Promise.reject(error);
     }
   }
