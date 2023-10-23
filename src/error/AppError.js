9fd75a356b490e69506a5a0412ed8b0618241847
brgndy <brgndy96@gmail.com>
2023-10-23 13:47:43 +0900

    refactor : 그 외 에러메세지 또한 상수처리

--- src/error/AppError.js
+++ src/error/AppError.js
@@ -1,3 +1,5 @@
+import { ERROR_MESSAGES } from '../constants/errorMessages.js';
+
 class AppError extends Error {
   /**
    * @type {string}
@@ -10,8 +12,13 @@ class AppError extends Error {
    */
   name;
 
+  /**
+   *
+   * @param {string} errorMessage
+   */
+
   constructor(errorMessage) {
-    const message = `${AppError.PREFIX} ${errorMessage || '알 수 없는 에러가 발생했어요!'}`;
+    const message = `${AppError.PREFIX} ${errorMessage || ERROR_MESSAGES.ETC}`;
     super(message);
     this.name = this.constructor.name;
   }
