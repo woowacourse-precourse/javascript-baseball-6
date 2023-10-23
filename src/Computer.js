0ef73d56074719d992e6deb301d879a2a4e9ef7a
yui880 <kimyuna521@gmail.com>
2023-10-23 23:06:52 +0900

    refactor: BaseBallGame 클래스의 이름 Computer로 변경
    랜덤 숫자를 생성하고 비교하는 과정은 컴퓨터가 수행하고, BaseballGame은 게임 진행에 어울리는 이름이라고 생각했다. 그래서 BaseballGame 클래스를 다른 객체를 위해 사용하기 위해 랜덤 번호 생성은 Computer로 바꿔주었다.

---
+++ src/Computer.js
@@ -0,0 +1,53 @@
+import { CONSTANT, RESULT } from './Constant.js';
+import { Random } from '@woowacourse/mission-utils';
+
+class Computer {
+  #answer;
+  constructor() {
+    this.#answer = this.#setRandomNumbers();
+  }
+
+  #setRandomNumbers() {
+    const randomNumbers = [];
+    while (randomNumbers.length < CONSTANT.MAX_NUM_LEN) {
+      const newNumber = Random.pickNumberInRange(1, 9);
+      if (!randomNumbers.includes(newNumber)) {
+        randomNumbers.push(newNumber);
+      }
+    }
+    return randomNumbers;
+  }
+
+  compareUserNumbersWithAnswer(userNumbers) {
+    const numsOfStrike = this.#countStrike(userNumbers);
+    const numsOfBall = this.#countBall(userNumbers);
+    return this.#makeResultString(numsOfStrike, numsOfBall);
+  }
+
+  #countStrike(userNumbers) {
+    return userNumbers.reduce((acc, cur, i) => {
+      if (cur === this.#answer[i]) return acc + 1;
+      return acc;
+    }, 0);
+  }
+
+  #countBall(userNumbers) {
+    return userNumbers.reduce((acc, cur, i) => {
+      const index = this.#answer.indexOf(cur);
+      if (index !== i && index >= 0) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
+  }
+
+  #makeResultString(strike, ball) {
+    let result = [];
+    if (!strike && !ball) return RESULT.NOTHING;
+    if (ball) result.push(ball + RESULT.BALL);
+    if (strike) result.push(strike + RESULT.STRIKE);
+    return result.join(' ');
+  }
+}
+
+export default Computer;
