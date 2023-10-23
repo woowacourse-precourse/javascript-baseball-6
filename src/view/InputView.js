8b14cc7a0a9159534f955a7f6c30e441ec522604
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-10-23 13:55:25 +0900

    ✨ Feat: 중복되는 숫자를 입력했을 경우 예외 처리 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -20,11 +20,13 @@ const InputView = {
 
   validateUserNumber(userNumber) {
     const userNumberArray = userNumber.split('')
-    
+
     if (userNumber.length !== NUMBER_SIZE)
       throw new Error(ErrorString.ERROR_USER_NUMBER_LENGTH)
     if (userNumber.replace(/[1-9]/g, '').length > 0)
       throw new Error(ErrorString.ERROR_USER_NOT_NUMBER)
+    if (userNumber.length !== new Set(userNumberArray).size)
+      throw new Error(ErrorString.ERROR_USER_DUPLICATED_NUMBER)
   },
 }
 
