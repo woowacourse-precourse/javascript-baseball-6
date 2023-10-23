765354b94b8495a04d3cb6c791a5a4c1ae63f7bf
greeneeya <suzythetravelerr@gmail.com>
2023-10-23 14:30:17 +0900

    [feature] 사용자의 세자리 수를 입력받는다 잘못된 입력일 경우 에러를 발생시킨다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { Random, Console } from '@woowacourse/mission-utils';
 class App {
-  async play() {}
+  async play() {
+    const computer = this.getRandomComputerNumber();
+    const user = await this.getUserNumber();
+    console.log(computer, user);
+  }
 
-  getRandomNumber() {
+  getRandomComputerNumber() {
     const computer = [];
     while (computer.length < 3) {
       const num = Random.pickNumberInRange(1, 9);
@@ -13,10 +17,25 @@ class App {
     return computer;
   }
 
-  getUserNumber() {}
+  async getUserNumber() {
+    const user = await Console.readLineAsync('숫자를 입력해주세요 : ');
+    if (isNaN(user)) throw new Error('[ERROR] 숫자만 입력 가능합니다.');
+    const checkedNum = this.checkDifferentThreeDigits(user);
+    const parsedNum = checkedNum.map((v) => parseInt(v));
+    return parsedNum;
+  }
+
+  checkDifferentThreeDigits(user) {
+    const array = user.split('');
+    if (array.length > 3) throw new Error('[ERROR] 세 자리 수여야 합니다.');
+    const dupCheck = array.filter((v, i) => user.indexOf(v) === i);
+    if (dupCheck.length < 3)
+      throw new Error('[ERROR] 각 자리 수는 모두 다른 수여야 합니다.');
+    return dupCheck;
+  }
 }
 
 const app = new App();
-app.getRandomNumber();
+app.play();
 
 export default App;
