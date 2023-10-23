e895369be8c63301c4efbf35d902f1422b06892d
sihyonn <sihyeon982@naver.com>
2023-10-24 02:44:46 +0900

    feat: 기본 입, 출력함수 정의

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    await init();
+  }
 }
+const print = (message) => Console.print(message);
+const input = async (input = {}) => await Console.readLineAsync(input);
 
 export default App;
