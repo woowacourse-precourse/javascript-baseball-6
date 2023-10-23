d6379d2eb24a2d78cba9ea978c8835da9ad3523c
hyzzzzy <shinhj4068@naver.com>
2023-10-23 23:03:29 +0900

    Feat: 야구 게임 로직 구현 1. 볼, 스트라이크 비교 함수 작성 2. 비교 결과 출력 함수 작성 3. 재시작 및 종료 구현 4. 생성자 함수 및 start(), end()로 함수 모듈화

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
 import { printMessage, readLineAsync, generateRandomNumber, isValidInput } from './utils';
-import { MESSAGE } from './constants';
+import { MESSAGE, SCORE, SETTING, GAME_STATUS, ERROR_MESSAGE } from './constants';
+
+const { START, INPUT_NUMBER, CORRECT, RETRY } = MESSAGE
+const { SIZE, RESTART_NUMBER, EXIT_NUMBER } = SETTING;
+const { BALL, STRIKE, NOTHING } = SCORE;
 
 class App {
-  gameStatus = true;
+  constructor() {
+    this.gameStatus = GAME_STATUS.READY;
+    this.answer = null;
+  }
+
+  start() {
+    this.answer = generateRandomNumber();
+    this.gameStatus = GAME_STATUS.START;
+  }
 
   async play() {
-    printMessage(MESSAGE.START);
-    generateRandomNumber();
+    printMessage(START);
+    this.start();
 
     try {
-      while (this.gameStatus) {
-        const input = await readLineAsync(MESSAGE.INPUT_NUMBER);
+      while (this.gameStatus === GAME_STATUS.START) {
+        const input = await readLineAsync(INPUT_NUMBER);
         isValidInput(input);
         const num = input.split('').map(Number);
-      }
+        
+        const score = this.compareScore(num, this.answer);
+        const scoreMessage = this.getScoreMessage(score);
+        printMessage(scoreMessage);
 
+        if (score.get(STRIKE) === SIZE) {
+          printMessage(CORRECT);
+          const input = await readLineAsync(RETRY);
+          this.isRetry(input);
+        }
+      }
     } catch (error) {
-      this.gameStatus = false;
+      this.end(error);
+    }
+  }
+
+  end(error) {
+    this.gameStatus = GAME_STATUS.END;
+    if (error) {
       throw new Error(error);
+    }
+  }
+
+  compareScore(num, answer) {
+    let score = new Map();
+
+    score.set(BALL, 0);
+    score.set(STRIKE, 0);
+
+    for (let i = 0; i < SIZE; i++) {
+      if (num[i] === answer[i]) {
+        score.set(STRIKE, score.get(STRIKE) + 1);
+      } else if (answer.includes(num[i])) {
+        score.set(BALL, score.get(BALL) + 1);
+      }
+    }
+
+    return score;
+  }
+
+  getScoreMessage(score) {
+    let message = []; 
+    let ball = score.get(BALL);
+    let strike = score.get(STRIKE);
+
+    if (ball > 0) {
+      message.push(`${ball}${BALL}`);
+    }
+
+    if (strike > 0) {
+      message.push(`${strike}${STRIKE}`);
+    }
+
+    if (ball === 0 && strike === 0) {
+      message.push(`${NOTHING}`);
+    }
+    
+    return message.join(' ');
+  }
+
+  isRetry(input) {
+    input = Number(input);
+    if (input === RESTART_NUMBER) {
+      this.start();
+    } else if (input === EXIT_NUMBER) {
+      this.end();
+    } else {
+      throw new Error(ERROR_MESSAGE.NOT_RETRY_NUMBER);
     }
   }
 }
