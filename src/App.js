2dc719917697629ee0b5ee062bc24ef8f3cfa3e4
westofsky <westofsky159@gmail.com>
2023-10-23 18:03:13 +0900

    feat: 랜덤한 숫자 만들어주는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Console } from '@woowacourse/mission-utils';
-import { COMMAND } from './Constant.js';
+import { Console, Random } from '@woowacourse/mission-utils';
+import { COMMAND, GAME } from './Constant.js';
 class App {
 	async play() {
 		this.showStartMessage();
@@ -8,8 +8,17 @@ class App {
 	showStartMessage() {
 		Console.print(COMMAND.START);
 	}
-	async gameStart() {
-		const userInput = await Console.readLineAsync('숫자를 입력해주세요 : ');
+	generateRandomNumber() {
+		const randomNumberList = [];
+		while (randomNumberList.length < GAME.NUMBER_LENGTH) {
+			const randomNumber = Random.pickNumberInRange(1, 9);
+			if (!randomNumberList.includes(randomNumber))
+				randomNumberList.push(randomNumber);
+		}
+		return randomNumberList.sort();
+	}
+	gameStart() {
+		const answer = this.generateRandomNumber();
 	}
 }
 const app = new App();
--- src/Constant.js
+++ src/Constant.js
@@ -1,3 +1,7 @@
 export const COMMAND = {
 	START: '숫자 야구 게임을 시작합니다.',
 };
+
+export const GAME = {
+	NUMBER_LENGTH: 3,
+};
