06b924fa0299b43f4c4663f26d93862f3b7d61db
olseul <seul9085@naver.com>
2023-10-23 10:36:31 +0900

    feat: define constants for game messages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+const THREE_STRIKES = "3스트라이크";
+const NOTHINGS = "낫싱";
+const GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+const GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+const USER_INPUT_PROMPT = "숫자를 입력해주세요 : ";
+const RESTART_PROMPT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
 class App {
   async play() {}
 }
