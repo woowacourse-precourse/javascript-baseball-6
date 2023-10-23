9e36e6b84867ad81605fc2e1e7a59ad890863f3a
JUMTBOX <yjey12@naver.com>
2023-10-23 10:57:20 +0900

    feat. 숫자 야구 게임 구현

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -3,13 +3,18 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
-
   MissionUtils.Console.readLineAsync.mockImplementation(() => {
     const input = inputs.shift();
     return Promise.resolve(input);
   });
 };
 
+/**
+  <.mockReturnValueOnce>
+  테스트 중 mock 함수의 return값을 주입 시킬 수 있음
+  여기서는 Random.pickNumberInRange가 mock함수이고 현재 return값은 undefined상태이다.
+  즉, 인자로 받아진 numbers배열의 요소들을 Random.pickNumberInRange의 return 값으로 만드는 역할이다. 
+*/
 const mockRandoms = (numbers) => {
   MissionUtils.Random.pickNumberInRange = jest.fn();
   numbers.reduce((acc, number) => {
@@ -29,7 +34,13 @@ describe("숫자 야구 게임", () => {
     const randoms = [1, 3, 5, 5, 8, 9];
     const answers = ["246", "135", "1", "597", "589", "2"];
     const logSpy = getLogSpy();
-    const messages = ["낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료"];
+    const messages = [
+      "낫싱",
+      "3스트라이크",
+      "1볼 1스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -51,10 +62,13 @@ describe("숫자 야구 게임", () => {
 
     mockRandoms(randoms);
     mockQuestions(answers);
-
     // when & then
     const app = new App();
 
+    /**
+     {.rejects}로 Promise가 fullfilled 되면 테스트가 자동으로 실패하게 만들고
+     {.toThrow}로 에러메시지를 던진다.
+    */
     await expect(app.play()).rejects.toThrow("[ERROR]");
   });
 });
