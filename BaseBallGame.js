c534db9d79251f11a015ee7e62d537557caa5b68
hyunyeee <5004ohy@naver.com>
2023-10-24 03:13:27 +0900

    feat: 잘못된 사용자 입력에 대한 예외처리

--- BaseBallGame.js
+++ BaseBallGame.js
@@ -1,13 +1,20 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-const START_MESSAGE = "숫자 야구 게임을 시작합니다."
-const INPUT_MESSAGE = "숫자를 입력해주세요 : "
+const START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+const INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+const ERROR_MESSAGE = "[ERROR] 입력 값이 잘못되었습니다.";
 
 class BaseBallGame {
   async play() {
-    MissionUtils.Console.print(START_MESSAGE);
-    this.#generateComputerNumbers();
-    const userInput = await this.#readUserInput();
+    try {
+      MissionUtils.Console.print(START_MESSAGE);
+      this.#generateComputerNumbers();
+      const userInput = await this.#readUserInput();
+      this.#checkValidation(userInput);
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      throw error;
+    }
   }
 
   #generateComputerNumbers() {
@@ -22,6 +29,12 @@ class BaseBallGame {
 
   async #readUserInput() {
     return await MissionUtils.Console.readLineAsync(INPUT_MESSAGE);
+  }
+
+  #checkValidation(userInput) {
+    if (userInput.length !== 3) {
+      throw new Error(ERROR_MESSAGE);
+    }
   }
 }
 
