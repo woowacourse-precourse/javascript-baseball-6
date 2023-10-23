a377834b68a156325f5be9f7d73f2f0d39d3cfca
yongZin-MAC <>
2023-10-23 18:43:24 +0900

    [숫자 야구 게임] 기능 목록 정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const randomNumber = () => { // 랜덤 숫자 생성
+  // 랜덤 숫자 생성 로직 구현
+}
+
 class App {
-  async play() {}
+  async play() {
+    const retry = async() => { // 재시작 이벤트
+      // 야구 게임 종료 후 재시작 로직 구현
+    }
+
+    const baseBallGame = async()  => { // 야구 게임 이벤트
+      // 야구 게임 로직 구현
+    }
+  }
 }
 
 export default App;
