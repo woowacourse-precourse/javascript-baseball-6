39225081ee2206048842939b60651b2756651f45
yohan11 <yipco77@naver.com>
2023-10-24 00:50:19 +0900

    refactor: 함수 모듈화

--- src/game/user.js
+++ src/game/user.js
@@ -2,26 +2,36 @@ import {MissionUtils} from "@woowacourse/mission-utils";
 
 const getUserNumber = async () => {
     try {
-        const userNumber = await MissionUtils.Console.readLineAsync("숫자를 입력하세요 : ");
-        return userNumber;
+        return await MissionUtils.Console.readLineAsync("숫자를 입력하세요 : ");
     } catch (error) {
         return;
     }
 }
 
+const isNumeric = (value) => !isNaN(Number(value));
+const isWithinRange = (value) => {
+    for (let num = 0; num < value.length; num++) {
+        if (!(Number(num) >= 1 && Number(num) <= 9)) {
+            return false;
+        }
+    }
+    return true;
+}
+const hasUniqueDigits = (value) => new Set(value).size === value.length;
+
 const validateUserNumber = (userNumber) => {
-    // input값이 숫자인지 확인
-    if (isNaN(Number(userNumber)) || userNumber.includes("0")) {
+    if (!isNumeric(userNumber) || !isWithinRange(userNumber)) {
         throw new Error("[ERROR] 숫자가 잘못된 형식입니다. 1~9 사이의 숫자를 입력해주세요.");
     }
+
     if (userNumber.length !== 3) {
         throw new Error("[ERROR] 숫자가 잘못된 형식입니다. 세자리의 숫자를 입력해주세요.")
     }
-    for (let idx = 1; idx < 3; idx++) {
-        if (userNumber.indexOf(userNumber[idx]) !== userNumber.lastIndexOf(userNumber[idx])) {
-            throw new Error("[ERROR] 숫자가 잘못된 형식입니다. 모두 다른 숫자를 입력해주세요.")
-        }
+
+    if (!hasUniqueDigits(userNumber)) {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다. 모두 다른 숫자를 입력해주세요.")
     }
+
     return true;
 }
 
