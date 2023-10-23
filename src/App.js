74a375b6c33ac22b8f3f113df2b708509df5f086
byungyong <quddyddlrk@naver.com>
2023-10-23 15:34:41 +0900

    feat:specify the number of computer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 class App {
-  async play() {}
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+
+    // 컴퓨터 숫자 랜덤 지정
+    const COMPUTER = [];
+
+    while (COMPUTER.length < 3) {
+      const NUMBER = Random.pickNumberInRange(1, 9);
+      if (!COMPUTER.includes(NUMBER)) {
+        COMPUTER.push(NUMBER);
+      }
+    }
+  }
 }
 
 export default App;
