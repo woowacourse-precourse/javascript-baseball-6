28e091f94abc4a9e49d6d6df74c1ca7474016336
민규 <xiahtic73@gmail.com>
2023-10-24 18:53:36 +0900

    modify: setCount, printMessage 메서드 병합 (printCount)

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
   static setComputerNum() {
-    const randomNumber = [];
+    const computerNum = [];
 
-    while (randomNumber.length < 3) {
+    while (computerNum.length < 3) {
       const num = Random.pickNumberInRange(1, 9);
-      if (!randomNumber.includes(num)) randomNumber.push(num);
+      if (!computerNum.includes(num)) computerNum.push(num);
     }
 
-    return randomNumber;
+    return computerNum;
   }
 
   static async setUserNum() {
@@ -19,45 +19,45 @@ class App {
       throw new Error("[ERROR] 세자리 숫자를 입력해주세요.");
     }
 
-    const answer = inputNumber.split("").map((item) => +item);
+    const userNum = inputNumber.split("").map((item) => +item);
 
     const duplicate = (arr) => {
       return arr.some((item, index) => arr.indexOf(item) !== index);
     };
 
-    if (duplicate(answer)) {
+    if (duplicate(userNum)) {
       throw new Error("[ERROR] 숫자가 중복되지 않도록 입력해주세요.");
     }
 
-    return answer;
+    return userNum;
   }
 
-  static setCount(computerNum, userNum) {
-    let strike = 0;
-    let ball = 0;
+  static printCount(computerNum, userNum) {
+    let strikeCount = 0;
+    let ballCount = 0;
 
-    computerNum.forEach((item, index) => (item === userNum[index] ? ++strike : userNum.includes(item) && ++ball));
+    computerNum.forEach((item, index) =>
+      item === userNum[index] ? ++strikeCount : userNum.includes(item) && ++ballCount
+    );
 
-    return [strike, ball];
-  }
+    const strikeMessage = strikeCount ? `${strikeCount}스트라이크` : "";
+    const ballMessage = ballCount ? `${ballCount}볼` : "";
+    const messageSpace = strikeCount && ballCount ? " " : "";
 
-  static printMessage(ballCount) {
-    const strikeMessage = ballCount[0] ? `${ballCount[0]}스트라이크` : "";
-    const ballMessage = ballCount[1] ? `${ballCount[1]}볼` : "";
-    const messageSpace = ballCount[0] && ballCount[1] ? " " : "";
-    const message = !ballCount[0] && !ballCount[1] ? "낫싱" : strikeMessage + messageSpace + ballMessage;
+    const message = !strikeCount && !ballCount ? "낫싱" : strikeMessage + messageSpace + ballMessage;
     Console.print(message);
+
+    return strikeCount === 3 ? true : false;
   }
 
   static async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
     const couputerNum = this.setComputerNum();
     Console.print(couputerNum); // todo delete
+
     const userNum = await this.setUserNum();
 
-    const ballCount = this.setCount(couputerNum, userNum);
-    
-    this.printMessage(ballCount);
+    this.printCount(couputerNum, userNum);
   }
 }
 
