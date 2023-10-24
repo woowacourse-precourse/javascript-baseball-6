6fd314520125d255f23b35d1d0e9713a26eb70bb
chiman <rlaclghks123@naver.com>
2023-10-24 17:45:31 +0900

    test: Player 클래스 테스트
    문자열을 전달 했을 때 숫자 형태의 배열이 잘 나오는지 테스트
    1. 숫자 형태의 배열이 통과되는지
    2. 문자열 형태의 배열이 실패하는지

---
+++ __tests__/Player.test.js
@@ -0,0 +1,15 @@
+import Player from '../src/Player';
+
+describe('사용자 인스턴스로 문자열을 전달했을때 숫자 형태의 배열의 형태로 잘 나타나는지 테스트', () => {
+  test('숫자 형태의 배열로 잘 나타나는지', () => {
+    const player = new Player('123');
+
+    expect(player.playerNumber).toEqual([1, 2, 3]);
+  });
+
+  test('문자 형태의 배열이 아닌지 ', () => {
+    const player = new Player('123');
+
+    expect(player.playerNumber).not.toEqual(['1', '2', '3']);
+  });
+});
