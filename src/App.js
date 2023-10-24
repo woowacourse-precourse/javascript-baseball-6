fa4913876d37223ccb427381ea9020582f63cc16
One-HyeWon <5876675@naver.com>
2023-10-24 18:33:48 +0900

    [refactor] 주석 제거

--- src/App.js
+++ src/App.js
@@ -56,17 +56,14 @@ class App {
   }
 
   isValidInput(input) {
-    // 입력값의 길이가 3인지 확인
     if (input.length !== 3) {
       return false;
     }
 
-    // 입력값이 숫자인지 확인
     if (isNaN(input)) {
       return false;
     }
 
-    // 입력값의 각 자리 수가 1에서 9까지인지 확인
     const digits = input.split("").map(Number);
     const validDigits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     return digits.every((digit) => validDigits.includes(digit));
