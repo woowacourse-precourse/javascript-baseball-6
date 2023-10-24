c0429f87172c7ea4eeba11435e891c23a2e0a7bc
song-eun <dmswo546@naver.com>
2023-10-25 03:56:24 +0900

    fix: 플레이어 숫자 유효검사 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
 
+function printHint({ ball, strike }) {
+  if (ball === 0 && strike === 0) Console.print(`낫싱`);
+  if (ball > 0 && strike > 0) Console.print(`${ball}볼 ${strike}스트라이크`);
+  if (ball > 0 && strike === 0) Console.print(`${ball}볼`);
+  if (ball === 0 && strike > 0) Console.print(`${strike}스트라이크`);
+}
+
+function calculateResult(computerNumber, playerNumber) {
+  const result = { strike: 0, ball: 0 };
+
+  playerNumber.split('').forEach((n, idx) => {
+    if (computerNumber.indexOf(n) === idx) result.strike++;
+    else if (computerNumber.split('').includes(n)) result.ball++;
+  });
+
+  return result;
+}
+
 function isValid(playerInput) {
-  if (playerInput.length !== 3)
-    throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
-  if (isNaN(Number(playerInput)))
-    throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+  if (playerInput.length !== 3) return false;
+  if (isNaN(Number(playerInput))) return false;
 
   let set = new Set(playerInput);
-  if (set.size !== 3) throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+  if (set.size !== 3) return false;
+
+  return true;
 }
 
 function makeComputerNumber() {
@@ -30,11 +48,15 @@ class App {
 
   async startGame(computer) {
     const user = await this.getPlayerNumber();
+    const result = calculateResult(computer, user);
+    printHint(result);
   }
 
   async getPlayerNumber() {
     const playerNumber = await Console.readLineAsync('숫자를 입력해주세요 : ');
-    isValid(playerNumber);
+    if (!isValid(playerNumber)) {
+      throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+    }
     return playerNumber;
   }
 }
