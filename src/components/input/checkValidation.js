cafcd2b3e69e634e5533562a36544e219a1bd030
naro_Kim <gracias9022@gmail.com>
2023-10-24 15:51:47 +0900

    feat: 기능 3 입력 유효성 검사 추가

---
+++ src/components/input/checkValidation.js
@@ -0,0 +1,11 @@
+import { getRandomNumber } from '@components/answer';
+
+// 유효한 입력인지 검사합니다.
+const checkValidation = (input) => {
+  const target = getRandomNumber();
+  let valid = true;
+
+  return valid;
+};
+
+export default checkValidation;
