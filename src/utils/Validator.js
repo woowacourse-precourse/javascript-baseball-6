aca9252f9896e27c9a919d11c9e6cfdffc52a3f9
songhaeunsong <gkdms3469@naver.com>
2023-10-24 21:12:32 +0900

    Feat: validator 강화

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -3,7 +3,8 @@ class ValidatorClass {
     if (input.length !== 3) return false;
     if (input[0] === input[1] || input[0] === input[2] || input[1] === input[2])
       return false;
-    if (input[0] === 0 || input[1] === 0 || input[2] === 0) return false;
+    if (input.includes(0)) return false;
+    if (isNaN(input[0]) || isNaN(input[1]) || isNaN(input[2])) return false;
 
     return true;
   }
