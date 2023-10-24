e55f4be482534a9abec050e7c53164d4354c3810
JungHun98 <j7papa@naver.com>
2023-10-24 11:38:51 +0900

    refactor: getNextStep함수 프롬프트 생성 airbnb style guide NamingConventions 23.10

--- src/modules/getNextStep.js
+++ src/modules/getNextStep.js
@@ -1,14 +1,17 @@
 import inputToUser from './inputToUser';
 import Constant from './Constant';
 
-const { INPUT_NEXT_STEP_PROMPT, EXIT, RESTART } = Constant;
+const { MAX_NUM_LENGTH, EXIT, RESTART } = Constant;
 
 const isExitOrRestartCommand = (string) => {
   return string === EXIT || string === RESTART;
 };
 
 const getNextStep = async () => {
-  const command = await inputToUser(INPUT_NEXT_STEP_PROMPT, isExitOrRestartCommand);
+  const inputNextStepPrompt = `${MAX_NUM_LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료
+  게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.
+  `;
+  const command = await inputToUser(inputNextStepPrompt, isExitOrRestartCommand);
 
   return command;
 };
