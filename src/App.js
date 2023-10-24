432e12a33dffef90e3ec51b5e7ac07cc88461afa
alexgoni <goni000211@gmail.com>
2023-10-24 21:07:07 +0900

    빌드 에러 수정

--- src/App.js
+++ src/App.js
@@ -19,21 +19,6 @@ export default class App {
   }
 
   async getUserChoiceToRestart() {
-    const userInput = await Console.readLineAsync(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
-    );
-
-    switch (userInput) {
-      case "1":
-        return true;
-      case "2":
-        return false;
-      default:
-        throw new Error("[ERROR] 올바른 입력이 아닙니다.");
-    }
-  }
-
-  async getUserChoiceToRestart() {
     try {
       const userInput = await Console.readLineAsync(
         "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
