f37606f162fb7e0a1da04989e64e50d668e6a622
eunsung <esk114477@gmail.com>
2023-10-24 14:24:45 +0900

    test: 중복숫자 testcase추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -85,4 +85,18 @@ describe("숫자 야구 게임", () => {
 
     await expect(app.play()).rejects.toThrow("[ERROR]");
   });
+
+  test("예외 테스트(중복숫자 체크)", async () => {
+    // given
+    const randoms = [4, 5, 2];
+    const answers = ["445"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
 });
