4e2fce2bc39e837b356fb31e5f0033e4aa849211
ohgus <anakin1108@naver.com>
2023-10-24 15:44:06 +0900

    에러 상황에 맞는 메시지 수정

--- src/App.js
+++ src/App.js
@@ -18,28 +18,27 @@ class App {
     this.console.print("숫자 야구 게임을 시작합니다.");
 
     while (this.isPlay) {
-      const answer = this.makeRandomNum();
-      // this.console.print(answer);
-  
-      while (this.strike < 3) {
-        this.ball = 0;
-        this.strike = 0;
-  
-        const userInput = await this.giveQuestion("숫자를 입력해 주세요 : ");
-        
-        this.validation(userInput, this.IN_GAME);
+        const answer = this.makeRandomNum();
 
-        this.compareInputAnswer(answer, userInput);
+        while (this.strike < 3) {
+          this.ball = 0;
+          this.strike = 0;
 
-        this.giveHint();
+          const userInput = await this.giveQuestion("숫자를 입력해 주세요 : ");
+          
+          this.validation(userInput, this.IN_GAME);
+
+          this.compareInputAnswer(answer, userInput);
+
+          this.giveHint();
+        }
+
+        const restartInput = await this.giveQuestion(`게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.${"\n"}`);
+
+        this.validation(restartInput, this.RESTART);
+
+        this.restart(restartInput);
       }
-
-      const restartInput = await this.giveQuestion(`게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.${"\n"}`);
-
-      this.validation(restartInput, this.RESTART);
-
-      this.restart(restartInput);
-    }
   }
 
   makeRandomNum() {
@@ -76,16 +75,16 @@ class App {
   validation(input, mode) {
     if (mode === this.IN_GAME) {
       const reg = /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/ ]/gim;
-      if (!Number(input)) throw new Error("[ERROR]");
-      else if (input.replace(reg, "").length !== 3) throw new Error("[ERROR]");
-      else if (input.includes(" ")) throw new Error("[ERROR]");
-      else if (input.includes("0")) throw new Error("[ERROR]");
-      else if (new Set(input).size !== 3) throw new Error("[ERROR]");
+      if (!Number(input)) throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      else if (input.replace(reg, "").length !== 3) throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      else if (input.includes(" ")) throw new Error("[ERROR] 공백은 포함할 수 없습니다.");
+      else if (input.includes("0")) throw new Error("[ERROR] 1~9 사이의 숫자만 입력이 가능합니다.");
+      else if (new Set(input).size !== 3) throw new Error("[ERROR] 중복된 숫자는 입력할 수 없습니다.");
     }
 
     if (mode === this.RESTART) {
       if (input !== "1" && input !== "2") {
-        throw new Error("[ERROR]");
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
       }
     }
   }
@@ -101,5 +100,5 @@ class App {
 
 export default App;
 
-// const app = new App();
-// app.play();
+const app = new App();
+app.play();
