abace25bd99607e6f2f7cea7c5110c3e5645c382
kim <kajinori@gachon.ac.kr>
2023-10-24 18:32:01 +0900

    feat: generateRandomNumber 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
   async play() {}
+  async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  }
 }
 
+const generateRandomNumber = () => {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer.join('');
+}
 export default App;
