5288720bbc99d8da3f2f5509099abd8e540b2ec5
CHHyuk <gksgur1038@naver.com>
2023-10-25 04:08:58 +0900

    구현기능 README, 정답숫자 만들기 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+// 랜덤으로 정답이 되는 숫자 생성
+function generateRandomNumber() {
+  const answer = [];
+  while (answer.length < 3) {
+    const number = Random.pickNumberInRange(1, 9);
+    if (!answer.includes(number)) {
+      answer.push(number);
+    }
+  }
+  return computer;
+}
+
+// 
+
+
+
+
 class App {
-  async play() {}
+  async play() {
+
+  }
 }
 
 export default App;
