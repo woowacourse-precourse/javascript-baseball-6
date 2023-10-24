92a24c099c8d7eaf8f331c1fe561081a3c4e48a2
23YUJIN <asdbnm066@daum.net>
2023-10-25 04:44:21 +0900

    feat: 숫자 비교 기능 추가

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ class App {
     let computer = this.generateRandomNumber();
     let human = await this.getUserNumberInput();
 
-    // 기능 test용(나중에 지울 것)
+    /* 기능 test용(나중에 지울 것) */
     console.log(computer);
     console.log(human);
+    /* ========================= */
+
+    while (this.compareNumberArray(human, computer) === false) {
+      human = await this.getUserNumberInput();
+    }
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
   }
 
   generateRandomNumber() {
@@ -32,6 +38,40 @@ class App {
     } catch (error) {
       console.log(error);
     }
+  }
+
+  compareNumberArray(human, computer) {
+    let strikeCount = 0;
+    let ballCount = 0;
+    for (let i = 0; i < 3; i++) {
+      if (human[i] === computer[i]) {
+        strikeCount++;
+      } else if (computer.includes(human[i])) {
+        ballCount++;
+      }
+    }
+    MissionUtils.Console.print(this.generateMessage(strikeCount, ballCount));
+    if (strikeCount === 3) {
+      return true;
+    }
+    return false;
+  }
+
+  generateMessage(strikeCount, ballCount) {
+    let message = "";
+    if (ballCount) {
+      message += `${ballCount}볼`;
+    }
+    if (strikeCount) {
+      if (ballCount) {
+        message += " ";
+      }
+      message += `${strikeCount}스트라이크`;
+    }
+    if (message.length === 0) {
+      message += "낫싱";
+    }
+    return message;
   }
 }
 
