2119fb6959163e37132e33b10ff69c127efa6f05
ss0526100 <ss0526100@gmail.com>
2023-10-24 20:13:04 +0900

    style(Print): modify string to const

--- src/modules/Print.js
+++ src/modules/Print.js
@@ -1,20 +1,23 @@
 import { Console } from '@woowacourse/mission-utils';
 
+const PLAY_START_MESSAGE = '숫자 야구 게임을 시작합니다.';
+const RUN_END_MESSAGE = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
+
 class Print {
   static resultsFrom({ ball, strike }) {
-    let ballResult = `${ball == 0 ? '' : ball + '볼 '}`;
-    let strikeResult = `${strike == 0 ? '' : strike + '스트라이크'}`;
+    const ballResult = `${ball == 0 ? '' : ball + '볼 '}`;
+    const strikeResult = `${strike == 0 ? '' : strike + '스트라이크'}`;
     let result = `${ballResult}${strikeResult}`;
     if (result === '') result = '낫싱';
     Console.print(result);
   }
 
   static playStartMessage() {
-    Console.print('숫자 야구 게임을 시작합니다.');
+    Console.print(PLAY_START_MESSAGE);
   }
 
   static runEndMessage() {
-    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    Console.print(RUN_END_MESSAGE);
   }
 
 }
