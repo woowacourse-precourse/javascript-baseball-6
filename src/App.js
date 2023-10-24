e2ce4d5628baf954c4a195793024dacb530f201a
Baek <woonn97@gmail.com>
2023-10-24 13:21:57 +0900

    feat: 랜덤 숫자 생성 함수 구현
    * 각 자리수가 중복되지 않도록 splice를 이용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 class App {
+  constructor() {
+    this.RANDOMNUMBER = this.GENERATENUMBER;
+  }
+  GENERATENUMBER() {
+    // 랜덤 숫자 생성 함수
+    const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const RANDOMNUMBER = [];
+
+    for (let i = 0; i < 3; i++) {
+      const randomIndex = Math.floor(Math.random() * digits.length);
+      RANDOMNUMBER.push(digits.splice(randomIndex, 1)[0]);
+    }
+
+    console.log(RANDOMNUMBER);
+  }
   async play() {}
 }
-
-export default App;
