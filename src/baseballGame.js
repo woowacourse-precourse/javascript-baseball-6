719e106c9ebfc47dc86d3ac45d12e179ae91be94
Sunghyun627 <kksscc4@gmail.com>
2023-10-24 14:54:50 +0900

    refactor : 숫자 입력 문구를 출력하는 기능을 print 메서드에서 readLineAsync 메서드로 변경

--- src/baseballGame.js
+++ src/baseballGame.js
@@ -2,18 +2,25 @@ import { GUIDE_MESSAGES } from './constants.js';
 import { Console } from '@woowacourse/mission-utils';
 
 class BaseballGame {
+  constructor() {
+    this.computerNumber = 0;
+    this.userNumber = 0;
+  }
+
   async playBaseball() {
     this.showGameStartMessage();
-    this.showEnterUserNumberMessage();
+    this.getUserNumber();
   }
 
   async showGameStartMessage() {
     Console.print(GUIDE_MESSAGES.GAME_START_MESSAGE);
   }
 
-  async showEnterUserNumberMessage() {
-    Console.print(GUIDE_MESSAGES.ENTER_USER_NUMBER_MESSAGE);
+  async getUserNumber() {
+    await Console.readLineAsync(GUIDE_MESSAGES.ENTER_USER_NUMBER_MESSAGE);
   }
+
+  async;
 }
 
 export default BaseballGame;
