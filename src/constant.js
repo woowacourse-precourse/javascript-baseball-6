2d5987b0aa6ce9c044062bd8db09c844bfe1dec0
BK <3031011@naver.com>
2023-10-24 17:29:22 +0900

    🦄 refactor: 게임 메시지 상수로 관리 및 모듈 분리
    결과 메시지, 에러 메시지, 프롬프트 메시지 상수로 관리 및 모듈 시스템 적용

---
+++ src/constant.js
@@ -0,0 +1,29 @@
+export const CORRECT_NUMBER = 3;
+
+export const ERROR_MESSAGE = {
+  INVALID_CHOICE: '[ERROR] : 올바르지 않은 입력입니다. 1 또는 2를 입력해주세요.',
+  INVALID_INPUT: '[ERROR] : 잘못된 입력입니다. 게임을 종료합니다',
+  INVALID_NUMBER: '[ERROR] : 유효하지 않은 입력입니다.'
+};
+
+export const PROMPT_MESSAGE = {
+  PRESS_NUMBER: '숫자를 입력해주세요 : ',
+  CHOICE_RESTART: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
+};
+
+export const ALERT_MESSAGE = {
+  resultMessage(strike, ball) {
+    if (strike === CORRECT_NUMBER) {
+      return `${CORRECT_NUMBER}스트라이크\n${CORRECT_NUMBER}개의 숫자를 모두 맞히셨습니다! 게임 종료`;
+    }
+
+    if (strike === 0 && ball === 0) {
+      return '낫싱';
+    }
+
+    if (strike > 0 || ball > 0) {
+      return `${ball ? ball + '볼' : ''} ${strike ? strike + '스트라이크' : ''}`.trim();
+    }
+  }, 
+  GAME_OVER: '게임 종료'
+};
