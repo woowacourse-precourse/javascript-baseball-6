4214fafb7c2e5045e94cb8cd8eaef8c974701ffc
Dabin Jeong <allempti@gmail.com>
2023-10-24 11:20:07 +0900

    feat: 사용자 입력
    - 사용자에게 입력값 받기
    - 사용자 입력값이 유효한지 확인
    - 잘못된 값이면 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  checkUserInput(value) {
+    if (value.length !== 3) {
+      return false;
+    } 
+    const set = new Set(value);
+    if (set.size !== 3) {
+      return false;
+    }
+    for (let i = 0; i < 3; i++) {
+      if (!/[1-9]/.test(value[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  async getUserInput() {
+    const userInput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+    if (!this.checkUserInput(userInput)) {
+      throw new Error('[ERROR] 숫자가 잘못된 형식입니다.')
+    }
+    return userInput;
+  }
+
+  async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+
+    const userInput = await this.getUserInput();
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
