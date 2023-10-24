8cc81f5ac57553fccd19c1653ba72bb79ada7ab2
gabi <70315572+Limgabi@users.noreply.github.com>
2023-10-24 20:52:56 +0900

    feat : 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+import { MESSAGE } from "./constants/constants.js";
+
 class App {
-  async play() {}
+  async play() {
+    Console.print(MESSAGE.START);
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const MESSAGE = {
+  START: "숫자 야구 게임을 시작합니다.",
+};
