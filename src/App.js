3094543567942798f134ab75fb4179e02ee4c79c
leey00nsu <dbstndla1212@naver.com>
2023-10-24 21:00:05 +0900

    feat: getResult 메서드 추가 볼과 스트라이크 수로 결과를 반환하는 메서드

--- src/App.js
+++ src/App.js
@@ -77,6 +77,25 @@ class App {
 
     return [ball, strike];
   }
+
+  getResult(result) {
+    const [ball, strike] = result;
+
+    if (ball === 0 && strike === 0) {
+      IOHelper.printMessage(`낫싱`);
+    }
+    if (ball > 0 && strike === 0) {
+      IOHelper.printMessage(`${ball}볼`);
+    }
+    if (strike > 0 && ball === 0) {
+      IOHelper.printMessage(`${strike}스트라이크`);
+    }
+    if (ball > 0 && strike > 0) {
+      IOHelper.printMessage(`${ball}볼 ${strike}스트라이크`);
+    }
+
+    return strike === DIGIT;
+  }
 }
 
 export default App;
