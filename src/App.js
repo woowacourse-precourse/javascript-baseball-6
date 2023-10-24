a871564e61b4d36c1401b9cea6be0fa53f250fc9
[NDCC] Park jee-hoon <jh.m02h.park@misumi.co.kr>
2023-10-24 17:38:51 +0900

    chore : 비동기 실행 함수로 수정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import playGame from "./game/playGame.js";
 export class App {
   async play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    playGame();
+    await playGame();
   }
 }
 export default App;
