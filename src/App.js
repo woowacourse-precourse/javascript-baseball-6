e8ab67aac31cedb885ed8bb8b9ec30b04d6b421c
myeongin <mungin0926@naver.com>
2023-10-24 22:38:16 +0900

    [feat] 숫자 정보를 저장하는 모델 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import GameView from "./gameView.js";
+import GameModel from "./gameModel.js";
 class App {
   constructor() {
     this.view = new GameView();
+    this.model = new GameModel();
   }
 
   async play() {
     this.view.printGameStartMessage();
+    try {
+      await this.gameStart();
+    } catch (error) {
+      throw new Error(`[ERROR] ${error}`);
+    }
+  }
+
+  async gameStart() {
+    this.controller.updateRandomComputerNumber();
   }
 }
 
