7be9a5681a8114b0c9a671aa6e68f8c22491d508
hyeona <90515062+BaeHyeonA@users.noreply.github.com>
2023-10-25 00:06:42 +0900

    fix : 기능 수정

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App {
   }
 
   async getUserInput(){
-    const userInput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+    const userInput = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
     const validatedUserInput = this.checkInputValidation(userInput);
 
     return validatedUserInput;
@@ -22,10 +22,10 @@ class App {
   checkInputValidation(userInputNumber){
     const userInputNumberArr = userInputNumber.split('').map(Number);
     
-    if(userInputNumber.length !== 3) throw new Error('[ERROR] 3자리 숫자가 아닙니다.')
-    if(new Set(userInputNumberArr).size !== 3) throw new Error('[ERROR] 중복된 숫자가 있습니다.')
-    if(userInputNumber.includes(0)) throw new Error('[ERROR] 1~9 사이의 숫자가 아닙니다.')
-    if(isNaN(userInputNumber)) throw new Error('[ERROR] 숫자가 아닙니다.')
+    if(userInputNumber.length !== 3) throw new Error("[ERROR] 3자리 숫자가 아닙니다.");
+    if(new Set(userInputNumberArr).size !== 3) throw new Error("[ERROR] 중복된 숫자가 있습니다.");
+    if(userInputNumber.includes(0)) throw new Error("[ERROR] 1~9 사이의 숫자가 아닙니다.");
+    if(isNaN(userInputNumber)) throw new Error("[ERROR] 숫자가 아닙니다.");
     
     return userInputNumberArr;
   }
@@ -38,16 +38,17 @@ class App {
       if(computerNumber[i] === userInput[i]) strikeCount++;
       else if(computerNumber.includes(userInput[i])) ballCount++;
     }
-    return {strikeCount, ballCount};
+    return [strikeCount, ballCount];
   }
 
-  async answerResult(computerNumber, userInput){
+  answerResult(computerNumber, userInput){
     if(computerNumber.join('') === userInput){
-      MissionUtils.Console.print('3스트라이크 \n 3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      MissionUtils.Console.print("3스트라이크 \n 3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       return true;
     } else {
-      const BALL_COUNT = this.compareNumber(computerNumber,userInput).ballCount;
-      const STRIKE_COUNT = this.compareNumber(computerNumber,userInput).strikeCount;
+      const compareResult = this.compareNumber(computerNumber,userInput);
+      const BALL_COUNT = compareResult[0];
+      const STRIKE_COUNT = compareResult[1];
 
       if(BALL_COUNT > 0 && STRIKE_COUNT > 0) MissionUtils.Console.print('${BALL_COUNT}볼 ${STRIKE_COUNT}스트라이크');
       else if(BALL_COUNT > 0 && STRIKE_COUNT === 0) MissionUtils.Console.print('${BALL_COUNT}볼');
@@ -62,27 +63,24 @@ class App {
     const computerAnswer = this.getRandomNumber();
     let gameResult = false;
 
-    while(true){
-      const userAnswer = this.getUserInput();
+    while(!gameResult){
+      const userAnswer = await this.getUserInput();
       gameResult = this.answerResult(computerAnswer, userAnswer);
-
-      if(gameResult) break;
-      else this.answerResult(computerAnswer, userAnswer);
     }
 
-    this.restartGame();
+    await this.restartGame();
   }
 
   async restartGame(){
-    const restartAnswer = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
-    if(restartAnswer === '1') await this.startGame();
-    else if(restartAnswer === '2') MissionUtils.Console.print("게임 종료");
-    else throw new Error('[ERROR] 1이나 2를 입력해주세요.')
+    const restartAnswer = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    if(restartAnswer === "1") this.startGame();
+    else if(restartAnswer === "2") MissionUtils.Console.print("게임 종료");
+    else throw new Error("[ERROR] 1이나 2를 입력해주세요.")
   }
 
 
   async play() {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     await this.startGame();
   }
 }
