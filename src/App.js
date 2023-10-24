98fb0ebd613023ca3c020cb65836e51d332a1b46
SuyeonYun <yusuyeon678@gmail.com>
2023-10-24 16:51:43 +0900

    feat : 초기 설정 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+
 class App {
-  async play() {}
+  async play() {
+
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+
+    let ComputerAnswer = generateAnswerNumber();
+
+    console.log(ComputerAnswer);
+
+  }
 }
+
+async function getUserAnswer() {
+  try {
+    const username = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+  } catch (error) {
+    // reject 되는 경우
+  }
+};
+
+function generateAnswerNumber() {
+  const answer = [];
+  while (answer.length < 3) {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!answer.includes(randomNumber)) {
+      answer.push(randomNumber);
+    }
+  }
+  return answer;
+};
 
 export default App;
