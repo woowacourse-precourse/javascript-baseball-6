e8c47f78602c18abf19379e593f5563ba0cb9df0
ihateindex <47144148+ihateindex@users.noreply.github.com>
2023-10-24 16:10:26 +0900

    feat: 재시작을 위한 재귀 추가
    1. strike 3개일시 게임종료, 그 후 재시작 요청시에 컴퓨터 숫자 리셋하고 재귀 호출
    2. 불필요한 ballGame 함수 제거
    3. resultText 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,88 +1,86 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-const REG_INPUT_NUMBER = /^(?!.*([1-9]).*\1)[1-9]{3}$/;
+const REG_INPUT_NUMBER = /^(?!.*(.).*\1)[1-9]{3}$/;
 
 class App {
     async play() {
         MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-        const computerNumber = createComputerNumber(3);
-        MissionUtils.Console.print('컴퓨터의 숫자는');
-        MissionUtils.Console.print(computerNumber);
-        baseBall(computerNumber);
-    }
-}
-
-// * 야구 함수 생성
-async function baseBall(computerNumber) {
-    // 유저 넘버 받고, 야구 점수 카운팅 함수에 전달
-    const userNumber = await inputUserNumber();
-    calculateResults(computerNumber, userNumber);
-}
-
-// * 유저 숫자를 받는 함수
-async function inputUserNumber() {
-    let inputValue = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
-
-    if (!REG_INPUT_NUMBER.test(inputValue)) {
-        throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+        const computerNumber = this.createComputerNumber();
+        return this.calculateResults(computerNumber);
     }
 
-    inputValue = [...inputValue].map(Number);
+    // * 유저 숫자를 받는 함수
+    async inputUserNumber() {
+        const inputValue = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+        // MissionUtils.Console.print(`유저의 숫자는 ${inputValue}`);
 
-    return inputValue;
-}
-
-// * 컴퓨터 숫자 생성 함수
-function createComputerNumber(length) {
-    const computer = [];
-    while (computer.length < length) {
-        const number = MissionUtils.Random.pickNumberInRange(1, 9);
-        if (!computer.includes(number)) {
-            computer.push(number);
+        if (!REG_INPUT_NUMBER.test(inputValue)) {
+            throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+        } else {
+            return Array.from(inputValue).map(Number);
         }
     }
-    return computer;
-}
 
-// * 야구 결과 계산 함수
-function calculateResults(computerNumber, userNumber) {
-    MissionUtils.Console.print('유저의 숫자는');
-    MissionUtils.Console.print(userNumber);
-    let score = {
-        strike: 0,
-        ball: 0,
-    };
-    computerNumber.forEach((element, index) => {
-        if (computerNumber[index] === userNumber[index]) {
-            score.strike++;
-        } else if (computerNumber.includes(userNumber[index])) {
-            score.ball++;
+    // * 컴퓨터 숫자 생성 함수
+    createComputerNumber() {
+        const computer = [];
+        while (computer.length < 3) {
+            const number = MissionUtils.Random.pickNumberInRange(1, 9);
+            if (!computer.includes(number)) {
+                computer.push(number);
+            }
         }
-    });
-
-    let resultText = '';
-    if (score.strike > 0) {
-        resultText += score.strike + '스트라이크 ';
-    }
-    if (score.ball > 0) {
-        resultText += score.ball + '볼';
+        return computer;
     }
 
-    if (resultText) {
-        MissionUtils.Console.print(resultText);
-    } else {
-        MissionUtils.Console.print('낫싱');
+    // * 야구 결과 계산 함수
+    async calculateResults(computerNumber) {
+        const userNumber = await this.inputUserNumber();
+        let score = {
+            strike: 0,
+            ball: 0,
+        };
+
+        computerNumber.forEach((element, index) => {
+            if (computerNumber[index] === userNumber[index]) {
+                score.strike++;
+            } else if (computerNumber.includes(userNumber[index])) {
+                score.ball++;
+            }
+        });
+
+        this.resultText(score);
+
+        if (score.strike !== 3) {
+            return this.calculateResults(computerNumber);
+        } else {
+            MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+            const inputValue = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+            if (inputValue === '1') {
+                const computerNumber = this.createComputerNumber();
+                return this.calculateResults(computerNumber);
+            } else if (inputValue === '2') {
+                return MissionUtils.Console.print('게임 종료');
+            } else {
+                throw new Error('[ERROR] 잘못된 입력입니다.');
+            }
+        }
     }
 
-    if (score.strike === 3) {
-        MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-        // TODO: 재시작 여부 묻기
-    } else {
-        baseBall(computerNumber);
+    resultText(score) {
+        if (score.strike <= 0 && score.ball <= 0) {
+            MissionUtils.Console.print('낫싱');
+        } else {
+            let result = [];
+            if (score.ball > 0) {
+                result.push(`${score.ball}볼`);
+            }
+            if (score.strike > 0) {
+                result.push(`${score.strike}스트라이크`);
+            }
+            MissionUtils.Console.print(result.join(' '));
+        }
     }
 }
-
-const app = new App();
-app.play();
 
 export default App;
