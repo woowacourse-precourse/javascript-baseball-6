ba73a2d15fdd03fd785c5eac244c835e1dbc1df3
parkjonggyeong18 <gd122572@gmail.com>
2023-10-24 15:08:05 +0900

    게임 재시작

--- src/App.js
+++ src/App.js
@@ -74,7 +74,23 @@ class App {
         }
     }
 
-   
+    //게임 재시작 여부 확인 및 재시작
+    async restart() {
+        Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+        const input = await Console.readLineAsync(" 숫자 입력 : ");
+        if (input === '1') {
+            return true
+        } else if (input === '2') {
+            return false
+        } else {
+            throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+        }
+
+    }
+    //예외처리
+    vaildData(userInput) {
+        return /^\d{3}$/.test(userInput) && new Set(userInput).size === 3;
+    }
 
 }
 
