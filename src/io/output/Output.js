7dcdfba20f12d107a36ea30f90426a71c83c0f74
pakxe <pigkill40@naver.com>
2023-10-25 05:18:08 +0900

    refactor(Output): 변수로 선언해 사용하지 않고 함수 그대로 사용하도록 수정

--- src/io/output/Output.js
+++ src/io/output/Output.js
@@ -1,6 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import GAME_MESSAGE from '../../constants/GameMessage.js';
-import allZero from '../../utils/allZero.js';
+import isAllZero from '../../utils/isAllZero.js';
 
 class Output {
 	static printStart() {
@@ -10,9 +10,9 @@ class Output {
 	// 더 단순하게 할 수 있을 것 같다.
 	static printResult([ballCount, strikeCount]) {
 		let judgeComment = '';
-		const isAllZero = allZero([ballCount, strikeCount]);
 
-		if (isAllZero) return Console.print(GAME_MESSAGE.nothing); // 낫싱
+		if (isAllZero([ballCount, strikeCount]))
+			return Console.print(GAME_MESSAGE.nothing); // 낫싱
 
 		judgeComment += ballCount !== 0 ? ballCount + GAME_MESSAGE.ball + ' ' : '';
 		judgeComment += strikeCount !== 0 ? strikeCount + GAME_MESSAGE.strike : '';
