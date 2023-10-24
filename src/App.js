2bcafb3d2ea5b2ce6ce6bc454224d1aa345a6656
osohyun0224 <osohyun0224@naver.com>
2023-10-25 04:42:24 +0900

    Feat: 게임 시작 메세지 출력
    feat:
    - 게임을 시작하고 난 뒤, 게임 메세지를 출력하는 기능을 구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-class App {
-  async play() {}
+import { Console} from "@woowacourse/mission-utils";
+
+export default class App {
+  constructor() {
+    this.isReplaying = true;
+  }
+
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    while (this.isReplaying) {
+      
+    }
+  }
 }
 
-export default App;
