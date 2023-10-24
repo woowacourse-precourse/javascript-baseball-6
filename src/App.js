db5aa6d520681cc84f01a41e109fd566bf97ad05
yonainthefish <yonaisgood@gmail.com>
2023-10-25 03:44:08 +0900

    Feat: 사용자로 부터 입력값 받기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import randomComputeNum from "./components/randomComputeNum.js"
+import validateNum from "./components/validateNum.js"
 
+async function inputNum() {
+  const userNum = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요: ');
+  return userNum;
+}
 class App {  
   async play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');    
 
     let computerNum = randomComputeNum();
     MissionUtils.Console.print(computerNum);
+
+    let userNum = await inputNum();
+
+    if(!validateNum(userNum)){
+      throw new Error("[ERROR] 잘못된 숫자 형식입니다.");
+    }
   }
 }
+
 const app = new App()
 app.play()
 
