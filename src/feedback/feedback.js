dadebdb181dd6310d539af3d3f68b0e2cad38fad
JunSeok <ppu1234@naver.com>
2023-10-24 21:03:07 +0900

    Refactor: forEach 메서드 사용

--- src/feedback/feedback.js
+++ src/feedback/feedback.js
@@ -1,14 +1,14 @@
-import print from './print.js'
+import print from './print.js';
 
 const feedback = async (computer, player) => {
-  let strike = 0
-  let ball = 0
-  for (let [idx, value] of computer.entries()) {
-    const playerNum = Number(player[idx])
-    if (value === playerNum) strike += 1
-    else if (computer.includes(playerNum)) ball += 1
-  }
-  return print(strike, ball)
-}
+  let strike = 0;
+  let ball = 0;
+  computer.forEach((value, idx) => {
+    const playerNum = Number(player[idx]);
+    if (value === playerNum) strike += 1;
+    else if (computer.includes(playerNum)) ball += 1;
+  });
+  return print(strike, ball);
+};
 
-export default feedback
+export default feedback;
