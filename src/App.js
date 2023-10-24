a212b0bdef842d1701cafebeb34e971ae9a6f835
sehyun <e6d1fe@gmail.com>
2023-10-24 16:24:58 +0900

    feat: user 입력 받은 후 입력값 검증 기능

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { message, NUM_LOWER_LIMIT, NUM_UPPER_LIMIT } from './Constants.js';
 class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
+    await this.generateNumbers();
   }
 
   async generateNumbers() {
@@ -14,6 +15,27 @@ class App {
       if (!numbers.includes(newNumber)) numbers.push(newNumber);
     }
     Console.print(numbers);
+    await this.getUserGuess();
+  }
+
+  async getUserGuess() {
+    try {
+      const userGuess = await Console.readLineAsync(message.REQUEST_GUESS_INPUT);
+      await this.checkUserGuessValidity(userGuess);
+      Console.print(`Your number ${userGuess} is a valid input.`);
+    } catch (error) {
+      Console.print(message.ERROR);
+    }
+  }
+
+  async checkUserGuessValidity(userGuess) {
+    if (isNaN(userGuess)) {
+      throw new Error();
+    } else if (String(userGuess).length !== 3) {
+      throw new Error();
+    } else if (userGuess !== [...new Set(String(userGuess).split(''))].join('')) {
+      throw new Error();
+    }
   }
 }
 
