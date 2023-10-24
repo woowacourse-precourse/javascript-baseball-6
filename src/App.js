aeae3f5479fea0c7906cb57a7acbed485a61e642
yonainthefish <yonaisgood@gmail.com>
2023-10-25 02:59:49 +0900

    Feat: 컴퓨터로 부터 중복 되지 않는 숫자 3가지 랜덤 숫자 출력

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import randomComputeNum from "./components/randomComputeNum.js"
 
-class App {
-  
+class App {  
   async play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');    
+
+    let computerNum = randomComputeNum();
+    MissionUtils.Console.print(computerNum);
   }
 }
 const app = new App()
