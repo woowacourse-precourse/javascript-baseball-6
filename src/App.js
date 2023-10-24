35576f69c30e60eb4c9b9e361c795e5080a013ac
lcukylucy <101085055+luckylucyj@users.noreply.github.com>
2023-10-24 21:24:58 +0900

    feat(JiYun): 문제 진행중

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-class App {
-  async play() {}
-}
 
-export default App;
+// let numberList;
+// let answerArray;
+
+// class App {
+
+//   constructor( numberList,answerArray){
+
+//   }
+//       function makeAnswer() {
+//         numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//         answerArray = [];
+//         for (let i = 0; i < 4; i++) {
+//           let picked = [];
+//           picked = numberList.splice(
+//             Math.floor(Math.random() * (10 - i)),
+//             1
+//           )[0];
+//           answerArray.push(picked);
+//         }
+//       }
+//       makeAnswer();
+//       console.log(answerArray);
+//   async play() {}
+// }
+
+// export default App;
+
+// const app = new App();
+// app.play();
