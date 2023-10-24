9c01d959a754435bbaf65a779e34686cf6f5a0c7
baegyeong <beth0929@naver.com>
2023-10-25 02:29:59 +0900

    feat: 숫자 예외처리(유효성 검사) 코드 작성

--- src/App.js
+++ src/App.js
@@ -26,6 +26,19 @@ class App {
       throw new Error("[Error] 숫자 입력이 들어오지 않았습니다.");
     }
   }
+  async settingNumber(number) {
+    if (number < 0) {
+      throw new Error("[Error] 숫자는 음수일 수 없습니다.");
+    }
+    if (!/[0-9]/g.test(number)) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+    if (number.length !== 3) {
+      throw new Error("[ERROR] 숫자는 3자리 수여야 합니다.");
+    }
+
+    this.input = number;
+    await Console.print(this.isIncluded());
   }
 
   async isIncluded() {
