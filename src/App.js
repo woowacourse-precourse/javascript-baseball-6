3e2996f7302db4b4e1789459e990626140cf4ba0
BaeDongWoo <ehddn5410@naver.com>
2023-10-25 03:00:16 +0900

    feat:사용자가 입력한 값이 다르면 출력,같으면 출려과 라운드 종료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import { Console } from '@woowacourse/mission-utils';
+import Computer from './Computer';
+import InputCheck from './InputCheck';
 class App {
   async play() {
-    const userNumber = await Console.readLineAsync('숫자를 입력해주세요 :');
-    Console.print(userNumber);
+    try {
+      Console.print('숫자 야구 게임을 시작합니다.');
+      const computer = new Computer();
+      const inputCheck = new InputCheck();
+      let computerNumber = computer.createRandomNumber();
+      let round = true;
+      while (round) {
+        let strike = 0;
+        let ball = 0;
+        const userNumber = await Console.readLineAsync('숫자를 입력해주세요 :');
+        inputCheck(userNumber);
+        let strikeBall = computer.countStrikeBall(computerNumber, userNumber);
+        strike = strikeBall[0];
+        ball = strikeBall[1];
+        if (strike === 3) round = false;
+        computer.resultPrint(strike, ball);
+      }
+    } catch (e) {
+      Console.log(e);
+      return Promise.reject(error);
+    }
   }
 }
 const app = new App();
