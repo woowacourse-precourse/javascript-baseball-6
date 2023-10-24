5232dc5a7dda1fd11f7d410da4b2e667b726bb21
CSKIM <rlackstjq987@gmail.com>
2023-10-24 23:24:19 +0900

    feat: 각 숫자를 비교하여 힌트를 제공하고 맞췄다면 게임을 종료한다

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   constructor() {
     this.secretNumber = [];
   }
-  genSecretNumber() {
+  setSecretNumber() {
+    this.secretNumber = [];
     while (this.secretNumber.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      const number = Random.pickNumberInRange(1, 9);
       if (!this.secretNumber.includes(number)) {
         this.secretNumber.push(number);
       }
@@ -20,23 +21,21 @@ class App {
     if (stringToArray.length !== 3) {
       return false;
     }
-
     stringToArray.forEach((item) => {
       if (isNaN(item)) {
         return false;
       }
-
       if (checkNumber.includes(item)) {
         return false;
       }
-
-      checkNumber.push(item);
+      checkNumber.push(parseInt(item));
     });
     return checkNumber;
   }
 
   async getInput() {
-    const guess = await MissionUtils.Console.readLineAsync();
+    Console.print("숫자를 입력해주세요 :");
+    const guess = await Console.readLineAsync();
     const validGuess = this.validateInput(guess);
     if (!validGuess) {
       throw Error;
@@ -44,11 +43,59 @@ class App {
     return validGuess;
   }
 
+  async getRestartOrder() {
+    const order = await Console.readLineAsync();
+    if (order === "1") {
+      return true;
+    }
+    if (order === "2") {
+      return false;
+    }
+    throw Error;
+  }
+
   async play() {
-    this.genSecretNumber();
-    const userInput = await this.getInput().catch((e) => {
-      throw Error("[ERROR] 숫자가 잘못된 형식입니다.");
-    });
+    this.setSecretNumber();
+    let endFlag = 1;
+    while (endFlag === 1) {
+      let ball = 0;
+      let strike = 0;
+      let printString = "";
+      const userInput = await this.getInput().catch((e) => {
+        throw Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      });
+      console.log(userInput.join(""));
+      userInput.forEach((item, index) => {
+        if (item === this.secretNumber[index]) {
+          strike += 1;
+        } else if (this.secretNumber.includes(item)) {
+          ball += 1;
+        }
+      });
+      if (ball === 0 && strike === 0) {
+        Console.print("낫싱");
+        continue;
+      }
+      if (ball !== 0) {
+        printString += `${ball}볼 `;
+      }
+      if (strike !== 0) {
+        printString += `${strike}스트라이크`;
+      }
+      Console.print(printString);
+      if (strike === 3) {
+        Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        const isRestart = await this.getRestartOrder().catch((e) => {
+          throw Error("[ERROR] 숫자가 잘못된 형식입니다.");
+        });
+        if (isRestart) {
+          this.setSecretNumber();
+          continue;
+        }
+        endFlag = 2;
+      }
+    }
   }
 }
 
