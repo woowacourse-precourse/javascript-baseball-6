c792875bb96eb2d3c7b142f191aad9cf45a9ba48
이원주 <turtle4265@naver.com>
2023-10-25 00:12:46 +0900

    [feat] 게임 시작 문구와 함께 컴퓨터가 세자리 숫자를 정한다

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
 class App {
+  constructor() {
+    this.computerNumbers = [];
+  }
+
+  initializeComputerNumbers() {
+    
+    Console.print("숫자 야구 게임을 시작합니다");
+    this.computerNumbers = [];
+    while (this.computerNumbers.length < 3) {
+      let number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.computerNumbers.includes(number)) {
+        this.computerNumbers.push(number);
+      }
+    }
+  }
+  
   async play() {}
 }
 
