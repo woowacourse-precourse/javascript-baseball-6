71cdeae2efbb7fa90d374ad0cb467ed6edcc768d
이동주 <leednj0113@naver.com>
2023-10-24 21:20:46 +0900

    Feat: 첫 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
+  /**
+   * 첫 게임 시작
+   */
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
 }
 
 export default App;
