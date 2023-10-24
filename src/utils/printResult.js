57f3cd44a6ce6bbf3ab3bdef80f80eb4964b8e93
stegano9744 <stegano9744@gmail.com>
2023-10-24 23:00:50 +0900

    ♻️ refactor: 코드 구조 개선
    무분별한 조건문 사용을 줄이고 구조를 개선하여 가독성을 향상시켰습니다.
    
    파일 경로: src/utils/printResult.js
    - 3스트라이크 초과 조건문 → Math.min()
    - 볼 스트라이크 출력 조건문 → result 배열 추가, 조건문 구조 개선

--- src/utils/printResult.js
+++ src/utils/printResult.js
@@ -5,10 +5,12 @@ import App from '../App.js';
 // 결과 출력
 export const printResult = async (ball, strike) => {
   if (strike >= 3) {
-    if (strike > 3) strike = 3;
+    strike = Math.min(strike, 3);
     MissionUtils.Console.print(`${strike}${PROMPT.STRIKE}`);
     MissionUtils.Console.print(PROMPT.END_GAME);
-    let user = await MissionUtils.Console.readLineAsync(PROMPT.RESTART_OR_EXIT);
+    const user = await MissionUtils.Console.readLineAsync(
+      PROMPT.RESTART_OR_EXIT
+    );
     if (user === '1') {
       const app = new App();
       app.play();
@@ -16,15 +18,10 @@ export const printResult = async (ball, strike) => {
     else throw new Error(PROMPT.ERROR);
   } else if (ball === 0 && strike === 0) {
     MissionUtils.Console.print(PROMPT.NOTHING);
-  } else if (strike > 0) {
-    if (ball > 0) {
-      MissionUtils.Console.print(
-        `${ball}${PROMPT.BALL} ${strike}${PROMPT.STRIKE}`
-      );
-    } else {
-      MissionUtils.Console.print(`${strike}${PROMPT.STRIKE}`);
-    }
   } else {
-    MissionUtils.Console.print(`${ball}${PROMPT.BALL}`);
+    const result = [];
+    if (ball > 0) result.push(`${ball}${PROMPT.BALL}`);
+    if (strike > 0) result.push(`${strike}${PROMPT.STRIKE}`);
+    MissionUtils.Console.print(result.join(' '));
   }
 };
