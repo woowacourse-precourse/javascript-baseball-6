f489ec5d36be3b5cea7149eca60b2785fbf38381
Klomachenko <leekyumin0901@naver.com>
2023-10-25 02:10:35 +0900

    feat: loop를 통한 게임 종료기능 구현

--- src/App.js
+++ src/App.js
@@ -17,43 +17,47 @@ class App {
     }
     correctAnswer = answer.join('');
 
-    let input = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+    while (!gameOver) {
 
-    if (input[0] == 0 || input[1] == 0 || input[2] == 0) {
-      MissionUtils.Console.print("잘못된 입력입니다. 0을 제외한 숫자를 입력해주세요 : ");
-    } else if (input[0] == input[1] || input[0] == input[2] || input[1] == input[2]) {
-      MissionUtils.Console.print("잘못된 입력입니다. 서로 다른 숫자를 입력해주세요 : ");
-    } else {
-      let strike = 0;
-      let ball = 0;
 
-      for (let i = 0; i < input.length; i++) {
-        if (correctAnswer[i] == input[i]) {
-          strike++
-        } else if (correctAnswer.includes(input[i])) {
-          ball++
-        }
-      }
-      if (strike == 3) {
-        MissionUtils.Console.print("3스트라이크");
-        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        const newGame = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+      let input = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
 
-        if (newGame == 1) {
-          const answer = []
-          while (answer.length < 3) {
-            const number = MissionUtils.Random.pickNumberInRange(1, 9);
-            answer.push(number)
-          }
-          correctAnswer = answer.join('')
-        }
-        else if (newGame == 2) {
-          gameOver = true
-        }
-      } else if (strike == 0 && ball == 0) {
-        MissionUtils.Console.print("낫싱");
+      if (input[0] == 0 || input[1] == 0 || input[2] == 0) {
+        MissionUtils.Console.print("잘못된 입력입니다. 0을 제외한 숫자를 입력해주세요 : ");
+      } else if (input[0] == input[1] || input[0] == input[2] || input[1] == input[2]) {
+        MissionUtils.Console.print("잘못된 입력입니다. 서로 다른 숫자를 입력해주세요 : ");
       } else {
-        MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+        let strike = 0;
+        let ball = 0;
+
+        for (let i = 0; i < input.length; i++) {
+          if (correctAnswer[i] == input[i]) {
+            strike++
+          } else if (correctAnswer.includes(input[i])) {
+            ball++
+          }
+        }
+        if (strike == 3) {
+          MissionUtils.Console.print("3스트라이크");
+          MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+          const newGame = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+
+          if (newGame == 1) {
+            const answer = []
+            while (answer.length < 3) {
+              const number = MissionUtils.Random.pickNumberInRange(1, 9);
+              answer.push(number)
+            }
+            correctAnswer = answer.join('')
+          }
+          else if (newGame == 2) {
+            gameOver = true
+          }
+        } else if (strike == 0 && ball == 0) {
+          MissionUtils.Console.print("낫싱");
+        } else {
+          MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+        }
       }
     }
   }
