8090b9ae9342904ce17542fac32b5a42a747d792
kim-young-dong <opa9904@gmail.com>
2023-10-24 16:17:08 +0900

    refactor: 입력값 검증 메서드 예외처리 리펙토링

--- src/App.js
+++ src/App.js
@@ -19,19 +19,19 @@ class App {
   // 입력값이 검증 메서드
   validateInput(input) {
     let error = new Error();
+    const isDuplicate = input.split('').some((number) => input.indexOf(number) !== input.lastIndexOf(number))
+    const setValidateError = (name, message) => {
+      error.name = name;
+      error.message = message;
+      throw error;
+    }
 
     if(isNaN(Number(input))) {
-      error.name = 'TypeError';
-      error.message = '[ERROR] 숫자를 입력해주세요';
-      throw error;
+      setValidateError('TypeError', '[ERROR] 숫자를 입력해주세요');
     } else if(input.length !== 3) {
-      error.name = 'RangeError';
-      error.message = '[ERROR] 3자리 숫자를 입력해주세요';
-      throw error;
-    } else if(input.split('').some((number) => input.indexOf(number) !== input.lastIndexOf(number))) {
-      error.name = 'Duplicated Number Error';
-      error.message = '[ERROR] 중복되지 않는 숫자를 입력해주세요';
-      throw error;
+      setValidateError('RangeError', '[ERROR] 3자리 숫자를 입력해주세요');
+    } else if(isDuplicate) {
+      setValidateError('Duplicated Number Error', '[ERROR] 중복되지 않는 숫자를 입력해주세요');
     } else {
       this.playerInput = input;
     }
