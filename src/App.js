1eaeac23d41c302bef081d4adc526262e0c76ecc
이황제 <happy_kaiser@hotmail.com>
2023-10-24 21:08:07 +0900

    랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import * as MissionUtils from "@woowacourse/mission-utils";
-const { Console, Random } = MissionUtilis;
+const { Console, Random } = MissionUtils;
 
 class App {
-  async play() {}
+  #computer;
+
+  async play() {
+    Console.log(print("게임을 시작합니다."));
+    this.setComputer;
+
+    return this.computerAnswer();
+  }
+
+  async setComputer() {
+    const NUMBERS = [];
+    while (NUMBERS.length < 3) {
+      const num = Random.pickNumberInRange(1, 9);
+      if (!NUMBERS.includes(num)) {
+        NUMBERS.push(num);
+      }
+    }
+
+    this.#computer = NUMBERS;
+
+    return;
+  }
 }
 
 export default App;
