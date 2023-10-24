469f5e25f753a38e2326847c315b0944d0d07adb
DingX2 <pourding@naver.com>
2023-10-25 02:41:24 +0900

    [FIX] 종료, 시작 예외처리 추가

--- src/components/restartGame.js
+++ src/components/restartGame.js
@@ -2,12 +2,14 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 /* 다시 시작하는 기능 */
 export default async function restartGame() {
-  const userInput = await MissionUtils.Console.readLineAsync(
-    `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. `
-  );
-  if (userInput === "1") {
-    return true;
-  } else if (userInput === "2") {
-    return false;
+  while (true) {
+    const userInput = await MissionUtils.Console.readLineAsync(
+      `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`
+    );
+    if (userInput === "1") {
+      return true;
+    } else if (userInput === "2") {
+      return false;
+    }
   }
 }
