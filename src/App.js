ef65fd38ee7136bfd1397657126f6a5a54aa524b
shimeeuisuk <euisuk95@gmail.com>
2023-10-24 22:12:30 +0900

    feat: 랜덤한 세자리 숫자 만들기 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  #answerArray;
+
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+
+    this.#answerArray = createRandomAnswer();
+  }
+}
+
+function createRandomAnswer() {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer;
 }
 
 export default App;
