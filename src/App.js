461302d718819bdbead1d86902caeec3ba2dd0e1
leey00nsu <dbstndla1212@naver.com>
2023-10-24 20:56:09 +0900

    feat: IOHelper 클래스 추가 유저 입출력 및 랜덤 함수를 mission-utils를 통해 구현하는 클래스

--- src/App.js
+++ src/App.js
@@ -1,4 +1,31 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+class IOHelper {
+  static printMessage(message) {
+    Console.print(message);
+  }
+  static async readLine(question) {
+    const response = await Console.readLineAsync(question);
+
+    return response;
+  }
+  static getRandomNumbers(x, y, n) {
+    const numbers = [];
+    while (numbers.length < n) {
+      const number = Random.pickNumberInRange(x, y);
+      if (!numbers.includes(number)) {
+        numbers.push(number);
+      }
+    }
+    return numbers;
+  }
+}
+
 class App {
+  constructor() {
+    IOHelper.printMessage("숫자 야구 게임을 시작합니다.");
+  }
+
   async play() {}
 }
 
