82c180e84bb7cd82aa65268da26353f4f6ec463a
pakxe <pigkill40@naver.com>
2023-10-25 05:16:56 +0900

    style(isAllZero): 함수명 변경

--- src/utils/isAllZero.js
+++ src/utils/isAllZero.js
@@ -1,8 +1,8 @@
-const allZero = (arr) => {
+const isAllZero = (arr) => {
 	for (let i = 0; i < arr.length; i++) {
 		if (arr[i] !== 0) return false;
 	}
 	return true;
 };
 
-export default allZero;
+export default isAllZero;
