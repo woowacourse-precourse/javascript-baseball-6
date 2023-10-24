ad4b568b653f91d03ab43cc70c26420cc543d2b9
fecapark <fecapark@gmail.com>
2023-10-24 12:58:45 +0900

    add: IOManager에 빈 메서드들 추가

--- src/IOManager.js
+++ src/IOManager.js
@@ -1,5 +1,17 @@
 class IOManager {
   constructor() {}
+
+  throwError() {}
+
+  printStartMessage() {}
+
+  printEndMessage() {}
+
+  printRoundResult() {}
+
+  async askReplay() {}
+
+  async askNumber() {}
 }
 
 export default IOManager;
