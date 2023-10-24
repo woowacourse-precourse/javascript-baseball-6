192e950ed6c68ee0dfe90bb4a414adec67e1080e
Youmi Kim <flordelavidaa@naver.com>
2023-10-25 00:23:52 +0900

    Feat: 정답 숫자 생성
    - 1~9 사이의 숫자 중 서로 다른 숫자 3개 랜덤으로 추출

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
 
-    const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
-    Console.print(randomNumber);
+    const answerNumberArray = generateAnswerNumber(); // 정답 숫자 생성
 
   }
 }
 
 export default App;
 
+function generateAnswerNumber() { // 정답 숫자 배열 생성
+  const numbers = [];
+  while (numbers.length < 3) {
+    const n = MissionUtils.Random.pickNumberInRange(1, 9);
 
+    if (!numbers.includes(n)) {
+      numbers.push(n)
+    }
+  }
+
+  return numbers;
+}
 
 const app = new App();
 app.play();
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+
+    // while(true){
+    //   let strikes = 0;
+    //   let balls = 0;
+    //   let nothing = 0;
+  
