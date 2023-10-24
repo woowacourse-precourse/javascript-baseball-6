be3a7f071119d4601dbe51ca25057c1fa57dc7bc
LJH <049494@ewhain.net>
2023-10-24 18:06:54 +0900

    ✨ 랜덤 숫자 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
+  constructor() {
+    this.isContinue = true;
+    this.computerInput = this.randomNumberGenerator();
+  }
+
+  randomNumberGenerator() {
+    const answer = [];
+    while (answer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!answer.includes(number)) {
+        answer.push(number);
+      }
+    }
+    return answer;
+  }
+
   async play() {}
 }
 
