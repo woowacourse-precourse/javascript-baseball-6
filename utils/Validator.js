ade04d69b4351b58b831ae9c199c4532f3981503
heonq <gkagjsrb@naver.com>
2023-10-24 10:34:29 +0900

    feat: 사용자가 입력한 재시작 여부 값에 대한 유효성 검사 기능 구현

--- utils/Validator.js
+++ utils/Validator.js
@@ -6,6 +6,9 @@ const Validator = {
     if (new Set(number).size !== 3) throw new Error(MESSAGES.duplicatedNumber);
     if (!/^[1-9]{3}$/.test(number.join(''))) throw new Error(MESSAGES.invalidValue);
   },
+  validateRetry(number) {
+    if (number !== 1 && number !== 2) throw new Error(MESSAGES.restartQuery);
+  },
 };
 
 export default Validator;
