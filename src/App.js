6d2e8cf29591f0affac79afa281cf690a128d6cb
KangYunJi <dbswl701@ajou.ac.kr>
2023-10-24 18:07:31 +0900

    feat: 사용자는 입력 과정을 3개의 숫자를 모두 맞힐 때 까지 반복한다.

--- src/App.js
+++ src/App.js
@@ -17,46 +17,52 @@ class App {
   }
 
   async play() {
-    try {
-      const number = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
-      // 컴퓨터는 사용자가 입력한 수에 대한 결과를 낫싱과, 볼, 스트라이크 개수로 표시한다.
-      this.num_list = [...number].map(v => +v);
-      console.log('computer : ', this.computer);
-      console.log('num_list : ', this.num_list);
-
-      this.computer.map((v, idx) => {
-        // 다 같으면 스트라이크
-        // console.log('here!', this.computer[idx], this.num_list[idx])
-        if (this.computer[idx] === this.num_list[idx]) {
-          this.strike++;
-          // console.log('스트라이크');
+    let gameNum = 1;
+    while(gameNum === 1) {
+      this.strike = 0;
+      this.ball = 0;
+      try {
+        const number = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+        // 컴퓨터는 사용자가 입력한 수에 대한 결과를 낫싱과, 볼, 스트라이크 개수로 표시한다.
+        this.num_list = [...number].map(v => +v);
+        console.log('computer : ', this.computer);
+        console.log('num_list : ', this.num_list);
+  
+        this.computer.map((v, idx) => {
+          // 다 같으면 스트라이크
+          // console.log('here!', this.computer[idx], this.num_list[idx])
+          if (this.computer[idx] === this.num_list[idx]) {
+            this.strike++;
+            // console.log('스트라이크');
+          }
+          // 들어는 있는데, 같은 자리가 아니라면, 볼
+          else if (this.num_list.includes(v)) {
+            this.ball++;
+            // console.log('볼')
+          }
+          // 아무것도 아닌 경우
+          else {
+            // console.log('낫싱');
+          }
+        })
+  
+        // 출력
+        if (this.ball > 0 && this.strike === 0) {
+          MissionUtils.Console.print(`${this.ball}볼`);
+        } else if (this.ball > 0 && this.strike > 0) {
+          MissionUtils.Console.print(`${this.ball}볼 ${this.strike}스트라이크`);
+        } else if (this.ball === 0 && this.strike > 0) {
+          MissionUtils.Console.print(`${this.strike}스트라이크`);
+          MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
+        } else {
+          MissionUtils.Console.print(`낫싱`);
         }
-        // 들어는 있는데, 같은 자리가 아니라면, 볼
-        else if (this.num_list.includes(v)) {
-          this.ball++;
-          // console.log('볼')
-        }
-        // 아무것도 아닌 경우
-        else {
-          // console.log('낫싱');
-        }
-      })
-
-      // 출력
-      if (this.ball > 0 && this.strike === 0) {
-        MissionUtils.Console.print(`${this.ball}볼`);
-      } else if (this.ball > 0 && this.strike > 0) {
-        MissionUtils.Console.print(`${this.ball}볼 ${this.strike}스트라이크`);
-      } else if (this.ball === 0 && this.strike > 0) {
-        MissionUtils.Console.print(`${this.strike}스트라이크`);
-        // MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
-      } else {
-        MissionUtils.Console.print(`낫싱`);
+      } catch (error) {
+        // reject 되는 경우
+        console.log('error:', error);
       }
-    } catch (error) {
-      // reject 되는 경우
-      console.log('error:', error);
     }
+ 
   }
 }
 
