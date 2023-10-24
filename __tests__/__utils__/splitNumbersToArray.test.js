43e82ea42294f4fa8cd107ec3d60c2543e175a9a
cobocho <rlaalsrb1111@naver.com>
2023-10-24 11:54:17 +0900

    Test: `splitNumbersToArray` 테스트 코드 작성

---
+++ __tests__/__utils__/splitNumbersToArray.test.js
@@ -0,0 +1,17 @@
+describe('splitNumbersToArray 테스트', () => {
+  it.each([
+    { input: '123', output: [1, 2, 3] },
+    { input: '123456', output: [1, 2, 3, 4, 5, 6] },
+    { input: '12', output: [1, 2] },
+    { input: '1', output: [1] },
+    { input: '', output: [] },
+    { input: '1ㄱㄴ', output: [1, NaN, NaN] },
+  ])(
+    'splitNumbersToArray는 문자로 이루어진 숫자 리스트를 숫자 배열로 변환한다',
+    ({ input, output }) => {
+      const result = splitNumbersToArray(input);
+
+      expect(result).toEqual(output);
+    },
+  );
+});
