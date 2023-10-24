c29570641872a53ea79bc7756d160e0e4fbf46dd
이황제 <happy_kaiser@hotmail.com>
2023-10-25 00:42:53 +0900

    일단 커밋

--- src/App.js
+++ src/App.js
@@ -37,27 +37,48 @@ class App {
     let strike = 0;
     let ball = 0;
 
-    USER_ANSWER.map((num,i) => {
-      if(num==this.#computer[i]) {
-        strike++
-      } else if (this.#computer.includes(num)){
-        ball++
+    USER_ANSWER.map((num, i) => {
+      if (num == this.#computer[i]) {
+        strike++;
+      } else if (this.#computer.includes(num)) {
+        ball++;
       }
-    })
+    });
 
-    this.giveHint(strike,ball) {
-      if(strike==0 && ball ==0){
-        Console.print("낫싱")
-      } else if (strike!==0 && ball==0) {
-        Console.print(`${strike}스트라이크`)
-      } else if (strike ==0 && ball!==0) {
-        Console.print(`${ball}볼`)
+    this.giveHint(strike, ball);
+    {
+      if (strike == 0 && ball == 0) {
+        Console.print("낫싱");
+      } else if (strike !== 0 && ball == 0) {
+        Console.print(`${strike}스트라이크`);
+      } else if (strike == 0 && ball !== 0) {
+        Console.print(`${ball}볼`);
       } else {
-        Console.print(`${ball}볼 ${strike}스트라이크`)
+        Console.print(`${ball}볼 ${strike}스트라이크`);
       }
-      return
+      return;
+    }
+  }
+
+  async restartOrEnd() {
+    Console.print(
+      "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    );
+
+    const SELETED = await Console.readLineAsync("");
+    if (SELETED == "1") {
+      this.setComputer();
+      return this.compareAnswer();
+    } else if (SELETED == "2") {
+      Console.print("게임 종료");
+      return;
+    } else {
+      throw new Error("[ERROR] 입력된 값이 1 또는 2가 아닙니다");
     }
   }
 }
+
+const app = new App();
+app.play();
 
 export default App;
