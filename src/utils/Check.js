19e6850d417ec681e93c31c4382a3659b2f7c73e
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2023-10-25 05:54:43 +0900

    fix: input validation 함수 return 오류 수정

--- src/utils/Check.js
+++ src/utils/Check.js
@@ -3,13 +3,13 @@ export function checkInputValidity(input){
     // 1. 1-9로 이루어진 세자리 숫자인지 확인
     const INPUT_REGEX = /^[1-9]\d{2}$/
     if(!INPUT_REGEX.test(input)){
-        throw new Error('[ERROR] 1-9로 이루어진 세자리 숫자가 아닙니다.')
+        return false
     }
 
     // 2. 중복성 확인
     const inputArray = String(input).split('')
     if(new Set(inputArray).size !== inputArray.length){
-        throw new Error('[ERROR] 중복된 숫자가 존재합니다.')
+        return false
     }
 
     // 1&2번 조건 모두 통과시 return true
