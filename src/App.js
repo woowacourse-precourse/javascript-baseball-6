b356ec9b77aaa4fa89e17de1bfe3bc0e73f435e3
myeongin <mungin0926@naver.com>
2023-10-25 00:50:01 +0900

    [feat] 게임 시작 알림

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
 }
 
+const app = new App();
+app.play();
 export default App;
