431667f68c3374c4e87e205cf31e8ea6df264bc5
Motoong <sonada903@gmail.com>
2023-10-24 14:13:57 +0900

    사용자 입력 판별기능 구현

--- src/App.js
+++ src/App.js
@@ -24,6 +24,32 @@ async function getUserNum() {
   return USER_NUM;
 }
 
+function checkUserNum(COM_NUM, USER_NUM) {
+
+  let strike = 0;
+  let ball = 0;
+
+  for (let i = 0; i < 3; i++) {
+    if (USER_NUM[i] === COM_NUM[i]) {
+      strike++;
+    } else if (USER_NUM.includes(COM_NUM[i])) {
+      ball++;
+    }
+  }
+
+  if (strike === 0 && ball === 0) {
+    Console.print("낫싱")
+  } else if (strike === 0) {
+    Console.print(`${ball}볼`)
+  } else if (ball === 0) {
+    Console.print(`${strike}스트라이크`)
+  } else {
+    Console.print(`${ball}볼 ${strike}스트라이크`)
+  }
+  return strike;
+}
+
+
 class App {
   async play() {
 
@@ -34,6 +60,14 @@ class App {
 
     while (true) {
       userNum = await getUserNum();
+
+      let strike = checkUserNum(COM_NUMBER, userNum);
+
+      if (strike === 3) {
+        Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        const OVERM = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 '1', 종료하려면 '2'를 입력하세요.");
+      }
+
     }
     
     }
