30b84fea8b921e59d64a16df0fcd994b4aa0e620
yonainthefish <yonaisgood@gmail.com>
2023-10-25 02:33:21 +0900

    Feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  
+  async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');    
+  }
 }
+const app = new App()
+app.play()
 
 export default App;
