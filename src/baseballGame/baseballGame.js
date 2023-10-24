8fe8a88e4b981d554a10d2cf45781cb3f3b38d17
yeong-hwan <jghff700@naver.com>
2023-10-24 22:21:22 +0900

    feat: [기능3] countStrike 구현
    [기능3] baseballGame과 countBall JSDoc 작성

---
+++ src/baseballGame/baseballGame.js
@@ -0,0 +1,10 @@
+/**
+ * 정답과 User Input을 받아와 스트라이크, 볼, 낫싱 여부를 반환한다
+ * 
+ * @param {string} answer
+ * @param {string} userInput
+ * @returns {string}
+ */
+export default function baseballGame() {
+
+}
