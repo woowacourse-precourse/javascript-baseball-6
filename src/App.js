105b951a4d85096c5d0a15fd6174cdc83a0f960d
ccconac <qwerpoiu91@naver.com>
2023-10-24 18:43:41 +0900

    :sparkles: feat: 사용자 입력 구현

--- src/App.js
+++ src/App.js
@@ -16,6 +16,25 @@ class App {
     };
 
     let computerValue = getComputerValue();
+
+    const isValidInput = (userInput) => {
+      const inputRegex = /^[0-9]{3}$/.test(userInput);
+      return inputRegex;
+    };
+
+    const getUserValue = async () => {
+      const userInput = await Console.readLineAsync("숫자를 입력해주세요 : ");
+      if (!isValidInput(userInput)) {
+        throw new Error("[ERROR] 서로 다른 숫자 3개만 입력 가능합니다.");
+      }
+      const userValue = [...userInput].map((value) => Number(value));
+      return userValue;
+    };
+
+    let playGame = true;
+    while (playGame) {
+      const userValue = await getUserValue();
+    }
   }
 }
 
