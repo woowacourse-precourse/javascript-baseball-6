eb8b28f8a38c0ae049440ccdfde072c24935a35d
badahertz52 <badahertz52@gmail.com>
2023-10-24 20:20:10 +0900

    [refactors] Rename numberRegExp to winRegExp

--- src/js/ValidTest.js
+++ src/js/ValidTest.js
@@ -4,20 +4,25 @@ import Output from './Output';
 class ValidTest {
   randomNumbersRegExp = /^[1-9]{3}$/;
 
-  numberRegExp = /^[1,2]$/;
+  winRegExp = /^[1,2]$/;
 
   output = new Output();
 
+  /**
+   * 게임 승리 여부에 따라, 입력값의 유효성 검사 진행
+   * @param {number[]} numberArray  : 유효성 검사를 진행할 숫자배열
+   * @param {boolean} win  : 게임 승리 여부
+   */
   test(numberArray, win) {
     const text = numberArray.join('');
     let pass = false;
     if (win) {
-      pass = this.numberRegExp.test(text);
+      pass = this.winRegExp.test(text);
     } else {
       pass = this.randomNumbersRegExp.test(text);
     }
     if (!pass) {
-      this.output.printErrorMessage();
+      this.output.printTypeErrorMessage();
       throw new Error(MESSAGE.TYPE_ERROR);
     }
   }
