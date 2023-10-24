91d1d6ffbe5702f1d01adc7573b8c86b2db7f6b5
23YUJIN <asdbnm066@daum.net>
2023-10-24 22:30:21 +0900

    feat: 숫자 랜덤 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    let computer = this.generateRandomNumber();
+
+    // 기능 test용(나중에 지울 것)
+    console.log(computer);
+  }
+
+  generateRandomNumber() {
+    const isGenerated = new Array(9).fill(false);
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (isGenerated[number] === false) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
