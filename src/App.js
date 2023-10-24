734fb1d07e3b39c44adc1dc6bfa4a104cb3cfac6
소인섭 <sis9745@gachon.ac.kr>
2023-10-25 08:55:31 +0900

    feat: App.js 파일에 컴퓨터 값 추가 및 테스트

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 import startGame from './startup.js';
+import Computer from './Computer.js';
 
 class App {
   async play() {
     startGame();
+    const computer = Computer.getComputer();
+    MissionUtils.Console.print(computer);
   }
 }
 
