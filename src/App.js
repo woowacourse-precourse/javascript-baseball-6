ce06fed9b1ea04e9ee13c92ac32b56398cf2f215
Dev-steadyBoy <85480086+Dev-steadyBoy@users.noreply.github.com>
2023-10-24 16:53:54 +0900

    refactor: [App] App객체 수정
    Baseball을 직접 호출하도록 변경, 불필요한 random 매개변수 삭제

--- src/App.js
+++ src/App.js
@@ -8,15 +8,12 @@ import Exception from '../Exception/Root';
 import GameCondition from '../Exception/GameCondition';
 
 class App {
-  #baseball;
   #exception;
 
   constructor() {
-    this.#baseball = Baseball;
     this.#exception = Exception;
   }
 
-  // 주어진 문자열을 출력하는 기능
   print(message) {
     Console.print(message);
   }
@@ -39,9 +36,9 @@ class App {
       const userFeedback = await Console.readLineAsync(COMMAND.RESTART);
 
       this.#exception.checkAllException(userFeedback);
-      this.print(this.#baseball.announceGameOutcome(random, userFeedback));
+      this.print(Baseball.announceGameOutcome(random, userFeedback));
 
-      if (this.#baseball.isStrikeOut(random, userFeedback)) {
+      if (Baseball.isStrikeOut(random, userFeedback)) {
         await this.askUserOpinion();
         return;
       }
@@ -52,10 +49,11 @@ class App {
     }
   }
 
-  async askUserOpinion(random) {
+  async askUserOpinion() {
     try {
       this.end();
       const ask = await Console.readLineAsync(COMMAND.ASK);
+
       GameCondition.checkAllError(ask);
       this.print(ask);
 
