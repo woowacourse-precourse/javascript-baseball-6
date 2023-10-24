722f609e6057ad4cf5c410519126395e9a3d5eab
pakxe <pigkill40@naver.com>
2023-10-25 03:44:25 +0900

    chore(Output): 불필요한 메서드 제거

--- src/io/output/Output.js
+++ src/io/output/Output.js
@@ -1,35 +1,31 @@
 import { Console } from '@woowacourse/mission-utils';
 import GAME_MESSAGE from '../../constants/GameMessage.js';
+import allZero from '../../utils/allZero.js';
 
 class Output {
 	static printStart() {
 		Console.print(GAME_MESSAGE.start);
 	}
 
-	static printResult(counts) {
-		const [ballCount, strikeCount, isAllZero] = counts;
-
+	// 더 단순하게 할 수 있을 것 같다.
+	static printResult([ballCount, strikeCount]) {
 		let judgeComment = '';
+		const isAllZero = allZero([ballCount, strikeCount]);
 
-		// nothing일 경우
-		if (isAllZero) return Console.print(GAME_MESSAGE.nothing);
+		if (isAllZero) return Console.print(GAME_MESSAGE.nothing); // 낫싱
 
 		judgeComment += ballCount !== 0 ? ballCount + GAME_MESSAGE.ball + ' ' : '';
 		judgeComment += strikeCount !== 0 ? strikeCount + GAME_MESSAGE.strike : '';
 
-		return Console.print(judgeComment);
+		Console.print(judgeComment);
 	}
 
 	static printClear() {
 		Console.print(`${GAME_MESSAGE.clear} ${GAME_MESSAGE.exit}`);
 	}
 
-	static printExit() {
-		Console.print();
-	}
-
-	// static printMore() {
-	// 	Console.print(GAME_MESSAGE.more);
+	// static printExit() {
+	// 	Console.print();
 	// }
 }
 
