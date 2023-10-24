160cfa281031e10b7cfed550f33312cfb5c7f993
kim-young-dong <opa9904@gmail.com>
2023-10-24 16:02:22 +0900

    feat: 입력값 검증 메서드 추가

--- src/App.js
+++ src/App.js
@@ -16,6 +16,26 @@ class App {
     }
     this.answerNumber = numbers.join('');
   }
+  // 입력값이 검증 메서드
+  validateInput(input) {
+    let error = new Error();
+
+    if(isNaN(Number(input))) {
+      error.name = 'TypeError';
+      error.message = '[ERROR] 숫자를 입력해주세요';
+      throw error;
+    } else if(input.length !== 3) {
+      error.name = 'RangeError';
+      error.message = '[ERROR] 3자리 숫자를 입력해주세요';
+      throw error;
+    } else if(input.split('').some((number) => input.indexOf(number) !== input.lastIndexOf(number))) {
+      error.name = 'Duplicated Number Error';
+      error.message = '[ERROR] 중복되지 않는 숫자를 입력해주세요';
+      throw error;
+    } else {
+      this.playerInput = input;
+    }
+  } 
 }
 
 export default App;
