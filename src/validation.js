6027485325c3da6a3f07aab64ee2f505510dcaa5
Sunghyun627 <kksscc4@gmail.com>
2023-10-24 16:33:42 +0900

    feat: 입력값의 길이가 3인지 확인하는 기능 구현

--- src/validation.js
+++ src/validation.js
@@ -5,10 +5,17 @@ export const isValidUserNumber = (input) => {
   if (!isAllCorrectRangeDigits(input)) {
     throw new Error(ERROR_MESSAGES.NOT_DIGIT_IN_RANGE);
   }
+  if (!isThreeDigits(input)) {
+    throw new Error(ERROR_MESSAGES.NOT_THREE_DIGITS);
+  }
   return true;
 };
 
 const isAllCorrectRangeDigits = (input) => {
   const possibleDigits = getUniqueNumbersInRange(1, 9, 9);
   if (input.split('').every((character) => possibleDigits.includes(+character))) return true;
+};
+
+const isThreeDigits = (input) => {
+  if (input.length === 3) return true;
 };
