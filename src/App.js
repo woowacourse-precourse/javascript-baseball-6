4f7d1701d03172972d24e94912cc016aafaa92cb
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-10-24 12:10:12 +0900

    refactor: 변경 재시작 여부 입력
    게임 종료 후 재시작 여부에 관한 응답 입력값을 객체로 치환

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 const VALID_INPUT_LENGTH = 3;
 const ERROR_HEADER = "[ERROR]";
+
+export const Replay = Object.freeze({
+  yes: "1",
+  no: "2",
+});
+
 export default class App {
   constructor() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
@@ -19,6 +25,7 @@ export default class App {
         this.computerNumber += randomNumber;
       }
     }
+    // console.log(this.computerNumber);
   }
 
   async getUserNumber() {
@@ -74,11 +81,11 @@ export default class App {
     const isReplay = await MissionUtils.Console.readLineAsync(
       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
     );
-    if (Number(isReplay) === 1) {
+    if (isReplay === Replay.yes) {
       this.startFlag = true;
       await this.play();
     }
-    if (Number(isReplay) === 2) {
+    if (isReplay === Replay.no) {
       return;
     }
   }
