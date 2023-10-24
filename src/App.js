9046ac2d8cf2fd32dcef77c3ff3e21c20c309247
이동주 <leednj0113@naver.com>
2023-10-24 21:21:47 +0900

    Feat: 해당 게임에서 사용할 컴퓨터 숫자 랜덤 생성

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
   async play() {}
+  constructor() {
+    this.computerNumber = [];
+  }
+  /**
+   * 컴퓨터 번호 랜덤 생성
+   */
+  makeComputerNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+
+    this.computerNumber = [...computer];
+  }
   /**
    * 첫 게임 시작
    */
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    this.makeComputerNumber();
   }
 }
 
