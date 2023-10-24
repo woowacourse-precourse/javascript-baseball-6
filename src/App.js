af326aa1024a7e64fe647f313f8ed009e1801787
rnjs010 <kyuri4809@naver.com>
2023-10-25 05:21:46 +0900

    feat : 난수 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+
+
+    MissionUtils.Console.print(computer);
+  }
+
 }
 
+
+const app = new App();
+app.play();
 export default App;
