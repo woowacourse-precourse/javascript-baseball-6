f9e86ad3a843c7734580672b919903936a5a051e
CSKIM <rlackstjq987@gmail.com>
2023-10-24 22:46:46 +0900

    feat: 사용자는 숫자를 입력한다

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Random } from "@woowacourse/mission-utils";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   constructor() {
@@ -12,8 +12,43 @@ class App {
       }
     }
   }
+
+  validateInput(input) {
+    const checkNumber = [];
+    const stringToArray = [...input];
+
+    if (stringToArray.length !== 3) {
+      return false;
+    }
+
+    stringToArray.forEach((item) => {
+      if (isNaN(item)) {
+        return false;
+      }
+
+      if (checkNumber.includes(item)) {
+        return false;
+      }
+
+      checkNumber.push(item);
+    });
+    return checkNumber;
+  }
+
+  async getInput() {
+    const guess = await MissionUtils.Console.readLineAsync();
+    const validGuess = this.validateInput(guess);
+    if (!validGuess) {
+      throw Error;
+    }
+    return validGuess;
+  }
+
   async play() {
     this.genSecretNumber();
+    const userInput = await this.getInput().catch((e) => {
+      throw Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    });
   }
 }
 
