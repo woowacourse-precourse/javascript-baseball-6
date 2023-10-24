b3089698a38d0a6911077793b57d29f3a22f7c74
myeongin <mungin0926@naver.com>
2023-10-25 00:55:21 +0900

    [feat] 컴퓨터 숫자 생성

--- src/App.js
+++ src/App.js
@@ -3,6 +3,23 @@ import { Random, Console } from "@woowacourse/mission-utils";
 class App {
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    try {
+      await this.gameStart();
+    } catch (error) {
+      throw new Error(`[ERROR] ${error}`);
+    }
+  }
+  async gameStart() {
+    this.computerNumber = this.generateRandomNumber();
+  }
+
+  generateRandomNumber() {
+    const numbers = new Set();
+    while (numbers.size < 3) {
+      const randomNumber = Random.pickNumberInRange(1, 9);
+      numbers.add(randomNumber);
+    }
+    return [...numbers];
   }
 }
 
