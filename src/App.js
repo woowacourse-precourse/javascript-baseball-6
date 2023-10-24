5c4feef74ff0ead91a86efad0e8082fdf786b1a8
mieum12 <qpdlqltb1215@gmail.com>
2023-10-24 17:59:01 +0900

    docs: 기능 요구사항 입력 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print("숫자야구 게임을 시작합니다.");
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
