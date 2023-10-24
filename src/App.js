5dc6da24c77cfe6226938fe8214304118efbacf2
suwonthugger <ddww1029wwdd@gmail.com>
2023-10-24 11:55:04 +0900

    Feat: 랜덤한 숫자 생성 기능 추가
    randomGenerator
    - 과제에서 주어진 API를 사용해서 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
+
+  randomGenerator() {
+    const randomNum = [];
+    while (randomNum.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+
+      if (!randomNum.includes(number)) {
+        randomNum.push(number);
+      }
+    }
+    return randomNum;
+  }
 }
 
 export default App;
