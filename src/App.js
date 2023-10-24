973ab3a10be101502bf8ee6895783178c72525ac
rheefine <rheefine@gmail.com>
2023-10-25 06:24:17 +0900

    refactor: App.js 불필요 await 제거 및 전체 리팩토링 완료

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import Controller from './Controller/Controller.js';
 
 class App {
   async play() {
-    const controller = await new Controller();
+    const controller = new Controller();
     await controller.mainGameController();
   }
 }
-
-//const app = new App();
-// app.play();
 
 export default App;
