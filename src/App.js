09228054d10a29fc4e6371d76d10d34d78d68e31
Dawon00 <dawon000203@gmail.com>
2023-10-25 00:12:22 +0900

    feat: 랜덤 숫자 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+    async play() {
+        console.log("숫자 야구 게임을 시작합니다.");
+
+        // Create Answer Numbers
+        var answerNumber = [];
+        while (answerNumber.length < 3) {
+            var randomNumber = MissionUtils.Random.pickNumberInRange(1, 9); // 숫자를 반환하는 함수를 호출하세요
+            if (!answerNumber.includes(randomNumber)) {
+                answerNumber.push(randomNumber);
+            }
+        }
+        console.log(answerNumber);
+    }
 }
+
+// Run
+const app = new App();
+app.play();
 
 export default App;
