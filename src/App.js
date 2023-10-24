8fe0ef08d0731db9bc6d0e8ca4e47ad109238fb0
meunrud <113085963+meunrud@users.noreply.github.com>
2023-10-24 20:17:39 +0900

    feat + style : 메서드 명 수정, 출력 문구 상수로 변경

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import { MissionUtils, Console } from '@woowacourse/mission-utils';
+const GAME_START_MESSAGE = '숫자 야구 게임을 시작합니다.';
+const INPUT_YOUR_NUMBER = "'숫자를 입력해주세요 :";
+const ERROR_MESSAGE = '[ERROR] 숫자를 잘 못 입력했습니다.';
+const WINNING_MASSAGE = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
+const AKS_RESTART_MASSAGE =
+  '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.';
 
 class App {
   async play() {
@@ -14,20 +20,19 @@ class App {
         let userInput = await this.promptUserInput();
 
         if (userInput.length !== 3 || new Set(userInput).size !== 3) {
-          throw new Error('[ERROR] 숫자를 잘 못 입력했습니다.');
+          throw new Error(ERROR_MESSAGE);
         }
 
         let result = this.evaluateGameResult(answerNumber, userInput);
 
         Console.print(result);
 
-        if (result.includes(3)) {
+        if (result.includes(3 + '스트라이크')) {
           this.endGameMessage();
           playAgain = await this.askForAnotherGame();
           if (playAgain === 1) {
             answerNumber = this.generateRandomNumbers();
           }
-
           if (playAgain === 2) {
             break;
           }
@@ -37,7 +42,7 @@ class App {
   }
 
   displayGameStartMessage() {
-    const gameStartMessage = '숫자 야구 게임을 시작합니다.';
+    const gameStartMessage = GAME_START_MESSAGE;
     Console.print(gameStartMessage);
   }
 
@@ -53,12 +58,12 @@ class App {
   }
 
   async promptUserInput() {
-    let playerInput = await Console.readLineAsync('숫자를 입력해주세요 :');
+    let playerInput = await Console.readLineAsync(INPUT_YOUR_NUMBER);
     let playerNumber = playerInput.split('').map((number) => parseInt(number));
     return playerNumber;
   }
 
-  strike = (a, b) => {
+  countStrike(a, b) {
     let howManyStrike = 0;
     for (let i = 0; i < a.length; i++) {
       if (a[i] === b[i]) {
@@ -66,57 +71,49 @@ class App {
       }
     }
     return howManyStrike;
-  };
-  ball = (a, b) => {
-    let ball = 0;
+  }
+
+  countBall(a, b) {
+    let howManyball = 0;
     for (let i = 0; i < a.length; i++) {
       if (a.includes(b[i]) && a[i] !== b[i]) {
-        ball++;
+        howManyball++;
       }
     }
-    return ball;
-  };
+    return howManyball;
+  }
 
-  nothing = (a, b) => {
+  nothing(a, b) {
     if (a.every((val, index) => val !== b[index])) {
       return '낫싱';
     }
-  };
+  }
 
   evaluateGameResult(answerNumber, playerNumber) {
-    let strike = this.strike(answerNumber, playerNumber);
-    let ball = this.ball(answerNumber, playerNumber);
-    let nothing = this.nothing(answerNumber, playerNumber);
+    const strike = this.countStrike(answerNumber, playerNumber);
+    const ball = this.countBall(answerNumber, playerNumber);
 
     if (strike === 0 && ball === 0) {
-      return nothing;
+      return this.nothing(answerNumber, playerNumber);
     }
 
-    if (strike === 0 && ball !== 0) {
-      return `${ball}볼`;
+    const resultParts = [];
+    if (ball > 0) {
+      resultParts.push(`${ball}볼`);
+    }
+    if (strike > 0) {
+      resultParts.push(`${strike}스트라이크`);
     }
 
-    if (ball === 0 && strike < 3) {
-      return `${strike}스트라이크`;
-    }
-
-    if (strike === 3) {
-      return `${strike}스트라이크`;
-    }
-
-    if (strike !== 0 && ball !== 0) {
-      return `${ball}볼 ${strike}스트라이크`;
-    }
+    return resultParts.join(' ');
   }
 
   endGameMessage() {
-    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    Console.print(WINNING_MASSAGE);
   }
 
   async askForAnotherGame() {
-    let plyerInputOneorTwo = await Console.readLineAsync(
-      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
-    );
+    let plyerInputOneorTwo = await Console.readLineAsync(AKS_RESTART_MASSAGE);
     let restartOrEnd = plyerInputOneorTwo
       .split('')
       .map((number) => parseInt(number));
@@ -124,7 +121,7 @@ class App {
       restartOrEnd.length !== 1 ||
       (restartOrEnd[0] !== 1 && restartOrEnd[0] !== 2)
     ) {
-      throw new Error('[ERROR]');
+      throw new Error(ERROR_MESSAGE);
     }
     return restartOrEnd[0]; // 반환값을 숫자로 변경
   }
