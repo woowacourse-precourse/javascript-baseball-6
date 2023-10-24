ed6702655e805e7f3cb6593eb1068e941d49ab0e
swarvy <swarvy0826@naver.com>
2023-10-24 12:28:29 +0900

    feat : Player Class Test 작성

---
+++ __tests__/model/PlayerTest.js
@@ -0,0 +1,13 @@
+import Player from '../../src/model/Player.js';
+
+describe('Player Class Test', () => {
+  it.each(['123', '456', '789'])('Input Split Test', (input) => {
+    // given
+    const player = new Player();
+
+    // when & then
+    player.setSelectNumber(input);
+    const result = player.getSelectNumber();
+    expect(result.size).toEqual(3);
+  });
+});
