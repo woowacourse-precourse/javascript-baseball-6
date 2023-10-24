49bcf8c4292dda46644b552363dfc52581fb9627
[NDCC] Park jee-hoon <jh.m02h.park@misumi.co.kr>
2023-10-24 10:09:24 +0900

    feat : 문자열 상수화를 위한 파일 생성

---
+++ src/comm/text.js
@@ -0,0 +1,9 @@
+export const TEXT = {
+    INPUT_NUMBER: '숫자를 입력해주세요 : ',
+    INPUT_ERROR: '[ERROR] 숫자가 잘못된 형식입니다.',
+    GAME_OVER: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+    NOTHING: '낫싱',
+    RESTART_YN: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+    STRIKE: '스트라이크',
+    BALL: '볼'
+}
