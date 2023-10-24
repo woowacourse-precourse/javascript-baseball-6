7ef2016045e615e7e528acfcc60da028265630d0
hs <tlsgustn93@naver.com>
2023-10-24 13:17:57 +0900

    userInput

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App {
     // 시작하는 부분의 로직
     Console.print(PROGRESS_MESSAGE.GAME_START);
     this.computerAnswer();
+    console.log(this.computerInput);
     return this.numberCompare();
   }
 
@@ -21,6 +22,17 @@ class App {
 
     // 비교하기 편하게 배열로 되어있는 값을 하나로 합칩니다.
     this.computerInput = computerInput.join("");
+  }
+
+  // 유저가 입력한 값을 받아옵니다.
+
+  async userAnswer() {
+    const userInput = await Console.readLineAsync(
+      PROGRESS_MESSAGE.INPUT_NUMBER
+    );
+
+    if (!this.isValid(userInput)) throw new Error("[ERROR]");
+    return userInput;
   }
 }
 
