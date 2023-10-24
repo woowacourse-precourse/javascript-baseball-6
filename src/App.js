10082b37d15b2c423f47f086bfc4681bfdc53a51
Daeeui Kim <kimeodml@gmail.com>
2023-10-24 21:55:16 +0900

    feat: 게임 시작 문구 출력 및 생성자 셋팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console, Random }  from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+
+  constructor() {
+    this.input = [];
+    this.computer = [];
+  }
+
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
 }
+
+const app = new App();
+app.play();
+
 
 export default App;
