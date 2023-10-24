f682fac53627eda3a7836c40c74354db071440e0
soyoung125 <sypark041754@gmail.com>
2023-10-24 21:00:35 +0900

    refactor: 함수 분리

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  
+
   constructor() {
-    this.numbers = [];
+    this.computer = [];
+    this.player = [];
   }
 
   /**
@@ -11,55 +12,62 @@ class App {
    */
   getRandomNumbers() {
     const numbers = new Set();
-    
+
     while (numbers.size < 3) {
       numbers.add(MissionUtils.Random.pickNumberInRange(1, 9));
     }
-    
-    this.numbers =  [...numbers];
+
+    this.computer = [...numbers];
   }
 
   /**
-   * 플레이어가 컴퓨터가 생성한 숫자를 맞출 떄까지 3자리 숫자를 입력받는다.
+   * 플레이어가 숫자를 맞출 떄까지 숫자를 입력받고 비교한다.
+   */
+  async guessNumber() {
+    while (true) {
+      await this.inputNumbers();
+
+      if (this.compareNumbers()) {
+        break;
+      }
+    }
+
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  }
+
+  /**
+   * 플레이어에게 3자리 숫자를 입력받는다.
    */
   async inputNumbers() {
     const numericRegex = /^[0-9]+$/;
 
-    while(true) {
-      const input = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
-      
-      if (input.length !== 3){ // 3자 이내의 숫자를 입력한 경우 오류 발생
-        throw new Error("[ERROR] 세 자리의 숫자를 입력해야 합니다.");
-      }
-      if (!numericRegex.test(input)){ // 문자가 포함된 경우 오류 발생
-        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
-      }
+    const input = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+    const numbers = input.split("");
 
-      if (this.compareNumbers(input)){
-        break;
-      }
+    if (input.length !== 3) { // 3자 이내의 숫자를 입력한 경우 오류 발생
+      throw new Error("[ERROR] 세 자리의 숫자를 입력해야 합니다.");
     }
-  }
-
-  /**
-   * 플레이어 입력한 숫자를 컴퓨터가 생성한 숫자와 비교하여 힌트를 출력하고 게임 종료 여부를 반환합니다.
-   * @param {string} inputNumbers 플레이어가 3자리 입력한 숫자
-   * @returns {boolean} 게임 종료 여부
-   */
-  compareNumbers(inputNumbers) {
-    const numbers = inputNumbers.split("");
-    let strike = 0;
-    let ball = 0;
-
-    if (new Set(numbers).size !== 3){ // 중복된 숫자가 있는 경우
+    if (!numericRegex.test(input)) { // 문자가 포함된 경우 오류 발생
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+    if (new Set(numbers).size !== 3) { // 중복된 숫자가 있는 경우
       throw new Error("[ERROR] 중복된 숫자가 존재합니다.");
     }
 
-    numbers.map((n, index) => {
-      const number = parseInt(n);
+    this.player = numbers.map(n => parseInt(n));
+  }
 
-      if (this.numbers.includes(number)) {
-        if (this.numbers[index] === number) {
+  /**
+   * 플레이어 입력한 숫자를 컴퓨터가 생성한 숫자와 비교하여 게임 종료 여부를 반환합니다.
+   * @returns {boolean} 게임 종료 여부
+   */
+  compareNumbers() {
+    let strike = 0;
+    let ball = 0;
+
+    this.player.forEach((n, index) => {
+      if (this.computer.includes(n)) {
+        if (this.computer[index] === n) {
           strike += 1;
         } else {
           ball += 1;
@@ -67,9 +75,23 @@ class App {
       }
     })
 
+    this.printHint(strike, ball);
+
+    if (strike === 3) {
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * 비교 결과 힌트를 출력합니다.
+   * @param {Number} strike 
+   * @param {Number} ball 
+   */
+  printHint(strike, ball) {
     if (strike === 3) {
       MissionUtils.Console.print(`${strike}스트라이크`);
-      return true;
     }
 
     if (strike === 0) {
@@ -85,19 +107,15 @@ class App {
         MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
       }
     }
-
-    return false;
   }
 
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 
-    while(true) {
+    while (true) {
       this.getRandomNumbers(); // 랜덤 숫사 배열 생성
-      
-      await this.inputNumbers(); // 플레이어 숫자 입력
-      
-      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+      await this.guessNumber(); // 숫자 맞추기
 
       const input = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
