4f4cbe4a45e07f9ba18c3330be771ca06d6915d6
limdumb <dlaruddls@nate.com>
2023-10-24 15:37:32 +0900

    [Refact] Restart 조건 변경

--- src/function/restartChecked.js
+++ src/function/restartChecked.js
@@ -2,8 +2,11 @@ import { Console } from "@woowacourse/mission-utils";
 
 const restartChecked = async () => {
   const inputValue = await Console.readLineAsync("");
-  
-  if (Number(inputValue) > 2 || isNaN(Number(inputValue))) {
+
+  if (
+    (Number(inputValue) === 1 && Number(inputValue) === 2) ||
+    isNaN(Number(inputValue))
+  ) {
     throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
   }
 
