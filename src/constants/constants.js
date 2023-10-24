45568638ca904a00792fbfdec5a7da8ab23b56bb
mieum12 <qpdlqltb1215@gmail.com>
2023-10-24 19:29:44 +0900

    feat: 메세지 상수화하기

---
+++ src/constants/constants.js
@@ -0,0 +1,14 @@
+export const CONSTANTS = {
+  MESSAGES: {
+    START: "숫자 야구 게임을 시작합니다.",
+    END: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+    RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+    EXIT: "게임을 종료합니다.",
+  },
+  ERROR: {
+    TYPE_VALID: "[ERROR] 숫자를 입력해주세요.",
+    LENGTH_VALID: "[ERROR] 숫자 3자리를 입력해주세요.",
+    RANGE_VALID: "[ERROR] 1~9까지의 숫자를 입력해주세요.",
+    DUPLICATE_VALID: "[ERROR] 중복되지 않은 숫자를 입력해주세요.",
+  },
+};
