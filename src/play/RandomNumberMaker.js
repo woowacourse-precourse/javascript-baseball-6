ae5fde8ab28b016cb3aa00a33168302a918af588
hyj5076 <hyj5076@hanmail.net>
2023-10-24 22:33:08 +0900

    refactor: 랜덤 수 생성 기능 로직 수정

--- src/play/RandomNumberMaker.js
+++ src/play/RandomNumberMaker.js
@@ -1,18 +1,17 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class RandomNumberMaker {
-  constructor(randomNumber = '') {
-    this.randomNumber = randomNumber;
-  }
-
   makeRandomNumber() {
-    while (this.randomNumber.length < 3) {
-      const newNumber = MissionUtils.Random.pickNumberInRange(1, 9).toString();
-      if (!this.randomNumber.includes(newNumber)) {
-        this.randomNumber += newNumber;
+    const computer = [];
+
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
       }
     }
-    return this.randomNumber;
+
+    return computer.join('');
   }
 }
 
