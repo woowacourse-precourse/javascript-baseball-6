7cde833ad60b49dffb33b50935d2a8e8cab181f8
sehyun <e6d1fe@gmail.com>
2023-10-24 15:45:29 +0900

    feat: generateNumbers 기능

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Console, MissionUtils } from '@woowacourse/mission-utils';
 
+import { message, NUM_LOWER_LIMIT, NUM_UPPER_LIMIT } from './Constants.js';
+
 class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
   }
-}
 
-// console.log(MissionUtils.Random.pickNumberInRange(1, 9));
+  async generateNumbers() {
+    const numbers = [];
+    while (numbers.length < 3) {
+      const newNumber = MissionUtils.Random.pickNumberInRange(NUM_LOWER_LIMIT, NUM_UPPER_LIMIT);
+      if (!numbers.includes(newNumber)) numbers.push(newNumber);
+    }
+    Console.print(numbers);
+  }
+}
 
 const app = new App();
 app.play();
