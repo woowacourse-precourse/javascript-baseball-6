49e30e8dcbc6bb858fa45b21f04a4fe407a43183
Si Hyun Lee <pongpongi@Pongpongbook.local>
2023-10-24 21:56:16 +0900

    :sparkles: Function for computer and user answer added
    printUserAnswer(), pushComputerNumber() added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import { MissionUtils, Random } from '@woowacourse/mission-utils';
+import { Console, MissionUtils, Random } from '@woowacourse/mission-utils';
 class App {
   async play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    MissionUtils.Console.readLineAsync()
+    pushComputerNumber();
+    printUserAnswer();
   }
 }
 
+class Computer {}
 
 // 컴퓨터가 랜덤으로 숫자를 고름
+const computer = [];
 
-class Computer {
-  constructor(number1, number2) {
-    this.number1 = push(Random.pickNumberInRange(1,10))
-    this.number2 = push(Random.pickNumberInRange(1,10))
-
+function pushComputerNumber() {
+  for (let i = 1; i < 4; i++) {
+    number = Random.pickNumberinRange(1, 10);
+    computer.push(number);
+  }
+}
+// 사용자의 대답 출력하기
+async function printUserAnswer() {
+  try {
+    await Console.readLineAsync('숫자를 입력해주세요: ');
+  } catch (error) {
+    // reject 되는 경우 error 출력
+    Console.print('[ERROR] ');
   }
 }
 
 // 서로 다른 세 자리 수 입력받기
-
-
 
 // 결과 출력하기
 
