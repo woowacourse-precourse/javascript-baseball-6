5defac996d1d30ab59aee9696ede4c075094fce0
today-is-first <diligent0644@gmail.com>
2023-10-24 21:46:12 +0900

    재시작

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { generateRandomBallNumber } from './Ball.js';
-import { messages } from './Message.js';
-import * as game from './Game.js';
 
 class App {
   constructor() {
-    this.answer = generateRandomBallNumber();
-    this.gameMode = 0;
   }
   async play() {
-    await MissionUtils.Console.print(messages.GAME_START);
-    await game.gameStart();
   }
 }
-
-export const app = new App();
-
-app.play();
 
 export default App;
