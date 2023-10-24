ac55fbfc5bba01f6757f5a37fe8034326c45f067
getsetgo1 <dnwls1313@gmail.com>
2023-10-24 20:34:05 +0900

    파일 정리

--- src/App.js
+++ src/App.js
@@ -94,17 +94,4 @@ class App {
 export default App;
 const app = new App();
 app.play();
-// RANDOM
-// 랜덤 숫자 생성
 
-// getNum()
-// 입력 받기
-// 입력 받은 것 예외 판단(3자리 아닌 것 또는 숫자 아닌 것 입력)
-
-// estimateScore()
-// 스트라이크 판단
-// 볼 낫싱 판단
-// 스트라이크 볼 출력
-
-// GameEnd = false 함수로 해야됨
-// 게임 종료 시 재시작 및 종료
