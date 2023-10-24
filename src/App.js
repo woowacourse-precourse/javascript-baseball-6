2479b0b9f1fe4f34d6305505a706c1085d1b2d0c
sojeong0302 <01duqrl@naver.com>
2023-10-24 23:03:14 +0900

    랜덤으로 3개의 숫자

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    console.log("숫자 야구 게임을 시작합니다.");
+    this.computerNumber();//숫자 출력해보기
+  }
+
+  computerNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    console.log("컴퓨터 숫자", computer);
+  }
 }
 
 export default App;
+
+//실행 시키기
+const app = new App();
+app.play();
