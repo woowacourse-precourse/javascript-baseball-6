32a863417aa4a5d308b1839e8a888c80c7cbc959
pakxe <pigkill40@naver.com>
2023-10-25 03:43:24 +0900

    feat(GameConsole): 재시작 커맨드의 유효성 검사를 위한 클래스 작성

---
+++ src/Model/GameConsole.js
@@ -0,0 +1,16 @@
+import ERROR from '../constants/Error.js';
+import GAME from '../constants/Game.js';
+import RestartCommandError from '../error/RestartCommandError.js';
+import isNan from '../utils/isNan.js';
+
+class GameConsole {
+	static isValidRestartCommand(input) {
+		if (isNan(input)) throw new RestartCommandError(ERROR.isNan);
+		if (parseInt(input) !== GAME.restart && parseInt(input) !== GAME.exit)
+			throw new RestartCommandError(ERROR.invalidRestartCommand);
+
+		return parseInt(input); // 형태 변화도 같이 해서 준다. 단순한 변환이므로 함께했다.
+	}
+}
+
+export default GameConsole;
