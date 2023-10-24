15108eccbb5afc2ee201f7f887c9b9671b075507
KangYunJi <dbswl701@ajou.ac.kr>
2023-10-24 18:21:51 +0900

    feat: 사용자는 게임이 끝났을 경우, 재시작과 종료를 구분하는 1, 2를 입력할 수 있다.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ class App {
   constructor() {
     this.strike = 0;
     this.ball = 0;
-
     // 컴퓨터는 1부터 9까지 서로 다른 임의의 수 3개를 선택한다.
     this.computer = [];
     while (this.computer.length < 3) {
@@ -17,8 +16,9 @@ class App {
   }
 
   async play() {
-    let gameNum = 1;
-    while(gameNum === 1) {
+    let gameNum = 0;
+    let isEnd = false;
+    while(!isEnd) {
       this.strike = 0;
       this.ball = 0;
       try {
@@ -54,6 +54,10 @@ class App {
         } else if (this.ball === 0 && this.strike > 0) {
           MissionUtils.Console.print(`${this.strike}스트라이크`);
           MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
+          gameNum = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+          if (gameNum === 1) isEnd = false;
+          else if(gameNum === 2) isEnd = true;
+          console.log('isEnd?: ', isEnd, 'gameNum', gameNum);
         } else {
           MissionUtils.Console.print(`낫싱`);
         }
