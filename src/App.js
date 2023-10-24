e02a16fea0580364681f88d18a6ecdcec4866ee6
An-jihyun <anjh322@gmail.com>
2023-10-25 05:22:18 +0900

    Feat: 랜덤 값 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-
+  getRandomNumber() {
+    let computerNum = [];
+    let tempNum = 0;
+    while (computerNum.length < 3) {
+      tempNum = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerNum.includes(tempNum)) {
+        computerNum.push(tempNum);
+      }
+    }
+    return computerNum;
+  }
 
   async play() {}
 }
