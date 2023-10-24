3f56a142bfa4fc10805492c3c2e9fd010557546c
sihyun10 <hyhy200170@gmail.com>
2023-10-25 00:39:12 +0900

    test: 컴퓨터 정답 테스트 코드 추가 랜덤으로 컴퓨터 정답을 생성해주는 generateComputerAnswer.js 파일을 테스트해보기 위해 테스트코드를 작성해보았습니다. 이 함수가 answerLength 길이의 배열을 반환하는지. 반환된 배열의 값들이 1~9 사이에 있는지. 마지막으로 중복값은 없는지 테스트해보았습니다.

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,6 @@
 import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { generateComputerAnswer } from "../src/generateComputerAnswer.js";
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -24,6 +25,22 @@ const getLogSpy = () => {
 };
 
 describe("숫자 야구 게임", () => {
+  test("컴퓨터 정답", () => {
+    const answerLength = 3;
+    const answer = generateComputerAnswer(answerLength);
+  
+    expect(Array.isArray(answer)).toBe(true);
+    expect(answer).toHaveLength(answerLength);
+  
+    answer.forEach((num) => {
+      expect(num).toBeGreaterThanOrEqual(1);
+      expect(num).toBeLessThanOrEqual(9);
+    });
+
+    const uniqueAnswer = new Set(answer);
+    expect(uniqueAnswer.size).toEqual(answer.length);
+  });
+
   test("게임 종료 후 재시작", async () => {
     // given
     const randoms = [1, 3, 5, 5, 8, 9];
