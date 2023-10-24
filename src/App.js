4e39a6be5f5b142bd5bfae7056461e459f6105cb
jw0097 <76507017+jw0097@users.noreply.github.com>
2023-10-24 12:50:44 +0900

    feat: implement code for restart

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import Computer from "./domain/Computer.js";
 import AnswerComparator from "./gameUtils/AnswerComparator.js";
 
 const computer = new Computer();
+let restartInput;
 class App {
   async play() {
     //시작 문구를 출력한다.
     OutputProcessor.output("숫자 야구 게임을 시작합니다.");
-    //정답을 생성한다.
-    App.#generateAnswer();
-    //사용자에게 정답을 입력받아 결과를 확안한다.
-    let result = false;
-    while (!result) {
-      result = await App.#checkAnswer();
-    }
-    //정답 문구를 출력한다.
-    OutputProcessor.output("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    do {
+      //정답을 생성한다.
+      App.#generateAnswer();
+      //사용자에게 정답을 입력받아 결과를 확안한다.
+      let result = false;
+      while (!result) {
+        result = await App.#checkAnswer();
+      }
+      //정답 문구를 출력한다.
+      OutputProcessor.output("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      //재시작 여부를 입력받는다.
+      restartInput = await App.#checkRestart();
+    } while (restartInput === "1");
   }
 
   static #generateAnswer() {
@@ -35,6 +40,10 @@ class App {
     if (result.strike === 3) booleanResult = true;
 
     return booleanResult;
+  }
+
+  static async #checkRestart() {
+    return await InputProcessor.restartInput();
   }
 }
 
