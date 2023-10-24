4f9cda7c1ebf239b93320019d8aade90dff62b96
고세종 <fulfilled7418@gmail.com>
2023-10-24 18:28:50 +0900

    style: 변경 코드 위치 변경 / refactor: 리팩토링 삼항연산자를 사용한 return 하나의 조건문으로 변경

--- src/App.js
+++ src/App.js
@@ -1,10 +1,95 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 
-class App {
-  async play() {
-    await playBaseBall();
+const checkGameIsEnd = (userInput) => {
+  if (userInput === '1') {
+    return false;
   }
-}
+  if (userInput === '2') {
+    return true;
+  }
+
+  return { isError: true };
+};
+
+const printScore = (scoreBoard) => {
+  const returnString =
+    scoreBoard.strike !== 0 || scoreBoard.ball !== 0
+      ? `${scoreBoard.ball === 0 ? '' : `${scoreBoard.ball}볼`} ${
+          scoreBoard.strike === 0 ? '' : `${scoreBoard.strike}스트라이크`
+        }`.trim()
+      : '낫싱';
+
+  return returnString;
+};
+
+const getUserInputForGameSet = async (scoreBoard) => {
+  Console.print(printScore(scoreBoard));
+  Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료.');
+  Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+  const userInput = await Console.readLineAsync('');
+
+  return checkGameIsEnd(userInput);
+};
+
+const makeRandomNumber = () => {
+  const randomNumber = [];
+  while (randomNumber.length < 3) {
+    const number = Random.pickNumberInRange(1, 9);
+    if (!randomNumber.includes(number)) {
+      randomNumber.push(number);
+    }
+  }
+
+  return randomNumber;
+};
+
+const checkIsNumber = (userInput) => {
+  const input = userInput.filter((number) => Number.isNaN(number));
+
+  return input.length === 0;
+};
+
+const checkIsNonDuplicated = (userInput) => {
+  const input = userInput
+    .sort()
+    .reduce((acc, cur) => (acc.includes(cur) ? acc : [...acc, cur]), []);
+
+  return input.length === 3;
+};
+
+const checkInputIsValid = (userInput) => {
+  const input = [...userInput];
+
+  const isNumber = checkIsNumber(input);
+  const isNonDuplicated = checkIsNonDuplicated(input);
+
+  return isNumber && isNonDuplicated ? true : false;
+};
+
+const calculateScore = (userInput, randomNumber) => {
+  const socreBoard = { strike: 0, ball: 0 };
+
+  randomNumber.forEach((number, randomIndex) => {
+    const userInputIndex = userInput.findIndex(
+      (inputNum) => inputNum === number
+    );
+
+    if (userInputIndex === randomIndex) {
+      socreBoard.strike += 1;
+    } else if (userInputIndex !== -1) {
+      socreBoard.ball += 1;
+    }
+  });
+
+  return socreBoard;
+};
+
+const getUserInput = async () => {
+  const userInput = await Console.readLineAsync('숫자를 입력해주세요 : ');
+  const input = Array.from(userInput, (number) => Number.parseInt(number, 10));
+
+  return input;
+};
 
 const playBaseBall = async () => {
   let randomNumber = makeRandomNumber();
@@ -38,96 +123,10 @@ const playBaseBall = async () => {
   }
 };
 
-const getUserInputForGameSet = async (scoreBoard) => {
-  Console.print(printScore(scoreBoard));
-  Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료.');
-  Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
-  const userInput = await Console.readLineAsync('');
-
-  return checkGameIsEnd(userInput);
-};
-
-const makeRandomNumber = () => {
-  const randomNumber = [];
-  while (randomNumber.length < 3) {
-    let number = Random.pickNumberInRange(1, 9);
-    if (!randomNumber.includes(number)) {
-      randomNumber.push(number);
-    }
+class App {
+  async play() {
+    await playBaseBall();
   }
-
-  return randomNumber;
-};
-
-const checkInputIsValid = (userInput) => {
-  const input = [...userInput];
-
-  const isNumber = checkIsNumber(input);
-  const isNonDuplicated = checkIsNonDuplicated(input);
-
-  return isNumber && isNonDuplicated ? true : false;
-};
-
-const checkIsNumber = (userInput) => {
-  const input = userInput.filter((number) => Number.isNaN(number));
-
-  return input.length === 0 ? true : false;
-};
-
-const checkIsNonDuplicated = (userInput) => {
-  const input = userInput
-    .sort()
-    .reduce((acc, cur) => (acc.includes(cur) ? acc : [...acc, cur]), []);
-
-  return input.length === 3 ? true : false;
-};
-
-const calculateScore = (userInput, randomNumber) => {
-  const socreBoard = { strike: 0, ball: 0 };
-
-  randomNumber.forEach((number, randomIndex) => {
-    const userInputIndex = userInput.findIndex(
-      (inputNum) => inputNum === number
-    );
-
-    if (userInputIndex === randomIndex) {
-      socreBoard.strike += 1;
-    } else if (userInputIndex != -1) {
-      socreBoard.ball += 1;
-    }
-
-    return;
-  });
-
-  return socreBoard;
-};
-
-const printScore = (scoreBoard) => {
-  let returnString =
-    scoreBoard.strike != 0 || scoreBoard.ball != 0
-      ? `${scoreBoard.ball === 0 ? '' : scoreBoard.ball + '볼'} ${
-          scoreBoard.strike === 0 ? '' : scoreBoard.strike + '스트라이크'
-        }`.trim()
-      : '낫싱';
-
-  return returnString;
-};
-
-const getUserInput = async () => {
-  const userInput = await Console.readLineAsync('숫자를 입력해주세요 : ');
-  const input = Array.from(userInput, (number) => parseInt(number));
-
-  return input;
-};
-
-const checkGameIsEnd = (userInput) => {
-  if (userInput === '1') {
-    return false;
-  } else if (userInput === '2') {
-    return true;
-  }
-
-  return { isError: true };
-};
+}
 
 export default App;
