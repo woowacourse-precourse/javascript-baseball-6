be16c0d8a8f8e97a86c8dffd9497a67cd90b2a60
stegano9744 <stegano9744@gmail.com>
2023-10-24 22:38:25 +0900

    🚀 perf: 반복문 시간 복잡도 개선
    파일 경로: src/utils/getBallsAndStrikes.js
    이전에는 이중 반복문을 사용하여 스트라이크와 볼을 계산했지만, Set으로 중복을 확인하고, 단일 반복문으로 성능을 향상시켰습니다.
    
    시간 복잡도: O(n²) → O(n)
    공간 복잡도: O(1) → O(n)

--- src/utils/getBallsAndStrikes.js
+++ src/utils/getBallsAndStrikes.js
@@ -1,11 +1,9 @@
 // 출력값 계산
 export const getBallsAndStrikes = (computer, user, baseball) => {
+  const userSet = new Set(user.split('').map(Number));
   for (let i = 0; i < 3; i++) {
-    if (computer[i] === Number(user[i])) {
-      baseball['strike']++;
-    }
-    for (let j = 0; j < 3; j++)
-      if (i !== j && computer[i] === Number(user[j])) baseball['ball']++;
+    if (computer[i] === Number(user[i])) baseball['strike']++;
+    else if (userSet.has(computer[i])) baseball['ball']++;
   }
   return baseball;
 };
