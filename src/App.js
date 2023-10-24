8aa05b63366da19e6d35fd5b7f9e375932aeea14
j1hwan <jihwan@pm.me>
2023-10-24 15:17:41 +0900

    feat: App 클래스에 게임 모듈 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import PlayBaseballGame from './PlayBaseball.js';
+
 class App {
-  async play() {}
+  async play() {
+    const game = new PlayBaseballGame();
+    await game.start();
+  }
 }
 
 export default App;
