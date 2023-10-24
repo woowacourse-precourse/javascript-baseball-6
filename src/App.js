0a137c27b04201a880c9f12779ff9905189b11cd
Motoong <sonada903@gmail.com>
2023-10-24 14:01:20 +0900

    3자리의 컴퓨터 숫자 난수 생성, 게임시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { Console, MissionUtils, Random } from "@woowacourse/mission-utils";
+
+function getRandomNumber() {
+  // 컴퓨터 숫자 생성 
+  let comNumbers = [];
+  for (let i = 0; i < 3; i++) {
+    let randomNumber = Random.pickNumberInRange(1, 9)
+    while (comNumbers.includes(randomNumber)) {
+      randomNumber = Random.pickNumberInRange(1, 9)
+    }
+    comNumbers.push(randomNumber);
+  }
+
+  return comNumbers.join("");
+}
 class App {
   async play() {
-  }
+
+    let COM_NUMBER = getRandomNumber()
+
+    Console.print("숫자 야구 게임을 시작합니다.")
+    
+    }
 }
 
-export default App;
 
+export default App;
