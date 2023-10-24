b27ac78c535916c7a182da6537a1d8171a6e178f
isy <dbswl330@gmail.com>
2023-10-25 01:45:26 +0900

    feat: 플레이어의 3자리 숫자 입력 함수 구현

--- src/GameView.js
+++ src/GameView.js
@@ -4,4 +4,9 @@ class GameView {
   printStartGame() {
     Console.print("숫자 야구 게임을 시작합니다.");
   }
+
+  async getPlayerGuess() {
+    const inputNumbers = await Console.readLineAsync("숫자를 입력해주세요 : ");
+    return inputNumbers;
+  }
 }
