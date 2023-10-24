13e97a7c0ea640896362282a2d888c7e5452f603
yeong-hwan <jghff700@naver.com>
2023-10-25 04:12:29 +0900

    refactor: [기능2, 4] User Input Validation 분리 User Input을 받는 함수들이 자체적으로 Validation 검증하도록 분리 JSDoc 비동기 반환값 수정

--- src/input/getRestartDecision.js
+++ src/input/getRestartDecision.js
@@ -3,18 +3,16 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 /**
  * 게임 재시작 여부를 반환한다
  * 
- * @returns {boolean}
+ * @returns {Promise<boolean>}
  * @throws 사용자가 잘못된 값을 입력했을 경우 throw Error
  */
 export default async function getRestartDecision() {
   const userInput = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
 
-  try {
-    if (userInput !== "1" && userInput !== "2"){
-      throw new Error("잘못된 값을 입력했습니다.");
-    }
-  } catch (err){
-    MissionUtils.Console.print("Error : " + err.message);
+
+  if (userInput !== "1" && userInput !== "2"){
+    MissionUtils.Console.print("잘못된 값을 입력했습니다.");
+    throw new Error("[ERROR]");
   }
 
   const restartDecision = (userInput === "1" ? true : false);
--- src/input/getUserInput.js
+++ src/input/getUserInput.js
@@ -1,12 +1,21 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import checkInputValid from "../input/checkInputValid.js";
 
 /**
  * 터미널로 유저에게 input을 입력받아 전달한다.
  * 
- * @returns {string}
+ * @returns {Promise<string>}
+ * @throws 사용자가 잘못된 값을 입력했을 경우 throw Error
  */
 export default async function getUserInput() {
   const userInput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
   
+  if (!checkInputValid(userInput)) {
+    MissionUtils.Console.print("잘못된 값을 입력했습니다.");
+    throw new Error("[ERROR]");
+  }
+
   return userInput;
 }
+
+
