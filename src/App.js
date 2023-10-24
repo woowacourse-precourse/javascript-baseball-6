8d7c043392b6a3d997977c2aab8504b2b6bad71c
Kled <1017yu@daum.net>
2023-10-24 15:12:10 +0900

    refactor: ViewOutput 호출 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { Console } from '@woowacourse/mission-utils';
-import { GUIDE_MESSAGES } from '../constants/index.js';
 import Player from './player/index.js';
+import ViewOutput from './view/viewOutput.js';
+import { GUIDE_MESSAGES } from '../constants/index.js';
 
-//숫자야구 어플리케이션의 메인 클래스
+// 숫자야구 어플리케이션의 메인 클래스
 class App {
   #player = new Player();
 
   /**
+   * 🖥️ '숫자 야구 게임을 시작합니다.' 메시지와 함께 player 인스턴스를 호출한다.
    * @async
-   * @returns {void}
+   * @returns
    */
   async play() {
-    Console.print(GUIDE_MESSAGES.START);
+    ViewOutput.printMessage(GUIDE_MESSAGES.START);
     await this.#player.startGame();
   }
 }
