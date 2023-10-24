d4ee9e2acd84eeeb688ae58fc39f4849a503c1e5
luna <luna@rtzr.ai>
2023-10-24 10:54:21 +0900

    feat: 게임 실행 및 종료 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,61 @@
+import { Console } from "@woowacourse/mission-utils";
+import BaseballGame from "./BaseballGame.js";
+import GameInteraction from "./GameInteraction.js";
+import MESSAGES from "./Messages.js";
+
 class App {
-  async play() {}
+  constructor() {
+    this.baseballGame = new BaseballGame();
+  }
+  async play() {
+    Console.print(MESSAGES.MSG_START);
+    let isGameOver = false;
+
+    while (!isGameOver) {
+      this.baseballGame.createComputerNumbers();
+      isGameOver = await this.playRound();
+    }
+  }
+
+  async playRound() {
+    let isRoundOver = false;
+
+    while (!isRoundOver) {
+      try {
+        const userNumbers = await GameInteraction.inputUserNumbers();
+        const result = this.baseballGame.calculateBallAndStrike(userNumbers);
+        GameInteraction.printResult(result);
+
+        if (result.strike === 3) {
+          Console.print(MESSAGES.MSG_END);
+          isRoundOver = true;
+          return await this.askToRestartOrExit();
+        }
+      } catch (error) {
+        Console.print(error.message);
+        throw error;
+      }
+    }
+  }
+
+  async askToRestartOrExit() {
+    try {
+      const answer = await GameInteraction.askToRestartGame();
+
+      if (answer === "1") {
+        this.baseballGame.resetComputerNumbers();
+        return false;
+      } else if (answer === "2") {
+        return true;
+      }
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
