4508a5aea5a7328bb579d200ad00030930b5d7aa
ChanYong Park <krap5075@naver.com>
2023-10-25 02:11:12 +0900

    feat: 숫자 입력 예외 함수 구현

--- src/App.js
+++ src/App.js
@@ -13,6 +13,29 @@ class App {
     }
     return computer;
   }
+  // 입력이 숫자인지 확인
+  checkUserInputNumber(input) {
+    const numInput = parseInt(input, 10);
+    if (isNaN(numInput)) {
+      return false;
+    }
+    return true;
+  }
+  // 입력된 숫자가 3자리인지 확인
+  checkUserInputLength(input) {
+    if (input.length !== 3) {
+      return false;
+    }
+    return true;
+  }
+  // 입력된 숫자가 중복안되는지 확인
+  checkUserInputDup(input) {
+    const inputSet = new Set(input);
+    if (inputSet.size !== input.length) {
+      return false;
+    }
+    return true;
+  }
 }
 
 const app = new App();
