cccb995e34c4a5e0a15d48c167699a43ed9053e3
song-eun <dmswo546@naver.com>
2023-10-24 15:20:32 +0900

    feat: 예외처리 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 const { MissionUtils } = require('@woowacourse/mission-utils');
 
-async function getPlayerNumbers() {
+function inputErrorHandle() {
+  throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+}
+
+function isValidNumber(str) {
+  if (str.length !== 3) return false;
+  if (!isNaN(Number(str))) return false;
+
+  let set = new Set(str);
+  if (set.size !== 3) return false;
+  return true;
+}
+
+async function getPlayerNumber() {
   // try {
   const playerNumber = await MissionUtils.Console.readLineAsync(
     '숫자를 입력해주세요 : '
@@ -20,8 +33,8 @@ class App {
   async play() {
     // 게임 시작 문구 출력
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    const playerPromise = getPlayerNumbers();
-    const playerNumbers = await playerPromise;
+    const playerPromise = getPlayerNumber();
+    const playerNumber = await playerPromise;
   }
 }
 
