ed963ef3f630c5c5a107d9da318dcfaabf38b5d3
JungHun98 <j7papa@naver.com>
2023-10-24 11:35:33 +0900

    refactor: RESTART_PROMPT 상수 제거 airbnb style guide Naming Conventions 23.10

--- src/modules/Constant.js
+++ src/modules/Constant.js
@@ -12,8 +12,6 @@ const NOTHING = '낫싱';
 const START_PROMPT = '숫자 야구 게임을 시작합니다.';
 const EXIT_PROMPT = '게임 종료';
 const INPUT_PROMPT = '숫자를 입력해주세요 : ';
-const RESTART_PROMPT = `${MAX_NUM_LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료
-게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`;
 
 const Constant = {
   MAX_NUM_LENGTH,
@@ -25,7 +23,6 @@ const Constant = {
   START_PROMPT,
   EXIT_PROMPT,
   INPUT_PROMPT,
-  RESTART_PROMPT,
 };
 
 export default Constant;
