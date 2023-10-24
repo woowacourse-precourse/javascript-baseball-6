18a4e89583086985595f38195d86846710117871
apfwnls14 <apfwnls14@naver.com>
2023-10-24 10:58:46 +0900

    fix: 0이나 숫자가 아닌 값이 들어왔을 경우 예외처리 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Random, Console } from "@woowacourse/mission-utils";
+import { Random, Console } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
-    Console.print("숫자 야구 게임을 시작합니다.");
+    Console.print('숫자 야구 게임을 시작합니다.');
     const computer = this.computerNumbers();
     await this.start(computer);
   }
@@ -18,16 +18,19 @@ class App {
   }
   async start(computer) {
     while (1) {
-      const number = await Console.readLineAsync("숫자를 입력해주세요 : ");
-      const user = number.split("").map(Number);
+      const number = await Console.readLineAsync('숫자를 입력해주세요 : ');
+      const user = number.split('').map(Number);
       if (user.length !== 3 || number === null || new Set(user).size !== 3) {
-        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+        throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+      }
+      if (user.includes(0) || user.includes(NaN)) {
+        throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
       }
       const { strike, ball } = this.hint(user, computer);
       this.displayHint(strike, ball);
       if (strike === 3) {
-        Console.print("3스트라이크");
-        Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        Console.print('3스트라이크');
+        Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
         break;
       }
     }
@@ -50,7 +53,7 @@ class App {
   }
   displayHint(strike, ball) {
     if (strike === 0 && ball === 0) {
-      Console.print("낫싱");
+      Console.print('낫싱');
     } else if (ball > 0 && strike === 0) {
       Console.print(`${ball}볼`);
     } else if (strike > 0 && ball === 0) {
@@ -61,9 +64,9 @@ class App {
   }
   async replay() {
     const replay = await Console.readLineAsync(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
     );
-    if (replay === "1") {
+    if (replay === '1') {
       await this.play();
     }
   }
