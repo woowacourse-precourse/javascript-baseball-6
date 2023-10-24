6083c03516403c5380a51f20973a627df3b97205
gloxinia106 <gloxinia106@gmail.com>
2023-10-24 22:57:43 +0900

    [기능 추가] 게임 시작 기능, 숫자 랜덤 생성 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-class App {
-  async play() {}
-}
+import { Console, Random } from "@woowacourse/mission-utils";
 
+class App {
+  constructor() {
+    this.computerNumbers = [];
+    this.userNumbers = [];
+  }
+
+  async play() {
+    this.startGame();
+  }
+
+  startGame() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    this.setComputerNumbers();
+  }
+
+  setComputerNumbers() {
+    while (this.computerNumbers.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!this.computerNumbers.includes(number)) {
+        this.computerNumbers.push(number);
+      }
+    }
+  }
+}
 export default App;
