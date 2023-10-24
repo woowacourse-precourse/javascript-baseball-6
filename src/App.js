466c3e5ba6d56a2843626617c80d517ca1052e88
badahertz52 <badahertz52@gmail.com>
2023-10-24 20:21:56 +0900

    [refactors] Move code about ball and strike to Game from App.js
    - Add new properties  and methods to Game
    - Modify App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
 import { Output, ValidTest, Player, Game } from './js';
 
 class App {
-  strike = 0;
-
-  ball = 0;
-
   validTest = new ValidTest();
 
   output = new Output();
@@ -14,85 +9,18 @@ class App {
 
   game = new Game();
 
-  /**
-   * 오류 시, 오류문을 throw하고 게임을 종료
-   * @param {*} errorMessage
-   */
-  throwError(errorMessage) {
-    this.game.over(true);
-    throw new Error(errorMessage);
-  }
-
-  /**
-   * strike, ball 초기화
-   */
-  resetStrikeAndBall() {
-    this.strike = 0;
-    this.ball = 0;
-  }
-
-  /**
-   * 입력한 숫자에 대한 스트라이크,볼 판단
-   */
-  compareNumbers() {
-    this.game.randomNumbers.forEach((v, i) => {
-      if (v === this.player.numbers[i]) {
-        this.strike += 1;
-      } else if (this.player.numbers.includes(v)) {
-        this.ball += 1;
-      }
-    });
-  }
-
-  /**
-   * 스트라이클, 볼, 낫싱 판단에 따른 결과를 표시
-   */
-  showGameResult() {
-    const isNothing = !this.strike && !this.ball;
-    if (this.strike === 3) {
-      this.game.win = true;
-      this.output.printThreeStrikeMessage();
-      this.output.printWinMessage();
-      this.output.printGameRestartMessage();
-    } else if (isNothing) {
-      this.output.printNothingMessage();
-    } else {
-      const strikeAndBall = `${this.ball ? `${this.ball}볼` : ''} ${
-        this.strike ? `${this.strike}스트라이크` : ''
-      }`;
-      this.output.printMessage(strikeAndBall);
-    }
-  }
-
-  /**
-   * 상황별, 입력한 숫자에 따른 게임 판정
-   */
-  judgeGame() {
-    if (this.game.win) {
-      const isRestart = this.player.numbers[0] === 1;
-      if (isRestart) {
-        this.game.restart();
-      } else {
-        this.game.over(true);
-      }
-    } else {
-      this.compareNumbers();
-      this.showGameResult();
-    }
-    this.resetStrikeAndBall();
-  }
-
   async play() {
     this.game.start();
     while (this.game.playing) {
-      if (!this.win) this.output.printInputMessage();
+      if (!this.game.win) this.output.printInputMessage();
       try {
         // eslint-disable-next-line
         await this.player.getNumbers();
         this.validTest.test(this.player.numbers, this.game.win);
-        this.judgeGame();
+        this.output.printMessage(this.player.numbers.join(''));
+        this.game.referee(this.player.numbers);
       } catch (error) {
-        this.throwError(`[Error]:${error}`);
+        throw new Error(`[Error]:${error}`);
       }
     }
   }
--- src/js/Game.js
+++ src/js/Game.js
@@ -8,7 +8,21 @@ class Game {
 
   randomNumbers = [];
 
+  strike = 0;
+
+  ball = 0;
+
+  score = '';
+
+  SCORE_NOTHING = '낫싱';
+
+  SCORE_THREE_STRIKE = '3스크라이크';
+
   output = new Output();
+
+  constructor() {
+    this.makeRandomNumbers();
+  }
 
   makeRandomNumbers() {
     let array = [];
@@ -25,8 +39,10 @@ class Game {
     this.randomNumbers = array;
   }
 
-  constructor() {
-    this.makeRandomNumbers();
+  resetScore() {
+    this.strike = 0;
+    this.ball = 0;
+    this.score = '';
   }
 
   reset() {
@@ -43,14 +59,78 @@ class Game {
     this.reset();
   }
 
-  /**
-   * 게임 종료
-   * @param {*} showMessage  : 게임종료 시, 이에 대한 메세지 표시 여부
-   */
-  over(showMessage) {
+  over() {
     this.playing = false;
-    this.win = false;
-    if (showMessage) this.output.printGameOverMessage();
+    this.output.printGameOverMessage();
+  }
+
+  /**
+   * 플레이어의 입력 숫자들과 랜덤 숫자들을 비교
+   * @param {number[]} playerNumbers
+   */
+  compareNumbers(playerNumbers) {
+    this.randomNumbers.forEach((v, i) => {
+      if (v === playerNumbers[i]) {
+        this.strike += 1;
+      } else if (playerNumbers.includes(v)) {
+        this.ball += 1;
+      }
+    });
+  }
+
+  /**
+   * 볼, 스트라이크를 계산해서 score에 반영
+   */
+  calculateScore() {
+    const isNothing = !this.strike && !this.ball;
+    const isThreeStrike = this.strike === 3;
+    const strikeAndBall = isThreeStrike
+      ? this.SCORE_THREE_STRIKE
+      : `${this.ball ? `${this.ball}볼` : ''} ${
+          this.strike ? `${this.strike}스트라이크` : ''
+        }`;
+    const result = isNothing ? this.SCORE_NOTHING : strikeAndBall;
+    this.score = result;
+  }
+
+  isWin() {
+    this.win = this.score === this.SCORE_THREE_STRIKE;
+  }
+
+  printScore() {
+    switch (this.score) {
+      case this.SCORE_NOTHING:
+        this.output.printNothingMessage();
+        break;
+      case this.SCORE_THREE_STRIKE:
+        this.output.printThreeStrikeMessage();
+        this.output.printWinMessage();
+        this.output.printGameRestartMessage();
+        break;
+      default:
+        this.output.printMessage(this.score);
+        break;
+    }
+  }
+
+  /**
+   * 게임 상태에 따라,플레이어의 입력값과 렌덤 숫자를 비교해 게임 결과를 보여주거나 게임 재시작 또는 종료를 진행
+   * @param {number[]} playerNumbers
+   */
+  referee(playerNumbers) {
+    if (this.win) {
+      if (playerNumbers[0] === 1) {
+        this.restart();
+      } else {
+        this.over();
+      }
+    } else {
+      this.compareNumbers(playerNumbers);
+      this.calculateScore();
+      this.isWin();
+      this.printScore();
+    }
+    this.resetScore();
   }
 }
 
