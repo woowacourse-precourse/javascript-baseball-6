43c59b0bf61fc547028187e72dd4c0c0f76c219f
yukyunglim <yukyung0826@naver.com>
2023-10-24 15:25:57 +0900

    feat : baseball play

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.CONTINUE_PLAYING = true;
+    this.NEW_GAME = "1";
+    this.QUIT_GAME = "2";
+  }
+
+  async play() {
+    while (this.CONTINUE_PLAYING) {
+      console.log("숫자 야구 게임을 시작합니다");
+      const { strike, ball } = this.start();
+      this.CONTINUE_PLAYING = strike !== 3;
+      this.printGameResultMessage(strike, ball);
+      if (!this.CONTINUE_PLAYING) {
+        this.CONTINUE_PLAYING = await this.shouldContinue();
+      }
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
