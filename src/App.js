bc4c0fdb3ab9c425ba186384f7da178f79c1068a
aaapark <parksc1534.dev@gmail.com>
2023-10-24 16:55:16 +0900

    check the user input number

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import {computerRandom} from './calculator/Computer.js'
 class App {
     async play() {
       const answer = computerRandom();
+      const arrayOfInput = this.makeUserInput()
+
     }
+
+    async makeUserInput() {
+      const inputNumber = await Console.readLineAsync('숫자를 입력해주세요 : ').then(
+        (value) => value
+      )
+      const arrayOfInput = Array.from(String(inputNumber),Number);
+      return arrayOfInput;
+    } 
 }
 
 export dafault App;
