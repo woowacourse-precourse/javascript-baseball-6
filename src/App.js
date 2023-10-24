cc5067447d6662ae6e64cdf4190baebb32479ba5
정민규 <jmg081005@naver.com>
2023-10-24 18:01:28 +0900

    refactor) 주석 & 변수변경
    1. 함수 및 전체적인 로직 주석 추가
    2. 의미를 파악하기 힘든 변수명 변경

--- src/App.js
+++ src/App.js
@@ -35,6 +35,12 @@ class App {
     throw new Error("[ERROR] 입력은 1혹은 2로만 이루어져야 합니다.");
   }
 
+  /**
+   * 숫자와 정답을 비교하여 숫자야구의 힌트를 반환
+   * @param {string} num
+   * @param {string} answer
+   * @returns 만들어낸 문자열로 반환
+   */
   isAnswerNum(num, answer) {
     let ballCnt = 0;
     let strikeCnt = 0;
@@ -48,6 +54,10 @@ class App {
     return ret.length ? ret : "낫싱";
   }
 
+  /**
+   * 이번 게임에서의 정답 번호를 랜덤으로 생성하는 함수
+   * @returns 중복되지 않는 숫자 3자리로 된 문자열
+   */
   makeAnswerNum() {
     const answer = [];
     while (answer.length < 3) {
@@ -59,22 +69,38 @@ class App {
     return answer.join("");
   }
 
+  /**
+   * 숫자야구를 플레이할 수 있게 하는 함수
+   */
   async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     while (1) {
-      const answer = this.makeAnswerNum();
+      const answer = this.makeAnswerNum(); //정답 배열 생성
 
       while (1) {
+        // 사용자에게 숫자3자리를 입력받음
         const baseballNum = await this.getInput("숫자를 입력해주세요 :");
         this.isRightBaseBallNum(baseballNum);
-        const retStr = this.isAnswerNum(baseballNum, answer);
-        MissionUtils.Console.print(retStr);
-        if (retStr === "3스트라이크") break;
+        // 정답과 비교하여 힌트 제공
+        const hintStr = this.isAnswerNum(baseballNum, answer);
+        MissionUtils.Console.print(hintStr);
+
+        //정답이라면 이번 게임 종료
+        if (hintStr === "3스트라이크") {
+          MissionUtils.Console.print(
+            "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+          );
+          break;
+        }
       }
 
+      //게임을 다시 시작할껀지 여부를 입력받음
       const input = await this.getInput(
         "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
       );
       this.isRightInputGameStart(input);
+
+      //2가 입력되었다면 게임 종료
       if (input === "2") {
         MissionUtils.Console.print("게임 종료");
         break;
