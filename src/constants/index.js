5d1db4301ff6995d54fd80a491c6cc7461486d89
junehee <dev.junehee@gmail.com>
2023-10-24 18:17:28 +0900

    refactor: 게임 종료 상수 텍스트 수정

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 export const MESSAGES = Object.freeze({
   game: {
     start: "숫자 야구 게임을 시작합니다.",
-    done: "숫자 야구 게임을 종료합니다.",
+    done: "게임 종료",
     success: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
     restartOrDone: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
     playerInput: "숫자를 입력해주세요 : ",
