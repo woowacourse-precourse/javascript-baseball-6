cf41d84e40bdeee64d681b30ae6d3aca1d4f7835
hojkim77 <rlaghwnd4534@gmail.com>
2023-10-24 16:32:50 +0900

    [feat] 게임Class - 게임 종료 및 재실행 메서드, 로직 완성

--- src/Game.js
+++ src/Game.js
@@ -2,13 +2,14 @@ import { Console } from "@woowacourse/mission-utils";
 import Computer from "./Computer.js";
 
 class Game {
-  constructor(computer = new Computer()) {
-    this.computer = computer;
+  constructor() {
+    this.computer;
   }
 
   gameStart() {
-    Console.print("숫자 야구 게임을 시작합니다.");
-    Console.print(this.computer.answer);
+    this.computer = new Computer();
+    //!Test
+    //Console.print(this.computer.answer);
     this.getInput();
   }
 
@@ -17,11 +18,13 @@ class Game {
     this.inputController(input);
   }
 
+  /**
+   *
+   * @param {string} input
+   */
   inputController(input) {
-    if (input.length !== 3)
-      throw new Error("[ERROR] 잘못된 값을 입력하셨습니다.");
-    if (!/^[1-9]{3}$/.test(input))
-      throw new Error("[ERROR] 잘못된 값을 입력하셨습니다.");
+    if (input.length !== 3) throw Error("[ERROR]");
+    if (!/^[1-9]{3}$/.test(input)) throw Error("[ERROR]");
 
     const newInput = input.split("").map(Number);
     const result = this.computer.resultGenerator(newInput);
@@ -30,13 +33,25 @@ class Game {
   }
 
   getOutput(result) {
-    //test
-    if (result.strike == 3) return;
+    if (result.strike == 3) {
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      return this.gameSelector();
+    }
 
     const printBall = result.ball ? `${result.ball}볼 ` : "";
     const printStrike = result.strike ? `${result.strike}스트라이크` : "";
-    Console.print(printBall + printStrike);
+    Console.print(
+      result.strike + result.ball ? printBall + printStrike : "낫싱".trim()
+    );
+
     this.getInput();
+  }
+  async gameSelector() {
+    const isStart = await Console.readLineAsync(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+    );
+    if (isStart == 2) return;
+    if (isStart == 1) this.gameStart();
   }
 }
 
