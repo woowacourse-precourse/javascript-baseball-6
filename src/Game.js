90f85f9c061de30c55e2b91022272b95463a6ac7
today-is-first <diligent0644@gmail.com>
2023-10-24 21:44:40 +0900

    재시작 전 기록

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,17 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { generateRandomBallNumber } from './Ball.js';
-import { restartMessage } from './Message.js';
+import { messages } from './Message.js';
+import { app } from './App.js';
 
-const answer = generateRandomBallNumber();
+export async function getInput() {
+  await MissionUtils.Console.readLine(messages.INPUT_NUMBER, (answer) => {
+    console.log(`${messages.INPUT_NUMBER}: ${answer}`);
+    app.input = answer;
+    printMessage();
+  });
+}
+
+export async function gameStart() {
+}
 
 /**
  * input을 answer와 비교한 후 Strike, Ball 개수 반환
@@ -10,9 +19,13 @@ const answer = generateRandomBallNumber();
  * @returns {[Number, Number]} Strike, Ball 개수
  */
 
-const getResult = (input) => {
-  const countBall = [...input].filter((el) => answer.includes(~~el)).length;
-  const countStrike = [...input].filter((num, i) => ~~num === answer[i]).length;
+const getResult = () => {
+  const countBall = [...app.input].filter((el) =>
+    app.answer.includes(~~el)
+  ).length;
+  const countStrike = [...app.input].filter(
+    (num, i) => ~~num === app.answer[i]
+  ).length;
   return [countBall - countStrike, countStrike];
 };
 
@@ -21,12 +34,12 @@ const getResult = (input) => {
  * @param {String} input
  * @returns {String} 결과
  */
-const resultMessage = (input) => {
+const resultMessage = () => {
   let resultText = [];
-  const [ball, strike] = getResult(input);
-  if (ball === 0 && strike === 0) resultText.push('낫싱');
-  if (ball) resultText.push(`${ball}볼`);
-  if (strike) resultText.push(`${strike}스트라이크`);
+  const [ball, strike] = getResult();
+  if (ball === 0 && strike === 0) resultText.push(messages.NOTHING);
+  if (ball) resultText.push(`${ball}${messages.BALL}`);
+  if (strike) resultText.push(`${strike}${messages.STRIKE}`);
   return resultText.join(' ');
 };
 
@@ -35,16 +48,10 @@ const resultMessage = (input) => {
  * @param {String} input
  */
 
-const printMessage = (input) => {
-  let message = resultMessage(input);
-  if (message === '3스트라이크') {
-    message = `${message}
-${restartMessage}`;
+export async function printMessage() {
+  app.message = resultMessage();
+  if (app.message === '3스트라이크') {
+    app.gameMode = 1;
   }
-  MissionUtils.Console.print(message);
-};
-
-console.log(answer);
-console.log(`getResult(): ${getResult('246')}`);
-console.log(`resultMessage(): ${resultMessage('246')}`);
-printMessage(answer);
+  MissionUtils.Console.print(app.message);
+}
