2efde60bffecfcd341cefe0e637c5cdd096169b3
rheefine <rheefine@gmail.com>
2023-10-25 01:53:30 +0900

    refactor: code convention 수정

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -9,27 +9,37 @@ const MESSAGE = Object.freeze({
 const validation = {
   checkCorrectMainNumber(input) {
     input.forEach(number => {
-      if (Number.isNaN(number)) throw new Error(MESSAGE.invalid_type);
+      if (Number.isNaN(number)) {
+        throw new Error(MESSAGE.invalid_type);
+      }
     });
   },
 
   checkCorrectMainNumbersize(input) {
-    if (input.length !== 3) throw new Error(MESSAGE.invalid_size);
+    if (input.length !== 3) {
+      throw new Error(MESSAGE.invalid_size);
+    }
   },
 
   checkCorrectMainNumberRange(input) {
     input.forEach(number => {
-      if (number < 1 || number > 9) throw new Error(MESSAGE.invalid_range);
+      if (number < 1 || number > 9) {
+        throw new Error(MESSAGE.invalid_range);
+      }
     });
   },
 
   checkDuplicationMainNumber(input) {
     const setInput = new Set(input);
-    if (setInput.size !== 3) throw new Error(MESSAGE.invalid_duplication);
+    if (setInput.size !== 3) {
+      throw new Error(MESSAGE.invalid_duplication);
+    }
   },
 
   checkOneOrTwo(input) {
-    if (!(input === '1' || input === '2')) throw new Error(MESSAGE.invalid_one_or_two);
+    if (!(input === '1' || input === '2')) {
+      throw new Error(MESSAGE.invalid_one_or_two);
+    }
   },
 };
 
