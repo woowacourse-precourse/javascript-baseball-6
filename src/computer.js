33a0487cf897af79e71bca3e322eae89688105a0
Jin Kwan Kim <kkole3897@gmail.com>
2023-10-24 16:26:01 +0900

    refac: 사용자 예측 입력값 검증 추가

--- src/computer.js
+++ src/computer.js
@@ -4,6 +4,18 @@ const NUMBER_LENGTH = 3;
 const MIN_NUMBER = 1;
 const MAX_NUMBER = 9;
 
+const toUniqueNumbers = (answer) => {
+  const numbers = [
+    ...new Set(
+      answer
+        .split("")
+        .map((character) => parseInt(character))
+    )
+  ];
+
+  return numbers;
+}
+
 class Computer {
   constructor() {
     this.numbers = [];
@@ -25,6 +37,22 @@ class Computer {
 
   getNumbers() {
     return [...this.numbers];
+  }
+
+  static validateAnswerLength(answer) {
+    return answer.length === NUMBER_LENGTH;
+  }
+
+  static validateAnswerCharacter(answer) {
+    const pattern = /^[1-9]*$/;
+
+    return pattern.test(answer);
+  }
+
+  static validateAnswerUnique(answer) {
+    const uniqueNumbers = toUniqueNumbers(answer);
+
+    return uniqueNumbers.length === NUMBER_LENGTH;
   }
 }
 
