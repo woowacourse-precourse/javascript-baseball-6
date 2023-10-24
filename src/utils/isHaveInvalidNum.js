53367ab447bcb7974799752e8093abba7154128d
pakxe <pigkill40@naver.com>
2023-10-25 06:58:56 +0900

    comment(isHaveInvalidNum): 함수에 대한 주석 작성

--- src/utils/isHaveInvalidNum.js
+++ src/utils/isHaveInvalidNum.js
@@ -1,8 +1,13 @@
 import GAME from '../constants/Game.js';
 
-const isHaveInvalidNum = (input) => {
-	for (let i = 0; i < input.length; i++) {
-		const curNum = input[i];
+/**
+ * 배열 값 중 게임의 유효 숫자 범위를 벗어난 요소가 있는지 판별하는 함수
+ * @param {number[]} arr
+ * @returns Boolean
+ */
+const isHaveInvalidNum = (arr) => {
+	for (let i = 0; i < arr.length; i++) {
+		const curNum = arr[i];
 		if (GAME.invalidNumArr.includes(curNum)) return true;
 	}
 
