7b914b7dea795f0f7ae19dd605be4157ba9cea18
leey00nsu <dbstndla1212@naver.com>
2023-10-24 20:58:09 +0900

    feat: Player,Computer 클래스 추가 야구게임에서 플레이어와 상대방 역할을 맡는 클래스

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
+// 숫자 야구의 자릿수
+export const DIGIT = 3;
+
 class IOHelper {
   static printMessage(message) {
     Console.print(message);
@@ -21,9 +24,36 @@ class IOHelper {
   }
 }
 
+class Player {
+  async setPlayerNumber() {
+    const response = await IOHelper.readLine("숫자를 입력해주세요 : ");
+
+    const regex = /^[1-9]*$/;
+    if (!regex.test(response)) {
+      throw new Error("[ERROR] 1부터 9까지의 수만 입력할 수 있습니다.");
+    }
+    if (response.length !== DIGIT) {
+      throw new Error(`[ERROR] ${DIGIT}자리의 수만 입력할 수 있습니다.`);
+    }
+    if (new Set(response).size !== DIGIT) {
+      throw new Error("[ERROR] 서로 다른 수만 입력할 수 있습니다.");
+    }
+
+    this.playerNumber = response.split("").map(Number);
+  }
+}
+
+export class Computer {
+  setComputerNumber() {
+    this.computerNumber = IOHelper.getRandomNumbers(1, 9, DIGIT);
+  }
+}
+
 class App {
   constructor() {
     IOHelper.printMessage("숫자 야구 게임을 시작합니다.");
+    this.player = new Player();
+    this.computer = new Computer();
   }
 
   async play() {}
