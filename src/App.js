ea2977895bfaf1067869976bdc890743082d2176
leehyeyeong <elto789@naver.com>
2023-10-24 10:48:05 +0900

    refactor: 컴퓨터 랜덤 숫자 생성 코드 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
+const START_MESSAGE = "숫자 야구 게임 시작을 시작합니다."
+const NUMBER_LENGTH = 3
+const RANGE_START = 1
+const RANGE_END = 9
+
+function computer_start() {
+  Console.print(START_MESSAGE);
+  const computer = [];
+  while (computer.length < NUMBER_LENGTH) {
+    const number = Random.pickNumberInRange(RANGE_START, RANGE_END);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  Console.print(computer);
+  return computer;
+}
+
 class App {
   async play() {
-    Console.print("숫자 야구 게임 시작을 시작합니다.");
-    const computer = [];
-    while (computer.length < 3) {
-      const number = Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer.push(number);
-      }
-    }
-    Console.print(computer);
+    const computer_number = computer_start();
   }
 }
 export default App;
