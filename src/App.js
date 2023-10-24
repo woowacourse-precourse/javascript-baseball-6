618cb2440338e5aaaf589b270e410bd1fb301b27
cho-hyeonjin <gusls2005@gmail.com>
2023-10-24 23:12:39 +0900

    요구사항 정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  async play() {
+    Console.print("숫자 야구 시작");
+    console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]));
+  }
 }
 
 export default App;
+console.log("test");
