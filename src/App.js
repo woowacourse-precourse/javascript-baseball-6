9f8074b74b5abc7260863b9f91155c4e682bc5e0
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2023-10-24 23:07:45 +0900

    구현방식 수정  App.js

--- src/App.js
+++ src/App.js
@@ -1,89 +1,116 @@
-class Random {
-    static PICK_NUMBER_IN_RANGE(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-  }
-  
-  class Console {
-    static async READ_LINE_ASYNC() {
-      return new Promise((resolve) => {
-        const readline = require("readline");
-        const rl = readline.createInterface({
-          input: process.stdin,
-          output: process.stdout,
-        });
-  
-        rl.question("숫자 야구 게임을 시작합니다. 숫자를 입력해주세요: ", (answer) => {
-          resolve(answer);
-          rl.close();
-        });
-      });
-    }
-  
-    static PRINT(message) {
-      console.log(message);
-    }
-  }
-  
-  class NumberBaseballGame {
-    constructor() {
-      this.COMPUTER_NUMBERS = this.GENERATE_COMPUTER_NUMBERS();
-    }
-  
-    GENERATE_COMPUTER_NUMBERS() {
-      const computer = [];
-      while (computer.length < 3) {
-        const number = Random.PICK_NUMBER_IN_RANGE(1, 9);
-        if (!computer.includes(number)) {
-          computer.push(number);
-        }
-      }
-      return computer;
-    }
-  
-    async PLAY() {
-      Console.PRINT("숫자 야구 게임을 시작합니다. 숫자를 입력해주세요: ");
-  
+// 게임 시작
+const app = new App();
+app.play();
+
+
+class App {
+  play() {
+    console.log("숫자 야구 게임을 시작합니다.");
+    
+    let playAgain = true;
+
+    while (playAgain) {
+      // 랜덤한 3자리 숫자 생성
+      const SECRET_NUMBER = this.generateRandomNumber();
+
       while (true) {
         try {
-          const input = await Console.READ_LINE_ASYNC();
-          const playerInput = input.split(" ").map(Number);
-  
-          if (playerInput.length !== 3 || playerInput.some(isNaN)) {
+          const userInput = Console.readLineAsync("숫자를 입력해주세요 : ");
+          if (!this.isValidInput(userInput)) {
             throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
           }
-  
-          const result = this.CHECK_GUESS(playerInput);
-  
-          Console.PRINT(`Result: ${result}`);
-  
-          if (result === "3 스트라이크") {
-            Console.PRINT("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+    generateRandomNumber() {
+        const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        let randomDigits = [];
+
+        while (randomDigits.length < 3) {
+        const randomIndex = Math.floor(Math.random() * digits.length);
+        const randomDigit = digits.splice(randomIndex, 1)[0];
+      randomDigits.push(randomDigit);
+    }
+
+    return randomDigits.join('');
+  }
+
+  isValidInput(input) {
+    return /^[1-9]{3}$/.test(input) && new Set(input).size === 3;
+  }
+
+          const result = this.checkGuess(userInput, SECRET_NUMBER);
+          Console.print(result);
+          if (result === "3스트라이크") {
+            console.log("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             break;
           }
         } catch (error) {
-          Console.PRINT(error.message);
+          console.log(error.message);
+          console.log("[ERROR] 게임을 종료합니다.");
+          return;
         }
       }
-    }
-  
-    CHECK_GUESS(playerInput) {
-      let strikes = 0;
-      let balls = 0;
-  
-      for (let i = 0; i < 3; i++) {
-        if (playerInput[i] === this.COMPUTER_NUMBERS[i]) {
-          strikes++;
-        } else if (this.COMPUTER_NUMBERS.includes(playerInput[i])) {
-          balls++;
-        }
+
+      // 게임 재시작 여부 확인
+      const choice = Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+      if (choice === "2") {
+        playAgain = false;
+        console.log("게임을 종료합니다.");
       }
-  
-      return `${strikes} Strikes ${balls} Balls`;
     }
   }
-  
-  const app = new NumberBaseballGame();
-  app.PLAY();
-  
-  
+
+
+  checkGuess(guess, secret) {
+    let strike = 0;
+    let ball = 0;
+    let nothing = 0;
+
+    for (let i = 0; i < 3; i++) {
+      if (guess[i] === secret[i]) {
+        strike++;
+      } else if (secret.includes(guess[i])) {
+        ball++;
+      } else {
+        nothing++;
+      }
+    }
+
+    let result = "";
+    if (strike > 0) {
+      result += `${strike}스트라이크`;
+    }
+    if (ball > 0) {
+      result += (result === "") ? `${ball}볼` : ` ${ball}볼`;
+    }
+    if (nothing === 3) {
+      result = "낫싱";
+    }
+
+    return result;
+  }
+}
+
+class Random {
+  static pickNumberInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+}
+
+class Console {
+  static readLineAsync(prompt) {
+    return new Promise((resolve) => {
+      const readline = require("readline").createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+      readline.question(prompt, (input) => {
+        readline.close();
+        resolve(input);
+      });
+    });
+  }
+
+  static print(message) {
+    console.log(message);
+  }
+}
