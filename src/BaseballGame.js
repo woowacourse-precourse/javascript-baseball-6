1a8e5c04da633b023ff08dedf8c5e874a81e73de
Blue-Kite <bluelemon112@gmail.com>
2023-10-24 20:48:05 +0900

    feat: 게임결과 판단 기능 추가

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -3,32 +3,42 @@ import { Message } from "./constants/Message.js";
 import { handleError } from "./HandleError.js";
 
 export default class BaseballGame {
-  start() {
-    const answer = '';
-    Console.print(Message.INIT);
-    Console.readLine(Message.INPUT, (playerNum) => {
-      handleError(playerNum);
-    }); 
-  }
-
-  removeDuplicate(num, nums) {
-    if (!nums.includes(num)){
+  removeDuplicate = (num, nums) => {
+    if (!nums.includes(num)) {
       nums.push(num);
     }
-  }
+  };
 
-  makeCoumputerNum() {
-    const nums = []
-    while (nums.length < 3){
+  makeCoumputerNum = () => {
+    const nums = [];
+    while (nums.length < 3) {
       const num = Random.pickNumberInRange(1, 9);
       this.removeDuplicate(num, nums);
     }
-    const computerNum = nums.join('');
+    const computerNum = nums.join("");
     return computerNum;
-  }
+  };
 
-  play() {
-    const playerAnswer = this.start();
-    const computerAnswer = this.makeCoumputerNum();
-  }
+  countResult = (playerNum, computerNum) => {
+    const playResult = [0, 0, 0]; //스트라이크, 볼, 아웃
+    for (let i = 0; i < 3; i++) {
+      if (playerNum[i] == computerNum[i]) {
+        playResult[0] += 1;
+      } else if (computerNum.includes(playerNum[i])) {
+        playResult[1] += 1;
+      } else {
+        playResult[2] += 1;
+      }
+    }
+    return playResult;
+  };
+
+  play = () => {
+    Console.print(Message.INIT);
+    Console.readLine(Message.INPUT, (playerNum) => {
+      handleError(playerNum);
+      const computerNum = this.makeCoumputerNum();
+      const playResult = this.countResult(playerNum, computerNum);
+    });
+  };
 }
