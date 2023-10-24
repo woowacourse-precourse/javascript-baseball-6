dd34cdaeea6dd89632e0be7553445162536e4d80
FirstBright <mipo8890@gmail.com>
2023-10-24 22:34:08 +0900

    d2f

--- src/App.js
+++ src/App.js
@@ -21,11 +21,12 @@ class App {
         ball++;
       }
     }
-    return strike,ball;
+    return [strike,ball];
   }
   printResult(strike,ball){
     if (ball===0&&strike>0){      
       if (strike ===3){
+        MissionUtils.Console.print(`${strike}스트라이크`);
         MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       }else{
         MissionUtils.Console.print(`${strike}스트라이크`);
@@ -44,53 +45,62 @@ class App {
   }
   playerErrorCheck(player){
     if (player.length !== 3) {
-      throw new Error("3자리 숫자를 입력해주세요");
+      throw new Error("[ERROR] 3자리 숫자가 아닙니다.");
     }
-    // if (typeof query !== "string") {
-    //   throw new Error("query must be string");
-    // }
-
-    // if (typeof callback !== "function") {
-    //   throw new Error("callback must be function");
-    // }
-
-    // if (callback.length !== 1) {
-    //   throw new Error("callback must have 1 argument");
-    // }
-    
-    
-  }
-  restartInputErrorCheck(){
-
-  }
-
-  async play() {
-    let restart = true;
-
-    while(restart){
-      restart = await this.playGame();
+    if (new Set(player).size !== player.length) {
+      throw new Error("[ERROR] 중복입니다.");
     }
   }
-
-  async playGame(){
-    await MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  async restartInputErrorCheck(restartInput){
+    
+    if (restartInput !== "1" && restartInput !=="2"){
+      throw new Error("[ERROR]");
+    }
+       
+  }
+  getInputAsIntArray(playerInput) {    
+    try {
+      const inputArray = playerInput.split(""); 
+      const intArray = inputArray.map((char) => parseInt(char, 10));
+      return intArray;
+    } catch (error) {
+      return [];
+    }
+    
+  }
+  async playSet(){
     const computer = this.computerNumber();
-
-    while (st !==3){
+    let strike;    
+    while(true){
       try{
-        const player = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+        const playerInput = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+        const player = this.getInputAsIntArray(playerInput)
         this.playerErrorCheck(player);
-        const [strike,ball] = this.checkResult(computer, player);
+        const [strike, ball] = this.checkResult(computer, player);        
         this.printResult(strike,ball);
       }
       catch(e){
-        MissionUtils.Console.print("[ERROR]"+e);
+        MissionUtils.Console.print(e);
+        break;
+      }
+      if(strike===3){
+        break;
       }
     }
+    
+  }
 
-    const restartInput =MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",)
-
-    return restartInput === "1";
+  async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    while(true){
+      await this.playSet()    
+      const restartInput = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n")
+      await this.restartInputErrorCheck(restartInput)       
+      if (restartInput === "2"){
+        break;
+      } 
+    }
+    
   }
 }
 
