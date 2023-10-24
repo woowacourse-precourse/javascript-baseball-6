1ae09cf5a8f00ff5c63fb3892d0b966a44373c93
조경은 <kavvy99@naver.com>
2023-10-24 17:55:23 +0900

    docs: computerNumbersArray와 userNumbersArray 비교에 관한 논리구조 및 추가 요구사항 정리

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## 🔍 구현 논리 구조 및 흐름
 
+### 0. 게임 시작
+
+- '숫자 야구 게임을 시작합니다.' 문구 출력
+
 ### 1. 컴퓨터가 가질 1~9 사이 서로 다른 세 자리 랜덤 숫자 생성
 
 - `@woowacourse/mission-utils`의 `Random` 및 `Console` API를 사용하여 구현해야 한다.Random 값 추출은 `Random.pickNumberInRange()`를 활용한다.
@@ -9,17 +13,35 @@
 
 ### 2. 유저가 input 창에 입력 후 확인버튼 누르면
 
-#### 2-1. 유저가 입력한 서로 다른 세 자리 숫자 배열 생성
+### 3. 유저가 입력한 서로 다른 세 자리 숫자 배열 `userNumbersArray` 생성
 
-- 사용자의 입력 값 배열 생성
-- 입력받은 세 자리 값 자체를 받는다
-- 그 세 자리 값의 숫자 하나하나씩을 `userNumbersArray`에 넣어준다.
+- 입력받은 값 자체를 받는다
+- 입력받은 값의 숫자 하나하나씩을 `userNumbersArray`에 넣어준다.
 
-#### 2-2. userNumbersArray의 유효성 검사
+### 4. userNumbersArray의 유효성 검사
 
 - `userNumbersArray`.length === 3 이어야 함
 - `userNumbersArray` 안의 값들은 모두 1~9 사이의 정수여야 함
 - 중복되는 숫자가 없어야 함
 - 사용자가 잘못된 값을 입력한 경우 throw문 사용해 예외를 발생시킨 후 어플리케이션 종료
 
-#### 2-3. computerNumbersArray와 computerNumbersArray 비교
+### 5. computerNumbersArray와 userNumbersArray 비교
+
+- for문으로 배열 전체 확인
+- 같은 인덱스 [i]에 같은 숫자가 위치하면 strike++
+- 위는 아닌데, computerNumbersArray가 userNumbersArray[i]를 포함하하고 있으면 ball++
+- 결과가 strike도 0, ball도 0이면 '낫싱'
+- 결과가 strike가 3이면 '3스트라이크 3개의 숫자를 모두 맞히셨습니다! 게임 종료
+  게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
+- 결과가 위 조건 둘다 아니면, `${strike}스트라이크 ${ball}볼`
+
+- (추가) 결과가 '3스트라이크'가 아닐 경우 #container 안에 .try 또 하나 생성
+- (추가) .try 또 하나 생성될 때마다 <h5 class="nth_try"></h5> 안에 'n번째 시도' 표현
+
+### 6. 컴퓨터가 선택한 3개의 숫자를 모두 맞혀 게임 종료 후 '1' 누르면 게임 재시작
+
+-
+
+### 7. 컴퓨터가 선택한 3개의 숫자를 모두 맞혀 게임 종료 후 '2' 누르면 완전히 종료
+
+-
--- app.js
+++ app.js
@@ -8,13 +8,17 @@ while (computerNumbersArray.length < 3) {
 }
 console.log(computerNumbersArray);
 
+
+const result = document.querySelector('.result');
+
+
 // 확인버튼 클릭했을 때 numArrayCompareEvent() 발생
 function numArrayCompareEvent() {
 
     // 1. 유저의 입력 배열 userNumbersArray 생성
     // 사용자의 입력 값 배열 생성
-    var userNum = document.querySelector('.userNum').value; 
-    var userNumbersArray = userNum.toString().split('').map(Number);
+    let userNum = document.querySelector('.userNum').value; 
+    let userNumbersArray = userNum.toString().split('').map(Number);
 
     console.log(userNumbersArray);
 
@@ -23,16 +27,16 @@ function numArrayCompareEvent() {
     if (userNumbersArray.length!==3) {
         alert("[ERROR] 세 자리 숫자가 아닙니다.");
     } else {
-        var isValid = true;
-        var uniqueUserNumbersArray = new Set(userNumbersArray);
-        console.log(uniqueUserNumbersArray);
-        console.log(uniqueUserNumbersArray.size);
-        console.log(userNumbersArray.length);
+        let isValid = true;
+        let uniqueUserNumbersArray = new Set(userNumbersArray);
+        // console.log(uniqueUserNumbersArray);
+        // console.log(uniqueUserNumbersArray.size);
+        // console.log(userNumbersArray.length);
         if (uniqueUserNumbersArray.size !== userNumbersArray.length) {
             alert("[ERROR] 중복된 값이 존재합니다.")
             isValid = false;
         }
-        for (var i=0; i<3; i++) {
+        for (let i=0; i<3; i++) {
             if (userNumbersArray[i] < 1 || userNumbersArray[i] > 9) {
                 alert("[ERROR] 1에서 9 사이의 범위를 벗어나는 숫자가 있습니다.");
                 isValid = false;
@@ -40,8 +44,10 @@ function numArrayCompareEvent() {
             }
         }
         if (isValid) {
-            // 3. computerNumbersArray와 userNumbersArray 비교
             console.log('이제 시작이다')
+
+            // 3. computerNumbersArray와 userNumbersArray 비교  
+
         }
     }
 }
