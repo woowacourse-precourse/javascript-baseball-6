2519ba262b6050eacabd335c46455fdb89efd231
yeong-hwan <jghff700@naver.com>
2023-10-25 00:22:06 +0900

    feat: [기능3] bassballGame 기능 동작 구현

--- src/baseballGame/baseballGame.js
+++ src/baseballGame/baseballGame.js
@@ -1,24 +1,45 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import makeAnswer from "../makeAnswer/makeAnswer";
-import getUserInput from "../input/getUserInput";
-import gameResult from "./gameResult";
+import makeAnswer from "../makeAnswer/makeAnswer.js";
+import getUserInput from "../input/getUserInput.js";
+import countBall from "./countBall.js";
+import countStrike from "./countStrike.js";
+import gameResult from "./gameResult.js";
 
 /**
  * 야구게임을 실행한다
  * 
+ * @todo indent 많아서 기능 분할 필요
  */
-export default function baseballGame() {
-  const answer = makeAnswer();
-  const userInput = getUserInput();
+export default async function baseballGame() {
+  let restart = true;
 
-  const strikeCount = countStrike(answer, userInput);
-  const ballCount = countBall(answer, userInput) - strikeCount;
+  while (restart) {
+    const answer = makeAnswer();
+    let wrongAnswer = true;
+    // console.log(`answer: ${answer}`);
+    // console.log(`restart: ${restart}, wrongAnswer: ${wrongAnswer}`);
 
-  if (strikeCount === 3) {
-    MissionUtils.Console.print('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-    // 재시작/종료 구현
+    while (wrongAnswer) {
+      const userInput = await getUserInput();
+      const strikeCount = countStrike(answer, userInput);
+      const ballCount = countBall(answer, userInput) - strikeCount;
+
+      if (strikeCount === 3) {
+        MissionUtils.Console.print('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        const restartChoice = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
+
+        if (restartChoice === "2") {
+          restart = false;
+        }
+        wrongAnswer = false;
+        // console.log(`restart: ${restart}, wrongAnswer: ${wrongAnswer}`);
+        break;
+      }
+
+      const resultMessage = gameResult(strikeCount, ballCount);
+      MissionUtils.Console.print(resultMessage);
+    }
   }
-  
-  const resultMessage = gameResult(strikeCount, ballCount);
 }
 
+baseballGame();
