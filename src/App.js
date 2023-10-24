050e34b477ef6f4477052be5c76a1af93fadcf4b
swJaNG12 <cbdjej38483@gmail.com>
2023-10-25 03:09:22 +0900

    README 추가

--- src/App.js
+++ src/App.js
@@ -7,45 +7,16 @@ class App {
       let isGameContinue = '1';
 
       while (isGameContinue === '1') {
-        const setNumbers = new Set();
-
-        // 중복되지 않은 랜덤 숫자 3개 뽑기
-        while (setNumbers.size < 3) {
-          const randomNumber = Random.pickNumberInRange(1, 9);
-          setNumbers.add(randomNumber);
-        }
-        const computerNumbers = [...setNumbers];
+        const computerNumbers = this.generateTargetNumbers();
         Console.print(computerNumbers);
 
         let strike = 0;
         let ball = 0;
 
         while (strike !== 3) {
-          // 유저가 입력한 숫자, 문자열 형태로 들어온다.
-          // const userInputString = await Console.readLineAsync(
-          //   '숫자를 입력해주세요'
-          // );
-
-          const userInputString = await this.userInput('숫자를 입력해주세요');
-
-          // 길이가 3인지 아닌지 확인
-          // const userInputLength = userInputString.length;
-          // if (userInputLength !== 3) {
-          //   throw new Error('[ERROR] 길이');
-          // }
-
-          // // 문자가 숫자로만 이루어져 있는지 확인
-          // const matchCount = userInputString.match(/[1-9]/g).length;
-          // if (matchCount !== 3) {
-          //   throw new Error('[ERROR] 노숫자');
-          // }
-
-          // // 중복이 있는지 확인
-          // const duplicateCount = new Set(userInputString.split('')).size;
-          // if (duplicateCount !== 3) {
-          //   throw new Error('[ERROR] 길이');
-          // }
-
+          const userInputString = await this.validUserInput(
+            '숫자를 입력해주세요'
+          );
           const userNumbers = userInputString.split('').map((v) => parseInt(v));
 
           // 입력한 숫자가 있는지 확인
@@ -78,9 +49,6 @@ class App {
           }
         }
         Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-        // const isGameContinue = await Console.readLineAsync(
-        //   '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
-        // );
 
         isGameContinue = await this.reGame();
       }
@@ -91,7 +59,7 @@ class App {
 
     return;
   }
-  async userInput(string) {
+  async validUserInput(string) {
     const userInputString = await Console.readLineAsync(string);
 
     // 길이가 3인지 아닌지 확인
@@ -113,6 +81,16 @@ class App {
     }
     return userInputString;
   }
+  generateTargetNumbers() {
+    const setNumbers = new Set();
+
+    // 중복되지 않은 랜덤 숫자 3개 뽑기
+    while (setNumbers.size < 3) {
+      const randomNumber = Random.pickNumberInRange(1, 9);
+      setNumbers.add(randomNumber);
+    }
+    return [...setNumbers];
+  }
 
   async reGame() {
     return await Console.readLineAsync(
@@ -124,12 +102,4 @@ class App {
 const app = new App();
 app.play();
 
-// app.play().catch((error) => {
-//   return Promise.reject();
-// });
-
 export default App;
-
-/**
- *
- * */
