0560c90f6160e647f3f6aa4f8e9577c2b5b5de47
kim-young-dong <opa9904@gmail.com>
2023-10-24 16:01:58 +0900

    feat: 랜덤 숫자 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.answerNumber = '';
+    this.playerInput = '';
+  }
+  // 랜덤 숫자 생성 메서드
+  setAnswerNumber() {
+    const numbers = [];
+    while (numbers.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!numbers.includes(number)) {
+        numbers.push(number);
+      }
+    }
+    this.answerNumber = numbers.join('');
+  }
 }
 
 export default App;
