1491be71b180778428d127d8b8c001599592c570
damdam6 <dambi626626@gmail.com>
2023-10-24 17:37:12 +0900

    fix: tryCatch설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-class App {
-  async play() {}
-}
+import gameStart from "../src/controller/game-controller.js";
 
+class App {
+  async play() {
+    try{
+      await gameStart();
+    }catch(err){
+      throw err;
+    }
+   
+  }
+}
 export default App;
+
