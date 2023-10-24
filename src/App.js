7882ad1cd272e04a2f952de30a1ba77ae01d9e37
bwlee-dix <extreme93@naver.com>
2023-10-25 03:13:38 +0900

    feat: 서로 다른 임의의 수 3개를 생성한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const computer = this.generateComputer();
+  }
+
+  //  서로 다른 임의의 수 3개를 생성한다.
+  generateComputer() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    const computerString = computer.join("");
+    MissionUtils.Console.print(`컴퓨터 생성 숫자: ${computerString}`);
+    return computer;
+  }
+
 }
 
 export default App;
