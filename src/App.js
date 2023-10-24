b067e6e4eca0371aa64514fe4c514f52a460bfe7
deeyo <130573068+jeehyojeon@users.noreply.github.com>
2023-10-24 12:59:07 +0900

    feat: computer class 생성, setter, getter method 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
-import GameStartPage from "./GameStartPage";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
     
+    class Computer {
+      constructor ( number ) {
+        this.number = number;
+      }
+
+      get number () {
+        while (computer.length < 3) {
+          const number = MissionUtils.Random.pickNumberInRange(1, 9);
+          if (!computer.includes(number)) {
+            computer.push(number);
+          }
+        }
+
+        return this.number;
+      }
+
+      
+    }
+
+    class User {
+      constructor ( number ) {
+        this.number = number;
+      }
+
+      get length () {
+        return 6
+      }
+
+      get number () {
+        if (
+          typeof(this.number) != "number" &&
+          ) {
+          throw '[ERROR] 숫자가 잘못된 형식입니다.'
+        }
+
+        return (await Console.readLineAsync('숫자를 입력해주세요.'));
+      }
+
+      get isEnd () {
+        return (await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'))
+      }
+    }
+  }
 }
 
 export default App;
