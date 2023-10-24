91c0fe9112e14e1223921c8d40da4bcbb8cee97b
pakxe <pigkill40@naver.com>
2023-10-25 05:43:51 +0900

    refactor(BaseBallGameComputer): 볼 개수를 세는 함수를 더 간단하게 리펙토링

--- src/Model/BaseBallGameComputer.js
+++ src/Model/BaseBallGameComputer.js
@@ -7,7 +7,9 @@ import pickNumberInRange from '../utils/pickNumberInRange.js';
 class BaseBallGameComputer {
 	#answerNumber;
 	#tryNumber;
+	#strikeCount; // 반복되어 사용되므로 변수로 사용하려함. getStrikeCount의 복잡도가 커지면 재사용이 문제가 될 수 있으므로
 
+	// 정답 생성
 	setAnswerNumber() {
 		this.#answerNumber = pickNumberInRange(
 			GAME.validNumMin,
@@ -16,6 +18,7 @@ class BaseBallGameComputer {
 		);
 	}
 
+	// 입력받은 시도 넘버의 유효성 검사 후 저장
 	setTryNumber(tryNumber) {
 		const tryNumberArr = tryNumber.split('').map(Number); // str -> numArr
 
@@ -24,16 +27,20 @@ class BaseBallGameComputer {
 		this.#tryNumber = tryNumberArr;
 	}
 
-	// TODO: 리펙터링
-	getBallAndStrikeCounts() {
-		return [this.#getBallCount(), this.#getStrikeCount()];
+	// 시도 넘버의 결과 반환
+	getResult() {
+		// 처음 스트라이크 개수에 접근하는 것이므로 여기서 세팅한다. (재사용을 줄이기 위해서 분리한건데 괜히 다른 함수에 영향을 미치기도 하네요)
+		this.#setStrikeCount();
+
+		return [this.#getBallCount(), this.#strikeCount];
 	}
 
-	res() {
-		return this.#getStrikeCount() === GAME.size;
+	// 클리어 여부
+	isClear() {
+		return this.#strikeCount === GAME.size;
 	}
 
-	// 볼카운트에는 스트라이크 카운트도 포함되어있다. 빼고 전해주는게 좋을 것 같다.
+	// 시도 넘버의 볼 개수 반환
 	#getBallCount() {
 		return this.#tryNumber.reduce((count, curNum, idx) => {
 			if (
@@ -45,13 +52,20 @@ class BaseBallGameComputer {
 		}, 0);
 	}
 
+	// 시도 넘버의 스트라이크 개수 반환
 	#getStrikeCount() {
-		return this.#tryNumber.reduce(
-			(count, curNum, idx) =>
-				curNum === this.#answerNumber[idx] ? count + 1 : count,
-			0
-		);
+		return this.#tryNumber.filter(
+			(num, i) =>
+				this.#answerNumber.includes(num) && num !== this.#answerNumber[i]
+		).length;
+	}
+
+	#setStrikeCount() {
+		this.#strikeCount = this.#getStrikeCount();
 	}
 }
 
+/**
+ * 스트라이크 개수가 많이 쓰이는데 이를
+ */
 export default BaseBallGameComputer;
