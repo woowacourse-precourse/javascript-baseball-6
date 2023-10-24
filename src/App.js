2f19cedbc0e98b8f114e273998abacc4fda8c162
Majesty-jun <hkwns01@gmail.com>
2023-10-25 02:51:54 +0900

    FEAT: 게임 종료 및 입력에 따른 게임 재시작 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import { Console, Random } from "@woowacourse/mission-utils";
+import getComputerInput from "./input/getComputerInput.js";
 
 class App {
   async play() {
-    const computer = [];
-    while (computer.length < 3) {
-      const number = String(Random.pickNumberInRange(1, 9));
-      if(!computer.includes(number)) {
-        computer.push(number);
-      }
-    }
+    let computer = getComputerInput();
     while(1) {
       let userInput = await Console.readLineAsync("숫자를 입력해주세요 : ");
       
@@ -34,10 +29,17 @@ class App {
       const RESULT = [];
       if(totalCount[0]) RESULT.push(`${totalCount[0]}볼`);
       if(totalCount[1]) RESULT.push(`${totalCount[1]}스트라이크`);
-      if(totalCount[1] === 3) RESULT.push(`\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
       if(totalCount[0] === 0 && totalCount[1] === 0) RESULT.push('낫싱');
 
       Console.print(RESULT.join(''));
+
+      let isGameOver = ''
+      if(totalCount[1] === 3) {
+        Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
+        isGameOver = await Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n")
+      };
+      if(isGameOver === '1') computer = getComputerInput();
+      if(isGameOver === '2') break;
     }
   }
 }
