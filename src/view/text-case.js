affc9585d7921585f142b50a1ede81a368b63aad
damdam6 <dambi626626@gmail.com>
2023-10-24 17:39:59 +0900

    feat: view텍스트 수정

--- src/view/text-case.js
+++ src/view/text-case.js
@@ -1,6 +1,16 @@
-
 const viewError = '[ERROR] 잘못된 입력입니다.';
 const viewGameContinue = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.';
+const viewGameStart = '숫자 야구 게임을 시작합니다.';
+const viewInputNum = '숫자를 입력해주세요 : ';
 
 
-export{ viewError, viewGameContinue}
+const pickView = function pickView(num){
+    switch(num){
+        case 1:
+            return viewGameStart;
+        case 2:
+            return viewInputNum;
+    }
+}
+
+export{ pickView, viewError, viewGameContinue, viewGameStart}
