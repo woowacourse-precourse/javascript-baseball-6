2749903de6dfbc5cce88888a8fc3050862a0dc19
lee juyoung <juyung0903@naver.com>
2023-10-24 21:04:21 +0900

    refactor: 형 변환 코드 및 문자열 quote수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   constructor() {
@@ -17,22 +17,21 @@ class App {
   //스트라이크 볼 개수 결과 출력
   showResults(strikeNumbers, ballNumbers) {
     if (strikeNumbers === 0 && ballNumbers === 0) {
-      MissionUtils.Console.print("낫싱");
-    } else {
-      const ballOutput = ballNumbers !== 0 ? `${ballNumbers}볼 ` : "";
-      const strikeOutput =
-        strikeNumbers !== 0 ? `${strikeNumbers}스트라이크` : "";
-      MissionUtils.Console.print(`${ballOutput}${strikeOutput}`);
+      MissionUtils.Console.print('낫싱');
+      return;
     }
+    const ballOutput = ballNumbers !== 0 ? `${ballNumbers}볼 ` : '';
+    const strikeOutput = strikeNumbers !== 0 ? `${strikeNumbers}스트라이크` : '';
+    MissionUtils.Console.print(`${ballOutput}${strikeOutput}`);
   }
   //유저의 숫자와 컴퓨터의 숫자 비교
   compareNumbers(guessNumber) {
     let strikeNumbers = 0,
       ballNumbers = 0;
     for (let i = 0; i < 3; i++) {
-      if (parseInt(guessNumber[i]) === this.computerNumber[i]) {
+      if (Number(guessNumber[i]) === this.computerNumber[i]) {
         strikeNumbers += 1;
-      } else if (this.computerNumber.includes(parseInt(guessNumber[i]))) {
+      } else if (this.computerNumber.includes(Number(guessNumber[i]))) {
         ballNumbers += 1;
       }
     }
@@ -54,33 +53,33 @@ class App {
       isNaN(guessNumber) ||
       this.verifyDigit(guessNumber)
     ) {
-      throw new Error("[ERROR] 잘못된 유저 입력값입니다.");
+      throw new Error('[ERROR] 잘못된 유저 입력값입니다.');
     }
   }
   // 유저가 게임을 시작할 것인지 여부를 물어보는 숫자 입력
   async askExitNumber() {
-    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
     this.exitNumber = await MissionUtils.Console.readLineAsync(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
     );
     this.gameExiter();
   }
   //유저의 입력값에 따라 재시작 or 종료
   async gameExiter() {
-    if (this.exitNumber === "1") {
+    if (this.exitNumber === '1') {
       this.computerNumber.length = 0;
       this.startGame();
       return;
-    } else if (this.exitNumber === "2") {
-      MissionUtils.Console.print("게임을 종료합니다.");
+    } else if (this.exitNumber === '2') {
+      MissionUtils.Console.print('게임을 종료합니다.');
       return;
     }
-    throw new Error("[ERROR] 잘못된 유저 입력값입니다.");
+    throw new Error('[ERROR] 잘못된 유저 입력값입니다.');
   }
   //유저가 숫자 입력
   async askNumber() {
     const guessNumber = await MissionUtils.Console.readLineAsync(
-      "숫자를 입력해주세요 : "
+      '숫자를 입력해주세요 : '
     );
     this.verityUserNumber(guessNumber);
     if (this.compareNumbers(guessNumber) === 3) {
