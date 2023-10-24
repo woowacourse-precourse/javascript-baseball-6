a31d55a4dca7fd1896c8069a3fe96a6bd3b7c5e1
guswns8431 <guswns8431@gmail.com>
2023-10-25 01:28:16 +0900

    fix : 출력형식 변경
    출력형식 예시와 다른 부분 수정

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { TEXT, PROGRAM_STATUS, ERROR_CODE } from "./constants.js";
 class App {
   async play() {
     let code = PROGRAM_STATUS.START;
+    await MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     while (code != PROGRAM_STATUS.END) {
       const computer = await computerNumber();
-      await MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
       try {
         code = await getUserInput(computer);
       } catch (error) {
