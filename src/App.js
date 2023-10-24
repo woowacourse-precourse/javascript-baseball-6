340f11e8ad45b251cf6c127e687fabed7c36cd8d
Si Hyun Lee <pongpongi@Pongpongbook.local>
2023-10-25 06:24:12 +0900

    :art: GetUserAnswer deleted but divided
    tried to get rid of gettery function which is for clean code +  modified some error message

--- src/App.js
+++ src/App.js
@@ -20,13 +20,13 @@ function compareNumbers(computerAnswer, userAnswer) {
       strike++;
       continue;
     }
-    if (userAnswer.includes(computerAnswer[i])) {
+    if (userAnswer.includes(computerAnswer[i]) && computerAnswer[i] !== userAnswer[i]) {
       ball++;
     }
   }
 
   if (strike === 3) {
-    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    Console.print(`${strike}스트라이트 \n 3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
     return;
   }
 
@@ -40,27 +40,27 @@ function isValidInput(input) {
   return uniqueDigits.every(digit => Number.isInteger(Number(digit))) && uniqueDigits.length === 3;
 }
 
+async function requestUserAnswer() {
+  const input = await Console.readLineAsync('숫자를 입력해주세요: ');
+
+  if (!isValidInput(input)) {
+    throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+  }
+
+  return input;
+}
+
 class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
     const computerAnswer = createComputerNumber();
 
     try {
-      const userAnswer = await this.getUserAnswer();
+      const userAnswer = await requestUserAnswer();
       compareNumbers(computerAnswer, userAnswer.split(''));
     } catch (error) {
       Console.print(error);
     }
-  }
-
-  async getUserAnswer() {
-    const input = await Console.readLineAsync('숫자를 입력해주세요: ');
-
-    if (!isValidInput(input)) {
-      throw new Error('잘못된 입력입니다. 3자리 숫자를 입력해주세요.');
-    }
-
-    return input;
   }
 }
 
