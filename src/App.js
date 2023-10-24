d15d765d6a1f5b7b2b1e42e44f63ad79f2621903
Dawon00 <dawon000203@gmail.com>
2023-10-25 02:04:55 +0900

    feat: 사용자 숫자 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+
+// Check dulicate of array
+function hasDuplicates(array) {
+    return new Set(array).size !== array.length;
+}
 
 class App {
-    async play() {
-        console.log("숫자 야구 게임을 시작합니다.");
+    // Get Input Number
+    async getNumber() {
+        try {
+            const inputString = await Console.readLineAsync(
+                "숫자를 입력해주세요 : "
+            );
+            const inputNumber = inputString.split("").map(Number);
+            // Check duplicate
+            if (hasDuplicates(inputNumber)) {
+                throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+            }
+            return inputNumber;
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
 
-        // Create Answer Numbers
-        var answerNumber = [];
+    // Create Answer Numbers
+    makeAnswerNumber() {
+        const answerNumber = [];
         while (answerNumber.length < 3) {
-            var randomNumber = MissionUtils.Random.pickNumberInRange(1, 9); // 숫자를 반환하는 함수를 호출하세요
+            const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
             if (!answerNumber.includes(randomNumber)) {
                 answerNumber.push(randomNumber);
             }
         }
-        console.log(answerNumber);
+        return answerNumber;
+    }
+
+    async play() {
+        console.log("숫자 야구 게임을 시작합니다.");
+        const answerNumber = this.makeAnswerNumber();
+        // console.log(answerNumber);
+
+        // Get input number
+        const inputNumber = await this.getNumber();
+        // console.log(inputNumber);
     }
 }
 
