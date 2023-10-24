9cc919bbc35084bfc588bb8857506a707d4160dc
YOON-CC <whdbscks77@gmail.com>
2023-10-24 13:38:34 +0900

    feat(App.js): 재시도 여부 숫자 입력 메서드 및 메시지 출력 메서드 구현

--- src/App.js
+++ src/App.js
@@ -13,9 +13,28 @@ class App {
   async play() {
     //Controller.js 메서드 호출
     await this.controller.gameStart();
+    const userInput = await this.showRestartNumberInput();
+    if (userInput === '1') {
+      return this.play(); 
+    } else if (userInput === '2') {
+      Console.print('게임을 종료합니다.');
+    } else {
+      //에러 처리
+    }
   }
 
 
+  async showRestartNumberInput() {
+    const userInput = await this.showRestartRequest();
+    if (userInput !== '1' && userInput !== '2') {
+      //에러 처리
+    }
+    return userInput;
+  }
+
+  showRestartRequest() {
+    return Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
+  }
 }
 
 export default App;
