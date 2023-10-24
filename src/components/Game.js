f456a6467aa29c306908d8a3d1e2baa1bce7056b
dongbin420 <dongbin420@gmail.com>
2023-10-24 17:26:06 +0900

    feat: get user input

--- src/components/Game.js
+++ src/components/Game.js
@@ -9,6 +9,14 @@ class Game {
     this.gameCount
       ? Console.print(`플레이한 게임 수: ${this.gameCount}`)
       : Console.print('숫자 야구 게임을 시작합니다.');
+
+    this.getUserInput();
+  }
+
+  async getUserInput() {
+    const USER_INPUT = await Console.readLineAsync(
+      '1~9를 이용하여 각 자리가 중복되지 않는 세자리 숫자를 입력해주세요 : '
+    );
   }
 }
 
