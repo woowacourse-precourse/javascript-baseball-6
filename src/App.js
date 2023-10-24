5b73f24d55a7bae02f553af28bb1d4834acbdbe5
SungChul Hong <hsc6263@gmail.com>
2023-10-24 23:32:24 +0900

    feat: 코드 병합 및 전체 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { getComputer } from './utils/getComputer.js';
+import { getPlayer } from './utils/getPlayer.js';
+import { getResult } from './utils/getResult.js';
+import { printResult } from './utils/printResult.js';
+import { restartHandler } from './utils/restartHandler.js';
+import { START } from './constants.js';
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    let computer = getComputer();
+
+    while (true) {
+      const player = await getPlayer();
+      const result = getResult(computer, player);
+
+      printResult(result);
+
+      if (result.strike === 3) {
+        const isRestarted = await restartHandler();
+        if (isRestarted === START) {
+          computer = getComputer();
+        } else {
+          break;
+        }
+      }
+    }
+  }
 }
 
 export default App;
