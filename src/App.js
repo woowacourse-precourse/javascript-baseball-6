f2ce9ecfd033781f34fb3f3696df9aec76de60f4
hs <tlsgustn93@naver.com>
2023-10-24 12:55:05 +0900

    computerInput 값 받아오기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { PROGRESS_MESSAGE } from "./constants.js";
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    // 시작하는 부분의 로직
+    Console.print(PROGRESS_MESSAGE.GAME_START);
+    this.computerAnswer();
+    return this.numberCompare();
+  }
+
+  // 컴퓨터의 랜덤한 3가지의 값을 받아옵니다.
+  computerAnswer() {
+    const computerInput = [];
+    while (computerInput.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerInput.includes(number)) {
+        computerInput.push(number);
+      }
+    }
+
+    // 비교하기 편하게 배열로 되어있는 값을 하나로 합칩니다.
+    this.computerInput = computerInput.join("");
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
