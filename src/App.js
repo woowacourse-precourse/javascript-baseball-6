9bc88409421aa200473b18e952ab3172debe2441
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-10-25 00:55:14 +0900

    Feat: 나머지 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,15 +2,41 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    let endPoint = false; //왜..?
+    let endPoint = false;
+
     while (!endPoint) {
       // 게임 시작
       MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-      //임의의 비밀 숫자 생성
-      const secret_number = randomNumber();
-      endPoint = await isThreeStrike(secret_number);
 
-      console.log(secret_number, "랜덤값");
+      // 임의의 비밀 숫자 생성
+      const secret_number = randomNumber();
+
+      let isGameOver = false;
+
+      while (!isGameOver) {
+        const userList = await getUserInput(secret_number);
+        const { ballCount, strikeCount } = ballStrike(userList, secret_number);
+        getScore(ballCount, strikeCount);
+
+        if (strikeCount === 3) {
+          MissionUtils.Console.print(
+            "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+          );
+
+          let startOver = await MissionUtils.Console.readLineAsync(
+            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+          );
+
+          startOver = Number(startOver);
+
+          if (startOver === 1) {
+            isGameOver = true;
+          } else {
+            endPoint = true;
+            isGameOver = true;
+          }
+        }
+      }
     }
   }
 }
@@ -44,4 +70,31 @@ function isValidInput(input) {
   return input.length == 3 ? true : false;
 }
 
+function ballStrike(computer, user) {
+  let strikeCount = 0;
+  let ballCount = 0;
+  for (let i = 0; i < computer.length; i++) {
+    if (computer.includes(user[i])) {
+      if (computer.indexOf(user[i]) == i) {
+        strikeCount++;
+      } else ballCount++;
+    }
+  }
+  return { ballCount, strikeCount };
+}
+
+function getScore(ball, strike) {
+  if (strike == 0 && ball == 0) MissionUtils.Console.print("낫싱");
+  else {
+    let score = "";
+    if (ball) {
+      score += `${ball}볼 `;
+    }
+    if (strike) {
+      score += `${strike}스트라이크`;
+    }
+
+    MissionUtils.Console.print(score);
+  }
+}
 export default App;
