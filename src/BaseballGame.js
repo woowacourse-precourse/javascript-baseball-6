0d33c3970f010aff6b6307f34a2cf27346a52f71
soyunim <togksk123@naver.com>
2023-10-24 18:21:20 +0900

    refactor:

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -6,11 +6,7 @@ const gameStart = async () => {
     while (true) {
       const COMPUTER_NUM = getComputerNum();
       await compareNum(COMPUTER_NUM);
-      const ANSWER = restartOrNot();
-      if (ANSWER == 2) {
-        endGame();
-        break;
-      }
+      restartOrNot();
     }
   } catch (error) {
     throw new Error("[ERROR]");
@@ -39,17 +35,12 @@ const getUserNum = async () => {
     throw new Error("[ERROR]");
   }
 };
-const userValid = (number) => {
-  if (number.length !== 3) {
-    throw new Error(ERROR_MSG.INPUT_ERROR_LEN);
-  }
-  const checkDup = new Set(number.split(""));
-  if (checkDup.length !== 3) {
-    throw new Error(ERROR_MSG.INPUT_ERROR_SAME);
-  }
-  if (isNaN(Number(number))) {
-    throw new Error(ERROR_MSG.INPUT_ERROR_NOT_NUM);
-  }
+// 2-1. 유저 validation
+const userValid = (num) => {
+  const checkDup = new Set(num.split(""));
+  if (checkDup.length !== 3) throw new Error(ERROR_MSG.INPUT_ERROR_SAME);
+  if (Number.isInteger(num)) throw new Error(ERROR_MSG.INPUT_ERROR_NOT_NUM);
+  if (num.length !== 3) throw new Error(ERROR_MSG.INPUT_ERROR_LEN);
 };
 
 // 3. 컴퓨터와 유저의 숫자 비교
@@ -59,17 +50,13 @@ const compareNum = async (COMPUTER_NUM) => {
       let strike = 0;
       let ball = 0;
       const USER_NUM = await getUserNum();
-      MissionUtils.Console.print(GAME_MSG.INPUT + USER_NUM);
+      //   MissionUtils.Console.print(GAME_MSG.INPUT + USER_NUM);
 
       for (let i = 0; i < 3; i++) {
-        if (COMPUTER_NUM[i] === USER_NUM[i]) {
-          strike++;
-        } else if (COMPUTER_NUM.includes(USER_NUM[i])) {
-          ball++;
-        }
+        if (COMPUTER_NUM[i] === USER_NUM[i]) strike++;
+        else if (COMPUTER_NUM.includes(USER_NUM[i])) ball++;
       }
 
-      // 4. 힌트 출력
       getHint(ball, strike);
       if (strike === 3) {
         MissionUtils.Console.print(GAME_MSG.END);
@@ -80,7 +67,7 @@ const compareNum = async (COMPUTER_NUM) => {
     throw new Error("[ERROR]");
   }
 };
-
+// 4. 힌트 출력
 const getHint = (ball, strike) => {
   if (ball === 0 && strike === 0) {
     MissionUtils.Console.print(GAME_MSG.NOTHING);
@@ -98,19 +85,22 @@ const getHint = (ball, strike) => {
 // 6. 게임 재시작/종료 선택
 const restartOrNot = async () => {
   try {
-    MissionUtils.Console.print(GAME_MSG.RESTART);
-    const ANSWER = await MissionUtils.Console.readLineAsync();
-    MissionUtils.Console.print(ANSWER);
-    if (ANSWER != 1 && ANSWER != 2) {
+    const ANSWER = await MissionUtils.Console.readLineAsync(GAME_MSG.RESTART);
+    // MissionUtils.Console.print(ANSWER);
+    if (ANSWER === "1") {
+      await gameStart();
+    } else if (ANSWER === "2") {
+      endGame();
+    } else {
       throw new Error(ERROR_MSG.RESTART_ERROR_NOT_ANS);
     }
-    return ANSWER;
   } catch (error) {
     throw new Error("[ERROR]");
   }
 };
 
 const endGame = () => {
+  MissionUtils.Console.print("게임 종료");
   return;
 };
 
