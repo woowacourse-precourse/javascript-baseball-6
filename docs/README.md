59ad695f325bd1f7860c6d6c445ebba3e2da899a
SWBAE0501 <tkddnjsgla@naver.com>
2023-10-24 20:55:05 +0900

    FEAT: print message with Console.print

--- docs/README.md
+++ docs/README.md
@@ -3,3 +3,6 @@
 0. API 가져오기
 @woowacourse/mission-utils의 Random 및 Console API
 
+1. 게임 시작 메시지 출력
+Console.print함수 사용
+숫자 야구 게임을 시작합니다.
--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import {Random, Console} from "@woowacourse/mission-utils";
 
 class App {
-  async play() {}
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    
+    
+  }
 }
 
 export default App;
