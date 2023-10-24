af5e25c87bd0a0c43daa4800dbc66a73ecf02d6a
bini59 <myuni0234@gmail.com>
2023-10-24 20:52:24 +0900

    feat : 입력값 유효성 검사
    - 입력값이 유효한지 검사 후, Error를 throw

--- src/Controller.js
+++ src/Controller.js
@@ -12,6 +12,17 @@ class Controller {
   async handleInput() {
     await this.view.inputNumber();
     const input = this.view.getInputNumber();
+    if (input.length !== 3) {
+      throw new Error('[ERROR] 3자리 숫자를 입력해주세요.');
+    }
+
+    if (input.split('').some((number) => Number.isNaN(Number(number)))) {
+      throw new Error('[ERROR] 숫자를 입력해주세요.');
+    }
+
+    if (input.split('').some((number) => input.indexOf(number) !== input.lastIndexOf(number))) {
+      throw new Error('[ERROR] 중복되지 않은 숫자를 입력해주세요.');
+    }
 
     this.model.update(input);
     return this;
