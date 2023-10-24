8001ad47d4d1133b43d3039dec44b8aa1933fcc8
Heejun Byeon <hejuby@gmail.com>
2023-10-24 19:55:58 +0900

    임의의 수 3개를 포함한 배열을 반환하는 generateRandomNumbers() 메소드 작성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ class App {
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
+
+  generateRandomNumbers() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+
+    return computer;
+  }
 }
 
 export default App;
