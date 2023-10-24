042b06b9531c4e18c625b3a4673b1bb966c59dd0
Galaxy <js_park95@naver.com>
2023-10-24 15:03:21 +0900

    fix: 테스트 실패 오류 수정
     return 값 설정, 오타 수정

--- src/App.js
+++ src/App.js
@@ -7,12 +7,9 @@ class App {
 
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
-    this.gameStart();
-  }
+    this.setComputer();
 
-  async gameStart(){
-    await this.setComputer();
-    await this.compareAnswer();
+    return this.compareAnswer();
   }
 
   // 컴퓨터 숫자 생성
@@ -21,12 +18,44 @@ class App {
     
     while(NUMBERS.length < 3){
       let num = Random.pickNumberInRange(1, 9);
-      if(!NUMBERS.includes(`${num}`)){
-        NUMBERS.push(`${num}`);
+      if(!NUMBERS.includes(num)){
+        NUMBERS.push(num);
       }
     };
 
     this.#computer = NUMBERS;
+
+    return;
+  }
+
+  // 사용자 입력값 받기
+  async userAnswer(){
+    const INPUT_ANSWER = await Console.readLineAsync('숫자를 입력해주세요 : ');
+    const TURN_INTO_NUMBER = INPUT_ANSWER.split('').map(Number);
+    await this.validateAnswer(TURN_INTO_NUMBER);
+
+    return TURN_INTO_NUMBER;
+  }
+  
+    // 3가지 예외 처리
+  async validateAnswer(answer){
+    const NUMBERS = Random.pickUniqueNumbersInRange(1, 9, 9);
+    
+    answer.map((num) => {
+      if(!NUMBERS.includes(num)){
+        throw new Error("[ERROR] 입력값이 숫자가 아닙니다.");
+      }
+    })
+
+    if(answer.length !== 3){
+      throw new Error("[ERROR] 입력값이 3자리가 아닙니다.");
+    }
+
+    if(answer[0] === answer[1] || answer[1] === answer[2] || answer[2] === answer[0]){
+      throw new Error("[ERROR] 중복된 값이 입력되었습니다.");
+    }
+
+    return;
   }
 
   // 사용자와 컴퓨터의 값 비교
@@ -35,51 +64,22 @@ class App {
     let strike = 0
     let ball = 0
 
-    USER_ANSWER.split('').map((num, i) => {
+    USER_ANSWER.map((num, i) => {
       if(num === this.#computer[i]){
         strike++
       }else if(this.#computer.includes(num)){
         ball++
       }
     })
-    
+
+    this.giveHint(strike, ball)
+
     if(strike !== 3){
-      await this.giveHint(strike, ball)
-      await this.compareAnswer()
+      return this.compareAnswer();
     }else if(strike === 3){
-      Console.print("3스트라이크")
-      await this.restartOrEnd()
+      return this.restartOrEnd();
     }
     
-  }
-
-  // 사용자 입력값 받기
-  async userAnswer(){
-    const USER_ANSWER = await Console.readLineAsync('숫자를 입력해주세요 : ');
-    await this.validateAnswer(USER_ANSWER)
-
-    return USER_ANSWER
-  }
-  
-    // 3가지 예외 처리
-  async validateAnswer(answer){
-    const NUMBERS = Random.pickUniqueNumbersInRange(1, 9, 9).toString();
-    const ANSWER = answer.split('');
-
-    ANSWER.map((num) => {
-      if(!NUMBERS.includes(num)){
-        throw new Error("[ERROR] 입력값이 숫자가 아닙니다.");
-      }
-    })
-
-    if(ANSWER.length !== 3){
-      throw new Error("[ERROR] 입력값이 3자리가 아닙니다.");
-    }
-
-    if(ANSWER[0] === ANSWER[1] || ANSWER[1] === ANSWER[2] || ANSWER[2] === ANSWER[0]){
-      throw new Error("[ERROR] 중복된 값이 입력되었습니다.");
-    }
-
   }
 
   // 정답이 아닐시 제공할 힌트
@@ -88,25 +88,34 @@ class App {
     if(strike === 0 && ball === 0){
       Console.print("낫싱");
     }else if(strike !== 0 && ball === 0){
-      Console.print(`${strike}스트라이트`);
+      Console.print(`${strike}스트라이크`);
     }else if(strike === 0 && ball !== 0){
       Console.print(`${ball}볼`);
     }else{
-      Console.print(`${strike}스트라이크 ${ball}볼`);
+      Console.print(`${ball}볼 ${strike}스트라이크`);
     }
+    
+    return;
   }
 
   // 게임 종료시 재시작 또는 종료 선택
   async restartOrEnd(){
-    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    const SELETED = await Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-    Console.print(SELETED);
+    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+    const SELETED = await Console.readLineAsync('');
     if(SELETED === '1'){
-      await this.gameStart();
+      this.setComputer();
+      return this.compareAnswer();
     }else if(SELETED === '2'){
+      Console.print("게임 종료")
       return;
+    }else{
+      throw new Error("[ERROR] 입력된 값이 1 또는 2가 아닙니다.")
     }
   }
 }
+
+const app = new App()
+app.play()
 
 export default App;
