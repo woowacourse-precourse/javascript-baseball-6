e31609326fa0508d7826b9872c4259a85d6c6359
isy <dbswl330@gmail.com>
2023-10-25 02:14:23 +0900

    feat: 게임 진행을 위한 루프 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 class App {
-  async play() {}
+  constructor() {
+    this.balls = new Balls();
+    this.view = new GameView();
+  }
+
+  async play() {
+    this.view.printStartGame();
+    while (true) {}
+  }
 }
 
 export default App;
+const app = new App();
+app.play();
