345f96103b224b22f9b38a065a9d1bb154b331a6
hyunyeee <5004ohy@naver.com>
2023-10-24 22:09:38 +0900

    feat: 게임 종료 후 다시 시작 여부 입력

--- BaseBallGame.js
+++ BaseBallGame.js
@@ -2,14 +2,15 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 const START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 const INPUT_MESSAGE = "숫자를 입력해주세요 : ";
-const ERROR_MESSAGE = "[ERROR] 입력 값이 잘못되었습니다.";
+const ERROR_MESSAGE = "[ERROR]";
 const CLEAR_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+const RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
 
 class BaseBallGame {
   async play() {
     try {
       MissionUtils.Console.print(START_MESSAGE);
-      this.#controller();
+      await this.#controller();
     } catch (error) {
       MissionUtils.Console.print(error.message);
       throw error;
@@ -18,18 +19,23 @@ class BaseBallGame {
 
   async #controller() {
     const computerNums = this.#generateComputerNumbers();
-
     let strikeCount = 0;
 
     while (strikeCount < 3) {
-      const userInput = await this.#readUserInput();
-      this.#checkValidation(userInput);
-      const result = this.#calculateResult(computerNums, userInput);
-      strikeCount = result.strikeCount;
+      try {
+        const userInput = await this.#readUserInput();
+        this.#checkValidation(userInput);
+        const result = this.#calculateResult(computerNums, userInput);
+        strikeCount = result.strikeCount;
 
-      if (strikeCount === 3) {
-        MissionUtils.Console.print(CLEAR_MESSAGE);
-        break;
+        if (strikeCount === 3) {
+          MissionUtils.Console.print(CLEAR_MESSAGE);
+          await this.#restartController();
+          return;
+        }
+      } catch (error) {
+        MissionUtils.Console.print(ERROR_MESSAGE);
+        throw error;  // 예외를 다시 던집니다.
       }
     }
   }
@@ -72,6 +78,16 @@ class BaseBallGame {
     MissionUtils.Console.print(result.trim())
     return { ballCount, strikeCount };
   }
+
+  async #restartController() {
+    const isRestart = await MissionUtils.Console.readLineAsync(RESTART_MESSAGE);
+
+    if (isRestart === '1') {
+      await this.#controller();
+    } else {
+      MissionUtils.Console.print("게임 종료");
+    }
+  }
 }
 
-export default BaseBallGame
+export default BaseBallGame;
