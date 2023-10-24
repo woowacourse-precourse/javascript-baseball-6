a719e8d44d40563df02e6b16bc5a5370b2af3e20
윤태웅 <omygog@naver.com>
2023-10-24 13:07:46 +0900

    feat: 사용자 입력 받기 및 유효성 검사

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
     const computerNumbers = [];
+    let playerAns = [];
     while(computerNumbers.length < 3){
       const randomNumber = Random.pickNumberInRange(1, 9);
       if(!computerNumbers.includes(randomNumber)){
         computerNumbers.push(randomNumber);
       }
     }
+    Console.print(computerNumbers);
+    playerAns = await Console.readLineAsync('숫자를 입력해주세요 : ');
+    this.inputValidator(playerAns);
+    Console.print(playerAns);
+  }
+
+  inputValidator(input) {
+    const regex = new RegExp(/^([1-9])(?!.*\1)([1-9])(?!.*\1|\2)([1-9])$/);
+    if(!regex.test(input)){
+      throw new Error('[ERROR] 서로 다른 3개의 숫자를 입력해주세요.');
+    }
+    return input;
   }
 }
 
