0e90620cd0461f5186b0d963d9addeda190bd8fc
Hayeon-Lee <papepopepe@naver.com>
2023-10-25 00:17:00 +0900

    fix: change for to .map
    for 반복문을 map으로 대체합니다.
    입력값의 예외검사를 이 파일에서 전부 진행합니다.

--- src/components/getUserNumber.js
+++ src/components/getUserNumber.js
@@ -1,10 +1,19 @@
 import { Console } from "@woowacourse/mission-utils";
 
 const getUsernumber = async () => {
-  try {
-    const usernumber = await Console.readLineAsync(inputQuery[0]);
-    return usernumber;
-  } catch (error) {}
+  const usernumber = await Console.readLineAsync("숫자를 입력해주세요. : ");
+  let usernumberList = [];
+
+  usernumberList = usernumber.split("").map(Number);
+  if (usernumberList.length !== 3) {
+    throw new Error("[ERROR] 3자리 숫자를 입력해주세요.");
+  } else if (usernumberList.some((num) => Number.isNaN(num))) {
+    throw new Error("[ERROR] 숫자를 입력해주세요.");
+  } else if (new Set(usernumberList).size !== 3) {
+    throw new Error("[ERROR] 중복되지 않은 3자리 숫자를 입력해주세요.");
+  }
+
+  return usernumberList;
 };
 
 export default getUsernumber;
