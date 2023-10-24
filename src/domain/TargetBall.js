9fcfefc3230266c492139b78b3d6d9908ec261e4
cobocho <rlaalsrb1111@naver.com>
2023-10-24 20:29:08 +0900

    Refactor: `CustomError` 변경사항 미수정 부분 반영

--- src/domain/TargetBall.js
+++ src/domain/TargetBall.js
@@ -5,42 +5,39 @@ import { ERROR_MESSAGE } from '../constants/error';
 import CustomError from '../exceptions/CustomError';
 
 export class TargetBall {
-  #number;
+	#number;
 
-  constructor(number) {
-    this.#validate(number);
-    this.#number = number;
-  }
+	constructor(number) {
+		this.#validate(number);
+		this.#number = number;
+	}
 
-  static MIN = 1;
+	static MIN = 1;
 
-  static MAX = 9;
+	static MAX = 9;
 
-  static #TARGET_NUMBERS = Object.fromEntries(
-    Array.from({ length: TargetBall.MAX }, (_, i) => [i + 1, new TargetBall(i + 1)]),
-  );
+	static #TARGET_NUMBERS = Object.fromEntries(
+		Array.from({ length: TargetBall.MAX }, (_, i) => [i + 1, new TargetBall(i + 1)])
+	);
 
-  static valueOf(value) {
-    const targetBall = TargetBall.#TARGET_NUMBERS[value];
-    return targetBall;
-  }
+	static valueOf(value) {
+		const targetBall = TargetBall.#TARGET_NUMBERS[value];
+		return targetBall;
+	}
 
-  getNumber() {
-    return this.#number;
-  }
+	getNumber() {
+		return this.#number;
+	}
 
-  #validate(number) {
-    if (typeof number !== 'number') {
-      throw new CustomError(ERROR_MESSAGE.common.notNumber);
-    }
-    if (!Number.isInteger(number)) {
-      throw new CustomError(ERROR_MESSAGE.common.notInteger);
-    }
-    if (isOutOfRange(number, TargetBall.MIN, TargetBall.MAX)) {
-      throw new CustomError(
-        ERROR_MESSAGE.common.outOfRange(TargetBall.MIN, TargetBall.MAX),
-        this.constructor.name,
-      );
-    }
-  }
+	#validate(number) {
+		if (typeof number !== 'number') {
+			throw new CustomError(ERROR_MESSAGE.common.notNumber);
+		}
+		if (!Number.isInteger(number)) {
+			throw new CustomError(ERROR_MESSAGE.common.notInteger);
+		}
+		if (isOutOfRange(number, TargetBall.MIN, TargetBall.MAX)) {
+			throw new CustomError(ERROR_MESSAGE.common.outOfRange(TargetBall.MIN, TargetBall.MAX));
+		}
+	}
 }
