a0c19d916f61a3ba673d2a8946c7d2a8b3e8ec3b
lottt105 <hyobin0601@naver.com>
2023-10-24 21:56:21 +0900

    Feat: 랜덤값(정답) 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const RANDOM_VALUE = this.getRandomNumber();
+  }
+
+  getRandomNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return `${computer[0]}${computer[1]}${computer[2]}`;
+  }
 }
 
 export default App;
