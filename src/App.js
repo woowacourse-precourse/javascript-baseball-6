2d65d4493823f431c2c147ecc79889a20284c7a3
23YUJIN <asdbnm066@daum.net>
2023-10-25 06:41:33 +0900

    feat: 게임 시작 메세지 출력 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async play() {
     let shouldExit = false;
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     while (shouldExit === false) {
       let computer = this.generateRandomNumber();
       let human = await this.getUserNumberInput();
