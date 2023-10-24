5023b08fcc6b94b3ed6c2986dffdb01d8da49c21
JungHun98 <j7papa@naver.com>
2023-10-24 10:25:11 +0900

    fix: 상수 재정의

--- src/modules/Constant.js
+++ src/modules/Constant.js
@@ -1,7 +1,7 @@
 // const numbers
 const MAX_NUM_LENGTH = 3;
-const EXIT = '1';
-const RESTART = '2';
+const RESTART = '1';
+const EXIT = '2';
 
 // result
 const STRIKE = '스트라이크';
@@ -10,6 +10,7 @@ const NOTHING = '낫싱';
 
 // prompt
 const START_PROMPT = '숫자 야구 게임을 시작합니다.';
+const EXIT_PROMPT = '게임 종료';
 const INPUT_PROMPT = '숫자를 입력해주세요 : ';
 const RESTART_PROMPT = `${MAX_NUM_LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료
 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`;
@@ -22,6 +23,7 @@ const Constant = {
   BALL,
   NOTHING,
   START_PROMPT,
+  EXIT_PROMPT,
   INPUT_PROMPT,
   RESTART_PROMPT,
 };
