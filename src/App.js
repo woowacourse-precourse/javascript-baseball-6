ef55815774dd07d196d7281feef8aff01d485fbe
Si Hyun Lee <pongpongi@Pongpongbook.local>
2023-10-25 03:56:45 +0900

    :sparkles: Play
    play() modified

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import { Console, MissionUtils, Random } from '@woowacourse/mission-utils';
-class App {
-  async play() {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    pushComputerNumber();
-    printUserAnswer();
-  }
+import { Console, Random } from '@woowacourse/mission-utils';
+
+function createComputerNumber() {
+  let numbers = [];
+  let number;
+  do {
+    number = Random.pickNumberInRange(1, 9);
+    if (!numbers.includes(number)) {
+      numbers.push(number);
+    }
+  } while (numbers.length < 3);
+  return numbers;
 }
 
-class Computer {}
-
-// 컴퓨터가 랜덤으로 숫자를 고름
-const computer = [];
-
-function pushComputerNumber() {
-  for (let i = 1; i < 4; i++) {
-    number = Random.pickNumberinRange(1, 10);
-    computer.push(number);
-  }
-}
-// 사용자의 대답 출력하기
 async function printUserAnswer() {
   try {
-    await Console.readLineAsync('숫자를 입력해주세요: ');
+    const answer = await Console.readLineAsync('숫자를 입력해주세요: ');
+    return answer;
   } catch (error) {
-    // reject 되는 경우 error 출력
-    Console.print('[ERROR] ');
+    Console.print('[ERROR]:', error);
   }
 }
 
-// 서로 다른 세 자리 수 입력받기
-
-// 결과 출력하기
+class App {
+  async play() {
+    try {
+      Console.print('숫자 야구 게임을 시작합니다.');
+      let computerAnswer = createComputerNumber();
+      let userAnswer = await Console.readLineAsync('숫자를 입력해주세요: ');
+      if (computerAnswer.join('') == userAnswer) {
+        Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      }
+    } catch (err) {
+      Console.print("[ERROR]: ", err)
+    }
+  }
+}
 
 const app = new App();
 app.play();
