875cceb6882475d7d50f5eb98a479add8d913df2
BaeDongWoo <ehddn5410@naver.com>
2023-10-25 02:18:38 +0900

    feat:사용자에게 입력 받기 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
 class App {
-  async play() {}
+  async play() {
+    const userNumber = await Console.readLineAsync('숫자를 입력해주세요 :');
+    Console.print(userNumber);
+  }
 }
-
+const app = new App();
+app.play();
 export default App;
