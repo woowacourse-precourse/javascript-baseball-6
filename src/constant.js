ee0d34e39f7a4079ce366dc073b70348ecea6633
mmeat512 <wndus0958@naver.com>
2023-10-24 17:03:22 +0900

    feat: 상수 추가

---
+++ src/constant.js
@@ -0,0 +1,20 @@
+export const RESTART_GAME = {
+  restart: '1',
+  exit: '2',
+};
+
+export const NUMBER_BASEBALL = {
+  length: 3,
+  start: '숫자 야구 게임을 시작합니다.',
+  input: '숫자를 입력해주세요 : ',
+  ball: '볼',
+  strike: '스트라이크',
+  nothing: '낫싱',
+  end: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  restart: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+};
+
+export const ERROR_MESSAGE = {
+  invalidData: '[ERROR] 숫자가 잘못된 형식입니다.',
+  duplicateNumber: '[ERROR] 중복된 숫자를 입력했습니다.',
+};
