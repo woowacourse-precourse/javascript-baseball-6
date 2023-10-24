6a5dc13d1e09e4ff44d4d8246fe30ef3c3782d97
leehyeyeong <elto789@naver.com>
2023-10-24 10:17:15 +0900

    feat: 컴퓨터 3개의 랜덤 값 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-class App {
-  async play() {}
-}
+import { Console, Random } from "@woowacourse/mission-utils";
 
+class App {
+  async play() {
+    Console.print("숫자 야구 게임 시작을 시작합니다.");
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    Console.print(computer);
+  }
+}
 export default App;
+
+const app = new App();
+app.play();
