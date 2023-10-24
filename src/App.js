cee07f1e6bc6ca5b9483ff7e0b2df53a6223b2c7
yohan <koyohanyoko@gmail.com>
2023-10-25 00:23:46 +0900

    [Feat] 사용자의 입력을 받는 기능

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ class App {
     while (this.isRunning) {
       const start = setAnswer();
       MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    
+      let result;
+      while (result !== "3 스트라이크") {
+        const input = await MissionUtils.Console.readLineAsync(
+          "숫자를 입력해주세요 : "
+        );
+      }
     }
   }
 
