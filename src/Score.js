080fa607d7c010e380ed566157da4a9c0adbdc00
yyeonkim <seyeon.dev@gmail.com>
2023-10-24 15:24:32 +0900

    chore: 조건문 수정

--- src/Score.js
+++ src/Score.js
@@ -58,10 +58,8 @@ class Score {
 
   /* 3 스트라이크 여부(숫자를 모두 맞췄는지)를 반환한다.  */
   isThreeStrike() {
-    if (this.#strike !== 3) {
-      return false;
-    }
-    return true;
+    if (this.#strike === 3) return true;
+    return false;
   }
 
   /* 점수를 초기화한다. */
