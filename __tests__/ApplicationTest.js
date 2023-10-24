5fc5455c567094264601b5062174a7451ef093ed
bl0ckp1n9 <blockp1n9@gmail.com>
2023-10-24 11:53:35 +0900

    test: mockRandoms 리팩토링

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -10,18 +10,15 @@ const mockQuestions = (inputs) => {
   });
 };
 
-const mockQuestion = (input) => {
-  MissionUtils.Console.readLineAsync = jest.fn();
-  MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    return Promise.resolve(input);
-  });
-};
-
 const mockRandoms = (numbers) => {
+  let originalPickNumberInRange = MissionUtils.Random.pickNumberInRange;
+
   MissionUtils.Random.pickNumberInRange = jest.fn();
   numbers.reduce((acc, number) => {
     return acc.mockReturnValueOnce(number);
   }, MissionUtils.Random.pickNumberInRange);
+
+  MissionUtils.Random.pickNumberInRange = originalPickNumberInRange;
 };
 
 const getLogSpy = () => {
@@ -32,48 +29,50 @@ const getLogSpy = () => {
 
 describe("숫자 야구 게임", () => {
   let app;
+
   beforeEach(() => {
     app = new App();
   });
-  // test("게임 종료 후 재시작", async () => {
-  //   // given
-  //   const randoms = [1, 3, 5, 5, 8, 9];
-  //   const answers = ["246", "135", "1", "597", "589", "2"];
-  //   const logSpy = getLogSpy();
-  //   const messages = [
-  //     "낫싱",
-  //     "3스트라이크",
-  //     "1볼 1스트라이크",
-  //     "3스트라이크",
-  //     "게임 종료",
-  //   ];
+
+  test("게임 종료 후 재시작", async () => {
+    // given
+    const randoms = [1, 3, 5, 5, 8, 9];
+    const answers = ["246", "135", "1", "597", "589", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "낫싱",
+      "3스트라이크",
+      "1볼 1스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when
+    const app = new App();
+    await expect(app.play()).resolves.not.toThrow();
+
+    // then
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
   //
-  //   mockRandoms(randoms);
-  //   mockQuestions(answers);
-  //
-  //   // when
-  //   const app = new App();
-  //   await expect(app.play()).resolves.not.toThrow();
-  //
-  //   // then
-  //   messages.forEach((output) => {
-  //     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-  //   });
-  // });
-  //
-  // test("예외 테스트", async () => {
-  //   // given
-  //   const randoms = [1, 3, 5];
-  //   const answers = ["1234"];
-  //
-  //   mockRandoms(randoms);
-  //   mockQuestions(answers);
-  //
-  //   // when & then
-  //   const app = new App();
-  //
-  //   await expect(app.play()).rejects.toThrow("[ERROR]");
-  // });
+  test("예외 테스트", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["1234"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
 
   describe("입력값 테스트", () => {
     test("3자리 숫자인 경우", () => {
@@ -99,16 +98,13 @@ describe("숫자 야구 게임", () => {
   });
 
   describe("랜덤값 테스트", () => {
-    test("자리수가 3자리인지 확인", () => {
+    test("랜덤값 길이 확인", () => {
       const randomNumber = app.generateRandomNumber();
-
       expect(randomNumber.length).toBe(3);
     });
-
-    test("중복값 확인", () => {
+    test("중복 확인", () => {
       for (let i = 0; i < 50; i++) {
         const randomNumber = app.generateRandomNumber();
-
         const regEx = /(\d).*\1/;
         expect(regEx.test(randomNumber)).toBe(false);
       }
