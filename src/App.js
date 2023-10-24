c99a3a81474f34d7f478b31d69bce64e58d810dc
sejineeee <sejinee0918@gmail.com>
2023-10-24 22:19:19 +0900

    feature: 랜덤 숫자를 반환하는 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  computer;
+
   constructor() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    this.computer = [];
   }
 
-  async play() {}
+  async play() {
+    this.getRandomNumber();
+  }
+
+  getRandomNumber() {
+    this.computer = [];
+    while (this.computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+
+      if (!this.computer.includes(number)) {
+        this.computer.push(number);
+      }
+    }
+  }
 }
 
 export default App;
