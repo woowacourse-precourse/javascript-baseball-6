77181a907f9460b7dafa2fdaa53cf17107712635
choekko <choekko@gmail.com>
2023-10-24 21:51:18 +0900

    feat: 게임 초기화 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from '@woowacourse/mission-utils'
+
 class App {
-  async play() {}
+  init() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
+  async play() {
+    this.init();
+  }
 }
 
 export default App;
