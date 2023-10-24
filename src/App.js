8fceb780217b965980a2cd9b96f8a386f5f62352
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-10-24 23:57:03 +0900

    Feat: 유저의 Input 수집& 잘못된 input 에러처리

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    const secret_number = random_number();
-    console.log(secret_number, "랜덤값");
+    let endPoint = false; //왜..?
+    while (!endPoint) {
+      // 게임 시작
+      MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+      //임의의 비밀 숫자 생성
+      const secret_number = randomNumber();
+      endPoint = await isThreeStrike(secret_number);
+
+      console.log(secret_number, "랜덤값");
+    }
   }
 }
-
-function random_number() {
+//함수: 임의의 비밀숫자 생성
+function randomNumber() {
   let computer = [];
   while (computer.length < 3) {
     const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
@@ -17,6 +24,24 @@ function random_number() {
     }
   }
   return computer;
+}
+//함수: 유저의 input 수집
+async function getUserInput() {
+  let user_guess = await MissionUtils.Console.readLineAsync(
+    "숫자를 입력해주세요 : "
+  );
+  //유저의 잘못된 input 에러 처리
+  if (!isValidInput(user_guess)) {
+    throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+  } else {
+    let user_guess_arr = user_guess.split("").map((e) => Number(e));
+    console.log(user_guess_arr, "유저");
+    return user_guess_arr;
+  }
+}
+//함수 : 유저의 잘못된 input 에러 처리
+function isValidInput(input) {
+  return input.length == 3 ? true : false;
 }
 
 export default App;
