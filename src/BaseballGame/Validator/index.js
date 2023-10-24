e634f64b0fd71a5ae78b813884255b2d85436abc
Collection50 <fkaus65@naver.com>
2023-10-24 22:06:30 +0900

    refactor: Validator 클래스의 메서드 명에 validate 접두사 추가하여 가독성 향상

--- src/BaseballGame/Validator/index.js
+++ src/BaseballGame/Validator/index.js
@@ -2,13 +2,13 @@ import { NUMS, ERROR } from '../constants/index.js';
 import ValidationError from './ValidationError/index.js';
 
 class Validator {
-  static guessNumber(input) {
+  static validateGuessNumber(input) {
     if (/[^1-9]/g.test(input) || new Set(input).size !== NUMS.THREE) {
       throw new ValidationError(ERROR.INVALID_LENGTH);
     }
   }
 
-  static controlNumber(input) {
+  static validateControlNumber(input) {
     if (![NUMS.REPLAY, NUMS.END].includes(input)) {
       throw new ValidationError(ERROR.INVALID_CONTROL_NUM);
     }
