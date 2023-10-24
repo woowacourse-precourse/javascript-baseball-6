53c359902436595893d6f4380e13be20bcc81baa
coolIlive <galilee155@naver.com>
2023-10-24 12:35:02 +0900

    Refactor: 재시작시 출력문 수정 및 필요없는 구문 삭제

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ const MESSAGE = {
   startMessage: `숫자 야구 게임을 시작합니다.`,
   inputUserNumber: `숫자를 입력해주세요 : `,
   allStrike: `3개의 숫자를 모두 맞히셨습니다! 게임 종료`,
-  askRetry: `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`,
+  askRetry: `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`,
   gameOver: `게임 종료`
 };
 
