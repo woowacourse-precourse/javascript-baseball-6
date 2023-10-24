b4e2604889d6702548a11d23aacd8780afe6f2c4
dooohun <ehgns0305@naver.com>
2023-10-24 21:51:31 +0900

    feat: 에러 메시지 상수 및 검증 패턴 추가

---
+++ src/constants/Errors.js
@@ -0,0 +1,9 @@
+export const ERROR = {
+  overlap: "[ERROR] 중복된 숫자를 입력했습니다.",
+  outOfRange: "[ERROR] 1에서 9까지의 숫자 외의 값을 입력했습니다.",
+  notThreeDigit: "[ERROR] 3자리 숫자가 아닙니다.",
+}
+
+export const OUT_OF_NUMBER_RANGE = /^[1-9]+$/;
+
+export const NUMBER_LENGTH = "3";
