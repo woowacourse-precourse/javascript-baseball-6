2cb3bdf2a22f6cb26339ed032ccab5c9628a9d91
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-10-24 22:01:14 +0900

    feat: random number 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import CreateRandomNumber from './RandomNumber/CreateRandomNumber.js';
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+
+    try {
+      const USER_NUMBER = await MissionUtils.Console.readLineAsync(
+        '숫자를 입력해주세요 : '
+      );
+      const COMPUTER_NUMBER = CreateRandomNumber();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 }
 
 export default App;
