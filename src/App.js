912a75c9177ed1bc8f735259f26459ed1c8fe68e
Jinjeon Shin <sjg729729@gmail.com>
2023-10-24 17:07:04 +0900

    feat: 사용자로부터 3개의 숫자를 입력받는 기능

--- src/App.js
+++ src/App.js
@@ -2,13 +2,42 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 const Message = {
   START: '숫자 야구 게임을 시작합니다.',
+  INPUT_MESSAGE: '숫자를 입력해주세요 : ',
+
+  INPUT_MESSAGE_ERROR: '3자리 숫자를 입력해야 합니다.',
 };
 
 class App {
   computer = [];
+  userInput;
 
   printStartMessage() {
     MissionUtils.Console.print(Message.START);
+  }
+
+  async getUserInput() {
+    const input = await MissionUtils.Console.readLineAsync(
+      Message.INPUT_MESSAGE
+    );
+
+    if (!Number.isInteger(+input)) {
+      throw new Error(Message.INPUT_MESSAGE_ERROR);
+    } else if (
+      input.length === 3 &&
+      input[0] !== '0' &&
+      input[1] !== '0' &&
+      input[2] !== '0'
+    ) {
+      this.userInput = input;
+    } else {
+      throw new Error(Message.INPUT_MESSAGE_ERROR);
+    }
+
+    if (input.length === 3 && Number.isInteger(+input)) {
+      this.userInput = input;
+    } else {
+      throw new Error(Message.INPUT_MESSAGE_ERROR);
+    }
   }
 
   pickComputerNumbers() {
@@ -24,7 +53,8 @@ class App {
   async play() {
     this.printStartMessage();
     this.pickComputerNumbers();
-    console.log(this.computer);
+    await this.getUserInput();
+    console.log(this.computer, this.userInput);
   }
 }
 
