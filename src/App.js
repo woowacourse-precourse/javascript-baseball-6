af43fbd4341109ceb35db129609b1a6c986bea00
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-10-24 23:31:44 +0900

    Feat: 게임시작&임의의 비밀숫자 생성

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    let computer = "";
-    while (computer.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer += number;
-      }
-    }
-    // console.log(computer, "출력값");
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    const secret_number = random_number();
+    console.log(secret_number, "랜덤값");
   }
+}
+
+function random_number() {
+  let computer = [];
+  while (computer.length < 3) {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(randomNumber)) {
+      computer.push(randomNumber);
+    }
+  }
+  return computer;
 }
 
 export default App;
