50747ead26843ca6217363b2c72bc838bdfd10d4
Jeon Eonseok <jseokeon@gmail.com>
2023-10-24 12:44:35 +0900

    feat: @woowacourse/mission-utils의 Random API를 사용하여 random 숫자값 생성하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+
+    const COMPUTER_GENERATED_NUM = [];
+
+    while (COMPUTER_GENERATED_NUM.length < 3) {
+      const RANDOM_SINGLE_NUM = Random.pickNumberInRange(1, 9);
+      
+      if (!COMPUTER_GENERATED_NUM.includes(RANDOM_SINGLE_NUM)) {
+        COMPUTER_GENERATED_NUM.push(RANDOM_SINGLE_NUM);
+      }
+    }
+  }
 }
 
 export default App;
