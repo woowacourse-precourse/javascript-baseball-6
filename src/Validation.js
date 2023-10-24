c497d0298c6f57a02b64625057ef476a82652351
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-10-25 01:23:46 +0900

    refactor: 유효성 검사에 사용되는 함수들을 모아 export 되도록 변경

--- src/Validation.js
+++ src/Validation.js
@@ -1,14 +1,16 @@
-export const isNumeric = (userInputValue) => /^[1-9]+$/.test(userInputValue);
+const isNumeric = (userInputValue) => /^[1-9]+$/.test(userInputValue);
 
-export const isUniqueDigits = (userInputValue) => {
+const isUniqueDigits = (userInputValue) => {
   const uniqueDigits = new Set(userInputValue.toString().split(''));
   return uniqueDigits.size === 3;
 };
 
 // eslint-disable-next-line import/prefer-default-export
-export const checkValidNumberDuringGame = (userInputValue) => {
+const checkValidNumberDuringGame = (userInputValue) => {
   if (userInputValue.toString().length !== 3) return false;
   if (!isUniqueDigits(userInputValue)) return false;
   if (!isNumeric(userInputValue)) return false;
   return true;
 };
+
+export { isNumeric, isUniqueDigits, checkValidNumberDuringGame };
