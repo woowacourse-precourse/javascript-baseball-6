e4c22c75794b44d91d9a34de288481fa4ff5754b
limdumb <dlaruddls@nate.com>
2023-10-24 14:10:11 +0900

    [Refact] userInput 함수 if문 조건 수정

--- src/function/userInput.js
+++ src/function/userInput.js
@@ -3,7 +3,7 @@ import { Console } from "@woowacourse/mission-utils";
 async function userInput() {
   const inputValue = await Console.readLineAsync("숫자를 입력해주세요 : ");
   
-  if (inputValue.length > 3 || isNaN(Number(inputValue))) {
+  if (inputValue.length !== 3 || isNaN(Number(inputValue))) {
     throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
   }
 
