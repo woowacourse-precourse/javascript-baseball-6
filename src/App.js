b0a1bcc4a6b38eb7db16cf94ca364e3fb20f9ae2
yhyem <yoonhyemin0120@gmail.com>
2023-10-25 01:01:38 +0900

    refactor: 함수 클래스 안으로 이동

--- src/App.js
+++ src/App.js
@@ -1,56 +1,56 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 
-function pickRandomNumber(array) {
-  while (array.length < 3) {
-    const number = Random.pickNumberInRange(1, 9);
-    if (!array.includes(number)) {
-      array.push(number);
+class App {
+  pickRandomNumber(array) {
+    while (array.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!array.includes(number)) {
+        array.push(number);
+      }
     }
   }
-}
 
-async function start() {
-  return Console.readLineAsync("숫자를 입력해주세요 : ");
-}
-
-function validate(data) {
-  if (data.length > 3 || data.length < 3 || isNaN(data)) {
-    throw new Error("[ERROR] 잘못된 값을 입력하였습니다.");
+  async start() {
+    return Console.readLineAsync("숫자를 입력해주세요 : ");
   }
-}
 
-function dataToArray(data, array) {
-  let count = 2;
-
-  while (array.length < 3) {
-    array.push(Math.floor(data / 10 ** count));
-    data = data % 10 ** count;
-
-    count--;
+  validate(data) {
+    if (data.length > 3 || data.length < 3 || isNaN(data)) {
+      throw new Error("[ERROR] 잘못된 값을 입력하였습니다.");
+    }
   }
-}
 
-function calculateStrike(randomArray, inputArray) {
-  let count = 0;
-  randomArray.map((data, index) => data === inputArray[index] && count++);
+  dataToArray(data, array) {
+    let count = 2;
 
-  return count;
-}
+    while (array.length < 3) {
+      array.push(Math.floor(data / 10 ** count));
+      data = data % 10 ** count;
 
-function calculateBall(randomArray, inputArray, strikeCount) {
-  let judgeArray = [];
+      count--;
+    }
+  }
 
-  judgeArray = randomArray.map((data) => inputArray.includes(data));
-  return judgeArray.filter((data) => data).length - strikeCount;
-}
+  calculateStrike(randomArray, inputArray) {
+    let count = 0;
+    randomArray.map((data, index) => data === inputArray[index] && count++);
 
-async function finish() {
-  return Console.readLineAsync(
-    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
-  );
-}
+    return count;
+  }
 
-class App {
+  calculateBall(randomArray, inputArray, strikeCount) {
+    let judgeArray = [];
+
+    judgeArray = randomArray.map((data) => inputArray.includes(data));
+    return judgeArray.filter((data) => data).length - strikeCount;
+  }
+
+  async finish() {
+    return Console.readLineAsync(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+    );
+  }
+
   async play() {
     let randomArray = [];
     let inputArray = [];
@@ -64,21 +64,21 @@ class App {
 
     while (resetCode !== "2") {
       inputArray = [];
-      pickRandomNumber(randomArray);
+      this.pickRandomNumber(randomArray);
 
-      inputData = await start();
-      validate(inputData);
+      inputData = await this.start();
+      this.validate(inputData);
 
-      dataToArray(inputData, inputArray);
+      this.dataToArray(inputData, inputArray);
 
-      strikeCount = calculateStrike(randomArray, inputArray);
-      ballCount = calculateBall(randomArray, inputArray, strikeCount);
+      strikeCount = this.calculateStrike(randomArray, inputArray);
+      ballCount = this.calculateBall(randomArray, inputArray, strikeCount);
 
       if (strikeCount === 3) {
         Console.print("3스트라이크");
         Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
-        resetCode = await finish();
+        resetCode = await this.finish();
         randomArray = [];
       } else if (strikeCount > 0 && ballCount > 0) {
         Console.print(ballCount + "볼 " + strikeCount + "스트라이크");
