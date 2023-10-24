2f1d8346cb983caa6f294adbeb9fcf4f81aa5c88
Hchan2 <seongchan_@naver.com>
2023-10-24 16:18:30 +0900

    컴퓨터 숫자 생성, 사용자 입력 기능 함수 분리 적용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { messageAllStrike, messageGameEnd, messageGameStart, messageInputNumber, messageRestartInputNumber } from "./constant/allPrintMessage";
-import { messageIsOverlap, messageNotNumber, messageNotOneToNine, messageNotRestartNumber, messageNotThreeDigits } from "./error/allErrorMessage";
+import { checkForReplay, isValidGuess } from "./error/validation";
+import generateRandomNumbers from "./computer/generateRandomNumbers";
+import getUserGuess from "./user/inputUserNumbers";
+import askForReplay from "./user/inputRestartNumber";
 
 class App {
   async play() {
     MissionUtils.Console.print(messageGameStart);
 
     while (true) {
-      const computer = this.generateRandomNumbers();
+      const computer = generateRandomNumbers();
       const gameResult = await this.playGame(computer);
 
       if (gameResult === "2") {
@@ -19,7 +22,7 @@ class App {
 
   async playGame(computer) {
     while (true) {
-      const userGuess = await this.getUserGuess();
+      const userGuess = await getUserGuess();
       const result = this.checkGuess(computer, userGuess);
 
       MissionUtils.Console.print(result);
@@ -30,41 +33,9 @@ class App {
       }
     }
 
-    const replay = await this.askForReplay();
-    this.checkForReplay(replay);
+    const replay = await askForReplay();
+    checkForReplay(replay);
     return replay;
-  }
-
-  generateRandomNumbers() {
-    const computer = [];
-    while (computer.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer.push(number);
-      }
-    }
-    return computer.join("");
-  }
-
-  async getUserGuess() {
-    let userGuess = await MissionUtils.Console.readLineAsync(messageInputNumber);
-    this.isValidGuess(userGuess)
-    return userGuess;
-  }
-
-  isValidGuess(guess) {
-    if (guess.length !== 3) {
-      throw new Error(messageNotThreeDigits);
-    }
-    if (isNaN(Number(guess))) {
-      throw new Error(messageNotNumber);
-    }
-    if (new Set(guess).size !== guess.length) {
-      throw new Error(messageIsOverlap);
-    }
-    if (guess < 1 && guess > 9) {
-      throw new Error(messageNotOneToNine);
-    }
   }
 
   checkGuess(computer, userGuess) {
@@ -81,16 +52,6 @@ class App {
       return "낫싱";
     }
     return `${balls > 0 ? balls + "볼 " : ""}${strikes > 0 ? strikes + "스트라이크" : ""}`;
-  }
-
-  async askForReplay() {
-    return await MissionUtils.Console.readLineAsync(messageRestartInputNumber);
-  }
-
-  async checkForReplay(replayInput) {
-    if (replayInput !== '1' && replayInput !== '2') {
-      throw new Error(messageNotRestartNumber);
-    }
   }
 }
 
