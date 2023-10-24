c3cae14cfff2f2d8c8385de3902f5bcdbaa06eed
stegano9744 <stegano9744@gmail.com>
2023-10-24 22:06:47 +0900

    🚀 perf: 배열 중복 검사 시간 복잡도 개선
    파일 경로: src/utils/get3DigitRandom.js
    이전에는 중복 확인에 O(n²) 시간 복잡도를 가지는 includes()를 사용했지만, Set을 사용하여 O(n)으로 개선했습니다
    
    시간 복잡도: O(n²) → O(n)
    공간 복잡도: O(n) → O(1)

--- src/utils/get3DigitRandom.js
+++ src/utils/get3DigitRandom.js
@@ -2,12 +2,10 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 // 컴퓨터 랜덤 숫자 생성
 export const get3DigitRandom = () => {
-  const computer = [];
-  while (computer.length < 3) {
+  const computer = new Set();
+  while (computer.size < 3) {
     const number = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!computer.includes(number)) {
-      computer.push(number);
-    }
+    computer.add(number);
   }
-  return computer;
+  return Array.from(computer);
 };
