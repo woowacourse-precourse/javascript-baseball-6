86c2fea9ba675eb5ef41df0697c78f46a7dc8288
dooohun <ehgns0305@naver.com>
2023-10-24 22:13:05 +0900

    refactor: 불필요한 줄넘김 덧셈 수정

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,15 +1,13 @@
 export const INPUT_MESSAGE = {
   start: '숫자를 입력해주세요 : ',
-  retry: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+  retry: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
 }
 
 export const OUTPUT_MESSAGE = {
   intro: "숫자 야구 게임을 시작합니다.",
-  outro: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-  end: "게임 종료",
+  outro: "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n",
+  end: "게임 종료"
 }
-
-export const FINISH_MESSAGE = OUTPUT_MESSAGE.outro + "\n" + INPUT_MESSAGE.retry + "\n";
 
 export const GAME_RESULT = {
   strike: "스트라이크",
@@ -18,3 +16,4 @@ export const GAME_RESULT = {
 }
 
 export const USER_CHOICE_RETRY = "1";
+export const FINISH_MESSAGE = OUTPUT_MESSAGE.outro + INPUT_MESSAGE.retry;
