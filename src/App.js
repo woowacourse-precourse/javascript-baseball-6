e7efbb4ecdc75219b86b9cb6cc4a8dac18a2b45b
naro_Kim <gracias9022@gmail.com>
2023-10-24 15:50:45 +0900

    feat: 기능 1. 게임 시작 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { getUserInput, getRandomNumber } from '@/components';
+import { print } from '@/utils';
+
 class App {
-  async play() {}
+  constructor() {
+    print('숫자 야구 게임을 시작합니다.');
+  }
+  async play() {
+    const computedAnswer = getRandomNumber();
+    const userInputNumber = getUserInput();
+
+    print(computedAnswer);
+    print(userInputNumber);
+  }
 }
 
 export default App;
