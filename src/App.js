feb9ab5cafca18c2ecd641ea9caa65bf8a9fab9a
pakxe <pigkill40@naver.com>
2023-10-25 07:19:43 +0900

    feat(App): 게임을 hw와 sw로 분리하여 실행시키도록 작성

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import BaseBallGameComputer from './Model/BaseBallGameComputer.js';
+import GameConsole from './Model/GameConsole.js';
 import BaseBallGame from './controller/BaseBallGame.js';
 
 class App {
-	#baseBallGame;
-	#model;
+	#gameConsole; // 게임 기기( 하드웨어 )
+	#gameSoftware; // 게임 칩 ( 소프트웨어 )
+	#controller;
 
 	constructor() {
-		this.#model = new BaseBallGameComputer();
-		this.#baseBallGame = new BaseBallGame(this.#model);
+		this.#gameConsole = new GameConsole();
+		this.#gameSoftware = new BaseBallGameComputer();
+		this.#controller = new BaseBallGame(this.#gameSoftware, this.#gameConsole);
 	}
 
 	async play() {
-		await this.#baseBallGame.start();
+		await this.#controller.start();
 	}
 }
 
 // const a = new App();
 // a.play();
+
 export default App;
