2d84a4ac87019f03209e28ee0bf0ca12be5169b5
DingX2 <pourding@naver.com>
2023-10-25 02:33:10 +0900

    [STYLE] 콘솔 띄어쓰기 수정

--- src/components/restartGame.js
+++ src/components/restartGame.js
@@ -3,7 +3,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 /* 다시 시작하는 기능 */
 export default async function restartGame() {
   const userInput = await MissionUtils.Console.readLineAsync(
-    `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`
+    `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. `
   );
   if (userInput === "1") {
     return true;
