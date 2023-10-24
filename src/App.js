78e2510fea9ef7ab218d5f8220cb867c629547e4
Brokyeom <goindol40@gmail.com>
2023-10-24 12:21:38 +0900

    feat : 컴퓨터 랜덤 숫자 생성자 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
+  constructor() {
+    this.computerNumbers = this.generateRandomNumbers();
+  }
+
+  generateRandomNumbers() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
+
   async play() {}
 }
 
