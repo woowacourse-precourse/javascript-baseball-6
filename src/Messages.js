b2fa6a89915ce9444c36cadbec244bd13543960a
Sonjusun <sonjs7554@naver.com>
2023-10-25 01:25:53 +0900

    feat : 스트라이크,볼,낫싱 구현 완료

--- src/Messages.js
+++ src/Messages.js
@@ -1,5 +1,10 @@
 const MESSAGES = Object.freeze({
   GAME_START: '숫자 야구 게임을 시작합니다',
   INSERT_NUMBER: '숫자를 입력해주세요 : ',
+  NOTHING: '낫싱',
+  STRIKE: '스트라이크',
+  BALL: '볼',
+  ANSWER: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  RESTART: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
 });
 export default MESSAGES;
--- src/controller/BaseballController.js
+++ src/controller/BaseballController.js
@@ -1,6 +1,6 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 import MESSAGES from '../Messages';
-import ERRORS from '../Errors.js';
+import ERRORS from '../Errors';
 
 class BaseballController {
   #Console;
@@ -26,15 +26,33 @@ class BaseballController {
       const number = Random.pickNumberInRange(1, 9);
       computer_balls.push(number);
     }
-    this.#Console.readLineAsync(MESSAGES.INSERT_NUMBER)
-      .then((result) => {
-        const inputArr = result.split(''); // ['1','2','3']
-        if (inputArr.length > 3) throw ERRORS.INPUT_LENGTH;
-        inputNumberTest(inputArr);
-      })
-      .catch((error) => {
-        console.log(`[ERROR]${error}`);
-      });
+    const gameResult = [0, 0];
+    do {
+      this.#Console.readLineAsync(MESSAGES.INSERT_NUMBER)
+        .then((result) => {
+          const inputArr = result.split(''); // ['1','2','3']
+          if (inputArr.length > 3) throw ERRORS.INPUT_LENGTH;
+          inputNumberTest(inputArr);
+          // 같은 숫자가 여러개 들어올 경우 에러 처리
+          computer_balls.forEach((computer_ball, comp_index) => {
+            inputArr.forEach((input_ball, user_index) => {
+              if (computer_ball === input_ball && comp_index === user_index) gameResult[0] += 1;
+              if (computer_ball === input_ball && comp_index !== user_index) gameResult[1] += 1;
+            });
+          });
+          if (gameResult[0] === 3) {
+            Console.print(MESSAGES.ANSWER);
+            this.#Console.readLineAsync(MESSAGES.RESTART);
+          }
+          if (gameResult === [0, 0]) Console.print(MESSAGES.NOTHING);
+          if (gameResult[1] === 0) Console.print(`${gameResult[0]}${MESSAGES.STRIKE}`);
+          if (gameResult[0] === 0) Console.print(`${gameResult[1]}${MESSAGES.BALL}`);
+          if (gameResult[0] !== 0 && gameResult[1] !== 0) Console.print(`${gameResult[1]}${MESSAGES.BALL} ${gameResult[0]}${MESSAGES.STRIKE}`);
+        })
+        .catch((error) => {
+          console.log(`[ERROR]${error}`);
+        });
+    } while (computer_balls.length === 3);// while 문의 경우, 거짓이 될 경우에 반복문 종료
   }
 }
 
