1970d6bb6582baff551758685dec4bb642269e44
00kang <kdb070970@gmail.com>
2023-10-25 00:35:43 +0900

    refactor: 사용자 입력 유효성 검사 함수로 분리

--- src/App.js
+++ src/App.js
@@ -6,12 +6,33 @@ class App {
     this.computerNum = this.generateComputerNumber();
   }
 
-  async play() {
-    Console.print('\n숫자 야구 게임을 시작합니다.');
+  // async play() {
+  //   Console.print('\n숫자 야구 게임을 시작합니다.');
 
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      // eslint-disable-next-line no-await-in-loop
+  //   while (true) {
+  //     const userNum = await this.getUserNumber(); // 사용자 숫자
+  //     if (userNum === null) {
+  //       Console.print('게임이 종료되었습니다.');
+  //       break;
+  //     }
+
+  //     const { strikes, balls } = this.compareNumbers(userNum); // 두 숫자 비교해서 strike와 ball 개수 세기
+
+  //     const isGameEnded = await this.checkResult(strikes, balls);
+  //     if (isGameEnded) {
+  //       // 비교 결과 확인후 출력, 이때 게임 종료 옵션(2)을 선택했다면 종료
+  //       break;
+  //     }
+  //   }
+
+  //   return null;
+  // }
+  async play() {
+    Console.print('\n숫자 야구 게임을 시작합니다');
+
+    let isGameEnded = false;
+
+    while (!isGameEnded) {
       const userNum = await this.getUserNumber(); // 사용자 숫자
       if (userNum === null) {
         Console.print('게임이 종료되었습니다.');
@@ -19,12 +40,10 @@ class App {
       }
 
       const { strikes, balls } = this.compareNumbers(userNum); // 두 숫자 비교해서 strike와 ball 개수 세기
-      // eslint-disable-next-line no-await-in-loop
-      if (await this.checkResult(strikes, balls)) {
-        // 비교 결과 확인후 출력, 이때 게임 종료 옵션(2)을 선택했다면 종료
-        break;
-      }
+      isGameEnded = await this.checkResult(strikes, balls);
     }
+
+    return null;
   }
 
   generateComputerNumber() {
@@ -42,54 +61,37 @@ class App {
   }
 
   async getUserNumber() {
-    let userNum;
+    try {
+      const userInput = await Console.readLineAsync('3자리 숫자를 입력하세요 (1~9): ');
+      const userNum = userInput ? userInput.trim() : '';
 
-    // eslint-disable-next-line no-unreachable-loop, no-constant-condition
-    while (true) {
-      try {
-        // eslint-disable-next-line no-await-in-loop
-        const userInput = await Console.readLineAsync('3자리 숫자를 입력하세요 (1~9): ');
-        userNum = userInput ? userInput.trim() : '';
-        const userNumbers = userNum.split('');
-        const userNumbersSet = new Set(userNumbers);
-
-        if (
-          userNum.length !== this.numberLength ||
-          !/^[1-9]{3}$/.test(userNum) ||
-          this.numberLength !== userNumbersSet.size
-        ) {
-          throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
-        } else {
-          return userNum;
-        }
-      } catch (error) {
-        Console.print('에러 발생: ' + error.message);
+      if (userNum === null) {
         return null;
       }
+
+      this.validateUserInput(userNum);
+      return userNum;
+    } catch (error) {
+      Console.print('에러 발생: ' + error.message);
+      return null;
     }
   }
-  // async getUserNumber() {
-  //   while (true) {
-  //     try {
-  //       const userInput = await Console.readLineAsync('3자리 숫자를 입력하세요 (1~9): ');
 
-  //       if (!userInput) {
-  //         Console.print('입력이 유효하지 않습니다. 다시 시도하세요.');
-  //         continue;
-  //       }
+  validateUserInput(userNum) {
+    if (userNum.length !== this.numberLength) {
+      throw new Error('[ERROR] 숫자는 3자리여야 합니다.');
+    }
 
-  //       const userNum = userInput.trim();
+    if (!/^[1-9]{3}$/.test(userNum)) {
+      throw new Error('[ERROR] 1부터 9까지의 숫자로만 입력해야 합니다.');
+    }
 
-  //       if (userNum.length !== 3 || !/^[1-9]{3}$/.test(userNum) || new Set(userNum).size !== 3) {
-  //         Console.print('[ERROR] 숫자가 잘못된 형식입니다. 다시 시도하세요.');
-  //       } else {
-  //         return userNum;
-  //       }
-  //     } catch (error) {
-  //       Console.print('에러 발생: ' + error.message);
-  //     }
-  //   }
-  // }
+    const userNumbersSet = new Set(userNum.split(''));
+
+    if (this.numberLength !== userNumbersSet.size) {
+      throw new Error('[ERROR] 중복된 숫자를 입력하면 안됩니다.');
+    }
+  }
 
   compareNumbers(userNum) {
     let strikes = 0;
