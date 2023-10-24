88c7f8dc71a4b52bb0296cfc33b7eb0224b1771c
mieum12 <qpdlqltb1215@gmail.com>
2023-10-24 20:04:49 +0900

    feat: 사용자 입력값 받기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-const { Console, Random } = require("@woowacourse/mission-utils");
+import { Console, Random } from "@woowacourse/mission-utils";
+import { CONSTANTS } from "./constants/constants";
+
 class App {
-  constructor() {}
+  constructor() {
+    this.userNum = "";
+    this.computerNum = [];
+  }
   // 컴퓨터 값 설정
+  SetComputerNumber() {}
   // 사용자 입력값 받기
+  async userInput() {
+    this.userNum = await Console.readLineAsync(CONSTANTS.MESSAGES.INPUT);
+    this.isValidate();
+  }
   // 입력값 유효성 검사 (타입, 길이, 0~9, 중복여부)
-  // 입력값을 컴퓨터 값과 비교하기
+  isValidate() {}
+  // 입력값을 컴퓨터 값과 비교해 결과 출력
+  async showResult() {}
   // 게임 종료 -> 재시작 여부 결정
+  async restartGame() {}
 
   // 게임 실행하기
   async play() {
-    MissionUtils.Console.print("숫자야구 게임을 시작합니다.");
+    Console.print(CONSTANTS.MESSAGES.START);
   }
 }
 
