ff6d9aba95e0ff791f4c3e365dcdf538e99cbc2a
cyj <cyd5538@naver.com>
2023-10-24 12:46:11 +0900

    Feat: user 넘버 받는 함수 구현

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ class App {
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
     const randomNumber = this.getRandomNumber();
+    this.getUserNumber()
   }
 
   getRandomNumber() {
@@ -15,6 +16,29 @@ class App {
       }
     }
     return computer;
+  }
+
+  async getUserNumber() {
+    try {
+      const number = await Console.readLineAsync("숫자를 입력해주세요 : ");
+      const numberDuplication = (new Set(number)).size !== number.length;
+
+      if(number.length !== 3) {
+        throw new Error("[ERROR] 숫자 3개를 입력해주세요.")
+      }
+
+      if(isNaN(number)) {
+        throw new Error("[ERROR] 숫자만 입력해 주세요.")
+      }
+
+      if(numberDuplication) {
+        throw new Error("[ERROR] 숫자를 중복되게 넣지 말아주세요.")
+      }
+      
+    } catch (error) {
+      console.log(error.message);
+      return;
+    }
   }
 }
 
