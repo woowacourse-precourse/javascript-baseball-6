fd061365267ac7b8cd24ea96b030db9c1592c591
doctork4 <doctor.k1994@gmail.com>
2023-10-24 15:34:09 +0900

    feat: 정답 생성을 위한 임의의 3자리 생성 기능

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { Console } from '@woowacourse/mission-utils';
 import { START_MESSAGE } from './constant.js';
 import receiveInput from './receiveInput.js';
 import validateUserInput from './validateUserInput.js';
+import makeAnswer from './makeAnswer.js';
 class App {
   async play() {
     Console.print(START_MESSAGE);
+    const answer = makeAnswer();
     const userInput = await receiveInput();
     validateUserInput(userInput);
   }
---
+++ src/makeAnswer.js
@@ -0,0 +1,14 @@
+import { Random } from "@woowacourse/mission-utils";
+
+function makeAnswer() {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer
+}
+
+export default makeAnswer;
