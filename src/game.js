587dc41436c954c9eb8a0c3bf8fd17f00483108a
yeonkyoung <keep.ykk@gmail.com>
2023-10-25 03:55:26 +0900

    feat: 볼과 스트라이크 결과에 대한 함수 생성

--- src/game.js
+++ src/game.js
@@ -9,3 +9,28 @@ const strikeCount = (playerInputNumberArray, computerPickedNumberArray) => {
     (number, index) => computerPickedNumberArray[index] === number
   ).length;
 };
+
+const isAllCompleted = (playerInputNumberArray, computerPickedNumberArray) => {
+  const ball = ballCount(playerInputNumberArray, computerPickedNumberArray);
+  const strike = strikeCount(playerInputNumberArray, computerPickedNumberArray);
+
+  if (strike === NUMBER_THREE) {
+    MissionUtils.Console.print(`${strike}${STRIKE_MESSAGE}`);
+    return true;
+  }
+
+  if (ball === NUMBER_ZERO) {
+    MissionUtils.Console.print(`${strike}${NOTHING_MESSAGE}`);
+    return false;
+  }
+
+  if (strike === NUMBER_ZERO) {
+    MissionUtils.Console.print(`${ball}${BALL_MESSAGE}`);
+    return false;
+  }
+
+  MissionUtils.Console.print(
+    `${ball - strike}${BALL_MESSAGE} ${strike}${STRIKE_MESSAGE}`
+  );
+  return false;
+};
