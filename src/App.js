0be2dc094dd19f2e38250402ed35f2735222de63
SU_VIN <pyotnqls25@naver.com>
2023-10-24 13:52:31 +0900

    Refactor: 기능요구에 맞게 시작 문구 출력 로직 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { MainGame } from "./MainGame.js";
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
   async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+
     const game = new MainGame();
-    await game.start();
+    return game.getResult();
   }
 }
 const app = new App();
