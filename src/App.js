b1656107b3ed1925c4b05bbd07b9939da8b8aa3d
An-jihyun <anjh322@gmail.com>
2023-10-25 05:29:28 +0900

    Feat: 입력 값 얻고 예외시 에러 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -13,6 +13,30 @@ class App {
     return computerNum;
   }
 
+  async getUserNum() {
+    let userNum = await MissionUtils.Console.readLineAsync(
+      "숫자를 입력해주세요 : "
+    );
+    userNum.split("").forEach((num, index) => {
+      if (isNaN(num)) {
+        throw new Error("[ERROR] 숫자를 입력해 주세요.");
+      }
+
+      if (num < 1 || num > 9) {
+        throw new Error("[ERROR] 1 ~ 9 사이의 숫자를 입력해 주세요.");
+      }
+
+      if (userNum.includes(num) && userNum.indexOf(num) !== index) {
+        throw new Error("[ERROR] 중복이 아닌 숫자를 입력해 주세요.");
+      }
+
+      if (userNum.length !== 3) {
+        throw new Error("[ERROR] 3자리 숫자를 입력해 주세요.");
+      }
+    });
+  }
+
+
   async play() {}
 }
 
