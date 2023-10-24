de666c5b39c9dd773056216b892b63fc0ff8a93a
gvsteve24 <junghyun30124@gmail.com>
2023-10-25 07:45:43 +0900

    added string type checking, modified generating 3 different digits and error message

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ class App {
   // 3. 숫자이외의 문자가 포함된 경우
   // 4. endGame입력: 1이나 2가 아닌 다른 문자가 주어지는 경우, 아니면 1도 아니고 2도 아닌 숫자
   async validateInput(mode, input) {
-    const inputString = input.toString();
     if (mode === 'guess') {
-      let a = inputString.length === 3;
-      let b = (/[1-9]{3}/).test(inputString);
-      let c = (new Set(inputString.split(''))).size === 3;
+      let a = input.length === 3;
+      let b = (/[1-9]{3}/).test(input);
+      let c = (new Set(input.split(''))).size === 3;
       return a && b && c;
     }else if (mode === 'resume') {
-      return (/[1-9]{1}/).test(inputString) && (input === 1 || input === 2);
+      let a = (/[1-9]{1}/).test(input);
+      let b = (input === "1" || input === "2");
+      return a && b;
     }
   }
 
@@ -28,24 +29,30 @@ class App {
     Console.print('숫자 야구 게임을 시작합니다.');
 
     while(resume) {
-      const target = Random.pickNumberInRange(111, 999);
-      const targetNums = [(Math.floor(target/100))%10, (Math.floor(target/10))%10, target%10];
-      if ((/[^1-9]/).test(String(target)) | (new Set(String(target).split('')).size != 3)) {
-        continue;
+      const targetNums = []; 
+      let pool = new Set(targetNums);
+      while(targetNums.length < 3) {
+        const target = Random.pickNumberInRange(1, 9);
+        if (pool.has(target)) {
+          continue;
+        }
+        pool.add(target);
+        targetNums.push(target);
       }
-      
+
       // guess 구간
       while(true) {
-        let inputNum = await this.getUserInput('숫자를 입력해주세요 : ');
+        let input = await this.getUserInput('숫자를 입력해주세요 : ');
         
         // exception handling
-        let validInput = await this.validateInput('guess', inputNum);
+        let validInput = await this.validateInput('guess', input);
         
         if (!validInput) {
           resume = false;
-          throw new Error("입력값이 유효하지 않습니다. 게임을 종료합니다.");
+          throw new Error("[ERROR]");
         }
-  
+        
+        let inputNum = parseInt(input, 10);
         let inputNums = [(Math.floor(inputNum/100))%10, (Math.floor(inputNum/10))%10, inputNum%10];
         
         let strike = 0, ball = 0;
@@ -66,10 +73,14 @@ class App {
           
           // exception handling
           let validInput = await this.validateInput('resume', endGame);
-          if (!validInput || endGame == 2) {
+          if (!validInput) {
+            throw new Error("[ERROR]");
+          }else if(endGame === "2") {
             resume = false;
+            break;
+          }else{
+            break;
           }
-          throw new Error("입력값이 유효하지 않습니다. 게임을 종료합니다.");
         }
   
         if ( strike > 0 && ball > 0 ) {
