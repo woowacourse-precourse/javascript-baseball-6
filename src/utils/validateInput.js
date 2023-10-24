bc3104c8af9c30e4152c6848acaa58c20c21a11f
stegano9744 <stegano9744@gmail.com>
2023-10-24 22:49:05 +0900

    🚀 perf: 공간 복잡도 개선
    파일 경로: src/utils/validateInput.js
    입력 문자열을 직접 검증하여 성능을 향상시켰습니다.
    
    시간 복잡도: O(n)
    공간 복잡도: O(n) → O(1)

--- src/utils/validateInput.js
+++ src/utils/validateInput.js
@@ -3,10 +3,6 @@ import { PROMPT } from '../constants/constants.js';
 // 사용자 입력값 검증
 export const validateInput = (user, LEN) => {
   const numberRangeRegex = /[^1-9]/g;
-  if (
-    LEN !== 3 ||
-    user !== user.replace(numberRangeRegex, '') ||
-    LEN !== [...new Set(user)].join('').length
-  )
+  if (LEN !== 3 || LEN !== user.replace(numberRangeRegex, '').length)
     throw new Error(PROMPT.ERROR);
 };
