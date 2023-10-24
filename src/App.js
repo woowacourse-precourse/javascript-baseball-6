b85c13740bcde7e2199be0e8cc77251d42bd9a7c
lottt105 <hyobin0601@naver.com>
2023-10-24 22:37:32 +0900

    Feat: 잘못된 입력값 확인

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ class App {
   async play() {
     const RANDOM_VALUE = this.getRandomNumber();
     const INPUT_VALUE = await this.getInputNumber();
+
+    this.checkInput(INPUT_VALUE);
   }
 
   getRandomNumber() {
@@ -21,6 +23,19 @@ class App {
     try {
       const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
       return input;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  // 입력 값이 숫자가 아니거나, 중복을 제거한 길이가 3이 아니면 throw error
+  checkInput(input) {
+    try {
+      const setInput = [...new Set(input)];
+
+      if (isNaN(parseInt(input)) || setInput.length !== 3) {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      }
     } catch (e) {
       console.error(e);
     }
