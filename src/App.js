59fc74f3c3ada905b4a886a6e51d9a0192bfb7a7
jaejun kim <wowns8114@gmail.com>
2023-10-24 11:10:03 +0000

    fix: 숫자를 맞췄을때 다시 시작하는기능이 고장난 문제를 해결
    - if 문 조건을 === 로 비교했는데 입력으로 받은 문자형과 enum을 비교하고 있어서 조건에 맞지않는 오류를 해결

--- src/App.js
+++ src/App.js
@@ -70,22 +70,22 @@ class App {
   static async play() {
     try {
       Console.print('숫자 야구 게임을 시작합니다.');
-      let cumputer = App.generateRandomNumber();
+      let computer = App.generateRandomNumber();
       while(true) {
         const number = await Console.readLineAsync('숫자를 입력해주세요 : ');
         App.isValidNumber(number);
         App.isSameNumber(number);
 
-        const gameWin = App.IsGameWin(cumputer, number.toString());
+        const gameWin = App.IsGameWin(computer, number.toString());
         if (gameWin){
           Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
           const input = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
           App.isValidRestart(input);
-          if (input === GameStatus.END) {
+          if (input == GameStatus.END) {
             Console.print('게임을 종료합니다.');
             return;
           }
-          cumputer = App.generateRandomNumber();
+          computer = App.generateRandomNumber();
         }
       }
     } catch (error) {
