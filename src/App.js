6799a7c7fa91810ee9c62ca88c5096a1f7f027bc
heu9422 <heu9422@naver.com>
2023-10-25 00:12:33 +0900

    feat: 예외처리 테스트 통과, 종료 후 재시작 테스트 미통과 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
+import { View } from "./View";
+import { BaseballGame } from "./BaseballGame";
+
 class App {
-  async play() {}
+  game;
+  view = View;
+
+  constructor() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  async play() {
+    this.game = new BaseballGame();
+    await this.guessNum();
+  }
+
+  async guessNum() {
+    const userNum = await this.view.readUserNum();
+    const { strike, ball } = this.game.compareNum(userNum);
+
+    this.printHint({ strike, ball });
+
+    if (strike !== 3) return this.guessNum();
+
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    this.chooseRestart();
+  }
+
+  printHint({ strike, ball }) {
+    this.view.printGameHint({ strike, ball });
+  }
+
+  async chooseRestart() {
+    const restart = await this.view.chooseRestart();
+
+    if (restart) return this.play();
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
