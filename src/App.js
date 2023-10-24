3f444e8eaff06d1f4e9c6b1aea8275a39b1e16bd
ccconac <qwerpoiu91@naver.com>
2023-10-24 18:42:54 +0900

    :sparkles: feat: 컴퓨터 숫자 랜덤 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+
+    const getComputerValue = () => {
+      const computer = [];
+      while (computer.length < 3) {
+        const number = Random.pickNumberInRange(1, 9);
+        if (!computer.includes(number)) {
+          computer.push(number);
+        }
+      }
+      return computer;
+    };
+
+    let computerValue = getComputerValue();
   }
 }
 
