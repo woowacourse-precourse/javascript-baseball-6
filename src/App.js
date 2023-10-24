c13caeeccff224f74168bc93b15bca30a8149436
seoyeon-jung <jungluchia1204@gmail.com>
2023-10-24 18:30:31 +0900

    feat: 컴퓨터 random 숫자 형성
    - 코드 전체를 다시 구성하기 위해 새로운 로직을 만들었씁니다
    - 컴퓨터 random 숫자를 생성하기 위한 makeComputerRandomNums() 메소드를 생성했습니다

--- src/App.js
+++ src/App.js
@@ -1,57 +1,24 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const { Random, Console } = MissionUtils;
 
 class App {
   constructor() {
-    this.randoms;
-    this.init();
+    this.computerRandomNums = [];
   }
 
-  init() {
-    this.randoms = this.getRandomNums();
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ", (answer) => {
-      if (!this.isValidBallNumber(answer)) {
-        throw new Error();
-      }
-    });
-  }
-
-  getRandomNums() {
-    const randomNums = [];
-    while (randomNums.length < 3) {
-      let num = MissionUtils.Random.pickNumberInRange(1, 9);
-
-      if (!randomNums.includes(num)) {
-        randomNums.push(num);
-      }
+  makeComputerRandomNums() {
+    while (this.computerRandomNums.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!this.computerRandomNums.includes(number))
+        this.computerRandomNums.push(number);
     }
-
-    return randomNums;
   }
 
-  isValidBallNumber(answer) {
-    if (answer.length !== 3) {
-      return false;
-    }
-    if (
-      answer[0] === answer[1] ||
-      answer[1] === answer[2] ||
-      answer[2] === answer[0]
-    ) {
-      return false;
-    }
-    if (isNaN(Number(answer))) {
-      return false;
-    }
-    return true;
-  }
-
-  play() {
-    console.log(this.randoms);
-  }
+  async play() {}
 }
 
 const app = new App();
 app.play();
 
-module.exports = App;
+export default App;
