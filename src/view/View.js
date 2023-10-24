1f27c897a5a117efce91396aebcbac20fe6697e5
Su Hwan CHOI <hap_hazard@naver.com>
2023-10-24 19:29:19 +0900

    refactor: View.js 내의 함수 클래스로 모듈화

--- src/view/View.js
+++ src/view/View.js
@@ -1,20 +1,20 @@
 import { Console } from '@woowacourse/mission-utils';
 
-/**
- * @description 사용자에게 메세지를 출력하는 함수
- * @param {string} message
- */
-function print(message) {
-  Console.print(message);
-}
+export default class View {
+  /**
+   * @description 사용자에게 메세지를 출력하는 함수
+   * @param {string} message
+   */
+  print(message) {
+    Console.print(message);
+  }
 
-/**
- * @description 사용자에게 메세지를 입력받고, 받은 입력을 출력하는 함수
- * @param {string} message
- * @returns {Promise<string>}
- */
-function input(message) {
-  return Console.readLineAsync(message);
+  /**
+   * @description 사용자에게 메세지를 입력받고, 받은 입력을 출력하는 함수
+   * @param {string} message
+   * @returns {Promise<string>}
+   */
+  input(message) {
+    return Console.readLineAsync(message);
+  }
 }
-
-export { print, input };
