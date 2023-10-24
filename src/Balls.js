a5037698c269f89faf6ae0832da31f5ea8387cb4
isy <dbswl330@gmail.com>
2023-10-25 02:02:03 +0900

    feat: 볼 개수 계산하는 함수 구현

--- src/Balls.js
+++ src/Balls.js
@@ -14,11 +14,13 @@ class Balls {
     Console.print(threeDigitNumber);
     return threeDigitNumber;
   }
+
   inputValidation(inputNumbers) {
     if (!(/^\d{3}$/.test(inputNumbers) && new Set(inputNumbers).size === 3)) {
       throw new Error("[ERROR]");
     }
   }
+
   calculateStrike(inputNumbers) {
     const guessNumbers = inputNumbers.split("").map(Number);
     let strikes = 0;
@@ -29,5 +31,19 @@ class Balls {
       }
     }
     return strikes;
+  }
+
+  calculateBall(inputNumbers) {
+    const guessNumbers = inputNumbers.split("").map(Number);
+    let balls = 0;
+    for (let i = 0; i < 3; i += 1) {
+      if (
+        guessNumbers[i] !== this.randomNumbers[i] &&
+        this.randomNumbers.includes(guessNumbers[i])
+      ) {
+        balls += 1;
+      }
+    }
+    return balls;
   }
 }
