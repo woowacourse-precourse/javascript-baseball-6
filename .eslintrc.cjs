b4ef712c4bef00fb2711d1b906be1f9ae0f6c371
ryan-dia <cjfdnjs158@naver.com>
2023-10-25 08:38:46 +0900

    chore: eslint 설정 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,10 +11,16 @@ module.exports = {
   rules: {
     // ESM 사용으로 인한 파일 확장자 표기 의무화
     'import/extensions': ['error', 'always', { ignorePackages: true }],
+
+    // JSDoc과 관련된 오류 침묵
+    'jsdoc/require-param-description': 'off',
+    'jsdoc/require-returns-description': 'off',
+
+    'import/export': 0,
   },
   overrides: [
     {
-      files: ['__tests__/**/*.js'],
+      files: ['__tests__/**/*.js', 'src/utils/validator/utils/**/*.js'],
       rules: {
         'max-lines-per-function': 'off',
         'no-new': 'off',
