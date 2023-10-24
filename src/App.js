20c22c626fd19dec6b6c62c895799a181e698e26
ChanYong Park <krap5075@naver.com>
2023-10-25 02:09:56 +0900

    feat: 3개 랜덤숫자 선택하는 함수구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
+  // 3개 숫자 선택
+  pickRandNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
 }
 
+const app = new App();
+app.play();
 export default App;
