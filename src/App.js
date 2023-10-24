a456aa091d517f3a7ee8ac05f8f20f501858895a
Klomachenko <leekyumin0901@naver.com>
2023-10-25 01:55:46 +0900

    feat: 정답을 생성하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async play() {
 
+    let correctAnswer;
+
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 
+    const answer = [];
+    while (answer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!answer.includes(number)) {
+        answer.push(number);
+      }
+    }
+    correctAnswer = answer.join('');
   }
 }
 
