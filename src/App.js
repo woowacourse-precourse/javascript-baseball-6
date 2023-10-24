37806b562e238486adfc49e7f67bd6da1cdd48be
Majesty-jun <hkwns01@gmail.com>
2023-10-24 15:19:20 +0900

    FEAT: 컴퓨터 3자리 랜덤 숫자 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = String(Random.pickNumberInRange(1, 9));
+      if(!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    console.log(computer);
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
