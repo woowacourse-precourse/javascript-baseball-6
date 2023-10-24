7ca82f34b63c1274437c1bed3f252a44322ad13e
meaculpa9450 <132068319+meaculpa9450@users.noreply.github.com>
2023-10-24 09:32:50 +0900

    style: 불필요한 분리(1줄 이내 함수) 통합

--- src/lib/HandleInput/index.js
+++ src/lib/HandleInput/index.js
@@ -1,12 +1,11 @@
-const ReadLine = require("./readLine");
-const Validate = require("./validate");
-const Parse = require("./parse");
+const { Console } = require("@woowacourse/mission-utils");
+const { COMMAND_HASH, ERROR_MESSAGE, INPUT_MESSAGE, VALIDATION_RULE } = require("../Constants");
 
 const handleGuessInput = async function () {
     let response;
     try {
-        response = await ReadLine.readGuessInput();
-        Validate.validateGuessInput(response);
+        response = await Console.readLineAsync(INPUT_MESSAGE.GUESS, (answer) => answer);
+        validateGuessInput(response);
     } catch(e) {
         throw e;
     };
@@ -16,13 +15,33 @@ const handleGuessInput = async function () {
 const handleCommandInput = async function () {
     let response;
     try {
-        response = await ReadLine.readCommandInput();
-        Validate.validateCommandInput(response);
-        response = Parse.parseCommandInput(response);
+        response = await Console.readLineAsync(INPUT_MESSAGE.COMMAND, (answer) => answer);
+        validateCommandInput(response);
+        response = COMMAND_HASH[response];
     } catch(e) {
         throw e;
     };
     return response;
 };
+
+const validateGuessInput = function (input) {
+    const visited = new Set();
+    if (parseInt(input) !== Number(input)) throw new Error(ERROR_MESSAGE.NOT_AN_INT);
+    if (input.length !== VALIDATION_RULE.VALID_GUESS_LENGTH) throw new Error(ERROR_MESSAGE.INVALID_GUESS_LENGTH);
+    for (const char of input) {
+        const number = parseInt(char);
+        if (visited.has(char)) throw new Error(ERROR_MESSAGE.UNIQUE_CONSTRAINT_VIOLATED);
+        if (number < VALIDATION_RULE.GUESS_MIN_VALUE_INCLUSIVE) throw new Error(ERROR_MESSAGE.INVALID_GUESS_VALUE);
+        if (VALIDATION_RULE.GUESS_MAX_VALUE_INCLUSIVE < number) throw new Error(ERROR_MESSAGE.INVALID_GUESS_VALUE);
+        visited.add(char);
+    };
+    return true;
+};
+
+const validateCommandInput = function (input) {
+    if (parseInt(input) !== Number(input)) throw new Error(ERROR_MESSAGE.NOT_AN_INT);
+    if (!VALIDATION_RULE.VALID_COMMAND.includes(input)) throw new Error(ERROR_MESSAGE.INVALID_COMMAND);
+    return true;
+}
 
 module.exports = { handleCommandInput, handleGuessInput };
--- src/lib/HandleInput/parse.js
+++
@@ -1,7 +0,0 @@
-const { COMMAND_HASH } = require("../Constants");
-
-const parseCommandInput = function (input) {
-    return COMMAND_HASH[input];
-};
-
-module.exports = { parseCommandInput };
