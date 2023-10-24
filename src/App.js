6ce4b3bca753d3b30097b67e19d8bbd965086bdd
seoyeon-jung <jungluchia1204@gmail.com>
2023-10-24 18:12:14 +0900

    feat: 게임 시작 초기화 및 랜덤 출력 기능
    - getRandomNum() : 중복 없는 숫자 3개를 배열에 추가하는 함수
    - play() 안에 있던 console을 init() 메소드로 리팩토링했습니다

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  play() {
+  constructor() {
+    this.randoms;
+    this.init();
+  }
+
+  init() {
+    this.randoms = this.getRandomNums();
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ", (answer) => {
       if (!this.isValidBallNumber(answer)) {
         throw new Error();
       }
     });
+  }
+
+  getRandomNums() {
+    const randomNums = [];
+    while (randomNums.length < 3) {
+      let num = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (!randomNums.includes(num)) {
+        randomNums.push(num);
+      }
+    }
+
+    return randomNums;
   }
 
   isValidBallNumber(answer) {
@@ -26,9 +45,13 @@ class App {
     }
     return true;
   }
+
+  play() {
+    console.log(this.randoms);
+  }
 }
 
-const init = new App();
-init.play();
+const app = new App();
+app.play();
 
 module.exports = App;
