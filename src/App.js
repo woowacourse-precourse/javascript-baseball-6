79fd6e2947b717ba5feee1a9a1c8ab19985af39e
Majesty-jun <hkwns01@gmail.com>
2023-10-24 15:50:46 +0900

    FEAT: 사용자 입력값 받기 및 유효성 검사 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Random } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
@@ -9,7 +9,22 @@ class App {
         computer.push(number);
       }
     }
-    console.log(computer);
+    while(1) {
+      let userInput = await Console.readLineAsync("숫자를 입력해주세요 : ");
+      
+      if(userInput.length !== 3) throw new Error("3자리의 숫자를 입력해주세요.");
+      
+      const UNIQUE_NUM = new Set();
+      for(let i = 0; i < userInput.length; i++) {
+        UNIQUE_NUM.add(userInput[i]);
+      }
+      if(UNIQUE_NUM.size !== 3) throw new Error("서로 다른 3자리의 수를 입력해주세요.");
+
+      let isOneToNineNum = userInput.split('').every(number => Number(number) >= 1 && Number(number) <= 9)
+      if(!(isOneToNineNum)) throw new Error('1~9 사이의 숫자를 입력해주세요.');
+
+      console.log(userInput);
+    }
   }
 }
 
