f337c6ecc5135ac85263b0713eb36591d7ccdb7d
LJH <049494@ewhain.net>
2023-10-24 18:52:10 +0900

    ♻️ 문구 출력 중복 코드 줄이기

--- src/App.js
+++ src/App.js
@@ -54,12 +54,8 @@ class App {
         if (STRIKE === 0 && BALL === 0) {
           MissionUtils.Console.print("낫싱");
         } else if (STRIKE === 3) {
-          MissionUtils.Console.print("3스트라이크");
           MissionUtils.Console.print(
-            "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
-          );
-          MissionUtils.Console.print(
-            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+            "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
           );
           const CHOICE = await MissionUtils.Console.readLineAsync("");
           if (CHOICE === "1") {
