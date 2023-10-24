b2ea16a10a789d74b3b94a771585884646b942a8
gwahyun <60453182+gwahyun@users.noreply.github.com>
2023-10-24 13:49:50 +0900

    Feat: 게임 정답을 생성하는 함수 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.answer;
+  }
+  async play() {
+    this.answer = this.randomNumbersArray();
+
+    Console.print('숫자 야구 게임을 시작합니다.');
+    Console.readLine('숫자를 입력해주세요 :', (userInput) => {
+    });
+
+
+  }
+
+  randomNumbersArray() {
+    return Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
+
 }
+
+const app = new App();
+app.play();
 
 export default App;
