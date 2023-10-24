dfa65c51d802d911fa2a501a3c94f9891313fb40
0jenn0 <jenn0.6n@gmail.com>
2023-10-25 06:11:38 +0900

    Refactor: Referee - getHint 메서드 나누기

--- src/Refree.js
+++ src/Refree.js
@@ -8,27 +8,36 @@ export default class Referee {
     this.ball = 0;
   }
 
-  getHint(answer, guess) {
-    this.strike = 0;
-    this.ball = 0;
-    for (let i = 0; i < 3; i++) {
-      if (answer[i] === guess[i]) {
+  calculateScore(answer, guess) {
+    [...guess].forEach((num, idx) => {
+      if (num === answer[idx]) {
         this.strike++;
-      } else if (answer.includes(guess[i])) {
+      } else if (answer.includes(num)) {
         this.ball++;
       }
-    }
+    });
+  }
 
+  printResult() {
     if (this.strike === 0 && this.ball === 0) {
       Console.print("낫싱");
       return;
     }
+
     const result = [];
     this.ball && result.push(`${this.ball}볼`);
     this.strike && result.push(`${this.strike}스트라이크`);
 
     const output = result.join(" ");
     if (output) Console.print(output);
+  }
+
+  getHint(answer, guess) {
+    this.strike = 0;
+    this.ball = 0;
+
+    this.calculateScore(answer, guess);
+    this.printResult();
   }
 
   isThreeStrikes() {
