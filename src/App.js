70da13a6d8e52bc806e4166081f9e1039717628e
jujube12 <gyubam1@gmail.com>
2023-10-25 02:41:20 +0900

    에러 처리 수정

--- src/App.js
+++ src/App.js
@@ -16,19 +16,15 @@ class App {
       // 숫자 입력 반복
       let answer = true;
       while(answer) {
-        try {
           let ball = 0;
           let strike = 0;
           let input = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
           // 입력 형식 체크 (숫자가 아니거나 3자리수가 아니거나 0이 들어가거나 중복된 값이 있으면 안됨)
           let Arr = input.split('');
           Arr = [...new Set(Arr)]
-          if(isNaN(input) || Arr.length != 3) {   
-            throw new Error();
+          if(isNaN(input) || Arr.length != 3 || Arr.indexOf('0') >= 0) {   
+            throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
           }
-            if(Arr.indexOf('0') >= 0){
-              throw new Error();
-            }
             // ball, strike 개수
             num.map((a, i)=> {
               if(Arr.indexOf(String(a)) >= 0) {
@@ -42,12 +38,12 @@ class App {
             // 정답을 맞췄을 때
             if(strike === 3) {
               MissionUtils.Console.print('3스트라이크');
-              MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+              MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다!');
+              MissionUtils.Console.print('게임 종료');
               MissionUtils.Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
               // 사용자 입력 체크
               while(true){
                 let a = await MissionUtils.Console.readLineAsync('');
-                try {
                   if (a === '1') {  //새로운 게임 시작
                     answer = false;
                     break;
@@ -56,11 +52,8 @@ class App {
                     conti = false;
                     break;
                   } else {  //에러처리
-                    throw Error();
+                    throw new Error("ERROR] 숫자가 잘못된 형식입니다.");
                   };
-                } catch(err) {
-                  MissionUtils.Console.print(`[ERROR] 숫자가 잘못된 형식입니다.`);
-                }
               }
             } 
             // 정답이 아닐 때
@@ -69,16 +62,13 @@ class App {
                 if (strike === 0) {
                   MissionUtils.Console.print(`낫싱`);
                 } else {
-                  MissionUtils.Console.print(`${strike} 스트라이크`);
+                  MissionUtils.Console.print(`${strike}스트라이크`);
                 }
               } else if (strike === 0) {
-                MissionUtils.Console.print(`${ball} 볼`);
+                MissionUtils.Console.print(`${ball}볼`);
               } else {
-                MissionUtils.Console.print(`${ball}볼 ${strike} 스트라이크`);
+                MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
               }
-            }
-        } catch(err) {
-          MissionUtils.Console.print(`[ERROR] 숫자가 잘못된 형식입니다.`);
         }
       }
     }
