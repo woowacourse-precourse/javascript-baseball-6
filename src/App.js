dd2d286554849cd763290feecabf9016c2ac908c
suwonthugger <ddww1029wwdd@gmail.com>
2023-10-24 15:52:30 +0900

    Feat: 사용자의 값을 입력 받고 예외 값을 처리하는 기능 추가
    게임이 종료된 경우 1또는 2의 값을 반환하고
    게임 진행 중인 경우에는 사용자가 입력한 서로 다른 3개의 값을 반환

--- src/App.js
+++ src/App.js
@@ -13,6 +13,60 @@ class App {
 
   async play() {}
 
+  async userInput(isCorrect) {
+    if (isCorrect) {
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+      const userInput = await Console.readLineAsync("");
+      const inputNum = Number(userInput);
+
+      if (inputNum !== 1 && inputNum !== 2) {
+        throw new Error("[ERROR]");
+      }
+
+      return inputNum;
+    }
+
+    const userInput = await Console.readLineAsync("숫자를 입력해주세요 : ");
+    const inputArray = Array.from(userInput, Number);
+    if (
+      !inputArray ||
+      inputArray.length !== 3 ||
+      inputArray.length !== new Set(inputArray).size
+    ) {
+      throw new Error("[ERROR]");
+    }
+
+    return inputArray;
+  }
+
+  async userInput(isCorrect) {
+    if (isCorrect) {
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+      const userInput = await Console.readLineAsync("");
+      const inputNum = Number(userInput);
+
+      if (inputNum !== 1 && inputNum !== 2) {
+        throw new Error("[ERROR]");
+      }
+
+      return inputNum;
+    }
+
+    const userInput = await Console.readLineAsync("숫자를 입력해주세요 : ");
+    const inputArray = Array.from(userInput, Number);
+    if (
+      !inputArray ||
+      inputArray.length !== 3 ||
+      inputArray.length !== new Set(inputArray).size
+    ) {
+      throw new Error("[ERROR]");
+    }
+
+    return inputArray;
+  }
+
   randomGenerator() {
     const randomNum = [];
     while (randomNum.length < 3) {
