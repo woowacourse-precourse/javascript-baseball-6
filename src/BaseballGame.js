1fc3e610df395e14de693592582a5d24d243771a
junehee <dev.junehee@gmail.com>
2023-10-24 17:52:42 +0900

    feat: 사용자 입력값(playerInput) 받기 및 유효성 검사 메소드 구현

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -21,6 +21,32 @@ class BaseballGame extends Computer {
     this.randomNumber = super.createRandomNumber();
     await this.getPlayerInput();
   }
+
+  // 사용자 입력값 받기
+  async getPlayerInput() {
+    try {
+      const playerInput = await Console.readLineAsync(
+        MESSAGES.game.playerInput
+      );
+
+      this.validationPlayerInput(playerInput);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // 사용자 입력값 유효성 검사 (3자리의 숫자인지 확인)
+  async validationPlayerInput(playerInput) {
+    const rInput = /^(?!.*(.).*\1)[1-9]{3}$/;
+
+    if (rInput.test(playerInput)) {
+      const strikeBall = super.countStrikeBall(this.randomNumber, playerInput);
+
+      this.printStrikeBall(strikeBall);
+    } else {
+      throw new Error(MESSAGES.errors.invalidNumber);
+    }
+  }
 }
 
 export default BaseballGame;
