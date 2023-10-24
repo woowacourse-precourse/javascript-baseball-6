03dac0d3666e817d48e4d472090a56d6913ce62d
hwsg0912@naver.com <hwsg0912@naver.com>
2023-10-24 18:08:50 +0900

    Refactor: 출력 메세지 모듈 사용

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import { Console, Random } from "@woowacourse/mission-utils";
+const MissionUtils = require('@woowacourse/mission-utils');
+const Messages = require('./constants/Messages');
 
 class App {
 
   async play() {
-    Console.print('숫자야구 게임을 시작합니다.');
-    Console.print(this.generateRandomNumbers())
-  }
-  generateRandomNumbers() {
-    const differentRandomNumbers = [];
-    while (differentRandomNumbers.length < 3) {
-      const number = Random.pickNumberInRange(1, 9);
-      if (!differentRandomNumbers.includes(number)) {
-        differentRandomNumbers.push(number);
+    // 서로 다른 임의의 3자리 숫자 생성
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
       }
     }
-    return differentRandomNumbers;
+    // 게임 시작시 메세지 출력
+    MissionUtils.Console.print(Messages.START);
   }
-  
 }
 let app = new App();
 app.play();
