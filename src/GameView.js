ee0757e28d9afa2c376d0351687267b406160c81
isy <dbswl330@gmail.com>
2023-10-25 02:06:29 +0900

    feat: 게임 실패 시 힌트 출력하는 함수 구현
    - 볼, 스트라이크 개수

--- src/GameView.js
+++ src/GameView.js
@@ -9,8 +9,17 @@ class GameView {
     const inputNumbers = await Console.readLineAsync("숫자를 입력해주세요 : ");
     return inputNumbers;
   }
+
   printSuccess() {
     Console.print("3스트라이크");
     Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  }
+
+  printHint(strikes, balls) {
+    if (strikes === 0 && balls === 0) {
+      Console.print("낫싱");
+    } else {
+      Console.print(`${balls}볼 ${strikes}스트라이크`);
+    }
   }
 }
