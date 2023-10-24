21b9db3cc48c410133cd3fbffdbddf51e209e9c2
dooohun <ehgns0305@naver.com>
2023-10-24 21:56:12 +0900

    feat: 야구게임 플레이 함수 구현

---
+++ src/BaseballGame.js
@@ -0,0 +1,56 @@
+import { INPUT_MESSAGE, FINISH_MESSAGE } from "./constants/messages.js";
+import { validateInput } from "./ValidateInput.js";
+import { generateRandomNumber } from "./RandomNumber.js";
+import { printIntroMessage, printGameResult, printEndMessage } from "./View.js";
+import { calculateScore } from "./CalculateScore.js";
+import { Console } from "@woowacourse/mission-utils";
+
+async function getUserInput() {
+  try {
+    const userNumber = await Console.readLineAsync(INPUT_MESSAGE.start);
+    validateInput(userNumber);
+    return userNumber;
+  }
+  catch (error) {
+    throw error;
+  }
+}
+
+async function askRestart() {
+  const retry = await Console.readLineAsync(FINISH_MESSAGE);
+  if (retry == "1") return true;
+  else return false;
+}
+
+async function startGame() {
+  const computer = generateRandomNumber();
+  let score;
+  let user;
+  try {
+    do {
+      user = await getUserInput();
+      score = calculateScore(computer, user);
+      printGameResult(score);
+    } while (score.strike !== 3)
+  }
+  catch (error) {
+    throw error;
+  }
+}
+
+async function playGame() {
+  let retry;
+	try{
+		do {
+	    printIntroMessage();
+	    await startGame();
+	    retry = await askRestart()
+	  } while(retry)
+	  printEndMessage();
+	}
+	catch (error) {
+		throw error;
+	}
+}
+
+export { playGame };
