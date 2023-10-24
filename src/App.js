8a1fe028e02aebe21c1bf2ec83ad9533ed9913c2
gwahyun <60453182+gwahyun@users.noreply.github.com>
2023-10-24 14:46:42 +0900

    Feat: 유저 입력값 검증 함수 작성

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App {
     this.answer = this.randomNumbersArray();
 
     Console.print('숫자 야구 게임을 시작합니다.');
-    Console.readLine('숫자를 입력해주세요 :', (userInput) => {
-    });
+    const userInput = await Console.readLineAsync('숫자를 입력해주세요 :');
+
+    this.checkUserInput(userInput);
 
 
   }
@@ -18,6 +19,11 @@ class App {
     return Random.pickUniqueNumbersInRange(1, 9, 3);
   }
 
+  checkUserInput(userInput) {
+    if (userInput.length > 3) {
+      throw Error('3자리의 수를 입력해주세요');
+    }
+  }
 }
 
 const app = new App();
