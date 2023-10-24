ed1eb214b690c373aea6fdedb8482008a78be69c
leehyeyeong <elto789@naver.com>
2023-10-24 11:56:51 +0900

    feat: 숫자 비교 코드 추가 - 배열을 순회하면서 안에 존재한다면 볼을 추가 인덱스도 동일하다면 스트라이크를 추가

--- src/App.js
+++ src/App.js
@@ -19,18 +19,49 @@ function computer_start() {
   return computer;
 }
 
-async function player() {
+async function player(computer_number) {
   let userNum = [];
   const input = await Console.readLineAsync(USER_MESSAGE);
   const user = (input + '').split('').map((num) => parseInt(num));
   user.map((item) => userNum.push(item));
-  Console.print(userNum)
+
+  compare(computer_number, userNum);
+
 }
 
+function compare(computer_number, player_number) {
+  let strike = 0;
+  let ball = 0;
+
+  for (let computer_item in computer_number) {
+    for (let player_item in player_number) {
+      if (computer_number[computer_item] === player_number[player_item]) {
+        if (computer_item === player_item) {
+          strike++;
+        } else {
+          ball++;
+        }
+      }
+    }
+  }
+  Console.print(strike);
+  Console.print(ball);
+
+  if (strike === 0 && ball === 0) {
+    Console.print("낫싱");
+  } else if (strike !== 0 && ball === 0) {
+    Console.print(`${strike}스트라이크`);
+  } else if (ball !== 0 && strike === 0) {
+    Console.print(`${ball}볼`);
+  } else {
+    Console.print(`${ball}볼 ${strike}스트라이크`);
+  }
+
+}
 class App {
   async play() {
     const computer_number = computer_start();
-    player();
+    const userNum = player(computer_number);
   }
 }
 export default App;
