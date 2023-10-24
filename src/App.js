42051740385d1f4db8365955730319f79eacbf39
park <call4892@gmail.com>
2023-10-24 19:33:56 +0900

    서로 다른 난수 3자리 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import * as MissionUtils from '@woowacourse/mission-utils';
+
+// 서로 다른 난수 3자리 함수
+const create_random_number = () => {
+  let number_list = '';
+  while (number_list.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+    number_list += number.toString();
+  }
+
+  return number_list;
+};
+
 class App {
   async play() {}
 }
-
-export default App;
