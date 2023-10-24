d2b0b36bca3c6ac3a26fb54beee0504be85a65b7
hyzzzzy <shinhj4068@naver.com>
2023-10-24 17:19:55 +0900

    Style: Game 클래스 주석 작성

--- src/Game.js
+++ src/Game.js
@@ -6,10 +6,16 @@ const { BALL, STRIKE, NOTHING } = SCORE;
 
 export class Game {
   constructor() {
+    /** @type {string} */
     this.status = GAME_STATUS.READY;
+    /** @type {Array<number>} */
     this.answer = this.generateRandomNumber();
   }
 
+  /**
+   * @description 1(MIN_NUMBER) ~ 9(MAX_NUMBER) 사이의 랜덤한 숫자 배열을 반환하는 함수
+   * @returns {Array<number>} computer: 길이가 3(SIZE)인 랜덤 숫자 배열
+   */
   generateRandomNumber() {
     const computer = [];
     while (computer.length < SIZE) {
@@ -21,6 +27,14 @@ export class Game {
     return computer;
   }
 
+  /**
+   * @param {Array<number>} num: 길이가 3(SIZE)인 사용자가 입력한 숫자 배열
+   * @description 사용자가 입력한 숫자와 랜덤 숫자를 비교하여 BALL, STRIKE 정보를 저장하는 함수
+   * - 숫자가 있으나 위치가 다른 경우 BALL
+   * - 숫자와 위치가 일치한 경우 STRIKE
+   * - 아무것도 일치하지 않으면 낫싱 (0볼0스트라이크)
+   * @returns {Map<string, number>} score: BALL과 STRIKE 정보를 담은 맵 객체
+   */
   compareScore(num) {
     const answer = this.answer;
     let score = new Map();
@@ -38,6 +52,11 @@ export class Game {
     return score;
   }
 
+  /**
+   * @param {Map<string, number>} score: BALL과 STRIKE 정보를 담은 맵 객체
+   * @description 저장된 BALL, STRIKE 정보를 출력하기 위한 문자열을 만드는 함수
+   * @returns {string} message: 판정결과 문자열
+   */
   getScoreMessage(score) {
     let message = []; 
     let ball = score.get(BALL);
