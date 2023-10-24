c2fcb16f5052641775523b07051b75bc20312468
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-10-24 21:09:13 +0900

    feat 출력 문자 상수선언

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { Random, Console } from "@woowacourse/mission-utils";
+const START_MENT = "숫자 야구 게임을 시작합니다.";
+const END_MENT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+const REPLAY_MENT = "게임을 새로 시작하려면 1, 종료하시려면 2를 입력하세요.";
+
 class App {
   async play() {}
 }
