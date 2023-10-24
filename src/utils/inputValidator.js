9d5a1831d69a9f7664ece51ff4a42f1e83d8b38c
Kled <1017yu@daum.net>
2023-10-24 15:13:18 +0900

    refactor: 상수명 변경 및 매직넘버 추가 상수화

--- src/utils/inputValidator.js
+++ src/utils/inputValidator.js
@@ -1,4 +1,8 @@
-import { PATTERN, ERROR_MESSAGES } from '../../constants/index.js';
+import {
+  PATTERN,
+  ERROR_MESSAGES,
+  RESTART_GAME_NUMBERS,
+} from '../../constants/index.js';
 
 /**
  * 플레이어가 입력한 numberSet에 대한 유효성 검사
@@ -6,8 +10,9 @@ import { PATTERN, ERROR_MESSAGES } from '../../constants/index.js';
  * @returns
  */
 export const numberSetValidator = (input) => {
-  if (!PATTERN.THREE_DIGIT_PATTERN.test(input))
+  if (!PATTERN.THREE_DIGIT_REGEX.test(input))
     throw new Error(ERROR_MESSAGES.INVALID_COUNT_NUMBER);
+
   return input;
 };
 
@@ -17,7 +22,11 @@ export const numberSetValidator = (input) => {
  * @returns
  */
 export const playAgainNumberValidator = (input) => {
-  if (input !== '1' && input !== '2')
+  if (
+    input !== RESTART_GAME_NUMBERS.RESTART &&
+    input !== RESTART_GAME_NUMBERS.END
+  )
     throw new Error(ERROR_MESSAGES.INVALID_REPLAY_NUMBER);
+
   return input;
 };
