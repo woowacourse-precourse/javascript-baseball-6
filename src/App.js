f7ae5749ed6cb3d9260bc7db5ec8a4669d7f0883
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-10-24 13:05:36 +0900

    feat: baseball game 01

--- src/App.js
+++ src/App.js
@@ -1,41 +1,109 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-import { Console } from "@woowacourse/mission-utils";
+import { MissionUtils, Console, Random } from "@woowacourse/mission-utils";
 
 class App {
-  async play(numberPlayer) {
-    numberPlayer = numberPlayer + "";
-    const arrayPlayer = [...numberPlayer];
+  constructor() {
+    this.round = 1;
+    this.isGameEnded = false;
+  }
 
-    console.log("숫자 야구 게임을 시작합니다.");
-    Console.readLineAsync("숫자를 입력해주세요.");
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
 
-    let computerPlayer = [];
-    computerPlayer = arrayPlayer.map((v) => parseInt(v));
-    console.log(computerPlayer);
+    while (!this.isGameEnded) {
+      const secretNumber = this.generateSecretNumber();
+      const userGuess = await this.getUserGuess();
 
-    const computer = [];
-    while (computer.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer.push(number);
+      if (userGuess === "2") {
+        this.handleGameEnd();
+      } else {
+        const result = this.compareNumbers(secretNumber, userGuess);
+        this.displayResult(result);
+
+        if (result === "3스트라이크") {
+          this.handleGameEnd();
+        }
+
+        this.round++;
       }
     }
-    console.log(computer);
+  }
 
-    let numberStrike = computer.filter(
-      (v, i) => computerPlayer.indexOf(v) === i
-    );
-    let numberBall = computer.filter((v) => computerPlayer.includes(v));
+  generateSecretNumber() {
+    const secretNumber = new Set();
 
-    Console.print(
-      `${numberBall.length - numberStrike.length}볼 ${
-        numberStrike.length
-      }스트라이크`
+    while (secretNumber.size < 3) {
+      secretNumber.add(Random.pickNumberInRange(1, 9));
+    }
+
+    return Array.from(secretNumber).join("");
+  }
+
+  async getUserGuess() {
+    let userGuess;
+    do {
+      userGuess = await Console.readLineAsync("숫자를 입력해주세요 :");
+    } while (this.isInvalidInput(userGuess));
+
+    return userGuess;
+  }
+
+  isInvalidInput(userGuess) {
+    return userGuess.length !== 3 || new Set(userGuess).size !== 3;
+  }
+
+  compareNumbers(secretNumber, userGuess) {
+    let strike = 0;
+    let ball = 0;
+
+    for (let i = 0; i < 3; i++) {
+      if (secretNumber[i] === userGuess[i]) {
+        strike++;
+      } else if (secretNumber.includes(userGuess[i])) {
+        ball++;
+      }
+    }
+
+    if (strike === 3) {
+      return "3스트라이크";
+    } else if (strike > 0 || ball > 0) {
+      return `${ball}볼 ${strike}스트라이크 `;
+    } else {
+      return "낫싱";
+    }
+  }
+
+  handleGameEnd() {
+    this.isGameEnded = true;
+
+    const restartOption = this.getRestartOption();
+
+    if (restartOption === "1") {
+      this.startNewGame();
+    } else if (restartOption === "2") {
+      Console.print("게임 종료.");
+    } else {
+      Console.print("잘못 누르셨습니다. Game over.");
+    }
+  }
+
+  async getRestartOption() {
+    return await Console.readLineAsync(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요 : "
     );
+  }
+
+  startNewGame() {
+    this.isGameEnded = false;
+    this.round = 1;
+    Console.print("새 게임 시작중...");
+  }
+
+  displayResult(result) {
+    Console.print(result);
   }
 }
 
 const app = new App();
-app.play(123);
+app.play();
 
 export default App;
