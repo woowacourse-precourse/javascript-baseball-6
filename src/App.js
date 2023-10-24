b9fe76a3d79cb55aa2fd953d5adeb1262173902f
Galaxy <js_park95@naver.com>
2023-10-24 11:12:39 +0900

    feat: 게임 시작, 컴퓨터 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import * as MissionUtils from '@woowacourse/mission-utils';
+const { Console, Random } = MissionUtils;
+
 class App {
-  async play() {}
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    this.gameStart();
+  }
+
+  async gameStart(){
+    await this.setComputer();
+  }
+
+   // 컴퓨터 숫자 생성
+   async setComputer(){
+    const NUMBERS = [];
+    
+    while(NUMBERS.length < 3){
+      let num = Random.pickNumberInRange(1, 9);
+      if(!NUMBERS.includes(`${num}`)){
+        NUMBERS.push(`${num}`);
+      }
+    };
+
+    this.#computer = NUMBERS;
+  }
+
 }
 
 export default App;
