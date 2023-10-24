9d61d4ac954316ba0f6eca4c96e1dbdad72cdf62
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-10-24 16:57:01 +0900

    fix: 유효성 검사가 되지 않았던 버그 수정

--- src/Validation.js
+++ src/Validation.js
@@ -1,22 +1,14 @@
-const isNumeric = (userInputValue) => /^[1-9]+$/.test(userInputValue);
+export const isNumeric = (userInputValue) => /^[1-9]+$/.test(userInputValue);
 
-const hasUniqueDigits = (userInputValue) => {
-  const uniqueDigits = new Set(userInputValue.split(''));
-  return uniqueDigits === 3;
+export const hasUniqueDigits = (userInputValue) => {
+  const uniqueDigits = new Set(userInputValue.toString().split(''));
+  return uniqueDigits.size === 3;
 };
 
-let isValidNumber = (userInputValue) => {
-  if (userInputValue.length === 3) return false;
+// eslint-disable-next-line import/prefer-default-export
+export const checkValidNumber = (userInputValue) => {
+  if (userInputValue.toString().length !== 3) return false;
   if (!hasUniqueDigits(userInputValue)) return false;
   if (!isNumeric(userInputValue)) return false;
   return true;
 };
-
-const checkValidNumber = (userInputValue) => {
-  if (!isValidNumber(userInputValue)) {
-    isValidNumber = false;
-  }
-  return isValidNumber;
-};
-
-module.exports = { checkValidNumber };
