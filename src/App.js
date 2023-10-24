b41c4c44561429cd10e5dad85ed02c7ad7d61383
An-jihyun <anjh322@gmail.com>
2023-10-25 05:53:15 +0900

    Feat: 비교 결과 출력, 재시작 여부 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  getRandomNumber() {
+  getRandomNum() {
     let computerNum = [];
     let tempNum = 0;
     while (computerNum.length < 3) {
@@ -41,8 +41,94 @@ class App {
     this.end = false;
   }
 
+  resultPrint(strike, ball) {
+    if (strike === 3) {
+      MissionUtils.Console.print("3스트라이크");
+      this.win = true;
+    }
 
-  async play() {}
+    if (strike === 2) {
+      MissionUtils.Console.print("2스트라이크");
+    }
+
+    if (strike === 1) {
+      if (ball === 2) {
+        MissionUtils.Console.print("2볼 1스트라이크");
+      }
+
+      if (ball === 1) {
+        MissionUtils.Console.print("1볼 1스트라이크");
+      }
+
+      if (ball === 0) {
+        MissionUtils.Console.print("1스트라이크");
+      }
+    }
+
+    if (strike === 0) {
+      if (ball === 3) {
+        MissionUtils.Console.print("3볼");
+      }
+
+      if (ball === 2) {
+        MissionUtils.Console.print("2볼");
+      }
+
+      if (ball === 1) {
+        MissionUtils.Console.print("1볼");
+      }
+      if (ball === 0) {
+        MissionUtils.Console.print("낫싱");
+      }
+    }
+  }
+
+  checkNum(computer, user) {
+    let strike = 0;
+    let ball = 0;
+
+    for (let i = 0; i < 3; i++) {
+      if (computer[i] === user[i]) {
+        strike++;
+      }
+      if (computer.includes(user[i]) && computer[i] !== user[i]) {
+        ball++;
+      }
+    }
+
+    this.resultPrint(strike, ball);
+  }
+
+  async gameRestart() {
+    let computerNum = this.getRandomNum();
+    while (!this.end) {
+      let userNum = await this.getUserNum();
+      this.checkNum(computerNum, userNum);
+
+      if (this.win) {
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        let res = await MissionUtils.Console.readLineAsync(
+          "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+        );
+
+        if (res === "1") {
+          this.win = false;
+          this.end = false;
+
+          comNum = this.getRandomNum();
+        } else if (res === "2") {
+          this.end = true;
+        } else {
+          throw new Error("[ERROR] 1 혹은 2를 입력해 주세요.");
+        }
+      }
+    }
+  }
+
+  async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    await this.gameRestart();
+  }
 }
 
 export default App;
