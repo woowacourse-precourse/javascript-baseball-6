a2e920c7e712320f3ca7d5918d997b69f7bf384c
leehyeyeong <elto789@naver.com>
2023-10-24 11:38:47 +0900

    feat: 사용자 입력 받기 - 컴퓨터 리스트로 받은 것처럼 플레이어도 리스트로 저장

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 const START_MESSAGE = "숫자 야구 게임 시작을 시작합니다."
+const USER_MESSAGE = "숫자를 입력해주세요 : "
 const NUMBER_LENGTH = 3
 const RANGE_START = 1
 const RANGE_END = 9
@@ -18,9 +19,18 @@ function computer_start() {
   return computer;
 }
 
+async function player() {
+  let userNum = [];
+  const input = await Console.readLineAsync(USER_MESSAGE);
+  const user = (input + '').split('').map((num) => parseInt(num));
+  user.map((item) => userNum.push(item));
+  Console.print(userNum)
+}
+
 class App {
   async play() {
     const computer_number = computer_start();
+    player();
   }
 }
 export default App;
