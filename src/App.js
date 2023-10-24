adc4a2568f0e975320f2e2d5eaa8c7b589e0f287
김예빈 <kimyebin@gim-yebin-ui-MacBookPro.local>
2023-10-24 23:36:20 +0900

    feat: 서로 다른 임의의 수 3개를 선택하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+
+    
+     
+  }
+
+  selectRandomNum() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
