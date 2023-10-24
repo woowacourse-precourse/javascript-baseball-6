36c5b7e1ecaffaabf94705f1f4140cc5ae1eecbe
daeun-Kim <82816029+ddaeunbb@users.noreply.github.com>
2023-10-24 14:21:11 +0900

    [Feat]: 게임 결과값 출력 및 게임 재개 사용자 입력 테스트 코드 작성 (#15)
    - 게임 결과에 대한 출력 테스트 코드를 작성해보았습니다. (스트라이크, 볼, 낫싱)
    - 정답을 맞춘 이후 게임 재개에 대한 사용자 입력결과에 대한 테스트 코드를 작성하였습니다.

---
+++ __tests__/RestartTest.js
@@ -0,0 +1,30 @@
+import App from "../src/App.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { MESSAGE, ERROR_MESSAGE } from '../src/App.js';
+
+const mockConsoleFn = (input) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+  MissionUtils.Console.readLineAsync.mockImplementation(()=> input);
+}
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+}
+
+describe("게임 종료 및 재개 테스트", () => {
+  test("게임을 종료하는 경우", async () => {
+    const app = new App();
+    const logSpy = getLogSpy();
+    mockConsoleFn('2');
+    await app.restart();
+    expect(logSpy).toHaveBeenCalledWith(MESSAGE.END);
+  })
+
+  test("1과 2가 아닌 다른 값을 입력했을 경우", async () => {
+    const app = new App();
+    mockConsoleFn('게임종료해줘.');
+    await expect(app.restart()).rejects.toThrow(ERROR_MESSAGE.RESTART);
+  })
+})
