e110b7848abd66f4b10f70c83c366746f79a8c76
hsgh085 <hsyu0917@gmail.com>
2023-10-25 01:58:53 +0900

    feat: 게임 종료

--- src/App.js
+++ src/App.js
@@ -94,6 +94,8 @@ class App {
     const restart = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     if (restart === "1") {
       this.getRandomNum();
+    } else if (restart === "2") {
+      MissionUtils.Console.print("게임 종료");
     }
   }
 }
