17cd59a20a1cc1656ba4439f169bc7339951105d
insidepixce <insidepixce@gmail.com>
2023-10-24 11:26:24 +0900

    노드로 실행했을때 성공

--- src/App.js
+++ src/App.js
@@ -1,11 +1,94 @@
-import { compareNumbers } from './components/compareNumbers.js';
+import readline from 'readline';
 
-const checkButton = document.querySelector('#submit');
-export let restartButton = document.querySelector('#game-restart-button');
-checkButton.addEventListener('click', handleCheckButtonFunction);
-restartButton.style.display = 'none';
+class App {
+  constructor() {
+    this.secretNumber = this.generateRandomNumber();
+    this.exitRequested = false;
+    this.play();
+  }
 
-function handleCheckButtonFunction(e) {
-  e.preventDefault();
-  compareNumbers();
+  generateRandomNumber() {
+    const numbers = Array.from({ length: 9 }, (_, i) => i + 1);
+    const computer = [];
+
+    while (computer.length < 3) {
+      const randomIndex = Math.floor(Math.random() * numbers.length);
+      computer.push(numbers[randomIndex]);
+      numbers.splice(randomIndex, 1);
+    }
+
+    return computer.join('');
+  }
+
+  async play() {
+    console.log("숫자 야구 게임을 시작합니다.");
+
+    while (!this.exitRequested) {
+      try {
+        const input = await this.getInput();
+        if (input === '2') {
+          console.log("게임을 종료합니다.");
+          this.exitRequested = true;
+        }
+
+        if (!this.exitRequested) {
+          if (this.isValidInput(input)) {
+            const result = this.checkGuess(input);
+            console.log(result);
+
+            if (result === '3스트라이크') {
+              console.log("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+              this.exitRequested = true;
+            }
+          } else {
+            console.log("[ERROR] 숫자가 잘못된 형식입니다.");
+          }
+        }
+      } catch (error) {
+        console.error(error);
+        this.exitRequested = true;
+      }
+    }
+  }
+
+  async getInput() {
+    return new Promise((resolve, reject) => {
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+
+      rl.question("숫자를 입력해주세요 : ", (answer) => {
+        rl.close();
+        resolve(answer);
+      });
+    });
+  }
+
+  isValidInput(input) {
+    return /^\d{3}$/.test(input) && new Set(input).size === 3;
+  }
+
+  checkGuess(guess) {
+    let strikes = 0;
+    let balls = 0;
+
+    for (let i = 0; i < 3; i++) {
+      if (guess[i] === this.secretNumber[i]) {
+        strikes++;
+      } else if (this.secretNumber.includes(guess[i])) {
+        balls++;
+      }
+    }
+
+    if (strikes === 3) {
+      return '3스트라이크';
+    } else if (strikes > 0 || balls > 0) {
+      return `${balls}볼 ${strikes}스트라이크`;
+    } else {
+      return '낫싱';
+    }
+  }
 }
+
+new App();
