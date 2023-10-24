f1b47e413d5242a378b018ed767f251980dcea5f
Hayeon-Lee <papepopepe@naver.com>
2023-10-25 00:03:53 +0900

    feat: Check Code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { MissionUtils, Console } from "@woowacourse/mission-utils";
+import { Random, Console } from "@woowacourse/mission-utils";
 
 const inputQuery = [
   "숫자를 입력해주세요. : ",
@@ -12,7 +12,7 @@ async function getUsernumber() {
   } catch (error) {}
 }
 
-function convertNumberToList(number) {
+async function convertNumberToList(number) {
   const str = String(number);
   let strList = [];
   for (let i = 0; i < str.length; i++) {
@@ -21,12 +21,12 @@ function convertNumberToList(number) {
   return strList;
 }
 
-function compareComputerAndUser(computerNumberList, userNumberList) {
+async function compareComputerAndUser(computerNumberList, userNumberList) {
   let ball = 0,
     strike = 0;
 
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
+  for (let i = 0; i < computerNumberList.length; i++) {
+    for (let j = 0; j < userNumberList.length; j++) {
       if (computerNumberList[i] === userNumberList[j]) {
         if (i == j) strike += 1;
         else ball += 1;
@@ -34,23 +34,38 @@ function compareComputerAndUser(computerNumberList, userNumberList) {
     }
   }
 
-  if (strike === 3) {
-    Console.print("3 개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  if (
+    strike === computerNumberList.length &&
+    userNumberList.length === computerNumberList.length
+  ) {
+    Console.print(`${strike}스트라이크`);
+    Console.print(`${strike}개의 숫자를 모두 맞히셨습니다! 게임 종료`);
     return 1;
   } else {
-    if (ball) {
-      Console.print(ball);
-      Console.print("볼");
-      if (!strike) Console.print("\n");
-    }
-    if (strike) {
-      Console.print(strike);
-      Console.print("스트라이크\n");
-    }
-    if (!ball && !strike) {
-      Console.print("낫싱");
+    if (ball === 0 && strike === 0) Console.print("낫싱");
+    else {
+      if (ball && strike) Console.print(`${ball}볼 ${strike}스트라이크`);
+      else if (ball) Console.print(`${볼}볼`);
+      else Console.print(`${strike}스트라이크`);
     }
     return 0;
+  }
+}
+
+async function getUserRepeat() {
+  try {
+    const userrepeat = await Console.readLineAsync(inputQuery[1]);
+    return userrepeat;
+  } catch (error) {}
+}
+
+async function checkGameRepeat(compareResult) {
+  if (compareResult === 1) {
+    const restartFlag = await getUserRepeat();
+    if (restartFlag === 2) {
+      Console.print("게임을 종료합니다. 수고하셨습니다.");
+      return 0;
+    } else return restartFlag;
   }
 }
 
@@ -58,30 +73,35 @@ class App {
   async play() {
     let repeatFlag = 1;
     Console.print("숫자 야구 게임을 시작합니다.");
-
+    //for (let k = 0; k < 3; k++)
+    //while (repeatFlag === 1)
     while (repeatFlag === 1) {
       let correctFlag = 0;
-      const computerNumber = MissionUtils.Random.pickNumberInRange(1, 999);
-
+      const computerNumber = Random.pickNumberInRange(100, 999);
+      //for (let v = 0; v < 3; v++)
+      //while (correctFlag === 0)
       while (correctFlag === 0) {
         const usernumber = await getUsernumber();
+        if (usernumber === undefined) {
+          throw "[ERROR]";
+        }
 
         //각각의 숫자를 배열로 변환
-        const computerNumberList = convertNumberToList(computerNumber);
-        const userNumberList = convertNumberToList(usernumber);
+        const computerNumberList = await convertNumberToList(computerNumber);
+        const userNumberList = await convertNumberToList(usernumber);
 
         //숫자 비교
-        const compareResult = compareComputerAndUser(
+        const compareResult = await compareComputerAndUser(
           computerNumberList,
           userNumberList
         );
 
-        if (compareResult === 1) {
-          correctFlag = 1;
-          const restartFlag = await Console.readLineAsync(inputQuery[1]);
-          if (restartFlag === 2) {
-            Console.print("게임을 종료합니다. 수고하셨습니다.");
+        if (compareResult === 1) correctFlag = 1;
+        if (correctFlag === 1) {
+          repeatFlag = await checkGameRepeat(compareResult);
+          if (repeatFlag !== 1 && repeatFlag !== 2) {
             repeatFlag = 0;
+            throw "[ERROR]";
           }
         }
       }
