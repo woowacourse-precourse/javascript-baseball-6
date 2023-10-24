637d9a4ad80f5e4310d7ff9cb704cc5dabad4904
yeeun426 <thsudkcla7@naver.com>
2023-10-24 11:38:41 +0900

    Feat : 입력한 값이 유효한 값인지 확인

--- src/BaseballInput.js
+++ src/BaseballInput.js
@@ -13,7 +13,19 @@ class BaseballInput {
 
   makeUserInput(random) {
     // 임의의 3개의 수 입력 하기
-    Console.readLine("숫자를 입력해주세요 : ", (input) => {});
+    Console.readLine("숫자를 입력해주세요 : ", (userNum) => {
+      const isValidNum = this.checkValidNum(userNum);
+      console.log(isValidNum);
+    });
+  }
+
+  checkValidNum(userNum) {
+    // 유효성 확인
+    if (isNaN(userNum)) throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    else if (userNum.length !== 3)
+      throw new Error("[ERROR] 입력한 수의 개수가 잘못된 형식입니다.");
+
+    return true;
   }
 }
 
