ee8e15f9abc05fc6546a0224384cba5ca1ab1a17
jeeyng <petitesize22@gmail.com>
2023-10-24 17:18:35 +0900

    사용자 입력을 예외처리하는 'validateInput' 메소드 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { Console, Random } from "@woowacourse/mission-utils";
 class App {
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    
     app.pickAnswer();
 
 
-    app.playerNumber();
-    
-    
+    app.playerInput();
+  
   }
   
   pickAnswer() {
@@ -24,15 +24,28 @@ class App {
     //console.log(answer)
     return answer;
     
-    
   }
 
-  playerNumber() {
+  playerInput() {
 
    Console.readLine('숫자를 입력해주세요 : ', answer => {
-  	console.log(answer);
+  	console.log(this.validateInput(answer));
  });
+  }
 
+  validateInput(input) {
+    var inputValid = /^[1-9]{3}$/; //1에서 9까지의 숫자 3자리 검사하는 정규식
+    if(!inputValid.test(input)) { 
+      if(input.includes(' ')) { //3자리 숫자를 입력했지만 공백이 포함된 경우 error
+        throw new Error('[ERROR] 띄어쓰기(공백) 없이 입력해주세요.');
+      }
+      //1에서 9까지 3자리 숫자가 아닌 경우
+      throw new Error('[ERROR] 1부터 9까지의 서로 다른 3자리 숫자를 입력해주세요.');
+    }
+    //3자리 수를 입력해서 검사를 통과했지만 중복된 수가 있는 경우   
+    if (new Set(input).size !== 3) { 
+      throw new Error('[ERROR] 중복되지 않은 서로 다른 3자리 숫자를 입력해주세요.');
+    }
   }
     
 }
