ee41b9d21b96cd8239772e18bc0e71e1b15c06d8
skdbsqls <skdbsqls98@naver.com>
2023-10-24 16:47:14 +0900

    feat: 게임 시작 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    gameStart();
+  }
 }
+
+// 1. 게임 시작
+const gameStart = () => {
+  // 1-1. 게임 시작 문구 출력
+  Console.print("숫자 야구 게임을 시작합니다.");
+
+  // 1-2. 컴퓨터의 랜덤값 생성
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+};
+
+const app = new App();
+app.play();
 
 export default App;
