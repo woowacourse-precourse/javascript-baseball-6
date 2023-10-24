c144fec41451047b63afcafe292b5be143a72cd7
hyeona <90515062+BaeHyeonA@users.noreply.github.com>
2023-10-24 20:42:10 +0900

    feat : 유저 입력 숫자 예외 체크 기능 구현

--- src/App.js
+++ src/App.js
@@ -15,6 +15,15 @@ class App {
     return MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
   }
 
+  checkInputValidation(userInputNumber){
+    const userInputNumberArr = [...new Set(userInputNumber.split(''))].map(Number);
+    
+    if(userInputNumber.length !== 3) throw new Error('[ERROR] 3자리 숫자가 아닙니다.')
+    if(userInputNumberArr.length !== 3) throw new Error('[ERROR] 중복된 숫자가 있습니다.')
+    if(userInputNumber.includes(0)) throw new Error('[ERROR] 1~9 사이의 숫자가 아닙니다.')
+    if(isNaN(userInputNumber)) throw new Error('[ERROR] 숫자가 아닙니다.')
+  }
+
   async play() {}
 }
 
