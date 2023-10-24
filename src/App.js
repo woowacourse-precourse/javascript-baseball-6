db6d7cb4f8fd32d8ab3a364a46f809dbec194cac
민규 <xiahtic73@gmail.com>
2023-10-24 16:49:25 +0900

    feat: 입력된 숫자 세자리 중복 확인 구현

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import { Random, Console } from '@woowacourse/mission-utils';
+import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
-  static setRandomNumber() {
+  static setComputerNum() {
     const randomNumber = [];
+
+    Console.print("숫자 야구 게임을 시작합니다.");
 
     while (randomNumber.length < 3) {
       const num = Random.pickNumberInRange(1, 9);
       if (!randomNumber.includes(num)) randomNumber.push(num);
     }
 
-    Console.print(randomNumber); // todo delete
+    return randomNumber;
   }
 
-  static async setAnswer() {
-    const inputNumber = await Console.readLineAsync('숫자를 입력해주세요 : ');
+  static async setUserNum() {
+    const inputNumber = await Console.readLineAsync("숫자를 입력해주세요 : ");
 
     if (!inputNumber.match(/^[1-9]{3}$/)) {
-      throw new Error('[ERROR] 세자리 숫자를 입력해주세요.');
+      throw new Error("[ERROR] 세자리 숫자를 입력해주세요.");
     }
+
+    const answer = inputNumber.split("").map((item) => +item);
+
+    const duplicate = (arr) => {
+      return arr.some((item, index) => arr.indexOf(item) !== index);
+    };
+
+    if (duplicate(answer)) {
+      throw new Error("[ERROR] 숫자가 중복되지 않도록 입력해주세요.");
+    }
+
+    return answer;
   }
 
-  static play() {
-    Console.print('숫자 야구 게임을 시작합니다.');
-
-    this.setRandomNumber();
-    this.setAnswer();
+  static async play() {
+    const computer = this.setComputerNum();
+    Console.print(computer); // todo delete
+    const user = await this.setUserNum();
+    Console.print(user); // todo delete
   }
 }
 
