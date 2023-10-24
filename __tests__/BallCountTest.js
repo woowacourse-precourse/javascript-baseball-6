540b76cfd6b20eeb5e637eae312a75e3952ae6a1
KimKyungYun <kky38225221@gmail.com>
2023-10-24 22:31:59 +0900

    update: readme추가, 테스트케이스 추가

--- __tests__/BallCountTest.js
+++ __tests__/BallCountTest.js
@@ -10,6 +10,14 @@ describe('볼 카운트 테스트', () => {
 		expect(ballCount).toEqual(2);
 	});
 
+	test('스트라이크 수 세기', () => {
+		const userNumbers = ['1', '2', '3'];
+		const computerNumbers = ['1', '5', '3'];
+		const { strikeCount } = getPitchingCount(computerNumbers, userNumbers);
+
+		expect(strikeCount).toEqual(2);
+	});
+
 	test('볼,스트라이크의 수 세기', () => {
 		const computerNumbers = ['1', '3', '6'];
 		const userNumbers = ['1', '2', '3'];
@@ -32,5 +40,29 @@ describe('볼 카운트 테스트', () => {
 		const logSpy = jest.spyOn(MissionUtils.Console, 'print');
 		printPitchingCount(ballCount, strikeCount);
 		expect(logSpy).toHaveBeenCalledWith('3볼');
+	});
+
+	test('스트라이크 카운트 출력', () => {
+		const computerNumbers = ['1', '3', '6'];
+		const userNumbers = ['1', '4', '6'];
+		const { ballCount, strikeCount } = getPitchingCount(
+			computerNumbers,
+			userNumbers
+		);
+		const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+		printPitchingCount(ballCount, strikeCount);
+		expect(logSpy).toHaveBeenCalledWith('2스트라이크');
+	});
+
+	test('볼 스트라이크 카운트 출력', () => {
+		const computerNumbers = ['1', '3', '6'];
+		const userNumbers = ['1', '6', '2'];
+		const { ballCount, strikeCount } = getPitchingCount(
+			computerNumbers,
+			userNumbers
+		);
+		const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+		printPitchingCount(ballCount, strikeCount);
+		expect(logSpy).toHaveBeenCalledWith('1볼 1스트라이크');
 	});
 });
