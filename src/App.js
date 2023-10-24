b3a9a754b5020bf30a139ea2fb223ec9bdffe8fb
seoyeon-jung <jungluchia1204@gmail.com>
2023-10-24 18:39:47 +0900

    feat: 볼&스트라이크 판정 기능 추가
    - checkStrikeNBall() 메소드를 통해 볼과 스트라이크 판정 기능을 구현했습니다
    - 컴퓨터로 생성된 숫자와 사용자가 입력한 숫자를 비교하도록 했습니다
    - checkMessage() 메소드를 통해 두 숫자를 비교하여 답을 출력하도록 했습니다

--- src/App.js
+++ src/App.js
@@ -15,7 +15,44 @@ class App {
     }
   }
 
-  async play() {}
+  async play() {
+    this.makeComputerRandomNums();
+    Console.print("숫자 야구 게임을 시작합니다.");
+    Console.print(this.computerRandomNums);
+
+    while (true) {
+      const number = await Console.readLineAsync("숫자를 입력해주세요: ");
+      const answer = this.checkStrikeNBall(number);
+      Console.print(answer);
+    }
+  }
+
+  checkStrikeNBall(number) {
+    const check = {
+      ball: 0,
+      strike: 0,
+    };
+
+    for (let i = 0; i < number.length; i++) {
+      const num = Number(number[i]);
+      const index = this.computerRandomNums.indexOf(num);
+
+      if (index === -1) continue;
+      else if (index > -1) {
+        if (index === i) check.strike += 1;
+        else check.ball += 1;
+      }
+    }
+
+    return this.makeMessage(check);
+  }
+
+  makeMessage({ ball, strike }) {
+    if (ball === 0 && strike === 0) return "낫싱";
+    if (ball > 0 && strike > 0) return `${ball}볼 ${strike}스트라이크`;
+    if (ball > 0) return `${ball}볼`;
+    else return `${strike}스트라이크`;
+  }
 }
 
 const app = new App();
