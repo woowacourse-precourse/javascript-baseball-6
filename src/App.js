1a7dca92b372b4d614057fcd58fed35f71914d86
gwahyun <60453182+gwahyun@users.noreply.github.com>
2023-10-24 17:06:00 +0900

    Fix: 게임 재시작시 전역변수 초기화하도록 수정

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ class App {
   }
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
+    this.answer = this.randomNumbersArray();
+    //Console.print(`정답은 : ${this.answer}`);
 
     while (this.playState) {
-      this.answer = this.randomNumbersArray();
-      Console.print(`정답은 : ${this.answer}`);
       const userInput = await Console.readLineAsync('숫자를 입력해주세요 :');
       this.checkUserInput(userInput);
       this.calculateGameResult(this.answer, userInput);
@@ -24,7 +24,10 @@ class App {
       } else {
         Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료 ');
         if (await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.') === '1') {
+          this.answer = this.randomNumbersArray();
           this.playState = true;
+          this.strike = 0;
+          this.ball = 0;
         }
       }
 
@@ -39,7 +42,7 @@ class App {
 
   checkUserInput(userInput) {
     if (userInput.length > 3) {
-      throw Error('3자리의 수를 입력해주세요');
+      throw Error('[ERROR] 3자리의 수를 입력해주세요');
     }
   }
 
@@ -76,7 +79,7 @@ class App {
     } else if (strike > 0 && ball === 0) {
       resultStr += `${strike}스트라이크 `
     } else {
-      resultStr += `$${ball}볼`
+      resultStr += `${ball}볼`
     }
 
     return resultStr;
