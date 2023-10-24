50bd05914e2f28e7228f0b89deb7ed74954545c1
yura <yuraa0302@gmail.com>
2023-10-24 16:30:48 +0900

    feat : 숫자야구에 쓰일 숫자의 유효성 검사

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -7,16 +7,29 @@ class BaseballGame {
   }
 
   start() {
-    this.#answer = BaseballGame.RandomNumber();
+    this.#answer = BaseballGame.randomNumber();
   }
 
   //   랜덤 세자리 숫자 반환
-  static RandomNumber() {
+  static randomNumber() {
     let set = new Set();
     while (set.size < 3) {
       set.add(Random.pickNumberInRange(1, 9));
     }
     return [...set];
+  }
+  static validNumber(number) {
+    if (
+      isNaN(number) ||
+      number < 100 ||
+      number > 999 ||
+      new Set(number).size !== 3 ||
+      !Number.isInteger(parseFloat(number))
+    ) {
+      throw new Error(
+        "[Error] 1~9까지로 구성된 서로 다른 3자리 수를 입력하세요"
+      );
+    }
   }
 }
 
