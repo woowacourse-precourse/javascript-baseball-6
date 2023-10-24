ffa8fe747dfd0d9fac38c3ce3a13f9c834b8c252
CSKIM <rlackstjq987@gmail.com>
2023-10-24 22:28:36 +0900

    feat: 게임이 시작되면 컴퓨터는 사용자가 맞출 숫자를 생성한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.secretNumber = [];
+  }
+  genSecretNumber() {
+    while (this.secretNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.secretNumber.includes(number)) {
+        this.secretNumber.push(number);
+      }
+    }
+  }
+  async play() {
+    this.genSecretNumber();
+  }
 }
 
 export default App;
