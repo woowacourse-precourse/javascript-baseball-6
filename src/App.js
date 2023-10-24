faf7ef9304be9def7f1cf23dabd9d07f761bff99
hzzzi <hji2004@naver.com>
2023-10-24 22:34:03 +0900

    에러 추가, 재시작/종료 구조 수정

--- src/App.js
+++ src/App.js
@@ -2,80 +2,99 @@ import { Console, MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
+    // 게임 시작 문구 출력
+    Console.print('숫자 야구 게임을 시작합니다.');
 
-    function newGame() {
-      // 게임 시작 문구 출력
-      Console.print('숫자 야구 게임을 시작합니다.');
 
-      // 컴퓨터 임의의 수 3개 선택
-      const computer = [];
-      while (computer.length < 3) {
-        const number = MissionUtils.Random.pickNumberInRange(1, 9);
-        if (!computer.includes(number)) {
-          computer.push(number);
-        }
+
+    // 컴퓨터 임의의 수 3개 선택
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
       }
-      // Console.print(computer) //test용
+    }
+    // Console.print(computer) //test용
 
 
-      //사용자 입력 
-      async function getGuess() {
-        try {
-          // 사용자 입력
-          const guess = await Console.readLineAsync('숫자를 입력해주세요 : ');
+    //사용자 입력
+    async function getGuess() {
+      try {
+        // 사용자 입력
+        const guess = await Console.readLineAsync('숫자를 입력해주세요 : ');
 
-          // 사용자 입력 수와 컴퓨터 임의의 수 비교
-          let strike = 0;
-          let ball = 0;
 
-          for (let i = 0; i < 3; i++) {
-            if (parseInt(guess[i]) === computer[i]) {
-              strike++;
-            }
-            for (let j = 0; j < 3; j++) {
-              if ((i !== j) && (parseInt(guess[i]) === computer[j])) {
-                ball++;
-              }
+        // ERROR
+        if (guess.length !== 3) {
+          throw new Error();
+        } else if (guess === null) {
+          throw new Error();
+        }
+
+
+        // 사용자 입력 수와 컴퓨터 임의의 수 비교
+        let strike = 0;
+        let ball = 0;
+
+        for (let i = 0; i < 3; i++) {
+          if (parseInt(guess[i]) === computer[i]) {
+            strike++;
+          }
+          for (let j = 0; j < 3; j++) {
+            if ((i !== j) && (parseInt(guess[i]) === computer[j])) {
+              ball++;
             }
           }
-
-          // 결과 출력
-          if (strike === 3) {
-            Console.print('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-
-            const answer = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
-            if (answer === '1') {
-              newGame();
-            } else if (answer === '2') {
-              process.exit();
-            }
-
-          } else if (strike !== 0 || ball !== 0) {
-            Console.print(`${ball}볼 ${strike}스트라이크`);
-            getGuess();
-
-          } else {
-            Console.print('낫싱');
-            getGuess();
-
-          }
-
-        } catch (error) {
-          // reject 되는 경우
-          Console.print('에러발생');
         }
-      }
 
-      getGuess();
+        // 결과 출력
+        if (strike === 3) {
+          Console.print('3스트라이크');
+          console.log('3개의 숫자를 모두 맞히셨습니다! 게임 종료')
+          askRetry();
+          return;
+
+        } else if (strike === 0 && ball !== 0) {
+          Console.print(`${ball}볼`);
+          getGuess();
+
+        } else if (strike !== 0 && ball === 0) {
+          Console.print(`${strike}스트라이크`);
+          getGuess();
+
+        } else if (strike !== 0 && ball !== 0) {
+          Console.print(`${ball}볼 ${strike}스트라이크`);
+          getGuess();
+
+        } else {
+          Console.print('낫싱');
+          getGuess();
+        }
+
+      } catch (Error) {
+        // reject 되는 경우
+        throw new Error();
+        Console.print("[ERROR]");
+
+        // return;
+      }
     }
 
-    newGame();
+    getGuess();
 
+    async function askRetry() {
+      const askRetry = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
+      if (askRetry === '1') {
+        App.play();
+      } else if (askRetry === '2') {
+        return;
+      }
+    }
 
   }
+
+
 }
 
 export default App;
-
-const app = new App();
-app.play();
