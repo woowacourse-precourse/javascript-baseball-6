255bb3e41ba9e522053502decb4e28e67cae1595
Klomachenko <leekyumin0901@naver.com>
2023-10-25 01:57:58 +0900

    feat: 입력값이 올바른지 검사하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -18,6 +18,14 @@ class App {
 
     let input = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
 
+    if (input[0] == 0 || input[1] == 0 || input[2] == 0) {
+      MissionUtils.Console.print("잘못된 입력입니다. 0을 제외한 숫자를 입력해주세요 : ");
+    } else if (input[0] == input[1] || input[0] == input[2] || input[1] == input[2]) {
+      MissionUtils.Console.print("잘못된 입력입니다. 서로 다른 숫자를 입력해주세요 : ");
+    } else {
+      let strike = 0;
+      let ball = 0;
+    }
   }
 }
 
