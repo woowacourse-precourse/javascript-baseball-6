c70ff8bd58a6801263b990ecc2ff23699d48cc05
BaeDongWoo <ehddn5410@naver.com>
2023-10-25 03:05:39 +0900

    feat:입력받은 재시작 여부가 올바른지 확인후 에러처리

--- src/InputCheck.js
+++ src/InputCheck.js
@@ -7,5 +7,16 @@ class InputCheck {
       throw new Error('[ERROR] 숫자를 입력해 주세요');
     }
   }
+  restartInputCheck(input) {
+    if (input.length !== 1) {
+      throw new Error('[ERROR] 숫자가 잘못된 형식입니다.1');
+    }
+    if (isNaN(input)) {
+      throw new Error('[ERROR] 숫자가 잘못된 형식입니다.2');
+    }
+    if (input !== '1' && input !== '2') {
+      throw new Error('[ERROR] 숫자가 잘못된 형식입니다.3');
+    }
+  }
 }
 export default InputCheck;
