ad567b02cfd1c941c526de872e052262f108ee10
soyunim <togksk123@naver.com>
2023-10-24 16:10:13 +0900

    refactor: 컴퓨터의 숫자 받기 코드 리팩토링

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -1,16 +1,23 @@
-const gameStart = (async) => {
-  // 0. 게임 시작 메세지 출력
-  // 1. 컴퓨터의 랜덤 숫자
-  const COMPUTER = [];
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { Messages } from "./Messages";
 
+// 0. 게임 시작 메세지 출력
+const gameStart = async () => {
   MissionUtils.Console.print(Messages.START);
+  const COMPUTER_NUMBER = getComputerNum();
+  MissionUtils.Console.print(COMPUTER_NUMBER);
+};
+
+// 1. 컴퓨터의 랜덤 숫자
+const getComputerNum = () => {
+  const COMPUTER = [];
   while (COMPUTER.length < 3) {
     let number = MissionUtils.Random.pickNumberInRange(1, 9);
     if (!COMPUTER.includes(number)) {
       COMPUTER.push(number);
     }
   }
-
-  const COMPUTER_NUMBER = COMPUTER.join("");
-  MissionUtils.Console.print(COMPUTER_NUMBER);
+  return COMPUTER.join("");
 };
+
+export { gameStart };
