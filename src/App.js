f1064ac4b6452fe6b58acbebb37f9deffc2e7bf2
Daeeui Kim <kimeodml@gmail.com>
2023-10-24 22:10:34 +0900

    feat: 숫자 입력 및 입력값 예외처리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Console, Random }  from "@woowacourse/mission-utils";
+const ONLY_NUMBER = /^[1-9]+$/;
 
 class App {
 
@@ -20,6 +21,23 @@ class App {
 
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
+    this.gameProcess();
+  }
+
+  gameProcess() {
+    this.inputNumber();
+  }
+
+  async inputNumber() {
+    const input = await Console.readLineAsync('숫자를 입력하세요: ');
+    this.input = input.split('').map((i) => Number(i));
+    this.checkValidation(this.input);
+  }
+
+  checkValidation(input) {
+    if (input.length !== 3 || new Set(input).size !== 3 || !ONLY_NUMBER.test(input.join(''))) {
+      throw Error('[ERROR] 숫자가 잘못된 형식입니다.');
+    }
   }
 
 }
