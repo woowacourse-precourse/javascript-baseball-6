c8e5708280a4a08d527c4ffb4b055ac1c6d747d9
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-24 17:39:34 +0900

    refactor: Validation 모듈 추가 & 예외 처리 세분화
    기존: 정규표현식에 모든 예외 규정을 삽입하여 예외를 처리
    변경: 누구나 쉽게 이해할 수 있도록 예외 처리를 세분화함
    또한, 유효 검증 기능을 추출하여 모듈로 만듦.

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,6 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { CONTINUE, LENGTH_OF_ANSWER, EXIT } from './Constants.js';
+import { CONTINUE, EXIT } from './Constants.js';
+import { validateGuessedNumber, validatePlayAgain } from './Valdiation.js';
 
 class Player {
   constructor() {
@@ -10,17 +11,7 @@ class Player {
     const input = await MissionUtils.Console.readLineAsync(
       '숫자를 입력해주세요 : ',
     );
-
-    const regexp = new RegExp(`^(?!.*(.).*\\1)[1-9]{${LENGTH_OF_ANSWER}}$`);
-    // ^: 문자열의 시작
-    // (?!.*(.).*\1): 중복 비허용
-    // [1-9]{n}: 1~9 사이 숫자를 n번 반복
-    // $: 문자열의 끝
-
-    if (regexp.test(input) === false) {
-      throw Error('[ERROR] 입력 값이 올바르지 않습니다.');
-    }
-
+    validateGuessedNumber(input);
     this.guessedNumber = Array.from(input, (char) => Number(char));
   }
 
@@ -28,9 +19,7 @@ class Player {
     const input = await MissionUtils.Console.readLineAsync(
       `게임을 새로 시작하려면 ${CONTINUE}, 종료하려면 ${EXIT}를 입력하세요.\n`,
     );
-    if (input !== CONTINUE && input !== EXIT) {
-      throw Error('[ERROR] 입력 값이 올바르지 않습니다.');
-    }
+    validatePlayAgain(input);
     return input;
   }
 }
