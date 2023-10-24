8d9a70a8d8279d6e6f3b56bec3b89222d42ac80e
today-is-first <diligent0644@gmail.com>
2023-10-24 22:05:05 +0900

    feat: 겹치지 않는 랜덤한 3개의 공 숫자 생성, 사용자의 입력 값을 입력받은 후 출력 docs: 구현 기능 목록 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import * as game from './Game.js';
+import { generateRandomBallNumber } from './Ball.js';
 
 class App {
   constructor() {
+    this.answer = generateRandomBallNumber();
   }
   async play() {
+    game.getInput();
   }
 }
+
+const app = new App();
+
+app.play();
 
 export default App;
--- src/Game.js
+++ src/Game.js
@@ -1,1 +1,11 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
+import { Console } from '@woowacourse/mission-utils';
+import { messages } from './Message.js';
+
+/**
+ * 사용자의 입력 값을 입력받은 후 출력
+ */
+export const getInput = () => {
+  Console.readLine(messages.INPUT_NUMBER, (input) => {
+    console.log(`${messages.INPUT_NUMBER} : ${input}`);
+  });
+}
