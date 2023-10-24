be7cae06d97d6d44f1e12389fcd15f3aa1ca1768
noodle <noodle>
2023-10-24 11:24:39 +0900

    refactor : 식별자를 의미있게 변경함으로 로직 파악 용이하게 리팩토링
    refactor : 식별자를 의미있게 변경함으로 로직 파악 용이하게 리팩토링

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   constructor() {
-    this.computerNumber = null;
+    this.randomNumbers = null;
     this.initMessage = false;
   }
 
@@ -11,8 +11,9 @@ class App {
       Console.print("숫자 야구 게임을 시작합니다.");
       this.initMessage = true;
     }
-    this.computerNumber = this.generateRandomNumber();
-    await this.getUserInput();
+
+    this.randomNumbers = this.generateRandomNumber();
+    await this.playGameCycle();
   }
 
   generateRandomNumber() {
@@ -26,72 +27,86 @@ class App {
     return randomNumbers;
   }
 
-  async getUserInput() {
-    const inputLine = await Console.readLineAsync("숫자를 입력해주세요 : ");
-    const isVerifiedNumber = this.validateInputLineNumber(inputLine);
-    await this.compareCorrectAnswer(this.computerNumber, isVerifiedNumber);
+  async playGameCycle() {
+    const userNumbers = await this.promptUserInput();
+    const validatedUserNumbers = this.validateInputNumber(userNumbers) && userNumbers;
+    const roundResult = this.compareRandomToUser(this.randomNumbers, validatedUserNumbers);
+    const isCorrect = this.showRoundResultMessage(roundResult);
+
+    isCorrect ? await this.handleGameContinuation() : await this.playGameCycle();
+  }
+  async promptUserInput() {
+    return await Console.readLineAsync('숫자를 입력해주세요 : ');
   }
 
-  validateInputLineNumber(userNumber) {
-    const modifyArray = userNumber.split("").map(Number);
-    if (
-      modifyArray.length !== 3 ||
-      modifyArray.includes(0) ||
-      new Set(modifyArray).size !== modifyArray.length ||
-      modifyArray.some(isNaN)
-    ) {
-      throw new Error("[ERROR]");
-    } else {
-      return modifyArray;
+  validateInputNumber(userNumbers) {
+    const threeDigitNumRegex = /^[1-9]{3}$/;
+    const userNumberArray = userNumbers.split('').map((num) => parseInt(num));
+    const isDuplicate = new Set(userNumberArray).size !== userNumberArray.length;
+
+    if (!threeDigitNumRegex.test(userNumbers) || isDuplicate) {
+      throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
     }
+    return true;
   }
 
-  async compareCorrectAnswer(com, user) {
+  compareRandomToUser(randomNumbers, userNumbers) {
     let strikes = 0;
     let balls = 0;
 
-    for (let i = 0; i < com.length; i++) {
-      if (com[i] === user[i]) strikes++;
-      else if (user.includes(com[i])) balls++;
+    const userNumberArray = userNumbers.split('').map((num) => parseInt(num));
+   
+    for (let i = 0; i < randomNumbers.length; i++) {
+      if (randomNumbers[i] === userNumberArray[i]) strikes++;
+      else if (userNumberArray.includes(randomNumbers[i])) balls++;
     }
 
-    if (strikes === 3 && balls === 0) {
-      Console.print("3스트라이크");
-      return await this.confirmContinueOrQuit();
-    } if (strikes > 0 && balls > 0) {
-      const resultMessage = `${balls}볼 ${strikes}스트라이크`;
-      Console.print(resultMessage);
-    } else if (balls > 0) {
-      const resultMessage = `${balls}볼`;
-      Console.print(resultMessage);
-    } else if (strikes > 0) {
-      const resultMessage = `${strikes}스트라이크`;
-      Console.print(resultMessage);
-    } else {
-      Console.print("낫싱");
-    }
-
-    return await this.getUserInput();
+    return { strikes, balls };
   }
 
-  async confirmContinueOrQuit() {
-    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  showRoundResultMessage(status) {
+    const { strikes, balls } = status;
+    let resultMessage = '';
 
-    const isReplay = await Console.readLineAsync(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
-    );
+    if (strikes === 3 && balls === 0) {
+      Console.print('3스트라이크');
+      return true;
+    }
+
+    if (balls > 0) {
+      resultMessage += `${balls}볼`;
+    }
+
+    if (strikes > 0) {
+      resultMessage += (resultMessage ? ' ' : '') + `${strikes}스트라이크`;
+    }
+
+    resultMessage = resultMessage || '낫싱';
+
+    Console.print(resultMessage);
+
+    return false;
+  }
+
+  async promptContinueOrQuit() {
+    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    return await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
+  }
+
+  async handleGameContinuation() {
+    const isReplay = await this.promptContinueOrQuit();
 
     switch (isReplay) {
-      case "1": {
-        this.play();
+      case '1': {
+        await this.play();
         break;
       }
-      case "2": {
-        Console.print("게임 종료");
+      case '2': {
+        Console.print('게임 종료');
         break;
       }
       default: {
-        throw new Error();
+        throw new Error('[ERROR] 올바른 선택을 입력하세요. 1 또는 2를 입력해주세요.');
       }
     }
   }
