6442eb22f759ff62c516b267b45274ca833454b0
dooohun <ehgns0305@naver.com>
2023-10-24 21:54:47 +0900

    feat: 게임 출력 기능 함수 추가

---
+++ src/View.js
@@ -0,0 +1,29 @@
+import { OUTPUT_MESSAGE, GAME_RESULT } from "./constants/messages.js";
+import { Console } from "@woowacourse/mission-utils";
+
+function printIntroMessage() {
+  Console.print(OUTPUT_MESSAGE.intro);
+}
+
+function printGameResult(score = {}) {
+  let message = "";
+  if (score.strike === 0 && score.ball === 0) {
+    message += GAME_RESULT.nothing;
+  }
+  else {
+    if (score.ball > 0) {
+      message += score.ball + GAME_RESULT.ball;
+    }
+    if (score.strike > 0) {
+      if (message !== "") message += " ";
+      message += score.strike + GAME_RESULT.strike;  
+    }
+  }
+  Console.print(message);
+}
+
+function printEndMessage() {
+  Console.print(OUTPUT_MESSAGE.end);
+}
+
+export { printIntroMessage, printGameResult, printEndMessage };
