fd94dcafa743c65bb2da7324d25db786d8c711a3
윤태웅 <omygog@naver.com>
2023-10-24 12:48:09 +0900

    feat: 컴퓨터의 숫자 랜덤으로 3개 선택

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Random, Console } from '@woowacourse/mission-utils'
+
 class App {
-  async play() {}
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    const computerNumbers = [];
+    while(computerNumbers.length < 3){
+      const randomNumber = Random.pickNumberInRange(1, 9);
+      if(!computerNumbers.includes(randomNumber)){
+        computerNumbers.push(randomNumber);
+      }
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
