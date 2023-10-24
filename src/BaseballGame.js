e4fba1aabdaaca9347f33323d7d9acfdd2664e39
soyunim <togksk123@naver.com>
2023-10-24 16:34:27 +0900

    feat: 유저의 숫자 입력

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -1,11 +1,11 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { Messages } from "./Messages";
+import { GAME_MSG, ERROR_MSG } from "./Messages";
 
 // 0. 게임 시작 메세지 출력
 const gameStart = async () => {
-  MissionUtils.Console.print(Messages.START);
-  const COMPUTER_NUMBER = getComputerNum();
-  MissionUtils.Console.print(COMPUTER_NUMBER);
+  MissionUtils.Console.print(GAME_MSG.START);
+  const COMPUTER_NUM = getComputerNum();
+  const USER_NUM = getUserNum();
 };
 
 // 1. 컴퓨터의 랜덤 숫자
@@ -18,6 +18,19 @@ const getComputerNum = () => {
     }
   }
   return COMPUTER.join("");
+};
+
+// 2. 유저의 숫자 입력
+const getUserNum = async () => {
+  try {
+    const USER_NUM = await MissionUtils.Console.readLineAsync(GAME_MSG.INPUT);
+    if (USER_NUM.length !== 3) {
+      throw new Error(ERROR_MSG.INPUT_ERROR_LEN);
+    }
+    return USER_NUM;
+  } catch (error) {
+    throw new Error("[ERROR]");
+  }
 };
 
 export { gameStart };
