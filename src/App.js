4061fce5d6f51cacf9447062a3827ae513804370
Lim-SeongJun <dlatjdwns331@naver.com>
2023-10-25 00:20:22 +0900

    feat: 게임 한 턴 실행 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-class App {
-  async play() {}
-}
+import generateRandomNumber from "./utils/generateRandomNumbers";
 
+const { Console } = require("@woowacourse/mission-utils");
+
+class App {
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    this.gameTurn();
+  }
+
+  gameTurn() {
+    const randomNumber = generateRandomNumber();
+    this.gameStart(randomNumber);
+  }
+
+  gameStart() {}
+}
 export default App;
