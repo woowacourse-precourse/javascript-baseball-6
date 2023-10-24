c05346c586ba2cef46b16b36c19a010b5c03393b
HyunJaeyeon <bomul0109@gmail.com>
2023-10-25 00:32:34 +0900

    Feat: input 예외처리
    - getUserInput(): input 예외처리
    - 불필요한 catch 삭제
    - readme 수정

--- docs/README.md
+++ docs/README.md
@@ -3,11 +3,8 @@
 ## 구조 이해
 
 함수 시작()
-
 먼저, 컴퓨터값 받아오고
-
 input은 하나,
-
 처음에 자동 시작 → 일단 숫자 세자리 받음.
 
 3 strike가 될 때까지 계속 돈다 → 계속 숫자 세자리
@@ -19,18 +16,13 @@ input은 하나,
 - 1이면 재귀
 - 2이면 종료 → _return 0?_
 
-## play(): async 함수 설계
+## gameStart(): async 함수 설계
 
-async play()
+## async gameStart()
 
----
-
-getComputer(); // computer값 받아옴
-
----
+## getComputer(); // computer값 받아옴
 
 { while strike !== 3 // 3strike가 되기 전까지 계속 돌림
-
 input 3자리를 받고,
 
 ---
@@ -38,41 +30,70 @@ input 3자리를 받고,
 { 한자리씩 for문 돌리기
 
 1. user[i] === computer[j] //
-
    1. 자리 수 i, j 까지 똑같다면?
-
       → strike += 1 (strike)
-
    2. 자리수는 다른데 숫자만 똑같음
-
       → ball += 1 (ball)
-
-}
+      }
 
 ---
 
 print(스트라이크, 볼 수 혹은 낫싱)
-
 continue;
-
 }
 
 ---
 
 print(3스트라이크);
-
 if user === 1
-
-→ play(); //재귀
-
+→ gameStart(); //재귀
 if else user ===2
-
 → return 0; //함수 종료
 
 ---
 
+## checkInput(): 인풋 예외처리
+
+### 게임 중일 때
+
+- 서로 다른 3자리 수만 입력해야함
+  **예외**
+
+1. 3자리가 아니다
+2. 중복되는 숫자를 입력했다
+3. 0이 있다.
+   → 3자리 확인 → 0 확인 → 중복 확인
+
+### 게임 끝난 후
+
+- 1, 2 만 입력해야 함
+  **예외**
+
+1. 1 또는 2가 아니다
+
+### 케이스 나누기
+
+게임 중인가 아닌가? boolean으로
+→ `playing` = true / false
+
+1. 게임 중 (true)
+
+```jsx
+//Set으로 중복 제거 후 받은 배열크기랑 같은지 확인
+let isDup = new Set(input).size !== input.length; //boolean, 중복이면 true
+if (input.length !== 3 || input.includes(0) || isDup)
+  // 하나라도 true 이면
+  throw Error;
+```
+
+- 이 때 input은 number[]여야 함
+
+2. 게임 끝 (false)
+
+- input ===1 && ===2
+
 ### 할 일
 
-- [ ] 인풋 예외 처리 생각해보기
-- [o] 초기 구현 해보기
+- [o] 인풋 예외 처리 생각해보기
+- [ ] 아웃풋 구조 정리
 - [ ] 리드미 더 보기 좋게 바꿔보기
--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async play() {
+    async function checkInput(isPlaying, userInput) {
+      const arrInput = [...userInput].map(Number);
+      //게임 중
+      if (isPlaying) {
+        let isDup = new Set(arrInput).size !== arrInput.length;
+        if (arrInput.length !== 3 || arrInput.includes(0) || isDup) {
+          throw new Error("세자리 숫자오류, 종료합니다.");
+        }
+      }
+      //게임 끝
+      else if (!isPlaying) {
+        if (arrInput[0] !== 1 && arrInput[0] !== 2) {
+          //1이나 2가 아닐 때
+          throw new Error("한자리 숫자오류, 종료합니다.");
+        }
+      }
+    }
+    //user 입력값 지정 함수
+    async function getUserInput(isPlaying) {
+      const input = await MissionUtils.Console.readLineAsync(
+        "숫자를 입력해주세요 : "
+      );
+      //resolve 되는 경우 입력값 출력
+      MissionUtils.Console.print(input);
+      //check input
+      await checkInput(isPlaying, input);
+      return input;
+    }
     //컴퓨터 랜덤값 지정 함수
     function getComputer() {
       const computer = [];
@@ -12,32 +40,18 @@ class App {
       }
       return computer;
     }
-    //user 입력값 지정 함수
-    async function getUserInput() {
-      try {
-        const input = await MissionUtils.Console.readLineAsync(
-          "숫자를 입력해주세요 : "
-        );
-        //resolve 되는 경우 입력값 출력
-        MissionUtils.Console.print(input);
-        return input;
-      } catch (error) {
-        // reject 되는 경우
-        MissionUtils.Console.print(error);
-      }
-    }
 
+    //Start game
     async function gameStart() {
       const computer = getComputer();
+      // console.log(computer);
       let strike = 0;
       let ball = 0;
-
+      let playing = true;
       //3strike 될 때까지 while문
       while (true) {
         //user input 3자리 받기
-        // let x = await MissionUtils.Console.readLineAsync("입력");
-        // MissionUtils.Console.print(x);
-        let x = await getUserInput();
+        let x = await getUserInput(playing);
         let user = [...x].map(Number);
         //한자리씩 비교
         for (let i = 0; i < computer.length; i++) {
@@ -58,19 +72,18 @@ class App {
         ball = 0;
         continue;
       }
-
+      playing = !playing;
       //3strike (while문 빠져나옴)
       MissionUtils.Console.print("3스트라이크, 빠져나왔다");
-      // let user = await MissionUtils.Console.readLineAsync("입력");
-      let user = await getUserInput();
+      let user = await getUserInput(playing);
 
       //재귀
-      if (user === "1") {
+      if (Number(user) === 1) {
         MissionUtils.Console.print("다시 들어감요");
         gameStart();
       }
       //함수 종료
-      else if (user === "2") {
+      else if (Number(user) === 2) {
         MissionUtils.Console.print("나감요");
         return 0;
       }
