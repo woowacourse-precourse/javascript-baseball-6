d01d26762ceb4a761186e297f8f28dc91bfa5ea2
BaeDongWoo <ehddn5410@naver.com>
2023-10-25 03:18:23 +0900

    feat:사용자의 입력에 따라 재시작 또는 종료

--- src/App.js
+++ src/App.js
@@ -7,26 +7,33 @@ class App {
       Console.print('숫자 야구 게임을 시작합니다.');
       const computer = new Computer();
       const inputCheck = new InputCheck();
-      let computerNumber = computer.createRandomNumber();
-      let round = true;
-      while (round) {
-        let strike = 0;
-        let ball = 0;
-        const userNumber = await Console.readLineAsync('숫자를 입력해주세요 :');
-        inputCheck(userNumber);
-        let strikeBall = computer.countStrikeBall(computerNumber, userNumber);
-        strike = strikeBall[0];
-        ball = strikeBall[1];
-        if (strike === 3) round = false;
-        computer.resultPrint(strike, ball);
+      let isGame = true;
+      while (isGame) {
+        let computerNumber = computer.createRandomNumber();
+        let round = true;
+        while (round) {
+          let strike = 0;
+          let ball = 0;
+          const userNumber = await Console.readLineAsync(
+            '숫자를 입력해주세요 :'
+          );
+          inputCheck.checkInputNumber(userNumber);
+          let strikeBall = computer.countStrikeBall(computerNumber, userNumber);
+          strike = strikeBall[0];
+          ball = strikeBall[1];
+          if (strike === 3) round = false;
+          computer.resultPrint(strike, ball);
+        }
+        Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        const input = await Console.readLineAsync(
+          '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
+        );
+        inputCheck.restartInputCheck(input);
+        if (input === '2') isGame = false;
       }
-      Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-      const input = await Console.readLineAsync(
-        '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
-      );
     } catch (e) {
-      Console.log(e);
-      return Promise.reject(error);
+      Console.print(e);
+      return Promise.reject(e);
     }
   }
 }
