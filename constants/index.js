223515c203cea0f1e48f72783e5526b4079103ec
Kled <1017yu@daum.net>
2023-10-25 01:13:18 +0900

    refactor: 코드 컨벤션 의거 semantic value 소문자로 변경

--- constants/index.js
+++ constants/index.js
@@ -1,35 +1,35 @@
 const GUIDE_MESSAGES = Object.freeze({
-  START: '숫자 야구 게임을 시작합니다.',
-  INPUT: '숫자를 입력해주세요 : ',
-  PLAYER_WIN: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
-  RESTART_GAME: `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`,
-  END: `숫자 야구 게임을 종료합니다.`,
+  start: '숫자 야구 게임을 시작합니다.',
+  input: '숫자를 입력해주세요 : ',
+  playerWin: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  restartGame: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
 });
 
 const ERROR_MESSAGES = Object.freeze({
-  INVALID_COUNT_NUMBER: '[ERROR] 서로 다른 3자리의 수를 입력해주세요.',
-  INVALID_REPLAY_NUMBER: '[ERROR] 1 또는 2를 입력해주세요.',
+  invalidCountNumber: '[ERROR] 서로 다른 3자리의 수를 입력해주세요.',
+  invalidRestartNumber: '[ERROR] 1 또는 2를 입력해주세요.',
 });
 
 const PATTERN = Object.freeze({
-  THREE_DIGIT_REGEX: new RegExp(/^([1-9])(?!.*\1)([1-9])(?!.*\1|\2)([1-9])$/),
+  threeDigitRegex: new RegExp(/^([1-9])(?!.*\1)([1-9])(?!.*\1|\2)([1-9])$/),
 });
 
 const BALL_COUNTS = Object.freeze({
-  BALL: '볼',
-  STRIKE: '스트라이크',
-  NOTHING: '낫싱',
+  ball: '볼',
+  strike: '스트라이크',
+  nothing: '낫싱',
 });
 
 const RESTART_GAME_NUMBERS = Object.freeze({
-  RESTART: '1', // 게임을 다시 시작하기 위한 입력값
-  END: '2', // 게임을 종료하기 위한 입력값
+  restart: '1', // 게임을 다시 시작하기 위한 입력값
+  end: '2', // 게임을 종료하기 위한 입력값
 });
 
 const SETTINGS = Object.freeze({
-  MAX_RANGE: 9, // 1부터 9까지의 숫자 범위의 상한값
-  MIN_RANGE: 1, // 1부터 9까지의 숫자 범위의 하한값
-  NUMBER_OF_RANDOM: 3, // 3개의 임의 숫자를 생성
+  maxRange: 9, // 1부터 9까지의 숫자 범위의 상한값
+  minRange: 1, // 1부터 9까지의 숫자 범위의 하한값
+  numberOfRandom: 3, // 3개의 임의 숫자를 생성
+  winningNumber: 3, // 승리에 필요한 스트라이크 수
 });
 
 export {
