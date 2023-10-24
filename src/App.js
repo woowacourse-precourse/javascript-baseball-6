7b0c6d5c2913b838d1c349696ff282f5584cd1cb
Klomachenko <leekyumin0901@naver.com>
2023-10-25 01:54:08 +0900

    feat: 게임 시작 문구 출력기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+  }
 }
 
 export default App;
