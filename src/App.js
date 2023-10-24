f7ee5e123d506b951d74e9e12d1fb5650d8e85d5
hs <tlsgustn93@naver.com>
2023-10-24 13:19:32 +0900

    userInput과 computerInput을 비교

--- src/App.js
+++ src/App.js
@@ -34,6 +34,29 @@ class App {
     if (!this.isValid(userInput)) throw new Error("[ERROR]");
     return userInput;
   }
+
+  // 유저가 입력한 값과 컴퓨터의 값을 비교합니다.
+  // 비교를 하기 쉽게 하기 위해 문자열을 배열로 바꿉니다.
+  // 컴퓨터의 값과 유저의 값이 위치도 같으면 strikeCount, 값만 존재한다면 ballCount를 ++합니다.
+  // strikeCount가 3이 되지 않는다면 계속 비교, 만약 같다면 다시 시작할 것인지를 물어봅니다.
+
+  async numberCompare() {
+    const userInput = await this.userAnswer();
+    const computerInput = this.computerInput;
+    const computerArr = `${computerInput}`.split("");
+    const userArr = `${userInput}`.split("");
+    let strikeCount = 0;
+    let ballCount = 0;
+    computerArr.forEach((ele, idx) => {
+      if (ele === userArr[idx]) strikeCount++;
+      else if (userArr.includes(ele)) ballCount++;
+    });
+
+    this.ballStrikeResult(strikeCount, ballCount);
+
+    if (strikeCount !== 3) return this.numberCompare();
+    return this.reStart();
+  }
 }
 
 const app = new App();
