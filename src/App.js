4dbefd8d6d48edcd313b30c68b3581cfabc39421
Romuru <pastelblue0721@gmail.com>
2023-10-24 21:15:25 +0900

    feat  (1)  : 무작위의 숫자 3개 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+
 const { Console, Random } = MissionUtils;
 
 class App {
-  async play() {}
+  constructor() {
+    this.randomNumberArray = [];
+  }
+
+  createRandomNumber() {
+    while (this.randomNumberArray.length < 3) {
+      const randomNumber = Random.pickNumberInRange(1, 9);
+      if (!this.randomNumberArray.includes(randomNumber)) {
+        this.randomNumberArray.push(randomNumber);
+      }
+    }
+  }
+
+  async play() {
+    this.createRandomNumber();
+  }
 }
+
+const app = new App();
+
+app.play();
 
 export default App;
