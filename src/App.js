c88e87543f62c7535047f820032df0af9dba6f60
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-10-25 02:48:01 +0900

    fix(baseballGame) : 객체 생성해서 BaseballGame 가져오기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 const { Console } = require("@woowacourse/mission-utils");
+const { BaseballGame } = require("./BaseballGame.js");
 const { GAME_MESSAGE } = require("./constants");
 
 class App {
   async play() {
-    
+    const baseballGame = new BaseballGame();
     // 게임 시작 메서드(BaseballGame) 호출
-    return this.BaseballGame(this.computerNumber);
+    return baseballGame.startBasballGame();
   }
 }
 
