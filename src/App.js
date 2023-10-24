45bc8eed2f9d58df67d1b94ae9199f7c68687554
nari <naringst@gmail.com>
2023-10-24 23:03:29 +0900

    feat : 에러 처리

--- src/App.js
+++ src/App.js
@@ -14,9 +14,23 @@ class App {
     }
 
     //플레이어 입력 받기
-    const userInput = await MissionUtils.Console.readLineAsync(
+    let userInput = await MissionUtils.Console.readLineAsync(
       "숫자를 입력해주세요 : "
     );
+    userInput = Number(userInput);
+    if (typeof userInput !== "number") {
+      MissionUtils.Console.print(typeof userInput);
+      MissionUtils.Console.print("[ERROR] 숫자를 입력해야 합니다.");
+    } else if (userInput.toString().length !== 3) {
+      MissionUtils.Console.print(userInput.toString().length);
+      MissionUtils.Console.print("[ERROR] 세 자리 숫자를 입력해야 합니다.");
+    } else {
+      MissionUtils.Console.print(typeof userInput);
+
+      //플레이어가 입력한 숫자에 대해 힌트 제공
+
+      //맞출 시 게임 종료 => 다시 시작(1) or 완전히 종료(2) 입력 받기
+    }
   }
 }
 
