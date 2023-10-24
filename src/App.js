8883ff8aea07a6616c7eba708865122c1105deb6
lottt105 <hyobin0601@naver.com>
2023-10-25 01:31:55 +0900

    Feat: 게임 시작 문구 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    
     const RANDOM_VALUE = this.getRandomNumber();
     let isSuccess =  false;
 
