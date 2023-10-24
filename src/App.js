368fa8294a05e623ea689ad4e9668d29ad41ba39
flowersayo <flowersayo0811@ewhain.net>
2023-10-25 00:12:08 +0900

    feat : 컴퓨터 랜덤 숫자 생성 기능 ( generateRandomNumbers )

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  generateRandomNumbers() {
+    const randomNumbers = [];
+    while (randomNumbers.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!randomNumbers.includes(number)) {
+        randomNumbers.push(number);
+      }
+    }
+
+    return randomNumbers;
+  }
+
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    this.computerNumbers = this.generateRandomNumbers();
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
