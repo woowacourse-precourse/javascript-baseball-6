690a7b34a73c17a04b38dac524eda355889b7aa7
HyunseokLEE <unducklee@gmail.com>
2023-10-25 06:38:31 +0900

    refact: double quotes 제거

--- src/Output/Output.js
+++ src/Output/Output.js
@@ -7,8 +7,8 @@ const Output = {
 
   printHintMessage(ballCount, strikeCount) {
     const hint = [];
-    if (ballCount !== 0) hint.push(`${ballCount}"볼"`);
-    if (strikeCount !== 0) hint.push(`${strikeCount}"스트라이크"`);
+    if (ballCount !== 0) hint.push(`${ballCount}볼`);
+    if (strikeCount !== 0) hint.push(`${strikeCount}스트라이크`);
     if (ballCount === 0 && strikeCount === 0) hint.push("낫싱");
 
     Console.print(hint.join(" "));
