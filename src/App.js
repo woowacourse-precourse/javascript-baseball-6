2c392ab21cf79c7f7a43eddd4a48013408bb1f7f
jomoo02 <jomootgrn88@naver.com>
2023-10-25 02:38:55 +0900

    feat: main
    메인 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { printStartApp } from './print.js';
+import main from './main.js';
+
 class App {
-  async play() {}
+  async play() {
+    const NUMBERS_LENGTH = 3;
+    printStartApp();
+    await main(NUMBERS_LENGTH);
+  }
 }
 
 export default App;
