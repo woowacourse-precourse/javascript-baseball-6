9e583524ed25b990356c40225238a2552a9bfbab
CHHyuk <gksgur1038@naver.com>
2023-10-25 04:58:05 +0900

    정답 숫자만들기, 형식 검사, 비교, 결과값 출력

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
-// 랜덤으로 정답이 되는 숫자 생성
+// 정답이 되는 숫자 만들기
 function generateRandomNumber() {
   const answer = [];
   while (answer.length < 3) {
@@ -9,17 +9,54 @@ function generateRandomNumber() {
       answer.push(number);
     }
   }
-  return computer;
+  return answer;
 }
 
-// 
+// 숫자 입력, 입력한 숫자가 형식에 맞는지 검사하기
+async function checkRightFormat() {
+  const userInput = await Console.readLineAsync("숫자를 입력해주세요 : ");
+
+  if (!/^[1-9]{3}$/.test(Number(userInput))) {
+    throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+  }
+
+  return userInput.split('').map(Number)
+}
 
 
+// 정답이 되는 숫자와 입력한 숫자를 비교하기
+function checkNumbers(userInput, answer) {
+  let strikes = 0;
+  let balls = 0;
 
+  for (let i = 0; i < 3; i++) {
+    if (userInput[i] === answer[i]) {
+      strikes += 1;
+    } else if (answer.includes(userInput[i])) {
+      balls += 1;
+    }
+  }
+
+  return { balls, strikes };
+}
+
+// 비교하여 나온 결과값을 출력하기
+function printChecked(balls, strikes) {
+  if (balls !== 0 && strikes !== 0) {
+    Console.print(`${balls}볼 ${strikes}스트라이크`);
+  }
+  if (balls !== 0) {
+    Console.print(`${balls}볼`);
+  }
+  if (strikes !== 0) {
+    Console.print(`${strikes}스트라이크`);
+  } else { Console.print('낫싱') }
+}
+
+// 게임 실행 제어
 
 class App {
   async play() {
-
   }
 }
 
