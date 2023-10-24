cbcd2e90fb5ea46159876ffdef9d3041fe3979e2
Dev-steadyBoy <85480086+Dev-steadyBoy@users.noreply.github.com>
2023-10-24 15:10:15 +0900

    refactor: [Constants] 상수 값을 담은 객체의 불변성 속성 추가

--- core/Constants.js
+++ core/Constants.js
@@ -1,27 +1,27 @@
-const COMMAND = {
+const COMMAND = Object.freeze({
   START: '숫자 야구 게임을 시작합니다.',
   END: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
   ENTER_NUMBER: '숫자를 입력해주세요 : ',
   ASK: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
   RESTART: '1',
-};
+});
 
-const COMPUTER = {
+const COMPUTER = Object.freeze({
   START_NUMBER: 1,
   END_NUMBER: 9,
   STORAGE_LIMIT: 3,
-};
+});
 
-const GAME = {
+const GAME = Object.freeze({
   STRIKE: '스트라이크',
   BALL: '볼',
   NOTHING: '낫싱',
   STRIKE_OUT: 3,
   ZERO: 0,
-};
+});
 
-const ERROR = {
+const ERROR = Object.freeze({
   INVALID_NUMBER: '숫자가 잘못된 형식입니다.',
-};
+});
 
 export { COMMAND, COMPUTER, GAME, ERROR };
