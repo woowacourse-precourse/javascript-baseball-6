f35c74ce9014f9b8357181b0ded3572dcd40e1bf
lord-potato <lordpotato1227@gmail.com>
2023-10-24 14:58:31 +0900

    fix : 쓸모없는 줄 삭제, 각주 수정

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ class App {
     // 게임 시작과 동시에 랜덤 숫자 (정답)을 생성합니다.
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다");
     let RAND_ANSWER = func.createRandomNumber();
-    // MissionUtils.Console.print(RAND_ANSWER);
     let RESTART = false;
 
     while (true) {
       //3스트라이크, 혹은 오류가 발생할때까지 계속해서 입력값을 받습니다.
       if (RESTART) {
         RAND_ANSWER = func.createRandomNumber();
-        MissionUtils.Console.print(RAND_ANSWER);
         RESTART = false;
       }
 
@@ -41,7 +39,7 @@ class App {
       if (STRIKE === 3) {
         MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         let USERCHOICE = await func.getUserChoice();
-        console.log;
+
         if (USERCHOICE === "1") {
           RESTART = true;
           continue;
