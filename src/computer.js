e12627139d2786607385f8bb767c5cb3468e7046
yeonkyoung <keep.ykk@gmail.com>
2023-10-25 03:45:34 +0900

    feat: 컴퓨터 3개의 숫자 배열 생성

---
+++ src/computer.js
@@ -0,0 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const pickNumbers = () => {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer;
+};
+
+export { pickNumbers };
