cf43298c6426156125dd9fb9bd20c8ac285a44ad
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-10-24 17:37:28 +0900

    Feat: 숫자 수동 선택 기능 완료

--- src/game/playGame.js
+++ src/game/playGame.js
@@ -11,8 +11,24 @@ const getComputerNumber = async () => {
   return computerNumbers;
 };
 
+const getPlayerNumbers = async () => {
+  try {
+    const answer = await MissionUtils.Console.readLineAsync(
+      "숫자를 입력해주세요: "
+    );
+    return answer;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const startGame = async (computerNumbers) => {
+  const playerNumbers = await getPlayerNumbers();
+};
+
 const initalStartGame = async () => {
   const computerNumbers = await getComputerNumber();
+  await startGame(computerNumbers);
 };
 
 export default initalStartGame;
