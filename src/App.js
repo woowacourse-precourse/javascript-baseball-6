bf21bc07666e88838399b51b30b290d490caa1fa
prgmr99 <johnyeom24@naver.com>
2023-10-24 21:36:16 +0900

    [feat] 예외처리 구체화 및 프로그램 종료 로직 변경

--- src/App.js
+++ src/App.js
@@ -3,98 +3,18 @@ import { Console, Random } from "@woowacourse/mission-utils";
 import init from "./init";
 
 class App {
-  // constructor() {
-  //   this.init();
-  // }
-
-  // generateRandomNumber() {
-  //   const computer = [];
-  //   while (computer.length < 3) {
-  //     const number = Random.pickNumberInRange(1, 9);
-  //     if (!computer.includes(number)) {
-  //       computer.push(number);
-  //     }
-  //   }
-  //   return computer;
-  // }
-
-  // init() {
-  //   Console.print("숫자 야구 게임을 시작합니다.");
-  //   this.strike = 0;
-  //   this.ball = 0;
-  //   this.randoms = this.generateRandomNumber();
-  // }
-
-  // printResult(strike, ball) {
-  //   let result = "낫싱";
-  //   if (strike !== 0 && ball === 0) {
-  //     result = `${strike}스트라이크`;
-  //   }
-  //   if (strike === 0 && ball !== 0) {
-  //     result = `${ball}볼`;
-  //   }
-  //   if (strike !== 0 && ball !== 0) {
-  //     result = `${ball}볼 ${strike}스트라이크`;
-  //   }
-  //   return result;
-  // }
-
-  // askPlay() {
-  //   try {
-  //     Console.readLine(
-  //       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
-  //       (num) => {
-  //         if (num === "1") {
-  //           return init();
-  //         } else if (num === "2") {
-  //           return 0;
-  //         } else {
-  //           console.log("여긴가?");
-  //           throw "[ERROR] 숫자가 잘못된 형식입니다.";
-  //         }
-  //       }
-  //     );
-  //   } catch (error) {
-  //     Console.print(error);
-  //   }
-  // }
-
-  // comparePitches(userNum, computerNum) {
-  //   const user = userNum.split("").map((e) => +e);
-
-  //   for (let i = 0; i < user.length; i++) {
-  //     let num = computerNum.indexOf(user[i]);
-  //     if (num === i) {
-  //       this.strike++;
-  //     } else if (num !== -1 && num !== i) {
-  //       this.ball++;
-  //     }
-  //   }
-
-  //   Console.print(printResult(this.strike, this.ball));
-
-  //   if (this.strike === 3) {
-  //     Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-  //     return askPlay();
-  //   }
-  //   inputPitches(computerNum);
-  // }
-
-  // async play() {
-  //   try {
-  //     const inputNumber = await Console.readLineAsync("숫자를 입력해주세요 : ");
-  //     if (inputNumber.length > 3) {
-  //       throw "[ERROR] 숫자가 잘못된 형식입니다.";
-  //     }
-  //     await comparePitches(inputNumber, this.randoms);
-  //   } catch (error) {
-  //     Console.print(error);
-  //   }
-  // }
   async play() {
-    //Console.print("숫자 야구 게임을 시작합니다.");
-    init();
+    try {
+      Console.print("숫자 야구 게임을 시작합니다.");
+      await init();
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
   }
 }
+
+const app = new App();
+app.play();
 
 export default App;
--- src/init.js
+++ src/init.js
@@ -2,7 +2,6 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 let STRIKE = 0;
 let BALL = 0;
-let NOTHING = 0;
 
 const generateRandomNumber = () => {
   const computer = [];
@@ -15,7 +14,7 @@ const generateRandomNumber = () => {
   return computer;
 };
 
-const comparePitches = (userNum, computerNum) => {
+const comparePitches = async (userNum, computerNum) => {
   const user = userNum.split("").map((e) => +e);
 
   for (let i = 0; i < user.length; i++) {
@@ -24,8 +23,6 @@ const comparePitches = (userNum, computerNum) => {
       STRIKE++;
     } else if (num !== -1 && num !== i) {
       BALL++;
-    } else {
-      NOTHING++;
     }
   }
 
@@ -33,8 +30,8 @@ const comparePitches = (userNum, computerNum) => {
 
   if (STRIKE === 3) {
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    MissionUtils.Console.print("1볼 1스트라이크");
-    return askPlay();
+    // MissionUtils.Console.print("1볼 1스트라이크");
+    return await askPlay();
   }
   inputPitches(computerNum);
 };
@@ -54,37 +51,43 @@ const printResult = (strike, ball) => {
 };
 
 const askPlay = async () => {
-  try {
-    const num = await MissionUtils.Console.readLineAsync(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
-    );
-    if (num === "1") return init();
-    if (num === "2") return 0;
-    else throw "[ERROR] 숫자가 잘못된 형식입니다.";
-  } catch (error) {
-    MissionUtils.Console.print(error);
+  const choose = await MissionUtils.Console.readLineAsync(
+    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+  );
+
+  if (choose === "1") await init();
+  else if (choose !== "2") throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+};
+
+const checkValidation = (userInput) => {
+  if (userInput.length > 3) {
+    return "[ERROR] 숫자가 잘못된 형식입니다.";
   }
+  if (userInput.includes(" ")) {
+    return "[ERROR] 숫자가 잘못된 형식입니다.";
+  }
+  if (Number.isNaN(userInput)) {
+    return "[ERROR] 숫자가 잘못된 형식입니다.";
+  }
+  return "VALID";
 };
 
 const inputPitches = async (computerNum) => {
-  STRIKE = BALL = NOTHING = 0;
-  try {
-    const userInput = await MissionUtils.Console.readLineAsync(
-      "숫자를 입력해주세요 : "
-    );
-    if (userInput.length > 3) {
-      throw "[ERROR] 숫자가 잘못된 형식입니다.";
-    }
-    // MissionUtils.Console.print(`숫자를 입력해주세요 : ${userInput}`);
-    comparePitches(userInput, computerNum);
-  } catch (error) {
-    MissionUtils.Console.print(error);
-  }
+  STRIKE = BALL = 0;
+
+  const userInput = await MissionUtils.Console.readLineAsync(
+    "숫자를 입력해주세요 : "
+  );
+  if (checkValidation(userInput) !== "VALID")
+    throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+  await MissionUtils.Console.print(`숫자를 입력해주세요 : ${userInput}`);
+
+  await comparePitches(userInput, computerNum);
 };
 
-const init = () => {
+const init = async () => {
   const computerNum = generateRandomNumber();
-  inputPitches(computerNum);
+  await inputPitches(computerNum);
 };
 
 export default init;
