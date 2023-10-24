ed9f3964170563fe80d897a04fa61e3847e7eb04
gyucheolshin <nsfsgc2295@gmail.com>
2023-10-25 02:51:24 +0900

    refactor : 유저 입력함수 추가 및 예외 리펙토링

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  constructor() {
-    this.computer = [];
-    this.userAnswer = [];
-  }
-
   async play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.gameStart();
   }
 
   gameStart() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.setComputerNumber();
-    MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ").then(
-      (answer) => {
-        this.answerCheck(answer);
-        this.userAnswer = answer.split("").map((v) => Number(v));
-        this.calculateScore(this.computer, this.userAnswer);
-      }
-    );
+    this.setUserNumber();
+  }
+
+  setUserNumber() {
+    const USER_NUMBER =
+      MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+    this.calculateScore(this.computer, USER_NUMBER);
   }
 
   setComputerNumber() {
@@ -39,10 +34,12 @@ class App {
   }
 
   answerCheck(answer) {
-    if (isNaN(Number(answer))) throw new Error("숫자만 입력해주세요.");
-    if (answer.length !== 3) throw new Error("3자리 숫자를 입력해주세요.");
-    if (!this.duplicateNumber(answer))
-      throw new Error("중복되지 않은 숫자를 입력해주세요.");
+    if (
+      isNaN(Number(answer)) ||
+      answer.length !== 3 ||
+      !this.duplicateNumber(answer)
+    )
+      throw new Error("only number");
   }
 
   reStartAnswerCheck(answer) {
