1b02243f7fe37822ebe36389e4984f08a3cab4ff
ChoByungHyun <cbj973@gmail.com>
2023-10-25 04:50:49 +0900

    ✨ Feat: Console 다루는 함수 생성

---
+++ src/util/ConsoleControll.js
@@ -0,0 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+import { INPUT_MESSAGE, OUTPUT_MESSAGE } from "../constants/message.js";
+
+const ConsoleView = {
+  async readNumber() {
+    return await Console.readLineAsync(INPUT_MESSAGE.NUMBER);
+  },
+
+  async readRetry() {
+    return await Console.readLineAsync(INPUT_MESSAGE.RETRY);
+  },
+
+  async printStart() {
+    await Console.print(OUTPUT_MESSAGE.START);
+  },
+};
+
+export default ConsoleView;
