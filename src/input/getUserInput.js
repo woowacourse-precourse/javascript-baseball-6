47890beae05776fd239706b7088e98f293cfa6fb
yeong-hwan <jghff700@naver.com>
2023-10-25 01:15:20 +0900

    refactor: [기능2] getUserInput에서 Validation 분리 baseballGame에서 useInput 입력과 checkInputValid을 각각 수행하게끔 변경

--- src/input/getUserInput.js
+++ src/input/getUserInput.js
@@ -1,23 +1,12 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import checkInputValid from "./checkInputValid.js";
 
 /**
  * 터미널로 유저에게 input을 입력받아 전달한다.
  * 
  * @returns {string}
- * @throws 사용자가 잘못된 값을 입력했을 경우 throw Error
  */
 export default async function getUserInput() {
   const userInput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
   
-  try {
-    if (!checkInputValid(userInput)) {
-      throw new Error("잘못된 값을 입력했습니다.");
-    }
-  } catch (err){
-    MissionUtils.Console.print("Error : " + err.message);
-  }
   return userInput;
 }
-
-// console.log(await getUserInput());
