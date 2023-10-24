ff2c26c272e11dbf5d88cfc23fd106bd7a6d751e
aaapark <parksc1534.dev@gmail.com>
2023-10-24 18:02:18 +0900

    게임진행의사묻기

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App {
 
         if (strike === 3) {
           userWillRetry = false;
-        }
+        } 
       }
     }
 
@@ -32,7 +32,23 @@ class App {
       const arrayOfInput = Array.from(String(inputNumber),Number);
       return arrayOfInput;
     } 
+
+    async isUserWillingToRestart() {
+      let restart = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.')
+      .then((value) => value);
+      if (restart === "1") {
+        MissionUtils.Console.print("1번을 눌렀습니다. 게임을 재시작합니다.");
+        return true;
+      } else if (restart === "2") {
+        MissionUtils.Console.print("2번을 눌렀습니다. 게임을 종료하겠습니다.");
+        return false;
+      } else {
+        this.isUserWillingToRestart("잘못된 숫자를 입력하였습니다. ")
+      }
+    }
 };
+
+
 
 const app = new App()
 app.play()
