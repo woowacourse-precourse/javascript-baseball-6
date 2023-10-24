dc721bf52e7c77e1044b99909f4ed19b84bb56f5
cyj <cyd5538@naver.com>
2023-10-24 13:08:57 +0900

    Feat: randomnumber와 user 반은 넘버 check하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
+
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
     const randomNumber = this.getRandomNumber();
-    this.getUserNumber()
+    let isGameOver  = true;
+    
+    while(isGameOver) {
+      const userNumber = await this.getUserNumber();
+      const result = this.checkNumbers(randomNumber, userNumber)
+      Console.print(result);
+      if(result === '3스트라이크') {
+        isGameOver = false;
+        Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      }
+    }
   }
 
   getRandomNumber() {
@@ -35,10 +46,33 @@ class App {
         throw new Error("[ERROR] 숫자를 중복되게 넣지 말아주세요.")
       }
       
+      return number;
     } catch (error) {
       console.log(error.message);
       return;
     }
+  }
+
+  checkNumbers(computer, user) {
+    let strike = 0;
+    let ball = 0;
+
+    for(let i=0; i<3; i++) {
+      if(computer[i] === +user[i]){
+        strike ++
+      } else if(computer.includes(+user[i])) {
+        ball ++
+      }
+    }
+    
+    if (strike === 3) {
+      return "3스트라이크"; 
+    } else if (strike === 0 && ball === 0) {
+      return "낫싱"; 
+    } else {
+      return `${ball}볼 ${strike}스트라이크`; 
+    }
+
   }
 }
 
