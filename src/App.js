bf3a67f76f392f41de232d4d76155417fbd1154b
hoya <wlsgh2018@naver.com>
2023-10-24 11:49:38 +0900

    feat: 컴퓨터 랜덤 숫자  생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  play() {}
+
+  generateRandomNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+  }
 }
 
 export default App;
