822459f9d28cfe277e3bb6dd0cb8d76a0b21635d
ChoiSenn <quitechoisen@gmail.com>
2023-10-25 00:55:29 +0900

    Create form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-class App {
-  async play() {}
+// 라이브러리 import
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+// 메인 App 클래스
+class App {  
+  async play() {  // play 메소드.   async는 비동기 처리를 할 때 await와 함께 쓰인다고 함.
+    
+  }
 }
 
 export default App;
+
+// app.play()로 실행
+const app = new App();
+app.play();
