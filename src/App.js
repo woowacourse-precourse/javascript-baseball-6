4d735ee3790803a8e6fcfa387c87306b9bbb6798
yohan <koyohanyoko@gmail.com>
2023-10-24 23:51:29 +0900

    [Feat] 랜덤 세자리 수를 설정하는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 class App {
   async play() {}
+
+  setAnswer() {
+    const computer = [];
+    while (computer.length < 3) {
+      const randomNum = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(randomNum)) computer.push(randomNum);
+    }
+  
+    return [...computer];
+  }
+
 }
 
 export default App;
