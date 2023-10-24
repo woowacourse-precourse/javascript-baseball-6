4195e001a790e5d07748a9c4caafa6cbb8871743
heonq <gkagjsrb@naver.com>
2023-10-24 13:57:44 +0900

    refactor: 메시지 출력과 관련된 기능을 OutputView로 분리 - MVC패턴에 맞게 리팩터링

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -1,22 +1,18 @@
-import {Console} from '@woowacourse/mission-utils';
-import MESSAGES from './Messages';
 import Computer from './Computer';
 import Validator from '../utils/Validator';
 import NumberChecker from './NumberChecker';
 import CONSTANTS from './Constants';
 import InputView from './Views/InputView';
+import OutputView from './Views/OutputView';
 
 class BaseballGame {
   computerNumber;
 
   async play() {
-    this.printStart();
+    OutputView.printStart();
     await this.startGame();
   }
 
-  printStart() {
-    Console.print(MESSAGES.start);
-  }
   async startGame() {
     this.generateComputer();
     await this.readNumber();
@@ -33,17 +29,10 @@ class BaseballGame {
   handleNumber(userNumber) {
     Validator.validateUserNumber(userNumber);
     const result = NumberChecker.getResult(userNumber, this.computerNumber);
-    this.printResult(result);
+    OutputView.printResult(result);
     this.handleResult(result);
   }
-  printResult({ball, strike}) {
-    let result = '';
-    if (ball > CONSTANTS.nothing) result += ball + MESSAGES.ball;
-    if (strike > CONSTANTS.nothing) result += strike + MESSAGES.strike;
-    if (ball === CONSTANTS.nothing && strike === CONSTANTS.nothing) result = MESSAGES.nothing;
-    Console.print(result.trim());
-    if (strike === CONSTANTS.winningStrike) Console.print(MESSAGES.correct);
-  }
+
   handleResult({_, strike}) {
     if (strike < CONSTANTS.winningStrike) return this.readNumber();
     this.readRetry();
@@ -51,7 +40,7 @@ class BaseballGame {
 
   async readRetry() {
     const userInput = await InputView.readRetry();
-    this.handleRetry(userInput);
+    await this.handleRetry(userInput);
   }
   handleRetry(userInput) {
     Validator.validateRetry(userInput);
