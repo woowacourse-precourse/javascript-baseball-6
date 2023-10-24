a3131096fca43c7459136e1be3aab98f63c8ae9a
yonainthefish <yonaisgood@gmail.com>
2023-10-25 04:30:48 +0900

    Feat: 컴퓨터랜덤 숫자와 사용자 숫자 비교 후 결과 출력

---
+++ src/components/compareNum.js
@@ -0,0 +1,31 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+async function compareNumbers(inputNum, randomComputeNum) {
+  let strike = 0;
+  let ball = 0;
+
+  for (let i = 0; i < inputNum.length; i++) {
+    if (inputNum[i] === randomComputeNum[i]) {
+      strike++;
+    } else if (randomComputeNum.includes(inputNum[i])) {
+      ball++;
+    }
+  }
+
+  if(strike === 3){
+    MissionUtils.Console.print("3개의 숫자를 모두 맞추어 게임이 종료됩니다.");
+    return true;
+  }else if(strike !== 0 || ball !== 0){
+    if(ball === 0){
+      MissionUtils.Console.print(strike + "스트라이크");
+    }else if(strike === 0){
+      MissionUtils.Console.print(ball + "볼");
+    }else{
+      MissionUtils.Console.print(ball + "볼" + strike + "스트라이크");
+    }
+  }else{
+    MissionUtils.Console.print("낫싱");
+  }
+}
+
+export default compareNumbers;
