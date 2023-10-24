df4a28bd859bdf4b8fbfd5e399c3c55ba44c827d
chiman <rlaclghks123@naver.com>
2023-10-24 18:21:36 +0900

    refactor: InputView에서 발생한 에러 catch에서 throw로 변경
    일반적으로 에러 객체로 전달될 것 같아 new Error()를 통해 새로운 에러 객체를 만들기 보다 그냥 throw하는것이 낫다고 생각하여 수정

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -5,7 +5,7 @@ const InputView = {
     try {
       return await Console.readLineAsync('숫자를 입력해주세요 : ');
     } catch (error) {
-      throw new Error(`[ERROR] ${error}`);
+      throw error;
     }
   },
 
@@ -13,7 +13,7 @@ const InputView = {
     try {
       return await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
     } catch (error) {
-      throw new Error(`[ERROR] ${error}`);
+      throw error;
     }
   },
 };
