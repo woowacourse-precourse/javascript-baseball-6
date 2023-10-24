871a07872d38a00d50bee5e347f71f69f3943ef4
pakxe <pigkill40@naver.com>
2023-10-25 06:58:30 +0900

    comment(isAllZero): 함수에 대한 주석 작성

--- src/utils/isAllZero.js
+++ src/utils/isAllZero.js
@@ -1,3 +1,8 @@
+/**
+ * 배열의 값이 모두 0인지 아닌지 판별하는 함수
+ * @param {number[]} arr 숫자 배열
+ * @returns Boolean
+ */
 const isAllZero = (arr) => {
 	for (let i = 0; i < arr.length; i++) {
 		if (arr[i] !== 0) return false;
