f1c4a5cc3906f90b697f253a8fff09ed6262c5db
pakxe <pigkill40@naver.com>
2023-10-25 06:59:24 +0900

    chore(Input): 불필요한 주석 제거

--- src/io/input/Input.js
+++ src/io/input/Input.js
@@ -3,7 +3,7 @@ import GAME_MESSAGE from '../../constants/GameMessage.js';
 
 class Input {
 	static getTryNumber() {
-		return Console.readLineAsync(GAME_MESSAGE.getNumber); // 입력으로 받아온 값을 반환한다.
+		return Console.readLineAsync(GAME_MESSAGE.getNumber);
 	}
 
 	static getRestart() {
