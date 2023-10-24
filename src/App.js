5405f6542ba58f97db2e06dc178cd4c7f143ec25
JiYoon Lee <wldbseja@daum.net>
2023-10-24 14:52:24 +0900

    feat : ✨ 게임 시작 문구 출력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Random, Console } from '@woowacourse/mission-utils'
+import {
+  NUM,
+  PRINT_STRING,
+  PRINT_ERROR_STRING,
+} from './constants.js'
+
 class App {
-  async play() {}
+
+  printGameStart() {
+    Console.print(PRINT_STRING.GAME_START)
+  }
+
+  async play() {
+    this.printGameStart()
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
