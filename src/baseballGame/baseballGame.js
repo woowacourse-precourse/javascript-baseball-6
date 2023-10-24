f3c28d2e1efaf42d316001ceb1c710568137e615
yeong-hwan <jghff700@naver.com>
2023-10-25 02:02:06 +0900

    refactor: [기능3, 4] Validation, Answer Check, Restart/Exit 순 흐름 개선 restart -> repeatsRestart로 boolean 직관성 개선

--- src/baseballGame/baseballGame.js
+++ src/baseballGame/baseballGame.js
@@ -4,40 +4,45 @@ import getUserInput from "../input/getUserInput.js";
 import countBall from "./countBall.js";
 import countStrike from "./countStrike.js";
 import gameResult from "./gameResult.js";
+import checkInputValid from "../input/checkInputValid.js";
 
 /**
  * 야구게임을 실행한다
  * 
  * @todo indent 많아서 기능 분할 필요
+ * @throws 사용자가 잘못된 값을 입력했을 경우 throw Error
  */
 export default async function baseballGame() {
-  let restart = true;
+  let repeatsRestart = true;
 
-  while (restart) {
+  while (repeatsRestart) {
     const answer = makeAnswer();
-    let wrongAnswer = true;
-    // console.log(`answer: ${answer}`);
-    // console.log(`restart: ${restart}, wrongAnswer: ${wrongAnswer}`);
+    let isAnswerCorrect = false;
+    console.log(`answer: ${answer}`);
 
-    while (wrongAnswer) {
+    while (!isAnswerCorrect) {
+      // userInput validation
       const userInput = await getUserInput();
-      const strikeCount = countStrike(answer, userInput);
-      const ballCount = countBall(answer, userInput) - strikeCount;
-
-      if (strikeCount === 3) {
-        MissionUtils.Console.print('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-        const restartChoice = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
-
-        if (restartChoice === "2") {
-          restart = false;
+      try {
+        if (!checkInputValid(userInput)) {
+          throw new Error("잘못된 값을 입력했습니다.");
         }
-        wrongAnswer = false;
-        // console.log(`restart: ${restart}, wrongAnswer: ${wrongAnswer}`);
-        break;
+      } catch (err){
+        MissionUtils.Console.print("Error : " + err.message);
+        return;
       }
 
-      const resultMessage = gameResult(strikeCount, ballCount);
-      MissionUtils.Console.print(resultMessage);
+      // check for correct answer
+      const strikeCount = countStrike(answer, userInput);
+      const ballCount = countBall(answer, userInput) - strikeCount;
+      const isAnswerCorrect = gameResult(strikeCount, ballCount);
+
+      // restart or exit
+      if (isAnswerCorrect) {
+        MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        repeatsRestart = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n') === "1" ? true : false;
+        break;
+      }
     }
   }
 }
