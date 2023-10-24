ea8a3d3fc66445dd2215123a0551df9ec2f95dc9
ChoByungHyun <cbj973@gmail.com>
2023-10-25 04:54:04 +0900

    ✨ Feat: 게임 결과에 따른 메세지 출력 함수 구현

---
+++ src/model/printResult.js
@@ -0,0 +1,23 @@
+import { Console } from "@woowacourse/mission-utils";
+import { BASEBALL } from "../constants/baseBall";
+import { OUTPUT_MESSAGE } from "../constants/message";
+import { ZERO } from "../constants/number";
+
+export default function printResult(data) {
+  const { ball, strike } = data;
+  const format = makeBallFormat(ball) + makeStrikeFormat(strike);
+  if (strike === 3) {
+    Console.print(`${data.strike}${BASEBALL.STRIKE}\n${OUTPUT_MESSAGE.FINISH}`);
+    return true;
+  } else {
+    format === "" ? Console.print(BASEBALL.NOTHING) : Console.print(format);
+    return false;
+  }
+}
+function makeBallFormat(ball) {
+  return ball !== ZERO ? `${ball}${BASEBALL.BALL} ` : "";
+}
+
+function makeStrikeFormat(strike) {
+  return strike !== ZERO ? `${strike}${BASEBALL.STRIKE}` : "";
+}
