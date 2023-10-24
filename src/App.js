83c783981ec2a2cb6ec95111c6cd93a7f339ebd1
btothey99 <btothey99@ewhain.net>
2023-10-25 02:34:20 +0900

    231025 오류 수정 및 함수 분할

--- src/App.js
+++ src/App.js
@@ -1,103 +1,63 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-import { Console } from "@woowacourse/mission-utils";
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
-    function setComputer() {
-      const computer = [];
-      while(computer.length < 3) {
-        const number = MissionUtils.Random.pickNumberInRange(1, 9);
-        if(!computer.includes(number)) {
-          computer.push(number);
-        }
-      }
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    await this.setAnswer();
+  }
 
-      playGame(computer);
-    }
-
-    Console.print("숫자 야구 게임을 시작합니다.");
-    setComputer();
-
-    //맞출 때까지 사용자 입력 받기
-    async function getNum() {
-      try {
-        //new Promise((function(t, n))) 객체를 반환
-        const USER_NUM = await Console.readLineAsync('숫자를 입력해주세요 : ');
-        if(USER_NUM === null) {
-          throw new Error('input error!');
-        } else if(USER_NUM.match(/\D/)) {
-          throw new Error('input error!');
-        } else if(USER_NUM.length != 3) {
-          throw new Error('input error!');
-        } else if(new Set(USER_NUM).size !==3) {
-          throw new Error('input error!');
-        }
-
-        return USER_NUM;
-      } catch (error) {
-        Console.print('[ERROR] 숫자가 잘못된 형식입니다.');
-        return;
+  async setAnswer() {
+    const computer = [];
+    while(computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if(!computer.includes(number)) {
+        computer.push(number);
       }
     }
+    await this.playGame(computer);
+  }
 
-    async function isContinue() {
-      try {
-        const USER_NUM = await Console.readLineAsync('');
-        if(USER_NUM === null) {
-          throw new Error('input error!');
-        } else if(USER_NUM.match(/\D/)) {
-          throw new Error('input error!');
-        } else if(USER_NUM.length != 1) {
-          throw new Error('input error!');
-        }
-        return USER_NUM;
-      } catch (error) {
-        Console.print('[ERROR] 숫자가 잘못된 형식입니다.');
-        return;
-      }
+  async playGame(computer) {
+    const USER_INPUT = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+    let result = '';
+
+    if(USER_INPUT === null || USER_INPUT.match(/\D/) || USER_INPUT.length !== 3 || new Set(USER_INPUT).size !== 3){
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
     }
 
-    function playGame(computer) {
-      getNum().then((USER_NUM) => {
-        let S = 0, B = 0;
-        //Console.print('정답 : ' + computer);
+    const { S, B } = this.calcScore(computer, USER_INPUT);
 
-        computer.forEach((elem, idx) => {
-          if(USER_NUM.indexOf(elem) == idx) S++;
-          else if(USER_NUM.includes(elem)) B++;
-        });
+    if (S !== 0) {
+      if (B !== 0) result = `${B}볼 ${S}스트라이크`;
+      else result = `${S}스트라이크`;
+    } else if (B !== 0) result = `${B}볼`;
+    else result ='낫싱';
 
-        if(S === 3) {
-          Console.print(`${S}스트라이크`);
-          Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-          Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
-          isContinue().then((INPUT_NUM) => {
-            if(INPUT_NUM === "1") setComputer();
-            else if(INPUT_NUM === "2") {
-              Console.print('게임 종료');
-              return;
-            }
-          }).catch(() => {
-            return;
-          });
-
-        } else if (B === 3) {
-          Console.print(`${B}스트라이크`);
-          playGame(computer)
-        } else if (S === 0 && B === 0) {
-          Console.print('낫싱');
-          playGame(computer)
-        } else {
-          Console.print(`${B}볼 ${S}스트라이크`);
-          playGame(computer)
-        }
-      }).catch((error) => {
-        return;
-      });
-      
-    }
+    MissionUtils.Console.print(result);
     
+    if (S === 3) {
+      MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      const num = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'); 
+      if (num === '1') {
+        this.setAnswer();
+      } else if (num === '2') {
+        MissionUtils.Console.print('프로그램 종료');
+      }
+    } else {
+      await this.playGame(computer);
+    }
+  }
+
+  calcScore(computer, input) {
+    let S = 0, B = 0;
+    computer.forEach((elem, idx) => {
+      if(input.indexOf(elem) == idx) S++;
+      else if(input.includes(elem)) B++;
+    });
+    return { S, B };
   }
 }
+
+
 
 export default App;
