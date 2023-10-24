2f92703ffb9d295fe14b4a56ca3aa14b70679a38
eunsung <esk114477@gmail.com>
2023-10-24 09:41:13 +0900

    test: 0체크 testcase 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -57,4 +57,18 @@ describe("숫자 야구 게임", () => {
 
     await expect(app.play()).rejects.toThrow("[ERROR]");
   });
+
+  test("예외 테스트(0 체크)", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["102"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
 });
