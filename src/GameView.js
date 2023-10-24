4ba241346e9139e18a2cf137ef9c04b11af95619
isy <dbswl330@gmail.com>
2023-10-25 02:05:35 +0900

    feat: 3스트라이크 시 콘솔 출력하는 함수 구현
    - 3 스트라이
    - 3개의 숫자를 모두 맞히셨습니다! 게임 종료

--- src/GameView.js
+++ src/GameView.js
@@ -9,4 +9,8 @@ class GameView {
     const inputNumbers = await Console.readLineAsync("숫자를 입력해주세요 : ");
     return inputNumbers;
   }
+  printSuccess() {
+    Console.print("3스트라이크");
+    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  }
 }
