853917f46431cad1f43699cacaad3ce1cf81dbee
Jin Kwan Kim <kkole3897@gmail.com>
2023-10-24 16:01:37 +0900

    refac: 컴퓨터 랜덤 숫자 선택 추가

--- src/computer.js
+++ src/computer.js
@@ -1,3 +1,31 @@
-class Computer {}
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const NUMBER_LENGTH = 3;
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 9;
+
+class Computer {
+  constructor() {
+    this.numbers = [];
+  }
+
+  selectNumbers() {
+    const numbers = [];
+
+    while (numbers.length < NUMBER_LENGTH) {
+      const number = MissionUtils.Random.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+
+      if (!numbers.includes(number)) {
+        numbers.push(number);
+      }
+    }
+
+    this.numbers = [...numbers];
+  }
+
+  getNumbers() {
+    return [...this.numbers];
+  }
+}
 
 export default Computer;
