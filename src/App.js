41344b503e3ff578241274273bf7dfe39e94a9b9
Daeeui Kim <kimeodml@gmail.com>
2023-10-24 22:01:33 +0900

    feat: 숫자야구 랜덤값 추출 함수

--- src/App.js
+++ src/App.js
@@ -5,11 +5,23 @@ class App {
   constructor() {
     this.input = [];
     this.computer = [];
+    this.computerRandomNumber();;
+  }
+
+  computerRandomNumber() {
+    this.computer = [];
+    while (this.computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!this.computer.includes(number)) {
+        this.computer.push(number);
+      }
+    }
   }
 
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
   }
+
 }
 
 const app = new App();
