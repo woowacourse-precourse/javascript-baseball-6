36cdd27f379cfc0ea11da9fdb8d35da2bf4e4c69
aaapark <parksc1534.dev@gmail.com>
2023-10-24 17:04:43 +0900

    check userInputValue validation

--- docs/README.md
+++ docs/README.md
@@ -16,13 +16,18 @@
 - array 형태로 만들기 (Array.from(String(inputNumber),Number);)
 
 3. 사용자의 입력값이 유효한지 확인하는 기능
-
+- 별도 파일 만들기: Validation.js
+- 하위 조건들 각각 만들기: 에러발생시 에러메시지 출력
 3-1. 숫자 길이가 3
 3-2. 숫자가 모두 숫자
-3-3. 숫자가 1~9 사이
+3-3. 숫자가 1~9 사이 (꼭 필요한지 모르겠음!)
 3-4. 숫자가 모두 다른 수
+- class로 내보내기 
+- 유효성 검사 마친 inputNumber값이 array 만드는 곳으로 넘어감
+(try {Validation.gameInputValidation(inputNumber);} catch(e) {throw e} )
 
 4. 컴퓨터가 정한 숫자와 사용자의 숫자를 비교하는 기능
+
 5. 사용자의 숫자에 대한 결과를 출력하는 기능
 6. 게임을 재시작하는 기능
     - 반복문 안에 선언하여 프로그램이 한번에 종료되지 않게 하기
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import {computerRandom} from './calculator/Computer.js'
+import Validation from "./calculator/Validation.js";
 
 class App {
     async play() {
@@ -12,6 +13,12 @@ class App {
       const inputNumber = await Console.readLineAsync('숫자를 입력해주세요 : ').then(
         (value) => value
       )
+      try {
+        Validation.gameInputValidation(inputNumber);
+      } catch(e) {
+        throw e
+      } 
+
       const arrayOfInput = Array.from(String(inputNumber),Number);
       return arrayOfInput;
     } 
