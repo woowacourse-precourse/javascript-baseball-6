38e9dad2e02903e17e98b9a84c4af2a7f6d29879
Hayeon-Lee <papepopepe@naver.com>
2023-10-25 00:19:57 +0900

    feat: convert compareUserandComputer to components

---
+++ src/components/compareComputerAndUser.js
@@ -0,0 +1,17 @@
+const compareComputerAndUser = (computer, user) => {
+  let ball = 0,
+    strike = 0;
+
+  for (let i = 0; i < computer.length; i++) {
+    for (let j = 0; j < user.length; j++) {
+      if (computer[i] === user[j]) {
+        if (i == j) strike += 1;
+        else ball += 1;
+      }
+    }
+  }
+
+  return { ball, strike };
+};
+
+export default compareComputerAndUser;
