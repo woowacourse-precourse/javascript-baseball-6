98792d46895f3385eba4757980ef70fa1bdc3858
yeonkyoung <keep.ykk@gmail.com>
2023-10-25 03:58:52 +0900

    feat: play 메서드 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { GAME_START_MESSAGE } from "./constants/messages.js";
+import { isAllCompleted, isRestartGame } from "./game.js";
+import { playerInput } from "./player.js";
+import { pickNumbers } from "./computer.js";
+import { validation } from "./validation.js";
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print(GAME_START_MESSAGE);
+    let computerPickedNumberArray = pickNumbers();
+
+    while (true) {
+      try {
+        const playerInputString = await playerInput();
+        const playerInputArray = validation(playerInputString);
+
+        if (isAllCompleted(playerInputArray, computerPickedNumberArray)) {
+          const playerChoice = await isRestartGame();
+          if (playerChoice) {
+            computerPickedNumberArray = pickNumbers();
+          } else {
+            return;
+          }
+        }
+      } catch (error) {
+        throw error;
+      }
+    }
+  }
 }
 
 export default App;
