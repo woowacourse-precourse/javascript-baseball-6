3fa45fe9749ccc5d76bcacc7f4acec1e5f42c0d0
hyj5076 <hyj5076@hanmail.net>
2023-10-24 22:50:20 +0900

    refactor(App): 게임 상태 관리 로직 추가 및 수정

--- src/App.js
+++ src/App.js
@@ -12,37 +12,64 @@ class App {
     this.maker = new RandomNumberMaker();
     this.reader = new UserNumberReader();
     this.replayManager = new ReplayManager();
+    this.gameState = 'ready';
   }
 
   async play() {
     try {
-      while (true) {
-        Console.print(Constants.GAME_START); // 게임 시작
-        const uniqueNumber = this.maker.makeRandomNumber();
-  
-        const userNumber = await Console.readLineAsync(); // 사용자 수 읽기
-        if (!correctNumber(userNumber)) {
-          throw new Error("[ERROR]"); // 예외 발생
-        }
+      Console.print(Constants.GAME_START); // 게임 시작
+      this.randomNumber = this.maker.getRandomNumber();
 
-        this.reader.setUserNumber(userNumber);
-        const userAnswer = this.reader.getUserNumber();
-        Console.print(`${Constants.NOTICE_INPUT} : ${userAnswer}`);
-        
-        showResult(uniqueNumber, userAnswer); // 결과 문구
-
-        if (this.isGameOver(uniqueNumber, userAnswer)) { 
-          await this.replayManager.handleReplay(this);
-          if (!replay) {
-            break; // 리플레이 하지 않으면 무한 루프 종료
-          }
-        }
+      while (this.gameState !== 'ended') {
+        await this.nextStep();
       }
     } catch (error) {
       Console.print(Constants.GAME_OVER);
       //Console.print(error.message);
       return Promise.reject(error);
     }
+  }
+
+  async nextStep() {
+    if (this.gameState === 'ready') {
+      await this.getUserInput();
+    } else if (this.gameState === 'playing') {
+      await this.checkAnswer();
+    } else if (this.gameState === 'finished') {
+      await this.handleReplay();
+    }
+  }
+
+  async getUserInput() {
+    const userNumber = await Console.readLineAsync(); // 사용자 수 읽기
+    if (!correctNumber(userNumber)) {
+      throw new Error("[ERROR]"); // 예외 발생
+    }
+
+    this.reader.setUserNumber(userNumber);
+    const userAnswer = this.reader.getUserNumber();
+    Console.print(`${Constants.NOTICE_INPUT} : ${userAnswer}`);
+
+    this.userAnswer = userAnswer;
+    this.gameState = 'playing';
+    await this.nextStep();
+  }
+
+  async checkAnswer() {
+    showResult(this.randomNumber, this.userAnswer); // 결과 문구
+
+    if (this.isGameOver(this.randomNumber, this.userAnswer)) {
+      Console.print(Constants.GOAL);
+      this.gameState = 'finished';
+    } else {
+      this.gameState = 'ready';
+    }
+    await this.nextStep();
+  }
+
+  async handleReplay() {
+    this.gameState = await this.replayManager.handleReplay();
+    await this.nextStep();
   }
 
   isGameOver(computerNumber, playerNumber) {
