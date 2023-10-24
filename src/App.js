48fd17a1c7f2ed68248f3242f389c62aac1528b9
soyunim <togksk123@naver.com>
2023-10-24 16:02:52 +0900

    refactor: App코드 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import { Console } from "@woowacourse/mission-utils";
-import { Random } from "@woowacourse/mission-utils";
-import { Messages } from "./Messages";
+import { gameStart } from "./BaseballGame";
 
-const computers = [];
 class App {
-  play() {
-    // 0. 게임 시작 메세지 출력
-    Console.print(Messages.START);
-
-    // 1. 컴퓨터의 숫자 랜덤입력
-    while (computers.length < 3) {
-      let number = Random.pickNumberInRange(1, 9);
-      if (!computers.includes(number)) {
-        computers.push(number);
-      }
+  async play() {
+    try {
+      await gameStart();
+    } catch (error) {
+      throw new Error("[ERROR]");
     }
-    const COMPUTER = computers.join("");
-    Console.print(COMPUTER);
   }
 }
-
-const app = new App();
-app.play();
 
 export default App;
