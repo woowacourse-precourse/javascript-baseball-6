d55e94cc6a744ca81a52041eb13271d5ee002c48
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-10-24 14:50:25 +0900

    feat: add validation methods

--- src/utils/User.js
+++ src/utils/User.js
@@ -1,21 +1,32 @@
-import { Console } from '@woowacourse/mission-utils';
-import { ErrorMessage, GuideText, SIZE } from '../constant';
+import { Console } from "@woowacourse/mission-utils";
+import {
+  ErrorMessage,
+  GuideText,
+  SIZE } from "../constant";
 
 export default class User {
   async getUserChoice() {
     const USER_INPUT = await Console.readLineAsync(GuideText.USER_INPUT);
-
-    if (!this.checkValidation(USER_INPUT)) {
-      throw new Error(ErrorMessage.INVALID_ANSWER);
-    }
-    
-    return USER_INPUT;
+    return this.checkInputValidation(String(USER_INPUT));
   }
 
-  checkValidation(input) {
-    const STR = String(input);
-    const PATTERN = /^[1-9]{3}$/;
+  checkInputValidation(INPUT) {
+    if (!this.checkInputLength(INPUT)) {
+      throw new Error(ErrorMessage.INVALID_LENGTH);
+    }
 
-    return STR.length === SIZE && PATTERN.test(STR);
+    if (!this.checkInputPattern(INPUT)) {
+      throw new Error(ErrorMessage.INVALID_PATTERN);
+    }
+
+    return Number(INPUT);
+  }
+
+  checkInputLength(INPUT) {
+    return INPUT.length === SIZE;
+  }
+
+  checkInputPattern(INPUT) {
+    return /^[1-9]{3}$/.test(INPUT);
   }
 }
