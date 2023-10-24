9e4e86f79a1bd30a01f58bc4432cd50d53305437
hyj5076 <hyj5076@hanmail.net>
2023-10-24 22:37:22 +0900

    refactor: 게임 재시작 선택 기능 로직 수정

--- src/play/ReplayManager.js
+++ src/play/ReplayManager.js
@@ -2,19 +2,20 @@ import { Console } from "@woowacourse/mission-utils";
 import * as Constants from '../const/Messages';
 
 class ReplayManager {
-    async handleReplay() {
-      Console.print(Constants.SUGGEST_REPLAY);
-      const userChoice = await Console.readLineAsync();
-      if (userChoice === '1') {
-        return true; // 새로운 게임 시작
-      } else if (userChoice === '2') {
-        return false; // 게임 종료
-      } else {
-        Console.print("올바르지 않음");
-        return false;
-      }
+  async handleReplay() {
+    Console.print(Constants.SUGGEST_REPLAY);
+    const userChoice = await Console.readLineAsync();
+    switch (userChoice) {
+      case '1':
+        return 'ready'; // 새로운 게임 시작
+      case '2':
+        return 'ended'; // 게임 종료
+      default:
+        Console.print("올바르지 않은 입력입니다. 게임을 종료합니다.");
+        return 'ended';
     }
   }
+}
 
 export default ReplayManager;
   
