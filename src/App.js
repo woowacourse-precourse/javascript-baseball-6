116fd8e311e16b9b429827afc99b23b1554e62ee
solssak <gswoong1012@gmail.com>
2023-10-24 22:44:44 +0900

    feat: Computer class 생성 후 컴퓨터가 처리하는 로직 분리

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { SETTING, SCORE, MESSAGE } from "./constants/GameConfig.js";
+import { MESSAGE } from "./constants/GameConfig.js";
 import { userInputNumberValidation } from "./utills/InputValidation.js";
+import Computer from "./Computer.js";
 class App {
-  showStartMessage() {
+  #computer;
+
+  constructor() {
+    this.#computer = new Computer();
+  }
+
+  #showStartMessage() {
     MissionUtils.Console.print(MESSAGE.GAME.START);
   }
 
-  setRandomNumber() {
-    const randomNumber = [];
-    while (randomNumber.length < SETTING.INPUT_NUMBER_LENGTH) {
-      let number = MissionUtils.Random.pickNumberInRange(SETTING.MIN_NUMBER, SETTING.MAX_NUMBER);
-      if (!randomNumber.includes(number)) {
-        randomNumber.push(number);
-      }
-    }
-    return randomNumber;
-  }
-
   async setUserInput(randomNumber) {
+    console.log(randomNumber);
     try {
       const inputNumber = await MissionUtils.Console.readLineAsync(MESSAGE.GAME.INPUT_NUMBER);
       const userInputNumber = userInputNumberValidation(inputNumber);
-      const baseBallCount = this.calcBallStrike(userInputNumber, randomNumber);
+      const baseBallCount = this.#computer.calcBallStrike(userInputNumber, randomNumber);
       if (baseBallCount.strike === 3) {
         MissionUtils.Console.print(MESSAGE.GAME.FINISH);
         this.gameRestart();
@@ -33,49 +30,12 @@ class App {
     }
   }
 
-  isStrike(randomNumber, userInputNumber, idx) {
-    return randomNumber.includes(userInputNumber) && randomNumber[idx] === userInputNumber;
-  }
-
-  isBall(randomNumber, userInputNumber, idx) {
-    return randomNumber.includes(userInputNumber) && randomNumber[idx] !== userInputNumber;
-  }
-
-  calcBallStrike(userInputNumber, randomNumber) {
-    const baseBallCount = {
-      strike: 0,
-      ball: 0,
-    };
-
-    userInputNumber.forEach((userInputNumber, idx) => {
-      if (this.isBall(randomNumber, userInputNumber, idx)) {
-        baseBallCount.ball++;
-      } else if (this.isStrike(randomNumber, userInputNumber, idx)) {
-        baseBallCount.strike++;
-      }
-    });
-
-    if (baseBallCount.ball > 0 && baseBallCount.strike > 0) {
-      MissionUtils.Console.print(
-        `${baseBallCount.ball}${SCORE.BALL} ${baseBallCount.strike}${SCORE.STRIKE}`,
-      );
-    } else if (baseBallCount.ball > 0) {
-      MissionUtils.Console.print(`${baseBallCount.ball}${SCORE.BALL}`);
-    } else if (baseBallCount.strike > 0) {
-      MissionUtils.Console.print(`${baseBallCount.strike}${SCORE.STRIKE}`);
-    } else if (baseBallCount.strike === 0 && baseBallCount.ball === 0) {
-      MissionUtils.Console.print(SCORE.NOTHING);
-    }
-
-    return baseBallCount;
-  }
-
   async gameRestart() {
     try {
       const userInput = await MissionUtils.Console.readLineAsync(`${MESSAGE.GAME.RESTART}\n`);
 
       if (userInput === "1") {
-        this.setUserInput(this.setRandomNumber());
+        this.setUserInput(this.#computer.setRandomNumber());
       } else if (userInput === "2") {
         MissionUtils.Console.print(MESSAGE.GAME.END);
       } else {
@@ -86,9 +46,13 @@ class App {
     }
   }
 
+  async gameStart() {
+    this.#showStartMessage();
+    await this.setUserInput(this.#computer.setRandomNumber());
+  }
+
   async play() {
-    this.showStartMessage();
-    await this.setUserInput(this.setRandomNumber());
+    await this.gameStart();
   }
 }
 
