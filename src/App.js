d3540477bc871d0648973c10cf58a11c7863eea6
kim <kajinori@gachon.ac.kr>
2023-10-25 00:53:21 +0900

    feat: finishGame 함수 추가

--- src/App.js
+++ src/App.js
@@ -85,6 +85,10 @@ const gameProcess = async (computerNumber, userNumber) => {
   }
 };
 
+const finishGame = () => {
+  MissionUtils.Console.print('');
+}
+
 const askReplay = async () => {
   MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
   const replay = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
