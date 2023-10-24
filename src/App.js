93aa5afada6f4d6de25af2767d2365e7065200a9
narinn-star <skfls2618@naver.com>
2023-10-24 21:40:53 +0900

    feat : 랜덤 숫자 선택

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+import MESSAGE from '../src/constants/messages.js';
+
 class App {
-  async play() {}
+  async play() {
+    Console.print(MESSAGE.GAME.INPUT);
+    console.log(MESSAGE.GAME.END);
+  }
 }
+
+const game = new App();
+game.play();
 
 export default App;
---
+++ src/constants/messages.js
@@ -0,0 +1,16 @@
+const MESSAGE = {
+  GAME: {
+    START: '숫자 야구 게임을 시작합니다.',
+    INPUT: '숫자를 입력해주세요 : ',
+    RESULT: {
+      BALL: '볼',
+      STRIKE: '스트라이크',
+      NOTHING: '낫싱',
+      SUCCESS: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+    },
+    END: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. \n',
+  },
+  ERROR: '[ERROR]',
+};
+
+export default MESSAGE;
