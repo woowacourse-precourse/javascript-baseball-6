2b6d5a3dfaa277fb0203e8a8840b76fa04fe388e
MinGu Jeong <mingu830@gmail.com>
2023-10-24 21:37:45 +0900

    refactor: 테스트 조건에 맞도록 수정

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+let replayFlag = true;
 class App {
   async play() {
-    try {
-      MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-      compareNumber();
-    } catch (error) {
-      MissionUtils.Console.print("프로그램을 종료합니다.");
-      return;
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    while (replayFlag) {
+      await compareNumber();
     }
   }
 }
+
+// const a = new App();
+// a.play();
 
 async function getUserNumber() {
-  try {
-    const userNumber = await MissionUtils.Console.readLineAsync(
-      "숫자를 입력해주세요 : "
-    );
-    if (userNumber.length !== 3) {
-      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
-    }
-    return userNumber;
-  } catch (error) {
-    MissionUtils.Console.print("[ERROR] 숫자가 잘못된 형식입니다.", error);
-    throw error;
+  const userNumber = await MissionUtils.Console.readLineAsync(
+    "숫자를 입력해주세요 : "
+  );
+  if (userNumber.length !== 3) {
+    throw new Error("[ERROR] 숫자는 3자리여야 합니다.");
+  } else if (userNumber.includes("0")) {
+    throw new Error("[ERROR] 숫자에 0이 포함되어 있습니다.");
+  } else if (userNumber.split("").some((num) => isNaN(num))) {
+    throw new Error("[ERROR] 숫자가 아닌 값이 포함되어 있습니다.");
+  } else if (
+    userNumber.split("").some((num, index, arr) => arr.indexOf(num) !== index)
+  ) {
+    throw new Error("[ERROR] 숫자가 중복되어 있습니다.");
+  } else if (userNumber.includes(" ")) {
+    throw new Error("[ERROR] 숫자에 공백이 포함되어 있습니다.");
   }
+  return userNumber;
 }
 
-function getComputerNumber() {
-  const uniqueNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-  const computerNumber = uniqueNumber.join("");
-  return computerNumber;
+async function getComputerNumber() {
+  const computerNumber = [];
+  while (computerNumber.length < 3) {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computerNumber.includes(randomNumber)) {
+      computerNumber.push(randomNumber);
+    }
+  }
+  console.log(computerNumber);
+  return computerNumber.join("");
 }
 
 async function compareNumber() {
   let strike = 0;
-  const computerNumber = getComputerNumber();
+  const computerNumber = await getComputerNumber();
 
   while (strike !== 3) {
     const userNumber = await getUserNumber();
@@ -54,7 +66,8 @@ async function compareNumber() {
 
     if (strike === 3) {
       MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-      isReplay();
+      await isReplay();
+      break;
     }
   }
 }
@@ -62,26 +75,26 @@ async function compareNumber() {
 function printHint(ball, strike) {
   if (ball === 0 && strike === 0) {
     MissionUtils.Console.print("낫싱");
+  } else if (ball === 0 && strike !== 0) {
+    MissionUtils.Console.print(`${strike}스트라이크`);
+  } else if (ball !== 0 && strike === 0) {
+    MissionUtils.Console.print(`${ball}볼`);
   } else {
     MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
   }
 }
 
 async function isReplay() {
-  try {
-    const isReplay = await MissionUtils.Console.readLineAsync(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
-    );
+  const answer = await MissionUtils.Console.readLineAsync(
+    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+  );
 
-    if (isReplay === "1") {
-      compareNumber();
-    } else if (isReplay === "2") {
-      MissionUtils.Console.print("게임을 종료합니다.");
-    } else {
-      MissionUtils.Console.print("[ERROR] 숫자가 잘못된 형식입니다.");
-    }
-  } catch (error) {
-    MissionUtils.Console.print("[ERROR] 숫자가 잘못된 형식입니다.", error);
+  if (answer === "1") {
+    replayFlag = true;
+  } else if (answer === "2") {
+    replayFlag = false;
+  } else {
+    throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
   }
 }
 
