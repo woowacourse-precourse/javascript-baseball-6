f39e8dedc1f2d43f88fd245c803d473de3e8f24f
cowhead9805 <cowhead9805@gmail.com>
2023-10-25 00:10:53 +0900

    난수 생성 컴퓨터 수정

--- src/computer.js
+++ src/computer.js
@@ -3,5 +3,3 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 export function generateRandomNumber() {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
 }
-
-console.log(generateRandomNumber());
