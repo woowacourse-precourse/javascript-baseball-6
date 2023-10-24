7796aa066feadbb688c7012ef98f1a43e4cb522a
JiYoon Lee <wldbseja@daum.net>
2023-10-24 16:10:20 +0900

    feat : ✨ 랜덤 숫자  값 생성 기능

--- src/App.js
+++ src/App.js
@@ -6,13 +6,27 @@ import {
 } from './constants.js'
 
 class App {
+  #computer
 
   printGameStart() {
     Console.print(PRINT_STRING.GAME_START)
   }
 
+  generateRandomNumbers() {
+    this.#computer = [];
+    while (this.#computer.length < NUM) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!this.#computer.includes(number)) {
+        this.#computer.push(number);
+      }
+    }
+  }
+
   async play() {
-    this.printGameStart()
+    if (!this.#computer) {
+      this.printGameStart();
+    }
+    this.generateRandomNumbers()
   }
 }
 
