f138945daf25b9ef1c56cf53a6cd331c90714d30
kim <kajinori@gachon.ac.kr>
2023-10-24 18:35:52 +0900

    feat: validateInput 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
-  async play() {}
   async play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
@@ -17,4 +16,18 @@ const generateRandomNumber = () => {
   }
   return computer.join('');
 }
+
+const validateInput = (userInput) => {
+  if (userInput.length !== 3) {
+    throw new Error('[ERROR] 3자리의 숫자를 입력해주세요.');
+  }
+  if (isNaN(Number(userInput))) {
+    throw new Error('[ERROR] 숫자만 입력할 수 있습니다.');
+  }
+  if (userInput[0] === userInput[1] || userInput[1] === userInput[2] || userInput[2] === userInput[0]) {
+    throw new Error('[ERROR] 모두 다른 숫자를 입력해주세요.');
+  }
+  return true;
+};
+
 export default App;
