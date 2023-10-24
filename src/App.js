45bc88a4f67d0ed0e58c88efc07e5961710ff4f6
ChoByungHyun <cbj973@gmail.com>
2023-10-25 04:56:04 +0900

    ✨ Feat: 게임 루프 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import getInputAndCheck from "./model/getInputAndCheck.js";
+import printResult from "./model/printResult.js";
+import restartGame from "./model/restartGame.js";
+import ballCount from "./util/ballCount.js";
+import ballNumberMaker from "./util/ballNumberMaker.js";
+import ConsoleView from "./util/ConsoleControll.js";
+
 class App {
-  async play() {}
+  async play() {
+    let computerNumbers = ballNumberMaker();
+    let userNumbers;
+    while (true) {
+      userNumbers = await getInputAndCheck();
+
+      if (printResult(ballCount(userNumbers, computerNumbers))) {
+        const RESTART = await restartGame();
+        if (RESTART) {
+          computerNumbers = ballNumberMaker();
+        } else {
+          break;
+        }
+      }
+    }
+  }
 }
 
 export default App;
+
+ConsoleView.printStart();
+
+// const app = new App();
+// app.play();
