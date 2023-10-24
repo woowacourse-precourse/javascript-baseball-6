db0b795283c2f5ff79bf62d1a010b606f2005a96
23YUJIN <asdbnm066@daum.net>
2023-10-25 05:09:54 +0900

    feat: 종료 여부 입력 받기 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    let computer = this.generateRandomNumber();
-    let human = await this.getUserNumberInput();
-
-    /* 기능 test용(나중에 지울 것) */
-    console.log(computer);
-    console.log(human);
-    /* ========================= */
-
-    while (this.compareNumberArray(human, computer) === false) {
-      human = await this.getUserNumberInput();
+    let shouldExit = false;
+    while (shouldExit === false) {
+      let computer = this.generateRandomNumber();
+      let human = await this.getUserNumberInput();
+      /* 기능 test용(나중에 지울 것) */
+      console.log(computer);
+      console.log(human);
+      /* ========================= */
+      while (this.compareNumberArray(human, computer) === false) {
+        human = await this.getUserNumberInput();
+      }
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      shouldExit = await this.getUserExitInput();
     }
-    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
   }
 
   generateRandomNumber() {
@@ -36,6 +38,24 @@ class App {
       );
       const numberArray = Array.from(userNumberInput).map(Number);
       return numberArray;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getUserExitInput() {
+    try {
+      let userEixtInput = " ";
+      while (userEixtInput !== "1" && userEixtInput !== "2") {
+        userEixtInput = await MissionUtils.Console.readLineAsync(
+          "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+        );
+      }
+      if (userEixtInput === "1") {
+        return false;
+      } else {
+        return true;
+      }
     } catch (error) {
       console.log(error);
     }
