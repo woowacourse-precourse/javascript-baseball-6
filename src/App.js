35085262a28ed273d9ace2aa2c576020e1ad1635
osohyun0224 <osohyun0224@naver.com>
2023-10-25 04:45:55 +0900

    Feat: (상대방)의 랜덤 숫자 생성 기능 구현
    feat:
    - 상대방이 생성하는 랜덤 숫자 기능을 구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Console} from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 export default class App {
   constructor() {
@@ -8,8 +8,39 @@ export default class App {
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
     while (this.isReplaying) {
-      
+      await this.compareNumbers();
     }
+  }
+
+  async getUserNumber() {
+    Console.print("숫자를 입력해주세요 : ");
+    const playerInput = await Console.readLineAsync("");
+
+    if (playerInput.length !== 3) {
+      throw new Error("[ERROR] 숫자는 3자리여야 합니다.");
+    } else if (playerInput.includes("0")) {
+      throw new Error("[ERROR] 숫자에 0이 포함되어 있습니다.");
+    } else if (playerInput.split("").some((num) => isNaN(num))) {
+      throw new Error("[ERROR] 숫자가 아닌 값이 포함되어 있습니다.");
+    } else if (
+      playerInput.split("").some((num, index, arr) => arr.indexOf(num) !== index)
+    ) {
+      throw new Error("[ERROR] 숫자가 중복되어 있습니다.");
+    } else if (playerInput.includes(" ")) {
+      throw new Error("[ERROR] 숫자에 공백이 포함되어 있습니다.");
+    }
+    return playerInput;
+  }
+
+  async getComputerNumber() {
+    const machineGeneratedNumber = [];
+    while (machineGeneratedNumber.length < 3) {
+      const randomDigit = Random.pickNumberInRange(1, 9);
+      if (!machineGeneratedNumber.includes(randomDigit)) {
+        machineGeneratedNumber.push(randomDigit);
+      }
+    }
+    return machineGeneratedNumber.join("");
   }
 }
 
