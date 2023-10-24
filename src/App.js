f5d73e4b259e86a1c1c8d3721141d57eb5c9da7d
Junseo <monstazo911@gmail.com>
2023-10-24 16:07:23 +0900

    Refactor: App 클래스 속성, 메소드 추가 및 수정
    ES2022부터 #을 통해 외부에서 접근 불가능한 private 속성과 메소드를 정의할 수 있다고 하여서 >외부에서 읽기, 쓰기 불가능하도록 secretNumber를 private으로 정의하였습니다.
    
    strike와 ball은 클래스 내부에서 유지되어야한다고 생각하여 속성에 추가하였고 이 또한 외부에서
     수정 불가하게 하기위해 private으로 정의하였습니다.
    하지만 사용자가 현재 몇 스트라이크, 몇 볼인지 항상 확인할 수 있는 것이 좋다고 생각하여서 pri
    ntCompareResult 메소드는 public으로 정의하였습니다.
    
    게임을 시작하는 play 메소드를 제외한 나머지 메소드들은 모두 클래스 내부에서만 사용하므로 private으로 정의했습니다.
    
    또한 클래스 인스턴스 생성 시, 외부에서 할당해줄 속성값이 없고, 자바스크립트 코드스타일 9.5에
     따르면 빈 생성자는 필요없으므로 생성자를 삭제했습니다.

--- src/App.js
+++ src/App.js
@@ -2,57 +2,54 @@ import { Random, Console } from "@woowacourse/mission-utils";
 
 // App 클래스를 사용자가 숫자 야구 게임을 할 수 있는 컴퓨터라고 생각하고 구현
 class App {
-  constructor() {
-    this.secretNumber = [null, null, null];
-  }
+  #secretNumber = [null, null, null];
+  #strike = 0;
+  #ball = 0;
 
-  static printGreeting() {
+  static #printGreeting() {
     Console.print("숫자 야구 게임을 시작합니다.");
   }
 
-  generateSecretNumber() {
-    while (this.secretNumber.length < 3) {
+  #generateSecretNumber() {
+    while (this.#secretNumber.length < 3) {
       const number = Random.pickNumberInRange(1, 9);
-      if (!this.secretNumber.includes(number)) {
-        this.secretNumber.push(number);
+      if (!this.#secretNumber.includes(number)) {
+        this.#secretNumber.push(number);
       }
     }
   }
 
   // 사용자에게 3자리 숫자를 입력받은 후, 각각의 숫자를 요소로 가지는 배열로 변환 후 반환
-  static async inputGuessNumber() {
-    const userInput = await Console.readLineAsync("숫자를 입력해주세요 : ");
+  static async #inputGuessNumber() {
+    const userInput = Console.readLineAsync("숫자를 입력해주세요 : ");
     const userInputArray = userInput.split("").map(Number);
     return userInputArray;
   }
 
-  static printCompareResult(strike, ball) {
+  printCompareResult() {
     let result = "";
-    result += strike !== 0 ? `${strike}스트라이크 ` : "";
-    result += ball !== 0 ? `${ball}볼` : "";
+    result += this.#strike !== 0 ? `${this.#strike}스트라이크 ` : "";
+    result += this.#ball !== 0 ? `${this.#ball}볼` : "";
     Console.print(result);
   }
 
-  compareNumber(guessNumber) {
-    let strike = 0;
-    let ball = 0;
+  #compareNumber(guessNumber) {
+    this.#strike = 0;
+    this.#ball = 0;
     for (let i = 0; i < 3; i++) {
-      if (guessNumber[i] === this.secretNumber[i]) {
-        strike += 1;
+      if (guessNumber[i] === this.#secretNumber[i]) {
+        this.#strike += 1;
         continue;
       }
-      if (this.secretNumber.includes(guessNumber[i])) ball += 1;
+      if (this.#secretNumber.includes(guessNumber[i])) this.#ball += 1;
     }
-    App.printCompareResult(strike, ball);
-
-    // 모두 맞혔는지 확인하기 위해서는 strike의 개수만 필요하므로 strike만 반환
-    return strike;
+    App.printCompareResult();
   }
 
-  playBaseBall() {
-    this.generateSecretNumber();
-    const guessNumber = App.inputGuessNumber();
-    this.compareNumber(guessNumber);
+  #playBaseBall() {
+    this.#generateSecretNumber();
+    const guessNumber = App.#inputGuessNumber();
+    this.#compareNumber(guessNumber);
   }
 
   play() {
