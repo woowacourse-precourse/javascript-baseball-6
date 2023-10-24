0ea5527d7a76893814ab7a7588ff34a1043b666b
Jinnmidnnight <vkstk0131@yonsei.ac.kr>
2023-10-24 18:01:33 +0900

    feat(InitialStage): 게임시작 메세지 출력 기능 수정 * chore: .eslintrc.yml rules 추가 * import/export 문법 수정

--- src/InitialStage.js
+++ src/InitialStage.js
@@ -1,11 +1,16 @@
-import { Console } from ('@woowacourse/mission-utils');
-import { Messages } from './Messages.js';
+import { Console } from "@woowacourse/mission-utils";
+import MESSAGES from "./Messages.js";
 
 /**
  * 초기 단계(사용자 입력 직전까지)를 담당하는 클래스
  */
 class InitialStage {
+  /**
+   * 게임 시작 메시지를 출력한다.
+   */
   messagePrint() {
-    Console.print(Messages.GAME_START);
-  };
+    Console.print(MESSAGES.GAME_START);
+  }
 }
+
+export default InitialStage;
