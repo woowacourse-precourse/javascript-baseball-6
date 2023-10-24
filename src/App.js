ab30a625d0e6fbd527fc18e8d76c3d3c928c67ac
dongminlee <gmgmgun@gmail.com>
2023-10-25 00:39:58 +0900

    modify: depth, 변수명 수정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import {MissionUtils} from "@woowacourse/mission-utils";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    function getNum() {
+    function getRanNum() {
       const computer = [];
+
       while (computer.length < 3) {
         const number = MissionUtils.Random.pickNumberInRange(1, 9);
         if (!computer.includes(number)) {
           computer.push(number);
         }
       }
+
       return computer;
     }
 
-    async function cal(computer) {
+    async function process(computer) {
       let answer = "";
+      let strike = 0;
+      let ball = 0;
+
       let number = await MissionUtils.Console.readLineAsync(
         "숫자를 입력해주세요."
       );
@@ -23,16 +28,11 @@ class App {
         throw new Error("[ERROR]");
       }
 
-      let strike = 0;
-      let ball = 0;
       for (let i = 0; i < computer.length; i++) {
         if (computer[i] === Number(number[i])) strike++;
-        else if (
-          computer[i] !== Number(number[i]) &&
-          computer.includes(Number(number[i]))
-        )
-          ball++;
+        else if (computer.includes(Number(number[i]))) ball++;
       }
+
       if (!ball && !strike) answer = "낫싱";
       if (ball && strike) answer = `${ball}볼 ${strike}스트라이크`;
       if (ball && !strike) answer = a`${ball}볼`;
@@ -40,28 +40,25 @@ class App {
       return answer;
     }
 
-    async function refresh() {
-      let computer = getNum();
+    async function restart() {
+      let computer = getRanNum();
       let answer = "";
+
       while (answer !== "3스트라이크") {
-        answer = await cal(computer);
+        answer = await process(computer);
         await MissionUtils.Console.print(answer);
       }
-      let ot = await MissionUtils.Console.readLineAsync(
+
+      let finish = await MissionUtils.Console.readLineAsync(
         "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
       );
-      if (ot === "1") {
-        await refresh();
-      } else if (ot === "2") {
-        await MissionUtils.Console.print("게임 종료");
-      }
+
+      finish === "1"
+        ? await restart()
+        : await MissionUtils.Console.print("게임 종료");
     }
 
-    async function foo() {
-      await refresh();
-    }
-
-    await foo();
+    await restart();
   }
 }
 
