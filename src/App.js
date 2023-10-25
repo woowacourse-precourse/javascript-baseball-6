051cc420a89b8c7f35635206ab2a489e20f68df3
theNum1Developer <thenum1developer@gamil.com>
2023-10-25 10:20:25 +0900

    feat: (결과 출력) 포맷에 맞게 결과 출력 구현

--- src/App.js
+++ src/App.js
@@ -77,6 +77,15 @@ class App {
 
       return this.formatResult(strikes, balls);
     }
+
+    
+    formatResult(strikes, balls) {
+      if (strikes === 0 && balls === 0) return "낫싱";
+      if (strikes === 3) return "3스트라이크";
+      if (balls === 0) return `${strikes}스트라이크`;
+      if (strikes === 0) return `${balls}볼`;
+      return `${balls}볼 ${strikes}스트라이크`;
+  }
 }
 
 export default App;
