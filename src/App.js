c06a745a2cfb59c7aa97a891a389e54c02878c20
KimDoToRi <60084235+KimKyungYun@users.noreply.github.com>
2023-10-25 09:19:39 +0900

    docs: 설명 주석 추가

--- src/App.js
+++ src/App.js
@@ -14,19 +14,20 @@ class App {
 		while (true) {
 			const input = await Console.readLineAsync(MESSAGE.pitch);
 			const userNumber = input.split('').map((num) => parseInt(num));
+			// 입력값 유효성 검사
 			checkIsValid(userNumber);
 
 			const { ballCount, strikeCount } = getPitchingCount(
 				this.computerNumber,
 				userNumber
 			);
-
+			// 볼,스트라이크 카운트에 따라 값을 출력
 			printPitchingCount(ballCount, strikeCount);
 
 			if (strikeCount === INPUT_LENGTH) {
 				Console.print(MESSAGE.end);
 				const choice = await Console.readLineAsync(MESSAGE.restart);
-
+				// 재시작 유무 입력값 유효성 검사
 				checkIsReplay(choice);
 
 				if (choice !== RESTART) {
