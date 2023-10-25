a4aa4518c9dc18e34f5defa4d7df1e569b9b533f
박도륜 <88328436+ryxxn@users.noreply.github.com>
2023-10-25 17:02:00 +0900

    [Add] CaculateResult 함수 구현

--- src/App.js
+++ src/App.js
@@ -20,6 +20,33 @@ class App {
     }
     return numbers;
   }
+  
+  /**
+   * 사용자 입력값과 컴퓨터 값을 계산하여 결과를 반환하는 함수
+   * @param {string} userInput 
+   * @param {number[]} computerNumbers 
+   * @returns {string}
+   */
+  calculateResult = (userInput, computerNumbers) => {
+    let strikes = 0;
+    let balls = 0;
+
+    const userInputList = userInput.split("").map(e => parseInt(e));
+
+    userInputList.forEach((userNum, i) => {
+      if (userNum == computerNumbers[i]) {
+        strikes++;
+        return;
+      }
+      if (computerNumbers.includes(userNum)) {
+        balls++;
+        return;
+      }
+    })
+
+    return this.getResultFromScore(strikes, balls)
+  }
+
 
 }
 
