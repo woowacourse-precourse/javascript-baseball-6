cd5d574436be07e4d687d23215d40a7e04a5b66a
Jinhyung Lee <luciancah@gmail.com>
2023-10-25 15:49:02 +0900

    feat: 숫자야구 로직 관련 함수 추가
    - gameLogic.js 파일에 위치
    - 검증된 컴퓨터 난수 / 사용자 입력 strike, ball 카운트 반환하는 countBaseballCounts() 추가
    - 사용자 입력에 따라 App Class 지속 중지 여부 결정하는 askToContinue() 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { printMessage } from "./utils/messages.js";
+import { generateComputerNumber, getUserInput } from "./inputValidation.js";
+import { countBaseballCounts, askToContinue } from "./gameLogic.js";
+
 class App {
   async play() {
     printMessage("숫자 야구 게임을 시작합니다.");
@@ -6,6 +9,16 @@ class App {
     while (true) {
       const computerNumber = generateComputerNumber();
       let result = { strike: 0, ball: 0 };
+
+      while (result.strike !== 3) {
+        const userInputNumber = await getUserInput();
+        result = countBaseballCounts(computerNumber, userInputNumber);
+      }
+
+      const isRestarted = await askToContinue();
+      if (!isRestarted) {
+        break;
+      }
     }
   }
 }
