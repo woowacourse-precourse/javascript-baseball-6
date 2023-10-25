2fa05cc17c8e3f832d2a0839a78e66edbac70879
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-10-25 14:29:56 +0900

    FEAT:서로 다른 임의의 수 3 개 로 구성된 정답 생성 기능 추가
    * Computer 클래스 추가
       - 게임 내 상대방의 역할
       - 데이터 은닉 기능이 필요하기 때문에 클래스 로 생성
    * #answer 속성 추가
    * #makeAnswer 메서드 추가
       - 라이브러리를 이용해 서로 다른 임의의 수 를 생성 후 #answer 에 삽입

---
+++ src/features/Computer.js
@@ -0,0 +1,14 @@
+import { Random } from "@woowacourse/mission-utils";
+
+export class Computer {
+  #answer = [];
+
+  makeAnswer() {
+    while (this.#answer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!this.#answer.includes(number)) {
+        this.#answer.push(number);
+      }
+    }
+  }
+}
