9476d7a5504343ee27ee824225788a1173be60d9
psst54 <psst5491@naver.com>
2023-10-25 17:02:34 +0900

    🔥 remove debug log

--- src/input/getUserInput.js
+++ src/input/getUserInput.js
@@ -4,7 +4,6 @@ export default async function getUserInput() {
   const input = await MissionUtils.Console.readLineAsync(
     "숫자를 입력해주세요 : "
   );
-  //   console.log("[input]", input);
 
   return input;
 }
