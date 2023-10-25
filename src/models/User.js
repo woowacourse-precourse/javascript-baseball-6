c788776d32fc14baadea223290ee9922f1a230e4
Jinnmidnnight <vkstk0131@yonsei.ac.kr>
2023-10-25 22:57:03 +0900

    feat(models): User모델에 재시작 여부 필드 및 관련 함수 추가 * setter, getter

--- src/models/User.js
+++ src/models/User.js
@@ -5,6 +5,9 @@ class User {
   /** @type {number} */
   #number;
 
+  /** @type {boolean} */
+  #isRestart;
+
   /**
    * 사용자의 입력 숫자를 저장한다.
    * @param {string} numberString
@@ -19,6 +22,20 @@ class User {
    */
   getNumber() {
     return this.#number;
+  }
+
+  /**
+   * 사용자가 재시작을 원하는지 여부를 저장한다.
+   */
+  setRestart(isRestart) {
+    this.#isRestart = isRestart;
+  }
+
+  /**
+   * 사용자가 재시작을 원하는지 여부를 반환한다.
+   */
+  getRestart() {
+    return this.#isRestart;
   }
 }
 
