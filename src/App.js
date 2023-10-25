2e302d0a3f7845ef79d2c10b19d1b4148d013b48
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-10-25 22:52:52 +0900

    feature: 게임 플레이 시 함수 작동되도록 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { STRINGS } from "./constants/strings";
+import { createRandomNumber } from "./createRandomNumber";
+import { getInputNumbers } from "./getInputNumbers";
+import { Judge } from "./judge";
+import { replay } from "./replay";
+
 class App {
-  async play() {}
-}
+	async playGame() {
+		MissionUtils.Console.print(STRINGS.GAME_START);
+		let restart = false;
+
+		do {
+			const answerNumbers = createRandomNumber();
+			let userNumbers;
+			let result;
+
+			do {
+				userNumbers = await getInputNumbers();
+
+				result = await Judge(answerNumbers, userNumbers);
+			} while (!result);
+
+			const replayChoice = await replay();
+			restart = replayChoice === "1";
+		} while (restart);
+		return restart;
+	}
 
 export default App;
