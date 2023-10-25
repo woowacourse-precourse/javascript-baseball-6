5937610bb8c772a0153ffb00ad33428b23ee397b
ryan-g00 <haeryang0126@naver.com>
2023-10-25 17:54:26 +0900

    if문 변경

--- src/Playgame.js
+++ src/Playgame.js
@@ -32,15 +32,12 @@ class Playgame {
             }
         };
     
-        if (strike > 0 && ball > 0) {
-            return `${ball}볼 ${strike}스트라이크`;
-        } else if (strike > 0) {
-            return `${strike}스트라이크`;
-        } else if (ball > 0) {
-            return `${ball}볼`;
-        } else {
-            return '낫싱';
-        }
+        if (strike === 0 && ball === 0) return '낫싱';
+        if (strike === 0 && ball !== 0) return `${ball}볼`;
+        if (strike !== 0 && ball === 0) return `${strike}스트라이크`;
+        if (strike === 3) return '3스트라이크';
+
+        return `${ball}볼 ${strike}스트라이크`;
 
     }
 }
