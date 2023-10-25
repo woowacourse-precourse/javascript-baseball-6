1bb32bc8114717df8fdbe893d0bc2bd04e548ba7
jayming66 <mjw8941@gmail.com>
2023-10-25 19:15:46 +0900

    feat : 숫자야구 게임 3스트라이크 맞힐때까지 while 반복

--- src/App.js
+++ src/App.js
@@ -39,32 +39,33 @@ class App {
     let randaomNumbers = randomNumber(); // 컴퓨터의 숫자 3개 생성
     Console.print(randaomNumbers);
 
-    let userInputs = await Console.readLineAsync("숫자를 입력해주세요 : "); // 쿼리를 보내야하고, await 해줘야한다.
-    userInputs = userInputs.split("").map((element) => parseInt(element, 10)); // input을 int형으로 바꾼다.
+    while (gameResult !== 3) {
+      let userInputs = await Console.readLineAsync("숫자를 입력해주세요 : "); // 쿼리를 보내야하고, await 해줘야한다.
+      userInputs = userInputs.split("").map((element) => parseInt(element, 10)); // input을 int형으로 바꾼다.
 
-    if (userInputs.length !== 3) throw new Error("3 Input require"); // 3개의 input인지 검증
-    const userInputSet = new Set(userInputs);
+      if (userInputs.length !== 3) throw new Error("3 Input require"); // 3개의 input인지 검증
+      const userInputSet = new Set(userInputs);
 
-    if (userInputSet.size !== 3) {
-      throw new Error("duplicated input");
-    } // 중복된 숫자가 없는지 검증
+      if (userInputSet.size !== 3) {
+        throw new Error("duplicated input");
+      } // 중복된 숫자가 없는지 검증
 
-    const [ball, strike] = getGameResult(randaomNumbers, userInputs);
-    gameResult = strike;
+      const [ball, strike] = getGameResult(randaomNumbers, userInputs);
+      gameResult = strike;
 
-    if (ball === 0 && strike === 0) {
-      Console.print("낫싱");
+      if (ball === 0 && strike === 0) {
+        Console.print("낫싱");
+      }
+      if (ball > 0 && strike === 0) {
+        Console.print(`${ball}볼`);
+      }
+      if (ball === 0 && strike > 0) {
+        Console.print(`${strike}스트라이크`);
+      }
+      if (ball > 0 && strike > 0) {
+        Console.print(`${ball}볼 ${strike}스트라이크`);
+      }
     }
-    if (ball > 0 && strike === 0) {
-      Console.print(`${ball}볼`);
-    }
-    if (ball === 0 && strike > 0) {
-      Console.print(`${strike}스트라이크`);
-    }
-    if (ball > 0 && strike > 0) {
-      Console.print(`${ball}볼 ${strike}스트라이크`);
-    }
-
     Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
   }
 }
