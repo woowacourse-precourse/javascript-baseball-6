a9caf3cefdd4b60f60178a6464579de082390f77
Byunggwon Park <82306066+startartart@users.noreply.github.com>
2023-10-25 18:32:38 +0900

    [Refactor] 클래스 메서드 수정 및 코드 리팩토링
    * refactor: 버그 수정 및 메서드 변경
    jest throw 버그 수정
    
    * docs: 야구게임 요구사항 완료

--- src/App.js
+++ src/App.js
@@ -1,124 +1,93 @@
-import { MissionUtils, Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 
 const REG_INPUT_NUMBER = /^(?!.*([1-9]).*\1)[1-9]{3}$/;
+const GAME_RESTART_NUMBER = 1;
+const GAME_EXIT__NUMBER = 2;
 
 class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
-    const computerNumber = CreateComputerNumber(3);
-    this.turn(computerNumber);
+    const computerNumber = this.CreateComputerNumber(3);
+    return this.calculateResults(computerNumber);
   }
 
-  async start() {
-    const computerNumber = CreateComputerNumber(3);
-    this.turn(computerNumber);
-  }
+  async inputUserNumber() {
+    const inputValue = await Console.readLineAsync('숫자를 입력해주세요 : ');
 
-  async turn(computerNumber) {
-    const number = await InputNumber();
-    const restart = await CompareNumber(computerNumber, number);
-    switch (restart) {
-      case 0:
-        this.turn(computerNumber);
-        break;
-      case 1:
-        this.start();
-        break;
-      case 2:
-        break;
-      default:
-        break;
+    if (!REG_INPUT_NUMBER.test(inputValue)) {
+      throw new Error('[ERROR] Input Value is Incorrect.');
+    } else {
+      return Array.from(inputValue).map(Number);
     }
   }
-}
 
-function PrintOutput(ball, strike) {
-  let output = '';
-  if (strike === 3) {
-    return ['3스트라이크', 1];
-  } else if (strike === 0 && ball === 0) {
-    return ['낫싱', 0];
+  CreateComputerNumber(digit) {
+    const number = [...Array(digit)].reduce((result) => {
+      let randomDigit;
+      do {
+        randomDigit = Random.pickNumberInRange(1, 9);
+      } while (result.includes(randomDigit));
+
+      result.push(randomDigit);
+      return result;
+    }, []);
+    return number;
   }
-  if (ball !== 0) output += `${ball}볼 `;
-  if (strike !== 0) output += `${strike}스트라이크`;
 
-  return [output, 0];
-}
+  async calculateResults(computerNumber) {
+    const myNumber = await this.inputUserNumber();
 
-function CheckString(input) {
-  if (!REG_INPUT_NUMBER.test(input))
-    throw new Error('[ERROR] Input Value is Incorrect.');
-}
+    let ball = 0;
+    let strike = 0;
+    let comDigits = 0;
 
-function CheckRestartInput(input) {
-  if (input === '1') return 1;
-  if (input === '2') return 2;
-
-  throw new Error(
-    '[ERROR] Input Value is Incorrect. You have to choose 1 or 2.'
-  );
-}
-
-async function InputNumber() {
-  try {
-    const yourNumber = await Console.readLineAsync('숫자를 입력해주세요 : ');
-    CheckString(yourNumber);
-    return yourNumber;
-  } catch (error) {
-    throw new Error(error, '[ERROR] readLineAsync Promise Rejected.');
-  }
-}
-
-function CreateComputerNumber(digit) {
-  const number = [...Array(digit)].reduce((result) => {
-    let randomDigit;
-    do {
-      randomDigit = MissionUtils.Random.pickNumberInRange(1, 9);
-    } while (result.includes(randomDigit));
-
-    result.push(randomDigit);
-    return result;
-  }, []);
-  return number;
-}
-
-function CompareNumber(computerNumber, myNumber) {
-  const myArrayNumber = myNumber.split('').map(Number);
-
-  let ball = 0;
-  let strike = 0;
-  let comDigits = 0;
-  computerNumber.forEach((comNum) => {
-    let myDigits = 0;
-    myArrayNumber.forEach((myNum) => {
-      if (comNum === myNum) {
-        if (comDigits === myDigits) strike++;
-        else ball++;
-      }
-      myDigits++;
+    computerNumber.forEach((comNum) => {
+      let myDigits = 0;
+      myNumber.forEach((myNum) => {
+        if (comNum === myNum) {
+          if (comDigits === myDigits) strike++;
+          else ball++;
+        }
+        myDigits++;
+      });
+      comDigits++;
     });
-    comDigits++;
-  });
-  const [result, isEnd] = PrintOutput(ball, strike);
-  Console.print(result);
-  return isRestartGame(isEnd);
-}
 
-async function isRestartGame(isEnd) {
-  if (!isEnd) return 0;
-  Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-  try {
-    const choice = await Console.readLineAsync(
-      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
+    const [result, isEnd] = this.PrintOutput(ball, strike);
+    Console.print(result);
+
+    if (!isEnd) {
+      return this.calculateResults(computerNumber);
+    }
+    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    const value = await Console.readLineAsync(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
     );
-    const restart = CheckRestartInput(choice);
-    return restart;
-  } catch (error) {
-    throw new Error(error, '[ERROR] readLineAsync Promise Rejected.');
+    switch (Number(value)) {
+      case GAME_RESTART_NUMBER:
+        const computerNumber = this.CreateComputerNumber(3);
+        return this.calculateResults(computerNumber);
+      case GAME_EXIT__NUMBER:
+        return;
+      default:
+        throw new Error(
+          '[ERROR] Input Value is Incorrect. You have to choose 1 or 2.'
+        );
+    }
+  }
+
+  PrintOutput(ball, strike) {
+    let output = '';
+    if (strike === 3) {
+      return ['3스트라이크', true];
+    } else if (strike === 0 && ball === 0) {
+      return ['낫싱', false];
+    }
+    if (ball !== 0) output += `${ball}볼 `;
+    if (strike !== 0) output += `${strike}스트라이크`;
+
+    return [output, false];
   }
 }
-
-const app = new App();
-app.play();
 
 export default App;
