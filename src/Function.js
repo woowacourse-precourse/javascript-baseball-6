f2bc455cc6acce6c9fbc637226fb1893108fe2f8
DWAN-00 <kkw1700@gmail.com>
2023-10-25 11:40:55 +0900

    feat: 게임 플레이 로직 추가

---
+++ src/Function.js
@@ -0,0 +1,60 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+export const computerRandom = () => {
+  const computerArr = [];
+  while (computerArr.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computerArr.includes(number)) {
+      computerArr.push(number);
+    }
+  }
+  const correctAnswer = computerArr.join("");
+  console.log(correctAnswer);
+  return correctAnswer;
+};
+
+export const numberReset = () => {
+  randomNumber = computerRandom();
+};
+
+export const validateInput = (randomNumber, input) => {
+  if (input.length !== 3) {
+    throw Error("[ERROR]: 3자리 숫자를 입력해주세요.");
+  }
+  if (input.includes("0")) {
+    throw Error("[ERROR]: 0은 입력할 수 없습니다.");
+  }
+  if (input[0] === input[1] || input[1] === input[2] || input[0] === input[2]) {
+    throw Error("[ERROR]: 중복되지 않은 숫자를 입력해주세요.");
+  } else {
+    return input;
+  }
+};
+
+export const countHandle = (computerNumber, answer) => {
+  let strikeCnt = 0;
+  let ballCnt = 0;
+  for (let i = 0; i < 3; i++) {
+    if (computerNumber[i] === answer[i]) {
+      strikeCnt++;
+    } else if (computerNumber.includes(answer[i])) {
+      ballCnt++;
+    }
+  }
+  return { strikeCnt, ballCnt };
+};
+
+export const resultHandle = (strikeCnt, ballCnt) => {
+  if (strikeCnt === 0 && ballCnt === 0) {
+    MissionUtils.Console.print("낫싱");
+  }
+  if (strikeCnt !== 0 && ballCnt == 0) {
+    MissionUtils.Console.print(`${strikeCnt}스트라이크`);
+  }
+  if (strikeCnt === 0 && ballCnt !== 0) {
+    MissionUtils.Console.print(`${ballCnt}볼`);
+  }
+  if (strikeCnt !== 0 && ballCnt !== 0) {
+    MissionUtils.Console.print(`${ballCnt}볼 ${strikeCnt}스트라이크`);
+  }
+};
