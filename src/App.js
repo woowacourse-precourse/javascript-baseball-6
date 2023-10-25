a680298c8c5ce806d1fa89e02f8e80e3f62ff960
future4ing <mirae4120@gmail.com>
2023-10-25 15:12:14 +0900

    feat : 답안 숫자 생성 기능 구현
    게임 답안을 생성하는 기능 구현
    제공된 라이브러리 @woowacourse/mission-utils의 Random API를 사용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
+  computer() {
+    const answer = [];
+
+    while (answer.length < 3) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!answer.includes(randomNumber)) 
+        answer.push(randomNumber); 
+    }
+    console.log(answer)
+    return answer;
+  }
+
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    
   }
-} 
+}
 
-const app = new App(); 
-app.play(); 
+const app = new App();
+app.play();
 
 export default App;
