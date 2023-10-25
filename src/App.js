5be0b2154ee8ab78d29fc6773c9adaeae570997a
HyeseonLee <hyesun1999@gmail.com>
2023-10-25 21:14:23 +0900

    fix: readLineAsync 함수에 인자가 없으면 오류가 나는 것을 몰랐다. 인자를 넣어주어 오류 해결

--- src/App.js
+++ src/App.js
@@ -46,9 +46,9 @@ class App {
     
     if(true){
       Console.print('3개의 숫자를 모두 맞히셨습니다! 게임종료');
-      Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+//      Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
       
-      const restart = await Console.readLineAsync();
+      const restart = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
       if(restart === '2'){
         return;
       }else if(restart ==='1'){
