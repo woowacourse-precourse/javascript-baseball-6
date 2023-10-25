577d8cb0186c4855bf52e0a8645af4ded7797901
Jeon Eonseok <jseokeon@gmail.com>
2023-10-25 18:05:46 +0900

    feat: 볼 스트라이크 판단 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
 import generateRandomNum from './generateRandomNum';
+import startGame from './startGame';
 
 class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
 
     const GENERATED_RANDOM_NUM = generateRandomNum();
+
+    startGame(GENERATED_RANDOM_NUM);
   }
 }
 
--- src/startGame.js
+++ src/startGame.js
@@ -4,14 +4,44 @@ import userInput from './userInput';
 import reGame from './regame';
 
 export default function startGame(COM_NUM) {
-  const USER_INPUT = userInput();
+  let endGame = false;
 
-  isValidNum(USER_INPUT);
-
-  if (USER_INPUT === COM_NUM) {
-    Console.print('3스트라이크');
-    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+  while(endGame) {
+    const USER_INPUT = userInput();
+  
+    isValidNum(USER_INPUT);
+  
+    if (USER_INPUT === COM_NUM) {
+      Console.print('3스트라이크');
+      Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      endGame = true;
+      
+      return reGame();
+    }
     
-    return reGame();
+    let strikeCount = 0;
+    let ballCount = 0;
+
+    USER_INPUT.forEach((elem, idx) => {
+      if (elem === COM_NUM[idx]) {
+        return strikeCount += 1;
+      }
+      if (COM_NUM.includes(elem)) {
+        return ballCount += 1;
+      }
+    });
+
+    if (strikeCount === 0 && ballCount === 0) {
+      return Console.print('낫싱');
+    }
+    if (strikeCount === 0 && ballCount !== 0) {
+      return Console.print(`${ballCount}볼`);
+    }
+    if (strikeCount !== 0 && ballCount === 0) {
+      return Console.print(`${strikeCount}스트라이크`);
+    }
+    if (strikeCount !== 0 && ballCount !== 0) {
+      return Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
+    }
   }
 }
