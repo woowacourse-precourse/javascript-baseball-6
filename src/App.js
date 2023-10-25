8d4682ae41b437b6bf76b7faf94eb6ff1ed5f786
hyun812 <sh980812@naver.com>
2023-10-25 16:49:58 +0900

    [feat] 초기 환경 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.computerNumbers = [];
+  }
+
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다");
+    while (true) {
+      this.getComputerNum();
+      
+    }
+  }
+
+  getComputerNum() {
+    this.computerNumbers = [];
+    while (this.computerNumbers.length < 3) {
+      let number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.computerNumbers.includes(number)) {
+        this.computerNumbers.push(number);
+      }
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
