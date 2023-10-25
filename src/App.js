7607c06d83c8267a937d2274fc2a53a4ecc880fb
KYHH <kyhh93@naver.com>
2023-10-25 23:55:53 +0900

    fix : 오류 내용 스트라이크와 볼 순서 변경

--- src/App.js
+++ src/App.js
@@ -28,24 +28,24 @@ class App {
 
 
   gameResult(computerNumbers, userNumbers) {
-
     const strikes = this.calculateStrikes(computerNumbers, userNumbers);
     const balls = this.calculateBalls(computerNumbers, userNumbers);
 
-
-    if (strikes > 0 || balls > 0) {
-      let resultString = '';
-      if (strikes > 0) {
-        resultString += `${strikes}스트라이크 `;
-      }
-      if (balls > 0) {
-        resultString += `${balls}볼`;
-      }
-      return resultString; // 결과 문자열 반환
-    } else {
-      return "낫싱"; // 낫싱인 경우 문자열 반환
+    let resultString = '';
+    if (balls > 0) {
+      resultString += `${balls}볼 `;
     }
+    if (strikes > 0) {
+      resultString += `${strikes}스트라이크`;
+    }
+
+    if (resultString === '') {
+      return '낫싱'; // 낫싱인 경우 문자열 반환
+    }
+
+    return resultString; // 결과 문자열 반환
   }
+
 
 
   async play() {
@@ -66,8 +66,8 @@ class App {
         if (this.calculateStrikes(computerNumbers, userNumbers) === 3) {
           Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
           const restart = await MissionUtils.Console.readLineAsync(`게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`);
-          if (restart !== '1') {
-            Console.print(`게임을 종료.`);
+          if (restart.trim() !== '1') {
+            Console.print(`게임 종료`);
             break; // 게임 종료
           } else {
             this.user = new User(); // User 객체 재생성
@@ -75,13 +75,12 @@ class App {
           }
         }
       } catch (error) {
-        // Console.print(error.message);
-        // break;
-        return Promise.reject(error); // 예외 테스트 통과 코드
+        throw error;
       }
     }
   }
 }
+
 
 class Computer {
   // Computer 클래스에 numbers 로 빈 배열을 만든다. 이 배열안에는 컴퓨터가 생성할 난수를 저장할 곳이다.
@@ -101,7 +100,7 @@ class Computer {
       }
     }
     // numbers 배열에 저장된 3개의 난수를 출력한다
-    // console.log(this.numbers);
+    // Console.print(this.numbers);
   }
 
   getNumbers() {
@@ -135,7 +134,7 @@ class User {
         }
 
         this.numbers = numbers;
-        // console.log("User Numbers:", this.numbers);
+        // Console.print("User Numbers:", this.numbers);
         break; // 올바른 입력을 받았으므로 루프를 탈출함
       }
     } catch (error) {
