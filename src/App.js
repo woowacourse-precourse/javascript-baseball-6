f13d1aeb93127c66113751b61534a73339e4433c
shgusgh12 <https://github.com/shgusgh12>
2023-10-25 16:22:02 +0900

    feat: 숫자 추첨하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import MESSAGE from './Constant.js';
 class App {
-  async play() {}
+  constructor() {
+    this.answer = [];
+  }
+
+  async play() {
+    Console.print(MESSAGE.START);
+    await this.start();
+  }
+  async start() {
+    this.answer = this.getRandomNumber();
+  }
+  getRandomNumber() {
+    const randomNumber = [];
+    while (randomNumber.length < 3) {
+      const pickNumber = Random.pickNumberInRange(1, 9);
+      if (!randomNumber.includes(pickNumber)) {
+        randomNumber.push(pickNumber);
+      }
+    }
+    return randomNumber;
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
