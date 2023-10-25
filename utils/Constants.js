85f87b92e9f066f005d3b40c5354feaf33a17f04
Seongjin Hong <hongseongjin.to@gmail.com>
2023-10-25 15:43:09 +0900

    refactor: 에러 메시지를 별도로 상수화

--- utils/Constants.js
+++ utils/Constants.js
@@ -8,9 +8,15 @@ const IN_GAME_MESSAGE = {
   startGame: "숫자 야구 게임을 시작합니다.",
   getUserAnswer: "숫자를 입력해주세요: ",
   getUserCommandToRestart: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
-  userInputError: "[ERROR] 올바르지 않은 형식의 입력입니다.",
   rightAnswer: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
   exitGame: "게임을 종료합니다.",
+};
+
+const IN_GAME_ERROR = {
+  invalidFormat: "[ERROR] 입력값은 3자리의 서로 다른 숫자여야 합니다.",
+  invalidRange: "[ERROR] 입력하신 각 자리의 숫자는 1에서 9 사이여야 합니다.",
+  duplicatedNumber: "[ERROR] 입력하신 각 자리의 숫자는 중복되지 않아야 합니다.",
+  invalidCommand: "[ERROR] 게임 재시작/종료 선택은 오직 1 또는 2로만 입력하셔야 합니다.",
 };
 
 const IN_GAME_RESULT_ITEM = {
@@ -24,4 +30,4 @@ const AFTER_GAME_USER_COMMAND = {
   exit: 2,
 };
 
-export { IN_GAME_SETTING, IN_GAME_MESSAGE, IN_GAME_RESULT_ITEM, AFTER_GAME_USER_COMMAND };
+export { IN_GAME_SETTING, IN_GAME_MESSAGE, IN_GAME_ERROR, IN_GAME_RESULT_ITEM, AFTER_GAME_USER_COMMAND };
