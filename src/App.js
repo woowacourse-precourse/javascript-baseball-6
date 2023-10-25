9fd816d7564c240dfd6d6c31e682457c07fe5206
Jinnmidnnight <vkstk0131@yonsei.ac.kr>
2023-10-25 11:48:54 +0900

    feat(develop): 전 스테이지 통합 * 게임 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import InitialStage from "./InitialStage.js";
+import PlayStage from "./PlayStage.js";
+import EndStage from "./EndStage.js";
+
 class App {
-  async play() {}
+  async play() {
+    const INITIAL_STAGE = new InitialStage();
+    INITIAL_STAGE.messagePrint();
+
+    const PLAY_STAGE = new PlayStage(INITIAL_STAGE.generateNumbers());
+    await PLAY_STAGE.run();
+
+    const END_STAGE = new EndStage();
+    if (await END_STAGE.restart()) {
+        await this.play();
+    }
+  }
 }
 
 export default App;
