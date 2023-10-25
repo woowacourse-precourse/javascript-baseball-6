0da11e811cd879c23f49809ab217638c3c25ff16
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-10-25 14:34:14 +0900

    FEAT:입력값과 정답을 비교, 그에 대한 결과 출력 기능 추가
    * #count 속성 추가
       - ball, strike 을 포함한 결과
    * #compareAnswer 메서드 추가
       - 입력값과 정답을 비교, 결과 를 #count 속성에  할당
    * #printResult 메서드 추가
       - #count 의 ball, strike 개수를 조건에 따라 출력

--- src/features/Computer.js
+++ src/features/Computer.js
@@ -2,6 +2,7 @@ import { Random } from "@woowacourse/mission-utils";
 
 export class Computer {
   #answer = [];
+  #count = { ball: 0, strike: 0 };
 
   makeAnswer() {
     while (this.#answer.length < 3) {
@@ -9,6 +10,40 @@ export class Computer {
       if (!this.#answer.includes(number)) {
         this.#answer.push(number);
       }
+    }
+  }
+
+  #compareAnswer(input) {
+    this.#answer.map((answer, index) => {
+      if (input.includes(answer)) {
+        let key = "ball";
+        if (index === input.indexOf(answer)) {
+          key = "strike";
+        }
+        this.#count = { ...this.#count, [key]: this.#count[key] + 1 };
+      }
+    });
+  }
+
+  #initializeCount() {
+    this.#count = { ...this.#count, strike: 0, ball: 0 };
+  }
+
+  #printResult() {
+    const { strike, ball } = this.#count;
+    if (strike === 0 && ball === 0) {
+      Console.print("낫싱");
+      return;
+    }
+
+    Console.print(
+      `${ball > 0 ? `${ball}볼 ` : ""}${
+        strike > 0 ? `${strike}스트라이크` : ""
+      }`
+    );
+
+    if (strike === 3) {
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
   }
 }
