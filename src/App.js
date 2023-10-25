d5b891060b31116fb77a8973b19a49ee31127c5f
OhHyukJun <kmh7277@naver.com>
2023-10-25 17:56:35 +0900

    Feat: 함수들이 동작할 app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { MESSAGE } from "./Constant/Constant.js";
+import Computer from "./Computer/Computer.js";
+import NumCompare from "./Computer/NumCompare.js";
+import User from "./User/User.js";
+
 class App {
-  async play() {}
+  constructor() {
+    this.user = new User();  
+    this.numCompare = new NumCompare(); 
+    this.isRunning = true;
+  }
+
+  async play() {
+    while (this.isRunning) {
+      const computer = new Computer();
+      const START = computer.makeAnswer();
+
+      MissionUtils.Console.print(MESSAGE.START);
+
+      let result;
+      while (result !== "3스트라이크") {
+        const userInput = await this.user.getInput();
+        result = this.numCompare.checkAnswer(userInput, START);
+        MissionUtils.Console.print(result);
+      }
+
+      MissionUtils.Console.print(MESSAGE.END);
+
+      const replayInput = await this.user.getReplay();
+
+      if (replayInput === "2") {
+        this.isRunning = false;
+      }
+    }
+  }
 }
 
 export default App;
