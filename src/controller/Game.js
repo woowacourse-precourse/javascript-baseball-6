d39826617625b308619e1eb8a169a1236491e24e
d-charlie-kim <d.charlie.kim@gmail.com>
2023-10-25 19:46:59 +0900

    Feat: 게임 시작 문구 출력 및 시작 기능

--- src/controller/Game.js
+++ src/controller/Game.js
@@ -1,3 +1,23 @@
-class Game {}
+import Answer from "../model/Answer.js";
+import OutputView from "../view/outputView.js";
+import InputView from "../view/inputView.js";
+
+class Game {
+  #answer;
+  #input;
+  #guessResult;
+
+  constructor() {
+    this.#answer = new Answer();
+    this.#guessResult = {};
+    this.#input = "";
+  }
+
+  async startGame() {
+    this.#answer.setAnswer();
+    OutputView.startMsg();
+    await this.turn();
+  }
+}
 
 export default Game;
