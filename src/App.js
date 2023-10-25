62a436c4f4bc72b098155cde60f6edf7c7f03b80
naro_Kim <gracias9022@gmail.com>
2023-10-25 19:24:15 +0900

    refactor: 기능 1. 게임 시작 로직 수정

--- src/App.js
+++ src/App.js
@@ -4,23 +4,41 @@ import {
   getRandomNumber,
   getResult,
   checkValidation,
-  setGame,
+  getGameCommand,
 } from './components';
+import { GAME_MESSAGE } from './constants';
 
 class App {
   _computedNumbers = [];
   _userNumbers = [];
+  _gameState = true;
 
   constructor() {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.print(GAME_MESSAGE.GAME_START_MESSAGE);
+  }
+
+  async setGameState() {
+    const command = await getGameCommand();
+    if (command === '1') {
+      await this.play();
+    }
+    if (command === '2') {
+      this._gameState = false;
+      return;
+    }
+    if (command !== '1' && command !== '2')
+      throw new Error('[ERROR] 잘못된 게임 커맨드 입력으로 게임이 종료됩니다.');
   }
 
   async play() {
     this._computedNumbers = getRandomNumber();
-    this._userNumbers = await getUserInput();
-    checkValidation(this._userNumbers);
-    const result = getResult(this._computedNumbers, this._userNumbers);
-    setGame(result);
+
+    while (this._gameState) {
+      this._userNumbers = await getUserInput();
+      checkValidation(this._userNumbers);
+      const result = getResult(this._computedNumbers, this._userNumbers);
+      if (result) await this.setGameState();
+    }
   }
 }
 
