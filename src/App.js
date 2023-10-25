e73efed6263db78793eebf6330f58f09766dcc99
SSY1203 <tlstjsdud566@naver.com>
2023-10-25 21:51:06 +0900

    [feature] 컴퓨터 수 3개 저장

--- src/App.js
+++ src/App.js
@@ -7,6 +7,21 @@ class App {
 
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
+    await this.gameStart();
+  }
+
+  async gameStart() {
+    this.pitcherNumbers = [];
+    await this.getPitcherNumbers();
+  }
+
+  async getPitcherNumbers() {
+    while (this.pitcherNumbers.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!this.pitcherNumbers.includes(number)) {
+        this.pitcherNumbers.push(number);
+      }
+    }
   }
 }
 
