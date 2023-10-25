fcbcb576a2fd9d49b31ee40ec4decd45f19a44e2
Jinnmidnnight <vkstk0131@yonsei.ac.kr>
2023-10-25 21:06:03 +0900

    refactor(utils): CheckValidation.js 수정 * Condtions 상수를 사용하도록 수정

--- src/utils/CheckValidation.js
+++ src/utils/CheckValidation.js
@@ -1,3 +1,5 @@
+import CONDITIONS from "../constants/Conditions.js";
+
 /**
  * 입력값의 유효성을 검사하는 함수
  * @param {string} text 입력값
@@ -8,13 +10,13 @@ const CHECK_VALIDATION = (text) => {
   // 1. 입력값에 숫자 이외의 문자가 있는지 확인한다.
   // 2. 입력값이 3자리인지 확인한다.
   // 3. 입력값이 1~9 사이의 숫자인지 확인한다.
-  if (!/^[1-9]{3}$/.test(text)) {
+  if (!CONDITIONS.NUMBER_REGEX.test(text)) {
     return false;
   }
 
   // 4. 입력값의 각 자리가 서로 다른지 확인한다.
   const numberSet = new Set(text);
-  return numberSet.size === 3;
+  return numberSet.size === CONDITIONS.NUMBER_LENGTH;
 };
 
 export default CHECK_VALIDATION;
