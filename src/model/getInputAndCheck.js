69b430c829f8ca2c1b1325e39a7f418be2690e6a
ChoByungHyun <cbj973@gmail.com>
2023-10-25 17:45:48 +0900

    ♻️ Refactor: 오류 테스트(라이브러리)

--- src/model/getInputAndCheck.js
+++ src/model/getInputAndCheck.js
@@ -1,12 +1,16 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 import consoleView from "../util/consoleControll.js";
 import {
   checkNumber,
   checkLength,
   checkDuplicate,
 } from "../util/inputValidation.js";
+import { INPUT_MESSAGE } from "../constants/message.js";
 
 export default async function getNumberAndCheck() {
-  const userInput = await consoleView.readNumber();
+  const userInput = await MissionUtils.Console.readLineAsync(
+    INPUT_MESSAGE.NUMBER
+  );
 
   checkNumber(userInput);
   checkLength(userInput);
--- src/model/restartGame.js
+++ src/model/restartGame.js
@@ -1,9 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 import consoleView from "../util/consoleControll.js";
 import { RETRY, EXIT } from "../constants/number.js";
 import { checkRetry } from "../util/inputValidation.js";
+import { INPUT_MESSAGE } from "../constants/message.js";
 
 export default async function restartGame() {
-  const userInput = await consoleView.readRetry();
+  const userInput = await MissionUtils.Console.readLineAsync(
+    INPUT_MESSAGE.RETRY
+  );
   checkRetry(userInput);
   if (userInput === RETRY) {
     return true;
