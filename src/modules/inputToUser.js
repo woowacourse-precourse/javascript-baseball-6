ee32edffaf8d505fee9586003d823091a909cd7e
JungHun98 <j7papa@naver.com>
2023-10-25 13:00:51 +0900

    refactor: 에러 메세지 상수화

--- src/modules/inputToUser.js
+++ src/modules/inputToUser.js
@@ -1,12 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
 
+const ERROR_MESSAGE = '[ERROR] 올바른 입력이 아닙니다.';
+
 const inputToUser = async (prompt, checkValidFunc) => {
   const inputValue = await Console.readLineAsync(prompt);
 
   const isValidInput = checkValidFunc(inputValue);
 
   if (!isValidInput) {
-    throw Error('[ERROR] 올바른 입력이 아닙니다.');
+    throw Error(ERROR_MESSAGE);
   }
 
   return inputValue;
