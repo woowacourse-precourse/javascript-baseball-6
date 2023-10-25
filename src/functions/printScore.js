7d4e7030fa573d96537d0cee8c3ce66e6ac1ebc6
gabi <70315572+Limgabi@users.noreply.github.com>
2023-10-25 14:21:36 +0900

    refactor : 오타 수정

--- src/functions/printScore.js
+++ src/functions/printScore.js
@@ -2,7 +2,7 @@ import { Console } from "@woowacourse/mission-utils";
 
 const printScore = (strike, ball) => {
   if (strike > 0 && ball > 0) {
-    Console.print(`${ball}볼 ${strike}스트라이트`);
+    Console.print(`${ball}볼 ${strike}스트라이크`);
   } else if (strike > 0) {
     Console.print(`${strike}스트라이크`);
   } else if (ball > 0) {
