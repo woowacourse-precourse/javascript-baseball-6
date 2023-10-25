e28ceb5f0cf26ef803779a1866987130fe9e4f4f
flowersayo <flowersayo0811@ewhain.net>
2023-10-25 16:44:29 +0900

    feat: 게임 힌트를 출력하는 기능 ( getHintString )

--- src/App.js
+++ src/App.js
@@ -31,12 +31,27 @@ class App {
     return { ball, strike };
   }
 
+  getHintString(ball, strike) {
+    if (ball === 0 && strike === 0) {
+      return '낫싱';
+    }
+    if (ball === 0) {
+      return `${strike}스트라이크`;
+    }
+    if (strike === 0) {
+      return `${ball}볼`;
+    }
+
+    return `${ball}볼 ${strike}스트라이크`;
+  }
+
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
     let computerNumbers = this.generateRandomNumbers();
     const userNumbers = await InputProcessor.inputNumber();
 
     const { ball, strike } = this.compare(computerNumbers, userNumbers);
+    Console.print(this.getHintString(ball, strike));
   }
 }
 
