cb6a67b2b2e0e9d1021703fd108637b74dcbebd7
flowersayo <flowersayo0811@ewhain.net>
2023-10-25 16:22:32 +0900

    feat: 사용자가 입력한 숫자에 대한 유효성 검증 기능 ( isValidNumberList )

--- src/InputProcessor.js
+++ src/InputProcessor.js
@@ -5,10 +5,34 @@ export default class InputProcessor {
     const inputString = await Console.readLineAsync('숫자를 입력해주세요: ');
     const inputArray = this.convertStringToNumArray(inputString);
 
+    if (!this.isValidNumberList(inputArray)) {
+      throw new Error('[ERROR] 잘못된 숫자 형식입니다.');
+    }
     return inputArray;
   }
 
   static convertStringToNumArray(inputString) {
     return [...inputString].map((char) => Number(char));
+  }
+
+  static isValidNumberList(inputArray) {
+    return (
+      this.isThreeDigit(inputArray) &&
+      this.isOnlyNumber(inputArray) &&
+      !this.isDuplicated(inputArray)
+    );
+  }
+
+  static isThreeDigit(inputArray) {
+    return inputArray.length === 3;
+  }
+
+  static isOnlyNumber(inputArray) {
+    return inputArray.every((item) => typeof item === 'number');
+  }
+
+  static isDuplicated(inputArray) {
+    const set = new Set(inputArray);
+    return set.size !== inputArray.length;
   }
 }
