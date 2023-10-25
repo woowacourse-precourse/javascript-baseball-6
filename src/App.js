894db1d55d62ad97c1c3349fc84c6155bc840b3f
jayming66 <mjw8941@gmail.com>
2023-10-25 19:36:56 +0900

    feat : 1 입력 시 게임 restart, 2 입력 시 종료, 이외에 에러처리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 let gameResult = 0;
+let restartResult = 1;
 
-function randomNumber() {
+function getRandomNumber() {
   const randomNumbers = [];
   while (randomNumbers.length < 3) {
     // for 문으로 랜덤 값이 2개가 나오는 현상 발견
@@ -35,38 +36,60 @@ function getGameResult(computer, user) {
 
 class App {
   async play() {
-    Console.print("숫자 야구 게임을 시작합니다."); // 게임 시작 문구
-    let randaomNumbers = randomNumber(); // 컴퓨터의 숫자 3개 생성
-    Console.print(randaomNumbers);
+    while (restartResult === 1) {
+      Console.print("숫자 야구 게임을 시작합니다."); // 게임 시작 문구
+      let randomNumbers = getRandomNumber(); // 컴퓨터의 숫자 3개 생성
+      Console.print(randomNumbers);
 
-    while (gameResult !== 3) {
-      let userInputs = await Console.readLineAsync("숫자를 입력해주세요 : "); // 쿼리를 보내야하고, await 해줘야한다.
-      userInputs = userInputs.split("").map((element) => parseInt(element, 10)); // input을 int형으로 바꾼다.
+      let ball = 0;
+      let strike = 0;
 
-      if (userInputs.length !== 3) throw new Error("3 Input require"); // 3개의 input인지 검증
-      const userInputSet = new Set(userInputs);
+      while (gameResult !== 3) {
+        let userInputs = await Console.readLineAsync("숫자를 입력해주세요 : "); // 쿼리를 보내야하고, await 해줘야한다.
+        userInputs = userInputs
+          .split("")
+          .map((element) => parseInt(element, 10)); // input을 int형으로 바꾼다.
 
-      if (userInputSet.size !== 3) {
-        throw new Error("duplicated input");
-      } // 중복된 숫자가 없는지 검증
+        if (userInputs.length !== 3) throw new Error("3 Input require"); // 3개의 input인지 검증
+        const userInputSet = new Set(userInputs);
 
-      const [ball, strike] = getGameResult(randaomNumbers, userInputs);
-      gameResult = strike;
+        if (userInputSet.size !== 3) {
+          throw new Error("duplicated input");
+        } // 중복된 숫자가 없는지 검증
 
-      if (ball === 0 && strike === 0) {
-        Console.print("낫싱");
+        [ball, strike] = getGameResult(randomNumbers, userInputs);
+        gameResult = strike;
+
+        if (ball === 0 && strike === 0) {
+          Console.print("낫싱");
+        }
+        if (ball > 0 && strike === 0) {
+          Console.print(`${ball}볼`);
+        }
+        if (ball === 0 && strike > 0) {
+          Console.print(`${strike}스트라이크`);
+        }
+        if (ball > 0 && strike > 0) {
+          Console.print(`${ball}볼 ${strike}스트라이크`);
+        }
       }
-      if (ball > 0 && strike === 0) {
-        Console.print(`${ball}볼`);
-      }
-      if (ball === 0 && strike > 0) {
-        Console.print(`${strike}스트라이크`);
-      }
-      if (ball > 0 && strike > 0) {
-        Console.print(`${ball}볼 ${strike}스트라이크`);
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      let restartInput = await Console.readLineAsync(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+      );
+      if (restartInput === "1") {
+        restartResult = 1;
+        ball = 0;
+        strike = 0;
+        gameResult = 0;
+      } else if (restartInput === "2") {
+        restartResult = 2;
+        return;
+      } else {
+        restartResult = 0;
+        throw new Error("not 1 or 2 value. game over");
       }
     }
-    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
   }
 }
 
