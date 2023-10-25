2c623e744e6a5da6fd91b2c194edd319add69856
이호연 <ho991217@kakao.com>
2023-10-25 13:16:46 +0900

    refactor: 주석 추가
    유효성 검사 함수에 주석 추가
    숫자 검증 로직 변경

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,15 +1,42 @@
 import InputError from '../errors/input-error';
 import { CONSTANTS } from '../constants';
 
-const isAllNumber = (numbers) => !numbers.some(Number.isNaN);
-const isInRange = (numbers) => numbers.every(
+/**
+ * 숫자로만 이루어진 배열인지 검사하는 함수
+ * @param {number[]} numbers
+ * @returns {boolean}
+ */
+export const isSafeInteger = (numbers) => Number.isSafeInteger(Number(numbers.join('')));
+
+/**
+ * 범위 내의 숫자로만 이루어진 배열인지 검사하는 함수
+ * @param {number[]} numbers
+ * @returns {boolean}
+ */
+export const isInRange = (numbers) => numbers.every(
   (number) => CONSTANTS.RANGE.from <= number && number <= CONSTANTS.RANGE.to,
 );
-const isThreeWordLong = (numbers) => numbers.length === CONSTANTS.MAX_INPUT_SIZE;
-const isUnique = (numbers) => new Set(numbers).size === numbers.length;
 
+/**
+ * 3자리 숫자로만 이루어진 배열인지 검사하는 함수
+ * @param {number[]} numbers
+ * @returns {boolean}
+ */
+export const isThreeWord = (numbers) => numbers.length === CONSTANTS.MAX_INPUT_SIZE;
+
+/**
+ * 중복되지 않는 숫자로만 이루어진 배열인지 검사하는 함수
+ * @param {numbers[]} numbers
+ * @returns {boolean}
+ */
+export const isUnique = (numbers) => new Set(numbers).size === numbers.length;
+
+/**
+ * 입력값을 검사하는 함수
+ * @param {number[]} numbers
+ */
 export default function validate(numbers) {
-  if (!isAllNumber(numbers)) {
+  if (!isSafeInteger(numbers)) {
     throw new InputError('숫자를 입력해주세요.');
   }
   if (!isInRange(numbers)) {
@@ -17,7 +44,7 @@ export default function validate(numbers) {
       `${CONSTANTS.RANGE.from}~${CONSTANTS.RANGE.to} 사이의 숫자를 입력해주세요.`,
     );
   }
-  if (!isThreeWordLong(numbers)) {
+  if (!isThreeWord(numbers)) {
     throw new InputError(`${CONSTANTS.MAX_INPUT_SIZE}자리 숫자를 입력해주세요.`);
   }
   if (!isUnique(numbers)) {
