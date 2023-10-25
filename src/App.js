c9867874599f08b8a9a07d3a279e4535f7dcce0d
junmiLee <junmi950918@gmail.com>
2023-10-25 16:44:21 +0900

    유저 인풋기능추가

--- src/App.js
+++ src/App.js
@@ -9,6 +9,18 @@ class App {
 
     this.generateComputerNumbers();
   }
-}
 
+  getUserInput() {
+    const input = Promise.resolve(Console.readLineAsync());
+    try {
+      this.userNumbers = input.split(" ").map(Number);
+      if (this.userNumbers.length !== 3) {
+        throw new Error("숫자를 3개 입력하세요.");
+      }
+    } catch (error) {
+      console.error(error);
+      return;
+    }
+  }
+}
 export default App;
