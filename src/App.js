32f939bea6308bd9e6c932a64e16e15a7d37a725
kwaksj329 <kwaksj329@gmail.com>
2023-10-25 10:57:24 +0900

    Feat: random한 정답 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+const ANSWER_LENGTH = 3;
+
 class App {
-  async play() {}
+
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    const answer = this.createAnswer();
+  }
+
+  createAnswer(){
+    const answer = [];
+    while (answer.length < ANSWER_LENGTH) {
+      const number = String(Random.pickNumberInRange(1, 9));
+      if (!answer.includes(number)) {
+        answer.push(number);
+      }
+    }
+    return answer.join("");
+  }
 }
 
 export default App;
