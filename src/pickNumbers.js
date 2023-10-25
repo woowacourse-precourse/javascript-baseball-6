7bc20d7d937d1fbe007aa1da8865aa68026fe533
jomoo02 <jomootgrn88@naver.com>
2023-10-25 19:24:15 +0900

    refactor: pickNumbers.js function pickUniqueNumbersInRange
    pickUniqueNumbersInRange의 기능을 분리
    makeNumbersLength 함수를 통해 배열에 길이가 주어진 길이가 될 때까지 반복 조건이 만족하면 주어진 길이의 배열 리턴
    getPushedUniqueNumberArr 함수를 통해 주어진 숫자가 배열에 있는 숫자와 겹치지 않으면 push

--- src/pickNumbers.js
+++ src/pickNumbers.js
@@ -4,18 +4,24 @@ const pickNumberInRange = () => MissionUtils.Random.pickNumberInRange(1, 9);
 
 const isUnique = (arr, number) => !arr.includes(number);
 
-const pickUniqueNumbersInRange = (numbersLength) => {
-    const numbers = [];
+const getPushedArr = (arr, number) => {
+    if (isUnique(arr, number)) {
+        return [...arr, number];
+    }
+};
 
-    while (numbers.length < numbersLength) {
-        const randomNumber = pickNumberInRange();
-
-        if (isUnique(numbers, randomNumber)) {
-            numbers.push(randomNumber);
-        }
+const makeNumbersLength = (numbersLength, arr = []) => {
+    if (arr.length < numbersLength) {
+        return makeNumbersLength(numbersLength, getPushedArr(arr, pickNumberInRange()));
     }
 
-    return numbers;
+    return [...arr];
+};
+
+
+
+const pickUniqueNumbersInRange = (numbersLength) => {
+    return makeNumbersLength(numbersLength);
 };
 
 export { pickUniqueNumbersInRange };
