4bdff445bf038bed666f4c23534fc80f05dacd65
thgus5335 <thgus5335@naver.com>
2023-10-25 23:04:56 +0900

    feat: 플레이어 숫자 입력 기능   추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const MissionUtils = require('@woowacourse/mission-utils');
 
 const NUM_LENGTH = 3;
 
 class App {
   async play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    const computerNum = computerPickNum();
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    while(true) {
+      const computerNum = computerPickNum();
+      startGame(computerNum);
+      
+    }
   }
 }
 
@@ -22,6 +26,23 @@ const computerPickNum = () => {
 }
 
 //(2)플레이어 숫자 입력
+const startGame = (computerNum) => {
+  while(true) {
+    const input = MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+    const userNum = input.split('').map(Number);
+
+    checkInput(input);
+  }
+}
+
+const checkInput = (input) => {
+  const userInput = new Set(input.split('').map(Number));
+
+  if (input.length !== NUM_LENGTH)
+    throw Error('[ERROR] 숫자가 잘못된 형식입니다. 숫자 3개를 입력해주세요.');
+  if ([...userInput].length !== NUM_LENGTH)
+    throw Error('[ERROR] 숫자가 잘못된 형식입니다. 중복되지 않는 숫자를 입력해주세요.');
+}
 
 //(3)결과 출력
 
