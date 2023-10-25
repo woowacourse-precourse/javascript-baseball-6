f368f6ece05ce927b6dac072455488005653685a
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-25 22:56:41 +0900

    refactor : Error 함수 빼기

--- src/Player.js
+++ src/Player.js
@@ -1,7 +1,7 @@
 import { Console } from "@woowacourse/mission-utils";
 import { INPUT_MESSAGE } from "./Define.js";
 import { validatePlayerInput } from "./GameRefree.js"
-import { printErrorMessage } from "./GameManager.js";
+import {printErrorMessage} from "./Error.js";
 
 export const getPlayerInput = async () => {
     const input = await Console.readLineAsync(INPUT_MESSAGE);
