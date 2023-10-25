18df67ea60627d38defb6662494419f3bc4f1c0a
uujeen <rkddmlwls2@gmail.com>
2023-10-25 20:07:44 +0900

    feat: 1차 기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import { MissionUtils, Console } from '@woowacourse/mission-utils';
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 const MINIMUMNUMBER = 1;
 const MAXIMUMNUMBER = 9;
 const NUMBERLENGTH = 3;
-const REGEX = /^(?!.*(\d).*\1)\d{3}$/;
+const REGEX = /^[1-9]+$/;
 const MESSAGE = {
   START: '숫자 야구 게임을 시작합니다.',
   INPUT: '숫자를 입력해주세요 : ',
   ERROR: '[ERROR] 잘못된 형식의 입력입니다.',
   NOTHING: '낫싱',
-  SUCCESS: '3스트라이크',
   BALL: '볼',
   STRIKE: '스트라이크',
+  SUCCESS: '3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  RESTART: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+  END: '게임을 종료합니다.',
+};
+const GAMESTATE = {
+  START: '1',
+  END: '2',
 };
 
 class App {
@@ -24,12 +30,16 @@ class App {
     };
   }
   async play() {
-    Console.print(MESSAGE.START);
-    this.setComputerHasNumber();
-    this.setUserHasNumber();
+    MissionUtils.Console.print(MESSAGE.START);
+    await this.start();
   }
 
+  async start() {
+    this.setComputerHasNumber();
+    await this.setUserHasNumber();
+  }
   setComputerHasNumber() {
+    this.computerHasNumber = [];
     while (this.computerHasNumber.length < NUMBERLENGTH) {
       const number = MissionUtils.Random.pickNumberInRange(
         MINIMUMNUMBER,
@@ -42,22 +52,32 @@ class App {
   }
 
   async setUserHasNumber() {
-    const number = await Console.readLineAsnync(MESSAGE.INPUT);
-    if (!REGEX.test(number)) {
-      throw new Error(MESSAGE.ERROR);
-    }
-
-    this.userHasNumber = [...number].map(Number);
+    await this.inputUserNumber();
 
     this.compareNumber();
     this.printScore();
+
+    if (this.score.strike === 3) {
+      await this.reStart();
+    } else await this.setUserHasNumber();
+  }
+
+  async inputUserNumber() {
+    const number = await MissionUtils.Console.readLineAsync(MESSAGE.INPUT);
+    if (!REGEX.test(number)) {
+      throw new Error(MESSAGE.ERROR);
+    }
+    this.userHasNumber = [...number].map(Number);
   }
 
   compareNumber() {
-    this.computerHasNumber.forEach((e, idx) => {
-      if (e === this.userHasNumber[idx]) {
+    this.score.strike = 0;
+    this.score.ball = 0;
+    this.computerHasNumber.forEach((num, idx) => {
+      if (num === this.userHasNumber[idx]) {
         this.score.strike += 1;
-      } else if (this.userHasNumber.includes(e)) {
+      }
+      if (num !== this.userHasNumber[idx] && this.userHasNumber.includes(num)) {
         this.score.ball += 1;
       }
     });
@@ -65,19 +85,36 @@ class App {
 
   printScore() {
     if (this.score.strike === 0 && this.score.ball === 0) {
-      Console.print(MESSAGE.NOTHING);
-    } else if (this.score.strike === 0 && this.score.ball !== 0) {
-      Console.print(`${this.score.ball}${MESSAGE.BALL}`);
-    } else if (this.score.strike !== 0 && this.score.ball !== 0) {
-      Console.print(
+      MissionUtils.Console.print(MESSAGE.NOTHING);
+    }
+    if (this.score.strike === 0 && this.score.ball > 0) {
+      MissionUtils.Console.print(`${this.score.ball}${MESSAGE.BALL}`);
+    }
+    if (this.score.strike > 0 && this.score.ball > 0) {
+      MissionUtils.Console.print(
         `${this.score.ball}${MESSAGE.BALL} ${this.score.strike}${MESSAGE.STRIKE}`
       );
-    } else if (this.score.strike !== 0 && this.score.ball === 0) {
-      Console.print(`${this.score.strike}${MESSAGE.STRIKE}`);
-    } else {
-      Console.print(MESSAGE.SUCCESS);
+    }
+    if (this.score.strike > 0 && this.score.ball === 0) {
+      MissionUtils.Console.print(`${this.score.strike}${MESSAGE.STRIKE}`);
+    }
+    if (this.score.strike === 3) {
+      MissionUtils.Console.print(MESSAGE.SUCCESS);
     }
   }
+
+  async reStart() {
+    MissionUtils.Console.print(MESSAGE.RESTART);
+    const number = await MissionUtils.Console.readLineAsync('');
+
+    if (number === GAMESTATE.START) await this.start();
+    if (number === GAMESTATE.END) MissionUtils.Console.print(MESSAGE.END);
+    if (number !== GAMESTATE.START && number !== GAMESTATE.END)
+      throw new Error(MESSAGE.ERROR);
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
