9a8eaa59bcfad456bab8120b80bb2fdbdc7ca1b0
gvsteve24 <junghyun30124@gmail.com>
2023-10-25 20:01:41 +0900

    added README.md and comments: explanation for valid input and control flow and applied airbnb coding convention

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
-  // input에 대한 예외케이스
-  // 1. 세자리가 아닌 숫자
-  // 2. 빈칸이 있는 숫자
-  // 3. 숫자이외의 문자가 포함된 경우
-  // 4. endGame입력: 1이나 2가 아닌 다른 문자가 주어지는 경우, 아니면 1도 아니고 2도 아닌 숫자
   async validateInput(mode, input) {
+    // 예측값 입력에 대한 예외케이스
+    //   길이가 3이 아닌 문자열
+    //   숫자(1-9)가 아닌 다른 문자를 포함하는 문자열
+    //   중복된 숫자를 가지지 않는 문자열
+    // 게임 재시작 여부에 대한 예외케이스
+    //   1이나 2가 아닌 다른 문자를 포함하는 문자열
     if (mode === 'guess') {
-      let a = input.length === 3;
-      let b = (/[1-9]{3}/).test(input);
-      let c = (new Set(input.split(''))).size === 3;
-      return a && b && c;
+      const LENGTH_CHECK = input.length === 3;
+      const THREE_DIGIT_CHECK = (/[1-9]{3}/).test(input);
+      const UNIQUE_CHECK = (new Set(input.split(''))).size === 3;
+      return LENGTH_CHECK && THREE_DIGIT_CHECK && UNIQUE_CHECK;
     }else if (mode === 'resume') {
-      let a = (/[1-9]{1}/).test(input);
-      let b = (input === "1" || input === "2");
-      return a && b;
+      const ONE_DIGIT_CHECK = (/[1-9]{1}/).test(input);
+      const CAND_CHECK = (input === '1' || input === '2');
+      return ONE_DIGIT_CHECK && CAND_CHECK;
     }
   }
 
@@ -25,62 +26,63 @@ class App {
 
   async play() {
     let resume = true;
-    // resume 구간
     Console.print('숫자 야구 게임을 시작합니다.');
-
+    
+    // 게임 재시작 여부에 따라 반복결정
     while(resume) {
-      const targetNums = []; 
-      let pool = new Set(targetNums);
-      while(targetNums.length < 3) {
-        const target = Random.pickNumberInRange(1, 9);
-        if (pool.has(target)) {
+      const TARGET_NUMS = []; 
+      const pool = new Set(TARGET_NUMS);
+      while(TARGET_NUMS.length < 3) {
+        const TARGET = Random.pickNumberInRange(1, 9);
+        if (pool.has(TARGET)) {
           continue;
         }
-        pool.add(target);
-        targetNums.push(target);
+        pool.add(TARGET);
+        TARGET_NUMS.push(TARGET);
       }
 
-      // guess 구간
+      // 예측값의 정답여부에 따라 반복결정
       while(true) {
         let input = await this.getUserInput('숫자를 입력해주세요 : ');
         
-        // exception handling
+        // 입력의 유효성 확인
         let validInput = await this.validateInput('guess', input);
         
         if (!validInput) {
           resume = false;
-          throw new Error("[ERROR]");
+          throw new Error('[ERROR]');
         }
         
-        let inputNum = parseInt(input, 10);
-        let inputNums = [(Math.floor(inputNum/100))%10, (Math.floor(inputNum/10))%10, inputNum%10];
+        let inputNums = [parseInt(input[0], 10), parseInt(input[1], 10), parseInt(input[2], 10)];
         
         let strike = 0, ball = 0;
         
         // 스트라이크 조건, 볼 조건
-        for(let i = 0; i < targetNums.length; i++) {
-          if (targetNums[i] === inputNums[i]) {
+        for(let i = 0; i < TARGET_NUMS.length; i++) {
+          if (TARGET_NUMS[i] === inputNums[i]) {
             strike++;
-          } else if (targetNums.includes(inputNums[i])){
+          } else if (TARGET_NUMS.includes(inputNums[i])){
             ball++;
           }
         }
   
-        if (strike == 3 && ball == 0) {
+        if (strike === 3 && ball === 0) {
           Console.print(`${strike}스트라이크`);
           Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
           let endGame = await this.getUserInput('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
           
-          // exception handling
+          // 입력의 유효성 확인
           let validInput = await this.validateInput('resume', endGame);
           if (!validInput) {
-            throw new Error("[ERROR]");
-          }else if(endGame === "2") {
-            resume = false;
-            break;
-          }else{
-            break;
+            throw new Error('[ERROR]');
           }
+          
+          // 사용자가 2를 입력하면 반복조건을 false로 설정
+          if(endGame === "2") {
+            resume = false;
+          }
+
+          break;
         }
   
         if ( strike > 0 && ball > 0 ) {
@@ -97,7 +99,7 @@ class App {
   }
 }
 
-const app = new App();
-app.play();
+const APP = new App();
+APP.play();
 
 export default App;
