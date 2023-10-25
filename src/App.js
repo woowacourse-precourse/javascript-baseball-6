4c35b7e34176c166fe45d6e4de42656086a4f59a
publdaze <publdaze@naver.com>
2023-10-25 17:55:12 +0900

    feat: 시작 문구 띄워주기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import BaseballGame from "./BaseballGame.js";
+
 class App {
-  async play() {}
+  async play() {
+    new BaseballGame();
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
---
+++ src/BaseballGame.js
@@ -0,0 +1,7 @@
+class BaseballGame {
+  constructor() {
+    console.log("숫자 야구 게임을 시작합니다.");
+  }
+}
+
+export default BaseballGame;
