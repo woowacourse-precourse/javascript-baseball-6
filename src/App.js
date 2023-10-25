855283d6fccf8c4f1a9eeaebf0df83dea38f74f5
delee3064 <delee3064@gmail.com>
2023-10-25 20:47:43 +0900

    feat : 랜덤값과 입력값을 비교하여 결과 출력

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
+
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     const computerNum = this.setNum();
     const userNum = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
     this.vaildation(userNum)
+    this.result(computerNum, userNum)
     
   }
 
+  // 랜덤값과 입력값을 비교
+  result(computerNum, userNum) {
+    const checkNum = Array.from(userNum, Number)
+    let strike = 0
+    let ball = 0
+
+    for (let i = 0; i < 3; i++) {
+      if (computerNum.includes(Number(checkNum[i]))) {
+        if (computerNum[i] === checkNum[i]) {
+          strike += 1;
+        }
+        else{
+          ball += 1;
+        }
+      }
+    }
+    this.printresult(ball, strike)
+  }
+
+  //비교값 출력
+  printresult(ball, strike) {
+    if (ball === 0 && strike === 0) {
+      MissionUtils.Console.print("낫싱")
+    }
+    
+    else if(ball > 0) {
+      if (strike > 0) {
+        MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`)
+      }
+      else {
+        MissionUtils.Console.print(`${ball}볼`)
+      }
+    }
+
+    else if (strike > 0) {
+      MissionUtils.Console.print(`${strike}스트라이크`)
+    }
+
+  }
+
+
+  //예외처리
   vaildation(userNum) {
 
     if(userNum.length === 0) {
@@ -27,6 +71,8 @@ class App {
 
   }
 
+
+  //랜덤으로 3자리의 수 선택
   setNum() {
     const computer = [];
     while (computer.length < 3) {
