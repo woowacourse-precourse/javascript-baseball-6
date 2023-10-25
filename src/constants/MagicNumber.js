539012c4bf6a75b2f9ecfb9a80ecb3f85895492d
dooohun <ehgns0305@naver.com>
2023-10-25 20:01:51 +0900

    fix: 중복된 export 선언 제거

--- src/constants/MagicNumber.js
+++ src/constants/MagicNumber.js
@@ -5,9 +5,8 @@ export const USER_CHOICE = {
   retry: '1',
   quit: '2'
 };
+
 export const RANGE_OF_RANDOM_NUMBER = {
   min: 1,
   max: 9
 };
-
-export { DELIMITER, NUMBER_LENGTH, USER_CHOICE_RETRY, RANGE_OF_RANDOM_NUMBER };
