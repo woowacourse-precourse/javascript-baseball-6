1175d320dfeb496a4bc545cd585caf9c803ebf9b
HarrySeop <parkpjs3083@naver.com>
2023-10-25 21:11:32 +0900

    feat: 숫자야구를 위한 랜덤함수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  
+
+
+}
+
+// 2. 게임을 위한 랜덤 숫자야구 생성해주는 함수
+function NewRandomAnswer() {
+  const CORRECT_ANSWER = [];
+
+  while (CORRECT_ANSWER.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if(!CORRECT_ANSWER.includes(number)) {
+      CORRECT_ANSWER.push(number);
+    }
+  }
+  return CORRECT_ANSWER;
 }
 
 export default App;
