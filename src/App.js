d894dc11e73b2bbd259ead15b8e01d6ae7fd3fd7
김도언 <vov06144@naver.com>
2023-10-25 20:58:48 +0900

    feat: 게임 결과 판단 함수, 재시도 검사 함수 추가

--- src/App.js
+++ src/App.js
@@ -4,19 +4,76 @@ class App {
   async play() {
     // 게임 시작 문구
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    // 사용자 입력 받기 전, 컴퓨터 랜덤 숫자 생성
 
-    const computer = getComputerNum();
+    // 게임 세팅
+    let computer = [];
+
+    computer = getComputerNum();
     console.log("COM", computer);
-    // 사용자 입력 받기
-    const playerInput = await MissionUtils.Console.readLineAsync(
-      "숫자를 입력해주세요 : "
-    );
-    // 사용자 입력 값이 정상인지 검사
-    try {
-      const player = checkPlayerInput(playerInput);
-    } catch (error) {
-      console.error("[ERROR] " + error.message);
+    let isGamePlaying = true;
+    let player = [];
+
+    while (isGamePlaying) {
+      console.log("🎈현재 com", computer);
+      // 사용자 입력 받기
+      const playerInput = await MissionUtils.Console.readLineAsync(
+        "숫자를 입력해주세요 : "
+      );
+      try {
+        // 사용자 입력 값이 정상인지 검사
+        player = checkPlayerInput(playerInput);
+      } catch (error) {
+        MissionUtils.Console.print("[ERROR] " + error.message);
+        return;
+      }
+      // 입력값에 따른 게임 결과 알아보기
+      const gameResult = getGameResult(computer, player);
+      const ballCnt = gameResult[0];
+      const strikeCnt = gameResult[1];
+
+      let resultStr = "";
+
+      if (ballCnt > 0) {
+        resultStr += `${ballCnt}볼 `;
+      }
+
+      if (strikeCnt > 0) {
+        resultStr += `${strikeCnt}스트라이크`;
+      }
+
+      if (ballCnt === 0 && strikeCnt === 0) {
+        resultStr += "낫싱";
+      }
+
+      MissionUtils.Console.print(resultStr);
+
+      if (strikeCnt === 3) {
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        // console.log("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        const isAgainInput = await MissionUtils.Console.readLineAsync(
+          "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. : "
+        );
+        let isAgain;
+
+        try {
+          // 사용자 입력 값이 정상인지 검사
+          isAgain = checkIsAgainInput(isAgainInput);
+        } catch (error) {
+          console.error("[ERROR] " + error.message);
+          return;
+        }
+
+        if (isAgain) {
+          //새로 시작
+          computer = getComputerNum();
+          console.log("COM", computer);
+          player = [];
+          continue;
+        } else {
+          console.log("완전 끝!@");
+          break;
+        }
+      }
     }
   }
 }
@@ -40,12 +97,12 @@ const checkPlayerInput = (input) => {
   }
 
   if (input.length !== 3) {
-    console.log("개수 문제!");
+    // console.log("개수 문제!");
     throw new Error("3자리의 숫자를 입력해주세요.");
   }
 
   if (input.includes("0")) {
-    console.log("0 포함!");
+    // console.log("0 포함!");
     throw new Error("1이상 9이하 숫자로 구성해주세요.");
   }
 
@@ -53,11 +110,40 @@ const checkPlayerInput = (input) => {
   const inputSet = new Set(inputNum);
 
   if (inputSet.size < 3) {
-    console.log("중복된 값 있음");
+    // console.log("중복된 값 있음");
     throw new Error("중복되지 않는 숫자로 구성해주세요.");
   }
-  console.log("test", inputSet);
+  // console.log("test", inputSet);
   return inputNum;
+};
+
+const getGameResult = (arrComputer, arrPlayer) => {
+  let strike = [];
+  let ball = 0;
+
+  // 스트라이크 먼저 검사
+  for (let idx = 0; idx < 3; idx++) {
+    if (arrPlayer[idx] === arrComputer[idx]) {
+      strike = [...strike, idx];
+    }
+
+    if (!strike.includes(idx) && arrComputer.includes(arrPlayer[idx])) {
+      ball += 1;
+    }
+  }
+
+  const result = [ball, strike.length];
+  return result;
+};
+
+const checkIsAgainInput = (input) => {
+  if (input === "1") {
+    return true;
+  } else if (input === "2") {
+    return false;
+  } else {
+    throw new Error("1 또는 2를 입력하세요.");
+  }
 };
 
 const app = new App();
