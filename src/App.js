13df12880f7da80d23ba5a9f09bb0443d4fc4772
jayming66 <mjw8941@gmail.com>
2023-10-25 17:07:52 +0900

    feat : 각 숫자가 중복된 숫자가 없는지 검증

--- src/App.js
+++ src/App.js
@@ -19,9 +19,14 @@ class App {
     let randaomNumbers = randomNumber(); // 컴퓨터의 숫자 3개 생성
     //Console.print(randaomNumbers);
 
-    let userInputs = await Console.readLineAsync("test"); // 쿼리를 보내야하고, await 해줘야한다.
+    let userInputs = await Console.readLineAsync("숫자를 입력해주세요 : "); // 쿼리를 보내야하고, await 해줘야한다.
     userInputs = userInputs.split("").map((element) => parseInt(element, 10)); // input을 int형으로 바꾼다.
     if (userInputs.length !== 3) throw new Error("3 Input require"); // 3개의 input인지 검증
+    const userInputSet = new Set(userInputs);
+    //Console.print(userInputSet.size);
+    if (userInputSet.size !== 3) {
+      throw new Error("duplicated input");
+    } // 중복된 숫자가 없는지 검증
     Console.print(userInputs);
   }
 }
