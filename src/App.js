643fff381d81bb7a735f7f351fe5d42371ee91a5
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-10-25 18:46:30 +0900

    feat: 랜덤 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
+  constructor() {
+    this.computer = [];
+  }
+
   async play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.makeRandomNumbers();
+  }
+
+  makeRandomNumbers() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) computer.push(number);
+    }
+    this.computer = computer;
   }
 }
 
