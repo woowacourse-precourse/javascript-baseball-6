df2c15bea288b2b244e9f96add48de00338dc231
Nagi <nakt4453@gmail.com>
2023-10-25 23:39:06 +0900

    feat: 입력 기능 추가 * 서로 다른 세자리 수 아닐때 에러 메세지 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -10,7 +10,26 @@ class App {
     MissionUtils.Console.print(MESSAGES.GAME_START);
   }
 
-  async play() {}
+  // 게임 시작
+  // async play() {
+
+  // }
 }
+
+  // 라운드 입력
+  async roundInput() {
+    try {
+      const guessNumbers = await MissionUtils.Console.readLineAsync(MESSAGES.ASK_GUESS);
+      if (!/^[1-9]{3}$/.test(guessNumbers) || new Set(guessNumbers.split('')).size !== 3) {
+        throw Error
+      } else {
+        return guessNumbers;
+      }
+    } catch (e) {
+      MissionUtils.Console.print(MESSAGES.ERROR_GUESS);
+    }
+  }
+
+
 
 export default App;
