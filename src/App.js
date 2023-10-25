9389d2388f1c04ae2d59e7838bb04726319e5e2a
HarrySeop <parkpjs3083@naver.com>
2023-10-25 21:24:51 +0900

    feat: 숫자야구 시작을 위한 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
 
 class App {
-  
 
+  async play() { 
+    // 변수 선언
+    let CORRECT_ANSWER;
+
+    // 초기 설정
+    Console.print("숫자 야구 게임을 시작합니다.");
+    CORRECT_ANSWER = NewRandomAnswer();
+  }
 
 }
 
