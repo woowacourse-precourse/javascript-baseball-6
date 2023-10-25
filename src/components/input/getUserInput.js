0bdfe4fe81bf0491fd6bad6afed176c63224afdb
naro_Kim <gracias9022@gmail.com>
2023-10-25 19:25:40 +0900

    refactor: 유지보수를 위해 메세지 상수로 변경

--- src/components/input/getUserInput.js
+++ src/components/input/getUserInput.js
@@ -1,11 +1,11 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { checkValidation } from './checkValidation';
+import { GAME_MESSAGE } from '../../constants';
 
 // 사용자로부터 숫자를 입력받습니다.
 const getUserInput = async () => {
   try {
     const input = await MissionUtils.Console.readLineAsync(
-      '숫자를 입력해주세요 :',
+      GAME_MESSAGE.INPUT_MESSAGE,
     );
     return input;
   } catch (error) {
--- src/constants.js
+++ src/constants.js
@@ -2,14 +2,17 @@ export const INPUT_RULES = {
   MIN_RANGE: 1,
   MAX_RANGE: 9,
   NUMBER_OF_DIGITS: 3,
-  REGEX: /[^1-9]/g,
 };
 
-export const ANSWER_MESSAGE = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
+export const GAME_MESSAGE = {
+  GAME_START_MESSAGE: '숫자 야구 게임을 시작합니다.',
+  COMMAND_MESSAGE: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+  INPUT_MESSAGE: '숫자를 입력해주세요 : ',
+  INPUT_ERROR: '[ERROR] 숫자가 잘못된 형식입니다.',
+};
 
-export const NOTHING_MESSAGE = '낫싱';
-
-export const ERROR_MESSAGE = '[ERROR] 숫자가 잘못된 형식입니다.';
-
-export const EXIT_MESSAGE =
-  '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.';
+export const RESULT_MESSAGE = {
+  ANSWER: `3스트라이크
+  3개의 숫자를 모두 맞히셨습니다! 게임 종료`,
+  NOTHING: '낫싱',
+};
