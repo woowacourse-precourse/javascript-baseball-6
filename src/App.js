863d14887aa784fb498d54c5960b295ad4461594
theNum1Developer <thenum1developer@gamil.com>
2023-10-25 09:48:54 +0900

    feat: (게임 설정) 세 자리 숫자 랜덤 생성 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import * as MissionUtils from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+    constructor() {
+        this.randomNumber = [];
+    }
+
+    initGame() {
+        this.randomNumber.length = 0;
+        while (this.randomNumber.length < 3) {
+            const number = MissionUtils.Random.pickNumberInRange(1, 9);
+            if (!this.randomNumber.includes(number))
+                this.randomNumber.push(number);
+        }
+    }
 }
 
 export default App;
