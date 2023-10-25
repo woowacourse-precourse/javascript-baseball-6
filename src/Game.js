cbe09a9d8dc31780ee7b2df746dca79ffb913735
hojkim77 <rlaghwnd4534@gmail.com>
2023-10-25 13:49:34 +0900

    [fix] 게임Class - 불필요 코드 제거 및 비동기 처리를 위한 getInput(), getOutput() 분리

--- src/Game.js
+++ src/Game.js
@@ -1,51 +1,56 @@
 import { Console } from "@woowacourse/mission-utils";
 import Computer from "./Computer.js";
 class Game {
-  constructor(computer) {
+  constructor(computer, result) {
     this.computer = computer;
     this.isStart = 1;
     this.isSuccess = false;
+    this.result = result;
   }
 
   async gameStart() {
     this.computer = new Computer();
-    while (this.isSuccess == false) await this.getInput();
+    while (this.isSuccess == false) {
+      await this.getInput();
+      await this.getOutput(this.result);
+    }
   }
 
   async getInput() {
     const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
     this.inputController(input);
-    return;
   }
 
   inputController(input) {
-    if (input.length !== 3) throw new Error("[ERROR]game", input.value);
+    if (input.length !== 3) throw new Error("[ERROR]game");
     if (!/^[1-9]{3}$/.test(input)) throw new Error("[ERROR]game");
 
     const newInput = input.split("").map(Number);
     const result = this.computer.resultGenerator(newInput);
 
-    this.getOutput(result);
+    this.result = result;
   }
 
   async getOutput(result) {
     if (result.strike == 3) {
       Console.print("3스트라이크");
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       const endSelector = await Console.readLineAsync(
-        "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
       );
       if (endSelector != "1" && endSelector != "2")
         throw new Error("[ERROE]end");
-      this.isStart = endSelector;
-      this.isSuccess = true;
-      return;
-    } else {
-      const printBall = result.ball ? `${result.ball}볼 ` : "";
-      const printStrike = result.strike ? `${result.strike}스트라이크` : "";
-      Console.print(
-        result.strike + result.ball ? printBall + printStrike : "낫싱".trim()
-      );
+      else {
+        this.isStart = endSelector;
+        this.isSuccess = true;
+        return;
+      }
     }
+    const printBall = result.ball ? `${result.ball}볼 ` : "";
+    const printStrike = result.strike ? `${result.strike}스트라이크` : "";
+    Console.print(
+      result.strike + result.ball ? printBall + printStrike : "낫싱"
+    );
   }
 }
 
