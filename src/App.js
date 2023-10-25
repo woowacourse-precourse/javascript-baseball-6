fbae55c30a43f01ace9beabb78e8db639b549d17
Jinhyung Lee <luciancah@gmail.com>
2023-10-25 15:46:19 +0900

    feat: 컴퓨터 생성 난수 / 사용자 입력 및 검증 함수 추가
    - inputValidaton 파일에 위치
    - 3자리 컴퓨터 난수 생성하는 generateComputerNumber() 추가
    - 사용자 입력 받는 getUserInput() 추가
    - 사용자 입력 주어진 조건에 따라 검증하는 validateInput() 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { printMessage } from "./utils/messages.js";
 class App {
-  async play() {}
+  async play() {
+    printMessage("숫자 야구 게임을 시작합니다.");
+
+    while (true) {
+      const computerNumber = generateComputerNumber();
+      let result = { strike: 0, ball: 0 };
+    }
+  }
 }
 
 export default App;
