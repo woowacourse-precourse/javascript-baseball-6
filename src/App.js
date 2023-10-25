98c36a936b11d7f9ce6a0ec64ec741cb08c14ff3
SWBAE0501 <tkddnjsgla@naver.com>
2023-10-25 14:58:21 +0900

    FIX: test  INPUT/OUTPUT case fixed

--- src/App.js
+++ src/App.js
@@ -1,77 +1,84 @@
-import { Random, Console } from "@woowacourse/mission-utils";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    Console.print("숫자 야구 게임을 시작합니다.");
-    
-    while (true) {
-      const secretNumber = this.generateRandomNumber();
-      let attempts = 0;
-
-      while (true) {
-        const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
-        const guess = input.split("").map(Number);
-
-        if (guess.length !== 3 || guess.some(isNaN)) {
-          throw new Error("[ERROR] 숫자가 잘못된 형식입니다. 게임 종료");
-        }
-
-        const [strikes, balls] = this.calculateScore(secretNumber, guess);
-
-        if (strikes === 3) {
-          Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-          break;
-        }
-
-        let feedback = "";
-        if (strikes > 0) {
-          feedback += `${strikes}스트라이크 `;
-        }
-        if (balls > 0) {
-          feedback += `${balls}볼`;
-        }
-        if (strikes === 0 && balls === 0) {
-          feedback += "낫싱";
-        } 
-        Console.print(feedback);
-
-        attempts++;
-      }
-
-      const playAgain = await Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
-      if (playAgain === "2") {
-        return;
-      } else if (playAgain !== "1") {
-        throw new Error("[ERROR] 1 또는 2를 입력하세요. 게임 종료");
+    this.generateRandomNumber();
+    MissionUtils.Console.print("숫자야구게임 시작");
+    return this.game();
+  }
+  
+  async generateRandomNumber() {
+    const RandNUM = [];
+    while (RandNUM.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!RandNUM.includes(number)) {
+        RandNUM.push(number);
       }
     }
+    this.RandNUM = RandNUM;
+    return 0;
   }
 
+  async playerGuess() {
 
+    const key_input = await MissionUtils.Console.readLineAsync(
+      "숫자를 입력해주세요 : "
+    );
+    const input = key_input.split("").map(Number);
+    if ( input.some(isNaN) || input.length != 3 || input.includes(0)) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다. 게임 종료");
+    } else {
+      let arr = [];
+      for (let i = 0; i < 3; i++) arr.push(input[i]);
+      return arr;
+    }
+  }
+  async game() {
+    const input = await this.playerGuess();
+    const ANSWER = [...this.RandNUM];
+    let strike = 0;
+    let ball = 0;
 
-  generateRandomNumber() {
-    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const result = [];
     for (let i = 0; i < 3; i++) {
-      const digit = Random.pickNumberInRange(1, numbers.length) - 1;
-      result.push(numbers.splice(digit, 1)[0]);
+      if (ANSWER.includes(input[i])) ball++;
     }
-    return result;
+    for (let i = 0; i < 3; i++) {
+      if (ANSWER[i] == input[i]) strike++;
+    }
+    ball = ball - strike;
+    await this.Umpire(strike, ball);
+    if (strike == 3) return this.playAgain();
+    else return this.game();
+  }
+  async playAgain() {
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+    const INPUT = await MissionUtils.Console.readLineAsync(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    );
+    if (INPUT == 1) {
+      this.generateRandomNumber();
+      return this.game();
+    } else if (INPUT == 2) {
+      MissionUtils.Console.print("게임 종료");
+      return;
+    } else {
+      throw new Error("[ERROR] 1 또는 2를 입력하세요. 게임 종료");
+    }
+  }
+  async Umpire(strike, ball) {
+    let message = "";
+  
+    if (strike === 0 && ball === 0) {
+      message = "낫싱";
+    } else {
+      if (ball > 0) message += `${ball}볼 `;
+      if (strike > 0) message += `${strike}스트라이크`;
+    }
+    MissionUtils.Console.print(message);
+    return;
   }
 
-  calculateScore(secret, guess) {
-    let strikes = 0;
-    let balls = 0;
-    for (let i = 0; i < 3; i++) {
-      if (secret[i] === guess[i]) {
-        strikes++;
-      } else if (secret.includes(guess[i])) {
-        balls++;
-      } else {
-        continue;
-      }
-    }
-    return [strikes, balls];
-  }
 }
+
 export default App;
+
