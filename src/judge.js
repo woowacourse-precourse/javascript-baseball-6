04626dcf42a4cecc71edeb21d477143094518146
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-10-25 22:55:03 +0900

    feature: 점수 출력하기

--- src/judge.js
+++ src/judge.js
@@ -24,5 +24,16 @@ export async function Judge(answer, userNumbers) {
 		});
 	}
 
+	//점수 출력해주기
+	if (strike === 0 && ball === 0) {
+		MissionUtils.Console.print("낫싱");
+	} else if (strike === 0) {
+		MissionUtils.Console.print(`${ball}볼`);
+	} else if (ball === 0) {
+		MissionUtils.Console.print(`${strike}스트라이크`);
+	} else if (strike > 0 && ball > 0) {
+		MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+	}
+
 	return false;
 }
