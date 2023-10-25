04b05c47dd7f5ba851a438232c6531b18cd7739f
01McCoy <unogle12@gmail.com>
2023-10-25 18:09:37 +0900

    chore: isValid 함수 userInputList 숫자형 변환

--- src/isValid.js
+++ src/isValid.js
@@ -1,18 +1,15 @@
 export function isValid(userInput) {
   const numbers = new Set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-  const userInputList = String(userInput).split("");
+  const userInputList = String(userInput)
+    .split("")
+    .map((element) => Number(element));
   const userInputLength = userInputList.length;
 
-  // 사용자 입력이 3자리수인지 확인
   if (userInputLength !== 3) return false;
 
-  // 사용자 입력이 숫자로만 이뤄졌는지 확인
   for (let num of userInputList) {
-    // String 을 Number 로 형변환
-    num = +num;
     if (!numbers.has(num)) return false;
   }
 
-  // 사용자 입력이 3자리의 숫자로만 이뤄졌다면 isVaild = true
   return true;
 }
