91a400397b2ec6acdcf00e2b95ea39e67628e7cd
jwoo08 <charm_ing0@naver.com>
2023-10-25 22:50:23 +0900

    fix: 스트라이크, 볼 카운트 로직 수정

--- src/Game.js
+++ src/Game.js
@@ -4,45 +4,41 @@ import { createNumber } from "./createNumber.js";
 class Game {
   constructor() {
     this.computer = createNumber();
+    this.ball = 0;
+    this.strike = 0;
+  }
+  resetCount() {
+    this.ball = 0;
+    this.strike = 0;
   }
 
-  countBall(numbers) {
-    let ball = 0;
-    numbers.map((num, idx) => {
-      if (num === this.computer[idx]) {
-        ball++;
+  countBallandStrike(user) {
+    this.computer.map((num, idx) => {
+      if (num === user[idx]) {
+        this.strike++;
+      } else if (this.computer.includes(user[idx])) {
+        this.ball++;
       }
     });
-    return ball;
-  }
-
-  countStrike(numbers) {
-    let strike = 0;
-    numbers.map((num, idx) => {
-      if (this.computer.includes(num) && this.computer.indexOf(num) != idx) {
-        strike++;
-      }
-    });
-    return strike;
   }
 
   checkNumbers(numbers) {
-    let ball = this.countBall(numbers);
-    let strike = this.countStrike(numbers);
     console.log("computer: ", this.computer);
+    this.resetCount();
+    this.countBallandStrike(numbers);
 
-    if (strike === 3) {
+    if (this.strike === 3) {
       Console.print("3스트라이크");
       Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       return true;
-    } else if (strike === 0 && ball === 0) {
+    } else if (this.strike === 0 && this.ball === 0) {
       Console.print("낫싱");
-    } else if (strike > 0 && ball > 0) {
-      Console.print(`${ball}볼 ${strike}스트라이크`);
-    } else if (strike === 0 && ball > 0) {
-      Console.print(`${ball}볼`);
-    } else if (strike > 0 && ball === 0) {
-      Console.print(`${strike}스트라이크`);
+    } else if (this.strike > 0 && this.ball > 0) {
+      Console.print(`${this.ball}볼 ${this.strike}스트라이크`);
+    } else if (this.strike === 0 && this.ball > 0) {
+      Console.print(`${this.ball}볼`);
+    } else if (this.strike > 0 && this.ball === 0) {
+      Console.print(`${this.strike}스트라이크`);
     }
     return false;
   }
