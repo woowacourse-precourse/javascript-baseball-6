701893e08452777a4e4647c832abf8228ffca633
devkyoung2 <devkyoung2@gmail.com>
2023-10-25 18:24:08 +0900

    refactor : 볼/스트라이크 판별 로직 수정
    삼항연산자 대신 if-else문을 사용하여 가독성 향상

--- src/App.js
+++ src/App.js
@@ -56,13 +56,15 @@ const printEnd = () =>
   Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
 const printResult = (ball, strike) => {
-  ball === 0 && strike === 0
-    ? Console.print("낫싱")
-    : ball === 0
-    ? Console.print(`${strike}스트라이크`)
-    : strike === 0
-    ? Console.print(`${ball}볼`)
-    : Console.print(`${ball}볼 ${strike}스트라이크`);
+  if (ball === 0 && strike === 0) {
+    Console.print("낫싱");
+  } else if (ball === 0) {
+    Console.print(`${strike}스트라이크`);
+  } else if (strike === 0) {
+    Console.print(`${ball}볼`);
+  } else {
+    Console.print(`${ball}볼 ${strike}스트라이크`);
+  }
 };
 
 class App {
@@ -76,7 +78,7 @@ class App {
   }
 }
 
-// const app = new App();
-// app.play();
+const app = new App();
+app.play();
 
 export default App;
