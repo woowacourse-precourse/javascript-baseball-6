48afaf6fd2e3b445dc71138a4023f2e2b0991eba
Da-Youn <dayoun202@gmail.com>
2023-10-25 23:34:08 +0900

    feat : 게임 시작

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  constructor() {
+    this.answer = null;
+  }
+
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    this.answer = this.generateAnswer();
+    await this.runGame();
+  }
+
 }
 
 export default App;
