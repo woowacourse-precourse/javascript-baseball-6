700481ac2598c8e30058bc10d3a1755ac125ec7f
Si Hyun Lee <pongpongi@Pongpongbook.local>
2023-10-25 11:30:17 +0900

    :sparkles: Looping within the game added
    Loop questions until user gets three strike out!

--- src/App.js
+++ src/App.js
@@ -30,18 +30,21 @@ function compareNumbers(computerAnswer, userAnswer) {
 
   if (strike === 3) {
     Console.print(
-      `${strike}스트라이크 \n 3개의 숫자를 모두 맞히셨습니다! 게임 종료`,
+      `${strike}스트라이크 \n3개의 숫자를 모두 맞히셨습니다! 게임 종료`,
     );
-    return;
+    return { strike, ball };
   }
 
   if (strike === 0 && ball === 0) {
     Console.print('낫싱');
-    return;
+    return { strike, ball };
   }
 
-  if (ball > 0) Console.print(`${ball}볼`);
-  if (strike > 0) Console.print(`${strike}스트라이크`);
+  if (ball > 0 && strike == 0) Console.print(`${ball}볼`);
+  if (ball > 0 && strike !== 0) Console.print(`${ball}볼 ${strike}스트라이크`);
+  if (strike > 0 && ball == 0) Console.print(`${strike}스트라이크`);
+  if (strike > 0 && ball !== 0) Console.print(`${ball}볼 ${strike}스트라이크`);
+  return { strike, ball };
 }
 
 function isValidInput(input) {
@@ -68,12 +71,16 @@ class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
     const computerAnswer = createComputerNumber();
-
-    try {
-      const userAnswer = await requestUserAnswer();
-      compareNumbers(computerAnswer, userAnswer);
-    } catch (error) {
-      Console.print(error);
+    let strike = 0;
+    // loop 추가
+    while (strike !== 3) {
+      try {
+        const userAnswer = await requestUserAnswer();
+        const result = compareNumbers(computerAnswer, userAnswer);
+        strike = result.strike;
+      } catch (error) {
+        Console.print(error);
+      }
     }
   }
 }
