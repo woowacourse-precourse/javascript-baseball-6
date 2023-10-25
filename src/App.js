61bace8c85032b29db05d124dba74f3a407c7ac4
hyun812 <sh980812@naver.com>
2023-10-25 12:17:54 +0900

    [refactor] 구조 수정

--- src/App.js
+++ src/App.js
@@ -4,36 +4,38 @@ import { getComputerNumber, compareNum, printHint } from './Computer.js';
 
 class App {
   async play() {
-    Console.print("숫자 야구 게임을 시작합니다.");
-    let computerNum = getComputerNumber(); 
     while(true){
-      try{
-        const userNum = await Console.readLineAsync("숫자를 입력해주세요 : ");
-        checkNum(userNum);
-        const result = compareNum(computerNum, userNum);
-        printHint(result.strike, result.ball);
+      Console.print("숫자 야구 게임을 시작합니다.");
+      let computerNum = getComputerNumber(); 
 
-        if(result.strike === 3){
-          Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-          const restart = await Console.readLineAsync(
-            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
-          );
-
-          if(restart === "2"){
+      while(true){
+        try{
+          const userNum = await Console.readLineAsync("숫자를 입력해주세요 : ");
+          checkNum(userNum);
+          const result = compareNum(computerNum, userNum);
+          printHint(result.strike, result.ball);
+  
+          if(result.strike === 3){
+            Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             break;
-          }else{
-            computerNum = getComputerNumber();
           }
+        }catch (error){
+          throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
         }
-
-      }catch (error){
-        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      }
+      
+      const restart = await Console.readLineAsync(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+      );
+      
+      if(restart === "2"){
+        break;
       }
     }
   }
 }
 
 const app = new App();
-app.play();
+app.play()
 
 export default App;
