ca86e87365ee8d5bcba76e84750827b79d7cb3b8
kim-young-dong <opa9904@gmail.com>
2023-10-25 15:35:38 +0900

    style: 문자열에 사용된 큰따옴표를 작은따옴표로 변경

--- src/App.js
+++ src/App.js
@@ -67,10 +67,10 @@ class App {
   // 게임 종료 후 재시작 여부 확인
   async askReplay() {
     const replay = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
-    if (replay === "1") {
+    if (replay === '1') {
       Console.print(replay);
       this.play();
-    } else if (replay === "2") {
+    } else if (replay === '2') {
       Console.print(replay);
       Console.print('게임을 종료합니다.');
     } else {
