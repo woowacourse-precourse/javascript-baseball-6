dd649f9ff8675b716641efbf49eee13a14d208e9
uujeen <rkddmlwls2@gmail.com>
2023-10-25 14:42:03 +0900

    feat: 컴퓨터가 서로 다른 3자리 숫자를 갖는다. 1. 사용할 상수를 변수처리한다. 2. 서로 다른 숫자를 갖기 위해 검사하고 배열에 넣는다.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import { MissionUtils, Console } from '@woowacourse/mission-utils';
 
+const MINIMUMNUMBER = 1;
+const MAXIMUMNUMBER = 9;
 const MESSAGE = {
   Start: '숫자 야구 게임을 시작합니다.',
 };
 class App {
+  constructor() {
+    this.computerHasNumber = [];
+  }
   async play() {
     Console.print(MESSAGE.Start);
+    this.setComputerHasNumber();
+  }
+
+  setComputerHasNumber() {
+    while (this.computerHasNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(
+        MINIMUMNUMBER,
+        MAXIMUMNUMBER
+      );
+      if (!this.computerHasNumber.includes(number)) {
+        this.computerHasNumber.push(number);
+      }
+    }
   }
 }
 
