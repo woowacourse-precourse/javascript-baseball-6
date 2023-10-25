31d15bc4b94eb39472c0dc70200e63b311acac06
d-charlie-kim <d.charlie.kim@gmail.com>
2023-10-25 19:47:49 +0900

    Feat: 정답 숫자 세팅 기능

---
+++ src/constants/constants.js
@@ -0,0 +1,13 @@
+export const RESTART_SIGN = "1";
+export const EXIT_SIGN = "2";
+
+export const MIN = 1;
+export const MAX = 9;
+
+export const ANSWER_LENGTH = 3;
+export const INPUT_LENGTH = 3;
+export const OUT_OF_RANGE_NUMBER = 0;
+
+export const MAX_STRIKE = 3;
+
+export const NUMBER_REGEXP = /^\[0-9]+$/;
--- src/model/Answer.js
+++ src/model/Answer.js
@@ -1,3 +1,21 @@
-class Answer {}
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { ANSWER_LENGTH, MIN, MAX } from "../constants/constants";
+
+class Answer {
+  #computerNum;
+
+  constructor() {
+    this.#computerNum = [];
+  }
+
+  setAnswer() {
+    while (this.#computerNum.length < ANSWER_LENGTH) {
+      let number = MissionUtils.Random.pickNumberInRange(MIN, MAX);
+      if (!this.#computerNum.includes(number)) {
+        this.#computerNum.push(number);
+      }
+    }
+  }
+}
 
 export default Answer;
