c60664ecd187af8965635744e53b862e7d9e0f92
gyucheolshin <nsfsgc2295@gmail.com>
2023-10-25 15:44:46 +0900

    fix : 입력 방법, 점수계산 방법, Throw 예외 수정

--- src/App.js
+++ src/App.js
@@ -1,74 +1,53 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  async play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    this.gameStart();
-  }
-
-  gameStart() {
-    this.setComputerNumber();
-    this.setUserNumber();
-  }
-
-  async setUserNumber() {
-    const USER_NUMBER = await MissionUtils.Console.readLineAsync(
-      "숫자를 입력해주세요 : "
-    );
-
-    this.calculateScore(this.computer, USER_NUMBER);
-  }
-
-  setComputerNumber() {
+  async setComputerNumber() {
     const computer = [];
     while (computer.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!computer.includes(number)) {
         computer.push(number);
       }
+      this.computer = computer;
     }
-    this.computer = [...computer];
+    return;
   }
 
   duplicateNumber(answer) {
     const userAnswer = [...new Set(answer)];
-    if (userAnswer.length !== 3) return false;
-    return true;
+    return userAnswer.length === 3;
   }
 
-  answerCheck(answer) {
-    if (
-      isNaN(Number(answer)) ||
-      answer.length !== 3 ||
-      !this.duplicateNumber(answer)
-    )
-      throw new Error("only number");
-    return answer.split("").map((nbr) => parseInt(nbr, 10));
-  }
-
-  reStartAnswerCheck(answer) {
-    if (answer !== "1" && answer !== "2")
-      throw new Error("잘못된 값을 입력하였습니다.");
-  }
-
-  async calculateScore(computer, answer) {
-    const USER_NUMBER = this.answerCheck(answer);
-    console.log(computer, USER_NUMBER);
-    const strikeNumber = computer.filter(
-      (number, index) => number === USER_NUMBER[index]
+  async answerCheck() {
+    const USER_NUMBER = await MissionUtils.Console.readLineAsync(
+      "숫자를 입력해주세요 : "
     );
+    if (
+      isNaN(USER_NUMBER) ||
+      USER_NUMBER.length !== 3 ||
+      !this.duplicateNumber(USER_NUMBER)
+    ) {
+      throw new Error("[ERROR]");
+    } else {
+      return USER_NUMBER.split("").map((v) => Number(v));
+    }
+  }
 
-    const ballNumber = computer
-      .filter((number) => USER_NUMBER.includes(number))
-      .filter((number) => !strikeNumber.includes(number));
+  async calculateScore() {
+    const COMPUTER_NUM = [...this.computer];
+    const USER_NUMBER = await this.answerCheck();
+    let strike = 0;
+    let ball = 0;
 
-    this.scoreDisplay(strikeNumber.length, ballNumber.length);
+    COMPUTER_NUM.forEach((value, index) => {
+      if (USER_NUMBER[index] === value) strike += 1;
+      else if (USER_NUMBER.includes(value)) ball += 1;
+    });
 
-    if (strikeNumber.length === 3) {
-      const RESET_NUMBER = await MissionUtils.Console.readLineAsync(
-        "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. "
-      );
-      this.reStart(RESET_NUMBER);
-    } else this.setUserNumber();
+    this.scoreDisplay(strike, ball);
+
+    if (strike === 3) {
+      return this.reStart();
+    } else return this.calculateScore();
   }
 
   scoreDisplay(strike, ball) {
@@ -80,14 +59,25 @@ class App {
     return MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
   }
 
-  async reStart(answer) {
-    this.reStartAnswerCheck(answer);
-    if (answer === "1") this.gameStart();
-    else return;
+  async reStart() {
+    const RESET_NUMBER = await MissionUtils.Console.readLineAsync(
+      "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    );
+    if (RESET_NUMBER === "1") {
+      this.setComputerNumber();
+      return this.calculateScore();
+    } else if (RESET_NUMBER === "2") {
+      MissionUtils.Console.print("게임 종료");
+      return;
+    } else {
+      throw new Error("[ERROR]");
+    }
+  }
+  async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.setComputerNumber();
+    return this.calculateScore();
   }
 }
-
-const test = new App();
-test.play();
 
 export default App;
