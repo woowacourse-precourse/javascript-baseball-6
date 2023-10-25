015f2d128e23adbe3aac32eb8e733dedead40af3
eternalclash <suchasucha@naver.com>
2023-10-25 16:04:46 +0900

    Refactor: 문자열 상수화

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
-import { NOTHING, STRIKE, BALL, END_OR_RESET_MESSAGE } from "./Utills.js";
+import { NOTHING, STRIKE, BALL, END_OR_RESET_MESSAGE, END_MESSAGE, CORRECT_MESSAGE, ERROR_BASE_WORD, INPUT_MESSAGE, START_MESSAGE } from "./Utills.js";
 class App {
   constructor() {
     this.init();
   }
   //비동기를 많이 배웁니다
   init() {
-    Console.print("숫자 야구 게임을 시작합니다.");
+    Console.print(START_MESSAGE);
     this.answer = this.makeAnswer();
   }
 
@@ -22,7 +22,7 @@ class App {
   }
 
   async getNumberInput() {
-    let numberInput = await Console.readLineAsync("숫자를 입력해주세요 : ");
+    let numberInput = await Console.readLineAsync(INPUT_MESSAGE);
     const result = this.validateInput(numberInput);
     if (result != "통과") throw new Error(result);
     return numberInput.split("");
@@ -59,31 +59,31 @@ class App {
       if (strikeResult == 3) return await this.endOrReset();
       this.makeHint(strikeResult, ballResult);
     } catch (error) {
-      throw new Error("[ERROR]" + error); //Error를 던지는 것이랑 콘솔의 차이
+      throw new Error(ERROR_BASE_WORD + error); //Error를 던지는 것이랑 콘솔의 차이
     }
     return await this.play();
   }
 
   validateInput(number) {
     const inputToSet = new Set(number.split("").map(Number));
-    if (number.length !== 3) return "입력값은 세자리 수를 입력해주세요.";
+    if (number.length !== 3) return ERROR_MAKE_THREE;
     if ([...inputToSet].length !== 3) {
-      return "중첩되지 않는 세자리 수를 입력해주세요.";
+      return ERROR_DUPLICATE_NUMBER;
     }
-    if (number.includes(" ")) return "공백은 넣지 말아주세요.";
-    if (Number.isNaN(number)) return "숫자만 입력해주세요.";
+    if (number.includes(" ")) return ERROR_BLANK_INPUT;
+    if (Number.isNaN(number)) return ERROR_ONLY_NUMBER ;
 
     return "통과";
   }
 
   async endOrReset() {
-    Console.print("3스트라이크");
+    Console.print(CORRECT_MESSAGE);
     const endMessage = await Console.readLineAsync(END_OR_RESET_MESSAGE);
     if (endMessage == 1) {
       this.answer = this.makeAnswer();
       await this.play();
     } else {
-      Console.print("게임 종료");
+      Console.print(END_MESSAGE);
     }
   }
 }
