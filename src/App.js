c0cf9d1914e174a3b8700b370344da817eaf4773
2NNS-V <msy3824@naver.com>
2023-10-25 23:37:32 +0900

    feat: 게임 재시작 여부 확인

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import { MissionUtils, Console } from "@woowacourse/mission-utils";
 
 class App {
+
+  constructor() {
+    this.computer = this.getRandomNum();
+    this.running = True;
+  }
+  
   async play() { // 게임 실행
     Console.print("숫자 야구 게임을 시작합니다. ");
 
-    while ( 
+    while (this.running) {
       await this.getUserNum(this.getRandomNum())
-    );
+    }
+
+    const [strike, ball] = this.CompareNum(input, random);
+    this.printResult(strike, ball);
+    
+    if (strike === 3) {
+      this.running = False;
+      Console.print('$3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      await this.Replay();
+    }
   }
 
   getRandomNum() { // 서로 다른 임의의 수 생성
@@ -29,9 +44,6 @@ class App {
         throw new Error('[ERROR] 입력값이 유효하지 않습니다.');
       }
     }
-    
-    const [strike, ball] = this.CompareNum(input, random);
-    this.printResult(strike, ball);
   }
 
   CompareNum(input, random) { // 입력받은 수와 일치여부 확인
@@ -50,7 +62,7 @@ class App {
     return [strike, ball];
   }
 
-  printResult(strike, ball) {
+  printResult(strike, ball) { // 결과 출력
     if (ball === 0 && strike === 0) {
       Console.print('낫싱');
     }
@@ -64,11 +76,20 @@ class App {
       Console.print('${ball}볼 ${strike}스트라이크');
     }
   }
+
+  async Replay() { // 재시작 여부 확인
+    const answer = await Console.readLineasync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    if (answer === "1"){
+      this.computer = this.getRandomNum();
+      this.isGameRunning = True;
+      this.play();
+    }
+    else if (answer === "2") {
+      Console.print("게임을 종료합니다.");
+    }
+    else {
+      Console.print("잘못된 값입니다. 게임이 종료됩니다.");
+    }
+  }
 }
-
-
-const app = new App();
-Console.log(app.computer);
-app.play();
-
 export default App;
