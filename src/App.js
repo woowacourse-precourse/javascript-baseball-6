258ebd1efa1352fb2a97514ca6edb55eaa2dabc0
Da-Youn <dayoun202@gmail.com>
2023-10-25 23:34:57 +0900

    feat : 게임 실행

--- src/App.js
+++ src/App.js
@@ -22,6 +22,25 @@ class App {
     return answer.join('');
   }
 
+  async runGame() {
+    let gameContinue = true;
+    while (gameContinue) {
+      const playerInput = await Console.readLineAsync('숫자를 입력해주세요 : ');
+      this.validateInput(playerInput);
+
+      const { strike, ball } = this.compareWithAnswer(playerInput);
+
+      if (strike === 3) {
+        Console.print(
+          `3스트라이크
+3개의 숫자를 모두 맞히셨습니다! 게임 종료`
+        );
+        gameContinue = await this.askForRestart();
+      } else {
+        Console.print(this.buildResultMessage({ strike, ball }));
+      }
+    }
+  }
 }
 
 export default App;
