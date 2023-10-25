e047a8293462c56fd6f5ee0ca9251d863818a217
JoSooha <dlrhdcjs@naver.com>
2023-10-25 17:11:15 +0900

    add: 예외 처리 코드 구현

--- src/App.js
+++ src/App.js
@@ -4,29 +4,42 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.")
-    while(1) {
-      const isAgain = await this.onePlay();
-      if(isAgain===2) break;
+    try {
+      MissionUtils.Console.print("숫자 야구 게임을 시작합니다.")
+      while(1) {
+        const isAgain = await this.onePlay();
+        if(isAgain==2) break;
+        if(!(isAgain==1 || isAgain==2)) throw new Error("[ERROR] 숫자 1과 2 중 하나만 입력해주세요")
+      }
+    } catch(error) {
+      console.error(error.message)
+      throw error;
     }
   }
 
   async onePlay() {
-    let computer = this.makeAnswer();
+    try {
+      let computer = this.makeAnswer();
+      console.log("computer", computer)
 
-    let isAnswer = false;
-    while(!isAnswer) {
-      const readInput = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
-      if(readInput) {
-        const [strikeNum, ballNum] = this.checkInput(readInput, computer);
-        const resultString = this.makeResultString(strikeNum, ballNum);
-        MissionUtils.Console.print(resultString);
-        if(strikeNum===3) break;
-      } else break;
+      let isAnswer = false;
+      while(!isAnswer) {
+        const readInput = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+        console.log("readInput", readInput)
+        if(readInput.length===3) {
+          const [strikeNum, ballNum] = this.checkInput(readInput, computer);
+          const resultString = this.makeResultString(strikeNum, ballNum);
+          MissionUtils.Console.print(resultString);
+          if(strikeNum===3) break;
+        } else throw new Error("[ERROR] 숫자의 개수는 3개여야 합니다.")
+      }
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+      const isAgain = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+      return isAgain
+    } catch(error) {
+      console.error(error.message);
+      throw error;
     }
-    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-    const isAgain = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
-    return isAgain
   }
 
   makeResultString(s, b) {
@@ -42,13 +55,15 @@ class App {
   }
 
   checkInput(input, answer) {
+    let inputArr = input.split("").map(Number);
+    answer = answer.map(Number);
     let strike = 0;
     let ball = 0;
     //잘못 들어온 경우 예외처리!
     for(let i=0; i<3; i++) {
-      if(input[i]===answer[i]) { //자리수가 같으면 스트
+      if(inputArr[i]===answer[i]) { //자리수가 같으면 스트
         strike++;
-      } else if(answer.includes(input[i])){ //같진 않지만 포함 시 볼 
+      } else if(answer.includes(inputArr[i])){ //같진 않지만 포함 시 볼 
         ball++;
       }
     }
@@ -73,5 +88,3 @@ class App {
 // const result = app.checkInput("132", "123")
 // console.log(result)
 export default App;
-
-// Console.readLineAsync, Console.print
