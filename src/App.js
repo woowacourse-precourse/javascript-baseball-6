69acab9cbe88474c4280d458247d24b7a7e8c7f8
Jeon Eonseok <jseokeon@gmail.com>
2023-10-25 18:18:48 +0900

    chore: 비동기 처리

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
 
-    const GENERATED_RANDOM_NUM = generateRandomNum();
+    const GENERATED_RANDOM_NUM = await generateRandomNum();
 
-    startGame(GENERATED_RANDOM_NUM);
+    await startGame(GENERATED_RANDOM_NUM);
   }
 }
 
