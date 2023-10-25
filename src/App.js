38dc20f94a58a5996700e6c0df3d31066cf122b0
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-25 19:05:12 +0900

    refactor: 앱의 게임 실행 부분을 GameManager에게 위임

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import { CREATE_RANDOM_NUMBER } from './Computer.js'
-import { PLAYER_INPUT } from './Player.js'
-import { RETURN_RESULT } from "./GameRefree.js";
+import {PLAY_GAME} from "./GameManager.js";
 class App {
   async play() {
-    //랜덤으로 값 입력
-    const RANDOM_NUMBER = CREATE_RANDOM_NUMBER();
-    console.log("컴퓨터"+RANDOM_NUMBER);
-    const PLAYER_NUMBER = (await PLAYER_INPUT()).split('').map(Number);
-    await RETURN_RESULT(PLAYER_NUMBER,RANDOM_NUMBER);
+    await PLAY_GAME();
   }
 }
 
