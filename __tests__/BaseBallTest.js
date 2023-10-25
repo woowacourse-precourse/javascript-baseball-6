bef5cb88559283a5a8317c7a3208d38f16212037
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-25 18:32:57 +0900

    test & refactor : class baseBall 테스트
    add :
    - test01 : countResult()의 결과로 받을 객체가 동일한지 확인
    refactor : randomNumbers를 baseball 생성시점에 받아오면서 로직이 변화되어 테스트 수정

--- __tests__/BaseBallTest.js
+++ __tests__/BaseBallTest.js
@@ -1,16 +1,16 @@
 import BaseBall from '../src/BaseBall';
 describe('class BaseBall test', () => {
-  let userInput;
+  let winningNumbers;
   let baseBall;
 
   beforeEach(() => {
-    userInput = 123;
-    baseBall = new BaseBall();
+    winningNumbers = 123;
+    baseBall = new BaseBall(winningNumbers);
   });
 
   describe('method test : _checkBallsAmount() ', () => {
-    test('_checkBallsAmount() password가 213이라면 ball은 2가 되는가 ?', () => {
-      baseBall._setPassword(213);
+    test('_checkBallsAmount() userInput이 932이라면 ball은 2가 되는가 ?', () => {
+      const userInput = 932;
       const ballsAmount = baseBall._checkBallsAmount(userInput);
 
       expect(ballsAmount).toBe(2);
@@ -18,8 +18,8 @@ describe('class BaseBall test', () => {
   });
 
   describe('method test : _checkStrikesAmount() ', () => {
-    test('password가 423이라면 strike는 2가 되는가 ?', () => {
-      baseBall._setPassword(423);
+    test('userInput이 423이라면 strike는 2가 되는가 ?', () => {
+      const userInput = 423;
       const strikeAmount = baseBall._checkStrikesAmount(userInput);
 
       expect(strikeAmount).toBe(2);
@@ -28,24 +28,34 @@ describe('class BaseBall test', () => {
 
   describe('method test : _checkNothing() ', () => {
     test('no ball no strike일때 true가 반환이 되는가 ?', () => {
-      baseBall._setPassword(456);
+      const userInput = 456;
       const isNothing = baseBall._checkNothing(userInput);
 
       expect(isNothing).toBe(true);
     });
 
     test('strike가 있으면 false가 반환되는가 ?', () => {
-      baseBall._setPassword(145);
+      const userInput = 156;
       const isNothing = baseBall._checkNothing(userInput);
 
       expect(isNothing).toBe(false);
     });
 
     test('ball이 있으면 false가 반환되는가 ?', () => {
-      baseBall._setPassword(345);
+      const userInput = 389;
       const isNothing = baseBall._checkNothing(userInput);
 
       expect(isNothing).toBe(false);
+    });
+  });
+
+  describe('method test : countResult', () => {
+    test('1볼 1스트라이크 일때 {ball : 1, strike : 2, isNothing : false} 가 반환 되는가', () => {
+      const userInput = 139;
+      const result = baseBall.countResult(userInput);
+      const countResult = { ball: 1, strike: 1, isNothing: false };
+
+      expect(result).toEqual(countResult);
     });
   });
 });
