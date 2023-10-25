ff477e661b2d818fe687f75bdb9827b101357846
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-25 21:45:49 +0900

    feat: define 추가

--- src/Define.js
+++ src/Define.js
@@ -1,2 +1,4 @@
 export const INPUT_MESSAGE="숫자를 입력해주세요 : ";
 export const ERROR_MESSAGE = "[ERROR] 입력값이 잘못되었습니다. ";
+export const END_GAME_MESSAGE= "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+export const RESTART_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
