81774a8bbf3e04c70e9214f8549de0aa1513308a
Jinnmidnnight <vkstk0131@yonsei.ac.kr>
2023-10-25 17:19:27 +0900

    refactor(constants): 함수 이름 대문자로 수정

--- src/constants/Conditions.js
+++ src/constants/Conditions.js
@@ -1,11 +1,11 @@
 /**
  * 입력 형식 등 여러 조건들을 정의한 객체
  */
-const Conditions = {
+const CONDITIONS = {
   RANGE: {
     MIN: 1,
     MAX: 9,
   },
 };
 
-export default Conditions;
+export default CONDITIONS;
