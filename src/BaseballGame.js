44a6d39b61ddb8172dbae7bee11e45ecda7ee650
seung <dmdgpdi@naver.com>
2023-10-25 11:23:06 +0900

    Feat: 임의의 수 3개 선택하고 저장하는 함수 구현

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -1,12 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class BaseballGame {
   #targetNumbers;
   #userNumbers;
   #strikeCnt;
   #ballCnt;
+
   constructor() {
     this.#targetNumbers = [];
     this.#userNumbers = [];
     this.#strikeCnt = 0;
     this.#ballCnt = 0;
   }
+
+  selectTargetNumbers() {
+    for (let i = 0; i < 3; i++) {
+      const randomNuber = MissionUtils.Random.pickNumberInRange(1, 9);
+      this.#userNumbers.push(randomNuber);
+    }
+  }
 }
+
+export default BaseballGame;
