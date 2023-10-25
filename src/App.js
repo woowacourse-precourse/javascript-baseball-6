0d565c0b88a4cb881b43b0df6ea70687b3e4d093
stupidJoon <junsang.yu3@gmail.com>
2023-10-25 19:21:33 +0900

    feat: add get 3 random number

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    while (
+      await this.startGame(this.getRandomNumbers())
+    );
+  }
+
+  async startGame(answer) {}
+
+  getRandomNumbers() {
+    const arr = [];
+    while (arr.length < 3) {
+      const rand = Random.pickNumberInRange(1, 9);
+      if (arr.includes(rand)) continue;
+      arr.push(rand);
+    }
+    return arr;
+  }
 }
 
 export default App;
