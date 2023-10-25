997ae8ca72cf96b33b09103f7ce240ce640aeb59
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-10-25 12:43:53 +0900

    refactor: 상수 목록에 있던 사소한 오탈자 수정

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const LOG_MESSAGE = Object.freeze({
   START_GAME: '숫자 야구 게임을 시작합니다.',
-  INPUT_NUMNER: '숫자를 입력해주세요 : ',
+  INPUT_NUMBER: '숫자를 입력해주세요 : ',
   CORRECT_END: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
   END_GAME: '게임 종료',
   RESTART_INPUT: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
