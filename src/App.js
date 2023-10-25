8a65290489f31407efde48e14bb9afb16ab24a1b
박도륜 <88328436+ryxxn@users.noreply.github.com>
2023-10-25 17:03:34 +0900

    [Add] isValid 함수 구현

--- src/App.js
+++ src/App.js
@@ -65,7 +65,32 @@ class App {
     return result.join(" ");
   }
 
-
+  /**
+   * 입력값의 유효성을 체크하는 함수
+   * @param {'1' | '2'} input 
+   * @param {boolean} restart 
+   * @returns 
+   */
+  isValid = (input, restart) => {
+    // 게임 중인 경우
+    if (restart) {
+      // input이 number가 아닌 경우
+      if (isNaN(input)) {
+        throw new Error("[ERROR] 입력값이 숫자가 아닙니다.");
+      }
+      // input이 3자리 수가 아닌 경우
+      if (input.length !== 3) {
+        throw new Error("[ERROR] 입력값이 3자리 숫자가 아닙니다.");
+      }
+      return;
+    }
+    // 게임 종료인 경우. input은 종료 여부 입력값이다.
+    // '1', '2'가 아닌 경우
+    if (!['1', '2'].includes(input)) {
+      throw new Error("[ERROR] 올바른 입력이 아닙니다.")
+    }
+    return;
+  }
 }
 
 export default App;
