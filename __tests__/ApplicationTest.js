8691dc92c7d33d4e641d73747c60f30d0541e9ab
fecapark <fecapark@gmail.com>
2023-10-25 22:07:59 +0900

    test: AnswerManager 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,6 +1,7 @@
 import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 import IOManager from "../src/IOManager.js";
+import AnswerManager from "../src/AnswerManager.js";
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -24,45 +25,49 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-describe("숫자 야구 게임", () => {
-  test("게임 종료 후 재시작", async () => {
-    // given
-    const randoms = [1, 3, 5, 5, 8, 9];
-    const answers = ["246", "135", "1", "597", "589", "2"];
-    const logSpy = getLogSpy();
-    const messages = [
-      "낫싱",
-      "3스트라이크",
-      "1볼 1스트라이크",
-      "3스트라이크",
-      "게임 종료",
-    ];
+describe("AnswerManager 테스트", () => {
+  let answerManager = null;
 
-    mockRandoms(randoms);
-    mockQuestions(answers);
-
-    // when
-    const app = new App();
-    await expect(app.play()).resolves.not.toThrow();
-
-    // then
-    messages.forEach((output) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-    });
+  beforeEach(() => {
+    answerManager = AnswerManager.getInstance();
   });
 
-  test("예외 테스트", async () => {
-    // given
-    const randoms = [1, 3, 5];
-    const answers = ["1234"];
+  it("1~9 생성 테스트", () => {
+    for (let i = 0; i < 100; i++) {
+      const number = parseInt(answerManager.generateRandomNonZeroDigit());
+      expect(number).toBeGreaterThanOrEqual(1);
+      expect(number).toBeLessThanOrEqual(9);
+    }
+  });
 
-    mockRandoms(randoms);
-    mockQuestions(answers);
+  it("답 제작 테스트", () => {
+    answerManager.setAnswer();
 
-    // when & then
-    const app = new App();
+    expect(answerManager.answer.length).toBe(answerManager.ANSWER_LENGTH);
 
-    await expect(app.play()).rejects.toThrow("[ERROR]");
+    for (let eachNumber of answerManager.answer) {
+      eachNumber = parseInt(eachNumber);
+      expect(eachNumber).toBeGreaterThanOrEqual(1);
+      expect(eachNumber).toBeLessThanOrEqual(9);
+    }
+  });
+
+  it("볼, 스트라이크 개수 구하기 테스트", () => {
+    answerManager.answer = "425";
+
+    const inputs = ["123", "456", "789"];
+    const outputs = [
+      [0, 1],
+      [1, 1],
+      [0, 0],
+    ];
+
+    for (let idx in inputs) {
+      expect(answerManager.getJudgedCountsFor(inputs[idx])).toEqual({
+        ballCount: outputs[idx][0],
+        strikeCount: outputs[idx][1],
+      });
+    }
   });
 });
 
@@ -134,5 +139,47 @@ describe("IOManager 테스트", () => {
     for (let _ of errorAnswers) {
       await expect(ioManager.askNumber()).rejects.toThrow("[ERROR]");
     }
+  });
+});
+
+describe("숫자 야구 게임", () => {
+  test("게임 종료 후 재시작", async () => {
+    // given
+    const randoms = [1, 3, 5, 5, 8, 9];
+    const answers = ["246", "135", "1", "597", "589", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "낫싱",
+      "3스트라이크",
+      "1볼 1스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when
+    const app = new App();
+    await expect(app.play()).resolves.not.toThrow();
+
+    // then
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("예외 테스트", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["1234"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow("[ERROR]");
   });
 });
