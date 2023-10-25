53f96a320807632751d345951f78a19719e6306e
naro_Kim <gracias9022@gmail.com>
2023-10-25 15:54:32 +0900

    refactor: 기능 4. 게임 결과 출력을 위한 ball, strike 판정 수정

--- src/components/answer/getResult.js
+++ src/components/answer/getResult.js
@@ -1,9 +1,46 @@
-// 수 입력을 토대로 스트라이크, 볼, 낫싱을 판정합니다.
-const getResult = () => {
-  let answer = [];
-  let ball = 0;
-  let strike = 0;
-  let nothing = false;
+import { ANSWER_MESSAGE, NOTHING_MESSAGE } from '../../constants';
+
+const getStrike = (computerNumbers, userNumbers) => {
+  return computerNumbers.reduce((acc, currentValue, index) => {
+    if (currentValue === userNumbers[index]) {
+      return acc + 1;
+    } else return acc;
+  }, 0);
+};
+
+const getBall = (computerNumbers, userNumbers) => {
+  return computerNumbers.reduce((acc, currentValue, index) => {
+    if (
+      currentValue !== userNumbers[index] &&
+      computerNumbers.includes(userNumbers[index])
+    ) {
+      return acc + 1;
+    } else return acc;
+  }, 0);
+};
+
+const printResult = (ball, strike) => {
+  if (strike === 3) {
+    return ANSWER_MESSAGE;
+  }
+  if (!strike && !ball) {
+    return NOTHING_MESSAGE;
+  }
+  if (!strike && ball) {
+    return `${ball}볼`;
+  }
+  if (strike && !ball) {
+    return `${strike}스트라이크`;
+  }
+  if (strike && ball) {
+    return `${ball}볼 ${strike}스트라이크`;
+  }
+};
+
+const getResult = (computerNumbers, userNumbers) => {
+  const ball = getBall(computerNumbers, userNumbers);
+  const strike = getStrike(computerNumbers, userNumbers);
+  return printResult(ball, strike);
 };
 
 export default getResult;
