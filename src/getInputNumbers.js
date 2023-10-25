3d54c420b61d952c5852f0a7275838bd9dae8cfc
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-10-25 22:54:21 +0900

    feature: 사용자 입력 값 예외처리

--- src/getInputNumbers.js
+++ src/getInputNumbers.js
@@ -5,4 +5,13 @@ export async function getInputNumbers() {
 	const userNumbers = await MissionUtils.Console.readLineAsync(
 		STRINGS.USER_INPUT
 	);
+	if (userNumbers.length !== 3) {
+		throw Error("[ERROR] 3개의 숫자를 입력해야 합니다.");
+	} else if (isNaN(userNumbers)) {
+		throw Error("[ERROR] 숫자만 입력 가능합니다.");
+	} else if (userNumbers.includes("0")) {
+		throw Error("[ERROR] 1부터 9까지의 숫자만 입력 가능합니다.");
+	} else {
+		return userNumbers.toString();
+	}
 }
