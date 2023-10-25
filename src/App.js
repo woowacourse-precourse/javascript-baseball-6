9a0dbffb7dcef4f7f0cb9ec8738a54439259e186
kwaksj329 <kwaksj329@gmail.com>
2023-10-25 11:52:41 +0900

    Feat: 예외 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 const ANSWER_LENGTH = 3;
+const NUMBER_RANGE = /^[1-9]+$/;
 
 class App {
 
+  constructor(){
+    this.answer = null;
+  }
+
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
-    const answer = this.createAnswer();
+    this.answer = this.createAnswer();
+    this.toggleGame('playing');
+  }
+
+  async toggleGame(status) {
+    switch(status){
+      case 'playing':{
+        const inputNumber = await Console.readLineAsync("숫자를 입력해주세요 : ");
+        this.validateAndThrowError(inputNumber)
+      }
+      case 'clear':
+      default: break;
+    }
   }
 
   createAnswer(){
@@ -18,6 +35,15 @@ class App {
       }
     }
     return answer.join("");
+  }
+
+  validateAndThrowError(inputNumber){
+    if (inputNumber.length !== ANSWER_LENGTH || new Set(inputNumber).size !== ANSWER_LENGTH)
+      throw new Error(`[ERROR] 1부터 9까지 서로 다른 수로 이루어진 ${ANSWER_LENGTH}자리 숫자를 입력해야 합니다.`);
+    if(!NUMBER_RANGE.test(inputNumber))
+      throw new Error(`[ERROR] 1부터 9까지의 수가 아닙니다.`);
+    if(inputNumber.includes(" "))
+      throw new Error(`[ERROR] 공백 없이 숫자를 입력해야합니다.`);
   }
 }
 
