f78d7b466e07b720063053d9b9ef2064b3754b01
JongInWon <jonginwon1203@gmail.com>
2023-10-25 15:16:16 +0900

    Style: "입력 길이 에러 메시지 문구 수정"
    3자리 초과,  3자리 미만, 띄어쓰기 포함 입력을 대비한 에러 메시지 문구로 수정

--- src/message/error/printInputLengthError.js
+++ src/message/error/printInputLengthError.js
@@ -1,5 +1,5 @@
 const printInputLengthError = () => {
-    throw new Error("[ERROR] 입력 가능한 숫자의 개수를 초과했습니다. 숫자 3개를 입력해 주세요.");
+    throw new Error("[ERROR] 숫자 3자리를 공백 없이 입력해 주세요.");
 };
 
 export default printInputLengthError;
