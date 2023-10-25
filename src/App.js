895ca5e3b90722db995c2cd7a808bbb90222d760
kimmoonju-102 <kimmun3549@naver.com>
2023-10-25 15:07:03 +0900

    Feat | 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-class App {
-  async play() {}
-}
+import { MissionUtils } from "@woowacourse/mission-utils";
 
+class App {
+  async play() {
+    await MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    await gameStart();
+  }
+}
 export default App;
+
+const gameStart = async () => {
+  const computer = await ComputerNumbers();
+  await getUserInput(computer);
+  console.log("computer", computer);
+};
+
+const ComputerNumbers = async () => {};
+
+const getUserInput = async (computer) => {};
+
+const checkInput = async (userInput) => {};
+
+const compareNumbers = async (user, computer) => {};
+
+const compareResult = async () => {};
+
+const playAgain = async () => {};
