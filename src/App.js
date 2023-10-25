58c008cf34936a0c221ccd52be994a484c3faf58
HarrySeop <parkpjs3083@naver.com>
2023-10-25 22:08:12 +0900

    feat: 사용자에게 입력받은 세자리 숫자를 유효성검사까지 하는 함수 생성

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ class App {
 
   async play() { 
     // 변수 선언
-    let CORRECT_ANSWER;
+    let GAME_END = false;
 
     // 초기 설정
     Console.print("숫자 야구 게임을 시작합니다.");
-    CORRECT_ANSWER = NewRandomAnswer();
+    const CORRECT_ANSWER = NewRandomAnswer();
+
+    while (!GAME_END) {
+    const USER_ANSWER = await InputAnswer();
+
+
+    }
   }
 
 }
@@ -24,6 +30,32 @@ function NewRandomAnswer() {
     }
   }
   return CORRECT_ANSWER;
+};
+
+async function InputAnswer() {
+  let USER_INPUT;
+  let USER_ANSWER;
+
+  USER_INPUT = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+
+  if (USER_INPUT.length !== 3) {
+    throw new Error("[ERROR] 세 자리가 아니면 안됩니다.")
+  }
+  if (isNaN(parseInt(USER_INPUT))) {
+    throw new Error("[ERROR] 숫자가 아닌 문자를 입력하시면 안됩니다.")
+  }
+
+  USER_ANSWER = Array.from(USER_INPUT).map(Number);
+
+  if (new Set(USER_ANSWER).size !== USER_ANSWER.length) {
+    throw new Error("[ERROR] 중복된 숫자를 입력하시면 압됩니다.");
+  }
+
+  if (USER_ANSWER.some(num => num < 1 || num > 9)) {
+    throw new Error("[ERROR] 1에서 9 사이의 숫자만 입력해야 합니다.");
+  }
+
+  return USER_ANSWER;
 }
 
 export default App;
