a1595050c730f1bc83e952cfbcb692903b415bc8
JongInWon <jonginwon1203@gmail.com>
2023-10-25 11:08:13 +0900

    Refactor: "printGameResult() 모듈 리팩터링"
    게임 결과를 출력하는 printGameResult() 모듈의 조건문을 리팩터링함

--- src/message/result/printGameResult.js
+++ src/message/result/printGameResult.js
@@ -4,13 +4,13 @@ const printGameResult = (winCondition, strikeCount, ballCount)=> {
     if (strikeCount === winCondition) {
         Console.print(`${winCondition}스트라이크`);
         Console.print(`${winCondition}개의 숫자를 모두 맞히셨습니다! 게임 종료`);
-    } else if (strikeCount && ballCount) {
+    } else if (strikeCount > 0 && ballCount > 0) {
         Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
-    } else if (strikeCount && !ballCount) {
+    } else if (strikeCount > 0) {
         Console.print(`${strikeCount}스트라이크`);
-    } else if (!strikeCount && ballCount) {
+    } else if (ballCount > 0) {
         Console.print(`${ballCount}볼`);
-    } else if (!strikeCount && !ballCount) {
+    } else {
         Console.print("낫싱");
     }
 };
