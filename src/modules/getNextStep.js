b0c976f99be2d239aa54f460b79a879ae837aaad
JungHun98 <j7papa@naver.com>
2023-10-25 14:22:28 +0900

    refactor: 종료여부 입력 프롬프트 수정 1 -> RESTART, 2 -> EXIT 상수 변환

--- src/modules/getNextStep.js
+++ src/modules/getNextStep.js
@@ -9,7 +9,7 @@ const isExitOrRestartCommand = (string) => {
 
 const getNextStep = async () => {
   const inputNextStepPrompt = `${MAX_NUM_LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료
-  게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.
+  게임을 새로 시작하려면 ${RESTART}, 종료하려면 ${EXIT}를 입력하세요.
   `;
   const command = await getUserInput(inputNextStepPrompt, isExitOrRestartCommand);
 
