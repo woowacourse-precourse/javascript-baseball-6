269d7e61eb8e26eac6436874ff3d34cbfc327add
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-25 20:17:48 +0900

    chore : 예치기 못한 에러 : validation 조건 변경

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,11 +2,13 @@ export const validation = {
   baseBallNumbersInputOfUser: (userInput) => {
     // if (/[^1-9]/g.test(userInput))
     //   throw new Error('[ERROR] 1-9 사이의 숫자만 입력해주세요');
-
+    if (String(userInput).match(/([1-9])/g)?.length !== 3) {
+      throw new Error('[ERROR] 1-9 사이의 숫자만 입력해주세요');
+    }
     if ([...String(userInput)].length !== 3)
       throw new Error('[ERROR] 3자리 숫자만 입력해주세요');
 
-    if (new Set([...String(userInput)]).size !== [...String(userInput)].length)
+    if (new Set([...String(userInput)]).size < [...String(userInput)].length)
       throw new Error('[ERROR] 중복되지 않은 숫자를 입력해주세요');
   },
   restartNumberInputOfUser: (userInput) => {
