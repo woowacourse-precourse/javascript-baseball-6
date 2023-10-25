8de8ec7fef1878504df270118798a95e1ee0a9e8
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-10-25 22:51:45 +0900

    chore: 상수 파일 생성

---
+++ src/constants/strings.js
@@ -0,0 +1,8 @@
+export const STRINGS = {
+	GAME_START: "숫자 야구 게임을 시작합니다.",
+	USER_INPUT: "숫자를 입력해주세요 : ",
+	THREE_STRIKE: "3스트라이크",
+	GAME_OVER: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+	ASK_REPLAY: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+	NO_REPLAY: "게임을 종료합니다.",
+};
