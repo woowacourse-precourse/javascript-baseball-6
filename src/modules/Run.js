d34626923c073e4cc648610cf2f5b7fbc48c642d
ss0526100 <ss0526100@gmail.com>
2023-10-25 22:51:41 +0900

    style: change to ''

--- src/modules/Run.js
+++ src/modules/Run.js
@@ -1,7 +1,7 @@
-import ConvertInputTo from "./ConvertInputTo.js";
-import Get from "./Get.js";
-import Print from "./Print.js";
-import Is from "./Is.js";
+import ConvertInputTo from './ConvertInputTo.js';
+import Get from './Get.js';
+import Print from './Print.js';
+import Is from './Is.js';
 
 class Run {
   static async baseball() {
