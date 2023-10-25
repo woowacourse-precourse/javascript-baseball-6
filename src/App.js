129732cf744effe298043c623a33b833b293df1c
DongjaJ <jdh981118@naver.com>
2023-10-25 21:15:36 +0900

    refactor: 메시지 상수로 분리, util함수들 파일로 분리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Console, Random } from "@woowacourse/mission-utils";
+import { GAME_MESSAGE } from "./constants.js";
+import { getResultMessage, isInValidCommand } from "./util.js";
 class App {
   computer = [];
 
@@ -13,9 +15,9 @@ class App {
   }
 
   async gameStart() {
-    const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
+    const input = await Console.readLineAsync(GAME_MESSAGE.INPUT);
     if (isInValidCommand(input)) {
-      throw new Error("[ERROR] : 올바르지 않은 입력값입니다.");
+      throw new Error(GAME_MESSAGE.INVALID_INPUT);
     }
     const inputToNumber = [...input].map((char) => +char);
     const { strike, ball } = this.computer.reduce(
@@ -39,8 +41,10 @@ class App {
     });
     Console.print(resultMessage);
 
-    if (strike === 3) {
-      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    const isCorrectAnswer = strike === 3;
+
+    if (isCorrectAnswer) {
+      Console.print(GAME_MESSAGE.END);
       return;
     }
     await this.gameStart();
@@ -48,13 +52,10 @@ class App {
 
   async play() {
     this.initComputer();
-    console.log(`answer: ${this.computer}`);
-    Console.print("숫자 야구 게임을 시작합니다.");
-
+    Console.print(GAME_MESSAGE.START);
     await this.gameStart();
-
     const command = await Console.readLineAsync(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+      GAME_MESSAGE.RESTART_OR_EXIT_GUIDE
     );
     if (command === "1") {
       this.play();
@@ -65,46 +66,8 @@ class App {
       return;
     }
 
-    throw new Error("[ERROR] : 잘못된 입력입니다!");
+    throw new Error(GAME_MESSAGE.INPUT);
   }
-}
-
-function getResultMessage({ strike, ball }) {
-  if (strike === 0 && ball === 0) {
-    return "낫싱";
-  }
-
-  return `${getBallMessage(ball)}${getStrikeMessage(strike)}`;
-}
-
-function getBallMessage(ball) {
-  return ball ? `${ball}볼 ` : "";
-}
-
-function getStrikeMessage(strike) {
-  return strike ? `${strike}스트라이크 ` : "";
-}
-
-/** TODO
- * commend: string
- *
- * 입력값이 숫자가 아닐때
- * 입력값의 길이가 3이 아닐때
- * 입력받은 세개의 숫자중 같은 숫자가 있을때
- */
-function isInValidCommand(command) {
-  const VALID_COMMAND_LENGTH = 3;
-  const uniqueCommand = new Set([...command]);
-
-  const isDuplicatedNumber = uniqueCommand.size !== VALID_COMMAND_LENGTH;
-  const isWrongLength = command.length !== VALID_COMMAND_LENGTH;
-  const isNotNumber = [...command].some((char) => isNaN(+char));
-
-  // console.log(isDuplicatedNumber, isWrongLength, isNotNumber);
-
-  return [isDuplicatedNumber, isWrongLength, isNotNumber].some(
-    (condition) => condition
-  );
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const GAME_MESSAGE = {
+  START: "숫자 야구 게임을 시작합니다.",
+  INPUT: "숫자를 입력해주세요 : ",
+  INVALID_INPUT: "[ERROR] : 올바르지 않은 입력값입니다.",
+  END: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  RESTART_OR_EXIT_GUIDE:
+    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+  RESULT_NOTHING: "낫싱",
+};
