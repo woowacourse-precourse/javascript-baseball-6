1c1340c7da416e11c5deb053d59a6da96686bc8c
seung <dmdgpdi@naver.com>
2023-10-25 10:58:47 +0900

    Feat: 클래스 멤버 변수 정의

---
+++ src/BaseballGame.js
@@ -0,0 +1,12 @@
+class BaseballGame {
+  #targetNumbers;
+  #userNumbers;
+  #strikeCnt;
+  #ballCnt;
+  constructor() {
+    this.#targetNumbers = [];
+    this.#userNumbers = [];
+    this.#strikeCnt = 0;
+    this.#ballCnt = 0;
+  }
+}
