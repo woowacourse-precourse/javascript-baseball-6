1e7b982a9f21a53672502207a4eb4b937e048933
yonainthefish <yonaisgood@gmail.com>
2023-10-25 22:01:42 +0900

    Refactor: 재시작 오류 수정

--- src/App.js
+++ src/App.js
@@ -2,40 +2,40 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 import randomComputeNum from "./components/randomComputeNum.js"
 import validateNum from "./components/validateNum.js"
 import compareNumbers from "./components/compareNum.js"
-import chooseRestart from "./components/chooseRestart.js"
+import {inputNum} from "./components/inputNum.js"
+import inputRestartNum from "./components/doneOrRestart.js"
 
-async function inputNum() {
-  const userNum = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요: ');
-  return userNum;
-}
 class App {  
-    async play() {
-      MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');    
-      let shouldContinue = true;
-      
-      while (shouldContinue) {
-        let computerNum = randomComputeNum();
-        MissionUtils.Console.print(computerNum);
+  async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');    
+    
+    let computerNum = randomComputeNum();
 
+    while(true){
       let userNum = await inputNum();
-
+      
       if(!validateNum(userNum)){
-        throw new Error("[ERROR] 잘못된 숫자 형식입니다.");
-      }
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.")
+      }      
+      // console.log(userNum)
 
-      const result = compareNumbers(computerNum, userNum);
-
-      if (result === "3스트라이크") {
-        const userChoice = await chooseRestart();
-        if (userChoice !== '1') {
-          shouldContinue = false;
+      if(compareNumbers(computerNum, userNum)){ // 3 스트라이크인 경우
+        let restartNum = await inputRestartNum();
+        if(restartNum === "1"){ // 1이면 재시작
+          computerNum = randomComputeNum();
+        }else if(restartNum === "2"){ // 2이면 종료
+          break;
+        }else{ // 이외는 에러 처리
+          throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
         }
       }
     }
   }
 }
+export default App;
+
+
 
 const app = new App()
 app.play()
 
-export default App;
