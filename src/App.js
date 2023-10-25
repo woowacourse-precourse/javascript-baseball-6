bbe716cb646073f708d60aedd4ba4706426d0765
jiyun1006 <jiyun4251@gmail.com>
2023-10-25 18:47:25 +0900

    🔋 feat : checkinputNumber 메서드 작성
    에러 사항들을 잡아주는 함수를 만들었고,
    이를 이용해서 catch로 error문을 나타내게 하였다.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const START_MESSAGE = '숫자 야구 게임을 시작합니다.';
 const INPUT_MESSAGE = '숫자를 입력해주세요 : ';
 const THREE_STRIKE_MESSAGE = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
 const RETRY_MESSAGE = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n';
+const ERROR_MESSAGE = '[ERROR] 잘못된 수의 형식입니다.'
 const NOTHING = '낫싱';
 const BALL = '볼';
 const STRIKE = '스트라이크';
@@ -11,30 +12,37 @@ const STRIKE = '스트라이크';
 class App {
   async play() {
     MissionUtils.Console.print(`${START_MESSAGE}`);
-    this.getGameResult();
-
-    
+    this.getGameResult();    
   }
 
   async getGameResult() {
     while (true) {
+      let inputNumber, retryNumber;
       const answerNumber = await this.makeAnsNumber();
       console.log(answerNumber);
       let isThreeStrike = true;
 
       while (isThreeStrike) {
-        let inputNumber = await this.makeInputNumber();
+        inputNumber = await this.makeInputNumber();
+        if (!inputNumber) break;
         let [strike, ball, out] = this.checkNumber(answerNumber, inputNumber);
         if (out === 3) MissionUtils.Console.print(`${NOTHING}`);
         else {
-          if (ball > 0) MissionUtils.Console.print(ball + `${BALL}` + strike + `${STRIKE}`);
+          if (ball > 0) {
+            if (strike === 0) {
+              MissionUtils.Console.print(ball + `${BALL}`);
+            } else {
+              MissionUtils.Console.print(ball + `${BALL}` + strike + `${STRIKE}`);
+            }
+          }
           else MissionUtils.Console.print(strike + `${STRIKE}`);
         }
         isThreeStrike = strike === 3 ? false : true;
       }
-
+      if (!inputNumber) break;
       MissionUtils.Console.print(`${THREE_STRIKE_MESSAGE}`);
-      const retryNumber = await this.inputRetry();
+      retryNumber = await this.inputRetry();
+      if (!retryNumber) break;
       if (retryNumber === 2) break;
     }
   }
@@ -52,23 +60,36 @@ class App {
 
   async makeInputNumber() {
     try {
-      let number = await MissionUtils.Console.readLineAsync(`${INPUT_MESSAGE}`);
-      number = [...number].map(el => +el);
-      return number;
-
+      let inputNumber = await MissionUtils.Console.readLineAsync(`${INPUT_MESSAGE}`);
+      this.checkInputNumber(inputNumber);
+      inputNumber = [...inputNumber].map(el => +el);
+      return inputNumber;
     } catch (error) {
-    // reject 되는 경우
+      MissionUtils.Console.print(error.message);
     }
+  }
+
+  checkInputNumber(inputNumber) {
+    if (inputNumber.length !== 3 || isNaN(inputNumber)) throw new Error(`${ERROR_MESSAGE}`);
+
   }
 
   async inputRetry() {
     try {
       let message = await MissionUtils.Console.readLineAsync(`${RETRY_MESSAGE}`);
+      this.checkRetryNumber(message);
       return +message;
     } catch (error) {
-      
+      MissionUtils.Console.print(error.message);
     }
   }
+
+  checkRetryNumber(inputNumber) {
+    if (inputNumber.length !== 1 || isNaN(inputNumber) ||
+    inputNumber < 0 || inputNumber > 2 
+    ) throw new Error(`${ERROR_MESSAGE}`);
+  }
+
 
   checkNumber(answerNumber, inputNumber) {
     let strike = 0;
