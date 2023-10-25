b13b7751829306408645c2856752b63e51e751bc
소인섭 <sis9745@gachon.ac.kr>
2023-10-25 21:00:56 +0900

    feat: 게임 종료 시, 메시지를 출력하는 기능 추가
    - 'printGameShutdownMessage' 함수로 게임 종료 시, '게임 종료' 문구 출력

---
+++ src/shutdown.js
@@ -0,0 +1,7 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+function printGameShutdownMessage() {
+  MissionUtils.Console.print('게임 종료');
+}
+
+export default printGameShutdownMessage;
