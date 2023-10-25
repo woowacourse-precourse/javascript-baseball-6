8246141f05b77d577b7750ba99f3cb77d6d50416
hyun812 <sh980812@naver.com>
2023-10-25 16:53:11 +0900

    [feat] 사용자로부터 입력 받은 숫자 유효성 검사

--- src/App.js
+++ src/App.js
@@ -9,8 +9,14 @@ class App {
     Console.print("숫자 야구 게임을 시작합니다");
     while (true) {
       this.getComputerNum();
-      
-      
+      while (true) {
+        try {
+          const input = await Console.readLineAsync("숫자를 입력해주세요: ");
+          this.checkUserNum(input);
+        } catch (error) {
+          throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+        }
+      }
     }
   }
 
