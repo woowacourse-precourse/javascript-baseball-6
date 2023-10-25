4135a21afb6ec790e56c23c3f8abda697c69931b
kwonja <kownja87@gmail.com>
2023-10-25 11:25:40 +0900

    feat 게임시작과 함께 컴퓨터가 숫자를 정한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  #COMPUTERNUMBERS=[];
+  initializeComputerNumer()
+  {
+    while (this.#COMPUTERNUMBERS.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.#COMPUTERNUMBERS.includes(number)) {
+        this.#COMPUTERNUMBERS.push(number);
+      }
+    }
+  }
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
 }
+
+const app = new App();
+app.play()
 
 export default App;
