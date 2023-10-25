ab155fb586b14acbd136b2fd7fe7ca9a87f7490a
장경빈 <wkdrudqls114@gmail.com>
2023-10-25 14:10:27 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { Random, Console } from "@woowacourse/mission-utils";
+import {Random} from "@woowacourse/mission-utils";
+import {Console} from "@woowacourse/mission-utils";
+
+// import console from "@woowacourse/mission-utils";
 
 class App {
-
   computerNumberPick() {
     const computer = [];
     while (computer.length < 3) {
@@ -12,13 +14,12 @@ class App {
     }
     return computer;
   }
-
   async play() {
-    let start_Num_Setting = 1; // 값이 1이면 루프 진행, 2이면 멈춤
+    let start_Num_Setting = 1; // 값이 1이면 게임 진행, 2이면 멈춤
     let computer = this.computerNumberPick();
     let count_Strike, count_Ball, count_Ball_And_Strike; // 스트라이크, 볼, 스트라이크 + 볼 횟수
-
-    Console.print("숫자 야구 게임을 시작합니다.");
+    
+    console.log("숫자 야구 게임을 시작합니다.");
 
     while (start_Num_Setting == 1) {
 
@@ -29,7 +30,7 @@ class App {
           throw new Error("숫자 3개를 입력하세요.");
         }
       } catch (error) {
-        Console.print("[ERROR]" + error.message);
+        console.log("[ERROR]" + error.message);
         continue;
       }
 
@@ -38,16 +39,16 @@ class App {
           throw new Error("띄어쓰기를 없애주세요.");
         }
       } catch (error) {
-        Console.print("[ERROR]" + error.message);
+        console.log("[ERROR]" + error.message);
         continue;
       }
 
-      try { // 띄어쓰기 감지
+      try { // 0을 감지
         if (user_Number.includes("0")) {
           throw new Error("0은 안됩니다.");
         }
       } catch (error) {
-        Console.print("[ERROR]" + error.message);
+        console.log("[ERROR]" + error.message);
         continue;
       }
 
@@ -56,7 +57,7 @@ class App {
           throw new Error("겹치지 않게 숫자를 입력하세요.");
         }
       } catch (error) {
-        Console.print("[ERROR]" + error.message);
+        console.log("[ERROR]" + error.message);
         continue;
       }
 
@@ -96,9 +97,9 @@ class App {
         print_Sentence = print_Sentence + count_Strike + "스트라이크";
       }
 
-      Console.print(print_Sentence);
+      console.log(print_Sentence);
       if (count_Strike == 3) {
-        Console.print(count_Strike + "개의 숫자를 모두 맞히셨습니다! 게임종료");
+        console.log(count_Strike + "개의 숫자를 모두 맞히셨습니다! 게임종료");
         start_Num_Setting = await Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         computer = this.computerNumberPick();
       }
@@ -106,7 +107,5 @@ class App {
   }
 }
 
-// const game = new App();
-// game.play();
 
 export default App;
