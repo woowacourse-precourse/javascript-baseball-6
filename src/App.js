a57ba90b21d52443d25c5cce407812233a428aaf
P.S.J <126138033+sojeong0302@users.noreply.github.com>
2023-10-25 14:31:55 +0900

    사용자로부터 숫자 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
     console.log("숫자 야구 게임을 시작합니다.");
-    this.computerNumber();//숫자 출력해보기
+    const userGuess = await this.getUserGuess();
+    this.computerNumber();
+    console.log("사용자 입력:", userGuess);
   }
 
   computerNumber() {
@@ -16,10 +18,23 @@ class App {
     }
     console.log("컴퓨터 숫자", computer);
   }
+
+  async getUserGuess() {
+    const guess = [];
+    while (guess.length < 3) {
+      const input = await MissionUtils.Console.readLineAsync(
+        "1부터 9까지의 숫자 중 서로 다른 숫자 3개를 입력하세요: "
+      );
+      const number = parseInt(input, 10);
+      if (isNaN(number) || number < 1 || number > 9 || guess.includes(number)) {
+        console.log("잘못된 입력입니다. 다시 입력하세요.");
+      } else {
+        guess.push(number);
+      }
+    }
+    return guess;
+  }
 }
 
-export default App;
-
-//실행 시키기
 const app = new App();
 app.play();
