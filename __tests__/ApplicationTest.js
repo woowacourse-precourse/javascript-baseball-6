4240687089b69ad86abce0d14cc45ac435bc664f
DESKTOP-55LJG2P\교육생47 <onmer99@gmail.com>
2023-10-25 23:38:35 +0900

    fix : 숫자야구게임 컴포넌트 구조 변경

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,124 +1,58 @@
-const MissionUtils = require('@woowacourse/mission-utils');
-const App = require('../src/App');
+import { App } from '../src/App';
+import { Console, Random } from "@woowacourse/mission-utils";
 
-const mockQuestions = answers => {
-  MissionUtils.Console.readLine = jest.fn();
-  answers.reduce(
-    (acc, input) =>
-      acc.mockImplementationOnce((question, callback) => {
-        callback(input);
-      }),
-    MissionUtils.Console.readLine,
-  );
+const mockQuestions = (inputs) => {
+  Console.readLineAsync = jest.fn();
+  Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
 };
 
-const mockRandoms = numbers => {
-  MissionUtils.Random.pickNumberInRange = jest.fn();
-  numbers.reduce(
-    (acc, number) => acc.mockReturnValueOnce(number),
-    MissionUtils.Random.pickNumberInRange,
-  );
+const mockRandoms = (numbers) => {
+  Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, Random.pickNumberInRange);
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
-  logSpy.mockClear();
+  const logSpy = jest.spyOn(Console, "print");
+  logSpy.mockImplementation(() => {});
   return logSpy;
 };
 
-describe('숫자 야구 게임', () => {
-  test('게임 종료 후 재시작', () => {
+describe("숫자 야구 게임", () => {
+  test("게임 종료 후 재시작", async () => {
+    // given
     const randoms = [1, 3, 5, 5, 8, 9];
-    const answers = ['246', '135', '1', '597', '589', '2'];
+    const answers = ["246", "135", "1", "597", "589", "2"];
     const logSpy = getLogSpy();
-    const messages = ['낫싱', '3스트라이크', '1볼 1스트라이크', '3스트라이크', '게임 종료'];
+    const messages = ["낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+  
+    const app = new App();
+    await expect(app.play()).resolves.not.toThrow();
+
+  
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("예외 테스트", async () => {
+  
+    const randoms = [1, 3, 5];
+    const answers = ["1234"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
 
     const app = new App();
-    app.play();
 
-    messages.forEach(output => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-    });
-  });
-
-  test('예외 테스트', () => {
-    const randoms = [1, 3, 5];
-    const answers = ['1234'];
-
-    mockRandoms(randoms);
-    mockQuestions(answers);
-
-    expect(() => {
-      const app = new App();
-      app.play();
-    }).toThrow();
+    await expect(app.play()).rejects.toThrow("[ERROR]");
   });
 });
-
-
-
-
-
-
-// const { App } = require('../src/index');
-// const { Console, Random } = require("@woowacourse/mission-utils");
-
-// const mockQuestions = (inputs) => {
-//   Console.readLineAsync = jest.fn();
-//   Console.readLineAsync.mockImplementation(() => {
-//     const input = inputs.shift();
-//     return Promise.resolve(input);
-//   });
-// };
-
-// const mockRandoms = (numbers) => {
-//   Random.pickNumberInRange = jest.fn();
-//   numbers.reduce((acc, number) => {
-//     return acc.mockReturnValueOnce(number);
-//   }, Random.pickNumberInRange);
-// };
-
-// const getLogSpy = () => {
-//   const logSpy = jest.spyOn(Console, "print");
-//   logSpy.mockImplementation(() => {});
-//   return logSpy;
-// };
-
-// describe("숫자 야구 게임", () => {
-//   test("게임 종료 후 재시작", async () => {
-//     // given
-//     const randoms = [1, 3, 5, 5, 8, 9];
-//     const answers = ["246", "135", "1", "597", "589", "2"];
-//     const logSpy = getLogSpy();
-//     const messages = ["낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료"];
-
-//     mockRandoms(randoms);
-//     mockQuestions(answers);
-
-//     // when
-//     const app = new App();
-//     await expect(app.play()).resolves.not.toThrow();
-
-//     // then
-//     messages.forEach((output) => {
-//       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-//     });
-//   });
-
-//   test("예외 테스트", async () => {
-//     // given
-//     const randoms = [1, 3, 5];
-//     const answers = ["1234"];
-
-//     mockRandoms(randoms);
-//     mockQuestions(answers);
-
-//     // when & then
-//     const app = new App();
-
-//     await expect(app.play()).rejects.toThrow("[ERROR]");
-//   });
-// });
