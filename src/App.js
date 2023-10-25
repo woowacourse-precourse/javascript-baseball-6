a293d406285469bf869c603e5e5cb6293ed153ef
bringvotrevin <bringvotrevin@gmail.com>
2023-10-25 22:04:14 +0900

    Feat: 비교 결과 출력

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import BaseballModel from './Model/baseballModel.js';
 import BaseballView from './View/baseballView.js';
+import {GAMEOVER, CONTINUE, RESTART} from './Util/constant.js';
 
 class App {
   constructor() {
-    this.model = new BaseballModel();
     this.printStartMessage();
   }
-
+  
   printStartMessage() {
     BaseballView.displayMessage('숫자 야구 게임을 시작합니다.');
   }
-
+  
   async play() {
-    await this.model.getUserInputNumber();
+    await this.start();
+  }
+  
+  async start() {
+    this.model = new BaseballModel();
+    await this.guessNumber();
   }
 
+  async guessNumber() {
+    await this.model.getUserInputNumber();
+    const status = this.model.checkMatchingNumber();
+    if (status === GAMEOVER) {
+      await this.askRestartOrExit();
+    } else if (status === CONTINUE) {
+      await this.guessNumber();
+    }
+  }
+
+  async askRestartOrExit() {
+  }
 }
 
-const app = new App();
-app.play();
+// const app = new App();
+// app.play();
 
 export default App;
--- src/Model/baseballModel.js
+++ src/Model/baseballModel.js
@@ -1,14 +1,15 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import BaseballView from "../View/baseballView.js";
 import validate from '../Util/validation.js';
-
+import outputFormat from '../Util/outputFormat.js';
+import {GAMEOVER, CONTINUE, RESTART} from '../Util/constant.js';
 
 class BaseballModel {
 	#randomNumber = [];
-	#userInputNumber;
+	#userInputNumber = [];
 	
 	constructor() {
-		this.generateBaseballNumber()
+		this.generateBaseballNumber();
 	}
 
 	getNumber() {
@@ -27,16 +28,30 @@ class BaseballModel {
 	async getUserInputNumber() {
 		const input = await BaseballView.getUserInput('숫자를 입력해주세요 : ');
 		validate.isNum(input);
-		this.#userInputNumber = input.split('');
+		this.#userInputNumber = input.split('').map((char) => {
+			return parseInt(char);
+		});
 		validate.sizeCheck(3, input);
 		validate.isDuplicatedNumber(input);
 	}
 
 	checkMatchingNumber() {
-
-	}
-
-	isGameOver() {
+		let ball = 0;
+		let strike = 0;
+		this.#randomNumber.forEach((number, i) => {
+			const index = this.#userInputNumber.indexOf(number);
+			if (index == -1) return ;
+			if (index == i) {
+				strike++;
+			} else {
+				ball++;
+			}
+		});
+		BaseballView.displayMessage(outputFormat(ball, strike));
+		if (strike === 3) {
+			return GAMEOVER;
+		}
+		return CONTINUE;
 	}
 }
 
