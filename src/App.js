b186935948a9883ccd862edf3e40424e87cb6be7
Seongjin Hong <hongseongjin.to@gmail.com>
2023-10-25 18:19:31 +0900

    feature: 게임의 턴 진행 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 import { IN_GAME_SETTING, IN_GAME_MESSAGE } from "../utils/Constants";
+import validateUserAnswer from "../utils/validateUserAnswer";
+import getUserScore from "../utils/getUserScore";
 
 class App {
   constructor() {
@@ -25,8 +27,26 @@ class App {
     await this.startGameTurn();
   }
 
-  // TODO: 게임의 각 턴으로써, 사용자 입력값을 받아 검증 후 카운트 결과 계산
-  async startGameTurn() {}
+  async startGameTurn() {
+    // 사용자로부터 3자리 숫자로 이루어진 입력값 수신
+    const userAnswer = await Console.readLineAsync(IN_GAME_MESSAGE.getUserAnswer);
+
+    // 입력값의 유효성 검증
+    validateUserAnswer(userAnswer);
+
+    // 검증 통과된 입력값을 정답으로 설정된 난수와 비교하여 카운트 결과 계산
+    const userScore = getUserScore(userAnswer, this.rightAnswer);
+
+    // 계산된 카운트 결과 출력
+    Console.print(convertUserScoreToMessage(userScore));
+
+    // 결과에 따른 다음 프로세스 분기 처리 후 턴 종료
+    if (userScore.strike === 3 && userScore.ball === 0) {
+      Console.print(IN_GAME_MESSAGE.rightAnswer);
+      return self.checkRestartGame();
+    }
+    return self.startGameTurn();
+  }
 
   // TODO: 게임 종료 후 사용자에게 재시작 여부를 확인
   async checkRestartGame() {}
