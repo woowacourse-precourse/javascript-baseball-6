772e9fbb4aaf5193eb0404f3150477e4ce757926
Seongjin Hong <hongseongjin.to@gmail.com>
2023-10-25 19:26:09 +0900

    fix: 사용자 답안 카운트시 사용자의 입력값 자료형 수정

--- utils/getUserScore.js
+++ utils/getUserScore.js
@@ -4,11 +4,13 @@ export default function getUserScore(userAnswer, rightAnswer) {
     strike: 0,
   };
 
+  const userAnswerNumbers = userAnswer.split("").map(Number);
+
   for (let i = 0; i < rightAnswer.length; i++) {
-    if (userAnswer[i] === rightAnswer[i]) {
+    if (userAnswerNumbers[i] === rightAnswer[i]) {
       userScore.strike++;
       continue;
-    } else if (rightAnswer.includes(userAnswer[i])) {
+    } else if (rightAnswer.includes(userAnswerNumbers[i])) {
       userScore.ball++;
     }
   }
