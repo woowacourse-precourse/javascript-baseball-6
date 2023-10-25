e16d5b4dee4ed41a89c8b68d202e61bf5fb97f14
hyunwlee-dev <hwlee3928@gmail.com>
2023-10-25 14:54:20 +0900

    feat: check inputedNumber validation from user

--- src/App.js
+++ src/App.js
@@ -11,21 +11,50 @@ class App {
       let inputedNumbers = '';
       try {
         inputedNumbers = await Console.readLineAsync('숫자를 입력해주세요 : ');
-        const countedResult = this._countStrikeAndBall(this.#answer, inputedNumbers);
-        Console.print(this._trimString(countedResult));
-        if (this._checkAnswer(countedResult.cntStrike)) {
-          Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-          const pickedType = await Console.readLineAsync(`게임을 새로 시작하려면 ${GameType.RESTART}, 종료하려면 ${GameType.END}를 입력하세요.`);
-          if (pickedType === GameType.END) {
-            this.#isPause = true;
-          } else if (pickedType === GameType.RESTART) {
-            this._initAnswer();
-          }
+      } catch (e) {
+        throw new Error(e);
+      }
+      this._checkInputedNumbersValidation(inputedNumbers);
+      const countedResult = this._countStrikeAndBall(this.#answer, inputedNumbers);
+      Console.print(this._trimString(countedResult));
+      if (this._checkAnswer(countedResult.cntStrike)) {
+        Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        const pickedType = await Console.readLineAsync(`게임을 새로 시작하려면 ${GameType.RESTART}, 종료하려면 ${GameType.END}를 입력하세요.`);
+        if (pickedType === GameType.END) {
+          this.#isPause = true;
+        } else if (pickedType === GameType.RESTART) {
+          this._initAnswer();
         }
-      } catch (error) {
-        Console.print('Error: ' + error.message);
       }
     }
+  }
+
+  _checkInputedNumbersValidation(inputedNumbers) {
+    if (inputedNumbers.length !== Rules.DIGIT_COUNT)
+      throw new Error("[ERROR] 3자리를 입력해주세요.");
+    if (this._isExistDuplication(inputedNumbers))
+      throw new Error("[ERROR] 중복된 숫자가 존재합니다.");
+    if (this._isContainZero(inputedNumbers))
+      throw new Error("[ERROR] 1 ~ 9사이의 숫자가 필요합니다.");
+  }
+
+  _isContainZero(inputedNumbers) {
+    if (inputedNumbers.split('').indexOf('0') === -1)
+      return false;
+    return true;
+  }
+
+  _isExistDuplication(inputedNumbers) {
+    let result = false;
+    inputedNumbers.split('').forEach((first, idx) => {
+      inputedNumbers.split('').forEach((second, jdx) => {
+        if (idx < jdx) {
+          if (first === second)
+            result = true;
+        }
+      })
+    })
+    return result;
   }
 
   _trimString({ cntStrike, cntBall }) {
