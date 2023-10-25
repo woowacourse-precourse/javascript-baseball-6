747590da4b95f52936bc7260ba779d90be4f8f91
shgusgh12 <https://github.com/shgusgh12>
2023-10-25 16:29:48 +0900

    feat: 추첨한 번호를 맞추는 과정 구현

--- src/App.js
+++ src/App.js
@@ -11,6 +11,18 @@ class App {
   }
   async start() {
     this.answer = this.getRandomNumber();
+    while (true) {
+      const userChoice = await Console.readLineAsync(MESSAGE.ASK_INPUT);
+      const isValid = this.checkInput(userChoice);
+      if (!isValid) {
+        throw new Error(MESSAGE.ERROR_CHOICE);
+      }
+      const [strike, ball] = this.compareWithAnswer(userChoice, this.answer);
+      Console.print(this.changeToMessage(strike, ball));
+      if (strike === 3) {
+        break;
+      }
+    }
   }
   getRandomNumber() {
     const randomNumber = [];
@@ -21,6 +33,38 @@ class App {
       }
     }
     return randomNumber;
+  }
+  checkInput(input) {
+    const inputSet = new Set(input);
+    const checkLength = input.length === 3;
+    const checkUniqueNumber = inputSet.size === 3;
+    const numberValidate = [...input].every((number) => !isNaN(Number(number)));
+    return checkLength && checkUniqueNumber && numberValidate;
+  }
+  compareWithAnswer(userChoice, answer) {
+    const choiceArray = userChoice.split('').map((input) => Number(input));
+    let [strike, ball] = [0, 0];
+    choiceArray.forEach((number, index) => {
+      if (answer.indexOf(number) === index) {
+        strike += 1;
+      } else if (answer.indexOf(number) !== -1) {
+        ball += 1;
+      }
+    });
+    return [strike, ball];
+  }
+  changeToMessage(strike, ball) {
+    let resultMessage;
+    if (strike > 0 && ball > 0) {
+      resultMessage = `${ball}볼 ${strike}스트라이크`;
+    } else if (strike > 0) {
+      resultMessage = `${strike}스트라이크`;
+    } else if (ball > 0) {
+      resultMessage = `${ball}볼`;
+    } else {
+      resultMessage = '낫싱';
+    }
+    return resultMessage;
   }
 }
 
