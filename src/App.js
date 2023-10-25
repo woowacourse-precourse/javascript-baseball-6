e54ad92430ebf95fce8496329dbd5fe145d889fc
jayming66 <mjw8941@gmail.com>
2023-10-25 23:01:31 +0900

    fix : user input 시 예외처리 추가(NaN값 등)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
-let gameResult = 0;
-
 function getRandomNumber() {
+  // computer가 가질 랜덤 수 3자리 생성
   const randomNumbers = [];
   while (randomNumbers.length < 3) {
-    // for 문으로 랜덤 값이 2개가 나오는 현상 발견
-    // 3개의 숫자
+    // for 문으로 랜덤 값이 2개가 나오는 현상 발견 -> while문으로 교체
     const randomNumber = Random.pickNumberInRange(1, 9); // 1 ~ 9의 범위
     if (randomNumbers.includes(randomNumber) != true) {
-      // 서로 다른 3개의 숫자가 되도록
+      // 서로 다른 3개의 숫자가 되도록, 중복 방지
       randomNumbers.push(randomNumber);
     }
   }
@@ -17,6 +15,7 @@ function getRandomNumber() {
 }
 
 function getGameResult(computer, user) {
+  // 두 숫자를 받아 ball, strike 갯수를 세기
   let ball = 0;
   let strike = 0;
 
@@ -35,6 +34,8 @@ function getGameResult(computer, user) {
 
 class App {
   restartResult = 1;
+  gameResult = 0;
+
   async play() {
     Console.print(`숫자 야구 게임을 시작합니다.`); // 게임 시작 문구
     while (this.restartResult === 1) {
@@ -44,23 +45,27 @@ class App {
       let ball = 0;
       let strike = 0;
 
-      while (gameResult !== 3) {
+      while (this.gameResult !== 3) {
         let userInputs = await Console.readLineAsync("숫자를 입력해주세요 : "); // 쿼리를 보내야하고, await 해줘야한다.
         userInputs = userInputs
           .split("")
           .map((element) => parseInt(element, 10)); // input을 int형으로 바꾼다.
 
         if (userInputs.length !== 3) throw new Error("[ERROR]3 Input require"); // 3개의 input인지 검증
-        const userInputSet = new Set(userInputs);
+        if (!userInputs.every((element) => element >= 1 && element <= 9)) {
+          throw new Error("[ERROR] Not valid input"); // 각 배열의 요소가 1-9의 숫자인지 검증.
+        }
+        const userInputSet = new Set(userInputs); // 중복된 숫자가 없는지 검증
 
         if (userInputSet.size !== 3) {
           throw new Error("[ERROR]duplicated input");
-        } // 중복된 숫자가 없는지 검증
+        }
 
-        [ball, strike] = getGameResult(randomNumbers, userInputs);
-        gameResult = strike;
+        [ball, strike] = getGameResult(randomNumbers, userInputs); // 두 숫자의 ball, strike 갯수
+        this.gameResult = strike;
 
         if (ball === 0 && strike === 0) {
+          // 각 케이스에 따라 결과 출력
           Console.print("낫싱");
         }
         if (ball > 0 && strike === 0) {
@@ -75,19 +80,22 @@ class App {
       }
       Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       let restartInput = await Console.readLineAsync(
+        // 게임 재시작 여부 입력
         `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`
       );
       if (restartInput === "1") {
+        // 게임 재시작
         this.restartResult = 1;
         ball = 0;
         strike = 0;
-        gameResult = 0;
+        this.gameResult = 0;
       } else if (restartInput === "2") {
+        // 게임 종료
         this.restartResult = 2;
         return;
       } else {
         this.restartResult = 0;
-        throw new Error("[ERROR]");
+        throw new Error("[ERROR] Not valid input"); // 1,2가 입력이 아닌 경우 에러처리
       }
     }
   }
