852f56a52507ddbe82f0dec53d9183d31df24bc3
uujeen <rkddmlwls2@gmail.com>
2023-10-25 16:43:30 +0900

    feat: 사용자가 숫자를 입력한다. 1. 사용자가 입력한 숫자를 검사한다. 1-1. 만약 에러가 있다면 에러 반환 1-2. 에러가 없다면 숫자 비교 2. 숫자를 비교하여 점수 판단 및 출력

--- src/App.js
+++ src/App.js
@@ -2,20 +2,35 @@ import { MissionUtils, Console } from '@woowacourse/mission-utils';
 
 const MINIMUMNUMBER = 1;
 const MAXIMUMNUMBER = 9;
+const NUMBERLENGTH = 3;
+const REGEX = /^(?!.*(\d).*\1)\d{3}$/;
 const MESSAGE = {
-  Start: '숫자 야구 게임을 시작합니다.',
+  START: '숫자 야구 게임을 시작합니다.',
+  INPUT: '숫자를 입력해주세요 : ',
+  ERROR: '[ERROR] 잘못된 형식의 입력입니다.',
+  NOTHING: '낫싱',
+  SUCCESS: '3스트라이크',
+  BALL: '볼',
+  STRIKE: '스트라이크',
 };
+
 class App {
   constructor() {
     this.computerHasNumber = [];
+    this.userHasNumber = [];
+    this.score = {
+      strike: 0,
+      ball: 0,
+    };
   }
   async play() {
-    Console.print(MESSAGE.Start);
+    Console.print(MESSAGE.START);
     this.setComputerHasNumber();
+    this.setUserHasNumber();
   }
 
   setComputerHasNumber() {
-    while (this.computerHasNumber.length < 3) {
+    while (this.computerHasNumber.length < NUMBERLENGTH) {
       const number = MissionUtils.Random.pickNumberInRange(
         MINIMUMNUMBER,
         MAXIMUMNUMBER
@@ -23,6 +38,44 @@ class App {
       if (!this.computerHasNumber.includes(number)) {
         this.computerHasNumber.push(number);
       }
+    }
+  }
+
+  async setUserHasNumber() {
+    const number = await Console.readLineAsnync(MESSAGE.INPUT);
+    if (!REGEX.test(number)) {
+      throw new Error(MESSAGE.ERROR);
+    }
+
+    this.userHasNumber = [...number].map(Number);
+
+    this.compareNumber();
+    this.printScore();
+  }
+
+  compareNumber() {
+    this.computerHasNumber.forEach((e, idx) => {
+      if (e === this.userHasNumber[idx]) {
+        this.score.strike += 1;
+      } else if (this.userHasNumber.includes(e)) {
+        this.score.ball += 1;
+      }
+    });
+  }
+
+  printScore() {
+    if (this.score.strike === 0 && this.score.ball === 0) {
+      Console.print(MESSAGE.NOTHING);
+    } else if (this.score.strike === 0 && this.score.ball !== 0) {
+      Console.print(`${this.score.ball}${MESSAGE.BALL}`);
+    } else if (this.score.strike !== 0 && this.score.ball !== 0) {
+      Console.print(
+        `${this.score.ball}${MESSAGE.BALL} ${this.score.strike}${MESSAGE.STRIKE}`
+      );
+    } else if (this.score.strike !== 0 && this.score.ball === 0) {
+      Console.print(`${this.score.strike}${MESSAGE.STRIKE}`);
+    } else {
+      Console.print(MESSAGE.SUCCESS);
     }
   }
 }
