5ae3f53dc76cb7a6398ca0d6b1b2d2b1fca802f6
Boeun <boeun0904@naver.com>
2023-10-25 23:47:20 +0900

    style: soft tab 변경

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Computer from './Computer.js';
 import User from './User.js';
 
 class App {
-    constructor() {
-        this.computer = new Computer();
-        this.user = new User();
-    }
-    async play() {
-        await this.computer.playGame(this.user);
-    }
+  constructor() {
+    this.computer = new Computer();
+    this.user = new User();
+  }
+  async play() {
+    await this.computer.playGame(this.user);
+  }
 }
 
 export default App;
--- src/User.js
+++ src/User.js
@@ -1,34 +1,34 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import Computer from './Computer';
 export default class User {
-    async getUserNumber() {
-        const input = await MissionUtils.Console.readLineAsync(
-            '숫자를 입력해주세요 : '
-        );
+  async getUserNumber() {
+    const input = await MissionUtils.Console.readLineAsync(
+      '숫자를 입력해주세요 : '
+    );
 
-        if (
-            input.length !== Computer.NUM_SIZE ||
-            input.includes('0') ||
-            Number.isNaN(input)
-        ) {
-            throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
-        }
-
-        return input;
+    if (
+      input.length !== Computer.NUM_SIZE ||
+      input.includes('0') ||
+      Number.isNaN(input)
+    ) {
+      throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
     }
 
-    async getUserReplay() {
-        const input = await MissionUtils.Console.readLineAsync(
-            '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
-        );
-        if (
-            input.length !== Computer.MODE_SIZE ||
-            (input !== Computer.REPLAY_CODE && input !== Computer.QUIT_CODE) ||
-            Number.isNaN(input)
-        ) {
-            throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
-        }
+    return input;
+  }
 
-        return input;
+  async getUserReplay() {
+    const input = await MissionUtils.Console.readLineAsync(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
+    );
+    if (
+      input.length !== Computer.MODE_SIZE ||
+      (input !== Computer.REPLAY_CODE && input !== Computer.QUIT_CODE) ||
+      Number.isNaN(input)
+    ) {
+      throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
     }
+
+    return input;
+  }
 }
