06418745b28a0f3b52364c02c9127734b7015c3a
Jinnmidnnight <vkstk0131@yonsei.ac.kr>
2023-10-25 22:37:17 +0900

    feat(controllers): matchNumber 함수 추가

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,13 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+
 import Opponent from "../models/Opponent.js";
 import User from "../models/User.js";
 
 import GAME_START_VIEW from "../views/GameStartView.js";
 import INPUT_NUMBER_VIEW from "../views/InputNumberView.js";
+import OUTPUT_VIEW from "../views/OutputView.js";
 
 import RANDOM_NUMBER from "../utils/RandomNumber.js";
 import CHECK_VALIDATION from "../utils/CheckValidation.js";
 
 import MESSAGES from "../constants/Messages.js";
+import CONDITIONS from "../constants/Conditions.js";
 
 /**
  * @class Controller
@@ -48,6 +52,33 @@ class Controller {
     if (!CHECK_VALIDATION(userInput)) throw new Error(MESSAGES.WRONG_INPUT);
     this.#user.setNumber(userInput);
     return this.#user.getNumber();
+  }
+
+  /**
+   * 상대방(컴퓨터)의 숫자와 사용자의 숫자를 비교하여 평가하는 함수
+   */
+  async matchNumber() {
+    const opponentNumberElements = this.#opponent
+      .getNumber()
+      .toString()
+      .split("")
+      .map(Number);
+
+    let strikeCount = 0;
+    let ballCount = 0;
+    while (strikeCount !== CONDITIONS.MAX_STRIKE_COUNT) {
+      const userNumber = await this.saveUserInput();
+      const userNumberElements = userNumber.toString().split("").map(Number);
+      for (let i = 0; i < CONDITIONS.NUMBER_LENGTH; i += 1) {
+        if (opponentNumberElements[i] === userNumberElements[i]) {
+          strikeCount += CONDITIONS.COUNT;
+        } else if (opponentNumberElements.includes(userNumberElements[i])) {
+          ballCount += CONDITIONS.COUNT;
+        }
+      }
+      Console.print(OUTPUT_VIEW(strikeCount, ballCount));
+    }
+    Console.print(MESSAGES.SUCCESS);
   }
 }
 
