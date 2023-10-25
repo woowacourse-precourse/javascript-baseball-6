a71e7d8b4c7b355548baab99d3ee13e256572f3c
SSY1203 <tlstjsdud566@naver.com>
2023-10-25 21:49:36 +0900

    [feature] 상수 선언

---
+++ src/Constant.js
@@ -0,0 +1,12 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+export const { Console, Random } = MissionUtils;
+
+export const GAME_START_TEXT = '숫자 야구 게임을 시작합니다.';
+export const NUMBER_OF_DIGITS = 3;
+export const PLZ_NUMBERS_INPUT = '숫자를 입력해주세요 :';
+export const STRIKE = '스트라이크';
+export const BALL = '볼';
+export const NOTHING = '낫싱';
+export const GAME_SUCCESS = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
+export const GAME_RESTART = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.';
+export const ERROR_TEXT = '[ERROR] 잘못된 입력입니다.';
