bbe060c22502c8e664ffc90b037c467b34f33214
suminson <sabi1111@nate.com>
2023-10-25 22:49:09 +0900

    feat: 컴퓨터 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
+  constructor() {
+    this.random = [];
+  }
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.randomNumber();
+  }
+
+  randomNumber() {
+    const random = [];
+    while (random.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!random.includes(number)) random.push(number);
+    }
+    this.random = random;
   }
 }
 
