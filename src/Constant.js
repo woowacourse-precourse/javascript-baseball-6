6d6859f371cdd70811261718e63da82350726083
shgusgh12 <https://github.com/shgusgh12>
2023-10-25 16:16:19 +0900

    feat : 게임 메세지 및 오류메세지 추가

---
+++ src/Constant.js
@@ -0,0 +1,12 @@
+const MESSAGE = {
+  START: '숫자 야구 게임을 시작합니다.',
+  END: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  ASK_INPUT: '숫자를 입력해 주세요 : ',
+  ASK_RESTART: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+  PLAYER_WIN: '3스트라이크',
+  ERROR_CHOICE:
+    '[ERROR] 1부터 9까지 서로 다른 수로 이루어진 3자리수를 입력하세요.',
+  ERROR_RESTART: '[ERROR] 1번 혹은 2번을 입력하세요.',
+};
+
+export default MESSAGE;
