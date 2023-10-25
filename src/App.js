3e5177f0a907cf29caae7891c1f9e461940a351b
cho-hyeonjin <gusls2005@gmail.com>
2023-10-25 20:28:12 +0900

    에러 처리 및 MD파일 생성

--- src/App.js
+++ src/App.js
@@ -1,91 +1,37 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 class App {
   async play() {
-    // ** 🖥️ 컴퓨터 클래스
-    class Computer {
-      // * 컴퓨터 숫자 랜덤 생성 메서드
-      static generateNumbers() {
-        // 컴퓨터의 숫자를 담을 배열
-        const computerNumbers = [];
-        // 컴퓨터 숫자를 담을 배열의 길이가 3이 되기 전까지만 숫자 push 작업이 반복되고, 3이 되면 더이상 작동하지 않게끔 조건 설정
-        while (computerNumbers.length < 3) {
-          // misson-utils의 Random 객체에서 pickNumberInRange 메서드를 사용해서 1~9까지의 숫자를 골라서
-          const randomNumber = Random.pickNumberInRange(1, 9);
-          // 컴퓨터 숫자를 담은 배열에 해당 값이 존재하지 않을 때에만 해당 배열에 push
-          if (computerNumbers.includes(randomNumber) === false) {
-            computerNumbers.push(randomNumber);
-          }
-
-          return computerNumbers;
-        }
-      }
-
-      // * 컴퓨터 숫자 vs 사용자 입력 숫자 비교해서 스트라이크, 볼 카운팅하는 메서드
-      static countStrikeBall(computerNumbers, gamePlayerNumbers) {
-        const counting = { strike: 0, ball: 0 }; // 카운팅 초기화
-        // computerNumbers요소 vs gamePlayerNumbers요소 인덱스별 비교 하며 문자열 연산
-        gamePlayerNumbers.forEach((el, i) => {
-          // 컴퓨터 숫자의 i번째 요소가 사용자 입력 숫자의 i번째 요소값과 일치할 때 strike+1 --> 같은 인덱스를 서로 다른 두 배열에 적용한 요소값 비교
-          if (computerNumbers[i] === el) {
-            counting.strike += 1;
-          } // 스트라이크가 아닌 경우 볼인지 확인 --> computerNumbers를 gamePlayerNumbers.length만큼 돌면서 computerNumbers에 el(각 회차에 해당하는 gamePlayerNumbers의 요소)가 포함되어 있는지 확인 後 true일 때마다 ball + 1
-          else if (computerNumbers.includes(el)) {
-            counting.ball += 1;
-          }
-        });
-        // 결과 출력 메서드에 완성된 counting객체 전달하며 호출
-        this.printResult(counting);
-        // strike가 3인 경우 result 출력 후 true 리턴 --> 이를 사용자 입력 메서드가 이어 받아 게임 종료 및 시작여부 프린트에 사용할 예정.
-        return counting.strike === 3; // true or false. false면 게임 종료 X && 게임 재시작 여부 프린트 X
-      }
-
-      // * 결과 출력 메서드
-      static printResult({ strike, ball }) {
-        // 일치하는 수 有 케이스는 문자열 연산하여 리턴
-        let result = "";
-        if (ball > 0) {
-          result += `${ball}볼`;
-        }
-        if (strike > 0) {
-          result += `${strike}스트라이크`;
-        }
-        if (strike === 0 && ball === 0) {
-          Console.print("낫싱");
-          return;
-        }
-      }
-    }
-
-    // ** 👩🏻‍💻 사용자 클래스
+    // ! 👩🏻‍💻 사용자 클래스
     class GamePlayer {
-      // * 게임 실행 메서드
+      // * 게임 실행 메서드 ✅
       static async playGame() {
         // 게임 시작을 알리는 문구 출력
         Console.print("숫자 야구 게임을 시작합니다.");
-        // 게임 진행 여부(Boolean) 담는 변수 (일종의 상태관리)
-        let isPlay = false; // 3 스트라이크 맞아서 재시작 여부 물었을 때 이 값을 true로 만들어서 아래 로직 이행
-        // isPlay 여부가 true일 때만 게임이 진행되도록
+        // 게임 진행 여부 담는 변수(Boolean, 상태관리)
+        let isPlay = false;
+        // isPlay가 true인 동안에는 게임을 계속 반복
         while (!isPlay) {
-          // 컴퓨터 클래스의 generateNumbers로 랜덤으로 생성된 숫자가 들어있는 배열을 computerNumbers변수에 담는다.
           const computerNumbers = Computer.generateNumbers();
-          // 사용자에게 숫자를 입력받는 메서드인 inputUserNumber에 컴퓨터 숫자를 인자로 넘기면서 호출한다.
-          await this.inputUserNumber(computerNumbers);
+          // 사용자에게 숫자를 입력받는 메서드에 컴퓨터가 생성한 숫자를 인자로 넘기면서 호출
+          await this.inputGamePlayerNumber(computerNumbers);
           // 재시작 및 시작 시 isPlay변수에 true값이 들어오게끔 하는 로직
-          isPlay = await this.isPlay();
+          isPlay = await this.isPlay(); // isPlay 메서드는 true or false값으로 평가된 값을 리턴함
         }
       }
 
-      // * 사용자에게 숫자를 입력받는 메서드
-      static async inputUserNumber(computerNumbers) {
-        // 3번의 스트라이크를 맞으면 아래 값이 true가 되면서 게임 종료 (일종의 상태관리)
+      // * 사용자에게 숫자를 입력받는 메서드 ✅
+      static async inputGamePlayerNumber(computerNumbers) {
+        // 3스트라이크면 3스트라이크와 종료를 알린 뒤 재시작, 종료 선택 로직으로 넘어가고, 그게 아닌 경우 게임 지속.
+        // 이를 위한 상태관리 변수(Booelan, true일 때만 게임 종료)
         let isTripleStrike = false;
-        // 3번의 스트라이크가 나오기 전까지는 아래 반복문 실행
+        // isTripleStrike가 false인 경우 계속 실행되도록
         while (!isTripleStrike) {
-          // mission-utils의 Console 객체에서 readLineAsync 메서드를 이용해서 숫자 입력 문구 출력 & 사용자가 입력한 답변을 Promise를 통해 반환
-          const userInput = await Console.readLineAsync(
+          const gamePlayerInput = await Console.readLineAsync(
             "숫자를 입력해주세요 : "
           );
-          const gamePlayerNumbers = this.userInputToArr(userInput);
+          // 사용자가 입력하는 문자열로 리턴됨
+          const gamePlayerNumbers =
+            Computer.gamePlayerInputToArr(gamePlayerInput);
           isTripleStrike = Computer.countStrikeBall(
             computerNumbers,
             gamePlayerNumbers
@@ -94,54 +40,120 @@ class App {
         Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       }
 
-      // * 게임 시작 or 종료 여부 선택 메서드
+      // * 게임 시작 or 종료 여부 선택 메서드 ✅
       static async isPlay() {
         const isPlay = await Console.readLineAsync(
           `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`
         );
-        // 1, 2 이외의 문자 입력 시 에러 발생
         if (isPlay !== "1" && isPlay !== "2") {
-          throw new Error("[ERROR] 숫자 1 또는 2만 입력할 수 있습니다.");
+          throw new Error("[ERROR] 잘못된 입력값입니다.");
         }
-        return isPlay === "2"; // true or false가 반환된다.
+        return isPlay === "2";
+      }
+    }
+
+    // ! 🖥️ 컴퓨터 클래스
+    class Computer {
+      // * 컴퓨터 숫자 랜덤 생성 메서드 ✅
+      static generateNumbers() {
+        const computerNumbers = [];
+        while (computerNumbers.length < 3) {
+          const randomNumber = Random.pickNumberInRange(1, 9);
+          if (!computerNumbers.includes(randomNumber)) {
+            computerNumbers.push(randomNumber);
+          }
+        }
+
+        return computerNumbers;
       }
 
-      // * (반복문을 돌며 컴퓨터의 숫자와 인덱스별 요소 비교를 위해) 사용자가 입력한 숫자를 배열화 하는 메서드
-      static userInputToArr(userInput) {
-        if (userInput.length !== 3) {
+      // * (반복문을 돌며 컴퓨터의 숫자와 인덱스별 요소 비교를 위해) 사용자가 입력한 숫자를 배열화 하는 메서드 ✅
+      static gamePlayerInputToArr(gamePlayerInput) {
+        if (gamePlayerInput.length !== 3) {
           throw new Error("[ERROR] 숫자는 반드시 3자리값이여야 합니다.");
         }
-        // userInput은 string이기때문에, 이를 ''기준으로 나눠서 먼저 배열로 만들고, 배열 內 각 요소를 넘버타입으로 바꾸기 위해 map(반복문을 돌며 새로운 값의 배열로 리턴하는 배열 메서드)이용한 메서드에 돌려준다.
-        const gamePlayerNumbers = this.userInputTypeCasting(
-          userInput.split("")
+        // this.gamePlayerInputToNumber에 gamePlayerInput을 '' 기준으로 잘라내서 인자로 던진다.
+        //
+        const gamePlayerNumbers = this.gamePlayerInputToNumber(
+          gamePlayerInput.split("")
         );
-        this.userNumbersValidation(gamePlayerNumbers);
+        // gamePlayerNumbers에는 3개의 숫자를 담은 배열이 들어있고, 이를 하나하나 돌면서 유효한 값인지 검사한 뒤
+        this.gamePlayerNumbersValidation(gamePlayerNumbers);
+        // 검사를 통과한 값을 리턴
         return gamePlayerNumbers;
       }
 
-      // * 사용자가 입력한 숫자는 readLindeAsync는 Promise를 통해 문자열로 반환됨, 이를 숫자로 바꾸기 위한 메서드.
-      static userInputTypeCasting(gamePlayerNumbers) {
+      // * 사용자가 입력한 숫자는 readLindeAsync를 거쳐 문자열로 반환. 이를 숫자로 바꾸기 위한 메서드 ✅
+      static gamePlayerInputToNumber(gamePlayerNumbers) {
+        // 기존 문자열이 담긴 gamePlayerNumbers에 배열 메서드 map(특정 연산을 거친 새로운 값을 배열의 요소로 push하여 새로운 배열 리턴)을 걸어서
         return gamePlayerNumbers.map((el) => {
+          // 특정 변수 선언하여 각 요소를 Number메서드를 거쳐 넘버타입으로 변환하여 담는다.
           const gamePlayerNumber = Number(el);
+          // 숫자로 변경할 수 없는 문자열을 걸러내는 과정, (ㄱ,ㄴ 등의 진짜 문자열 입력했을 때)
           if (Number.isNaN(gamePlayerNumber)) {
             throw new Error("[ERROR] 숫자가 아닙니다.");
           }
+          // 숫자로 변환된 값 리턴, map은 이 값을 매핑하여 배열로 리턴.
           return gamePlayerNumber;
         });
       }
 
-      // * userNumbers 유효성 검사 메서드
-      static userNumbersValidation(userNumbers) {
-        userNumbers.forEach((el) => {
-          // 1. 중복 숫자 존재 여부 검사 : filter로 걸러낸 배열에 담기는 값은 컴퓨터값 === 사용자값인 값. 근데 이 배열의 길이가 1보다 크면 중복된 숫자가 있다는 뜻.
-          if (userNumbers.filter((number) => number === el).length > 1) {
+      // * gamePlayerNumbers 유효성 검사 메서드 ✅
+      static gamePlayerNumbersValidation(gamePlayerNumbers) {
+        // 중복 숫자 존재 여부 검사: filter로 걸러낸 배열에 담기는 값은 컴퓨터가 낸 숫자 === 사용자가 낸 숫자 인 값. 이 배열의 길이가 1 이상이라는 것은 같은 수가 2개 이상이라는 뜻. ∴ 중복요소 존재 O
+        gamePlayerNumbers.forEach((el) => {
+          if (
+            gamePlayerNumbers.filter(
+              (gamePlayerNumber) => gamePlayerNumber === el
+            ).length > 1
+          ) {
             throw new Error("[ERROR] 중복되지 않는 숫자만 입력할 수 있습니다.");
           }
-          // 2. 범위를 벗어난 숫자 입력 여부 검사
+          // 입력 범위(1~9)를 벗어난 숫자 입력 여부 검사
           if (el < 1 || el > 9) {
             throw new Error("[ERROR] 1부터 9까지의 숫자만 입력할 수 있습니다.");
           }
         });
+      }
+
+      // * 컴퓨터 숫자 vs 사용자 입력 숫자 비교해서 스트라이크, 볼 카운팅하는 메서드 ✅
+      static countStrikeBall(computerNumbers, gamePlayerNumbers) {
+        const counting = { strike: 0, ball: 0 };
+        // computerNumbers의 요소와 gamePlayerNumbers의 요소를 공통된 인덱스로 조회하면서 비교, 값이 같은 경우 strike의 counting+1
+        gamePlayerNumbers.forEach((el, i) => {
+          if (computerNumbers[i] === el) {
+            counting.strike += 1;
+          } // 그게 아닌 경우 매 회의 el요소를 computerNumbers가 포함하고 있는지 확인. true/false로 리턴되고 true인 경우 본문이 실행되어 ball의 counting+1
+          else if (computerNumbers.includes(el)) {
+            counting.ball += 1;
+          }
+        });
+        // printResult 메서드에 counting 객체 인자로 전달하며 호출
+        this.printResult(counting);
+        return counting.strike === 3; // 이게 평가되서 true로 리턴되는 경우 inputUserNumber메서드의 isTripleStrike에 true가 할당되면서 while문을 지나 맨 아래의 Console.print("3개의 숫자를 모두 맞히셨습니다! 게임종료") 문이 실행됨.
+      }
+
+      // * 결과 출력 메서드 ✅
+      static printResult({ strike, ball }) {
+        // 결과로 출력할 문자열을 담을 변수 선은
+        let result = "";
+        // ball의 값이 0 초과면 초기 설정값 "" + `${ball}볼 ` 문자열 연산 진행되게 설정
+        if (ball > 0) {
+          result += `${ball}볼 `;
+        }
+        // strike의 값이 0 초과면 `${ball}볼 `+ `${strike}스트라이크` 문자열 연산 진행되게 설정
+        if (strike > 0) {
+          result += `${strike}스트라이크`;
+        }
+
+        // 둘 다 0이면 '낫싱'을 바로 프린트하고 리턴
+        if (strike === 0 && ball === 0) {
+          Console.print("낫싱");
+          return;
+        }
+
+        // 그게 아닌 경우 문자열 연산이 완료된 결과 result를 리턴
+        Console.print(result);
       }
     }
 
