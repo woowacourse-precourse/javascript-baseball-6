037832b93b0e19c4fc5f6097ecfc17e5283c56bb
junmiLee <junmi950918@gmail.com>
2023-10-25 16:42:18 +0900

    생성자 정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.computerNumbers = [];
+    this.userNumbers = [];
+    this.attempts = 0;
+    this.isGameOver = false;
+
+    this.generateComputerNumbers();
+  }
 }
 
 export default App;
