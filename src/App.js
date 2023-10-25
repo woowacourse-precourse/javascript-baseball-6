8c9893448a8e90e08f27247cfc39183ba1f58828
suzinxix <suzinxix@gmail.com>
2023-10-25 15:39:48 +0900

    feat: 서로 다른 3자리 수 random값 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      // 서로 다른 random값 추출
+      const computer = [];
+      while (computer.length < 3) {
+        const number = MissionUtils.Random.pickNumberInRange(1, 9);
+        if (!computer.includes(number)) {
+          computer.push(number);
+        }
+      }
+
+    } catch (error) {
+      // reject 되는 경우
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
