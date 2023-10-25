70487a8a361efe7e9e81d31db00cc4ee8357cb84
KangYunJi <dbswl701@ajou.ac.kr>
2023-10-25 16:28:14 +0900

    refactor: 종료 확인 입력 함수로 빼고 변수명 수정

--- src/App.js
+++ src/App.js
@@ -11,15 +11,12 @@ class App {
 
   setComputerNum() {
     this.computerNum = [];
-
     while (this.computerNum.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!this.computerNum.includes(number)) {
-        this.computerNum.push(number);
+      const INPUT = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.computerNum.includes(INPUT)) {
+        this.computerNum.push(INPUT);
       }
     }
-
-    console.log('computer : ', this.computerNum);
   }
 
   print(){
@@ -29,11 +26,15 @@ class App {
     else MissionUtils.Console.print(`낫싱`);
   }
 
-  errorCheck() {
+  inputStartErrorCheck() {
     if (isNaN(Number(this.userNum))) throw new Error("[ERROR] 숫자만 입력해주세요.");
     if (this.userNum.length !== 3) throw new Error("[ERROR] 3자리 숫자를 입력해주세요.");
     if (this.userNum.includes(0)) throw new Error("[ERROR] 0은 입력할 수 없습니다.");
     if (this.userNum[0] === this.userNum[1] || this.userNum[1] === this.userNum[2] || this.userNum[2] === this.userNum[0]) throw new Error("[ERROR] 서로 다른 3개의 숫자를 입력해주세요.");
+  }
+
+  inputEndErrorCheck(input) {
+    if (input != 1 && input != 2) throw new Error("[ERROR] 1 또는 2만 입력할 수 있습니다.");
   }
 
   async play() {
@@ -51,7 +52,7 @@ class App {
       this.userNum = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
 
       // 입력 확인
-      this.errorCheck();
+      this.inputStartErrorCheck();
 
       // 스트라이크, 볼 확인
       this.computerNum.map((v, idx) => {
@@ -66,15 +67,13 @@ class App {
       if (this.strike === 3) {
         MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
         gameNum = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
-        if (gameNum == 1) {
-          isEnd = false;
-          this.setComputerNum();
-        }
+        this.inputEndErrorCheck(gameNum);
+        
+        if (gameNum == 1) this.setComputerNum();
         else if(gameNum == 2) isEnd = true;
       }
-
     } 
- 
+
   }
 }
 
