dec04ff3bb18265fae018ec4acb37dfbf612801a
psst54 <psst5491@naver.com>
2023-10-25 14:07:07 +0900

    ✨ 게임 시작 & 정답 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import generateAnswerArray from "./generateAnswerArray";
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    const answer = await generateAnswerArray();
+    // console.log("[answer]", answer);
+  }
 }
 
 export default App;
