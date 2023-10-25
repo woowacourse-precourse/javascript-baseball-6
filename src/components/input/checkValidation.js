d6ac18c2157b1ff5744ccb53f11d7a07981b962b
naro_Kim <gracias9022@gmail.com>
2023-10-25 15:53:54 +0900

    refactor: 기능 3. 입력과 유효성 검사 분리

--- src/components/input/checkValidation.js
+++ src/components/input/checkValidation.js
@@ -1,11 +1,36 @@
-import { getRandomNumber } from '@components/answer';
+import { ERROR_MESSAGE } from '../../constants';
+
+const checkRange = (userNumber) => {
+  let flag = true;
+  for (let i = 0; i < userNumber.length; i++) {
+    if (userNumber[i] <= 0 && userNumber[i] > 9) flag = !flag;
+  }
+  return flag;
+};
+
+const checkLength = (userNumber) => {
+  if (userNumber.length === 3) return true;
+  else return false;
+};
+
+const checkUniqueness = (userNumber) => {
+  if (userNumber.length === [...new Set(userNumber)].length) return true;
+  else return false;
+};
 
 // 유효한 입력인지 검사합니다.
-const checkValidation = (input) => {
-  const target = getRandomNumber();
-  let valid = true;
-
-  return valid;
+const checkValidation = (userNumber) => {
+  if (userNumber === 'NaN' || !userNumber) {
+    throw new Error(ERROR_MESSAGE);
+  }
+  if (
+    !checkRange(userNumber) ||
+    !checkLength(userNumber) ||
+    !checkUniqueness(userNumber)
+  ) {
+    throw new Error(ERROR_MESSAGE);
+  }
+  return true;
 };
 
 export default checkValidation;
--- src/components/input/getUserInput.js
+++ src/components/input/getUserInput.js
@@ -1,13 +1,16 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { checkValidation } from '@components/checkValidation';
-import { ERROR_MESSAGE } from '@/constants';
+import { checkValidation } from './checkValidation';
 
 // 사용자로부터 숫자를 입력받습니다.
-const getUserInput = () => {
-  MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ', (input) => {
-    let validation = checkValidation(input);
-    validation ? MissionUtils.Console.print(input) : ERROR_MESSAGE;
-  });
+const getUserInput = async () => {
+  try {
+    const input = await MissionUtils.Console.readLineAsync(
+      '숫자를 입력해주세요 :',
+    );
+    return input;
+  } catch (error) {
+    new Error('[ERROR]');
+  }
 };
 
 export default getUserInput;
