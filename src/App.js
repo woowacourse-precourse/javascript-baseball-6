4d454f9de186083e2311644085ae820c0c583a4c
happyrina <riniversal@gmail.com>
2023-10-25 23:14:49 +0900

    feat : 컴퓨터 숫자 생성, 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-class App {
-  async play() {}
-}
+import { MissionUtils } from "@woowacourse/mission-utils";
 
-export default App;
+export default class App {
+    constructor() {
+        this.computerNumbers = [];
+        this.addComputerNumbers(); 
+    }
+
+    addComputerNumbers() {
+        while (this.computerNumbers.length < 3) {
+            const randomDigit = MissionUtils.Random.pickNumberInRange(1, 9);
+            if (!this.computerNumbers.includes(randomDigit)) {
+                this.computerNumbers.push(randomDigit);
+            }
+        }
+    }
+}
