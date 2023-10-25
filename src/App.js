e132168e7225cd1724953505fcefc5c20c6d435a
future4ing <mirae4120@gmail.com>
2023-10-25 15:23:53 +0900

    feat : 숫자 입력 받는 기능 추가
    - 사용자에게 숫자를 입력받기
    - 입력 값이 잘못된 값이면 에러 생성

--- src/App.js
+++ src/App.js
@@ -6,11 +6,37 @@ class App {
 
     while (answer.length < 3) {
       const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!answer.includes(randomNumber)) 
-        answer.push(randomNumber); 
+      if (!answer.includes(randomNumber))
+        answer.push(randomNumber);
     }
     console.log(answer)
     return answer;
+  }
+
+  async user() {
+    const userInputNumber = [];
+    
+    try {
+      const inputValue = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+      if (inputValue == undefined || inputValue.length != 3)
+        throw new Error("[ERROR] 숫자가 3자리여야 합니다.");
+      
+      for (var i = 0; i < 3; i++) {
+        if (+inputValue[i] == NaN)
+          throw new Error("[ERROR] 입력값은 숫자여야 합니다.");
+        if (userInputNumber.includes(parseInt(inputValue[i])))
+          throw new Error("[ERROR] 숫자가 중복되지 않아야 합니다.");
+        if (inputValue[i] == 0)
+          throw new Error("[ERROR] 숫자는 1~9 사이의 숫자여야 합니다.");
+        else
+          userInputNumber.push(parseInt(inputValue[i]));
+      }
+
+      return userInputNumber;
+
+    } catch (error) {
+      return Promise.reject(error);
+    }
   }
 
   async play() {
