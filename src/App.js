65b2ecf4cce865d311fc3d47d2381f77cfe19f1b
j-nary <dlwls0921@naver.com>
2023-10-25 22:55:07 +0900

    숫자야구 구현 완료

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App {
           const result = this.compareNumbers(randomNumber, userInput);
 
           if (result === "3스트라이크") {
+            MissionUtils.Console.print(result);
             MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             const restart = await MissionUtils.Console.readLineAsync(
               "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
