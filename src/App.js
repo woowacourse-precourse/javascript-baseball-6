844b88638630bed1866ed8eb7dce96edea41e259
happyrina <riniversal@gmail.com>
2023-10-25 23:15:54 +0900

    feat : 게임시작 및 사용자 입력받기

--- src/App.js
+++ src/App.js
@@ -14,4 +14,15 @@ export default class App {
             }
         }
     }
+    async play() {
+      MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  
+      while (true) {
+          const userInput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+  
+          if (userInput.length !== 3 || !this.isInputValid(userInput)) {
+              throw new Error('[ERROR] 잘못된 입력입니다.');
+          }
+      }
+  }
 }
