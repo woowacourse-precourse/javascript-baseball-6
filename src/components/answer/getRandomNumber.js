aa26db983c8369693232cd0d1daadcbe5946b1c3
naro_Kim <gracias9022@gmail.com>
2023-10-25 15:53:37 +0900

    refactor: 기능 2. API 제한에 따라 랜덤 숫자 생성 기능 수정

--- src/components/answer/getRandomNumber.js
+++ src/components/answer/getRandomNumber.js
@@ -1,14 +1,17 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { INPUT_RULES } from '@/constants';
+import { INPUT_RULES } from '../../constants';
 
 // 게임이 시작될 때 마다, 3자리까지 각 자리마다 무작위 수를 선정합니다.
 const getRandomNumber = () => {
-  const NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(
-    INPUT_RULES.MIN_RANGE,
-    INPUT_RULES.MAX_RANGE,
-    INPUT_RULES.NUMBER_OF_DIGITS,
-  );
-  return Random.shuffle(NUMBERS);
+  const computedNumber = [];
+  while (computedNumber.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(
+      INPUT_RULES.MIN_RANGE,
+      INPUT_RULES.MAX_RANGE,
+    );
+    if (!computedNumber.includes(number)) computedNumber.push(number);
+  }
+  return computedNumber;
 };
 
 export default getRandomNumber;
