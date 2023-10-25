1cb39a277314cbb7403f4165f9c7138bab7d8ed6
고세종 <fulfilled7418@gmail.com>
2023-10-25 21:09:10 +0900

    docs: 수정 README 문서 스타일 변경

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const GAME_START_MESSAGE = '숫자 야구 게임을 시작합니다.';
 const ASK_NEW_GAME_MESSAGE =
   '3개의 숫자를 모두 맞히셨습니다! 게임 종료.\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.';
 const ASK_ENTER_INPUT_MESSAGE = '숫자를 입력해주세요 : ';
-const ERROR_MESSAGE = '[ERROR] 숫자가 잘못된 형식입니다.';
+const ERROR_MESSAGE = '[ERROR] 잘못된 입력입니다.';
 
 const checkGameIsEnd = (userInput) => {
   if (userInput === '1') {
