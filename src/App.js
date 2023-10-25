7548daff62dbbff0ea60f23ac8f09a94bb84b3f3
Ryugi62 <xorjf1027@naver.com>
2023-10-25 16:54:08 +0900

    처음 실행시 값 선언과 초기값 지정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,38 @@
 class App {
+  constructor() {
+    this.isPlaying = false;
+    this.isCorrect = false;
+
+    this.computer = [];
+    this.user = [];
+
+    this.status = {
+      strike: 0,
+      ball: 0,
+      out: 0,
+    };
+
+    this.init();
+  }
+
+  init() {
+    this.isPlaying = true;
+    this.isCorrect = true;
+
+    this.computer = [];
+    this.user = [];
+
+    this.clearStatus();
+  }
+
+  clearStatus() {
+    this.status = {
+      strike: 0,
+      ball: 0,
+      out: 0,
+    };
+  }
+
   async play() {}
 }
 
