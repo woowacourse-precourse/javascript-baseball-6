19d2593d364948e4f3cbb79635e49013a925131f
kim <kajinori@gachon.ac.kr>
2023-10-25 13:26:43 +0900

    refactor: 모듈 분리 및 검증 순서, 중복 검사 수정(set)

---
+++ src/validator.js
@@ -0,0 +1,12 @@
+export const validateInput = (userInput) => {
+  if (isNaN(Number(userInput))) {
+    throw new Error('[ERROR] 숫자만 입력할 수 있습니다.');
+  }
+  if (userInput.length !== 3) {
+    throw new Error('[ERROR] 3자리의 숫자를 입력해주세요.');
+  }
+  if (new Set(userInput).size !== 3) {
+    throw new Error('[ERROR] 모두 다른 숫자를 입력해주세요.');
+  }
+  return true;
+};
