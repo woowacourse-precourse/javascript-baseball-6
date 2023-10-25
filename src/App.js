00d500a08cbdecb1d98bb005e009156c0df7d80a
uujeen <rkddmlwls2@gmail.com>
2023-10-25 14:31:54 +0900

    feat: 게임 시작 시 안내 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { MissionUtils, Console } from '@woowacourse/mission-utils';
+
+const MESSAGE = {
+  Start: '숫자 야구 게임을 시작합니다.',
+};
 class App {
-  async play() {}
+  async play() {
+    Console.print(MESSAGE.Start);
+  }
 }
 
 export default App;
