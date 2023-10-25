271284549d0c82c2914c3bcb435bf5ba8c074ffc
joony <tjd1wns15@gmail.com>
2023-10-25 20:36:33 +0900

    fix: 버그 수정중

--- src/App.js
+++ src/App.js
@@ -1,73 +1,87 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   // #1 랜덤으로 3개의 숫자 생성
   async setComputerNum() {
     const setNum = [];
     for (let i = 0; i < 3; i++) {
-      let pickOneNumber = await MissionUtils.Random.pickNumberInRange();
+      let pickOneNumber = Random.pickNumberInRange();
       if (!setNum.includes(pickOneNumber)) setNum.push(pickOneNumber);
     }
-    return this.setNum;
+    this.setNum = setNum;
+    return;
   }
-  // #2 사용자의 숫자를 받고 생성한 숫자와 비교
+
+  // #2 사용자의 숫자를 입력받음
+  async getPlayerNum() {
+    const inputNum = await Console.readLineAsync("숫자를 입력해주세요 : ");
+    // inputNum 예외처리
+    if (isNaN(inputNum) || inputNum.length !== 3 || inputNum.includes(0)) {
+      throw new Error("[ERROR]");
+    } else {
+      return Array.from(inputNum).map(Number);
+    }
+  }
+  // #3 사용자의 숫자와 생성한 숫자와 비교
   async compareNum() {
     const computerNum = [...this.setNum];
-    const playerNum = await MissionUtils.Console.readLineAsync(
-      "숫자를 입력해주세요 : "
-    );
+    const playerNum = await this.getPlayerNum();
+
     let strike = 0;
     let ball = 0;
 
-    //strike
+    // case1. strike
     for (let i = 0; i < computerNum.length; i++) {
       if (computerNum[i] === playerNum[i]) strike++;
-      computerNum.splice(i, 1);
-      playerNum.splice(i, 1);
-      i--;
     }
-    //ball
+    // case2. ball
     for (let i = 0; i < computerNum.length; i++) {
-      if (computerNum.includes(playerNum[i])) ball++;
+      if (computerNum.includes(playerNum[i]) && computerNum[i] !== playerNum[i])
+        ball++;
     }
-    await printResult();
+    await this.printResult(strike, ball);
+
+    if (strike !== 3) return this.compareNum();
+    else return this.endGamePoint();
   }
 
-  // #3 2의 결과 출력
-  async printResult() {
-    if (this.strike === 0 && this.ball === 0) {
-      MissionUtils.Console.print("낫싱");
-    } else if (this.strike !== 0 && this.ball === 0) {
-      MissionUtils.Console.print(`${this.strike}스트라이크`);
-    } else if (this.strike === 0 && this.ball !== 0) {
-      MissionUtils.Console.print(`${this.ball}볼`);
-    } else if (this.strike !== 0 && this.ball !== 0) {
-      MissionUtils.Console.print(`${this.ball}볼 ${this.strike}스트라이크`);
+  // #4 3의 결과 출력
+  async printResult(strike, ball) {
+    if (strike === 3) {
+      Console.print("3스트라이크");
+    } else if (strike === 0 && ball === 0) {
+      Console.print("낫싱");
+    } else if (strike !== 0 && ball === 0) {
+      Console.print(`${strike}스트라이크`);
+    } else if (strike === 0 && ball !== 0) {
+      Console.print(`${ball}볼`);
+    } else if (strike !== 0 && ball !== 0) {
+      Console.print(`${ball}볼 ${strike}스트라이크`);
     }
     return;
   }
 
-  // #4 3스트라이크 시 재시작/종료 구분하는 메세지 출력 및 실행
+  // #5 3스트라이크 시 재시작/종료 구분하는 메세지 출력 및 실행
   async endGamePoint() {
-    const endGameInput = MissionUtils.Console
-      .readLineAsync(`3개의 숫자를 모두 맞히셨습니다! 게임 종료
+    const endGameInput =
+      await Console.readLineAsync(`3개의 숫자를 모두 맞히셨습니다! 게임 종료
     게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`);
 
-    if (endGameInput !== 1 || endGameInput !== 2) {
-      throw new Error("[ERROR]");
-    } else if (endGameInput === 1) {
+    if (endGameInput === "1") {
+      // this.setComputerNum();
       return this.play();
-    } else if (endGameInput === 2) {
-      MissionUtils.Console.print("숫자 야구 게임을 종료합니다.");
+    } else if (endGameInput === "2") {
+      Console.print("숫자 야구 게임을 종료합니다.");
       return;
+    } else {
+      throw new Error("[ERROR]");
     }
   }
   // #0 게임 실행
   async play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다");
-    await this.setComputerNum();
-    await this.compareNum();
-    await this.printResult();
+    Console.print("숫자 야구 게임을 시작합니다");
+    this.setComputerNum();
+    return this.compareNum();
   }
 }
 
