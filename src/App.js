1c5be56fc78fba78b39e6ee435744d781a6e11dc
yunji <dbswl330@gmail.com>
2023-10-25 14:55:37 +0900

    refactor: import Ball,Game

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import Balls from "./Balls.js";
+import GameView from "./GameView.js";
+
 class App {
   constructor() {
     this.balls = new Balls();
     this.view = new GameView();
   }
-
   async play() {
-    this.view.printStartGame();
-    while (true) {}
+    this.view.printStartGame("숫자 야구 게임을 시작합니다.");
+
+    while (true) {
+      const inputNumbers = await this.view.getPlayerGuess();
+      this.balls.isValidInput(inputNumbers);
+    }
   }
 }
-
 export default App;
 const app = new App();
 app.play();
