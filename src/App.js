695022fa0edea4914989137918607c8729a9e287
happyrina <riniversal@gmail.com>
2023-10-25 23:20:35 +0900

    feat : 사용자 입력 유효성 검사

--- src/App.js
+++ src/App.js
@@ -14,28 +14,40 @@ export default class App {
             }
         }
     }
+
     async play() {
-      MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-  
-      while (true) {
-          const userInput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
-  
-          if (userInput.length !== 3 || !this.isInputValid(userInput)) {
-              throw new Error('[ERROR] 잘못된 입력입니다.');
-          }
-          if (strike === 3) {
-            MissionUtils.Console.print('3스트라이크');
-            MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-        
-            const userChoice = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
-            if (userChoice === '1') {
-                this.computerNumbers = [];
-                this.addComputerNumbers();
-                continue;
-            } else {
-                break;
-            }  
-      }
-}
+        MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+
+        while (true) {
+            const userInput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+
+            if (userInput.length !== 3 || !this.isInputValid(userInput)) {
+                throw new Error('[ERROR] 잘못된 입력입니다.');
+            }
+
+            const { strike, ball } = this.getStrikeAndBallCount(userInput);
+
+            if (strike === 3) {
+                MissionUtils.Console.print('3스트라이크');
+                MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+
+                const userChoice = await MissionUtils.Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+                if (userChoice === '1') {
+                    this.computerNumbers = [];
+                    this.addComputerNumbers();
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+            let resultMessage = this.generateResultMessage(strike, ball);
+            MissionUtils.Console.print(resultMessage);
+        }
     }
+
+    isInputValid(input) {
+        const uniqueDigits = [...new Set(input.split(''))];
+        return uniqueDigits.length === 3 && uniqueDigits.every(digit => digit >= '1' && digit <= '9');
+      } 
   }
