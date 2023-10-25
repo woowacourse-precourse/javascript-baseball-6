fbc49825cd1359a08235fcb8044bfc33c316366d
jiyun1006 <jiyun4251@gmail.com>
2023-10-25 20:45:03 +0900

    🔨 refactor : 오류사항 발견
    try catch문을 이용해 error를 처리하던것이 문제였다.
    throw문만 사용해서 해결.

--- src/App.js
+++ src/App.js
@@ -4,50 +4,72 @@ const START_MESSAGE = '숫자 야구 게임을 시작합니다.';
 const INPUT_MESSAGE = '숫자를 입력해주세요 : ';
 const THREE_STRIKE_MESSAGE = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
 const RETRY_MESSAGE = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n';
-const ERROR_MESSAGE = '[ERROR] 숫자가 잘못된 형식입니다.'
+const ERROR_MESSAGE = '[ERROR]숫자가 잘못된 형식입니다.'
 const NOTHING = '낫싱';
 const BALL = '볼';
 const STRIKE = '스트라이크';
 
 class App {
+  constructor (){
+    this.answerNumber = this.makeAnsNumber();
+  }
   async play() {
     Console.print(`${START_MESSAGE}`);
-    // this.getGameResult();  
     while (true) {
       let inputNumber, retryNumber;
-      const answerNumber = await this.makeAnsNumber();
-      let isThreeStrike = true;
 
-      while (isThreeStrike) {
-        inputNumber = await this.makeInputNumber();
-        if (!inputNumber) break;
-        let [strike, ball, out] = this.checkNumber(answerNumber, inputNumber);
-        if (out === 3) Console.print(`${NOTHING}`);
-        else {
-          if (ball > 0) {
-            if (strike === 0) {
-              Console.print(ball + `${BALL}`);
-            } else {
-              Console.print(ball + `${BALL}` + strike + `${STRIKE}`);
-            }
-          }
-          else Console.print(strike + `${STRIKE}`);
-        }
-        isThreeStrike = strike === 3 ? false : true;
-      }
+      // 숫자를 입력받고 볼, 스트라이크 판정하는 부분.
+      inputNumber = await this.makeInputNumber();
       if (!inputNumber) break;
-      Console.print(`${THREE_STRIKE_MESSAGE}`);
-      retryNumber = await this.inputRetry();
-      if (!retryNumber) break;
-      if (retryNumber === 2) break;
+      let [strike, ball] = this.checkNumber(this.answerNumber, inputNumber);
+      if (strike === 0 && ball === 0) Console.print(`${NOTHING}`);
+      else if (strike === 0) Console.print(ball + `${BALL}`);
+      else if (ball === 0) Console.print(strike + `${STRIKE}`);
+      else Console.print(ball + `${BALL}` + ' ' + strike + `${STRIKE}`);
+      
+      // 3스트라이크인 경우
+      if (strike === 3) {
+        Console.print(`${THREE_STRIKE_MESSAGE}`);
+
+        retryNumber = await this.inputRetry();
+        if (retryNumber === 1) {
+          this.answerNumber = this.makeAnsNumber();
+        }
+        else break;
+      }
     }
   }
 
-  // async getGameResult() {
-    
-  // }
+  
 
-  async makeAnsNumber() {
+  async makeInputNumber() {
+    
+    let inputNumber = await Console.readLineAsync(`${INPUT_MESSAGE}`);
+    this.checkInputNumber(inputNumber);
+    inputNumber = [...inputNumber].map(el => +el);
+    return inputNumber;
+  }
+
+  checkInputNumber(inputNumber) {
+    if (inputNumber.length !== 3 || isNaN(inputNumber)) throw new Error(`${ERROR_MESSAGE}`);
+
+  }
+
+  async inputRetry() {
+    let message = await Console.readLineAsync(`${RETRY_MESSAGE}`);
+    this.checkRetryNumber(message);
+    return +message;
+   
+  }
+
+  checkRetryNumber(inputNumber) {
+    if (inputNumber.length !== 1 || isNaN(inputNumber) ||
+    inputNumber < 0 || inputNumber > 2 
+    ) throw new Error(`${ERROR_MESSAGE}`);
+  }
+
+  // 정답 수 배열 만들기.
+  makeAnsNumber() {
     const numberArr = [];
     while (numberArr.length < 3) {
       let number = MissionUtils.Random.pickNumberInRange(1, 9);
@@ -58,51 +80,18 @@ class App {
     return numberArr;
   }
 
-  async makeInputNumber() {
-    try {
-      let inputNumber = await Console.readLineAsync(`${INPUT_MESSAGE}`);
-      this.checkInputNumber(inputNumber);
-      inputNumber = [...inputNumber].map(el => +el);
-      return inputNumber;
-    } catch (error) {
-      Console.print(error.message);
-    }
-  }
-
-  checkInputNumber(inputNumber) {
-    if (inputNumber.length !== 3 || isNaN(inputNumber)) throw new Error(`${ERROR_MESSAGE}`);
-
-  }
-
-  async inputRetry() {
-    try {
-      let message = await Console.readLineAsync(`${RETRY_MESSAGE}`);
-      this.checkRetryNumber(message);
-      return +message;
-    } catch (error) {
-      Console.print(error.message);
-    }
-  }
-
-  checkRetryNumber(inputNumber) {
-    if (inputNumber.length !== 1 || isNaN(inputNumber) ||
-    inputNumber < 0 || inputNumber > 2 
-    ) throw new Error(`${ERROR_MESSAGE}`);
-  }
-
-
+  // 스트라이크, 볼 체크
   checkNumber(answerNumber, inputNumber) {
     let strike = 0;
     let ball = 0;
-    let out = 0;
     inputNumber.map((num) => {
       let idx = inputNumber.indexOf(num);
       if (answerNumber.includes(num)) {
         if (answerNumber[idx] === num) strike++;
         else ball++;
-      } else out++;
+      }
     })
-    return [strike, ball, out];
+    return [strike, ball];
   }
 
 }
