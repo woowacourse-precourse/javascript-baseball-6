08f1ad5674c753ca278dc20de5190aed4e130c08
FordangIT <fordang0819@gmail.com>
2023-10-25 17:54:42 +0900

    feat: 메세지 객체 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import {
+  ANSWER_TABLE,
+  CONTINUE_OR_EXIT_CODE,
+  MESSAGE_TABLE,
+} from "./constants/index.js";
 class App {
-  async play() {}
+  #randomNumber;
+
+  constructor() {
+    this.#randomNumber = this.generateRandomNumber();
+    this.isContinuous = true;
+  }
+
+  async play() {
+    Console.print(MESSAGE_TABLE.GAME_START);
+
+    while (this.isContinuous) {
+      const userInput = await this.parsingUserInput();
+
+      if (!this.checkValidAnswer(userInput)) {
+        Console.print(MESSAGE_TABLE.INVALID_INPUT);
+        continue;
+      }
+
+      let usersAnswer = this.validateParsedInput(userInput);
+      const computerAnswer = "0b3s";
+
+      this.printResult(usersAnswer);
+
+      if (usersAnswer === computerAnswer) {
+        Console.print(MESSAGE_TABLE.CORRECT_ANSWER);
+        this.isContinuous = false;
+        await this.checkContinueOrExit();
+      }
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,27 @@
+export const ANSWER_TABLE = {
+  "0b0s": "낫싱",
+  "1b0s": "1볼",
+  "2b0s": "2볼",
+  "3b0s": "3볼",
+  "0b1s": "1스트라이크",
+  "1b1s": "1볼 1스트라이크",
+  "2b1s": "2볼 1스트라이크",
+  "0b2s": "2스트라이크",
+  "1b2s": "1볼 2스트라이크",
+  "0b3s": "3스트라이크",
+};
+
+export const CONTINUE_OR_EXIT_CODE = {
+  CONTINUE: "1",
+  EXIT: "2",
+};
+
+export const MESSAGE_TABLE = {
+  GAME_START: "숫자 야구 게임을 시작합니다.",
+  REQUIRED_INPUT: "숫자를 입력해주세요: ",
+  INVALID_INPUT: "입력값이 유효하지 않습니다. 세자리 숫자로 입력해주세요.",
+  CORRECT_ANSWER: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  ERROR_INPUT: "[ERROR] 입력값이 유효하지 않습니다. 프로그램을 종료합니다.",
+  CONTINUE_OR_EXIT: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+  EXIT_GAME: "게임을 종료합니다.",
+};
