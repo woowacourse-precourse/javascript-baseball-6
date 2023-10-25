aab592a6aafe322e6a325b4a2ac1aa19d0e95e91
Boeun <boeun0904@naver.com>
2023-10-25 21:08:47 +0900

    feat: 사용자 입력 예외 발생

--- src/User.js
+++ src/User.js
@@ -1,6 +1,14 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 export default class User {
-    returnUserQuery(query) {
-        return MissionUtils.Console.readLineAsync(query);
+    async returnUserQuery(query) {
+        const INPUT = await MissionUtils.Console.readLineAsync(query);
+        if (!this.checkValidation(INPUT))
+            throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+        return INPUT;
+    }
+    checkValidation(str) {
+        if (str.length > 3) return false;
+        else if (isNaN(str)) return false;
+        else return true;
     }
 }
