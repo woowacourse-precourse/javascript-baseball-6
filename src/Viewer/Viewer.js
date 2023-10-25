aec40ec5a8b1f33b559cd6b881c07e5ce429cb6d
MAGHC <windowsapple95@gmail.com>
2023-10-25 20:11:57 +0900

    fix: view 게임시작 문구 테스트에 맞게 변경

--- src/Viewer/Viewer.js
+++ src/Viewer/Viewer.js
@@ -1,7 +1,7 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 class Viewer {
-  #START = '숫자 야구 게임을 시작합니다.';
+  #START = '숫자 야구 게임을 시작합니다';
 
   #CLEAR = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
 
