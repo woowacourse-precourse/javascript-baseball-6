8afe69247955529df17e582e7adca3d70a8fc2d3
seung <dmdgpdi@naver.com>
2023-10-25 12:00:47 +0900

    Feat: 사용자 숫자 입력 함수 구현 및 임의의 수 3개 선택하고 저장하는 함수 변경

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -16,8 +16,15 @@ class BaseballGame {
   selectTargetNumbers() {
     for (let i = 0; i < 3; i++) {
       const randomNuber = MissionUtils.Random.pickNumberInRange(1, 9);
-      this.#userNumbers.push(randomNuber);
+      this.#targetNumbers.push(randomNuber);
     }
+  }
+
+  getUserNumbers() {
+    const consoleMessage = "숫자를 입력해주세요 : ";
+    MissionUtils.Console.readLine(consoleMessage, (numbers) => {
+      this.#userNumbers = [...numbers].map((number) => Number(number));
+    });
   }
 }
 
