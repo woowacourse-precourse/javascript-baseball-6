04775e36dd5c315611c9af436548b31e5150d653
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-10-25 23:25:18 +0900

    feat: 컴퓨터 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -7,10 +7,22 @@ class App {
 
   async play() {
     this.gameStartMessage();
+    this.generateRandomNumber();
   }
 
   gameStartMessage() {
     Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  generateRandomNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    this.answer = computer;
   }
 }
 
