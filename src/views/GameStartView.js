cc016589f938be5dcda1affb80fa49af14b7eede
Jinnmidnnight <vkstk0131@yonsei.ac.kr>
2023-10-25 19:52:27 +0900

    feat(views): views 함수 수정 및 추가 * GameStart 파일/함수를 GameStartView로 변경 * InputNumberView 파일/함수 추가

--- src/views/GameStartView.js
+++ src/views/GameStartView.js
@@ -1,8 +1,11 @@
 import { Console } from "@woowacourse/mission-utils";
 import MESSAGES from "../constants/Messages.js";
 
-const GAME_START = () => {
+/**
+ * 게임 시작 메시지를 출력한다.
+ */
+const GAME_START_VIEW = () => {
   Console.print(MESSAGES.GAME_START);
 };
 
-export default GAME_START;
+export default GAME_START_VIEW;
