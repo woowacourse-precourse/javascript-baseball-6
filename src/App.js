2ee99c3829e6543abb23988050631d9a5b37f71a
alsth712 <alsth712@naver.com>
2023-10-25 21:18:18 +0900

    feat: 랜덤한 3자리 수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+const LIMIT_NUM_LENGTH = 3;
+// !!!!!
+
 class App {
-  async play() {}
+  async play() {
+    const randomNum = this.getRandomNum();
+  }
+
+  getRandomNum() {
+    Console.print(`숫자 야구 게임을 시작합니다.`);
+    let computer = [];
+    while (computer.length < LIMIT_NUM_LENGTH) {
+      let NUMB = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(NUMB)) {
+        computer.push(NUMB);
+      }
+    }
+    return computer;
+  }
 }
 
 export default App;
