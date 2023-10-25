398d4c54d47d1284f7a507706a4937dc38f5c022
mingyeong:D <63152086+Kim-Mingyeong@users.noreply.github.com>
2023-10-25 17:02:40 +0900

    refactor: 코드 구조 개선

--- src/App.js
+++ src/App.js
@@ -2,95 +2,92 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 import { ERROR, GAME } from "./Message";
 
 class App {
-  async play() {
-    function generateRandomNumber() {
-      // 컴퓨터가 1~9 사이의 서로 다른 임의의 수 3개 선택하는 함수
+  constructor() {
+    this.computerValue = this.generateRandomNumber();
+    this.playGame = true;
+  }
 
-      const computer = [];
-      while (computer.length < 3) {
-        const number = MissionUtils.Random.pickNumberInRange(1, 9);
-        if (!computer.includes(number)) {
-          computer.push(number);
-        }
+  // generateRandomNumber(): 컴퓨터가 1~9 사이의 서로 다른 임의의 수 3개 생성하는 메소드
+  // 랜덤으로 생성된 3자리 숫자 배열 반환
+  generateRandomNumber = () => {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
       }
-      return computer;
+    }
+    return computer;
+  };
+
+  // isValidInput(): 사용자 입력 검증 메소드 (0~9까지 3자리 수)
+  // 입력 검증 후 true 또는 false 반환
+  isValidInput = (userInput) => {
+    const inputRegex = /^[0-9]{3}$/.test(userInput);
+    return inputRegex;
+  };
+
+  // inputNumber(): 사용자로부터 입력 받는 메소드
+  // 잘못된 값을 입력했을 시 throw 사용해 예외 처리
+  // 사용자에게 입력 받은 값 배열 반환
+  getUserValue = async () => {
+    const userGuess = await MissionUtils.Console.readLineAsync(GAME.INPUT);
+    if (isNaN(userGuess)) throw new Error(ERROR.NUMBER);
+    else if (userGuess < "1" || userGuess > "9") throw new Error(ERROR.RANGE);
+    if (userGuess.length !== 3) throw new Error(ERROR.LENGTH);
+    if (new Set(userGuess).size !== userGuess.length)
+      throw new Error(ERROR.REPEATED);
+
+    return [...userGuess].map((value) => Number(value));
+  };
+
+  // calculateResult(): 입력한 수에 대한 결과를 판단하는 메소드
+  // 게임이 종료됐을 때 재시작 여부를 묻기 위한 true 또는 false 반환
+  calculateResult = (userGuess) => {
+    let strikes = 0;
+    let balls = 0;
+
+    for (let i = 0; i < 3; i++) {
+      if (this.computerValue[i] === userGuess[i]) strikes++;
+      else if (this.computerValue.includes(userGuess[i])) balls++;
     }
 
-    function calculateResult(computer, guess) {
-      // 입력한 수에 대한 결과를 볼, 스트라이크 개수 계산 함수
-      const result = {
-        strikes: 0,
-        balls: 0,
-      };
-
-      for (let i = 0; i < 3; i++) {
-        if (computer[i] === guess[i]) result.strikes++;
-        else if (computer.includes(guess[i])) result.balls++;
-      }
-
-      return result;
-    }
-
-    async function inputNumber() {
-      // 사용자로부터 서로 다른 3자리의 수 입력 받는 함수
-
-      const userGuess = await MissionUtils.Console.readLineAsync(GAME.INPUT);
-      if (isNaN(userGuess)) throw new ERROR(ERROR.NUMBER);
-      else if (userGuess < "1" || userGuess > "9") throw new ERROR(ERROR.RANGE);
-      if (userGuess.length !== 3) throw new ERROR(ERROR.LENGTH);
-      if (new Set(userGuess).size !== userGuess.length)
-        throw new ERROR(ERROR.REPEATED);
-
-      return [...userGuess].map(Number);
-    }
-
-    function printResult(result) {
-      // 결과 출력하는 함수
-      if (result.balls !== 0 || result.strikes !== 0) {
-        if (result.strikes === 3) {
-          MissionUtils.Console.print(
-            "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
-          );
-          return true;
-        } else if (result.balls === 0)
-          MissionUtils.Console.print(`${result.strikes}스트라이크`);
-        else if (result.strikes === 0)
-          MissionUtils.Console.print(`${result.balls}볼`);
-        else
-          MissionUtils.Console.print(
-            `${result.balls}볼 ${result.strikes}스트라이크`
-          );
-      } else MissionUtils.Console.print("낫싱");
-      return false;
-    }
-
-    async function reGame() {
-      // 게임 재시작 or 종료
-
-      while (true) {
-        const userInput = await MissionUtils.Console.readLineAsync(
-          GAME.RESTART
+    if (balls !== 0 || strikes !== 0) {
+      if (strikes === 3) {
+        MissionUtils.Console.print(
+          "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"
         );
-        if (userInput === "1") return true;
-        else if (userInput === "2") return false;
+        return true;
+      } else if (balls === 0)
+        MissionUtils.Console.print(`${strikes}스트라이크`);
+      else if (strikes === 0) MissionUtils.Console.print(`${balls}볼`);
+      else MissionUtils.Console.print(`${balls}볼 ${strikes}스트라이크`);
+    } else MissionUtils.Console.print("낫싱");
+    return false;
+  };
+
+  // reGame(): 게임 재시작 or 종료를 수행하는 메소드
+  // 1 또는 2가 아닌 숫자를 입력했을 시 throw 사용해 예외 처리
+  reGame = async (userValue) => {
+    const result = calculateResult(userValue);
+
+    if (result) {
+      const restart = await MissionUtils.Console.readLineAsync(GAME.RESTART);
+      if (restart === "1") this.computerValue = this.generateRandomNumber();
+      else if (restart === "2") this.playGame = false;
+      else {
+        throw new Error("[ERROR] 1 또는 2의 숫자만 입력해주세요.");
       }
     }
+  };
 
-    async function palyGame() {
-      // 숫자 야구 게임
+  async play() {
+    MissionUtils.Console.print(GAME.START);
 
-      MissionUtils.Console.print(GAME.START);
-      let computer = generateRandomNumber();
-      let RESTART = true;
-      do {
-        let userGuess = await inputNumber();
-        if (printResult(calculateResult(computer, userGuess))) {
-          RESTART = await reGame();
-          if (RESTART) computer = generateRandomNumber();
-        }
-      } while (RESTART);
+    while (this.playGame) {
+      const userValue = await this.getUserValue();
+      await this.reGame(userValue);
     }
-    palyGame();
   }
 }
 
