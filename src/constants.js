8b5749c269bb4457ec6191e88ce46f3387ba67a4
naro_Kim <gracias9022@gmail.com>
2023-10-25 15:55:51 +0900

    refactor: 출력 메세지 상수 추가

--- src/constants.js
+++ src/constants.js
@@ -5,4 +5,11 @@ export const INPUT_RULES = {
   REGEX: /[^1-9]/g,
 };
 
+export const ANSWER_MESSAGE = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
+
+export const NOTHING_MESSAGE = '낫싱';
+
 export const ERROR_MESSAGE = '[ERROR] 숫자가 잘못된 형식입니다.';
+
+export const EXIT_MESSAGE =
+  '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.';
