4c373ddbc3d94acd562692062733ead4856f6e4d
정수아 <sua@jeongsuaui-MacBookAir.local>
2023-10-25 12:43:31 +0900

    [feat] getUserNum, getComNum 으로 함수 분리

--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 class App {
   async play() {
-    console.log("?");
-    const GOAL = [];
 
+    try {
+      const GOAL = await this.getComNum();
+      const number = await this.getUserNum();
+
+      const testg = [1,2,3];
+      if(testg === number){
+        console.log("goal=number")
+      }else{
+        console.log(`${GOAL} ${number}`);
+      }
+
+      //Console.print(`유저가 입력한 숫자 : ${number} ${Array.isArray(number)}`);
+    } catch (error) {
+      Console.print(`에러 발생: ${error.message}`);
+      this.play()
+    }
+  }
+
+  async getComNum() {
+    const GOAL = [];
     while (GOAL.length < 3) {
       const RANDOM_NUMBER = Random.pickNumberInRange(1, 9);
       if (!GOAL.includes(RANDOM_NUMBER)) {
         GOAL.push(RANDOM_NUMBER);
       }
     }
+    return GOAL;
+  }
+  async getUserNum() {
+    const number = await Console.readLineAsync("숫자를 입력해주세요 : ");
 
-    try {
-      const number = await this.getUserNum();
+    const EXIST = new Set();
 
-      const EXIST = new Set();
-
-      for (const digit of number) {
-        EXIST.add(digit);
-      }
-
-      if (EXIST.size !== 3) {
-        Console.print(`${number} 현재넘버는 에러 발생 ${EXIST.size}`);
-        throw new Error("중복을 제외한 숫자의 길이가 3 이 아님");
-      }
-
-      Console.print(`${number}넘버넘버 덴2`);
-    } catch (error) {
-      // 에러 처리 로직을 작성합니다.
-      Console.print(`에러 발생: ${error.message}`);
+    for (const digit of number) {
+      EXIST.add(digit);
     }
 
-    Console.print(GOAL);
+    if(EXIST.size === 3){
+      return Array.from(EXIST);
+    }else{
+      Console.print(`${number} 의 길이가 3 이 아니거나 중복숫자가 있습니다. ${EXIST.size}`);
+      this.getUserNum();
+    }
   }
-
-  async getUserNum() {
-    const NUM = await Console.readLineAsync("숫자를 입력해주세요 : ");
-    Console.print(`${NUM} 유저가 입력한 숫자`);
-    return NUM;
-  }
-
   
 }
 
@@ -50,3 +56,16 @@ app.play();
 
 
 export default App;
+
+
+
+/** const EXIST = new Set();
+
+      for (const digit of number) {
+        EXIST.add(digit);
+      }
+
+      if (EXIST.size !== 3 || number.length > 3) {
+        Console.print(`${number} 의 길이가 3 이 아니거나 중복숫자가 있습니다. ${EXIST.size}`);
+        throw new Error("유저가 입력한 숫자가 포맷을 벗어남");
+      } */
