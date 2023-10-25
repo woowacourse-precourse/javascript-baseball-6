0f6df96b80fa61b8d7946026868f492c088b5eea
jinjaehwan <jjh099@icloud.com>
2023-10-25 16:52:11 +0900

    refactor : 상수로 쓰이는 값들 static 상수로 변환

--- src/Computer.js
+++ src/Computer.js
@@ -1,7 +1,14 @@
 import { Random, Console } from "@woowacourse/mission-utils";
-import Player from "./Player.js";
 
 class Computer {
+  static LENGTH_INPUT = 3;
+  static MAX_NUM = 9;
+  static MIN_NUM = 1;
+  static NOTHING_ERROR = "낫싱";
+  static SUCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+  static SELECT_MESSAGE =
+    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+
   constructor() {
     // 생성자에서 answer를 초기화
     this.answer = this.createAnswer();
@@ -10,10 +17,12 @@ class Computer {
 
   // 정답을 만드는 함수
   createAnswer = () => {
-    let arr = []; // 정답을 담을 배열
-    // 1 ~ 9 사이의 중복되지 않는 숫자 3개를 arr 배열에 push
-    while (arr.length < 3) {
-      const number = Random.pickNumberInRange(1, 9);
+    let arr = [];
+    while (arr.length < Computer.LENGTH_INPUT) {
+      const number = Random.pickNumberInRange(
+        Computer.MIN_NUM,
+        Computer.MAX_NUM,
+      );
       if (!arr.includes(number)) {
         arr.push(number);
       }
@@ -23,32 +32,27 @@ class Computer {
 
   // 정답과 player의 input값을 비교할 함수
   compareAnswer = (playerInput) => {
-    // player의 입력값을 숫자 배열로 변환
     const inputNumber = playerInput.split("").map(Number);
     let strike = 0;
     let ball = 0;
-    // player의 입력값과 정답을 비교해 strike와 ball을 변경
-    for (let i = 0; i < this.answer.length; i++) {
-      // 인덱스와 값이 모두 같다면 strike
+    for (let i = 0; i < Computer.LENGTH_INPUT; i++) {
       if (this.answer[i] === inputNumber[i]) {
         strike++;
-        // 인덱스는 같지 않지만 값이 포함되어 있다면 ball
       } else if (this.answer.includes(inputNumber[i])) {
         ball++;
       }
     }
     this.printResult(ball, strike);
-    if (strike === 3) {
+    if (strike === Computer.LENGTH_INPUT) {
       return false;
     } else {
       return true;
     }
-    // return [ball, strike];
   };
   // 결과를 출력하는 함수
   printResult = (ball, strike) => {
     if (ball === 0 && strike === 0) {
-      Console.print("낫싱");
+      Console.print(Computer.NOTHING_ERROR);
     }
     if (ball > 0 && strike === 0) {
       Console.print(`${ball}볼`);
@@ -60,12 +64,11 @@ class Computer {
       Console.print(`${ball}볼 ${strike}스트라이크`);
     }
   };
+
   // Playe가 정답을 맞췄을 경우
   async correctAnswer() {
-    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    const select = await Console.readLineAsync(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
-    );
+    Console.print(Computer.SUCESS_MESSAGE);
+    const select = await Console.readLineAsync(Computer.SELECT_MESSAGE);
     return select;
   }
 }
--- src/Player.js
+++ src/Player.js
@@ -2,33 +2,46 @@ import { Console } from "@woowacourse/mission-utils";
 import Computer from "./Computer.js";
 
 class Player {
+  static MESSAGE_INPUT = "숫자를 입력해주세요 : ";
+  static LENGTH_INPUT = 3;
+  static MAX_NUM = 9;
+  static MIN_NUM = 1;
+  static RESTART_COMMAND = "1";
+  static END_COMMAND = "2";
+  static LENGTH_ERROR = "[ERROR]입력값은 3자리여야 합니다.";
+  static DUPLICATION_ERROR = "[ERROR]입력값에 중복된 숫자가 있습니다.";
+  static RANGE_ERROR = "[ERROR]입력값은 1에서 9 사이의 숫자여야 합니다.";
+
+  // 게임 시작, 입력 값 받기, 재시작 or 종료 함수
   async inputValue() {
+    Console.print("숫자 야구 게임을 시작합니다.");
     this.computer = new Computer();
     var result = true;
     while (result) {
-      const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
+      const input = await Console.readLineAsync(Player.MESSAGE_INPUT);
       this.checkValue(input);
       result = this.computer.compareAnswer(input);
     }
     const select = await this.computer.correctAnswer();
-    if (select === "1") {
+    if (select === Player.RESTART_COMMAND) {
       this.inputValue();
     }
-    if (select === "2") {
+    if (select === Player.END_COMMAND) {
       return;
     }
   }
+  // Player의 입력값 검증하는 함수
   checkValue = (input) => {
-    if (input.length !== 3) {
-      throw new Error("[ERROR]입력값은 3자리여야 합니다.");
+    if (input.length !== Player.LENGTH_INPUT) {
+      throw new Error(Player.LENGTH_ERROR);
     }
     const inputArray = input.split("").map(Number);
     if (!this.isUnique(inputArray)) {
-      throw new Error("[ERROR]입력값에 중복된 숫자가 있습니다.");
+      throw new Error(Player.DUPLICATION_ERROR);
     }
 
     if (!this.isWithinRange(inputArray)) {
-      throw new Error("[ERROR]입력값은 1에서 9 사이의 숫자여야 합니다.");
+      throw new Error(Player.RANGE_ERROR);
     }
   };
   // 중복값 확인하는 함수
@@ -37,7 +50,9 @@ class Player {
   };
   // 1 ~ 9 사이의 수로 이루어졌는지 확인하는 함수
   isWithinRange = (arr) => {
-    return arr.every((number) => number >= 1 && number <= 9);
+    return arr.every(
+      (number) => number >= Player.MIN_NUM && number <= Player.MAX_NUM,
+    );
   };
 }
 
