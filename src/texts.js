22ec480e1edf69660a098922bb7616c66642af5b
mso901 <147568709+mso901@users.noreply.github.com>
2023-10-25 23:55:51 +0900

    Create texts.js

---
+++ src/texts.js
@@ -0,0 +1,6 @@
+export const ERROR = {
+  NUMBER: "[ERROR] 입력값은 숫자여야 합니다.",
+  INCLUDE_0: "[ERROR] 입력값은 1~9사이의 숫자여야 합니다.",
+  LENGTH: "[ERROR] 입력값은 세 자리여야 합니다.",
+  REPEATED: "[ERROR] 입력값의 각 자리 수는 서로 다른 숫자여야 합니다.",
+};
