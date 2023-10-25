3c46f9ad03d7c32a740931b9881a83783c298d08
baegyeong <beth0929@naver.com>
2023-10-25 14:53:55 +0900

    feat: constant.js에 상수데이터 정의

---
+++ src/constant.js
@@ -0,0 +1,14 @@
+export const constant = {
+  GAME_START: "숫자 야구 게임을 시작합니다.",
+  INPUT_NUMBER_MSG: "숫자를 입력해주세요 : ",
+  CORRECT_NUMBER: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  RESTART_OPTION: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+  ERROR: {
+    INPUT_VALUE: "[Error] 숫자 입력이 들어오지 않았습니다.",
+    DUPLICATE: "[Error] 숫자는 서로 중복될 수 없습니다.",
+    NEGATIVE: "[Error] 숫자는 음수일 수 없습니다.",
+    THREE_LENGTH: "[ERROR] 숫자는 3자리 수여야 합니다.",
+    IS_NAN: "[ERROR] 입력은 숫자 형식이어야 합니다.",
+    ONE_OR_TWO: "[ERROR] 1과 2 중에서 입력 해야합니다.",
+  },
+};
