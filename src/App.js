73a2a870cccba49011b318ba60864d8c5cfa0297
lottt105 <hyobin0601@naver.com>
2023-10-25 15:04:25 +0900

    Style: 주석 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Random, Console } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
+    // 게임 시작 문구 출력
     Console.print('숫자 야구 게임을 시작합니다.');
     
     const RANDOM_VALUE = this.getRandomNumber();
     let isSuccess =  false;
 
+    // 3스트라이크 된 경우 isSuccess를 true로 변경하여 게임 종료
     while (!isSuccess) {
       try {
         isSuccess = await this.playOneInput(RANDOM_VALUE);
@@ -16,6 +18,7 @@ class App {
       }
     }
 
+    // 게임 종료 후 재시작, 완전 종료 확인
     this.startGameAgain();
   }
 
@@ -37,6 +40,7 @@ class App {
     return this.printResult(RESULT);
   }
 
+  // 정답으로 사용될 랜덤값 생성
   getRandomNumber() {
     const COMPUTER = [];
     while (COMPUTER.length < 3) {
@@ -48,12 +52,14 @@ class App {
     return `${COMPUTER[0]}${COMPUTER[1]}${COMPUTER[2]}`;
   }
 
+  // 세자리 숫자 입력
   async getInputNumber() {
     const INPUT = await Console.readLineAsync('숫자를 입력해주세요 : ');
     this.checkInputNumber(INPUT);
     return INPUT;
   }
 
+  // 게임 종료 후 재시작, 완전 종료 확인 
   async startGameAgain() {
     let input = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
     this.checkInput(input);
@@ -61,7 +67,7 @@ class App {
     return;
   }
 
-  // 입력 값이 숫자가 아니거나, 중복을 제거한 길이가 3이 아니면 throw error
+  // 세자리 숫자 예외 처리 - 입력 값이 숫자가 아니거나, 중복을 제거한 길이가 3이 아니면 throw error
   checkInputNumber(input) {
     const SET_INPUT = [...new Set(input)];
 
@@ -70,12 +76,14 @@ class App {
     }
   }
 
+  // 재시작, 완전 종료 예외 처리 - 입력 값이 '1'이나 '2'가 아니면 throw error
   checkInput(input) {
     if (input !== '1' && input !== '2') {
       throw new Error('[ERROR] 잘못된 형식입니다.');
     }
   }
 
+  // 입력된 세자리 숫자 확인 결과 출력
   printResult(result) {
     if (result.strike === 3) {
       Console.print('3스트라이크');
