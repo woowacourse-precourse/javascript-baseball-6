32187cd37384cd7d4ea75f77adb6052453ba835a
skdltn210 <rhdmstn202@gmail.com>
2023-10-25 16:47:30 +0900

    fix: print message fix

--- src/App.js
+++ src/App.js
@@ -64,6 +64,7 @@ function result(ball, strike) {
 
 class App {
   async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
     let computer = setComputerNumber();
     while (true) {
       let num = await Console.readLineAsync("숫자를 입력해주세요 : ");
@@ -83,8 +84,6 @@ class App {
     }
   }
 }
-
-Console.print("숫자 야구 게임을 시작합니다.");
 
 const app = new App();
 app.play();
