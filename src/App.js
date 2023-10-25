2541ae68bf2083c2b5a624028fc13ec4d0f2e1f5
elice31 <elice31@elice31ui-iMac.local>
2023-10-25 20:44:59 +0900

    feat: 프리코스 1주차 과제 진행중-숫자 야구

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+/**
+ * -구현할 기능
+ * 1. 플레이어가 1부터 9사이 무작위 숫자 3개 입력
+ * 2. 
+ */
 class App {
-  async play() {}
+  async play() {
+
+  }
+}
+
+const computer = [];
+while(computer.length < 3){
+  const number = MissionUtils.Random.pickNumberInRange(1, 9);
+  if(!computer.includes(number)){
+    computer.push(number);
+  }
 }
 
 export default App;
