f64d07a8376a2f68665f6843869d3fd70803895e
ppacman <smwfkim2@naver.com>
2023-10-25 19:53:21 +0900

    feat : 랜덤값 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import { Random, Console } from "@woowacourse/mission-utils";
 
 
 class App {
+
+  getRandomNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer; //3개의 배열로 이루어진 랜덤값 생성
+  }
+
   async play() {}
 }
 
