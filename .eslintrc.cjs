97c546d9604b82eecd857a07ee2a7155184ad8ce
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2023-10-25 21:19:45 +0900

    chore: 세팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,22 +4,22 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: ["airbnb-base", "plugin:jsdoc/recommended", "prettier"],
+  extends: ['airbnb-base', 'plugin:jsdoc/recommended', 'prettier'],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
   },
   rules: {
     // ESM 사용으로 인한 파일 확장자 표기 의무화
-    "import/extensions": ["error", "always", { ignorePackages: true }],
+    'import/extensions': ['error', 'always', { ignorePackages: true }],
   },
   overrides: [
     {
-      files: ["__tests__/**/*.js"],
+      files: ['__tests__/**/*.js'],
       rules: {
-        "max-lines-per-function": "off",
-        "no-new": "off",
-        "no-undef": "off",
-        "arrow-body-style": "off",
+        'max-lines-per-function': 'off',
+        'no-new': 'off',
+        'no-undef': 'off',
+        'arrow-body-style': 'off',
       },
     },
   ],
