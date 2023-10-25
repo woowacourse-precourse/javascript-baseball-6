99cbc2e8fb7f03395cf5fbc228b3e7bf8b2a0cfc
jiyun1006 <jiyun4251@gmail.com>
2023-10-25 17:26:10 +0900

    🔋 feat : getGameResult 메서드 수정
    getGameResult에서 게임을 계속해서 진행하도록 변경하고,
    종료문구로 재시작할지 말지에 대한 답을 얻고, 게임을 진행하도록 변경.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 const START_MESSAGE = '숫자 야구 게임을 시작합니다.';
 const INPUT_MESSAGE = '숫자를 입력해주세요 : ';
+const THREE_STRIKE_MESSAGE = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
+const RETRY_MESSAGE = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n';
 const NOTHING = '낫싱';
 const BALL = '볼';
 const STRIKE = '스트라이크';
@@ -9,24 +11,32 @@ const STRIKE = '스트라이크';
 class App {
   async play() {
     MissionUtils.Console.print(`${START_MESSAGE}`);
-    let isThreeStrike = true;
-    const answerNumber = await this.makeAnsNumber();
-    while (isThreeStrike) {
-      isThreeStrike = await this.getGameResult(answerNumber);
+    this.getGameResult();
 
-    }
     
   }
 
-  async getGameResult(answerNumber) {
-    let inputNumber = await this.makeInputNumber();
-    let [strike, ball, out] = this.checkNumber(answerNumber, inputNumber);
-    if (out === 3) MissionUtils.Console.print(`${NOTHING}`);
-    else {
-      if (ball > 0) MissionUtils.Console.print(ball + `${BALL}` + strike + `${STRIKE}`);
-      else MissionUtils.Console.print(strike + `${STRIKE}`);
+  async getGameResult() {
+    while (true) {
+      const answerNumber = await this.makeAnsNumber();
+      console.log(answerNumber);
+      let isThreeStrike = true;
+
+      while (isThreeStrike) {
+        let inputNumber = await this.makeInputNumber();
+        let [strike, ball, out] = this.checkNumber(answerNumber, inputNumber);
+        if (out === 3) MissionUtils.Console.print(`${NOTHING}`);
+        else {
+          if (ball > 0) MissionUtils.Console.print(ball + `${BALL}` + strike + `${STRIKE}`);
+          else MissionUtils.Console.print(strike + `${STRIKE}`);
+        }
+        isThreeStrike = strike === 3 ? false : true;
+      }
+
+      MissionUtils.Console.print(`${THREE_STRIKE_MESSAGE}`);
+      const retryNumber = await this.inputRetry();
+      if (retryNumber === 2) break;
     }
-    return strike === 3 ? false : true;
   }
 
   async makeAnsNumber() {
@@ -48,6 +58,15 @@ class App {
 
     } catch (error) {
     // reject 되는 경우
+    }
+  }
+
+  async inputRetry() {
+    try {
+      let message = await MissionUtils.Console.readLineAsync(`${RETRY_MESSAGE}`);
+      return +message;
+    } catch (error) {
+      
     }
   }
 
