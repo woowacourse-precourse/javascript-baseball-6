235c33a921214de0f127567b1e7f60bd20baf25f
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-10-25 20:45:12 +0900

    refactor: baseballNumber 생성자의 매개변수를 유연하게 처리한다(string, number, Array 허용)

--- src/Model/BaseballNumber.js
+++ src/Model/BaseballNumber.js
@@ -1,21 +1,30 @@
 import { BaseballNumberError } from '../Model/Error.js';
 import { BASEBALL_NUMBER } from '../constants/baseballGame.js';
 import { ERROR } from '../constants/error.js';
+import { TYPE } from '../constants/type.js';
 
 export class BaseballNumber {
   #numberList;
 
   constructor(numberList) {
-    if (typeof numberList === 'string') numberList = numberList.split('');
+    const normalizedInput = this.#normalizeInput(numberList);
 
-    this.#validation(numberList);
+    this.#validation(normalizedInput);
 
-    this.#numberList = numberList.map(Number);
+    this.#numberList = normalizedInput.map(Number);
+  }
+
+  #normalizeInput(input) {
+    if (typeof input === TYPE.NUMBER) return String(input).split('');
+    if (typeof input === TYPE.STRING) return input.split('');
+    if (Array.isArray(input)) return input;
+
+    throw new Error('잘못된 type입니다. (Number, String, Array만 가능합니다)');
   }
 
   #validation(numberList) {
     if (numberList.length !== BASEBALL_NUMBER.DIGIT)
-      throw new BaseballNumberError(ERROR.MESSAGE.INVALID_DIGITS);
+      throw new BaseballNumberError(`${ERROR.MESSAGE.INVALID_DIGITS}`);
 
     if (new Set(numberList).size !== numberList.length)
       throw new BaseballNumberError(ERROR.MESSAGE.DUPLICATE_NUMBERS);
---
+++ src/constants/type.js
@@ -0,0 +1,4 @@
+export const TYPE = Object.freeze({
+  NUMBER: 'number',
+  STRING: 'string',
+});
