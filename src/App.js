52f46e6cd99a71ded6a96bcfb57e5e990fe2a737
장경빈 <wkdrudqls114@gmail.com>
2023-10-25 23:04:04 +0900

    not yet

--- src/App.js
+++ src/App.js
@@ -1,104 +1,113 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 
-class App {
-  computerNumberPick() {
-    const computer = [];
-    while (computer.length < 3) {
-      const number = Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer.push(number);
-      }
+function computerNumberPick() {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
     }
-    return computer;
   }
-  async play() {
-    let start_Num_Setting = 1; // 값이 1이면 게임 진행, 2이면 멈춤
-    let computer = this.computerNumberPick();
-    let count_Strike, count_Ball, count_Ball_And_Strike; // 스트라이크, 볼, 스트라이크 + 볼 횟수
+  return computer;
+}
 
-    Console.print("숫자 야구 게임을 시작합니다.");
+async function playGame() {
+  let computer = computerNumberPick();
+  let count_Strike, count_Ball, count_Ball_And_Strike; // 스트라이크, 볼, 스트라이크 + 볼 횟수
 
-    while (start_Num_Setting == 1) {
-      let user_Number = await Console.readLineAsync("숫자를 입력해주세요 : ");
+  while (true) {
 
-      try { // 숫자 3개 확인
-        if (user_Number.length != 3 || isNaN(user_Number)) {
-          throw new Error("숫자 3개를 입력하세요.");
-        }
-      } catch (error) {
-        Console.print("[ERROR]" + error.message);
-        continue;
+    let user_Number = await Console.readLineAsync("숫자를 입력해주세요 : ");
+
+    try { // 숫자 3개 확인
+      if (user_Number.length != 3 || isNaN(user_Number)) {
+        throw new Error("숫자 3개를 입력하세요.");
       }
+    } catch (error) {
+      Console.print("[ERROR]" + error.message);
+      continue;
+    }
 
-      try { //띄어쓰기 감지
-        if (user_Number.includes(" ")) {
-          throw new Error("띄어쓰기를 없애주세요.");
-        }
-      } catch (error) {
-        Console.print("[ERROR]" + error.message);
-        continue;
+    try { //띄어쓰기 감지
+      if (user_Number.includes(" ")) {
+        throw new Error("띄어쓰기를 없애주세요.");
       }
+    } catch (error) {
+      Console.print("[ERROR]" + error.message);
+      continue;
+    }
 
-      try { // 0을 감지
-        if (user_Number.includes("0")) {
-          throw new Error("0은 안됩니다.");
-        }
-      } catch (error) {
-        Console.print("[ERROR]" + error.message);
-        continue;
+    try { // 0을 감지
+      if (user_Number.includes("0")) {
+        throw new Error("0은 안됩니다.");
       }
+    } catch (error) {
+      Console.print("[ERROR]" + error.message);
+      continue;
+    }
 
-      try { // 겹치는 숫자 확인
-        if (new Set(user_Number).size !== user_Number.length) {
-          throw new Error("겹치지 않게 숫자를 입력하세요.");
-        }
-      } catch (error) {
-        Console.print("[ERROR]" + error.message);
-        continue;
+    try { // 겹치는 숫자 확인
+      if (new Set(user_Number).size != user_Number.length) {
+        throw new Error("겹치지 않게 숫자를 입력하세요.");
       }
+    } catch (error) {
+      Console.print("[ERROR]" + error.message);
+      continue;
+    }
 
-      user_Number = user_Number.split('').map(Number); // 문자로 받은 userNumber를 배열로 바꾸기
+    user_Number = user_Number.split('').map(Number); // 문자로 받은 userNumber를 배열로 바꾸기
 
-      count_Strike = 0;
-      count_Ball_And_Strike = 0;
+    count_Strike = 0;
+    count_Ball_And_Strike = 0;
 
-      // 볼+스트라이크, 스트라이크 갯수 카운트
-      for (let count = 0; count < 3; count++) {
-        if (user_Number[count] == computer[count]) {
-          count_Strike++;
-        }
-        if (user_Number.includes(computer[count])) {
-          count_Ball_And_Strike++;
-        }
+    // 볼+스트라이크, 스트라이크 갯수 카운트
+    for (let count = 0; count < 3; count++) {
+      if (user_Number[count] == computer[count]) {
+        count_Strike++;
       }
-
-      count_Ball = count_Ball_And_Strike - count_Strike;
-
-      // 출력할 문장 변수
-      let print_Sentence = "";
-
-      if (count_Ball_And_Strike == 0) {
-        print_Sentence = "낫싱";
-      }
-
-      if (count_Ball != 0) {
-        print_Sentence = print_Sentence + count_Ball + "볼";
-      }
-
-      if (count_Strike != 0) {
-        print_Sentence = print_Sentence + count_Strike + "스트라이크";
-      }
-
-      Console.print(print_Sentence);
-
-      if (count_Strike == 3) {
-        Console.print(count_Strike + "개의 숫자를 모두 맞히셨습니다! 게임종료");
-        start_Num_Setting = await Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        computer = this.computerNumberPick();
-        continue;
+      if (user_Number.includes(computer[count])) {
+        count_Ball_And_Strike++;
       }
     }
+
+    count_Ball = count_Ball_And_Strike - count_Strike;
+
+    // 출력할 문장 변수
+    let print_Sentence = "";
+
+    if (count_Ball_And_Strike == 0) {
+      print_Sentence = "낫싱";
+    }
+
+    if (count_Ball != 0) {
+      print_Sentence += count_Ball + "볼";
+    }
+
+    if (count_Strike != 0) {
+      print_Sentence += count_Strike + "스트라이크";
+    }
+
+    Console.print(print_Sentence);
+
+    if (count_Strike == 3) {
+      Console.print(count_Strike + "개의 숫자를 모두 맞히셨습니다! 게임종료");
+      break;
+    }
+  }
+  let start_Num_Setting = await Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+  if (start_Num_Setting == 1)(await playGame());
+}
+
+class App {
+
+  async play() {
+    // 값이 1이면 게임 진행, 2이면 멈춤
+    await playGame();
   }
 }
+
+const game = new App();
+game.play();
+
 
 export default App;
