925ea5113bc5909bc4f6e36a51b6ab836275553f
song-eun <dmswo546@naver.com>
2023-10-25 17:12:49 +0900

    refactor: 상수 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -9,11 +9,11 @@ const MESSAGE = {
   START: '숫자 야구 게임을 시작합니다.',
   INPUT_NUMBER: '숫자를 입력해주세요 : ',
   CORRECT_ANSWER: `${ANSWER_LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료`,
-  RESTART_STOP: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+  RESTART_STOP: `게임을 새로 시작하려면 ${GAME.RESTART}, 종료하려면 ${GAME.STOP}를 입력하세요.`,
   STOP: `게임 종료`,
   ERROR: {
     INPUT: '[ERROR] 숫자가 잘못된 형식입니다.',
-    RESTART: '[ERROR] 숫자가 1 또는 2가 아닙니다.',
+    RESTART: `[ERROR] 숫자가 ${GAME.RESTART} 또는 ${GAME.STOP}가 아닙니다.`,
   },
 };
 
--- src/utils/makeComputerNumber.js
+++ src/utils/makeComputerNumber.js
@@ -1,9 +1,10 @@
 const { Random } = require('@woowacourse/mission-utils');
+const { ANSWER_LENGTH } = require('../constants/constants').default;
 
 function makeComputerNumber() {
   let computer = '';
 
-  while (computer.length < 3) {
+  while (computer.length < ANSWER_LENGTH) {
     const number = Random.pickNumberInRange(1, 9);
     // 중복 없이 생성
     if (!computer.includes(number)) {
