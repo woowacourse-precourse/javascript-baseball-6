78433af5bbf1f9c388ef44a0a73c0ee963efdf2d
ChoByungHyun <cbj973@gmail.com>
2023-10-25 21:56:58 +0900

    ♻️ Refactor: 테스트 버그 수정

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,6 +1,6 @@
 const INPUT_MESSAGE = Object.freeze({
   NUMBER: "숫자를 입력해주세요 : ",
-  RETRY: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  RETRY: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
 });
 
 const OUTPUT_MESSAGE = Object.freeze({
