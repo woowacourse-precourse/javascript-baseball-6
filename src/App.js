fb192f6e79320eb9e4b9aa93fecfe6ac68fac3a4
yunji <dbswl330@gmail.com>
2023-10-25 15:13:09 +0900

    refactor: 게임 종료 여부를 결정하는 isGameEnded 변수 추가

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ class App {
   constructor() {
     this.balls = new Balls();
     this.view = new GameView();
+    this.isGameEnded = false;
   }
+
   async play() {
     this.view.printStartGame("숫자 야구 게임을 시작합니다.");
 
-    while (true) {
+    while (!this.isGameEnded) {
       const inputNumbers = await this.view.getPlayerGuess();
       this.balls.isValidInput(inputNumbers);
 
