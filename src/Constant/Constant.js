51cdecb3e51329d744af60b1f54e5ba0146e9d7c
OhHyukJun <kmh7277@naver.com>
2023-10-25 17:57:31 +0900

    Chore: 상수 설정

---
+++ src/Constant/Constant.js
@@ -0,0 +1,12 @@
+export const MESSAGE = {
+    START: "숫자 야구 게임을 시작합니다.",
+    INPUT: "숫자를 입력해주세요 : ",
+    END: "3스트라이크!\n3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+    RESET: "게임을 다시 시작하려면 1, 게임을 종료하려면 2를 입력하세요.",
+  };
+  
+  export const ERROR = {
+    INVALID_INPUT: "[ERROR] 제시된 조건을 확인해주세요 (1~9까지의 서로 다른 3자리의 수)",
+    INVALID_REPLAY: "[ERROR] 1 또는 2를 입력해주세요.",
+  };
+  
