cb58abaf2418e4906307b6245dc166b90de000da
Dobbymin <gmin0701@knu.ac.kr>
2023-10-25 22:09:39 +0900

    [FIX]: strike, ball 변수명 변경

--- src/components/checkGameResults.js
+++ src/components/checkGameResults.js
@@ -1,12 +1,12 @@
 export default function checkGameResults(userNumbers, computerNumbers) {
     const value = {
-        strikeCount: 0,
-        ballCount: 0,
+        strike: 0,
+        ball: 0,
     };
 
     for (let i = 0; i < computerNumbers.length; i++) {
         if (userNumbers[i] === computerNumbers[i]) {
-            value.strikeCount++;
+            value.strike++;
         } else if (computerNumbers.includes(userNumbers[i])) {
             value.ball++;
         }
