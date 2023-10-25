d1f21ee12aaba26f5017f3f06e0a65624aa7ce73
DESKTOP-55LJG2P\교육생47 <onmer99@gmail.com>
2023-10-25 14:20:01 +0900

    feat: 숫자야구게임 구현

---
+++ __tests__/FeatureTest.js
@@ -0,0 +1,138 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const App = require('../src/App');
+
+const mockQuestions = answers => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce(
+    (acc, input) =>
+      acc.mockImplementationOnce((question, callback) => {
+        callback(input);
+      }),
+    MissionUtils.Console.readLine,
+  );
+};
+
+const mockRandoms = numbers => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickNumberInRange,
+  );
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('숫자 야구 게임', () => {
+  test('게임 시작 인사 출력 확인', () => {
+    const logSpy = getLogSpy();
+    const app = new App();
+    app.printStartNotification();
+
+    expect(logSpy).toHaveBeenCalledWith('숫자 야구 게임을 시작합니다.');
+  });
+
+  test('랜덤 숫자 생성 확인', () => {
+    const randoms = [2, 3, 5];
+
+    mockRandoms(randoms);
+
+    const app = new App();
+    app.play();
+
+    const currentRandom = app.randomNumber.join('');
+
+    expect(currentRandom).toBe('235');
+  });
+
+  test('사용자의 값 입력 확인', () => {
+    const answers = ['245'];
+
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    const currentUserInput = app.userInput.join('');
+
+    expect(currentUserInput).toBe('245');
+  });
+
+  test('사용자 입력 값과 랜덤 생성 숫자 비교 확인', () => {
+    const randoms = [1, 3, 6];
+    const answers = ['789', '689', '189', '163', '136'];
+    const logSpy = getLogSpy();
+    const messages = [
+      '낫싱',
+      '1볼',
+      '1스트라이크',
+      '2볼 1스트라이크',
+      '3스트라이크',
+      '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach(output => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  })
+
+  test('게임 종료 후 재시작 테스트', () => {
+    const randoms = [1, 3, 5, 5, 8, 9];
+    const answers = ['246', '135', '1', '597', '589', '2'];
+    const logSpy = getLogSpy();
+    const messages = [
+      '낫싱',
+      '3스트라이크',
+      '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+      '1볼 1스트라이크',
+      '3스트라이크',
+      '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+      '게임 종료',
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach(output => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test('예측 숫자 입력 값 예외 테스트', () => {
+    const randoms = [1, 3, 5];
+    const answers = ['1234'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow(new Error('잘못된 값을 입력하셨습니다.'));
+  });
+
+  test('재시작 입력 값 예외 테스트', () => {
+    const randoms = [1, 3, 5, 5, 8, 9];
+    const answers = ['246', '135', '3'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow(new Error('잘못된 값을 입력하셨습니다.'));
+  });
+});
--- src/App.js
+++ src/App.js
@@ -1,13 +1,135 @@
-// src/index.js
-const { Console, Random } = require('@woowacourse/mission-utils');
-const Game = require('./components/Game');
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const { Console, Random } = MissionUtils;
 
 class App {
-  async play() {
-    await Game.startGame();
-    Console.close();
+  #userInput;
+
+  #randomNumber;
+
+  constructor() {
+    this.#userInput = '';
+    this.#randomNumber = '';
+    this.gameResults = {};
+    this.compare = {
+      isStrike: (num, idx) => this.#randomNumber[idx] === num,
+      isBall: (num, idx) => this.#randomNumber[idx] !== num && this.#randomNumber.includes(num),
+    };
+    this.inputCheck = {
+      isLenThree: answer => answer.length === 3,
+      isInt: answer => Number.isInteger(+answer),
+      isNegative: answer => Math.sign(answer) === -1,
+    };
+  }
+
+  print(message) {
+    Console.print(message);
+  }
+
+  printStartNotification() {
+    this.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  generateRandomNumber() {
+    while (this.#randomNumber.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!this.#randomNumber.includes(number)) this.#randomNumber += `${number}`;
+    }
+    this.#randomNumber = [...this.#randomNumber];
+  }
+
+  getUserInput() {
+    Console.readLine('숫자를 입력해주세요 : ', answer => {
+      const { isLenThree, isInt, isNegative } = this.inputCheck;
+      if (!isLenThree(answer) || !isInt(answer) || isNegative(answer))
+        throw new Error('잘못된 값을 입력하셨습니다.');
+
+      this.#userInput = [...answer];
+      this.gameResults = {};
+
+      this.getResult();
+      this.printResult();
+      this.restart();
+    });
+  }
+
+  getResult() {
+    const { gameResults } = this;
+    const { isStrike, isBall } = this.compare;
+
+    this.#userInput.forEach((num, idx) => {
+      if (isStrike(num, idx)) gameResults.strike = gameResults.strike + 1 || 1;
+      if (isBall(num, idx)) gameResults.ball = gameResults.ball + 1 || 1;
+    });
+  }
+
+  printResult() {
+    const { strike, ball } = this.gameResults;
+    const { print } = this;
+
+    const ballMessage = `${ball ? `${ball}볼 ` : ''}`;
+    const strikeMessage = `${strike ? `${strike}스트라이크 ` : ''}`;
+    const nothingMessage = `${!strike && !ball ? '낫싱' : ''}`;
+
+    print(ballMessage + strikeMessage + nothingMessage);
+    if (strike === 3) print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    else this.getUserInput();
+  }
+
+  restart() {
+    Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', answer => {
+      if (answer === '1') {
+        this.#randomNumber = '';
+        this.start();
+        return;
+      }
+      if (answer === '2') {
+        this.print('게임 종료');
+        Console.close();
+        return;
+      }
+      Console.close();
+      throw new Error('잘못된 값을 입력하셨습니다.');
+    });
+  }
+
+  get userInput() {
+    return this.#userInput;
+  }
+
+  get randomNumber() {
+    return this.#randomNumber;
+  }
+
+  start() {
+    this.generateRandomNumber();
+    this.getUserInput();
+  }
+
+  play() {
+    this.printStartNotification();
+    this.start();
   }
 }
 
 const app = new App();
 app.play();
+
+module.exports = App;
+
+
+
+
+// // src/index.js
+// const { Console, Random } = require('@woowacourse/mission-utils');
+// const Game = require('./components/Game');
+
+// class App {
+//   async play() {
+//     await Game.startGame();
+//     Console.close();
+//   }
+// }
+
+// const app = new App();
+// app.play();
