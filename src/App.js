a8d990ada87f120c5f59ae88405dc59ac3e5e3a7
JoSooha <dlrhdcjs@naver.com>
2023-10-25 16:01:20 +0900

    feat: 정상 input에 대한 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,77 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+// const MISSIONUTILS = require("@woowacourse/mission-utils");
+
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.")
+    while(1) {
+      const isAgain = await this.onePlay();
+      if(isAgain===2) break;
+    }
+  }
+
+  async onePlay() {
+    let computer = this.makeAnswer();
+
+    let isAnswer = false;
+    while(!isAnswer) {
+      const readInput = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+      if(readInput) {
+        const [strikeNum, ballNum] = this.checkInput(readInput, computer);
+        const resultString = this.makeResultString(strikeNum, ballNum);
+        MissionUtils.Console.print(resultString);
+        if(strikeNum===3) break;
+      } else break;
+    }
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    const isAgain = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+    return isAgain
+  }
+
+  makeResultString(s, b) {
+    let string;
+    if(s===0) {
+      if(b===0) string = "낫싱";
+      else string = `${b}볼`;
+    } else { //스트 존재
+      if(b===0) string = `${s}스트라이크`;
+      else string = `${b}볼 ${s}스트라이크`
+    }
+    return string
+  }
+
+  checkInput(input, answer) {
+    let strike = 0;
+    let ball = 0;
+    //잘못 들어온 경우 예외처리!
+    for(let i=0; i<3; i++) {
+      if(input[i]===answer[i]) { //자리수가 같으면 스트
+        strike++;
+      } else if(answer.includes(input[i])){ //같진 않지만 포함 시 볼 
+        ball++;
+      }
+    }
+
+    return [strike, ball]
+  }
+
+  makeAnswer() {
+    const computer = []; //야구 게임 한번 할때마다 
+    while(computer.length < 3) {
+      const num = MissionUtils.Random.pickNumberInRange(1,9);
+      if(!computer.includes(num)){
+        computer.push(num);
+      }
+    }
+    return computer
+  }
 }
 
+// const app = new App();
+// app.play();
+// const result = app.checkInput("132", "123")
+// console.log(result)
 export default App;
+
+// Console.readLineAsync, Console.print
