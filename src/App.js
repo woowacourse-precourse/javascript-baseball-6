98435cdd0dfe2f38c05ed1fdd4ad802d2577e727
delee3064 <delee3064@gmail.com>
2023-10-25 17:40:50 +0900

    feat : 랜덤으로 서로 다른 3자리 수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    const num = this.setNum()
+  }
+  
+  setNum() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
 }
 
 export default App;
