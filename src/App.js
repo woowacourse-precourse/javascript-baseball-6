7cf5dfb7bfe02423726be104d997f1c0ac92e2ea
jiyun1006 <jiyun4251@gmail.com>
2023-10-25 16:13:58 +0900

    🔋 feat : makeNumber, inputNumber 메서드 작성
    정답 수를 만드는 메서드와 입력 수를 받는 메서드 작성 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
-class App {
-  async play() {}
-}
+import { MissionUtils } from "@woowacourse/mission-utils";
 
+
+class App {
+  async play() {
+    
+
+  }
+
+  async makeNumber() {
+    const numberArr = [];
+    while (numberArr.length < 3) {
+      let number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!numberArr.includes(number)) {
+        numberArr.push(number);
+      }
+    }
+    return numberArr;
+  }
+
+  async inputNumber() {
+    try {
+      let number = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요. : ');
+      number = [...number].map(el => +el);
+      return number;
+
+    } catch (error) {
+    // reject 되는 경우
+    }
+  }
+
+  checkNumber(answerNumber, inputNumber) {
+    
+  }
+
+}
+const a = new App();
+a.play();
 export default App;
