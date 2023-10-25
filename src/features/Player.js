b505fb030b1730d9641eb1764a87ebb470846a68
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-10-25 15:30:28 +0900

    FEAT:예외 발생 기능 추가
    * 유효성 검증이 실패하면, 에러 메세지를 담은 에러 객체 를 throw, 실패 프로미즈 반환

--- src/features/Player.js
+++ src/features/Player.js
@@ -1,8 +1,21 @@
 import { Console } from "@woowacourse/mission-utils";
+import { Validation } from "./Validation.js";
 
 export const player = {
   input: async function (inputString) {
-    const input = await Console.readLineAsync(inputString);
-    return input;
+    try {
+      const input = await Console.readLineAsync(inputString);
+      const validation = new Validation(input);
+      const valid = validation.validate();
+      if (!valid) {
+        throw new Error(validation.errorMessage);
+      }
+
+      return input;
+    } catch (error) {
+      return new Promise((_, reject) => {
+        reject(error);
+      });
+    }
   },
 };
