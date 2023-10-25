912a265243d51b62d8b75687015a75d27c409324
Jaehyeok <milkboy2564@naver.com>
2023-10-25 14:00:20 +0900

    feature: 숫자 야구 기능 완성

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-import readline from "readline";
+import { Console, Random } from "@woowacourse/mission-utils";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const ANSWER_TABLE = {
+  "0b0s": "낫싱",
+  "1b0s": "1볼",
+  "2b0s": "2볼",
+  "3b0s": "3볼",
+  "0b1s": "1스트라이크",
+  "1b1s": "1볼 1스트라이크",
+  "2b1s": "2볼 1스트라이크",
+  "0b2s": "2스트라이크",
+  "1b2s": "1볼 2스트라이크",
+  "0b3s": "3스트라이크",
+};
 
 class App {
-  _randomNumber = [];
-  _usersNumber = [];
-
   constructor() {
-    console.log("숫자 야구 게임을 시작합니다.");
+    Console.print("숫자 야구 게임을 시작합니다.");
     this._randomNumber = this.generateRandomNumber();
   }
 
-  async initGame() {
-    while (true) {
-      const userInput = await MissionUtils.Console.readLineAsync(
-        "숫자를 입력해주세요: "
-      );
-
-      this.validateUserInput(userInput);
-    }
+  play() {
+    this.getUserInput();
   }
 
   generateRandomNumber() {
     const randomNumber = [];
 
     while (randomNumber.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      const number = Random.pickNumberInRange(1, 9);
 
       if (!randomNumber.includes(number)) {
         randomNumber.push(number);
@@ -39,21 +37,65 @@ class App {
     return randomNumber;
   }
 
-  validateUserInput(number) {
-    let isCorrectAnswer;
+  async getUserInput() {
+    const userInput = await Console.readLineAsync("숫자를 입력해주세요: ");
 
-    // input 검증
+    this.validateUserInput(userInput);
+  }
 
-    if (isCorrectAnswer) {
+  validateUserInput(userInput) {
+    let usersAnswer = this.checkCorrectAnswer(userInput);
+    let isValidAnswer = this.checkValidAnswer(userInput);
+
+    if (!isValidAnswer) {
+      throw new Error(
+        "[ERROR] 올바르지 않은 숫자입니다. 프로그램을 종료합니다."
+      );
+    }
+
+    if (usersAnswer === "0b3s") {
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       this.checkContinueOrExit();
     } else {
-      this.initGame();
+      Console.print(ANSWER_TABLE[usersAnswer]);
+      this.getUserInput();
     }
   }
 
+  checkValidAnswer(userInput) {
+    if (
+      userInput.length !== 3 ||
+      new Set(userInput).size !== 3 ||
+      userInput.split("").some(isNaN)
+    ) {
+      return false;
+    }
+
+    return true;
+  }
+
+  checkCorrectAnswer(userInput) {
+    let strike = 0;
+    let ball = 0;
+
+    const usersNumberArr = userInput.split("").map((num) => parseInt(num));
+
+    usersNumberArr.forEach((num, idx) => {
+      if (!this._randomNumber.includes(num)) return;
+
+      if (this._randomNumber.indexOf(num) === idx) {
+        strike++;
+      } else {
+        ball++;
+      }
+    });
+
+    return `${ball}b${strike}s`;
+  }
+
   async checkContinueOrExit() {
-    const userInput = await MissionUtils.Console.readLineAsync(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    const userInput = await Console.readLineAsync(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
     );
 
     if (userInput === "1") {
@@ -61,20 +103,13 @@ class App {
     }
 
     if (userInput === "2") {
-      this.checkExitGame();
+      Console.print("게임을 종료합니다.");
     }
   }
 
   checkContinueGame() {
-    this.initGame();
-  }
-
-  checkExitGame() {
-    // 게임 종료
-  }
-
-  async play() {
-    this.initGame();
+    this._randomNumber = this.generateRandomNumber();
+    this.play();
   }
 }
 
