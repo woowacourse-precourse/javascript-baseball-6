c5086fd980cd6c871910cfa9b93ca625d62ef871
yohan <koyohanyoko@gmail.com>
2023-10-25 16:55:21 +0900

    [Refact] 상수 const SNAKE_CASE 수정

--- src/App.js
+++ src/App.js
@@ -6,32 +6,32 @@ class App {
   }
   async play() {
     while (this.isRunning) {
-      const start = this.makeAnswer();
+      const START = this.makeAnswer();
       MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     
       let result;
       while (result !== "3스트라이크") {
-        const input = await MissionUtils.Console.readLineAsync(
+        const INPUT = await MissionUtils.Console.readLineAsync(
           "숫자를 입력해주세요 : "
         );
         if (
-          input.length !== 3 ||
-          new Set(input).size !== 3 ||
-          [...input].some((item) => Number(item) < 1 || Number(item) > 9)
+          INPUT.length !== 3 ||
+          new Set(INPUT).size !== 3 ||
+          [...INPUT].some((item) => Number(item) < 1 || Number(item) > 9)
         ) {
           throw new Error("[ERROR] 제시된 조건을 확인해주세요(1~9까지의 서로 다른 3자리의 수)");
         }
-        result = this.checkAnswer(input, start);
+        result = this.checkAnswer(INPUT, START);
         MissionUtils.Console.print(result);
       }
 
       MissionUtils.Console.print("3스트라이크 3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
-      const replay = await MissionUtils.Console.readLineAsync(
+      const REPLAY = await MissionUtils.Console.readLineAsync(
         "게임을 다시 시작하려면 1 / 게임을 종료하려면 2를 입력하세요."
       );
 
-      if (replay === "2") {
+      if (REPLAY === "2") {
         this.isRunning = false;
       }
     }
@@ -40,8 +40,8 @@ class App {
   makeAnswer() {
     const computer = [];
     while (computer.length < 3) {
-      const randomNum = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(randomNum)) computer.push(randomNum);
+      const RANDOM_NUM = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(RANDOM_NUM)) computer.push(RANDOM_NUM);
     }
   
     return [...computer];
@@ -60,7 +60,9 @@ class App {
     }
     if (strike === 0 && ball === 0) return "낫싱";
 
-    return `${ball ? ball + "볼" : ""} ${strike ? strike + "스트라이크" : ""}`.trim();
+    const ANSWER = `${ball ? ball + "볼" : ""} ${strike ? strike + "스트라이크" : ""}`.trim();
+    
+    return ANSWER;
   }
 }
 
