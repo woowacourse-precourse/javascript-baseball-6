87277e3d1205ab0e1a16cc568ca28c58b9c21a0d
jayming66 <mjw8941@gmail.com>
2023-10-25 12:19:45 +0900

    feat : 컴퓨터의 숫자 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-class App {
-  async play() {}
+import { Console, Random } from "@woowacourse/mission-utils";
+
+function randomNumber() {
+  const randomNumbers = [];
+  for (let i = 0; i < 3; i++) {
+    // 3개의 숫자
+    const randomNumber = Random.pickNumberInRange(1, 9); // 1 ~ 9의 범위
+    if (randomNumbers.includes(randomNumber) != true) {
+      // 서로 다른 3개의 숫자가 되도록
+      randomNumbers.push(randomNumber);
+    }
+  }
+  return randomNumbers;
 }
+
+class App {
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다."); // 게임 시작 문구
+    let randaomNumbers = randomNumber(); // 컴퓨터의 숫자 3개 생성
+    Console.print(randaomNumbers);
+  }
+}
+
+const app = new App();
+app.play();
 
 export default App;
