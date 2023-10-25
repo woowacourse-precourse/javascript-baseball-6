9760dab3fefd332b7e5504c7b70c89c4f7d0a57a
suminson <sabi1111@nate.com>
2023-10-25 22:54:44 +0900

    feat: 사용자 숫자 입력받기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   constructor() {
-    this.random = [];
+    this.randomNum = [];
+    this.userNum = [];
   }
   async play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.randomNumber();
+    await this.userNumber();
   }
 
   randomNumber() {
-    const random = [];
-    while (random.length < 3) {
+    const randomNum = [];
+    while (randomNum.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!random.includes(number)) random.push(number);
+      if (!randomNum.includes(number)) randomNum.push(number);
     }
-    this.random = random;
+    this.randomNum = randomNum;
+  }
+  async userNumber() {
+    const userNum = await MissionUtils.Console.readLineAsync(
+      "숫자를 입력해주세요 : "
+    );
+    this.userNum = userNum.split("").map(Number);
   }
 }
 
