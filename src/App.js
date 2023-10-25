ba7ce251ec39aba95c9ccd5f3d3ad8dabace3c42
정수아 <sua@jeongsuaui-MacBookAir.local>
2023-10-25 20:27:11 +0900

    [feat] 컴퓨터 숫자와 유저 숫자 비교하는 함수

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 class App {
 
+  //컴퓨터의 3자리 난수 생성
+  genComputerNum = () => {
+  const GOAL = [];
+    while (GOAL.length < 3) {
+      const RANDOM_NUMBER = Random.pickNumberInRange(1, 9);
+      if (!GOAL.includes(RANDOM_NUMBER)) {
+        GOAL.push(RANDOM_NUMBER);
+      }
+    }
+    return GOAL;
+  }
+
+  //유저의 입력 받기
   async getUserNum() {
     console.log("getusernum fun")
     const input = await Console.readLineAsync("숫자를 입력해주세요 : ");
@@ -40,6 +53,7 @@ class App {
     return(arrayToSet.size !== 3 ? false : true)
   }
 
+  //유저가 입력한 숫자가 유효한지 판별
   checkUserNum = (inputNumArray) => {
     console.log("checkusernum fun")
     const valNum = this.checkNum(inputNumArray);
@@ -50,11 +64,43 @@ class App {
     return (valNum && valLen && valDuple)
   }
 
+  //컴퓨터가 생성한 수와 유저가 입력한 수를 비교하여 스트라이크와 볼 수를 반환
+  compare = (GOAL, INPUT) => {
+    let strike =0;
+    let ball = 0;
+    for(let i=0; i<GOAL.length ; i++){
+      if(GOAL[i] === INPUT[i]){
+        strike++;
+      }else if(INPUT.includes(GOAL[i])){
+        ball++;
+      }
+    }
+    return({strike, ball})
+  }
+
   async play() {
     console.log("play fun")
+    const GOAL = this.genComputerNum();
     const inputNumArray = await this.getUserNum();
     const validateNum = this.checkUserNum(inputNumArray);
 
+    const STRIKE = this.compare(GOAL, inputNumArray).strike;
+    const BALL = this.compare(GOAL, inputNumArray).ball;
+
+    let OUTTEXT = '';
+    if(BALL > 0 && STRIKE > 0){
+      OUTTEXT = `${BALL} 볼 ${STRIKE} 스트라이크`
+    }else if(BALL > 0){
+      OUTTEXT = `${BALL} 볼`
+    }else if(STRIKE > 0){
+      OUTTEXT = `${STRIKE} 스트라이크`
+    }else{
+      OUTTEXT = '낫싱';
+    }
+
+    if(STRIKE === 3){
+      //end
+    }
     console.log(validateNum);
   }  
 }
