8c907f1ca0ff743dbf3a82d2f263b840eddb03d1
luckylucyj <jeyun96@gmail.com>
2023-10-25 16:37:55 +0900

    docs : README.md 기능 구현 리스트 정리

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 
-// let numberList;
-// let answerArray;
 
-// class App {
+class App {
 
-//   constructor( numberList,answerArray){
+ 
+  async play() {}
 
-//   }
-//       function makeAnswer() {
-//         numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-//         answerArray = [];
-//         for (let i = 0; i < 4; i++) {
-//           let picked = [];
-//           picked = numberList.splice(
-//             Math.floor(Math.random() * (10 - i)),
-//             1
-//           )[0];
-//           answerArray.push(picked);
-//         }
-//       }
-//       makeAnswer();
-//       console.log(answerArray);
-//   async play() {}
-// }
 
-// export default App;
+}
 
-// const app = new App();
-// app.play();
+
+
+
+export default App;
+
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import App from "../src/App.js";
+import {Console, Ramdom} from "../node_modules/@woowacourse/mission-utils";
+let gameStart=prompt('게임을 시작할까요?');
+
+const app = new App();
+app.play();
