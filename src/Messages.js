629982becec77f6ec05761972b34fbd3cae9d9c4
01McCoy <unogle12@gmail.com>
2023-10-25 18:07:40 +0900

    chore: 메시지 상수 지정 및 동결

---
+++ src/Messages.js
@@ -0,0 +1,13 @@
+const MESSAGES = {
+  GAME_START: "숫자 야구 게임을 시작합니다.",
+  GET_INPUT_NUMBER: "숫자를 입력해주세요 : ",
+  USER_WIN: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  GAME_END: "게임 종료",
+  ERROR: {
+    INVALID_INPUT_NUMBER: "[ERROR] 숫자의 형식이 잘못되었습니다.",
+    INVALID_RESTART_NUMBER: "[ERROR] 1 또는 2 중 하나의 숫자를 입력해주세요.",
+  },
+};
+
+export default MESSAGES;
