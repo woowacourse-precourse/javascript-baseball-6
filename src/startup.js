dabf8b59983f9b26ca2a95e9138ec30ed4b47c1e
소인섭 <sis9745@gachon.ac.kr>
2023-10-25 21:02:14 +0900

    refactor: 게임 시작 시, 메시지 출력하는 함수명 변경
    - 기존: startGame
    - 변경: printGameStartMessage

--- src/startup.js
+++ src/startup.js
@@ -1,7 +1,7 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-function startGame() {
+function printGameStartMessage() {
   MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
 }
 
-export default startGame;
+export default printGameStartMessage;
