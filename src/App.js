9800f0b29ff10b61bb4b06526e26d3fd912818eb
kimmoonju-102 <kimmun3549@naver.com>
2023-10-25 16:17:19 +0900

    Feat | 사용자에게 숫자 3자리 받기 및 유효성 검사

--- src/App.js
+++ src/App.js
@@ -25,9 +25,32 @@ const ComputerNumbers = async () => {
   return computer;
 };
 
-const getUserInput = async (computer) => {};
+const getUserInput = async (computer) => {
+  let userInput = "";
+  try {
+    userInput = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 :");
+    await checkInput(userInput);
+    const user = userInput.split("").map(Number);
+    await compareNumbers(user, computer);
+  } catch (error) {
+    await MissionUtils.Console.print(error);
+  }
+};
 
-const checkInput = async (userInput) => {};
+const checkInput = async (input) => {
+  let inputArr = input.toString().split("");
+  if (isNaN(input) || !Number(input)) {
+    throw "숫자가 아닙니다.";
+  } else if (input < 0) {
+    throw "양수가 아닙니다.";
+  } else if (inputArr.length !== 3) {
+    throw "세자리 수가 아닙니다.";
+  } else if (new Set(inputArr).size !== 3) {
+    throw "중복되지 않은 세자리 수가 아닙니다.";
+  } else if (inputArr.includes("0")) {
+    throw "0을 포함하고 있습니다.";
+  }
+};
 
 const compareNumbers = async (user, computer) => {};
 
