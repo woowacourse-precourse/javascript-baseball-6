53ffef112b3695f74ad10be4f76731912b93c4c7
jwoo08 <charm_ing0@naver.com>
2023-10-25 23:00:52 +0900

    feat: App.js에 게임 제어 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Console } from "@woowacourse/mission-utils";
+import Game from "./Game.js";
+import { getContinueGame, getNumber } from "./getInput.js";
+
 class App {
-  async play() {}
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+
+    let play = true;
+    let game = new Game();
+
+    while (play) {
+      const numbers = await getNumber();
+      const result = game.checkNumbers(numbers);
+      if (result) {
+        let restart = await getContinueGame();
+        if (restart === "1") {
+          game = new Game();
+        } else {
+          break;
+        }
+      }
+    }
+  }
 }
 
 export default App;
