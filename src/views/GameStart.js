d601c9a3817978b86c5d935e9dde65d99d4c620a
Jinnmidnnight <vkstk0131@yonsei.ac.kr>
2023-10-25 16:53:16 +0900

    feat(views): GameStart.js 생성

---
+++ src/views/GameStart.js
@@ -0,0 +1,8 @@
+import { Console } from "@woowacourse/mission-utils";
+import MESSAGES from "../constants/Messages.js";
+
+const GAME_START = () => {
+  Console.print(MESSAGES.GAME_START);
+};
+
+export default GAME_START;
