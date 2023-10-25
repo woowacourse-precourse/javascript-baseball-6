25c725d6c45ccab08c6372286764e695cd276d68
UBiiiii <willowman55@gmail.com>
2023-10-25 17:21:37 +0900

    [feat] game() 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import { Random, Console } from "@woowacourse/mission-utils"
+
 class App {
   async play() {
 
   }
 
   async game() {
+    const value = [];
+    while (value.length < 3) {
+      const num = Random.pickNumberInRange(1, 9);
+      if (!value.includes(num)) value.push(num);
+    }
+    while (true) {
+      const sign = [0, 0];
+      let line;
+      try {
+        line = await Console.readLineAsync("숫자를 입력해주세요 : ");
+        if (line.length !== 3) throw new Error("[ERROR] 숫자 형식이 맞지 않습니다.")
+      } catch (error) {
+        Console.print(error.message);
+        continue;
+      }
 
+      for (let i = 0; i < 3; i++) {
+        const idx = line.indexOf(value[i]);
+        if (idx == i) sign[0]++;
+        else if (idx != -1) sign[1]++;
+      }
+
+      if (sign[0] == 3) {
+        Console.print("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        break;
+      }
+      else {
+        let answer = '';
+
+        if (sign[1] > 0) answer += sign[1] + "볼";
+        if (sign[0] > 0) answer += sign[0] + "스트라이크";
+        if (answer.length == 0) answer = "낫싱"
+
+        Console.print(answer);
+      }
+    }
   }
 
   async check() {
