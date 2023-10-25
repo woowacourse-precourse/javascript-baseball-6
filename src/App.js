de4ba8ea04dd74ab91a35b803725b91d4db6359a
greeneeya <suzythetravelerr@gmail.com>
2023-10-25 16:01:15 +0900

    [refactor] message 파일 분리

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import { Random, Console } from '@woowacourse/mission-utils';
-export const ErrorMessage = Object.freeze({
-  oneOrTwo: '[ERROR] 1 또는 2만 입력 가능합니다.',
-  onlyNumbers: '[ERROR] 숫자만 입력 가능합니다.',
-  threeDigits: '[ERROR] 세 자리 수여야 합니다.',
-  oneToNine: '[ERROR] 각 자리 수는 1~9 사이의 수여야 합니다.',
-  differentDigits: '[ERROR] 각 자리 수는 모두 다른 수여야 합니다.',
-});
-export const ConsoleMessage = Object.freeze({
-  userNumber: '숫자를 입력해주세요 : ',
-  allCorrect: '3개의 숫자를 모두 맞히셨습니다! 게임종료',
-  resumeGame: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
-});
+import { ErrorMessage, ConsoleMessage } from './Messages.js';
+
 class App {
   #computer;
-
   static DIGITS_COUNT = 3;
   static MIN_DIGIT = 1;
   static MAX_DIGIT = 9;
@@ -22,7 +11,6 @@ class App {
 
   async play() {
     this.init();
-    console.log(this.computer);
     this.playBaseball(this.computer);
   }
 
@@ -82,6 +70,7 @@ class App {
     if (strike && !ball) Console.print(`${strike}스트라이크`);
     if (!strike && ball) Console.print(`${ball}볼`);
     if (!strike && !ball) Console.print('낫싱');
+
     if (strike === App.DIGITS_COUNT) return true;
     else return false;
   }
---
+++ src/Messages.js
@@ -0,0 +1,13 @@
+export const ErrorMessage = Object.freeze({
+  oneOrTwo: '[ERROR] 1 또는 2만 입력 가능합니다.',
+  onlyNumbers: '[ERROR] 숫자만 입력 가능합니다.',
+  threeDigits: '[ERROR] 세 자리 수여야 합니다.',
+  oneToNine: '[ERROR] 각 자리 수는 1~9 사이의 수여야 합니다.',
+  differentDigits: '[ERROR] 각 자리 수는 모두 다른 수여야 합니다.',
+});
+
+export const ConsoleMessage = Object.freeze({
+  userNumber: '숫자를 입력해주세요 : ',
+  allCorrect: '3개의 숫자를 모두 맞히셨습니다! 게임종료',
+  resumeGame: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+});
