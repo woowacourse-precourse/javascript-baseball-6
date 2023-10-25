5b8b7416ff583725a7dc944419f89d8c49f5c1fb
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-10-25 18:52:56 +0900

    feat: 플레이어 입력 받는 함수 생성

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 class App {
   constructor() {
     this.computer = [];
+    this.userInput = [];
   }
 
   async play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     this.makeRandomNumbers();
+    await this.getUserInput();
   }
 
   makeRandomNumbers() {
@@ -17,6 +19,13 @@ class App {
       if (!computer.includes(number)) computer.push(number);
     }
     this.computer = computer;
+  }
+
+  async getUserInput() {
+    const userInput = await MissionUtils.Console.readLineAsync(
+      '숫자를 입력해주세요 : ',
+    );
+    this.userInput = userInput.split('').map(Number);
   }
 }
 
