3dad742997e0b6533c1646e617b98006ecd19300
SSY1203 <tlstjsdud566@naver.com>
2023-10-25 21:50:20 +0900

    [feature] 숫자 야구 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 class App {
-  async play() {}
+  constructor() {
+    this.pitcherNumbers = [];
+    this.hitterNumbers = [];
+    this.score = [0, 0]; // [ball, strike]
+  }
+
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
 }
 
 export default App;
