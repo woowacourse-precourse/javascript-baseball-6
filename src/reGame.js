58f36b200a90702de0d247a1a9a9dcbe715d4d6d
Jeon Eonseok <jseokeon@gmail.com>
2023-10-25 22:07:20 +0900

    fix: startGame import 추가

--- src/reGame.js
+++ src/reGame.js
@@ -1,5 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import generateRandomNum from './generateRandomNum';
+import startGame from './startGame';
 
 export default async function reGame() {
   const RE_GAME = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
