91cdccc0553ccba37c4f59af8e120a4ab34e10c0
jadewisemann <jadewisemann@gmail.com>
2023-10-25 19:56:44 +0900

    refactor: 코딩 컨벤션에 맞게 수정함

--- src/App.js
+++ src/App.js
@@ -1,51 +1,30 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 
-
 class App {
   async play() {
-    let KEEP_PLAY = true
-    
-    while (KEEP_PLAY) {
-      let HOME_RUN = false
-      let TARGET_NUMBER = getRandomNumber()
-
+    let isKeepPlaying = true
+    while (isKeepPlaying) {
+      let isCorrectAnswer = false
+      let targetNumber = getRandomNumber()
       Console.print("숫자 야구 게임을 시작합니다.")
 
-      while (!HOME_RUN) {
-        let printMsg = ""
+      while (!isCorrectAnswer) {
+        let printMassage = ""
+
         const userInputNumber = await Console.readLineAsync('숫자를 입력해 주세요 : ')
-
-        if (!checkExceptionalInput(userInputNumber)) {
-          throw new Error('[ERROR] 중복되지 않은 3자리 숫자를 입력해 주세요.')
-        }
-
-        const userInputArray = numberToArray(userInputNumber)
-        let result = compareTwo3digitNumbers(TARGET_NUMBER, userInputArray)
-        
-        result[1] == 3 ?  HOME_RUN = true : {}
-        printMsg = `${result[0] !== 0 ? (result[0] + '볼 ') : ""}${result[1] !== 0 ? (result[1] + '스트라이크') : ""}`
-        !printMsg && (printMsg = "낫싱")
-        Console.print(printMsg)
+        if (!checkExceptionalInput(userInputNumber)) { throw new Error('[ERROR] 중복되지 않은 3자리 숫자를 입력해 주세요.') }
+        const { ballCounter, strikeCounter } = compareTwo3digitNumbers(targetNumber, userInputNumber)
+        strikeCounter === 3 ?  isCorrectAnswer = true : {}
+        printMassage = `${ballCounter !== 0 ? (ballCounter + '볼 ') : ''}${strikeCounter !== 0 ? (strikeCounter + '스트라이크') : ""}`
+        !printMassage && (printMassage = '낫싱')
+        Console.print(printMassage)
       }
       Console.print('3개의 숫자를 모두맞히셨습니다! 게임 종료')
       let userInput = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'); 
-      (userInput == 2) && (KEEP_PLAY = false)
+      (userInput === 2) && (isKeepPlaying = false)
     }
   }
-
-  
-
-  getRandomNumber() {
-    const randomNumber = []; 
-    
-    while (randomNumber.length < 3) {
-      const digit = Random.pickNumberInRange(1, 9)
-      !randomNumber.includes(digit) && randomNumber.push(digit)
-    }
-
-    return randomNumber
-  }  
 }
 
 function getRandomNumber() {
@@ -57,24 +36,20 @@ function getRandomNumber() {
   return randomNumber
 }
 
-
 function checkExceptionalInput(input) {
-  return input.toString().length == 3 && !isNaN(input) ? true : false
+  return input.toString().length === 3 && !isNaN(input) ? true : false
 }
 
 function compareTwo3digitNumbers(targetNumber, userInputNumber) {
-  let strike = 0;
-  let ball = 0; 
-
-  userInputNumber.forEach((digit, index) => {
+  let [ballCounter, strikeCounter] = [0,0]
+  numberToArray(userInputNumber).forEach((digit, index) => {
     targetNumber.includes(digit) && (
-      targetNumber[index] == digit 
-        ? strike ++
-        : ball++
+      targetNumber[index] === digit
+        ? strikeCounter++
+        : ballCounter++
     )
   })
-
-  return [ball, strike];
+  return { ballCounter, strikeCounter };
 }
 
 function numberToArray(targetNumber) { 
