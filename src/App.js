269fa179207f014202547d9b4b33aec57f217eac
future4ing <mirae4120@gmail.com>
2023-10-25 16:09:59 +0900

    feat : 힌트 출력하기
    - 스트라이크와 볼의 숫자에 따라 힌트 출력

--- src/App.js
+++ src/App.js
@@ -67,6 +67,18 @@ class App {
 
           [strike, ball] = this.compare(answer, userInputNumber);
 
+          if (strike > 0) {
+            if (ball > 0)
+              MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+            else
+              MissionUtils.Console.print(`${strike}스트라이크`);
+          } else {
+            if (ball > 0)
+              MissionUtils.Console.print(`${ball}볼`);
+            else // strike == ball == 0 일 때
+              MissionUtils.Console.print("낫싱");
+          }
+
         }
 
         MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
