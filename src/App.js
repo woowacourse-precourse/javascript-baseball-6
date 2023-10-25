cc3d89634a1b70fc73aca6d73f523f660f66c7b6
kwonja <kownja87@gmail.com>
2023-10-25 14:14:30 +0900

    add : 사용자는 서로 다른 3개의 숫자를 입력한다

--- src/App.js
+++ src/App.js
@@ -19,15 +19,19 @@ class App {
   }
   async userNumberInput(){
       const userNumber = await Console.readLineAsync('숫자를 입력해주세요 : ');
-      
+      if(this.validation(userNumber)) return userNumber;
+      else{
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다");
+      }
   }
+
   async play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    this.userNumberInput();
   }
 }
 
 const app = new App();
 app.play()
-app.userNumberInput();
 
 export default App;
