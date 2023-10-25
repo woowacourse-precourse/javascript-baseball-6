94bd08f0ae913aac89dcb27526564cb487a40592
jadewisemann <jadewisemann@gmail.com>
2023-10-25 20:04:08 +0900

    fix: 테스트 통과하도록 종료 조건에서 형비교 제외

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ class App {
     while (isKeepPlaying) {
       let isCorrectAnswer = false
       let targetNumber = getRandomNumber()
-      Console.print("숫자 야구 게임을 시작합니다.")
 
+      Console.print("숫자 야구 게임을 시작합니다.")
       while (!isCorrectAnswer) {
         let printMassage = ""
 
         const userInputNumber = await Console.readLineAsync('숫자를 입력해 주세요 : ')
-        if (!checkExceptionalInput(userInputNumber)) { throw new Error('[ERROR] 중복되지 않은 3자리 숫자를 입력해 주세요.') }
+        if (!checkExceptionalInput(userInputNumber)) {
+          throw new Error('[ERROR] 중복되지 않은 3자리 숫자를 입력해 주세요.')
+        }
+
         const { ballCounter, strikeCounter } = compareTwo3digitNumbers(targetNumber, userInputNumber)
         strikeCounter === 3 ?  isCorrectAnswer = true : {}
         printMassage = `${ballCounter !== 0 ? (ballCounter + '볼 ') : ''}${strikeCounter !== 0 ? (strikeCounter + '스트라이크') : ""}`
@@ -22,7 +25,7 @@ class App {
       }
       Console.print('3개의 숫자를 모두맞히셨습니다! 게임 종료')
       let userInput = await Console.readLineAsync('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'); 
-      (userInput === 2) && (isKeepPlaying = false)
+      (userInput == 2) && (isKeepPlaying = false)
     }
   }
 }
