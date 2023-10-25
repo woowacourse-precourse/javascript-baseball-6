657318f672e0343f1d74d73de0210bcd5744201d
Jinnmidnnight <vkstk0131@yonsei.ac.kr>
2023-10-25 22:32:04 +0900

    feat(views): OutputView 파일/함수 추가 * strikeCount와 ballCount에 따라 출력할 결과값 반환

---
+++ src/views/OutputView.js
@@ -0,0 +1,23 @@
+import MESSAGES from "../constants/Messages.js";
+
+/**
+ * 결과값을 출력하는 함수
+ * @param {number} strikeCount
+ * @param {number} ballCount
+ * @returns {string} result
+ */
+const OUTPUT_VIEW = (strikeCount, ballCount) => {
+  let result = "";
+  if (strikeCount === 0 && ballCount === 0) {
+    result = MESSAGES.NOTHING;
+  } else if (strikeCount === 0) {
+    result = `${ballCount}${MESSAGES.BALL}`;
+  } else if (ballCount === 0) {
+    result = `${strikeCount}${MESSAGES.STRIKE}`;
+  } else {
+    result = `${ballCount}${MESSAGES.BALL} ${strikeCount}${MESSAGES.STRIKE}`;
+  }
+  return result;
+};
+
+export default OUTPUT_VIEW;
