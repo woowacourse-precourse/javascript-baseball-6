97434a9316530f885a00f59b1ba5e10015021311
hong <ahi200@naver.com>
2023-10-25 18:14:18 +0900

    - 상수파일 추가 - this가 없는 클래스 메소드를 static으로 변경

--- src/Baseball.js
+++ src/Baseball.js
@@ -1,16 +1,18 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-const MSG_GAME_START = '숫자 야구 게임을 시작합니다.';
-const MSG_GAME_OVER = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
-const MSG_GAME_RESTART = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.';
-const MSG_ENTER_THE_NUMBER = '숫자를 입력해주세요 : ';
-const ERROR_MSG_INPUT_NUMBER = '[ERROR] 숫자가 잘못된 형식입니다.';
-const VALUE_RESTART_GAME = '1';
-const VALUE_END_GAME = '2';
-const VALUE_MAX_GAME_INPUT_DIGIT = 3;
-const RANDOM_NUMBER_RANGE_FROM = 1;
-const RANDOM_NUMBER_RANGE_TO = 9;
-const USER_INPUT_RANGE_FROM = '1';
-const USER_INPUT_RANGE_TO = '9';
+import {
+  MSG_GAME_START,
+  MSG_GAME_OVER,
+  MSG_GAME_RESTART,
+  MSG_ENTER_THE_NUMBER,
+  ERROR_MSG_INPUT_NUMBER,
+  VALUE_RESTART_GAME,
+  VALUE_END_GAME,
+  VALUE_MAX_GAME_INPUT_DIGIT,
+  RANDOM_NUMBER_RANGE_FROM,
+  RANDOM_NUMBER_RANGE_TO,
+  USER_INPUT_RANGE_FROM,
+  USER_INPUT_RANGE_TO,
+} from './constants.js';
 
 class Baseball {
   #computerRandomNumber;
@@ -25,7 +27,9 @@ class Baseball {
     while (isPlaying) {
       const userInput = await this.getUserInput(false);
       const strikeBallCount = this.calcStrikeBallCount(userInput);
-      MissionUtils.Console.print(this.makeGameResultString(strikeBallCount));
+      MissionUtils.Console.print(
+        Baseball.makeGameResultString(strikeBallCount)
+      );
       if (strikeBallCount.strikeCount === VALUE_MAX_GAME_INPUT_DIGIT) {
         isPlaying = await this.checkGameOver();
       }
@@ -42,14 +46,14 @@ class Baseball {
       return false;
     }
   }
-  async getUserInput(gameOver) {
+  async getUserInput(isGameOver) {
     try {
       const userinput = await MissionUtils.Console.readLineAsync(
         MSG_ENTER_THE_NUMBER
       );
-      if (!gameOver && this.checkInputGamePlaying(userinput)) {
+      if (!isGameOver && Baseball.checkInputGamePlaying(userinput)) {
         return userinput;
-      } else if (gameOver && this.checkInputGameOver(userinput)) {
+      } else if (isGameOver && Baseball.checkInputGameOver(userinput)) {
         return userinput;
       }
       throw new Error(ERROR_MSG_INPUT_NUMBER);
@@ -58,7 +62,7 @@ class Baseball {
     }
   }
 
-  checkInputGamePlaying(userinput) {
+  static checkInputGamePlaying(userinput) {
     if (userinput.length !== VALUE_MAX_GAME_INPUT_DIGIT) {
       return false;
     }
@@ -74,7 +78,7 @@ class Baseball {
     return false;
   }
 
-  checkInputGameOver(userinput) {
+  static checkInputGameOver(userinput) {
     if (userinput === VALUE_RESTART_GAME || userinput === VALUE_END_GAME) {
       return true;
     }
@@ -99,7 +103,7 @@ class Baseball {
     let strikeCount = 0;
     let ballCount = 0;
 
-    for (let i = 0; i < this.#computerRandomNumber.length; i++) {
+    for (let i = 0; i < VALUE_MAX_GAME_INPUT_DIGIT; i++) {
       if (this.#computerRandomNumber.charAt(i) === userInput.charAt(i)) {
         strikeCount += 1;
       } else if (this.#computerRandomNumber.includes(userInput.charAt(i))) {
@@ -109,7 +113,7 @@ class Baseball {
     return { strikeCount, ballCount };
   }
 
-  makeGameResultString({ strikeCount, ballCount }) {
+  static makeGameResultString({ strikeCount, ballCount }) {
     if (strikeCount === 0 && ballCount === 0) {
       return `낫싱`;
     } else if (strikeCount !== 0 && ballCount === 0) {
