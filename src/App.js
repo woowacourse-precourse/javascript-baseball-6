87055c97aa14b5edc0aedc8c500509c824b61bde
kwonja <kownja87@gmail.com>
2023-10-25 15:15:12 +0900

    feat : 사용자 숫자와 컴퓨터 숫자를 비교한 결과를 출력한다.

--- src/App.js
+++ src/App.js
@@ -11,27 +11,55 @@ class App {
       }
     }
   }
-  validation(checknumer)
+  validation(checkNumber)
   {
     const REGEXP=/^\d{3}$/;
-    if(!REGEXP.test(checknumer)) return false;
-    return checknumer;
+    if(!REGEXP.test(checkNumber)) return false;
+    return checkNumber;
   }
   async userNumberInput(){
-      const userNumber = await Console.readLineAsync('숫자를 입력해주세요 : ');
-      if(this.validation(userNumber)) return userNumber;
+      const userNumbers = await Console.readLineAsync('숫자를 입력해주세요 : ');
+      if(this.validation(userNumbers)) return userNumbers;
       else{
         throw new Error("[ERROR] 숫자가 잘못된 형식입니다");
       }
   }
+  compareResult(userNumbers)
+  {
+    let STRIKE=0; let BALL=0;
+    this.#COMPUTERNUMBERS.forEach( (comNumber,comIndex)=>{
+      for(let i=0; i<userNumbers.length;i++){
+        if(comNumber === parseInt(userNumbers[i]))
+        {
+          if(comIndex === i) STRIKE++;
+          else BALL++;
+        }
+      }
+    })
+    return {STRIKE,BALL};
+  }
+  comparePrint(STRIKE,BALL)
+  {
+    (STRIKE && BALL) && Console.print(`${BALL}볼 ${STRIKE}스트라이크`);
+    (!STRIKE && BALL) && Console.print(`${BALL}볼`);
+    (STRIKE && !BALL) && Console.print(`${STRIKE}스트라이크`);
+    (!STRIKE && !BALL) && Console.print(`낫싱`);
+  }
+  compareNumber(userNumbers)
+  {
+     let { STRIKE,BALL} = this.compareResult(userNumbers);
+     this.comparePrint(STRIKE,BALL);
+  }
 
   async play() {
-    Console.print("숫자 야구 게임을 시작합니다.");
-    this.userNumberInput();
+
+      Console.print("숫자 야구 게임을 시작합니다.");
+      const userNumbers = await this.userNumberInput();
+      this.compareNumber(userNumbers);
   }
 }
 
 const app = new App();
-app.play()
-
+app.initializeComputerNumer();
+app.play();
 export default App;
