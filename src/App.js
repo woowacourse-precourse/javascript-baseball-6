3c468f73fa84eac3b92fbc0f9a8e50616c566580
BellYun <95991290+BellYun@users.noreply.github.com>
2023-10-25 23:36:16 +0900

    feat: 로직 변경 및 README 작성

--- src/App.js
+++ src/App.js
@@ -1,58 +1,108 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import Computer from "./Computer";
 
 class App {
   async play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다")
-    while(1){
-      let computerNum =await com.createNumber();
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다");
+    let computerNumber = await this.createNumber();
+    
+    
+    while (true) {
+      let input = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요: ");
+      let userNumbers = this.setNumber(input);
+      const result = await this.checkNumber(userNumbers, computerNumber);
+      MissionUtils.Console.print(result);
+    
+      if (result === '3스트라이크') {
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        const replay = await MissionUtils.Console.readLineAsync(
+          "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: "
+        );
 
-
-      //숫자 맞추기 시작
-      while(1){
-        let inputNumber = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 :");
-        let setNumber = await this.setNumber(inputNumber)
-        let menti = com.checkNumber(setNumber, computerNum)
-      
-        MissionUtils.Console.print(menti)
-        if(menti === '3스트라이크'){
-          break
+        const restart = replay?.split("").map(Number);
+        if(restart.length !==1){
+          throw new Error("[ERROR]");
+        } 
+        else if (replay === "1") {
+          computerNumber = await this.createNumber();
+        } else if (replay === "2") {
+          MissionUtils.Console.print("게임 종료");
+          return
         }
       }
-
-
-      await MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-      let replay = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-      let restart = replay?.split("").map(Number);
-      if(restart.length !== 1){
-        throw new Error("[ERROR]");
-      }
-      else if(restart === 1){
-        continue
-      }else if(restart === 2){
-        break
-      }
-    }
-  }
-  
-  setNumber(inputNumber){
-    let Num = inputNumber?.split("").map(Number);
-    if(Num.length !==3){
-        throw new Error("[ERROR]");
-    }else if(
-        Num[0] === Num[1] ||
-        Num[1] === Num[2] ||
-        Num[2] === Num[0]){
-        throw new Error("[ERROR]");
-    }else{
-        return Num  
     }
 }
 
+async getUserNumbers() {
+  while (true) {
+    
+    const userNumbers = input?.split("").map(Number);
+    if (userNumbers.length !== 3 || 
+        userNumbers[0] === userNumbers[1] ||
+        userNumbers[1] === userNumbers[2] ||
+        userNumbers[2] === userNumbers[0]) {
+      throw new Error("[ERROR]");
+    } else {
+      return userNumbers;
+    }
+  }
+}
+
+setNumber(inputNumber){
+  let Num = inputNumber?.split("").map(Number);
+  if(Num.length !==3){
+      throw new Error("[ERROR]");
+  }else if(
+      Num[0] === Num[1] ||
+      Num[1] === Num[2] ||
+      Num[2] === Num[0]){
+      throw new Error("[ERROR]");
+  }else{
+      return Num  
+  }
+}
+
+createNumber() {
+  let computerNumber = []
+  while(computerNumber.length<3){
+      let number = MissionUtils.Random.pickNumberInRange(1,9)
+      if(!computerNumber.includes(number)){
+          computerNumber.push(number)
+      }
+  }
+  return computerNumber
+}
+
+checkNumber(setNumber,computerNum){
+  let message = ''
+  let ball = 0
+  let strike = 0
+  for(let i = 0; i<3; i++){
+      if (setNumber.includes(computerNum[i])) {
+          if (setNumber[i] === computerNum[i]) {
+            strike++;
+          } else {
+            ball++;
+          }
+        }
+  }
+  
+  if(ball !== 0){
+      message += `${ball}볼`
+  }
+  if(strike !== 0){
+      if(message !== ''){
+          message += ' '
+      }
+      message += `${strike}스트라이크`
+  }
+  if(ball === 0 && strike === 0){
+      message = '낫싱'
+  }
+  return message
+}
 }
 
 const app = new App;
-const com = new Computer;
 app.play;
 
 export default App;
