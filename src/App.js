0d3c53395c8f86a02bdedfdee80d430c1d842f2f
jayming66 <mjw8941@gmail.com>
2023-10-25 16:57:05 +0900

    feat : 유저의 입력을 받고 3개의 input인지 확인

--- src/App.js
+++ src/App.js
@@ -17,7 +17,12 @@ class App {
   async play() {
     Console.print("숫자 야구 게임을 시작합니다."); // 게임 시작 문구
     let randaomNumbers = randomNumber(); // 컴퓨터의 숫자 3개 생성
-    Console.print(randaomNumbers);
+    //Console.print(randaomNumbers);
+
+    let userInputs = await Console.readLineAsync("test"); // 쿼리를 보내야하고, await 해줘야한다.
+    userInputs = userInputs.split("").map((element) => parseInt(element, 10)); // input을 int형으로 바꾼다.
+    if (userInputs.length !== 3) throw new Error("3 Input require"); // 3개의 input인지 검증
+    Console.print(userInputs);
   }
 }
 
