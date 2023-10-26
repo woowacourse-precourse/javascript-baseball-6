87f0bb7e28b036560090a7311ac88fc6fff90ddd
thewronghand <penfreak77@gmail.com>
2023-10-27 04:55:46 +0900

    style: 맥락 이해를 돕기 위한 공백 추가

--- src/utils/viewUtils.js
+++ src/utils/viewUtils.js
@@ -4,11 +4,13 @@ export function getResultString([ball, strike]) {
   if (!ball && !strike) {
     return GAME_RESULTS.noMatch;
   }
+
   const text = [GAME_RESULTS.ball, GAME_RESULTS.strike];
   const parsedResults = [ball, strike].map((item, idx) => {
     if (!item) return;
     return item.toString() + text[idx];
   });
+
   const resultString = parsedResults.join(' ');
   if (strike === GAME_CONSTANTS.strikeOutCount) {
     return `${resultString}\n${GAME_MESSAGES.finish}`.trim();
