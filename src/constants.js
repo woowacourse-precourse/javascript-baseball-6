f35e5d493404df50fb4d06556a1576c25179dc9b
youngeun <jessicak0828@gmail.com>
2023-10-26 22:21:29 +0900

    refactor: constants.js 에 export 키워드 추가

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,17 @@
-const COMMENT_START = '숫자 야구 게임을 시작합니다.';
-const COMMENT_END = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.';
-const COMMENT_ASK_INPUT = '숫자를 입력해주세요 : ';
+export const COMMENT_START = '숫자 야구 게임을 시작합니다.';
+export const COMMENT_END =
+	'게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.';
+export const COMMENT_ASK_INPUT = '숫자를 입력해주세요 : ';
 
-const RESULT = {
+export const RESULT = {
 	BALL: '볼',
 	STRIKE: '스트라이크',
 	NOTHING: '낫싱',
 	ALL_STRIKE: '3스트라이크 \n 3개의 숫자를 모두 맞히셨습니다! 게임 종료 '
 };
 
-const ERROR_NAME = '[ERROR]';
-const ERROR_MESSAGE = {
+export const ERROR_NAME = '[ERROR]';
+export const ERROR_MESSAGE = {
 	INVALID_INPUT: '유효한 입력값이 아닙니다.',
 	ONLY_THREE: '3글자만 입력해주세요.',
 	ONLY_ONE_TO_NINE: '1부터 9까지의 자연수만 입력해주세요.',
