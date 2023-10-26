0f4a3761aca2ef569fd0ccaba249b1cb972d6529
youngeun <jessicak0828@gmail.com>
2023-10-26 23:28:33 +0900

    chore: 안 쓰는 코드 정리

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 import { Random, Console } from '@woowacourse/mission-utils';
-// import {
-// 	COMMENT_START,
-// 	COMMENT_ASK_INPUT,
-// 	COMMENT_END,
-// 	RESULT,
-// 	DECISION,
-// 	ERROR_NAME,
-// 	ERROR_MESSAGE
-// } from './constants';
-
-export const COMMENT_START = '숫자 야구 게임을 시작합니다.';
-export const COMMENT_END =
-	'게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.';
-export const COMMENT_ASK_INPUT = '숫자를 입력해주세요 : ';
-
-export const RESULT = {
-	BALL: '볼',
-	STRIKE: '스트라이크',
-	NOTHING: '낫싱',
-	ALL_STRIKE: '3스트라이크 \n 3개의 숫자를 모두 맞히셨습니다! 게임 종료 '
-};
-
-export const DECISION = {
-	RESTART: '1',
-	END: '2'
-};
-
-export const ERROR_NAME = '[ERROR]';
-export const ERROR_MESSAGE = {
-	INVALID_INPUT: '유효한 입력값이 아닙니다.',
-	ONLY_THREE: '3글자만 입력해주세요.',
-	ONLY_ONE_TO_NINE: '1부터 9까지의 자연수만 입력해주세요.',
-	ONLY_ONE_OR_TWO: '1 또는 2 중에서 입력해주세요.'
-};
+import {
+	COMMENT_START,
+	COMMENT_ASK_INPUT,
+	COMMENT_END,
+	RESULT,
+	DECISION,
+	ERROR_NAME,
+	ERROR_MESSAGE
+} from './constants';
 
 const getComputerInput = () => {
 	try {
