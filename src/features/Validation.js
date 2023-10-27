9c07d15c34300f9c90dc0fcbe7e1a0aa297c2c06
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-10-27 14:21:43 +0900

    REFACTOR:유효성 검증 조건 상수 분리

--- src/features/Validation.js
+++ src/features/Validation.js
@@ -5,25 +5,31 @@ const ERROR_MESSAGE = {
   UNDEFINED: "입력값을 확인할 수 없습니다. 종료하겠습니다.",
 };
 
+const VALIDATION_CONDITIONS = {
+  NOT_NUMBER: (input) => Number.isNaN(parseInt(input)),
+  OVER_OR_UNDER_LIMIT: (input) => input.length !== 3,
+  DUPLICATED: (inputArray) =>
+    inputArray.findIndex(
+      (item) => inputArray.indexOf(item) !== inputArray.lastIndexOf(item)
+    ) !== -1,
+  UNDEFINED: (input) => input !== "1" && input !== "2",
+};
+
 export const validation = {
   validatePlayNumber(input) {
     const inputArray = input.split("");
     const { NOT_NUMBER, OVER_OR_UNDER_LIMIT, DUPLICATED } = ERROR_MESSAGE;
-    if (Number.isNaN(parseInt(input))) {
+    if (VALIDATION_CONDITIONS.NOT_NUMBER(input)) {
       throw new Error(
         `[ERROR] ${NOT_NUMBER} 서로 다른 3 자리 의 숫자를 입력해주세요.`
       );
     }
-    if (input.length !== 3) {
+    if (VALIDATION_CONDITIONS.OVER_OR_UNDER_LIMIT(input)) {
       throw new Error(
         `[ERROR] ${OVER_OR_UNDER_LIMIT} 서로 다른 3 자리 의 숫자를 입력해주세요.`
       );
     }
-    if (
-      inputArray.findIndex(
-        (item) => inputArray.indexOf(item) !== inputArray.lastIndexOf(item)
-      ) !== -1
-    ) {
+    if (VALIDATION_CONDITIONS.DUPLICATED(inputArray)) {
       throw new Error(
         `[ERROR] ${DUPLICATED} 서로 다른 3 자리 의 숫자를 입력해주세요.`
       );
@@ -31,7 +37,7 @@ export const validation = {
   },
 
   validateSelectReplayOrExit(input) {
-    if (input !== "1" && input !== "2") {
+    if (VALIDATION_CONDITIONS.UNDEFINED(input)) {
       throw new Error(ERROR_MESSAGE.UNDEFINED);
     }
   },
