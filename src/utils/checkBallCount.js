1cc9da106002baf72c93059783670efd0c8650bb
yeokyoung song <spolice0324@gmail.com>
2023-11-01 17:33:56 +0900

    refactor : 볼 카운트 체크 함수 코드 로직 개선
    - 고차함수 reduce를 사용하여 비교 결과에 따라 값을 누적하는 방식으로 변경

--- src/utils/checkBallCount.js
+++ src/utils/checkBallCount.js
@@ -1,18 +1,18 @@
 const checkBallCount = (inputNumber, randomNumber) => {
-  const inputNumberArr = inputNumber.split("");
-  const randomNumberArr = randomNumber.split("");
+  const scoreCount = inputNumber.split('').reduce(
+    (count, digit, index) => {
+      const randomDigit = randomNumber[index];
 
-  let scoreCount = { ball: 0, strike: 0 };
+      if (digit === randomDigit) {
+        count.strike += 1;
+      } else if (randomNumber.includes(digit)) {
+        count.ball += 1;
+      }
 
-  for (let i = 0; i < inputNumberArr.length; i++) {
-    if (inputNumberArr[i] === randomNumberArr[i]) {
-      scoreCount.strike++;
-      continue;
-    }
-    if (randomNumberArr.includes(inputNumberArr[i])) {
-      scoreCount.ball++;
-    }
-  }
+      return count;
+    },
+    { ball: 0, strike: 0 },
+  );
 
   return scoreCount;
 };
