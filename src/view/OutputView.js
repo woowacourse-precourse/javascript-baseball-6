822712c33cbcf8e6da3168db64cde970f0fc5996
leedbswo107 <ljyu1@naver.com>
2023-11-20 22:07:55 +0900

    refactor: 출력 분리

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -5,5 +5,25 @@ class OutputView{
     intro() {
         Console.print(`${GAME.start}`);
     }
+    exit() {
+        Console.print(`${GAME.gameEnd}`);
+    }
+    result(score) {
+        if(score[0]==0 && score[1]==0){
+            Console.print("낫싱");
+        }else if(score[0]==3){
+            Console.print(`${score[0]}스트라이크`);
+        return 0;
+        }else if(score[0]==0 && score[1]>0){
+            Console.print(`${score[1]}볼`);
+        }else if(score[0]>0 && score[1]==0){
+            Console.print(`${score[0]}스트라이크`);
+        }else{
+            Console.print(`${score[1]}볼 ${score[0]}스트라이크`);
+        }
+    }
+    endGame() {
+        Console.print(`${GAME.gameEnd}`);
+    }
 }
 export default OutputView;
