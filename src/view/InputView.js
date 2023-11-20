6d1155104935e6aa5d9fed394acad6cd294c6f56
leedbswo107 <ljyu1@naver.com>
2023-11-20 22:18:26 +0900

    add: validator 기능 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -17,15 +17,15 @@ class InputView {
         while (true) {
             try {
                 this.#numbers = await Console.readLineAsync(GAME.inputNumber);
-                this.#validator.validateInputLength(this.#numbers);
-                this.#splitNumbers = new SplitNumbers(this.#numbers);
-                this.#finalNumbers = this.#splitNumbers.getSplitNum();
+                await this.#validator.validateInputLength(this.#numbers);
+                this.#splitNumbers = await new SplitNumbers(this.#numbers);
+                this.#finalNumbers = await this.#splitNumbers.getSplitNum();
                 this.#validator.validateNaN(this.#finalNumbers);
                 this.#validator.validateNumberRange(this.#finalNumbers);
                 this.#validator.validateDuplicate(this.#finalNumbers);
                 return this.#finalNumbers;    
             } catch (error) {
-                Console.print(error.message);
+                await Console.print(error);
             }
         }
     }
