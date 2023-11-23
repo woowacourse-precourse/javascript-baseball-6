0ba9d4c75a291d14924f2ae4ec557af612e5c52b
leedbswo107 <ljyu1@naver.com>
2023-11-23 19:24:20 +0900

    refactor: 불필요 라인 삭제

--- src/domain/CheckNumber.js
+++ src/domain/CheckNumber.js
@@ -2,11 +2,8 @@ class CheckNumber{
     checkNumber(inputNum,randomNum) {
         let score = [0,0];
         for(let i=0;i<3;i++){
-            if(randomNum[i]===inputNum[i]){
-            score[0]+=1;
-            }else if(randomNum.includes(inputNum[i])){
-            score[1]+=1;
-            }
+            if(inputNum[i]===randomNum[i]) score[0]+=1;
+            else if(randomNum.includes(inputNum[i])) score[1]+=1;
         }
         return score;
     }
