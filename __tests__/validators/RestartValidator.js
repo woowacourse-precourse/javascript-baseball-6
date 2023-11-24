4a588527fdeeaad3cd2e0036e17ed7770bf06831
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-24 22:32:53 +0900

    Test: 게임 재시작 여부 입력이 1, 2가 아닌 다른 값을 입력한 경우 테스트 추가

---
+++ __tests__/validators/RestartValidator.js
@@ -0,0 +1,16 @@
+import ERROR from '../../src/constants/error.js';
+import RestartValidator from '../../src/validators/RestartValidator.js';
+
+describe('게임 재시작 여부 입력 예외 상황 테스트', () => {
+  test('1, 2가 아닌 다른 값을 입력한 경우 예외처리를 한다.', () => {
+    const value = '0';
+    const result = () => RestartValidator.validateRestartChoice(value);
+    expect(result).toThrow(ERROR.restart.choice);
+  });
+
+  test('1, 2의 값을 입력한 경우 true를 반환한다.', () => {
+    const value = '1';
+    const result = RestartValidator.validateRestartChoice(value);
+    expect(result).toEqual(true);
+  });
+});
