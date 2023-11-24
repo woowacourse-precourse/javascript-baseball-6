e313d929ceb1b4c266c6f9365eea2f1ea0741b46
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-24 21:44:26 +0900

    Test: 숫자가 중복일 경우 테스트 추가

--- __tests__/validators/NumbersValidators.js
+++ __tests__/validators/NumbersValidators.js
@@ -37,4 +37,16 @@ describe('숫자 입력 예외 상황 테스트', () => {
     const result = NumbersValidator.validateNaN(value);
     expect(result).toEqual(true);
   });
+
+  test('숫자가 중복된 경우 예외처리를 한다', () => {
+    const value = '122';
+    const result = () => NumbersValidator.validateDuplicated(value);
+    expect(result).toThrow(ERROR.numbers.duplicated);
+  });
+
+  test('숫자가 중복되지 않은 경우 true를 반환한다.', () => {
+    const value = '123';
+    const result = NumbersValidator.validateNaN(value);
+    expect(result).toEqual(true);
+  });
 });
