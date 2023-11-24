1af9a766214074359c7d9ee4f31f3370a8475a53
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-24 21:29:39 +0900

    Test: 숫자가 음수일 경우 테스트 추가

--- __tests__/validators/NumbersValidators.js
+++ __tests__/validators/NumbersValidators.js
@@ -8,7 +8,7 @@ describe('숫자 입력 예외 상황 테스트', () => {
     expect(result).toThrow(ERROR.numbers.length);
   });
 
-  test('숫자가 3자리인 경우 undefined를 반환한다.', () => {
+  test('숫자가 3자리인 경우 true를 반환한다.', () => {
     const value = '123';
     const result = NumbersValidator.validateLength(value);
     expect(result).toEqual(true);
@@ -21,6 +21,18 @@ describe('숫자 입력 예외 상황 테스트', () => {
   });
 
   test('숫자인 경우 true를 반환한다.', () => {
+    const value = '123';
+    const result = NumbersValidator.validateNaN(value);
+    expect(result).toEqual(true);
+  });
+
+  test('숫자가 음수인 경우 예외처리를 한다.', () => {
+    const value = '-123';
+    const result = () => NumbersValidator.validateNegative(value);
+    expect(result).toThrow(ERROR.numbers.negative);
+  });
+
+  test('숫자가 양수인 경우 true를 반환한다.', () => {
     const value = '123';
     const result = NumbersValidator.validateNaN(value);
     expect(result).toEqual(true);
