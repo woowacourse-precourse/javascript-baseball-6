e4cd36667da23c6c4d2b25a01a0297eb8520251e
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-24 22:38:04 +0900

    Feat: 게임 재시작 여부 입력이 음수일 경우 유효성 검사 추가

--- src/validators/RestartValidator.js
+++ src/validators/RestartValidator.js
@@ -3,13 +3,18 @@ import ERROR from '../constants/error.js';
 
 const RestartValidator = {
   validateRestart(restart) {
-    const validators = [this.validateRestartChoice];
+    const validators = [this.validateNegative, this.validateRestartChoice];
     validators.forEach(validator => validator(restart));
   },
 
   validateRestartChoice(restart) {
     if (Number(restart) < CONSTANTS.restart.start || Number(restart) > CONSTANTS.restart.exit)
       throw new Error(ERROR.restart.choice);
+    return true;
+  },
+
+  validateNegative(restart) {
+    if (Number(restart) < CONSTANTS.number.zero) throw new Error(ERROR.numbers.negative);
     return true;
   },
 };
