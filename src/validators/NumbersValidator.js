8fc32ccd38eb40baa8868d67f2ed5836db00b89e
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-24 21:22:07 +0900

    Feat: 숫자가 아닌 경우 유효성 검사 추가

--- src/validators/NumbersValidator.js
+++ src/validators/NumbersValidator.js
@@ -3,12 +3,17 @@ import ERROR from '../constants/error.js';
 
 class NumbersValidator {
   static validateNumbers(numbers) {
-    const validators = [this.validateLength];
+    const validators = [this.validateNaN, this.validateLength];
     validators.forEach(validator => validator(numbers));
   }
 
   static validateLength(numbers) {
     if (numbers.length !== CONSTANTS.number.maxLength) throw new Error(ERROR.numbers.length);
+    return true;
+  }
+
+  static validateNaN(numbers) {
+    if (isNaN(numbers)) throw new Error(ERROR.numbers.notANumber);
     return true;
   }
 }
