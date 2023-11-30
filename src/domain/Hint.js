f88435cae771cff5414c8b9042b5d5d1d3982f5c
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-30 18:23:58 +0900

    Feat: 볼, 스트라이크 결과 메시지를 반환하는 기능 추가

--- src/domain/Hint.js
+++ src/domain/Hint.js
@@ -1,3 +1,6 @@
+import CONSTANTS from '../constants/constants.js';
+import MESSAGE from '../constants/message.js';
+
 class Hint {
   #numbers;
 
@@ -20,6 +23,17 @@ class Hint {
         return this.#computerNumbers.includes(digit) ? count + 1 : count;
       }, 0) - strikeCount
     );
+  }
+
+  generateHintMessage(strikeCount, ballCount) {
+    const hintMessage = [];
+    if (ballCount !== CONSTANTS.number.zero) hintMessage.push(`${ballCount}${MESSAGE.print.ball}`);
+    if (strikeCount !== CONSTANTS.number.zero)
+      hintMessage.push(`${strikeCount}${MESSAGE.print.strike}`);
+    if (ballCount === CONSTANTS.number.zero && strikeCount === CONSTANTS.number.zero)
+      hintMessage.push(MESSAGE.print.nothing);
+
+    return hintMessage;
   }
 }
 
