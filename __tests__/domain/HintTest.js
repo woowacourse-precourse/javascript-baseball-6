0d137ec3c6fef1c49eaae6ebd3b302b209417704
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-30 18:23:07 +0900

    Test: 볼, 스트라이크 결과 메시지를 반환하는 기능 테스트 추가

--- __tests__/domain/HintTest.js
+++ __tests__/domain/HintTest.js
@@ -40,4 +40,27 @@ describe('Hint 클래스 테스트', () => {
       },
     );
   });
+
+  describe('generateHintMessage 메서드는 strikeCount와 ballCount를 입력받아 결과 메시지의 배열을 반환한다.', () => {
+    const cases = [
+      { numbers: [1, 2, 3], computerNumbers: [2, 3, 1], expected: ['3볼'] },
+      { numbers: [1, 2, 3], computerNumbers: [1, 2, 3], expected: ['3스트라이크'] },
+      { numbers: [4, 2, 1], computerNumbers: [4, 1, 2], expected: ['2볼', '1스트라이크'] },
+    ];
+
+    test.each(cases)(
+      '스트라이크 개수 $strikeCount와 볼의 개수 $ballCount가 주어지는 경우, generateHintMessage()는 결과 메시지를 배열 형태의 $expected로 반환한다.',
+      ({ numbers, computerNumbers, expected }) => {
+        // when
+        const hint = new Hint(numbers, computerNumbers);
+        const strikeCount = hint.calculateStrikeCount();
+        const ballCount = hint.calculateBallCount(strikeCount);
+        const hintMessage = hint.generateHintMessage(strikeCount, ballCount);
+
+        console.log(hintMessage);
+        // then
+        expect(hintMessage).toEqual(expected);
+      },
+    );
+  });
 });
